e02f0dae96aedf19a7641113cfe6896935ef7274
Mani Parkhe <mani@databricks.com>
2018-06-07 13:20:40 -0700

    fix for '#18: When comparing results, parameters are incorrectly displayed'

--- example/quickstart/test.py
+++ example/quickstart/test.py
@@ -1,12 +1,12 @@
 import os
-from random import random
+from random import random, randint
 
 from mlflow import log_metric, log_param, log_artifacts
 
 if __name__ == "__main__":
     print("Running test.py")
 
-    log_param("param1", 5)
+    log_param("param1", randint(0, 100))
     
     log_metric("foo", random())
     log_metric("foo", random() + 1)
