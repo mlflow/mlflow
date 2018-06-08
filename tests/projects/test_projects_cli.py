cefce8a5d455c557614570712b1651080c8fb634
Sid Murching <sid.murching@databricks.com>
2018-06-08 01:46:14 -0700

    lint

--- tests/projects/test_projects_cli.py
+++ tests/projects/test_projects_cli.py
@@ -9,20 +9,19 @@ def _assert_succeeded(run_output):
     assert "=== Run succeeded ===" in run_output
 
 
-class TestProjectsCli(object):
+def test_run_local():
+    with TempDir() as tmp:
+        with update_temp_env({mlflow.tracking._TRACKING_URI_ENV_VAR: tmp.path()}):
+            excitement_arg = 2
+            res = invoke_cli_runner(cli.run, [TEST_PROJECT_DIR, "-e", "greeter", "-P",
+                                              "greeting=hi", "-P", "name=friend",
+                                              "-P", "excitement=%s" % excitement_arg])
+            _assert_succeeded(res.output)
 
-    def test_run_local(self):
-        with TempDir() as tmp:
-            with update_temp_env({mlflow.tracking._TRACKING_URI_ENV_VAR: tmp.path()}):
-                excitement_arg = 2
-                res = invoke_cli_runner(cli.run, [TEST_PROJECT_DIR, "-e", "greeter", "-P",
-                                                  "greeting=hi", "-P", "name=friend",
-                                                  "-P", "excitement=%s" % excitement_arg])
-                _assert_succeeded(res.output)
 
-    def test_run_git(self):
-        with TempDir() as tmp:
-            with update_temp_env({mlflow.tracking._TRACKING_URI_ENV_VAR: tmp.path()}):
-                res = invoke_cli_runner(cli.run, [GIT_PROJECT_URI, "-P", "alpha=0.5"])
-                assert "python train.py 0.5 0.1" in res.output
-                _assert_succeeded(res.output)
+def test_run_git():
+    with TempDir() as tmp:
+        with update_temp_env({mlflow.tracking._TRACKING_URI_ENV_VAR: tmp.path()}):
+            res = invoke_cli_runner(cli.run, [GIT_PROJECT_URI, "-P", "alpha=0.5"])
+            assert "python train.py 0.5 0.1" in res.output
+            _assert_succeeded(res.output)
