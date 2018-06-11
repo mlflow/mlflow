d356144ce7d1815f49bde9b7053a494cc17b9a2d
tomasatdatabricks <tomas.nykodym@databricks.com>
2018-06-11 12:55:29 -0700

    Fixed typos.

--- mlflow/sagemaker/__init__.py
+++ mlflow/sagemaker/__init__.py
@@ -22,8 +22,8 @@ def build_image(name=DEFAULT_IMAGE_NAME):
     """
     Build new mlflow sagemaker image and assign it given name.
 
-    This function builds a docker image defined in mlflow/sgameker/container/Dockerfile. It requires
-    docker to run. It is intended to build and upload rivate copy of a mlflow sagemaker container.
+    This function builds a docker image defined in mlflow/sagemaker/container/Dockerfile.
+    The image is buitl locally and so it requires docker to run.
 
     :param name: image name
     :return:
