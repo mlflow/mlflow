f0ea13dec833a0860b3fc0567ed0eaed65737858
Juntai Zheng <juntai.zheng@databricks.com>
2018-06-13 17:04:05 -0700

    code cleanup

--- mlflow/tensorflow.py
+++ mlflow/tensorflow.py
@@ -3,9 +3,7 @@
 from __future__ import absolute_import
 
 import os
-import dill as pickle
 
-import numpy as np
 import pandas
 import tensorflow as tf
 
@@ -15,37 +13,36 @@ from mlflow.models import Model
 import mlflow.tracking
 
 
-class TFWrapper(object):
-    """Wrapper class that creates a predict function such that 
-    predict(data: pandas.DataFrame) -> pandas.DataFrame"""
-    def __init__(self, saved_model_dir, signature_def_name):
+class _TFWrapper(object):
+    """
+    Wrapper class that creates a predict function such that 
+    predict(data: pandas.DataFrame) -> pandas.DataFrame
+    """
+    def __init__(self, saved_model_dir, signature_def_name=None):
         self._saved_model_dir = saved_model_dir
         self._signature_def_name = signature_def_name
 
     def predict(self, df):
-        self._graph = tf.Graph()
-        with tf.Session(graph=self._graph) as sess:
+        graph = tf.Graph()
+        with tf.Session(graph=graph) as sess:
             meta_graph_def = tf.saved_model.loader.load(sess, 
                                                         [tf.saved_model.tag_constants.SERVING], 
                                                         self._saved_model_dir)
             if not self._signature_def_name:
                 self._signature_def_name = tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY
-            sig_def = tf.contrib.saved_model.get_signature_def_by_key(meta_graph_def, self._signature_def_name)
-            for key in meta_graph_def.signature_def:
-                print("STORED SIG DEF KEY:", key)
-                print("STORED SIG DEF KEY contents:", meta_graph_def.signature_def[key])
-            print("CHOSEN SIG DEF KEY:", self._signature_def_name)
-            self._feed_mapping = {}
+            sig_def = tf.contrib.saved_model.get_signature_def_by_key(meta_graph_def, 
+                                                                      self._signature_def_name)
+
+            # Determining input tensors.
+            feed_mapping = {}
             feed_names = []
             for sigdef_key, tnsr_info in sig_def.inputs.items():
                 print("SIF DEF INPUT ITEM:", sigdef_key, tnsr_info)
                 tnsr_name = tnsr_info.name
-                self._feed_mapping[sigdef_key] = tnsr_name
+                feed_mapping[sigdef_key] = tnsr_name
                 feed_names.append(tnsr_name)
 
-            print("FEED_MAPPING:", self._feed_mapping)    
-            print("FEED_NAMES:", feed_names)
-
+            # Determining output tensors.
             fetch_mapping = {}
             fetch_names = []
             for sigdef_key, tnsr_info in sig_def.outputs.items():
@@ -53,22 +50,13 @@ class TFWrapper(object):
                 fetch_mapping[sigdef_key] = tnsr_name
                 fetch_names.append(tnsr_name)
 
-            print("FETCH_MAPPING:", fetch_mapping)
-            print("FETCH_NAMES:", fetch_names)
-
-            self._fetches = [self._graph.get_tensor_by_name(tnsr_name) for tnsr_name in fetch_names]
+            fetches = [graph.get_tensor_by_name(t_name) for t_name in fetch_names]
 
             feed_dict = {}
-            for col in self._feed_mapping:
-                # placeholder = tf.placeholder("float", shape=None, name=col)
-                feed_dict[self._graph.get_tensor_by_name(self._feed_mapping[col])] = df[col].values#np.expand_dims(df[col].values,
-                                                                                     #               axis=-1)
-            print("fetches:", self._fetches)
-            print("feed_dict:", feed_dict)
-            # init = tf.global_variables_initializer()
-            # sess.run(init)
-            data = sess.run(self._fetches, feed_dict=feed_dict)
-            print("DATA:", data)
+            for col in feed_mapping:
+                tnsr_name = feed_mapping[col]
+                feed_dict[graph.get_tensor_by_name(tnsr_name)] = df[col].values
+            data = sess.run(fetches, feed_dict=feed_dict)
             return pandas.DataFrame(data=data[0])
 
 
@@ -84,5 +72,5 @@ def log_saved_model(saved_model_dir, artifact_path):
 
 
 def load_pyfunc(saved_model_dir, signature_def_name=None):
-    return TFWrapper(saved_model_dir, signature_def_name)
+    return _TFWrapper(saved_model_dir, signature_def_name)
 
--- tests/mlflow/tensorflow/test_tensorflow_model_export.py
+++ tests/mlflow/tensorflow/test_tensorflow_model_export.py
@@ -14,24 +14,9 @@ from mlflow import tensorflow, pyfunc
 from mlflow import tracking
 from mlflow.utils.file_utils import TempDir
 
-def load_pyfunc(path):
-    model_fn = None
-    model_dir = None
-    for filename in os.listdir(path):
-        if filename == "model_fn.pkl":
-            with open(os.path.join(path, filename), "rb") as f:
-                model_fn = pickle.load(f)
-                print("found function")
-        elif filename == "model_dir.pkl":
-            with open(os.path.join(path, filename), "rb") as f:
-                model_dir = pickle.load(f)
-                print("found dir")
-    return tf.estimator.Estimator(model_fn, model_dir=model_dir)
-
 
 class TestModelExport(unittest.TestCase):
     def setUp(self):
-        self._tmp = tempfile.mkdtemp()
         iris = datasets.load_iris()
         self._X = iris.data[:, :2]  # we only take the first two features.
         self._y = iris.target
@@ -45,15 +30,19 @@ class TestModelExport(unittest.TestCase):
             self._trainingFeatures[iris.feature_names[i]] = iris.data[:, i:i+1]
         self._tf_feat_cols = []
         self._feature_names = iris.feature_names[:2]
+        # Creating Tensorflow-specific numeric columns for input.
         for col in iris.feature_names[:2]:
             self._tf_feat_cols.append(tf.feature_column.numeric_column(col))
+        # Creating input training function.
         self._input_train = tf.estimator.inputs.numpy_input_fn(self._trainingFeatures, 
                                                                     self._y, 
                                                                     shuffle=False, 
                                                                     batch_size=1)
+        # Creating Deep Neural Network Regressor. 
         self._dnn = tf.estimator.DNNRegressor(feature_columns=self._tf_feat_cols, 
                                                 hidden_units=[1])
         self._sess = tf.Session()
+        # Training and creating expected predictions on training dataset.
         self._dnn.train(self._input_train, steps=100)
         self._dnn_predict = self._dnn.predict(self._input_train)
 
@@ -61,31 +50,24 @@ class TestModelExport(unittest.TestCase):
         with TempDir(chdr=False, remove_on_exit=True) as tmp:
             tracking.start_run()
             try:
-                # feature_spec = tf.feature_column.make_parse_example_spec(self._tf_feat_cols)
-                # print("FEATURE_SPEC:", feature_spec)
+                # Creating dict of features names (str) to placeholders (tensors)
                 feature_spec = {}
                 for name in self._feature_names:
                     feature_spec[name] = tf.placeholder("float", name=name, shape=[150])
+                # Creating receiver function for model saving.
                 receiver_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(feature_spec)
                 saved_model_path = tmp.path("model")
                 os.makedirs(saved_model_path)
                 os.makedirs(tmp.path("hello"))
-                saved_model_path = self._dnn.export_savedmodel(saved_model_path, receiver_fn).decode("utf-8")
-                # os.makedirs(tmp.path("hello"))
-                # saved_model_path = tmp.path("model")
-                # x = tf.placeholder("float", name="sepallengthcm")
-                # y = tf.placeholder("float", name="sepalwidthcm")
-                # z = tf.placeholder("float", name="z")
-                # #print("check model/ has files", os.listdir(saved_model_path))
-                # tf.saved_model.simple_save(self._sess, saved_model_path,
-                #                            inputs={"sepallengthcm":x, "sepalwidthcm":y},
-                #                            outputs={"z":z})
-                tensorflow.log_saved_model(saved_model_dir=saved_model_path, artifact_path=tmp.path("hello"))
+                # Saving Tensorflow model.
+                saved_model_path = self._dnn.export_savedmodel(saved_model_path, 
+                                                               receiver_fn).decode("utf-8")
+                # Logging the Tensorflow model just saved.
+                tensorflow.log_saved_model(saved_model_dir=saved_model_path, 
+                                           artifact_path=tmp.path("hello"))
+                # Loading the saved Tensorflow model as a pyfunc.
                 x = tensorflow.load_pyfunc(saved_model_path, "predict")
-                # print("data:", self._X)
-                # print("columns:", self._feature_names)
-                # print("X:", x)
-                print("DataFrame:", pandas.DataFrame(data=self._X, columns=self._feature_names))
+                # Predicting on the iris dataset using the pyfunc.
                 xpred = x.predict(pandas.DataFrame(data=self._X, columns=self._feature_names))
                 saved = []
                 for s in self._dnn_predict:
@@ -93,49 +75,7 @@ class TestModelExport(unittest.TestCase):
                 loaded = []
                 for index, rows in xpred.iterrows():
                     loaded.append(rows)
+                # Asserting that the loaded model predictions are as expected.
                 np.testing.assert_array_equal(saved, loaded)
             finally:
                 tracking.end_run()
-
-    # def test_model_save_load(self):
-    #     with TempDir(chdr=True, remove_on_exit=True) as tmp:
-    #         path = tmp.path("dnn")
-    #         tensorflow.save_model(tf_model=self._dnn, path=path)
-    #         x = tensorflow.load_model(path)
-    #         xpred = x.predict(pandas.DataFrame(data=self._X, columns=self._feature_names))
-    #         saved = []
-    #         for s in self._dnn_predict:
-    #             saved.append(s['predictions'])
-    #         loaded = []
-    #         for index, rows in xpred.iterrows():
-    #             loaded.append(rows)
-    #         np.testing.assert_array_equal(saved, loaded)
-    #         # sklearn should also be stored as a valid pyfunc model
-    #         # test pyfunc compatibility
-    #         y = pyfunc.load_pyfunc(path)
-    #         ypred = y.predict(pandas.DataFrame(data=self._X, columns=self._feature_names))
-    #         loaded = []
-    #         for index, rows in ypred.iterrows():
-    #             loaded.append(rows)
-    #         np.testing.assert_array_equal(saved, loaded)
-
-    # def test_model_log(self):
-    #     with TempDir(chdr=True, remove_on_exit=True):
-    #         tracking.start_run()
-    #         try:
-    #             tensorflow.log_model(tf_model=self._dnn, artifact_path="dnn")
-    #             x = tensorflow.load_model("dnn", run_id=tracking.active_run().info.run_uuid)
-    #             xpred = x.predict(pandas.DataFrame(data=self._X, columns=self._feature_names))
-    #             saved = []
-    #             for s in self._dnn_predict:
-    #                 saved.append(s['predictions'])
-    #             loaded = []
-    #             for index, rows in xpred.iterrows():
-    #                 loaded.append(rows)
-    #             np.testing.assert_array_equal(saved, loaded)
-    #         finally:
-    #             tracking.end_run()
-
-
-if __name__ == '__main__':
-    unittest.main()
