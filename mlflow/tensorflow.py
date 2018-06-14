c2ce22c2329fcc6d16d20b9a17f4abb36e6a6a57
Juntai Zheng <juntai.zheng@databricks.com>
2018-06-13 18:09:41 -0700

    fixed lint message

--- mlflow/tensorflow.py
+++ mlflow/tensorflow.py
@@ -28,10 +28,8 @@ class _TFWrapper(object):
                                                         [tf.saved_model.tag_constants.SERVING], 
                                                         self._saved_model_dir)
             if not self._signature_def_name:
-                """
-                TODO: add support for replacing "predict" with 
-                #tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY
-                """ 
+                # TODO: add support for replacing "predict" with 
+                # tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY
                 self._signature_def_name = "predict"
             sig_def = tf.contrib.saved_model.get_signature_def_by_key(meta_graph_def, 
                                                                       self._signature_def_name)
