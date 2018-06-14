309bc8936aabbf4e91a40160bc3ada961ff54278
Juntai Zheng <juntai.zheng@databricks.com>
2018-06-14 12:09:47 -0700

    adding part of predict back in

--- mlflow/tensorflow.py
+++ mlflow/tensorflow.py
@@ -2,6 +2,9 @@
 
 from __future__ import absolute_import
 
+import pandas
+import tensorflow as tf
+
 
 class _TFWrapper(object):
     """
@@ -11,3 +14,16 @@ class _TFWrapper(object):
     def __init__(self, saved_model_dir, signature_def_name=None):
         self._saved_model_dir = saved_model_dir
         self._signature_def_name = signature_def_name
+
+    def predict(self, df):
+        graph = tf.Graph()
+        with tf.Session(graph=graph) as sess:
+            meta_graph_def = tf.saved_model.loader.load(sess, 
+                                                        [tf.saved_model.tag_constants.SERVING], 
+                                                        self._saved_model_dir)
+            if not self._signature_def_name:
+                # TODO: add support for replacing "predict" with 
+                # tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY
+                self._signature_def_name = "predict"
+            sig_def = tf.contrib.saved_model.get_signature_def_by_key(meta_graph_def, 
+                                                                      self._signature_def_name)
