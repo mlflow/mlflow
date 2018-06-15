04bc4d84743add624ba0b66a85d8669f1a1e1ce9
Juntai Zheng <juntai.zheng@databricks.com>
2018-06-15 10:09:33 -0700

    pruning to minimal tf

--- mlflow/tensorflow.py
+++ mlflow/tensorflow.py
@@ -5,19 +5,6 @@ from __future__ import absolute_import
 import tensorflow as tf
 
 
-class _TFWrapper(object):
-    """
-    Wrapper class that creates a predict function such that 
-    predict(data: pandas.DataFrame) -> pandas.DataFrame
-    """
-    def __init__(self, saved_model_dir, signature_def_name=None):
-        self._saved_model_dir = saved_model_dir
-        self._signature_def_name = signature_def_name
-
-    def predict(self, df):
-        graph = tf.Graph()
-        with tf.Session(graph=graph) as sess:
-            meta_graph_def = tf.saved_model.loader.load(sess, 
-                                                        [tf.saved_model.tag_constants.SERVING], 
-                                                        self._saved_model_dir)
-            return meta_graph_def, df
+def predict(df):
+    graph = tf.Graph()
+    return graph, df
