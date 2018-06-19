57917513062d5eec2fd4bb2488dd57aea6a45e06
tomasatdatabricks <tomas.nykodym@databricks.com>
2018-06-19 16:00:00 -0700

    Minor fix in click option definitions

--- mlflow/utils/cli_args.py
+++ mlflow/utils/cli_args.py
@@ -5,10 +5,10 @@ import click
 
 MODEL_PATH = click.option("--model-path", "-m", default=None, metavar="PATH", required=True,
                           help="Path to the model. The path is relative to the run with the given "
-                               + "RUN-ID or local filesystem path without RUN_ID.")
+                               + "RUN_ID or local filesystem path without RUN_ID.")
 
-MLFLOW_HOME = click.option("--mlflow-home", default=None, metavar="MLFLOW-HOME",
+MLFLOW_HOME = click.option("--mlflow-home", default=None, metavar="PATH",
                            help="Path to local clone of MLflow project. Use for development only.")
 
-RUN_ID = click.option("--run-id", "-r", default=None, required=False, metavar="RUN-ID",
+RUN_ID = click.option("--run-id", "-r", default=None, required=False, metavar="ID",
                       help="ID of the MLflow run that generated the referenced content.")
