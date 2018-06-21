573c6012196d89b5c91b115d64523715ed676db4
tomasatdatabricks <33237569+tomasatdatabricks@users.noreply.github.com>
2018-06-21 14:45:02 -0700

    Model export update. (#36)
    1. MLflow is installed from pip inside the container, unless MLFLOW_DEV environment variable is set. This applies to both Azure ML and SageMaker containers.
    2. The SageMaker init script is translated to Python. 
    3. Refactored mlflow sagemaker code. 
       - Python api moved to sagemaker/__init__. This is to provide python api (cli 
          targets can not be easily called from python) usable e.g. from notebooks.
    4. Translated some of sh script to build and push image to ECR to Python.  
    5. Added test for mlflow sagemaker run-local.

--- mlflow/__init__.py
+++ mlflow/__init__.py
@@ -5,7 +5,7 @@ import os
 os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
 
 # pylint: disable=wrong-import-position
-import mlflow.projects as projects # noqa
+import mlflow.projects as projects  # noqa
 import mlflow.tracking as tracking  # noqa
 
 log_param = tracking.log_param
@@ -20,14 +20,7 @@ set_tracking_uri = tracking.set_tracking_uri
 get_tracking_uri = tracking.get_tracking_uri
 create_experiment = tracking.create_experiment
 
-
 run = projects.run
-
-
-# get project relative path
-def _relpath(*rel_path_elms):
-    return os.path.join(os.path.abspath(os.path.dirname(__file__)), *rel_path_elms)
-
 
 __all__ = ["log_param", "log_metric", "log_artifacts", "log_artifact", "active_run",
            "start_run", "end_run", "get_artifact_uri", "set_tracking_uri", "create_experiment"]
