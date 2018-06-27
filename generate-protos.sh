2d421d54bf682403b394c1ad77f3b4517655455d
Andrew Chen <andrewmnchen@gmail.com>
2018-06-27 11:06:35 -0700

    check test-generate-protos.sh

--- generate-protos.sh
+++ generate-protos.sh
@@ -14,8 +14,8 @@ protoc -I="$PROTOS" --python_out="$PROTOS" "$PROTOS"/databricks.proto \
 
 OLD_SCALAPB="from scalapb import scalapb_pb2 as scalapb_dot_scalapb__pb2"
 NEW_SCALAPB="from .scalapb import scalapb_pb2 as scalapb_dot_scalapb__pb2"
-sed -i '' -e s/"$OLD_SCALAPB"/"$NEW_SCALAPB"/g "$PROTOS"/databricks_pb2.py "$PROTOS"/service_pb2.py
+sed -i '' -e "s/$OLD_SCALAPB/$NEW_SCALAPB/g" "$PROTOS/databricks_pb2.py" "$PROTOS/service_pb2.py"
 
 OLD_DATABRICKS="import databricks_pb2 as databricks__pb2"
 NEW_DATABRICKS="from . import databricks_pb2 as databricks__pb2"
-sed -i '' -e s/"$OLD_DATABRICKS"/"$NEW_DATABRICKS"/g "$PROTOS"/service_pb2.py
+sed -i '' -e "s/$OLD_DATABRICKS/$NEW_DATABRICKS/g" "$PROTOS/service_pb2.py"
