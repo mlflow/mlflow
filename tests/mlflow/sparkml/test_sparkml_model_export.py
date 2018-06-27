ce3e9a1caddc1755d50236754a76830dc8173cc7
tomasatdatabricks <tomas.nykodym@databricks.com>
2018-06-27 13:25:55 -0700

    Added missing conda_env to sparkml.save() in sparkml test

--- tests/mlflow/sparkml/test_sparkml_model_export.py
+++ tests/mlflow/sparkml/test_sparkml_model_export.py
@@ -43,8 +43,7 @@ def test_model_export(tmpdir):
     preds_df = model.transform(spark_df)
     print(preds_df.show())
     preds1 = [x.prediction for x in preds_df.select("prediction").collect()]
-    sparkml.save_model(model, path=str(model_path))
-
+    sparkml.save_model(model, path=str(model_path), conda_env=conda_env)
     m = pyfunc.load_pyfunc(str(model_path))
     preds2 = m.predict(pandas_df)
     assert preds1 == preds2
