b58a420ec1ef935b45c4f9cb3bbe91b17b0f1628
Vahe Hakobyan <gfoxvh@gmail.com>
2018-07-03 10:55:13 -0700

    Make metrics with one element display as a bar chart(#118)

--- mlflow/server/js/src/components/MetricView.js
+++ mlflow/server/js/src/components/MetricView.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { LineChart, XAxis, Tooltip, CartesianGrid, Line, YAxis, ResponsiveContainer, Legend } from 'recharts';
+import { LineChart, BarChart, Bar, XAxis, Tooltip, CartesianGrid, Line, YAxis, ResponsiveContainer, Legend } from 'recharts';
 import { connect } from 'react-redux';
 import Utils from '../utils/Utils';
 import { getMetricsByKey } from '../reducers/MetricReducer';
@@ -22,32 +22,57 @@ class MetricView extends Component {
     runUuids: PropTypes.arrayOf(String).isRequired,
   };
 
-  render() {
-    return (
-      <div className="MetricView">
-        <h2 className="MetricView-title">{this.props.title}</h2>
-        <ResponsiveContainer width="100%" aspect={1.55}>
-          <LineChart
-            data={Utils.convertTimestampToInt(this.props.metrics)}
-            margin={{ top: 10, right: 10, left: 10, bottom: 10 }}
-          >
-            <XAxis dataKey="index" type="number"/>
-            <Tooltip isAnimationActive={false} labelStyle={{display: "none"}}/>
-            <CartesianGrid strokeDasharray="3 3"/>
-            <Legend verticalAlign="bottom"/>
-            <YAxis/>
-            {this.props.runUuids.map((uuid, idx) => (
-              <Line type="linear"
-                    dataKey={uuid}
-                    isAnimationActive={false}
-                    connectNulls
-                    stroke={COLORS[idx % COLORS.length]}/>
-            ))}
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
-    )
-  }
+    render() {
+        if (this.props.metrics.length == 1) {
+            return (
+                <div className="MetricView">
+                    <h2 className="MetricView-title">{this.props.title} </h2>
+                    <ResponsiveContainer width="100%" aspect={1.55}>
+                        <BarChart
+                            data={this.props.metrics}
+                            margin={{top: 10, right: 10, left: 10, bottom: 10}}
+                        >
+                            <XAxis dataKey="index"/>
+                            <Tooltip isAnimationActive={false} labelStyle={{display: "none"}}/>
+                            <CartesianGrid strokeDasharray="3 3"/>
+                            <Legend verticalAlign="bottom"/>
+                            <YAxis/>
+                            {this.props.runUuids.map((uuid, idx) => (
+                                <Bar  dataKey={uuid}
+                                      isAnimationActive={false}
+                                      fill={COLORS[idx % COLORS.length]}/>
+                            ))}
+                        </BarChart>
+                    </ResponsiveContainer>
+                </div>
+            )
+        } else {
+            return (
+                <div className="MetricView">
+                    <h2 className="MetricView-title">{this.props.title} </h2>
+                    <ResponsiveContainer width="100%" aspect={1.55}>
+                        <LineChart
+                            data={Utils.convertTimestampToInt(this.props.metrics)}
+                            margin={{top: 10, right: 10, left: 10, bottom: 10}}
+                        >
+                            <XAxis dataKey="index" type="number"/>
+                            <Tooltip isAnimationActive={false} labelStyle={{display: "none"}}/>
+                            <CartesianGrid strokeDasharray="3 3"/>
+                            <Legend verticalAlign="bottom"/>
+                            <YAxis/>
+                            {this.props.runUuids.map((uuid, idx) => (
+                                <Line type="linear"
+                                      dataKey={uuid}
+                                      isAnimationActive={false}
+                                      connectNulls
+                                      stroke={COLORS[idx % COLORS.length]}/>
+                            ))}
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
+            )
+        }
+    }
 }
 
 const mapStateToProps = (state, ownProps) => {
