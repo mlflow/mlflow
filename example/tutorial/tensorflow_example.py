eddf405e470b659d65d1f1f686e339a05af47b3d
Juntai Zheng <juntai.zheng@databricks.com>
2018-07-03 11:14:18 -0700

    added logging and removed magic numbers

--- example/tutorial/tensorflow_example.py
+++ example/tutorial/tensorflow_example.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import mlflow
 from mlflow import tensorflow, tracking
 import numpy as np
 import pandas as pd
@@ -14,21 +15,25 @@ def main(argv):
     # Builds, trains and evaluates a tf.estimator. Then, exports it for inference, logs the exported model 
     # with MLflow, and loads the fitted model back as a PyFunc to make predictions.
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.boston_housing.load_data()
+    # There are 13 features we are using for inference.
     feat_cols = [tf.feature_column.numeric_column(key="features", shape=(13,))]
-    feat_spec = {"features":tf.placeholder("float", name="features", shape=[102, 13])}
+    feat_spec = {"features":tf.placeholder("float", name="features", shape=[None, 13])}
     regressor = tf.estimator.DNNRegressor(
     hidden_units=[50, 20],
     feature_columns=feat_cols)
-    train_input_fn = tf.estimator.inputs.numpy_input_fn({"features": x_train}, y_train, batch_size=404, num_epochs=None, shuffle=True)
+    train_input_fn = tf.estimator.inputs.numpy_input_fn({"features": x_train}, y_train, num_epochs=None, shuffle=True)
+    tracking.start_run()
+    mlflow.log_param("Hidden Units", [50,20])
+    mlflow.log_param("Steps", 1000)
     regressor.train(train_input_fn, steps=1000)
-    test_input_fn = tf.estimator.inputs.numpy_input_fn({"features": x_test}, y_test, batch_size=404, num_epochs=None, shuffle=True)
+    test_input_fn = tf.estimator.inputs.numpy_input_fn({"features": x_test}, y_test, num_epochs=None, shuffle=True)
     # Compute mean squared error
-    regressor.evaluate(test_input_fn, steps=100)
+    mse = regressor.evaluate(test_input_fn, steps=100)
+    mlflow.log_metric("Mean Square Error", mse['average_loss'])
     # Building a receiver function for exporting
     receiver_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(feat_spec)
     temp = tempfile.mkdtemp()
     try:
-        tracking.start_run()
         saved_estimator_path = regressor.export_savedmodel(temp, receiver_fn).decode("utf-8")
         # Logging the saved model
         tensorflow.log_saved_model(saved_model_dir=saved_estimator_path, signature_def_key="predict", artifact_path="model")
@@ -37,6 +42,7 @@ def main(argv):
         df = pd.DataFrame(data=x_test, columns=["features"] * 13)
         # Predicting on the loaded Python Function
         predict_df = pyfunc.predict(df)
+        predict_df['original_labels'] = y_test
         print(predict_df)
     finally:
         shutil.rmtree(temp)
