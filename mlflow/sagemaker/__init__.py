d1c9ea12547d513f71e70355db6d3801c6f95b2f
Corey Zumar <corey.zumar@databricks.com>
2018-07-12 12:47:54 -0500

    Remove datetime import

--- mlflow/sagemaker/__init__.py
+++ mlflow/sagemaker/__init__.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 import os
 from subprocess import Popen, PIPE, STDOUT
 from six.moves import urllib 
-from datetime import datetime
 import tarfile
 import uuid
 
