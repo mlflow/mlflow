f644203264598f903d8a925f1d0770080798f3f4
Brett Nekolny <brett.nekolny@brandfolder.com>
2018-07-17 11:16:28 -0600

    Convert from unittest to pytest.

--- tests/store/test_gcs_artifact_repo.py
+++ tests/store/test_gcs_artifact_repo.py
@@ -1,57 +1,55 @@
 import os
 import mock
-import unittest
+import pytest
 
 from google.cloud.storage import client as gcs_client
 
 from mlflow.store.artifact_repo import ArtifactRepository, GCSArtifactRepository
-from mlflow.utils.file_utils import TempDir
 
 
-class TestGCSArtifactRepo(unittest.TestCase):
-    def setUp(self):
-        # Make sure that the environment variable isn't set to actually make calls
-        self.old_GOOGLE_APPLICATION_CREDENTIALS = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/dev/null'
+@pytest.fixture
+def gcs_mock():
+    # Make sure that the environment variable isn't set to actually make calls
+    old_GOOGLE_APPLICATION_CREDENTIALS = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/dev/null'
 
-        self.gcs = mock.MagicMock(autospec=gcs_client)
+    yield mock.MagicMock(autospec=gcs_client)
 
-    def tearDown(self):
-        if self.old_GOOGLE_APPLICATION_CREDENTIALS:
-            os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = self.old_GOOGLE_APPLICATION_CREDENTIALS
-        self.gcs.stop()
+    if old_GOOGLE_APPLICATION_CREDENTIALS:
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = old_GOOGLE_APPLICATION_CREDENTIALS
 
-    def test_artifact_uri_factory(self):
-        repo = ArtifactRepository.from_artifact_uri("gs://test_bucket/some/path")
-        self.assertIsInstance(repo, GCSArtifactRepository)
+def test_artifact_uri_factory():
+    repo = ArtifactRepository.from_artifact_uri("gs://test_bucket/some/path")
+    assert isinstance(repo, GCSArtifactRepository)
 
-    def test_list_artifacts_empty(self):
-        repo = GCSArtifactRepository("gs://test_bucket/some/path", self.gcs)
-        self.gcs.Client.return_value.get_bucket.return_value\
-            .list_blobs.return_value = []
-        self.assertListEqual(repo.list_artifacts(), [])
+def test_list_artifacts_empty(gcs_mock):
+    repo = GCSArtifactRepository("gs://test_bucket/some/path", gcs_mock)
+    gcs_mock.Client.return_value.get_bucket.return_value\
+        .list_blobs.return_value = []
+    assert repo.list_artifacts() == []
 
-    def test_list_artifacts(self):
-        repo = GCSArtifactRepository("gs://test_bucket/some/path", self.gcs)
-        mockobj = mock.Mock()
-        mockobj.configure_mock(
-                name='/some/path/mockeryname',
-                f='/mockeryname',
-                size=1,
-        )
-        self.gcs.Client.return_value.get_bucket.return_value\
-            .list_blobs.return_value = [mockobj]
-        self.assertEqual(repo.list_artifacts()[0].path, mockobj.f)
-        self.assertEqual(repo.list_artifacts()[0].file_size, mockobj.size)
+def test_list_artifacts(gcs_mock):
+    repo = GCSArtifactRepository("gs://test_bucket/some/path", gcs_mock)
+    mockobj = mock.Mock()
+    mockobj.configure_mock(
+            name='/some/path/mockeryname',
+            f='/mockeryname',
+            size=1,
+    )
+    gcs_mock.Client.return_value.get_bucket.return_value\
+        .list_blobs.return_value = [mockobj]
+    assert repo.list_artifacts()[0].path == mockobj.f
+    assert repo.list_artifacts()[0].file_size == mockobj.size
 
-    def test_log_artifact(self):
-        repo = GCSArtifactRepository("gs://test_bucket/some/path", self.gcs)
-        with TempDir() as tmp:
-            if not os.path.exists(tmp.path()):
-                os.makedirs(tmp.path())
-            with open(tmp.path("test.txt"), "w") as f:
-                f.write("Hello world!")
+def test_log_artifact(gcs_mock, tmpdir):
+    repo = GCSArtifactRepository("gs://test_bucket/some/path", gcs_mock)
 
-            self.gcs.Client.return_value.get_bucket.return_value\
-                .upload_from_filename.side_effect = os.path.isfile
-            repo.log_artifact(tmp.path('test.txt'))
+    d = tmpdir.mkdir("data")
+    f = d.join("test.txt")
+    f.write("hello world!")
+    fpath = d + '/test.txt'
+    fpath = fpath.strpath
+
+    gcs_mock.Client.return_value.get_bucket.return_value\
+        .upload_from_filename.side_effect = os.path.isfile(fpath)
+    repo.log_artifact(fpath)
