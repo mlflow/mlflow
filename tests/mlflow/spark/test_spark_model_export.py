64b06316f5cf6b9f5be48bb5ca2a7b378dfd4a54
Juntai Zheng <juntai.zheng@databricks.com>
2018-07-16 17:27:28 -0700

    removed pytest large

--- tests/mlflow/spark/test_spark_model_export.py
+++ tests/mlflow/spark/test_spark_model_export.py
@@ -18,7 +18,6 @@ from mlflow.utils.environment import _mlflow_conda_env
 from tests.helper_functions import score_model_in_sagemaker_docker_container
 
 
-@pytest.mark.large
 def test_model_export(tmpdir):
     conda_env = os.path.join(str(tmpdir), "conda_env.yml")
     _mlflow_conda_env(conda_env, additional_pip_deps=["pyspark=={}".format(pyspark_version)])
