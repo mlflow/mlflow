d1180977df1e8beb3fd9d7b355d5d7ea87e11f53
Aaron Davidson <aaron@databricks.com>
2018-07-18 07:44:05 -0700

    Fix path

--- tests/mlflow/tensorflow/test_tensorflow_model_export.py
+++ tests/mlflow/tensorflow/test_tensorflow_model_export.py
@@ -102,7 +102,7 @@ class TestModelExport(unittest.TestCase):
         for reference code.
         """
         with TempDir(chdr=False, remove_on_exit=True) as tmp:
-            path = tf.contrib.keras.utils.get_file("imports-85.data", "../../data/uci-autos-imports-85.data")
+            path = os.path.abspath("tests/data/uci-autos-imports-85.data")
             # Order is important for the csv-readers, so we use an OrderedDict here.
             defaults = collections.OrderedDict([
                 ("body-style", [""]),
