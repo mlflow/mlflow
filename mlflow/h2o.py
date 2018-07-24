dafe398e5c07d50dc17902040061b593eee44e73
Toon Baeyens <toon.baeyens@kbc.be>
2018-07-24 13:05:56 +0200

    load_pyfunc didn't keep pandas' index

--- mlflow/h2o.py
+++ mlflow/h2o.py
@@ -68,7 +68,9 @@ class _H2OModelWrapper:
 
     def predict(self, dataframe):
         import h2o
-        return self.h2o_model.predict(h2o.H2OFrame(dataframe)).as_data_frame()
+        predicted = self.h2o_model.predict(h2o.H2OFrame(dataframe)).as_data_frame()
+        predicted.index = dataframe.index
+        return predicted
 
 
 def load_pyfunc(path):
