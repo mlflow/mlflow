f4593678044efefd89b26a6d405a9226f2975188
Javier <javierluraschi@hotmail.com>
2018-07-24 20:15:05 -0700

    export mlflow_install and mlflow_ui

--- R/install.R
+++ R/install.R
@@ -1,3 +1,11 @@
+#' Install MLflow
+#'
+#' Installs MLflow for individual use.
+#'
+#' Notice that MLflow requires Python and pip to be installed.
+#' See \url{https://www.python.org/getit/} and \url{https://pip.pypa.io/}.
+#'
+#'  @export
 mlflow_install <- function() {
   python_run(c("pip3", "pip"), "install", "--user", "pandas")
   python_run(c("pip3", "pip"), "install", "--user", "mlflow")
--- R/ui.R
+++ R/ui.R
@@ -1,3 +1,8 @@
+#' MLflow User Interface
+#'
+#' Launches MLflow user interface in a web browser.
+#'
+#' @export
 mlflow_ui <- function() {
   mlflow_cli("ui")
 }
