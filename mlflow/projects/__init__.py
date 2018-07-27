e2edadd236c7f209301d07af58fd687fa270fbb7
Stephanie Bodoff <stephanie.bodoff@databricks.com>
2018-07-27 10:20:17 -0700

    Update __init__.py

--- mlflow/projects/__init__.py
+++ mlflow/projects/__init__.py
@@ -217,7 +217,7 @@ def _fetch_project(uri, subdirectory, version, dst_dir, git_username, git_passwo
 
 def _fetch_git_repo(uri, version, dst_dir, git_username, git_password):
     """
-    Clones the git repo at ``uri`` into ``dst_dir``, checking out commit ``version`` (or defaulting
+    Clone the git repo at ``uri`` into ``dst_dir``, checking out commit ``version`` (or defaulting
     to the head commit of the repository's master branch if version is unspecified). If ``git_username``
     and ``git_password`` are specified, uses them to authenticate while fetching the repo. Otherwise,
     assumes authentication parameters are specified by the environment, e.g. by a Git credential
