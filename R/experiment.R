c7e0bd800566b0e2cb33c59fe34707518d0d4940
kevinykuo <kevinykuo@gmail.com>
2018-07-29 12:55:59 -0400

    add mlflow_log_parameter()

--- R/experiment.R
+++ R/experiment.R
@@ -139,6 +139,26 @@ mlflow_log_metric <- function(run_uuid, key, value, timestamp = NULL) {
   invisible(NULL)
 }
 
+#' Log Parameter
+#'
+#' API to log a parameter used for this run. Examples are params and hyperparams
+#'   used for ML training, or constant dates and values used in an ETL pipeline.
+#'   A params is a STRING key-value pair. For a run, a single parameter is allowed
+#'   to be logged only once.
+#'
+#' @param run_uuid Unique ID for the run for which parameter is recorded.
+#' @param key Name of the parameter.
+#' @param value String value of the parameter.
+#' @export
+mlflow_log_parameter <- function(run_uuid, key, value) {
+  response <- mlflow_rest("runs", "log-parameter", verb = "POST", data = list(
+    run_uuid = run_uuid,
+    key = key,
+    value = as.character(value)
+  ))
+  invisible(NULL)
+}
+
 mlflow_relative_paths <- function(paths) {
   gsub(paste0("^", file.path(getwd(), "")), "", paths)
 }
