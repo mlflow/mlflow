017cb08b1f75ae1bbbfdd9f202bb023e08b0a76f
Aaron Davidson <aaron@databricks.com>
2018-07-30 10:04:46 -0700

    Add support for Databricks-backed RestStore (#200)
    * [WIP] Support Databricks-backed REST API store
    
    * test
    
    * Add support for Databricks-backed RestStore
    
    * lint
    
    * Fix mocking structure & add rest_store test
    
    * Add docs
    
    * Tiny typo change
    
    * Add test!
    
    * python3 compatibility

--- mlflow/utils/rest_utils.py
+++ mlflow/utils/rest_utils.py
@@ -8,47 +8,68 @@ import requests
 from mlflow.utils.logging_utils import eprint
 
 
-def get_databricks_hostname_and_auth():
-    """
-    Reads the hostname & auth token to use for running on Databricks from the config file created
-    by the Databricks CLI. Returns a tuple of (hostname, auth, token) to use when making API
-    requests.
-    """
-    profile = provider.DEFAULT_SECTION
-    config = provider.get_config_for_profile(profile)
-    if config.username is not None and config.password is not None:
-        return config.host, (config.username, config.password), config.token
-    if config.token:
-        return config.host, None, config.token
+def _fail_malformed_databricks_auth(profile):
     raise Exception("Got malformed Databricks CLI profile '%s'. Please make sure the Databricks "
                     "CLI is properly configured as described at "
                     "https://github.com/databricks/databricks-cli." % profile)
 
 
+def get_databricks_http_request_kwargs_or_fail(profile=None):
+    """
+    Reads in configuration necessary to make HTTP requests to a Databricks server. This
+    uses the Databricks CLI's ConfigProvider interface to load the DatabricksConfig object.
+    This method will throw an exception if sufficient auth cannot be found.
+
+    :param profile: Databricks CLI profile. If not provided, we will read the default profile.
+    :return: Dictionary with parameters that can be passed to http_request(). This will
+             at least include the hostname and headers sufficient to authenticate to Databricks.
+    """
+    if not profile:
+        profile = provider.DEFAULT_SECTION
+    config = provider.get_config_for_profile(profile)
+
+    hostname = config.host
+    if not hostname:
+        _fail_malformed_databricks_auth(profile)
+
+    basic_auth_str = None
+    if config.username is not None and config.password is not None:
+        basic_auth_str = ("%s:%s" % (config.username, config.password)).encode("utf-8")
+    elif config.token:
+        basic_auth_str = ("token:%s" % config.token).encode("utf-8")
+    if not basic_auth_str:
+        _fail_malformed_databricks_auth(profile)
+
+    headers = {
+        "Authorization": "Basic " + base64.standard_b64encode(basic_auth_str).decode("utf-8")
+    }
+
+    secure_verify = True
+    if hasattr(config, 'insecure') and config.insecure:
+        secure_verify = False
+
+    return {
+        'hostname': hostname,
+        'headers': headers,
+        'secure_verify': secure_verify,
+    }
+
+
 def databricks_api_request(endpoint, method, req_body_json=None, params=None):
-    hostname, auth, token = get_databricks_hostname_and_auth()
     final_endpoint = "/api/2.0/%s" % endpoint
-    if token is not None:
-        token_bytes = ("token:%s" % token).encode("utf-8")
-        headers = {
-            "Authorization": "Basic " + base64.standard_b64encode(token_bytes).decode("utf-8")
-        }
-    else:
-        headers = None
-    # TODO: Remove `verify=False`, currently need it to run against dev shards.
-    return http_request(hostname=hostname, endpoint=final_endpoint, method=method, auth=auth,
-                        headers=headers, req_body_json=req_body_json, params=params)
+    request_params = get_databricks_http_request_kwargs_or_fail()
+    return http_request(endpoint=final_endpoint, method=method, req_body_json=req_body_json,
+                        params=params, **request_params)
 
 
-def http_request(hostname, endpoint, method, auth, headers, req_body_json, params,
-                 retries=3, retry_interval=3):
+def http_request(hostname, endpoint, method, headers=None, req_body_json=None, params=None,
+                 secure_verify=True, retries=3, retry_interval=3):
     """
     Makes an HTTP request with the specified method to the specified hostname/endpoint. Retries
     up to `retries` times if a request fails with a server error (e.g. error code 500), waiting
     `retry_interval` seconds between successive retries. Parses the API response (assumed to be
     JSON) into a Python object and returns it.
 
-    :param auth: Auth tuple for Basic/Digest/Custom HTTP Auth
     :param headers: Request headers to use when making the HTTP request
     :param req_body_json: Dictionary containing the request body
     :param params: Query parameters for the request
@@ -56,8 +77,8 @@ def http_request(hostname, endpoint, method, auth, headers, req_body_json, param
     """
     url = "%s%s" % (hostname, endpoint)
     for i in range(retries):
-        response = requests.request(method=method, url=url, headers=headers, verify=False,
-                                    params=params, json=req_body_json, auth=auth)
+        response = requests.request(method=method, url=url, headers=headers, verify=secure_verify,
+                                    params=params, json=req_body_json)
         if response.status_code >= 200 and response.status_code < 500:
             return json.loads(response.text)
         else:
