9a31bd46692e58bead8b0d2f1d2b924ed1263961
Andrew Chen <andrewmnchen@gmail.com>
2018-08-03 14:36:22 -0700

    fix bugs from QA

--- mlflow/store/rest_store.py
+++ mlflow/store/rest_store.py
@@ -238,10 +238,10 @@ class DatabricksStore(RestStore):
                                'hostname', 'headers', and 'secure_verify' are required.
     """
     def __init__(self, http_request_kwargs):
-        if not http_request_kwargs['hostname']:
+        if http_request_kwargs['hostname'] is None:
             raise Exception('hostname must be provided to DatabricksStore')
-        if not http_request_kwargs['headers']:
+        if http_request_kwargs['headers'] is None:
             raise Exception('headers must be provided to DatabricksStore')
-        if not http_request_kwargs['verify']:
+        if http_request_kwargs['verify'] is None:
             raise Exception('verify must be provided to DatabricksStore')
         super(DatabricksStore, self).__init__(http_request_kwargs)
