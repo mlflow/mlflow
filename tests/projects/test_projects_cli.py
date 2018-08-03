c5330438bc5efce3f00abefa1fa7841748bb9970
Siddharth Murching <smurching@gmail.com>
2018-08-03 14:39:51 -0700

    Fix bug with fetching projects from SSH URIs (#236)
    Fixes bug where we'd fetch git projects at SSH URIs into a local directory with the same name as the URI (instead of into a temporary directory).

--- tests/projects/test_projects_cli.py
+++ tests/projects/test_projects_cli.py
@@ -1,25 +1,35 @@
 import pytest
 
-import mlflow
 from mlflow import cli
-from mlflow.utils.file_utils import TempDir
-from tests.integration.utils import invoke_cli_runner, update_temp_env
-from tests.projects.utils import TEST_PROJECT_DIR, GIT_PROJECT_URI
+from tests.integration.utils import invoke_cli_runner
+from tests.projects.utils import TEST_PROJECT_DIR, GIT_PROJECT_URI, SSH_PROJECT_URI
+from tests.projects.utils import tracking_uri_mock  # pylint: disable=unused-import
 
 
 @pytest.mark.large
-def test_run_local():
-    with TempDir() as tmp:
-        with update_temp_env({mlflow.tracking._TRACKING_URI_ENV_VAR: tmp.path()}):
-            excitement_arg = 2
-            name = "friend"
-            invoke_cli_runner(cli.run, [TEST_PROJECT_DIR, "-e", "greeter", "-P",
-                                        "greeting=hi", "-P", "name=%s" % name,
-                                        "-P", "excitement=%s" % excitement_arg])
+def test_run_local(tracking_uri_mock):  # pylint: disable=unused-argument
+    excitement_arg = 2
+    name = "friend"
+    invoke_cli_runner(cli.run, [TEST_PROJECT_DIR, "-e", "greeter", "-P",
+                                "greeting=hi", "-P", "name=%s" % name,
+                                "-P", "excitement=%s" % excitement_arg])
 
 
 @pytest.mark.large
-def test_run_git():
-    with TempDir() as tmp:
-        with update_temp_env({mlflow.tracking._TRACKING_URI_ENV_VAR: tmp.path()}):
-            invoke_cli_runner(cli.run, [GIT_PROJECT_URI, "--no-conda", "-P", "alpha=0.5"])
+def test_run_git_https(tracking_uri_mock):  # pylint: disable=unused-argument
+    # Invoke command twice to ensure we set Git state in an isolated manner (e.g. don't attempt to
+    # create a git repo in the same directory twice, etc)
+    assert GIT_PROJECT_URI.startswith("https")
+    invoke_cli_runner(cli.run, [GIT_PROJECT_URI, "-P", "alpha=0.5"])
+    invoke_cli_runner(cli.run, [GIT_PROJECT_URI, "-P", "alpha=0.5"])
+
+
+@pytest.mark.large
+@pytest.mark.requires_ssh
+def test_run_git_ssh(tracking_uri_mock):  # pylint: disable=unused-argument
+    # Note: this test requires SSH authentication to GitHub, and so is disabled in Travis, where SSH
+    # keys are unavailable. However it should be run locally whenever logic related to running
+    # Git projects is modified.
+    assert SSH_PROJECT_URI.startswith("git@")
+    invoke_cli_runner(cli.run, [SSH_PROJECT_URI, "-P", "alpha=0.5"])
+    invoke_cli_runner(cli.run, [SSH_PROJECT_URI, "-P", "alpha=0.5"])
