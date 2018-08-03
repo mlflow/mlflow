3b851618c3895d3ebd44750c7cc1dd95ec4b0dc7
Joel Akeret <joel.akeret@gmail.com>
2018-08-03 23:18:21 +0200

    Fix GCSArtifactRepository issue when calling list_artifacts on a path containing nested directories (#233)
    * fixing GCS issue with nested directories
    
    * renaming, testing artifact listing at root and sub dir level

--- mlflow/store/gcs_artifact_repo.py
+++ mlflow/store/gcs_artifact_repo.py
@@ -69,7 +69,7 @@ class GCSArtifactRepository(ArtifactRepository):
 
         bkt = self.gcs.Client().get_bucket(bucket)
 
-        infos = self._list_folders(bkt, prefix)
+        infos = self._list_folders(bkt, prefix, artifact_path)
 
         results = bkt.list_blobs(prefix=prefix, delimiter="/")
         for result in results:
@@ -78,13 +78,13 @@ class GCSArtifactRepository(ArtifactRepository):
 
         return sorted(infos, key=lambda f: f.path)
 
-    def _list_folders(self, bkt, prefix):
+    def _list_folders(self, bkt, prefix, artifact_path):
         results = bkt.list_blobs(prefix=prefix, delimiter="/")
         dir_paths = set()
         for page in results.pages:
             dir_paths.update(page.prefixes)
 
-        return [FileInfo(path[len(prefix):-1], True, None)for path in dir_paths]
+        return [FileInfo(path[len(artifact_path)+1:-1], True, None)for path in dir_paths]
 
     def download_artifacts(self, artifact_path):
         with TempDir(remove_on_exit=False) as tmp:
