e553854b0d68146f92cb6b0b2c2fb043094d0847
kevinykuo <kevinykuo@gmail.com>
2018-08-03 04:15:08 -0400

    mlflow_run() should not be a generic

--- R/mlflow/r/run.R
+++ R/mlflow/r/run.R
@@ -3,17 +3,11 @@
 #' Runs the given file, expression or function within
 #' the context of an MLflow run.
 #'
-#' @param x A file, expression or function to run.
+#' @param x A directory or an R script.
+#' @param params A list of parameters.
 #'
 #' @export
-mlflow_run <- function(x) {
-  UseMethod("mlflow_run")
+mlflow_run <- function(x, params = NULL) {
+  # do stuff
 }
 
-#' @export
-mlflow_run.character <- function(x) {
-}
-
-#' @export
-mlflow_run.function <- function(x) {
-}
