2252b6b317fd8e7f61f668ae475d22534977b182
Sid Murching <sid.murching@databricks.com>
2018-08-03 11:45:48 -0700

    Add asserts on URIs

--- tests/projects/test_projects_cli.py
+++ tests/projects/test_projects_cli.py
@@ -19,12 +19,14 @@ def test_run_local(tracking_uri_mock):  # pylint: disable=unused-argument
 def test_run_git_https(tracking_uri_mock):  # pylint: disable=unused-argument
     # Invoke command twice to ensure we set Git state in an isolated manner (e.g. don't attempt to
     # create a git repo in the same directory twice, etc)
+    assert GIT_PROJECT_URI.startswith("https")
     invoke_cli_runner(cli.run, [GIT_PROJECT_URI, "-P", "alpha=0.5"])
     invoke_cli_runner(cli.run, [GIT_PROJECT_URI, "-P", "alpha=0.5"])
 
 
 @pytest.mark.large
 def test_run_git_ssh(tracking_uri_mock):  # pylint: disable=unused-argument
+    assert SSH_PROJECT_URI.startswith("https")
     # Disable host-key checking so the test can run without prompting for approval
     with update_temp_env({"GIT_SSH_COMMAND": "ssh -o StrictHostKeyChecking=no"}):
         invoke_cli_runner(cli.run, [SSH_PROJECT_URI, "-P", "alpha=0.5"])
