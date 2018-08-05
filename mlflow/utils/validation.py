383a683433d29c1d8b0b2ca159a4f9d151e1984c
Aaron Davidson <aaron@databricks.com>
2018-08-04 18:05:02 -0700

    Add a concise explanation to the error message

--- mlflow/utils/validation.py
+++ mlflow/utils/validation.py
@@ -5,25 +5,31 @@ import re
 
 # Regex for metric and parameter names: we only allow [a-zA-Z0-9_- ] ([\w-]), / and . characters,
 # and we do not allow adjacent, leading or trailing / or path components that are empty or only
-# contain dots. The goal is to make our names be valid, unique path names too in order to treat
-# them as such in the UI and file store.
+# contain dots or whitespace. The goal is to make our names be valid, unique path names too in
+# order to treat them as such in the UI and file store.
 _METRIC_AND_PARAM_NAME_REGEX = re.compile(
     r"^([\w.\- ]*[\w\-][\w.\- ]*)(/[\w.\- ]*[\w\-][\w.\- ]*)*$")
 
 # Regex for valid run IDs: must be a 32-character hex string.
 _RUN_ID_REGEX = re.compile(r"^[0-9a-f]{32}$")
 
+# An error message which strives to be concise and mostly-correct.
+CONCISE_EXPLANATION_MESSAGE = (
+    "Names must contain at least one alphanumeric, underscore (_) or dash (-), and may be"
+    " composed additionally of periods (.), whitespace ( ), and slashes (/)."
+)
+
 
 def _validate_metric_name(name):
     """Check that `name` is a valid metric name and raise an exception if it isn't."""
     if _METRIC_AND_PARAM_NAME_REGEX.match(name) is None:
-        raise Exception("Invalid metric name: '%s'" % name)
+        raise Exception("Invalid metric name: '%s'. %s" % (name, CONCISE_EXPLANATION_MESSAGE))
 
 
 def _validate_param_name(name):
     """Check that `name` is a valid parameter name and raise an exception if it isn't."""
     if _METRIC_AND_PARAM_NAME_REGEX.match(name) is None:
-        raise Exception("Invalid parameter name: '%s'" % name)
+        raise Exception("Invalid parameter name: '%s'. %s" % (name, CONCISE_EXPLANATION_MESSAGE))
 
 
 def _validate_run_id(run_id):
