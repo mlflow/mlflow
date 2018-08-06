2360330ca0c251d12a49e369bed8426e8d24978c
Aaron Davidson <aaron@databricks.com>
2018-08-06 09:06:39 -0700

    Add artifact location to create-experiment, add POST /runs/search (#232)

--- mlflow/experiments.py
+++ mlflow/experiments.py
@@ -17,12 +17,18 @@ def commands():
 
 @commands.command()
 @click.argument("experiment_name")
-def create(experiment_name):
+@click.option("--artifact-location", "-l",
+              help="Base location for runs to store artifact results. Artifacts will be stored "
+                   "at $artifact_location/$run_id/artifacts. See "
+                   "https://mlflow.org/docs/latest/tracking.html#where-runs-get-recorded for "
+                   "more info on the properties of artifact location. "
+                   "If no location is provided, the tracking server will pick a default.")
+def create(experiment_name, artifact_location):
     """
     Creates a new experiment in the configured tracking server.
     """
     store = _get_store()
-    exp_id = store.create_experiment(experiment_name)
+    exp_id = store.create_experiment(experiment_name, artifact_location)
     print("Created experiment '%s' with id %d" % (experiment_name, exp_id))
 
 
