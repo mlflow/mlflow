c53e2f29d613610e7bd66af448e0b095fc3ad8ea
vfdev-5 <vfdev.5@gmail.com>
2018-08-08 09:44:55 +0200

    Addresses #198: handle pytorch models + tests

--- mlflow/pytorch.py
+++ mlflow/pytorch.py
@@ -4,20 +4,13 @@ Manages logging and loading PyTorch models as Python Functions. You are expected
 ``saved_models`` and pass their paths to ``log_saved_model()``
 so that MLflow can track the models.
 
-In order to load the model to predict on it again, you can call
-``model = mlflow.pyfunc.load_pyfunc(saved_model_dir)``, followed by
-``prediction= model.predict(pandas DataFrame)`` in order to obtain a prediction in a pandas
-DataFrame.
-
-Note that the loaded PyFunc model does not expose any APIs for model training.
 """
 
 from __future__ import absolute_import
 
 import os
-import collections
 
-import pandas
+import numpy as np
 import torch
 
 from mlflow import pyfunc
@@ -66,8 +59,8 @@ def save_model(pytorch_model, path, conda_env=None, mlflow_model=Model(), **kwar
     model_path = os.path.join(path, "model.pth")
 
     # Save pytorch model
-    save_location = torch.save(pytorch_model, model_path, **kwargs)
-    model_file = os.path.basename(save_location)
+    torch.save(pytorch_model, model_path, **kwargs)
+    model_file = os.path.basename(model_path)
 
     mlflow_model.add_flavor(FLAVOR_NAME, model_data=model_file, pytorch_version=torch.__version__)
     pyfunc.add_to_model(mlflow_model, loader_module="mlflow.pytorch",
@@ -76,18 +69,20 @@ def save_model(pytorch_model, path, conda_env=None, mlflow_model=Model(), **kwar
 
 
 def _load_model(path, **kwargs):
-
     mlflow_model_path = os.path.join(path, "MLmodel")
     if not os.path.exists(mlflow_model_path):
         raise RuntimeError("MLmodel is not found at '{}'".format(path))
+
     mlflow_model = Model.load(mlflow_model_path)
 
     assert FLAVOR_NAME in mlflow_model.flavors, \
         "Stored model can not be loaded with mlflow.pytorch"
 
     # This maybe replaced by a warning and then try/except torch.load
-    assert torch.__version__ == mlflow_model.flavors["pytorch_version"], \
-        "Unfortunately stored model version '{}' does not match ".format(mlflow_model.flavors["pytorch_version"]) + \
+    flavor = mlflow_model.flavors[FLAVOR_NAME]
+    assert torch.__version__ == flavor["pytorch_version"], \
+        "Unfortunately stored model version '{}' does not match "\
+        .format(flavor["pytorch_version"]) + \
         "installed PyTorch version '{}'".format(torch.__version__)
 
     path = os.path.abspath(path)
@@ -98,7 +93,8 @@ def _load_model(path, **kwargs):
 def load_model(path, run_id=None, **kwargs):
     """
     Load a PyTorch model from a local file (if run_id is None) or a run.
-    :param path: Local filesystem path or Run-relative artifact path to the model saved by `mlflow.pytorch.log_model`.
+    :param path: Local filesystem path or Run-relative artifact path to the model saved by
+        `mlflow.pytorch.log_model`.
     :param run_id: Run ID. If provided it is combined with path to identify the model.
     :param kwargs: kwargs to pass to `torch.load` method
     """
@@ -114,34 +110,24 @@ def load_pyfunc(path, **kwargs):
     :param path: Local filesystem path to the model saved by `mlflow.pytorch.log_model`.
     :param kwargs: kwargs to pass to `torch.load` method
     """
-    return _PyTorchWrapper(_load_model(path, **kwargs))
+    return _PyTorchWrapper(_load_model(os.path.dirname(path), **kwargs))
 
 
 class _PyTorchWrapper(object):
     """
     Wrapper class that creates a predict function such that
-    predict(data: ndarray or iterable) -> model's output
+    predict(data: ndarray) -> model's output as numpy.ndarray
     """
     def __init__(self, pytorch_model):
         self.pytorch_model = pytorch_model
 
-    @staticmethod
-    def _convert_to_tensor(data, device):
-        if torch.is_tensor(data):
-            if device:
-                data = data.to(device=device)
-            return data
-        elif isinstance(data, torch._six.string_classes):
-            return data
-        elif isinstance(data, collections.Mapping):
-            return {k: _PyTorchWrapper._convert_to_tensor(sample, device=device) for k, sample in data.items()}
-        elif isinstance(data, collections.Sequence):
-            return [_PyTorchWrapper._convert_to_tensor(sample, device=device) for sample in data]
-        else:
-            raise TypeError(("input must contain tensors, dicts or lists; found {}"
-                             .format(type(data))))
-
-    def predict(self, data, device):
-
-        input_tensor = self._convert_to_tensor(data, device)
-        return self.pytorch_model(input_tensor)
+    def predict(self, data, device='cpu'):
+        assert isinstance(data, np.ndarray), "Input data should be numpy.ndarray"
+        self.pytorch_model.eval()
+        with torch.no_grad():
+            input_tensor = torch.from_numpy(data).to(device)
+            # Add batch dimension
+            input_tensor = input_tensor.unsqueeze(dim=0)
+            preds = self.pytorch_model(input_tensor)
+            # Remove batch dim and convert to numpy array
+            return preds.squeeze(dim=0).numpy()
--- tests/pytorch/test_pytorch_model_export.py
+++ tests/pytorch/test_pytorch_model_export.py
@@ -2,71 +2,126 @@ from __future__ import print_function
 
 import unittest
 
-import sklearn.datasets as datasets
-import torch
+import numpy as np
 
-import tempfile
-import mlflow.h2o
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+import mlflow.pytorch
 from mlflow import pyfunc
 from mlflow import tracking
 from mlflow.utils.file_utils import TempDir
 
 
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.shape[0], -1)
+
+
 class TestModelExport(unittest.TestCase):
     def setUp(self):
 
-        # Setup dataset
+        torch.manual_seed(12345)
 
-        # h2o.init()
-        # iris = datasets.load_iris()
-        # data = h2o.H2OFrame({
-        #     'feature1': list(iris.data[:, 0]),
-        #     'feature2': list(iris.data[:, 1]),
-        #     'target': list(map(lambda i: "Flower %d" % i, iris.target))
-        # })
-        # train, self.test = data.split_frame(ratios=[.7])
-        self.test = None
+        num_train_samples = 100
+        num_test_samples = 30
+
+        train_dataset = [[torch.rand(3, 32, 32),
+                          torch.randint(0, 10, size=(1,), dtype=torch.long).item()]
+                         for _ in range(num_train_samples)]
+        self.test_dataset = [[torch.rand(3, 32, 32),
+                              torch.randint(0, 10, size=(1,), dtype=torch.long).item()]
+                             for _ in range(num_test_samples)]
+
+        batch_size = 16
+        num_workers = 4
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size,
+                                      num_workers=num_workers, shuffle=True, drop_last=True)
+
+        self.test_dataloader = DataLoader(self.test_dataset, batch_size=batch_size,
+                                          num_workers=num_workers, shuffle=False, drop_last=False)
 
         # Setup model
-        self.model = None
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=(3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2)),
+            nn.Conv2d(32, 16, kernel_size=(3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2)),
+            nn.Conv2d(16, 16, kernel_size=(3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2)),
+            Flatten(),
+            nn.Linear(64, 10)
+        )
 
         # Train
-        # self.model.train(['feature1', 'feature2'], 'target', training_frame=train)
+        criterion = nn.CrossEntropyLoss()
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=0.01)
+        self.model.train()
+        for epoch in range(5):
+            for batch in train_dataloader:
+                optimizer.zero_grad()
+                y_pred = self.model(batch[0])
+                loss = criterion(y_pred, batch[1])
+                loss.backward()
+                optimizer.step()
+
         # Predict
-        # self.predicted = self.gbm.predict(self.test).as_data_frame()
-        self.predicted = None
+        self.predictions = self._predict(self.model, self.test_dataloader)
+
+    @staticmethod
+    def _predict(model, test_dataloader):
+        batch_size = test_dataloader.batch_size
+        predictions = np.zeros((len(test_dataloader.sampler),))
+        model.eval()
+        with torch.no_grad():
+            for i, batch in enumerate(test_dataloader):
+                y_probas = F.softmax(model(batch[0]), dim=1).numpy()
+                y_preds = np.argmax(y_probas, axis=1)
+                predictions[i * batch_size:(i + 1) * batch_size] = y_preds
+        return predictions
 
     def test_log_model(self):
-        pass
-        # old_uri = tracking.get_tracking_uri()
-        # # should_start_run tests whether or not calling log_model() automatically starts a run.
-        # for should_start_run in [False, True]:
-        #     with TempDir(chdr=True, remove_on_exit=True) as tmp:
-        #         try:
-        #             tracking.set_tracking_uri("test")
-        #             if should_start_run:
-        #                 tracking.start_run()
-        #             mlflow.h2o.log_model(self.gbm, artifact_path="gbm")
-        #
-        #             # Load model
-        #             gbm_loaded = mlflow.h2o.load_model("gbm",
-        #                                                run_id=tracking.active_run().info.run_uuid)
-        #             assert all(gbm_loaded.predict(self.test).as_data_frame() == self.predicted)
-        #         finally:
-        #             tracking.end_run()
-        #             tracking.set_tracking_uri(old_uri)
+        old_uri = tracking.get_tracking_uri()
+        # should_start_run tests whether or not calling log_model() automatically starts a run.
+        for should_start_run in [False, True]:
+            with TempDir(chdr=True, remove_on_exit=True) as tmp:
+                try:
+                    tracking.set_tracking_uri("test")
+                    if should_start_run:
+                        tracking.start_run()
+
+                    mlflow.pytorch.log_model(self.model, artifact_path="pytorch")
+
+                    # Load model
+                    run_id = tracking.active_run().info.run_uuid
+                    model_loaded = mlflow.pytorch.load_model("pytorch", run_id=run_id)
+
+                    test_predictions = self._predict(model_loaded, self.test_dataloader)
+                    assert all(test_predictions == self.predictions)
+                finally:
+                    tracking.end_run()
+                    tracking.set_tracking_uri(old_uri)
 
     def test_save_and_load_model(self):
-        pass
-        # with TempDir(chdr=True, remove_on_exit=True) as tmp:
-        #     path = tmp.path("model")
-        #     mlflow.h2o.save_model(self.gbm, path)
-        #
-        #     # Loading h2o model
-        #     gbm_loaded = mlflow.h2o.load_model(path)
-        #     assert all(gbm_loaded.predict(self.test).as_data_frame() == self.predicted)
-        #
-        #     # Loading pyfunc model
-        #     pyfunc_loaded = mlflow.pyfunc.load_pyfunc(path)
-        #     assert all(pyfunc_loaded.predict(self.test.as_data_frame()) == self.predicted)
+        with TempDir(chdr=True, remove_on_exit=True) as tmp:
+            path = tmp.path("model")
+            mlflow.pytorch.save_model(self.model, path)
 
+            # Loading pytorch model
+            model_loaded = mlflow.pytorch.load_model(path)
+            assert all(self._predict(model_loaded, self.test_dataloader) == self.predictions)
+
+            # Loading pyfunc model
+            pyfunc_loaded = mlflow.pyfunc.load_pyfunc(path)
+
+            with torch.no_grad():
+                for i, dp in enumerate(self.test_dataset):
+                    data = dp[0].numpy()
+                    y_proba = pyfunc_loaded.predict(data)
+                    y_pred = y_proba.argmax()
+                    assert y_pred == self.predictions[i]
