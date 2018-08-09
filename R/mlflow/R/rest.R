d0f3c55549d0458e8ca74dc59bd0735e036882a7
kevinykuo <kevinykuo@gmail.com>
2018-08-09 14:52:57 -0400

    refactor `mlflow_rest()`

--- R/mlflow/R/rest.R
+++ R/mlflow/R/rest.R
@@ -38,14 +38,10 @@ mlflow_rest_timeout <- function() {
 mlflow_rest <- function(..., query = NULL, data = NULL, verb = "GET", version = "2.0") {
   args <- list(...)
 
-  tracking_uri <- mlflow_tracking_uri()
-  tracking_uri <- if (startsWith(tracking_uri, "http")) tracking_uri else {
-    mc <- mlflow_connect()
-    mc$tracking_uri
-  }
+  mc <- mlflow_get_or_create_active_connection()
 
   api_url <- file.path(
-    tracking_uri,
+    mc$tracking_uri,
     mlflow_rest_path(version),
     paste(args, collapse = "/")
   )
