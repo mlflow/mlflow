3ae67327e3a6da8491d65b91a7fddbced5c7ba22
Javier <javierluraschi@hotmail.com>
2018-08-09 11:53:18 -0700

    mlflow_log_artifact() for local uris

--- R/mlflow/R/artifact.R
+++ R/mlflow/R/artifact.R
@@ -3,7 +3,7 @@ mlflow_active_artifact_uri <- function() {
   run_info$info$artifact_uri
 }
 
-mlflow_artifact_uri <- function(artifact_uri) {
+mlflow_artifact_type <- function(artifact_uri) {
   artifact_type <- NULL
   if (dir.exists(artifact_uri)) {
     artifact_type <- "local_artifact"
@@ -36,40 +36,56 @@ mlflow_artifact_uri <- function(artifact_uri) {
 #' Logs an specific file or directory as an artifact.
 #'
 #' @param path The file or directory to log as an artifact.
+#' @param artifact_path Destination path within the run’s artifact URI.
 #'
 #' @export
-mlflow_log_artifact <- function(path) {
+mlflow_log_artifact <- function(path, artifact_path = NULL) {
 
   artifact_uri <- mlflow_active_artifact_uri()
   artifact_type <- mlflow_artifact_type(artifact_uri)
 
-  artifact <- structure(
+  artifact_uri <- structure(
     class = c(artifact_type),
-    list(
-      path = path,
-      artifact_uri = artifact_uri
-    )
+    artifact_uri
   )
 
-  mlflow_log_artifact_impl(artifact)
+  mlflow_log_artifact_impl(artifact_uri, path, artifact_path)
 }
 
-mlflow_log_artifact_impl <- function(artifact) {
-  UseMethod("mlflow_save_artifact")
+mlflow_log_artifact_impl <- function(artifact_uri, path, artifact_path) {
+  UseMethod("mlflow_log_artifact_impl")
 }
 
-mlflow_log_artifact_impl.local_artifact <- function(artifact) {
+mlflow_log_artifact_impl.local_artifact <- function(artifact_uri, path, artifact_path) {
+  destination_path <- ifelse(
+    is.null(artifact_path),
+    artifact_uri,
+    file.path(artifact_uri, artifact_path)
+  )
+
+  if (!dir.exists(destination_path)) {
+    dir.create(destination_path, recursive = TRUE)
+  }
+
+  if (dir.exists(path)) {
+    for (file in dir(path))
+      file.copy(file.path(path, file), destination_path)
+  }
+  else {
+    file.copy(path, destination_path)
+  }
+
+  invisible(NULL)
+}
+
+mlflow_log_artifact_impl.s3_artifact <- function(artifact_uri, path, artifact_path) {
   stop("Not implemented.")
 }
 
-mlflow_log_artifact_impl.s3_artifact <- function(artifact) {
+mlflow_log_artifact_impl.google_artifact <- function(artifact_uri, path, artifact_path) {
   stop("Not implemented.")
 }
 
-mlflow_log_artifact_impl.google_artifact <- function(artifact) {
-  stop("Not implemented.")
-}
-
-mlflow_log_artifact_impl.azure_artifact <- function(artifact) {
+mlflow_log_artifact_impl.azure_artifact <- function(artifact_uri, path, artifact_path) {
   stop("Not implemented.")
 }
