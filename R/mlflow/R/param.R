09a3ecfac029c52392404e232c461a17919b0361
Javier <javierluraschi@hotmail.com>
2018-08-09 09:24:30 -0700

    mlflow_param() inadvertently removed

--- R/mlflow/R/param.R
+++ R/mlflow/R/param.R
@@ -1,3 +1,21 @@
+#' Read Command Line Parameter
+#'
+#' Reads a command line parameter.
+#'
+#' @param name The name for this parameter.
+#' @param default The default value for this parameter.
+#' @param type Type of this parameter. Required if `default` is not set.
+#' @param description Optional description for this parameter.
+#'
+#' @export
+mlflow_param <- function(name, default = NULL, type = NULL, description = NULL) {
+  if (is.null(default) && is.null(type))
+    stop("At least one of `default` or `type` must be specified", call. = FALSE)
+  if (!is.null(default) && !is.null(type) && !identical(typeof(default), type))
+    stop("`default` value is not of type ", type, ".", call. = FALSE)
+  .globals$run_params[[name]] %||% default
+}
+
 # from rstudio/tfruns R/flags.R
 # parse command line arguments
 parse_command_line <- function(arguments) {
