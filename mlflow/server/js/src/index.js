57acfa45d6d3ac3fe3837ce902792153bd6cb9ef
Andrew Chen <andrewmnchen@gmail.com>
2018-08-14 10:57:18 -0700

    add csrf

--- mlflow/server/js/src/index.js
+++ mlflow/server/js/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import setupCsrf from './setupCsrf';
+import { setupCsrf } from './setupCsrf';
 import { Provider } from 'react-redux';
 import store from './Store';
 
