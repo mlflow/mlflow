cdd15fcd889af98525ad340bf6f079424cc85468
Corey Zumar <corey.zumar@databricks.com>
2018-08-15 13:18:01 -0700

    Revert helper functions for tests

--- tests/helper_functions.py
+++ tests/helper_functions.py
@@ -1,12 +1,9 @@
 import os
 import random
-
 import requests
 import string
 from subprocess import Popen, PIPE, STDOUT
 import time
-
-import pandas as pd
 
 
 def random_int(lo=1, hi=1e10):
@@ -22,10 +19,6 @@ def random_file(ext):
 
 
 def score_model_in_sagemaker_docker_container(model_path, data):
-    """
-    :param data: The data to send to the docker container for testing. This is either a 
-                 Pandas dataframe or a JSON-formatted string.
-    """
     env = dict(os.environ)
     env.update(LC_ALL="en_US.UTF-8", LANG="en_US.UTF-8")
     proc = Popen(['mlflow', 'sagemaker', 'run-local', '-m', model_path], stdout=PIPE, stderr=STDOUT,
@@ -48,9 +41,8 @@ def score_model_in_sagemaker_docker_container(model_path, data):
         print("server up, ping status", ping_status)
         if ping_status.status_code != 200:
             raise Exception("ping failed, server is not happy")
-        if type(data) == pd.DataFrame: 
-            data = data.to_dict(orient='records')
-        y = requests.post(url='http://localhost:5000/invocations', json=data)
+        x = data.to_dict(orient='records')
+        y = requests.post(url='http://localhost:5000/invocations', json=x)
         import json
         return json.loads(y.content)
     finally:
