8ec193d1537e11c81c38777b33b47b5cd349b828
Matei Zaharia <mateiz@users.noreply.github.com>
2018-08-14 19:18:10 -0700

    Add ESLint config and fix a bunch of lint issues (#298)

--- mlflow/server/js/src/utils/ArtifactUtils.js
+++ mlflow/server/js/src/utils/ArtifactUtils.js
@@ -9,7 +9,7 @@ export class ArtifactNode {
   }
 
   deepCopy() {
-    let node = new ArtifactNode(this.isRoot, this.fileInfo, undefined);
+    const node = new ArtifactNode(this.isRoot, this.fileInfo, undefined);
     node.isLoaded = this.isLoaded;
     if (this.children) {
       const copiedChildren = {};
@@ -21,10 +21,10 @@ export class ArtifactNode {
     return node;
   }
 
-  static setChildren(node, fileInfos) {
+  setChildren(fileInfos) {
     if (fileInfos) {
-      node.children = {};
-      node.isLoaded = true;
+      this.children = {};
+      this.isLoaded = true;
       fileInfos.forEach((fileInfo) => {
         // basename is the last part of the path for this fileInfo.
         const pathParts = fileInfo.path.split("/");
@@ -33,10 +33,10 @@ export class ArtifactNode {
         if (fileInfo.is_dir) {
           children = [];
         }
-        node.children[basename] = new ArtifactNode(false, fileInfo, children);
+        this.children[basename] = new ArtifactNode(false, fileInfo, children);
       });
     } else {
-      node.isLoaded = true;
+      this.isLoaded = true;
     }
   }
 
