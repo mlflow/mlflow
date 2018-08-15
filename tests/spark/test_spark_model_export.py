467ce5c0c326949ea11ff0b6096df3395e958da8
Corey Zumar <corey.zumar@databricks.com>
2018-08-15 13:17:24 -0700

    Revert tests

--- tests/spark/test_spark_model_export.py
+++ tests/spark/test_spark_model_export.py
@@ -9,23 +9,20 @@ from pyspark.version import __version__ as pyspark_version
 import pytest
 from sklearn import datasets
 import shutil
-from collections import namedtuple
 
-from mlflow import pyfunc, mleap
+from mlflow import pyfunc
 from mlflow import spark as sparkm
 from mlflow import tracking
-from mlflow.models import Model
-from mlflow.sparkml import FLAVOR_NAME as SPARKML_FLAVOR_NAME
-from mlflow.sparkml import DFS_TMP as SPARKML_DFS_TMP
 
 from mlflow.utils.environment import _mlflow_conda_env
 from tests.helper_functions import score_model_in_sagemaker_docker_container
+
 from tests.pyfunc.test_spark import score_model_as_udf
 
 
 def test_hadoop_filesystem(tmpdir):
     # copy local dir to and back from HadoopFS and make sure the results match
-    from mlflow.sparkml import _HadoopFileSystem as FS
+    from mlflow.spark import _HadoopFileSystem as FS
     test_dir_0 = os.path.join(str(tmpdir), "expected")
     test_file_0 = os.path.join(test_dir_0, "root", "file_0")
     test_dir_1 = os.path.join(test_dir_0, "root", "subdir")
@@ -60,19 +57,10 @@ def test_hadoop_filesystem(tmpdir):
     assert not os.path.exists(FS._remote_path(remote).toString())  # skip file: prefix
 
 
-@pytest.fixture
-def spark_conda_env(tmpdir):
+@pytest.mark.large
+def test_model_export(tmpdir):
     conda_env = os.path.join(str(tmpdir), "conda_env.yml")
     _mlflow_conda_env(conda_env, additional_pip_deps=["pyspark=={}".format(pyspark_version)])
-    return conda_env
-
-
-SparkModelWithData = namedtuple("SparkModelWithData",
-                                ["model", "training_df", "inference_df"])
-
-
-@pytest.fixture
-def spark_model_iris():
     iris = datasets.load_iris()
     X = iris.data  # we only take the first two features.
     y = iris.target
@@ -81,49 +69,52 @@ def spark_model_iris():
     spark_session = pyspark.sql.SparkSession.builder \
         .config(key="spark_session.python.worker.reuse", value=True) \
         .master("local-cluster[2, 1, 1024]") \
-        .config(key='spark.jars.packages',
-                value='ml.combust.mleap:mleap-spark-base_2.11:0.10.0,'
-                      'ml.combust.mleap:mleap-spark_2.11:0.10.0') \
         .getOrCreate()
     spark_df = spark_session.createDataFrame(pandas_df)
+    model_path = tmpdir.mkdir("model")
     assembler = VectorAssembler(inputCols=iris.feature_names, outputCol="features")
     lr = LogisticRegression(maxIter=50, regParam=0.1, elasticNetParam=0.8)
     pipeline = Pipeline(stages=[assembler, lr])
     # Fit the model
     model = pipeline.fit(spark_df)
-    return SparkModelWithData(model=model, training_df=spark_df, inference_df=pandas_df)
-
-
-@pytest.fixture
-def model_path(tmpdir):
-    return str(tmpdir.mkdir("model"))
-
-
-@pytest.mark.large
-def test_model_export(spark_model_iris, model_path, spark_conda_env):
-    preds_df = spark_model_iris.model.transform(spark_model_iris.training_df)
+    # Print the coefficients and intercept for multinomial logistic regression
+    preds_df = model.transform(spark_df)
     preds1 = [x.prediction for x in preds_df.select("prediction").collect()]
-    sparkm.save_model(spark_model_iris.model, path=model_path,
-                      conda_env=spark_conda_env)
-    reloaded_model = sparkm.load_model(path=model_path)
-    preds_df_1 = reloaded_model.transform(spark_model_iris.training_df)
+    sparkm.save_model(model, path=str(model_path), conda_env=conda_env)
+    reloaded_model = sparkm.load_model(path=str(model_path))
+    preds_df_1 = reloaded_model.transform(spark_df)
     preds1_1 = [x.prediction for x in preds_df_1.select("prediction").collect()]
     assert preds1 == preds1_1
-    m = pyfunc.load_pyfunc(model_path)
-    preds2 = m.predict(spark_model_iris.inference_df)
+    m = pyfunc.load_pyfunc(str(model_path))
+    preds2 = m.predict(pandas_df)
     assert preds1 == preds2
-    preds3 = score_model_in_sagemaker_docker_container(model_path=model_path,
-                                                       data=spark_model_iris.inference_df)
+    preds3 = score_model_in_sagemaker_docker_container(model_path=str(model_path), data=pandas_df)
     assert preds1 == preds3
-    assert os.path.exists(SPARKML_DFS_TMP)
-    print(os.listdir(SPARKML_DFS_TMP))
-    assert not os.listdir(SPARKML_DFS_TMP)
+    assert os.path.exists(sparkm.DFS_TMP)
+    print(os.listdir(sparkm.DFS_TMP))
+    assert not os.listdir(sparkm.DFS_TMP)
 
 
 @pytest.mark.large
-def test_model_log(tmpdir, spark_model_iris):
+def test_model_log(tmpdir):
+    conda_env = os.path.join(str(tmpdir), "conda_env.yml")
+    _mlflow_conda_env(conda_env, additional_pip_deps=["pyspark=={}".format(pyspark_version)])
+    iris = datasets.load_iris()
+    feature_names = ["0", "1", "2", "3"]
+    pandas_df = pd.DataFrame(iris.data, columns=feature_names)  # to make spark_udf work
+    pandas_df['label'] = pd.Series(iris.target)
+    spark_session = pyspark.sql.SparkSession.builder \
+        .config(key="spark_session.python.worker.reuse", value=True) \
+        .master("local-cluster[2, 1, 1024]") \
+        .getOrCreate()
+    spark_df = spark_session.createDataFrame(pandas_df)
+    assembler = VectorAssembler(inputCols=feature_names, outputCol="features")
+    lr = LogisticRegression(maxIter=50, regParam=0.1, elasticNetParam=0.8)
+    pipeline = Pipeline(stages=[assembler, lr])
+    # Fit the model
+    model = pipeline.fit(spark_df)
     # Print the coefficients and intercept for multinomial logistic regression
-    preds_df = spark_model_iris.model.transform(spark_model_iris.training_df)
+    preds_df = model.transform(spark_df)
     preds1 = [x.prediction for x in preds_df.select("prediction").collect()]
     old_tracking_uri = tracking.get_tracking_uri()
     cnt = 0
@@ -139,27 +130,25 @@ def test_model_log(tmpdir, spark_model_iris):
                 artifact_path = "model%d" % cnt
                 cnt += 1
                 if dfs_tmp_dir:
-                    sparkm.log_model(artifact_path=artifact_path,
-                                     spark_model=spark_model_iris.model,
+                    sparkm.log_model(artifact_path=artifact_path, spark_model=model,
                                      dfs_tmpdir=dfs_tmp_dir)
                 else:
-                    sparkm.log_model(artifact_path=artifact_path,
-                                     spark_model=spark_model_iris.model)
+                    sparkm.log_model(artifact_path=artifact_path, spark_model=model)
                 run_id = tracking.active_run().info.run_uuid
                 # test pyfunc
                 x = pyfunc.load_pyfunc(artifact_path, run_id=run_id)
-                preds2 = x.predict(spark_model_iris.inference_df)
+                preds2 = x.predict(pandas_df)
                 assert preds1 == preds2
                 # test load model
                 reloaded_model = sparkm.load_model(artifact_path, run_id=run_id)
-                preds_df_1 = reloaded_model.transform(spark_model_iris.training_df)
+                preds_df_1 = reloaded_model.transform(spark_df)
                 preds3 = [x.prediction for x in preds_df_1.select("prediction").collect()]
                 assert preds1 == preds3
-                # test spark_udf
-                preds4 = score_model_as_udf(artifact_path, run_id, spark_model_iris.inference_df)
+                # test spar_udf
+                preds4 = score_model_as_udf(artifact_path, run_id, pandas_df)
                 assert preds1 == preds4
                 # make sure we did not leave any temp files behind
-                x = dfs_tmp_dir or SPARKML_DFS_TMP
+                x = dfs_tmp_dir or sparkm.DFS_TMP
                 assert os.path.exists(x)
                 assert not os.listdir(x)
                 shutil.rmtree(x)
@@ -167,67 +156,3 @@ def test_model_log(tmpdir, spark_model_iris):
                 tracking.end_run()
                 tracking.set_tracking_uri(old_tracking_uri)
                 shutil.rmtree(tracking_dir)
-
-
-@pytest.mark.large
-def test_container_scoring_with_sparkml_and_mleap_outputs_same_format(
-        spark_model_iris, model_path, spark_conda_env):
-    sparkml_model = Model()
-    sparkm.save_model(spark_model_iris.model, path=model_path,
-                      conda_env=spark_conda_env, mlflow_model=sparkml_model)
-    assert SPARKML_FLAVOR_NAME in sparkml_model.flavors
-    assert mleap.FLAVOR_NAME not in sparkml_model.flavors
-    sparkml_preds = score_model_in_sagemaker_docker_container(model_path=model_path,
-                                                              data=spark_model_iris.inference_df)
-
-    shutil.rmtree(model_path)
-    assert not os.path.exists(model_path)
-    os.makedirs(model_path)
-    assert os.path.exists(model_path)
-
-    mleap_model = Model()
-    sparkm.save_model(spark_model_iris.model, path=model_path,
-                      sample_input=spark_model_iris.training_df,
-                      mlflow_model=mleap_model)
-    assert mleap.FLAVOR_NAME in mleap_model.flavors
-    mleap_preds = score_model_in_sagemaker_docker_container(model_path=model_path,
-                                                            data=spark_model_iris.inference_df)
-
-    assert type(mleap_preds) == type(sparkml_preds) == list
-    assert len(mleap_preds) == len(sparkml_preds)
-    assert not any([type(mleap_preds[i]) != type(sparkml_preds[i]) 
-        for i in range(len(mleap_preds))])
-
-
-def test_container_scoring_handles_bad_inputs_with_mleap_flavor(spark_model_iris, model_path):
-    mleap_model = Model()
-    sparkm.save_model(spark_model_iris.model, path=model_path,
-                      sample_input=spark_model_iris.training_df,
-                      mlflow_model=mleap_model)
-    assert mleap.FLAVOR_NAME in mleap_model.flavors
-    mleap_response = score_model_in_sagemaker_docker_container(model_path=model_path,
-                                                               data="invalid")
-    assert "Error" in mleap_response.keys()
-    print(mleap_response["Error"])
-
-
-def test_model_save_without_sample_output_produces_sparkml_flavor(spark_model_iris, model_path):
-    sparkm.save_model(spark_model=spark_model_iris.model,
-                      path=model_path,
-                      sample_input=None)
-    config_path = os.path.join(model_path, "MLmodel")
-    assert os.path.exists(config_path)
-    config = Model.load(config_path)
-    assert SPARKML_FLAVOR_NAME in config.flavors
-
-
-def test_model_save_with_sample_output_produces_sparkml_and_mleap_flavors(spark_model_iris,
-        model_path):
-    sparkm.save_model(spark_model=spark_model_iris.model,
-                      path=model_path,
-                      sample_input=spark_model_iris.training_df)
-    config_path = os.path.join(model_path, "MLmodel")
-    assert os.path.exists(config_path)
-    config = Model.load(config_path)
-    assert SPARKML_FLAVOR_NAME in config.flavors
-    assert mleap.FLAVOR_NAME in config.flavors
