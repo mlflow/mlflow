f8805e2251503ef711f2439ed86fef68e14e8a3c
Corey Zumar <corey.zumar@databricks.com>
2018-08-15 16:55:41 -0700

    Serialization utils docs

--- java/src/main/java/com/databricks/mlflow/utils/SerializationUtils.java
+++ java/src/main/java/com/databricks/mlflow/utils/SerializationUtils.java
@@ -9,18 +9,35 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
+/**
+ * Utilities for serializing and deserializing objects to and from various
+ * persistence formats, such as JSON and YAML
+ */
 public class SerializationUtils {
     private static final ObjectMapper jsonMapper = new ObjectMapper(new JsonFactory());
     private static final ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
 
+    /**
+     * Produces a JSON string representation of a Java object
+     *
+     * @return A string in valid JSON format
+     */
     public static String toJson(Object object) throws JsonProcessingException {
         return jsonMapper.writeValueAsString(object);
     }
 
+    /**
+     * Produces a Java object representation of a JSON-formatted string
+     *
+     * @param json A string in valid JSON format
+     */
     public static <T> T fromJson(String json) throws IOException {
         return jsonMapper.readValue(json, new TypeReference<T>() {});
     }
 
+    /**
+     * Produces a Java object representation of a JSON-formatted file
+     */
     public static <T> T parseJsonFromFile(String filePath, Class<T> objectClass)
         throws IOException {
         File jsonFile = new File(filePath);
