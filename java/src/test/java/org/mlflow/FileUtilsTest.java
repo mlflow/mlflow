92821483fa14795f472106351e3b3d3304b7ad45
Corey Zumar <corey.zumar@databricks.com>
2018-08-16 11:38:08 -0700

    Remove trailing  from test case names

---
+++ java/src/test/java/org/mlflow/FileUtilsTest.java
@@ -0,0 +1,25 @@
+package org.mlflow.utils;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class FileUtilsTest {
+    @Test
+    public void testFilePathsAreConcatenatedCorrectly() {
+        String outputPathRelative = "this/is/an/mlflow/test/path";
+
+        String joinedPath1 = FileUtils.join("this", "is/", "an", "mlflow/", "test", "path/");
+        String joinedPath2 = FileUtils.join("this/", "is", "an", "mlflow", "test/", "path");
+
+        Assert.assertEquals(joinedPath1, outputPathRelative);
+        Assert.assertEquals(joinedPath2, outputPathRelative);
+
+        String outputPathAbsolute = "/this/is/an/mlflow/test/path";
+
+        String joinedPath3 = FileUtils.join("/this", "is/", "an", "mlflow/", "test", "path/");
+        String joinedPath4 = FileUtils.join("/this/", "is", "an", "mlflow", "test/", "path");
+
+        Assert.assertEquals(joinedPath3, outputPathAbsolute);
+        Assert.assertEquals(joinedPath4, outputPathAbsolute);
+    }
+}
---
+++ java/src/test/java/org/mlflow/SerializationUtilsTest.java
@@ -0,0 +1,98 @@
+package org.mlflow.utils;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.io.IOException;
+import java.io.File;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+/**
+ * Unit tests for the {@link SerializationUtils} module
+ */
+public class SerializationUtilsTest {
+    @Test
+    public void testSupportedJavaObjectsCanBeJsonSerializedAndDeserializedInTheSameFormat() {
+        List<Integer> inputList = new ArrayList<>();
+        for (int i = 0; i < 100; ++i) {
+            inputList.add(i);
+        }
+        try {
+            String listJson = SerializationUtils.toJson(inputList);
+            List<Integer> loadedList = SerializationUtils.fromJson(listJson);
+            Assert.assertEquals(loadedList, inputList);
+        } catch (IOException e) {
+            Assert.fail(
+                "Encountered an exception while serializing and deserializing list JSON content!");
+        }
+
+        Map<String, Integer> inputMap = new HashMap<>();
+        for (int i = 0; i < 100; ++i) {
+            inputMap.put(String.valueOf(i), i);
+        }
+
+        try {
+            String mapJson = SerializationUtils.toJson(inputMap);
+            Map<String, Integer> loadedMap = SerializationUtils.fromJson(mapJson);
+            Assert.assertEquals(loadedMap, inputMap);
+        } catch (IOException e) {
+            Assert.fail(
+                "Encountered an exception while serializing and deserializing map JSON content!");
+        }
+    }
+
+    @Test
+    public void testJsonObjectsAreParsedFromFilesAndFilePathsCorrectly() throws IOException {
+        Map<String, Object> inputMap = new HashMap<>();
+        inputMap.put("field1", 1);
+        inputMap.put("field2", "two");
+        inputMap.put("field3", 3.0);
+
+        File tempFile = File.createTempFile("json", "filetest", new File("/tmp"));
+        String tempFilePath = tempFile.getAbsolutePath();
+        tempFile.deleteOnExit();
+
+        ObjectMapper jsonMapper = new ObjectMapper(new JsonFactory());
+        jsonMapper.writeValue(tempFile, inputMap);
+
+        Map<String, Object> loadedFileMap =
+            SerializationUtils.parseJsonFromFile(tempFile, Map.class);
+        Assert.assertEquals(inputMap, loadedFileMap);
+
+        Map<String, Object> loadedPathMap =
+            SerializationUtils.parseJsonFromFile(tempFilePath, Map.class);
+        Assert.assertEquals(inputMap, loadedPathMap);
+    }
+
+    @Test
+    public void testYamlObjectsAreParsedFromFilesAndFilePathsCorrectly() throws IOException {
+        Map<String, Object> inputMap = new HashMap<>();
+        inputMap.put("field1", 1);
+        inputMap.put("field2", "two");
+        inputMap.put("field3", 3.0);
+
+        File tempFile = File.createTempFile("yaml", "filetest", new File("/tmp"));
+        String tempFilePath = tempFile.getAbsolutePath();
+        tempFile.deleteOnExit();
+
+        ObjectMapper jsonMapper = new ObjectMapper(new YAMLFactory());
+        jsonMapper.writeValue(tempFile, inputMap);
+
+        Map<String, Object> loadedFileMap =
+            SerializationUtils.parseYamlFromFile(tempFile, Map.class);
+        Assert.assertEquals(inputMap, loadedFileMap);
+
+        Map<String, Object> loadedPathMap =
+            SerializationUtils.parseYamlFromFile(tempFilePath, Map.class);
+        Assert.assertEquals(inputMap, loadedPathMap);
+    }
+}
