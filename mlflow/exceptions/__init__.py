94fe000a47a0c8070611e658003188560d83366f
Sid Murching <sid.murching@databricks.com>
2018-08-16 11:37:12 -0700

    Also expose MLflowException

--- mlflow/exceptions/__init__.py
+++ mlflow/exceptions/__init__.py
@@ -5,8 +5,9 @@ from mlflow.data import DownloadException
 from mlflow.store.rest_store import RestException
 from mlflow.utils.process import ShellCommandException
 from mlflow.store.dbfs_artifact_repo import IllegalArtifactPathError
+from mlflow.base_exception import MLflowException
 
 
 __all__ = [
     "ExecutionException", "DownloadException", "RestException", "ShellCommandException",
-    "IllegalArtifactPathError"]
+    "IllegalArtifactPathError", "MLflowException"]
