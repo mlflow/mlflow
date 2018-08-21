2a773e46d0ed511cd1e7ee92240baf28a02cea1b
kevinykuo <kevinykuo@gmail.com>
2018-08-21 05:53:26 -0400

    import _get_model_log_dir from mlflow.tracking.utils

--- mlflow/rfunc/cli.py
+++ mlflow/rfunc/cli.py
@@ -5,7 +5,7 @@ import os
 import subprocess
 import sys
 
-from mlflow.tracking import _get_model_log_dir
+from mlflow.tracking.utils import _get_model_log_dir
 from mlflow.utils import cli_args
 from mlflow.utils.logging_utils import eprint
 
