5108f0b71fd3b0e881d1ff8ae32bff28d5a523e3
Corey Zumar <corey.zumar@databricks.com>
2018-08-21 13:11:00 -0700

    reorder tests

--- tests/spark/test_spark_model_export.py
+++ tests/spark/test_spark_model_export.py
@@ -23,48 +23,6 @@ from tests.helper_functions import score_model_in_sagemaker_docker_container
 
 from tests.pyfunc.test_spark import score_model_as_udf
 
-# Set Py4J logging level to `ERROR` to prune test log outputs
-import logging
-logging.getLogger("py4j").setLevel(logging.ERROR)
-
-
-def test_hadoop_filesystem(tmpdir):
-    # copy local dir to and back from HadoopFS and make sure the results match
-    from mlflow.spark import _HadoopFileSystem as FS
-    test_dir_0 = os.path.join(str(tmpdir), "expected")
-    test_file_0 = os.path.join(test_dir_0, "root", "file_0")
-    test_dir_1 = os.path.join(test_dir_0, "root", "subdir")
-    test_file_1 = os.path.join(test_dir_1, "file_1")
-    os.makedirs(os.path.dirname(test_file_0))
-    with open(test_file_0, "w") as f:
-        f.write("test0")
-    os.makedirs(os.path.dirname(test_file_1))
-    with open(test_file_1, "w") as f:
-        f.write("test1")
-    remote = "/tmp/mlflow/test0"
-    FS.copy_from_local_file(test_dir_0, remote, removeSrc=False)
-    local = os.path.join(str(tmpdir), "actual")
-    FS.copy_to_local_file(remote, local, removeSrc=True)
-    assert sorted(os.listdir(os.path.join(local, "root"))) == sorted([
-        "subdir", "file_0", ".file_0.crc"])
-    assert sorted(os.listdir(os.path.join(local, "root", "subdir"))) == sorted([
-        "file_1", ".file_1.crc"])
-    # compare the files
-    with open(os.path.join(test_dir_0, "root", "file_0")) as expected_f:
-        with open(os.path.join(local, "root", "file_0")) as actual_f:
-            assert expected_f.read() == actual_f.read()
-    with open(os.path.join(test_dir_0, "root", "subdir", "file_1")) as expected_f:
-        with open(os.path.join(local, "root", "subdir", "file_1")) as actual_f:
-            assert expected_f.read() == actual_f.read()
-
-    # make sure we cleanup
-    assert not os.path.exists(FS._remote_path(remote).toString())  # skip file: prefix
-    FS.copy_from_local_file(test_dir_0, remote, removeSrc=False)
-    assert os.path.exists(FS._remote_path(remote).toString())  # skip file: prefix
-    FS.delete(remote)
-    assert not os.path.exists(FS._remote_path(remote).toString())  # skip file: prefix
-
-
 @pytest.fixture
 def spark_conda_env(tmpdir):
     conda_env = os.path.join(str(tmpdir), "conda_env.yml")
@@ -81,7 +39,8 @@ def spark_model_iris():
     iris = datasets.load_iris()
     X = iris.data  # we only take the first two features.
     y = iris.target
-    pandas_df = pd.DataFrame(X, columns=iris.feature_names)
+    feature_names = ["0", "1", "2", "3"]
+    pandas_df = pd.DataFrame(X, columns=feature_names) # to make spark_udf work
     pandas_df['label'] = pd.Series(y)
     spark_session = pyspark.sql.SparkSession.builder \
         .config(key="spark_session.python.worker.reuse", value=True) \
@@ -91,7 +50,7 @@ def spark_model_iris():
         .master("local-cluster[2, 1, 1024]") \
         .getOrCreate()
     spark_df = spark_session.createDataFrame(pandas_df)
-    assembler = VectorAssembler(inputCols=iris.feature_names, outputCol="features")
+    assembler = VectorAssembler(inputCols=feature_names, outputCol="features")
     lr = LogisticRegression(maxIter=50, regParam=0.1, elasticNetParam=0.8)
     pipeline = Pipeline(stages=[assembler, lr])
     # Fit the model
@@ -268,3 +227,40 @@ def test_mleap_module_model_save_with_unsupported_transformer_raises_exception(
         mleap.save_model(spark_model=unsupported_model,
                          path=model_path,
                          sample_input=spark_model_iris.training_df)
+
+
+def test_hadoop_filesystem(tmpdir):
+    # copy local dir to and back from HadoopFS and make sure the results match
+    from mlflow.spark import _HadoopFileSystem as FS
+    test_dir_0 = os.path.join(str(tmpdir), "expected")
+    test_file_0 = os.path.join(test_dir_0, "root", "file_0")
+    test_dir_1 = os.path.join(test_dir_0, "root", "subdir")
+    test_file_1 = os.path.join(test_dir_1, "file_1")
+    os.makedirs(os.path.dirname(test_file_0))
+    with open(test_file_0, "w") as f:
+        f.write("test0")
+    os.makedirs(os.path.dirname(test_file_1))
+    with open(test_file_1, "w") as f:
+        f.write("test1")
+    remote = "/tmp/mlflow/test0"
+    FS.copy_from_local_file(test_dir_0, remote, removeSrc=False)
+    local = os.path.join(str(tmpdir), "actual")
+    FS.copy_to_local_file(remote, local, removeSrc=True)
+    assert sorted(os.listdir(os.path.join(local, "root"))) == sorted([
+        "subdir", "file_0", ".file_0.crc"])
+    assert sorted(os.listdir(os.path.join(local, "root", "subdir"))) == sorted([
+        "file_1", ".file_1.crc"])
+    # compare the files
+    with open(os.path.join(test_dir_0, "root", "file_0")) as expected_f:
+        with open(os.path.join(local, "root", "file_0")) as actual_f:
+            assert expected_f.read() == actual_f.read()
+    with open(os.path.join(test_dir_0, "root", "subdir", "file_1")) as expected_f:
+        with open(os.path.join(local, "root", "subdir", "file_1")) as actual_f:
+            assert expected_f.read() == actual_f.read()
+
+    # make sure we cleanup
+    assert not os.path.exists(FS._remote_path(remote).toString())  # skip file: prefix
+    FS.copy_from_local_file(test_dir_0, remote, removeSrc=False)
+    assert os.path.exists(FS._remote_path(remote).toString())  # skip file: prefix
+    FS.delete(remote)
+    assert not os.path.exists(FS._remote_path(remote).toString())  # skip file: prefix
