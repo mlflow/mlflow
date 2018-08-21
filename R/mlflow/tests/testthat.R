b71433a57784e3d9d4724645f36743dd71123364
Javier <javierluraschi@hotmail.com>
2018-08-21 12:23:21 -0700

    install mlflow for r tests and latest sources

--- R/mlflow/tests/testthat.R
+++ R/mlflow/tests/testthat.R
@@ -2,5 +2,8 @@ library(testthat)
 library(mlflow)
 
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
+  mlflow_install()
+  reticulate::conda_install("r-mlflow", "../../../.", pip = TRUE)
+
   test_check("mlflow")
 }
