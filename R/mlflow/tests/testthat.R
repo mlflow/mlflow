10cc7c6e288f39e7cbaded0566edca7af4574cb3
Javier Luraschi <javierluraschi@hotmail.com>
2018-08-22 13:41:14 -0700

    use mlflow_home in tests if set

--- R/mlflow/tests/testthat.R
+++ R/mlflow/tests/testthat.R
@@ -3,7 +3,9 @@ library(mlflow)
 
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   mlflow_install()
-  reticulate::conda_install("r-mlflow", "../../../.", pip = TRUE)
+
+  mlflow_home <- Sys.getenv("MLFLOW_HOME", "../../../.")
+  reticulate::conda_install("r-mlflow", mlflow_home, pip = TRUE)
 
   test_check("mlflow")
 }
