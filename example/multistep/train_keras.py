2f48f330575f5744ba92f72b3ed13dc44d9b50c6
Aaron Davidson <aaron@databricks.com>
2018-08-24 16:02:38 -0700

    Save model

--- example/multistep/train_keras.py
+++ example/multistep/train_keras.py
@@ -7,6 +7,7 @@ import click
 import tempfile
 
 import mlflow
+import mlflow.keras
 import mlflow.spark
 import time
 
@@ -98,6 +99,7 @@ def train_keras(ratings_data, als_model_uri, hidden_units):
     mlflow.log_metric("train_mse", train_mse)
 
     print('The model had a MSE on the test set of {0}'.format(test_mse))
+    mlflow.keras.save_model(model, "keras-model")
 
 
 if __name__ == '__main__':
