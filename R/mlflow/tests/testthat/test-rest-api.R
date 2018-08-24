621693a25a80def8237ba06b5fc5f982eb374b77
kevinykuo <kevinykuo@gmail.com>
2018-08-24 10:47:46 -0400

    add test for mlflow_list_experiments()

--- R/mlflow/tests/testthat/test-rest-api.R
+++ R/mlflow/tests/testthat/test-rest-api.R
@@ -20,3 +20,14 @@ test_that("mlflow_create_experiment() checks for existing experiment with same n
     "Experiment with name \"foo2\" already exists\\."
   )
 })
+
+test_that("mlflow_list_experiments() works properly", {
+  cat(getwd())
+  mlflow_clear_test_dir("mlruns")
+  mlflow_create_experiment("foo1", "art_loc1")
+  mlflow_create_experiment("foo2", "art_loc2")
+  experiments_list <- mlflow_list_experiments()
+  expect_identical(experiments_list$experiment_id, c("0", "1", "2"))
+  expect_identical(experiments_list$name, c("Default", "foo1", "foo2"))
+  expect_identical(experiments_list$artifact_location, c("mlruns/0", "art_loc1", "art_loc2"))
+})
