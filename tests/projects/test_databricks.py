f2fb93dec7e45535b9e0d105af57100d25385b24
Sid Murching <sid.murching@databricks.com>
2018-08-27 16:13:07 -0700

    Refactor databricks run implementation. DatabricksJobRunner now contains logic needed to launch a run, SubmittedRun contains logic needed to monitor a run

--- tests/projects/test_databricks.py
+++ tests/projects/test_databricks.py
@@ -7,18 +7,20 @@ import shutil
 import pytest
 
 import mlflow
+from mlflow.projects.databricks import DatabricksJobRunner
 from mlflow.entities import RunStatus
 from mlflow.projects import databricks, ExecutionException
 from mlflow.utils import file_utils
 
-from tests.projects.utils import validate_exit_status, TEST_PROJECT_DIR, assert_dirs_equal
+from tests.projects.utils import validate_exit_status, TEST_PROJECT_DIR
 from tests.projects.utils import tracking_uri_mock  # pylint: disable=unused-import
 
 
 @pytest.fixture()
 def runs_cancel_mock():
     """Mocks the Jobs Runs Cancel API request"""
-    with mock.patch("mlflow.projects.databricks._jobs_runs_cancel") as runs_cancel_mock:
+    with mock.patch("mlflow.projects.databricks.DatabricksSubmittedRun._jobs_runs_cancel")\
+            as runs_cancel_mock:
         runs_cancel_mock.return_value = None
         yield runs_cancel_mock
 
@@ -26,7 +28,8 @@ def runs_cancel_mock():
 @pytest.fixture()
 def runs_submit_mock():
     """Mocks the Jobs Runs Submit API request"""
-    with mock.patch("mlflow.projects.databricks._jobs_runs_submit") as runs_submit_mock:
+    with mock.patch("mlflow.projects.databricks.DatabricksJobRunner._jobs_runs_submit")\
+            as runs_submit_mock:
         runs_submit_mock.return_value = {"run_id": "-1"}
         yield runs_submit_mock
 
@@ -34,7 +37,8 @@ def runs_submit_mock():
 @pytest.fixture()
 def runs_get_mock():
     """Mocks the Jobs Runs Get API request"""
-    with mock.patch("mlflow.projects.databricks._jobs_runs_get") as runs_get_mock:
+    with mock.patch("mlflow.projects.databricks.DatabricksSubmittedRun._jobs_runs_get")\
+            as runs_get_mock:
         yield runs_get_mock
 
 
@@ -52,20 +56,21 @@ def dbfs_root_mock(tmpdir):
 
 @pytest.fixture()
 def upload_to_dbfs_mock(dbfs_root_mock):
-    def upload_mock_fn(src_path, dbfs_uri, _):
+    def upload_mock_fn(_, src_path, dbfs_uri):
         mock_dbfs_dst = os.path.join(dbfs_root_mock, dbfs_uri.split("/dbfs/")[1])
         os.makedirs(os.path.dirname(mock_dbfs_dst))
         shutil.copy(src_path, mock_dbfs_dst)
 
     with mock.patch.object(
-            mlflow.projects.databricks, "_upload_to_dbfs",
+            mlflow.projects.databricks.DatabricksJobRunner, "_upload_to_dbfs",
             new=upload_mock_fn) as upload_mock:
         yield upload_mock
 
 
 @pytest.fixture()
 def dbfs_path_exists_mock(dbfs_root_mock):  # pylint: disable=unused-argument
-    with mock.patch("mlflow.projects.databricks._dbfs_path_exists") as path_exists_mock:
+    with mock.patch("mlflow.projects.databricks.DatabricksJobRunner._dbfs_path_exists")\
+            as path_exists_mock:
         yield path_exists_mock
 
 
@@ -76,7 +81,7 @@ def dbfs_mocks(dbfs_path_exists_mock, upload_to_dbfs_mock):  # pylint: disable=u
 
 @pytest.fixture()
 def before_run_validations_mock():  # pylint: disable=unused-argument
-    with mock.patch("mlflow.projects.databricks._before_run_validations"):
+    with mock.patch("mlflow.projects.databricks.DatabricksJobRunner._before_run_validations"):
         yield
 
 
@@ -106,8 +111,9 @@ def test_upload_project_to_dbfs(
         upload_to_dbfs_mock):  # pylint: disable=unused-argument
     # Upload project to a mock directory
     dbfs_path_exists_mock.return_value = False
-    dbfs_uri = databricks._upload_project_to_dbfs(
-        project_dir=TEST_PROJECT_DIR, experiment_id=0, profile="DEFAULT")
+    runner = DatabricksJobRunner(profile="DEFAULT")
+    dbfs_uri = runner._upload_project_to_dbfs(
+        project_dir=TEST_PROJECT_DIR, experiment_id=0)
     # Get expected tar
     local_tar_path = os.path.join(dbfs_root_mock, dbfs_uri.split("/dbfs/")[1])
     expected_tar_path = str(tmpdir.join("expected.tar.gz"))
@@ -120,10 +126,12 @@ def test_upload_project_to_dbfs(
 
 def test_upload_existing_project_to_dbfs(dbfs_path_exists_mock):  # pylint: disable=unused-argument
     # Check that we don't upload the project if it already exists on DBFS
-    with mock.patch("mlflow.projects.databricks._upload_to_dbfs") as upload_to_dbfs_mock:
+    with mock.patch("mlflow.projects.databricks.DatabricksJobRunner._upload_to_dbfs")\
+            as upload_to_dbfs_mock:
         dbfs_path_exists_mock.return_value = True
-        databricks._upload_project_to_dbfs(
-            project_dir=TEST_PROJECT_DIR, experiment_id=0, profile="DEFAULT")
+        runner = DatabricksJobRunner(profile="DEFAULT")
+        runner._upload_project_to_dbfs(
+            project_dir=TEST_PROJECT_DIR, experiment_id=0)
         assert upload_to_dbfs_mock.call_count == 0
 
 
@@ -133,8 +141,8 @@ def test_run_databricks_validations(
     """
     Tests that running on Databricks fails before making any API requests if validations fail.
     """
-    with mock.patch("mlflow.utils.rest_utils.databricks_api_request") as db_api_req_mock,\
-            mock.patch("mlflow.projects.databricks._check_databricks_auth_available"):
+    with mock.patch("mlflow.projects.databricks.DatabricksJobRunner._check_auth_available"),\
+        mock.patch("mlflow.utils.rest_utils.databricks_api_request") as db_api_req_mock:
         # Test bad tracking URI
         tracking_uri_mock.return_value = tmpdir.strpath
         with pytest.raises(ExecutionException):
@@ -155,8 +163,9 @@ def test_run_databricks_validations(
         assert db_api_req_mock.call_count == 0
         db_api_req_mock.reset_mock()
         # Test that validations pass with good tracking URIs
-        databricks._before_run_validations("http://", cluster_spec_mock, profile="DEFAULT")
-        databricks._before_run_validations("databricks", cluster_spec_mock, profile="DEFAULT")
+        runner = DatabricksJobRunner(profile="DEFAULT")
+        runner._before_run_validations("http://", cluster_spec_mock)
+        runner._before_run_validations("databricks", cluster_spec_mock)
 
 
 def test_run_databricks(
@@ -191,17 +200,3 @@ def test_run_databricks_cancel(
     runs_get_mock.return_value = mock_runs_get_result(succeeded=False)
     with pytest.raises(mlflow.projects.ExecutionException):
         run_databricks_project(cluster_spec_mock, block=True)
-
-
-def test_fetch_and_clean_project(tmpdir):
-    project_with_mlruns = tmpdir.mkdir("with-mlruns")
-    project_with_mlruns.mkdir("mlruns").join("some-file").write("hi")
-    project_without_mlruns = tmpdir.mkdir("without-mlruns")
-    for proj in [project_with_mlruns, project_without_mlruns]:
-        proj.join("MLproject").write("Hello")
-    fetched0 = databricks._fetch_and_clean_project(project_with_mlruns.strpath)
-    fetched1 = databricks._fetch_and_clean_project(project_without_mlruns.strpath)
-    assert_dirs_equal(fetched0, fetched1)
-    for fetched_dir in [fetched0, fetched1]:
-        with open(os.path.join(fetched_dir, "MLproject")) as handle:
-            assert handle.read() == "Hello"
