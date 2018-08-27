a6b8654a053b5a45ee39108cbf32f26787e259dd
Aaron Davidson <aaron@databricks.com>
2018-08-27 15:36:57 -0700

    Undo change partially

--- mlflow/projects/databricks.py
+++ mlflow/projects/databricks.py
@@ -188,7 +188,10 @@ def _run_shell_command_job(project_uri, command, env_vars, cluster_spec):
             'command': command,
             "env_vars": env_vars
         },
-        "libraries": [{"pypi": {"package": mlflow_package}}]
+        # NB: We use <= on the version specifier to allow running projects on pre-release
+        # versions, where we will select the most up-to-date mlflow version available.
+        # Also note, that we escape this so '<' is not treated as a shell pipe.
+        "libraries": [{"pypi": {"package": "'mlflow<=%s'" % VERSION}}]
     }
     run_submit_res = _jobs_runs_submit(req_body_json)
     databricks_run_id = run_submit_res["run_id"]
