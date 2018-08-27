7e644c4af3af66f24734e5564c5f8f426b281516
Aaron Davidson <aaron@databricks.com>
2018-08-27 16:26:53 -0700

    Fix escaping issue with "mlflow<=VERSION" installation in remote runs (#387)

--- mlflow/projects/databricks.py
+++ mlflow/projects/databricks.py
@@ -187,7 +187,8 @@ def _run_shell_command_job(project_uri, command, env_vars, cluster_spec):
         },
         # NB: We use <= on the version specifier to allow running projects on pre-release
         # versions, where we will select the most up-to-date mlflow version available.
-        "libraries": [{"pypi": {"package": "mlflow<=%s" % VERSION}}]
+        # Also note, that we escape this so '<' is not treated as a shell pipe.
+        "libraries": [{"pypi": {"package": "'mlflow<=%s'" % VERSION}}]
     }
     run_submit_res = _jobs_runs_submit(req_body_json)
     databricks_run_id = run_submit_res["run_id"]
