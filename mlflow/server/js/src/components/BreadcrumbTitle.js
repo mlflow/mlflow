a090efc38edc05950e21dbeabdb4210c41ce357c
Sid Murching <sid.murching@databricks.com>
2018-08-27 18:45:16 -0700

    WIP

--- mlflow/server/js/src/components/BreadcrumbTitle.js
+++ mlflow/server/js/src/components/BreadcrumbTitle.js
@@ -4,17 +4,23 @@ import { Experiment } from "../sdk/MlflowMessages";
 import { Link } from 'react-router-dom';
 import Routes from "../Routes";
 import Utils from '../utils/Utils';
+import DropdownMenuView from './DropdownMenuView';
 
 /**
  * A title component that creates a <h1> with breadcrumbs pointing to an experiment and optionally
  * a run or a run comparison page.
  */
+
+const DROPDOWN_MENU = 'dropdownMenu';
+
+
 export default class BreadcrumbTitle extends Component {
   constructor(props) {
     super(props);
     this.onSetRunName = this.onSetRunName.bind(this);
     debugger;
     this.onSetTag = this.props.onSetTag.bind(this);
+    this.menuShowing = false;
   }
   // title={Utils.getRunDisplayName(this.props.tags, run.getRunUuid())}
   static propTypes = {
@@ -32,6 +38,47 @@ export default class BreadcrumbTitle extends Component {
     }
   }
 
+  /**
+   * Hide the dropdown menu for a table item.
+   **/
+  hideMenu() {
+    this.setState({
+      menuShowing: null,
+      above: false,
+      dropdownHeight: 0,
+    });
+  }
+
+  renameRun() {
+    console.log("Hi! In renameRun() in BreadCrumbTitle.js");
+  }
+
+  getMenuItems() {
+    // Table specific menu options
+    const menuItems = [];
+
+    const renameOnClick = this.renameRun.bind(this);
+    menuItems.push(
+      <a
+        key='rename-item'
+        data-name='Rename'
+        className='sidebar-dropdown-link'
+        onClick={renameOnClick}
+      >Rename
+      </a>
+    );
+    return menuItems;
+  }
+
+  renderDropdown() {
+    return (<DropdownMenuView
+            ref={DROPDOWN_MENU}
+            getItems={this.getMenuItems}
+            outsideClickHandler={this.hideMenu}
+            ignoreClickClasses={['sidebar-dropdown']}
+          />)
+  }
+
   render() {
     const {experiment, runUuids, title} = this.props;
     const experimentId = experiment.getExperimentId();
@@ -47,15 +94,11 @@ export default class BreadcrumbTitle extends Component {
           <Link to={Routes.getRunPageRoute(experimentId, runUuids[0])} key="link">
             {Utils.getRunDisplayName(this.props.tags, runUuids[0])}
           </Link>
-          <div className="run-info">
-            <span className="metadata-header">Run Name: </span>
-            <div className="filter-wrapper">
-              <input type="text"
-                     placeholder={Utils.getRunName(this.props.tags)}
-                     onBlur={this.onSetRunName}
-              />
-            </div>
-          </div>
+          {this.state.menuShowing ?
+            this.renderDropdown(this.state.menuShowing.model, this.state.menuShowing.position,
+              this.state.above, this.state.dropdownHeight) :
+            null}
+
         </div>
         :
         <Link to={Routes.getCompareRunPageRoute(runUuids, experimentId)} key="link">
