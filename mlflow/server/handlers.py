81c523bb1002ef0be0fa3fb4a1ac262fb13d70a3
shenggan <csg19971016@gmail.com>
2018-08-28 17:47:40 +0800

    UI Artifact support for .log files as text

--- mlflow/server/handlers.py
+++ mlflow/server/handlers.py
@@ -67,7 +67,8 @@ def get_handler(request_class):
     return HANDLERS.get(request_class, _not_implemented)
 
 
-_TEXT_EXTENSIONS = ['txt', 'yaml', 'json', 'js', 'py', 'csv', 'md', 'rst', 'MLmodel', 'MLproject']
+_TEXT_EXTENSIONS = ['txt', 'log', 'yaml', 'json', 'js', 'py',
+                    'csv', 'md', 'rst', 'MLmodel', 'MLproject']
 
 
 def get_artifact_handler():
