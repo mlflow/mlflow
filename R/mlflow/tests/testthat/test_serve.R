4e5c8a72782e3b2ed94e505e20bfeb6b7c4e3a46
kevinykuo <kevinykuo@gmail.com>
2018-08-29 03:36:41 -0400

    fix serving test

--- R/mlflow/tests/testthat/test_serve.R
+++ R/mlflow/tests/testthat/test_serve.R
@@ -3,7 +3,7 @@ context("Serve")
 test_that("mlflow can serve a model function", {
   mlflow_clear_test_dir("model")
 
-  model <- lm(Sepal.Width ~ Sepal.Length, iris)
+  model <- lm(Sepal.Width ~ Sepal.Length + Petal.Width, iris)
 
   fn <- crate(~ stats::predict(model, .x), model)
   mlflow_save_model(fn)
@@ -20,26 +20,29 @@ test_that("mlflow can serve a model function", {
     stdout = "|",
     stderr = "|"
   )
-  on.exit(model_server$kill())
 
   Sys.sleep(5)
 
   status_code <- httr::status_code(httr::GET("http://127.0.0.1:8090"))
   expect_equal(status_code, 200)
 
+  newdata <- iris[1:2, c("Sepal.Length", "Petal.Width")]
+
   http_prediction <- httr::content(
     httr::POST(
       "http://127.0.0.1:8090/predict/",
-      body = jsonlite::toJSON(iris[1,])
+      body = jsonlite::toJSON(as.list(newdata))
     )
   )
   if (is.character(http_prediction)) {
     stop(http_prediction)
   }
 
+  model_server$kill()
+
   expect_equal(
     unlist(http_prediction),
-    as.vector(predict(model, iris[1,])),
+    as.vector(predict(model, newdata)),
     tolerance = 1e-5
   )
 })
