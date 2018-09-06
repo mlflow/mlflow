20099c4409225907b8c2e513f26a5e124e292ac2
tomasatdatabricks <33237569+tomasatdatabricks@users.noreply.github.com>
2018-09-06 15:00:36 -0700

    Added explicit tf graph and session to Keras pyfunc model. (#440)
    * Added explicit tf graph and session to Keras pyfunc model.
    
    Tf models rely on default graph and session.
    The scoring fails if the default graph changes in between loading and
    scoring the model which is the case during model serving.
    
    * Addressed linter complaints
    
    * Fixed spark tests.
    
    * Fix
    
    * Test fix.
    
    * Test fix
    
    * Adressed review comments.

--- tests/helper_functions.py
+++ tests/helper_functions.py
@@ -1,6 +1,7 @@
 import os
 import random
 
+import re
 import requests
 import string
 from subprocess import Popen, PIPE, STDOUT
@@ -28,15 +29,42 @@ def score_model_in_sagemaker_docker_container(model_path, data):
     """
     env = dict(os.environ)
     env.update(LC_ALL="en_US.UTF-8", LANG="en_US.UTF-8")
-    proc = Popen(['mlflow', 'sagemaker', 'run-local', '-m', model_path], stdout=PIPE, stderr=STDOUT,
+    proc = Popen(['mlflow', 'sagemaker', 'run-local', '-m', model_path, '-p', "5000"],
+                 stdout=PIPE,
+                 stderr=STDOUT,
                  universal_newlines=True, env=env)
+    r = _score_proc(proc, 5000, data, "json").content
+    import json
+    return json.loads(r)  # TODO: we should return pd.Dataframe the same as pyfunc serve
+
+
+def pyfunc_serve_and_score_model(model_path, data):
+    env = dict(os.environ)
+    env.update(LC_ALL="en_US.UTF-8", LANG="en_US.UTF-8")
+    cmd = ['mlflow', 'pyfunc', 'serve', '-m', model_path, "-p", "0"]
+    proc = Popen(cmd,
+                 stdout=PIPE,
+                 stderr=STDOUT,
+                 universal_newlines=True,
+                 env=env)
+    for x in iter(proc.stdout.readline, ""):
+        print(x)
+        m = re.match(pattern=".*Running on http://127.0.0.1:(\\d+).*", string=x)
+        if m:
+            return pd.read_json(_score_proc(proc, int(m.group(1)), data, data_type="csv").content,
+                                orient="records")
+
+    raise Exception("Failed to start server")
+
+
+def _score_proc(proc, port, data, data_type):
     try:
         for i in range(0, 50):
             assert proc.poll() is None, "scoring process died"
             time.sleep(5)
             # noinspection PyBroadException
             try:
-                ping_status = requests.get(url='http://localhost:5000/ping')
+                ping_status = requests.get(url='http://localhost:%d/ping' % port)
                 print('connection attempt', i, "server is up! ping status", ping_status)
                 if ping_status.status_code == 200:
                     break
@@ -44,17 +72,31 @@ def score_model_in_sagemaker_docker_container(model_path, data):
                 print('connection attempt', i, "failed, server is not up yet")
 
         assert proc.poll() is None, "scoring process died"
-        ping_status = requests.get(url='http://localhost:5000/ping')
+        ping_status = requests.get(url='http://localhost:%d/ping' % port)
         print("server up, ping status", ping_status)
         if ping_status.status_code != 200:
             raise Exception("ping failed, server is not happy")
-        if type(data) == pd.DataFrame:
-            data = data.to_dict(orient='records')
-        y = requests.post(url='http://localhost:5000/invocations', json=data)
-        import json
-        return json.loads(y.content)
+        if data_type == "json":
+            if type(data) == pd.DataFrame:
+                data = data.to_dict(orient="records")
+            r = requests.post(url='http://localhost:%d/invocations' % port,
+                              json=data)
+        elif data_type == "csv":
+            data = data.to_csv(index=False, header=True)
+            r = requests.post(url='http://localhost:%d/invocations' % port,
+                              data=data,
+                              headers={"Content-Type": "text/csv"})
+        else:
+            raise Exception("Unexpected data_type %s" % data_type)
+        if r.status_code != 200:
+            raise Exception("scoring failed, status code = {}. Response = '{}' ".format(
+                r.status_code,
+                r))
+        return r
     finally:
         if proc.poll() is None:
             proc.terminate()
         print("captured output of the scoring process")
+        print("-------------------------STDOUT------------------------------")
         print(proc.stdout.read())
+        print("==============================================================")
--- tests/keras/test_keras_model_export.py
+++ tests/keras/test_keras_model_export.py
@@ -3,7 +3,6 @@
 from __future__ import print_function
 
 import os
-import tempfile
 import pytest
 from keras.models import Sequential
 from keras.layers import Dense
@@ -14,6 +13,7 @@ import numpy as np
 import mlflow.keras
 import mlflow
 from mlflow import pyfunc
+from tests.helper_functions import pyfunc_serve_and_score_model
 from tests.projects.utils import tracking_uri_mock  # pylint: disable=unused-import
 
 
@@ -24,7 +24,7 @@ def data():
                         columns=iris['feature_names'] + ['target'])
     y = data['target']
     x = data.drop('target', axis=1)
-    return (x, y)
+    return x, y
 
 
 @pytest.fixture(scope='module')
@@ -56,10 +56,13 @@ def test_model_save_load(tmpdir, model, data, predicted):
     pyfunc_loaded = mlflow.pyfunc.load_pyfunc(path)
     assert all(pyfunc_loaded.predict(x).values == predicted)
 
+    # pyfunc serve
+    preds = pyfunc_serve_and_score_model(model_path=os.path.abspath(path), data=pd.DataFrame(x))
+    assert all(preds.values.astype(np.float32) == predicted)
+
 
 def test_model_log(tracking_uri_mock, model, data, predicted):  # pylint: disable=unused-argument
     x, y = data
-    old_uri = mlflow.get_tracking_uri()
     # should_start_run tests whether or not calling log_model() automatically starts a run.
     for should_start_run in [False, True]:
         try:
