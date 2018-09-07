2084652b948e44d3687e05d7603b0edf73e776dd
Sid Murching <sid.murching@databricks.com>
2018-09-07 11:30:59 -0700

    Fix some small typos & lint

--- mlflow/keras.py
+++ mlflow/keras.py
@@ -23,9 +23,10 @@ def save_model(keras_model, path, conda_env=None, mlflow_model=Model()):
     >>> # Build, compile, and train your model
     >>> keras_model = ...
     >>> keras_model_path = ...
-    >>> keras_model.compile(optimizer="rmsprop",loss="mse", metrics["accuracy"])
-    >>> results = keras_model.fit(x_train, y_train, epochs=20, batch_size = 128, validation_data=(x_val, y_val))
-    ... # save the model in h5 format
+    >>> keras_model.compile(optimizer="rmsprop", loss="mse", metrics["accuracy"])
+    >>> results = keras_model.fit(
+    ...     x_train, y_train, epochs=20, batch_size = 128, validation_data=(x_val, y_val))
+    ... # Save the model as an MLflow Model
     >>> mlflow.keras.save_model(keras_model, keras_model_path)
     """
 
@@ -49,16 +50,16 @@ def log_model(keras_model, artifact_path, **kwargs):
     Log a Keras model as an MLflow artifact for the current run.
 
     :param keras_model: Keras model type to be logged as an artifact
-    :param artifact_path: path or directory name under artifacts. Usually, "models" is a good choice
-    :param kwargs:
+    :param artifact_path: path or directory name under artifacts.
+    :param kwargs: Additional keyword arguments to pass to mlflow.keras.save_model
 
     >>> from keras import Dense, layers
     >>> import mlflow
     >>> # Build, compile, and train your model
     >>> keras_model = ...
     >>> keras_model.compile(optimizer="rmsprop", loss="mse", metrics["accuracy"])
-    >>> results = keras_model.fit(x_train, y_train, epochs=20, batch_size = 128, validation_data=(x_val, y_val))
-    ...
+    >>> results = keras_model.fit(
+    ...     x_train, y_train, epochs=20, batch_size = 128, validation_data=(x_val, y_val))
     >>> # Log metrics and log the model
     >>> with mlflow.start_run() as run:
     >>>   mlflow.keras.log_model(keras_model, "models")
@@ -91,8 +92,9 @@ def load_pyfunc(model_file):
     :return: The model as PyFunc.
 
     >>> model_file = "/tmp/pyfunc-keras-model"
-    >>> keras_model = mlflow.keras.load_pyfunc(mode_file)
-    >>> predictions = keras_mode.predict(x_test)
+    >>> keras_model = mlflow.keras.load_pyfunc(model_file)
+    >>> # We can apply the loaded PyFunc for inference on a pandas DataFrame via predict()
+    >>> predictions = keras_model.predict(x_test)
     """
     return _KerasModelWrapper(_load_model(model_file))
 
@@ -100,12 +102,12 @@ def load_pyfunc(model_file):
 def load_model(path, run_id=None):
     """
     Load a Keras model from a local file (if run_id is None) or a run.
-    
+
     :param path: artifact path
     :param run_id: run_id of a particular run
     :return: Keras model
 
-    >>> #Load the model as Keras model or as pyfunc and use its predict() method
+    >>> # Load persisted model as a Keras model or as a PyFunc, call predict() on a Pandas DataFrame
     >>> keras_model = mlflow.keras.load_model("models", run_id="96771d893a5e46159d9f3b49bf9013e2")
     >>> predictions = keras_model.predict(x_test)
     """
