d588226514ea5a2e65426a087e0f714764ae1d30
zahir <hamroune.z@gmail.com>
2018-09-07 20:20:31 +0200

    PR-450

--- mlflow/store/s3_artifact_repo.py
+++ mlflow/store/s3_artifact_repo.py
@@ -16,7 +16,6 @@ class S3ArtifactRepository(ArtifactRepository):
     def parse_s3_uri(uri):
         """Parse an S3 URI, returning (bucket, path)"""
         parsed = urllib.parse.urlparse(uri)
-        print("URI S3 ==> "+uri)
         if parsed.scheme != "s3":
             raise Exception("Not an S3 URI: %s" % uri)
         path = parsed.path
@@ -24,20 +23,23 @@ class S3ArtifactRepository(ArtifactRepository):
             path = path[1:]
         return parsed.netloc, path
 
+    def _get_s3_client(self):
+        s3_endpoint_url = os.environ.get('MLFLOW_S3_ENDPOINT_URL')
+        return boto3.client('s3', endpoint_url=s3_endpoint_url)
+
     def log_artifact(self, local_file, artifact_path=None):
         (bucket, dest_path) = data.parse_s3_uri(self.artifact_uri)
         if artifact_path:
             dest_path = build_path(dest_path, artifact_path)
         dest_path = build_path(dest_path, os.path.basename(local_file))
-        s3_endpoint_url = os.environ.get('MLFLOW_S3_ENDPOINT_URL')
-        boto3.client('s3', endpoint_url=s3_endpoint_url).upload_file(local_file, bucket, dest_path)
+        s3_client = self._get_s3_client()
+        s3_client.upload_file(local_file, bucket, dest_path)
 
     def log_artifacts(self, local_dir, artifact_path=None):
         (bucket, dest_path) = data.parse_s3_uri(self.artifact_uri)
         if artifact_path:
             dest_path = build_path(dest_path, artifact_path)
-        s3_endpoint_url = os.environ.get('MLFLOW_S3_ENDPOINT_URL')
-        s3 = boto3.client('s3', endpoint_url=s3_endpoint_url)
+        s3_client = self._get_s3_client()
         local_dir = os.path.abspath(local_dir)
         for (root, _, filenames) in os.walk(local_dir):
             upload_path = dest_path
@@ -45,7 +47,7 @@ class S3ArtifactRepository(ArtifactRepository):
                 rel_path = get_relative_path(local_dir, root)
                 upload_path = build_path(dest_path, rel_path)
             for f in filenames:
-                s3.upload_file(build_path(root, f), bucket, build_path(upload_path, f))
+                s3_client.upload_file(build_path(root, f), bucket, build_path(upload_path, f))
 
     def list_artifacts(self, path=None):
         (bucket, artifact_path) = data.parse_s3_uri(self.artifact_uri)
@@ -89,7 +91,6 @@ class S3ArtifactRepository(ArtifactRepository):
         else:
             (bucket, s3_path) = data.parse_s3_uri(self.artifact_uri)
             s3_path = build_path(s3_path, artifact_path)
-            s3_endpoint_url = os.environ.get('MLFLOW_S3_ENDPOINT_URL')
-            client = boto3.client('s3', endpoint_url=s3_endpoint_url)
-            client.download_file(bucket, s3_path, local_path)
+            s3_client = self._get_s3_client()
+            s3_client.download_file(bucket, s3_path, local_path)
         return local_path
