6ac60dfc8a9543b6ca2506d2122beb50cc601a67
hamroune zahir <hamroune.z@gmail.com>
2018-09-09 02:46:41 +0200

    Add MLFLOW_S3_ENDPOINT_URL to specify custom S3 endpoint (#451)
    * issue 247 : Add MLFLOW_S3_ENDPOINT_URL to specify custom S3 endpoint
    
    * correct KeyError
    
    * Prepare for publish Java Tracking & Scoring APIs to Maven (#435)
    
    * Expose ArtifactRepository from MlflowClient (#438)
    
    * issue 247 : Add documentation
    
    * Issue-247 : small change in doc
    
    * PR-450
    
    * PR-450: add self._get_s3_client() in list_artifacts

--- mlflow/store/s3_artifact_repo.py
+++ mlflow/store/s3_artifact_repo.py
@@ -23,19 +23,23 @@ class S3ArtifactRepository(ArtifactRepository):
             path = path[1:]
         return parsed.netloc, path
 
+    def _get_s3_client(self):
+        s3_endpoint_url = os.environ.get('MLFLOW_S3_ENDPOINT_URL')
+        return boto3.client('s3', endpoint_url=s3_endpoint_url)
+
     def log_artifact(self, local_file, artifact_path=None):
         (bucket, dest_path) = data.parse_s3_uri(self.artifact_uri)
         if artifact_path:
             dest_path = build_path(dest_path, artifact_path)
         dest_path = build_path(dest_path, os.path.basename(local_file))
-
-        boto3.client('s3').upload_file(local_file, bucket, dest_path)
+        s3_client = self._get_s3_client()
+        s3_client.upload_file(local_file, bucket, dest_path)
 
     def log_artifacts(self, local_dir, artifact_path=None):
         (bucket, dest_path) = data.parse_s3_uri(self.artifact_uri)
         if artifact_path:
             dest_path = build_path(dest_path, artifact_path)
-        s3 = boto3.client('s3')
+        s3_client = self._get_s3_client()
         local_dir = os.path.abspath(local_dir)
         for (root, _, filenames) in os.walk(local_dir):
             upload_path = dest_path
@@ -43,7 +47,7 @@ class S3ArtifactRepository(ArtifactRepository):
                 rel_path = get_relative_path(local_dir, root)
                 upload_path = build_path(dest_path, rel_path)
             for f in filenames:
-                s3.upload_file(build_path(root, f), bucket, build_path(upload_path, f))
+                s3_client.upload_file(build_path(root, f), bucket, build_path(upload_path, f))
 
     def list_artifacts(self, path=None):
         (bucket, artifact_path) = data.parse_s3_uri(self.artifact_uri)
@@ -52,7 +56,8 @@ class S3ArtifactRepository(ArtifactRepository):
             dest_path = build_path(dest_path, path)
         infos = []
         prefix = dest_path + "/"
-        paginator = boto3.client('s3').get_paginator("list_objects_v2")
+        s3_client = self._get_s3_client()
+        paginator = s3_client.get_paginator("list_objects_v2")
         results = paginator.paginate(Bucket=bucket, Prefix=prefix, Delimiter='/')
         for result in results:
             # Subdirectories will be listed as "common prefixes" due to the way we made the request
@@ -85,5 +90,6 @@ class S3ArtifactRepository(ArtifactRepository):
         else:
             (bucket, s3_path) = data.parse_s3_uri(self.artifact_uri)
             s3_path = build_path(s3_path, artifact_path)
-            boto3.client('s3').download_file(bucket, s3_path, local_path)
+            s3_client = self._get_s3_client()
+            s3_client.download_file(bucket, s3_path, local_path)
         return local_path
