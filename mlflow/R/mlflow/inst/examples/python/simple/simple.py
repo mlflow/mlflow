452b03ea1679a408bc5aa0151e33e5712ff0651a
Javier Luraschi <javierluraschi@hotmail.com>
2018-09-11 19:02:31 -0700

    fix couple lintr issues

--- mlflow/R/mlflow/inst/examples/python/simple/simple.py
+++ mlflow/R/mlflow/inst/examples/python/simple/simple.py
@@ -2,6 +2,6 @@ import os
 import mlflow
 
 if __name__ == "__main__":
-  with mlflow.start_run():
-    mlflow.log_param("parameter", 5)
-    mlflow.log_metric("metric", 0)
+    with mlflow.start_run():
+        mlflow.log_param("parameter", 5)
+        mlflow.log_metric("metric", 0)
