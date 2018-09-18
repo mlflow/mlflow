9dfbf648589112f7027045a3ab1d6120029fc607
tomasatdatabricks <tomas.nykodym@databricks.com>
2018-09-18 15:33:01 -0700

    Removed accidental changes

--- examples/hyperparam/search_hyperopt.py
+++ examples/hyperparam/search_hyperopt.py
@@ -130,7 +130,6 @@ def train(training_data, max_runs, epochs, metric, algo, seed, training_experime
     space = [
         hp.uniform('lr', 1e-5, 1e-1),
         hp.uniform('momentum', .0, 1.0),
-
     ]
 
     with mlflow.start_run() as run:
