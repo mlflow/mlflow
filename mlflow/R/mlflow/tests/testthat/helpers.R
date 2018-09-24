4c9df23cf13f11cef1b768f6aec2e05a4fcef030
kevinykuo <kevinykuo@gmail.com>
2018-09-24 02:41:32 -0400

    deregister local servers in tests

--- mlflow/R/mlflow/tests/testthat/helpers.R
+++ mlflow/R/mlflow/tests/testthat/helpers.R
@@ -2,5 +2,10 @@ mlflow_clear_test_dir <- function(path) {
   if (dir.exists(path)) {
     unlink(path, recursive = TRUE)
   }
-  mlflow_disconnect()
+  mlflow_deregister_local_servers()
+}
+
+mlflow_deregister_local_servers <- function() {
+  purrr::walk(as.list(.globals$url_mapping), ~ .x$handle$kill())
+  .globals$url_mapping <- NULL
 }
