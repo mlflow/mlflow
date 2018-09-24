a9ec7da8bab529f7640a46e775308962d1a5d3b7
kevinykuo <kevinykuo@gmail.com>
2018-09-24 01:41:01 -0400

    revise tests

--- mlflow/R/mlflow/tests/testthat/test-rest-api.R
+++ mlflow/R/mlflow/tests/testthat/test-rest-api.R
@@ -8,25 +8,12 @@ test_that("mlflow_create_experiment() works properly", {
   expect_identical(experiment$experiment$artifact_location, "art_loc")
 })
 
-test_that("mlflow_create_experiment() `activate` parameter is respected", {
-  experiment_id <- mlflow_create_experiment("foo1", "art_loc")
-  expect_identical(mlflow_active_experiment(), experiment_id)
-  mlflow_create_experiment("foo2", "art_loc", activate = FALSE)
-  expect_identical(mlflow_active_experiment(), experiment_id)
-})
-
-test_that("mlflow_create_experiment() checks for existing experiment with same name", {
-  expect_message(
-    mlflow_create_experiment("foo2", "art_loc"),
-    "Experiment with name \"foo2\" already exists\\."
-  )
-})
-
 test_that("mlflow_list_experiments() works properly", {
   mlflow_clear_test_dir("mlruns")
-  mlflow_create_experiment("foo1", "art_loc1")
-  mlflow_create_experiment("foo2", "art_loc2")
-  experiments_list <- mlflow_list_experiments()
+  client <- mlflow_client()
+  mlflow_create_experiment(client = client, "foo1", "art_loc1")
+  mlflow_create_experiment(client = client, "foo2", "art_loc2")
+  experiments_list <- mlflow_list_experiments(client = client)
   expect_setequal(experiments_list$experiment_id, c("0", "1", "2"))
   expect_setequal(experiments_list$name, c("Default", "foo1", "foo2"))
   expect_setequal(experiments_list$artifact_location, c("mlruns/0", "art_loc1", "art_loc2"))
