2d5ef5e3e2f37d333faf04b1741b5f8ec1535fc3
kevinykuo <kevinykuo@gmail.com>
2018-09-23 21:34:30 -0400

    reorg files

--- mlflow/R/mlflow/R/mlflow-tracking-rest.R
+++ mlflow/R/mlflow/R/mlflow-tracking-rest.R
@@ -1,26 +1,81 @@
-new_mlflow_client <- function(tracking_uri, server_url = NULL) {
-  structure(
-    list(
-      tracking_uri = tracking_uri,
-      server_url = server_url %||% tracking_uri
-    ),
-    class = "mlflow_client"
+mlflow_rest_path <- function(version) {
+  switch(
+    version,
+    "2.0" = "ajax-api/2.0/preview/mlflow"
   )
 }
 
-#' Initialize an MLflow client
-#'
-#' @param tracking_uri The tracking URI
-#'
-#' @export
-mlflow_client <- function(tracking_uri = NULL) {
-  tracking_uri <- tracking_uri %||% mlflow_tracking_uri()
-  if (!startsWith(tracking_uri, "http") && is.null(mlflow_local_server(tracking_uri))) {
-    local_server <- mlflow_server(file_store = tracking_uri, port = mlflow_connect_port())
-    mlflow_register_local_server(tracking_uri = tracking_uri, local_server = local_server)
-  }
-  new_mlflow_client(tracking_uri, server_url = mlflow_local_server(tracking_uri)$tracking_uri)
+mlflow_rest_body <- function(data) {
+  data <- Filter(length, data)
+  paste0(
+    "\"",
+    gsub(
+      "\\\"",
+      "\\\\\"",
+      as.character(
+        jsonlite::toJSON(data, auto_unbox = TRUE)
+      )
+    ),
+    "\""
+  )
 }
+
+#' @importFrom httr add_headers
+mlflow_rest_headers <- function() {
+  add_headers("Content-Type" = "application/json")
+}
+
+#' @importFrom httr timeout
+mlflow_rest_timeout <- function() {
+  timeout(getOption("mlflow.rest.timeout", 1))
+}
+
+#' @importFrom httr content
+#' @importFrom httr GET
+#' @importFrom httr POST
+#' @importFrom jsonlite fromJSON
+#' @importFrom xml2 as_list
+mlflow_rest <- function(..., client, query = NULL, data = NULL, verb = "GET", version = "2.0") {
+  args <- list(...)
+
+  # if (is.null(args$mc)) {
+  #   mc <- mlflow_get_or_create_active_connection()
+  # }
+  # else {
+  #   mc <- args$mc
+  #   args$mc <- NULL
+  # }
+
+  tracking_url <- if (inherits(client, "mlflow_connection")) {
+    client$tracking_uri
+  } else {
+    client$server_url
+  }
+
+  api_url <- file.path(
+    tracking_url,
+    mlflow_rest_path(version),
+    paste(args, collapse = "/")
+  )
+
+  response <- switch(
+    verb,
+    GET = GET(api_url, query = query, mlflow_rest_timeout()),
+    POST = POST(api_url,
+                body = mlflow_rest_body(data),
+                mlflow_rest_headers(),
+                mlflow_rest_timeout()),
+    stop("Verb '", verb, "' is unsupported.")
+  )
+
+  if (identical(response$status_code, 500L)) {
+    stop(as_list(content(response))$html$body$p[[1]])
+  }
+
+  text <- content(response, "text", encoding = "UTF-8")
+  jsonlite::fromJSON(text)
+}
+
 
 mlflow_client_create_experiment <- function(client, name, artifact_location) {
   response <- mlflow_rest(
--- mlflow/R/mlflow/R/mlflow-tracking.R
+++ mlflow/R/mlflow/R/mlflow-tracking.R
@@ -1,3 +1,27 @@
+new_mlflow_client <- function(tracking_uri, server_url = NULL) {
+  structure(
+    list(
+      tracking_uri = tracking_uri,
+      server_url = server_url %||% tracking_uri
+    ),
+    class = "mlflow_client"
+  )
+}
+
+#' Initialize an MLflow client
+#'
+#' @param tracking_uri The tracking URI
+#'
+#' @export
+mlflow_client <- function(tracking_uri = NULL) {
+  tracking_uri <- tracking_uri %||% mlflow_tracking_uri()
+  if (!startsWith(tracking_uri, "http") && is.null(mlflow_local_server(tracking_uri))) {
+    local_server <- mlflow_server(file_store = tracking_uri, port = mlflow_connect_port())
+    mlflow_register_local_server(tracking_uri = tracking_uri, local_server = local_server)
+  }
+  new_mlflow_client(tracking_uri, server_url = mlflow_local_server(tracking_uri)$tracking_uri)
+}
+
 #' Create Experiment
 #'
 #' Creates an MLflow experiment.
@@ -297,15 +321,70 @@ mlflow_set_terminated <- function(
   tidy_run_info(response$run_info)
 }
 
-current_time <- function() {
-  round(as.numeric(Sys.time()) * 1000)
-}
+#' Log Artifact
+#'
+#' Logs an specific file or directory as an artifact.
+#'
+#' @param path The file or directory to log as an artifact.
+#' @param artifact_path Destination path within the run’s artifact URI.
+#' @param run_uuid The run associated with this artifact.
+#'
+#' @details
+#'
+#' When logging to Amazon S3, ensure that the user has a proper policy
+#' attach to it, for instance:
+#'
+#' \code{
+#' {
+#' "Version": "2012-10-17",
+#' "Statement": [
+#'   {
+#'     "Sid": "VisualEditor0",
+#'     "Effect": "Allow",
+#'     "Action": [
+#'       "s3:PutObject",
+#'       "s3:GetObject",
+#'       "s3:ListBucket",
+#'       "s3:GetBucketLocation"
+#'       ],
+#'     "Resource": [
+#'       "arn:aws:s3:::mlflow-test/*",
+#'       "arn:aws:s3:::mlflow-test"
+#'       ]
+#'   }
+#'   ]
+#' }
+#' }
+#'
+#' Additionally, at least the \code{AWS_ACCESS_KEY_ID} and \code{AWS_SECRET_ACCESS_KEY}
+#' environment variables must be set to the corresponding key and secrets provided
+#' by Amazon IAM.
+#'
+#' @export
+mlflow_log_artifact <- function(path, artifact_path = NULL, run_uuid = NULL) {
+  run_uuid <- run_uuid %||%
+    mlflow_active_run()$run_info$run_uuid %||%
+    stop("`run_uuid` must be specified when there is no active run.")
 
-milliseconds_to_date <- function(x) as.POSIXct(as.double(x) / 1000, origin = "1970-01-01")
+  artifact_param <- NULL
+  if (!is.null(artifact_path)) artifact_param <- "--artifact-path"
 
-tidy_run_info <- function(run_info) {
-  df <- as.data.frame(run_info, stringsAsFactors = FALSE)
-  df$start_time <- milliseconds_to_date(df$start_time %||% NA)
-  df$end_time <- milliseconds_to_date(df$end_time %||% NA)
-  df
+  if (as.logical(fs::is_file(path))) {
+    command <- "log-artifact"
+    local_param <- "--local-file"
+  } else {
+    command <- "log-artifacts"
+    local_param <- "--local-dir"
+  }
+
+  mlflow_cli("artifacts",
+             command,
+             local_param,
+             path,
+             artifact_param,
+             artifact_path,
+             "--run-id",
+             run_uuid)
+
+  invisible(NULL)
 }
