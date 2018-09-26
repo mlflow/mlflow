bd3fc9f37c5f6e5b92bb0995278c5e4c0a2c676a
tomasatdatabricks <tomas.nykodym@databricks.com>
2018-09-25 23:58:19 -0700

    Added code to install Keras.

--- mlflow/R/mlflow/.travis.R
+++ mlflow/R/mlflow/.travis.R
@@ -2,4 +2,6 @@ parent_dir <- dir("../", full.names = TRUE)
 package <- parent_dir[grepl("mlflow_", parent_dir)]
 install.packages(package)
 install.packages("keras", repos='http://cran.rstudio.com/')
+library(keras)
+install_keras()
 source("testthat.R")
