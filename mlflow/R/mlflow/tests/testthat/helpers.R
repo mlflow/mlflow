933b1488750bfaa5808127699d0472aca37438c7
kevinykuo <kevinykuo@gmail.com>
2018-09-26 20:56:11 -0400

    shorten function name

--- mlflow/R/mlflow/tests/testthat/helpers.R
+++ mlflow/R/mlflow/tests/testthat/helpers.R
@@ -4,10 +4,10 @@ mlflow_clear_test_dir <- function(path) {
   if (dir.exists(path)) {
     unlink(path, recursive = TRUE)
   }
-  mlflow_deregister_local_servers()
+  deregister_local_servers()
 }
 
-mlflow_deregister_local_servers <- function() {
+deregister_local_servers <- function() {
   purrr::walk(as.list(mlflow:::.globals$url_mapping), ~ .x$handle$kill())
   rlang::env_unbind(mlflow:::.globals, "url_mapping")
 }
