7e9cbfcdb8167b63dc0ed4887bf8ff230239da08
dbczumar <39497902+dbczumar@users.noreply.github.com>
2018-10-05 17:03:42 -0700

    Convert Exceptions in file store to MlflowExceptions (#601)
    * Convert exception objects in file store to MlflowException objects
    
    * Whitespace
    
    * Address comments, add docs
    
    * Appease linter

--- mlflow/exceptions.py
+++ mlflow/exceptions.py
@@ -4,7 +4,12 @@ from mlflow.protos.databricks_pb2 import INTERNAL_ERROR, ErrorCode
 
 
 class MlflowException(Exception):
-    """Base exception in MLflow."""
+    """
+    Generic exception thrown to surface failure information about external-facing operations.
+    The error message associated with this exception may be exposed to clients in HTTP responses
+    for debugging purposes. If the error text is sensitive, raise a generic `Exception` object
+    instead.
+    """
     def __init__(self, message, error_code=INTERNAL_ERROR):
         try:
             self.error_code = ErrorCode.Name(error_code)
