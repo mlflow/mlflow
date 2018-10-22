1e4cdcaa27db2ffc4dbdf37a986c5db17ef9334f
Corey Zumar <corey.zumar@databricks.com>
2018-10-22 16:04:49 -0700

    Added dev check

--- mlflow/azureml/__init__.py
+++ mlflow/azureml/__init__.py
@@ -220,9 +220,15 @@ def _create_dockerfile(output_path, mlflow_path=None):
     if mlflow_path is not None:
         mlflow_install_cmd = "RUN pip install -e {mlflow_path}".format(
             mlflow_path=_get_container_path(mlflow_path))
-    else:
+    elif not mlflow_version.endswith("dev"):
         mlflow_install_cmd = "RUN pip install mlflow=={mlflow_version}".format(
             mlflow_version=mlflow_version)
+    else:
+        raise MlflowException(
+                "You are running a 'dev' version of MLflow: `{mlflow_version}` that cannot be"
+                " installed from pip. In order to build a container image, either specify the"
+                " path to a local copy of the MLflow GitHub repository or install a release version"
+                " of MLflow from pip".format(mlflow_version=mlflow_version))
     docker_cmds.append(mlflow_install_cmd)
 
     with open(output_path, "w") as f:
