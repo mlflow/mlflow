ac5cb8a9c848c8eadb3a637335c50f4d4f7aa09a
Corey Zumar <corey.zumar@databricks.com>
2018-11-09 14:31:17 -0800

    Model utils tests

--- mlflow/utils/environment.py
+++ mlflow/utils/environment.py
@@ -10,17 +10,18 @@ channels:
 
 
 def _mlflow_conda_env(path=None, additional_conda_deps=None, additional_pip_deps=None,
-        additional_conda_channels=None):
+                      additional_conda_channels=None):
     """
-    Create conda environment file. Contains default dependency on current python version.
-    :param path: Local filesystem path where the conda env file is to be created. If unspecified,
-                 the conda env will be returned as a string.
+    Creates a Conda environment with the specified package channels and dependencies.
+    Optionally, saves the Conda environment to an output path, if specified.
+
+    :param path: Local filesystem path where the conda env file is to be written. If unspecified,
+                 the conda env will not be written to the filesystem; it will still be returned
+                 in string format.
     :param additional_conda_deps: List of additional conda dependencies passed as strings.
     :param additional_pip_deps: List of additional pip dependencies passed as strings.
     :param additional_channels: List of additional conda channels to search when resolving packages.
-    :return: Either:
-                * The path where the conda environment has been created, if ``path`` is specified.
-                * The conda environment definition as a string, if ``path`` is not specified.
+    :return: The text of the conda environment that was created.
     """
     env = yaml.load(_conda_header)
     env["dependencies"] = ["python={}".format(PYTHON_VERSION)]
@@ -31,12 +32,13 @@ def _mlflow_conda_env(path=None, additional_conda_deps=None, additional_pip_deps
     if additional_conda_channels is not None:
         env["channels"] += additional_conda_channels
 
+    env_text = yaml.safe_dump(env, stream=None, default_flow_style=False)
+
     if path is not None:
         with open(path, "w") as f:
-            yaml.safe_dump(env, stream=f, default_flow_style=False)
-        return path
-    else:
-        return yaml.safe_dump(env, stream=None, default_flow_style=False)
+            f.write(env_text)
+
+    return env_text
 
 
 def _get_base_env():
--- tests/utils/test_environment.py
+++ tests/utils/test_environment.py
@@ -1,14 +1,32 @@
 import os
+import pytest
+import yaml
 
 from mlflow.utils.environment import _mlflow_conda_env
 
 
-def test_save(tmpdir):
-    filename = os.path.join(str(tmpdir), "conda_env.yml")
-    _mlflow_conda_env(filename, additional_conda_deps=["conda-dep-1=0.0.1", "conda-dep-2"],
-                      additional_pip_deps=["pip-dep-1", "pip-dep2==0.1.0"])
-    print("")
-    print("env start")
-    with open(filename) as f:
-        print(f.read())
-    print("env end")
+@pytest.fixture
+def conda_env_path(tmpdir):
+    return os.path.join(tmpdir.strpath, "conda_env.yaml")
+
+
+def test_mlflow_conda_env_returns_env_text_when_output_path_is_specified(conda_env_path):
+    env_text = _mlflow_conda_env(
+            path=conda_env_path, 
+            additional_conda_deps=["conda-dep-1=0.0.1", "conda-dep-2"],
+            additional_pip_deps=["pip-dep-1", "pip-dep2==0.1.0"])
+
+    with open(conda_env_path, "r") as f:
+        assert env_text == f.read()
+
+
+def test_mlflow_conda_env_returns_env_text_when_output_path_is_not_specified(conda_env_path):
+    conda_deps = ["conda-dep-1=0.0.1", "conda-dep-2"]
+
+    env_text = _mlflow_conda_env(
+                path=conda_env_path, 
+                additional_conda_deps=conda_deps)
+
+    env_parsed = yaml.safe_load(env_text)
+    for conda_dep in conda_deps:
+        assert conda_dep in env_parsed["dependencies"]
