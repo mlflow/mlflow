74550dd49566ea64338d259a6eae55a37ed9d55c
Corey Zumar <corey.zumar@databricks.com>
2018-11-12 23:51:54 -0800

    reduce pytorch test time

--- tests/pytorch/test_pytorch_model_export.py
+++ tests/pytorch/test_pytorch_model_export.py
@@ -248,7 +248,7 @@ def test_sagemaker_docker_model_scoring_with_default_conda_env(model, model_path
             data=data[0],
             content_type=pyfunc_scoring_server.CONTENT_TYPE_JSON_SPLIT_ORIENTED,
             flavor=mlflow.pyfunc.FLAVOR_NAME,
-            activity_polling_timeout_seconds=500)
+            activity_polling_timeout_seconds=360)
     deployed_model_preds = pd.DataFrame(json.loads(scoring_response.content))
 
     np.testing.assert_array_almost_equal(
