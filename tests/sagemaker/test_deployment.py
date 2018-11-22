0a31c6513ec1d9181bde83be30e18abac5010c48
dbczumar <39497902+dbczumar@users.noreply.github.com>
2018-11-22 01:21:19 -0800

    Test infrastructure for SageMaker (#718)
    * Add SparkJava plugin
    
    * Add MLeap flavor and modify SparkML module to use it
    
    * Fixes
    
    * Add mleap
    
    * Import model in test
    
    * Import mleap
    
    * Add missing assert
    
    * Reorder spark session creation params in test
    
    * Docs fix
    
    * revert pom xml change
    
    * remove java
    
    * Add standalone  to mleap
    
    * Import fix
    
    * Add docs and
    
    * Add warning about py incompatibility
    
    * Address comments
    
    * Code spacing fix
    
    * Revert log test changes
    
    * Whitespace fixes
    
    * Exception import fixes
    
    * Fix lint issues
    
    * Fix method call
    
    * callfix
    
    * Remove unused imports
    
    * Py4j log level fix
    
    * reorder tests
    
    * testfix
    
    * test fixes
    
    * Spacing fix
    
    * lint fixes
    
    * Add mleap schema test
    
    * Fix test
    
    * Whitespace fix
    
    * Test fix
    
    * Bump version to 0.5.3dev0
    
    * Update container + build function to support MLeap
    
    * Fix lint errors
    
    * Install maven
    
    * Test support infra
    
    * Fixws
    
    * Add tests
    
    * Path fixes
    
    * line fix
    
    * Lint fixes
    
    * Revert scoring server
    
    * Update test asserts, remove unused file
    
    * Specify server port
    
    * Add mock for sagemaker functions that we use
    
    * Sigquit >> sigterm
    
    * accept any version of mlflow jar
    
    * Address some comments, fix bug in scoring server argument parsing (whoops :/), add support for specifying deployment flavor
    
    * Address comments
    
    * Update ScoringServer.java
    
    * Update ScoringServer.java
    
    * Update __init__.py
    
    * varfix
    
    * Revert tempdir changes
    
    * Mark test as large
    
    * Use sagemaker env var instead of file upload
    
    * Parameter fixes
    
    * CLI fixes
    
    * CLI Fixes
    
    * CLI support, tests
    
    * Make linter happy
    
    * Fix serving command
    
    * Name change for get_serving_flavor
    
    * Address comments
    
    * Make linter happy
    
    * Address comments
    
    * Add  cli command and refactor flavor selection/validation
    
    * Test fixes
    
    * Add print statements
    
    * Varfix
    
    * Run local in shell to get docker env var successfully
    
    * Lint fix
    
    * Iteration fix
    
    * Deployment env var fixes for local serving
    
    * Sagemaker mock progress
    
    * Rename service mock
    
    * Mock test
    
    * Lint
    
    * Revert extraneous files
    
    * Revert
    
    * Tests
    
    * Mock tests and fixes
    
    * Better test coverage
    
    * Update test
    
    * Docs
    
    * Revert examples
    
    * Use mocks in real tests
    
    * Lint
    
    * lint
    
    * Add mocks test
    
    * Lint
    
    * Unpin moto and boto
    
    * Fix doc spacing
    
    * Add mock boto credentials
    
    * Fix tests
    
    * Lint
    
    * Test fix
    
    * Lint
    
    * Check more specific exception
    
    * Wraps fix
    
    * Lint fix
    
    * Remove needless encoding

--- tests/sagemaker/test_deployment.py
+++ tests/sagemaker/test_deployment.py
@@ -2,6 +2,7 @@ import os
 import pytest
 from collections import namedtuple
 
+import boto3
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 
@@ -28,6 +29,58 @@ def pretrained_model():
         mlflow.sklearn.log_model(lr, model_path)
         run_id = mlflow.active_run().info.run_uuid
         return TrainedModel(model_path, run_id)
+
+
+@pytest.fixture
+def sagemaker_client():
+    return boto3.client("sagemaker", region_name="us-west-2")
+
+
+@pytest.fixture(scope='session', autouse=True)
+def set_boto_credentials():
+    os.environ["AWS_ACCESS_KEY_ID"] = "NotARealAccessKey"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "NotARealSecretAccessKey"
+    os.environ["AWS_SESSION_TOKEN"] = "NotARealSessionToken"
+
+
+def mock_sagemaker_aws_services(fn):
+    # Import `wraps` from `six` instead of `functools` to properly set the
+    # wrapped function's `__wrapped__` attribute to the required value
+    # in Python 2
+    from six import wraps
+    from moto import mock_s3, mock_ecr, mock_sts, mock_iam
+    from tests.sagemaker.mock import mock_sagemaker
+
+    @mock_ecr
+    @mock_iam
+    @mock_s3
+    @mock_sagemaker
+    @mock_sts
+    @wraps(fn)
+    def mock_wrapper(*args, **kwargs):
+        # Create an ECR repository for the `mlflow-pyfunc` SageMaker docker image
+        ecr_client = boto3.client("ecr", region_name="us-west-2")
+        ecr_client.create_repository(repositoryName=mfs.DEFAULT_IMAGE_NAME)
+
+        # Create the moto IAM role
+        role_policy = """
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Action": "*",
+                    "Resource": "*"
+                }
+            ]
+        }
+        """
+        iam_client = boto3.client("iam", region_name="us-west-2")
+        iam_client.create_role(RoleName="moto", AssumeRolePolicyDocument=role_policy)
+
+        return fn(*args, **kwargs)
+
+    return mock_wrapper
 
 
 def test_deployment_with_unsupported_flavor_raises_exception(pretrained_model):
@@ -85,3 +138,50 @@ def test_get_preferred_deployment_flavor_obtains_valid_flavor_from_model(pretrai
 
     assert selected_flavor in mfs.SUPPORTED_DEPLOYMENT_FLAVORS
     assert selected_flavor in model_config.flavors
+
+
+@mock_sagemaker_aws_services
+def test_deploy_creates_sagemaker_resources_with_expected_names(pretrained_model, sagemaker_client):
+    app_name = "test-app"
+    mfs.deploy(app_name=app_name,
+               model_path=pretrained_model.model_path,
+               run_id=pretrained_model.run_id,
+               mode=mfs.DEPLOYMENT_MODE_CREATE)
+
+    models_response = sagemaker_client.list_models()
+    found_matching_model = False
+    for model in models_response["Models"]:
+        if app_name in model["ModelName"]:
+            found_matching_model = True
+            break
+    assert found_matching_model
+
+    endpoint_configs_response = sagemaker_client.list_endpoint_configs()
+    found_matching_config = False
+    for config in endpoint_configs_response["EndpointConfigs"]:
+        if app_name in config["EndpointConfigName"]:
+            found_matching_config = True
+            break
+    assert found_matching_config
+
+    endpoints_response = sagemaker_client.list_endpoints()
+    assert app_name in [endpoint["EndpointName"] for endpoint in endpoints_response["Endpoints"]]
+
+
+@mock_sagemaker_aws_services
+def test_deploying_application_with_preexisting_name_in_create_mode_throws_exception(
+        pretrained_model):
+    app_name = "test-app"
+    mfs.deploy(app_name=app_name,
+               model_path=pretrained_model.model_path,
+               run_id=pretrained_model.run_id,
+               mode=mfs.DEPLOYMENT_MODE_CREATE)
+
+    with pytest.raises(MlflowException) as exc:
+        mfs.deploy(app_name=app_name,
+                   model_path=pretrained_model.model_path,
+                   run_id=pretrained_model.run_id,
+                   mode=mfs.DEPLOYMENT_MODE_CREATE)
+
+    assert "an application with the same name already exists" in exc.value.message
+    assert exc.value.error_code == ErrorCode.Name(INVALID_PARAMETER_VALUE)
