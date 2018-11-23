912f37fe4b8a0fe92536c79e81e0cfda4253d1ce
Krishna Sangeeth <kskrishnasangeeth@gmail.com>
2018-11-23 14:11:10 +0530

    fix failing tests

--- tests/projects/test_databricks.py
+++ tests/projects/test_databricks.py
@@ -198,7 +198,7 @@ def test_run_databricks(
             assert submitted_run.wait() == run_succeeded
             assert submitted_run.run_id is not None
             assert runs_submit_mock.call_count == 1
-            assert set_tag_mock.call_count == 3
+            assert set_tag_mock.call_count == 4
             set_tag_args, _ = set_tag_mock.call_args_list[0]
             assert set_tag_args[1] == MLFLOW_DATABRICKS_RUN_URL
             assert set_tag_args[2] == 'test_url'
