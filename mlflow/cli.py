d827011ad0c734b9ffe4ddb537ac227497e4f9ee
Siddharth Murching <smurching@gmail.com>
2018-11-24 23:03:45 -0800

    Update Databricks project execution docs to clarify how to run against different workspaces (#715)
    Updates docstrings for the mlflow run CLI & mlflow.run Python API to describe how to run projects against different Databricks workspaces

--- mlflow/cli.py
+++ mlflow/cli.py
@@ -53,10 +53,14 @@ def cli():
 @click.option("--mode", "-m", metavar="MODE",
               help="Execution mode to use for run. Supported values: 'local' (runs project"
                    "locally) and 'databricks' (runs project on a Databricks cluster)."
-                   "Defaults to 'local'. If running against Databricks, will run against the "
-                   "Databricks workspace specified in the default Databricks CLI profile. "
-                   "See https://github.com/databricks/databricks-cli for more info on configuring "
-                   "a Databricks CLI profile.")
+                   "Defaults to 'local'. If running against Databricks, will run against a "
+                   "Databricks workspace determined as follows: if a Databricks tracking URI "
+                   "of the form 'databricks://profile' has been set (e.g. by setting "
+                   "the MLFLOW_TRACKING_URI environment variable), will run against the "
+                   "workspace specified by <profile>. Otherwise, runs against the workspace "
+                   "specified by the default Databricks CLI profile. See "
+                   "https://github.com/databricks/databricks-cli for more info on configuring a "
+                   "Databricks CLI profile.")
 @click.option("--cluster-spec", "-c", metavar="FILE",
               help="Path to JSON file (must end in '.json') or JSON string describing the cluster"
                    "to use when launching a run on Databricks. See "
