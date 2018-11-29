b68941b05ce606e2ab58bbc00c9a0ea28bb9d4e6
Andrew Chen <andrewmnchen@gmail.com>
2018-11-28 19:43:06 -0800

    fix lint

--- mlflow/server/js/.eslintrc.js
+++ mlflow/server/js/.eslintrc.js
@@ -209,7 +209,7 @@ module.exports = {
     'no-param-reassign': 2,
     'no-path-concat': 2,
     'no-plusplus': 0,
-    'no-process-env': 2,
+    'no-process-env': 0,
     'no-process-exit': 2,
     'no-proto': 2,
     'no-prototype-builtins': 0,
