22e5ab3be44b3536211e0b4b859cbcfd624873d1
Aaron Davidson <aaron@databricks.com>
2018-11-30 16:04:39 -0800

    Oops, add test

---
+++ mlflow/server/js/src/setupAjaxHeaders.test.js
@@ -0,0 +1,12 @@
+import { getRequestHeaders, setupAjaxHeaders } from './setupAjaxHeaders';
+
+test('If activeExperimentId is defined then choose that one', () => {
+  const headers = getRequestHeaders("");
+  expect(headers).toEqual({});
+});
+
+test('If activeExperimentId is defined then choose that one', () => {
+  const headers = getRequestHeaders(
+    "a=b; mlflow-request-header-My-CSRF=1; mlflow-request-header-Hello=World; c=d");
+  expect(headers).toEqual({"My-CSRF": "1", "Hello": "World"});
+});
