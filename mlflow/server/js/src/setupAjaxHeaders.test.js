5f9b0af73dc7a73ce520cfea593d99c9543071f7
Aaron Davidson <aaron@databricks.com>
2018-11-30 16:16:05 -0800

    Fix test names

--- mlflow/server/js/src/setupAjaxHeaders.test.js
+++ mlflow/server/js/src/setupAjaxHeaders.test.js
@@ -1,11 +1,11 @@
-import getRequestHeaders from './setupAjaxHeaders';
+import { getRequestHeaders } from './setupAjaxHeaders';
 
-test('If activeExperimentId is defined then choose that one', () => {
+test('empty cookie should result in no headers', () => {
   const headers = getRequestHeaders("");
   expect(headers).toEqual({});
 });
 
-test('If activeExperimentId is defined then choose that one', () => {
+test('cookies prefixed with mlflow-request-header- should be returned', () => {
   const headers = getRequestHeaders(
     "a=b; mlflow-request-header-My-CSRF=1; mlflow-request-header-Hello=World; c=d");
   expect(headers).toEqual({"My-CSRF": "1", "Hello": "World"});
