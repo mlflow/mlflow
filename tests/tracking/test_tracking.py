09a1e4224979e7554bed086e5f23d42b363224e7
Sid Murching <smurching@gmail.com>
2018-12-20 15:19:31 -0800

    Fix experiment ID bug

--- tests/tracking/test_tracking.py
+++ tests/tracking/test_tracking.py
@@ -246,3 +246,13 @@ def test_start_deleted_run():
         with mlflow.start_run(run_uuid=run_id):
             pass
     assert mlflow.active_run() is None
+
+
+def test_start_run_exp_id_0(tracking_uri_mock, reset_active_experiment):
+    mlflow.set_experiment("some-experiment")
+    with mlflow.start_run() as active_run:
+        exp_id = active_run.info.experiment_id
+        assert exp_id != 0
+        assert MlflowClient().get_experiment(exp_id).name == "some-experiment"
+    with mlflow.start_run(experiment_id=0) as active_run:
+        assert active_run.info.experiment_id == 0
