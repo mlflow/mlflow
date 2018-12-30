00ee4532055d34fd570ad6ad64f0e1d2a11e2bc6
Dor Indivo <dor7238@gmail.com>
2018-12-30 18:58:56 +0200

    fix lint issues

--- mlflow/utils/time_utils.py
+++ mlflow/utils/time_utils.py
@@ -1,6 +1,7 @@
 import datetime
 from pytz import reference
 
+
 def conv_longdate_to_str(longdate, local_tz=True):
     date_time = datetime.datetime.fromtimestamp(longdate / 1000.0)
     str_long_date = date_time.strftime('%Y-%m-%d %H:%M:%S')
