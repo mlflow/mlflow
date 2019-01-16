ea02b5a009b74db99b07a747ceff66dea6c95804
sueann <sueann@databricks.com>
2019-01-16 13:54:08 -0800

    comments

--- mlflow/store/file_store.py
+++ mlflow/store/file_store.py
@@ -170,7 +170,8 @@ class FileStore(AbstractStore):
                 raise MlflowException(
                     "Experiment '%s' already exists in deleted state. "
                     "You can restore the experiment, or permanently delete the experiment "
-                    "from the .trash folder to create a new one." % experiment.name,
+                    "from the .trash folder (under tracking server's root folder) before "
+                    "creating a new one with the same name." % experiment.name,
                     databricks_pb2.RESOURCE_ALREADY_EXISTS)
             else:
                 raise MlflowException("Experiment '%s' already exists." % experiment.name,
