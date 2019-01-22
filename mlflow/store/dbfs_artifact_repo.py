4d066d198ca07ba7c6882dff64a14fcee666c9a2
Siddharth Murching <smurching@gmail.com>
2019-01-22 10:51:45 -0800

    Cherry pick #818 (1cd1776a9f2a3bed114c79472880683348639c04) and fix merge conflicts

--- mlflow/store/dbfs_artifact_repo.py
+++ mlflow/store/dbfs_artifact_repo.py
@@ -70,9 +70,16 @@ class DbfsArtifactRepository(ArtifactRepository):
             http_endpoint = self._get_dbfs_endpoint(os.path.join(artifact_path, basename))
         else:
             http_endpoint = self._get_dbfs_endpoint(os.path.basename(local_file))
-        with open(local_file, 'rb') as f:
+        if os.stat(local_file).st_size == 0:
+            # The API frontend doesn't like it when we post empty files to it using
+            # `requests.request`, potentially due to the bug described in
+            # https://github.com/requests/requests/issues/4215
             self._databricks_api_request(
-                endpoint=http_endpoint, method='POST', data=f, allow_redirects=False)
+                endpoint=http_endpoint, method='POST', data="", allow_redirects=False)
+        else:
+            with open(local_file, 'rb') as f:
+                self._databricks_api_request(
+                    endpoint=http_endpoint, method='POST', data=f, allow_redirects=False)
 
     def log_artifacts(self, local_dir, artifact_path=None):
         if artifact_path:
