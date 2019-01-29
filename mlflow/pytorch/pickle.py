25c1882338510bb563e98063dc7f1a00b0a0dc93
Corey Zumar <corey.zumar@databricks.com>
2019-01-28 18:10:00 -0800

    Module docs improvements

--- mlflow/pytorch/pickle.py
+++ mlflow/pytorch/pickle.py
@@ -2,7 +2,8 @@
 This module imports contents from CloudPickle in a way that is compatible with the
 ``pickle_module`` parameter of PyTorch's model persistence function: ``torch.save``
 (see https://github.com/pytorch/pytorch/blob/692898fe379c9092f5e380797c32305145cd06e1/torch/
-serialization.py#L192).
+serialization.py#L192). It is included as a distinct module from :mod:`mlflow.pytorch` to avoid
+polluting the namespace with wildcard imports.
 
 Calling ``torch.save(..., pickle_module=mlflow.pytorch.pickle)`` will persist PyTorch model
 definitions using CloudPickle, leveraging improved pickling functionality such as the ability
