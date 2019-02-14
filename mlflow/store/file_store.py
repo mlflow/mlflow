75cf7fe13cbf6aaf887728ec9559cbf572c4a821
Mani Parkhe <mani@databricks.com>
2019-02-13 16:17:33 -0800

    MLFLOW_EXPERIMENT_NAME env variable and --experiment-name arguments for CLI command (#889)
    * MLFLOW_EXPERIMENT_NAME env variable and --experiment-name arguments for CLI command
    * get_experiment_by_name() for SQL store
    * tests for env variables (MLFLOW_EXPERIMENT_NAME and MLFLOW_EXPERIMENT_ID)

--- mlflow/store/file_store.py
+++ mlflow/store/file_store.py
@@ -64,7 +64,7 @@ class FileStore(AbstractStore):
         # Create root directory if needed
         if not exists(self.root_directory):
             mkdir(self.root_directory)
-            self._create_experiment_with_id(name="Default",
+            self._create_experiment_with_id(name=Experiment.DEFAULT_EXPERIMENT_NAME,
                                             experiment_id=Experiment.DEFAULT_EXPERIMENT_ID,
                                             artifact_uri=None)
         # Create trash folder if needed
@@ -220,10 +220,10 @@ class FileStore(AbstractStore):
                                   databricks_pb2.RESOURCE_DOES_NOT_EXIST)
         return experiment
 
-    def get_experiment_by_name(self, name):
+    def get_experiment_by_name(self, experiment_name):
         self._check_root_dir()
         for experiment in self.list_experiments(ViewType.ALL):
-            if experiment.name == name:
+            if experiment.name == experiment_name:
                 return experiment
         return None
 
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,7 @@
 from click.testing import CliRunner
 from mock import mock
 
-from mlflow.cli import server
+from mlflow.cli import server, run
 
 
 def test_server_static_prefix_validation():
@@ -19,3 +19,28 @@ def test_server_static_prefix_validation():
         result = CliRunner().invoke(server, ["--static-prefix", "/mlflow/"])
         assert "--static-prefix should not end with a '/'." in result.output
         run_server_mock.assert_not_called()
+
+
+def test_mlflow_run():
+    with mock.patch("mlflow.cli.projects") as mock_projects:
+        result = CliRunner().invoke(run)
+        mock_projects.run.assert_not_called()
+        assert 'Missing argument "URI"' in result.output
+
+    with mock.patch("mlflow.cli.projects") as mock_projects:
+        CliRunner().invoke(run, ["project_uri"])
+        mock_projects.run.assert_called_once()
+
+    with mock.patch("mlflow.cli.projects") as mock_projects:
+        CliRunner().invoke(run, ["--experiment-id", "5", "project_uri"])
+        mock_projects.run.assert_called_once()
+
+    with mock.patch("mlflow.cli.projects") as mock_projects:
+        CliRunner().invoke(run, ["--experiment-name", "random name", "project_uri"])
+        mock_projects.run.assert_called_once()
+
+    with mock.patch("mlflow.cli.projects") as mock_projects:
+        result = CliRunner().invoke(run, ["--experiment-id", "51",
+                                          "--experiment-name", "name blah", "uri"])
+        mock_projects.run.assert_not_called()
+        assert "Specify only one of 'experiment-name' or 'experiment-id' options." in result.output
