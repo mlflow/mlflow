bab054e10860ac5facaf3f60963d1906ab98c74a
Víctor Zabalza <vzabalza@gmail.com>
2019-02-18 10:58:16 +0000

    Add docstrings to TrackingStoreRegistry

--- mlflow/tracking/utils.py
+++ mlflow/tracking/utils.py
@@ -189,6 +189,19 @@ def _get_databricks_rest_store(store_uri, **_):
 
 
 class TrackingStoreRegistry:
+    """Scheme-based registry for tracking store implementations
+
+    This class allows the registration of a function or class to provide an
+    implementation for a given scheme of `store_uri` through the `register`
+    methods. Implementations declared though the entrypoints
+    `mlflow.tracking_store` group can be automatically registered through the
+    `register_entrypoints` method.
+
+    When instantiating a store through the `get_store` method, the scheme of
+    the store URI provided (or inferred from environment) will be used to
+    select which implementation to instantiate, which will be called with same
+    arguments passed to the `get_store` method.
+    """
 
     def __init__(self):
         self._registry = {}
@@ -197,7 +210,7 @@ class TrackingStoreRegistry:
         self._registry[scheme] = store_builder
 
     def register_entrypoints(self):
-        # Register tracking stores provided by other packages
+        """Register tracking stores provided by other packages"""
         for entrypoint in entrypoints.get_group_all("mlflow.tracking_store"):
             try:
                 self.register(entrypoint.name, entrypoint.load())
@@ -210,6 +223,17 @@ class TrackingStoreRegistry:
                 )
 
     def get_store(self, store_uri=None, artifact_uri=None):
+        """Get a store from the registry based on the scheme of store_uri
+
+        :param store_uri: The store URI. If None, it will be inferred from the environment. This URI
+                          is used to select which tracking store implementation to instantiate and
+                          is passed to the constructor of the implementation.
+        :param artifact_uri: Artifact repository URI. Passed through to the tracking store
+                             implementation.
+
+        :return: An instance of `mlflow.store.AbstractStore` that fulfills the store URI
+                 requirements.
+        """
         store_uri = store_uri if store_uri is not None else get_tracking_uri()
 
         if store_uri == 'databricks':
