78519e9e1e5567d694c6d8c0bc25690f278afa51
Andrew Crozier <wacrozier@gmail.com>
2019-02-20 16:40:37 +0000

    Add context provider registry and resolution logic

--- mlflow/tracking/context.py
+++ mlflow/tracking/context.py
@@ -1,7 +1,12 @@
 import os
 import sys
 import logging
+import itertools
+import warnings
 from abc import ABCMeta, abstractmethod
+
+import entrypoints
+from six.moves import reduce
 
 from mlflow.entities import SourceType
 from mlflow.utils.databricks_utils import (
@@ -126,3 +131,60 @@ class DatabricksNotebookContext(ContextProvider):
         if webapp_url is not None:
             tags[MLFLOW_DATABRICKS_WEBAPP_URL] = webapp_url
         return tags
+
+
+class ContextProviderRegistry(object):
+    """Registry for context provider implementations
+
+    This class allows the registration of a context provider which can be used to infer meta
+    information about the context of an MLflow experiment run. Implementations declared though the
+    entrypoints `mlflow.context_provider` group can be automatically registered through the
+    `register_entrypoints` method.
+    """
+
+    def __init__(self):
+        self._registry = []
+
+    def register(self, context_provider_cls):
+        self._registry.append(context_provider_cls())
+
+    def register_entrypoints(self):
+        """Register tracking stores provided by other packages"""
+        for entrypoint in entrypoints.get_group_all("mlflow.context_provider"):
+            try:
+                self.register(entrypoint.load())
+            except (AttributeError, ImportError) as exc:
+                warnings.warn(
+                    'Failure attempting to register context provider "{}": {}'.format(
+                        entrypoint.name, str(exc)
+                    ),
+                    stacklevel=2
+                )
+
+    def __iter__(self):
+        return iter(self._registry)
+
+
+_context_provider_registry = ContextProviderRegistry()
+_context_provider_registry.register(DefaultContext)
+_context_provider_registry.register(GitContext)
+_context_provider_registry.register(DatabricksNotebookContext)
+
+_context_provider_registry.register_entrypoints()
+
+
+def _merge_tags(base, new):
+    return dict(itertools.chain(base.items(), new.items()))
+
+
+def resolve_tags(tags=None):
+
+    tag_sets = []
+    for provider in _context_provider_registry:
+        if provider.in_context():
+            tag_sets.append(provider.tags())
+
+    if tags is not None:
+        tag_sets.append(tags)
+
+    return reduce(_merge_tags, tag_sets)
