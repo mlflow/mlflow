dc8d8c892d0b62d447b184db4940a44259a8a1ba
Corey Zumar <corey.zumar@databricks.com>
2019-02-26 10:55:24 -0800

    Simplify test fixture

--- tests/tracking/test_fluent.py
+++ tests/tracking/test_fluent.py
@@ -31,7 +31,7 @@ class HelperEnv:
             del os.environ[_EXPERIMENT_NAME_ENV_VAR]
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def reset_experiment_id():
     HelperEnv.set_values()
     mlflow.tracking.fluent._active_experiment_id = None
@@ -68,9 +68,7 @@ def test_get_experiment_id_from_env():
         assert _get_experiment_id_from_env() == exp_id
 
 
-def test_get_experiment_id_with_active_experiment_returns_active_experiment_id(
-        reset_experiment_id):
-    # pylint: disable=unused-argument
+def test_get_experiment_id_with_active_experiment_returns_active_experiment_id():
     # Create a new experiment and set that as active experiment
     with TempDir(chdr=True):
         name = "Random experiment %d" % random.randint(1, 1e6)
@@ -80,14 +78,11 @@ def test_get_experiment_id_with_active_experiment_returns_active_experiment_id(
         assert _get_experiment_id() == exp_id
 
 
-def test_get_experiment_id_with_no_active_experiments_returns_default_experiment_id(
-        reset_experiment_id):
-    # pylint: disable=unused-argument
+def test_get_experiment_id_with_no_active_experiments_returns_default_experiment_id():
     assert _get_experiment_id() == Experiment.DEFAULT_EXPERIMENT_ID
 
 
-def test_get_experiment_id_in_databricks_detects_notebook_id_by_default(reset_experiment_id):
-    # pylint: disable=unused-argument
+def test_get_experiment_id_in_databricks_detects_notebook_id_by_default():
     notebook_id = 768
 
     with mock.patch("mlflow.tracking.fluent.is_in_databricks_notebook") as notebook_detection_mock,\
@@ -97,9 +92,7 @@ def test_get_experiment_id_in_databricks_detects_notebook_id_by_default(reset_ex
         assert _get_experiment_id() == notebook_id
 
 
-def test_get_experiment_id_in_databricks_with_active_experiment_returns_active_experiment_id(
-        reset_experiment_id):
-    # pylint: disable=unused-argument
+def test_get_experiment_id_in_databricks_with_active_experiment_returns_active_experiment_id():
     with TempDir(chdr=True):
         exp_name = "random experiment %d" % random.randint(1, 1e6)
         exp_id = mlflow.create_experiment(exp_name)
@@ -115,9 +108,7 @@ def test_get_experiment_id_in_databricks_with_active_experiment_returns_active_e
         assert _get_experiment_id() == exp_id
 
 
-def test_get_experiment_id_in_databricks_with_experiment_defined_in_env_returns_env_experiment_id(
-        reset_experiment_id):
-    # pylint: disable=unused-argument
+def test_get_experiment_id_in_databricks_with_experiment_defined_in_env_returns_env_experiment_id():
     with TempDir(chdr=True):
         exp_name = "random experiment %d" % random.randint(1, 1e6)
         exp_id = mlflow.create_experiment(exp_name)
