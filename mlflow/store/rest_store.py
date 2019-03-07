de1cae6ce73dd5f93cf440b8c546e26960dc2047
Sid Murching <smurching@gmail.com>
2019-03-06 23:27:46 -0800

    Remove fluent API changes

--- mlflow/store/rest_store.py
+++ mlflow/store/rest_store.py
@@ -236,4 +236,9 @@ class RestStore(AbstractStore):
         self._call_endpoint(RestoreRun, req_body)
 
     def log_batch(self, run_id, metrics, params, tags):
-        raise MlflowException("The LogBatch REST API is not yet implemented")
+        metric_protos = [metric.to_proto() for metric in metrics]
+        param_protos = [param.to_proto() for param in params]
+        tag_protos = [tag.to_proto() for tag in tags]
+        req_body = message_to_json(
+            LogBatch(metrics=metric_protos, params=param_protos, tags=tag_protos, run_id=run_id))
+        self._call_endpoint(LogBatch, req_body)
