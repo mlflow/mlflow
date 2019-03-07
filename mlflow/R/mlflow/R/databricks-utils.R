dc902be7d7a3e55bb3c6688d56396457cd6f748b
tomasatdatabricks <tomas.nykodym@databricks.com>
2019-03-06 17:54:57 -0800

    Fix - added missing commas.

--- mlflow/R/mlflow/R/databricks-utils.R
+++ mlflow/R/mlflow/R/databricks-utils.R
@@ -105,10 +105,10 @@ mlflow_get_context_tags.mlflow_client <- function(client) {
 }
 
 MLFLOW_DATABRICKS_TAGS <- list(
-  MLFLOW_DATABRICKS_NOTEBOOK_ID = "mlflow.databricks.notebookID"
-  MLFLOW_DATABRICKS_NOTEBOOK_PATH = "mlflow.databricks.notebookPath"
-  MLFLOW_DATABRICKS_WEBAPP_URL = "mlflow.databricks.webappURL"
-  MLFLOW_DATABRICKS_RUN_URL = "mlflow.databricks.runURL"
-  MLFLOW_DATABRICKS_SHELL_JOB_ID = "mlflow.databricks.shellJobID"
+  MLFLOW_DATABRICKS_NOTEBOOK_ID = "mlflow.databricks.notebookID",
+  MLFLOW_DATABRICKS_NOTEBOOK_PATH = "mlflow.databricks.notebookPath",
+  MLFLOW_DATABRICKS_WEBAPP_URL = "mlflow.databricks.webappURL",
+  MLFLOW_DATABRICKS_RUN_URL = "mlflow.databricks.runURL",
+  MLFLOW_DATABRICKS_SHELL_JOB_ID = "mlflow.databricks.shellJobID",
   MLFLOW_DATABRICKS_SHELL_JOB_RUN_ID = "mlflow.databricks.shellJobRunID"
 )
