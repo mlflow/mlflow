0cfe8010599e5891df55dc102af4ee9675523d56
Andrew Crozier <wacrozier@gmail.com>
2019-03-10 07:21:41 +0000

    Generate run metadata with pluggable context provider system (#930)

--- tests/resources/mlflow-test-plugin/mlflow_test_plugin/__init__.py
+++ tests/resources/mlflow-test-plugin/mlflow_test_plugin/__init__.py
@@ -2,6 +2,7 @@ from six.moves import urllib
 
 from mlflow.store.file_store import FileStore
 from mlflow.store.local_artifact_repo import LocalArtifactRepository
+from mlflow.tracking.context import RunContextProvider
 
 
 class PluginFileStore(FileStore):
@@ -16,3 +17,13 @@ class PluginFileStore(FileStore):
 class PluginLocalArtifactRepository(LocalArtifactRepository):
     """LocalArtifactRepository provided through plugin system"""
     is_plugin = True
+
+
+class PluginRunContextProvider(RunContextProvider):
+    """RunContextProvider provided through plugin system"""
+
+    def in_context(self):
+        return False
+
+    def tags(self):
+        return {"test": "tag"}
