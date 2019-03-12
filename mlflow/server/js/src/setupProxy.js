508d5e109968a12da9fd041ed88636174c990532
Sid Murching <smurching@gmail.com>
2019-03-12 00:04:55 -0700

    Upgrade a number of significant dependency versions (including create-react-app, etc)

---
+++ mlflow/server/js/src/setupProxy.js
@@ -0,0 +1,11 @@
+const proxy = require('http-proxy-middleware');
+module.exports = function(app) {
+  // Configured as described in https://stackoverflow.com/a/52620241
+  // and https://www.npmjs.com/package/http-proxy-middleware#tldr
+  app.use(proxy('/ajax-api',
+    { target: 'http://localhost:5000' }
+  ));
+  app.use(proxy('/get-artifact',
+    { target: 'http://localhost:5000', ws: true }
+  ));
+};
