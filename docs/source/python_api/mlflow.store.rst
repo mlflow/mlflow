e0897e57defeb56e4d1573721a788867a3ca03b9
jaroslawk <jaroslaw.konczak@gmail.com>
2019-03-21 11:47:07 +0100

    updating store package documentation. assumes https://github.com/mlflow/mlflow/pull/1011 has been merged

---
+++ docs/source/python_api/mlflow.store.rst
@@ -0,0 +1,41 @@
+mlflow.store
+===============
+
+.. automodule:: mlflow.store
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. autoclass:: mlflow.store.artifact_repo.ArtifactRepository
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. autoclass:: mlflow.store.artifact_repository_registry.ArtifactRepositoryRegistry
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. autoclass:: mlflow.store.azure_blob_artifact_repo.AzureBlobArtifactRepository
+    :members:
+
+.. autoclass:: mlflow.store.dbfs_artifact_repo.DbfsArtifactRepository
+    :members:
+
+.. autoclass:: mlflow.store.ftp_artifact_repo.FTPArtifactRepository
+    :members:
+
+.. autoclass:: mlflow.store.gcs_artifact_repo.GCSArtifactRepository
+    :members:
+
+.. autoclass:: mlflow.store.hdfs_artifact_repo.HdfsArtifactRepository
+    :members:
+
+.. autoclass:: mlflow.store.local_artifact_repo.LocalArtifactRepository
+    :members:
+
+.. autoclass:: mlflow.store.s3_artifact_repo.S3ArtifactRepository
+    :members:
+
+.. autoclass:: mlflow.store.sftp_artifact_repo.SFTPArtifactRepository
+    :members:
--- mlflow/store/__init__.py
+++ mlflow/store/__init__.py
@@ -1,9 +1,20 @@
 """
-An MLflow tracking server has two properties related to how data is stored: *backend store* to
-record ML experiments, runs, parameters, metrics, etc., and *artifact store* to store run
-artifacts like models, plots, images, etc.
+The ``mlflow.store`` module provides an API for saving artifacts into different *artifact store* to
+persist run artifacts like models, plots, images, etc.
 
-Several constants are used by multiple backend store implementations.
+Currently there are implementations available:
+
+- :py:mod:`mlflow.store.artifact_repo.ArtifactRepository`
+- :py:mod:`mlflow.store.artifact_repository_registry.ArtifactRepositoryRegistry`
+- :py:mod:`mlflow.store.azure_blob_artifact_repo.AzureBlobArtifactRepository`
+- :py:mod:`mlflow.store.dbfs_artifact_repo.DbfsArtifactRepository`
+- :py:mod:`mlflow.store.ftp_artifact_repo.FTPArtifactRepository`
+- :py:mod:`mlflow.store.gcs_artifact_repo.GCSArtifactRepository`
+- :py:mod:`mlflow.store.hdfs_artifact_repo.HdfsArtifactRepository`
+- :py:mod:`mlflow.store.local_artifact_repo.LocalArtifactRepository`
+- :py:mod:`mlflow.store.s3_artifact_repo.S3ArtifactRepository`
+- :py:mod:`mlflow.store.sftp_artifact_repo.SFTPArtifactRepository`
+
 """
 
 # Path to default location for backend when using local FileStore or ArtifactStore.
