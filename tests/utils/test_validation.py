131d7834e9e0eaf93ec1acce5480008dad165d9e
Eduardo de Leon <eddeleon@microsoft.com>
2019-03-21 18:23:05 -0400

    Fix validation test

--- tests/utils/test_validation.py
+++ tests/utils/test_validation.py
@@ -46,8 +46,9 @@ def test_validate_tag_name():
 def test_validate_run_id():
     for good_id in ["a" * 32, "f0" * 16, "abcdef0123456789" * 2, "a" * 33, "a" * 31,
                     "A" * 32, "g" * 32, "a_" * 32]:
-    for bad_id in ["a/bc" * 8, "", "a" * 129, "*" * 5]:
-        with pytest.raises(MlflowException, match="Invalid run ID"):
+        _validate_run_id(good_id)
+    for bad_id in ["a/bc" * 8, "", "a" * 400, "*" * 5]:
+        with pytest.raises(MlflowException, match="Invalid run ID") as e:
             _validate_run_id(bad_id)
         assert e.value.error_code == ErrorCode.Name(INVALID_PARAMETER_VALUE)
 
