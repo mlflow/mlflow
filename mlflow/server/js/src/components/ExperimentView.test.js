18ec5d8c6a6137d121fe51d982c9f7a16ba74502
Sid Murching <smurching@gmail.com>
2019-03-29 16:29:42 -0700

    Revert "Try testing state directly?"
    This reverts commit fb2cd9bed57bf7f162273e9f2c01da0c6ea50dd8.

--- mlflow/server/js/src/components/ExperimentView.test.js
+++ mlflow/server/js/src/components/ExperimentView.test.js
@@ -25,17 +25,17 @@ const getExperimentViewMock = () => {
 
 test('Entering filter input updates component state', () => {
   const wrapper = getExperimentViewMock();
+  wrapper.instance().setState = jest.fn();
   // Test entering param filter input
   wrapper.find('.ExperimentView-paramKeyFilter input').first().simulate(
     'change', {target: { value: 'param name'}});
-  wrapper.update();
-  expect(wrapper.state('paramKeyFilterInput')).toEqual('param name');
+  expect(wrapper.instance().setState).toBeCalledWith({paramKeyFilterInput: 'param name'});
   // Test entering metric filter input
   wrapper.find('.ExperimentView-metricKeyFilter input').first().simulate(
     'change', {target: { value: 'metric name'}});
-  expect(wrapper.state('metricKeyFilterInput')).toEqual('metric name');
+  expect(wrapper.instance().setState).toBeCalledWith({metricKeyFilterInput: 'metric name'});
   // Test entering search input
   wrapper.find('.ExperimentView-search-input input').first().simulate(
     'change', {target: { value: 'search input string'}});
-  expect(wrapper.state('searchInput')).toEqual('search input string');
+  expect(wrapper.instance().setState).toBeCalledWith({searchInput: 'search input string'});
 });
