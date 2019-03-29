2f55d007795a2f910a8dacd349fe37dcd0fa370b
Sid Murching <smurching@gmail.com>
2019-03-29 16:30:37 -0700

    Remove test that's also included in another PR

--- mlflow/server/js/src/components/ExperimentView.test.js
+++ mlflow/server/js/src/components/ExperimentView.test.js
@@ -23,23 +23,6 @@ const getExperimentViewMock = () => {
   />);
 };
 
-test('Entering filter input updates component state', () => {
-  const wrapper = getExperimentViewMock();
-  wrapper.instance().setState = jest.fn();
-  // Test entering param filter input
-  wrapper.find('.ExperimentView-paramKeyFilter input').first().simulate(
-    'change', {target: { value: 'param name'}});
-  expect(wrapper.instance().setState).toBeCalledWith({paramKeyFilterInput: 'param name'});
-  // Test entering metric filter input
-  wrapper.find('.ExperimentView-metricKeyFilter input').first().simulate(
-    'change', {target: { value: 'metric name'}});
-  expect(wrapper.instance().setState).toBeCalledWith({metricKeyFilterInput: 'metric name'});
-  // Test entering search input
-  wrapper.find('.ExperimentView-search-input input').first().simulate(
-    'change', {target: { value: 'search input string'}});
-  expect(wrapper.instance().setState).toBeCalledWith({searchInput: 'search input string'});
-});
-
 test('Expanding and collapsing a parent run updates component state', () => {
   const wrapper = getExperimentViewMock();
   wrapper.instance().onExpand(Fixtures.sortedRunIds[0], Fixtures.childRunIds);
