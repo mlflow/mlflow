f76bcc8c4628eb8e4e8d021e96df9c64125f19d1
Andrew Crozier <wacrozier@gmail.com>
2019-04-02 12:00:09 +0100

    Need to subclass to pass Python 2.7 tests

--- tests/store/test_abstract_store.py
+++ tests/store/test_abstract_store.py
@@ -1,56 +1,84 @@
 import mock
-import pytest
 
 from mlflow.store.abstract_store import AbstractStore
 
 
-@pytest.fixture
-def mock_abstract_methods():
-    with mock.patch.object(AbstractStore, "list_experiments"), \
-        mock.patch.object(AbstractStore, "create_experiment"), \
-        mock.patch.object(AbstractStore, "get_experiment"), \
-        mock.patch.object(AbstractStore, "delete_experiment"), \
-        mock.patch.object(AbstractStore, "restore_experiment"), \
-        mock.patch.object(AbstractStore, "rename_experiment"), \
-        mock.patch.object(AbstractStore, "get_run"), \
-        mock.patch.object(AbstractStore, "update_run_info"), \
-        mock.patch.object(AbstractStore, "create_run"), \
-        mock.patch.object(AbstractStore, "delete_run"), \
-        mock.patch.object(AbstractStore, "restore_run"), \
-        mock.patch.object(AbstractStore, "get_metric_history"), \
-        mock.patch.object(AbstractStore, "search_runs"), \
-            mock.patch.object(AbstractStore, "log_batch"):
-        yield
+class ConcreteStore(AbstractStore):
+
+    def list_experiments(self, *args, **kwargs):
+        pass
+
+    def create_experiment(self, *args, **kwargs):
+        pass
+
+    def get_experiment(self, *args, **kwargs):
+        pass
+
+    def delete_experiment(self, *args, **kwargs):
+        pass
+
+    def restore_experiment(self, *args, **kwargs):
+        pass
+
+    def rename_experiment(self, *args, **kwargs):
+        pass
+
+    def get_run(self, *args, **kwargs):
+        pass
+
+    def update_run_info(self, *args, **kwargs):
+        pass
+
+    def create_run(self, *args, **kwargs):
+        pass
+
+    def delete_run(self, *args, **kwargs):
+        pass
+
+    def restore_run(self, *args, **kwargs):
+        pass
+
+    def get_metric_history(self, *args, **kwargs):
+        pass
+
+    def search_runs(self, *args, **kwargs):
+        pass
+
+    def log_batch(self, *args, **kwargs):
+        pass
 
 
-def test_log_metric(mock_abstract_methods):  # pylint: disable=unused-argument
+def test_log_metric():
     run_id = mock.Mock()
     metric = mock.Mock()
 
-    store = AbstractStore()
-    store.log_metric(run_id, metric)
-    store.log_batch.assert_called_once_with(
-        run_id, metrics=[metric], params=[], tags=[]
-    )
+    with mock.patch.object(ConcreteStore, "log_batch"):
+        store = ConcreteStore()
+        store.log_metric(run_id, metric)
+        store.log_batch.assert_called_once_with(
+            run_id, metrics=[metric], params=[], tags=[]
+        )
 
 
-def test_log_param(mock_abstract_methods):  # pylint: disable=unused-argument
+def test_log_param():
     run_id = mock.Mock()
     param = mock.Mock()
 
-    store = AbstractStore()
-    store.log_param(run_id, param)
-    store.log_batch.assert_called_once_with(
-        run_id, metrics=[], params=[param], tags=[]
-    )
+    with mock.patch.object(ConcreteStore, "log_batch"):
+        store = ConcreteStore()
+        store.log_param(run_id, param)
+        store.log_batch.assert_called_once_with(
+            run_id, metrics=[], params=[param], tags=[]
+        )
 
 
-def test_set_tag(mock_abstract_methods):  # pylint: disable=unused-argument
+def test_set_tag():
     run_id = mock.Mock()
     tag = mock.Mock()
 
-    store = AbstractStore()
-    store.set_tag(run_id, tag)
-    store.log_batch.assert_called_once_with(
-        run_id, metrics=[], params=[], tags=[tag]
-    )
+    with mock.patch.object(ConcreteStore, "log_batch"):
+        store = ConcreteStore()
+        store.set_tag(run_id, tag)
+        store.log_batch.assert_called_once_with(
+            run_id, metrics=[], params=[], tags=[tag]
+        )
