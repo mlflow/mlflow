68456e3a33beab9d97f8b2fbe88febe28a5b7642
tomasatdatabricks <tomas.nykodym@databricks.com>
2019-04-04 15:06:10 -0700

    Removed debug print.

--- mlflow/R/mlflow/tests/testthat/test-tracking.R
+++ mlflow/R/mlflow/tests/testthat/test-tracking.R
@@ -15,7 +15,6 @@ test_that("mlflow_client_list_experiments() works properly", {
   mlflow_client_create_experiment(client = client, "foo1", "art_loc1")
   mlflow_client_create_experiment(client = client, "foo2", "art_loc2")
   experiments_list <- mlflow_client_list_experiments(client = client)
-  print(experiments_list)
   expect_setequal(experiments_list$experiment_id, c("0", "1", "2"))
   expect_setequal(experiments_list$name, c("Default", "foo1", "foo2"))
   expect_setequal(experiments_list$artifact_location, c("mlruns/0", "art_loc1", "art_loc2"))
