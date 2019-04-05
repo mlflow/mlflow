d812b70a6350eb8a6e85a4d0d41131731ecb530d
Sid Murching <smurching@gmail.com>
2019-04-05 09:32:43 -0700

    Add large-only to exclusively run large tests

--- run-large-tests.sh
+++ run-large-tests.sh
@@ -9,18 +9,18 @@
 #     echo "Sagemaker container build failed, output:";
 #     cat $SAGEMAKER_OUT;
 #   fi
-pytest --cov=mlflow --verbose --large --ignore=tests/h2o --ignore=tests/keras \
+pytest --cov=mlflow --verbose --large-only --ignore=tests/h2o --ignore=tests/keras \
   --ignore=tests/pytorch --ignore=tests/pyfunc --ignore=tests/sagemaker --ignore=tests/sklearn \
   --ignore=tests/spark --ignore=tests/tensorflow
 # Run ML framework tests in their own Python processes. TODO: find a better method of isolating
 # tests.
-pytest --verbose tests/h2o --large
+pytest --verbose tests/h2o --large-only
 # TODO(smurching): Re-enable Keras tests once they're no longer flaky
-# - pytest --verbose tests/keras --large
-pytest --verbose tests/pytorch --large
-pytest --verbose tests/pyfunc --large
-pytest --verbose tests/sagemaker --large
-pytest --verbose tests/sagemaker/mock --large
-pytest --verbose tests/sklearn --large
-pytest --verbose tests/spark --large
-pytest --verbose tests/tensorflow --large
+# - pytest --verbose tests/keras --large-only
+pytest --verbose tests/pytorch --large-only
+pytest --verbose tests/pyfunc --large-only
+pytest --verbose tests/sagemaker --large-only
+pytest --verbose tests/sagemaker/mock --large-only
+pytest --verbose tests/sklearn --large-only
+pytest --verbose tests/spark --large-only
+pytest --verbose tests/tensorflow --large-only
