d673fb945efcb7c458f752639634f6f24d8445a0
Jim Thompson <jimthompson5802@aol.com>
2019-04-06 16:55:34 -0400

    checkpoint code

--- tests/examples/test_sklearn_examples.py
+++ tests/examples/test_sklearn_examples.py
@@ -2,9 +2,16 @@ from __future__ import print_function
 
 from subprocess import Popen, PIPE
 import pytest
+import shutil
+import os.path
+
+
+
+
 
 def test_sklearn_logistic_regression():
-    command="cd examples/sklearn_logistic_regression/ && python train.py"
+
+    command="cd examples/sklearn_logistic_regression/ &&  rm -fr mlruns && python train.py"
     stdout_response = "Score: 0.6666666666666666\nModel saved in run"
 
     test_run = Popen(command,shell=True,stdout=PIPE)
@@ -19,7 +26,21 @@ def test_sklearn_logistic_regression():
 
 
 def test_sklearn_elasticnet_wine():
-    command = "cd examples/sklearn_elasticnet_wine && mlflow run . -P alpha=0.5"
+    # setup conda environment for test
+    setup_run = Popen("conda env remove -n tutorial -y",shell=True,stdout=PIPE,stderr=PIPE)  #ignore any errors
+    setup_run.wait()
+    print(setup_run.stdout.read())
+    print(setup_run.stderr.read())
+
+    setup_run = Popen("conda env create -f examples/sklearn_elasticnet_wine/conda.yaml",shell=True,
+                      stderr=PIPE, stdout=PIPE)
+    setup_run.wait()
+    print(setup_run.stdout.read())
+    print(setup_run.stderr.read())
+    assert setup_run.returncode == 0
+
+
+    command = "cd examples/sklearn_elasticnet_wine && rm -fr mlruns && . activate tutorial && mlflow run . -P alpha=0.5 --no-conda"
     stdout_response = "Elasticnet model (alpha=0.500000, l1_ratio=0.100000):\n" + \
         "  RMSE: 0.7947931019036529\n" + \
         "  MAE: 0.6189130834228138\n" + \
@@ -31,25 +52,29 @@ def test_sklearn_elasticnet_wine():
     test_run.wait()
     assert test_run.returncode == 0
 
-    #  check for expected output
-    out_text = test_run.stdout.read()
-    print(out_text)
-    assert out_text[:len(stdout_response)] == stdout_response
+    # #  check for expected output
+    # out_text = test_run.stdout.read()
+    # print(out_text)
+    # assert out_text[:len(stdout_response)] == stdout_response
 
 
-def test_sklearn_elasticnet_diabetes():
-    command = "cd examples/sklearn_elasticnet_diabetes/linux && mlflow run ."
-    stdout_response = "Elasticnet model (alpha=0.010000, l1_ratio=0.100000):\n" +\
-        "  RMSE: 70.90385970638309\n  MAE: 59.63089172068812\n  R2: 0.23986643454764633\n" +\
-        "Computing regularization path using the elastic net.\n"
+# def test_sklearn_elasticnet_diabetes():
+#     command = "cd examples/sklearn_elasticnet_diabetes/linux  && rm -fr mlruns && mlflow run ."
+#     stdout_response = "Elasticnet model (alpha=0.010000, l1_ratio=0.100000):\n" +\
+#         "  RMSE: 70.90385970638309\n  MAE: 59.63089172068812\n  R2: 0.23986643454764633\n" +\
+#         "Computing regularization path using the elastic net.\n"
+#
+#     test_run = Popen(command,shell=True,stdout=PIPE)
+#
+#     # check exit code for return code 0
+#     test_run.wait()
+#     assert test_run.returncode == 0
+#
+#     #  check for expected output
+#     out_text = test_run.stdout.read()
+#     print(out_text)
+#     assert out_text[:len(stdout_response)] ==
 
-    test_run = Popen(command,shell=True,stdout=PIPE)
 
-    # check exit code for return code 0
-    test_run.wait()
-    assert test_run.returncode == 0
-
-    #  check for expected output
-    out_text = test_run.stdout.read()
-    print(out_text)
-    assert out_text[:len(stdout_response)] == stdout_response
+if __name__ == "__main__":
+    test_sklearn_elasticnet_wine()
