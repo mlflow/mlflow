10c8444cf76d6a9d39493c243c508f3d041c60a2
Eduardo de Leon <eddeleon@microsoft.com>
2019-04-09 18:23:22 -0400

    Remove all references to Long experimentId in java implementation

--- mlflow/java/client/src/main/java/org/mlflow/tracking/MlflowProtobufMapper.java
+++ mlflow/java/client/src/main/java/org/mlflow/tracking/MlflowProtobufMapper.java
@@ -16,21 +16,21 @@ class MlflowProtobufMapper {
     return print(builder);
   }
 
-  String makeDeleteExperimentRequest(long experimentId) {
+  String makeDeleteExperimentRequest(String experimentId) {
     DeleteExperiment.Builder builder = DeleteExperiment.newBuilder();
-    builder.setExperimentId(Long.toString(experimentId));
+    builder.setExperimentId(experimentId);
     return print(builder);
   }
 
-  String makeRestoreExperimentRequest(long experimentId) {
+  String makeRestoreExperimentRequest(String experimentId) {
     RestoreExperiment.Builder builder = RestoreExperiment.newBuilder();
-    builder.setExperimentId(Long.toString(experimentId));
+    builder.setExperimentId(experimentId);
     return print(builder);
   }
 
-  String makeUpdateExperimentRequest(long experimentId, String newExperimentName) {
+  String makeUpdateExperimentRequest(String experimentId, String newExperimentName) {
     UpdateExperiment.Builder builder = UpdateExperiment.newBuilder();
-    builder.setExperimentId(Long.toString(experimentId));
+    builder.setExperimentId(experimentId);
     builder.setNewName(newExperimentName);
     return print(builder);
   }
