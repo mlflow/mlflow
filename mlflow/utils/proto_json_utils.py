27ab7151dbbbe92ff8e73c3f6f9bb80ab1c5bf6b
Eduardo De Leon <eddeleon@microsoft.com>
2019-04-11 11:34:48 -0400

    Revert "Handle and test experiment_id int to string before deserialization to avoid failure in create_experiment with existing servers"
    This reverts commit 0a8d9446523bede0a2ab975afc1c2a9dbdf08f60.

--- mlflow/utils/proto_json_utils.py
+++ mlflow/utils/proto_json_utils.py
@@ -6,16 +6,6 @@ def message_to_json(message):
     return MessageToJson(message, preserving_proto_field_name=True)
 
 
-def handle_backcompat(js_dict):
-    #  experiment_id changed from int to string, ParseDict handles string to int but not back
-    #  This allows new clients to interact with old servers
-    experiment_id_key = "experiment_id"
-    if experiment_id_key in js_dict and isinstance(js_dict[experiment_id_key], int):
-        js_dict[experiment_id_key] = str(js_dict[experiment_id_key])
-    return js_dict
-
-
 def parse_dict(js_dict, message):
-    js_dict = handle_backcompat(js_dict)
     """Parses a JSON dictionary into a message proto, ignoring unknown fields in the JOSN."""
     ParseDict(js_dict=js_dict, message=message, ignore_unknown_fields=True)
