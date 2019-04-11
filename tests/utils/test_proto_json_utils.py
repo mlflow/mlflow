fba009482f5bcd662d03f934d1b6ffb1506af292
Eduardo De Leon <eddeleon@microsoft.com>
2019-04-11 10:47:06 -0400

    Handle and test experiment_id int to string before deserialization to avoid failure in create_experiment with existing servers

--- tests/utils/test_proto_json_utils.py
+++ tests/utils/test_proto_json_utils.py
@@ -33,3 +33,13 @@ def test_parse_dict_int_as_string_backcompat():
     parse_dict(in_json, message)
     experiment = Metric.from_proto(message)
     assert experiment.timestamp == 123
+
+
+def test_parse_dict_string_as_int():
+    in_json = {"experiment_id": 123, "name": "name", "unknown": "field"}
+    message = ProtoExperiment()
+    parse_dict(in_json, message)
+    experiment = Experiment.from_proto(message)
+    assert experiment.experiment_id == "123"
+    assert experiment.name == 'name'
+    assert experiment.artifact_location == ''
