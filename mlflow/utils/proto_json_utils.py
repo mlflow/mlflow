255136770db41ac6bbcd9dbdb3fadec0cc728093
Eduardo De Leon <eddeleon@microsoft.com>
2019-04-11 17:34:28 -0400

    Add deserializer for old requests, add tests for permutations

--- mlflow/utils/proto_json_utils.py
+++ mlflow/utils/proto_json_utils.py
@@ -6,6 +6,23 @@ def message_to_json(message):
     return MessageToJson(message, preserving_proto_field_name=True)
 
 
+def backcompat_helper(js_dict):
+    #  Update int experiment_id from old servers to string
+    for key in js_dict:
+        if key == "experiment_ids" and isinstance(js_dict[key], list):
+            js_dict[key] = [str(val) for val in js_dict[key]]
+        elif isinstance(js_dict[key], list):
+            for child in js_dict[key]:
+                if isinstance(js_dict[key][child], dict):
+                    backcompat_helper(js_dict[key][child])
+        elif isinstance(js_dict[key], dict):
+            backcompat_helper(js_dict[key])
+        else:
+            if key == "experiment_id" and isinstance(js_dict[key], int):
+                js_dict[key] = str(js_dict[key])
+
+
 def parse_dict(js_dict, message):
     """Parses a JSON dictionary into a message proto, ignoring unknown fields in the JOSN."""
+    backcompat_helper(js_dict)
     ParseDict(js_dict=js_dict, message=message, ignore_unknown_fields=True)
--- tests/utils/test_proto_json_utils.py
+++ tests/utils/test_proto_json_utils.py
@@ -1,10 +1,11 @@
 import json
+import pytest
 
 from mlflow.entities import Experiment, Metric
 from mlflow.protos.service_pb2 import Experiment as ProtoExperiment
 from mlflow.protos.service_pb2 import Metric as ProtoMetric
 
-from mlflow.utils.proto_json_utils import message_to_json, parse_dict
+from mlflow.utils.proto_json_utils import message_to_json, parse_dict, backcompat_helper
 
 
 def test_message_to_json():
@@ -33,3 +34,64 @@ def test_parse_dict_int_as_string_backcompat():
     parse_dict(in_json, message)
     experiment = Metric.from_proto(message)
     assert experiment.timestamp == 123
+
+
+def test_parse_legacy_experiment():
+    in_json = {"experiment_id": 123, "name": "name", "unknown": "field"}
+    message = ProtoExperiment()
+    parse_dict(in_json, message)
+    experiment = Experiment.from_proto(message)
+    assert experiment.experiment_id == "123"
+    assert experiment.name == 'name'
+    assert experiment.artifact_location == ''
+
+
+def test_back_compat():
+    in_json = {"experiment_id": 123,
+               "name": "name",
+               "unknown": "field",
+               "experiment_ids": [1, 2, 3, 4, 5],
+               "things": {"experiment_id": 4,
+                          "more_things": {"experiment_id": 7, "experiment_ids": [2, 3, 4, 5]}}}
+    verify_string_experiment_ids(in_json, int)
+    backcompat_helper(in_json)
+    verify_string_experiment_ids(in_json, str)
+
+
+def test_verify_experiment_id_type():
+    in_json = {"experiment_id": 123,
+               "name": "name",
+               "unknown": "field",
+               "experiment_ids": [1, 2, 3, 4, 5],
+               "things": {"experiment_id": 4,
+                          "more_things": {"experiment_id": 7, "experiment_ids": ["2", 3, 4, 5]}}}
+    with pytest.raises(AssertionError):
+        verify_string_experiment_ids(in_json, int)
+    with pytest.raises(AssertionError):
+        verify_string_experiment_ids(in_json, str)
+
+    valid_int_json = {"experiment_id": 123,
+                      "name": "name",
+                      "unknown": "field",
+                      "experiment_ids": [1, 2, 3, 4, 5],
+                      "things": {"experiment_id": 4,
+                                 "more_things": {"experiment_id": 7,
+                                                 "experiment_ids": [2, 3, 4, 5]}}}
+    with pytest.raises(AssertionError):
+        verify_string_experiment_ids(valid_int_json, str)
+    verify_string_experiment_ids(valid_int_json, int)
+
+
+def verify_string_experiment_ids(js_dict, expected_type):
+    for key in js_dict:
+        if key == "experiment_id":
+            assert type(js_dict[key]) == expected_type
+        elif key == "experiment_ids":
+            for val in js_dict[key]:
+                assert type(val) == expected_type
+        elif isinstance(js_dict[key], dict):
+            verify_string_experiment_ids(js_dict[key], expected_type)
+        elif isinstance(js_dict[key], list):
+            for val in js_dict[key]:
+                if isinstance(val, dict):
+                    verify_string_experiment_ids(js_dict[key], expected_type)
