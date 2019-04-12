83a1a8e3ac42c64362199458a911d60da1c08fa9
tomasatdatabricks <33237569+tomasatdatabricks@users.noreply.github.com>
2019-04-11 18:54:42 -0700

    [mlflow/R] Fix mlflow_run (#1119)
    Fixes mlflow_run on R when running an MLflow project on Databricks. Also adds support for passing experiment name to mlflow_run - if no experiment name or ID is specified, we attempt to use the currently active experiment for the project run.

--- mlflow/R/mlflow/tests/testthat/test-run.R
+++ mlflow/R/mlflow/tests/testthat/test-run.R
@@ -9,3 +9,40 @@ test_that("mlflow can run and save model", {
   expect_true(dir.exists("mlruns/0"))
   expect_true(file.exists("mlruns/0/meta.yaml"))
 })
+
+
+test_that("mlflow run uses active experiment if not specified", {
+  with_mock(.env = "mlflow", mlflow_get_active_experiment_id = function() {
+    123}, {
+      with_mock(.env = "mlflow", mlflow_cli = function(...){
+        args <- list(...)
+        expect_true("--experiment-id" %in% args)
+        expect_false("--experiment-name" %in% args)
+        id <- which(args == "--experiment-id") + 1
+        expect_true(args[[id]] == 123)
+        list(stderr = "=== Run (ID '48734e7e2e8f44228a11c0c2cbcdc8b0') succeeded ===")
+      }, {
+        mlflow_run("project")
+      })
+      with_mock(.env = "mlflow", mlflow_cli = function(...){
+        args <- list(...)
+        expect_true("--experiment-id" %in% args)
+        expect_false("--experiment-name" %in% args)
+        id <- which(args == "--experiment-id") + 1
+        expect_true(args[[id]] == 321)
+        list(stderr = "=== Run (ID '48734e7e2e8f44228a11c0c2cbcdc8b0') succeeded ===")
+      }, {
+        mlflow_run("project", experiment_id = 321)
+      })
+      with_mock(.env = "mlflow", mlflow_cli = function(...){
+        args <- list(...)
+        expect_false("--experiment-id" %in% args)
+        expect_true("--experiment-name" %in% args)
+        id <- which(args == "--experiment-name") + 1
+        expect_true(args[[id]] == "name")
+        list(stderr = "=== Run (ID '48734e7e2e8f44228a11c0c2cbcdc8b0') succeeded ===")
+      }, {
+        mlflow_run("project", experiment_name = "name")
+      })
+  })
+})
