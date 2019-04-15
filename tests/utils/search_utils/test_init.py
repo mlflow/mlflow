5d6092a7b4240da12a1fe0ca93b48fc3065ad6b0
Andrew Crozier <wacrozier@gmail.com>
2019-04-15 17:47:21 +0100

    Test combination logic in SearchFilter

--- tests/utils/search_utils/test_init.py
+++ tests/utils/search_utils/test_init.py
@@ -1,21 +1,57 @@
 import pytest
 import mock
 
+import mlflow.utils.search_utils
 from mlflow.utils.search_utils import SearchFilter
 from mlflow.exceptions import MlflowException
 
 
-def test_search_filter_basics():
-    search_filter = "This is a filter string"
-    anded_expressions = [mock.Mock(), mock.Mock()]
+COMPARISON_TEST_CASES = [
+    ([], True),
+    ([True, True], True),
+    ([True, False, True], False),
+    ([False, False], False)
+]
 
-    # only anded_expressions
-    SearchFilter(anded_expressions=anded_expressions)
 
-    # only search filter
-    SearchFilter(filter_string=search_filter)
+@pytest.mark.parametrize("comparison_results, expected_result", COMPARISON_TEST_CASES)
+def test_search_filter_filter_string(comparison_results, expected_result):
 
-    # both
+    filter_string = mock.Mock()
+    comparisons = [mock.Mock(filter=mock.Mock(return_value=r)) for r in comparison_results]
+    run = mock.Mock()
+
+    search_filter = SearchFilter(filter_string)
+
+    with mock.patch("mlflow.utils.search_utils.parse_filter_string", return_value=comparisons):
+        assert search_filter.filter(run) is expected_result
+        mlflow.utils.search_utils.parse_filter_string.assert_called_once_with(filter_string)
+
+    for comparison in comparisons:
+        comparison.filter.assert_called_once_with(run)
+
+
+@pytest.mark.parametrize("comparison_results, expected_result", COMPARISON_TEST_CASES)
+def test_search_filter_anded_expressions(comparison_results, expected_result):
+
+    anded_expressions = [mock.Mock() for _ in comparison_results]
+    comparisons = [mock.Mock(filter=mock.Mock(return_value=r)) for r in comparison_results]
+    run = mock.Mock()
+
+    search_filter = SearchFilter(anded_expressions=anded_expressions)
+
+    with mock.patch("mlflow.utils.search_utils.search_expression_to_comparison",
+                    side_effect=comparisons):
+        assert search_filter.filter(run) is expected_result
+        assert mlflow.utils.search_utils.search_expression_to_comparison.call_args_list == [
+            mock.call(expression) for expression in anded_expressions
+        ]
+
+    for comparison in comparisons:
+        comparison.filter.assert_called_once_with(run)
+
+
+def test_search_filter_both():
     with pytest.raises(MlflowException,
                        match="Can specify only one of 'filter' or 'search_expression'"):
-        SearchFilter(anded_expressions=anded_expressions, filter_string=search_filter)
+        SearchFilter(filter_string=mock.Mock(), anded_expressions=mock.Mock())
