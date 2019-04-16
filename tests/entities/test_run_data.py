9199dfcdf27b7395e5094acececa52b26ca4e6e2
Siddharth Murching <smurching@gmail.com>
2019-04-16 16:57:23 -0700

    Expose RunData fields (metrics, params, tags) as dictionaries in Python API (#1078)
    This PR makes a breaking change to the Python API, migrating the fields (metrics, params, tags) of RunData to dicts from lists.
    
    In particular: 
    * RunData.metrics is now a dictionary of metric key (string) to metric value.
    * RunData.params is a dictionary of param key (string) to param value.
    * RunData.tags is a dictionary of tag key (string) to tag value.
    
    See the PR description (https://github.com/mlflow/mlflow/pull/1078) for information on how to migrate user code to handle this change, to be included in the next MLflow release changelog.

--- tests/entities/test_run_data.py
+++ tests/entities/test_run_data.py
@@ -1,63 +1,50 @@
 import time
 import unittest
 
-from mlflow.entities import Metric, Param, RunData, RunTag
+from mlflow.entities import Metric, RunData, Param, RunTag
 from tests.helper_functions import random_str, random_int
 
 
 class TestRunData(unittest.TestCase):
-    def _check_metrics(self, metrics_1, metrics_2):
-        for metric in metrics_1:
-            self.assertIsInstance(metric, Metric)
-        self.assertEqual(set([m.key for m in metrics_1]), set([m.key for m in metrics_2]))
-        self.assertEqual(set([m.value for m in metrics_1]), set([m.value for m in metrics_2]))
-        self.assertEqual(set([m.timestamp for m in metrics_1]),
-                         set([m.timestamp for m in metrics_2]))
+    def _check_metrics(self, metric_objs, metrics_dict, expected_metrics):
+        self.assertEqual(set([m.key for m in metric_objs]),
+                         set([m.key for m in expected_metrics]))
+        self.assertEqual(set([m.value for m in metric_objs]),
+                         set([m.value for m in expected_metrics]))
+        self.assertEqual(set([m.timestamp for m in metric_objs]),
+                         set([m.timestamp for m in expected_metrics]))
+        assert len(metrics_dict) == len(expected_metrics)
+        assert metrics_dict == {m.key: m.value for m in expected_metrics}
 
-    def _check_params(self, params_1, params_2):
-        for param in params_1:
-            self.assertIsInstance(param, Param)
-        self.assertEqual(set([p.key for p in params_1]), set([p.key for p in params_2]))
-        self.assertEqual(set([p.value for p in params_1]), set([p.value for p in params_2]))
+    def _check_params(self, params_dict, expected_params):
+        self.assertEqual(params_dict, {p.key: p.value for p in expected_params})
 
-    def _check_tags(self, tags_1, tags_2):
-        for tag in tags_1:
-            self.assertIsInstance(tag, RunTag)
-        self.assertEqual(set([t.key for t in tags_1]), set([t.key for t in tags_2]))
-        self.assertEqual(set([t.value for t in tags_2]), set([t.value for t in tags_2]))
+    def _check_tags(self, tags_dict, expected_tags):
+        self.assertEqual(tags_dict, {t.key: t.value for t in expected_tags})
 
     def _check(self, rd, metrics, params, tags):
         self.assertIsInstance(rd, RunData)
-        self._check_metrics(rd.metrics, metrics)
+        self._check_metrics(rd._metric_objs, rd.metrics, metrics)
         self._check_params(rd.params, params)
         self._check_tags(rd.tags, tags)
 
     @staticmethod
     def _create():
-        metrics = [Metric(random_str(10), random_int(0, 1000),
-                          int(time.time() + random_int(-1e4, 1e4)))
-                   for _ in range(100)]
+        metrics = [Metric(random_str(10),
+                          random_int(0, 1000), int(time.time()) + random_int(-1e4, 1e4))]
         params = [Param(random_str(10), random_str(random_int(10, 35))) for _ in range(10)]  # noqa
         tags = [RunTag(random_str(10), random_str(random_int(10, 35))) for _ in range(10)]  # noqa
-        rd = RunData()
-        for p in params:
-            rd._add_param(p)
-        for m in metrics:
-            rd._add_metric(m)
-        for t in tags:
-            rd._add_tag(t)
+        rd = RunData(metrics=metrics, params=params, tags=tags)
         return rd, metrics, params, tags
 
     def test_creation_and_hydration(self):
         rd1, metrics, params, tags = self._create()
         self._check(rd1, metrics, params, tags)
 
-        as_dict = {"metrics": metrics, "params": params, "tags": tags}
+        as_dict = {"metrics": {m.key: m.value for m in metrics},
+                   "params": {p.key: p.value for p in params},
+                   "tags": {t.key: t.value for t in tags}}
         self.assertEqual(dict(rd1), as_dict)
-
         proto = rd1.to_proto()
         rd2 = RunData.from_proto(proto)
         self._check(rd2, metrics, params, tags)
-
-        rd3 = RunData.from_dictionary(as_dict)
-        self._check(rd3, metrics, params, tags)
