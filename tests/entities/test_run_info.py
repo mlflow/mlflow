d6e59a6a0071b4a7f26ceb1d512dced4e692fc48
Sid Murching <smurching@gmail.com>
2019-04-18 15:24:29 -0700

    WIP migrating all entity tests to pytest format - will probably revert this commit & do the reactoring in a separate PR

--- tests/entities/test_run_info.py
+++ tests/entities/test_run_info.py
@@ -5,24 +5,25 @@ from mlflow.entities import RunInfo, LifecycleStage
 from tests.helper_functions import random_str, random_int
 
 
-class TestRunInfo(unittest.TestCase):
-    def _check(self, ri, run_uuid, experiment_id, name, source_type, source_name,
+class TestRunInfo(object):
+    @staticmethod
+    def _check(ri, run_uuid, experiment_id, name, source_type, source_name,
                entry_point_name, user_id, status, start_time, end_time, source_version,
                lifecycle_stage, artifact_uri):
-        self.assertIsInstance(ri, RunInfo)
-        self.assertEqual(ri.run_uuid, run_uuid)
-        self.assertEqual(ri.experiment_id, experiment_id)
-        self.assertEqual(ri.name, name)
-        self.assertEqual(ri.source_type, source_type)
-        self.assertEqual(ri.source_name, source_name)
-        self.assertEqual(ri.entry_point_name, entry_point_name)
-        self.assertEqual(ri.user_id, user_id)
-        self.assertEqual(ri.status, status)
-        self.assertEqual(ri.start_time, start_time)
-        self.assertEqual(ri.end_time, end_time)
-        self.assertEqual(ri.source_version, source_version)
-        self.assertEqual(ri.lifecycle_stage, lifecycle_stage)
-        self.assertEqual(ri.artifact_uri, artifact_uri)
+        assert type(ri) == RunInfo
+        assert ri.run_uuid == run_uuid
+        assert ri.experiment_id == experiment_id
+        assert ri.name == name
+        assert ri.source_type == source_type
+        assert ri.source_name == source_name
+        assert ri.entry_point_name == entry_point_name
+        assert ri.user_id == user_id
+        assert ri.status == status
+        assert ri.start_time == start_time
+        assert ri.end_time == end_time
+        assert ri.source_version == source_version
+        assert ri.lifecycle_stage == lifecycle_stage
+        assert ri.artifact_uri == artifact_uri
 
     @staticmethod
     def _create():
@@ -49,43 +50,43 @@ class TestRunInfo(unittest.TestCase):
                 user_id, status, start_time, end_time, source_version, lifecycle_stage,
                 artifact_uri)
 
-    def test_creation_and_hydration(self):
-        (ri1, run_uuid, experiment_id, name, source_type, source_name, entry_point_name,
-         user_id, status, start_time, end_time, source_version, lifecycle_stage,
-         artifact_uri) = self._create()
-        self._check(ri1, run_uuid, experiment_id, name, source_type, source_name, entry_point_name,
-                    user_id, status, start_time, end_time, source_version, lifecycle_stage,
-                    artifact_uri)
-        as_dict = {
-            "run_uuid": run_uuid,
-            "experiment_id": experiment_id,
-            "name": name,
-            "source_type": source_type,
-            "source_name": source_name,
-            "entry_point_name": entry_point_name,
-            "user_id": user_id,
-            "status": status,
-            "start_time": start_time,
-            "end_time": end_time,
-            "source_version": source_version,
-            "lifecycle_stage": lifecycle_stage,
-            "artifact_uri": artifact_uri
-        }
-        self.assertEqual(dict(ri1), as_dict)
+def test_creation_and_hydration():
+    (ri1, run_uuid, experiment_id, name, source_type, source_name, entry_point_name,
+     user_id, status, start_time, end_time, source_version, lifecycle_stage,
+     artifact_uri) = _create()
+    _check(ri1, run_uuid, experiment_id, name, source_type, source_name, entry_point_name,
+                user_id, status, start_time, end_time, source_version, lifecycle_stage,
+                artifact_uri)
+    as_dict = {
+        "run_uuid": run_uuid,
+        "experiment_id": experiment_id,
+        "name": name,
+        "source_type": source_type,
+        "source_name": source_name,
+        "entry_point_name": entry_point_name,
+        "user_id": user_id,
+        "status": status,
+        "start_time": start_time,
+        "end_time": end_time,
+        "source_version": source_version,
+        "lifecycle_stage": lifecycle_stage,
+        "artifact_uri": artifact_uri
+    }
+    assert dict(ri1) == as_dict
 
-        proto = ri1.to_proto()
-        ri2 = RunInfo.from_proto(proto)
-        self._check(ri2, run_uuid, experiment_id, name, source_type, source_name, entry_point_name,
-                    user_id, status, start_time, end_time, source_version, lifecycle_stage,
-                    artifact_uri)
-        ri3 = RunInfo.from_dictionary(as_dict)
-        self._check(ri3, run_uuid, experiment_id, name, source_type, source_name, entry_point_name,
-                    user_id, status, start_time, end_time, source_version, lifecycle_stage,
-                    artifact_uri)
-        # Test that we can add a field to RunInfo and still deserialize it from a dictionary
-        dict_copy_0 = as_dict.copy()
-        dict_copy_0["my_new_field"] = "new field value"
-        ri4 = RunInfo.from_dictionary(dict_copy_0)
-        self._check(ri4, run_uuid, experiment_id, name, source_type, source_name, entry_point_name,
-                    user_id, status, start_time, end_time, source_version, lifecycle_stage,
-                    artifact_uri)
+    proto = ri1.to_proto()
+    ri2 = RunInfo.from_proto(proto)
+    _check(ri2, run_uuid, experiment_id, name, source_type, source_name, entry_point_name,
+                user_id, status, start_time, end_time, source_version, lifecycle_stage,
+                artifact_uri)
+    ri3 = RunInfo.from_dictionary(as_dict)
+    _check(ri3, run_uuid, experiment_id, name, source_type, source_name, entry_point_name,
+                user_id, status, start_time, end_time, source_version, lifecycle_stage,
+                artifact_uri)
+    # Test that we can add a field to RunInfo and still deserialize it from a dictionary
+    dict_copy_0 = as_dict.copy()
+    dict_copy_0["my_new_field"] = "new field value"
+    ri4 = RunInfo.from_dictionary(dict_copy_0)
+    _check(ri4, run_uuid, experiment_id, name, source_type, source_name, entry_point_name,
+                user_id, status, start_time, end_time, source_version, lifecycle_stage,
+                artifact_uri)
---
+++ tests/entities/utils.py
@@ -0,0 +1,8 @@
+
+def check_run_data():
+    pass
+
+def create_run_data():
+    pass
+
+def check
