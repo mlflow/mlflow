aca72697212c5de53b009514f1175bc7c3bd114b
cclauss <cclauss@me.com>
2019-04-23 07:56:50 -0400

    Update lint.sh

--- lint.sh
+++ lint.sh
@@ -5,7 +5,7 @@ set -e
 FWDIR="$(cd "`dirname $0`"; pwd)"
 cd "$FWDIR"
 
-flake8 --max-line-length=100 --exclude mlflow/protos,mlflow/server/js -- mlflow tests
+flake8 --max-line-length=100 --exclude mlflow/protos,mlflow/server/js .
 pylint --msg-template="{path} ({line},{column}): [{msg_id} {symbol}] {msg}" --rcfile="$FWDIR/pylintrc" -- mlflow tests
 
 rstcheck README.rst
