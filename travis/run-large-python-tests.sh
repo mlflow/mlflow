ad2c8e9bbc5fe385575faaa362194c88a96453b9
Aaron Davidson <aaron@databricks.com>
2019-04-29 18:37:18 -0700

    Update run-large-python-tests.sh

--- travis/run-large-python-tests.sh
+++ travis/run-large-python-tests.sh
@@ -15,6 +15,7 @@ trap 'err=1' ERR
 #     echo "Sagemaker container build failed, output:";
 #     cat $SAGEMAKER_OUT;
 #   fi
+# NB: Also add --ignore'd tests to run-large-python-tests.sh
 pytest tests --large --ignore=tests/h2o --ignore=tests/keras \
   --ignore=tests/pytorch --ignore=tests/pyfunc --ignore=tests/sagemaker --ignore=tests/sklearn \
   --ignore=tests/spark --ignore=tests/tensorflow --ignore tests/azureml tests
