86dd4ab7308a9e38be96230e5d00f6b595bf633f
Sid Murching <smurching@gmail.com>
2019-04-30 14:19:57 -0700

    Remove unused R test code

--- mlflow/R/mlflow/tests/testthat/test-tracking-runs.R
+++ mlflow/R/mlflow/tests/testthat/test-tracking-runs.R
@@ -1,20 +1,5 @@
 context("Tracking")
 
-mlflow_clear_test_dir <- function(path) {
-  purrr::safely(mlflow_end_run)()
-  mlflow_set_active_experiment_id(NULL)
-  if (dir.exists(path)) {
-    unlink(path, recursive = TRUE)
-  }
-  deregister_local_servers()
-}
-
-deregister_local_servers <- function() {
-  purrr::walk(as.list(mlflow:::.globals$url_mapping), ~ .x$handle$kill())
-  rlang::env_unbind(mlflow:::.globals, "url_mapping")
-}
-
-
 test_that("mlflow_start_run()/mlflow_get_run() work properly", {
   mlflow_clear_test_dir("mlruns")
   client <- mlflow_client()
