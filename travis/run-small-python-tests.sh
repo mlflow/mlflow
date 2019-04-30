3a3a447105ad91cef29c95d5d17c4dcba6312ba1
Aaron Davidson <aaron@databricks.com>
2019-04-29 18:36:50 -0700

    Update run-small-python-tests.sh

--- travis/run-small-python-tests.sh
+++ travis/run-small-python-tests.sh
@@ -5,6 +5,7 @@ set -x
 err=0
 trap 'err=1' ERR
 
+# NB: Also add --ignore'd tests to run-large-python-tests.sh
 pytest --cov=mlflow --verbose --ignore=tests/h2o --ignore=tests/keras \
   --ignore=tests/pytorch --ignore=tests/pyfunc --ignore=tests/sagemaker --ignore=tests/sklearn \
   --ignore=tests/spark --ignore=tests/tensorflow --ignore tests/azureml tests
