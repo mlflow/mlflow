062a8b7fa54d087987d81e526b8e3fa7079f0b5e
Drew McDonald <drewmcdo@gmail.com>
2019-05-01 15:32:48 -0400

    add gcs reference to download cli help text

--- mlflow/data.py
+++ mlflow/data.py
@@ -84,7 +84,7 @@ def download_uri(uri, output_path):
               help="Output path into which to download the artifact.")
 def download(uri, output_path):
     """
-    Download the artifact at the specified DBFS or S3 URI into the specified local output path, or
+    Download the artifact at the specified DBFS, S3, or GCS URI into the specified local output path, or
     the current directory if no output path is specified.
     """
     if output_path is None:
