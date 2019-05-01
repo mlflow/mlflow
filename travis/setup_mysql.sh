1132f7dd5de596062dc809ddd5b701381b3b7aef
Sid Murching <smurching@gmail.com>
2019-05-01 15:22:57 -0700

    Debug

--- travis/setup_mysql.sh
+++ travis/setup_mysql.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
+mysql --version
 export MYSQL_TEST_USERNAME="root"
 export MYSQL_TEST_PASSWORD="password"
 sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('password') where User='root'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
