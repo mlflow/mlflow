f38548f4a4955d39c8e1e05ff5a0420bc80f248e
marcusrehm <marcusrehm@brkambiental.com.br>
2019-05-02 15:16:57 -0300

    lint tests

--- tests/projects/test_kubernetes.py
+++ tests/projects/test_kubernetes.py
@@ -1,10 +1,6 @@
 import mock
-import yaml
 import json
-import kubernetes
-import mock
 from mlflow.projects import kubernetes as kb
-from mlflow import tracking
 
 
 def test_run_command_creation():  # pylint: disable=unused-argument
@@ -43,15 +39,15 @@ def test_run_kubernetes_job():
     kube_context = "docker-for-desktop"
     with mock.patch("kubernetes.config.load_kube_config") as kube_config_mock:
         with mock.patch("kubernetes.client.BatchV1Api.create_namespaced_job") as kube_api_mock:
-                job_name = kb.run_kubernetes_job(image=image, job_namespace=namespace,
-                                                 image_namespace=namespace, parameters=parameters,
-                                                 env_vars=env_vars, kube_context=kube_context)
-                assert job_name.startswith(image)
-                assert kube_api_mock.call_count == 1
-                args = kube_api_mock.call_args_list
-                assert args[0][1]['namespace'] == namespace
-                args = kube_config_mock.call_args_list
-                assert args[0][1]['context'] == kube_context
+            job_name = kb.run_kubernetes_job(image=image, job_namespace=namespace,
+                                                image_namespace=namespace, parameters=parameters,
+                                                env_vars=env_vars, kube_context=kube_context)
+            assert job_name.startswith(image)
+            assert kube_api_mock.call_count == 1
+            args = kube_api_mock.call_args_list
+            assert args[0][1]['namespace'] == namespace
+            args = kube_config_mock.call_args_list
+            assert args[0][1]['context'] == kube_context
 
 
 def test_push_image_to_registry():
