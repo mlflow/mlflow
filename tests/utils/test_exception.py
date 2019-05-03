0a7c5c3415155e5989e8d7504567b451675f69a2
Eduardo de Leon <eddeleon@microsoft.com>
2019-05-03 02:16:03 -0400

    Do not raise if error_code is missing. Avoid dropping response body of the error

--- tests/utils/test_exception.py
+++ tests/utils/test_exception.py
@@ -1,6 +1,11 @@
-from mlflow.exceptions import ExecutionException
+from mlflow.exceptions import ExecutionException, RestException
 
 
 def test_execution_exception_string_repr():
     exc = ExecutionException("Uh oh")
     assert str(exc) == "Uh oh"
+
+
+def test_rest_exception_default_error_code():
+    exc = RestException({"message": "something important."})
+    assert "something important." in str(exc)
