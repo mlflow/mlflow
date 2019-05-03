4fcf310f5b266293f782d45251f8afe56f0c4331
EDC.Rick.Bahague <bahague.rt@energy.com.ph>
2019-05-03 09:52:40 +0800

    support digit parameter to control number of digits returned by predict

--- mlflow/R/mlflow/R/model-serve.R
+++ mlflow/R/mlflow/R/model-serve.R
@@ -13,6 +13,7 @@
 #'   `httpuv::stopDaemonizedServer()` with the handle returned from this call.
 #' @param browse Launch browser with serving landing page?
 #' @param restore Should \code{mlflow_restore_snapshot()} be called before serving?
+#' @param digits number of significant digits to be returned on api call, defaults to jsonlite's digit = 4.
 #'
 #' @examples
 #' \dontrun{
@@ -39,14 +40,15 @@ mlflow_rfunc_serve <- function(
   port = 8090,
   daemonized = FALSE,
   browse = !daemonized,
-  restore = FALSE
+  restore = FALSE,
+  digits = 4
 ) {
   mlflow_restore_or_warning(restore)
 
   model_path <- resolve_model_path(model_path, run_id)
 
   httpuv_start <- if (daemonized) httpuv::startDaemonizedServer else httpuv::runServer
-  serve_run(model_path, host, port, httpuv_start, browse && interactive())
+  serve_run(model_path, host, port, httpuv_start, browse && interactive(), digits)
 }
 
 serve_content_type <- function(file_path) {
@@ -126,7 +128,7 @@ serve_empty_page <- function(req, sess, model) {
   )
 }
 
-serve_handlers <- function(host, port) {
+serve_handlers <- function(host, port, digits) {
   handlers <- list(
     "^/swagger.json" = function(req, model) {
       list(
@@ -160,7 +162,7 @@ serve_handlers <- function(host, port) {
           "Content-Type" = paste0(serve_content_type("json"), "; charset=UTF-8")
         ),
         body = charToRaw(enc2utf8(
-          jsonlite::toJSON(list(predictions = results), auto_unbox = TRUE)
+          jsonlite::toJSON(list(predictions = results), auto_unbox = TRUE, digits = digits )
         ))
       )
     },
@@ -188,14 +190,14 @@ message_serve_start <- function(host, port, model) {
 }
 
 #' @importFrom utils browseURL
-serve_run <- function(model_path, host, port, start, browse) {
+serve_run <- function(model_path, host, port, start, browse, digits) {
   model <- mlflow_load_model(model_path)
 
   message_serve_start(host, port, model)
 
   if (browse) browseURL(paste0("http://", host, ":", port))
 
-  handlers <- serve_handlers(host, port)
+  handlers <- serve_handlers(host, port, digits)
 
   start(host, port, list(
     onHeaders = function(req) {
