ea53fa81bc05955d04890641bf22626d26756401
Sue Ann Hong <sueann@databricks.com>
2019-05-02 17:27:32 -0700

    [Runs URI][Python] changes in deploy methods (#1174)
    * sagemaker.deploy
    
    * still working on fixing sagemaker tests
    
    * some cleanup
    
    * Change AzureML to model URI
    
    * Use model name as s3 bucket prefix instead of model path
    
    * Docs improvements, remove run_id from local server
    
    * Lint
    
    * Remove duplicate docs
    
    * Fix test reqs changes
    
    * Fix test
    
    * Update AzureML tests
    
    * Remove TODO
    
    * Add relative path to docs
    
    * Docs fix
    
    * Lint and docfix
    
    * Add S3 test
    
    * Test building from remote model on Azure
    
    * SageMaker CLI fix with tests
    
    * Run local CLI test updates
    
    * Lint
    
    * Address review comments

--- mlflow/utils/cli_args.py
+++ mlflow/utils/cli_args.py
@@ -7,6 +7,13 @@ MODEL_PATH = click.option("--model-path", "-m", default=None, metavar="PATH", re
                           help="Path to the model. The path is relative to the run with the given "
                                "run-id or local filesystem path without run-id.")
 
+MODEL_URI = click.option("--model-uri", "-m", default=None, metavar="URI", required=True,
+                         help="URI to the model. This may be a local path, a 'runs:/' URI, or a"
+                              " remote storage URI (e.g., an 's3://' URI). For more information"
+                              " about supported remote URIs for model artifacts, see"
+                              " https://mlflow.org/docs/latest/tracking.html"
+                              "#supported-artifact-stores")
+
 MLFLOW_HOME = click.option("--mlflow-home", default=None, metavar="PATH",
                            help="Path to local clone of MLflow project. Use for development only.")
 
