1a16fcc0b68131d4d0467d3658b3db599ab8e163
Eduardo de Leon <eddeleon@microsoft.com>
2019-05-08 10:19:33 -0400

    Remove changed response related tests

--- tests/utils/test_exception.py
+++ tests/utils/test_exception.py
@@ -11,17 +11,6 @@ def test_rest_exception_default_error_code():
     assert "something important." in str(exc)
 
 
-def test_rest_exception_without_message():
-    exc = RestException({"my_property": "something important."})
-    assert "something important." in str(exc)
-
-
-def test_rest_exception_error_code_and_no_message():
-    exc = RestException({"error_code": 2, "messages": "something important."})
-    assert "something important." in str(exc)
-    assert "2" in str(exc)
-
-
 def test_rest_exception_error_code_is_not_none():
     error_string = "something important."
     exc = RestException({"message": error_string})
