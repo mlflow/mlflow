a8c0c414fe988625b7dd8b6eb15618bb3018684a
Eduardo de Leon <eddeleon@microsoft.com>
2019-05-08 10:18:16 -0400

    Undo message change, requires discussion, current change is more clear

--- mlflow/exceptions.py
+++ mlflow/exceptions.py
@@ -38,7 +38,7 @@ class RestException(MlflowException):
     """Exception thrown on non 200-level responses from the REST API"""
     def __init__(self, json):
         error_code = json.get('error_code', INTERNAL_ERROR)
-        message = "Response body: '%s'" % (json)
+        message = error_code
         if 'message' in json:
             message = "%s: %s" % (error_code, json['message'])
         super(RestException, self).__init__(message, error_code=error_code)
