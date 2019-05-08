fd6767f31276bc5b6ee1a26a191d27f0fb1ba708
eedeleon <31962564+eedeleon@users.noreply.github.com>
2019-05-08 16:13:53 -0400

    Do not raise if error_code is missing from RestException (#1192)

--- tests/utils/test_exception.py
+++ tests/utils/test_exception.py
@@ -1,6 +1,18 @@
-from mlflow.exceptions import ExecutionException
+from mlflow.exceptions import ExecutionException, RestException
 
 
 def test_execution_exception_string_repr():
     exc = ExecutionException("Uh oh")
     assert str(exc) == "Uh oh"
+
+
+def test_rest_exception_default_error_code():
+    exc = RestException({"message": "something important."})
+    assert "something important." in str(exc)
+
+
+def test_rest_exception_error_code_is_not_none():
+    error_string = "something important."
+    exc = RestException({"message": error_string})
+    assert "None" not in error_string
+    assert "None" not in str(exc)
