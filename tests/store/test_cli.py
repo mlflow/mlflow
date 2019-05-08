08dea4b9c439e0729ee540eadef8c77a4e3448ec
tomasatdatabricks <33237569+tomasatdatabricks@users.noreply.github.com>
2019-05-07 23:19:08 -0700

    Cli command to download artifacts from a URI. Updated R apis for loading / scoring models. (#1206)
    * Added a clie comand to download artifacts from a URI, updated R apis for loading models to use the uri instead of run-id + path.
    
    * Added tests.
    
    * Reverted changes made during debugging.
    
    * Addressed review comments: use template to document uses of model-uri.
    
    * Addressed review comment, update rfunc clis.
    
    * Addressed review comment, update comments.
    
    * Removed extra newlines at the end of a file.
    
    * Some fixes and some additional debug infor to be removed later.
    
    * Updated debug info.
    
    * Fix.
    
    * Fixed linter complaint.

--- tests/store/test_cli.py
+++ tests/store/test_cli.py
@@ -1,7 +1,13 @@
 import json
+import os
+import posixpath
 
+import mlflow
+import mlflow.pyfunc
 from mlflow.entities import FileInfo
 from mlflow.store.cli import _file_infos_to_json
+from mlflow.utils.file_utils import TempDir
+from subprocess import Popen, STDOUT, PIPE
 
 
 def test_file_info_to_json():
@@ -18,3 +24,24 @@ def test_file_info_to_json():
         "path": "/my/dir",
         "is_dir": True,
     }]
+
+
+def test_download_artifacts_from_uri():
+    with mlflow.start_run() as run:
+        with TempDir() as tmp:
+            local_path = tmp.path("test")
+            with open(local_path, "w") as f:
+                f.write("test")
+            mlflow.log_artifact(local_path, "test")
+    command = ["mlflow", "artifacts", "download-from-uri", "-a"]
+    # Test with run uri
+    run_uri = "runs:/{run_id}/test".format(run_id=run.info.run_id)
+    actual_uri = posixpath.join(run.info.artifact_uri, "test")
+    for uri in (run_uri, actual_uri):
+        p = Popen(command + [uri], stdout=PIPE,
+                  stderr=STDOUT)
+        output = p.stdout.readlines()
+        downloaded_file_path = output[-1].strip()
+        downloaded_file = os.listdir(downloaded_file_path)[0]
+        with open(os.path.join(downloaded_file_path, downloaded_file), "r") as f:
+            assert f.read() == "test"
