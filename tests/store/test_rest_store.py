884336f4c5908b3e046bee204d6b8a3598253058
Andrew Crozier <wacrozier@gmail.com>
2019-05-09 16:36:39 +0100

    Make the tests Python 2/3 agnostic

--- tests/store/test_rest_store.py
+++ tests/store/test_rest_store.py
@@ -81,12 +81,6 @@ class TestRestStore(unittest.TestCase):
     def _verify_requests(self, http_request, host_creds, endpoint, method, json_body):
         http_request.assert_called_with(**(self._args(host_creds, endpoint, method, json_body)))
 
-    def _verify_request_has_calls(self, http_request, host_creds, call_args):
-        http_request.assert_has_calls(calls=[mock.call(**(self._args(host_creds, endpoint, method,
-                                                                     json_body)))
-                                             for endpoint, method, json_body in call_args],
-                                      any_order=True)
-
     @mock.patch('requests.request')
     def test_requestor(self, request):
         response = mock.MagicMock
@@ -120,9 +114,20 @@ class TestRestStore(unittest.TestCase):
                                                                       value='LOCAL'),
                                                           ProtoRunTag(key='mlflow.user',
                                                                       value=user_name)]))
+                expected_kwargs = self._args(creds, "runs/create", "POST", cr_body)
+
                 assert mock_http.call_count == 1
-                exp_calls = [("runs/create", "POST", cr_body)]
-                self._verify_request_has_calls(mock_http, creds, exp_calls)
+                actual_kwargs = mock_http.call_args[1]
+
+                # Test the passed tag values separately from the rest of the request
+                # Tag order is inconsistent on Python 2 and 3, but the order does not matter
+                expected_tags = expected_kwargs['json'].pop('tags')
+                actual_tags = actual_kwargs['json'].pop('tags')
+                assert (
+                    sorted(expected_tags, key=lambda t: t['key']) ==
+                    sorted(actual_tags, key=lambda t: t['key'])
+                )
+                assert expected_kwargs == actual_kwargs
 
         with mock.patch('mlflow.store.rest_store.http_request_safe') as mock_http:
             store.log_param("some_uuid", Param("k1", "v1"))
