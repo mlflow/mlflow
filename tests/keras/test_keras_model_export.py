fcc87c4965e2141fff996826b5e2f9780a996059
dbczumar <39497902+dbczumar@users.noreply.github.com>
2019-05-08 17:37:23 -0700

    Remove _get_model_log_dir() method (#1224)
    * Initial changes to load_model and tests
    
    * UDF and CLI methods
    
    * Move S3 and boto fixtures into helper functions
    
    * Revert space diff
    
    * Test fix
    
    * Lint
    
    * Lint2
    
    * Remote URI tests for h2o, keras, sklearn, pytorch
    
    * Remote URI tests for tensorflow and spark
    
    * Test cases fixes
    
    * Lint
    
    * Pytorch tests fix
    
    * Test fixes
    
    * Param fixes
    
    * Address subset of comments
    
    * Address remaining comments
    
    * Lint
    
    * Remove remote URI test
    
    * Remove unused variable
    
    * H2o and keras
    
    * Param fix in Keras
    
    * Remove get_model_log_dir from tests
    
    * Arg fix for pytorch tests
    
    * Remove _get_model_log_dir

--- tests/keras/test_keras_model_export.py
+++ tests/keras/test_keras_model_export.py
@@ -18,7 +18,7 @@ import mlflow.pyfunc.scoring_server as pyfunc_scoring_server
 from mlflow import pyfunc
 from mlflow.models import Model
 from mlflow.store.s3_artifact_repo import S3ArtifactRepository
-from mlflow.tracking.artifact_utils import _get_model_log_dir
+from mlflow.tracking.artifact_utils import _download_artifact_from_uri
 from mlflow.utils.environment import _mlflow_conda_env
 from mlflow.utils.model_utils import _get_flavor_configuration
 from tests.helper_functions import pyfunc_serve_and_score_model
@@ -176,8 +176,8 @@ def test_model_log_persists_specified_conda_env_in_mlflow_model_directory(model,
     with mlflow.start_run():
         mlflow.keras.log_model(
             keras_model=model, artifact_path=artifact_path, conda_env=keras_custom_env)
-        run_id = mlflow.active_run().info.run_id
-    model_path = _get_model_log_dir(artifact_path, run_id)
+        model_path = _download_artifact_from_uri("runs:/{run_id}/{artifact_path}".format(
+            run_id=mlflow.active_run().info.run_id, artifact_path=artifact_path))
 
     pyfunc_conf = _get_flavor_configuration(model_path=model_path, flavor_name=pyfunc.FLAVOR_NAME)
     saved_conda_env_path = os.path.join(model_path, pyfunc_conf[pyfunc.ENV])
@@ -209,8 +209,8 @@ def test_model_log_without_specified_conda_env_uses_default_env_with_expected_de
     artifact_path = "model"
     with mlflow.start_run():
         mlflow.keras.log_model(keras_model=model, artifact_path=artifact_path, conda_env=None)
-        run_id = mlflow.active_run().info.run_id
-    model_path = _get_model_log_dir(artifact_path, run_id)
+        model_path = _download_artifact_from_uri("runs:/{run_id}/{artifact_path}".format(
+            run_id=mlflow.active_run().info.run_id, artifact_path=artifact_path))
 
     pyfunc_conf = _get_flavor_configuration(model_path=model_path, flavor_name=pyfunc.FLAVOR_NAME)
     conda_env_path = os.path.join(model_path, pyfunc_conf[pyfunc.ENV])
