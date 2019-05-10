75416cad94f37dbf7711ebf831b818b696dfc375
Sue Ann Hong <sueann@databricks.com>
2019-05-10 13:15:35 -0700

    Autogenerate CLI docs (#1231)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ from languagesections import *
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
+    'sphinx_click.ext',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
