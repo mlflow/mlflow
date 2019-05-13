4b2379d8650fb026ea6d6f195c8707dd801ccf1c
kevinykuo <kevinykuo@gmail.com>
2019-05-13 18:01:06 -0400

    install roxygen2 during travis build

--- mlflow/R/mlflow/.travis.R
+++ mlflow/R/mlflow/.travis.R
@@ -2,6 +2,7 @@ parent_dir <- dir("../", full.names = TRUE)
 package <- parent_dir[grepl("mlflow_", parent_dir)]
 install.packages(package)
 install.packages("keras", repos='http://cran.rstudio.com/')
+install.packages("roxygen2")
 library(keras)
 install_keras()
 devtools::check(error_on = "warning")
