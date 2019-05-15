c7086a04c56b206a2d27f3f45dde42d3c2780ef4
Aaron Davidson <aaron@databricks.com>
2019-05-14 18:59:53 -0700

    Revert "Correct R travis setup (#1259)"
    This reverts commit 601d75fe015fd72a6833ce96324c12ea832fcd86.

--- mlflow/R/mlflow/.travis.R
+++ mlflow/R/mlflow/.travis.R
@@ -2,8 +2,6 @@ parent_dir <- dir("../", full.names = TRUE)
 package <- parent_dir[grepl("mlflow_", parent_dir)]
 install.packages(package)
 install.packages("keras", repos='http://cran.rstudio.com/')
-install.packages("roxygen2")
 library(keras)
 install_keras()
-devtools::check(error_on = "warning", args = "--no-tests")
 source("testthat.R")
