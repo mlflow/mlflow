acf8c3cd79eccebb9bb588c9aa516291411cbe7b
eedeleon <31962564+eedeleon@users.noreply.github.com>
2019-05-17 02:00:01 -0400

    Refactor rest store and rest utils to support custom verification/processing of the response (#1261)
    * Refactor rest store and rest utils to support custom deserialization of response in RestStore
    
    * Fix lint and error message bugs
    
    * Resolve minor comments
    
    * Keep message as string and test json se(de)rialization
    
    * Add back error_code for rest_utils tests

--- tests/utils/test_exception.py
+++ tests/utils/test_exception.py
@@ -1,9 +1,11 @@
+import json
 from mlflow.exceptions import ExecutionException, RestException
 
 
 def test_execution_exception_string_repr():
     exc = ExecutionException("Uh oh")
     assert str(exc) == "Uh oh"
+    json.loads(exc.serialize_as_json())
 
 
 def test_rest_exception_default_error_code():
@@ -16,3 +18,17 @@ def test_rest_exception_error_code_is_not_none():
     exc = RestException({"message": error_string})
     assert "None" not in error_string
     assert "None" not in str(exc)
+    json.loads(exc.serialize_as_json())
+
+
+def test_rest_exception_without_message():
+    exc = RestException({"my_property": "something important."})
+    assert "something important." in str(exc)
+    json.loads(exc.serialize_as_json())
+
+
+def test_rest_exception_error_code_and_no_message():
+    exc = RestException({"error_code": 2, "messages": "something important."})
+    assert "something important." in str(exc)
+    assert "2" in str(exc)
+    json.loads(exc.serialize_as_json())
