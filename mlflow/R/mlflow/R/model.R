353a9c47d64ef2a747ed21d6b65e78e4223ac1a2
kevinykuo <kevinykuo@gmail.com>
2019-05-21 01:00:23 -0400

    move model-flavor.R to model.R

--- mlflow/R/mlflow/R/model.R
+++ mlflow/R/mlflow/R/model.R
@@ -115,6 +115,37 @@ mlflow_flavor <- function(flavor) {
   new_mlflow_flavor(flavor, paste0("mlflow_flavor_", flavor))
 }
 
+#' Load MLflow Model Flavor
+#'
+#' Loads an MLflow model flavor, to be used by package authors
+#' to extend the supported MLflow models.
+#'
+#' @param flavor An MLflow flavor object.
+#' @param model_path The path to the MLflow model wrapped in the correct
+#'   class.
+#'
+#' @export
+mlflow_load_flavor <- function(flavor, model_path) {
+  UseMethod("mlflow_load_flavor")
+}
+
+#' Generate Prediction with MLflow Model
+#'
+#' Performs prediction over a model loaded using
+#' \code{mlflow_load_model()}, to be used by package authors
+#' to extend the supported MLflow models.
+#'
+#' @param model The loaded MLflow model flavor.
+#' @param data A data frame to perform scoring.
+#' @param ... Optional additional arguments passed to underlying predict
+#'   methods.
+#'
+#' @export
+mlflow_predict <- function(model, data, ...) {
+  UseMethod("mlflow_predict")
+}
+
+
 # Generate predictions using a saved R MLflow model.
 # Input and output are read from and written to a specified input / output file or stdin / stdout.
 #
