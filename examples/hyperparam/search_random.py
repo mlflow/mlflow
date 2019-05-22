6f7b3248b8ac98086113593effb2e3550d7a524b
tomasatdatabricks <33237569+tomasatdatabricks@users.noreply.github.com>
2019-05-22 11:28:05 -0700

    Adapt hyperparam example to new MLflow APIs. (#1294)
    Updates hyperparam example to use MLflow 1.0 APIs

--- examples/hyperparam/search_random.py
+++ examples/hyperparam/search_random.py
@@ -9,11 +9,6 @@ results.
 Several runs can be run in parallel.
 """
 
-import math
-
-import os
-import shutil
-import tempfile
 from concurrent.futures import ThreadPoolExecutor
 
 import click
@@ -24,6 +19,9 @@ import mlflow
 import mlflow.sklearn
 import mlflow.tracking
 import mlflow.projects
+from mlflow.tracking.client import MlflowClient
+
+_inf = np.finfo(np.float64).max
 
 
 @click.command(help="Perform grid search over train (main entry point).")
@@ -37,10 +35,8 @@ import mlflow.projects
               help="Metric to optimize on.")
 @click.option("--seed", type=click.INT, default=97531,
               help="Seed for the random generator")
-@click.option("--training-experiment-id", type=click.INT, default=-1,
-              help="Maximum number of runs to evaluate. Inherit parent;s experiment if == -1.")
 @click.argument("training_data")
-def run(training_data, max_runs, max_p, epochs, metric, seed, training_experiment_id):
+def run(training_data, max_runs, max_p, epochs, metric, seed):
     train_metric = "train_{}".format(metric)
     val_metric = "val_{}".format(metric)
     test_metric = "test_{}".format(metric)
@@ -49,87 +45,81 @@ def run(training_data, max_runs, max_p, epochs, metric, seed, training_experimen
 
     def new_eval(nepochs,
                  experiment_id,
-                 null_train_loss=math.inf,
-                 null_val_loss=math.inf,
-                 null_test_loss=math.inf):
+                 null_train_loss=_inf,
+                 null_val_loss=_inf,
+                 null_test_loss=_inf):
         def eval(parms):
             lr, momentum = parms
-            p = mlflow.projects.run(
-                uri=".",
-                entry_point="train",
-                parameters={
-                    "training_data": training_data,
-                    "epochs": str(nepochs),
-                    "learning_rate": str(lr),
-                    "momentum": str(momentum),
-                    "seed": str(seed)},
-                experiment_id=experiment_id,
-                synchronous=False)
-            if p.wait():
+            with mlflow.start_run(nested=True) as run:
+                p = mlflow.projects.run(
+                    run_id=run.info.run_id,
+                    uri=".",
+                    entry_point="train",
+                    parameters={
+                        "training_data": training_data,
+                        "epochs": str(nepochs),
+                        "learning_rate": str(lr),
+                        "momentum": str(momentum),
+                        "seed": str(seed)},
+                    experiment_id=experiment_id,
+                    synchronous=False)
+                succeeded = p.wait()
+            if succeeded:
                 training_run = tracking_client.get_run(p.run_id)
-
-                def get_metric(metric_name):
-                    return training_run.data.metrics[metric_name].value
-
+                metrics = training_run.data.metrics
                 # cap the loss at the loss of the null model
-                train_loss = min(null_train_loss, get_metric(train_metric))
-                val_loss = min(null_val_loss, get_metric(val_metric))
-                test_loss = min(null_test_loss, get_metric(test_metric))
+                train_loss = min(null_train_loss, metrics[train_metric])
+                val_loss = min(null_val_loss, metrics[val_metric])
+                test_loss = min(null_test_loss, metrics[test_metric])
             else:
                 # run failed => return null loss
                 tracking_client.set_terminated(p.run_id, "FAILED")
                 train_loss = null_train_loss
                 val_loss = null_val_loss
                 test_loss = null_test_loss
-            mlflow.log_metric(train_metric, train_loss)
-            mlflow.log_metric(val_metric, val_loss)
-            mlflow.log_metric(test_metric, test_loss)
+            mlflow.log_metrics({
+                "train_{}".format(metric): train_loss,
+                "val_{}".format(metric): val_loss,
+                "test_{}".format(metric): test_loss
+            })
             return p.run_id, train_loss, val_loss, test_loss
 
         return eval
 
     with mlflow.start_run() as run:
-        experiment_id = run.info.experiment_id if training_experiment_id == -1 \
-            else training_experiment_id
+        experiment_id = run.info.experiment_id
         _, null_train_loss, null_val_loss, null_test_loss = new_eval(0, experiment_id)((0, 0))
         runs = [(np.random.uniform(1e-5, 1e-1), np.random.uniform(0, 1.0)) for _ in range(max_runs)]
-        best_train_loss = math.inf
-        best_val_loss = math.inf
-        best_test_loss = math.inf
-        best_run = None
         with ThreadPoolExecutor(max_workers=max_p) as executor:
-            result = executor.map(new_eval(epochs,
-                                           experiment_id,
-                                           null_train_loss,
-                                           null_val_loss,
-                                           null_test_loss),
-                                  runs)
-        tmp = tempfile.mkdtemp()
-        results_file_path = os.path.join(str(tmp), "results.txt")
-        with open(results_file_path, "w") as f:
-            for res in result:
-                run_id, train_loss, val_loss, test_loss = res
-                if val_loss < best_val_loss:
-                    best_run = run_id
-                    best_train_loss = train_loss
-                    best_val_loss = val_loss
-                    best_test_loss = test_loss
-                f.write("{run_id} {train} {val} {test}\n".format(run_id=run_id,
-                                                                 train=train_loss,
-                                                                 val=val_loss,
-                                                                 test=test_loss))
-        mlflow.log_artifact(results_file_path, "training_runs.txt")
-        # record which run produced the best results, store it as an artifact
-        best_run_path = os.path.join(os.path.join(tmp, "best_run.txt"))
-        with open(best_run_path, "w") as f:
-            f.write("{run_id} {train} {val} {test}\n".format(run_id=best_run,
-                                                             train=best_train_loss,
-                                                             val=best_val_loss,
-                                                             test=best_test_loss))
-        mlflow.log_artifact(best_run_path, "best-run")
-        mlflow.log_metric(val_metric, best_val_loss)
-        mlflow.log_metric(test_metric, best_test_loss)
-        shutil.rmtree(tmp)
+            _ = executor.map(new_eval(epochs,
+                                      experiment_id,
+                                      null_train_loss,
+                                      null_val_loss,
+                                      null_test_loss),
+                             runs)
+
+        # find the best run, log its metrics as the final metrics of this run.
+        client = MlflowClient()
+        runs = client.search_runs([experiment_id],
+                                  "tags.mlflow.parentRunId = '{run_id}' ".format(
+                                      run_id=run.info.run_id
+                                  ))
+        best_val_train = _inf
+        best_val_valid = _inf
+        best_val_test = _inf
+        best_run = None
+        for r in runs:
+            if r.data.metrics["val_rmse"] < best_val_valid:
+                best_run = r
+                best_val_train = r.data.metrics["train_rmse"]
+                best_val_valid = r.data.metrics["val_rmse"]
+                best_val_test = r.data.metrics["test_rmse"]
+        mlflow.set_tag("best_run", best_run.info.run_id)
+        mlflow.log_metrics({
+            "train_{}".format(metric): best_val_train,
+            "val_{}".format(metric): best_val_valid,
+            "test_{}".format(metric): best_val_test
+        })
 
 
 if __name__ == '__main__':
