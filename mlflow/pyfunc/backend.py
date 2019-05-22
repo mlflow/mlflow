b10175dd994be7612509a1c318ba5b2f80871e89
Andrew Chen <andrewmnchen@gmail.com>
2019-05-21 17:04:04 -0700

    init

--- mlflow/pyfunc/backend.py
+++ mlflow/pyfunc/backend.py
@@ -27,46 +27,44 @@ class PyFuncBackend(FlavorBackend):
         Generate predictions using generic python model saved with MLflow.
         Return the prediction results as a JSON.
         """
-        with TempDir() as tmp:
-            local_path = _download_artifact_from_uri(model_uri, output_path=tmp.path())
-            if not self._no_conda and ENV in self._config:
-                conda_env_path = os.path.join(local_path, self._config[ENV])
-                # NOTE: We're calling main in the pyfunc scoring server belonging to the current
-                # conda environment. The model environment may contain mlflow with different version
-                # than the one in the current active environment. This is the intended behavior.
-                # We need to make sure the scoring server is consistent with the outside mlflow
-                # while the model that is being loaded may depend on a different version of mlflow.
-                # The hope is that the scoring server is self contained enough and does not have
-                # external mlflow dependencies that would be incompatible between mlflow versions.
-                if input_path is None:
-                    input_path = "__stdin__"
-                if output_path is None:
-                    output_path = "__stdout__"
-                command = "python {0} predict {1} {2} {3} {4} {5}".format(scoring_server.__file__,
-                                                                          shlex_quote(local_path),
-                                                                          shlex_quote(input_path),
-                                                                          shlex_quote(output_path),
-                                                                          content_type,
-                                                                          json_format)
-                return scoring_server._execute_in_conda_env(conda_env_path, command)
-            else:
-                scoring_server._predict(local_path, input_path, output_path, content_type,
-                                        json_format)
+        local_path = _download_artifact_from_uri(model_uri)
+        if not self._no_conda and ENV in self._config:
+            conda_env_path = os.path.join(local_path, self._config[ENV])
+            # NOTE: We're calling main in the pyfunc scoring server belonging to the current
+            # conda environment. The model environment may contain mlflow with different version
+            # than the one in the current active environment. This is the intended behavior.
+            # We need to make sure the scoring server is consistent with the outside mlflow
+            # while the model that is being loaded may depend on a different version of mlflow.
+            # The hope is that the scoring server is self contained enough and does not have
+            # external mlflow dependencies that would be incompatible between mlflow versions.
+            if input_path is None:
+                input_path = "__stdin__"
+            if output_path is None:
+                output_path = "__stdout__"
+            command = "python {0} predict {1} {2} {3} {4} {5}".format(scoring_server.__file__,
+                                                                      shlex_quote(local_path),
+                                                                      shlex_quote(input_path),
+                                                                      shlex_quote(output_path),
+                                                                      content_type,
+                                                                      json_format)
+            return scoring_server._execute_in_conda_env(conda_env_path, command)
+        else:
+            scoring_server._predict(local_path, input_path, output_path, content_type,
+                                    json_format)
 
     def serve(self, model_uri, port, host):
         """
         Serve pyfunc model locally.
         """
-        with TempDir() as tmp:
-            local_path = shlex_quote(_download_artifact_from_uri(model_uri, output_path=tmp.path()))
-            if not self._no_conda and ENV in self._config:
-                conda_env_path = os.path.join(local_path, self._config[ENV])
-                command = "python {0} serve {1} {2} {3}".format(scoring_server.__file__,
-                                                                shlex_quote(local_path),
-                                                                port, host)
-                return scoring_server._execute_in_conda_env(conda_env_path, command)
-            else:
-                scoring_server._serve(local_path, port, host)
+        local_path = shlex_quote(_download_artifact_from_uri(model_uri))
+        if not self._no_conda and ENV in self._config:
+            conda_env_path = os.path.join(local_path, self._config[ENV])
+            command = "python {0} serve {1} {2} {3}".format(scoring_server.__file__,
+                                                            shlex_quote(local_path),
+                                                            port, host)
+            return scoring_server._execute_in_conda_env(conda_env_path, command)
+        else:
+            scoring_server._serve(local_path, port, host)
 
     def can_score_model(self):
         if self._no_conda:
