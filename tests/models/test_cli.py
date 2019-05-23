2a3d4e34ec0bce920b3f568dd703d10e14fd43c2
tomasatdatabricks <33237569+tomasatdatabricks@users.noreply.github.com>
2019-05-22 21:03:17 -0700

    Update how we handle mlflow dependency when dealing with models.  (#1308)
    * MLflow is now a default dependency for all models. Do not pip install mlflow automatically in predict and serve by default. Predict and serve cal into cli in side the environment.
    
    * Minor fix.
    
    * instead of using cli, use internal functions mirroring the cli. Updated tests.
    
    * Addressed linter complaints.
    
    * Addressed linter complaints.
    
    * Addressed linter complaints.
    
    * Updated comment.
    
    * Nit.
    
    * Fix.
    
    * Fix.
    
    * Lint.
    
    * Fixes.
    
    * Moved models/test_cli back to small tests to run on Windows.
    
    * Addressed comments.
    
    * Fixes.
    
    * Fixed winwodws and python2 tests, added extra test to verify we do fail politely in case there are no flavors to dploy.
    
    * Addressed review comments.

--- tests/models/test_cli.py
+++ tests/models/test_cli.py
@@ -13,15 +13,21 @@ except ImportError:
     from io import StringIO
 
 import mlflow
-import mlflow.pyfunc as pyfunc
+from mlflow import pyfunc
 import mlflow.sklearn
-from mlflow.utils.file_utils import TempDir
-from mlflow.tracking.utils import path_to_local_file_uri
+from mlflow.utils.file_utils import TempDir, path_to_local_file_uri
+from mlflow.utils.environment import _mlflow_conda_env
+from mlflow.utils import PYTHON_VERSION
 from tests.models import test_pyfunc
 
 in_travis = 'TRAVIS' in os.environ
 # NB: for now, windows tests on Travis do not have conda available.
 no_conda = ["--no-conda"] if in_travis and sys.platform == "win32" else []
+
+# NB: need to install mlflow since the pip version does not have mlflow models cli.
+install_mlflow = ["--install-mlflow"] if not no_conda else []
+
+extra_options = no_conda + install_mlflow
 
 
 @pytest.fixture(scope="module")
@@ -43,13 +49,15 @@ def sk_model(iris_data):
 def test_predict_with_old_mlflow_in_conda_and_with_orient_records(iris_data):
     if no_conda:
         pytest.skip("This test needs conda.")
+    # TODO: Enable this test after 1.0 is out to ensure we do not break the serve / predict
+    # TODO: Also add a test for serve, not just predict.
+    pytest.skip("TODO: enable this after 1.0 release is out.")
     x, _ = iris_data
     with TempDir() as tmp:
         input_records_path = tmp.path("input_records.json")
         pd.DataFrame(x).to_json(input_records_path, orient="records")
         output_json_path = tmp.path("output.json")
         test_model_path = tmp.path("test_model")
-        from mlflow.utils.environment import _mlflow_conda_env
         test_model_conda_path = tmp.path("conda.yml")
         # create env with odl mlflow!
         _mlflow_conda_env(path=test_model_conda_path,
@@ -70,29 +78,68 @@ def test_predict_with_old_mlflow_in_conda_and_with_orient_records(iris_data):
         assert all(expected == actual)
 
 
-def test_predict(iris_data, sk_model):
-    with mlflow.start_run() as active_run:
-        mlflow.sklearn.log_model(sk_model, "model")
+def test_mlflow_is_not_installed_unless_specified():
+    if no_conda:
+        pytest.skip("This test requires conda.")
+    with TempDir(chdr=True) as tmp:
+        fake_model_path = tmp.path("fake_model")
+        fake_env_path = tmp.path("fake_env.yaml")
+        _mlflow_conda_env(path=fake_env_path, install_mlflow=False)
+        mlflow.pyfunc.save_model(fake_model_path, loader_module=__name__, conda_env=fake_env_path)
+        # The following should fail because there should be no mlflow in the env:
+        p = subprocess.Popen(["mlflow", "models", "predict", "-m", fake_model_path],
+                             stderr=subprocess.PIPE, cwd=tmp.path(""))
+        _, stderr = p.communicate()
+        stderr = stderr.decode("utf-8")
+        print(stderr)
+        assert p.wait() != 0
+        if PYTHON_VERSION.startswith("3"):
+            assert "ModuleNotFoundError: No module named 'mlflow'" in stderr
+        else:
+            assert "ImportError: No module named mlflow.pyfunc.scoring_server" in stderr
 
-    model_uri = "runs:/{run_id}/model".format(run_id=active_run.info.run_id)
-    with TempDir() as tmp:
+
+def test_model_with_no_deployable_flavors_fails_pollitely():
+    from mlflow.models import Model
+    with TempDir(chdr=True) as tmp:
+        m = Model(artifact_path=None, run_id=None, utc_time_created="now",
+                  flavors={"some": {}, "useless": {}, "flavors": {}})
+        os.mkdir(tmp.path("model"))
+        m.save(tmp.path("model", "MLmodel"))
+        # The following should fail because there should be no suitable flavor
+        p = subprocess.Popen(["mlflow", "models", "predict", "-m", tmp.path("model")],
+                             stderr=subprocess.PIPE, cwd=tmp.path(""))
+        _, stderr = p.communicate()
+        stderr = stderr.decode("utf-8")
+        print(stderr)
+        assert p.wait() != 0
+        assert "No suitable flavor backend was found for the model." in stderr
+
+
+def test_predict(iris_data, sk_model):
+    with TempDir(chdr=True) as tmp:
+        with mlflow.start_run() as active_run:
+            mlflow.sklearn.log_model(sk_model, "model")
+            model_uri = "runs:/{run_id}/model".format(run_id=active_run.info.run_id)
         input_json_path = tmp.path("input.json")
         input_csv_path = tmp.path("input.csv")
         output_json_path = tmp.path("output.json")
         x, _ = iris_data
         pd.DataFrame(x).to_json(input_json_path, orient="split")
         pd.DataFrame(x).to_csv(input_csv_path, index=False)
+
+        # Test with no conda
         p = subprocess.Popen(["mlflow", "models", "predict", "-m", model_uri, "-i", input_json_path,
-                              "-o", output_json_path, "--no-conda"])
+                              "-o", output_json_path, "--no-conda"], stderr=subprocess.PIPE)
         assert p.wait() == 0
         actual = pd.read_json(output_json_path, orient="records")
         actual = actual[actual.columns[0]].values
         expected = sk_model.predict(x)
         assert all(expected == actual)
 
-        # with conda this time
+        # With conda + --install-mlflow
         p = subprocess.Popen(["mlflow", "models", "predict", "-m", model_uri, "-i", input_json_path,
-                              "-o", output_json_path] + no_conda)
+                              "-o", output_json_path] + extra_options)
         assert 0 == p.wait()
         actual = pd.read_json(output_json_path, orient="records")
         actual = actual[actual.columns[0]].values
@@ -101,7 +148,7 @@ def test_predict(iris_data, sk_model):
 
         # explicit json format with default orient (should be split)
         p = subprocess.Popen(["mlflow", "models", "predict", "-m", model_uri, "-i", input_json_path,
-                              "-o", output_json_path, "-t", "json"] + no_conda)
+                              "-o", output_json_path, "-t", "json"] + extra_options)
         assert 0 == p.wait()
         actual = pd.read_json(output_json_path, orient="records")
         actual = actual[actual.columns[0]].values
@@ -111,7 +158,7 @@ def test_predict(iris_data, sk_model):
         # explicit json format with orient==split
         p = subprocess.Popen(["mlflow", "models", "predict", "-m", model_uri, "-i", input_json_path,
                               "-o", output_json_path, "-t", "json", "--json-format", "split"]
-                             + no_conda)
+                             + extra_options)
         assert 0 == p.wait()
         actual = pd.read_json(output_json_path, orient="records")
         actual = actual[actual.columns[0]].values
@@ -120,7 +167,7 @@ def test_predict(iris_data, sk_model):
 
         # read from stdin, write to stdout.
         p = subprocess.Popen(["mlflow", "models", "predict", "-m", model_uri, "-t", "json",
-                              "--json-format", "split"] + no_conda,
+                              "--json-format", "split"] + extra_options,
                              universal_newlines=True,
                              stdin=subprocess.PIPE,
                              stdout=subprocess.PIPE,
@@ -138,7 +185,7 @@ def test_predict(iris_data, sk_model):
 
         # csv
         p = subprocess.Popen(["mlflow", "models", "predict", "-m", model_uri, "-i", input_csv_path,
-                              "-o", output_json_path, "-t", "csv"] + no_conda)
+                              "-o", output_json_path, "-t", "csv"] + extra_options)
         assert 0 == p.wait()
         actual = pd.read_json(output_json_path, orient="records")
         actual = actual[actual.columns[0]].values
