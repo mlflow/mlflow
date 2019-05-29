14fb773d33f462fb6fea2f9d8f54e07f8125616f
tomasatdatabricks <33237569+tomasatdatabricks@users.noreply.github.com>
2019-05-29 16:03:34 -0700

    Fixed bug in not sharing sqlite db file with docker container. (#1347)
    * Fixed bug in not sharing sqlite db file with docker container.
    
    * Use existing utils for generating file and sqlite uris.
    
    * Fixed linter complaints, missing import.
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * windows fix.

--- tests/resources/example_docker_project/docker_tracking_test.py
+++ tests/resources/example_docker_project/docker_tracking_test.py
@@ -8,22 +8,7 @@ def call_tracking_apis():
     mlflow.log_metric("some_key", 3)
 
 
-def patch_run_yamls():  # Temporary patch while tranistioning to string experiment_id
-    run_meta_file = os.path.join("/mlflow/tmp/mlruns/0/", os.environ["MLFLOW_RUN_ID"], "meta.yaml")
-    with open(run_meta_file, "r") as stream:
-        lines = stream.readlines()
-    with open(run_meta_file, "w") as stream:
-        for line in lines:
-            if line.startswith("experiment_id"):
-                stream.write(line.replace("'", ""))
-            else:
-                stream.write(line)
-
-
 def main(use_start_run):
-    print(mlflow.__version__)
-    if mlflow.__version__ == "0.8.1":
-        patch_run_yamls()  # TODO remove this after 1.0 release
 
     if use_start_run:
         print("Running with start_run API")
