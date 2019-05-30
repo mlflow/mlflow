9e5caf12a9ba3629cabf0831522feaf932e5d7e7
Aaron Davidson <aaron@databricks.com>
2019-05-29 17:38:34 -0700

    Fine

--- mlflow/R/mlflow/R/python.R
+++ mlflow/R/mlflow/R/python.R
@@ -6,7 +6,7 @@ get_python_bin <- function() {
   envs <- conda_list(conda = conda)
   print(paste("Conda envs", envs))
   mlflow_env <- envs[envs$name == mlflow_conda_env_name(), ]
-  print(paste("Conda env name", mlflow_conda_env_name))
+  print(paste("Conda env name", mlflow_conda_env_name()))
   if (nrow(mlflow_env) == 0) {
     stop("MLflow not configured, please run install_mlflow().")
   }
