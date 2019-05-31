9e3bd606281b5a9050d1312e10d70f166f4a4466
Corey Zumar <corey.zumar@databricks.com>
2019-05-31 01:22:15 -0700

    Fix edge case where absolute path is not detected with trailing slash

--- mlflow/utils/file_utils.py
+++ mlflow/utils/file_utils.py
@@ -355,7 +355,7 @@ def path_to_local_file_uri(path):
     Convert local filesystem path to local file uri.
     """
     path = pathname2url(path)
-    if path == posixpath.abspath(path):
+    if posixpath.isabs(path):
         return "file://{path}".format(path=path)
     else:
         return "file:{path}".format(path=path)
