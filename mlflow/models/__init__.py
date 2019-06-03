7435feda477c2a8410898d42c28c3082206513be
Aaron Davidson <aaron@databricks.com>
2019-06-03 13:48:17 -0700

    Fix more docs warnings

--- mlflow/models/__init__.py
+++ mlflow/models/__init__.py
@@ -133,6 +133,6 @@ class FlavorBackend(object):
     def can_build_image(self):
         """
         :return: True if this flavor has a `build_image` method defined for building a docker
-        container capable of serving the model, False otherwise.
+                 container capable of serving the model, False otherwise.
         """
         return callable(getattr(self.__class__, 'build_image', None))
