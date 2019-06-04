91820ea3c72af5c41852dd6b4b7066bc6713ddc1
Andrew Chen <andrewmnchen@gmail.com>
2019-06-03 19:38:07 -0700

    java fluent sdk example

---
+++ mlflow/java/client/src/main/java/org/mlflow/tracking/ActiveRun.java
@@ -0,0 +1,92 @@
+package org.mlflow.tracking;
+
+import org.mlflow.MlflowMetric;
+import org.mlflow.MlflowParam;
+import org.mlflow.MlflowTag;
+import org.mlflow.api.proto.Service.*;
+
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class ActiveRun {
+    protected MlflowClient client;
+    protected RunInfo runInfo;
+
+    protected String getId() {
+        return runInfo.getRunId();
+    }
+
+    public ActiveRun(RunInfo runInfo, MlflowClient client) {
+        this.runInfo = runInfo;
+        this.client = client;
+    }
+
+    /**
+     * Synchronous and will throw MlflowClientException on failures.
+     */
+    public void logParam(String key, String value) {
+        client.logParam(getId(), key, value);
+    }
+
+    public void setTag(String key, String value) {
+        client.setTag(getId(), key, value);
+    }
+
+    public void logMetric(String key, double value) {
+        logMetric(key, value, 0);
+    }
+
+    public void logMetric(String key, double value, int step) {
+        client.logMetric(getId(), key, value, System.currentTimeMillis(), step);
+    }
+
+    public void logMetrics(Iterable<MlflowMetric> metrics) {
+        logMetrics(metrics, 0);
+    }
+
+    // TODO(andrew): Should this be it's own object in org.mlflow or should it be just the proto object.
+    public void logMetrics(Iterable<MlflowMetric> metrics, int step) {
+        List<Metric> protoMetrics = StreamSupport.stream(metrics.spliterator(), false).map((metric) ->
+                Metric.newBuilder()
+                        .setKey(metric.key)
+                        .setValue(metric.value)
+                        .setTimestamp(System.currentTimeMillis())
+                        .setStep(step)
+                        .build()
+        ).collect(Collectors.toList());
+        client.logBatch(getId(), protoMetrics, Collections.emptyList(), Collections.emptyList());
+    }
+
+    public void logParams(Iterable<MlflowParam> params) {
+        List<Param> protoParams = StreamSupport.stream(params.spliterator(), false).map((param) ->
+                Param.newBuilder()
+                        .setKey(param.key)
+                        .setValue(param.value)
+                        .build()
+        ).collect(Collectors.toList());
+        client.logBatch(getId(), Collections.emptyList(), protoParams, Collections.emptyList());
+    }
+
+    public void setTags(Iterable<MlflowTag> tags) {
+        List<RunTag> protoTags = StreamSupport.stream(tags.spliterator(), false).map((tag) ->
+                RunTag.newBuilder().setKey(tag.key).setValue(tag.value).build()
+        ).collect(Collectors.toList());
+        client.logBatch(getId(), Collections.emptyList(), Collections.emptyList(), protoTags);
+    }
+
+    public void logArtifact(Path localPath) {
+        client.logArtifact(getId(), localPath.toFile());
+    }
+
+    public void logArtifact(Path localPath, String artifactPath) {
+        client.logArtifact(getId(), localPath.toFile(), artifactPath);
+    }
+
+    public String getArtifactUri() {
+        return this.runInfo.getArtifactUri();
+    }
+}
---
+++ mlflow/java/client/src/main/java/org/mlflow/tracking/MlflowTrackingContext.java
@@ -0,0 +1,59 @@
+package org.mlflow.tracking;
+
+import org.mlflow.api.proto.Service.*;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MlflowTrackingContext {
+
+    private MlflowClient client;
+    private String experimentId;
+    private Deque<ActiveRun> activeRunStack = new ArrayDeque<>();
+
+    public MlflowTrackingContext() {
+        this(new MlflowClient(), MlflowClient.DEFAULT_EXPERIMENT_ID);
+    }
+
+    public MlflowTrackingContext(MlflowClient client) {
+        this(client, MlflowClient.DEFAULT_EXPERIMENT_ID);
+    }
+
+    public MlflowTrackingContext(MlflowClient client, String experimentId) {
+        this.client = client;
+        this.experimentId = experimentId;
+    }
+
+    public MlflowTrackingContext(String experimentId) {
+        this(new MlflowClient(), experimentId);
+    }
+
+    public static MlflowTrackingContext fromExperimentName(String experimentName) {
+        return null;
+    }
+
+    public ActiveRun startRun(String runName) {
+        CreateRun createRunRequest = CreateRun.newBuilder()
+                .setExperimentId(experimentId)
+                .setStartTime(System.currentTimeMillis())
+                .build();
+        RunInfo runInfo = client.createRun(experimentId);
+        ActiveRun activeRun = new ActiveRun(runInfo, client);
+        activeRunStack.push(activeRun);
+        return activeRun;
+    }
+
+    public ActiveRun endRun() {
+        ActiveRun endedRun = activeRunStack.pop();
+        client.setTerminated(endedRun.runInfo.getRunId(), RunStatus.FINISHED);
+        return endedRun;
+    }
+
+    /**
+     * NULLABLE
+     * @return
+     */
+    public ActiveRun getActiveRun() {
+        return activeRunStack.getFirst();
+    }
+}
