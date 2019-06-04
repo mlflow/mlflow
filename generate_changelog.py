6513ccd2d6efb04b80ee2eb94e0a99fba24296c4
sueann <sueann@databricks.com>
2019-06-03 17:38:23 -0700

    clean up ish

--- generate_changelog.py
+++ generate_changelog.py
@@ -18,9 +18,9 @@ def main():
                     " $ python generate_changelog.py --prev-branch branch-0.9.1 > commits-parsed-0.9.1-master.txt")
     parser.add_argument("--prev-branch", nargs='?', required=True,
                         help="Previous release branch to compare to, e.g. branch-0.8")
-    parser.add_argument("--curr-branch", nargs='?', default="master",
+    parser.add_argument("--curr-branch", nargs='?', default="upstream/master",
                         help="Current release (candidate) branch to compare to, e.g. branch-0.9. "
-                             "Defaults to 'master'.")
+                             "Defaults to 'upstream/master'.")
     parser.add_argument("--skip-github", dest="skip_github", const=True, default=False, action='store_const',
                         help="Skip querying github and use the cached commits file.")
 
@@ -46,70 +46,6 @@ def main():
         # for development of this script
         with open(raw_github_cache_file, "rb") as f:
             raw_commits = pickle.load(f)
-
-        original_info = {}  # won't need to do this next time. just set original_info = commits
-        for commit in commits:
-            original_info[commit["pr"]] = commit
-        extra_info = {}
-        sorted_prs = defaultdict(list)
-        multilabel_prs = []
-        for rjson in raw_commits:
-            # TODO: move this bit to the else block below once done debugging
-            pr_num = rjson['number']
-            labels = [n for n in [l['name'] for l in rjson['labels']] if n.startswith('rn/')]
-            extra_info[pr_num] = {
-                'pr': pr_num,
-                'labels': labels,
-                'body': rjson['body'],
-            }
-            if len(labels) > 1:
-                multilabel_prs.append(pr_num)
-            if len(labels) == 0:
-                sorted_prs["no-label"].append(pr_num)
-            for label in labels:
-                sorted_prs[label].append(pr_num)
-        for k in sorted_prs.keys():
-            assert k in ["rn/feature", "rn/breaking-change", "rn/bug-fix", "rn/documentation", "rn/none", "no-label"], k
-
-        def pr_list_text(categories, title):
-            prs = []
-            for cat in categories:
-                prs += sorted_prs[cat]
-            return title + " (%d)\n\n" % len(prs) + \
-                   "\n".join([format_oneline(original_info[pr_num]) for pr_num in prs]) + "\n"
-
-        # rn/breaking-change
-        breaking_text = pr_list_text(["rn/breaking-change"], "Breaking changes:")
-
-        # rn/feature
-        feature_text = pr_list_text(["rn/feature"],
-                                    "Features (to be divided into major and other):")
-
-        # rn/bug-fix, rn/documentation
-        bug_doc_text = pr_list_text(["rn/bug-fix", "rn/documentation"],
-                                    "Bug fixes and documentation updates:")
-
-        # rn/none
-        handle_to_prs = defaultdict(list)
-        for pr_num in sorted_prs["rn/none"]:
-            handle = original_info[pr_num]['author_github_handle']
-            handle_to_prs[handle].append("#" + str(pr_num))
-        small_list_text = "(" + "; ".join([", ".join(pr_nums + ["@" + handle])
-                                           for handle, pr_nums in handle_to_prs.items()]) + ")"
-        small_text = "Small bug fixes and doc updates " + \
-                     small_list_text + "\n"
-
-        # no label PRs
-        no_label_text = pr_list_text("no-label", "The following PRs need to be categorized:")
-
-        # PRs with multiple labels - let the release notes author know
-        multiple_text = "The following PRs were found to have multiple release notes labels: " + \
-            ", ".join([str(pr_num) for pr_num in multilabel_prs]) + "\n"
-
-        text_for_author = "\n".join([breaking_text, feature_text, bug_doc_text, small_text,
-                                     no_label_text, multiple_text])
-        print(text_for_author)
-
     else:
         raw_commits = []
         commits = {}
@@ -141,8 +77,82 @@ def main():
         with open(raw_github_cache_file, "wb") as f:
             pickle.dump(raw_commits, f)
 
-    print("All commits (%d):\n" % len(commits))
+    original_info = {}
+    if type(commits) is dict:
+        original_info = commits
+    else:
+        # older version of script saved a list instead of a dict
+        for commit in commits:
+            original_info[commit["pr"]] = commit
+    extra_info = {}
+    sorted_prs = defaultdict(list)
+    multilabel_prs = []
+    for rjson in raw_commits:
+        pr_num = rjson['number']
+        labels = [n for n in [l['name'] for l in rjson['labels']] if n.startswith('rn/')]
+        extra_info[pr_num] = {
+            'pr': pr_num,
+            'labels': labels,
+            'body': rjson['body'],
+        }
+        if len(labels) > 1:
+            multilabel_prs.append(pr_num)
+        if len(labels) == 0:
+            sorted_prs["no-label"].append(pr_num)
+        for label in labels:
+            sorted_prs[label].append(pr_num)
+    for k in sorted_prs.keys():
+        assert k in ["rn/feature", "rn/breaking-change", "rn/bug-fix", "rn/documentation", "rn/none", "no-label"], k
+
+    def pr_list_text(categories, title):
+        prs = []
+        for cat in categories:
+            prs += sorted_prs[cat]
+        return title + " (%d)\n\n" % len(prs) + \
+               "\n".join([format_oneline(original_info[pr_num]) for pr_num in prs]) + "\n"
+
+    # rn/breaking-change
+    breaking_text = pr_list_text(["rn/breaking-change"], "Breaking changes:")
+
+    # rn/feature
+    feature_text = pr_list_text(["rn/feature"],
+                                "Features (to be divided into major and other):")
+
+    # rn/bug-fix, rn/documentation
+    bug_doc_text = pr_list_text(["rn/bug-fix", "rn/documentation"],
+                                "Bug fixes and documentation updates:")
+
+    # rn/none
+    handle_to_prs = defaultdict(list)
+    for pr_num in sorted_prs["rn/none"]:
+        handle = original_info[pr_num]['author_github_handle']
+        handle_to_prs[handle].append("#" + str(pr_num))
+    small_list_text = "(" + "; ".join([", ".join(pr_nums + ["@" + handle])
+                                       for handle, pr_nums in handle_to_prs.items()]) + ")"
+    small_text = "Small bug fixes and doc updates " + \
+                 small_list_text + "\n"
+
+    # no label PRs
+    no_label_text = pr_list_text("no-label", "The following PRs need to be categorized:")
+
+    # PRs with multiple labels - let the release notes author know
+    multiple_text = "** The following PRs were found to have multiple release notes labels: " + \
+                    ", ".join([str(pr_num) for pr_num in multilabel_prs]) + "\n"
+
+    # Contributor name list for the blog post
+    contributor_text = g.log('--left-right', '--graph', '--cherry-pick', '--pretty=format:%an',
+                             'upstream/'+prev_branch+'...'+curr_branch)
+    contributors = sorted(set([c[2:] for c in contributor_text.split("\n")]))
+    contributor_text = "** For the blog post, the list of contributors: " + \
+                       ", ".join(contributors) + "\n"
+
+    text_for_author = "\n".join([breaking_text, feature_text, bug_doc_text, small_text,
+                                 no_label_text, multiple_text, contributor_text])
+    print(text_for_author)
+
+    print("** All commits (%d):\n" % len(commits))
     print("\n".join([format_oneline(c) for c in commits]))
+
 
 if __name__ == '__main__':
     main()
