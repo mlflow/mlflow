6e9e272e7dbfbbc025b110274c16ea0570eabbaf
Andrew Chen <andrewmnchen@gmail.com>
2019-06-20 10:47:42 -0700

    touch ups to achieve parentRunID and databricks magic

--- mlflow/java/client/src/main/java/org/mlflow/tracking/MlflowTrackingContext.java
+++ mlflow/java/client/src/main/java/org/mlflow/tracking/MlflowTrackingContext.java
@@ -1,81 +1,114 @@
 package org.mlflow.tracking;
 
 import org.mlflow.api.proto.Service.*;
+import org.mlflow.tracking.utils.DatabricksContext;
+import org.mlflow.tracking.utils.MlflowTagConstants;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 import java.util.function.Consumer;
 
 public class MlflowTrackingContext {
+  private MlflowClient client;
+  private String experimentId;
+  private Deque<ActiveRun> activeRunStack = new ArrayDeque<>();
 
-    private MlflowClient client;
-    private String experimentId;
-    private Deque<ActiveRun> activeRunStack = new ArrayDeque<>();
+  public MlflowTrackingContext() {
+    this(new MlflowClient(), getDefaultExperimentId());
+  }
 
-    public MlflowTrackingContext() {
-        this(new MlflowClient(), MlflowClient.DEFAULT_EXPERIMENT_ID);
+  public MlflowTrackingContext(MlflowClient client) {
+    this(client, getDefaultExperimentId());
+  }
+
+  public MlflowTrackingContext(MlflowClient client, String experimentId) {
+    this.client = client;
+    this.experimentId = experimentId;
+  }
+
+  public MlflowTrackingContext(String experimentId) {
+    this(new MlflowClient(), experimentId);
+  }
+
+  public ActiveRun startRun(String runName) {
+    return startRun(runName, false);
+  }
+
+  public ActiveRun startRun(String runName, boolean nested) {
+    if (!nested && !activeRunStack.isEmpty()) {
+      String existingRunId = getActiveRun().get().getId();
+      throw new IllegalArgumentException(String.format("Run with ID %s is already active. To start a nested run call " +
+        "startRun with nested=true", existingRunId));
+    }
+    Map<String, String> tags = new HashMap<>();
+    tags.put(MlflowTagConstants.RUN_NAME, runName);
+    tags.put(MlflowTagConstants.USER, System.getProperty("user.name"));
+    tags.put(MlflowTagConstants.SOURCE_TYPE, "LOCAL");
+
+    if (!activeRunStack.isEmpty()) {
+      tags.put(MlflowTagConstants.PARENT_RUN_ID, getActiveRun().get().getId());
     }
 
-    public MlflowTrackingContext(MlflowClient client) {
-        this(client, MlflowClient.DEFAULT_EXPERIMENT_ID);
+    // Add tags from DatabricksContext if they exist
+    DatabricksContext databricksContext = DatabricksContext.createIfAvailable();
+    if (databricksContext != null) {
+      tags.putAll(databricksContext.getTags());
     }
 
-    public MlflowTrackingContext(MlflowClient client, String experimentId) {
-        this.client = client;
-        this.experimentId = experimentId;
+    CreateRun.Builder createRunBuilder = CreateRun.newBuilder()
+      .setExperimentId(experimentId)
+      .setStartTime(System.currentTimeMillis());
+    for (Map.Entry<String, String> tag: tags.entrySet()) {
+      createRunBuilder.addTags(
+        RunTag.newBuilder().setKey(tag.getKey()).setValue(tag.getValue()).build());
     }
+    RunInfo runInfo = client.createRun(createRunBuilder.build());
 
-    public MlflowTrackingContext(String experimentId) {
-        this(new MlflowClient(), experimentId);
-    }
+    ActiveRun activeRun = new ActiveRun(runInfo, client);
+    activeRunStack.push(activeRun);
+    return activeRun;
+  }
 
-    public static MlflowTrackingContext fromExperimentName(String experimentName) {
-        return null;
+  // Context APIs
+  public void withActiveRun(String runName, Consumer<ActiveRun> activeRunFunction) {
+    ActiveRun newRun = startRun(runName);
+    try {
+      activeRunFunction.accept(newRun);
+    } catch(Exception e) {
+      endRun(RunStatus.FAILED);
+      return;
     }
+    endRun(RunStatus.FINISHED);
+  }
 
-    public ActiveRun startRun(String runName) {
-        CreateRun createRunRequest = CreateRun.newBuilder()
-                .setExperimentId(experimentId)
-                .setStartTime(System.currentTimeMillis())
-                .addTags(RunTag.newBuilder().setKey("mlflow.runName").setValue(runName).build())
-                .addTags(RunTag.newBuilder().setKey("mlflow.user")
-                        .setValue(System.getProperty("user.name")).build())
-                .addTags(RunTag.newBuilder().setKey("mlflow.source.type")
-                        .setValue("LOCAL").build())
-                .build();
-        RunInfo runInfo = client.createRun(createRunRequest);
-        ActiveRun activeRun = new ActiveRun(runInfo, client);
-        activeRunStack.push(activeRun);
-        return activeRun;
-    }
+  public ActiveRun endRun() {
+    return endRun(RunStatus.FINISHED);
+  }
 
-    // Context APIs
-    public void withActiveRun(String runName, Consumer<ActiveRun> activeRunFunction) {
-        ActiveRun newRun = startRun(runName);
-        try {
-            activeRunFunction.accept(newRun);
-        } catch(Exception e) {
-            endRun(RunStatus.FAILED);
-            return;
-        }
-        endRun(RunStatus.FINISHED);
-    }
+  public ActiveRun endRun(RunStatus status) {
+    ActiveRun endedRun = activeRunStack.pop();
+    client.setTerminated(endedRun.runInfo.getRunId(), status);
+    return endedRun;
+  }
 
-    public ActiveRun endRun() {
-        return endRun(RunStatus.FINISHED);
+  /**
+   * NULLABLE
+   * @return
+   */
+  public Optional<ActiveRun> getActiveRun() {
+    if (activeRunStack.isEmpty()) {
+      return Optional.empty();
     }
+    return Optional.of(activeRunStack.getFirst());
+  }
 
-    public ActiveRun endRun(RunStatus status) {
-        ActiveRun endedRun = activeRunStack.pop();
-        client.setTerminated(endedRun.runInfo.getRunId(), status);
-        return endedRun;
+  private static String getDefaultExperimentId() {
+    DatabricksContext databricksContext = DatabricksContext.createIfAvailable();
+    if (databricksContext != null) {
+      String notebookId = databricksContext.getNotebookId();
+      if (notebookId != null) {
+        return notebookId;
+      }
     }
-
-    /**
-     * NULLABLE
-     * @return
-     */
-    public ActiveRun getActiveRun() {
-        return activeRunStack.getFirst();
-    }
+    return MlflowClient.DEFAULT_EXPERIMENT_ID;
+  }
 }
---
+++ mlflow/java/client/src/main/java/org/mlflow/tracking/utils/DatabricksContext.java
@@ -0,0 +1,108 @@
+package org.mlflow.tracking.utils;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DatabricksContext {
+  public static final String CONFIG_PROVIDER_CLASS_NAME =
+    "com.databricks.config.DatabricksClientSettingsProvider";
+  private static final Logger logger = LoggerFactory.getLogger(
+    DatabricksContext.class);
+  private final Map<String, String> configProvider;
+
+  private DatabricksContext(Map<String, String> configProvider) {
+    this.configProvider = configProvider;
+  }
+
+  public static DatabricksContext createIfAvailable() {
+      Map<String, String> configProvider = getConfigProviderIfAvailable(CONFIG_PROVIDER_CLASS_NAME);
+      if (configProvider == null) {
+        return null;
+      }
+      return new DatabricksContext(configProvider);
+  }
+
+  public Map<String, String> getTags() {
+    Map<String, String> tags = new HashMap<>();
+    if (!isInDatabricksNotebook()) {
+      return tags;
+    }
+    String notebookId = getNotebookId();
+    if (notebookId != null) {
+      tags.put(MlflowTagConstants.DATABRICKS_NOTEBOOK_ID, notebookId);
+    }
+    String notebookPath = getNotebookPath();
+    if (notebookPath != null) {
+      tags.put(MlflowTagConstants.SOURCE_NAME, notebookPath);
+      tags.put(MlflowTagConstants.DATABRICKS_NOTEBOOK_PATH, notebookPath);
+      tags.put(MlflowTagConstants.SOURCE_TYPE, "NOTEBOOK");
+    }
+    String webappUrl = getWebappUrl();
+    if (webappUrl != null) {
+      tags.put(MlflowTagConstants.DATABRICKS_WEBAPP_URL, webappUrl);
+    }
+    return tags;
+  }
+
+  private boolean isInDatabricksNotebook() {
+    String aclPathOfAclRoot = configProvider.get("aclPathOfAclRoot");
+    return aclPathOfAclRoot != null && aclPathOfAclRoot.startsWith("/workspace");
+  }
+
+  /**
+   * Should only be called if isInDatabricksNotebook() is true.
+   */
+  public String getNotebookId() {
+    if (!isInDatabricksNotebook()) {
+      throw new IllegalArgumentException(
+        "getNotebookId() should not be called when isInDatabricksNotebook() is false"
+      );
+    };
+    String aclPathOfAclRoot = configProvider.get("aclPathOfAclRoot");
+    String[] paths = aclPathOfAclRoot.split("/");
+    if (paths.length == 0) {
+      return null;
+    }
+    return paths[paths.length-1];
+  }
+
+  /**
+   * Should only be called if isInDatabricksNotebook() is true.
+   */
+  private String getNotebookPath() {
+    if (!isInDatabricksNotebook()) {
+      throw new IllegalArgumentException(
+        "getNotebookPath() should not be called when isInDatabricksNotebook() is false"
+      );
+    };
+    return configProvider.get("notebookPath");
+  }
+
+  /**
+   * Should only be called if isInDatabricksNotebook() is true.
+   */
+  private String getWebappUrl() {
+    if (!isInDatabricksNotebook()) {
+      throw new IllegalArgumentException(
+        "getWebappUrl() should not be called when isInDatabricksNotebook() is false"
+      );
+    };
+    return configProvider.get("host");
+  }
+
+  public static Map<String, String> getConfigProviderIfAvailable(String className) {
+    try {
+      Class<?> cls = Class.forName(className);
+      return (Map<String, String>) cls.newInstance();
+    } catch (ClassNotFoundException e) {
+      return null;
+    } catch (IllegalAccessException | InstantiationException e) {
+      logger.warn("Found but failed to invoke dynamic config provider", e);
+      return null;
+    }
+  }
+}
