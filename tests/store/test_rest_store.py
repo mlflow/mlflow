611477abb582efa6c87a9e45137d1994254c43e1
Andrew Crozier <wacrozier@gmail.com>
2019-06-21 18:54:53 +0100

    Fix mocks

--- tests/store/test_rest_store.py
+++ tests/store/test_rest_store.py
@@ -116,10 +116,11 @@ class TestRestStore(unittest.TestCase):
         source_name = "rest test"
 
         source_name_patch = mock.patch(
-            "mlflow.tracking.context._get_source_name", return_value=source_name
+            "mlflow.tracking.context.default_context._get_source_name", return_value=source_name
         )
         source_type_patch = mock.patch(
-            "mlflow.tracking.context._get_source_type", return_value=SourceType.LOCAL
+            "mlflow.tracking.context.default_context._get_source_type",
+            return_value=SourceType.LOCAL
         )
         with mock.patch('mlflow.store.rest_store.http_request') as mock_http, \
                 mock.patch('mlflow.tracking.utils._get_store', return_value=store), \
