b212b8af77908eb8757085abad72a23ad44bc5fd
Richard Zang <Zangr@users.noreply.github.com>
2019-06-23 01:55:31 -0700

    refactor to string state

--- mlflow/server/js/src/components/ParallelCoordinatesPlotView.js
+++ mlflow/server/js/src/components/ParallelCoordinatesPlotView.js
@@ -5,8 +5,6 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 
 const AXIS_LABEL_CLS = '.pcp-plot .parcoords .y-axis .axis-heading .axis-title';
-const DIM_TYPE_PARAM = 'param';
-const DIM_TYPE_METRIC = 'metric';
 
 export class ParallelCoordinatesPlotView extends React.Component {
   static propTypes = {
@@ -18,52 +16,56 @@ export class ParallelCoordinatesPlotView extends React.Component {
   };
 
   state = {
-    dimensions: [...this.props.paramDimensions, ...this.props.metricDimensions],
+    sequence: [...this.props.paramKeys, ...this.props.metricKeys],
   };
 
   static getDerivedStateFromProps(props, state) {
-    const dimensionsFromState = state.dimensions.map((dimension) => dimension.label);
-    const dimensionsFromProps = [...props.paramKeys, ...props.metricKeys];
-    if (dimensionsFromState.sort().join() !== dimensionsFromProps.sort().join()) {
-      console.log('set derived state = ', [...props.paramKeys, ...props.metricKeys].join(','));
-      return { dimensions: [...props.paramDimensions, ...props.metricDimensions] };
+    const keysFromProps = [...props.paramKeys, ...props.metricKeys];
+    const keysFromState = state.sequence;
+    if (!_.isEqual(_.sortBy(keysFromProps), _.sortBy(keysFromState))) {
+      console.log('set derived state = ', keysFromProps.join(','));
+      return { sequence: keysFromProps };
     }
     return null;
   }
 
   getData() {
-    const { dimensions } = this.state;
-    const lastMetricDimension = this.findLastMetricDimensionFromState();
+    const { sequence } = this.state;
+    const { paramDimensions, metricDimensions } = this.props;
+    const lastMetricKey = this.findLastMetricKeyFromState();
+    const lastMetricDimension = this.props.metricDimensions.find((d) => d.label === lastMetricKey);
     const colorScaleConfigs =
       ParallelCoordinatesPlotView.getColorScaleConfigsForDimension(lastMetricDimension);
+    const orderedDimensions = ParallelCoordinatesPlotView.getDimensionsOrderedBySequence(
+      [...paramDimensions, ...metricDimensions],
+      sequence,
+    );
     return [
       {
         type: 'parcoords',
-        line: {
-          ...colorScaleConfigs,
-        },
-        dimensions: _.clone(dimensions), // dimensions will be mutated by plotly
+        line: { ...colorScaleConfigs },
+        dimensions: orderedDimensions,
       },
     ];
   };
 
-  static getAllAxisLabelElementsFromDom = () =>
-    Array.from(document.querySelectorAll(AXIS_LABEL_CLS));
+  static getDimensionsOrderedBySequence(dimensions, sequence) {
+    return _.sortBy(dimensions, [(dimension) => sequence.indexOf(dimension.label)]);
+  }
 
-  static getSequencedAxisLabelsFromDom = () =>
-    ParallelCoordinatesPlotView.getAllAxisLabelElementsFromDom().map((el) => el.innerHTML);
+  static getSequenceFromDom = () =>
+    Array.from(document.querySelectorAll(AXIS_LABEL_CLS)).map((el) => el.innerHTML);
 
-  findLastMetricDimensionFromState() {
-    const { dimensions } = this.state;
+  findLastMetricKeyFromState() {
+    const { sequence } = this.state;
     const metricsKeySet = new Set(this.props.metricKeys);
-    return _.findLast(dimensions, (dimension) => metricsKeySet.has(dimension.label));
+    return _.findLast(sequence, (key) => metricsKeySet.has(key));
   }
 
   findLastMetricFromDom() {
-    // The source of truth of current label element is in the dom
-    const dimensionLabels = ParallelCoordinatesPlotView.getSequencedAxisLabelsFromDom();
+    const sequence = ParallelCoordinatesPlotView.getSequenceFromDom();
     const metricsKeySet = new Set(this.props.metricKeys);
-    return _.findLast(dimensionLabels, (label) => metricsKeySet.has(label));
+    return _.findLast(sequence, (key) => metricsKeySet.has(key));
   }
 
   static getColorScaleConfigsForDimension(dimension) {
@@ -79,14 +81,13 @@ export class ParallelCoordinatesPlotView extends React.Component {
     }
   }
 
-  updateMetricAxisLabels = () => {
-    const { metricDimensions } = this.props;
-    const metricsLabelSet = new Set(metricDimensions.map((dimension) => dimension.label));
+  updateMetricAxisLabelStyle = () => {
+    const metricsKeySet = new Set(this.props.metricKeys);
     const axisLabelElements = document.querySelectorAll(AXIS_LABEL_CLS);
     // Note(Zangr) 2019-06-20 This assumes name uniqueness across params & metrics. Find a way to
     // make it more deterministic. Ex. Add add different data attributes to indicate axis kind.
     Array.from(axisLabelElements)
-      .filter((el) => metricsLabelSet.has(el.innerHTML))
+      .filter((el) => metricsKeySet.has(el.innerHTML))
       .forEach((el) => {
         el.style.fill = 'green';
         el.style.fontWeight = 'bold';
@@ -94,27 +95,23 @@ export class ParallelCoordinatesPlotView extends React.Component {
   };
 
   maybeUpdateStateForColorScale = () => {
-    const lastMetricDimensionFromState = this.findLastMetricDimensionFromState();
+    const lastMetricKeyFromState = this.findLastMetricKeyFromState();
     const lastMetricFromDom = this.findLastMetricFromDom();
-    // If we found diff on the last(right most) metric dimension, set state with dimension sorted
-    // based on current axis label order from DOM
-    if (lastMetricDimensionFromState && lastMetricDimensionFromState.label !== lastMetricFromDom) {
-      const labelSequence = ParallelCoordinatesPlotView.getSequencedAxisLabelsFromDom();
-      const sortedDimensions = [...this.state.dimensions.sort((d1, d2) => {
-        return labelSequence.indexOf(d1.label) - labelSequence.indexOf(d2.label);
-      })];
-      console.log('colorUpdate state = ', sortedDimensions.map(d => d.label).join());
-      this.setState({ dimensions: sortedDimensions });
+    // If we found diff on the last(right most) metric dimension, update sequence and rerender to
+    // trigger color scale change.
+    if (lastMetricKeyFromState !== lastMetricFromDom) {
+      const sequenceFromDom = ParallelCoordinatesPlotView.getSequenceFromDom();
+      this.setState({ sequence: sequenceFromDom });
     }
   };
 
   handlePlotUpdate = () => {
-    this.updateMetricAxisLabels();
+    this.updateMetricAxisLabelStyle();
     this.maybeUpdateStateForColorScale();
   };
 
   render() {
-    console.log('render state = ', this.state.dimensions.map(d => d.label).join());
+    console.log('render state = ', this.state.sequence.join());
     return (
       <Plot
         layout={{ autosize: true }}
@@ -138,7 +135,6 @@ const mapStateToProps = (state, ownProps) => {
       const { value } = paramsByRunUuid[runUuid][paramKey];
       return isNaN(value) ? value : Number(value);
     }),
-    dimType: DIM_TYPE_PARAM,
   }));
   const metricDimensions = metricKeys.map((metricKey) => ({
     label: metricKey,
@@ -146,7 +142,6 @@ const mapStateToProps = (state, ownProps) => {
       const { value } = latestMetricsByRunUuid[runUuid][metricKey];
       return isNaN(value) ? value : Number(value);
     }),
-    dimType: DIM_TYPE_METRIC,
   }));
   return { paramDimensions, metricDimensions };
 };
