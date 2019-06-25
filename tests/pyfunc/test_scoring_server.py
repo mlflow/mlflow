0492ff79f773bb6d8016caf2265c9592421b0667
Zhun Xu <zhunxu@AdminisatorsMBP.fios-router.home>
2019-06-25 17:27:23 -0400

    format minor fix in tests

--- tests/pyfunc/test_scoring_server.py
+++ tests/pyfunc/test_scoring_server.py
@@ -223,8 +223,7 @@ def test_parse_json_input_records_oriented_to_numpy_array():
                         ("col_z", [random_str(4) for _ in range(size)]),
                         ("col_a", [random_int() for _ in range(size)])])
     p0 = pd.DataFrame.from_dict(data)
-    p1 = np.array([[a, b, c] for a, b, c in zip(data['col_m'], data['col_z'], data['col_a'])],
-                  )
+    p1 = np.array([[a, b, c] for a, b, c in zip(data['col_m'], data['col_z'], data['col_a'])])
     p2 = pyfunc_scoring_server.parse_records_oriented_json_input_to_numpy(
         p0.to_json(orient="records"))
     np.testing.assert_array_equal(p1, p2)
@@ -237,8 +236,7 @@ def test_parse_json_input_split_oriented_to_numpy_array():
                         ("col_z", [random_str(4) for _ in range(size)]),
                         ("col_a", [random_int() for _ in range(size)])])
     p0 = pd.DataFrame.from_dict(data)
-    p1 = np.array([[a, b, c] for a, b, c in zip(data['col_m'], data['col_z'], data['col_a'])],
-                  )
+    p1 = np.array([[a, b, c] for a, b, c in zip(data['col_m'], data['col_z'], data['col_a'])])
     p2 = pyfunc_scoring_server.parse_split_oriented_json_input_to_numpy(
         p0.to_json(orient="split"))
     np.testing.assert_array_equal(p1, p2)
