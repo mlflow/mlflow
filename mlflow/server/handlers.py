22154e0af9c5790e65dc293975d7f88a62909dac
Uwe L. Korn <xhochy@users.noreply.github.com>
2019-06-30 20:13:16 +0200

    Make requests to ajax-api in JavaScript relative (#1413)
    Make requests to ajax-api in JavaScript relative, so the mlflow UI can now be served from a (non-root) path behind a proxy.
    
    When running the mlflow UI behind a proxy, it may no longer be served directly
     on `/` but in a subpath. As it is a single page web-app, the JavaScript code will
    always be executed in the same path and thus we don't need any absolute references.
    
    Fixes #1120

--- mlflow/server/handlers.py
+++ mlflow/server/handlers.py
@@ -22,6 +22,14 @@ from mlflow.utils.proto_json_utils import message_to_json, parse_dict
 from mlflow.utils.validation import _validate_batch_log_api_req
 
 _store = None
+STATIC_PREFIX_ENV_VAR = "_MLFLOW_STATIC_PREFIX"
+
+
+def _add_static_prefix(route):
+    prefix = os.environ.get(STATIC_PREFIX_ENV_VAR)
+    if prefix:
+        return prefix + route
+    return route
 
 
 def _get_file_store(store_uri, artifact_uri):
@@ -372,7 +380,7 @@ def _get_paths(base_path):
     We should register paths like /api/2.0/preview/mlflow/experiment and
     /ajax-api/2.0/preview/mlflow/experiment in the Flask router.
     """
-    return ['/api/2.0{}'.format(base_path), '/ajax-api/2.0{}'.format(base_path)]
+    return ['/api/2.0{}'.format(base_path), _add_static_prefix('/ajax-api/2.0{}'.format(base_path))]
 
 
 def get_endpoints():
