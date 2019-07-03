39836b999585ed1dff17dd9581779d31e911a56e
Apurva Koti <apurva.koti@databricks.com>
2019-07-03 15:33:03 -0700

    Fix type error

--- mlflow/tensorflow.py
+++ mlflow/tensorflow.py
@@ -373,10 +373,9 @@ class __MLflowTfKerasCallback(Callback):
             lr = opt._lr if type(opt._lr) is float else tensorflow.keras.backend.eval(opt._lr)
             mlflow.log_param('learning_rate', lr)
         if hasattr(opt, '_epsilon'):
-            lr = opt._epsilon if type(opt._epsilon) is float \
+            epsilon = opt._epsilon if type(opt._epsilon) is float \
                 else tensorflow.keras.backend.eval(opt._epsilon)
-            mlflow.log_param('epsilon',
-                             tensorflow.keras.backend.eval(opt._epsilon))
+            mlflow.log_param('epsilon', epsilon)
         l = []
         self.model.summary(print_fn=l.append)
         summary = '\n'.join(l)
