254292d8d181ee35327d405301102cea71d3e20c
tomasatdatabricks <tomas.nykodym@databricks.com>
2019-07-03 14:15:09 -0700

    Fixed linter complaint.

--- mlflow/keras/_load_tf_keras.py
+++ mlflow/keras/_load_tf_keras.py
@@ -2,5 +2,5 @@ import mlflow.keras
 
 
 def _load_pyfunc(path):
-    import tensorflow.keras
+    import tensorflow.keras  # pylint: disable=import-error
     return mlflow.keras._load_pyfunc(path, keras_module=tensorflow.keras)
