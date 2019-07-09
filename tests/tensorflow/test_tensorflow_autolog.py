013856c2ea2890cd062e639b3741a97ccd2db240
Apurva Koti <apurva.koti@databricks.com>
2019-07-09 15:50:55 -0700

    Addressing code review

--- tests/tensorflow/test_tensorflow_autolog.py
+++ tests/tensorflow/test_tensorflow_autolog.py
@@ -14,200 +14,165 @@ from tensorflow.python.keras import layers
 
 import mlflow
 import mlflow.tensorflow
+import mlflow.keras
 
 SavedModelInfo = collections.namedtuple(
         "SavedModelInfo",
         ["path", "meta_graph_tags", "signature_def_key", "inference_df", "expected_results_df"])
 
+client = mlflow.tracking.MlflowClient()
+
+
+def random_one_hot_labels(shape):
+    n, n_class = shape
+    classes = np.random.randint(0, n_class, n)
+    labels = np.zeros((n, n_class))
+    labels[np.arange(n), classes] = 1
+    return labels
 
 @pytest.fixture
-def tf_keras_random_data():
+def random_train_data():
+    return np.random.random((1000, 32))
+
+@pytest.fixture
+def random_train_data_labels():
+    return random_one_hot_labels((1000, 10))
+
+@pytest.fixture
+def tf_keras_random_data_run(random_train_data, random_train_data_labels):
     mlflow.tensorflow.autolog(metrics_every_n_steps=2)
-    def random_one_hot_labels(shape):
-        n, n_class = shape
-        classes = np.random.randint(0, n_class, n)
-        labels = np.zeros((n, n_class))
-        labels[np.arange(n), classes] = 1
-        return labels
+    with mlflow.start_run() as run:
+        data = random_train_data
+        labels = random_train_data_labels
 
-    data = np.random.random((1000, 32))
-    labels = random_one_hot_labels((1000, 10))
+        model = tf.keras.Sequential()
 
-    model = tf.keras.Sequential()
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(10, activation='softmax'))
 
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(10, activation='softmax'))
+        model.compile(optimizer=tf.train.AdamOptimizer(0.001),
+                      loss='categorical_crossentropy',
+                      metrics=['accuracy'])
 
-    model.compile(optimizer=tf.train.AdamOptimizer(0.001),
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-    run = mlflow.start_run()
-    model.fit(data, labels, epochs=10)
-    mlflow.end_run()
-    return run.info.run_id
+        model.fit(data, labels, epochs=10)
+
+    return client.get_run(run.info.run_id)
 
 
-@pytest.fixture
-def get_tf_keras_random_data_run_instance(tf_keras_random_data):
-    client = mlflow.tracking.MlflowClient()
-    run = client.get_run(tf_keras_random_data)
-    return run
+def test_tf_keras_autolog_logs_expected_data(tf_keras_random_data_run):
+    data = tf_keras_random_data_run.data
 
-
-@pytest.mark.small
-def test_tf_keras_autolog_logs_epoch_acc(get_tf_keras_random_data_run_instance):
-    assert 'epoch_acc' in get_tf_keras_random_data_run_instance.data.metrics
-
-
-@pytest.mark.small
-def test_tf_keras_autolog_logs_epoch_loss(get_tf_keras_random_data_run_instance):
-    assert 'epoch_loss' in get_tf_keras_random_data_run_instance.data.metrics
-
-
-@pytest.mark.small
-def test_tf_keras_autolog_logs_optimizer_name(get_tf_keras_random_data_run_instance):
-    assert 'optimizer_name' in get_tf_keras_random_data_run_instance.data.params
-
-
-@pytest.mark.small
-def test_tf_keras_autolog_logs_model_summary(get_tf_keras_random_data_run_instance):
-    assert 'summary' in get_tf_keras_random_data_run_instance.data.tags
-    assert get_tf_keras_random_data_run_instance.data.tags['summary'] is not ''
+    assert 'epoch_acc' in data.metrics
+    assert 'epoch_loss' in data.metrics
+    assert 'optimizer_name' in data.params
+    assert data.params['optimizer_name'] == 'AdamOptimizer'
+    assert 'summary' in tf_keras_random_data_run.data.tags
+    assert 'Model: "sequential"' in tf_keras_random_data_run.data.tags['summary']
+    assert 'Total params: 6,922' in tf_keras_random_data_run.data.tags['summary']
 
 
 @pytest.mark.large
-def test_tf_keras_autolog_saves_model(get_tf_keras_random_data_run_instance):
-    client = mlflow.tracking.MlflowClient()
-    artifacts = client.list_artifacts(get_tf_keras_random_data_run_instance.info.run_id)
+def test_tf_keras_autolog_model_can_load_from_artifact(tf_keras_random_data_run):
+    artifacts = client.list_artifacts(tf_keras_random_data_run.info.run_id)
     artifacts = map(lambda x: x.path, artifacts)
     assert 'model' in artifacts
     assert 'tensorboard_logs' in artifacts
+    model = mlflow.keras.load_model("runs:/" + tf_keras_random_data_run.info.run_id +
+                                    "/model")
+
 
 
 @pytest.fixture
 def tf_core_random_tensors():
     mlflow.tensorflow.autolog(metrics_every_n_steps=1)
-    run = mlflow.start_run()
-    sess = tf.Session()
+    with mlflow.start_run() as run:
+        sess = tf.Session()
+        a = tf.constant(3.0, dtype=tf.float32)
+        b = tf.constant(4.0)
+        total = a + b
+        tf.summary.scalar('a', a)
+        tf.summary.scalar('b', b)
+        merged = tf.summary.merge_all()
+        dir = tempfile.mkdtemp()
+        writer = tf.summary.FileWriter(dir, sess.graph)
+        with sess.as_default():
+            summary, _ = sess.run([merged, total])
+        writer.add_summary(summary)
+        shutil.rmtree(dir)
+        writer.close()
 
-    a = tf.constant(3.0, dtype=tf.float32)
-    b = tf.constant(4.0)  # also tf.float32 implicitly
-    total = a + b
-    tf.summary.scalar('a', a)
-    tf.summary.scalar('b', b)
-    merged = tf.summary.merge_all()
-    dir = tempfile.mkdtemp()
-    writer = tf.summary.FileWriter(dir, sess.graph)
-    with sess.as_default():
-        summary, _ = sess.run([merged, total])
-
-    writer.add_summary(summary)
-
-    shutil.rmtree(dir)
-
-    writer.close()
-
-    mlflow.end_run()
-
-    return run.info.run_id
+    return client.get_run(run.info.run_id)
 
 
-@pytest.fixture
-def get_tf_core_random_tensors_run_instance(tf_core_random_tensors):
-    client = mlflow.tracking.MlflowClient()
-    run = client.get_run(tf_core_random_tensors)
-    return run
-
-
-@pytest.mark.small
-def test_tf_core_autolog_logs_scalars(get_tf_core_random_tensors_run_instance):
-    assert 'a' in get_tf_core_random_tensors_run_instance.data.metrics
-    assert get_tf_core_random_tensors_run_instance.data.metrics['a'] == 3.0
-    assert 'b' in get_tf_core_random_tensors_run_instance.data.metrics
-    assert get_tf_core_random_tensors_run_instance.data.metrics['b'] == 4.0
+def test_tf_core_autolog_logs_scalars(tf_core_random_tensors):
+    assert 'a' in tf_core_random_tensors.data.metrics
+    assert tf_core_random_tensors.data.metrics['a'] == 3.0
+    assert 'b' in tf_core_random_tensors.data.metrics
+    assert tf_core_random_tensors.data.metrics['b'] == 4.0
 
 
 @pytest.fixture
 def tf_estimator_random_data():
     mlflow.tensorflow.autolog(metrics_every_n_steps=1)
-    run = mlflow.start_run()
-    dir = tempfile.mkdtemp()
-    CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
-    SPECIES = ['Setosa', 'Versicolor', 'Virginica']
+    with mlflow.start_run() as run:
+        dir = tempfile.mkdtemp()
+        CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
+        SPECIES = ['Setosa', 'Versicolor', 'Virginica']
 
-    train_path = tf.keras.utils.get_file(
-        "iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv")
-    test_path = tf.keras.utils.get_file(
-        "iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
+        train_path = tf.keras.utils.get_file(
+            "iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv")
+        test_path = tf.keras.utils.get_file(
+            "iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
 
-    train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
-    test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
+        train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
+        test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
 
-    train_y = train.pop('Species')
-    test_y = test.pop('Species')
+        train_y = train.pop('Species')
+        test_y = test.pop('Species')
 
-    def input_fn(features, labels, training=True, batch_size=256):
-        """An input function for training or evaluating"""
-        # Convert the inputs to a Dataset.
-        dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+        def input_fn(features, labels, training=True, batch_size=256):
+            """An input function for training or evaluating"""
+            # Convert the inputs to a Dataset.
+            dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
 
-        # Shuffle and repeat if you are in training mode.
-        if training:
-            dataset = dataset.shuffle(1000).repeat()
+            # Shuffle and repeat if you are in training mode.
+            if training:
+                dataset = dataset.shuffle(1000).repeat()
 
-        return dataset.batch(batch_size)
+            return dataset.batch(batch_size)
 
-    my_feature_columns = []
-    for key in train.keys():
-        my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+        my_feature_columns = []
+        for key in train.keys():
+            my_feature_columns.append(tf.feature_column.numeric_column(key=key))
 
-    feature_spec = {}
-    for feature in CSV_COLUMN_NAMES:
-        feature_spec[feature] = tf.placeholder(dtype="float", name=feature, shape=[150])
+        feature_spec = {}
+        for feature in CSV_COLUMN_NAMES:
+            feature_spec[feature] = tf.placeholder(dtype="float", name=feature, shape=[150])
 
-    receiver_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(feature_spec)
+        receiver_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(feature_spec)
 
-    classifier = tf.estimator.DNNClassifier(
-        feature_columns=my_feature_columns,
-        # Two hidden layers of 10 nodes each.
-        hidden_units=[30, 10],
-        # The model must choose between 3 classes.
-        n_classes=3,
-        model_dir=dir)
+        classifier = tf.estimator.DNNClassifier(
+            feature_columns=my_feature_columns,
+            # Two hidden layers of 10 nodes each.
+            hidden_units=[30, 10],
+            # The model must choose between 3 classes.
+            n_classes=3,
+            model_dir=dir)
 
-    classifier.train(
-        input_fn=lambda: input_fn(train, train_y, training=True),
-        steps=5000)
-    classifier.export_saved_model(dir, receiver_fn)
-    mlflow.end_run()
+        classifier.train(
+            input_fn=lambda: input_fn(train, train_y, training=True),
+            steps=5000)
+        classifier.export_saved_model(dir, receiver_fn)
+
     shutil.rmtree(dir)
-    return run.info.run_id
+    return client.get_run(run.info.run_id)
 
-
-@pytest.fixture
-def get_tf_estimator_random_data_run_instance(tf_estimator_random_data):
-    client = mlflow.tracking.MlflowClient()
-    run = client.get_run(tf_estimator_random_data)
-    return run
-
-
-@pytest.mark.small
-def test_tf_estimator_autolog_logs_metrics(get_tf_estimator_random_data_run_instance):
-    metrics = get_tf_estimator_random_data_run_instance.data.metrics
-    assert len(metrics) > 0
 
 @pytest.mark.large
-def test_tf_estimator_autolog_saves_model(get_tf_estimator_random_data_run_instance):
-    client = mlflow.tracking.MlflowClient()
-    artifacts = client.list_artifacts(get_tf_estimator_random_data_run_instance.info.run_id)
+def test_tf_estimator_autolog_logs_metrics_artifacts(tf_estimator_random_data):
+    metrics = tf_estimator_random_data.data.metrics
+    assert len(metrics) > 0
+    artifacts = client.list_artifacts(tf_estimator_random_data.info.run_id)
     artifacts = map(lambda x: x.path, artifacts)
     assert 'model' in artifacts
-
-
-
-
-
-
-
-
