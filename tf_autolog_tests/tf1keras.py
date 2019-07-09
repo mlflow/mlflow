bced27e3fd473d578f5481759596a26d719e0a0b
Apurva Koti <apurva.koti@databricks.com>
2019-07-09 13:17:57 -0700

    Removing keras autologging temporarily

---
+++ tf_autolog_tests/tf1keras.py
@@ -0,0 +1,62 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import layers
+from tensorflow.keras.callbacks import TensorBoard
+
+import mlflow.tensorflow
+
+mlflow.tensorflow.autolog()
+
+
+model = tf.keras.Sequential()
+# Adds a densely-connected layer with 64 units to the model:
+model.add(layers.Dense(64, activation='relu'))
+# Add another:
+model.add(layers.Dense(64, activation='relu'))
+# Add a softmax layer with 10 output units:
+model.add(layers.Dense(10, activation='softmax'))
+
+# Create a sigmoid layer:
+layers.Dense(64, activation='sigmoid')
+# Or:
+layers.Dense(64, activation=tf.sigmoid)
+
+# A linear layer with L1 regularization of factor 0.01 applied to the kernel matrix:
+layers.Dense(64, kernel_regularizer=tf.keras.regularizers.l1(0.01))
+
+# A linear layer with L2 regularization of factor 0.01 applied to the bias vector:
+layers.Dense(64, bias_regularizer=tf.keras.regularizers.l2(0.01))
+
+# A linear layer with a kernel initialized to a random orthogonal matrix:
+layers.Dense(64, kernel_initializer='orthogonal')
+
+# A linear layer with a bias vector initialized to 2.0s:
+layers.Dense(64, bias_initializer=tf.keras.initializers.constant(2.0))
+
+model = tf.keras.Sequential([
+# Adds a densely-connected layer with 64 units to the model:
+layers.Dense(64, activation='relu', input_shape=(32,)),
+# Add another:
+layers.Dense(64, activation='relu'),
+# Add a softmax layer with 10 output units:
+layers.Dense(10, activation='softmax')])
+
+model.compile(optimizer=tf.train.AdamOptimizer(0.001),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+def random_one_hot_labels(shape):
+    n, n_class = shape
+    classes = np.random.randint(0, n_class, n)
+    labels = np.zeros((n, n_class))
+    labels[np.arange(n), classes] = 1
+    return labels
+
+
+data = np.random.random((1000, 32))
+labels = random_one_hot_labels((1000, 10))
+
+model.fit(data, labels, epochs=10, batch_size=32, callbacks=[])
