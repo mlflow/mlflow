a4c667ef024805d6eaf010cf6f9d3efedb270ee1
tomasatdatabricks <tomas.nykodym@databricks.com>
2019-07-11 15:08:15 -0700

    Updated migration - the new column is not not nullable and is part of the primary key.

--- mlflow/store/db_migrations/versions/181f10493468_allow_nulls_for_metric_values.py
+++ mlflow/store/db_migrations/versions/181f10493468_allow_nulls_for_metric_values.py
@@ -7,7 +7,7 @@ Create Date: 2019-07-10 22:40:18.787993
 """
 from alembic import op
 import sqlalchemy as sa
-
+from sqlalchemy import orm, Column, Integer, String, ForeignKey, PrimaryKeyConstraint
 
 # revision identifiers, used by Alembic.
 revision = '181f10493468'
@@ -17,7 +17,19 @@ depends_on = None
 
 
 def upgrade():
-    op.add_column('metrics', sa.Column('is_nan', sa.Boolean(), nullable=True, server_default="0"))
+    op.add_column('metrics', sa.Column('is_nan', sa.Boolean(), nullable=False, server_default='0'))
+    # bind = op.get_bind()
+    # session = orm.Session(bind=bind)
+    # metrics = session.query(SqlMetric).all()
+    # for metric in metrics:
+    #     metric.is_nan = False
+    #     session.merge(metric)
+    # session.commit()
+    with op.batch_alter_table("metrics") as batch_op:
+        batch_op.drop_constraint(constraint_name='metric_pk', type_="primary")
+        batch_op.create_primary_key(
+            constraint_name='metric_pk',
+            columns=['key', 'timestamp', 'step', 'run_uuid', 'value', 'is_nan'])
 
 
 def downgrade():
