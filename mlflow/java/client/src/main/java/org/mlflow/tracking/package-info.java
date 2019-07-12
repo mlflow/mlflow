7aee768671a407e46c0d3b4ecbcec7d786dcedd8
Andrew Chen <andrewmnchen@gmail.com>
2019-07-12 13:33:00 -0700

    Java Fluent SDK (#1508)
    * java fluent sdk example
    
    * some changes
    
    * add more stuf
    
    * add a scala example
    
    * touch ups to achieve parentRunID and databricks magic
    
    * fix notebookId issue
    
    * fix linter
    
    * fix test and such
    
    * run based 1
    
    * init new context based API
    
    * init
    
    * some updates
    
    * add more examples
    
    * update
    
    * fix indent
    
    * remove scala
    
    * fix tests
    
    * add some tests
    
    * init
    
    * fix tests
    
    * self review
    
    * add docs
    
    * fix formatting
    
    * address aaron
    
    * add extra newline
    
    * add unit tests
    
    * address aaron again

--- mlflow/java/client/src/main/java/org/mlflow/tracking/package-info.java
+++ mlflow/java/client/src/main/java/org/mlflow/tracking/package-info.java
@@ -1,2 +1,5 @@
-/** MLflow Tracking provides a Java CRUD interface to MLflow Experiments and Runs. */
+/**
+ * MLflow Tracking provides a Java CRUD interface to MLflow Experiments and Runs --
+ * to create and log to MLflow runs, use the {@link org.mlflow.tracking.MlflowContext} interface.
+ */
 package org.mlflow.tracking;
---
+++ mlflow/java/client/src/main/java/org/mlflow/tracking/utils/MlflowTagConstants.java
@@ -0,0 +1,12 @@
+package org.mlflow.tracking.utils;
+
+public class MlflowTagConstants {
+  public static final String PARENT_RUN_ID = "mlflow.parentRunId";
+  public static final String RUN_NAME = "mlflow.runName";
+  public static final String USER = "mlflow.user";
+  public static final String SOURCE_TYPE = "mlflow.source.type";
+  public static final String SOURCE_NAME = "mlflow.source.name";
+  public static final String DATABRICKS_NOTEBOOK_ID = "mlflow.databricks.notebookID";
+  public static final String DATABRICKS_NOTEBOOK_PATH = "mlflow.databricks.notebookPath";
+  public static final String DATABRICKS_WEBAPP_URL = "mlflow.databricks.webappURL";
+}
