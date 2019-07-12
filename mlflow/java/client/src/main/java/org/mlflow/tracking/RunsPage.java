47a800ceeb5aa9cf04ce8ceecd0b26b4f48dd113
Max Allen <max.allen@databricks.com>
2019-07-11 19:16:19 -0700

    Update indentation

--- mlflow/java/client/src/main/java/org/mlflow/tracking/RunsPage.java
+++ mlflow/java/client/src/main/java/org/mlflow/tracking/RunsPage.java
@@ -8,92 +8,92 @@ import org.mlflow.api.proto.Service.*;
 
 public class RunsPage implements Page<Run> {
 
-    private final String token;
-    private final List<Run> runs;
+  private final String token;
+  private final List<Run> runs;
 
-    private final MlflowClient client;
-    private final List<String> experimentIds;
-    private final String searchFilter;
-    private final ViewType runViewType;
-    private final List<String> orderBy;
-    private final int maxResults;
+  private final MlflowClient client;
+  private final List<String> experimentIds;
+  private final String searchFilter;
+  private final ViewType runViewType;
+  private final List<String> orderBy;
+  private final int maxResults;
 
-    /**
-     * Creates a fixed size page of Runs.
-     */
-    public RunsPage(List<Run> runs,
-                    String token,
-                    List<String> experimentIds,
-                    String searchFilter,
-                    ViewType runViewType,
-                    int maxResults,
-                    List<String> orderBy,
-                    MlflowClient client) {
-        this.runs = Collections.unmodifiableList(runs);
-        this.token = token;
-        this.experimentIds = experimentIds;
-        this.searchFilter = searchFilter;
-        this.runViewType = runViewType;
-        this.orderBy = orderBy;
-        this.maxResults = maxResults;
-        this.client = client;
+  /**
+   * Creates a fixed size page of Runs.
+   */
+  public RunsPage(List<Run> runs,
+                  String token,
+                  List<String> experimentIds,
+                  String searchFilter,
+                  ViewType runViewType,
+                  int maxResults,
+                  List<String> orderBy,
+                  MlflowClient client) {
+    this.runs = Collections.unmodifiableList(runs);
+    this.token = token;
+    this.experimentIds = experimentIds;
+    this.searchFilter = searchFilter;
+    this.runViewType = runViewType;
+    this.orderBy = orderBy;
+    this.maxResults = maxResults;
+    this.client = client;
+  }
+
+  /**
+   * @return The number of runs in the page.
+   */
+  public int getPageSize() {
+    return this.runs.size();
+  }
+
+  /**
+   * @return True if a token for the next page exists and isn't empty. Otherwise returns false.
+   */
+  public boolean hasNextPage() {
+    return this.token != null && this.token != "";
+  }
+
+  /**
+   * @return An optional with the token for the next page. 
+   * Empty if the token doesn't exist or is empty.
+   */
+  public Optional<String> getNextPageToken() {
+    if (this.hasNextPage()) {
+      return Optional.of(this.token);
+    } else {
+      return Optional.empty();
     }
+  }
 
-    /**
-     * @return The number of runs in the page.
-     */
-    public int getPageSize() {
-        return this.runs.size();
+  /**
+   * @return The next page of runs matching the search criteria. 
+   * If there are no more pages, an empty page will be returned with an empty token.
+   */
+  public RunsPage getNextPage() {
+    if (this.hasNextPage()) {
+      return this.client.searchRuns(this.experimentIds,
+                                    this.searchFilter,
+                                    this.runViewType,
+                                    this.maxResults,
+                                    this.orderBy,
+                                    this.token);
+    } else {
+      return new RunsPage(new ArrayList<>(),
+                          "",
+                          this.experimentIds,
+                          this.searchFilter,
+                          this.runViewType,
+                          this.maxResults,
+                          this.orderBy,
+                          this.client);
     }
+  }
 
-    /**
-     * @return True if a token for the next page exists and isn't empty. Otherwise returns false.
-     */
-    public boolean hasNextPage() {
-        return this.token != null && this.token != "";
-    }
-
-    /**
-     * @return An optional with the token for the next page. 
-     * Empty if the token doesn't exist or is empty.
-     */
-    public Optional<String> getNextPageToken() {
-        if (this.hasNextPage()) {
-            return Optional.of(this.token);
-        } else {
-            return Optional.empty();
-        }
-    }
-
-    /**
-     * @return The next page of runs matching the search criteria. 
-     * If there are no more pages, an empty page will be returned with an empty token.
-     */
-    public RunsPage getNextPage() {
-        if (this.hasNextPage()) {
-            return client.searchRuns(experimentIds,
-                                       searchFilter,
-                                       runViewType,
-                                       maxResults,
-                                       orderBy,
-                                       token);
-        } else {
-            return new RunsPage(new ArrayList<>(),
-                                "",
-                                this.experimentIds,
-                                this.searchFilter,
-                                this.runViewType,
-                                this.maxResults,
-                                this.orderBy,
-                                this.client);
-        }
-    }
-
-    /**
-     * @return An iterable over the runs in this page.
-     */
-    public List<Run> getItems() {
-        return runs;
-    }
+  /**
+   * @return An iterable over the runs in this page.
+   */
+  public List<Run> getItems() {
+    return runs;
+  }
 
 }
