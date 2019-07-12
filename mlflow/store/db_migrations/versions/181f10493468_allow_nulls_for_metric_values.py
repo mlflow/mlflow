e4121be36ae1fd5b12c1bb029719d4e6848036c0
tomasatdatabricks <tomas.nykodym@databricks.com>
2019-07-12 10:31:31 -0700

    Fixed constraint.

--- mlflow/store/db_migrations/versions/181f10493468_allow_nulls_for_metric_values.py
+++ mlflow/store/db_migrations/versions/181f10493468_allow_nulls_for_metric_values.py
@@ -17,7 +17,8 @@ depends_on = None
 
 def upgrade():
     with op.batch_alter_table("metrics") as batch_op:
-        batch_op.add_column(sa.Column('is_nan', sa.Boolean(), nullable=False, server_default='0'))
+        batch_op.add_column(sa.Column('is_nan', sa.Boolean(create_constraint=False), nullable=False,
+                                      server_default='0'))
         batch_op.drop_constraint(constraint_name='metric_pk', type_="primary")
         batch_op.create_primary_key(
             constraint_name='metric_pk',
