077d1d5a53cc6e2823eae0355252464929956104
apurva-koti <51172624+apurva-koti@users.noreply.github.com>
2019-07-17 15:21:04 -0700

    Autolog from Keras to MLflow (#1601)
    Use mlflow.keras.autolog() to automatically log accuracy, loss, optimizer data and post-training model checkpointing from your Keras (note: not tensorflow.keras) training session.

--- mlflow/keras.py
+++ mlflow/keras.py
@@ -13,6 +13,9 @@ from __future__ import absolute_import
 import importlib
 import os
 import yaml
+import gorilla
+import warnings
+
 import pandas as pd
 
 from mlflow import pyfunc
@@ -22,6 +25,8 @@ from mlflow.exceptions import MlflowException
 from mlflow.tracking.artifact_utils import _download_artifact_from_uri
 from mlflow.utils.environment import _mlflow_conda_env
 from mlflow.utils.model_utils import _get_flavor_configuration
+from mlflow.utils.annotations import experimental
+
 
 FLAVOR_NAME = "keras"
 # File name to which custom objects cloudpickle is saved - used during save and load
@@ -311,3 +316,67 @@ def load_model(model_uri, **kwargs):
         local_model_path,
         flavor_conf.get("data", _MODEL_SAVE_PATH))
     return _load_model(model_path=keras_model_artifacts_path, keras_module=keras_module, **kwargs)
+
+
+@experimental
+def autolog():
+    """
+    Enable automatic logging from TensorFlow to MLflow.
+    Logs loss and any other metrics specified in the fit
+    function, and optimizer data as parameters. Model checkpoints
+    are logged as artifacts to a 'models' directory.
+    """
+    import keras
+
+    class __MLflowKerasCallback(keras.callbacks.Callback):
+        """
+        Callback for auto-logging metrics and parameters.
+        Records available logs after each epoch.
+        Records model structural information as params after training finishes.
+        """
+
+        def on_epoch_end(self, epoch, logs=None):
+            if not logs:
+                return
+            try:
+                mlflow.log_metrics(logs, step=epoch)
+            except mlflow.exceptions.MlflowException as e:
+                warnings.warn("Logging to MLflow failed: " + str(e))
+
+        def on_train_end(self, logs=None):
+            try:
+                mlflow.log_param('num_layers', len(self.model.layers))
+                mlflow.log_param('optimizer_name', type(self.model.optimizer).__name__)
+                if hasattr(self.model.optimizer, 'lr'):
+                    lr = self.model.optimizer if \
+                        type(self.model.optimizer.lr) is float \
+                        else keras.backend.eval(self.model.optimizer.lr)
+                    mlflow.log_param('learning_rate', lr)
+                if hasattr(self.model.optimizer, 'epsilon'):
+                    epsilon = self.model.optimizer if \
+                        type(self.model.optimizer.epsilon) is float \
+                        else keras.backend.eval(self.model.optimizer.epsilon)
+                    mlflow.log_param('epsilon', epsilon)
+                sum_list = []
+                self.model.summary(print_fn=sum_list.append)
+                summary = '\n'.join(sum_list)
+                mlflow.set_tag('summary', summary)
+                log_model(self.model, artifact_path='model')
+            except mlflow.exceptions.MlflowException as e:
+                warnings.warn("Logging to Mlflow failed: " + str(e))
+
+    @gorilla.patch(keras.Model)
+    def fit(self, *args, **kwargs):
+        original = gorilla.get_original_attribute(keras.Model, 'fit')
+        if len(args) >= 6:
+            l = list(args)
+            l[5] += [__MLflowKerasCallback()]
+            args = tuple(l)
+        elif 'callbacks' in kwargs:
+            kwargs['callbacks'] += [__MLflowKerasCallback()]
+        else:
+            kwargs['callbacks'] = [__MLflowKerasCallback()]
+        return original(self, *args, **kwargs)
+    settings = gorilla.Settings(allow_hit=True, store_hit=True)
+    patch = gorilla.Patch(keras.Model, 'fit', fit, settings=settings)
+    gorilla.apply(patch)
--- travis/run-large-python-tests.sh
+++ travis/run-large-python-tests.sh
@@ -19,7 +19,7 @@ trap 'err=1' ERR
 pytest tests --large --ignore=tests/h2o --ignore=tests/keras \
   --ignore=tests/pytorch --ignore=tests/pyfunc --ignore=tests/sagemaker --ignore=tests/sklearn \
   --ignore=tests/spark --ignore=tests/tensorflow --ignore=tests/azureml --ignore=tests/onnx \
-  --ignore=tests/autologging
+  --ignore=tests/keras_autolog --ignore=tests/tensorflow_autolog
 # Run ML framework tests in their own Python processes to avoid OOM issues due to per-framework
 # overhead
 pytest --verbose tests/h2o --large
@@ -35,5 +35,6 @@ pytest --verbose tests/spark --large
 pytest --verbose tests/tensorflow --large
 pytest --verbose tests/azureml --large
 pytest --verbose tests/models --large
-pytest --verbose tests/autologging --large
+pytest --verbose tests/tensorflow_autolog --large
+pytest --verbose tests/keras_autolog --large
 test $err = 0
