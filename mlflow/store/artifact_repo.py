28764c6e6544cd4eb66f89d51a0b85cfe60aab2f
sueann <sueann@databricks.com>
2019-07-16 17:25:05 -0700

    prevent an infinite loop in download_artifacts

--- mlflow/store/artifact_repo.py
+++ mlflow/store/artifact_repo.py
@@ -85,6 +85,9 @@ class ArtifactRepository:
                 if not os.path.exists(local_path):
                     os.mkdir(local_path)
                 for file_info in listing:
+                    # prevent an infinite loop (sometimes the current path is listed e.g. as “.”)
+                    if file_info.path == "." or file_info.path == artifact_path:
+                        continue
                     download_artifacts_into(artifact_path=file_info.path, dest_dir=local_path)
             else:
                 self._download_file(remote_file_path=artifact_path, local_path=local_path)
