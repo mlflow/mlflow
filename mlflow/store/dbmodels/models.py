5e33b04e72cbbdcaaa2b66577dfd81be1e83b15e
Theophile Henri <t.henri@criteo.com>
2019-08-01 10:00:39 +0200

    Lint fix

--- mlflow/store/dbmodels/models.py
+++ mlflow/store/dbmodels/models.py
@@ -217,7 +217,8 @@ class SqlRun(Base):
                        SqlMetric.run_uuid == metrics_with_max_timestamp.c.run_uuid,
                        SqlMetric.key == metrics_with_max_timestamp.c.key,
                        SqlMetric.step == metrics_with_max_timestamp.c.step)) \
-            .group_by(SqlMetric.run_uuid, SqlMetric.key, SqlMetric.step, SqlMetric.timestamp, SqlMetric.is_nan) \
+            .group_by(SqlMetric.run_uuid, SqlMetric.key,
+                      SqlMetric.step, SqlMetric.timestamp, SqlMetric.is_nan) \
             .all()
         return metrics_with_max_value
 
