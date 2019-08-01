4fe6bc731ed5d37cd20e2d7c13eb37241b138c29
Ankit Mathur <52183359+ankitmathur-db@users.noreply.github.com>
2019-07-31 20:48:00 -0700

    add notes to experiments in UI (#1678)
    * add store implementation of experiment tags with v1 of store API
    
    * add initial migration
    
    * update migration, give up on autodiff
    
    * update experiment sqlalchemy model to return tags, update test with typo:
    
    * update test db schema, update rest_store test with typo
    
    * new proto schema
    
    * revert proto change
    
    * fix tests
    
    * add client API endpoints for setExperimentTag
    
    * lints
    
    * add client API endpoints for setExperimentTag
    
    * lints
    
    * mandate set_experiment_tag via AbstractStore
    
    * add client API endpoints for setExperimentTag
    
    * lints
    
    * update protos in storage commit, update requestor test
    
    * add client API endpoints for setExperimentTag
    
    * lints
    
    * lints
    
    * add client API endpoints for setExperimentTag
    
    * lints
    
    * fix schema length bugs, add tag length validation, remove unused tags key from experiment meta
    
    * lints on response commit
    
    * add client API endpoints for setExperimentTag
    
    * lints
    
    * responses to nits
    
    * Javadoc regeneration on protos
    
    * tag size clarifications, responses to nits
    
    * proto docs update
    
    * lints
    
    * add client API endpoints for setExperimentTag
    
    * lints
    
    * responses to nits
    
    * add experimenttag js endpoint
    
    * make notes field show up
    
    * make notes save correctly
    
    * finalize styling, include markdown rendering
    
    * revert weird merging stuff
    
    * clean up
    
    * add show/hide interface for description component
    
    * fix lints, attempt to fix tests
    
    * fix tests
    
    * fix CSS
    
    * respond to nits, issues pointed out by @Zangr
    
    * responses to comments from @Zangr
    
    * correct structure of state
    
    * lints

--- mlflow/server/js/src/components/NoteShowView.js
+++ mlflow/server/js/src/components/NoteShowView.js
@@ -7,26 +7,43 @@ class NoteShowView extends Component {
   constructor(props) {
     super(props);
     this.converter = getConverter();
+    this.noteType = props.noteType;
   }
 
   static propTypes = {
     content: PropTypes.string.isRequired,
+    noteType: PropTypes.string.isRequired,
   };
 
   render() {
     const htmlContent = sanitizeConvertedHtml(this.converter.makeHtml(this.props.content));
-    return (
-      <div className="note-view-outer-container">
-        <div className="note-view-text-area">
-            <div className="note-view-preview note-editor-preview">
-              <div className="note-editor-preview-content"
-                   // eslint-disable-next-line react/no-danger
-                   dangerouslySetInnerHTML={{ __html: htmlContent }}>
-              </div>
+    if (this.noteType === "run") {
+      return (
+            <div className="note-view-outer-container">
+                <div className="note-view-text-area">
+                    <div className="note-view-preview note-editor-preview">
+                        <div className="note-editor-preview-content"
+                            // eslint-disable-next-line react/no-danger
+                             dangerouslySetInnerHTML={{ __html: htmlContent }}>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-      </div>
-    );
+      );
+    } else if (this.noteType === "experiment") {
+      return (
+            <div className="note-view-outer-container">
+                <div className="note-view-text-area">
+                    <div className="note-editor-preview-content"
+                        // eslint-disable-next-line react/no-danger
+                         dangerouslySetInnerHTML={{ __html: htmlContent }}>
+                    </div>
+                </div>
+            </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
 
