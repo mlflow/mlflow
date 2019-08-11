4b054630bf5451cae41fa03c707b5c26672aaea0
Drew McDonald <mcdonaldd@dnc.org>
2019-08-11 17:14:50 -0400

    restructure and extend MLproject validation testing

--- tests/projects/test_project_spec.py
+++ tests/projects/test_project_spec.py
@@ -76,22 +76,109 @@ def test_load_docker_project(tmpdir):
     assert project.docker_env.get("image") == "some-image"
 
 
-@pytest.mark.parametrize("invalid_project_contents, expected_error_msg", [
-    (textwrap.dedent("""
-    docker_env:
-        image: some-image
-    conda_env: some-file.yaml
-    """), "Invalid MLproject file:"),
-    (textwrap.dedent("""
-    docker_env:
-        not-image-attribute: blah
-    """), "Invalid MLproject file:"),
-    (textwrap.dedent("""
-    docker_env: blah
-    """), "Invalid MLproject file:"),
+bad_ml_project_file_test_cases = (
+    # bad sub-entries at various points
+    (
+        "not one of allowed entries",
+        (
+            """
+            name: test
+            blah: blah
+            """,
+            """
+            docker_env:
+              image: some-image
+              some_entry: shrug
+            """,
+            """
+            entry_points:
+              ep:
+                parameters:
+                  p:
+                    type: test
+                    blah: test
+                command: pass
+            """,
+            """
+            entry_points:
+              ep:
+                command: pass
+                blah: blah
+            """,
+            """
+            docker_env:
+              image: hi
+              blah: blah
+            """
+        )
+    ),
+    # conda and docker in the same project
+    (
+        "conda_env and docker_env in the same project",
+        (
+            """
+            docker_env:
+              image: some-image
+            conda_env: some-file.yaml
+            """,
+        )
+    ),
+    # entry point parameter with entries must list type
+    (
+        "parameter p in entry point ep must specify a type",
+        (
+            """
+            entry_points:
+              ep:
+                parameters:
+                  p: {default: 1}
+                command: pass
+            """,
+        )
+    ),
+    # docker env has only a string image entry
+    (
+        "must have an 'image' entry",
+        (
+            """
+            docker_env: blah
+            """,
+            """
+            docker_env:
+              not-image-attribute: blah
+            """
+        )
+    ),
+    # conda is a path
+    (
+        "conda_env should be a string representing the relative path",
+        (
+            """
+            conda_env:
+              some_item: blah
+            """,
+        )
+    ),
+    # name is simple string
+    (
+        "name must be a single string",
+        (
+            """
+            name:
+              some_item: blah
+            """,
+        )
+    )
+)
+
+
+@pytest.mark.parametrize("invalid_project_content_strings, expected_error_msg", [
+    ((textwrap.dedent(p) for p in bad_ml_projects), error_fragment)
+    for error_fragment, bad_ml_projects in bad_ml_project_file_test_cases
 ])
-def test_load_invalid_project(tmpdir, invalid_project_contents, expected_error_msg):
-    tmpdir.join("MLproject").write(invalid_project_contents)
-    with pytest.raises(ExecutionException) as e:
-        _project_spec.load_project(tmpdir.strpath)
-    assert expected_error_msg in str(e.value)
+def test_load_invalid_project(tmpdir, invalid_project_content_strings, expected_error_msg):
+    for content in invalid_project_content_strings:
+        tmpdir.join("MLproject").write(content)
+        with pytest.raises(ExecutionException) as e:
+            _project_spec.load_project(tmpdir.strpath)
+        assert expected_error_msg in str(e.value)
