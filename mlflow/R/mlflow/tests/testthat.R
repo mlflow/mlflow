cdc38aed285811a314823618c444b151e71a5bba
dbczumar <39497902+dbczumar@users.noreply.github.com>
2019-08-11 21:05:03 -0700

    Fix Java & R travis build failures (#1722)
    Adds an optional python_version argument to the R mlflow_install API for specifying the Python version (e.g. "3.5") to use within the conda environment created for installing the MLflow CLI. If python_version is unspecified, mlflow_install defaults to using Python 3.6.

--- mlflow/R/mlflow/tests/testthat.R
+++ mlflow/R/mlflow/tests/testthat.R
@@ -3,7 +3,7 @@ library(mlflow)
 library(reticulate)
 
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
-  mlflow:::mlflow_maybe_create_conda_env()
+  mlflow:::mlflow_maybe_create_conda_env(python_version = "3.6")
   message("Current working directory: ", getwd())
   mlflow_home <- Sys.getenv("MLFLOW_HOME", "../../../../.")
   conda_install(c(mlflow_home), envname = mlflow:::mlflow_conda_env_name(), pip = TRUE)
