e9b34867aec33ad68b4485744b247c95e66b650f
Apurva Koti <apurva.koti@databricks.com>
2019-08-15 13:49:41 -0700

    storing google csv files locally

--- tests/tensorflow_autolog/test_tensorflow_autolog.py
+++ tests/tensorflow_autolog/test_tensorflow_autolog.py
@@ -130,15 +130,8 @@ def tf_estimator_random_data_run():
         CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
         SPECIES = ['Setosa', 'Versicolor', 'Virginica']
 
-        train_path = tf.keras.utils.get_file(
-            "iris_training.csv", "https://storage.googleapis.com/download"
-                                 ".tensorflow.org/data/iris_training.csv")
-        test_path = tf.keras.utils.get_file(
-            "iris_test.csv", "https://storage.googleapis.com/download"
-                             ".tensorflow.org/data/iris_test.csv")
-
-        train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
-        test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
+        train = pd.read_csv("iris_training.csv", names=CSV_COLUMN_NAMES, header=0)
+        test = pd.read_csv("iris_test.csv", names=CSV_COLUMN_NAMES, header=0)
 
         train_y = train.pop('Species')
         test_y = test.pop('Species')
