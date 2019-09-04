833a787ff8bec15a09baab9c11f55630bd2d8645
Nicolas Laille <nlaille@xebia.fr>
2019-09-04 16:51:00 +0200

    remove mlflow/projects/docker package

--- tests/projects/test_docker_projects.py
+++ tests/projects/test_docker_projects.py
@@ -93,18 +93,18 @@ def test_docker_uri_mode_validation(
         mlflow.projects.run(TEST_DOCKER_PROJECT_DIR, backend="databricks")
 
 
-@mock.patch('mlflow.projects.docker._get_git_commit')
+@mock.patch('mlflow.projects._get_git_commit')
 def test_docker_image_uri_with_git(get_git_commit_mock):
     get_git_commit_mock.return_value = '1234567890'
-    image_uri = mlflow.projects.docker._get_docker_image_uri("my_project", "my_workdir")
+    image_uri = mlflow.projects._get_docker_image_uri("my_project", "my_workdir")
     assert image_uri == "my_project:1234567"
     get_git_commit_mock.assert_called_with('my_workdir')
 
 
-@mock.patch('mlflow.projects.docker._get_git_commit')
+@mock.patch('mlflow.projects._get_git_commit')
 def test_docker_image_uri_no_git(get_git_commit_mock):
     get_git_commit_mock.return_value = None
-    image_uri = mlflow.projects.docker._get_docker_image_uri("my_project", "my_workdir")
+    image_uri = mlflow.projects._get_docker_image_uri("my_project", "my_workdir")
     assert image_uri == "my_project"
     get_git_commit_mock.assert_called_with('my_workdir')
 
@@ -112,7 +112,7 @@ def test_docker_image_uri_no_git(get_git_commit_mock):
 def test_docker_valid_project_backend_local():
     work_dir = "./examples/docker"
     project = _project_spec.load_project(work_dir)
-    mlflow.projects.docker._validate_docker_env(project)
+    mlflow.projects._validate_docker_env(project)
 
 
 def test_docker_invalid_project_backend_local():
@@ -120,7 +120,7 @@ def test_docker_invalid_project_backend_local():
     project = _project_spec.load_project(work_dir)
     project.name = None
     with pytest.raises(ExecutionException):
-        mlflow.projects.docker._validate_docker_env(project)
+        mlflow.projects._validate_docker_env(project)
 
 
 @pytest.mark.parametrize("artifact_uri, host_artifact_uri, container_artifact_uri, should_mount", [
@@ -140,7 +140,7 @@ def test_docker_mount_local_artifact_uri(artifact_uri, host_artifact_uri,
     image = mock.MagicMock()
     image.tags = ["image:tag"]
 
-    docker_command = mlflow.projects.docker._get_docker_command(image, active_run)
+    docker_command = mlflow.projects._get_docker_command(image, active_run)
 
     docker_volume_expected = "-v {}:{}".format(host_artifact_uri, container_artifact_uri)
     assert (docker_volume_expected in " ".join(docker_command)) == should_mount
@@ -155,7 +155,7 @@ def test_docker_s3_artifact_cmd_and_envs_from_env():
     with mock.patch.dict("os.environ", mock_env), \
             mock.patch("posixpath.exists", return_value=False):
         cmds, envs = \
-            mlflow.projects.docker._get_docker_artifact_storage_cmd_and_envs("s3://mock_bucket")
+            mlflow.projects._get_docker_artifact_storage_cmd_and_envs("s3://mock_bucket")
         assert cmds == []
         assert envs == mock_env
 
@@ -166,7 +166,7 @@ def test_docker_s3_artifact_cmd_and_envs_from_home():
             mock.patch("posixpath.exists", return_value=True), \
             mock.patch("posixpath.expanduser", return_value="mock_volume"):
         cmds, envs = \
-            mlflow.projects.docker._get_docker_artifact_storage_cmd_and_envs("s3://mock_bucket")
+            mlflow.projects._get_docker_artifact_storage_cmd_and_envs("s3://mock_bucket")
         assert cmds == ["-v", "mock_volume:/.aws"]
         assert envs == mock_env
 
@@ -182,7 +182,7 @@ def test_docker_wasbs_artifact_cmd_and_envs_from_home():
     wasbs_uri = "wasbs://container@account.blob.core.windows.net/some/path"
     with mock.patch.dict("os.environ", mock_env), \
             mock.patch("azure.storage.blob.BlockBlobService"):
-        cmds, envs = mlflow.projects.docker._get_docker_artifact_storage_cmd_and_envs(wasbs_uri)
+        cmds, envs = mlflow.projects._get_docker_artifact_storage_cmd_and_envs(wasbs_uri)
         assert cmds == []
         assert envs == mock_env
 
@@ -193,7 +193,7 @@ def test_docker_gcs_artifact_cmd_and_envs_from_home():
     }
     gs_uri = "gs://mock_bucket"
     with mock.patch.dict("os.environ", mock_env):
-        cmds, envs = mlflow.projects.docker._get_docker_artifact_storage_cmd_and_envs(gs_uri)
+        cmds, envs = mlflow.projects._get_docker_artifact_storage_cmd_and_envs(gs_uri)
         assert cmds == ["-v", "mock_credentials_path:/.gcs"]
         assert envs == {"GOOGLE_APPLICATION_CREDENTIALS": "/.gcs"}
 
@@ -207,7 +207,7 @@ def test_docker_hdfs_artifact_cmd_and_envs_from_home():
     }
     hdfs_uri = "hdfs://host:8020/path"
     with mock.patch.dict("os.environ", mock_env):
-        cmds, envs = mlflow.projects.docker._get_docker_artifact_storage_cmd_and_envs(hdfs_uri)
+        cmds, envs = mlflow.projects._get_docker_artifact_storage_cmd_and_envs(hdfs_uri)
         assert cmds == ["-v", "/mock_ticket_cache:/mock_ticket_cache"]
         assert envs == mock_env
 
@@ -215,7 +215,7 @@ def test_docker_hdfs_artifact_cmd_and_envs_from_home():
 def test_docker_local_artifact_cmd_and_envs():
     host_path_expected = os.path.abspath("./mlruns")
     container_path_expected = "/mlflow/projects/code/mlruns"
-    cmds, envs = mlflow.projects.docker._get_docker_artifact_storage_cmd_and_envs("file:./mlruns")
+    cmds, envs = mlflow.projects._get_docker_artifact_storage_cmd_and_envs("file:./mlruns")
     assert cmds == ["-v", "{}:{}".format(host_path_expected, container_path_expected)]
     assert envs == {}
 
@@ -228,7 +228,7 @@ def test_docker_databricks_tracking_cmd_and_envs(ProfileConfigProvider):
     ProfileConfigProvider.return_value = mock_provider
 
     cmds, envs = \
-        mlflow.projects.docker._get_docker_tracking_cmd_and_envs("databricks://some-profile")
+        mlflow.projects._get_docker_tracking_cmd_and_envs("databricks://some-profile")
     assert envs == {"DATABRICKS_HOST": "host",
                     "DATABRICKS_USERNAME": "user",
                     "DATABRICKS_PASSWORD": "pass",
