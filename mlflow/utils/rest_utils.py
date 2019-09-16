f5ecc5265fbca21b26827de94e70cc1e770f17ce
tomasatdatabricks <tomas.nykodym@databricks.com>
2019-09-16 13:36:32 -0700

    nit

--- mlflow/utils/rest_utils.py
+++ mlflow/utils/rest_utils.py
@@ -23,8 +23,8 @@ def http_request(host_creds, endpoint, retries=3, retry_interval=3,
     """
     Makes an HTTP request with the specified method to the specified hostname/endpoint. Ratelimit
     error code (429) will be retried with an exponential back off (1, 2, 4, ... seconds) for at most
-    `max_rate_limit_interval` seconds.  Internal errors (500s) will be retried up to `retries` times,
-    waiting `retry_interval` seconds between successive retries. Parses the API response
+    `max_rate_limit_interval` seconds.  Internal errors (500s) will be retried up to `retries` times
+    , waiting `retry_interval` seconds between successive retries. Parses the API response
     (assumed to be JSON) into a Python object and returns it.
 
     :param host_creds: A :py:class:`mlflow.rest_utils.MlflowHostCreds` object containing
