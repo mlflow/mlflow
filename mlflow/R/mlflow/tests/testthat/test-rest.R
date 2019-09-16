4900c00d5e2261fe6788222b33eba202c2cc6d97
tomasatdatabricks <tomas.nykodym@databricks.com>
2019-09-16 12:20:27 -0700

    Added retries for 429s in the R  REST client and a test.

--- mlflow/R/mlflow/tests/testthat/test-rest.R
+++ mlflow/R/mlflow/tests/testthat/test-rest.R
@@ -1,5 +1,8 @@
 context("rest")
 
+library(withr)
+
+
 test_that("user-agent header is set", {
   config <- list()
   config$insecure <- FALSE
@@ -48,4 +51,34 @@ test_that("insecure is used", {
   rest_config <- mlflow:::get_rest_config(config)
 
   expect_equal(rest_config$config, httr::config(ssl_verifypeer = 0, ssl_verifyhost = 0))
+})
+
+
+test_that("429s are retried", {
+  responses = list(list(status_code = 429), list(status_code = 429), list(status_code = 200))
+  next_id <- 1
+  client <- client1 <- mlflow:::mlflow_client("local")
+  with_mock(GET = function(...) {
+    res <- responses[[next_id]]
+    next_id <- next_id
+    res
+  }, {
+    tryCatch({
+      mlflow_rest(client, max_rate_limit_interval=0)
+      stop("The rest call should have returned 429 and the function should have thrown.")
+    })
+    x <- mlflow_rest(client, max_rate_limit_interval=2)
+    expect_equal(200, x$status_code)
+    next_id <- 1
+    x <- mlflow_rest(client, max_rate_limit_interval=2)
+    expect_equal(200, x$status_code)
+    next_id <- 1
+    x <- mlflow_rest(client, max_rate_limit_interval=3)
+    expect_equal(200, x$status_code)
+    next_id <- 1
+    tryCatch({
+      mlflow_rest(client, max_rate_limit_interval=1)
+      stop("The rest call should have returned 429 and the function should have thrown.")
+    })
+  })
 })
