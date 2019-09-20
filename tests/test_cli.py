faae14eef9019d36a821c03f60466f87979f0d44
Jean-Denis Lesage <jd.lesage@criteo.com>
2019-09-20 18:44:44 +0200

    Add CLI command to export runs of an experiment into a csv (#1705)
    * Add CLI command to export runs of an experiment into a csv
    
    * Use python2 compatible syntax for typing
    
    * Add tests, change parameter name and print message
    
    * Fix linting + python2.7 syntax
    
    * Replace namedtemporarydir that does not exist in python2.7
    
    * Force columns order for python2
    
    * filename option shortcut is -o. f
    
    * Filename is optional.
    * Print in stdout if filename is missing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,16 @@
 from click.testing import CliRunner
 from mock import mock
+import numpy as np
+import os
+import pandas as pd
 import pytest
+import shutil
+import tempfile
+import textwrap
 
 from mlflow.cli import run, server, ui
 from mlflow.server import handlers
+from mlflow import experiments
 
 
 def test_server_static_prefix_validation():
@@ -76,3 +83,29 @@ def test_mlflow_run():
                                           "--experiment-name", "name blah", "uri"])
         mock_projects.run.assert_not_called()
         assert "Specify only one of 'experiment-name' or 'experiment-id' options." in result.output
+
+
+def test_csv_generation():
+    with mock.patch('mlflow.experiments.fluent.search_runs') as mock_search_runs:
+        mock_search_runs.return_value = pd.DataFrame({
+            "run_id": np.array(["all_set", "with_none", "with_nan"]),
+            "experiment_id": np.array([1, 1, 1]),
+            "param_optimizer": np.array(["Adam", None, "Adam"]),
+            "avg_loss": np.array([42.0, None, np.nan], dtype=np.float32)},
+            columns=["run_id", "experiment_id", "param_optimizer", "avg_loss"])
+        expected_csv = textwrap.dedent("""\
+        run_id,experiment_id,param_optimizer,avg_loss
+        all_set,1,Adam,42.0
+        with_none,1,,
+        with_nan,1,Adam,
+        """)
+        tempdir = tempfile.mkdtemp()
+        try:
+            result_filename = os.path.join(tempdir, "result.csv")
+            CliRunner().invoke(experiments.generate_csv_with_runs,
+                               ["--experiment-id", "1",
+                                "--filename", result_filename])
+            with open(result_filename, 'r') as fd:
+                assert expected_csv == fd.read()
+        finally:
+            shutil.rmtree(tempdir)
