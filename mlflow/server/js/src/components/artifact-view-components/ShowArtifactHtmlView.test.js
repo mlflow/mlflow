62d1c99e3477d82dc7882fd42380310080dec215
Sid Murching <sid.murching@databricks.com>
2019-09-19 19:51:13 -0700

    Write test, even though it's mostly fruitless

--- mlflow/server/js/src/components/artifact-view-components/ShowArtifactHtmlView.test.js
+++ mlflow/server/js/src/components/artifact-view-components/ShowArtifactHtmlView.test.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { shallow } from 'enzyme';
 import ShowArtifactHtmlView from './ShowArtifactHtmlView';
 
 
 test('ShowArtifactHtmlView - simple website', (done) => {
   const simpleHtml = '<html><body><div id="html-test">Test Page</div></body></html>';
+  // Mock out fetch handler to silence react error from trying to fetch nonexistent file
+  // The FileReader interface seems to reject the Blob we define below while reading it,
+  // yielding an event of the form {isTrusted: false} instead of actual file contents,
+  // so we manually set the component's HTML below. TODO(Sid): find a way to fix this
+  // later on.
   const fetchMock = jest.fn();
   fetchMock.mockReturnValue(new Promise((resolve) => {
     resolve({
@@ -13,6 +18,8 @@ test('ShowArtifactHtmlView - simple website', (done) => {
   }));
   global.fetch = fetchMock;
   const wrapper = shallow(<ShowArtifactHtmlView path="fakepath" runUuid="fakerunuuid"/>);
+  // Mock out our blob-to-url helper to return HTML directly as our URL, since unfortunately
+  // we can't mock out URL.createObjectURL as per https://github.com/jsdom/jsdom/issues/1721
   wrapper.instance().getBlobURL = () => {
     return simpleHtml;
   };
@@ -20,9 +27,7 @@ test('ShowArtifactHtmlView - simple website', (done) => {
     wrapper.render();
     const iframe = wrapper.find('.html-iframe');
     const props = iframe.props();
-    const nestedVal = iframe.find('.sid-test');
-    // console.log(nestedVal.props());
-    // console.log(props);
-    return done();
+    expect(props.src).toEqual(simpleHtml);
+    done();
   });
 });
