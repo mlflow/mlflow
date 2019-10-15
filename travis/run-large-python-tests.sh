64179091e4c164a85338170f288aafc7952e5732
Juntai Zheng <juntai.zheng@databricks.com>
2019-10-14 17:25:36 -0700

    changed tests to run against keras==2.3.1, fixed keras_autolog test for 2.3.1

--- travis/run-large-python-tests.sh
+++ travis/run-large-python-tests.sh
@@ -35,11 +35,9 @@ pytest --verbose tests/tensorflow/test_tensorflow_model_export.py --large
 pytest --verbose tests/tensorflow_autolog/test_tensorflow_autolog.py --large
 pytest --verbose tests/azureml --large
 pytest --verbose tests/models --large
-pytest --verbose tests/keras --large
-pytest --verbose tests/keras_autolog --large
 pip install 'tensorflow>=2.0.0'
 pytest --verbose tests/tensorflow/test_tensorflow2_model_export.py --large
 pytest --verbose tests/tensorflow_autolog/test_tensorflow2_autolog.py --large
-pip install 'keras>=2.3.1'
 pytest --verbose tests/keras --large
+pytest --verbose tests/keras_autolog --large
 test $err = 0
