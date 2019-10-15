de9d996060b44bb093383b33fd832a2598ae41fa
juntai-zheng <39497939+juntai-zheng@users.noreply.github.com>
2019-10-15 12:34:12 -0700

    Keras support update for TensorFlow 2.0 compatibility (#1927)
    MLflow now supports saving & loading tf.keras models with TensorFlow 2.0.

--- travis/run-large-python-tests.sh
+++ travis/run-large-python-tests.sh
@@ -24,8 +24,6 @@ pytest tests --large --ignore=tests/h2o --ignore=tests/keras \
 # Run ML framework tests in their own Python processes to avoid OOM issues due to per-framework
 # overhead
 pytest --verbose tests/h2o --large
-# TODO(smurching): Re-enable Keras tests once they're no longer flaky
-# - pytest --verbose tests/keras --large
 pytest --verbose tests/onnx --large;
 pytest --verbose tests/pytorch --large
 pytest --verbose tests/pyfunc --large
@@ -37,8 +35,9 @@ pytest --verbose tests/tensorflow/test_tensorflow_model_export.py --large
 pytest --verbose tests/tensorflow_autolog/test_tensorflow_autolog.py --large
 pytest --verbose tests/azureml --large
 pytest --verbose tests/models --large
-pytest --verbose tests/keras_autolog --large
 pip install 'tensorflow>=2.0.0'
 pytest --verbose tests/tensorflow/test_tensorflow2_model_export.py --large
 pytest --verbose tests/tensorflow_autolog/test_tensorflow2_autolog.py --large
+pytest --verbose tests/keras --large
+pytest --verbose tests/keras_autolog --large
 test $err = 0
