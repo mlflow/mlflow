e0a313ac3df8077e787309940b15ab18fe4b0169
Corey Zumar <corey.zumar@databricks.com>
2019-10-27 15:55:27 -0700

    Fix tests

--- tests/tracking/_model_registry/test_model_registry_fluent.py
+++ tests/tracking/_model_registry/test_model_registry_fluent.py
@@ -1,13 +1,14 @@
 import mock
 import pytest
 
-from mlflow import register_model, set_tracking_uri
+from mlflow import register_model, set_tracking_uri, get_tracking_uri 
 from mlflow.entities.model_registry import ModelVersion, RegisteredModel
 from mlflow.exceptions import MlflowException
 from mlflow.protos.databricks_pb2 import (
         ErrorCode, INTERNAL_ERROR, RESOURCE_ALREADY_EXISTS, FEATURE_DISABLED
 )
 from mlflow.tracking import MlflowClient
+from mlflow.tracking._tracking_service.utils import is_tracking_uri_set 
 from mlflow.utils.file_utils import TempDir
 
 
@@ -18,10 +19,14 @@ def test_register_model_raises_exception_with_unsupported_registry_store():
     features (e.g., FileStore).
     """
     with TempDir() as tmp:
-        set_tracking_uri(tmp.path())
-        with pytest.raises(MlflowException) as exc:
-            register_model(model_uri="runs:/1234/some_model", name="testmodel")
-            assert exc.value.error_code == ErrorCode.Name(FEATURE_DISABLED)
+        old_tracking_uri = get_tracking_uri() if is_tracking_uri_set() else None
+        try:
+            set_tracking_uri(tmp.path())
+            with pytest.raises(MlflowException) as exc:
+                register_model(model_uri="runs:/1234/some_model", name="testmodel")
+                assert exc.value.error_code == ErrorCode.Name(FEATURE_DISABLED)
+        finally:
+            set_tracking_uri(old_tracking_uri)
 
 
 def test_register_model_with_runs_uri():
