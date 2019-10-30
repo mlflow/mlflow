7018d2473aacb95290028bf0f7546c706c0acb5a
Mani Parkhe <mani@databricks.com>
2019-10-29 19:40:30 -0700

    backout wrong change

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,3 @@
-#from mlflow.utils.logging_utils import _configure_mlflow_loggers
+from mlflow.utils.logging_utils import _configure_mlflow_loggers
 
-#_configure_mlflow_loggers(root_module_name=__name__)
+_configure_mlflow_loggers(root_module_name=__name__)
