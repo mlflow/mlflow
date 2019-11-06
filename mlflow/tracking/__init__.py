da7debaa7ce10c416cfbac25e888d73b3cf8248a
Fabian Höring <f.horing@criteo.com>
2019-11-06 20:45:41 +0100

    Expose mlflow.tracking.is_tracking_uri_set() (#2026)

--- mlflow/tracking/__init__.py
+++ mlflow/tracking/__init__.py
@@ -6,8 +6,13 @@ For a higher level API for managing an "active run", use the :py:mod:`mlflow` mo
 """
 
 from mlflow.tracking.client import MlflowClient
-from mlflow.tracking._tracking_service.utils import set_tracking_uri, get_tracking_uri, \
-    _get_store, _TRACKING_URI_ENV_VAR
+from mlflow.tracking._tracking_service.utils import (
+    set_tracking_uri,
+    get_tracking_uri,
+    is_tracking_uri_set,
+    _get_store,
+    _TRACKING_URI_ENV_VAR
+)
 from mlflow.tracking.fluent import _EXPERIMENT_ID_ENV_VAR, _EXPERIMENT_NAME_ENV_VAR, \
     _RUN_ID_ENV_VAR
 
@@ -15,6 +20,7 @@ __all__ = [
     "MlflowClient",
     "get_tracking_uri",
     "set_tracking_uri",
+    "is_tracking_uri_set",
     "_get_store",
     "_EXPERIMENT_ID_ENV_VAR",
     "_RUN_ID_ENV_VAR",
