76e385eea431c8584be8ebd23866f4d0a14e580a
Juntai Zheng <juntai.zheng@databricks.com>
2019-11-07 13:22:18 -0800

    refactored keras_random_data_run

--- tests/keras_autolog/test_keras_autolog.py
+++ tests/keras_autolog/test_keras_autolog.py
@@ -20,20 +20,22 @@ def fit_variant():
     return "fit"
 
 
-@pytest.fixture
-def keras_random_data_run(random_train_data, fit_variant):
-    mlflow.keras.autolog()
+@pytest.fixture()
+def random_one_hot_labels():
+    n, n_class = (1000, 10)
+    classes = np.random.randint(0, n_class, n)
+    labels = np.zeros((n, n_class))
+    labels[np.arange(n), classes] = 1
+    return labels
 
-    def random_one_hot_labels(shape):
-        n, n_class = shape
-        classes = np.random.randint(0, n_class, n)
-        labels = np.zeros((n, n_class))
-        labels[np.arange(n), classes] = 1
-        return labels
+
+@pytest.fixture
+def keras_random_data_run(random_train_data, fit_variant, random_one_hot_labels):
+    mlflow.keras.autolog()
 
     with mlflow.start_run() as run:
         data = random_train_data
-        labels = random_one_hot_labels((1000, 10))
+        labels = random_one_hot_labels
 
         model = keras.Sequential()
 
@@ -82,11 +84,60 @@ def test_keras_autolog_model_can_load_from_artifact(keras_random_data_run, rando
 
 
 @pytest.mark.large
-def test_autolog_ends_auto_created_run():
-    pass
+@pytest.mark.parametrize('fit_variant', ['fit', 'fit_generator'])
+def test_autolog_ends_auto_created_run(random_train_data, random_one_hot_labels, fit_variant):
+    mlflow.keras.autolog()
+
+    data = random_train_data
+    labels = random_one_hot_labels
+
+    model = keras.Sequential()
+
+    model.add(layers.Dense(64, activation='relu', input_shape=(32,)))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(10, activation='softmax'))
+
+    model.compile(optimizer=keras.optimizers.Adam(lr=0.001, epsilon=1e-07),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    if fit_variant == 'fit_generator':
+        def generator():
+            while True:
+                yield data, labels
+        model.fit_generator(generator(), epochs=10, steps_per_epoch=1)
+    else:
+        model.fit(data, labels, epochs=10)
+
+    assert mlflow.active_run() is None
 
 
 @pytest.mark.large
-def test_autolog_persists_manually_created_run():
-    pass
+@pytest.mark.parametrize('fit_variant', ['fit', 'fit_generator'])
+def test_autolog_persists_manually_created_run(random_train_data, random_one_hot_labels, fit_variant):
+    mlflow.keras.autolog()
+
+    with mlflow.start_run() as run:
+        data = random_train_data
+        labels = random_one_hot_labels
+
+        model = keras.Sequential()
+
+        model.add(layers.Dense(64, activation='relu', input_shape=(32,)))
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(10, activation='softmax'))
+
+        model.compile(optimizer=keras.optimizers.Adam(lr=0.001, epsilon=1e-07),
+                      loss='categorical_crossentropy',
+                      metrics=['accuracy'])
+
+        if fit_variant == 'fit_generator':
+            def generator():
+                while True:
+                    yield data, labels
+            model.fit_generator(generator(), epochs=10, steps_per_epoch=1)
+        else:
+            model.fit(data, labels, epochs=10)
+
+    return client.get_run(run.info.run_id)
 
