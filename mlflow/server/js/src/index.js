aabc9a83d0a0f1cdab67f5b6cb8551cbb136a36b
Sid Murching <sid.murching@databricks.com>
2019-11-08 10:14:15 -0800

    Fix lint

--- mlflow/server/js/src/index.js
+++ mlflow/server/js/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 import { setupAjaxHeaders } from './setupAjaxHeaders';
 import { Provider } from 'react-redux';
 import store from './Store';
