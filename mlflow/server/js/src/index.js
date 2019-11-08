fd216d03cf1a40589519b278a8b9f0f00fcef44d
Sid Murching <sid.murching@databricks.com>
2019-11-08 09:20:35 -0800

    Disable service worker registration

--- mlflow/server/js/src/index.js
+++ mlflow/server/js/src/index.js
@@ -16,4 +16,9 @@ const root = (
 );
 ReactDOM.render(root, document.getElementById('root'));
 
-registerServiceWorker();
+// Disable service worker registration as it adds unnecessary debugging complexity
+// (see https://github.com/facebook/create-react-app/issues/2398) for relatively small gain
+// (caching of static assets, as described in
+// https://create-react-app.dev/docs/making-a-progressive-web-app/, which should also be handled
+// by most browsers)
+// registerServiceWorker();
