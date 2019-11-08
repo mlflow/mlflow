13bca2adde91a10092e90b1822c68527318ff4c2
Siddharth Murching <sid.murching@databricks.com>
2019-11-08 13:12:33 -0800

    [UI] Disable service worker registration (#2062)
    Disable service worker registration in MLflow UI. The use of a service worker adds unnecessary debugging complexity (see facebook/create-react-app#2398) and has actually been disabled by default in newer versions of create-react-app.

--- mlflow/server/js/src/index.js
+++ mlflow/server/js/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 import { setupAjaxHeaders } from './setupAjaxHeaders';
 import { Provider } from 'react-redux';
 import store from './Store';
@@ -16,4 +15,9 @@ const root = (
 );
 ReactDOM.render(root, document.getElementById('root'));
 
-registerServiceWorker();
+// Disable service worker registration as it adds unnecessary debugging complexity
+// (see https://github.com/facebook/create-react-app/issues/2398) for relatively small gain
+// (caching of static assets, as described in
+// https://create-react-app.dev/docs/making-a-progressive-web-app/, which should also be handled
+// by most browsers)
+// registerServiceWorker();
