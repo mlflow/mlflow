be3716e709770cf7b167efab11d5c809f3c70cb4
Jim Thompson <jimthompson5802@aol.com>
2019-11-13 20:47:07 -0500

    Revert "chore: code refactor"
    This reverts commit 5e90fc87

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -11,13 +11,13 @@ from tests.projects.utils import tracking_uri_mock
 
 EXAMPLES_DIR = 'examples'
 
+@pytest.mark.large
 @pytest.mark.parametrize("directory, params", [
     ('sklearn_logistic_regression', []),
     ("sklearn_elasticnet_wine", ["-P", "alpha=0.5"]),
     (os.path.join("sklearn_elasticnet_diabetes", "linux"), []),
     ('h2o', []),
 ])
-@pytest.mark.large
 def test_mlflow_run_example(tracking_uri_mock, directory, params):
     cli_run_list = [os.path.join(EXAMPLES_DIR, directory)] + params
     invoke_cli_runner(cli.run, cli_run_list)
