37f728d56019f126d4bbf502d31d2b6ff3310799
Juntai Zheng <juntai.zheng@databricks.com>
2019-11-14 14:30:32 -0800

    added try_mlflow_log

--- mlflow/tensorflow.py
+++ mlflow/tensorflow.py
@@ -712,7 +712,7 @@ def autolog(every_n_iter=100):
     def fit_generator(self, *args, **kwargs):
         global _AUTO_END_RUN
         if not mlflow.active_run():
-            mlflow.start_run()
+            try_mlflow_log(mlflow.start_run())
             _AUTO_END_RUN = True
 
         original = gorilla.get_original_attribute(tensorflow.keras.Model, 'fit_generator')
@@ -732,7 +732,7 @@ def autolog(every_n_iter=100):
         shutil.rmtree(log_dir)
 
         if _AUTO_END_RUN:
-            mlflow.end_run()
+            try_mlflow_log(mlflow.end_run())
         _AUTO_END_RUN = False
 
         return result
