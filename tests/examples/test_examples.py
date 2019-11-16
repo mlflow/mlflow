794959a63d145305bdbec253b660c591813d4c6d
Jim Thompson <jimthompson5802@aol.com>
2019-11-15 19:08:12 -0500

    fix: refactor code

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -13,20 +13,13 @@ from tests.projects.utils import tracking_uri_mock
 EXAMPLES_DIR = 'examples'
 
 
+@pytest.mark.large
 @pytest.mark.parametrize("directory, params", [
     ('sklearn_logistic_regression', []),
     ('sklearn_elasticnet_wine', ['-P', 'alpha=0.5']),
+    (os.path.join("sklearn_elasticnet_diabetes", "linux"), []),
     ('h2o', []),
 ])
-def test_mlflow_run_example(tracking_uri_mock, directory, params):
-    cli_run_list = [os.path.join(EXAMPLES_DIR, directory)] + params
-    invoke_cli_runner(cli.run, cli_run_list)
-
-
-@pytest.mark.parametrize("directory, params", [
-    (os.path.join("sklearn_elasticnet_diabetes", "linux"), []),
-])
-@pytest.mark.large
 def test_mlflow_run_example(tracking_uri_mock, directory, params):
     cli_run_list = [os.path.join(EXAMPLES_DIR, directory)] + params
     invoke_cli_runner(cli.run, cli_run_list)
@@ -36,7 +29,6 @@ def test_mlflow_run_example(tracking_uri_mock, directory, params):
     ('quickstart', ['python', 'mlflow_tracking.py']),
 ])
 def test_command_example(tmpdir, directory, command):
-
     os.environ['MLFLOW_TRACKING_URI'] = path_to_local_file_uri(str(tmpdir.join("mlruns")))
     cwd_dir = os.path.join(EXAMPLES_DIR, directory)
 
