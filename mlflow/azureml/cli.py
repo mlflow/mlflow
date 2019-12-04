0dad90c11cbdde4c0ec2243c2795bcdedeb87737
matallanas <ematallanas@plainconcepts.com>
2019-12-04 22:35:03 +0100

    Added the authentication through service principal in order to be able to authenticate for CI/CD

--- mlflow/azureml/cli.py
+++ mlflow/azureml/cli.py
@@ -44,9 +44,13 @@ def commands():
                     " key-value pairs, to associate with the Azure Container Image and the Azure"
                     " Model that are created. These tags are added to a set of default tags"
                     " that include the model path, the model run id (if specified), and more."))
+@click.option("--service-principal", is_flag=True, help="Authenticate to the workspace through a service principal")
+@click.option("--tenant-id", default=None, help="Tenant Id of the Azure subscription.")
+@click.option("--user-id", "-u", default=None, help="User Id of the azure application.")
+@click.option("--password", "-p", default=None, help="Password of the azure application")
 @experimental
 def build_image(model_uri, workspace_name, subscription_id, image_name, model_name,
-                mlflow_home, description, tags):
+                mlflow_home, description, tags, service_principal, tenant_id, user_id, password):
     """
     Register an MLflow model with Azure ML and build an Azure ML ContainerImage for deployment.
     The resulting image can be deployed as a web service to Azure Container Instances (ACI) or
@@ -61,8 +65,14 @@ def build_image(model_uri, workspace_name, subscription_id, image_name, model_na
     # upon command invocation.
     # pylint: disable=import-error
     from azureml.core import Workspace
+    from azureml.core.authentication import ServicePrincipalAuthentication
 
-    workspace = Workspace.get(name=workspace_name, subscription_id=subscription_id)
+    auth = None
+
+    if(service_principal):
+      auth = ServicePrincipalAuthentication(tenant_id=tenant_id, service_principal_id=user_id, service_principal_password=password)
+
+    workspace = Workspace.get(name=workspace_name, subscription_id=subscription_id, auth=auth)
     if tags is not None:
         tags = json.loads(tags)
     mlflow.azureml.build_image(
