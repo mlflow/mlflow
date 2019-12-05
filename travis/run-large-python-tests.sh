66a9b39d729663385adb1bdf01f481c599f34d10
Harutaka Kawamura <hkawamura0130@gmail.com>
2019-12-05 10:00:50 +0900

    Add support for LightGBM flavor (#2136)
    Adds support for a LightGBM model flavor. You can now save & load LightGBM models using the mlflow.lightgbm.save_model, mlflow.lightgbm.log_model, and mlflow.lightgbm.load_model APIs

--- travis/run-large-python-tests.sh
+++ travis/run-large-python-tests.sh
@@ -21,7 +21,7 @@ pytest tests --large --ignore=tests/examples --ignore=tests/h2o --ignore=tests/k
   --ignore=tests/pytorch --ignore=tests/pyfunc --ignore=tests/sagemaker --ignore=tests/sklearn \
   --ignore=tests/spark --ignore=tests/tensorflow --ignore=tests/azureml --ignore=tests/onnx \
   --ignore=tests/keras_autolog --ignore=tests/tensorflow_autolog --ignore=tests/gluon \
-  --ignore=tests/gluon_autolog --ignore=tests/xgboost
+  --ignore=tests/gluon_autolog --ignore=tests/xgboost --ignore=tests/lightgbm
 # Run ML framework tests in their own Python processes to avoid OOM issues due to per-framework
 # overhead
 pytest --verbose tests/examples --large
@@ -38,6 +38,7 @@ pytest --verbose tests/tensorflow_autolog/test_tensorflow_autolog.py --large
 pytest --verbose tests/azureml --large
 pytest --verbose tests/models --large
 pytest --verbose tests/xgboost --large
+pytest --verbose tests/lightgbm --large
 pip install 'tensorflow>=2.0.0'
 pytest --verbose tests/tensorflow/test_tensorflow2_model_export.py --large
 pytest --verbose tests/tensorflow_autolog/test_tensorflow2_autolog.py --large
