fe276888d471aa865901c32fef1f8509b43e85cd
Richard Zang <Zangr@users.noreply.github.com>
2019-12-10 11:21:34 -0800

    Fix project run conda path issue (#2147)
    * fix path
    
    * change to append, keep python path

--- mlflow/projects/databricks.py
+++ mlflow/projects/databricks.py
@@ -256,7 +256,7 @@ def _get_databricks_run_cmd(dbfs_fuse_tar_uri, run_id, entry_point, parameters):
             mlflow_run_arr.extend(["-P", "%s=%s" % (key, value)])
     mlflow_run_cmd = " ".join(mlflow_run_arr)
     shell_command = textwrap.dedent("""
-    export PATH=$DB_HOME/conda/bin:$DB_HOME/python/bin:$PATH &&
+    export PATH=$PATH:$DB_HOME/python/bin &&
     mlflow --version &&
     # Make local directories in the container into which to copy/extract the tarred project
     mkdir -p {tarfile_base} {projects_base} &&
