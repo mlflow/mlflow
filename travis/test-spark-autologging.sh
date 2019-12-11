363a85e022b0bbf3399dc605d4bb30c152958fb2
Sid Murching <sid.murching@databricks.com>
2019-12-11 14:56:20 -0800

    Update .travis.yml

--- travis/test-spark-autologging.sh
+++ travis/test-spark-autologging.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
+# Test Spark autologging against the Spark 3.0 preview. This script is temporary and should be
+# removed once Spark 3.0 is released in favor of simply updating all tests to run against Spark 3.0
+# (i.e. updating the pyspark dependency version in travis/large-requirements.txt)
+set -ex
 
-# Test that mlflow installation does not modify (downgrade/upgrade/uninstall) packages from a
-# specific Anaconda distribution.
+# Build Java package
+pushd mlflow/java/spark
+mvn package -DskipTests
+popd
 
-set -eux
+# Install PySpark 3.0 preview & run tests. For faster local iteration, you can also simply download
+# the .tgz used below (http://mirror.cogentco.com/pub/apache/spark/spark-3.0.0-preview/spark-3.0.0-preview-bin-hadoop2.7.tgz),
+# extract it, and set SPARK_HOME to the path of the extracted folder while invoking pytest as
+# shown below
+TEMPDIR=$(mktemp -d)
+pushd $TEMPDIR
+wget -qO- http://mirror.cogentco.com/pub/apache/spark/spark-3.0.0-preview/spark-3.0.0-preview-bin-hadoop2.7.tgz | tar xvz -
+pip install -e python
+popd
 
-git clone https://github.com/apache/spark/tree/master/bin
+SPARK_HOME=$TEMPDIR pytest tests/spark_autologging/test_spark_datasource_autologging.py
+rm -rf $TEMPDIR
+
+# Reinstall old dependencies
+pip install -r travis/large-requirements.txt
