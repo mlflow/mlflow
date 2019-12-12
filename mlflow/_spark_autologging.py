1b9c56883025a03ed3e0b6f9e0f444ab9785c3d0
Sid Murching <sid.murching@databricks.com>
2019-12-11 16:42:44 -0800

    Put module imports inside methods so import mlflow.spark still works without having PySpark installed

--- mlflow/_spark_autologging.py
+++ mlflow/_spark_autologging.py
@@ -1,19 +1,21 @@
+import logging
 import os
+import warnings
 
 import mlflow
 from mlflow.utils import experimental
 from mlflow.utils.databricks_utils import is_in_databricks_notebook
 from mlflow.tracking.client import MlflowClient
 
-from pyspark import SparkContext
-from py4j.java_gateway import CallbackServerParameters
 
 _JAVA_PACKAGE = "org.mlflow.spark.autologging"
 _REPL_ID_JAVA_PACKAGE = "org.mlflow.spark.autologging.databricks"
 _SPARK_TABLE_INFO_TAG_NAME = "sparkTableInfo"
 _SPARK_TABLE_INFO_LISTENER = None
+_logger = logging.getLogger(__name__)
 
 def _get_java_package():
+    from pyspark import SparkContext
     sc = SparkContext.getOrCreate()
     spark_version_parts = sc.version.split(".")
     spark_major_version = None
@@ -33,39 +35,27 @@ def _get_jvm_event_publisher():
       to watch for datasource reads)
     - register(subscriber) for registering subscribers to receive datasource events
     """
+    from pyspark import SparkContext
     jvm = SparkContext._gateway.jvm
     qualified_classname = "{}.{}".format(_get_java_package(), "MlflowAutologEventPublisher")
     return getattr(jvm, qualified_classname)
 
-def _initialize_listener():
-
-
-@experimental
-def autolog():
-    """
-    Enables automatic logging of Spark datasource paths, versions (if applicable), and formats
-    when they are read.
-
-    Datasource information is logged under the current active MLflow run,
-    creating an active run if necessary. Note that autologging of Spark ML (MLlib) models is not
-    currently supported via this API.
-
-    Datasource-autologging is best-effort, meaning that if Spark is under heavy load or MLflow
-    logging fails for any reason (e.g. if the MLflow server is unavailable), logging may be
-    dropped.
-
-    This method assumes a SparkSession already exists & should be called on the Spark driver,
-    not on the executors (i.e. do not call this method within a parallelized by
-    Spark).
-
-    For any unexpected issues with autologging, check Spark driver and executor logs in addition
-    to stderr & stdout generated from your MLflow code - datasource information is pulled from
-    Spark, so logs relevant to debugging may show up amongst the Spark logs.
-    """
+def _autolog():
+    """Implementation of Spark datasource autologging"""
+    from pyspark import SparkContext
+    from py4j.java_gateway import CallbackServerParameters
 
     global _SPARK_TABLE_INFO_LISTENER
     if _SPARK_TABLE_INFO_LISTENER is None:
         gw = SparkContext._gateway
+        if gw is None:
+            warnings.warn(
+                "No active SparkContext found, refusing to enable Spark datasource "
+                "autologging. Please create a SparkSession e.g. via "
+                "SparkSession.builder.getOrCreate() (see API docs at "
+                "https://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.SparkSession) "
+                "before attempting to enable autologging")
+            return
         params = gw.callback_server_parameters
         callback_server_params = CallbackServerParameters(
             address=params.address, port=params.port, daemonize=True, daemonize_connections=True,
@@ -80,6 +70,32 @@ def autolog():
         _SPARK_TABLE_INFO_LISTENER.register()
 
 
+@experimental
+def autolog():
+    """
+    Enables automatic logging of Spark datasource paths, versions (if applicable), and formats
+    when they are read. This method is not threadsafe and assumes a SparkSession already exists.
+    It should be called on the Spark driver, not on the executors (i.e. do not call this method
+    within a function parallelized by Spark).
+
+    Datasource information is logged under the current active MLflow run, creating an active run
+    if none exists. Note that autologging of Spark ML (MLlib) models is not currently supported
+    via this API.
+
+    Datasource-autologging is best-effort, meaning that if Spark is under heavy load or MLflow
+    logging fails for any reason (e.g. if the MLflow server is unavailable), logging may be
+    dropped.
+
+    For any unexpected issues with autologging, check Spark driver and executor logs in addition
+    to stderr & stdout generated from your MLflow code - datasource information is pulled from
+    Spark, so logs relevant to debugging may show up amongst the Spark logs.
+    """
+    try:
+        _autolog()
+    except Exception as e:
+        warnings.warn("Could not enable Spark datasource autologging, got error:\n%s" % e)
+
+
 class PythonSubscriber(object):
     """
     Subscriber, intended to be instantiated once per Python process, that logs Spark table
@@ -92,7 +108,7 @@ class PythonSubscriber(object):
         self.uuid = None
 
     def toString(self):
-        # For debugging purposes, e.g. inspecting Manager.listeners
+        # For debugging
         return "PythonSubscriber<uuid=%s>" % self.uuid
 
     def ping(self):
@@ -100,7 +116,8 @@ class PythonSubscriber(object):
 
     def _get_table_info_string(self, path, version, format):
         if format == "delta":
-            return "path={path},version={version},format={format}".format(path=path, version=version, format=format)
+            return "path={path},version={version},format={format}".format(
+                path=path, version=version, format=format)
         return "path={path},format={format}".format(path=path, format=format)
 
     def notify(self, path, version, format):
@@ -141,7 +158,7 @@ class PythonSubscriber(object):
         return self.uuid
 
     def replId(self):
-        # TODO will this work in OSS?
+        from pyspark import SparkContext
         return SparkContext.getOrCreate().getLocalProperty("spark.databricks.replId")
 
     class Java:
