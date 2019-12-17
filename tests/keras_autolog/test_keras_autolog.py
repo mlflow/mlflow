117c3d830754dcecdc8a9646fe7a3c067baa991a
Juntai Zheng <juntai.zheng@databricks.com>
2019-12-17 13:31:48 -0800

    first draft of tests

--- tests/keras_autolog/test_keras_autolog.py
+++ tests/keras_autolog/test_keras_autolog.py
@@ -1,5 +1,7 @@
 import pytest
 import numpy as np
+np.random.seed(1337)
+
 import keras
 import keras.layers as layers
 
@@ -154,3 +156,70 @@ def test_keras_autolog_model_can_load_from_artifact(keras_random_data_run, rando
     assert 'model' in artifacts
     model = mlflow.keras.load_model("runs:/" + run_id + "/model")
     model.predict(random_train_data)
+
+
+@pytest.fixture
+def all_zeros_train_data():
+    return np.zeros((1000, 32))
+
+
+@pytest.fixture
+def all_zeros_one_hot_labels():
+    n, n_class = (1000, 10)
+    classes = np.zeros(n, dtype=int)
+    labels = np.zeros((n, n_class))
+    labels[np.arange(n), classes] = 0
+    return labels
+
+
+@pytest.fixture
+def keras_random_data_run_with_early_stop(all_zeros_train_data, fit_variant, all_zeros_one_hot_labels,
+                                          manual_run, restore_best_weights):
+    mlflow.keras.autolog()
+
+    data = all_zeros_train_data
+    labels = all_zeros_one_hot_labels
+
+    model = create_model()
+    callback = keras.callbacks.callbacks.EarlyStopping(monitor='loss', patience=5,
+                                                       restore_best_weights=restore_best_weights)
+
+    if fit_variant == 'fit_generator':
+        def generator():
+            while True:
+                yield data, labels
+        history = model.fit_generator(generator(), epochs=10, callbacks=[callback],
+                                      steps_per_epoch=1, shuffle=False)
+    else:
+        history = model.fit(data, labels, epochs=10, callbacks=[callback])
+
+    return client.get_run(client.list_run_infos(experiment_id='0')[0].run_id), history, callback
+
+
+@pytest.mark.large
+@pytest.mark.parametrize('fit_variant,restore_best_weights', [('fit', True),
+                                                              ('fit_generator', True)])
+def test_keras_autolog_early_stop_logs_extra_step(keras_random_data_run_with_early_stop):
+    run, history, callback = keras_random_data_run_with_early_stop
+    assert 'loss' in history.history
+    num_of_epochs = len(history.history['loss'])
+    metric_history = client.get_metric_history(run.info.run_id, 'loss')
+    # Check the test epoch numbers are correct
+    assert num_of_epochs == callback.patience + 1
+    # Check that MLflow has logged the metrics of the "best" model
+    assert len(metric_history) == num_of_epochs + 1
+    # Check that MLflow has logged the correct data
+    assert history.history['loss'][callback.stopped_epoch] == metric_history[-1].value
+
+
+@pytest.mark.large
+@pytest.mark.parametrize('fit_variant,restore_best_weights', [('fit', False),
+                                                              ('fit_generator', False)])
+def test_keras_autolog_early_stop_no_restore_does_not_log(keras_random_data_run_with_early_stop):
+    run, history, callback = keras_random_data_run_with_early_stop
+    assert 'loss' in history.history
+    num_of_epochs = len(history.history['loss'])
+    metric_history = client.get_metric_history(run.info.run_id, 'loss')
+    # Check the test epoch numbers are correct
+    assert num_of_epochs == callback.patience + 1
+    assert len(metric_history) == num_of_epochs
