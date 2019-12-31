2a0f47ebbd5a16bbc6479834a2469a2c304037df
harupy <hkawamura0130@gmail.com>
2019-12-31 21:08:54 +0900

    Fix lint

--- tests/xgboost/test_xgboost_autolog.py
+++ tests/xgboost/test_xgboost_autolog.py
@@ -165,7 +165,8 @@ def test_xgb_autolog_logs_metrics_with_multi_validation_data_and_metrics(bst_par
     for eval_name in [e[1] for e in evals]:
         for metric_name in params['eval_metric']:
             metric_key = '{}-{}'.format(eval_name, metric_name)
-            metric_history = [x.value for x in client.get_metric_history(run.info.run_id, metric_key)]
+            metric_history = [x.value for x
+                              in client.get_metric_history(run.info.run_id, metric_key)]
             assert metric_key in data.metrics
             assert len(metric_history) == 20
             assert metric_history == evals_result[eval_name][metric_name]
@@ -186,7 +187,8 @@ def test_xgb_autolog_logs_metrics_with_early_stopping(bst_params, dtrain):
     for eval_name in [e[1] for e in evals]:
         for metric_name in params['eval_metric']:
             metric_key = '{}-{}'.format(eval_name, metric_name)
-            metric_history = [x.value for x in client.get_metric_history(run.info.run_id, metric_key)]
+            metric_history = [x.value for x
+                              in client.get_metric_history(run.info.run_id, metric_key)]
             assert metric_key in data.metrics
             assert len(metric_history) == 20 + 1
 
