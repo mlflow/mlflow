8c4368b9791e9e3411f86533a3b8315b6e658047
Theophile Henri <t.henri@criteo.com>
2020-01-03 15:34:28 +0100

    Fix linter

--- mlflow/store/artifact/hdfs_artifact_repo.py
+++ mlflow/store/artifact/hdfs_artifact_repo.py
@@ -6,7 +6,6 @@ from contextlib import contextmanager
 from six.moves import urllib
 
 from mlflow.entities import FileInfo
-from mlflow.exceptions import MlflowException
 from mlflow.store.artifact.artifact_repo import ArtifactRepository
 from mlflow.utils.file_utils import mkdir, relative_path_to_artifact_path
 
