6cc51891f1c682b468e9e1c74406a741d45ac662
Andy Chow <andy.chow@databricks.com>
2020-01-02 18:06:33 -0800

    Add timing command to run-large-python-tests.sh

--- travis/run-large-python-tests.sh
+++ travis/run-large-python-tests.sh
@@ -7,8 +7,9 @@ trap 'err=1' ERR
 export MLFLOW_HOME=$(pwd)
 
 SAGEMAKER_OUT=$(mktemp)
-if mlflow sagemaker build-and-push-container --no-push --mlflow-home . > $SAGEMAKER_OUT 2>&1; then
+if time mlflow sagemaker build-and-push-container --no-push --mlflow-home . > $SAGEMAKER_OUT 2>&1; then
   echo "Sagemaker container build succeeded.";
+  cat $SAGEMAKER_OUT;
 else
   echo "Sagemaker container build failed, output:";
   cat $SAGEMAKER_OUT;
@@ -22,25 +23,25 @@ pytest tests --large --ignore=tests/examples --ignore=tests/h2o --ignore=tests/k
   --ignore=tests/gluon_autolog --ignore=tests/xgboost --ignore=tests/lightgbm
 # Run ML framework tests in their own Python processes to avoid OOM issues due to per-framework
 # overhead
-pytest --verbose tests/h2o --large
-pytest --verbose tests/onnx --large
-pytest --verbose tests/pytorch --large
-pytest --verbose tests/pyfunc --large
-pytest --verbose tests/sagemaker --large
-pytest --verbose tests/sagemaker/mock --large
-pytest --verbose tests/sklearn --large
-pytest --verbose tests/spark --large
-pytest --verbose tests/tensorflow/test_tensorflow_model_export.py --large
-pytest --verbose tests/tensorflow_autolog/test_tensorflow_autolog.py --large
-pytest --verbose tests/azureml --large
-pytest --verbose tests/models --large
-pytest --verbose tests/xgboost --large
-pytest --verbose tests/lightgbm --large
-pip install 'tensorflow>=2.0.0'
-pytest --verbose tests/tensorflow/test_tensorflow2_model_export.py --large
-pytest --verbose tests/tensorflow_autolog/test_tensorflow2_autolog.py --large
-pytest --verbose tests/keras --large
-pytest --verbose tests/keras_autolog --large
-pytest --verbose tests/gluon --large
-pytest --verbose tests/gluon_autolog --large
+time pytest --verbose tests/h2o --large
+time pytest --verbose tests/onnx --large
+time pytest --verbose tests/pytorch --large
+time pytest --verbose tests/pyfunc --large
+time pytest --verbose tests/sagemaker --large
+time pytest --verbose tests/sagemaker/mock --large
+time pytest --verbose tests/sklearn --large
+time pytest --verbose tests/spark --large
+time pytest --verbose tests/tensorflow/test_tensorflow_model_export.py --large
+time pytest --verbose tests/tensorflow_autolog/test_tensorflow_autolog.py --large
+time pytest --verbose tests/azureml --large
+time pytest --verbose tests/models --large
+time pytest --verbose tests/xgboost --large
+time pytest --verbose tests/lightgbm --large
+time pip install 'tensorflow>=2.0.0'
+time pytest --verbose tests/tensorflow/test_tensorflow2_model_export.py --large
+time pytest --verbose tests/tensorflow_autolog/test_tensorflow2_autolog.py --large
+time pytest --verbose tests/keras --large
+time pytest --verbose tests/keras_autolog --large
+time pytest --verbose tests/gluon --large
+time pytest --verbose tests/gluon_autolog --large
 test $err = 0
