af6efb6be83724d49c416a1ea7b3eef7da435b3a
Juntai Zheng <juntai.zheng@databricks.com>
2020-01-06 16:48:26 -0800

    added example tests, alphabetized tests

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -15,13 +15,21 @@ EXAMPLES_DIR = 'examples'
 
 @pytest.mark.large
 @pytest.mark.parametrize("directory, params", [
+    ('flower_classifier', []),
+    ('h2o', []),
+    ('hyperparam', ['-e', 'train']),
+    ('hyperparam', ['-e', 'random']),
+    ('hyperparam', ['-e', 'gpyopt']),
+    ('hyperparam', ['-e', 'hyperopt']),
+    ('lightgbm', ['-P', 'colsample-bytree=0.8', '-P', 'subsample=0.9']),
+    ('multistep_workflows', ['-P', 'als_max_iter=20', '-P', 'keras_hidden_units=50']),
+    ('prophet', []),
+    ('pytorch', []),
     ('sklearn_logistic_regression', []),
     ('sklearn_elasticnet_wine', ['-P', 'alpha=0.5']),
-    (os.path.join("sklearn_elasticnet_diabetes", "linux"), []),
-    ('h2o', []),
-    ('prophet', []),
-    ('xgboost', ['-P', 'conda-env=examples/xgboost/conda.yaml']),
-    ('lightgbm', ['-P', 'conda-env=examples/lightgbm/conda.yaml']),
+    (os.path.join('sklearn_elasticnet_diabetes', 'linux'), []),
+    (os.path.join('tensorflow', 'tf1'), ['-P', 'steps=10']),
+    ('xgboost', ['-P', 'colsample-bytree=0.8', '-P', 'subsample=0.9'])
 ])
 def test_mlflow_run_example(tracking_uri_mock, directory, params):
     cli_run_list = [os.path.join(EXAMPLES_DIR, directory)] + params
@@ -30,9 +38,13 @@ def test_mlflow_run_example(tracking_uri_mock, directory, params):
 
 @pytest.mark.large
 @pytest.mark.parametrize("directory, command", [
+    ('docker', ['docker', 'build', '-t', 'mlflow-docker-example', '-f', 'Dockerfile', '.']),
+    ('gluon', ['python', 'train.py']),
+    ('keras', ['python', 'train.py']),
+    ('lightgbm', ['python', 'train.py', '--colsample-bytree', '0.8', '--subsample', '0.9']),
     ('quickstart', ['python', 'mlflow_tracking.py']),
-    ('xgboost', ['python', 'train.py']),
-    ('lightgbm', ['python', 'train.py']),
+    ('remote_store', ['python', 'remote_server.py']),
+    ('xgboost', ['python', 'train.py', '--colsample-bytree', '0.8', '--subsample', '0.9'])
 ])
 def test_command_example(tmpdir, directory, command):
     os.environ['MLFLOW_TRACKING_URI'] = path_to_local_file_uri(str(tmpdir.join("mlruns")))
