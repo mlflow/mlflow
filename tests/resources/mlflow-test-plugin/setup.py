5ba0dc3bce19e351590391d3fbd72b0210c03705
Sid Murching <sid.murching@databricks.com>
2020-01-07 10:31:57 -0800

    Add comment in actual test

--- tests/resources/mlflow-test-plugin/setup.py
+++ tests/resources/mlflow-test-plugin/setup.py
@@ -2,17 +2,23 @@ from setuptools import setup, find_packages
 
 
 setup(
-    # Define a package
     name="mflow-test-plugin",
     version="0.0.1",
     description="Test plugin for MLflow",
     packages=find_packages(),
+    # Require MLflow as a dependency of the plugin, so that plugin users can simply install
+    # the plugin & then immediately use it with MLflow
     install_requires=["mlflow"],
     entry_points={
+        # Define a tracking AbstractStore plugin for tracking URIs with scheme 'file-plugin'
         "mlflow.tracking_store": "file-plugin=mlflow_test_plugin:PluginFileStore",
+        # Define a ArtifactRepository plugin for artifact URIs with scheme 'file-plugin'
         "mlflow.artifact_repository":
             "file-plugin=mlflow_test_plugin:PluginLocalArtifactRepository",
+        # Define a RunContextProvider plugin. The entry point name for run context providers
+        # is not used, and so is set to the string "unused" here
         "mlflow.run_context_provider": "unused=mlflow_test_plugin:PluginRunContextProvider",
+        # Define a model-registry AbstractStore plugin for tracking URIs with scheme 'file-plugin'
         "mlflow.model_registry_store":
             "file-plugin=mlflow_test_plugin:PluginRegistrySqlAlchemyStore",
     },
