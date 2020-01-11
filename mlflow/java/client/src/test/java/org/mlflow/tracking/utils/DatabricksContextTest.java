671e3fafd1e63510dd2ac9ec2980a8f09f708b43
dbczumar <39497902+dbczumar@users.noreply.github.com>
2020-01-10 20:16:09 -0800

    Add autodetection of job environments to Java client (#2286)
    * Initial
    
    * Some docs
    
    * Cleanup
    
    * Impl with some tests
    
    * Pluralize
    
    * Rename run id field
    
    * test fix
    
    * Simplify

--- mlflow/java/client/src/test/java/org/mlflow/tracking/utils/DatabricksContextTest.java
+++ mlflow/java/client/src/test/java/org/mlflow/tracking/utils/DatabricksContextTest.java
@@ -39,7 +39,7 @@ public class DatabricksContextTest {
   }
 
   @Test
-  public void testGetTags() {
+  public void testGetTagsWithEmptyNotebookAndJobDetails() {
     // Will return empty map if not in Databricks notebook.
     {
       baseMap.put("notebookId", null);
@@ -48,8 +48,25 @@ public class DatabricksContextTest {
       Assert.assertFalse(context.isInDatabricksNotebook());
       Assert.assertEquals(context.getTags(), Maps.newHashMap());
     }
+    // Will return empty map if not in Databricks job.
+    {
+      baseMap.put("jobId", null);
+      baseMap.put("jobRunId", null);
+      DatabricksContext context = DatabricksContext.createIfAvailable(MyDynamicProvider.class.getName());
+      Assert.assertFalse(context.isInDatabricksNotebook());
+      Assert.assertEquals(context.getTags(), Maps.newHashMap());
+    }
+    // Will return empty map if not config map is empty.
+    {
+      DatabricksContext context = DatabricksContext.createIfAvailable(MyDynamicProvider.class.getName());
+      Assert.assertFalse(context.isInDatabricksNotebook());
+      Assert.assertEquals(context.getTags(), Maps.newHashMap());
+    }
+  }
 
-    // Will return all tags if context is set as expected.
+  @Test
+  public void testGetTagsForNotebook() {
+    // Will return all tags if notebook context is set as expected.
     {
       baseMap = new HashMap<>();
       Map<String, String> expectedTags = ImmutableMap.of(
@@ -62,7 +79,6 @@ public class DatabricksContextTest {
       DatabricksContext context = DatabricksContext.createIfAvailable(MyDynamicProvider.class.getName());
       Assert.assertEquals(context.getTags(), expectedTags);
     }
-
     // Will not set notebook path tags if context doesn't have a notebookPath member.
     {
       baseMap = new HashMap<>();
@@ -70,6 +86,38 @@ public class DatabricksContextTest {
         MlflowTagConstants.DATABRICKS_NOTEBOOK_ID, "1");
       baseMap.put("notebookId", "1");
       baseMap.put("notebookPath", null);
+      DatabricksContext context = DatabricksContext.createIfAvailable(MyDynamicProvider.class.getName());
+      Assert.assertEquals(context.getTags(), expectedTags);
+    }
+  }
+
+  @Test
+  public void testGetTagsForJob() {
+    // Will return all tags if job context is set as expected.
+    {
+      baseMap = new HashMap<>();
+      Map<String, String> expectedTags = ImmutableMap.of(
+        MlflowTagConstants.DATABRICKS_JOB_ID, "70",
+        MlflowTagConstants.DATABRICKS_JOB_RUN_ID, "5",
+        MlflowTagConstants.DATABRICKS_JOB_TYPE, "notebook",
+        MlflowTagConstants.SOURCE_TYPE, "JOB",
+        MlflowTagConstants.SOURCE_NAME, "jobs/70/run/5");
+      baseMap.put("jobId", "70");
+      baseMap.put("jobRunId", "5");
+      baseMap.put("jobType", "notebook");
+      DatabricksContext context = DatabricksContext.createIfAvailable(MyDynamicProvider.class.getName());
+      Assert.assertEquals(context.getTags(), expectedTags);
+    }
+    // Will not set job type tag if job type is absent.
+    {
+      baseMap = new HashMap<>();
+      Map<String, String> expectedTags = ImmutableMap.of(
+        MlflowTagConstants.DATABRICKS_JOB_ID, "70",
+        MlflowTagConstants.DATABRICKS_JOB_RUN_ID, "5",
+        MlflowTagConstants.SOURCE_TYPE, "JOB",
+        MlflowTagConstants.SOURCE_NAME, "jobs/70/run/5");
+      baseMap.put("jobId", "70");
+      baseMap.put("jobRunId", "5");
       DatabricksContext context = DatabricksContext.createIfAvailable(MyDynamicProvider.class.getName());
       Assert.assertEquals(context.getTags(), expectedTags);
     }
