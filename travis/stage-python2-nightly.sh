e556cbecdbbb4aca3b371b201320df0c8dff0a9d
Juntai Zheng <juntai.zheng@databricks.com>
2020-01-15 13:12:20 -0800

    WIP

---
+++ travis/stage-python2-nightly.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+if [[ "$TRAVIS_EVENT_TYPE" == "cron" || "$CHANGED_FILES" == *"examples"* ]]
+then
+    pytest --verbose ../tests/examples --large
+fi
+if [[ "$TRAVIS_EVENT_TYPE" == "cron" || "$CHANGED_FILES" == *"Dockerfile"* ]]
+then
+    docker build -t mlflow_test_build . && docker images | grep mlflow_test_build
+fi
