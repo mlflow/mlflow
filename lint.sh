167c9b9d56096d87212bb2a8f84af7e11ab5cc58
Sid Murching <sid.murching@databricks.com>
2020-01-21 22:35:40 -0800

    Update lint

--- lint.sh
+++ lint.sh
@@ -5,7 +5,8 @@ set -e
 FWDIR="$(cd "`dirname $0`"; pwd)"
 cd "$FWDIR"
 
-pycodestyle --max-line-length=100 --exclude mlflow/protos,mlflow/server/js,mlflow/store/db_migrations,mlflow/temporary_db_migrations_for_pre_1_users -- mlflow tests
+# TODO: remove gluon from this list once https://github.com/pypa/pip/issues/7626 is resolved
+pycodestyle --max-line-length=100 --exclude mlflow/gluon.py,mlflow/protos,mlflow/server/js,mlflow/store/db_migrations,mlflow/temporary_db_migrations_for_pre_1_users -- mlflow tests
 pylint --msg-template="{path} ({line},{column}): [{msg_id} {symbol}] {msg}" --rcfile="$FWDIR/pylintrc" -- mlflow tests
 
 rstcheck README.rst
