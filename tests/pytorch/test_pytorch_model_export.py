519e397e27e5c53249cb50c5e020453b34749259
tomasatdatabricks <tomas.nykodym@databricks.com>
2020-01-23 14:32:06 -0800

    Fix test.

--- tests/pytorch/test_pytorch_model_export.py
+++ tests/pytorch/test_pytorch_model_export.py
@@ -438,9 +438,7 @@ def test_save_model_with_wrong_codepaths_fails_corrrectly(
             pytorch_model=module_scoped_subclassed_model,
             conda_env=None,
             code_paths="some string")
-
-    assert str(exc_info).endswith("TypeError: Argument code_paths should be a list, "
-                                  "not <class 'str'>")
+    assert "TypeError: Argument code_paths should be a list, not <class 'str'>" in str(exc_info)
     assert not os.path.exists(model_path)
 
 
