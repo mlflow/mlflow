e112cd46cf08990c679aba6df045ca079c4c06aa
Juntai Zheng <juntai.zheng@databricks.com>
2020-01-22 16:31:03 -0800

    reverting pytorch test change

--- tests/pytorch/test_pytorch_model_export.py
+++ tests/pytorch/test_pytorch_model_export.py
@@ -1,8 +1,5 @@
 from __future__ import print_function
 
-import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader
 import importlib
 import os
 import json
@@ -15,7 +12,10 @@ import numpy as np
 import pandas as pd
 import pandas.testing
 import sklearn.datasets as datasets
+import torch
+import torch.nn as nn
 import yaml
+from torch.utils.data import DataLoader
 
 import mlflow.pyfunc as pyfunc
 import mlflow.pytorch
