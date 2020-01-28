687818979907d6354b3c427cdc1fa2f84e362dd1
Corey Zumar <corey.zumar@databricks.com>
2020-01-28 11:21:30 -0800

    Progress

--- mlflow/store/db_migrations/versions/cfd24bdc0731_update_run_status_constraint_with_killed.py
+++ mlflow/store/db_migrations/versions/cfd24bdc0731_update_run_status_constraint_with_killed.py
@@ -6,9 +6,13 @@ Create Date: 2019-10-11 15:55:10.853449
 
 """
 from alembic import op, context
-from mlflow.entities import RunStatus
+from mlflow.entities import RunStatus, ViewType
+from mlflow.entities.lifecycle_stage import LifecycleStage
 from sqlalchemy import CheckConstraint, Enum
 from sqlalchemy.engine.reflection import Inspector
+
+from mlflow.store.tracking.dbmodels.models import SqlRun, SourceTypes
+
 
 # revision identifiers, used by Alembic.
 revision = 'cfd24bdc0731'
@@ -16,7 +20,7 @@ down_revision = '2b4d017a5e9b'
 branch_labels = None
 depends_on = None
 
-new_run_status = [
+new_run_statuses = [
     RunStatus.to_string(RunStatus.SCHEDULED),
     RunStatus.to_string(RunStatus.FAILED),
     RunStatus.to_string(RunStatus.FINISHED),
@@ -24,7 +28,7 @@ new_run_status = [
     RunStatus.to_string(RunStatus.KILLED)
 ]
 
-previous_run_status = [
+previous_run_statuses = [
     RunStatus.to_string(RunStatus.SCHEDULED),
     RunStatus.to_string(RunStatus.FAILED),
     RunStatus.to_string(RunStatus.FINISHED),
@@ -49,26 +53,35 @@ def get_check_constraints():
                    if constraint['name'] != 'status']
     return constraints
 
+def expected_check_constraints():
+    return (
+        CheckConstraint(SqlRun.source_type.in_(SourceTypes), name='source_type'),
+        CheckConstraint(SqlRun.status.in_(previous_run_statuses), name='status'),
+        CheckConstraint(SqlRun.lifecycle_stage.in_(LifecycleStage.view_type_to_stages(ViewType.ALL)),
+                        name='runs_lifecycle_stage'),
+    )
 
 def upgrade():
-    if _has_check_constraints():
-        with op.batch_alter_table("runs", table_args=get_check_constraints()) as batch_op:
-            # Update the "status" constraint via the SqlAlchemy `Enum` type. Specify
-            # `native_enum=False` to create a check constraint rather than a
-            # database-backend-dependent enum (see https://docs.sqlalchemy.org/en/13/core/
-            # type_basics.html#sqlalchemy.types.Enum.params.native_enum)
-            batch_op.alter_column("status",
-                                  type_=Enum(*new_run_status, create_constraint=True,
-                                             constraint_name="status", native_enum=False))
+    print(get_check_constraints())
+
+    # if _has_check_constraints():
+    # with op.batch_alter_table("runs", table_args=get_check_constraints()) as batch_op:
+    # with op.batch_alter_table("runs", copy_from=SqlRun.__table__, table_args={'extend_existing': True}) as batch_op:
+    # with op.batch_alter_table("runs", copy_from=SqlRun.__table__, extend_existing=True) as batch_op:
+    with op.batch_alter_table("runs", table_args=expected_check_constraints()) as batch_op:
+    # with op.batch_alter_table("runs", table_args={'extend_existing': True}) as batch_op:
+    # with op.batch_alter_table("runs") as batch_op:
+        # Update the "status" constraint via the SqlAlchemy `Enum` type. Specify
+        # `native_enum=False` to create a check constraint rather than a
+        # database-backend-dependent enum (see https://docs.sqlalchemy.org/en/13/core/
+        # type_basics.html#sqlalchemy.types.Enum.params.native_enum)
+        batch_op.alter_column("status",
+                              type_=Enum(*new_run_statuses, create_constraint=True,
+                                         constraint_name="status", native_enum=False))
 
 
 def downgrade():
-    if _has_check_constraints():
-        with op.batch_alter_table("runs", table_args=get_check_constraints()) as batch_op:
-            # Update the "status" constraint via the SqlAlchemy `Enum` construct. Specify
-            # `native_enum=False` to create a check constraint rather than a
-            # database-backend-dependent enum (see https://docs.sqlalchemy.org/en/13/core/
-            # type_basics.html#sqlalchemy.types.Enum.params.native_enum)
-            batch_op.alter_column("status",
-                                  type_=Enum(*previous_run_status, create_constraint=True,
-                                             constraint_name="status", native_enum=False))
+    # Omit downgrade logic for now - we don't currently provide users a command/API for
+    # reverting a database migration, instead recommending that they take a database backup
+    # before running the migration.
+    pass
