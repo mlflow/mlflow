a2eb995fba5b42ebe294e905f69ab1c52ebf9918
juntai-zheng <39497939+juntai-zheng@users.noreply.github.com>
2020-01-28 13:51:45 -0800

    Add testing for most examples (#2269)
    Add testing for most MLflow examples & fixes broken ones

--- travis/stage-python2-nightly.sh
+++ travis/stage-python2-nightly.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
+CHANGED_FILES=$(git diff --name-only master..HEAD | grep "tests/examples\|examples\|Dockerfile") || true
 if [[ "$TRAVIS_EVENT_TYPE" == "cron" || "$CHANGED_FILES" == *"examples"* ]]
 then
     pytest --verbose tests/examples --large
