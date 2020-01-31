c5a9e998257cd6bea89d29005d718f576a2258fa
Sid Murching <sid.murching@databricks.com>
2020-01-31 11:30:52 -0800

    WIP

--- travis/run-small-python-tests.sh
+++ travis/run-small-python-tests.sh
@@ -7,10 +7,17 @@ trap 'err=1' ERR
 export MLFLOW_HOME=$(pwd)
 
 # NB: Also add --ignore'd tests to run-large-python-tests.sh
-pytest --cov=mlflow --verbose --ignore=tests/h2o --ignore=tests/keras \
+pytest tests --testmon --ignore=tests/h2o --ignore=tests/keras \
   --ignore=tests/pytorch --ignore=tests/pyfunc --ignore=tests/sagemaker --ignore=tests/sklearn \
   --ignore=tests/spark --ignore=tests/tensorflow --ignore=tests/keras_autolog \
   --ignore=tests/tensorflow_autolog --ignore tests/azureml --ignore tests/onnx \
-  --ignore=tests/xgboost --ignore=tests/lightgbm tests --ignore=tests/spark_autologging
+  --ignore=tests/xgboost --ignore=tests/lightgbm tests --ignore=tests/spark_autologging --ignore=tests/models --ignore=tests/examples
+
+# We expect this to not run the same tests again/exit immediately
+pytest tests --testmon --ignore=tests/h2o --ignore=tests/keras \
+  --ignore=tests/pytorch --ignore=tests/pyfunc --ignore=tests/sagemaker --ignore=tests/sklearn \
+  --ignore=tests/spark --ignore=tests/tensorflow --ignore=tests/keras_autolog \
+  --ignore=tests/tensorflow_autolog --ignore tests/azureml --ignore tests/onnx \
+  --ignore=tests/xgboost --ignore=tests/lightgbm tests --ignore=tests/spark_autologging --ignore=tests/models --ignore=tests/examples
 
 test $err = 0
