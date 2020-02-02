239a3fb800abf297f4bb95ac615c27348f640040
dbczumar <39497902+dbczumar@users.noreply.github.com>
2020-02-02 09:49:03 -0800

    Fix broken MLflow DB README link in CLI docs (#2377)
    * Fix parameter reading logic
    
    * Fix link
    
    * Reverts

--- mlflow/db.py
+++ mlflow/db.py
@@ -20,7 +20,7 @@ def upgrade(url):
     **IMPORTANT**: Schema migrations can be slow and are not guaranteed to be transactional -
     **always take a backup of your database before running migrations**. The migrations README,
     which is located at
-    https://github.com/mlflow/mlflow/blob/master/mlflow/store/db_migrations/README, describes
+    https://github.com/mlflow/mlflow/blob/master/mlflow/store/db_migrations/README.md, describes
     large migrations and includes information about how to estimate their performance and
     recover from failures.
     """
