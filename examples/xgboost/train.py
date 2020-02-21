f475cb9fabaa0f49f953cdbf0aa6be3fe74db066
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-02-21 11:21:38 +0900

    Update XGBoost and LightGBM examples (#2378)

--- examples/xgboost/train.py
+++ examples/xgboost/train.py
@@ -4,9 +4,13 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, log_loss
 import xgboost as xgb
+import matplotlib as mpl
+
 
 import mlflow
 import mlflow.xgboost
+
+mpl.use('Agg')
 
 
 def parse_args():
@@ -15,8 +19,6 @@ def parse_args():
                         help='subsample ratio of columns when constructing each tree (default: 1.0)')
     parser.add_argument('--subsample', type=float, default=1.0,
                         help='subsample ratio of the training instances (default: 1.0)')
-    parser.add_argument('--conda-env', type=str, default=None,
-                        help='the path to a conda environment yaml file (default: None)')
     return parser.parse_args()
 
 
@@ -32,29 +34,30 @@ def main():
     dtrain = xgb.DMatrix(X_train, label=y_train)
     dtest = xgb.DMatrix(X_test, label=y_test)
 
-    # train model
-    params = {
-        'objective': 'multi:softprob',
-        'num_class': 3,
-        'eval_metric': 'mlogloss',
-        'colsample_bytree': args.colsample_bytree,
-        'subsample': args.subsample,
-        'seed': 42,
-    }
-    model = xgb.train(params, dtrain)
+    # enable auto logging
+    mlflow.xgboost.autolog()
 
-    # evaluate model
-    y_proba = model.predict(dtest)
-    y_pred = y_proba.argmax(axis=1)
-    loss = log_loss(y_test, y_proba)
-    acc = accuracy_score(y_test, y_pred)
+    with mlflow.start_run():
 
-    # log parameters, metrics, and model
-    with mlflow.start_run() as run:
-        mlflow.log_params(params)
+        # train model
+        params = {
+            'objective': 'multi:softprob',
+            'num_class': 3,
+            'eval_metric': 'mlogloss',
+            'colsample_bytree': args.colsample_bytree,
+            'subsample': args.subsample,
+            'seed': 42,
+        }
+        model = xgb.train(params, dtrain, evals=[(dtrain, 'train')])
+
+        # evaluate model
+        y_proba = model.predict(dtest)
+        y_pred = y_proba.argmax(axis=1)
+        loss = log_loss(y_test, y_proba)
+        acc = accuracy_score(y_test, y_pred)
+
+        # log metrics
         mlflow.log_metrics({'log_loss': loss, 'accuracy': acc})
-        mlflow.xgboost.log_model(model, 'model', args.conda_env)
-        print('Model logged in run {}'.format(run.info.run_uuid))
 
 
 if __name__ == '__main__':
