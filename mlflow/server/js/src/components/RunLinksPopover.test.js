298a9c2c34d28fa5a2d1699337a5eb5e34ba9003
harupy <hkawamura0130@gmail.com>
2020-02-22 11:57:22 +0900

    Fix test

--- mlflow/server/js/src/components/RunLinksPopover.test.js
+++ mlflow/server/js/src/components/RunLinksPopover.test.js
@@ -7,21 +7,29 @@ import Routes from '../Routes';
 
 describe('unit tests', () => {
   let wrapper;
-  const minimumProps = { experimentId: 0 };
+  let minimalProps;
+
+  beforeEach(() => {
+    minimalProps = {
+      experimentId: 0,
+      visible: false,
+      x: 0,
+      y: 0,
+      runItems: [],
+      handleClose: jest.fn(),
+      handleKeyDown: jest.fn(),
+      handleVisibleChange: jest.fn(),
+    };
+  });
 
   test('should render with minimal props without exploding', () => {
-    wrapper = shallow(<RunLinksPopover {...minimumProps} />);
+    wrapper = shallow(<RunLinksPopover {...minimalProps} />);
     expect(wrapper.length).toBe(1);
   });
 
-  test('should render two run links when two run items are given', () => {
-    wrapper = mount(
-      <MemoryRouter>
-        <RunLinksPopover {...minimumProps} />
-      </MemoryRouter>
-    ).find(RunLinksPopover);
-
-    const state = {
+  test('should render two links when two run items are given', () => {
+    const props = {
+      ...minimalProps,
       visible: true,
       runItems: [
         {
@@ -37,15 +45,20 @@ describe('unit tests', () => {
       ],
     };
 
-    wrapper.instance().setState(state);
+    wrapper = mount(
+      <MemoryRouter>
+        <RunLinksPopover {...props} />
+      </MemoryRouter>
+    ).find(RunLinksPopover);
+
     // The popover is attached to the document root and can't be found with wrapper.find.
     const popover = document.getElementsByClassName('ant-popover')[0];
     const links = popover.querySelectorAll('a[href]');
     expect(links.length).toBe(2);
 
-    state.runItems.forEach(({ name, runUuid, color }, index) => {
+    props.runItems.forEach(({ name, runUuid, color }, index) => {
       const link = links[index];
-      const hrefExpected = Routes.getRunPageRoute(minimumProps.experimentId, runUuid);
+      const hrefExpected = Routes.getRunPageRoute(props.experimentId, runUuid);
       expect(link.getAttribute('href')).toBe(hrefExpected);
 
       const p = link.querySelector('p');
