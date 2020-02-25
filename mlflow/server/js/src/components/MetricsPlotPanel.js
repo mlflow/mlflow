mlflow/mlflow

commit 2932f50a8072b4369f242e93d8ed9dafdea85ef3

Prevent metrics plots from resetting zoom when line smoothness or other attributes change (#2408)
Fixes issue where metric plot's zoom state would reset when line smoothness or other plot attributes were modified.

mlflow/server/js/src/components/MetricsPlotPanel.js
@@ -7,7 +7,12 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 import { MetricsPlotView } from './MetricsPlotView';
 import { getRunTags } from '../reducers/Reducers';
-import { MetricsPlotControls, X_AXIS_RELATIVE, X_AXIS_STEP } from './MetricsPlotControls';
+import {
+  MetricsPlotControls,
+  X_AXIS_WALL,
+  X_AXIS_RELATIVE,
+  X_AXIS_STEP,
+} from './MetricsPlotControls';
 import qs from 'qs';
 import { withRouter } from 'react-router-dom';
 import Routes from '../Routes';
@@ -40,10 +45,14 @@ export class MetricsPlotPanel extends React.Component {
       selectedMetricKeys,
       showPoint: false,
       historyRequestIds: [],
-      yAxisLogScale: false,
       lineSmoothness: 0,
+      // Used to keep track of the most-recent linear y-axis plot range, to handle the specific
+      // case where we toggle a plot with negative y-axis bounds from linear to log scale,
+      // and then back to linear scale (we save the initial negative linear y-axis bounds so
+      // that we can restore them when converting from log back to linear scale)
+      lastLinearYAxisRange: [],
     };
-    this.loadMetricHistory(this.props.runUuids, selectedMetricKeys);
+    this.loadMetricHistory(this.props.runUuids, this.state.selectedMetricKeys);
   }
 
   static predictChartType(metrics) {
@@ -105,12 +114,153 @@ export class MetricsPlotPanel extends React.Component {
     return metrics;
   };
 
+  /**
+   * Handle changes in the scale type of the y-axis
+   * @param yAxisLogScale: Boolean - if true, y-axis should be converted to log scale, and if false,
+   * y-axis scale should be converted to a linear scale.
+   */
   handleYAxisLogScaleChange = (yAxisLogScale) => {
-    this.setState({ yAxisLogScale });
+    const newLayout = _.cloneDeep(this.state.layout);
+    const newAxisType = yAxisLogScale ? "log" : "linear";
+
+    // Handle special case of a linear y-axis scale with negative values converted to log scale &
+    // now being restored to linear scale, by restoring the old linear-axis range from
+    // this.state.linearYAxisRange. In particular, we assume that if this.state.linearYAxisRange
+    // is non-empty, it contains a linear y axis range with negative values.
+    if (!yAxisLogScale && this.state.lastLinearYAxisRange &&
+        this.state.lastLinearYAxisRange.length > 0) {
+      newLayout.yaxis = {
+        type: "linear",
+        range: this.state.lastLinearYAxisRange,
+      };
+      this.setState({ layout: newLayout, lastLinearYAxisRange: [] });
+      return;
+    }
+
+    // Otherwise, if plot previously had no y axis range configured, simply set the axis type to
+    // log or linear scale appropriately
+    if (!this.state.layout.yaxis || !this.state.layout.yaxis.range) {
+      newLayout.yaxis = { type: newAxisType, autorange: true };
+      this.setState({ layout: newLayout, lastLinearYAxisRange: [] });
+      return;
+    }
+
+    // lastLinearYAxisRange contains the last range used for a linear-scale y-axis. We set
+    // this state attribute if and only if we're converting from a linear-scale y-axis with
+    // negative bounds to a log scale axis, so that we can restore the negative bounds if we
+    // subsequently convert back to a linear scale axis. Otherwise, we reset this attribute to an
+    // empty array
+    let lastLinearYAxisRange = [];
+
+    // At this point, we know the plot previously had a y axis specified with range bounds
+    // Convert the range to/from log scale as appropriate
+    const oldLayout = this.state.layout;
+    const oldYRange = oldLayout.yaxis.range;
+    if (yAxisLogScale) {
+      if (oldYRange[0] <= 0) {
+        lastLinearYAxisRange = oldYRange;
+        // When converting to log scale, handle negative values (which have no log-scale
+        // representation as taking the log of a negative number is not possible) as follows:
+        // If bottom of old Y range is negative, then tell plotly to infer the log y-axis scale
+        // (set 'autorange' to true), and preserve the old range in the lastLinearYAxisRange
+        // state attribute so that we can restore it if the user converts back to a linear-scale
+        // y axis. We defer to Plotly's autorange here under the assumption that it will produce
+        // a reasonable y-axis log scale for plots containing negative values.
+        newLayout.yaxis = {
+          type: 'log',
+          autorange: true,
+        };
+      } else {
+        newLayout.yaxis = {
+          type: 'log',
+          range: [Math.log(oldYRange[0]) / Math.log(10), Math.log(oldYRange[1]) / Math.log(10)],
+        };
+      }
+    } else {
+      // Otherwise, convert from log to linear scale normally
+      newLayout.yaxis = {
+        type: 'linear',
+        range: [Math.pow(10, oldYRange[0]), Math.pow(10, oldYRange[1])],
+      };
+    }
+    this.setState({ layout: newLayout, lastLinearYAxisRange });
   };
 
+  /**
+   * Handle changes in the type of the metric plot's X axis (e.g. changes from wall-clock
+   * scale to relative-time scale to step-based scale).
+   * @param e: Selection event such that e.target.value is a string containing the new X axis type
+   */
   handleXAxisChange = (e) => {
-    this.setState({ selectedXAxis: e.target.value });
+    // Set axis value type, & reset axis scaling via autorange
+    const axisEnumToPlotlyType = {
+      [X_AXIS_WALL]: "date",
+      [X_AXIS_RELATIVE]: "linear",
+      [X_AXIS_STEP]: "linear",
+    };
+    const axisType = axisEnumToPlotlyType[e.target.value] || "linear";
+    const newLayout = {
+      ...this.state.layout,
+      xaxis: {
+        autorange: true,
+        type: axisType,
+      },
+    };
+    this.setState({ selectedXAxis: e.target.value, layout: newLayout });
+  };
+
+  /**
+   * Handle changes to metric plot layout (x & y axis ranges), e.g. specifically if the user
+   * zooms in or out on the plot.
+   *
+   * @param newLayout: Object containing the new Plot layout. See
+   * https://plot.ly/javascript/plotlyjs-events/#update-data for details on the object's fields
+   * and schema.
+   */
+  handleLayoutChange = (newLayout) => {
+    // Unfortunately, we need to parse out the x & y axis range changes from the onLayout event...
+    // see https://plot.ly/javascript/plotlyjs-events/#update-data
+    const newXRange0 = newLayout["xaxis.range[0]"];
+    const newXRange1 = newLayout["xaxis.range[1]"];
+    const newYRange0 = newLayout["yaxis.range[0]"];
+    const newYRange1 = newLayout["yaxis.range[1]"];
+    let mergedLayout = {
+      ...this.state.layout,
+    };
+    let lastLinearYAxisRange = [...this.state.lastLinearYAxisRange];
+    if (newXRange0 !== undefined && newXRange1 !== undefined) {
+      mergedLayout = {
+        ...mergedLayout,
+        xaxis: {
+          range: [newXRange0, newXRange1],
+        },
+      };
+    }
+    if (newYRange0 !== undefined && newYRange1 !== undefined) {
+      lastLinearYAxisRange = [];
+      mergedLayout = {
+        ...mergedLayout,
+        yaxis: {
+          range: [newYRange0, newYRange1],
+        },
+      };
+    }
+    if (newLayout["xaxis.autorange"] === true) {
+      mergedLayout = {
+        ...mergedLayout,
+        xaxis: { autorange: true },
+      };
+    }
+    if (newLayout["yaxis.autorange"] === true) {
+      lastLinearYAxisRange = [];
+      const axisType = this.state.layout && this.state.layout.yaxis &&
+        this.state.layout.yaxis.type === 'log' ? "log" : "linear";
+      mergedLayout = {
+        ...mergedLayout,
+        yaxis: { autorange: true, type: axisType },
+      };
+    }
+    this.setState({ layout: mergedLayout, lastLinearYAxisRange });
   };
 
   handleMetricsSelectChange = (metricValues, metricLabels, { triggerValue }) => {
@@ -133,7 +283,6 @@ export class MetricsPlotPanel extends React.Component {
       showPoint,
       selectedXAxis,
       selectedMetricKeys,
-      yAxisLogScale,
       lineSmoothness,
     } = this.state;
     const metrics = this.getMetrics();
@@ -167,8 +316,9 @@ export class MetricsPlotPanel extends React.Component {
             showPoint={showPoint}
             chartType={chartType}
             isComparing={MetricsPlotPanel.isComparing(location.search)}
-            yAxisLogScale={yAxisLogScale}
             lineSmoothness={lineSmoothness}
+            extraLayout={this.state.layout}
+            onLayoutChange={this.handleLayoutChange}
           />
         </RequestStateWrapper>
       </div>

mlflow/server/js/src/components/MetricsPlotView.js
@@ -19,8 +19,9 @@ export class MetricsPlotView extends React.Component {
     showPoint: PropTypes.bool.isRequired,
     chartType: PropTypes.string.isRequired,
     isComparing: PropTypes.bool.isRequired,
-    yAxisLogScale: PropTypes.bool.isRequired,
     lineSmoothness: PropTypes.number,
+    extraLayout: PropTypes.object,
+    onLayoutChange: PropTypes.func.isRequired,
   };
 
   static getLineLegend = (metricKey, runDisplayName, isComparing) => {
@@ -40,7 +41,7 @@ export class MetricsPlotView extends React.Component {
   };
 
   getPlotPropsForLineChart = () => {
-    const { metrics, xAxis, showPoint, yAxisLogScale, lineSmoothness, isComparing } = this.props;
+    const { metrics, xAxis, showPoint, lineSmoothness, isComparing } = this.props;
     const data = metrics.map((metric) => {
       const { metricKey, runDisplayName, history } = metric;
       const isSingleHistory = history.length === 0;
@@ -60,17 +61,16 @@ export class MetricsPlotView extends React.Component {
       };
     });
     const props = { data };
-    if (yAxisLogScale) {
-      props.layout = {
-        yaxis: { type: 'log', autorange: true },
-      };
-    }
+    props.layout = {
+      ...props.layout,
+      ...this.props.extraLayout,
+    };
     return props;
   };
 
   getPlotPropsForBarChart = () => {
     /* eslint-disable no-param-reassign */
-    const { runUuids, runDisplayNames, yAxisLogScale } = this.props;
+    const { runUuids, runDisplayNames } = this.props;
 
     // A reverse lookup of `metricKey: { runUuid: value, metricKey }`
     const historyByMetricKey = this.props.metrics.reduce((map, metric) => {
@@ -90,7 +90,6 @@ export class MetricsPlotView extends React.Component {
     );
 
     const sortedMetricKeys = arrayOfHistorySortedByMetricKey.map((history) => history.metricKey);
-
     const data = runUuids.map((runUuid, i) => ({
       name: Utils.truncateString(runDisplayNames[i], MAX_RUN_NAME_DISPLAY_LENGTH),
       x: sortedMetricKeys,
@@ -100,13 +99,15 @@ export class MetricsPlotView extends React.Component {
 
     const layout = { barmode: 'group' };
     const props = { data, layout };
-    if (yAxisLogScale) {
-      props.layout.yaxis = { type: 'log', autorange: true };
-    }
+    props.layout = {
+      ...props.layout,
+      ...this.props.extraLayout,
+    };
     return props;
   };
 
   render() {
+    const { onLayoutChange } = this.props;
     const plotProps =
       this.props.chartType === CHART_TYPE_BAR
         ? this.getPlotPropsForBarChart()
@@ -116,8 +117,11 @@ export class MetricsPlotView extends React.Component {
         <Plot
           {...plotProps}
           useResizeHandler
+          onRelayout={(newLayout) => {
+            onLayoutChange(newLayout);
+          }}
           style={{ width: '100%', height: '100%' }}
-          layout={{ ...plotProps.layout, ...{ autosize: true } }}
+          layout={_.cloneDeep(plotProps.layout)}
           config={{
             displaylogo: false,
             scrollZoom: true,

mlflow/server/js/src/components/ExperimentView.test.js
@@ -42,6 +42,7 @@ const getExperimentViewMock = () => {
     handleLoadMoreRuns={jest.fn()}
     orderByKey={null}
     orderByAsc={false}
+    setExperimentTagApi={jest.fn()}
     location={{pathname: "/"}}
   />);
 };

mlflow/server/js/src/components/MetricsPlotPanel.test.js
@@ -167,4 +167,42 @@ describe('unit tests', () => {
     metrics[0].history.sort(Utils.compareByStepAndTimestamp); // sort in place before comparison
     expect(instance.getMetrics()).toEqual(metrics);
   });
+
+  test('handleYAxisLogScale properly converts layout between log and linear scales', () => {
+    const props = {
+      ...minimalPropsForLineChart,
+    };
+    wrapper = shallow(<MetricsPlotPanel {...props} />);
+    wrapper.setState({ layout: {xaxis: {range: [2, 4]}, yaxis: {range: [1, 100]}} });
+    instance = wrapper.instance();
+    instance.handleYAxisLogScaleChange(true);
+    expect(instance.state.layout).toEqual({xaxis: {range: [2, 4]},
+      yaxis: {range: [0, 2], type: "log"}});
+    instance.handleYAxisLogScaleChange(false);
+    expect(instance.state.layout).toEqual(
+        {xaxis: {range: [2, 4]}, yaxis: {range: [1, 100], type: "linear"}});
+    // Test converting to & from log scale for a layout with negative Y axis
+    wrapper.setState({ layout: {xaxis: {range: [-5, 5]}, yaxis: {range: [-3, 6]}} });
+    instance.handleYAxisLogScaleChange(true);
+    expect(instance.state.layout).toEqual({xaxis: {range: [-5, 5]},
+      yaxis: {autorange: true, type: "log"}});
+    instance.handleYAxisLogScaleChange(false);
+    expect(instance.state.layout).toEqual(
+        {xaxis: {range: [-5, 5]}, yaxis: {range: [-3, 6], type: "linear"}});
+    // Test converting to & from log scale for a layout with zero-valued Y axis bound
+    wrapper.setState({ layout: {xaxis: {range: [-5, 5]}, yaxis: {range: [0, 6]}} });
+    instance.handleYAxisLogScaleChange(true);
+    expect(instance.state.layout).toEqual({xaxis: {range: [-5, 5]},
+      yaxis: {autorange: true, type: "log"}});
+    instance.handleYAxisLogScaleChange(false);
+    expect(instance.state.layout).toEqual(
+        {xaxis: {range: [-5, 5]}, yaxis: {range: [0, 6], type: "linear"}});
+    // Test converting to & from log scale for an empty layout (e.g. a layout without any
+    // user-specified zoom)
+    wrapper.setState({ layout: {} });
+    instance.handleYAxisLogScaleChange(true);
+    expect(instance.state.layout).toEqual({yaxis: {type: "log", autorange: true}});
+    instance.handleYAxisLogScaleChange(false);
+    expect(instance.state.layout).toEqual({yaxis: {type: "linear", autorange: true}});
+  });
 });

mlflow/server/js/src/components/MetricsPlotView.test.js
@@ -94,6 +94,9 @@ describe('unit tests', () => {
       isComparing: false,
       yAxisLogScale: false,
       lineSmoothness: 0,
+      onLayoutChange: jest.fn(),
+      onDoubleClick: jest.fn(),
+      onClick: jest.fn(),
     };
     minimalPropsForBarChart = {
       ...minimalPropsForLineChart,
@@ -138,6 +141,7 @@ describe('unit tests', () => {
           "marker": {"opacity": 0},
         },
       ],
+      layout: {},
     });
   });
 

mlflow/server/js/src/components/ParallelCoordinatesPlotPanel.test.js
@@ -14,6 +14,8 @@ describe('unit tests', () => {
       runUuids: ['runUuid_0', 'runUuid_1'],
       sharedParamKeys: ['param_0', 'param_1'],
       sharedMetricKeys: ['metric_0', 'metric_1'],
+      allParamKeys: ['param_0', 'param_1', 'param_2'],
+      allMetricKeys: ['metric_0', 'metric_1', 'metric_2'],
     };
   });
 

tests/generate_ui_test_data.py
@@ -49,6 +49,20 @@ if __name__ == '__main__':
             log_params(parameters)
             log_metrics(metrics)
 
+    # Runs with multiple values for a single metric so that we can QA the time-series metric
+    # plot
+    for i in range(3):
+        with mlflow.start_run():
+            for j in range(10):
+                sign = random.choice([-1, 1])
+                mlflow.log_metric("myReallyLongTimeSeriesMetricName-abcdefghijklmnopqrstuvwxyz",
+                                  random.random() * sign)
+                mlflow.log_metric("Another Timeseries Metric", rand() * sign)
+                mlflow.log_metric("Yet Another Timeseries Metric", rand() * sign)
+            if i == 0:
+                mlflow.log_metric("Special Timeseries Metric", rand() * sign)
+            mlflow.log_metric("Bar chart metric", rand())
+
     # Big parameter values
     with mlflow.start_run(run_name='ipython'):
         parameters = {

commit 5a19b8c42d7b633e5be64a812865e9a5450a6593

Fix handleLayoutChange to update dragmode (#2459)
Fixes a bug introduced in #2408, namely modifying handleLayoutChange in MetricsPlotPanel to update dragmode of the plot.

mlflow/server/js/src/components/MetricsPlotPanel.js
@@ -51,6 +51,7 @@ export class MetricsPlotPanel extends React.Component {
       // and then back to linear scale (we save the initial negative linear y-axis bounds so
       // that we can restore them when converting from log back to linear scale)
       lastLinearYAxisRange: [],
+      layout: {},
     };
     this.loadMetricHistory(this.props.runUuids, this.state.selectedMetricKeys);
   }
@@ -220,46 +221,58 @@ export class MetricsPlotPanel extends React.Component {
   handleLayoutChange = (newLayout) => {
     // Unfortunately, we need to parse out the x & y axis range changes from the onLayout event...
     // see https://plot.ly/javascript/plotlyjs-events/#update-data
-    const newXRange0 = newLayout["xaxis.range[0]"];
-    const newXRange1 = newLayout["xaxis.range[1]"];
-    const newYRange0 = newLayout["yaxis.range[0]"];
-    const newYRange1 = newLayout["yaxis.range[1]"];
+    const {
+      "xaxis.range[0]": newXRange0,
+      "xaxis.range[1]": newXRange1,
+      "yaxis.range[0]": newYRange0,
+      "yaxis.range[1]": newYRange1,
+      "xaxis.autorange": xAxisAutorange,
+      "yaxis.autorange": yAxisAutorange,
+      "yaxis.showspikes": yAxisShowSpikes,
+      "xaxis.showspikes": xAxisShowSpikes,
+      ...restFields
+    } = newLayout;
+
     let mergedLayout = {
       ...this.state.layout,
+      ...restFields,
     };
     let lastLinearYAxisRange = [...this.state.lastLinearYAxisRange];
+
+    // Set fields for x axis
+    const newXAxis = mergedLayout.xaxis || {};
     if (newXRange0 !== undefined && newXRange1 !== undefined) {
-      mergedLayout = {
-        ...mergedLayout,
-        xaxis: {
-          range: [newXRange0, newXRange1],
-        },
-      };
+      newXAxis.range = [newXRange0, newXRange1];
+      newXAxis.autorange = false;
     }
+    if (xAxisShowSpikes) {
+      newXAxis.showspikes = true;
+    }
+    if (xAxisAutorange) {
+      newXAxis.autorange = true;
+    }
+    // Set fields for y axis
+    const newYAxis = mergedLayout.yaxis || {};
     if (newYRange0 !== undefined && newYRange1 !== undefined) {
-      lastLinearYAxisRange = [];
-      mergedLayout = {
-        ...mergedLayout,
-        yaxis: {
-          range: [newYRange0, newYRange1],
-        },
-      };
+      newYAxis.range = [newYRange0, newYRange1];
+      newYAxis.autorange = false;
     }
-    if (newLayout["xaxis.autorange"] === true) {
-      mergedLayout = {
-        ...mergedLayout,
-        xaxis: { autorange: true },
-      };
+    if (yAxisShowSpikes) {
+      newYAxis.showspikes = true;
     }
-    if (newLayout["yaxis.autorange"] === true) {
+    if (yAxisAutorange) {
       lastLinearYAxisRange = [];
       const axisType = this.state.layout && this.state.layout.yaxis &&
         this.state.layout.yaxis.type === 'log' ? "log" : "linear";
-      mergedLayout = {
-        ...mergedLayout,
-        yaxis: { autorange: true, type: axisType },
-      };
+      newYAxis.autorange = true;
+      newYAxis.type = axisType;
     }
+    // Merge new X & Y axis info into layout
+    mergedLayout = {
+      ...mergedLayout,
+      xaxis: newXAxis,
+      yaxis: newYAxis,
+    };
     this.setState({ layout: mergedLayout, lastLinearYAxisRange });
   };
 

mlflow/server/js/src/components/MetricsPlotView.js
@@ -117,9 +117,7 @@ export class MetricsPlotView extends React.Component {
         <Plot
           {...plotProps}
           useResizeHandler
-          onRelayout={(newLayout) => {
-            onLayoutChange(newLayout);
-          }}
+          onRelayout={onLayoutChange}
           style={{ width: '100%', height: '100%' }}
           layout={_.cloneDeep(plotProps.layout)}
           config={{