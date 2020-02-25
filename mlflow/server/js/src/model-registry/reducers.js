31d633a8882bd4d3fc16407fde8f090aec955f3d
Aaron Davidson <aaron@databricks.com>
2020-02-25 08:57:10 -0800

    OK

--- mlflow/server/js/src/model-registry/reducers.js
+++ mlflow/server/js/src/model-registry/reducers.js
@@ -1,8 +1,8 @@
 import {
   LIST_REGISTRED_MODELS,
   SEARCH_MODEL_VERSIONS,
-  GET_REGISTERED_MODEL_DETAILS,
-  GET_MODEL_VERSION_DETAILS,
+  GET_REGISTERED_MODEL,
+  GET_MODEL_VERSION,
   DELETE_MODEL_VERSION,
   DELETE_REGISTERED_MODEL,
 } from './actions';
@@ -12,7 +12,7 @@ import _ from 'lodash';
 const modelByName = (state = {}, action) => {
   switch (action.type) {
     case fulfilled(LIST_REGISTRED_MODELS): {
-      const detailedModels = action.payload.registered_models_detailed;
+      const detailedModels = action.payload.registered_models;
       const models = detailedModels && detailedModels.map(inlineModel);
       const nameToModelMap = {};
       if (models) {
@@ -22,8 +22,8 @@ const modelByName = (state = {}, action) => {
         ...nameToModelMap,
       };
     }
-    case fulfilled(GET_REGISTERED_MODEL_DETAILS): {
-      const detailedModel = action.payload.registered_model_detailed;
+    case fulfilled(GET_REGISTERED_MODEL): {
+      const detailedModel = action.payload.registered_model;
       const inlinedModel = detailedModel && inlineModel(detailedModel);
       const { modelName } = action.meta;
       const modelWithUpdatedMetadata = {
@@ -47,8 +47,8 @@ const modelByName = (state = {}, action) => {
 // 2-levels lookup for model version indexed by (modelName, version)
 const modelVersionsByModel = (state = {}, action) => {
   switch (action.type) {
-    case fulfilled(GET_MODEL_VERSION_DETAILS): {
-      const modelVersion = action.payload.model_version_detailed;
+    case fulfilled(GET_MODEL_VERSION): {
+      const modelVersion = action.payload.model_version;
       const inlinedModelVersion = inlineModelVersion(modelVersion);
       const { modelName } = action.meta;
       const updatedMap = {
@@ -61,7 +61,7 @@ const modelVersionsByModel = (state = {}, action) => {
       };
     }
     case fulfilled(SEARCH_MODEL_VERSIONS): {
-      const modelVersions = action.payload.model_versions_detailed;
+      const modelVersions = action.payload.model_versions;
       if (!modelVersions) {
         return state;
       }
@@ -116,7 +116,7 @@ export const getAllModelVersions = (state) => {
 
 // Inline the `name` field nested inside `registered_model` and `version` in `model_version`
 const inlineModel = (model) => {
-  const { registered_model, latest_versions } = model;
+  const { latest_versions } = model;
   return {
     ...model,
     latest_versions: latest_versions && latest_versions.map(inlineModelVersion),
