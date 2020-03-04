d0149155314d89cac3c959672c999b2e8a83696e
Xiangrui Meng <meng@databricks.com>
2020-03-04 08:21:51 -0800

    cross test anaconda 2019.03 and 2019.10

--- travis/stage-python3.sh
+++ travis/stage-python3.sh
@@ -10,7 +10,10 @@ then
     ./travis/run-small-python-tests.sh && ./test-generate-protos.sh
   else
     ./travis/run-large-python-tests.sh
-    ./travis/test-anaconda-compatibility.sh
+    ./travis/test-anaconda-compatibility.sh "anaconda3:2019.10"
+    ./travis/test-anaconda-compatibility.sh "anaconda:2019.10"
+    ./travis/test-anaconda-compatibility.sh "anaconda3:2019.03"
+    ./travis/test-anaconda-compatibility.sh "anaconda:2019.03"
   fi
 fi
 CHANGED_FILES=$(git diff --name-only master..HEAD | grep "tests/examples\|examples") || true
