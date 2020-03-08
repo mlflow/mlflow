5812c0c076a54b96ab65d38f541e688dc94928fc
Ambika Santhosh <ambika.santhosh@genpact.com>
2020-03-08 22:03:48 +0530

    Fix curl ssl issue and skip testcase for python2.7
    Signed-off-by: Ambika Santhosh <ambika.santhosh@genpact.com>

--- mlflow/models/docker_utils.py
+++ mlflow/models/docker_utils.py
@@ -17,7 +17,7 @@ FROM ubuntu:18.04
 {install_os_dependencies}
 
 # Download and setup miniconda
-RUN curl -k https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh >> miniconda.sh
+RUN curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh >> miniconda.sh
 RUN bash ./miniconda.sh -b -p /miniconda; rm ./miniconda.sh;
 ENV PATH="/miniconda/bin:$PATH"
 
@@ -49,6 +49,8 @@ PYTHON_DEP = """
 RUN apt-get -y update && apt-get install -y --no-install-recommends \
          curl \
          nginx \
+         ca-certificates \
+         bzip2 \
     && rm -rf /var/lib/apt/lists/*
 """
 
