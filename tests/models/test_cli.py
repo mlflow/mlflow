cb7c5ff7ab02c191e28f47eb44c37058590a6cde
Ambika Santhosh <ambika.santhosh@genpact.com>
2020-03-08 08:59:34 +0530

    Add new test for docker build with --no-conda option

--- tests/models/test_cli.py
+++ tests/models/test_cli.py
@@ -306,6 +306,19 @@ def test_build_docker(iris_data, sk_model):
 
 
 @pytest.mark.large
+def test_build_docker_optimize(iris_data, sk_model):
+    with mlflow.start_run() as active_run:
+        mlflow.sklearn.log_model(sk_model, "model")
+        model_uri = "runs:/{run_id}/model".format(run_id=active_run.info.run_id)
+    x, _ = iris_data
+    df = pd.DataFrame(x)
+    image_name = pyfunc_build_image(model_uri, extra_args=["--install-mlflow", "--no-conda"])
+    host_port = get_safe_port()
+    scoring_proc = pyfunc_serve_from_docker_image(image_name, host_port)
+    _validate_with_rest_endpoint(scoring_proc, host_port, df, x, sk_model)
+
+
+@pytest.mark.large
 def test_build_docker_with_env_override(iris_data, sk_model):
     with mlflow.start_run() as active_run:
         mlflow.sklearn.log_model(sk_model, "model")
