41c73ffb7b28c44b13c752c44b4caadfc53b8f3d
Juntai Zheng <juntai.zheng@databricks.com>
2020-03-09 17:18:39 -0700

    finished tests

--- mlflow/server/js/src/model-registry/components/ModelView.test.js
+++ mlflow/server/js/src/model-registry/components/ModelView.test.js
@@ -1,24 +1,27 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { ModelView } from './ModelView.js'
 import React from "react";
-
+import {BrowserRouter} from "react-router-dom";
+import {getCompareModelVersionsPageRoute} from "../routes";
 
 
 describe('unconnected tests', () => {
   let wrapper;
   let minimumProps;
+  let historyMock;
 
   beforeEach(() => {
+    historyMock = jest.fn();
     minimumProps = {
       model: {
         name: 'test_name',
         creation_timestamp: 123456789,
         last_updated_timestamp: 132456789
       },
-      modelVersions: [{'current_stage': 'None'}],
+      modelVersions: [ {'current_stage': 'None'} ],
       handleEditDescription: jest.fn(),
       handleDelete: jest.fn(),
-      history: {},
+      history: { push: historyMock },
     };
   });
 
@@ -27,14 +30,31 @@ describe('unconnected tests', () => {
     expect(wrapper.length).toBe(1);
   });
 
-  test('compare button is disabled when no/1 runs selected, active when 2+ runs selected', () => {
-    wrapper = shallow(<ModelView {...minimumProps}/>);
-    console.log(wrapper.debug());
-    // debugger;
-    expect(wrapper.find('btn-primary')).toEqual(true);
-    wrapper.setState({
-      runsSelected: {'run_id_1': 'version_1'}
-    })
+  test('compare button is disabled when no/1 run selected, active when 2+ runs selected', () => {
+    wrapper = mount(
+      <BrowserRouter>
+        <ModelView {...minimumProps}/>
+      </BrowserRouter>
+    );
 
+    expect(wrapper.find('.btn').length).toBe(1);
+    expect(wrapper.find('.btn').props().disabled).toEqual(true);
+
+    wrapper.find(ModelView).instance().setState({
+      runsSelected: {'run_id_1': 'version_1'}
+    });
+    wrapper.update();
+    expect(wrapper.find('.btn').props().disabled).toEqual(true);
+
+    let twoRunsSelected = {'run_id_1': 'version_1',
+                           'run_id_2': 'version_2'};
+    wrapper.find(ModelView).instance().setState({
+      runsSelected: twoRunsSelected
+    });
+    wrapper.update();
+    expect(wrapper.find('.btn').props().disabled).toEqual(false);
+
+    wrapper.find('.btn').simulate('click');
+    expect(historyMock).toHaveBeenCalledWith(getCompareModelVersionsPageRoute(minimumProps['model']['name'], twoRunsSelected))
   });
 });
