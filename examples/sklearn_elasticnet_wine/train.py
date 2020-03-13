13424790ed0cf4da7263ae909cb95fdeaa23be53
Netanel Malka <netanel246@gmail.com>
2020-03-13 09:41:10 +0200

    Added usage of the model registry to the log_model function,  along with docs about more detailed use cases

--- examples/sklearn_elasticnet_wine/train.py
+++ examples/sklearn_elasticnet_wine/train.py
@@ -72,4 +72,15 @@ if __name__ == "__main__":
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
-        mlflow.sklearn.log_model(lr, "model")
+        tracking_url_type_store = mlflow.get_tracking_uri().split(":")[0]
+
+        # Model registry does not work with file store
+        if tracking_url_type_store != "file":
+
+            # Register the model
+            # There are other ways to use the Model Registry, which depends on the use case,
+            # please refer to the doc for more information:
+            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+            mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetWineModel")
+        else:
+            mlflow.sklearn.log_model(lr, "model")
