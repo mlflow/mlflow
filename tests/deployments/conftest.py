57eb48fc0c81f0324da71d8abb7ff30a23eb87fc
hhsecond <sherin@tensorwerk.com>
2020-03-14 01:04:34 +0530

    python api tests

---
+++ tests/deployments/conftest.py
@@ -0,0 +1,51 @@
+from mlflow.deployments.plugin_manager import DeploymentPlugins
+from mlflow.deployments import BasePlugin
+import pytest
+
+
+f_model_uri = 'fake_model_uri'
+f_deployment_id = 'fake_deployment_id'
+f_flavor = 'fake_flavor'
+
+
+class FakePlugin(BasePlugin):
+    def create(self, model_uri, flavor=None, **kwargs):
+        return {'deployment_id': f_deployment_id, 'flavor': flavor}
+
+    def delete(self, deployment_id, **kwargs):
+        return None
+
+    def update(self, deployment_id, model_uri=None, rollback=False, **kwargs):
+        return None
+
+    def list(self, **kwargs):
+        if kwargs.get('raiseError'):
+            raise RuntimeError('Error requested')
+        return [f_deployment_id]
+
+    def describe(self, deployment_id, **kwargs):
+        return {'key1': 'val1', 'key2': 'val2'}
+
+
+def custom_getitem_for_plugin_store(self, name):
+    return FakePlugin()
+
+
+def custom_register_entrypoints_for_plugin_store(fn):
+    def wrapper(self, *args, **kwargs):
+        if not hasattr(self, 'entrypoint_calling_count'):
+            self.entrypoint_calling_count = 0
+        else:
+            self.entrypoint_calling_count += 1
+        return fn(self, *args, **kwargs)
+    return wrapper
+
+
+@pytest.fixture()
+def patched_plugin_store(monkeypatch):
+    monkeypatch.setattr(DeploymentPlugins, '__getitem__', custom_getitem_for_plugin_store)
+    monkeypatch.setattr(DeploymentPlugins, 'register_entrypoints',
+                        custom_register_entrypoints_for_plugin_store(
+                            DeploymentPlugins.register_entrypoints))
+    yield None
+
---
+++ tests/deployments/test_deployments.py
@@ -0,0 +1,85 @@
+import pytest
+from mlflow import deployments
+from mlflow.deployments.plugin_manager import DeploymentPlugins
+
+
+f_model_uri = 'fake_model_uri'
+f_deployment_id = 'fake_deployment_id'
+f_flavor = 'fake_flavor'
+f_target = 'fake_target'
+
+
+def test_create_success(patched_plugin_store):
+    ret = deployments.create(f_target, f_model_uri, f_flavor)
+    assert isinstance(ret, dict)
+    assert ret['deployment_id'] == f_deployment_id
+    assert ret['flavor'] == f_flavor
+
+    ret2 = deployments.create(f_target, f_model_uri)
+    assert ret['deployment_id'] == ret2['deployment_id']
+    assert ret2['flavor'] is None
+
+
+def test_delete_success(patched_plugin_store):
+    ret = deployments.delete(f_target, f_deployment_id)
+    assert ret is None
+
+
+def test_update_success(patched_plugin_store):
+    ret = deployments.update(f_target, f_deployment_id)
+    assert ret is None
+
+
+def test_update_both_rollback_and_model_uri(patched_plugin_store):
+    with pytest.raises(RuntimeError):
+        deployments.update(f_target, f_deployment_id, f_model_uri, rollback=True)
+
+
+def test_missing_arguments(patched_plugin_store):
+    with pytest.raises(TypeError):
+        deployments.create(f_target)
+
+
+def test_list_success(patched_plugin_store):
+    ret = deployments.list(f_target)
+    assert ret[0] == f_deployment_id
+
+
+def test_describe_success(patched_plugin_store):
+    ret = deployments.describe(f_target, f_deployment_id)
+    assert ret['key1'] == 'val1'
+
+
+def test_invalid_interface(patched_plugin_store):
+    with pytest.raises(AttributeError):
+        deployments.wrong_interface(f_target)
+
+
+def test_plugin_not_inherited_from_BasePlugin():
+    class DummyPlugin:
+        ...
+
+    dummy_plugin = DummyPlugin()
+    plugin_manager = DeploymentPlugins()
+    plugin_manager.register('dummy',  dummy_plugin)
+    with pytest.raises(RuntimeError):
+        plugin_manager.register_entrypoints()
+
+
+def test_entrypoints_not_reloading(patched_plugin_store):
+    from mlflow.deployments.interface import plugin_store
+    plugin_store._has_plugins_loaded = False  # Turning the guard off
+    deployments.list(f_target)  # triggering the reload of entrypoints
+    assert plugin_store.entrypoint_calling_count == 0  # count is set to 0
+    deployments.list(f_target)  # triggering the reload
+    assert plugin_store.entrypoint_calling_count == 0  # Guard autoset
+    plugin_store._has_plugins_loaded = False  # Turning the guard again off
+    deployments.list(f_target)  # triggering the reload
+    assert plugin_store.entrypoint_calling_count == 1  # count increased
+
+
+def test_plugin_raising_error(patched_plugin_store):
+    with pytest.raises(RuntimeError):
+        ret = deployments.list(f_target, raiseError=True)  # special case to raise error
+
+
