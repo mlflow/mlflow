2ffebea3ba44f9fa47451e59a78e65239684af08
Mani Parkhe <mani@databricks.com>
2020-03-17 08:41:38 -0700

    Fixing py docs: arguments for client APIs (#2593)

--- mlflow/tracking/client.py
+++ mlflow/tracking/client.py
@@ -496,9 +496,7 @@ class MlflowClient(object):
 
         :param name: Registered model name.
         :param version: Registered model version.
-        :param new_stage: New desired stage for this model version.
-        :param archive_existing_versions: If this flag is set, all existing model
-               versions in the stage will be atomically moved to the "archived" stage.
+        :param stage: New desired stage for this model version.
 
         :return: A single :py:class:`mlflow.entities.model_registry.ModelVersion` object.
         """
