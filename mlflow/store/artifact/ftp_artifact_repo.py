6dfdef0f7dbfda746c045b2574c45e6528600bda
Andy Chow <andy.chow@databricks.com>
2020-04-09 15:20:23 -0700

    Update methodology for fixing the problem

--- mlflow/store/artifact/ftp_artifact_repo.py
+++ mlflow/store/artifact/ftp_artifact_repo.py
@@ -74,29 +74,20 @@ class FTPArtifactRepository(ArtifactRepository):
                 ftp.storbinary('STOR ' + os.path.basename(local_file), f)
 
     def log_artifacts(self, local_dir, artifact_path=None):
-        dest_path = self.path
-        if artifact_path and artifact_path != 'model':
-            dest_path = posixpath.join(self.path, artifact_path)
-
-        dest_path = posixpath.join(
-            dest_path, os.path.split(local_dir)[1])
-        dest_path_re = os.path.split(local_dir)[1]
-        if artifact_path and artifact_path != 'model':
-            dest_path_re = posixpath.join(artifact_path, os.path.split(local_dir)[1])
-
-        local_dir = os.path.abspath(local_dir)
-        for (root, _, filenames) in os.walk(local_dir):
-            upload_path = dest_path
-            if root != local_dir:
-                rel_path = os.path.relpath(root, local_dir)
+        artifact_path = artifact_path or ''
+        for (dirpath, _, filenames) in os.walk(local_dir):
+            artifact_subdir = artifact_path
+            if dirpath != local_dir:
+                rel_path = os.path.relpath(dirpath, local_dir)
                 rel_path = relative_path_to_artifact_path(rel_path)
-                upload_path = posixpath.join(dest_path_re, rel_path)
+                artifact_subdir = posixpath.join(artifact_path, rel_path)
             if not filenames:
                 with self.get_ftp_client() as ftp:
-                    self._mkdir(ftp, posixpath.join(self.path, upload_path))
-            for f in filenames:
-                if os.path.isfile(os.path.join(root, f)):
-                    self.log_artifact(os.path.join(root, f), upload_path)
+                    self._mkdir(ftp, posixpath.join(self.path, artifact_path))
+            for name in filenames:
+                file_path = os.path.join(dirpath, name)
+                if os.path.isfile(file_path):
+                    self.log_artifact(file_path, artifact_subdir)
 
     def _is_directory(self, artifact_path):
         artifact_dir = self.path
