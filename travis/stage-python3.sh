172d5ed0d4eba18b28306d7876270fe9101f94fb
tomasatdatabricks <33237569+tomasatdatabricks@users.noreply.github.com>
2020-04-15 15:08:32 -0700

    Fix travis script. Was not failing where some small tests were broken. (#2706)

--- travis/stage-python3.sh
+++ travis/stage-python3.sh
@@ -7,7 +7,8 @@ then
      echo "skipping this step on windows."
   elif [[ "$TRAVIS_BUILD_STAGE_NAME" == "Small" ]]
   then
-    ./travis/run-small-python-tests.sh && ./test-generate-protos.sh
+    ./travis/run-small-python-tests.sh
+    ./test-generate-protos.sh
   else
     ./travis/run-large-python-tests.sh
     ./travis/test-anaconda-compatibility.sh "anaconda3:2020.02"
