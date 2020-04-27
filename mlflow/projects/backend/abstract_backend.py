ecc4c683d342a2bbbf0e1d23f287525d4213f434
Sid Murching <sid.murching@databricks.com>
2020-04-27 00:15:29 -0700

    Fix lint

--- mlflow/projects/backend/abstract_backend.py
+++ mlflow/projects/backend/abstract_backend.py
@@ -3,26 +3,32 @@ from abc import abstractmethod, ABCMeta
 
 class AbstractBackend():
     """
-    Abstract class for MLflow Project Execution Backend
-    A backend must implement the run method to launch a new job
+    Abstract plugin class defining the interface needed to execute MLflow projects. You can define
+    subclasses of ``AbstractBackend`` and expose them as third-party plugins to enable running
+    MLflow projects against custom execution backends (e.g. to run projects against your team's
+    in-house cluster or job scheduler). See `MLflow Plugins <../../plugins.html>`_ for more
+    information.
     """
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def run(self, run_id, experiment_id, project_uri, entry_point, params,
-            version, backend_config, tracking_store_uri):
+    def run(self, project_uri, entry_point, params,
+            version, backend_config, tracking_uri, experiment_id):
         """
-        Submit an entrypoint. It must returns a SubmittedRun object to track the execution
+        Submit an entrypoint. It must return a SubmittedRun object to track the execution
 
-        :param run_id: Current MLflow run run_id. Everything will registred in this run.
-        :param experiment_id: Experiment id where to add the run
-        :param project_uri: URI to the project (could be a local or git URI).
-                    This is the parameter given to mlflow run command.
-        :param entry_point: name of the entry point to execute.
+        :param project_uri: URI of the project to execute, e.g. a local filesystem path
+               or a Git repository URI like https://github.com/mlflow/mlflow-example
+        :param entry_point: Entry point to run within the project.
         :param params: Dict of parameters to pass to the entry point
-        :param version: for Git-based projects, either a commit hash or a branch name.
-        :param backend_config: Dict to pass parameters to the backend
-        :param tracking_store_uri: Uri to the tracking store
+        :param version: For git-based projects, either a commit hash or a branch name.
+        :param backend_config: A dictionary, or a path to a JSON file (must end in '.json'), which
+                               will be passed as config to the backend. The exact content which
+                               should be provided is different for each execution backend and is
+                               documented at https://www.mlflow.org/docs/latest/projects.html.
+        :param tracking_uri: URI of tracking server against which to log run information related
+                             to project execution.
+        :param experiment_id: ID of experiment under which to launch the run.
 
         :return: A :py:class:`mlflow.projects.SubmittedRun`. This function is expected to run
                  the project asynchronously, i.e. it should trigger project execution and then
