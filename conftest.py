7b8121c7786952a586b35f38e2b41c415ad0335b
harupy <hkawamura0130@gmail.com>
2020-04-29 00:30:38 +0900

    Add --ignore-flavors option for pytest

--- conftest.py
+++ conftest.py
@@ -1,3 +1,6 @@
+import os
+
+
 def pytest_addoption(parser):
     parser.addoption('--large-only', action='store_true', dest="large_only",
                      default=False, help="Run only tests decorated with 'large' annotation")
@@ -9,6 +12,8 @@ def pytest_addoption(parser):
                      default=False, help="Run tests decorated with 'requires_ssh' annotation. "
                                          "These tests require keys to be configured locally "
                                          "for SSH authentication.")
+    parser.addoption("--ignore-flavors", action='store_true', dest="ignore_flavors",
+                     default=False, help="Ignore tests for model flavors.")
 
 
 def pytest_configure(config):
@@ -25,3 +30,35 @@ def pytest_configure(config):
         markexpr.append('not requires_ssh')
     if len(markexpr) > 0:
         setattr(config.option, 'markexpr', " and ".join(markexpr))
+
+
+def pytest_ignore_collect(path, config):
+    if not config.getoption("--ignore-flavors"):
+        return False
+
+    flavors_to_ignore = [
+        "tests/examples",
+        "tests/h2o",
+        "tests/keras",
+        "tests/pytorch",
+        "tests/pyfunc",
+        "tests/sagemaker",
+        "tests/sklearn",
+        "tests/spark",
+        "tests/tensorflow",
+        "tests/azureml",
+        "tests/onnx",
+        "tests/keras_autolog",
+        "tests/tensorflow_autolog",
+        "tests/gluon",
+        "tests/gluon_autolog",
+        "tests/xgboost",
+        "tests/lightgbm",
+        "tests/spacy",
+        "tests/spark_autologging",
+        "tests/models",
+    ]
+
+    relpath = os.path.relpath(str(path), config.rootdir)
+
+    return relpath in flavors_to_ignore
