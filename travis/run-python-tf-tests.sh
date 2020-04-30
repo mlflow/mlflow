df10afbe774226e7bb070094e45a61432b255d4f
Juntai Zheng <juntai.zheng@databricks.com>
2020-04-30 16:11:11 -0700

    pin tensorflow

--- travis/run-python-tf-tests.sh
+++ travis/run-python-tf-tests.sh
@@ -6,6 +6,7 @@ err=0
 trap 'err=1' ERR
 export MLFLOW_HOME=$(pwd)
 
+pip install 'tensorflow==1.15.2'
 pytest --verbose tests/keras --large
 pytest --verbose tests/tensorflow/test_tensorflow_model_export.py --large
 pytest --verbose tests/tensorflow_autolog/test_tensorflow_autolog.py --large
