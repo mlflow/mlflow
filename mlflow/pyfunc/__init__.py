6c1af89e042fd3c0f0bc4da734ea53a5954ea622
tomasatdatabricks <tomas.nykodym@databricks.com>
2020-05-07 12:15:11 -0700

    lint

--- mlflow/pyfunc/__init__.py
+++ mlflow/pyfunc/__init__.py
@@ -275,8 +275,13 @@ class PyFuncModel(object):
     """
     MLflow 'python function' model.
 
-    Wrapper around model implementation and metadata. This class is not meant to be constructed directly. Instead, instances of this class are constructed and returned from py:func:`mlflow.pyfunc.load_model`.
-    ``model_impl`` can be any Python object that implements the `Pyfunc interface <https://mlflow.org/docs/latest/python_api/mlflow.pyfunc.html#pyfunc-inference-api>`_, and is loaded in `load_model` by invoking the appropriate method within ``loader_module``.
+    Wrapper around model implementation and metadata. This class is not meant to be constructed
+    directly. Instead, instances of this class are constructed and returned from
+    py:func:`mlflow.pyfunc.load_model`.
+
+    ``model_impl`` can be any Python object that implements the `Pyfunc interface
+    <https://mlflow.org/docs/latest/python_api/mlflow.pyfunc.html#pyfunc-inference-api>`_, and is
+    by invoking the model's ``loader_module``.
     
     ``model_meta`` is loaded by `:py:func:load_model` from the model metadata (MLmodel) file
     The metadata is loaded from the model metadata (MLmodel) file .
