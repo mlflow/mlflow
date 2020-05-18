f652081781adc57fcf538ca5ee0d6b8425a5f031
dmatrix <dmatrix@comcast.net>
2020-05-18 08:46:00 -0700

    A minor documentation fix to clarify an ambiguity in the mlflow models serve command (#2829)
    document that pyfunc and crate flavors are supported by mlflow models serve.

--- mlflow/models/cli.py
+++ mlflow/models/cli.py
@@ -33,8 +33,9 @@ def commands():
 @cli_args.INSTALL_MLFLOW
 def serve(model_uri, port, host, workers, no_conda=False, install_mlflow=False):
     """
-    Serve a model saved with MLflow by launching a webserver on the specified host and port. For
-    information about the input data formats accepted by the webserver, see the following
+    Serve a model saved with MLflow by launching a webserver on the specified host and port.
+    The command supports models with the ``python_function`` or ``crate`` (R Function) flavor.
+    For information about the input data formats accepted by the webserver, see the following
     documentation: https://www.mlflow.org/docs/latest/models.html#built-in-deployment-tools.
 
     You can make requests to ``POST /invocations`` in pandas split- or record-oriented formats.
