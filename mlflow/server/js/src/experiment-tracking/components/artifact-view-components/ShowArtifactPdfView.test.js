204523121879071328242f47a969c40c2823f9ef
Ankit Mathur <ankmathur96@gmail.com>
2020-05-19 14:22:49 -0700

    lints again

--- mlflow/server/js/src/experiment-tracking/components/artifact-view-components/ShowArtifactPdfView.test.js
+++ mlflow/server/js/src/experiment-tracking/components/artifact-view-components/ShowArtifactPdfView.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import ShowArtifactPdfView from './ShowArtifactPdfView';
-import ShowArtifactTextView from './ShowArtifactTextView';
 
 describe('ShowArtifactPdfView', () => {
   let wrapper;
