8fef42312c358c6e07db1ee030ee6b053a99bf86
harupy <hkawamura0130@gmail.com>
2020-05-21 21:24:21 +0900

    fix IconButton

--- mlflow/server/js/src/common/components/IconButton.js
+++ mlflow/server/js/src/common/components/IconButton.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'antd';
 
-export class IconButton extends Component {
-  static propTypes = {
-    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
-    style: PropTypes.object,
-    restProps: PropTypes.object,
-  };
+export const IconButton = ({ icon, className, style, ...restProps }) => {
+  return (
+    <Button type='link' className={className} style={{ padding: 0, ...style }} {...restProps}>
+      {icon}
+    </Button>
+  );
+};
 
-  render() {
-    const { style, children, ...restProps } = this.props;
-    return (
-      <Button type='link' style={{ padding: 0, ...style }} {...restProps}>
-        {children}
-      </Button>
-    );
-  }
-}
+IconButton.propTypes = {
+  icon: PropTypes.node.isRequired,
+  style: PropTypes.object,
+  className: PropTypes.string,
+  restProps: PropTypes.object,
+};
