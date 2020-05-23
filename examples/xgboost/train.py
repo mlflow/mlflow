cc1b56fe66b5c1c12542721b3fcc0f0e463ba060
harupy <hkawamura0130@gmail.com>
2020-05-23 13:27:37 +0900

    Fix xgb and lgb examples

--- examples/xgboost/train.py
+++ examples/xgboost/train.py
@@ -15,9 +15,9 @@ mpl.use('Agg')
 
 def parse_args():
     parser = argparse.ArgumentParser(description='XGBoost example')
-    parser.add_argument('--learning-rate', type=float, default=0.3,
+    parser.add_argument('--learning_rate', type=float, default=0.3,
                         help='learning rate to update step size at each boosting step (default: 0.3)')
-    parser.add_argument('--colsample-bytree', type=float, default=1.0,
+    parser.add_argument('--colsample_bytree', type=float, default=1.0,
                         help='subsample ratio of columns when constructing each tree (default: 1.0)')
     parser.add_argument('--subsample', type=float, default=1.0,
                         help='subsample ratio of the training instances (default: 1.0)')
