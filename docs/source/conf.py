e225e7f1c41eae0496a42262f8130f7a07bed11e
t-henri <51711739+t-henri@users.noreply.github.com>
2020-06-05 13:34:26 +0200

    Fix doc generation (#180)
    Co-authored-by: Theophile Henri <t.henri@criteo.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,8 @@ import os
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('.'))
 
+from languagesections import *
+
 
 # -- General configuration ------------------------------------------------
 
