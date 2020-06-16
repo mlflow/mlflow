5831295fae550e9ea56dfff374b8eb2ca9c7c035
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-06-17 06:38:11 +0900

    Fix integer experimentId to string to remove invalid-prop-type warnings. (#2940)

--- mlflow/server/js/src/experiment-tracking/components/RunView.test.js
+++ mlflow/server/js/src/experiment-tracking/components/RunView.test.js
@@ -21,7 +21,7 @@ describe('RunView', () => {
   beforeEach(() => {
     minimalProps = {
       runUuid: 'uuid-1234-5678-9012',
-      experimentId: 12345,
+      experimentId: '12345',
       getMetricPagePath: jest.fn(),
       handleSetRunTag: jest.fn(),
     };
@@ -45,8 +45,8 @@ describe('RunView', () => {
         },
         artifactsByRunUuid: { 'uuid-1234-5678-9012': new ArtifactNode(true) },
         experimentsById: {
-          12345: Experiment.fromJs({
-            experiment_id: 12345,
+          '12345': Experiment.fromJs({
+            experiment_id: '12345',
             name: 'my experiment',
             artifact_location: 'dbfs:/databricks/abc',
             lifecycle_stage: 'active',
