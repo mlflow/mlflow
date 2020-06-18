ba69f96105de14736d008b523aca38b60817908e
Corey Zumar <corey.zumar@databricks.com>
2020-06-18 12:15:48 -0700

    Revert test tweaks

--- travis/run-python-tf-tests.sh
+++ travis/run-python-tf-tests.sh
@@ -11,7 +11,7 @@ pytest --verbose tests/tensorflow/test_tensorflow_model_export.py --large
 pytest --verbose tests/tensorflow_autolog/test_tensorflow_autolog.py --large
 # TODO(smurching) Unpin TensorFlow dependency version once test failures with TF 2.1.0 have been
 # fixed
-pip install 'tensorflow==2.2.0'
+pip install 'tensorflow==2.0.0'
 pytest --verbose tests/tensorflow/test_tensorflow2_model_export.py --large
 pytest --verbose tests/tensorflow_autolog/test_tensorflow2_autolog.py --large
 pytest --verbose tests/keras --large
