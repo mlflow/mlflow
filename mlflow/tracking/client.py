2b68e2717eede9d6bbde89aa5cc03ccc549c29aa
harupy <hkawamura0130@gmail.com>
2020-06-21 23:48:30 +0900

    Fix doc

--- mlflow/tracking/client.py
+++ mlflow/tracking/client.py
@@ -405,12 +405,13 @@ class MlflowClient(object):
                                page_token=None):
         """
         List of all registered models
+
         :param max_results: Maximum number of registered models desired.
         :param page_token: Token specifying the next page of results. It should be obtained from
-        a ``list_registered_models`` call.
+                           a ``list_registered_models`` call.
         :return: A PagedList of :py:class:`mlflow.entities.model_registry.RegisteredModel` objects
-        that can satisfy the search expressions. The pagination token for the next page can be
-        obtained via the ``token`` attribute of the object.
+                 that can satisfy the search expressions. The pagination token for the next page
+                 can be obtained via the ``token`` attribute of the object.
         """
         return self._get_registry_client().list_registered_models(max_results, page_token)
 
