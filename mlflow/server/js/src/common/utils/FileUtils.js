c024e2a605bc61306e2257e568ec0061bed9538a
harupy <hkawamura0130@gmail.com>
2020-07-03 11:31:57 +0900

    Add SyntaxHighlighter

--- mlflow/server/js/src/common/utils/FileUtils.js
+++ mlflow/server/js/src/common/utils/FileUtils.js
@@ -8,6 +8,15 @@ export const getExtension = (path) => {
   return parts[parts.length - 1];
 };
 
+export const getLanguage = (path) => {
+  const ext = getExtension(path).toLowerCase();
+  if (ext in MLFLOW_FILE_LANGUAGES) {
+    return MLFLOW_FILE_LANGUAGES[ext];
+  }
+
+  return ext;
+};
+
 export const IMAGE_EXTENSIONS = new Set(['jpg', 'bmp', 'jpeg', 'png', 'gif', 'svg']);
 export const TEXT_EXTENSIONS = new Set([
   'txt',
@@ -28,3 +37,8 @@ export const TEXT_EXTENSIONS = new Set([
 export const HTML_EXTENSIONS = new Set(['html']);
 export const MAP_EXTENSIONS = new Set(['geojson']);
 export const PDF_EXTENSIONS = new Set(['pdf']);
+
+export const MLFLOW_FILE_LANGUAGES = {
+  mlproject: 'yaml',
+  mlmodel: 'yaml',
+};
