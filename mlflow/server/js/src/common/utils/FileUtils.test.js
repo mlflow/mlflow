8f34e35448fd777be27b6905635e0242a3bdcc86
harupy <hkawamura0130@gmail.com>
2020-07-04 15:41:05 +0900

    Add test case

--- mlflow/server/js/src/common/utils/FileUtils.test.js
+++ mlflow/server/js/src/common/utils/FileUtils.test.js
@@ -3,6 +3,7 @@ import { getLanguage } from './FileUtils';
 describe('FileUtils', () => {
   test('getLanguage', () => {
     expect(getLanguage('foo.py')).toBe('py');
+    expect(getLanguage('foo.bar.py')).toBe('py');
     expect(getLanguage('foo/bar.py')).toBe('py');
     expect(getLanguage('foo')).toBe('foo');
     expect(getLanguage('MLmodel')).toBe('yaml');
