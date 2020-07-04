f668088047f1bee47066e15499840c159e577b3b
harupy <hkawamura0130@gmail.com>
2020-07-04 21:19:35 +0900

    py -> js

--- mlflow/server/js/src/common/utils/FileUtils.test.js
+++ mlflow/server/js/src/common/utils/FileUtils.test.js
@@ -2,9 +2,9 @@ import { getLanguage } from './FileUtils';
 
 describe('FileUtils', () => {
   test('getLanguage', () => {
-    expect(getLanguage('foo.py')).toBe('py');
-    expect(getLanguage('foo.bar.py')).toBe('py');
-    expect(getLanguage('foo/bar.py')).toBe('py');
+    expect(getLanguage('foo.js')).toBe('js');
+    expect(getLanguage('foo.bar.js')).toBe('js');
+    expect(getLanguage('foo/bar.js')).toBe('js');
     expect(getLanguage('foo')).toBe('foo');
     expect(getLanguage('MLmodel')).toBe('yaml');
     expect(getLanguage('MLproject')).toBe('yaml');
