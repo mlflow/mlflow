7b283f5f5ea79d5425fe6b6ad658d9d2e871d02e
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-07-05 20:33:34 +0200

    Move R tests to GitHub Actions (#3048)
    Run R tests using github actions
    
    Signed-off-by: Devin Robison <drobison@nvidia.com>

--- mlflow/R/mlflow/tests/testthat.R
+++ mlflow/R/mlflow/tests/testthat.R
@@ -6,6 +6,7 @@ if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   mlflow:::mlflow_maybe_create_conda_env(python_version = "3.6")
   message("Current working directory: ", getwd())
   mlflow_home <- Sys.getenv("MLFLOW_HOME", "../../../../.")
+  message('MLFLOW_HOME: ', mlflow_home)
   conda_install(c(mlflow_home), envname = mlflow:::mlflow_conda_env_name(), pip = TRUE)
   test_check("mlflow")
 }
