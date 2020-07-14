95f45d290c5410e66ab82fac0b13f8c3fd84003a
harupy <17039389+harupy@users.noreply.github.com>
2020-07-14 23:24:20 +0900

    Add archiveExistingVersions to handleStageTransitionDropdownSelect
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/server/js/src/model-registry/components/ModelVersionPage.js
+++ mlflow/server/js/src/model-registry/components/ModelVersionPage.js
@@ -81,7 +81,7 @@ export class ModelVersionPageImpl extends React.Component {
       });
   }
 
-  handleStageTransitionDropdownSelect = (activity) => {
+  handleStageTransitionDropdownSelect = (activity, archiveExistingVersions) => {
     const { modelName, version } = this.props;
     const toStage = activity.to_stage;
     if (activity.type === ActivityTypes.APPLIED_TRANSITION) {
@@ -90,6 +90,7 @@ export class ModelVersionPageImpl extends React.Component {
           modelName,
           version,
           toStage,
+          archiveExistingVersions,
           this.transitionModelVersionStageRequestId,
         )
         .then(this.loadData)
