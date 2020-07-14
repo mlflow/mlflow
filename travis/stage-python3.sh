b7412a43bb8a1f8bd5ffa90ce24abbb69975758b
harupy <17039389+harupy@users.noreply.github.com>
2020-07-14 11:38:55 +0900

    Fix condition to run example tests
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- travis/stage-python3.sh
+++ travis/stage-python3.sh
@@ -7,7 +7,7 @@ then
   ./travis/test-anaconda-compatibility.sh "anaconda3:2019.03"
 fi
 CHANGED_FILES=$(git diff --name-only master..HEAD | grep "tests/examples\|examples") || true
-if [[ "$TRAVIS_EVENT_TYPE" == "cron" || "$CHANGED_FILES" == *"examples"* ]] && [[ "$TRAVIS_BUILD_STAGE_NAME" == "Nightly" ]]
+if [[ "$TRAVIS_EVENT_TYPE" == "cron" || "$CHANGED_FILES" == *"examples"* ]]
 then
   pytest --verbose tests/examples --large;
 fi
