d91b1fcb89ec30540c68fa43871f6e85d1240a72
Diego <diegovillaveza@gmail.com>
2020-07-15 09:38:20 +0200

    delete invalid import
    Signed-off-by: Andrew Parsons <parsonsandrew1@gmail.com>

--- tests/gensim/test_gensim_model_export.py
+++ tests/gensim/test_gensim_model_export.py
@@ -22,8 +22,6 @@ from gensim.models.word2vec import Word2Vec
 from tests.helper_functions import set_boto_credentials  # pylint: disable=unused-import
 from tests.helper_functions import mock_s3_bucket  # pylint: disable=unused-import
 from tests.helper_functions import score_model_in_sagemaker_docker_container
-from tests.projects.utils import tracking_uri_mock  # pylint: disable=unused-import
-
 
 ModelWithData = namedtuple("ModelWithData", ["model", "inference_data"])
 
