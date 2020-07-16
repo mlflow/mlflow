81b02bdf4ff202a07d26e400b97ab3906af4c55f
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-07-16 15:32:23 +0900

    Migrate the conda compatibility tests to GitHub Actions (#3102)
    * Migrate the conda compatibility tests to GitHub Actions
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * Remove compatibility tests in stage-python3.sh
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    Signed-off-by: Devin Robison <drobison@nvidia.com>

--- travis/stage-python3.sh
+++ travis/stage-python3.sh
@@ -1,7 +1,1 @@
 #!/bin/bash
-set -ex
-if ! [[ "$TRAVIS_EVENT_TYPE" == "cron" || "$TRAVIS_BUILD_STAGE_NAME" == "Nightly" ]]
-then
-  ./travis/test-anaconda-compatibility.sh "anaconda3:2020.02"
-  ./travis/test-anaconda-compatibility.sh "anaconda3:2019.03"
-fi
