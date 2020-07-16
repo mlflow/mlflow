9745160310c302edc10888888c28d26a6c5084a2
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-07-16 14:18:12 +0900

    Migrate the example and docker tests to GitHub Actions (#3103)

--- travis/stage-python3.sh
+++ travis/stage-python3.sh
@@ -5,12 +5,3 @@ then
   ./travis/test-anaconda-compatibility.sh "anaconda3:2020.02"
   ./travis/test-anaconda-compatibility.sh "anaconda3:2019.03"
 fi
-CHANGED_FILES=$(git diff --name-only master..HEAD | grep "tests/examples\|examples") || true
-if [[ "$TRAVIS_EVENT_TYPE" == "cron" || "$CHANGED_FILES" == *"examples"* ]]
-then
-  pytest --verbose tests/examples --large;
-fi
-if [[ "$TRAVIS_EVENT_TYPE" == "cron" || "$CHANGED_FILES" == *"Dockerfile"* ]]
-then
-  docker build -t mlflow_test_build . && docker images | grep mlflow_test_build
-fi
