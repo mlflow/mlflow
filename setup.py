c5a699e7d83e7f523bb7b4d372d80f69fecf20c0
harupy <17039389+harupy@users.noreply.github.com>
2020-07-17 20:24:46 +0900

    Remove exras in extras_require
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -54,16 +54,6 @@ setup(
         'prometheus-flask-exporter',
     ],
     extras_require={
-        'extras': [
-            "scikit-learn; python_version >= '3.5'",
-            # scikit-learn 0.20 is the last version to support Python 2.x  & Python 3.4.
-            "scikit-learn==0.20; python_version < '3.5'",
-            'boto3>=1.7.12',
-            'mleap>=0.16.0',
-            'azure-storage-blob>=12.0',
-            'google-cloud-storage',
-            'azureml-core>=1.2.0'
-        ],
          'sqlserver': [
             "mlflow-dbstore",
         ],
