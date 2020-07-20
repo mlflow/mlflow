470ed306afd832db07e7cd6e7d7630b255c0b9a8
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-07-21 08:35:20 +0900

    Fix prop type for creation_timestamp and last_updated_timestamp (#3129)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/server/js/src/model-registry/test-utils.js
+++ mlflow/server/js/src/model-registry/test-utils.js
@@ -1,7 +1,7 @@
 export const mockRegisteredModelDetailed = (name, latestVersions = []) => {
   return {
-    creation_timestamp: 1571344731467,
-    last_updated_timestamp: 1573581360069,
+    creation_timestamp: '1571344731467',
+    last_updated_timestamp: '1573581360069',
     latest_versions: latestVersions,
     name,
   };
@@ -12,8 +12,8 @@ export const mockModelVersionDetailed = (name, version, stage, status) => {
     name,
     // Use version-based timestamp to make creation_timestamp differ across model versions
     // and prevent React duplicate key warning.
-    creation_timestamp: version,
-    last_updated_timestamp: version + 1,
+    creation_timestamp: version.toString(),
+    last_updated_timestamp: (version + 1).toString(),
     user_id: 'richard@example.com',
     current_stage: stage,
     description: '',
