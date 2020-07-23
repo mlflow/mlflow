a3fc481ce6948a515dc462f737d6677cb0e4f5f8
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-07-24 00:21:35 +0900

    Fix a bug for `_enforce_type` in pandas >= 1.0 (#3130)
    * Fix a bug for _enforce_type in pandas >= 1.0
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * Remove pandas version restriction
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/pyfunc/__init__.py
+++ mlflow/pyfunc/__init__.py
@@ -292,16 +292,6 @@ def _enforce_type(name, values: pandas.Series, t: DataType):
     if values.dtype == np.object and t not in (DataType.binary, DataType.string):
         values = values.infer_objects()
 
-    if values.dtype in (t.to_pandas(), t.to_numpy()):
-        # The types are already compatible => conversion is not necessary.
-        return values
-
-    if t == DataType.binary and values.dtype.kind == t.binary.to_numpy().kind:
-        # NB: bytes in numpy have variable itemsize depending on the length of the longest
-        # element in the array (column). Since MLflow binary type is length agnostic, we ignore
-        # itemsize when matching binary columns.
-        return values
-
     if t == DataType.string and values.dtype == np.object:
         #  NB: strings are by default parsed and inferred as objects, but it is
         # recommended to use StringDtype extension type if available. See
@@ -316,6 +306,16 @@ def _enforce_type(name, values: pandas.Series, t: DataType):
                 "Failed to convert column {0} from type {1} to {2}.".format(
                   name, values.dtype, t)
             )
+
+    if values.dtype in (t.to_pandas(), t.to_numpy()):
+        # The types are already compatible => conversion is not necessary.
+        return values
+
+    if t == DataType.binary and values.dtype.kind == t.binary.to_numpy().kind:
+        # NB: bytes in numpy have variable itemsize depending on the length of the longest
+        # element in the array (column). Since MLflow binary type is length agnostic, we ignore
+        # itemsize when matching binary columns.
+        return values
 
     numpy_type = t.to_numpy()
     is_compatible_type = values.dtype.kind == numpy_type.kind
