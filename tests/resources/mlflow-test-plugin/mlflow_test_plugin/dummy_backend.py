e9848876605bac999e072a197cfbad52e5600176
Fabian Höring <f.horing@criteo.com>
2020-07-24 19:53:38 +0200

    Fix order of parameters in AbstractBackend.run (#3115)
    Signed-off-by: Fabian Höring <f.horing@criteo.com>
    Signed-off-by: Devin Robison <drobison@nvidia.com>

--- tests/resources/mlflow-test-plugin/mlflow_test_plugin/dummy_backend.py
+++ tests/resources/mlflow-test-plugin/mlflow_test_plugin/dummy_backend.py
@@ -1,6 +1,5 @@
 from mlflow.entities import RunStatus
-from mlflow.projects.utils import fetch_and_validate_project, get_or_create_run,\
-    log_project_params_and_tags
+from mlflow.projects.utils import fetch_and_validate_project, get_or_create_run
 from mlflow.projects.backend.abstract_backend import AbstractBackend
 from mlflow.projects.submitted_run import SubmittedRun
 
@@ -28,9 +27,9 @@ class DummySubmittedRun(SubmittedRun):
 
 
 class PluginDummyProjectBackend(AbstractBackend):
-    def run(self, run_id, experiment_id, project_uri, entry_point, params,
-            version, backend_config, tracking_store_uri):
+    def run(self, project_uri, entry_point, params,
+            version, backend_config, tracking_uri, experiment_id):
         work_dir = fetch_and_validate_project(project_uri, version, entry_point, params)
-        active_run = get_or_create_run(run_id, project_uri, experiment_id, work_dir, version,
+        active_run = get_or_create_run(None, project_uri, experiment_id, work_dir, version,
                                        entry_point, params)
-        return DummySubmittedRun(active_run.run_id)
+        return DummySubmittedRun(active_run.info.run_id)
