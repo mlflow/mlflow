85e4c52a776e98c0716de91b08c2e68cb88cbf19
Ankit Mathur <52183359+ankitmathur-db@users.noreply.github.com>
2020-07-26 17:14:03 -0700

    [Multi-Workspace Registry] add client changes for populating registry lineage through run-link (#3164)
    * add client changes for populating registry lineage and tests
    
    Signed-off-by: Ankit Mathur <ankit.mathur@databricks.com>
    
    * remove unnecessary comments
    
    Signed-off-by: Ankit Mathur <ankit.mathur@databricks.com>
    
    * stop using localhost, since it creates a directory and fix text
    
    Signed-off-by: Ankit Mathur <ankit.mathur@databricks.com>
    
    * updates wrt @sueann comments
    
    Signed-off-by: Ankit Mathur <ankit.mathur@databricks.com>
    
    * add warning if no workspace_id set
    
    Signed-off-by: Ankit Mathur <ankit.mathur@databricks.com>
    
    * Update tests/utils/test_databricks_utils.py
    
    Co-authored-by: Sue Ann Hong <sueann@databricks.com>
    Signed-off-by: Ankit Mathur <ankit.mathur@databricks.com>
    
    * Update tests/utils/test_databricks_utils.py
    
    Co-authored-by: Sue Ann Hong <sueann@databricks.com>
    Signed-off-by: Ankit Mathur <ankit.mathur@databricks.com>
    
    * Update tests/tracking/test_client.py
    
    Co-authored-by: Sue Ann Hong <sueann@databricks.com>
    Signed-off-by: Ankit Mathur <ankit.mathur@databricks.com>
    
    * spaces in print statement
    
    Signed-off-by: Ankit Mathur <ankit.mathur@databricks.com>
    
    Co-authored-by: Sue Ann Hong <sueann@databricks.com>
    Signed-off-by: Devin Robison <drobison@nvidia.com>

--- tests/utils/test_databricks_utils.py
+++ tests/utils/test_databricks_utils.py
@@ -4,6 +4,8 @@ import pytest
 from mlflow.utils import databricks_utils
 from databricks_cli.configure.provider import DatabricksConfig
 
+from mlflow.utils.databricks_utils import get_workspace_info_from_dbutils, \
+    get_workspace_info_from_databricks_secrets, is_databricks_default_tracking_uri
 from mlflow.utils.uri import construct_db_uri_from_profile
 
 
@@ -83,6 +85,50 @@ def test_databricks_single_slash_in_uri_scheme_throws(get_config):
     get_config.return_value = None
     with pytest.raises(Exception):
         databricks_utils.get_databricks_host_creds("databricks:/profile/path")
+
+
+def test_get_workspace_info_from_databricks_secrets():
+    mock_dbutils = mock.MagicMock()
+    mock_dbutils.secrets.get.return_value = 'workspace-placeholder-info'
+    with mock.patch("mlflow.utils.databricks_utils._get_dbutils", return_value=mock_dbutils):
+        workspace_host, workspace_id = \
+            get_workspace_info_from_databricks_secrets('databricks://profile/prefix')
+        mock_dbutils.secrets.get.assert_any_call(key='prefix-host', scope='profile')
+        mock_dbutils.secrets.get.assert_any_call(key='prefix-workspace-id', scope='profile')
+        assert workspace_host == 'workspace-placeholder-info'
+        assert workspace_id == 'workspace-placeholder-info'
+
+
+def test_get_workspace_info_from_dbutils():
+    mock_dbutils = mock.MagicMock()
+    mock_dbutils.notebook.entry_point.getDbutils.return_value.notebook.return_value.getContext\
+        .return_value.toJson.return_value = '{"extraContext":' \
+                                            '{"api_url": "https://mlflow.databricks.com"},' \
+                                            '"tags": {"orgId" : "1111"}}'
+    with mock.patch("mlflow.utils.databricks_utils._get_dbutils", return_value=mock_dbutils):
+        workspace_host, workspace_id = get_workspace_info_from_dbutils()
+        assert workspace_host == 'https://mlflow.databricks.com'
+        assert workspace_id == '1111'
+
+
+def test_get_workspace_info_from_dbutils_when_no_dbutils_available():
+    with mock.patch("mlflow.utils.databricks_utils._get_dbutils", return_value=None):
+        workspace_host, workspace_id = get_workspace_info_from_dbutils()
+        assert workspace_host is None
+        assert workspace_id is None
+
+
+@pytest.mark.parametrize("tracking_uri, result", [
+    ('databricks', True),
+    ('databricks://profile/prefix', False),
+    ('nondatabricks', False),
+    ('databricks\t\r', True),
+    ('databricks\n', True),
+    ('databricks://', False),
+    ('databricks://aAbB', False)
+])
+def test_is_databricks_default_tracking_uri(tracking_uri, result):
+    assert(is_databricks_default_tracking_uri(tracking_uri) == result)
 
 
 @mock.patch('databricks_cli.configure.provider.ProfileConfigProvider')
