214db17138fba900ba82a83ab5b6ae8c36ad9fa1
harupy <17039389+harupy@users.noreply.github.com>
2020-07-29 16:46:30 +0900

    Add black
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- lint.sh
+++ lint.sh
@@ -5,6 +5,7 @@ set -e
 FWDIR="$(cd "`dirname $0`"; pwd)"
 cd "$FWDIR"
 
+black --check --line-length=100 .
 pycodestyle --max-line-length=100 --exclude mlflow/protos,mlflow/server/js,mlflow/store/db_migrations,mlflow/temporary_db_migrations_for_pre_1_users -- mlflow tests
 pylint --msg-template="{path} ({line},{column}): [{msg_id} {symbol}] {msg}" --rcfile="$FWDIR/pylintrc" -- mlflow tests
 
