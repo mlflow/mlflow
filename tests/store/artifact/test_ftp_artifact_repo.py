b892dbe2f29e00b411639b3bfb355f28b870a29b
harupy <17039389+harupy@users.noreply.github.com>
2020-07-31 15:04:45 +0900

    Fix directory doubling issue for FTPArtifactRepository.log_artifacts
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/store/artifact/test_ftp_artifact_repo.py
+++ tests/store/artifact/test_ftp_artifact_repo.py
@@ -1,4 +1,6 @@
 # pylint: disable=redefined-outer-name
+import os
+
 from mock import MagicMock
 import pytest
 import posixpath
@@ -176,7 +178,8 @@ def test_log_artifact_multiple_calls(ftp_mock, tmpdir):
     assert ftp_mock.storbinary.call_args_list[0][0][0] == 'STOR test2.txt'
 
 
-def test_log_artifacts(ftp_mock, tmpdir):
+@pytest.mark.parametrize("artifact_path", [None, 'dir', 'dir1/dir2'])
+def test_log_artifacts(artifact_path, ftp_mock, tmpdir):
     repo = FTPArtifactRepository("ftp://test_ftp/some/path")
 
     repo.get_ftp_client = MagicMock()
@@ -190,10 +193,15 @@ def test_log_artifacts(ftp_mock, tmpdir):
 
     ftp_mock.cwd = MagicMock(side_effect=[ftplib.error_perm, None, None, None, None, None])
 
-    repo.log_artifacts(subd.strpath)
+    repo.log_artifacts(subd.strpath, artifact_path)
 
-    ftp_mock.mkd.assert_any_call('/some/path/subdir')
-    ftp_mock.cwd.assert_any_call('/some/path/subdir')
+    if artifact_path is None:
+        ftp_mock.mkd.assert_any_call('/some/path')
+        ftp_mock.cwd.assert_any_call('/some/path')
+    else:
+        ftp_mock.mkd.assert_any_call(os.path.join('/some/path', artifact_path))
+        ftp_mock.cwd.assert_any_call(os.path.join('/some/path', artifact_path))
+
     assert ftp_mock.storbinary.call_count == 3
     storbinary_call_args = sorted([ftp_mock.storbinary.call_args_list[i][0][0] for i in range(3)])
     assert storbinary_call_args == ['STOR a.txt', 'STOR b.txt', 'STOR c.txt']
