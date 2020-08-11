ba1f772a677fea4d708bff80499d47a1389669c1
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-08-12 01:28:51 +0900

    Rename `travis` folder to `dev` (#3253)
    * Rename travis folder to dev
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * Rename travis folder to dev
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * Update README
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * revert previous commit
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * Rename .travis.R
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * rename
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * rename
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * Fix .Rbuildignore
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- dev/install-common-deps.sh
+++ dev/install-common-deps.sh
@@ -36,11 +36,11 @@ if [[ "$INSTALL_SMALL_PYTHON_DEPS" == "true" ]]; then
   # When downloading large packages from PyPI, the connection is sometimes aborted by the
   # remote host. See https://github.com/pypa/pip/issues/8510.
   # As a workaround, we retry installation of large packages.
-  retry-with-backoff pip install --quiet -r ./travis/small-requirements.txt
+  retry-with-backoff pip install --quiet -r ./dev/small-requirements.txt
 fi
 if [[ "$INSTALL_LARGE_PYTHON_DEPS" == "true" ]]; then
-  retry-with-backoff pip install --quiet -r ./travis/large-requirements.txt
-  # Hack: make sure all spark-* scripts are executable. 
+  retry-with-backoff pip install --quiet -r ./dev/large-requirements.txt
+  # Hack: make sure all spark-* scripts are executable.
   # Conda installs 2 version spark-* scripts and makes the ones spark
   # uses not executable. This is a temporary fix to unblock the tests.
   ls -lha $(find $CONDA_DIR/envs/test-environment/ -path "*bin/spark-*")
