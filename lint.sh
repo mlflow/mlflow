727df095a133e23c88327285f01d9bad325a5789
harupy <17039389+harupy@users.noreply.github.com>
2020-08-12 12:01:46 +0900

    Fix pylint issues
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- lint.sh
+++ lint.sh
@@ -5,8 +5,43 @@ set -e
 FWDIR="$(cd "`dirname $0`"; pwd)"
 cd "$FWDIR"
 
+# https://stackoverflow.com/a/17841619
+function join {
+  local d=$1
+  shift
+  echo -n "$1"
+  shift
+  printf "%s" "${@/#/$d}"
+}
+
+include_dirs=(
+  "mlflow"
+  "tests"
+)
+
+exclude_dirs=(
+  "mlflow/protos"
+  "mlflow/server/js"
+  "mlflow/store/db_migrations"
+  "mlflow/temporary_db_migrations_for_pre_1_users"
+)
+
+# Exclude proto files because they are auto-generated
 black --check --line-length=100 --exclude=mlflow/protos .
-pycodestyle --max-line-length=100 --ignore=E203,W503 --exclude mlflow/protos,mlflow/server/js,mlflow/store/db_migrations,mlflow/temporary_db_migrations_for_pre_1_users -- mlflow tests
-pylint --msg-template="{path} ({line},{column}): [{msg_id} {symbol}] {msg}" --rcfile="$FWDIR/pylintrc" -- mlflow tests
+
+exclude=$(join "," "${exclude_dirs[@]}")
+include=$(join " " "${include_dirs[@]}")
+pycodestyle --max-line-length=100 --ignore=E203,W503,E7 --exclude=$exclude -- $include
+
+# pylint's `--ignore` option filters files based on their base names, not paths.
+# see: http://pylint.pycqa.org/en/latest/user_guide/run.html#command-line-options
+# This behavior might cause us to unintentionally ignore some files
+# To avoid this issue, select files to lint using `git ls-files` and `grep`.
+exclude="^\($(join "\|" "${exclude_dirs[@]}")\)/.\+\.py$"
+include="^\($(join "\|" "${include_dirs[@]}")\)/.\+\.py$"
+msg_template="{path} ({line},{column}): [{msg_id} {symbol}] {msg}"
+
+git ls-files | grep $include | grep -v $exclude | \
+xargs pylint --msg-template="$msg_template" --rcfile="$FWDIR/pylintrc"
 
 rstcheck README.rst
