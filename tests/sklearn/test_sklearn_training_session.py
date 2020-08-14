5bd3e3e3b4ee7e00139f1b27be9b9627bbc43d4c
harupy <17039389+harupy@users.noreply.github.com>
2020-08-14 15:49:02 +0900

    Add a new test case
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/sklearn/test_sklearn_training_session.py
+++ tests/sklearn/test_sklearn_training_session.py
@@ -11,10 +11,14 @@ class Child:
     pass
 
 
+class Grandchild:
+    pass
+
+
 def assert_session_stack(classes):
     assert len(_SklearnTrainingSession._session_stack) == len(classes)
 
-    for idx, (sess, (clazz, parent_clazz)) in enumerate(
+    for idx, (sess, (parent_clazz, clazz)) in enumerate(
         zip(_SklearnTrainingSession._session_stack, classes)
     ):
         assert sess.clazz == clazz
@@ -31,20 +35,39 @@ def allow_children(request):
 
 def test_only_root_session(allow_children):
     with _SklearnTrainingSession(Parent, allow_children=allow_children) as p:
+        assert_session_stack([(None, Parent)])
         assert p.should_log()
 
-        assert_session_stack([(Parent, None)])
     assert_session_stack([])
 
 
 def test_nested_once(allow_children):
     with _SklearnTrainingSession(Parent, allow_children=allow_children) as p:
-        assert_session_stack([(Parent, None)])
+        assert_session_stack([(None, Parent)])
 
         with _SklearnTrainingSession(Child) as c:
+            assert_session_stack([(None, Parent), (Parent, Child)])
             assert p.should_log()
             assert c.should_log() == allow_children
 
-            assert_session_stack([(Parent, None), (Child, Parent)])
-        assert_session_stack([(Parent, None)])
+        assert_session_stack([(None, Parent)])
+    assert_session_stack([])
+
+
+def test_parent_session_overrides_second_level_sessions_allow_children():
+    with _SklearnTrainingSession(Parent, allow_children=False) as p:
+        assert_session_stack([(None, Parent)])
+
+        with _SklearnTrainingSession(Child, allow_children=True) as c:
+            assert_session_stack([(None, Parent), (Parent, Child)])
+
+            with _SklearnTrainingSession(Grandchild) as g:
+                assert_session_stack([(None, Parent), (Parent, Child), (Child, Grandchild)])
+
+                assert p.should_log()
+                assert not c.should_log()
+                assert not g.should_log()
+
+            assert_session_stack([(None, Parent), (Parent, Child)])
+        assert_session_stack([(None, Parent)])
     assert_session_stack([])
