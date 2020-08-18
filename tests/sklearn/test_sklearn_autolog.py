6d87e1b726846ae04f306ffdc52d59448723077d
harupy <17039389+harupy@users.noreply.github.com>
2020-08-18 23:46:14 +0900

    Fix assertion order
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/sklearn/test_sklearn_autolog.py
+++ tests/sklearn/test_sklearn_autolog.py
@@ -162,11 +162,11 @@ def test_autolog_marks_run_as_failed_when_fit_fails():
     with mock.patch("logging.Logger.warning") as mock_warning:
         model = sklearn.svm.LinearSVC(C=-8).fit(*get_iris())
 
-        mock_warning.assert_called_once()
-        assert mock_warning.call_args[0][0].startswith("LinearSVC.fit failed")
         assert model is None
         assert mlflow.active_run() is None
         assert get_run(run._info.run_id)._info.status == "FAILED"
+        mock_warning.assert_called_once()
+        assert mock_warning.call_args[0][0].startswith("LinearSVC.fit failed")
 
 
 def test_autolog_emits_warnings_message_when_score_fails():
