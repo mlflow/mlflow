e39257ebe8b57215576498efffb1d9683373fde6
harupy <17039389+harupy@users.noreply.github.com>
2020-08-22 00:31:41 +0900

    Fix lint
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/sklearn/test_sklearn_autolog.py
+++ tests/sklearn/test_sklearn_autolog.py
@@ -89,9 +89,10 @@ def fit_func_name(request):
 
 @pytest.fixture(autouse=True, scope="function")
 def force_try_mlflow_log_to_fail(request):
-    # autolog contains multiple `try_mlflow_log`. They may hide unintentional errors (often caused by
-    # incorrect test settings) and allow tests that should not pass to pass (without us noticing).
-    # To prevent that, temporarily turns a warning emitted by `try_mlflow_log` into errors.
+    # autolog contains multiple `try_mlflow_log`. They may hide unintentional errors (often
+    # caused by incorrect test settings) and allow tests that should not pass to pass
+    # (without us noticing). To prevent that, temporarily turns a warning emitted by
+    # `try_mlflow_log` into errors.
     if "disable_force_try_mlflow_log_to_fail" in request.keywords:
         yield
     else:
