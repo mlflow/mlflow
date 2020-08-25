b44cbb3eda0c038e17f10e88473177aec41b56c1
harupy <17039389+harupy@users.noreply.github.com>
2020-08-25 11:33:43 +0900

    Add examples for sklearn autolog
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

---
+++ examples/sklearn_autolog/pipeline.py
@@ -0,0 +1,44 @@
+from pprint import pprint
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+import mlflow
+
+
+def fetch_logged_data(run_id):
+    client = mlflow.tracking.MlflowClient()
+    data = client.get_run(run_id).data
+    tags = {k: v for k, v in data.tags.items() if not k.startswith("mlflow.")}
+    artifacts = [f.path for f in client.list_artifacts(run_id, "model")]
+    return {
+        "keys": data.params,
+        "metrics": data.metrics,
+        "tags": tags,
+        "artifacts": artifacts,
+    }
+
+
+def main():
+    # enable autologging
+    mlflow.sklearn.autolog()
+
+    # prepare training data
+    X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+    y = np.dot(X, np.array([1, 2])) + 3
+
+    # train a model
+    pipe = Pipeline([("scaler", StandardScaler()), ("lr", LinearRegression())])
+    with mlflow.start_run() as run:
+        pipe.fit(X, y)
+
+    # show logged data
+    for key, data in fetch_logged_data(run._info.run_id).items():
+        print("-" * 20)
+        pprint(data)
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/sklearn_autolog/simple.py
@@ -0,0 +1,42 @@
+from pprint import pprint
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+import mlflow
+
+
+def fetch_logged_data(run_id):
+    client = mlflow.tracking.MlflowClient()
+    data = client.get_run(run_id).data
+    tags = {k: v for k, v in data.tags.items() if not k.startswith("mlflow.")}
+    artifacts = [f.path for f in client.list_artifacts(run_id, "model")]
+    return {
+        "keys": data.params,
+        "metrics": data.metrics,
+        "tags": tags,
+        "artifacts": artifacts,
+    }
+
+
+def main():
+    # enable autologging
+    mlflow.sklearn.autolog()
+
+    # prepare training data
+    X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+    y = np.dot(X, np.array([1, 2])) + 3
+
+    # train a model
+    model = LinearRegression()
+    with mlflow.start_run() as run:
+        model.fit(X, y)
+
+    # show logged data
+    for key, data in fetch_logged_data(run._info.run_id).items():
+        print("-" * 20)
+        pprint(data)
+
+
+if __name__ == "__main__":
+    main()
