1f47e40cc8675be4145beed37428894707de2a31
harupy <17039389+harupy@users.noreply.github.com>
2020-08-26 18:46:42 +0900

    update
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- a.py
+++ a.py
@@ -1,3 +1,5 @@
+import os
+
 from sklearn.linear_model import ElasticNet
 
 import mlflow
@@ -8,3 +10,7 @@ if __name__ == "__main__":
     with mlflow.start_run():
         lr = ElasticNet()
         mlflow.sklearn.log_model(lr, "model")
+
+    for r, ds, fs in os.walk("mlruns"):
+        for f in fs:
+            print(os.path.join(r, f))
