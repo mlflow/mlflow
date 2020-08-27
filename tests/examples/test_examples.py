97d14966e9e9afc75a514c73228f2201b134b3db
harupy <17039389+harupy@users.noreply.github.com>
2020-08-27 22:34:38 +0900

    rename
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -105,7 +105,7 @@ def test_mlflow_run_example(directory, params, tmpdir):
                 "0.9",
             ],
         ),
-        ("sklearn_autolog", ["python", "simple.py"]),
+        ("sklearn_autolog", ["python", "linear_regression.py"]),
         ("sklearn_autolog", ["python", "pipeline.py"]),
         ("sklearn_autolog", ["python", "grid_search_cv.py"]),
     ],
