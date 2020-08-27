09f21f326400a4cec213e2b24301adf6d1b3b0a6
harupy <17039389+harupy@users.noreply.github.com>
2020-08-27 16:47:09 +0900

    Try library(mlflow)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/.run-tests.R
+++ mlflow/R/mlflow/.run-tests.R
@@ -1,7 +1,8 @@
-parent_dir <- dir("../", full.names = TRUE)
-package <- parent_dir[grepl("mlflow_", parent_dir)]
-install.packages(package)
+# parent_dir <- dir("../", full.names = TRUE)
+# package <- parent_dir[grepl("mlflow_", parent_dir)]
+# install.packages(package)
 
+library(mlflow)
 library(reticulate)
 use_condaenv(mlflow:::mlflow_conda_env_name())
 devtools::check_built(path = package, error_on = "note", args = "--no-tests")
