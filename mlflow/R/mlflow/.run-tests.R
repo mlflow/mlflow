8aa40a9466d8236295ba59ecea8a1c7407211404
harupy <17039389+harupy@users.noreply.github.com>
2020-08-28 22:35:47 +0900

    Use chdir
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/.run-tests.R
+++ mlflow/R/mlflow/.run-tests.R
@@ -1,10 +1,9 @@
 parent_dir <- dir("./", full.names = TRUE)
 package <- parent_dir[grepl("mlflow_", parent_dir)]
 
-library(mlflow)
 library(reticulate)
 use_condaenv(mlflow:::mlflow_conda_env_name())
 # TODO(harupy): Add `error_on = "note"` once the issue below has been fixed:
 # https://stackoverflow.com/questions/63613301/r-cmd-check-note-unable-to-verify-current-time/63616156#63616156
 devtools::check_built(path = package, args = "--no-tests")
-source("tests/testthat.R")
+source("tests/testthat.R", chdir = TRUE)
