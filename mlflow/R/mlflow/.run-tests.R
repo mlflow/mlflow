12a78d1a8812a6a823171baca584bef0b50fffe5
harupy <17039389+harupy@users.noreply.github.com>
2020-08-29 00:35:24 +0900

    Remove useless lines
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/.run-tests.R
+++ mlflow/R/mlflow/.run-tests.R
@@ -1,6 +1,3 @@
-parent_dir <- dir("./", full.names = TRUE)
-package <- parent_dir[grepl("mlflow_", parent_dir)]
-
 library(reticulate)
 use_condaenv(mlflow:::mlflow_conda_env_name())
 # TODO(harupy): Add `error_on = "note"` once the issue below has been fixed:
