0c4293707ea76034af07e74deaa5bfe41be9a649
harupy <17039389+harupy@users.noreply.github.com>
2020-08-29 00:17:08 +0900

    Run check_built in current dir
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/.run-tests.R
+++ mlflow/R/mlflow/.run-tests.R
@@ -5,5 +5,5 @@ library(reticulate)
 use_condaenv(mlflow:::mlflow_conda_env_name())
 # TODO(harupy): Add `error_on = "note"` once the issue below has been fixed:
 # https://stackoverflow.com/questions/63613301/r-cmd-check-note-unable-to-verify-current-time/63616156#63616156
-devtools::check_built(path = package, args = "--no-tests")
+devtools::check_built(path = ".", args = "--no-tests")
 source("tests/testthat.R", chdir = TRUE)
