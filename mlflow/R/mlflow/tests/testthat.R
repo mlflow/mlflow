9f24a9d9f8c98dcd19900387f871310da3a43c17
harupy <17039389+harupy@users.noreply.github.com>
2020-08-28 22:27:49 +0900

    Fix path in testthat.R
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/tests/testthat.R
+++ mlflow/R/mlflow/tests/testthat.R
@@ -3,7 +3,7 @@ library(mlflow)
 
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   message("Current working directory: ", getwd())
-  mlflow_home <- Sys.getenv("MLFLOW_HOME", "../../../../.")
+  mlflow_home <- Sys.getenv("MLFLOW_HOME", "../../../.")
   message('MLFLOW_HOME: ', mlflow_home)
   test_check("mlflow")
 }
