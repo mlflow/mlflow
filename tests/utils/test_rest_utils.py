6b916b005923dd95c23c6a22597e998e8255c17d
bramrodenburg <bramrodenburg@live.nl>
2020-09-04 02:52:55 +0200

    Verify valid JSON format in REST output (#3302)
    * Check for JSON return format in verify_test_response.
    
    Signed-off-by: Bram Rodenburg <bramrodenburg@live.nl>
    
    * Added and fixed unit tests.
    
    Signed-off-by: Bram Rodenburg <bramrodenburg@live.nl>
    
    * Fixed linting issues
    
    Signed-off-by: Bram Rodenburg <bramrodenburg@live.nl>
    
    * Improved description
    
    Signed-off-by: Bram Rodenburg <bramrodenburg@live.nl>
    
    * Applied black linting to test_rest_utils
    
    Signed-off-by: Bram Rodenburg <bramrodenburg@live.nl>
    
    * Fixed tests
    
    Signed-off-by: Bram Rodenburg <bramrodenburg@live.nl>
    
    * Fixed linting issues in test_dbfs_rest_artifact_repo
    
    Signed-off-by: Bram Rodenburg <bramrodenburg@live.nl>
    
    * Implemented PR feedback. Check exception message.
    
    Signed-off-by: Bram Rodenburg <bramrodenburg@live.nl>
    
    * Reverted unrelated formatting changes
    
    Signed-off-by: Bram Rodenburg <bramrodenburg@live.nl>
    
    * Reverted more unrelated formatting changes.
    
    Signed-off-by: Bram Rodenburg <bramrodenburg@live.nl>

--- tests/utils/test_rest_utils.py
+++ tests/utils/test_rest_utils.py
@@ -30,6 +30,23 @@ def test_well_formed_json_error_response():
             call_endpoint(host_only, "/my/endpoint", "GET", "", response_proto)
 
 
+def test_non_json_ok_response():
+    with mock.patch("requests.request") as request_mock:
+        host_only = MlflowHostCreds("http://my-host")
+        response_mock = mock.MagicMock()
+        response_mock.status_code = 200
+        response_mock.text = "<html></html>"
+        request_mock.return_value = response_mock
+
+        response_proto = GetRun.Response()
+        with pytest.raises(
+            MlflowException,
+            match="API request to endpoint was successful but the response body was not "
+            "in a valid JSON format",
+        ):
+            call_endpoint(host_only, "/my/endpoint", "GET", "", response_proto)
+
+
 @pytest.mark.parametrize(
     "response_mock",
     [
@@ -179,6 +196,7 @@ def test_http_request_wrapper(request):
     host_only = MlflowHostCreds("http://my-host", ignore_tls_verification=True)
     response = mock.MagicMock()
     response.status_code = 200
+    response.text = "{}"
     request.return_value = response
     http_request_safe(host_only, "/my/endpoint")
     request.assert_called_with(
