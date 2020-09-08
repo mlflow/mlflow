34c10ffd134f6315ded4712df3966e2d4fb22370
Shrinath Suresh <shrinath@ideas2it.com>
2020-09-08 22:57:21 +0530

    Fixing Style Issues in pytorch __init__.py and test_pytorch_model_export
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

--- tests/pytorch/test_pytorch_model_export.py
+++ tests/pytorch/test_pytorch_model_export.py
@@ -98,6 +98,7 @@ def get_subclassed_model_definition():
     can be invoked within a module to define the class in the module's scope.
     """
 
+    # pylint: disable=W0223
     class SubclassedModel(torch.nn.Module):
         def __init__(self):
             super(SubclassedModel, self).__init__()
@@ -123,6 +124,7 @@ def main_scoped_subclassed_model(data):
     return model
 
 
+# pylint: disable=W0223
 class ModuleScopedSubclassedModel(get_subclassed_model_definition()):
     """
     A custom PyTorch model class defined in the test module scope. This is a subclass of
