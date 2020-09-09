02e254ad8d7bcfde32e7649635d43becc39957e0
Andy Chow <58712524+andychow-db@users.noreply.github.com>
2020-09-09 16:35:45 -0700

    Await model version creation (#3376)
    * Await model version creation
    
    Model versions are asynchronously created hence the reason for the
    status 'PENDING_REGISTRATION', 'READY' and 'FAILED_REGISTRATION'.
    Depending on the backing store, it may copy the model artifacts to
    another location owned by the backing store.
    
    Previously, the create_model_version, register_model and log_model APIs
    would not wait for the model version to complete creation before
    attempting to use the model version. This would invariable cause errors
    because the model version was not 'READY'.
    
    To reduce such incidents, I have introduced a new parameters,
    await_creation_for for create_model_version functions in the client
    and await_registrtion_for for log_model and register_model.
    
    The new parameters causes the client to wait until the model version has
    been created and the model version status is READY or FAILED_REGISTRATION.
    
    By default, the client will wait upto five minutes for the model version to be available.
    Users can disable the wait by specifying None or O for await_creation_for or
    await_registration_for.
    
    Signed-off-by: Andy Chow <andy.chow@databricks.com>
    Co-authored-by: Sue Ann Hong <sueann@databricks.com>

--- mlflow/fastai.py
+++ mlflow/fastai.py
@@ -31,6 +31,7 @@ from mlflow.utils.environment import _mlflow_conda_env
 from mlflow.utils.model_utils import _get_flavor_configuration
 from mlflow.utils.annotations import experimental
 from mlflow.utils.autologging_utils import try_mlflow_log, log_fn_args_as_params, wrap_patch
+from mlflow.tracking._model_registry import DEFAULT_AWAIT_MAX_SLEEP_SECONDS
 
 
 FLAVOR_NAME = "fastai"
@@ -153,6 +154,7 @@ def log_model(
     registered_model_name=None,
     signature: ModelSignature = None,
     input_example: ModelInputExample = None,
+    await_registration_for=DEFAULT_AWAIT_MAX_SLEEP_SECONDS,
     **kwargs
 ):
     """
@@ -201,6 +203,9 @@ def log_model(
                           base64-encoded.
 
     :param kwargs: kwargs to pass to `fastai.Learner.export`_ method.
+    :param await_registration_for: Number of seconds to wait for the model version to finish
+                            being created and is in ``READY`` status. By default, the function
+                            waits for five minutes. Specify 0 or None to skip waiting.
     """
     Model.log(
         artifact_path=artifact_path,
@@ -210,6 +215,7 @@ def log_model(
         conda_env=conda_env,
         signature=signature,
         input_example=input_example,
+        await_registration_for=await_registration_for,
         **kwargs
     )
 
--- mlflow/keras.py
+++ mlflow/keras.py
@@ -29,6 +29,7 @@ from mlflow.utils.environment import _mlflow_conda_env
 from mlflow.utils.model_utils import _get_flavor_configuration
 from mlflow.utils.annotations import experimental
 from mlflow.utils.autologging_utils import try_mlflow_log, log_fn_args_as_params, wrap_patch
+from mlflow.tracking._model_registry import DEFAULT_AWAIT_MAX_SLEEP_SECONDS
 
 
 FLAVOR_NAME = "keras"
@@ -264,6 +265,7 @@ def log_model(
     registered_model_name=None,
     signature: ModelSignature = None,
     input_example: ModelInputExample = None,
+    await_registration_for=DEFAULT_AWAIT_MAX_SLEEP_SECONDS,
     **kwargs
 ):
     """
@@ -320,7 +322,9 @@ def log_model(
                           model. The given example will be converted to a Pandas DataFrame and then
                           serialized to json using the Pandas split-oriented format. Bytes are
                           base64-encoded.
-
+    :param await_registration_for: Number of seconds to wait for the model version to finish
+                            being created and is in ``READY`` status. By default, the function
+                            waits for five minutes. Specify 0 or None to skip waiting.
     :param kwargs: kwargs to pass to ``keras_model.save`` method.
 
     .. code-block:: python
@@ -347,6 +351,7 @@ def log_model(
         registered_model_name=registered_model_name,
         signature=signature,
         input_example=input_example,
+        await_registration_for=await_registration_for,
         **kwargs
     )
 
