1e91fb7b173d5e4e2f9c81806320b48b99646889
harupy <17039389+harupy@users.noreply.github.com>
2020-09-10 13:13:56 +0900

    Add a test case for non-api endpoint with non-json response
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/utils/test_rest_utils.py
+++ tests/utils/test_rest_utils.py
@@ -202,6 +202,12 @@ def test_http_request_wrapper(request):
     request.assert_called_with(
         url="http://my-host/my/endpoint", verify=False, headers=_DEFAULT_HEADERS,
     )
+    response.text = "non json"
+    request.return_value = response
+    http_request_safe(host_only, "/my/endpoint")
+    request.assert_called_with(
+        url="http://my-host/my/endpoint", verify=False, headers=_DEFAULT_HEADERS,
+    )
     response.status_code = 400
     response.text = ""
     request.return_value = response
