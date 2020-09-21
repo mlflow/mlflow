240125f4b27d61b360972b1dde6fca62d1e3ca51
Shrinath Suresh <shrinath@ideas2it.com>
2020-09-21 13:24:56 +0530

    Adding tests for run start and run end

--- tests/pytorch/test_pytorch_autolog.py
+++ tests/pytorch/test_pytorch_autolog.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 import pytorch_lightning as pl
+import torch
 from iris import IrisClassification
 import mlflow
 import mlflow.pytorch
@@ -55,6 +56,28 @@ def test_pytorch_autolog_logs_expected_data(pytorch_model):
 
 
 @pytest.fixture
+def pytorch_model_initialized():
+    trainer = pl.Trainer(
+        max_epochs=NUM_EPOCHS
+    )
+    model = IrisClassification()
+    trainer.fit(model)
+    return trainer
+
+
+def test_pytorch_autolog_persists_manually_created_run(pytorch_model_initialized):
+    autolog()
+    with mlflow.start_run() as manual_run:
+        assert mlflow.active_run()
+        assert mlflow.active_run().info.run_id == manual_run.info.run_id
+
+
+def test_pytorch_autolog_ends_auto_created_run(pytorch_model_initialized):
+    autolog()
+    assert mlflow.active_run() is None
+
+
+@pytest.fixture
 def pytorch_model_with_callback(patience):
     autolog()
     model = IrisClassification()
@@ -85,12 +108,25 @@ def pytorch_model_with_callback(patience):
 
 
 @pytest.mark.parametrize("patience", [3])
-def test_pytorch_early_stop_metrics_logged(pytorch_model_with_callback, patience):
+def test_pytorch_early_stop_artifacts_logged(pytorch_model_with_callback):
     trainer, run = pytorch_model_with_callback
     client = mlflow.tracking.MlflowClient()
     artifacts = client.list_artifacts(run.info.run_id)
     artifacts = map(lambda x: x.path, artifacts)
     assert "restored_model_checkpoint" in artifacts
+
+
+@pytest.mark.parametrize("patience", [3])
+def test_pytorch_autolog_model_can_load_from_artifact(pytorch_model_with_callback):
+    _, run = pytorch_model_with_callback
+    run_id = run.info.run_id
+    client = mlflow.tracking.MlflowClient()
+    artifacts = client.list_artifacts(run_id)
+    artifacts = map(lambda x: x.path, artifacts)
+    assert "models" in artifacts
+    model = mlflow.pytorch.load_model("runs:/" + run_id + "/models")
+    result = model(torch.Tensor([1.5, 2, 2.5, 3.5]).unsqueeze(0))
+    assert result is not None
 
 
 @pytest.mark.parametrize("patience", [0, 1, 5])
@@ -112,6 +148,14 @@ def test_pytorch_early_stop_metrics_logged(pytorch_model_with_callback, patience
     assert "Stopped_Epoch" in data.metrics
     assert "Wait_Count" in data.metrics
     assert "Restored_Epoch" in data.metrics
+
+
+def test_pytorch_autolog_non_early_stop_callback_does_not_log(pytorch_model):
+    trainer, run = pytorch_model
+    client = mlflow.tracking.MlflowClient()
+    metric_history = client.get_metric_history(run.info.run_id, "loss")
+    assert trainer.max_epochs == NUM_EPOCHS
+    assert len(metric_history) == NUM_EPOCHS
 
 
 @pytest.fixture
