5503e7b1302f176380fd6f0d3997662fafdab7dc
harupy <17039389+harupy@users.noreply.github.com>
2020-10-01 13:08:35 +0900

    Use wildcard
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/R/model-h2o.R
+++ mlflow/R/mlflow/R/model-h2o.R
@@ -37,7 +37,7 @@ mlflow_save_model.H2OModel <- function(model,
   )
 
   conda_env <- create_default_conda_env_if_absent(
-    path, conda_env, default_pip_deps = list("mlflow", paste0("h2o>=", version))
+    path, conda_env, default_pip_deps = list("mlflow", str_interp("h2o==${version}.*"))
   )
 
   h2o_conf <- list(
