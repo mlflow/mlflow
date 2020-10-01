e7aa5c6a9257cf3dd37cf38ec5ec360cb010bd34
Andrew Nitu <andrewnitu@gmail.com>
2020-10-01 14:34:17 -0400

    stash
    Signed-off-by: Andrew Nitu <andrewnitu@gmail.com>

--- tests/utils/test_autologging_utils.py
+++ tests/utils/test_autologging_utils.py
@@ -255,24 +255,24 @@ def test_avoids_inferring_signature_if_not_needed(logger):
     logger.warning.assert_not_called()
 
 
-# def test_universal_autolog_calls_specific_autologs_correctly():
-#     autolog_integrations = ["tensorflow", "keras", "mxnet.gluon", "xgboost", "lightgbm", "spark", "sklearn", "fastai"]
+def test_universal_autolog_calls_specific_autologs_correctly():
+    autolog_integrations = ["tensorflow", "keras", "mxnet.gluon", "xgboost", "lightgbm", "spark", "sklearn", "fastai"]
 
-#     for integration_name in autolog_integrations:
-#         setattr(getattr(mlflow, integration_name.split(".")[-1]), "autolog", Mock())
-#         autolog_fn = getattr(getattr(mlflow, integration_name.split(".")[-1]), "autolog")
-#         autolog_fn.assert_not_called()
+    for integration_name in autolog_integrations:
+        setattr(getattr(mlflow, integration_name.split(".")[-1]), "autolog", Mock())
+        autolog_fn = getattr(getattr(mlflow, integration_name.split(".")[-1]), "autolog")
+        autolog_fn.assert_not_called()
 
-#     universal_autolog(True, True)
+    universal_autolog(True, True)
 
-#     for integration_name in autolog_integrations:
-#         autolog_fn = getattr(getattr(mlflow, integration_name.split(".")[-1]), "autolog")
+    for integration_name in autolog_integrations:
+        autolog_fn = getattr(getattr(mlflow, integration_name.split(".")[-1]), "autolog")
 
-#         autolog_fn.assert_not_called()
+        autolog_fn.assert_not_called()
 
-#         importlib.__import__(integration_name)
+        importlib.__import__(integration_name)
 
-#         if integration_name in ["xgboost", "lightgbm", "sklearn"]:
-#             autolog_fn.assert_called_once_with(True, True)
-#         else:
-#             autolog_fn.assert_called_once_with()
+        if integration_name in ["xgboost", "lightgbm", "sklearn"]:
+            autolog_fn.assert_called_once_with(True, True)
+        else:
+            autolog_fn.assert_called_once_with()
