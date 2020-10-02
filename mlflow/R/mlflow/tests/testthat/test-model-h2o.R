8c61f93828092f75c26c8c28cd632f44c73fd670
juntai-zheng <39497939+juntai-zheng@users.noreply.github.com>
2020-10-02 15:11:29 -0700

    R test fix (#3477)
    * initial test
    
    Signed-off-by: Juntai Zheng <juntai.zheng@databricks.com>
    
    * add missing quote
    
    Signed-off-by: Juntai Zheng <juntai.zheng@databricks.com>
    
    * remove init name
    
    Signed-off-by: Juntai Zheng <juntai.zheng@databricks.com>
    
    * fix version
    
    Signed-off-by: Juntai Zheng <juntai.zheng@databricks.com>
    
    * one more try
    
    Signed-off-by: Juntai Zheng <juntai.zheng@databricks.com>
    
    * try shutdown first
    
    Signed-off-by: Juntai Zheng <juntai.zheng@databricks.com>
    
    * missing #
    
    Signed-off-by: Juntai Zheng <juntai.zheng@databricks.com>
    
    * testing reinstallation
    
    Signed-off-by: Juntai Zheng <juntai.zheng@databricks.com>
    
    * try equal
    
    Signed-off-by: Juntai Zheng <juntai.zheng@databricks.com>
    
    * final fix
    
    Signed-off-by: Juntai Zheng <juntai.zheng@databricks.com>

--- mlflow/R/mlflow/tests/testthat/test-model-h2o.R
+++ mlflow/R/mlflow/tests/testthat/test-model-h2o.R
@@ -7,6 +7,15 @@ predictors <- setdiff(colnames(iris), prediction)
 train <- iris[idx[1:100], ]
 test <- iris[idx[101:nrow(iris)], ]
 
+# Installing most recent h2o package, see https://docs.h2o.ai/h2o/latest-stable/h2o-docs/downloading.html#install-in-r
+if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
+if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
+pkgs <- c("RCurl","jsonlite")
+for (pkg in pkgs) {
+  if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
+}
+install.packages("h2o", type="source", repos=(c("http://h2o-release.s3.amazonaws.com/h2o/latest_stable_R")))
+
 h2o::h2o.init()
 
 model <- h2o::h2o.randomForest(
