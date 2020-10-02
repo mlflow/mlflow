b861d4f2312641eebb3f65b46af9f4ec4a300954
harupy <17039389+harupy@users.noreply.github.com>
2020-10-02 09:58:00 +0900

    Use the same h2o-py version
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/.run-tests.R
+++ mlflow/R/mlflow/.run-tests.R
@@ -10,7 +10,7 @@ keras::install_keras(method = "conda", envname = mlflow:::mlflow_conda_env_name(
 reticulate::conda_install(Sys.getenv("MLFLOW_HOME", "../../../../."), envname = mlflow:::mlflow_conda_env_name(), pip = TRUE)
 reticulate::conda_install("xgboost", envname = mlflow:::mlflow_conda_env_name())
 reticulate::conda_install("h2o", envname = mlflow:::mlflow_conda_env_name())
-reticulate::conda_install("h2o-py", envname = mlflow:::mlflow_conda_env_name())
+reticulate::conda_install(paste0("h2o-py==", packageVersion("h2o")), envname = mlflow:::mlflow_conda_env_name())
 
 # TODO(harupy): Add `error_on = "note"` once the issue below has been fixed:
 # https://stackoverflow.com/questions/63613301/r-cmd-check-note-unable-to-verify-current-time/63616156#63616156
