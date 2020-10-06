19b8ac3711b7889372f40e228254763860eb8c2b
Jules Damji <dmatrix@comcast.net>
2020-10-06 08:59:59 -0700

    Remove exception from log_batch; simplified list_artifacts as part of PR review feedback
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

--- mlflow/tracking/client.py
+++ mlflow/tracking/client.py
@@ -713,22 +713,15 @@ class MlflowClient(object):
             metrics = [Metric('m', 1.5, timestamp, 1)]
             params = [Param("p", 'p')]
             tags = [RunTag("t", "t")]
-            failed = False
             experiment_id = "0"
             client = MlflowClient()
             run = client.create_run(experiment_id)
 
-            # Log entities, terminate the run, and fetch updated run status
-            try:
-                client.log_batch(run.info.run_id, metrics=metrics, params=params, tags=tags)
-            except MlflowException as ex:
-                failed = True
-                print(ex.message)
-            finally:
-                client.set_terminated(run.info.run_id) if not failed else
-                    client.set_terminated(run.info.run_id, status="FAILED")
-                run = client.get_run(run.info.run_id)
-                print_run_info(run)
+            # Log entities, terminate the run, and fetch run status
+            client.log_batch(run.info.run_id, metrics=metrics, params=params, tags=tags)
+            client.set_terminated(run.info.run_id)
+            run = client.get_run(run.info.run_id)
+            print_run_info(run)
 
         .. code-block:: text
             :caption: Output
@@ -859,9 +852,9 @@ class MlflowClient(object):
             run = client.create_run(experiment_id)
 
             # Create some artifacts and log under the above run
-            for file in ["features", "labels"]:
+            for file, content in [("features", features), ("labels", labels)]:
                 with open("{}.txt".format(file), 'w') as f:
-                    f.write(features) if file == "features" else f.write(labels)
+                    f.write(content)
                 client.log_artifact(run.info.run_id, "{}.txt".format(file))
 
             # Fetch the logged artifacts
