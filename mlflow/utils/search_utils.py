2b7c393c518cd0728be444d39105720f88c01f74
harupy <17039389+harupy@users.noreply.github.com>
2020-10-13 07:50:28 +0900

    Unpin sqlparse
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/utils/search_utils.py
+++ mlflow/utils/search_utils.py
@@ -260,6 +260,10 @@ class SearchUtils(object):
             return False
         elif token.match(ttype=TokenType.Keyword, values=["AND", "IN"]):
             return False
+        elif token.match(ttype=TokenType.Comparison, values=["IN"]):
+            # `IN` is a comparison token in sqlparse >= 0.4.0:
+            # https://github.com/andialbrecht/sqlparse/pull/567/files
+            return False
         else:
             return True
 
