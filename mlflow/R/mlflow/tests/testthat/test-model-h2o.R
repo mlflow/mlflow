27a45c71ab56fda000d8177ad87097c8bc4c5648
harupy <17039389+harupy@users.noreply.github.com>
2020-10-12 12:01:15 +0900

    Use cran version
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/tests/testthat/test-model-h2o.R
+++ mlflow/R/mlflow/tests/testthat/test-model-h2o.R
@@ -17,7 +17,7 @@ pkgs <- c("RCurl","jsonlite")
 for (pkg in pkgs) {
   if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
 }
-install.packages("h2o", type="source", repos=(c("http://h2o-release.s3.amazonaws.com/h2o/latest_stable_R")))
+install.packages("h2o")
 
 h2o::h2o.init()
 
