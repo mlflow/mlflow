b8e2e84990e133bac9a6b825d8073752d9603895
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-10-17 03:13:53 +0900

    [R] Create a test conda env in a separate step (#3533)
    * Separate test environemtn creation and test
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * Add cd
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * fix cd path
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * Add .create-test-env.R to .Rbuildignore
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

---
+++ mlflow/R/mlflow/.create-test-env.R
@@ -0,0 +1,13 @@
+parent_dir <- dir("../", full.names = TRUE)
+package <- parent_dir[grepl("mlflow_", parent_dir)]
+install.packages(package)
+
+mlflow:::mlflow_maybe_create_conda_env(python_version = "3.6")
+library(reticulate)
+use_condaenv(mlflow:::mlflow_conda_env_name())
+# pinning tensorflow version to 1.14 until test_keras_model.R is fixed
+keras::install_keras(method = "conda", envname = mlflow:::mlflow_conda_env_name(), tensorflow="1.15.2")
+reticulate::conda_install(Sys.getenv("MLFLOW_HOME", "../../../../."), envname = mlflow:::mlflow_conda_env_name(), pip = TRUE)
+reticulate::conda_install("xgboost", envname = mlflow:::mlflow_conda_env_name())
+# Pin h2o to prevent version-mismatch between python and R
+reticulate::conda_install("h2o==3.30.1.3", envname = mlflow:::mlflow_conda_env_name(), pip = TRUE)
