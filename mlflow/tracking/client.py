320512e68ddc820b8269201343738c6b65d7d212
Jules Damji <dmatrix@comcast.net>
2020-10-17 15:24:56 -0700

    Added delete_registered_model_tag() code snippet
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

--- mlflow/tracking/client.py
+++ mlflow/tracking/client.py
@@ -1439,6 +1439,48 @@ class MlflowClient(object):
         :param name: Registered model name.
         :param key: Registered model tag key.
         :return: None
+
+        .. code-block:: python
+            :caption: Example
+
+            import mlflow
+            from mlflow.tracking import MlflowClient
+
+            def print_registered_models_info(r_models):
+                print("--")
+                for rm in r_models:
+                    print("name: {}".format(rm.name))
+                    print("tags: {}".format(rm.tags))
+
+            mlflow.set_tracking_uri("sqlite:///mlruns.db")
+            client = MlflowClient()
+
+            # Register a couple of models with respective names and tags
+            for name, tags in [("name1", {"t1": "t1"}),("name2", {"t2": "t2"})]:
+            client.create_registered_model(name, tags)
+
+            # Fetch all registered models
+            models = client.list_registered_models()
+            print_registered_models_info(models)
+
+            # Delete a tag from model `name2`
+            client.delete_registered_model_tag("name2", 't2')
+            models = client.list_registered_models()
+            print_registered_models_info(models)
+
+        .. code-block:: text
+            :caption: Output
+
+            --
+            name: name1
+            tags: {'t1': 't1'}
+            name: name2
+            tags: {'t2': 't2'}
+            --
+            name: name1
+            tags: {'t1': 't1'}
+            name: name2
+            tags: {}
         """
         self._get_registry_client().delete_registered_model_tag(name, key)
 
