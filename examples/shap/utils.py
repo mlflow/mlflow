c411fa40ee8a8fdfb4ccf90d102e583fe2f9bb54
harupy <17039389+harupy@users.noreply.github.com>
2020-10-19 10:30:20 +0900

    Add docstring
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- examples/shap/utils.py
+++ examples/shap/utils.py
@@ -3,12 +3,19 @@ import pandas as pd
 
 
 def to_pandas_Xy(dataset):
+    """
+    Extracts (data, target) from a scikit-learn dataset and returns them as a pandas DataFrame
+    and Series.
+    """
     X = pd.DataFrame(dataset.data, columns=dataset.feature_names)
     y = pd.Series(dataset.target, name="target")
     return X, y
 
 
 def show_image(path):
+    """
+    Reads an image from a file and shows it.
+    """
     img = plt.imread(path)
     plt.imshow(img)
     plt.axis("off")
