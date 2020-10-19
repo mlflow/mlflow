5a272eb4807647fee644c122770153637a6285c6
harupy <17039389+harupy@users.noreply.github.com>
2020-10-19 10:05:41 +0900

    Add binary classification example
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

---
+++ examples/shap/binary_classification.py
@@ -0,0 +1,42 @@
+import os
+
+import numpy as np
+from sklearn.datasets import load_breast_cancer
+from sklearn.ensemble import RandomForestClassifier
+
+import mlflow
+from utils import to_dataframe, show_image
+
+
+# prepare training data
+X, y = to_dataframe(load_breast_cancer())
+X = X.iloc[:50, :8]
+y = y.iloc[:50]
+
+# train a model
+model = RandomForestClassifier()
+model.fit(X, y)
+
+# log an explanation
+with mlflow.start_run() as run:
+    mlflow.shap.log_explanation(lambda X: model.predict_proba(X)[:, 1], X)
+
+# list artifacts
+client = mlflow.tracking.MlflowClient()
+artifact_path = "model_explanations_shap"
+artifacts = [x.path for x in client.list_artifacts(run.info.run_id, artifact_path)]
+print("# artifacts:")
+print(artifacts)
+
+# load back the logged explanation
+dst_path = client.download_artifacts(run.info.run_id, artifact_path)
+base_values = np.load(os.path.join(dst_path, "base_values.npy"))
+shap_values = np.load(os.path.join(dst_path, "shap_values.npy"))
+
+print("\n# base_values")
+print(base_values)
+print("\n# shap_values")
+print(shap_values[:3])
+
+# show the summary bar plot
+show_image(os.path.join(dst_path, "summary_bar_plot.png"))
