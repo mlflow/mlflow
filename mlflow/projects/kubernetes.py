00453649aa944bd6a2941a1957162472332f24e8
Poruri Sai Rahul <rporuri@enthought.com>
2020-10-19 10:22:13 +0000

    Almost remove dependence on six (#3538)
    * CLN : Remove use of six.iteritems
    
    	modified:   examples/multistep_workflow/main.py
    	modified:   mlflow/tracking/_tracking_service/client.py
    
    Signed-off-by: Sai Rahul Poruri <rporuri@enthought.com>
    
    * CLN : Remove use of six.moves.shlex_quote
    
    and replace it with shlex.quote from the standard library
    
    	modified:   mlflow/projects/_project_spec.py
    	modified:   mlflow/projects/databricks.py
    	modified:   mlflow/projects/kubernetes.py
    	modified:   mlflow/rfunc/backend.py
    
    Signed-off-by: Sai Rahul Poruri <rporuri@enthought.com>
    
    * CLN : Replace use of six.string_types
    
    with just str
    
    	modified:   mlflow/utils/string_utils.py
    
    Signed-off-by: Sai Rahul Poruri <rporuri@enthought.com>
    
    * CLN : Replace use of six.moves.reload_module
    
    with importlib.reload
    
    	modified:   tests/store/artifact/test_artifact_repository_registry.py
    	modified:   tests/tracking/_tracking_service/test_utils.py
    	modified:   tests/tracking/context/test_registry.py
    	modified:   tests/tracking/test_fluent.py
    
    Signed-off-by: Sai Rahul Poruri <rporuri@enthought.com>
    
    * CLN : Remove conditionals that use six.PY2
    
    	modified:   tests/pyfunc/test_model_export_with_loader_module_and_data_path.py
    	modified:   tests/utils/test_file_utils.py
    
    Signed-off-by: Sai Rahul Poruri <rporuri@enthought.com>
    
    * CLN : Replace six.wraps
    
    with functools.wraps
    
    	modified:   tests/sagemaker/test_deployment.py
    
    Signed-off-by: Sai Rahul Poruri <rporuri@enthought.com>
    
    * CLN : Replace use of six.text_type
    
    with just str
    
    	modified:   tests/spacy/test_spacy_model_export.py
    
    Signed-off-by: Sai Rahul Poruri <rporuri@enthought.com>
    
    * FIX : Replace one more use of six.iteritems
    
    with just dict.items
    
    	modified:   tests/store/tracking/test_rest_store.py
    
    Signed-off-by: Sai Rahul Poruri <rporuri@enthought.com>
    
    * FIX : Fix a silly mistake when importing shlex
    
    Signed-off-by: Sai Rahul Poruri <rporuri@enthought.com>
    
    	modified:   mlflow/projects/kubernetes.py

--- mlflow/projects/kubernetes.py
+++ mlflow/projects/kubernetes.py
@@ -13,7 +13,7 @@ from mlflow.projects.submitted_run import SubmittedRun
 from mlflow.entities import RunStatus
 
 from shlex import split
-from six.moves import shlex_quote as quote
+from shlex import quote
 
 _logger = logging.getLogger(__name__)
 
