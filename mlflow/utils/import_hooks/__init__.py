fc8c64afe083e6bdd7d113c568fb5194456b9402
Corey Zumar <corey.zumar@databricks.com>
2020-10-20 15:40:26 -0500

    Docs
    Signed-off-by: Andrew Nitu <andrewnitu@gmail.com>

--- mlflow/utils/import_hooks/__init__.py
+++ mlflow/utils/import_hooks/__init__.py
@@ -138,16 +138,23 @@ def register_generic_import_hook(hook, name, hook_dict, overwrite):
 
 @synchronized(_import_error_hooks_lock)
 def register_import_error_hook(hook, name, overwrite=True):
+    """
+    :param hook: A function or string entrypoint to invoke when the specified module is imported
+                 and an error occurs.
+    :param name: The name of the module for which to fire the hook at import error detection time.
+    :param overwrite: Specifies the desired behavior when a preexisting hook for the same
+                      function / entrypoint already exists for the specified module. If `True`,
+                      all preexisting hooks matching the specified function / entrypoint will be
+                      removed and replaced with a single instance of the specified `hook`.
+    """
     register_generic_import_hook(hook, name, _import_error_hooks, overwrite)
 
 
 @synchronized(_post_import_hooks_lock)
 def register_post_import_hook(hook, name, overwrite=True):
     """
-    :param hook: A function or string entrypoint to invoke
-                 when the specified module is imported.
-    :param name: The name of the module for which to fire the
-                 hook at import time.
+    :param hook: A function or string entrypoint to invoke when the specified module is imported.
+    :param name: The name of the module for which to fire the hook at import time.
     :param overwrite: Specifies the desired behavior when a preexisting hook for the same
                       function / entrypoint already exists for the specified module. If `True`,
                       all preexisting hooks matching the specified function / entrypoint will be
