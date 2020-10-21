8d820b0cba76a46aeef67fde9ee47ef3ac646733
harupy <17039389+harupy@users.noreply.github.com>
2020-10-21 11:46:06 +0900

    error
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/__init__.py
+++ mlflow/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=wrong-import-position
 """
-The ``mlflow`` module provides a high-level "fluent" API for starting and managing MLflow runs.
+The ``mlflow`` module provides a high-level "fluent" API for starting and managing MLflow runs. Yes foo
 For example:
 
 .. code:: python
