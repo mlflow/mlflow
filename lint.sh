d8b8b55e8323cd184f017e2338c272d31c1a26ee
harupy <17039389+harupy@users.noreply.github.com>
2020-10-21 10:44:23 +0900

    fix script
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- lint.sh
+++ lint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-set -e
+# Set err=1 if any commands exit with non-zero status as described in
+# https://stackoverflow.com/a/42219754
+err=0
+trap 'err=1' ERR
 
 FWDIR="$(cd "`dirname $0`"; pwd)"
 cd "$FWDIR"
@@ -26,13 +29,16 @@ exclude_dirs=(
   "mlflow/temporary_db_migrations_for_pre_1_users"
 )
 
+echo -e "\n========== black ==========\n"
 # Exclude proto files because they are auto-generated
 black --check --line-length=100 --exclude=mlflow/protos .
 
+echo -e "\n========== pycodestyle ==========\n"
 exclude=$(join "," "${exclude_dirs[@]}")
 include=$(join " " "${include_dirs[@]}")
 pycodestyle --max-line-length=100 --ignore=E203,W503 --exclude=$exclude -- $include
 
+echo -e "\n========== pylint ==========\n" "pylint"
 # pylint's `--ignore` option filters files based on their base names, not paths.
 # see: http://pylint.pycqa.org/en/latest/user_guide/run.html#command-line-options
 # This behavior might cause us to unintentionally ignore some files.
@@ -46,4 +52,7 @@ msg_template="{path} ({line},{column}): [{msg_id} {symbol}] {msg}"
 git ls-files | grep $include | grep -v $exclude | \
 xargs pylint --msg-template="$msg_template" --rcfile="$FWDIR/pylintrc"
 
+echo -e "\n========== rstcheck ==========\n"
 rstcheck README.rst
+
+test $err = 0
