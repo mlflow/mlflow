15989485ac0fead18243c5d4d701ce7542e4e0ee
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-10-23 22:35:29 +0200

    only check run_id if attribute exists
    Signed-off-by: Lorenz Walthert <lorenz.walthert@icloud.com>

--- mlflow/pyfunc/__init__.py
+++ mlflow/pyfunc/__init__.py
@@ -433,7 +433,7 @@ class PyFuncModel(object):
     def __repr__(self):
         info = {}
         if self._model_meta is not None:
-            if self._model_meta.run_id is not None:
+            if hasattr(self._model_meta, "run_id") and self._model_meta.run_id is not None:
                 info["run_id"] = self._model_meta.run_id
             if self._model_meta.artifact_path is not None:
                 info["artifact_path"] = self._model_meta.artifact_path
