0527c24281688a4dd2955452985e302eec8ab735
Corey Zumar <corey.zumar@databricks.com>
2020-10-27 14:57:15 -0500

    Unpin TF
    Signed-off-by: Corey Zumar <corey.zumar@databricks.com>

--- dev/run-python-tf-tests.sh
+++ dev/run-python-tf-tests.sh
@@ -9,9 +9,7 @@ export MLFLOW_HOME=$(pwd)
 pytest --verbose tests/keras --large
 pytest --verbose tests/tensorflow/test_tensorflow_model_export.py --large
 pytest --verbose tests/tensorflow_autolog/test_tensorflow_autolog.py --large
-# TODO(smurching) Unpin TensorFlow dependency version once test failures with TF 2.1.0 have been
-# fixed
-pip install 'tensorflow==2.0.0'
+pip install 'tensorflow'
 pytest --verbose tests/tensorflow/test_tensorflow2_model_export.py --large
 pytest --verbose tests/tensorflow_autolog/test_tensorflow2_autolog.py --large
 pytest --verbose tests/keras --large
