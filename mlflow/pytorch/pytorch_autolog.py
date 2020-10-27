a77e46f82bba5a3f55dff8c6dbc5a4eb0fe2bb5f
Shrinath Suresh <shrinath@ideas2it.com>
2020-10-28 00:00:46 +0530

    Autolog for pytorch lightning with examples and unit tests
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

---
+++ mlflow/pytorch/pytorch_autolog.py
@@ -0,0 +1,197 @@
+import gorilla
+import logging
+import mlflow.pytorch
+import os
+import pytorch_lightning as pl
+import shutil
+import tempfile
+from pytorch_lightning.core.memory import ModelSummary
+from mlflow.utils.autologging_utils import try_mlflow_log
+
+logging.basicConfig(level=logging.ERROR)
+
+every_n_iter = 1
+
+
+# autolog module uses `try_mlflow_log` - mlflow utility to log param/metrics/artifacts into mlflow
+# Eventhough the same can be achieved using
+# MlflowLogger(Pytorch Lightning's inbuild class),
+# following are the downsides in using MlflowLogger.
+# 1. MlflowLogger doesn't provide a mechanism to dump an entire model into mlflow.
+#    Only model checkpoint is saved.
+# 2. For dumping the model into mlflow `mlflow.pytorch` library is used
+# and the library expects `mlflow` object to be instantiated.
+# In case of MlflowLogger, Run management is completely controlled by the class and
+# hence mlflow object needs to be reinstantiated by setting
+# tracking uri, experiment_id and run_id which may lead to a race condition.
+
+
+def autolog(log_every_n_iter=1):
+    global every_n_iter
+    every_n_iter = log_every_n_iter
+
+    class __MLflowPLCallback(pl.Callback):
+        """
+        Callback for auto-logging metrics and parameters.
+        """
+
+        def __init__(self):
+            self.early_stopping = False
+
+        def on_epoch_end(self, trainer, pl_module):
+            """
+            Log loss and other metrics values after each epoch
+
+            :param trainer: pytorch lightning model instance
+            :param pl_module: pytorch lightning base module
+            """
+            if (pl_module.current_epoch + 1) % every_n_iter == 0:
+                for key, value in trainer.callback_metrics.items():
+                    try_mlflow_log(
+                        mlflow.log_metric, key, float(value + 1), step=pl_module.current_epoch
+                    )
+
+            for callback in trainer.callbacks:
+                if isinstance(callback, pl.callbacks.early_stopping.EarlyStopping):
+                    self._early_stop_check(callback)
+
+        def on_train_start(self, trainer, pl_module):
+            """
+            Logs Optimizer related metrics when the train begins
+
+            :param trainer: pytorch lightning model instance
+            :param pl_module: pytorch lightning base module
+            """
+            mlflow.set_tag(key="Mode", value="training")
+            try_mlflow_log(mlflow.log_param, "epochs", trainer.max_epochs)
+
+            for callback in trainer.callbacks:
+                if isinstance(callback, pl.callbacks.early_stopping.EarlyStopping):
+                    self.early_stopping = True
+                    self._log_early_stop_params(callback)
+
+            if hasattr(trainer, "optimizers"):
+                for optimizer in trainer.optimizers:
+                    try_mlflow_log(mlflow.log_param, "optimizer_name", type(optimizer).__name__)
+
+                    if hasattr(optimizer, "defaults"):
+                        defaults_dict = optimizer.defaults
+                        if "lr" in defaults_dict:
+                            try_mlflow_log(mlflow.log_param, "learning_rate", defaults_dict["lr"])
+
+                        if "eps" in defaults_dict:
+                            try_mlflow_log(mlflow.log_param, "epsilon", defaults_dict["eps"])
+
+            summary = str(ModelSummary(pl_module, mode="full"))
+            tempdir = tempfile.mkdtemp()
+            try:
+                summary_file = os.path.join(tempdir, "model_summary.txt")
+                with open(summary_file, "w") as f:
+                    f.write(summary)
+
+                try_mlflow_log(mlflow.log_artifact, local_path=summary_file)
+            finally:
+                shutil.rmtree(tempdir)
+
+        def on_train_end(self, trainer, pl_module):
+            """
+            Logs the model checkpoint into mlflow - models folder on the training end
+
+            :param trainer: pytorch lightning model instance
+            :param pl_module: pytorch lightning base module
+            """
+
+            mlflow.pytorch.log_model(pytorch_model=trainer.model, artifact_path="models")
+
+            if self.early_stopping and trainer.checkpoint_callback.best_model_path:
+                try_mlflow_log(
+                    mlflow.log_artifact,
+                    local_path=trainer.checkpoint_callback.best_model_path,
+                    artifact_path="restored_model_checkpoint",
+                )
+
+        def on_test_end(self, trainer, pl_module):
+            """
+            Logs accuracy and other relevant metrics on the testing end
+
+            :param trainer: pytorch lightning model instance
+            :param pl_module: pytorch lightning base module
+            """
+            mlflow.set_tag(key="Mode", value="testing")
+            for key, value in trainer.callback_metrics.items():
+                try_mlflow_log(mlflow.log_metric, key, float(value))
+
+        @staticmethod
+        def _log_early_stop_params(early_stop_obj):
+            """
+            Logs Early Stop parameters into mlflow
+
+            :param early_stop_obj: Early stopping callback dict
+            """
+            if hasattr(early_stop_obj, "monitor"):
+                try_mlflow_log(mlflow.log_param, "monitor", early_stop_obj.monitor)
+
+            if hasattr(early_stop_obj, "mode"):
+                try_mlflow_log(mlflow.log_param, "mode", early_stop_obj.mode)
+
+            if hasattr(early_stop_obj, "patience"):
+                try_mlflow_log(mlflow.log_param, "patience", early_stop_obj.patience)
+
+            if hasattr(early_stop_obj, "min_delta"):
+                try_mlflow_log(mlflow.log_param, "min_delta", early_stop_obj.min_delta)
+
+            if hasattr(early_stop_obj, "stopped_epoch"):
+                try_mlflow_log(mlflow.log_param, "stopped_epoch", early_stop_obj.stopped_epoch)
+
+        @staticmethod
+        def _early_stop_check(early_stop_callback):
+            """
+            Logs all early stopping metrics
+
+            :param early_stop_callback: Early stopping callback object
+            """
+            if early_stop_callback.stopped_epoch != 0:
+
+                if hasattr(early_stop_callback, "stopped_epoch"):
+                    try_mlflow_log(
+                        mlflow.log_metric, "Stopped_Epoch", early_stop_callback.stopped_epoch
+                    )
+                    restored_epoch = early_stop_callback.stopped_epoch - max(
+                        1, early_stop_callback.patience
+                    )
+                    try_mlflow_log(mlflow.log_metric, "Restored_Epoch", restored_epoch)
+
+                if hasattr(early_stop_callback, "best_score"):
+                    try_mlflow_log(mlflow.log_metric, "Best_Score", early_stop_callback.best_score)
+
+                if hasattr(early_stop_callback, "wait_count"):
+                    try_mlflow_log(mlflow.log_metric, "Wait_Count", early_stop_callback.wait_count)
+
+    def _run_and_log_function(self, original, args, kwargs):
+        """
+        This method would be called from patched fit method and
+        It adds the custom callback class into callback list.
+        """
+        if not mlflow.active_run():
+            try_mlflow_log(mlflow.start_run)
+            auto_end_run = True
+        else:
+            auto_end_run = False
+
+        if not any(isinstance(callbacks, __MLflowPLCallback) for callbacks in self.callbacks):
+            self.callbacks += [__MLflowPLCallback()]
+        result = original(self, *args, **kwargs)
+        if auto_end_run:
+            try_mlflow_log(mlflow.end_run)
+        return result
+
+    @gorilla.patch(pl.Trainer)
+    def fit(self, *args, **kwargs):
+        """
+        Patching trainer.fit method to add autolog class into callback
+        """
+        original = gorilla.get_original_attribute(pl.Trainer, "fit")
+        return _run_and_log_function(self, original, args, kwargs)
+
+    settings = gorilla.Settings(allow_hit=True, store_hit=True)
+    gorilla.apply(gorilla.Patch(pl.Trainer, "fit", fit, settings=settings))
---
+++ tests/pytorch/test_pytorch_autolog.py
@@ -0,0 +1,168 @@
+import os
+import pytest
+import pytorch_lightning as pl
+import torch
+from iris import IrisClassification
+import mlflow
+import mlflow.pytorch
+from mlflow.pytorch.pytorch_autolog import autolog
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+NUM_EPOCHS = 20
+
+
+@pytest.fixture
+def pytorch_model():
+    autolog()
+    model = IrisClassification()
+    trainer = pl.Trainer(max_epochs=NUM_EPOCHS)
+    trainer.fit(model)
+    client = mlflow.tracking.MlflowClient()
+    run = client.get_run(client.list_run_infos(experiment_id="0")[0].run_id)
+    return trainer, run
+
+
+def test_pytorch_autolog_logs_default_params(pytorch_model):
+    _, run = pytorch_model
+    data = run.data
+    assert "learning_rate" in data.params
+    assert "epsilon" in data.params
+    assert "optimizer_name" in data.params
+
+
+def test_pytorch_autolog_logs_expected_data(pytorch_model):
+    _, run = pytorch_model
+    data = run.data
+
+    # Checking if metrics are logged
+    assert "loss" in data.metrics
+    assert "val_loss" in data.metrics
+
+    # Testing unwanted parameters are not logged
+    assert "callbacks" not in data.params
+
+    # Testing optimizer parameters are logged
+    assert "optimizer_name" in data.params
+    assert data.params["optimizer_name"] == "Adam"
+
+    # Testing model_summary.txt is saved
+    client = mlflow.tracking.MlflowClient()
+    artifacts = client.list_artifacts(run.info.run_id)
+    artifacts = map(lambda x: x.path, artifacts)
+    assert "model_summary.txt" in artifacts
+
+
+@pytest.fixture
+def pytorch_model_initialized():
+    trainer = pl.Trainer(max_epochs=NUM_EPOCHS)
+    model = IrisClassification()
+    trainer.fit(model)
+    return trainer
+
+
+def test_pytorch_autolog_persists_manually_created_run(pytorch_model_initialized):
+    _ = pytorch_model_initialized
+    autolog()
+    with mlflow.start_run() as manual_run:
+        assert mlflow.active_run()
+        assert mlflow.active_run().info.run_id == manual_run.info.run_id
+
+
+def test_pytorch_autolog_ends_auto_created_run(pytorch_model_initialized):
+    _ = pytorch_model_initialized
+    autolog()
+    assert mlflow.active_run() is None
+
+
+@pytest.fixture
+def pytorch_model_with_callback(patience):
+    autolog()
+    model = IrisClassification()
+    early_stopping = EarlyStopping(monitor="val_loss", mode="min", patience=patience, verbose=True)
+
+    checkpoint_callback = ModelCheckpoint(
+        filepath=os.getcwd(), save_top_k=1, verbose=True, monitor="val_loss", mode="min", prefix=""
+    )
+
+    trainer = pl.Trainer(
+        max_epochs=NUM_EPOCHS * 2,
+        callbacks=[early_stopping],
+        checkpoint_callback=checkpoint_callback,
+    )
+    trainer.fit(model)
+
+    client = mlflow.tracking.MlflowClient()
+    run = client.get_run(client.list_run_infos(experiment_id="0")[0].run_id)
+
+    return trainer, run
+
+
+@pytest.mark.parametrize("patience", [3])
+def test_pytorch_early_stop_artifacts_logged(pytorch_model_with_callback):
+    _, run = pytorch_model_with_callback
+    client = mlflow.tracking.MlflowClient()
+    artifacts = client.list_artifacts(run.info.run_id)
+    artifacts = map(lambda x: x.path, artifacts)
+    assert "restored_model_checkpoint" in artifacts
+
+
+@pytest.mark.parametrize("patience", [3])
+def test_pytorch_autolog_model_can_load_from_artifact(pytorch_model_with_callback):
+    _, run = pytorch_model_with_callback
+    run_id = run.info.run_id
+    client = mlflow.tracking.MlflowClient()
+    artifacts = client.list_artifacts(run_id)
+    artifacts = map(lambda x: x.path, artifacts)
+    assert "models" in artifacts
+    model = mlflow.pytorch.load_model("runs:/" + run_id + "/models")
+    result = model(torch.Tensor([1.5, 2, 2.5, 3.5]).unsqueeze(0))
+    assert result is not None
+
+
+@pytest.mark.parametrize("patience", [0, 1, 5])
+def test_pytorch_early_stop_params_logged(pytorch_model_with_callback, patience):
+    _, run = pytorch_model_with_callback
+    data = run.data
+    assert "monitor" in data.params
+    assert "mode" in data.params
+    assert "patience" in data.params
+    assert float(data.params["patience"]) == patience
+    assert "min_delta" in data.params
+    assert "stopped_epoch" in data.params
+
+
+@pytest.mark.parametrize("patience", [3])
+def test_pytorch_early_stop_metrics_logged(pytorch_model_with_callback):
+    _, run = pytorch_model_with_callback
+    data = run.data
+    assert "Stopped_Epoch" in data.metrics
+    assert "Wait_Count" in data.metrics
+    assert "Restored_Epoch" in data.metrics
+
+
+def test_pytorch_autolog_non_early_stop_callback_does_not_log(pytorch_model):
+    trainer, run = pytorch_model
+    client = mlflow.tracking.MlflowClient()
+    metric_history = client.get_metric_history(run.info.run_id, "loss")
+    assert trainer.max_epochs == NUM_EPOCHS
+    assert len(metric_history) == NUM_EPOCHS
+
+
+@pytest.fixture
+def pytorch_model_tests():
+    model = IrisClassification()
+
+    trainer = pl.Trainer(max_epochs=NUM_EPOCHS)
+    trainer.fit(model)
+    trainer.test()
+    client = mlflow.tracking.MlflowClient()
+    run = client.get_run(client.list_run_infos(experiment_id="0")[0].run_id)
+    return trainer, run
+
+
+def test_pytorch_test_metrics_logged(pytorch_model_tests):
+    _, run = pytorch_model_tests
+    data = run.data
+    assert "test_loss" in data.metrics
+    assert "test_acc" in data.metrics
