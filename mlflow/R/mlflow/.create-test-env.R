3e9153a18c751be04a51fbdee552ac6bf328eb2f
harupy <17039389+harupy@users.noreply.github.com>
2020-11-02 10:26:07 +0900

    nit
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/.create-test-env.R
+++ mlflow/R/mlflow/.create-test-env.R
@@ -7,7 +7,7 @@ library(reticulate)
 use_condaenv(mlflow:::mlflow_conda_env_name())
 # pinning tensorflow version to 1.14 until test_keras_model.R is fixed
 keras::install_keras(method = "conda", envname = mlflow:::mlflow_conda_env_name(), tensorflow="1.15.2")
-# pinning h5py < 3 to avoid this issue: https://github.com/h5py/h5py/issues/1732
+# pinning h5py < 3.0.0 to avoid this issue: https://github.com/h5py/h5py/issues/1732
 reticulate::conda_install("'h5py<3.0.0'", envname = mlflow:::mlflow_conda_env_name(), pip = TRUE)
 reticulate::conda_install(Sys.getenv("MLFLOW_HOME", "../../../../."), envname = mlflow:::mlflow_conda_env_name(), pip = TRUE)
 reticulate::conda_install("xgboost", envname = mlflow:::mlflow_conda_env_name())
