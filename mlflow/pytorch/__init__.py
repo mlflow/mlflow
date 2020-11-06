71301baab5d328de760f1ab94371c0563f3d6002
Sid Murching <sid.murching@databricks.com>
2020-11-06 10:26:21 -0800

    Update docs, fix autologging docstring
    Signed-off-by: Sid Murching <sid.murching@databricks.com>

--- mlflow/pytorch/__init__.py
+++ mlflow/pytorch/__init__.py
@@ -614,15 +614,14 @@ class _PyTorchWrapper(object):
 
 def autolog(log_every_n_epoch=1):
     """
-    Wrapper for `mlflow.pytorch._pytorch_autolog.autolog` method.
     Automatically log metrics, params, and models from `PyTorch Lightning
     <https://pytorch-lightning.readthedocs.io/en/latest>`_ model training.
-    Autologging is performed when you call the `fit` method of `pytorch_lightning.Trainer()
-    <https://pytorch-lightning.readthedocs.io/en/latest/trainer.html#>`_.
+    Autologging is performed when you call the `fit` method of
+    `pytorch_lightning.Trainer() <https://pytorch-lightning.readthedocs.io/en/latest/trainer.html#>`_.
 
     **Note**: Autologging is only supported for PyTorch Lightning models,
-    i.e. models that subclass `pytorch_lightning.LightningModule
-    <https://pytorch-lightning.readthedocs.io/en/latest/lightning_module.html>`_.
+    i.e. models that subclass
+    `pytorch_lightning.LightningModule <https://pytorch-lightning.readthedocs.io/en/latest/lightning_module.html>`_.
     In particular, autologging support for vanilla Pytorch models that only subclass
     `torch.nn.Module <https://pytorch.org/docs/stable/generated/torch.nn.Module.html>`
     is not yet available.
