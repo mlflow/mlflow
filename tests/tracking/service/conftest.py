b8df785be04d59bbabc5a9f563d61ba67336bd96
Eduardo de Leon <eddeleon@microsoft.com>
2020-11-11 00:33:44 -0500

    Try and fix experiment id issue in mocking fixture
    Signed-off-by: Eduardo de Leon <eddeleon@microsoft.com>

--- tests/tracking/service/conftest.py
+++ tests/tracking/service/conftest.py
@@ -4,11 +4,18 @@ import mlflow
 
 
 @pytest.fixture(autouse=True)
+@pytest.mark.usefixtures("reset_active_experiment")
 def tracking_uri_mock(tmpdir, request):
-    from mlflow.utils.file_utils import path_to_local_file_uri
+    try:
+        from mlflow.utils.file_utils import path_to_local_file_uri
 
-    tracking_uri = path_to_local_file_uri(os.path.join(tmpdir.strpath, "mlruns"))
-    mlflow.set_tracking_uri(tracking_uri)
-    if "notrackingurimock" not in request.keywords:
-        if "MLFLOW_TRACKING_URI" in os.environ:
+        tracking_uri = path_to_local_file_uri(os.path.join(tmpdir.strpath, "mlruns"))
+        mlflow.set_tracking_uri(tracking_uri)
+        if "notrackingurimock" not in request.keywords:
+            if "MLFLOW_TRACKING_URI" in os.environ:
+                del os.environ["MLFLOW_TRACKING_URI"]
+        yield tmpdir
+    finally:
+        mlflow.set_tracking_uri(None)
+        if "notrackingurimock" not in request.keywords:
             del os.environ["MLFLOW_TRACKING_URI"]
