6143eab5992e30403db3b2e842ad3e80ad69dd89
Eduardo de Leon <eddeleon@microsoft.com>
2020-11-11 00:38:20 -0500

    Make the fixture map the sqlite one
    Signed-off-by: Eduardo de Leon <eddeleon@microsoft.com>

--- tests/tracking/service/conftest.py
+++ tests/tracking/service/conftest.py
@@ -9,11 +9,10 @@ def tracking_uri_mock(tmpdir, request):
     try:
         from mlflow.utils.file_utils import path_to_local_file_uri
 
-        tracking_uri = path_to_local_file_uri(os.path.join(tmpdir.strpath, "mlruns"))
-        mlflow.set_tracking_uri(tracking_uri)
         if "notrackingurimock" not in request.keywords:
-            if "MLFLOW_TRACKING_URI" in os.environ:
-                del os.environ["MLFLOW_TRACKING_URI"]
+            tracking_uri = path_to_local_file_uri(os.path.join(tmpdir.strpath, "mlruns"))
+            mlflow.set_tracking_uri(tracking_uri)
+            os.environ["MLFLOW_TRACKING_URI"] = tracking_uri
         yield tmpdir
     finally:
         mlflow.set_tracking_uri(None)
