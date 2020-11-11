6842aca5a66e545838e7750fff7485cd5a9c2e91
Shrinath Suresh <shrinath@ideas2it.com>
2020-11-12 02:28:34 +0530

    Enabling MNIST example 2 in CLI
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -57,7 +57,7 @@ def replace_mlflow_with_dev_version(yml_path):
         ),
         ("fastai", ["-P", "lr=0.02", "-P", "epochs=3"]),
         # (os.path.join("pytorch", "MNIST/example1"), ["-P", "max_epochs=1"]),
-        # (os.path.join("pytorch", "MNIST/example2"), ["-P", "max_epochs=1"]),
+        (os.path.join("pytorch", "MNIST/example2"), ["-P", "max_epochs=1"]),
         # (
         #     os.path.join("pytorch", "BertNewsClassification"),
         #     ["-P", "max_epochs=1", "-P", "num_samples=100"],
@@ -119,10 +119,10 @@ def test_mlflow_run_example(directory, params, tmpdir):
         ("shap", ["python", "multiclass_classification.py"]),
         ("pytorch/MNIST/example1", ["python", "mnist_autolog_example1.py", "--max_epochs", "1"]),
         ("pytorch/MNIST/example2", ["python", "mnist_autolog_example2.py", "--max_epochs", "1"]),
-        (
-            "pytorch/BertNewsClassification",
-            ["python", "bert_classification.py", "--max_    epochs", "1", "--num-samples", "100"],
-        ),
+        # (
+        #     "pytorch/BertNewsClassification",
+        #     ["python", "bert_classification.py", "--max_    epochs", "1", "--num-samples", "100"],
+        # ),
     ],
 )
 def test_command_example(directory, command):
