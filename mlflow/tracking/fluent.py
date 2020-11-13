9f3ec3cbd3e7b3d9b21fc2d928f4514eb79294c1
harupy <17039389+harupy@users.noreply.github.com>
2020-11-13 15:50:20 +0900

    Fix comments in example
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/tracking/fluent.py
+++ mlflow/tracking/fluent.py
@@ -571,10 +571,10 @@ def log_text(text, artifact_file):
         import mlflow
 
         with mlflow.start_run():
-            # Log text in the artifact root
+            # Log text to a file under the run's root artifact directory
             mlflow.log_text("text1", "file1.txt")
 
-            # Log text in a subdirectory
+            # Log text in a subdirectory of the run's root artifact directory
             mlflow.log_text("text2", "dir/file2.txt")
 
             # Log HTML text
