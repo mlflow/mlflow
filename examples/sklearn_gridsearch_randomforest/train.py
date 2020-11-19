9cfc2ec4ffd04c0015eadce6f896d179ca0a7388
jsupanova <jonathan.m.novak@gmail.com>
2020-11-19 16:30:53 -0600

    Example of logging metrics for sklearn gridsearch
    This example shows how to find the best random forest model using grid search and outputting the best model, performance metric, and best parameters to MLflow.

---
+++ examples/sklearn_gridsearch_randomforest/train.py
@@ -0,0 +1,28 @@
+#Import mlflow
+import mlflow
+import mlflow.sklearn
+
+#Import sklearn
+from sklearn.datasets import load_breast_cancer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+#Load data
+data = load_breast_cancer()
+x = data['data']
+y = data['target']
+
+#Build model using grid search and cross fold validation
+parameters = {'n_estimators':[10,50,100], 'criterion':['gini','entropy'], 'max_depth':[3,5,10]}
+metrics = scoring = ['f1','recall','precision','roc_auc','neg_log_loss','neg_brier_score'
+                     ,'average_precision','balanced_accuracy']
+rf = RandomForestClassifier(max_depth=2, random_state=0)
+clf = GridSearchCV(rf, parameters, scoring=metrics,refit='f1')
+clf.fit(x, y)
+
+#Log artifacts to MLflow
+mlflow.sklearn.log_model(clf.best_estimator_, "best model")
+mlflow.log_metric('best score',clf.best_score_)
+for k in clf.best_params_.keys():
+  mlflow.log_param(k, clf.best_params_[k])
+print("Model saved in run %s" % mlflow.active_run().info.run_uuid)
