7bc05d09273df7096ac4a1e420d3dc228b5d7492
Mohamad Arabi <mohamad.arabi@databricks.com>
2020-11-19 21:05:41 -0800

    fix variable
    Signed-off-by: Mohamad Arabi <mohamad.arabi@databricks.com>

--- mlflow/pytorch/_pytorch_autolog.py
+++ mlflow/pytorch/_pytorch_autolog.py
@@ -123,7 +123,7 @@ def _autolog(log_every_n_epoch=1, log_models=True):
                 :param trainer: pytorch lightning trainer instance
                 :param pl_module: pytorch lightning base module
                 """
-                if self.log_models:
+                if log_models:
                     mlflow.pytorch.log_model(pytorch_model=trainer.model, artifact_path="model")
 
                     if self.early_stopping and trainer.checkpoint_callback.best_model_path:
