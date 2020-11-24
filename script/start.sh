af517c53bc16cff1c38d45a5a2077587f1d83388
Eric Carlson <eric.carlson@grandrounds.com>
2020-11-24 15:17:47 -0700

    Patch MLFlow

--- script/start.sh
+++ script/start.sh
@@ -2,7 +2,7 @@ set -x
 set -e
 
 mlflow server \
-    --backend-store-uri "$JOBSERVER_RDB_URL"
+    --backend-store-uri "$JOBSERVER_RDB_URL" \
     --default-artifact-root "s3://grnds-$AWS_ENVIRONMENT-cortex-shared/mlflow" \
     --host 0.0.0.0 \
     --port 5000
