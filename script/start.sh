c8a7e6f8628f0a939513c16c9205f08d677b54ed
Eric Carlson <eric.carlson@grandrounds.com>
2020-11-24 16:22:03 -0700

    add shebang

--- script/start.sh
+++ script/start.sh
@@ -1,8 +1,12 @@
+#!/usr/bin/env bash
 set -x
 set -e
+
+mlflow db upgrade "$JOBSERVER_RDB_URL"
 
 mlflow server \
     --backend-store-uri "$JOBSERVER_RDB_URL" \
     --default-artifact-root "s3://grnds-$AWS_ENVIRONMENT-cortex-shared/mlflow" \
     --host 0.0.0.0 \
     --port 5000
+
