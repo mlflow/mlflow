0ddf622e0fe3e619321e36e98580bd4afe9ed00b
harupy <17039389+harupy@users.noreply.github.com>
2020-12-08 11:53:27 +0900

    Deprecate Python 3.5
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/__init__.py
+++ mlflow/__init__.py
@@ -66,14 +66,13 @@ import mlflow.shap as shap  # noqa: E402
 
 _configure_mlflow_loggers(root_module_name=__name__)
 
-if sys.version_info.major == 2:
+if sys.version_info[:2] == (3, 5):
     warnings.warn(
-        "MLflow support for Python 2 is deprecated and will be dropped in a future "
-        "release. At that point, existing Python 2 workflows that use MLflow will "
-        "continue to work without modification, but Python 2 users will no longer "
+        "MLflow support for Python 3.5 is deprecated and will be dropped in a future "
+        "release. At that point, existing Python 3.5 workflows that use MLflow will "
+        "continue to work without modification, but Python 3.5 users will no longer "
         "get access to the latest MLflow features and bugfixes. We recommend that "
-        "you upgrade to Python 3 - see https://docs.python.org/3/howto/pyporting.html "
-        "for a migration guide.",
+        "you upgrade to Python > 3.5 - see https://www.python.org/downloads",
         DeprecationWarning,
     )
 
