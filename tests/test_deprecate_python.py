d48164011c517f5ee169bad95c8c795046f461d3
harupy <17039389+harupy@users.noreply.github.com>
2020-12-09 09:56:25 +0900

    Add match
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/test_deprecate_python.py
+++ tests/test_deprecate_python.py
@@ -5,5 +5,5 @@ import pytest
 
 @pytest.mark.skipif(sys.version_info[:2] != (3, 5), reason="This test fails in Python != 3.5")
 def test_deprecate_python():
-    with pytest.warns(DeprecationWarning):
+    with pytest.warns(DeprecationWarning, match="Python 3.5 is deprecated"):
         import mlflow  # noqa: F401
