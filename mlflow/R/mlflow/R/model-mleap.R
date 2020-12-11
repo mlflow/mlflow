77d14a44092b8cf50824e1290410f8ebc6e50155
Yitao Li <yitao@rstudio.com>
2020-12-11 16:59:53 +0000

    support MLeap for R
    Signed-off-by: Yitao Li <yitao@rstudio.com>

---
+++ mlflow/R/mlflow/R/model-mleap.R
@@ -0,0 +1,52 @@
+#' @include model-utils.R
+NULL
+
+.model_filename <- "model.zip"
+
+#' @rdname mlflow_save_model
+#' @param sample_input Sample Spark DataFrame input that the model can evaluate. This is required by MLeap for data schema inference.
+#'
+#' @export
+mlflow_save_model.ml_pipeline_model <- function(model,
+                                                path,
+                                                model_spec = list(),
+                                                conda_env = NULL,
+                                                sample_input = NULL,
+                                                ...) {
+  if (is.null(sample_input)) {
+    stop("`sample_input` is required by MLeap for data schema inference.")
+  }
+
+  assert_pkg_installed("mleap")
+  if (dir.exists(path)) unlink(path, recursive = TRUE)
+  dir.create(path)
+  model_path <- file.path(path, .model_filename)
+  mleap::ml_write_bundle(model, sample_input = sample_input, path = model_path)
+  version <- mleap::mleap_installed_versions()$mleap %>%
+    purrr::map(~ numeric_version(.x)) %>%
+    purrr::reduce(~ (if (.x > .y) .x else .y)) %>%
+    as.character()
+
+  conda_env <- create_default_conda_env_if_absent(
+    path, conda_env, default_pip_deps = list("mlflow", paste("mleap>=", version, sep = ""))
+  )
+  mleap_conf <- list(
+    mleap = list(mleap_version = version, model_filename = .model_filename)
+  )
+  model_spec$flavors <- append(model_spec$flavors, mleap_conf)
+
+
+  mlflow_write_model_spec(path, model_spec)
+}
+
+#' @export
+mlflow_load_flavor.mlflow_flavor_mleap <- function(flavor, model_path) {
+  assert_pkg_installed("mleap")
+  mleap::mleap_load_bundle(file.path(model_path, .model_filename))
+}
+
+#' @export
+mlflow_predict.mleap_transformer <- function(model, data, ...) {
+  assert_pkg_installed("mleap")
+  mleap::mleap_transform(model, data, ...)
+}
