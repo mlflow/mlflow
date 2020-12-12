0abb2a84d29c9ac07723c5b19cd7ace75fe387a8
Corey Zumar <corey.zumar@databricks.com>
2020-12-12 01:57:31 -0800

    Docstring
    Signed-off-by: Corey Zumar <corey.zumar@databricks.com>

--- tests/autologging/test_autologging_safety_integration.py
+++ tests/autologging/test_autologging_safety_integration.py
@@ -5,6 +5,7 @@ import pytest
 from unittest import mock
 
 import mlflow
+from mlflow.utils import gorilla
 from mlflow.utils.autologging_utils import (
     safe_patch,
     get_autologging_config,
@@ -48,8 +49,6 @@ def test_autologging_integrations_expose_configs_and_support_disablement():
 
 
 def test_autologging_integrations_use_safe_patch_for_monkey_patching():
-    from mlflow.utils import gorilla
-
     for integration in AUTOLOGGING_INTEGRATIONS_TO_TEST:
         with mock.patch(
             "mlflow.utils.gorilla.apply", wraps=gorilla.apply
@@ -58,4 +57,9 @@ def test_autologging_integrations_use_safe_patch_for_monkey_patching():
         ) as safe_patch_mock:
             integration.autolog(disable=False)
             assert safe_patch_mock.call_count > 0
+            # `safe_patch` leverages `gorilla.apply` in its implementation. Accordingly, we expect
+            # that the total number of `gorilla.apply` calls to be equivalent to the number of
+            # `safe_patch` calls. This verifies that autologging integrations are leveraging
+            # `safe_patch`, rather than calling `gorilla.apply` directly (which does not provide
+            # exception safety properties)
             assert safe_patch_mock.call_count == gorilla_mock.call_count
