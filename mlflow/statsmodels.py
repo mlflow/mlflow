a1f2a39733b07d7229c8854fd6035052f5fb1a3e
harupy <17039389+harupy@users.noreply.github.com>
2020-12-14 16:48:19 +0900

    Introduce _is_numeric
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/statsmodels.py
+++ mlflow/statsmodels.py
@@ -30,6 +30,7 @@ from mlflow.utils.model_utils import _get_flavor_configuration
 from mlflow.exceptions import MlflowException
 from mlflow.utils.annotations import experimental
 from mlflow.utils.autologging_utils import try_mlflow_log, log_fn_args_as_params
+from mlflow.utils.validation import _is_numeric
 
 import itertools
 import inspect
@@ -464,9 +465,7 @@ def autolog():
                         renamed_keys_dict = prepend_to_keys(d, f)
                         results_dict.update(renamed_keys_dict)
 
-                    # `not isinstance(field, bool)` is required because `bool` is a subclass of
-                    # `int`
-                    elif not isinstance(field, bool) and isinstance(field, (int, float)):
+                    elif _is_numeric(field):
                         results_dict[f] = field
 
             except AttributeError:
