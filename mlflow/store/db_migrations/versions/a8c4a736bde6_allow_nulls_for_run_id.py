a3673fd83d0ab58d2c818f3c98679146c05e6fd0
Theophile Henri <t.henri@criteo.com>
2020-12-14 11:52:58 +0100

    Add existing type to alter_column in allows_null_for_run_id alembic migration
    Signed-off-by: Theophile Henri <t.henri@criteo.com>

--- mlflow/store/db_migrations/versions/a8c4a736bde6_allow_nulls_for_run_id.py
+++ mlflow/store/db_migrations/versions/a8c4a736bde6_allow_nulls_for_run_id.py
@@ -19,7 +19,7 @@ depends_on = None
 
 def upgrade():
     with op.batch_alter_table(SqlModelVersion.__tablename__) as batch_op:
-        batch_op.alter_column("run_id", nullable=True)
+        batch_op.alter_column("run_id", nullable=True, existing_type=sa.VARCHAR(32))
 
 
 def downgrade():
