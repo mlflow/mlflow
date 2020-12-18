455019debfc85ab9cc6a474126c8c7586f1b5ecb
Yitao Li <yitao@rstudio.com>
2020-12-18 16:51:00 +0000

    fix a bug with loading attributes of MLflow model flavor
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- mlflow/R/mlflow/tests/testthat.R
+++ mlflow/R/mlflow/tests/testthat.R
@@ -1,3 +1,21 @@
+# `trivial` is a fictional MLflow flavor that exists only for unit test purposes
+
+mlflow_save_model.trivial <- function(model, path, model_spec = list(), ...) {
+  if (dir.exists(path)) unlink(path, recursive = TRUE)
+  dir.create(path, recursive = TRUE)
+  path <- normalizePath(path)
+
+  trivial_conf = list(
+    trivial = list(type = "trivial")
+  )
+  model_spec$flavors <- c(model_spec$flavors, trivial_conf)
+  mlflow:::mlflow_write_model_spec(path, model_spec)
+}
+
+mlflow_load_flavor.mlflow_flavor_trivial <- function(flavor, model_path) {
+  list(flavor = flavor)
+}
+
 library(testthat)
 library(mlflow)
 
