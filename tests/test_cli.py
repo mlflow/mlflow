8c0d499ba13b87a5803ff666f623015925fbd136
Eduardo de Leon <eddeleon@microsoft.com>
2020-12-21 15:21:11 -0500

    Move mock to mlflow.server._run_server
    Signed-off-by: Eduardo de Leon <eddeleon@microsoft.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -23,24 +23,24 @@ from mlflow.entities import ViewType
 
 
 def test_server_static_prefix_validation():
-    with mock.patch("mlflow.cli._run_server") as run_server_mock:
+    with mock.patch("mlflow.server._run_server") as run_server_mock:
         CliRunner().invoke(server)
         run_server_mock.assert_called_once()
-    with mock.patch("mlflow.cli._run_server") as run_server_mock:
+    with mock.patch("mlflow.server._run_server") as run_server_mock:
         CliRunner().invoke(server, ["--static-prefix", "/mlflow"])
         run_server_mock.assert_called_once()
-    with mock.patch("mlflow.cli._run_server") as run_server_mock:
+    with mock.patch("mlflow.server._run_server") as run_server_mock:
         result = CliRunner().invoke(server, ["--static-prefix", "mlflow/"])
         assert "--static-prefix must begin with a '/'." in result.output
         run_server_mock.assert_not_called()
-    with mock.patch("mlflow.cli._run_server") as run_server_mock:
+    with mock.patch("mlflow.server._run_server") as run_server_mock:
         result = CliRunner().invoke(server, ["--static-prefix", "/mlflow/"])
         assert "--static-prefix should not end with a '/'." in result.output
         run_server_mock.assert_not_called()
 
 
 def test_server_default_artifact_root_validation():
-    with mock.patch("mlflow.cli._run_server") as run_server_mock:
+    with mock.patch("mlflow.server._run_server") as run_server_mock:
         result = CliRunner().invoke(server, ["--backend-store-uri", "sqlite:///my.db"])
         assert result.output.startswith("Option 'default-artifact-root' is required")
         run_server_mock.assert_not_called()
@@ -49,7 +49,7 @@ def test_server_default_artifact_root_validation():
 @pytest.mark.parametrize("command", [server, ui])
 def test_tracking_uri_validation_failure(command):
     handlers._tracking_store = None
-    with mock.patch("mlflow.cli._run_server") as run_server_mock:
+    with mock.patch("mlflow.server._run_server") as run_server_mock:
         # SQLAlchemy expects postgresql:// not postgres://
         CliRunner().invoke(
             command,
@@ -67,7 +67,7 @@ def test_tracking_uri_validation_failure(command):
 def test_tracking_uri_validation_sql_driver_uris(command):
     handlers._tracking_store = None
     handlers._model_registry_store = None
-    with mock.patch("mlflow.cli._run_server") as run_server_mock, mock.patch(
+    with mock.patch("mlflow.server._run_server") as run_server_mock, mock.patch(
         "mlflow.store.tracking.sqlalchemy_store.SqlAlchemyStore"
     ) as tracking_store_mock, mock.patch(
         "mlflow.store.model_registry.sqlalchemy_store.SqlAlchemyStore"
