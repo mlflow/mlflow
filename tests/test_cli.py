9ba032f417cfa61eaafc42b16e6f83cdab743d25
Eduardo de Leon <eddeleon@microsoft.com>
2020-12-21 19:20:31 -0500

    Remove server calls but not the other test_cli.py changes
    Signed-off-by: Eduardo de Leon <eddeleon@microsoft.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -21,30 +21,6 @@ from mlflow.exceptions import MlflowException
 from mlflow.entities import ViewType
 
 
-def test_server_static_prefix_validation():
-    with mock.patch("mlflow.server._run_server") as run_server_mock:
-        CliRunner().invoke(server)
-        run_server_mock.assert_called_once()
-    with mock.patch("mlflow.server._run_server") as run_server_mock:
-        CliRunner().invoke(server, ["--static-prefix", "/mlflow"])
-        run_server_mock.assert_called_once()
-    with mock.patch("mlflow.server._run_server") as run_server_mock:
-        result = CliRunner().invoke(server, ["--static-prefix", "mlflow/"])
-        assert "--static-prefix must begin with a '/'." in result.output
-        run_server_mock.assert_not_called()
-    with mock.patch("mlflow.server._run_server") as run_server_mock:
-        result = CliRunner().invoke(server, ["--static-prefix", "/mlflow/"])
-        assert "--static-prefix should not end with a '/'." in result.output
-        run_server_mock.assert_not_called()
-
-
-def test_server_default_artifact_root_validation():
-    with mock.patch("mlflow.server._run_server") as run_server_mock:
-        result = CliRunner().invoke(server, ["--backend-store-uri", "sqlite:///my.db"])
-        assert result.output.startswith("Option 'default-artifact-root' is required")
-        run_server_mock.assert_not_called()
-
-
 def test_mlflow_run():
     with mock.patch("mlflow.cli.projects") as mock_projects:
         result = CliRunner().invoke(run)
