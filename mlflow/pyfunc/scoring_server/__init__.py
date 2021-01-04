06518bdfde6d358006a927599d01c387f08d113a
Secbone <secbone@gmail.com>
2021-01-04 14:04:27 +0800

    update: code format for lint
    Signed-off-by: Secbone <secbone@gmail.com>

--- mlflow/pyfunc/scoring_server/__init__.py
+++ mlflow/pyfunc/scoring_server/__init__.py
@@ -186,11 +186,15 @@ def init(model: PyFuncModel):
             data = parse_csv_input(csv_input=csv_input)
         elif flask.request.content_type in [CONTENT_TYPE_JSON, CONTENT_TYPE_JSON_SPLIT_ORIENTED]:
             data = parse_json_input(
-                json_input=StringIO(flask.request.data.decode("utf-8")), orient="split", schema=input_schema
+                json_input=StringIO(flask.request.data.decode("utf-8")),
+                orient="split",
+                schema=input_schema,
             )
         elif flask.request.content_type == CONTENT_TYPE_JSON_RECORDS_ORIENTED:
             data = parse_json_input(
-                json_input=StringIO(flask.request.data.decode("utf-8")), orient="records", schema=input_schema
+                json_input=StringIO(flask.request.data.decode("utf-8")),
+                orient="records",
+                schema=input_schema,
             )
         elif flask.request.content_type == CONTENT_TYPE_JSON_SPLIT_NUMPY:
             data = parse_split_oriented_json_input_to_numpy(flask.request.data.decode("utf-8"))
