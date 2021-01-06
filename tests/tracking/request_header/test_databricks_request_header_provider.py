879d153066f10b38038ec5e8421a55117d528220
Jimmy Xu <jimmy.xu@databricks.com>
2021-01-06 17:04:01 -0500

    lint
    Signed-off-by: Jimmy Xu <jimmy.xu@databricks.com>

--- tests/tracking/request_header/test_databricks_request_header_provider.py
+++ tests/tracking/request_header/test_databricks_request_header_provider.py
@@ -30,6 +30,8 @@ def test_databricks_request_header_provider_in_context(
             or is_in_cluster
         )
 
+
+# test that request_headers returns whatever is available
 @pytest.mark.parametrize(
     "is_in_databricks_notebook,is_in_databricks_job,is_in_cluster",
     itertools.product(bool_values, bool_values, bool_values),
@@ -44,7 +46,17 @@ def test_databricks_request_header_provider_request_headers(
         "mlflow.utils.databricks_utils.is_in_databricks_job", return_value=is_in_databricks_job
     ), mock.patch(
         "mlflow.utils.databricks_utils.is_in_cluster", return_value=is_in_cluster
-    ):
+    ), mock.patch(
+        "mlflow.utils.databricks_utils.get_notebook_id"
+    ) as notebook_id_mock, mock.patch(
+        "mlflow.utils.databricks_utils.get_job_id"
+    ) as job_id_mock, mock.patch(
+        "mlflow.utils.databricks_utils.get_job_run_id"
+    ) as job_run_id_mock, mock.patch(
+        "mlflow.utils.databricks_utils.get_job_type"
+    ) as job_type_mock, mock.patch(
+        "mlflow.utils.databricks_utils.get_cluster_id"
+    ) as cluster_id_mock:
         request_headers = DatabricksRequestHeaderProvider().request_headers()
 
         if is_in_databricks_notebook:
@@ -65,50 +77,3 @@ def test_databricks_request_header_provider_request_headers(
             assert request_headers["cluster_id"] == cluster_id_mock.return_value
         else:
             assert "cluster_id" not in request_headers
-
-# @pytest.mark.parametrize("is_in_databricks_notebook", bool_values)
-# def test_notebook_request_headers(is_in_databricks_notebook):
-#     with mock.patch(
-#         "mlflow.utils.databricks_utils.is_in_databricks_notebook",
-#         return_value=is_in_databricks_notebook,
-#     ), mock.patch("mlflow.utils.databricks_utils.get_notebook_id") as notebook_id_mock:
-#         request_headers = DatabricksRequestHeaderProvider().request_headers()
-
-#         if is_in_databricks_notebook:
-#             assert request_headers["notebook_id"] == notebook_id_mock.return_value
-#         else:
-#             assert "notebook_id" not in request_headers
-
-
-# @pytest.mark.parametrize("is_in_databricks_job", bool_values)
-# def test_job_request_headers(is_in_databricks_job):
-#     with mock.patch(
-#         "mlflow.utils.databricks_utils.is_in_databricks_job", return_value=is_in_databricks_job
-#     ), mock.patch("mlflow.utils.databricks_utils.get_job_id") as job_id_mock, mock.patch(
-#         "mlflow.utils.databricks_utils.get_job_run_id"
-#     ) as job_run_id_mock, mock.patch(
-#         "mlflow.utils.databricks_utils.get_job_type"
-#     ) as job_type_mock:
-#         request_headers = DatabricksRequestHeaderProvider().request_headers()
-
-#         if is_in_databricks_job:
-#             assert request_headers["job_id"] == job_id_mock.return_value
-#             assert request_headers["job_run_id"] == job_run_id_mock.return_value
-#             assert request_headers["job_type"] == job_type_mock.return_value
-#         else:
-#             assert "job_id" not in request_headers
-#             assert "job_run_id" not in request_headers
-#             assert "job_type" not in request_headers
-
-
-# @pytest.mark.parametrize("is_in_cluster", bool_values)
-# def test_cluster_request_headers(is_in_cluster):
-#     with mock.patch(
-#         "mlflow.utils.databricks_utils.is_in_cluster", return_value=is_in_cluster,
-#     ), mock.patch("mlflow.utils.databricks_utils.get_cluster_id") as cluster_id_mock:
-#         request_headers = DatabricksRequestHeaderProvider().request_headers()
-
-#         if is_in_cluster:
-#             assert request_headers["cluster_id"] == cluster_id_mock.return_value
-#         else:
-#             assert "cluster_id" not in request_headers
