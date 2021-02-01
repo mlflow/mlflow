50a6d205086ce8eaa7816a96d0c129882b8d6cf9
Secbone <secbone@gmail.com>
2021-02-02 02:49:57 +0800

    Update type of input json data to `StringIO` (#3921)
    * Update type of input json data to `StringIO`
    
    fix error when scoring_serving parsing input JSON with pandas which version later than `1.1.0`. Fixed #3920
    [pandas JSON reader](https://pandas.pydata.org/pandas-docs/stable/user_guide/io.html#io-json-reader)
    
    Signed-off-by: zhouweipeng <zhouweipeng@lattebank.com>
    
    * fixed: parse json content from HTTP with pandas 1.1.0+
    
    Signed-off-by: zhouweipeng <zhouweipeng@lattebank.com>
    
    * update: code format for lint
    
    Signed-off-by: Secbone <secbone@gmail.com>
    
    * test: add test case for url path in json input
    
    Signed-off-by: Secbone <secbone@gmail.com>
    
    * Trigger Build
    
    Signed-off-by: Secbone <secbone@gmail.com>
    
    * lint: remove spaces
    
    Signed-off-by: Secbone <secbone@gmail.com>
    
    * lint: fix code style
    
    Signed-off-by: Secbone <secbone@gmail.com>
    
    * fix: testcase for json including path
    
    Signed-off-by: Secbone <secbone@gmail.com>
    
    Co-authored-by: zhouweipeng <zhouweipeng@lattebank.com>

--- tests/pyfunc/test_scoring_server.py
+++ tests/pyfunc/test_scoring_server.py
@@ -619,3 +619,27 @@ def test_get_jsonnable_obj():
     assert json.dumps(py_ary, cls=NumpyEncoder) == json.dumps(np_ary, cls=NumpyEncoder)
     np_ary = _get_jsonable_obj(np.array(py_ary, dtype=type(str)))
     assert json.dumps(py_ary, cls=NumpyEncoder) == json.dumps(np_ary, cls=NumpyEncoder)
+
+
+@pytest.mark.large
+def test_parse_json_input_including_path():
+    class TestModel(PythonModel):
+        def predict(self, context, model_input):
+            return 1
+
+    with mlflow.start_run() as run:
+        mlflow.pyfunc.log_model("model", python_model=TestModel())
+
+    pandas_split_content = pd.DataFrame(
+        {
+            "url": ["http://foo.com", "https://bar.com"],
+            "bad_protocol": ["aaa://bbb", "address:/path"],
+        }
+    ).to_json(orient="split")
+
+    response_records_content_type = pyfunc_serve_and_score_model(
+        model_uri="runs:/{}/model".format(run.info.run_id),
+        data=pandas_split_content,
+        content_type=pyfunc_scoring_server.CONTENT_TYPE_JSON,
+    )
+    assert response_records_content_type.status_code == 200
