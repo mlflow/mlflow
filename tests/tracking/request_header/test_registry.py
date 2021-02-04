c6d5352ea3e1039f08d957085e0b455fabf4e30f
Jimmy Xu <jimmy.xu@databricks.com>
2021-02-03 19:58:42 -0500

    removed unused param
    Signed-off-by: Jimmy Xu <jimmy.xu@databricks.com>

--- tests/tracking/request_header/test_registry.py
+++ tests/tracking/request_header/test_registry.py
@@ -89,7 +89,7 @@ def test_registry_instance_loads_entrypoints():
 
 
 @pytest.mark.large
-def test_run_context_provider_registry_with_installed_plugin(tmp_wkdir):
+def test_run_context_provider_registry_with_installed_plugin():
     """This test requires the package in tests/resources/mlflow-test-plugin to be installed"""
 
     reload(mlflow.tracking.request_header.registry)
