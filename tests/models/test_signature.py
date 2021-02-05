259ff0d3108f73a09316a02b5f95a7faf5dae922
caleboverman <coverman@fanthreesixty.com>
2021-02-05 12:07:00 -0600

    Reuse existing pandas test
    Signed-off-by: caleboverman <coverman@fanthreesixty.com>

--- tests/models/test_signature.py
+++ tests/models/test_signature.py
@@ -1,56 +1,10 @@
 import json
-import math
 
 import numpy as np
-import pandas as pd
-import pytest
 
 from mlflow.models.signature import ModelSignature, infer_signature
 from mlflow.types import DataType
 from mlflow.types.schema import Schema, ColSpec
-
-
-@pytest.fixture
-def pandas_df_with_all_types():
-    df = pd.DataFrame(
-        {
-            "boolean": [True, False, True],
-            "integer": np.array([1, 2, 3], np.int32),
-            "long": np.array([1, 2, 3], np.int64),
-            "float": np.array([math.pi, 2 * math.pi, 3 * math.pi], np.float32),
-            "double": [math.pi, 2 * math.pi, 3 * math.pi],
-            "binary": [bytes([1, 2, 3]), bytes([4, 5, 6]), bytes([7, 8, 9])],
-            "string": ["a", "b", "c"],
-            "boolean_ext": [True, False, pd.NA],
-            "integer_ext": [1, 2, pd.NA],
-            "string_ext": ["a", "b", "c"],
-        }
-    )
-    df["boolean_ext"] = df["boolean_ext"].astype("boolean")
-    df["integer_ext"] = df["integer_ext"].astype("Int64")
-    df["string_ext"] = df["string_ext"].astype("string")
-    return df
-
-
-def test_pandas_signature_inference(pandas_df_with_all_types):
-    signature1 = ModelSignature(
-        inputs=Schema(
-            [
-                ColSpec(DataType.boolean, "boolean"),
-                ColSpec(DataType.integer, "integer"),
-                ColSpec(DataType.long, "long"),
-                ColSpec(DataType.float, "float"),
-                ColSpec(DataType.double, "double"),
-                ColSpec(DataType.binary, "binary"),
-                ColSpec(DataType.string, "string"),
-                ColSpec(DataType.boolean, "boolean_ext"),
-                ColSpec(DataType.long, "integer_ext"),
-                ColSpec(DataType.string, "string_ext"),
-            ]
-        )
-    )
-    signature2 = infer_signature(pandas_df_with_all_types)
-    assert signature1 == signature2
 
 
 def test_model_signature():
