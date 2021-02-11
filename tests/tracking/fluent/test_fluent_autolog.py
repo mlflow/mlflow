b29fadc6f237a47c33b68aaec4ae9eaf84b6e7ff
Apurva Koti <apurva.koti@databricks.com>
2021-02-11 11:52:07 -0800

    switch
    Signed-off-by: Apurva Koti <apurva.koti@databricks.com>

--- tests/tracking/fluent/test_fluent_autolog.py
+++ tests/tracking/fluent/test_fluent_autolog.py
@@ -196,34 +196,34 @@ def test_universal_autolog_makes_expected_event_logging_calls():
 
 def test_autolog_obeys_disabled():
     mlflow.autolog(disable=True)
-    mlflow.utils.import_hooks.notify_module_loaded(tensorflow)
-    assert get_autologging_config("tensorflow", "disable")
+    mlflow.utils.import_hooks.notify_module_loaded(sklearn)
+    assert get_autologging_config("sklearn", "disable")
 
     mlflow.autolog()
-    mlflow.utils.import_hooks.notify_module_loaded(tensorflow)
+    mlflow.utils.import_hooks.notify_module_loaded(sklearn)
     mlflow.autolog(disable=True)
-    mlflow.utils.import_hooks.notify_module_loaded(tensorflow)
-    assert get_autologging_config("tensorflow", "disable")
+    mlflow.utils.import_hooks.notify_module_loaded(sklearn)
+    assert get_autologging_config("sklearn", "disable")
 
     mlflow.autolog(disable=False)
-    mlflow.utils.import_hooks.notify_module_loaded(tensorflow)
-    assert not get_autologging_config("tensorflow", "disable", False)
+    mlflow.utils.import_hooks.notify_module_loaded(sklearn)
+    assert not get_autologging_config("sklearn", "disable", False)
     mlflow.tensorflow.autolog(disable=True)
-    assert get_autologging_config("tensorflow", "disable")
+    assert get_autologging_config("sklearn", "disable")
 
 
 def test_autolog_success_message_obeys_disabled():
     with mock.patch("mlflow.tracking.fluent._logger.info") as autolog_logger_mock:
         mlflow.autolog(disable=True)
-        mlflow.utils.import_hooks.notify_module_loaded(sklearn)
+        mlflow.utils.import_hooks.notify_module_loaded(tensorflow)
         autolog_logger_mock.assert_not_called()
 
         mlflow.autolog()
-        mlflow.utils.import_hooks.notify_module_loaded(sklearn)
+        mlflow.utils.import_hooks.notify_module_loaded(tensorflow)
         autolog_logger_mock.assert_called()
 
         autolog_logger_mock.reset_mock()
 
         mlflow.autolog(disable=False)
-        mlflow.utils.import_hooks.notify_module_loaded(sklearn)
+        mlflow.utils.import_hooks.notify_module_loaded(tensorflow)
         autolog_logger_mock.assert_called()
