6578d73149d288c415e6e97945ec8d4688b607c0
Arjun DCunha <61438563+arjundc-db@users.noreply.github.com>
2021-02-11 14:24:58 -0800

    Add multi-dimensional array (aka Tensor) support (#4079)
    * TensorSpec
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * Most of the PR
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * more tests
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * Undo
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * Docs
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * Fixes
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * More fixes
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * model examples
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * more fixes
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * Test fixes
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * lint
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * Comments
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * test
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * revert
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * why
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * Chjeck
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * Extended types
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * Better ux
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * lint
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * More lint
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * last
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * Better JSON
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * Better messages
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * tset
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * Check
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * Fional
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>
    
    * PLease
    
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>

--- tests/xgboost/test_xgboost_autolog.py
+++ tests/xgboost/test_xgboost_autolog.py
@@ -435,9 +435,7 @@ def test_xgb_autolog_infers_model_signature_correctly(bst_params):
 
     assert "outputs" in signature
     assert json.loads(signature["outputs"]) == [
-        {"type": "float"},
-        {"type": "float"},
-        {"type": "float"},
+        {"type": "tensor", "tensor-spec": {"dtype": "float32", "shape": [-1, 3]}},
     ]
 
 
