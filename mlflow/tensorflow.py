a3d4d87f602b13b6fde30f61d3c57f57b9854c76
harupy <17039389+harupy@users.noreply.github.com>
2021-02-18 12:40:13 +0900

    remove unused import
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/tensorflow.py
+++ mlflow/tensorflow.py
@@ -36,7 +36,6 @@ from mlflow.utils.annotations import keyword_only, experimental
 from mlflow.utils.environment import _mlflow_conda_env
 from mlflow.utils.file_utils import _copy_file_or_tree, TempDir
 from mlflow.utils.model_utils import _get_flavor_configuration
-from mlflow.utils.mlflow_tags import MLFLOW_AUTOLOGGING
 from mlflow.utils.autologging_utils import (
     autologging_integration,
     safe_patch,
