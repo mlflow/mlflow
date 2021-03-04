14a952d942bced881ceb1307d1075ffc7fa1fe8f
Eduardo de Leon <eddeleon@microsoft.com>
2021-03-04 13:31:39 -0500

    Avoid breaking builds by changing tracking uri
    Signed-off-by: Eduardo de Leon <eddeleon@microsoft.com>

--- tests/test_use_alembic_logger.py
+++ tests/test_use_alembic_logger.py
@@ -1,12 +1,6 @@
-import uuid
-
-import pytest
-
 import mlflow
-from mlflow.pyfunc import PythonModel
 
-def test_verify_alembic_runs():
-    from mlflow.pyfunc import PythonModel
 
-    mlflow.set_tracking_uri('sqlite:///mlflowtest.db')
+def test_verify_alembic_runs(tracking_uri_mock):
+    mlflow.set_tracking_uri(tracking_uri_mock)
     mlflow.set_experiment("exp_name_example")
