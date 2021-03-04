260e829a949301d61bee23db7bae715434c146b9
harupy <17039389+harupy@users.noreply.github.com>
2021-03-04 20:45:57 +0900

    store spark version to file
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/.create-test-env.R
+++ mlflow/R/mlflow/.create-test-env.R
@@ -15,6 +15,14 @@ reticulate::conda_install("xgboost", envname = mlflow:::mlflow_conda_env_name())
 # Pin h2o to prevent version-mismatch between python and R
 reticulate::conda_install("h2o==3.30.1.3", envname = mlflow:::mlflow_conda_env_name(), pip = TRUE)
 
-# Install MLeap runtime and required dependencies (spark is installed via `.install-spark.R`)
+
+# The default timeout value (60 seconds) can be insufficient for `spark_install` to complete
+options(timeout=60 * 60)
+
+spark_version_file <- ".spark.version"
+spark_version <- trimws(readChar(spark_version_file, file.info(spark_version_file)$size))
+
+# Install MLeap runtime and required dependencies
+sparklyr::spark_install(version = spark_version, verbose = TRUE)
 mleap::install_maven()
 mleap::install_mleap(version = "0.16.0")
--- mlflow/R/mlflow/.install-spark.R
+++
@@ -1,9 +0,0 @@
-parent_dir <- dir("../", full.names = TRUE)
-package <- parent_dir[grepl("mlflow_", parent_dir)]
-install.packages(package)
-
-# The default timeout value (60 seconds) can be insufficient for `spark_install` to complete
-options(timeout=60 * 60)
-
-# Install MLeap runtime and required dependencies
-sparklyr::spark_install(version = "2.4.5", verbose = TRUE)
