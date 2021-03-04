c3c418874bb7c375c3a59a7a640a88a12510969c
Eduardo de Leon <eddeleon@microsoft.com>
2021-03-04 13:15:49 -0500

    Add tests for missing alembic logger
    Signed-off-by: Eduardo de Leon <eddeleon@microsoft.com>

--- tests/test_use_alembic_logger.py
+++ tests/test_use_alembic_logger.py
@@ -5,7 +5,7 @@ import pytest
 import mlflow
 from mlflow.pyfunc import PythonModel
 
-def test_repro_missing_alembic_config():
+def test_verify_alembic_runs():
     from mlflow.pyfunc import PythonModel
 
     mlflow.set_tracking_uri('sqlite:///mlflowtest.db')
