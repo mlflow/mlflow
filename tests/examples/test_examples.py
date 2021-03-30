6672429353c6838d487ee10fe4f762c06d33150a
ankan94 <51693147+ankan94@users.noreply.github.com>
2021-03-31 05:28:59 +0530

    [PyTorch] Adding Example for Ax Hyperparameter Optimization (#3885)
    * Adding Example for Ax Hyperparameter Optimization
    
    Signed-off-by: ankan94 <ankan@ideas2it.com>
    
    * Updating implementation to use Iris Classification example
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Adding Tests for ax example
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Fixing Lint errors
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * removing print statements
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Removing parameters specific to Adam Optimizer
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Adding conditions for SGD parameters
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * removing prefix parameter
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Fixed Pythonpath issue
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Moving Iris Classification class to Ax Folder
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Removing the unused code
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Addressing review comments
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Addressing review comments
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Removing pandas from dependency
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * removing unused packages from conda.yaml
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Changing project name
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * reverting iris file changes
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Changing filename
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Reverting iris files from upstream master branch
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Reverting pytorch autolog file from upstream master
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Fixing doc strings
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Removing unused iris class
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Adding torchvision
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    * Fixing iris.py
    
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>
    
    Co-authored-by: Shrinath Suresh <shrinath@ideas2it.com>
    Co-authored-by: harupy <17039389+harupy@users.noreply.github.com>
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -111,6 +111,10 @@ def report_free_disk_space(capsys):
             os.path.join("pytorch", "IterativePruning"),
             ["-P", "max_epochs=1", "-P", "total_trials=1"],
         ),
+        (
+            os.path.join("pytorch", "AxHyperOptimizationPTL"),
+            ["-P", "max_epochs=10", "-P", "total_trials=1"],
+        ),
     ],
 )
 def test_mlflow_run_example(directory, params, tmpdir):
