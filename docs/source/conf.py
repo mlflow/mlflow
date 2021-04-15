fdc44be617012b77789014abe4011e96bbbfd7ce
harupy <17039389+harupy@users.noreply.github.com>
2021-04-16 00:15:31 +0900

    remove unused re
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@
 
 import sys
 import os
-import re
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
