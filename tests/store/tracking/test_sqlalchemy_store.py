3f7374576a578b82350ef9cc2f587209e41c0e0b
Naor Volkovich <naor2013@icloud.com>
2021-04-18 16:31:19 +0300

    Added a test
    Signed-off-by: Naor Volkovich <naor2013@icloud.com>

--- tests/store/tracking/test_sqlalchemy_store.py
+++ tests/store/tracking/test_sqlalchemy_store.py
@@ -1989,5 +1989,9 @@ def test_get_orderby_clauses():
         with pytest.raises(MlflowException, match=match):
             _get_orderby_clauses(["tag.t", "tag.t"], session)
 
+        # test that "=" is used rather than "is" when comparing to True
+        parsed = [str(x) for x in _get_orderby_clauses(["metric.a"], session)[0]]
+        assert "is_nan = true" in parsed[0]
+
         # test that an exception is NOT raised when key types are different
         _get_orderby_clauses(["param.a", "metric.a", "tag.a"], session)
