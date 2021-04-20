c0ba353e9094c546dcea868ecff021c4e63acce4
harupy <17039389+harupy@users.noreply.github.com>
2021-04-20 19:53:24 +0900

    skip test_sagemaker_docker_model_scoring_with_default_conda_env
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/spark/test_spark_model_export.py
+++ tests/spark/test_spark_model_export.py
@@ -283,6 +283,10 @@ def test_model_deployment(spark_model_iris, model_path, spark_custom_env):
 
 
 @pytest.mark.large
+@pytest.mark.skipif(
+    "dev" in pyspark.__version__,
+    reason="This test fails with the dev version of pyspark built from the source",
+)
 def test_sagemaker_docker_model_scoring_with_default_conda_env(spark_model_iris, model_path):
     sparkm.save_model(spark_model_iris.model, path=model_path, conda_env=None)
 
