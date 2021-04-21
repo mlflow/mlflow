6b79cd90caca8e53b18da0a9003b688b891e9485
harupy <17039389+harupy@users.noreply.github.com>
2021-04-21 20:56:30 +0900

    remove divider
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/db/show_column_info.py
+++ tests/db/show_column_info.py
@@ -7,18 +7,18 @@ from mlflow.tracking._tracking_service.utils import _TRACKING_URI_ENV_VAR
 assert _TRACKING_URI_ENV_VAR in os.environ
 
 uri = os.environ[_TRACKING_URI_ENV_VAR]
+db_type = uri.split(":")[0]
 engine = sa.create_engine(uri)
 
 with engine.begin() as conn:
     for table_name in engine.table_names():
-        print("=" * 40, table_name, "=" * 40 + "\n")
-        if uri.startswith("postgres") or uri.startswith("mysql"):
+        if db_type in ["postgresql", "mysql"]:
             sql = "SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{}'".format(
                 table_name
             )
             df = pd.read_sql(sql, conn)
             df = df.rename(str.lower, axis="columns").set_index("column_name", drop=True)
-        elif uri.startswith("sqlite"):
+        elif db_type == "sqlite":
             sql = "PRAGMA table_info('{}')".format(table_name)
             df = pd.read_sql(sql, conn)
             df = df.set_index("name", drop=True)
@@ -26,5 +26,5 @@ with engine.begin() as conn:
             raise ValueError(f"Invalid tracking URI: {uri}")
 
         for column_name, column_info in df.iterrows():
-            new_name = table_name + " / " + column_name
+            new_name = " / ".join([db_type, table_name, column_name])
             print(column_info.fillna("").rename(new_name).to_markdown(tablefmt="grid") + "\n")
