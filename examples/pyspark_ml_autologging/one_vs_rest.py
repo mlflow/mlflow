5e2a12924ba1a2730d3a5390f34fee3e206de537
harupy <17039389+harupy@users.noreply.github.com>
2021-04-23 18:55:42 +0900

    Address comments
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- examples/pyspark_ml_autologging/one_vs_rest.py
+++ examples/pyspark_ml_autologging/one_vs_rest.py
@@ -1,15 +1,15 @@
 from pyspark.ml.classification import LogisticRegression, OneVsRest
-from pyspark.ml.linalg import Vectors
+from pyspark.ml.feature import VectorAssembler
 from pyspark.sql import SparkSession
 from sklearn.datasets import load_iris
 
 import mlflow
 
 spark = SparkSession.builder.getOrCreate()
-X, y = load_iris(return_X_y=True)
-df = spark.createDataFrame(
-    [(Vectors.dense(features), int(label)) for features, label in zip(X, y)], ["features", "label"]
-)
+
+df = load_iris(as_frame=True).frame.rename(columns={"target": "label"})
+df = spark.createDataFrame(df)
+df = VectorAssembler(inputCols=df.columns[:-1], outputCol="features").transform(df)
 train, test = df.randomSplit([0.8, 0.2])
 
 mlflow.pyspark.ml.autolog()
@@ -21,3 +21,5 @@ with mlflow.start_run():
 
 pred = ovrModel.transform(test)
 pred.select(ovrModel.getPredictionCol()).show(10)
+
+spark.stop()
