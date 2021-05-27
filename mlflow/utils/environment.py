bcd1f53763fef6850be00727f719f675a21866a7
Weichen Xu <weichen.xu@databricks.com>
2021-05-27 11:36:33 +0800

    revert _mlflow_conda_env change
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/utils/environment.py
+++ mlflow/utils/environment.py
@@ -9,9 +9,15 @@ channels:
 """
 
 
-def _mlflow_conda_env(path=None, additional_pip_deps=None, install_mlflow=True, pip_version=None):
+def _mlflow_conda_env(
+    path=None,
+    additional_conda_deps=None,
+    additional_pip_deps=None,
+    additional_conda_channels=None,
+    install_mlflow=True,
+):
     """
-    Creates a Conda environment with the specified pip dependencies. If there are
+    Creates a Conda environment with the specified package channels and dependencies. If there are
     any pip dependencies, including from the install_mlflow parameter, then pip will be added to
     the conda dependencies. This is done to ensure that the pip inside the conda environment is
     used to install the pip dependencies.
@@ -19,26 +25,27 @@ def _mlflow_conda_env(path=None, additional_pip_deps=None, install_mlflow=True, 
     :param path: Local filesystem path where the conda env file is to be written. If unspecified,
                  the conda env will not be written to the filesystem; it will still be returned
                  in dictionary format.
+    :param additional_conda_deps: List of additional conda dependencies passed as strings.
     :param additional_pip_deps: List of additional pip dependencies passed as strings.
-    :param pip_version: specify pip version
+    :param additional_conda_channels: List of additional conda channels to search when resolving
+                                      packages.
     :return: ``None`` if ``path`` is specified. Otherwise, the a dictionary representation of the
              Conda environment.
     """
     pip_deps = (["mlflow"] if install_mlflow else []) + (
         additional_pip_deps if additional_pip_deps else []
     )
-
-    if pip_version:
-        pip_package = f"pip=={pip_version}"
-    else:
-        pip_package = "pip"
-    conda_deps = [pip_package] if pip_deps else []
+    conda_deps = (additional_conda_deps if additional_conda_deps else []) + (
+        ["pip"] if pip_deps else []
+    )
 
     env = yaml.safe_load(_conda_header)
     env["dependencies"] = ["python={}".format(PYTHON_VERSION)]
     if conda_deps is not None:
         env["dependencies"] += conda_deps
     env["dependencies"].append({"pip": pip_deps})
+    if additional_conda_channels is not None:
+        env["channels"] += additional_conda_channels
 
     if path is not None:
         with open(path, "w") as out:
--- tests/utils/test_environment.py
+++ tests/utils/test_environment.py
@@ -11,34 +11,42 @@ def conda_env_path(tmpdir):
 
 def test_mlflow_conda_env_returns_none_when_output_path_is_specified(conda_env_path):
     env_creation_output = _mlflow_conda_env(
-        path=conda_env_path, additional_pip_deps=["pip-dep-1", "pip-dep2==0.1.0"],
+        path=conda_env_path,
+        additional_conda_deps=["conda-dep-1=0.0.1", "conda-dep-2"],
+        additional_pip_deps=["pip-dep-1", "pip-dep2==0.1.0"],
     )
 
     assert env_creation_output is None
 
 
 def test_mlflow_conda_env_returns_expected_env_dict_when_output_path_is_not_specified():
-    pip_deps = ["pip-dep-1=0.0.1", "pip-dep-2"]
-    env = _mlflow_conda_env(path=None, additional_pip_deps=pip_deps)
+    conda_deps = ["conda-dep-1=0.0.1", "conda-dep-2"]
+    env = _mlflow_conda_env(path=None, additional_conda_deps=conda_deps)
 
-    for pip_dep in pip_deps:
-        assert pip_dep in env["dependencies"][-1]["pip"]
+    for conda_dep in conda_deps:
+        assert conda_dep in env["dependencies"]
 
 
-def test_mlflow_conda_env_includes_pip_dependencies_but_pip_is_not_specified():
+@pytest.mark.parametrize("conda_deps", [["conda-dep-1=0.0.1", "conda-dep-2"], None])
+def test_mlflow_conda_env_includes_pip_dependencies_but_pip_is_not_specified(conda_deps):
     additional_pip_deps = ["pip-dep==0.0.1"]
-    env = _mlflow_conda_env(path=None, additional_pip_deps=additional_pip_deps)
+    env = _mlflow_conda_env(
+        path=None, additional_conda_deps=conda_deps, additional_pip_deps=additional_pip_deps
+    )
+    if conda_deps is not None:
+        for conda_dep in conda_deps:
+            assert conda_dep in env["dependencies"]
     assert "pip" in env["dependencies"]
 
 
-@pytest.mark.parametrize("pip_version", [None, "20.0.02"])
-def test_mlflow_conda_env_includes_pip_dependencies_and_pip_is_specified(pip_version):
+@pytest.mark.parametrize("pip_specification", ["pip", "pip==20.0.02"])
+def test_mlflow_conda_env_includes_pip_dependencies_and_pip_is_specified(pip_specification):
+    conda_deps = ["conda-dep-1=0.0.1", "conda-dep-2", pip_specification]
     additional_pip_deps = ["pip-dep==0.0.1"]
     env = _mlflow_conda_env(
-        path=None, additional_pip_deps=additional_pip_deps, pip_version=pip_version
+        path=None, additional_conda_deps=conda_deps, additional_pip_deps=additional_pip_deps
     )
-    if pip_version:
-        assert f"pip=={pip_version}" in env["dependencies"]
-    else:
-        assert "pip" in env["dependencies"]
-    assert "pip-dep==0.0.1" in env["dependencies"][-1]["pip"]
+    for conda_dep in conda_deps:
+        assert conda_dep in env["dependencies"]
+    assert pip_specification in env["dependencies"]
+    assert env["dependencies"].count("pip") == (2 if pip_specification == "pip" else 1)
