1f0b6344e4588003732f2fce7a9e7afe1dd6a63d
Steven Chen <84401539+stevenchen-db@users.noreply.github.com>
2021-06-03 14:35:32 -0700

    Update tests/pytorch/test_pytorch_model_export.py
    Co-authored-by: Ankit Mathur <52183359+ankit-db@users.noreply.github.com>
    Signed-off-by: Steven Chen <s.chen@databricks.com>

--- tests/pytorch/test_pytorch_model_export.py
+++ tests/pytorch/test_pytorch_model_export.py
@@ -919,7 +919,7 @@ def test_requirements_file_log_model(create_requirements_file, sequential_model)
             run_id=mlflow.active_run().info.run_id, model_path="models"
         )
 
-        # check if the requirements file is different from the default requirements file
+        # Verify that explicitly specified requirements file overrides default requirements file
         conda_env = get_default_conda_env()
         pip_deps = conda_env["dependencies"][-1]["pip"]
         assert _mlflow_additional_pip_env(pip_deps) != content_expected
