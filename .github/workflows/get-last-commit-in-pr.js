d7fa2669ab959d301e53d1e1e2ea3d978be13c5c
harupy <17039389+harupy@users.noreply.github.com>
2021-06-10 20:00:50 +0900

    fix
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- .github/workflows/get-last-commit-in-pr.js
+++ .github/workflows/get-last-commit-in-pr.js
@@ -1,5 +1,4 @@
 module.exports = async ({ github, core, context }) => {
-  const { after } = context;
   const { number: pull_number } = context.payload.pull_request;
 
   const { data } = await github.pulls.get({
@@ -8,5 +7,5 @@ module.exports = async ({ github, core, context }) => {
     pull_number,
   });
 
-  return { skip: data.head.sha === lastCommitSha };
+  return { skip: data.head.sha === context.after };
 };
