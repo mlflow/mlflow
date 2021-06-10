b6154dc0d89aebf5ee4f56ba525662879a3dc998
harupy <17039389+harupy@users.noreply.github.com>
2021-06-10 15:50:46 +0900

    import pyspark
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- dev/test_mlflow_lazily_imports_pyspark.py
+++ dev/test_mlflow_lazily_imports_pyspark.py
@@ -3,3 +3,6 @@ import mlflow  # pylint: disable=unused-import
 
 cached_packages = set(k.split(".")[0] for k in sys.modules.keys())
 assert "pyspark" not in cached_packages
+
+# Make sure `pyspark` can be imported
+import pyspark  # pylint: disable=unused-import
