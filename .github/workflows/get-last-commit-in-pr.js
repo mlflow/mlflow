8e308467661c4a931d927b3e42a6cc8b7882ea66
harupy <17039389+harupy@users.noreply.github.com>
2021-06-10 19:48:08 +0900

    reove
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- .github/workflows/get-last-commit-in-pr.js
+++ .github/workflows/get-last-commit-in-pr.js
@@ -1,7 +1,7 @@
 module.exports = async ({ github, core, context }) => {
   const { number: pull_number } = context.payload.pull_request;
 
-  const { data } = await github.rest.pulls.get({
+  const { data } = await github.pulls.get({
     owner: "mlflow",
     repo: "mlflow",
     pull_number,
