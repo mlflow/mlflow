e8b7de298ea119630d4dfd04411b1278c781be06
harupy <17039389+harupy@users.noreply.github.com>
2021-06-11 18:48:17 +0900

    use pytest.mark
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/test_mlflow_lazily_imports_ml_packages.py
+++ tests/test_mlflow_lazily_imports_ml_packages.py
@@ -1,10 +1,19 @@
-# This test file must be executed via:
-# $ python tests/test_mlflow_lazily_imports_pspark
+# If test test script is run after other test script that imports ML packages in the same pytest
+# session, this test script fails. As a workaround, this test script needs to be run in a separate
+# pytest session with the following command:
+# $ pytest tests/test_mlflow_lazily_imports_ml_packages.py --lazy-import
+import sys
 
-if __name__ == "__main__":
-    import sys
-    import mlflow  # pylint: disable=unused-import
+import pytest
 
+
+def _get_loaded_packages():
+    return {k.split(".")[0] for k in sys.modules.keys()}
+
+
+# Run this test only when the `--lazy-import` flag is specified
+@pytest.mark.lazy_import
+def test_mlflow_lazily_import_ml_packages():
     ml_packages = {
         "catboost",
         "fastai",
@@ -25,5 +34,10 @@ if __name__ == "__main__":
         "xgboost",
     }
 
-    loaded_packages = {k.split(".")[0] for k in sys.modules.keys()}
-    assert loaded_packages.intersection(ml_packages) == set()
+    # Ensure ML packages are not loaded before importing mlflow
+    assert _get_loaded_packages().intersection(ml_packages) == set()
+
+    import mlflow  # pylint: disable=unused-import
+
+    # Ensure ML packages are not loaded after importing mlflow
+    assert _get_loaded_packages().intersection(ml_packages) == set()
