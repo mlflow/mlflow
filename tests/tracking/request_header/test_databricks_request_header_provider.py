3c2cf4ca4ad5f87a26721df4175b8a4ccb6fe442
Jimmy Xu <jimmy.xu@databricks.com>
2021-06-28 15:10:22 -0700

    oops
    Signed-off-by: Jimmy Xu <jimmy.xu@databricks.com>

--- tests/tracking/request_header/test_databricks_request_header_provider.py
+++ tests/tracking/request_header/test_databricks_request_header_provider.py
@@ -80,7 +80,7 @@ def test_databricks_request_header_provider_request_headers(
         else:
             assert "cluster_id" not in request_headers
 
-        if command_run_id_mock.return_value not None:
+        if command_run_id_mock.return_value is not None:
             assert request_headers["command_run_id"] == command_run_id_mock.return_value
         else:
             assert "command_run_id" not in request_headers
