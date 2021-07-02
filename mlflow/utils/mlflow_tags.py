38c2e97e862d58686a7c34fbf40fabe55fd86c91
Jerry Liang <66143562+jerrylian-db@users.noreply.github.com>
2021-07-02 13:52:27 -0700

    Created command context provider to set job group ID as part of run tags (#4521)

--- mlflow/utils/mlflow_tags.py
+++ mlflow/utils/mlflow_tags.py
@@ -27,6 +27,8 @@ MLFLOW_DATABRICKS_NOTEBOOK_PATH = "mlflow.databricks.notebookPath"
 MLFLOW_DATABRICKS_WEBAPP_URL = "mlflow.databricks.webappURL"
 MLFLOW_DATABRICKS_RUN_URL = "mlflow.databricks.runURL"
 MLFLOW_DATABRICKS_CLUSTER_ID = "mlflow.databricks.cluster.id"
+# The unique ID of a command execution in a Databricks notebook
+MLFLOW_DATABRICKS_NOTEBOOK_COMMAND_ID = "mlflow.databricks.notebook.commandID"
 # The SHELL_JOB_ID and SHELL_JOB_RUN_ID tags are used for tracking the
 # Databricks Job ID and Databricks Job Run ID associated with an MLflow Project run
 MLFLOW_DATABRICKS_SHELL_JOB_ID = "mlflow.databricks.shellJobID"
