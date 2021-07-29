383ba7f4d48ba401924552380e375454ae244010
harupy <hkawamura0130@gmail.com>
2021-07-29 12:14:53 +0900

    patch import_module
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/utils/_capture_modules.py
+++ mlflow/utils/_capture_modules.py
@@ -4,6 +4,7 @@ This script should be executed in a fresh python interpreter process using `subp
 import argparse
 import builtins
 import functools
+import importlib
 
 import mlflow
 
@@ -11,9 +12,13 @@ import mlflow
 IMPORTED_MODULES = set()
 
 
+def _get_top_level_module(full_module_name):
+    return full_module_name.split(".")[0]
+
+
 def _wrap_import(original_import):
     """
-    Wraps `builtins.__import__` to capture imported modules and store them in `IMPORTED_MODULES`.
+    Wraps `builtins.__import__` to capture imported modules in `IMPORTED_MODULES`.
     """
 
     # pylint: disable=redefined-builtin
@@ -21,10 +26,24 @@ def _wrap_import(original_import):
     def wrapper(name, globals=None, locals=None, fromlist=(), level=0):
         is_absolute_import = level == 0
         if not name.startswith("_") and is_absolute_import:
-            IMPORTED_MODULES.add(name.split(".")[0])
+            top_level_module = _get_top_level_module(name)
+            IMPORTED_MODULES.add(top_level_module)
+        return original_import(name, globals, locals, fromlist, level)
 
-        original_result = original_import(name, globals, locals, fromlist, level)
-        return original_result
+    return wrapper
+
+
+def _wrap_import_module(original_import_module):
+    """
+    Wraps `importlib.import_module` to capture imported modules in `IMPORTED_MODULES`.
+    """
+
+    @functools.wraps(original_import_module)
+    def wrapper(name, *args, **kwargs):
+        if not name.startswith("_"):
+            top_level_module = _get_top_level_module(name)
+            IMPORTED_MODULES.add(top_level_module)
+        return original_import_module(name, *args, **kwargs)
 
     return wrapper
 
@@ -38,9 +57,12 @@ def _wrap_load_pyfunc(original_load_pyfunc):
     @functools.wraps(original_load_pyfunc)
     def wrapper(*args, **kwargs):
         original_import = builtins.__import__
+        original_import_module = importlib.import_module
         builtins.__import__ = _wrap_import(original_import)
+        importlib.import_module = _wrap_import_module(original_import_module)
         result = original_load_pyfunc(*args, **kwargs)
         builtins.__import__ = original_import
+        importlib.import_module = original_import_module
         return result
 
     return wrapper
