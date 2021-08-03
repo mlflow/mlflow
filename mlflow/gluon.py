0615c7f28dfec106430a7d9b4f1fcfa97e9c3595
Harutaka Kawamura <hkawamura0130@gmail.com>
2021-08-03 19:00:39 +0900

    Add `_get_pinned_requirement` to accurately obtain package versions (#4632)
    * add _strip_local_version_identifier
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * remove importlib_metadata
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix comment
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix param doc
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Add _get_pinned_requirement
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Fix param doc
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * use _get_pinned_requirement
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * lint
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * comma
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * error handling
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * test
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/gluon.py
+++ mlflow/gluon.py
@@ -23,6 +23,7 @@ from mlflow.utils.environment import (
     _REQUIREMENTS_FILE_NAME,
     _CONSTRAINTS_FILE_NAME,
 )
+from mlflow.utils.requirements_utils import _get_pinned_requirement
 from mlflow.utils.docstring_utils import format_docstring, LOG_MODEL_PARAM_DOCS
 from mlflow.utils.file_utils import write_to
 from mlflow.utils.autologging_utils import (
@@ -258,9 +259,7 @@ def get_default_pip_requirements():
              Calls to :func:`save_model()` and :func:`log_model()` produce a pip environment
              that, at minimum, contains these requirements.
     """
-    import mxnet as mx
-
-    return ["mxnet=={}".format(mx.__version__)]
+    return [_get_pinned_requirement("mxnet")]
 
 
 def get_default_conda_env():
--- mlflow/pyfunc/model.py
+++ mlflow/pyfunc/model.py
@@ -26,6 +26,7 @@ from mlflow.utils.environment import (
     _REQUIREMENTS_FILE_NAME,
     _CONSTRAINTS_FILE_NAME,
 )
+from mlflow.utils.requirements_utils import _get_pinned_requirement
 from mlflow.utils.file_utils import write_to
 from mlflow.utils.model_utils import _get_flavor_configuration
 from mlflow.utils.file_utils import TempDir, _copy_file_or_tree
@@ -43,7 +44,7 @@ def get_default_pip_requirements():
              Calls to :func:`save_model()` and :func:`log_model()` produce a pip environment
              that, at minimum, contains these requirements.
     """
-    return ["cloudpickle=={}".format(cloudpickle.__version__)]
+    return [_get_pinned_requirement("cloudpickle")]
 
 
 def get_default_conda_env():
