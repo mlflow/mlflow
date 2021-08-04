603be982372380f5e5cb6a3378dbf15809a9fdfb
harupy <hkawamura0130@gmail.com>
2021-08-04 14:22:09 +0900

    sleep
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- .github/workflows/notify-dco-failure.js
+++ .github/workflows/notify-dco-failure.js
@@ -1,17 +1,21 @@
 module.exports = async ({ context, github }) => {
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
   const { sha: ref } = context;
   const { owner, repo } = context.repo;
   const { number: issue_number } = context.issue;
   console.log(context);
   console.log(ref, owner, repo);
 
+  await sleep(10000);
   const resp = await github.checks.listForRef({
     owner,
     repo,
     ref,
     app_id: 1861, // ID of the DCO check app
   });
-  console.log(resp);
   const dcoCheck = resp.data.check_runs[0];
   const { html_url, conclusion } = dcoCheck;
 
