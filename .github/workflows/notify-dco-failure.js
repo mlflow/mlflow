3b311a2c4243c6f9d8ae1cff55e4ac8b708e6d1e
harupy <hkawamura0130@gmail.com>
2021-08-04 16:19:08 +0900

    log
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- .github/workflows/notify-dco-failure.js
+++ .github/workflows/notify-dco-failure.js
@@ -1,15 +1,17 @@
 module.exports = async ({ context, github }) => {
-  const { after: ref } = context.payload;
   const { owner, repo } = context.repo;
   const { number: issue_number } = context.issue;
+  const { sha: ref } = context.payload.pull_request.head;
 
   function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   async function getDcoCheck() {
-    for (const sec of [0, 2, 4, 6, 8]) {
-      await sleep(sec * 1000);
+    const backoffs = [0, 2, 4, 6, 8];
+    const numAttempts = backoffs.length;
+    for (const [index, backoff] of backoff.entries()) {
+      await sleep(backoff * 1000);
       const resp = await github.checks.listForRef({
         owner,
         repo,
@@ -18,15 +20,18 @@ module.exports = async ({ context, github }) => {
       });
 
       const { check_runs } = resp.data;
-      if (check_runs.length > 0) {
+      if (check_runs.length > 0 && check_runs[0].status === "completed") {
         return check_runs[0];
       }
+      console.log(
+        `[Attempt ${index + 1}/${numAttempts}]`,
+        "The DCO check hasn't completed yet."
+      );
     }
   }
 
   const dcoCheck = await getDcoCheck();
   const { html_url, conclusion } = dcoCheck;
-
   if (conclusion === "success") {
     const body = `The DCO check failed. Please sign off your commits:\n${html_url}`;
     await github.issues.createComment({
