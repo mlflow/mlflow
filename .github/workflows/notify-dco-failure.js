907bd5c75ae417eab2bc02da973e9f5a0db3f808
harupy <hkawamura0130@gmail.com>
2021-08-04 14:35:26 +0900

    Fix ref
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- .github/workflows/notify-dco-failure.js
+++ .github/workflows/notify-dco-failure.js
@@ -1,22 +1,32 @@
 module.exports = async ({ context, github }) => {
-  function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  const { sha: ref } = context;
+  const { after: ref } = context.payload;
   const { owner, repo } = context.repo;
   const { number: issue_number } = context.issue;
   console.log(context);
   console.log(ref, owner, repo);
 
-  await sleep(10000);
-  const resp = await github.checks.listForRef({
-    owner,
-    repo,
-    ref,
-    app_id: 1861, // ID of the DCO check app
-  });
-  const dcoCheck = resp.data.check_runs[0];
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  function getDcoCheck() {
+    for (const sec of [0, 2, 4, 6, 8]) {
+      await sleep(sec * 1000);
+      const resp = await github.checks.listForRef({
+        owner,
+        repo,
+        ref,
+        app_id: 1861, // ID of the DCO check app
+      });
+
+      const { check_runs } = resp.data;
+      if (check_runs.length > 0) {
+        return check_runs[0];
+      }
+    }
+  }
+
+  const dcoCheck = getDcoCheck();
   const { html_url, conclusion } = dcoCheck;
 
   if (conclusion !== "success") {
