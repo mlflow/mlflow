6abd8f4fbd0b2db9a85fcdeea11e92bdad4f5b51
harupy <hkawamura0130@gmail.com>
2021-08-10 16:54:07 +0900

    remove flavor tests
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- dev/run-python-flavor-tests.sh
+++ dev/run-python-flavor-tests.sh
@@ -8,17 +8,10 @@ export MLFLOW_HOME=$(pwd)
 
 # Run ML framework tests in their own Python processes to avoid OOM issues due to per-framework
 # overhead
-pytest tests/h2o --large
-pytest tests/onnx --large
 pytest tests/pyfunc --large
 pytest tests/azureml --large
 pytest tests/models --large
 pytest tests/statsmodels --large
-pytest tests/gluon --large
-pytest tests/gluon_autolog --large
-pytest tests/spacy --large
-pytest tests/fastai --large
-pytest tests/shap --large
 pytest tests/paddle --large
 pytest tests/utils/test_model_utils.py --large
 pytest tests/tracking/fluent/test_fluent_autolog.py --large
