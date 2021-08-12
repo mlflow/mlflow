ece6c9e34e281906ca0a45d2c44eab6d043b06c7
harupy <hkawamura0130@gmail.com>
2021-08-12 14:46:32 +0900

    Add repos option
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/R/mlflow/.dump-r-dependencies.R
+++ mlflow/R/mlflow/.dump-r-dependencies.R
@@ -1,5 +1,8 @@
 # Make sure the current working directory is 'mlflow/R/mlflow'
 print(R.version)
+repos = getOption("repos")
+repos["CRAN"] = "http://cran.us.r-project.org"
+options(repos = repos)
 install.packages('remotes')
 saveRDS(remotes::dev_package_deps(".", dependencies = TRUE), "depends.Rds", version = 2)
 writeLines(sprintf("R-%i.%i", getRversion()$major, getRversion()$minor), "R-version")
