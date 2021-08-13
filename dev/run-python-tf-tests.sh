02f25589add959a15b7b6f19baa6086b45ef67b8
harupy <hkawamura0130@gmail.com>
2021-08-13 18:52:17 +0900

    Remove keras and tf
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- dev/run-python-tf-tests.sh
+++ dev/run-python-tf-tests.sh
@@ -6,10 +6,6 @@ err=0
 trap 'err=1' ERR
 export MLFLOW_HOME=$(pwd)
 
-
-# TODO: Fix `test_spark_datasource_autologging_crossframework.py` and remove this line
-pip install 'tensorflow==1.15.4' 'keras==2.2.5'
-
 # Run Spark autologging tests, which rely on tensorflow
 ./dev/test-spark-autologging.sh
 
