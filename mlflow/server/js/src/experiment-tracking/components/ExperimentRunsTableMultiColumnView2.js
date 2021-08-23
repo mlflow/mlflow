c5b25fd7e4097f08962fe2a71a2c75f8e0bf8289
dbczumar <corey.zumar@databricks.com>
2021-08-23 16:59:44 -0700

    Add a comment
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/server/js/src/experiment-tracking/components/ExperimentRunsTableMultiColumnView2.js
+++ mlflow/server/js/src/experiment-tracking/components/ExperimentRunsTableMultiColumnView2.js
@@ -94,6 +94,11 @@ export class ExperimentRunsTableMultiColumnView2 extends React.Component {
   constructor(props) {
     super(props);
     this.getColumnDefs = this.getColumnDefs.bind(this);
+    // In some cases, the API request to fetch run info resolves
+    // before this component is constructed and rendered. We need to get
+    // column defs here to handle that case, as well as the one already
+    // handled in componentDidUpdate for when the request resolves after the
+    // fact.
     this.columnDefs = this.getColumnDefs();
   }
 
