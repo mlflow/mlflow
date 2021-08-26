5ca16a72c7582a01285ffe0dafb2e7ea960a5058
dbczumar <corey.zumar@databricks.com>
2021-08-25 22:15:37 -0700

    Move packages_distributions inside get_requires_recursive
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/utils/requirements_utils.py
+++ mlflow/utils/requirements_utils.py
@@ -126,16 +126,6 @@ def _canonicalize_package_name(pkg_name):
     return pkg_name.lower().replace("_", "-")
 
 
-_MODULE_TO_PACKAGES = importlib_metadata.packages_distributions()
-
-
-def _module_to_packages(module_name):
-    """
-    Returns a list of packages that provide the specified module.
-    """
-    return _MODULE_TO_PACKAGES.get(module_name, [])
-
-
 def _get_requires_recursive(pkg_name):
     """
     Recursively yields both direct and transitive dependencies of the specified package.
@@ -255,8 +245,9 @@ def _infer_requirements(model_uri, flavor):
     :param: flavor: The flavor name of the model.
     :return: A list of inferred pip requirements.
     """
+    modules_to_packages = importlib_metadata.packages_distributions()
     modules = _capture_imported_modules(model_uri, flavor)
-    packages = _flatten(map(_module_to_packages, modules))
+    packages = _flatten([modules_to_packages.get(module, []) for module in modules])
     packages = map(_canonicalize_package_name, packages)
     packages = _prune_packages(packages)
     excluded_packages = [
@@ -268,7 +259,7 @@ def _infer_requirements(model_uri, flavor):
         # Exclude a package that provides the mlflow module (e.g. mlflow, mlflow-skinny).
         # Certain flavors (e.g. pytorch) import mlflow while loading a model, but mlflow should
         # not be counted as a model requirement.
-        *_MODULE_TO_PACKAGES.get("mlflow", []),
+        *modules_to_packages.get("mlflow", []),
     ]
     packages = packages - set(excluded_packages)
     return sorted(map(_get_pinned_requirement, packages))
