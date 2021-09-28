561d88ca69340430e07b75f7d5c8b0b3102ee970
Zach Bloss <zachbloss@quickenloans.com>
2021-09-28 13:55:01 -0400

    add option to use protected-route.js to secure individual routes
    Signed-off-by: Zach Bloss <ZachBloss@quickenloans.com>
    Signed-off-by: Zach Bloss <zacharybloss@gmail.com>

---
+++ mlflow/server/js/src/experiment-tracking/components/Auth/protected-route.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import { withAuthenticationRequired } from "@auth0/auth0-react";
+
+const ProtectedRoute = ({ component, ...args }) => (
+  <Route
+    component={withAuthenticationRequired(component)}
+    {...args}
+  />
+);
+
+export default ProtectedRoute;
