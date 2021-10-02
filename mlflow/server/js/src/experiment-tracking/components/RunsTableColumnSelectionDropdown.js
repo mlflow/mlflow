4e3ef7adafdd7e90ad139ac29b13d4c7f8a6af72
Marijn Valk <marijncv@hotmail.com>
2021-10-02 22:23:12 +0200

    Remove not needed state in column dropdown
    Signed-off-by: Marijn Valk <marijncv@hotmail.com>

--- mlflow/server/js/src/experiment-tracking/components/RunsTableColumnSelectionDropdown.js
+++ mlflow/server/js/src/experiment-tracking/components/RunsTableColumnSelectionDropdown.js
@@ -25,7 +25,6 @@ export class RunsTableColumnSelectionDropdown extends React.Component {
 
   state = {
     menuVisible: false,
-    diffViewSelected: false,
   };
 
   handleCheck = (checkedKeys, allKeys) => {
