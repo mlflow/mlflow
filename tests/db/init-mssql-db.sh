e511ca73a26a6bb8d4b472dad737c817ac8eb230
Marijn Valk <marijncv@hotmail.com>
2021-10-12 10:01:20 +0200

    retry mechanism in mssql test
    Signed-off-by: Marijn Valk <marijncv@hotmail.com>

--- tests/db/init-mssql-db.sh
+++ tests/db/init-mssql-db.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-sleep 60 # wait for SQL Server to startup
-/opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P 1Secure*Password1 -d master -i init-mssql-db.sql
+for BACKOFF in 0 1 2 4 8 16 32 64; do
+    if /opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P 1Secure*Password1 -d master -i init-mssql-db.sql; then
+        exit 0
+    fi
+    echo "Could not connect to SQL Server"
+    echo "Trying again in ${BACKOFF} seconds"
+    sleep $BACKOFF
+done
+exit 1
