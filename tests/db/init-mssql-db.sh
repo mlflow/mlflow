8a26bacee95a56c3326a77da11ec55ade7b0c89f
Marijn Valk <marijncv@hotmail.com>
2021-10-12 14:31:35 +0200

    improved init script
    Signed-off-by: Marijn Valk <marijncv@hotmail.com>

--- tests/db/init-mssql-db.sh
+++ tests/db/init-mssql-db.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-for BACKOFF in 0 1 2 4 8 16 32 64; do
+for BACKOFF in 0 1 2 4 8 16; do
+    if [ "$BACKOFF" -ne "0" ]; then
+        echo "Could not connect to SQL Server"
+        echo "Trying again in ${BACKOFF} seconds"
+        sleep $BACKOFF
+    fi
     if /opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P 1Secure*Password1 -d master -i init-mssql-db.sql; then
         exit 0
     fi
-    echo "Could not connect to SQL Server"
-    echo "Trying again in ${BACKOFF} seconds"
-    sleep $BACKOFF
 done
 exit 1
--- tests/db/init-mssql-db.sql
+++ tests/db/init-mssql-db.sql
@@ -3,13 +3,13 @@ GO
 
 USE mlflowdb;
 
-CREATE LOGIN mlflowuser   
-    WITH PASSWORD = 'Mlfl*wpassword1';  
-GO  
+CREATE LOGIN mlflowuser
+    WITH PASSWORD = 'Mlfl*wpassword1';
+GO
 
-CREATE USER mlflowuser FOR LOGIN mlflowuser;  
-GO  
+CREATE USER mlflowuser FOR LOGIN mlflowuser;
+GO
 
 ALTER ROLE db_owner
-	ADD MEMBER mlflowuser;  
+    ADD MEMBER mlflowuser;
 GO
