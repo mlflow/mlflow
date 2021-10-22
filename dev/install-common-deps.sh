2161120b0e779c51ea212cb209e325e388ef9ef2
harupy <17039389+harupy@users.noreply.github.com>
2021-10-22 18:26:01 +0900

    use --name
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- dev/install-common-deps.sh
+++ dev/install-common-deps.sh
@@ -28,8 +28,6 @@ conda config --get channels
 # Useful for debugging any issues with conda
 conda info -a
 conda create -q -n test-environment python=3.6
-source activate test-environment
-
 # Uninstall `certifi` via conda to avoid encoutering the following error when installing `mlflow` via pip
 # ```
 #   Attempting uninstall: certifi
@@ -37,7 +35,8 @@ source activate test-environment
 # ERROR: Cannot uninstall 'certifi'. It is a distutils installed project and thus we cannot
 # accurately determine which files belong to it which would lead to only a partial uninstall.
 # ```
-conda remove --force certifi
+conda remove --name test-environment --force certifi
+source activate test-environment
 
 python --version
 pip install --upgrade pip
