4d623c95c2dd30091d019f865e39752369c35fd6
harupy <hkawamura0130@gmail.com>
2021-10-28 20:21:49 +0900

    comment
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/utils/arguments_utils.py
+++ mlflow/utils/arguments_utils.py
@@ -8,6 +8,6 @@ def _get_arg_names(f):
     :param f: A function.
     :return: A list of argument names.
     """
-    # `inspect.getargspec` or `inspect.getfullargspec` doesn't return a wrapped function's argspec
-    # See: https://hynek.me/articles/decorators#mangled-signatures
+    # `inspect.getargspec` or `inspect.getfullargspec` doesn't work properly for a wrapped function.
+    # See https://hynek.me/articles/decorators#mangled-signatures for details.
     return list(inspect.signature(f).parameters.keys())
