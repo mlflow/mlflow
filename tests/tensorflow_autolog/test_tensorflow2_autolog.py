55afd896de5a30a8d980ffba8521ef432c3a6111
Weichen Xu <weichen.xu@databricks.com>
2021-11-04 10:21:55 +0800

    fix lint
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- tests/tensorflow_autolog/test_tensorflow2_autolog.py
+++ tests/tensorflow_autolog/test_tensorflow2_autolog.py
@@ -914,6 +914,7 @@ def test_import_tensorflow_with_fluent_autolog_enables_tf_autologging():
     # so the original keras autologging is disabled
     if Version(tf.__version__) >= Version("2.6"):
         import keras  # pylint: disable=unused-variable,unused-import
+
         assert autologging_is_disabled(mlflow.keras.FLAVOR_NAME)
 
 
