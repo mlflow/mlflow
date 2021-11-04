75280598710049c9d6a1e27744d13ba0f608f3a1
harupy <17039389+harupy@users.noreply.github.com>
2021-11-04 11:35:16 +0900

    update
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- create_temp_file.py
+++ create_temp_file.py
@@ -4,7 +4,7 @@ import tempfile
 for name in ["COM6", "foo"]:
     try:
         with tempfile.TemporaryDirectory() as tmpdir:
-            path = os.path.join(tmpdir, "COM6")
+            path = os.path.join(tmpdir, name)
 
             with open(path, "w") as f:
                 f.write("bar")
