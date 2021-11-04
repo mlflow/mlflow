8af47ba4b2f2e601852bd0e90ad7ea06ee8d81c7
harupy <17039389+harupy@users.noreply.github.com>
2021-11-04 11:29:58 +0900

    test
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

---
+++ create_temp_file.py
@@ -0,0 +1,13 @@
+import os
+import tempfile
+
+for name in ["COM6", "foo"]:
+    try:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            path = os.path.join(tmpdir, "COM6")
+
+            with open(path, "w") as f:
+                f.write("bar")
+        print("succeeded")
+    except Exception:
+        print("failed")
