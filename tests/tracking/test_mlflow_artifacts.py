ff67b4f7512039dae4709c002ef03abf7e2d3125
harupy <hkawamura0130@gmail.com>
2021-11-05 23:32:21 +0900

    update example
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/tracking/test_mlflow_artifacts.py
+++ tests/tracking/test_mlflow_artifacts.py
@@ -232,7 +232,7 @@ def test_mlflow_artifacts_example(tmpdir):
     cmd = f"""
 set -ex
 ./build.sh
-docker-compose run -v ${{PWD}}/run.py:/app/run.py client python run.py
+docker-compose run -v ${{PWD}}/example.py:/app/example.py client python example.py
 docker-compose logs
 docker-compose down {rmi_option} --volumes --remove-orphans
 """
