52dd059cb8489af8ddeff586182707b88c1c6da7
Jeremy Jordan <jejordan@duosecurity.com>
2021-11-10 09:37:13 -0500

    track metrics for all endpoints, remove high cardinality path label
    Signed-off-by: Jeremy Jordan <jejordan@duosecurity.com>

--- mlflow/server/prometheus_exporter.py
+++ mlflow/server/prometheus_exporter.py
@@ -1,32 +1,19 @@
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 from flask import request
 
+import mlflow
+
 
 def activate_prometheus_exporter(app):
-    metrics = GunicornInternalPrometheusMetrics(app, export_defaults=False)
+    def mlflow_version(req: request):
+        return mlflow.__version__
 
-    endpoint = app.view_functions
-    histogram = metrics.histogram(
-        "mlflow_requests_by_status_and_path",
-        "Request latencies and count by status and path",
-        labels={
-            "status": lambda r: r.status_code,
-            "path": lambda: change_path_for_metric(request.path),
-        },
+    metrics = GunicornInternalPrometheusMetrics(
+        app, 
+        export_defaults=True, 
+        defaults_prefix="mlflow",
+        excluded_paths=["/health"],
+        group_by=mlflow_version
     )
-    for func_name, func in endpoint.items():
-        if func_name in ["_search_runs", "_log_metric", "_log_param", "_set_tag", "_create_run"]:
-            app.view_functions[func_name] = histogram(func)
 
     return app
-
-
-def change_path_for_metric(path):
-    """
-    Replace the '/' in the metric path by '_' so grafana can correctly use it.
-    :param path: path of the metric (example: runs/search)
-    :return: path with '_' instead of '/'
-    """
-    if "mlflow/" in path:
-        path = path.split("mlflow/")[-1]
-    return path.replace("/", "_")
