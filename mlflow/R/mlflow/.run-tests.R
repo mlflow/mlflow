7c74fc4c985faf54a8f495c3bac8a7cd481c4cd4
harupy <hkawamura0130@gmail.com>
2021-11-11 18:38:01 +0900

    rename variable
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/R/mlflow/.run-tests.R
+++ mlflow/R/mlflow/.run-tests.R
@@ -12,8 +12,8 @@ install.packages(c("xml2", "rvest"))
 library(xml2)
 library(rvest)
 
-URL <- "https://cran.r-project.org/web/packages/mlflow/index.html"
-html <- read_html(URL)
+url <- "https://cran.r-project.org/web/packages/mlflow/index.html"
+html <- read_html(url)
 xpath <- '//td[text()="Published:"]/following-sibling::td[1]/text()'
 published_date <- as.Date(html_text(html_nodes(html, xpath=xpath)))
 today <- Sys.Date()
