e7d8a76fbf89b6716ea888f73d6c948d41934249
Weichen Xu <weichen.xu@databricks.com>
2021-11-16 16:53:22 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- tests/resources/mlflow-test-plugin/mlflow_test_plugin/dummy_evaluator.py
+++ tests/resources/mlflow-test-plugin/mlflow_test_plugin/dummy_evaluator.py
@@ -1,30 +1,76 @@
 import mlflow
 from mlflow.evaluation import ModelEvaluator, EvaluationMetrics, \
     EvaluationArtifact, EvaluationResult, EvaluationDataset
+from sklearn import metrics as sk_metrics
+import numpy as np
+import pickle
+
+
+class DummyEvaluationArtifact(EvaluationArtifact):
+
+    def __init__(self, content, location):
+        self.content = content
+        self.location = location
+
+    @property
+    def content(self):
+        return self.content
+
+    @property
+    def location(self) -> str:
+        return self.location
 
 
 class DummyEvaluationResult(EvaluationResult):
-    def __init__(self, predict, dataset, run_id, evaluator_config):
-        # Dummy implementation, only store arguments passed to `evaluate`, for testing
-        self.predict = predict
-        self.dataset = dataset
-        self.run_id = run_id
-        self.evaluator_config = evaluator_config
+
+    def __init__(self, metric_values, artifact_content, artifact_location):
+        self.metric_values = metric_values
+        self.artifact_content = artifact_content
+        self.artifact_location = artifact_location
+
+    @classmethod
+    def load(cls, path):
+        with open(path, 'r') as f:
+            obj = pickle.load(f)
+        return obj
+
+    def save(self, path):
+        with open(path, 'w') as f:
+            # TODO: skip dump artifact, instead, download artifact content when loading
+            pickle.dump(self, f)
+
+    @property
+    def metrics(self):
+        return self.metric_values()
+
+    @property
+    def artifacts(self):
+        return DummyEvaluationArtifact(
+            content=self.artifact_content,
+            location=self.artifact_location
+        )
 
 
-class DummyClassifierEvaluationResult(DummyEvaluationResult):
-    pass
-
-
-class DummyClassifierEvaluator(ModelEvaluator):
+class DummyEvaluator(ModelEvaluator):
 
     def can_evaluate(
         self, model_type, evaluator_config=None, **kwargs
     ):
-        return model_type == 'classifier' and evaluator_config.get('can_evaluate')
+        return evaluator_config.get('can_evaluate')
 
     def _evaluate(self, predict, dataset, run_id, evaluator_config):
-        return DummyClassifierEvaluationResult()
+        X = dataset.data
+        assert isinstance(X, np.ndarray), 'Only support array type feature input'
+        assert dataset.name is not None, 'Dataset name required'
+        y = dataset.labels
+        y_pred = predict(X)
+
+        metrics_to_calc = evaluator_config.get('metrics_to_calc')
+        metric_values = {}
+        for metric_name in metrics_to_calc:
+            metric_values[metric_name] = getattr(sk_metrics, metric_name)(y, y_pred)
+
+        return DummyEvaluationResult(metric_values)
 
     def evaluate(
         self, predict, dataset, run_id, evaluator_config=None
@@ -36,25 +82,21 @@ class DummyClassifierEvaluator(ModelEvaluator):
                 return self._evaluate(self, predict, dataset, run.info.run_id, evaluator_config)
 
 
-class DummyRegressorEvaluationResult(DummyEvaluationResult):
-    pass
-
-
-class DummyRegressorEvaluator(ModelEvaluator):
+class DummyRegressorEvaluator(DummyEvaluator):
 
     def can_evaluate(
         self, model_type, evaluator_config=None, **kwargs
     ):
-        return model_type == 'regressor' and evaluator_config.get('can_evaluate')
+        return model_type == 'regressor' and super(DummyRegressorEvaluator, self).can_evaluate(
+            model_type, evaluator_config=None, **kwargs
+        )
 
-    def _evaluate(self, predict, dataset, run_id, evaluator_config):
-        return DummyRegressorEvaluationResult()
 
-    def evaluate(
-        self, predict, dataset, run_id, evaluator_config=None
+class DummyClassifierEvaluator(DummyEvaluator):
+
+    def can_evaluate(
+        self, model_type, evaluator_config=None, **kwargs
     ):
-        if run_id is not None:
-            return self._evaluate(self, predict, dataset, run_id, evaluator_config)
-        else:
-            with mlflow.start_run() as run:
-                return self._evaluate(self, predict, dataset, run.info.run_id, evaluator_config)
+        return model_type == 'classifier' and super(DummyClassifierEvaluator, self).can_evaluate(
+            model_type, evaluator_config=None, **kwargs
+        )
