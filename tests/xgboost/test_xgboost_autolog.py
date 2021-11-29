b0b5ad1e64924aed975995361e8541953e4baa9e
Junwen Yao <jwyiao@gmail.com>
2021-11-28 21:25:04 -0800

    Autologging functionality for scikit-learn integration with XGBoost (Part 2) (#5078)
    * new commit, resolve conflict
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * add example
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * fix lint
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * address review
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * fix build_doc
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * Update mlflow/sklearn/__init__.py
    
    remove additional lines
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * remove extra lines
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * address review > TODO:(1)doc(2)test
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * address review + add tests > TODO:doc,README
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * address review + complete doc
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * fix lint
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * update examples + fix example tests
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * address review
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * address review: update example
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    Signed-off-by: lakshikaparihar <pariharlakhsika@gmail.com>

--- tests/xgboost/test_xgboost_autolog.py
+++ tests/xgboost/test_xgboost_autolog.py
@@ -142,6 +142,28 @@ def test_xgb_autolog_logs_specified_params(bst_params, dtrain):
 
 
 @pytest.mark.large
+def test_xgb_autolog_sklearn():
+
+    mlflow.xgboost.autolog()
+
+    X, y = datasets.load_iris(return_X_y=True)
+    params = {"n_estimators": 10, "reg_lambda": 1}
+    model = xgb.XGBRegressor(**params)
+
+    with mlflow.start_run() as run:
+        model.fit(X, y)
+        model_uri = mlflow.get_artifact_uri("model")
+
+    client = mlflow.tracking.MlflowClient()
+    run = client.get_run(run.info.run_id)
+    assert run.data.metrics.items() <= params.items()
+    artifacts = set(x.path for x in client.list_artifacts(run.info.run_id))
+    assert artifacts >= set(["feature_importance_weight.png", "feature_importance_weight.json"])
+    loaded_model = mlflow.xgboost.load_model(model_uri)
+    np.testing.assert_allclose(loaded_model.predict(X), model.predict(X))
+
+
+@pytest.mark.large
 def test_xgb_autolog_logs_metrics_with_validation_data(bst_params, dtrain):
     mlflow.xgboost.autolog()
     evals_result = {}
