2ea29c62bfffc5461bf77f3da15b5c00f51de19b
Weichen Xu <weichen.xu@databricks.com>
2021-11-29 19:59:33 +0800

    fix conftest.py
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import os
 import inspect
+import shutil
 from unittest import mock
 
 import pytest
@@ -96,3 +97,14 @@ def prevent_infer_pip_requirements_fallback(request):
             yield
     else:
         yield
+
+
+@pytest.fixture(autouse=True, scope="module")
+def clean_up_mlruns_direcotry(request):
+    """
+    Clean up an `mlruns` directory on each test module teardown.
+    """
+    yield
+    mlruns_dir = os.path.join(request.config.rootpath, "mlruns")
+    if os.path.exists(mlruns_dir):
+        shutil.rmtree(mlruns_dir)
