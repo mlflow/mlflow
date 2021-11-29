ea841bb7223f1cba05ec40a23d3c9c18fadb317e
Junwen Yao <jwyiao@gmail.com>
2021-11-28 21:25:04 -0800

    Autologging functionality for scikit-learn integration with XGBoost (Part 2) (#5078)
    * new commit, resolve conflict
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * add example
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * fix lint
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * address review
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * fix build_doc
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * Update mlflow/sklearn/__init__.py
    
    remove additional lines
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * remove extra lines
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * address review > TODO:(1)doc(2)test
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * address review + add tests > TODO:doc,README
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * address review + complete doc
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * fix lint
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * update examples + fix example tests
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * address review
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>
    
    * address review: update example
    
    Signed-off-by: Junwen Yao <jwyiao@gmail.com>

---
+++ examples/xgboost/xgboost_sklearn/utils.py
@@ -0,0 +1,26 @@
+import mlflow
+
+
+def yield_artifacts(run_id, path=None):
+    """Yield all artifacts in the specified run"""
+    client = mlflow.tracking.MlflowClient()
+    for item in client.list_artifacts(run_id, path):
+        if item.is_dir:
+            yield from yield_artifacts(run_id, item.path)
+        else:
+            yield item.path
+
+
+def fetch_logged_data(run_id):
+    """Fetch params, metrics, tags, and artifacts in the specified run"""
+    client = mlflow.tracking.MlflowClient()
+    data = client.get_run(run_id).data
+    # Exclude system tags: https://www.mlflow.org/docs/latest/tracking.html#system-tags
+    tags = {k: v for k, v in data.tags.items() if not k.startswith("mlflow.")}
+    artifacts = list(yield_artifacts(run_id))
+    return {
+        "params": data.params,
+        "metrics": data.metrics,
+        "tags": tags,
+        "artifacts": artifacts,
+    }
