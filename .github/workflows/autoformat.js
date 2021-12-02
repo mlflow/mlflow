775b518619b9ffeac998c08aac988cc4018b9ddb
harupy <hkawamura0130@gmail.com>
2021-12-02 12:19:47 +0900

    fix
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- .github/workflows/autoformat.js
+++ .github/workflows/autoformat.js
@@ -17,14 +17,17 @@ const createStatus = async (context, github, core) => {
   const { owner, repo } = context.repo;
   const pull_number = context.issue.number;
   const pr = await github.pulls.get({ owner, repo, pull_number });
-  const { sha, ref } = pr.data.head;
-  const repository = pr.data.repo.full_name;
+  const {
+    sha,
+    ref,
+    repo: { full_name },
+  } = pr.data.head;
   await createCommitStatus(context, github, sha, 'pending');
-  if (repository === 'mlflow/mlflow' && ref === 'master') {
+  if (full_name === 'mlflow/mlflow' && ref === 'master') {
     core.setFailed('Running autoformat bot against master branch of mlflow/mlflow is not allowed.');
   }
   return {
-    repository,
+    repository: full_name,
     pull_number,
     sha,
     ref,
