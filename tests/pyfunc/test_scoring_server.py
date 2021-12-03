d1acba78a6b1d0115c1cdb2f317fc25c9b6c879e
Andrei Batomunkuev <55283735+abatomunkuev@users.noreply.github.com>
2021-12-03 01:40:31 -0500

    BUG: fixed model serve fail with HTTP 400 on Bad Request. (#5003)
    * BUG: fixed model serve fail with HTTP 400 on Bad Request.
    
    Signed-off-by: Andrei Batomunkuev <abatomunkuev@myseneca.ca>
    
    * TEST: Updated a test case to reflect an update with HTTP errors fix
    Signed-off-by: Andrei Batomunkuev <abatomunkuev@myseneca.ca>
    
    * Updated assert statement, changed from MALFORMED_REQUEST to BAD_REQUEST
    Signed-off-by: Andrei Batomunkuev <abatomunkuev@myseneca.ca>
    
    * Updated tests: changed from MALFORMED request to BAD_REQUEST
    Signed-off-by: Andrei Batomunkuev <abatomunkuev@myseneca.ca>
    
    * BUG: updated all instances from MALFORMED_REQUEST to BAD_REQUEST
    Signed-off-by: Andrei Batomunkuev <abatomunkuev@myseneca.ca>
    
    * Changed error code when parsing tf_serving_input
    Signed-off-by: Andrei Batomunkuev <abatomunkuev@myseneca.ca>
    
    * Changed the error code when parsing tf_serving_input
    Signed-off-by: Andrei Batomunkuev <abatomunkuev@myseneca.ca>
    
    * Format
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Updated the value of the data argument that passes invalid json
    Signed-off-by: Andrei Batomunkuev <abatomunkuev@myseneca.ca>
    
    * fix test failures
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix assert message
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    Co-authored-by: dbczumar <corey.zumar@databricks.com>
    Co-authored-by: harupy <hkawamura0130@gmail.com>
    Signed-off-by: lakshikaparihar <pariharlakhsika@gmail.com>

--- tests/pyfunc/test_scoring_server.py
+++ tests/pyfunc/test_scoring_server.py
@@ -15,7 +15,7 @@ from mlflow.exceptions import MlflowException
 import mlflow.pyfunc.scoring_server as pyfunc_scoring_server
 import mlflow.sklearn
 from mlflow.models import ModelSignature, infer_signature
-from mlflow.protos.databricks_pb2 import ErrorCode, MALFORMED_REQUEST, BAD_REQUEST
+from mlflow.protos.databricks_pb2 import ErrorCode, BAD_REQUEST
 from mlflow.pyfunc import PythonModel
 from mlflow.pyfunc.scoring_server import get_cmd
 from mlflow.types import Schema, ColSpec, DataType
@@ -107,7 +107,7 @@ def test_scoring_server_responds_to_invalid_json_input_with_stacktrace_and_error
     )
     response_json = json.loads(response.content)
     assert "error_code" in response_json
-    assert response_json["error_code"] == ErrorCode.Name(MALFORMED_REQUEST)
+    assert response_json["error_code"] == ErrorCode.Name(BAD_REQUEST)
     assert "message" in response_json
     assert "stack_trace" in response_json
 
@@ -119,7 +119,7 @@ def test_scoring_server_responds_to_invalid_json_input_with_stacktrace_and_error
     )
     response_json = json.loads(response.content)
     assert "error_code" in response_json
-    assert response_json["error_code"] == ErrorCode.Name(MALFORMED_REQUEST)
+    assert response_json["error_code"] == ErrorCode.Name(BAD_REQUEST)
     assert "message" in response_json
     assert "stack_trace" in response_json
 
@@ -138,7 +138,7 @@ def test_scoring_server_responds_to_malformed_json_input_with_stacktrace_and_err
     )
     response_json = json.loads(response.content)
     assert "error_code" in response_json
-    assert response_json["error_code"] == ErrorCode.Name(MALFORMED_REQUEST)
+    assert response_json["error_code"] == ErrorCode.Name(BAD_REQUEST)
     assert "message" in response_json
     assert "stack_trace" in response_json
 
@@ -159,7 +159,7 @@ def test_scoring_server_responds_to_invalid_pandas_input_format_with_stacktrace_
     )
     response_json = json.loads(response.content)
     assert "error_code" in response_json
-    assert response_json["error_code"] == ErrorCode.Name(MALFORMED_REQUEST)
+    assert response_json["error_code"] == ErrorCode.Name(BAD_REQUEST)
     assert "message" in response_json
     assert "stack_trace" in response_json
 
@@ -198,7 +198,7 @@ def test_scoring_server_responds_to_invalid_csv_input_with_stacktrace_and_error_
     )
     response_json = json.loads(response.content)
     assert "error_code" in response_json
-    assert response_json["error_code"] == ErrorCode.Name(MALFORMED_REQUEST)
+    assert response_json["error_code"] == ErrorCode.Name(BAD_REQUEST)
     assert "message" in response_json
     assert "stack_trace" in response_json
 
