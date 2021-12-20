35b3bc7ee2ac23e37cee51b40e4c8323365baddf
harupy <17039389+harupy@users.noreply.github.com>
2021-12-20 14:16:25 +0900

    specify path
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/.build-package.R
+++ mlflow/R/mlflow/.build-package.R
@@ -6,10 +6,10 @@ options(timeout=300)
 # Install dependencies required for the submission check.
 devtools::install_deps(".", dependencies = TRUE)
 # Bundle up the package into a .tar.gz file. This file will be submitted to CRAN.
-package_path <- devtools::build(".")
+package_path <- devtools::build(".", path = ".")
 # Run the submission check against the built package.
 devtools::check_built(
-    package_path,
+    pkg = normalizePath(package_path),
     remote = should_enable_cran_incoming_checks(),
     error_on = "note",
     args = c("--no-tests", "--as-cran"),
