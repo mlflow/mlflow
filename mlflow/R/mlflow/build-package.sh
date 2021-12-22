eebd9d667abfbb634101503035cefe242dba2d51
harupy <17039389+harupy@users.noreply.github.com>
2021-12-22 15:13:07 +0900

    fix volume target
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/build-package.sh
+++ mlflow/R/mlflow/build-package.sh
@@ -3,4 +3,4 @@ set -ex
 
 image_name="mlflow-r-dev"
 docker build -f Dockerfile.dev -t $image_name .
-docker run --rm -v $(pwd):/mlflow $image_name Rscript -e 'source(".build-package.R", echo = TRUE)'
+docker run --rm -v $(pwd):/mlflow/mlflow/R/mlflow $image_name Rscript -e 'source(".build-package.R", echo = TRUE)'
