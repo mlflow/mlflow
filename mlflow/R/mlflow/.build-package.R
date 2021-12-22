9d42cbf02ef76d1b20b14a30f6ad75ac4590d198
Harutaka Kawamura <hkawamura0130@gmail.com>
2021-12-22 21:30:49 +0900

    Build R documentation using Docker (#5188)
    * rename dockerfile
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * build R doc using docker
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * update .Rbuildignore
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * fix volume target
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * use working_directory
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * add comment
    
    Signed-off-by: lakshikaparihar <pariharlakhsika@gmail.com>

--- mlflow/R/mlflow/.build-package.R
+++ mlflow/R/mlflow/.build-package.R
@@ -1,10 +1,5 @@
 source(".utils.R")
 
-# Increase the timeout length for `utils::download.file` because the default value (60 seconds)
-# could be too short to download large packages such as h2o.
-options(timeout=300)
-# Install dependencies required for the submission check.
-devtools::install_deps(".", dependencies = TRUE)
 # Bundle up the package into a .tar.gz file. This file will be submitted to CRAN.
 package_path <- devtools::build(".", path = ".")
 # Run the submission check against the built package.
