4d19a73e31a9220865095b7b166e8f1f8e67c078
lakshikaparihar <pariharlakhsika@gmail.com>
2021-12-24 16:24:17 +0530

    formatted code to pass build
    Signed-off-by: lakshikaparihar <pariharlakhsika@gmail.com>

--- examples/heart_attack_predictor/log_model.py
+++ examples/heart_attack_predictor/log_model.py
@@ -8,41 +8,46 @@ from mlflow.models.signature import ModelSignature
 
 
 # Loading model
-infile = open('heart-attack-prediction.pkl','rb')
+infile = open("heart-attack-prediction.pkl", "rb")
 model = pickle.load(infile)
-print ('Loaded Model')
+print("Loaded Model")
 infile.close()
 
 # Creating Signature
 mlflow.set_tracking_uri("sqlite:///mlruns.db")
 
-input_schema = Schema([
-  ColSpec("double", "age"),
-  ColSpec("integer", "sex"),
-  ColSpec("integer", "cp"),
-  ColSpec("double", "trtbps"),
-  ColSpec("double", "chol"),
-  ColSpec("integer", "fbs"),
-  ColSpec("integer", "restecg"),
-  ColSpec("double", "thalachh"),
-  ColSpec("integer", "exng"),
-  ColSpec("double", "oldpeak"),
-  ColSpec("integer", "slp"),
-  ColSpec("integer", "caa"),
-  ColSpec("integer", "thall"),
-])
+input_schema = Schema(
+    [
+        ColSpec("double", "age"),
+        ColSpec("integer", "sex"),
+        ColSpec("integer", "cp"),
+        ColSpec("double", "trtbps"),
+        ColSpec("double", "chol"),
+        ColSpec("integer", "fbs"),
+        ColSpec("integer", "restecg"),
+        ColSpec("double", "thalachh"),
+        ColSpec("integer", "exng"),
+        ColSpec("double", "oldpeak"),
+        ColSpec("integer", "slp"),
+        ColSpec("integer", "caa"),
+        ColSpec("integer", "thall"),
+    ]
+)
 
 signature = ModelSignature(inputs=input_schema)
 
 
-#Logging Model artifacts
-mlflow.sklearn.log_model(model, "heart-attack-prediction-model",registered_model_name="heart-attack-prediction-model",signature=signature)
-print ('Logged another model')
+# Logging Model artifacts
+mlflow.sklearn.log_model(
+    model,
+    "heart-attack-prediction-model",
+    registered_model_name="heart-attack-prediction-model",
+    signature=signature,
+)
+print("Logged another model")
 
-#Transition into production
+# Transition into production
 client = MlflowClient()
 client.transition_model_version_stage(
-    name="heart-attack-prediction-model",
-    version=1,
-    stage="Production"
-    )
+    name="heart-attack-prediction-model", version=1, stage="Production"
+)
