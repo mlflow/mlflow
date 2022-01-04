edfb5135808a2c84a776ed8cab6e6e9b648c6733
singankit <anksing@microsoft.com>
2022-01-03 17:13:30 -0800

    Model creation timestamp for log model in R
    Signed-off-by: singankit <anksing@microsoft.com>

--- mlflow/R/mlflow/R/model.R
+++ mlflow/R/mlflow/R/model.R
@@ -59,7 +59,7 @@ mlflow_timestamp <- function() {
     c(digits.secs = 2),
     format(
       as.POSIXlt(Sys.time(), tz = "GMT"),
-      "%y-%m-%dT%H:%M:%S.%OS"
+      "%y-%m-%dT%H:%M:%OS"
     )
   )
 }
