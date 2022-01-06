8f675ca667b46dfae4620aca0c67cdd1c6a78bbe
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-01-06 18:33:33 +0900

    Use python 3.7 (#5226)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/.create-test-env.R
+++ mlflow/R/mlflow/.create-test-env.R
@@ -2,7 +2,7 @@ parent_dir <- dir("../", full.names = TRUE)
 package <- parent_dir[grepl("mlflow_", parent_dir)]
 install.packages(package)
 
-mlflow:::mlflow_maybe_create_conda_env(python_version = "3.6")
+mlflow:::mlflow_maybe_create_conda_env(python_version = "3.7")
 library(reticulate)
 use_condaenv(mlflow:::mlflow_conda_env_name())
 # pinning tensorflow version to 1.14 until test_keras_model.R is fixed
