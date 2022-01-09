dd0d5aa22a3525d75509e2f5cc8527f1e4cd34e6
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-01-09 23:16:46 +0900

    Clean up CI job for R (#5232)
    * clean r job
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * build package once
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix named argument
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * remove system dependencies
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/R/mlflow/.create-test-env.R
+++ mlflow/R/mlflow/.create-test-env.R
@@ -1,15 +1,15 @@
-parent_dir <- dir("../", full.names = TRUE)
-package <- parent_dir[grepl("mlflow_", parent_dir)]
+# Install MLflow for R
+files <- dir(".", full.names = TRUE)
+package <- files[grepl("mlflow_.+\\.tar\\.gz$", files)]
 install.packages(package)
 
 mlflow:::mlflow_maybe_create_conda_env(python_version = "3.7")
-library(reticulate)
-use_condaenv(mlflow:::mlflow_conda_env_name())
+# Install python dependencies
+reticulate::conda_install(Sys.getenv("MLFLOW_HOME", "../../.."), envname = mlflow:::mlflow_conda_env_name(), pip = TRUE)
 # pinning tensorflow version to 1.14 until test_keras_model.R is fixed
 keras::install_keras(method = "conda", envname = mlflow:::mlflow_conda_env_name(), tensorflow="1.15.2")
 # pinning h5py < 3.0.0 to avoid this issue:  https://github.com/tensorflow/tensorflow/issues/44467
 # TODO: unpin after we use tensorflow >= 2.4
 reticulate::conda_install("'h5py<3.0.0'", envname = mlflow:::mlflow_conda_env_name(), pip = TRUE)
-reticulate::conda_install(Sys.getenv("MLFLOW_HOME", "../../../../."), envname = mlflow:::mlflow_conda_env_name(), pip = TRUE)
 reticulate::conda_install("xgboost", envname = mlflow:::mlflow_conda_env_name())
 reticulate::conda_install(paste0("h2o==", packageVersion("h2o")), envname = mlflow:::mlflow_conda_env_name(), pip = TRUE)
--- mlflow/R/mlflow/.run-tests.R
+++ mlflow/R/mlflow/.run-tests.R
@@ -1,23 +1,3 @@
-source("../.utils.R")
+reticulate::use_condaenv(mlflow:::mlflow_conda_env_name())
 
-parent_dir <- dir("../", full.names = TRUE)
-package <- parent_dir[grepl("mlflow_", parent_dir)]
-
-library(reticulate)
-use_condaenv(mlflow:::mlflow_conda_env_name())
-
-devtools::check_built(
-    path = package,
-    cran = TRUE,
-    remote = should_enable_cran_incoming_checks(),
-    error_on = "note",
-    args = "--no-tests"
-)
-# This runs checks that are disabled when `cran` is TRUE (e.g. unused import check).
-devtools::check_built(
-    path = package,
-    cran = FALSE,
-    error_on = "note",
-    args = "--no-tests"
-)
-source("testthat.R")
+source("testthat.R", echo = TRUE)
