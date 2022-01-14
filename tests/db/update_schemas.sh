a86a8c6db5d1da8ac0f11ccbbc1c524d69edc859
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-01-14 23:40:55 +0900

    Dump table schemas and make CI fail when they change (#5264)
    * dump table schemas
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * detect diff
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix echos
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * add exit 1
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * create directory on host
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * ignore ordering
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * lint
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * use sorted
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * update migration instructions
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

---
+++ tests/db/update_schemas.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -ex
+
+mkdir -p schemas
+./build_wheel.sh
+docker-compose down --volumes --remove-orphans
+docker-compose pull
+docker image ls | grep -E '(REPOSITORY|postgres|mysql|mssql)'
+docker-compose build
+docker-compose run mlflow-sqlite
+docker-compose run mlflow-postgres
+docker-compose run mlflow-mysql
+docker-compose run mlflow-mssql
