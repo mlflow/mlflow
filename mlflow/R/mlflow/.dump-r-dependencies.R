3f89591885871e99448a8be48f2dd8307c6aeac1
Liang Zhang <liang.zhang@databricks.com>
2022-01-26 14:39:18 -0800

    debug
    Signed-off-by: Liang Zhang <liang.zhang@databricks.com>

--- mlflow/R/mlflow/.dump-r-dependencies.R
+++ mlflow/R/mlflow/.dump-r-dependencies.R
@@ -1,6 +1,7 @@
 # Make sure the current working directory is 'mlflow/R/mlflow'
 print(R.version)
 install.packages('remotes')
+remotes::package_deps("mlflow", dependencies = TRUE)
 remotes::dev_package_deps(".", dependencies = TRUE)
 saveRDS(remotes::dev_package_deps(".", dependencies = TRUE), "depends.Rds", version = 2)
 writeLines(sprintf("R-%i.%i", getRversion()$major, getRversion()$minor), "R-version")
