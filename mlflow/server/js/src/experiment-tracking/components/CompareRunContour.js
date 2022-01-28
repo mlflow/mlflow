533136acf3816cd553fb64b31f1d6c9eb5e98fc7
Liang Zhang <liang.zhang@databricks.com>
2022-01-27 16:27:57 -0800

    fix duplicate points test
    Signed-off-by: Liang Zhang <liang.zhang@databricks.com>

--- mlflow/server/js/src/experiment-tracking/components/CompareRunContour.js
+++ mlflow/server/js/src/experiment-tracking/components/CompareRunContour.js
@@ -152,10 +152,10 @@ export class CompareRunContour extends Component {
 
     const maybeRenderPlot = () => {
       const invalidAxes = [];
-      if (xs.length < 2) {
+      if ([...new Set(xs)].length < 2) {
         invalidAxes.push('X');
       }
-      if (ys.length < 2) {
+      if ([...new Set(ys)].length < 2) {
         invalidAxes.push('Y');
       }
       if (invalidAxes.length > 0) {
