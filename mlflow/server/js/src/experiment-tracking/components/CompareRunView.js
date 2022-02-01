577c0af359120775cb84467cae33f81635eb459d
WeichenXu <weichen.xu@databricks.com>
2022-02-01 11:31:36 +0800

    Condense run comparison table (#5306)
    * init
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * use antd tooltip
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * support safari
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * fix js lint
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update test
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * fix
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update var names
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * test1
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * adjust col width by state
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * use state control visibility
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * use react ref
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * fix lint
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * address comments
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * address comments
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * address comments
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * clean
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/server/js/src/experiment-tracking/components/CompareRunView.js
+++ mlflow/server/js/src/experiment-tracking/components/CompareRunView.js
@@ -12,7 +12,7 @@ import { Link } from 'react-router-dom';
 import { getLatestMetrics } from '../reducers/MetricReducer';
 import CompareRunUtil from './CompareRunUtil';
 import Utils from '../../common/utils/Utils';
-import { Tabs } from 'antd';
+import { Tabs, Tooltip, Switch } from 'antd';
 import ParallelCoordinatesPlotPanel from './ParallelCoordinatesPlotPanel';
 import { PageHeader } from '../../shared/building_blocks/PageHeader';
 import { CollapsibleSection } from '../../common/components/CollapsibleSection';
@@ -37,6 +37,39 @@ export class CompareRunView extends Component {
     intl: PropTypes.shape({ formatMessage: PropTypes.func.isRequired }).isRequired,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableWidth: null,
+      onlyShowParamDiff: false,
+      onlyShowMetricDiff: false,
+    };
+    this.onResizeHandler = this.onResizeHandler.bind(this);
+    this.onTableBlockScrollHandler = this.onCompareRunTableScrollHandler.bind(this);
+    this.onCompareRunTableScrollHandler = this.onCompareRunTableScrollHandler.bind(this);
+
+    this.runDetailsTableRef = React.createRef();
+    this.compareRunViewRef = React.createRef();
+  }
+
+  onResizeHandler(e) {
+    const table = this.runDetailsTableRef.current;
+    if (table !== null) {
+      const containerWidth = table.clientWidth;
+      this.setState({ tableWidth: containerWidth });
+    }
+  }
+
+  onCompareRunTableScrollHandler(e) {
+    const blocks = this.compareRunViewRef.current.querySelectorAll('.compare-run-table');
+    blocks.forEach((_, index) => {
+      const block = blocks[index];
+      if (block !== e.target) {
+        block.scrollLeft = e.target.scrollLeft;
+      }
+    });
+  }
+
   componentDidMount() {
     const pageTitle = this.props.intl.formatMessage(
       {
@@ -48,12 +81,41 @@ export class CompareRunView extends Component {
       },
     );
     Utils.updatePageTitle(pageTitle);
+
+    window.addEventListener('resize', this.onResizeHandler, true);
+    window.dispatchEvent(new Event('resize'));
+  }
+
+  componentWillUnmount() {
+    // Avoid registering `onResizeHandler` every time this component mounts
+    window.removeEventListener('resize', this.onResizeHandler, true);
+  }
+
+  getTableColumnWidth() {
+    const minColWidth = 200;
+    let colWidth = minColWidth;
+
+    if (this.state.tableWidth !== null) {
+      colWidth = Math.round(this.state.tableWidth / (this.props.runInfos.length + 1));
+      if (colWidth < minColWidth) {
+        colWidth = minColWidth;
+      }
+    }
+    return colWidth;
   }
 
   render() {
     const { experiment } = this.props;
     const experimentId = experiment.getExperimentId();
     const { runInfos, runNames } = this.props;
+
+    const colWidth = this.getTableColumnWidth();
+    const colWidthStyle = {
+      width: `${colWidth}px`,
+      minWidth: `${colWidth}px`,
+      maxWidth: `${colWidth}px`,
+    };
+
     const title = (
       <FormattedMessage
         defaultMessage='Comparing {runs} Runs'
@@ -68,12 +130,13 @@ export class CompareRunView extends Component {
       <Link to={Routes.getExperimentPageRoute(experimentId)}>{experiment.getName()}</Link>,
       title,
     ];
+
     return (
-      <div className='CompareRunView'>
+      <div className='CompareRunView' ref={this.compareRunViewRef}>
         <PageHeader title={title} breadcrumbs={breadcrumbs} />
         <CollapsibleSection
           title={
-            <h1>
+            <h1 style={{ marginTop: 0, marginBottom: 0 }}>
               <FormattedMessage
                 defaultMessage='Visualizations'
                 description='Tabs title for plots on the compare runs page'
@@ -126,7 +189,7 @@ export class CompareRunView extends Component {
         </CollapsibleSection>
         <CollapsibleSection
           title={
-            <h1>
+            <h1 style={{ marginTop: 0, marginBottom: 0 }}>
               <FormattedMessage
                 defaultMessage='Run details'
                 // eslint-disable-next-line max-len
@@ -135,121 +198,175 @@ export class CompareRunView extends Component {
             </h1>
           }
         >
-          <div className='responsive-table-container'>
-            <table className='compare-table table'>
-              <thead>
-                <tr>
-                  <th scope='row' className='row-header'>
-                    <FormattedMessage
-                      defaultMessage='Run ID:'
-                      description='Row title for the run id on the experiment compare runs page'
-                    />
-                  </th>
-                  {this.props.runInfos.map((r) => (
-                    <th scope='column' className='data-value' key={r.run_uuid}>
+          <table
+            className='table compare-table compare-run-table'
+            ref={this.runDetailsTableRef}
+            onScroll={this.onCompareRunTableScrollHandler}
+          >
+            <thead>
+              <tr>
+                <th scope='row' className='head-value sticky-header' style={colWidthStyle}>
+                  <FormattedMessage
+                    defaultMessage='Run ID:'
+                    description='Row title for the run id on the experiment compare runs page'
+                  />
+                </th>
+                {this.props.runInfos.map((r) => (
+                  <th scope='row' className='data-value' key={r.run_uuid} style={colWidthStyle}>
+                    <Tooltip
+                      title={r.getRunUuid()}
+                      color='gray'
+                      placement='topLeft'
+                      overlayStyle={{ maxWidth: '400px' }}
+                      mouseEnterDelay={1.0}
+                    >
                       <Link to={Routes.getRunPageRoute(r.getExperimentId(), r.getRunUuid())}>
                         {r.getRunUuid()}
                       </Link>
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope='row' className='data-value'>
-                    <FormattedMessage
-                      defaultMessage='Run Name:'
-                      description='Row title for the run name on the experiment compare runs page'
-                    />
+                    </Tooltip>
                   </th>
-                  {runNames.map((runName, i) => {
-                    return (
-                      <td className='meta-info' key={runInfos[i].run_uuid}>
-                        <div
-                          className='truncate-text single-line'
-                          style={styles.compareRunTableCellContents}
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row' className='head-value sticky-header' style={colWidthStyle}>
+                  <FormattedMessage
+                    defaultMessage='Run Name:'
+                    description='Row title for the run name on the experiment compare runs page'
+                  />
+                </th>
+                {runNames.map((runName, i) => {
+                  return (
+                    <td className='data-value' key={runInfos[i].run_uuid} style={colWidthStyle}>
+                      <div className='truncate-text single-line'>
+                        <Tooltip
+                          title={runName}
+                          color='gray'
+                          placement='topLeft'
+                          overlayStyle={{ maxWidth: '400px' }}
+                          mouseEnterDelay={1.0}
                         >
                           {runName}
-                        </div>
-                      </td>
-                    );
-                  })}
-                </tr>
-                <tr>
-                  <th scope='row' className='data-value'>
-                    <FormattedMessage
-                      defaultMessage='Start Time:'
-                      // eslint-disable-next-line max-len
-                      description='Row title for the start time of runs on the experiment compare runs page'
-                    />
-                  </th>
-                  {this.props.runInfos.map((run) => {
-                    const startTime = run.getStartTime()
-                      ? Utils.formatTimestamp(run.getStartTime())
-                      : '(unknown)';
-                    return (
-                      <td className='meta-info' key={run.run_uuid}>
-                        {startTime}
-                      </td>
-                    );
-                  })}
-                </tr>
-                <tr>
-                  <th
-                    scope='rowgroup'
-                    className='inter-title'
-                    colSpan={this.props.runInfos.length + 1}
-                  >
-                    <h2>
-                      <FormattedMessage
-                        defaultMessage='Parameters'
-                        // eslint-disable-next-line max-len
-                        description='Row group title for parameters of runs on the experiment compare runs page'
-                      />
-                    </h2>
-                  </th>
-                </tr>
-                {this.renderDataRows(this.props.paramLists, true)}
-                <tr>
-                  <th
-                    scope='rowgroup'
-                    className='inter-title'
-                    colSpan={this.props.runInfos.length + 1}
-                  >
-                    <h2>
-                      <FormattedMessage
-                        defaultMessage='Metrics'
-                        // eslint-disable-next-line max-len
-                        description='Row group title for metrics of runs on the experiment compare runs page'
-                      />
-                    </h2>
-                  </th>
-                </tr>
-                {this.renderDataRows(
-                  this.props.metricLists,
-                  false,
-                  (key, data) => {
-                    return (
-                      <Link
-                        to={Routes.getMetricPageRoute(
-                          this.props.runInfos
-                            .map((info) => info.run_uuid)
-                            .filter((uuid, idx) => data[idx] !== undefined),
-                          key,
-                          experimentId,
-                        )}
-                        title='Plot chart'
+                        </Tooltip>
+                      </div>
+                    </td>
+                  );
+                })}
+              </tr>
+              <tr>
+                <th scope='row' className='head-value sticky-header' style={colWidthStyle}>
+                  <FormattedMessage
+                    defaultMessage='Start Time:'
+                    // eslint-disable-next-line max-len
+                    description='Row title for the start time of runs on the experiment compare runs page'
+                  />
+                </th>
+                {this.props.runInfos.map((run) => {
+                  const startTime = run.getStartTime()
+                    ? Utils.formatTimestamp(run.getStartTime())
+                    : '(unknown)';
+                  return (
+                    <td className='data-value' key={run.run_uuid} style={colWidthStyle}>
+                      <Tooltip
+                        title={startTime}
+                        color='gray'
+                        placement='topLeft'
+                        overlayStyle={{ maxWidth: '400px' }}
+                        mouseEnterDelay={1.0}
                       >
-                        {key}
-                        <i className='fas fa-chart-line' style={{ paddingLeft: '6px' }} />
-                      </Link>
-                    );
-                  },
-                  Utils.formatMetric,
-                )}
-              </tbody>
-            </table>
-          </div>
+                        {startTime}
+                      </Tooltip>
+                    </td>
+                  );
+                })}
+              </tr>
+            </tbody>
+          </table>
+        </CollapsibleSection>
+        <CollapsibleSection
+          title={
+            <h1 style={{ marginTop: 0, marginBottom: 0 }}>
+              <FormattedMessage
+                defaultMessage='Parameters'
+                // eslint-disable-next-line max-len
+                description='Row group title for parameters of runs on the experiment compare runs page'
+              />
+            </h1>
+          }
+        >
+          <Switch
+            checkedChildren='Show diff only'
+            unCheckedChildren='Show diff only'
+            onChange={(checked, e) => this.setState({ onlyShowParamDiff: checked })}
+          />
+          <br />
+          <br />
+          <table
+            className='table compare-table compare-run-table'
+            style={{ maxHeight: '500px' }}
+            onScroll={this.onCompareRunTableScrollHandler}
+          >
+            <tbody>
+              {this.renderDataRows(
+                this.props.paramLists,
+                colWidth,
+                this.state.onlyShowParamDiff,
+                true,
+              )}
+            </tbody>
+          </table>
+        </CollapsibleSection>
+        <CollapsibleSection
+          title={
+            <h1 style={{ marginTop: 0, marginBottom: 0 }}>
+              <FormattedMessage
+                defaultMessage='Metrics'
+                // eslint-disable-next-line max-len
+                description='Row group title for metrics of runs on the experiment compare runs page'
+              />
+            </h1>
+          }
+        >
+          <Switch
+            checkedChildren='Show diff only'
+            unCheckedChildren='Show diff only'
+            onChange={(checked, e) => this.setState({ onlyShowMetricDiff: checked })}
+          />
+          <br />
+          <br />
+          <table
+            className='table compare-table compare-run-table'
+            style={{ maxHeight: '300px' }}
+            onScroll={this.onCompareRunTableScrollHandler}
+          >
+            <tbody>
+              {this.renderDataRows(
+                this.props.metricLists,
+                colWidth,
+                this.state.onlyShowMetricDiff,
+                false,
+                (key, data) => {
+                  return (
+                    <Link
+                      to={Routes.getMetricPageRoute(
+                        this.props.runInfos
+                          .map((info) => info.run_uuid)
+                          .filter((uuid, idx) => data[idx] !== undefined),
+                        key,
+                        experimentId,
+                      )}
+                      title='Plot chart'
+                    >
+                      {key}
+                      <i className='fas fa-chart-line' style={{ paddingLeft: '6px' }} />
+                    </Link>
+                  );
+                },
+                Utils.formatMetric,
+              )}
+            </tbody>
+          </table>
         </CollapsibleSection>
       </div>
     );
@@ -258,7 +375,9 @@ export class CompareRunView extends Component {
   // eslint-disable-next-line no-unused-vars
   renderDataRows(
     list,
-    highlightChanges = false,
+    colWidth,
+    onlyShowDiff,
+    highlightDiff = false,
     headerMap = (key, data) => key,
     formatter = (value) => value,
   ) {
@@ -270,41 +389,54 @@ export class CompareRunView extends Component {
       records.forEach((r) => (data[r.key][i] = r.value));
     });
 
+    const colWidthStyle = {
+      width: `${colWidth}px`,
+      minWidth: `${colWidth}px`,
+      maxWidth: `${colWidth}px`,
+    };
+
+    function isAllEqual(k) {
+      return data[k].every((x) => x === data[k][0]);
+    }
+
     return keys.map((k) => {
-      let row_class = undefined;
-      if (highlightChanges) {
-        const all_equal = data[k].every((x) => x === data[k][0]);
-        if (!all_equal) {
-          row_class = 'row-changed';
-        }
+      const allEqual = isAllEqual(k);
+
+      let rowClass = undefined;
+      if (highlightDiff && !allEqual) {
+        rowClass = 'diff-row';
       }
 
-      return (
-        <tr key={k} className={row_class}>
-          <th scope='row' className='rowHeader'>
+      return onlyShowDiff && allEqual ? null : (
+        <tr key={k} className={rowClass}>
+          <th scope='row' className='head-value sticky-header' style={colWidthStyle}>
             {headerMap(k, data[k])}
           </th>
-          {data[k].map((value, i) => (
-            <td className='data-value' key={this.props.runInfos[i].run_uuid}>
-              <span
-                className='truncate-text single-line'
-                style={styles.compareRunTableCellContents}
+          {data[k].map((value, i) => {
+            const cellText = value === undefined ? '' : formatter(value);
+            return (
+              <td
+                className='data-value'
+                key={this.props.runInfos[i].run_uuid}
+                style={colWidthStyle}
               >
-                {value === undefined ? '' : formatter(value)}
-              </span>
-            </td>
-          ))}
+                <Tooltip
+                  title={cellText}
+                  color='gray'
+                  placement='topLeft'
+                  overlayStyle={{ maxWidth: '400px' }}
+                  mouseEnterDelay={1.0}
+                >
+                  <span className='truncate-text single-line'>{cellText}</span>
+                </Tooltip>
+              </td>
+            );
+          })}
         </tr>
       );
     });
   }
 }
-
-const styles = {
-  compareRunTableCellContents: {
-    maxWidth: '200px',
-  },
-};
 
 const mapStateToProps = (state, ownProps) => {
   const runInfos = [];
