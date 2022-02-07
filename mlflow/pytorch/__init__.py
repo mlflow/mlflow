3ca777207b596fc9b34d02151d56c2ed5042fe51
Yogita Mehta <yogita.mehta@databricks.com>
2022-02-07 09:25:53 -0800

    Address review comments.
    Signed-off-by: Yogita Mehta <yogita.mehta@databricks.com>

--- mlflow/pytorch/__init__.py
+++ mlflow/pytorch/__init__.py
@@ -130,10 +130,11 @@ def log_model(
 
         .. warning::
 
-            Log the model with signature to avoid inference errors. Pytorch float precision default
-            is float32, while numpy float precision default is float64. Adding the signature will 
-            ensure correct precision is used when the model is logged and the model can be queried 
-            accurately.
+            Log the model with a signature to avoid inference errors.
+            For models without signatures, the MLflow Model Server relies on the default inferred data type from NumPy.
+            However, PyTorch often expects different defaults, particularly when parsing floats.
+            Include the signature to ensure that the model is logged with the correct data type so that the MLflow model
+             server can correctly produce valid input to the model.
 
     :param pytorch_model: PyTorch model to be saved. Can be either an eager model (subclass of
                           ``torch.nn.Module``) or scripted model prepared via ``torch.jit.script``
