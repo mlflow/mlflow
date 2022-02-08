3492e8c84e2ac2556d9a296f58f055e395e643c8
James Tran <tmnhat2001@gmail.com>
2022-02-08 18:38:39 -0500

    Add `config` as an optional argument to the deployment plugin interface. This is so that we can support deleting SageMaker endpoints.
    The deployment CLI now accepts `config` as a flag argument. The config arguments will
    only be passed to the `delete_deployment()` function when the function accepts it
    as an argument. Otherwise, the config arguments will be omitted to
    maintain backwards compatibility with the existing plugin implementations.
    
    Signed-off-by: James Tran <tmnhat2001@gmail.com>

--- mlflow/deployments/base.py
+++ mlflow/deployments/base.py
@@ -131,12 +131,14 @@ class BaseDeploymentClient(abc.ABC):
         pass
 
     @abc.abstractmethod
-    def delete_deployment(self, name):
+    def delete_deployment(self, name, config=None):
         """
         Delete the deployment with name ``name`` from the specified target. Deletion should be
         idempotent (i.e. deletion should not fail if retried on a non-existent deployment).
 
         :param name: Name of deployment to delete
+        :param config: (optional) dict containing updated target-specific configuration for the
+                       deployment
         :return: None
         """
         pass
