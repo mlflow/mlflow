5ae03e0d9aa89a1b89e55d512bf494586bae027e
Yogita Mehta <yogita.mehta@databricks.com>
2022-02-09 17:04:48 -0800

    Update the DatabricksArtifactRepository test to enforce one read credential per one artifact download.
    Signed-off-by: Yogita Mehta <yogita.mehta@databricks.com>

--- mlflow/store/artifact/databricks_artifact_repo.py
+++ mlflow/store/artifact/databricks_artifact_repo.py
@@ -442,7 +442,9 @@ class DatabricksArtifactRepository(ArtifactRepository):
         read_credentials = self._get_read_credential_infos(
             run_id=self.run_id, paths=[run_relative_remote_file_path]
         )
-        assert len(read_credentials) > 0
+        # Read credentials for only one file were requested. So we expected only one value in
+        # the response.
+        assert len(read_credentials) == 1
         self._download_from_cloud(
             cloud_credential_info=read_credentials[0], dst_local_file_path=local_path
         )
