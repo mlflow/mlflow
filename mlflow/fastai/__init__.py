694aac23d1f6ff0adf65b0816e86656e430e49d6
dbczumar <corey.zumar@databricks.com>
2022-02-25 10:18:33 -0800

    Restore registered_model_name
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/fastai/__init__.py
+++ mlflow/fastai/__init__.py
@@ -380,6 +380,7 @@ def autolog(
     exclusive=False,
     disable_for_unsupported_versions=False,
     silent=False,
+    registered_model_name=None,
 ):  # pylint: disable=unused-argument
     """
     Enable automatic logging from Fastai to MLflow.
@@ -404,6 +405,9 @@ def autolog(
     :param silent: If ``True``, suppress all event logs and warnings from MLflow during Fastai
                    autologging. If ``False``, show all events and warnings during Fastai
                    autologging.
+    :param registered_model_name: If given, each time a model is trained, it is registered as a
+                                  new model version of the registered model with this name.
+                                  The registered model is created if it does not already exist.
 
     .. code-block:: python
         :caption: Example
