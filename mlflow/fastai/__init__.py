43596ea3319002134ee1000d507ca2c0de717d37
Facundo Santiago <santiagof@outlook.com>
2022-02-25 14:32:53 -0500

    Adding support for multiclass models or models returning multiple probabilities in fastai and the pyfunc flavor (#5411)
    * Signed-off-by: Facundo Santiago <santiagof@outlook.com>
    
    Adding support for multiclass models or models returning multiple probabilities in `fastai` and the `pyfunc` flavor.
    
    * Restore registered_model_name
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    Co-authored-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/fastai/__init__.py
+++ mlflow/fastai/__init__.py
@@ -317,7 +317,7 @@ class _FastaiModelWrapper:
     def predict(self, dataframe):
         dl = self.learner.dls.test_dl(dataframe)
         preds, _ = self.learner.get_preds(dl=dl)
-        return pd.DataFrame(map(np.array, preds.numpy()), columns=["predictions"])
+        return pd.Series(map(np.array, preds.numpy())).to_frame("predictions")
 
 
 def _load_pyfunc(path):
