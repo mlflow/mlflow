ba3dd85e1dad2d3cf9ff9eae4b5f4ad57562f191
dbczumar <corey.zumar@databricks.com>
2022-03-01 14:23:07 -0800

    Format
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/utils/environment.py
+++ mlflow/utils/environment.py
@@ -46,7 +46,7 @@ def _mlflow_conda_env(
     pip_deps = (["mlflow"] if install_mlflow else []) + (
         additional_pip_deps if additional_pip_deps else []
     )
-    conda_deps = (additional_conda_deps if additional_conda_deps else [])
+    conda_deps = additional_conda_deps if additional_conda_deps else []
     if pip_deps:
         pip_version = _get_pip_version()
         if pip_version is not None:
