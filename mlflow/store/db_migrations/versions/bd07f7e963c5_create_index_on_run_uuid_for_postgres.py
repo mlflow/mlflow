cbf749844f5f26e91b145e3c2189a99bcaff4db5
harupy <17039389+harupy@users.noreply.github.com>
2022-03-04 08:56:49 +0900

    apply migration to all databases
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/store/db_migrations/versions/bd07f7e963c5_create_index_on_run_uuid_for_postgres.py
+++ mlflow/store/db_migrations/versions/bd07f7e963c5_create_index_on_run_uuid_for_postgres.py
@@ -6,7 +6,6 @@ Create Date: 2022-03-03 10:14:34.037978
 
 """
 from alembic import op
-import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
@@ -17,13 +16,10 @@ depends_on = None
 
 
 def upgrade():
-    # This part of the migration is only relevant for PostgreSQL and SQLite.
     # As a fix for https://github.com/mlflow/mlflow/issues/3785, create an index on run_uuid columns
     # that have a foreign key constraint to speed up SQL operations.
-    bind = op.get_bind()
-    if bind.engine.name in ["postgresql", "sqlite"]:
-        for table in ["params", "metrics", "latest_metrics", "tags"]:
-            op.create_index(f"index_{table}_run_uuid", table, ["run_uuid"])
+    for table in ["params", "metrics", "latest_metrics", "tags"]:
+        op.create_index(f"index_{table}_run_uuid", table, ["run_uuid"])
 
 
 def downgrade():
