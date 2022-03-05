a2327e6649f62a62dfe292649b713496ab7b8c8b
Moritz Krüger <moritz.krueger@fn.de>
2022-03-05 02:02:07 +0100

    autoformat
    Signed-off-by: Moritz Krüger <moritz.krueger@fn.de>

--- tests/store/artifact/test_gcs_artifact_repo.py
+++ tests/store/artifact/test_gcs_artifact_repo.py
@@ -137,15 +137,17 @@ def test_log_artifact(gcs_mock, tmpdir):
             "blob",
             "upload_from_filename",
         ],
-        side_effect=custom_isfile
+        side_effect=custom_isfile,
     )
     repo.log_artifact(fpath)
 
     gcs_mock.Client().bucket.assert_called_with("test_bucket")
-    gcs_mock.Client().bucket().blob.assert_called_with("some/path/test.txt",
-                                                       chunk_size=repo._GCS_UPLOAD_CHUNK_SIZE)
-    gcs_mock.Client().bucket().blob().upload_from_filename \
-        .assert_called_with(fpath, timeout=repo._GCS_DEFAULT_TIMEOUT)
+    gcs_mock.Client().bucket().blob.assert_called_with(
+        "some/path/test.txt", chunk_size=repo._GCS_UPLOAD_CHUNK_SIZE
+    )
+    gcs_mock.Client().bucket().blob().upload_from_filename.assert_called_with(
+        fpath, timeout=repo._GCS_DEFAULT_TIMEOUT
+    )
 
 
 def test_log_artifacts(gcs_mock, tmpdir):
@@ -176,12 +178,15 @@ def test_log_artifacts(gcs_mock, tmpdir):
     gcs_mock.Client().bucket.assert_called_with("test_bucket")
     gcs_mock.Client().bucket().blob().upload_from_filename.assert_has_calls(
         [
-            mock.call(os.path.normpath("%s/a.txt" % subd.strpath),
-                      timeout=repo._GCS_DEFAULT_TIMEOUT),
-            mock.call(os.path.normpath("%s/b.txt" % subd.strpath),
-                      timeout=repo._GCS_DEFAULT_TIMEOUT),
-            mock.call(os.path.normpath("%s/c.txt" % subd.strpath),
-                      timeout=repo._GCS_DEFAULT_TIMEOUT),
+            mock.call(
+                os.path.normpath("%s/a.txt" % subd.strpath), timeout=repo._GCS_DEFAULT_TIMEOUT
+            ),
+            mock.call(
+                os.path.normpath("%s/b.txt" % subd.strpath), timeout=repo._GCS_DEFAULT_TIMEOUT
+            ),
+            mock.call(
+                os.path.normpath("%s/c.txt" % subd.strpath), timeout=repo._GCS_DEFAULT_TIMEOUT
+            ),
         ],
         any_order=True,
     )
@@ -210,8 +215,9 @@ def test_download_artifacts_calls_expected_gcs_client_methods(gcs_mock, tmpdir):
     repo.download_artifacts("test.txt")
     assert os.path.exists(os.path.join(tmpdir.strpath, "test.txt"))
     gcs_mock.Client().bucket.assert_called_with("test_bucket")
-    gcs_mock.Client().bucket().blob.assert_called_with("some/path/test.txt",
-                                                       chunk_size=repo._GCS_DOWNLOAD_CHUNK_SIZE)
+    gcs_mock.Client().bucket().blob.assert_called_with(
+        "some/path/test.txt", chunk_size=repo._GCS_DOWNLOAD_CHUNK_SIZE
+    )
     download_calls = gcs_mock.Client().bucket().blob().download_to_filename.call_args_list
     assert len(download_calls) == 1
     download_path_arg = download_calls[0][0][0]
