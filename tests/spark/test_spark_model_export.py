d2d9af507c3e85c4419be6f5977a3b09b38ae8c9
Xinyue Ruan <serena.rxy@gmail.com>
2022-03-07 16:39:31 +0800

    feat: support Transformers for model tracking
    Signed-off-by: Xinyue Ruan <serena.rxy@gmail.com>

--- tests/spark/test_spark_model_export.py
+++ tests/spark/test_spark_model_export.py
@@ -21,7 +21,7 @@ import mlflow.pyfunc.scoring_server as pyfunc_scoring_server
 import mlflow.tracking
 from mlflow import pyfunc
 from mlflow import spark as sparkm
-from mlflow.exceptions import MlflowException
+from mlflow.exceptions import MlflowException  # pylint: disable=unused-import
 from mlflow.models import Model, infer_signature
 from mlflow.models.utils import _read_example
 from mlflow.store.artifact.s3_artifact_repo import S3ArtifactRepository
@@ -275,10 +275,12 @@ def test_estimator_model_export(spark_model_estimator, model_path, spark_custom_
 
 @pytest.mark.large
 def test_transformer_model_export(spark_model_transformer, model_path, spark_custom_env):
-    with pytest.raises(MlflowException, match="Cannot serialize this model"):
-        sparkm.save_model(
-            spark_model_transformer.model, path=model_path, conda_env=spark_custom_env
-        )
+    sparkm.save_model(spark_model_transformer.model, path=model_path, conda_env=spark_custom_env)
+    # score and compare the reloaded sparkml model
+    reloaded_model = sparkm.load_model(model_uri=model_path)
+    preds_df = reloaded_model.transform(spark_model_transformer.spark_df)
+    preds = [x.features for x in preds_df.select("features").collect()]
+    assert spark_model_transformer.predictions == preds
 
 
 @pytest.mark.large
@@ -391,10 +393,39 @@ def test_sparkml_estimator_model_log(
 
 
 @pytest.mark.large
-def test_sparkml_model_log_invalid_args(spark_model_transformer, model_path):
-    # pylint: disable=unused-argument
-    with pytest.raises(MlflowException, match="Cannot serialize this model"):
-        sparkm.log_model(spark_model=spark_model_transformer.model, artifact_path="model0")
+@pytest.mark.parametrize("should_start_run", [False, True])
+@pytest.mark.parametrize("use_dfs_tmpdir", [False, True])
+def test_sparkml_transformer_model_log(
+    tmpdir, spark_model_transformer, should_start_run, use_dfs_tmpdir
+):
+    old_tracking_uri = mlflow.get_tracking_uri()
+    if use_dfs_tmpdir:
+        dfs_tmpdir = None
+    else:
+        dfs_tmpdir = tmpdir.join("test").strpath
+
+    try:
+        tracking_dir = os.path.abspath(str(tmpdir.join("mlruns")))
+        mlflow.set_tracking_uri("file://%s" % tracking_dir)
+        if should_start_run:
+            mlflow.start_run()
+        artifact_path = "model"
+        sparkm.log_model(
+            artifact_path=artifact_path,
+            spark_model=spark_model_transformer.model,
+            dfs_tmpdir=dfs_tmpdir,
+        )
+        model_uri = "runs:/{run_id}/{artifact_path}".format(
+            run_id=mlflow.active_run().info.run_id, artifact_path=artifact_path
+        )
+
+        reloaded_model = sparkm.load_model(model_uri=model_uri, dfs_tmpdir=dfs_tmpdir)
+        preds_df = reloaded_model.transform(spark_model_transformer.spark_df)
+        preds = [x.features for x in preds_df.select("features").collect()]
+        assert spark_model_transformer.predictions == preds
+    finally:
+        mlflow.end_run()
+        mlflow.set_tracking_uri(old_tracking_uri)
 
 
 @pytest.mark.large
