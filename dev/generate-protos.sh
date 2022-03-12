ab286ae1b31cc7bec8b21a04aa4b2e7f29af0e91
Raphael Randschau <nicolai86@me.com>
2022-03-11 17:24:28 -0800

    generate go files based on proto

--- dev/generate-protos.sh
+++ dev/generate-protos.sh
@@ -2,14 +2,15 @@
 
 set -ex
 PROTOC_VERSION="$(protoc --version)"
-if [[ "$PROTOC_VERSION" != 'libprotoc 3.6.0' && "$PROTOC_VERSION" != 'libprotoc 3.6.1' ]]; then
-	echo "Required libprotoc versions to be 3.6.0 or 3.6.1 (preferred)."
-	echo "We found: $PROTOC_VERSION"
-	exit 1
-fi
+# if [[ "$PROTOC_VERSION" != 'libprotoc 3.6.0' && "$PROTOC_VERSION" != 'libprotoc 3.6.1' ]]; then
+# 	echo "Required libprotoc versions to be 3.6.0 or 3.6.1 (preferred)."
+# 	echo "We found: $PROTOC_VERSION"
+# 	exit 1
+# fi
 PROTOS="mlflow/protos"
 protoc -I="$PROTOS" \
     --python_out="$PROTOS" \
+    --go_out="mlflow/go/client" --go-grpc_out="mlflow/go/client" --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative \
     --java_out="mlflow/java/client/src/main/java" \
     "$PROTOS"/databricks.proto \
     "$PROTOS"/service.proto \
