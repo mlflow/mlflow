96051eb9603a4d226cbeb46303c5afb7f5a47b12
harupy <17039389+harupy@users.noreply.github.com>
2022-03-16 20:26:46 +0900

    create Dockerfile.devel
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/build-package.sh
+++ mlflow/R/mlflow/build-package.sh
@@ -2,5 +2,11 @@
 set -ex
 
 image_name="mlflow-r-dev"
-docker build -f Dockerfile.dev -t $image_name .
+
+if [ "${USE_DEVEL:-false}" = "true" ]
+then
+  docker_file="Dockerfile.dev"
+else
+  docker_file="Dockerfile.devel"
+docker build -f $docker_file -t $image_name .
 docker run --rm -v $(pwd):/mlflow/mlflow/R/mlflow $image_name Rscript -e 'source(".build-package.R", echo = TRUE)'
