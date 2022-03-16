392db02f42a3419ba4c48cc7fa350e4c1aa2465d
Weichen Xu <weichen.xu@databricks.com>
2022-03-16 16:16:36 +0800

    debug ci
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- tests/pyfunc/test_spark.py
+++ tests/pyfunc/test_spark.py
@@ -66,7 +66,7 @@ def get_spark_session(conf):
     conf.set(key="spark_session.python.worker.reuse", value=True)
     return (
         pyspark.sql.SparkSession.builder.config(conf=conf)
-        .master("local-cluster[2, 1, 1024]")
+        .master("local[2]")  # local-cluster[2, 1, 1024]
         .getOrCreate()
     )
 
