58ea6cc2a659528733061e9700dfa159f98b1011
harupy <17039389+harupy@users.noreply.github.com>
2022-03-17 11:57:42 +0900

    swap
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/R/mlflow/build-package.sh
+++ mlflow/R/mlflow/build-package.sh
@@ -6,9 +6,9 @@ image_name="mlflow-r-dev"
 # Set the default to false if R-devel is unstable
 if [ "${USE_DEVEL:-true}" = "true" ]
 then
-  docker_file="Dockerfile.dev"
-else
   docker_file="Dockerfile.devel"
+else
+  docker_file="Dockerfile.dev"
 fi
 docker build -f $docker_file -t $image_name .
 docker run --rm -v $(pwd):/mlflow/mlflow/R/mlflow $image_name Rscript -e 'source(".build-package.R", echo = TRUE)'
