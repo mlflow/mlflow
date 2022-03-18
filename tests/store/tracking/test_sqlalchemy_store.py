cf726b637c7315bbc05b26397f21eb370827b0bb
Ben Wilson <39283302+BenWilson2@users.noreply.github.com>
2022-03-17 21:49:48 -0400

    fix for MSSQL paginated list experiments on behalf of @HaraldVanWoerkom (#5508)
    * fix for MSSQL paginated list experiments on behalf of @HaraldVanWoerkom
    
    Signed-off-by: Ben Wilson <benjamin.wilson@databricks.com>
    
    * test it against the db test suite for mssql
    
    Signed-off-by: Ben Wilson <benjamin.wilson@databricks.com>

--- tests/store/tracking/test_sqlalchemy_store.py
+++ tests/store/tracking/test_sqlalchemy_store.py
@@ -1754,18 +1754,22 @@ class TestSqlAlchemyStoreSqlite(unittest.TestCase, AbstractStoreTest):
         This JSON dump can be replicated by installing MLflow version 1.2.0 and executing the
         following code from the directory containing this test suite:
 
-        >>> import json
-        >>> import mlflow
-        >>> from mlflow.tracking.client import MlflowClient
-        >>> mlflow.set_tracking_uri(
-        ...     "sqlite:///../../resources/db/db_version_7ac759974ad8_with_metrics.sql")
-        >>> client = MlflowClient()
-        >>> summary_metrics = {
-        ...     run.info.run_id: run.data.metrics for run
-        ...     in client.search_runs(experiment_ids="0")
-        ... }
-        >>> with open("dump.json", "w") as dump_file:
-        >>>     json.dump(summary_metrics, dump_file, indent=4)
+        .. code-block:: python
+
+          import json
+          import mlflow
+          from mlflow.tracking.client import MlflowClient
+
+          mlflow.set_tracking_uri(
+              "sqlite:///../../resources/db/db_version_7ac759974ad8_with_metrics.sql")
+          client = MlflowClient()
+          summary_metrics = {
+              run.info.run_id: run.data.metrics for run
+              in client.search_runs(experiment_ids="0")
+          }
+          with open("dump.json", "w") as dump_file:
+              json.dump(summary_metrics, dump_file, indent=4)
+
         """
         current_dir = os.path.dirname(os.path.abspath(__file__))
         db_resources_path = os.path.normpath(
