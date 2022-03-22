5cb45ee358d74d615731c6a676517a50af2e9dff
harupy <hkawamura0130@gmail.com>
2022-03-22 23:49:45 +0900

    remove grep
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/db/entrypoint.sh
+++ tests/db/entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -ex
 
-# Note this script is executed after mounting volumes
-if [ -z "$(pip list | grep '^mlflow ')" ]; then
-  pip install --no-deps -e .
-fi
+# Install mlflow
+pip install --no-deps -e .
 
 # For Microsoft SQL server, wait until the database is up and running
 if [[ $MLFLOW_TRACKING_URI == mssql* ]]; then
   ./tests/db/init-mssql-db.sh
 fi
 
-$@
+# Run the command
+"$@"
