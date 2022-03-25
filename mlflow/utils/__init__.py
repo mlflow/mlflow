6c7df90f7708288b91f27e0825af294ddec95032
WeichenXu <weichen.xu@databricks.com>
2022-03-25 17:30:21 +0800

    Restoring virtual python environment for mlflow pyfunc spark_udf (#5487)
    * init
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * debug ci
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * black
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * fix tests
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * fix test
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    Signed-off-by: Varun Kumar Vijay <vvijay@bolt.com>

--- mlflow/utils/__init__.py
+++ mlflow/utils/__init__.py
@@ -172,3 +172,16 @@ def _inspect_original_var_name(var, fallback_name):
 
     except Exception:
         return fallback_name
+
+
+def find_free_port():
+    """
+    Find free socket port on local machine.
+    """
+    import socket
+    from contextlib import closing
+
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(("", 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
