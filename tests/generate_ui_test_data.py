43c15f7aea7ca737ce41c02d1d5e996006aa3006
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-03-29 09:43:44 +0900

    Use latest version of black fo fix lint check (#5548)
    * Use latest version of black
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * run black
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/generate_ui_test_data.py
+++ tests/generate_ui_test_data.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
         parameters = {
             "this is a pretty long parameter name": "NA10921-test_file_2018-08-10.txt",
         }
-        metrics = {"grower": [i ** 1.2 for i in range(10)]}
+        metrics = {"grower": [i**1.2 for i in range(10)]}
         log_params(parameters)
         log_metrics(metrics)
 
