33fbbe241609cea1a123b2852998623b32a795ed
Arjun DCunha <arjun.dcunha@databricks.com>
2022-04-04 15:53:02 -0700

    arg
    Signed-off-by: Arjun DCunha <arjun.dcunha@databricks.com>

--- mlflow/pyfunc/scoring_server/__init__.py
+++ mlflow/pyfunc/scoring_server/__init__.py
@@ -142,9 +142,7 @@ def parse_csv_input(csv_input, schema: Schema = None):
                       containing such a string representation.
     :param schema: Optional schema specification to be used during parsing.
     """
-
     try:
-        csv_input = StringIO(csv_input)
         if schema is None:
             return pd.read_csv(csv_input)
         else:
@@ -267,7 +265,8 @@ def init(model: PyFuncModel):
         # Convert from CSV to pandas
         if mime_type == CONTENT_TYPE_CSV and not content_format:
             data = flask.request.data.decode("utf-8")
-            data = parse_csv_input(csv_input=data, schema=input_schema)
+            csv_input = StringIO(data)
+            data = parse_csv_input(csv_input=csv_input, schema=input_schema)
         elif mime_type == CONTENT_TYPE_JSON and not content_format:
             json_str = flask.request.data.decode("utf-8")
             data = infer_and_parse_json_input(json_str, input_schema)
