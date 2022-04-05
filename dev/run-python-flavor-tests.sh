4255a1ccbc334855511efa189ebc25251d33ccf2
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-04-05 15:08:57 +0900

    Remove duplicate spark autologging ML tests (#5602)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- dev/run-python-flavor-tests.sh
+++ dev/run-python-flavor-tests.sh
@@ -19,7 +19,6 @@ pytest tests/paddle --large
 
 pytest tests/tracking/fluent/test_fluent_autolog.py --large
 pytest tests/autologging --large
-find tests/spark/autologging/ml -name 'test*.py' | xargs -L 1 pytest --large
 pytest tests/test_mlflow_lazily_imports_ml_packages.py --lazy-import
 
 test $err = 0
