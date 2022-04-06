f3475ec69d1557f5c0c648eb0544d87cf35818d2
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-04-07 01:18:15 +0900

    Disable `import/no-anonymous-default-export` warnings (#5615)
    * disable warnings
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    
    * use eslint-disable-next-line
    
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/server/js/src/experiment-tracking/utils/test-utils/Fixtures.js
+++ mlflow/server/js/src/experiment-tracking/utils/test-utils/Fixtures.js
@@ -21,6 +21,7 @@ const createRunInfo = () => {
   return RunInfo.fromJs({ run_uuid: 0 });
 };
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   createExperiment,
   createRunInfo,
