2e6d20ae2ebe2f8d001fc671d3f55fa813cb933f
harupy <17039389+harupy@users.noreply.github.com>
2022-04-08 08:22:50 +0900

    warning
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/utils/virtualenv.py
+++ mlflow/utils/virtualenv.py
@@ -153,9 +153,10 @@ def _get_python_env(local_model_path):
         build_packages = ("python", *PythonEnv.BUILD_PACKAGES)
         conda_deps = [d for d in conda_deps if _get_package_name(d) not in build_packages]
         if conda_deps:
-            raise MlflowException(
-                f"Cannot restore this model's environment with virtualenv because it contains "
-                f"conda dependencies: {conda_deps}."
+            _logger.warning(
+                f"Found this model contains conda dependencies: {conda_deps}. The resulting "
+                "virtualenv environment will not install them and may not be able to load the "
+                "model."
             )
         if requirements_file.exists():
             deps = PythonEnv.get_dependencies_from_conda_yaml(conda_env_file)
