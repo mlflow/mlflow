b099966037b76dfb0a4a250a9226f10b83ab89da
Corey Zumar <39497902+dbczumar@users.noreply.github.com>
2022-04-08 11:35:45 -0700

    Update tests/utils/test_process_utils.py
    Co-authored-by: Harutaka Kawamura <hkawamura0130@gmail.com>

--- tests/utils/test_process_utils.py
+++ tests/utils/test_process_utils.py
@@ -51,7 +51,7 @@ def test_cache_return_value_per_process():
         # We don't create child process by `multiprocessing.Process` because
         # `multiprocessing.Process` creates child process by pickling the target function
         # and start a new process to run the pickled function. But the global variable
-        # `_per_process_value_cache_map` dict content is not pickled, this make child process
+        # `_per_process_value_cache_map` dict content is not pickled, this makes child process
         # automatically clear the `_per_process_value_cache_map` dict content.
         pid = os.fork()
         if pid > 0:
