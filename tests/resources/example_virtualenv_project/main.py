2cc2a523334dc94b607a2c6aa0d20f9c6b3ace6f
harupy <hkawamura0130@gmail.com>
2022-04-11 14:20:37 +0900

    use mlflow
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/resources/example_virtualenv_project/main.py
+++ tests/resources/example_virtualenv_project/main.py
@@ -3,8 +3,13 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 
+import mlflow
+
 X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
 y = np.array([1, 1, 2, 2])
 
 clf = make_pipeline(StandardScaler(), SVC(gamma="auto"))
 clf.fit(X, y)
+
+with mlflow.start_run():
+    mlflow.sklearn.log_model(clf, artifact_path="model")
