14187bfc87f7ebaeaa826ed35f75d66f3997c545
harupy <hkawamura0130@gmail.com>
2022-04-20 18:21:30 +0900

    fix comment
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/projects/test_virtualenv_projects.py
+++ tests/projects/test_virtualenv_projects.py
@@ -9,7 +9,8 @@ def test_virtualenv_project_execution():
 
 
 def test_virtualenv_project_execution_without_env_manager():
-    # Project should be executed using virtualenv without explicitly specifing `env_manager`
+    # python_env project should be executed using virtualenv without explicitly specifying
+    # env_manager="virtualenv"
     submitted_run = mlflow.projects.run(TEST_VIRTUALENV_PROJECT_DIR)
     submitted_run.wait()
 
