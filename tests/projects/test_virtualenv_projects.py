96161ef2b6fa91dcc7e6ef94786cb6977276c957
harupy <hkawamura0130@gmail.com>
2022-04-21 14:24:11 +0900

    improvt tests
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/projects/test_virtualenv_projects.py
+++ tests/projects/test_virtualenv_projects.py
@@ -16,7 +16,9 @@ spy_on_create_virtualenv = mock.patch(
 
 @spy_on_create_virtualenv
 def test_virtualenv_project_execution(create_virtualenv_spy):
-    submitted_run = mlflow.projects.run(TEST_VIRTUALENV_PROJECT_DIR, env_manager="virtualenv")
+    submitted_run = mlflow.projects.run(
+        TEST_VIRTUALENV_PROJECT_DIR, entry_point="test", env_manager="virtualenv"
+    )
     submitted_run.wait()
     create_virtualenv_spy.assert_called_once()
 
@@ -25,21 +27,25 @@ def test_virtualenv_project_execution(create_virtualenv_spy):
 def test_virtualenv_project_execution_without_env_manager(create_virtualenv_spy):
     # python_env project should be executed using virtualenv without explicitly specifying
     # env_manager="virtualenv"
-    submitted_run = mlflow.projects.run(TEST_VIRTUALENV_PROJECT_DIR)
+    submitted_run = mlflow.projects.run(TEST_VIRTUALENV_PROJECT_DIR, entry_point="test")
     submitted_run.wait()
     create_virtualenv_spy.assert_called_once()
 
 
 @spy_on_create_virtualenv
 def test_virtualenv_project_execution_local(create_virtualenv_spy):
-    submitted_run = mlflow.projects.run(TEST_VIRTUALENV_PROJECT_DIR, env_manager="local")
+    submitted_run = mlflow.projects.run(
+        TEST_VIRTUALENV_PROJECT_DIR, entry_point="main", env_manager="local"
+    )
     submitted_run.wait()
     create_virtualenv_spy.assert_not_called()
 
 
 @spy_on_create_virtualenv
 def test_virtualenv_conda_project_execution(create_virtualenv_spy):
-    submitted_run = mlflow.projects.run(TEST_VIRTUALENV_CONDA_PROJECT_DIR, env_manager="virtualenv")
+    submitted_run = mlflow.projects.run(
+        TEST_VIRTUALENV_CONDA_PROJECT_DIR, entry_point="test", env_manager="virtualenv"
+    )
     submitted_run.wait()
     create_virtualenv_spy.assert_called_once()
 
--- tests/resources/example_virtualenv_project/entrypoint.py
+++ tests/resources/example_virtualenv_project/entrypoint.py
@@ -1,9 +1,24 @@
+import argparse
+import sys
+
 import numpy as np
+import sklearn
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 
 import mlflow
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--test",
+    action="store_true",
+    help="Check python and sklearn versions are correct",
+)
+args = parser.parse_args()
+if args.test:
+    assert sys.version_info[:3] == (3, 8, 13)
+    assert sklearn.__version__ == "1.0.2"
 
 X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
 y = np.array([1, 1, 2, 2])
