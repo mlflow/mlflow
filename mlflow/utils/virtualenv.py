212a5b77b2118333f039bfe8cdd7e07148794a7a
harupy <hkawamura0130@gmail.com>
2022-04-25 11:30:22 +0900

    remove unused imports
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/utils/virtualenv.py
+++ mlflow/utils/virtualenv.py
@@ -8,7 +8,7 @@ from packaging.version import Version
 
 from mlflow.exceptions import MlflowException
 from mlflow.utils.process import _exec_cmd, _join_commands, _IS_UNIX
-from mlflow.utils.requirements_utils import _get_package_name, _parse_requirements
+from mlflow.utils.requirements_utils import _parse_requirements
 from mlflow.utils.environment import (
     _PythonEnv,
     _PYTHON_ENV_FILE_NAME,
@@ -17,7 +17,7 @@ from mlflow.utils.environment import (
     _get_mlflow_env_name,
     _get_pip_install_mlflow,
 )
-from mlflow.utils.conda import _get_conda_dependencies, _PIP_CACHE_DIR
+from mlflow.utils.conda import _PIP_CACHE_DIR
 from mlflow.utils.databricks_utils import is_in_databricks_runtime
 
 _MLFLOW_ENV_ROOT_ENV_VAR = "MLFLOW_ENV_ROOT"
