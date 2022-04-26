f2a26e6a411201f2a7c087cbd66f978aa090e42f
Mercè Martín Prats <merce@bigml.com>
2022-04-26 12:48:08 +0200

    Adding different types of model to tests
    Signed-off-by: Mercè Martín Prats <merce@bigml.com>

--- tests/bigml/test_bigml_deploy.py
+++ tests/bigml/test_bigml_deploy.py
@@ -18,6 +18,23 @@ def res_filename(file):
     return pkg_resources.resource_filename("mlflow", os.path.join(MODELS_PATH, file))
 
 
+def local_model_check(model, examples, model_path):
+    """Generic function to test local model registry, recovery and scoring"""
+    local_model = SupervisedModel(model)
+    predictions = [local_model.predict(example, full=True) for example in examples]
+    save_model(model, path=model_path)
+    loaded_model = load_model(model_path)
+    loaded_model_predictions = [loaded_model.predict(example, full=True) for example in examples]
+    for index, prediction in enumerate(predictions):
+        assert prediction == loaded_model_predictions[index]
+
+    # Loading pyfunc model
+    pyfunc_loaded = pyfunc.load_model(model_path)
+    pyfunc_predictions = pyfunc_loaded.predict(pd.DataFrame.from_records(examples))
+    for index, prediction in enumerate(predictions):
+        assert pyfunc_predictions[index] == prediction
+
+
 @pytest.fixture
 def diabetes_examples():
     filename = res_filename("logistic_regression.json")
@@ -51,9 +68,19 @@ def diabetes_logistic():
 
 @pytest.fixture
 def diabetes_ensemble():
+    model_list = []
     filename = res_filename("ensemble.json")
     with open(filename) as handler:
-        return json.load(handler)
+        ensemble = json.load(handler)
+        model_list.append(ensemble)
+    try:
+        for model in ensemble["object"]["models"]:
+            filename = model.replace("/", "_")
+            with open(res_filename(filename)) as handler:
+                model_list.append(json.load(handler))
+        return model_list
+    except KeyError:
+        raise ValueError("This is not a correct ensemble model")
 
 
 @pytest.fixture
@@ -69,19 +96,15 @@ def model_path(tmpdir):
 
 
 @pytest.mark.large
-def test_model_save_load(diabetes_logistic, diabetes_examples, model_path):
-    local_model = SupervisedModel(diabetes_logistic)
-    predictions = [local_model.predict(example, full=True) for example in diabetes_examples]
-    save_model(diabetes_logistic, path=model_path)
-    loaded_model = load_model(model_path)
-    loaded_model_predictions = [
-        loaded_model.predict(example, full=True) for example in diabetes_examples
-    ]
-    for index, prediction in enumerate(predictions):
-        assert prediction == loaded_model_predictions[index]
+def test_logistic_save_load(diabetes_logistic, diabetes_examples, model_path):
+    local_model_check(diabetes_logistic, diabetes_examples, model_path)
 
-    # Loading pyfunc model
-    pyfunc_loaded = pyfunc.load_model(model_path)
-    pyfunc_predictions = pyfunc_loaded.predict(pd.DataFrame.from_records(diabetes_examples))
-    for index, prediction in enumerate(predictions):
-        assert pyfunc_predictions[index] == prediction
+
+@pytest.mark.large
+def test_linear_save_load(wines_linear, wines_examples, model_path):
+    local_model_check(wines_linear, wines_examples, model_path)
+
+
+@pytest.mark.large
+def test_ensemble_save_load(diabetes_ensemble, diabetes_examples, model_path):
+    local_model_check(diabetes_ensemble, diabetes_examples, model_path)
