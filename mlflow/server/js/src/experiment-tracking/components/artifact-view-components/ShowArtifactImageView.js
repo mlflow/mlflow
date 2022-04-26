d71ddbfb46ae69b7e1f2577a54224a4f94895ac2
harupy <hkawamura0130@gmail.com>
2022-04-27 08:54:44 +0900

    improve
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/server/js/src/experiment-tracking/components/artifact-view-components/ShowArtifactImageView.js
+++ mlflow/server/js/src/experiment-tracking/components/artifact-view-components/ShowArtifactImageView.js
@@ -2,29 +2,40 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './ShowArtifactImageView.css';
 import { getSrc } from './ShowArtifactPage';
-import { Image, Skeleton } from 'antd';
+import { Image } from 'antd';
+import { Skeleton } from '@databricks/design-system';
+import { css } from 'emotion';
 
 const ShowArtifactImageView = ({ runUuid, path }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [previewVisible, setPreviewVisible] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
   }, [runUuid, path]);
 
+  const src = getSrc(path, runUuid);
   return (
-    <div className='image-outer-container'>
+    <div className={classNames.imageOuterContainer}>
       {isLoading && <Skeleton active />}
-      <Image
-        style={
-          isLoading
-            ? { display: 'none' }
-            : // Workaround for
-              // https://github.com/ant-design/ant-design/discussions/28434#discussion-77663
-              { display: 'inline' }
-        }
-        src={getSrc(path, runUuid)}
-        onLoad={() => setIsLoading(false)}
-      />
+      <div className={isLoading ? classNames.hidden : classNames.imageWrapper}>
+        <img
+          className={classNames.image}
+          src={src}
+          onLoad={() => setIsLoading(false)}
+          onClick={() => setPreviewVisible(true)}
+        />
+      </div>
+      <div className={classNames.hidden}>
+        <Image.PreviewGroup
+          preview={{
+            visible: previewVisible,
+            onVisibleChange: (visible) => setPreviewVisible(visible),
+          }}
+        >
+          <Image src={src} />
+        </Image.PreviewGroup>
+      </div>
     </div>
   );
 };
@@ -32,6 +43,25 @@ const ShowArtifactImageView = ({ runUuid, path }) => {
 ShowArtifactImageView.propTypes = {
   runUuid: PropTypes.string.isRequired,
   path: PropTypes.string.isRequired,
+};
+
+const classNames = {
+  imageOuterContainer: css({
+    minHeight: '500px',
+    maxHeight: '1500px',
+    padding: '10px',
+    overflow: 'auto',
+  }),
+  imageWrapper: css({ display: 'inline-block' }),
+  image: css({
+    width: '100%',
+    height: 'auto',
+    cursor: 'pointer',
+    '&:hover': {
+      boxShadow: '0 0 4px gray',
+    },
+  }),
+  hidden: css({ display: 'none' }),
 };
 
 export default ShowArtifactImageView;
