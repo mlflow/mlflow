b7c252a9af25f4ea1ff92e703405fd134753364c
Theodoros Ntakouris <ntakouris@lambda-automata.eu>
2022-05-03 21:33:22 +0300

    rm to str for tensors and ndarrays
    Signed-off-by: Theodoros Ntakouris <ntakouris@lambda-automata.eu>

--- tests/tracking/fluent/test_metric_value_conversion_utils.py
+++ tests/tracking/fluent/test_metric_value_conversion_utils.py
@@ -28,31 +28,12 @@ class TestMetricValueConversionUtils(unittest.TestCase):
             convert_metric_value_to_float_if_possible(multi_item_torch_tensor)
 
     def test_convert_metric_value_to_str(self):
-        str_mertic_val = random_str(random_int(10, 50))
-
-        self.assertEqual(str_mertic_val, convert_metric_value_to_str_if_possible(str_mertic_val))
+        str_mertic_val = random_str()
 
         random_parent = pyspark.ml.util.Identifiable()
         pyspark_ml_param = pyspark.ml.param.Param(random_parent, str_mertic_val, "doc")
 
         self.assertEqual(str_mertic_val, convert_metric_value_to_str_if_possible(pyspark_ml_param))
-
-        ndarray_val = np.random.rand(1)
-        self.assertEqual(
-            f"[{float(ndarray_val[0])}]", convert_metric_value_to_str_if_possible(ndarray_val)
-        )
-
-        tf_tensor_val = tf.random.uniform([])
-        self.assertEqual(
-            f"[{float(tf_tensor_val.numpy())}]",
-            convert_metric_value_to_str_if_possible(tf_tensor_val),
-        )
-
-        torch_tensor_val = torch.rand(1)
-        self.assertEqual(
-            f"[{float(torch_tensor_val[0])}]",
-            convert_metric_value_to_str_if_possible(torch_tensor_val),
-        )
 
     def test_convert_metric_value_to_float(self):
         float_metric_val = float(random_int(10, 50))
