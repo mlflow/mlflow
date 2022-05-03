ebfd6af2092b0d8f35045e564919c4243143fb1d
Ying Xiong <ying@xiongs.org>
2022-05-03 10:13:45 -0700

    Pass in the `pos_label` parameter to `eval_and_log_metrics` (#5807)
    * Pass in the pos_label parameter to eval_and_log_metrics
    
    Signed-off-by: Ying Xiong <ying@xiongs.org>
    Signed-off-by: Ying Xiong <ying.xiong@databricks.com>
    
    * Add new unit test
    
    Signed-off-by: Ying Xiong <ying@xiongs.org>
    Signed-off-by: Ying Xiong <ying.xiong@databricks.com>
    
    * Use RandomForestClassifier instead of LogisticRegression in unit test
    
    Signed-off-by: Ying Xiong <ying.xiong@databricks.com>
    
    * Fix lint
    
    Signed-off-by: Ying Xiong <ying.xiong@databricks.com>
    
    * Add approx in assert
    
    Signed-off-by: Ying Xiong <ying.xiong@databricks.com>
    
    * Update documentation
    
    Signed-off-by: Ying Xiong <ying.xiong@databricks.com>
    
    * autoformat
    
    Signed-off-by: Ying Xiong <ying.xiong@databricks.com>

--- tests/sklearn/test_sklearn_autolog.py
+++ tests/sklearn/test_sklearn_autolog.py
@@ -1194,6 +1194,63 @@ def test_eval_and_log_metrics_for_binary_classifier():
     assert sorted(artifacts) == sorted(eval_artifacts)
 
 
+def test_eval_and_log_metrics_for_binary_classifier_with_pos_label():
+    # disable autologging so that we can check for the sole existence of eval-time metrics
+    mlflow.sklearn.autolog(disable=True)
+
+    import sklearn.ensemble
+
+    model = sklearn.ensemble.RandomForestClassifier(max_depth=2, random_state=0, n_estimators=10)
+    X, y = sklearn.datasets.load_breast_cancer(return_X_y=True)
+    X_eval = X[:-1, :]
+    y_eval = y[:-1]
+
+    with mlflow.start_run() as run:
+        model = fit_model(model, X, y, "fit")
+        eval_metrics = mlflow.sklearn.eval_and_log_metrics(
+            model=model, X=X_eval, y_true=y_eval, prefix="val_", pos_label=1
+        )
+
+    y_pred = model.predict(X_eval)
+    y_pred_prob = model.predict_proba(X_eval)
+    # For binary classification, y_score only accepts the probability of greater label
+    y_pred_prob_roc = y_pred_prob[:, 1]
+
+    expected_metrics = {
+        "val_score": model.score(X_eval, y_eval),
+        "val_accuracy_score": sklearn.metrics.accuracy_score(y_eval, y_pred),
+        "val_precision_score": sklearn.metrics.precision_score(y_eval, y_pred, pos_label=1),
+        "val_recall_score": sklearn.metrics.recall_score(y_eval, y_pred, pos_label=1),
+        "val_f1_score": sklearn.metrics.f1_score(y_eval, y_pred, pos_label=1),
+        "val_log_loss": sklearn.metrics.log_loss(y_eval, y_pred_prob),
+    }
+    if _is_metric_supported("roc_auc_score"):
+        expected_metrics["val_roc_auc_score"] = sklearn.metrics.roc_auc_score(
+            y_eval,
+            y_score=y_pred_prob_roc,
+            average="weighted",
+            multi_class="ovo",
+        )
+    assert eval_metrics == pytest.approx(expected_metrics)
+
+    eval_artifacts = []
+    if _is_plotting_supported():
+        eval_artifacts.extend(
+            [
+                "{}.png".format("val_confusion_matrix"),
+                "{}.png".format("val_roc_curve"),
+                "{}.png".format("val_precision_recall_curve"),
+            ]
+        )
+
+    # Check that logged artifacts/metrics are the same as the ones returned by the method
+    run_id = run.info.run_id
+    _, metrics, _, artifacts = get_run_data(run_id)
+
+    assert metrics == eval_metrics
+    assert sorted(artifacts) == sorted(eval_artifacts)
+
+
 def test_eval_and_log_metrics_matches_training_metrics():
     mlflow.sklearn.autolog()
 
