fcc30a9637f32ce6cac9df264aa91ac518bcf30a
harupy <hkawamura0130@gmail.com>
2022-05-11 01:58:55 +0900

    skip on windows
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/store/artifact/test_sftp_artifact_repo.py
+++ tests/store/artifact/test_sftp_artifact_repo.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import subprocess
 import tempfile
 import time
@@ -14,14 +13,13 @@ from mlflow.store.artifact.sftp_artifact_repo import SFTPArtifactRepository
 
 
 pytestmark = pytest.mark.skipif(
-    shutil.which("docker") is None, reason="Docker is required to run tests in this module"
+    os.name == "nt", reason="`atmoz/sftp` image cannot be used on Windows"
 )
-
-ROOT_SFTP_URI = "sftp://user:pass@localhost:2222/upload"
 
 
 class SFTP(NamedTuple):
     path: Path
+    uri: str
 
 
 @pytest.fixture(autouse=True, scope="module")
@@ -62,7 +60,7 @@ def sftp():
         else:
             raise Exception(f"Failed to launch SFTP server: {prc.stdout}")
 
-        yield SFTP(tmpdir)
+        yield SFTP(tmpdir, "sftp://user:pass@localhost:2222/upload")
         # Stop and remove the container
         subprocess.run(["docker", "stop", container], check=True)
         subprocess.run(["docker", "rm", container], check=True)
@@ -73,18 +71,18 @@ def rand_str():
     return uuid.uuid4().hex
 
 
-def test_artifact_uri_factory():
-    assert isinstance(get_artifact_repository(ROOT_SFTP_URI), SFTPArtifactRepository)
+def test_artifact_uri_factory(sftp):
+    assert isinstance(get_artifact_repository(sftp.uri), SFTPArtifactRepository)
 
 
-def test_list_artifacts_empty():
+def test_list_artifacts_empty(sftp):
     artifact_subdir = rand_str()
-    store = SFTPArtifactRepository(ROOT_SFTP_URI + "/" + artifact_subdir)
+    store = SFTPArtifactRepository(sftp.uri + "/" + artifact_subdir)
     assert store.list_artifacts() == []
 
 
 @pytest.mark.parametrize("artifact_path", [None, "sub_dir", "very/nested/sub/dir"])
-def test_list_artifacts(tmp_path, artifact_path):
+def test_list_artifacts(tmp_path, sftp, artifact_path):
     file_content1 = rand_str()
     file_content2 = rand_str()
     local_file1 = tmp_path.joinpath("file1")
@@ -95,7 +93,7 @@ def test_list_artifacts(tmp_path, artifact_path):
     local_file2.write_text(file_content2)
 
     artifact_subdir = rand_str()
-    store = SFTPArtifactRepository(ROOT_SFTP_URI + "/" + artifact_subdir)
+    store = SFTPArtifactRepository(sftp.uri + "/" + artifact_subdir)
     store.log_artifacts(tmp_path, artifact_path)
 
     artifacts = store.list_artifacts(artifact_path)
@@ -123,10 +121,11 @@ def test_log_artifact(tmp_path, sftp, artifact_path):
     local_file = tmp_path.joinpath("file")
     local_file.write_text(file_content)
 
-    store = SFTPArtifactRepository(ROOT_SFTP_URI)
+    artifact_subdir = rand_str()
+    store = SFTPArtifactRepository(sftp.uri + "/" + artifact_subdir)
     store.log_artifact(local_file, artifact_path)
 
-    remote_file = sftp.path.joinpath(artifact_path or ".", local_file.name)
+    remote_file = sftp.path.joinpath(artifact_subdir, artifact_path or ".", local_file.name)
     assert remote_file.read_text() == file_content
 
 
@@ -142,7 +141,7 @@ def test_log_artifacts(tmp_path, sftp, artifact_path):
     local_file2.write_text(file_content2)
 
     artifact_subdir = rand_str()
-    store = SFTPArtifactRepository(ROOT_SFTP_URI + "/" + artifact_subdir)
+    store = SFTPArtifactRepository(sftp.uri + "/" + artifact_subdir)
     store.log_artifacts(tmp_path, artifact_path)
 
     remote_dir = sftp.path.joinpath(artifact_subdir, artifact_path or ".")
@@ -157,9 +156,9 @@ def test_delete_artifact(tmp_path, sftp, artifact_path):
     file_content = rand_str()
     local_file = tmp_path.joinpath("file")
     local_file.write_text(file_content)
-    artifact_subdir = rand_str()
 
-    store = SFTPArtifactRepository(ROOT_SFTP_URI + "/" + artifact_subdir)
+    artifact_subdir = rand_str()
+    store = SFTPArtifactRepository(sftp.uri + "/" + artifact_subdir)
     store.log_artifact(local_file, artifact_path)
 
     remote_dir = sftp.path.joinpath(artifact_subdir, artifact_path or ".")
@@ -184,7 +183,7 @@ def test_delete_artifacts(tmp_path, sftp, artifact_path):
     local_file2.write_text(file_content2)
 
     artifact_subdir = rand_str()
-    store = SFTPArtifactRepository(ROOT_SFTP_URI + "/" + artifact_subdir)
+    store = SFTPArtifactRepository(sftp.uri + "/" + artifact_subdir)
     store.log_artifacts(tmp_path, artifact_path)
 
     remote_dir = sftp.path.joinpath(artifact_subdir, artifact_path or ".")
