8476857b9e7257440c1ed968e2a5abb05b5679ce
harupy <hkawamura0130@gmail.com>
2022-05-18 19:38:31 +0900

    add logging
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/projects/utils.py
+++ mlflow/projects/utils.py
@@ -201,6 +201,7 @@ def _fetch_git_repo(uri, version, dst_dir):
     else:
         origin.fetch(depth=GIT_FETCH_DEPTH)
         branch = str(origin.refs[0].name).split("/")[-1]
+        _logger.info("Fetched '%s' branch", branch)
         repo.create_head(branch, origin.refs[0])
         repo.heads[branch].checkout()
     repo.submodule_update(init=True, recursive=True)
