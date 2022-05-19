c0daec250aaa1915b0938fd0f8a0557f42cb9544
Ankit Singhal <anksing@microsoft.com>
2022-05-19 21:27:08 +0000

    Updating training script

--- examples/sklearn_elasticnet_wine/train.py
+++ examples/sklearn_elasticnet_wine/train.py
@@ -5,6 +5,7 @@
 import os
 import warnings
 import sys
+import argparse
 
 import pandas as pd
 import numpy as np
@@ -21,26 +22,20 @@ logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
 
-def eval_metrics(actual, pred):
-    rmse = np.sqrt(mean_squared_error(actual, pred))
-    mae = mean_absolute_error(actual, pred)
-    r2 = r2_score(actual, pred)
-    return rmse, mae, r2
-
-
-if __name__ == "__main__":
+def main(args):
+    mlflow.autolog(log_input_examples=True)
     warnings.filterwarnings("ignore")
     np.random.seed(40)
 
     # Read the wine-quality csv file from the URL
-    csv_url = (
-        "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
-    )
+    # csv_url = (
+    #     "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+    # )
     try:
-        data = pd.read_csv(csv_url, sep=";")
+        data = pd.read_csv(args.wine_csv, sep=";")
     except Exception as e:
         logger.exception(
-            "Unable to download training & test CSV, check your internet connection. Error: %s", e
+            "Unable to download training & test CSV. Error: %s", e
         )
 
     # Split the data into training and test sets. (0.75, 0.25) split.
@@ -52,37 +47,37 @@ if __name__ == "__main__":
     train_y = train[["quality"]]
     test_y = test[["quality"]]
 
-    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    alpha = args.alpha
+    l1_ratio = args.l1_ratio
 
-    with mlflow.start_run():
+    with mlflow.start_run() as run:
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
 
         predicted_qualities = lr.predict(test_x)
 
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+        mlflow.register_model(
+            "runs:/{}/model".format(run.info.run_id),
+            "ElasticnetWineModel"
+        )
 
-        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-        print("  RMSE: %s" % rmse)
-        print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
+def parse_args():
+    # setup arg parser
+    parser = argparse.ArgumentParser()
 
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-        mlflow.log_metric("mae", mae)
+    # add arguments
+    parser.add_argument("--wine-csv", type=str, default="http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv")
+    parser.add_argument("--alpha", type=int, default=0.5)
+    parser.add_argument("--l1-ratio", type=str, default=0.5)
 
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+    # parse args
+    args = parser.parse_args()
 
-        # Model registry does not work with file store
-        if tracking_url_type_store != "file":
+    # return args
+    return args
 
-            # Register the model
-            # There are other ways to use the Model Registry, which depends on the use case,
-            # please refer to the doc for more information:
-            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetWineModel")
-        else:
-            mlflow.sklearn.log_model(lr, "model")
+if __name__ == "__main__":
+    args = parse_args()
+
+    # run main function
+    main(args)
