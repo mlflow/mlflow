d7faa0adf52c93450dc2678e879f358b9ae820ff
harupy <hkawamura0130@gmail.com>
2022-06-01 21:57:18 +0900

    improve test
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/sklearn/test_sklearn_autolog_without_matplotlib.py
+++ tests/sklearn/test_sklearn_autolog_without_matplotlib.py
@@ -1,9 +1,11 @@
 import pytest
 from sklearn.datasets import load_breast_cancer
 from sklearn.ensemble import RandomForestClassifier
+from unittest import mock
 
 import mlflow
 from mlflow.tracking import MlflowClient
+from tests.helper_functions import AnyStringWith
 
 
 def is_matplotlib_installed():
@@ -22,8 +24,11 @@ def test_sklearn_autolog_works_without_matplotlib():
     mlflow.sklearn.autolog()
     model = RandomForestClassifier(max_depth=2, random_state=0, n_estimators=10)
     X, y = load_breast_cancer(return_X_y=True)
-    with mlflow.start_run() as run:
+    with mlflow.start_run() as run, mock.patch(
+        "mlflow.sklearn.utils._logger.warning"
+    ) as mock_warning:
         model.fit(X, y)
+        mock_warning.assert_called_once_with(AnyStringWith("Failed to import matplotlib"))
 
     run = MlflowClient().get_run(run.info.run_id)
     expected_metric_keys = {
