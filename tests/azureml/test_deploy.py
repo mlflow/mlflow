8123a7d355198e9ed93aebf4074de0212c62b2e6
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-06-06 13:39:57 +0900

    Define mock_s3_bucket in conftest.py (#6029)
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/azureml/test_deploy.py
+++ tests/azureml/test_deploy.py
@@ -22,7 +22,6 @@ from mlflow.protos.databricks_pb2 import INVALID_PARAMETER_VALUE
 from mlflow.store.artifact.s3_artifact_repo import S3ArtifactRepository
 from mlflow.utils.file_utils import TempDir
 
-from tests.helper_functions import mock_s3_bucket  # pylint: disable=unused-import
 
 pytestmark = pytest.mark.skipif(
     (sys.version_info < (3, 0)), reason="Tests require Python 3 to run!"
