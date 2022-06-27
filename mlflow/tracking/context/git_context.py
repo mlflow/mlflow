14ba4d1b9dc993f002c76a3817a75b23380be29f
dbczumar <corey.zumar@databricks.com>
2022-06-27 02:13:44 -0700

    Impl
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/tracking/context/git_context.py
+++ mlflow/tracking/context/git_context.py
@@ -3,7 +3,12 @@ import logging
 
 from mlflow.tracking.context.abstract_context import RunContextProvider
 from mlflow.tracking.context.default_context import _get_main_file
-from mlflow.utils.mlflow_tags import MLFLOW_GIT_COMMIT
+from mlflow.utils.git_utils import get_git_repo_url
+from mlflow.utils.mlflow_tags import (
+    MLFLOW_GIT_COMMIT,
+    MLFLOW_GIT_REPO_URL,
+    LEGACY_MLFLOW_GIT_REPO_URL,
+)
 
 _logger = logging.getLogger(__name__)
 
@@ -35,6 +40,13 @@ def _get_source_version():
     return None
 
 
+def _get_git_repo_url():
+    main_file = _get_main_file()
+    if main_file is not None:
+        return get_git_repo_url(main_file)
+    return None
+
+
 class GitRunContext(RunContextProvider):
     def __init__(self):
         self._cache = {}
@@ -45,8 +57,18 @@ class GitRunContext(RunContextProvider):
             self._cache["source_version"] = _get_source_version()
         return self._cache["source_version"]
 
+    @property
+    def _git_repo_url(self):
+        if "git_repo_url" not in self._cache:
+            self._cache["git_repo_url"] = _get_git_repo_url()
+        return self._cache["git_repo_url"]
+
     def in_context(self):
         return self._source_version is not None
 
     def tags(self):
-        return {MLFLOW_GIT_COMMIT: self._source_version}
+        return {
+            MLFLOW_GIT_COMMIT: self._source_version,
+            MLFLOW_GIT_REPO_URL: self._git_repo_url,
+            LEGACY_MLFLOW_GIT_REPO_URL: self._git_repo_url,
+        }
