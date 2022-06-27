f3e43ced365d8dac29443c76ee07b4bfaf4a7ebb
dbczumar <corey.zumar@databricks.com>
2022-06-27 13:09:43 -0700

    Fixes
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/pipelines/utils/tracking.py
+++ mlflow/pipelines/utils/tracking.py
@@ -16,9 +16,10 @@ from mlflow.tracking.default_experiment import DEFAULT_EXPERIMENT_ID
 from mlflow.tracking.fluent import set_experiment as fluent_set_experiment, _get_experiment_id
 from mlflow.utils.databricks_utils import is_in_databricks_runtime
 from mlflow.utils.file_utils import path_to_local_sqlite_uri, path_to_local_file_uri
-from mlflow.utils.git_utils import get_git_repo_url, get_git_commit
+from mlflow.utils.git_utils import get_git_repo_url, get_git_commit, get_git_branch
 from mlflow.utils.mlflow_tags import (
     MLFLOW_SOURCE_NAME,
+    MLFLOW_GIT_BRANCH,
     MLFLOW_GIT_COMMIT,
     MLFLOW_GIT_REPO_URL,
     LEGACY_MLFLOW_GIT_REPO_URL,
@@ -215,15 +216,19 @@ def get_run_tags_env_vars(pipeline_root_path: str) -> Dict[str, str]:
     run_context_tags = resolve_tags()
 
     git_tags = {}
-    git_commit = get_git_commit(path=pipeline_root_path)
-    if git_commit:
-        git_tags[MLFLOW_GIT_COMMIT] = git_commit
     git_repo_url = get_git_repo_url(path=pipeline_root_path)
     if git_repo_url:
         git_tags[MLFLOW_SOURCE_NAME] = git_repo_url
         git_tags[MLFLOW_GIT_REPO_URL] = git_repo_url
         git_tags[LEGACY_MLFLOW_GIT_REPO_URL] = git_repo_url
+    git_commit = get_git_commit(path=pipeline_root_path)
+    if git_commit:
+        git_tags[MLFLOW_GIT_COMMIT] = git_commit
+    git_branch = get_git_branch(path=pipeline_root_path)
+    if git_branch:
+        git_tags[MLFLOW_GIT_BRANCH] = git_branch
 
+    print("GIT TAGS", git_tags)
     return {MLFLOW_RUN_CONTEXT_ENV_VAR: json.dumps({**run_context_tags, **git_tags})}
 
 
--- mlflow/utils/git_utils.py
+++ mlflow/utils/git_utils.py
@@ -35,7 +35,7 @@ def get_git_commit(path: str) -> Optional[str]:
     repository.
     """
     try:
-        import git
+        from git import Repo
     except ImportError as e:
         _logger.warning(
             "Failed to import Git (the Git executable is probably not on your PATH),"
@@ -46,8 +46,32 @@ def get_git_commit(path: str) -> Optional[str]:
     try:
         if os.path.isfile(path):
             path = os.path.dirname(path)
-        repo = git.Repo(path, search_parent_directories=True)
+        repo = Repo(path, search_parent_directories=True)
         commit = repo.head.commit.hexsha
         return commit
+    except Exception:
+        return None
+
+
+def get_git_branch(path: str) -> Optional[str]:
+    """
+    Obtains the name of the current branch of the git repository associated with the specified
+    path, returning ``None`` if the path does not correspond to a git repository.
+    """
+    try:
+        from git import Repo
+    except ImportError as e:
+        _logger.warning(
+            "Failed to import Git (the Git executable is probably not on your PATH),"
+            " so Git SHA is not available. Error: %s",
+            e,
+        )
+        return None
+    
+    try:
+        if os.path.isfile(path):
+            path = os.path.dirname(path)
+        repo = Repo(path, search_parent_directories=True)
+        return repo.active_branch.name
     except Exception:
         return None
