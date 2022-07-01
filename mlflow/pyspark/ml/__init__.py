9860e0edd70dff203e2db4577e57969e4bac27ef
Serena Ruan <82044803+serena-ruan@users.noreply.github.com>
2022-07-01 08:38:08 +0800

    feat: support read log_model_allowlist file from url (#6036)
    * feat: support read log_model_allowlist file from url
    
    Signed-off-by: Xinyue Ruan <serena.rxy@gmail.com>
    
    * address comments
    
    Signed-off-by: Xinyue Ruan <serena.rxy@gmail.com>
    
    * address comments
    
    Signed-off-by: Xinyue Ruan <serena.rxy@gmail.com>
    
    * fix failing tests
    
    Signed-off-by: Xinyue Ruan <serena.rxy@gmail.com>
    
    * fix failing tests
    
    Signed-off-by: Xinyue Ruan <serena.rxy@gmail.com>
    Signed-off-by: Michal Karzynski <michal.karzynski@intel.com>

--- mlflow/pyspark/ml/__init__.py
+++ mlflow/pyspark/ml/__init__.py
@@ -2,12 +2,15 @@ from collections import defaultdict, namedtuple, OrderedDict
 import logging
 import numpy as np
 import time
+import os
 from pkg_resources import resource_filename
+from urllib.parse import urlparse
 import weakref
 
 import mlflow
 from mlflow.tracking.client import MlflowClient
 from mlflow.entities import Metric, Param
+from mlflow.exceptions import MlflowException
 from mlflow.utils import (
     _chunk_dict,
     _truncate_dict,
@@ -23,6 +26,11 @@ from mlflow.utils.autologging_utils import (
 from mlflow.utils.autologging_utils import get_method_call_arg_value
 from mlflow.utils.file_utils import TempDir
 from mlflow.utils.mlflow_tags import MLFLOW_AUTOLOGGING, MLFLOW_PARENT_RUN_ID
+from mlflow.utils.rest_utils import (
+    augmented_raise_for_status,
+    http_request,
+    MlflowHostCreds,
+)
 from mlflow.utils.validation import (
     MAX_PARAMS_TAGS_PER_BATCH,
     MAX_PARAM_VAL_LENGTH,
@@ -39,14 +47,36 @@ AUTOLOGGING_INTEGRATION_NAME = "pyspark.ml"
 
 
 def _read_log_model_allowlist_from_file(allowlist_file):
-    allowlist = set()
-    with open(allowlist_file) as f:
-        for line in f:
+    def _parse_allowlist_file(line_iter):
+        allowlist = set()
+        for line in line_iter:
             stripped = line.strip()
             is_blankline_or_comment = stripped == "" or stripped.startswith("#")
             if not is_blankline_or_comment:
                 allowlist.add(stripped)
-    return allowlist
+        return allowlist
+
+    url_parsed = urlparse(allowlist_file)
+    scheme = url_parsed.scheme
+    path = url_parsed.path
+    if os.name == "nt" and not url_parsed.hostname:
+        path = scheme + "://" + path
+        scheme = ""
+    if scheme in ("file", ""):
+        if not os.path.exists(path):
+            raise MlflowException.invalid_parameter_value(f"{allowlist_file} does not exist")
+
+        with open(allowlist_file) as f:
+            return _parse_allowlist_file(f)
+    else:
+        host_creds = MlflowHostCreds(
+            host=scheme + "://" + (url_parsed.hostname or ""),
+            username=url_parsed.username,
+            password=url_parsed.password,
+        )
+        response = http_request(host_creds=host_creds, endpoint=path, method="GET")
+        augmented_raise_for_status(response)
+        return _parse_allowlist_file(response.iter_lines(decode_unicode=True))
 
 
 def _read_log_model_allowlist():
