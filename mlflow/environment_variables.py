51ab62a6b4ce3e3ec19ca5dd882ff8ef70d128ec
Weichen Xu <weichen.xu@databricks.com>
2022-07-06 14:01:01 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/environment_variables.py
+++ mlflow/environment_variables.py
@@ -3,13 +3,13 @@ This module defines environment variable names used in MLflow.
 """
 from mlflow.utils.env import EnvironmentVariable
 
-#: The environment variable to specify maximum retries for mlflow http request.
+#: Specify maximum retries for mlflow http request, default value is 5.
 MLFLOW_HTTP_REQUEST_MAX_RETRIES = EnvironmentVariable("MLFLOW_HTTP_REQUEST_MAX_RETRIES", int, 5)
 
-#: The environment variable to specify backoff factor for mlflow http request.
+#: Specify backoff factor for mlflow http request, default value is 2.
 MLFLOW_HTTP_REQUEST_BACKOFF_FACTOR = EnvironmentVariable(
     "MLFLOW_HTTP_REQUEST_BACKOFF_FACTOR", int, 2
 )
 
-#: The environment variable to specify timeout in seconds for mlflow http request.
+#: Specify timeout in seconds for mlflow http request, default value is 120.
 MLFLOW_HTTP_REQUEST_TIMEOUT = EnvironmentVariable("MLFLOW_HTTP_REQUEST_TIMEOUT", int, 120)
