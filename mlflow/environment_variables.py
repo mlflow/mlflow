ec8c3ec821d0736b8b761d52d50cb4838d2eab67
Weichen Xu <weichen.xu@databricks.com>
2022-07-06 15:39:03 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/environment_variables.py
+++ mlflow/environment_variables.py
@@ -1,15 +1,44 @@
 """
 This module defines environment variable names used in MLflow.
 """
-from mlflow.utils.env import EnvironmentVariable
+import os
 
-#: Specify maximum retries for mlflow http request, default value is 5.
-MLFLOW_HTTP_REQUEST_MAX_RETRIES = EnvironmentVariable("MLFLOW_HTTP_REQUEST_MAX_RETRIES", int, 5)
 
-#: Specify backoff factor for mlflow http request, default value is 2.
-MLFLOW_HTTP_REQUEST_BACKOFF_FACTOR = EnvironmentVariable(
+class _EnvironmentVariable:
+    """
+    Define a environment variable
+    """
+    def __init__(self, name, type, default):
+        self.name = name
+        self.type = type
+        self.default = default
+
+    def get(self):
+        """
+        Get environment variable value.
+        """
+        val = os.getenv(self.name)
+        if val:
+            try:
+                return self.type(val)
+            except Exception as e:
+                raise ValueError(f"Failed to convert {val} to {self.type} for {self.name}: {e}")
+        return self.default
+
+    def __str__(self):
+        return f"Environment variable: name={self.name}, type={self.type}, default={self.default}"
+
+    def __repr__(self):
+        return repr(self.name)
+
+
+#: Specify maximum retries for MLflow http request ``(default: 5)``.
+MLFLOW_HTTP_REQUEST_MAX_RETRIES = _EnvironmentVariable("MLFLOW_HTTP_REQUEST_MAX_RETRIES", int, 5)
+
+#: Specify backoff factor for MLflow http request ``(default: 2)``.
+MLFLOW_HTTP_REQUEST_BACKOFF_FACTOR = _EnvironmentVariable(
     "MLFLOW_HTTP_REQUEST_BACKOFF_FACTOR", int, 2
 )
 
-#: Specify timeout in seconds for mlflow http request, default value is 120.
-MLFLOW_HTTP_REQUEST_TIMEOUT = EnvironmentVariable("MLFLOW_HTTP_REQUEST_TIMEOUT", int, 120)
+#: Specify timeout in seconds for MLflow http request ``(default: 120)``.
+MLFLOW_HTTP_REQUEST_TIMEOUT = _EnvironmentVariable("MLFLOW_HTTP_REQUEST_TIMEOUT", int, 120)
--- mlflow/utils/env.py
+++ mlflow/utils/env.py
@@ -8,24 +8,3 @@ def get_env(variable_name):
 def unset_variable(variable_name):
     if variable_name in os.environ:
         del os.environ[variable_name]
-
-
-class EnvironmentVariable:
-    def __init__(self, name, type, default):
-        self.name = name
-        self.type = type
-        self.default = default
-
-    def get(self):
-        val = os.getenv(self.name)
-        if val:
-            try:
-                return self.type(val)
-            except Exception as e:
-                raise ValueError(
-                    f"Parse environment config {self.name}'s value '{val}' failed. (error: {repr(e)})"
-                )
-        return self.default
-
-    def __str__(self):
-        return f"Environment variable: name={self.name}, type={self.type}, default={self.default}"
