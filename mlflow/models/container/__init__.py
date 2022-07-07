5917ac4af74640420ea0480a1d9b5d30b891c3ef
dbczumar <corey.zumar@databricks.com>
2022-07-07 12:12:25 -0700

    Docstring
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/models/container/__init__.py
+++ mlflow/models/container/__init__.py
@@ -123,10 +123,10 @@ def _install_pyfunc_deps(model_path=None, install_mlflow=False, enable_mlserver=
         raise Exception("Failed to install serving dependencies into the model environment.")
 
     if has_env and install_mlflow:
-        # Explicitly install protobuf==3.20.1 to replace any potential protobuf >= 4.0  versions
-        # preinstalled in the model's conda environment. This is necessary for compatibility with
-        # MLflow versions <= 1.26.0, which are incompatible with protobuf >= 4.0 but do not
-        # explicitly require protobuf < 4.0 in the package requirements
+        # Explicitly install protobuf==3.20.1 before installing MLflow in the model's conda
+        # environment. Otherwise, installing MLflow <= 1.26.0 would install protobuf >= 4.0, which
+        # is not compatible with MLflow <= 1.26.0, because MLflow <= 1.26.0 does not place an
+        # upper bound on the protobuf library version in its requirements
         install_protobuf_cmd = ["pip install protobuf==3.20.1"]
         install_mlflow_cmd = [
             "pip install /opt/mlflow/."
