7f0c48659df01ae6a178a0f842c629e2d51626d9
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-07-07 10:20:58 +0900

    Fix regular expression to capture closing-issue sytanx (#6199)
    * Fix regular expression
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * comment
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    Signed-off-by: Michal Karzynski <michal.karzynski@intel.com>

--- .github/workflows/closing-pr.js
+++ .github/workflows/closing-pr.js
@@ -1,11 +1,18 @@
+// Regular expressions to capture a closing syntax in the PR body
 // https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue
-const CLOSING_SYNTAX_REGEX = /(?:close|fixe?|resolve)[sd]?\s+#(\d+)/i;
+const CLOSING_SYNTAX_PATTERNS = [
+  /(?:close|fixe?|resolve)[sd]?\s+(?:mlflow\/mlflow)?#(\d+)/i,
+  /(?:close|fixe?|resolve)[sd]?\s+(?:https?:\/\/github.com\/mlflow\/mlflow\/issues\/)(\d+)/i,
+];
 const HAS_CLOSING_PR_LABEL = 'has-closing-pr';
 
 module.exports = async ({ context, github }) => {
   const { body } = context.payload.pull_request;
   // Find a closing syntax after removing comments
-  const match = body.replace(/<!--(.+?)-->/gs, '').match(CLOSING_SYNTAX_REGEX);
+  const commentsExcluded = body.replace(/<!--(.+?)-->/gs, '');
+  const match = CLOSING_SYNTAX_PATTERNS.map((pattern) => commentsExcluded.match(pattern)).find(
+    (match) => match,
+  );
   if (match) {
     const issue_number = match[1];
     const { owner, repo } = context.repo;
