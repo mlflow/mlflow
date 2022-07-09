f8833025aab8d1fcb43044d5b3e9853aafff4811
Jasper Ginn <jasperginn@gmail.com>
2022-07-09 10:07:20 +0200

    add yaml file opt to CLI

--- mlflow/pipelines/cli.py
+++ mlflow/pipelines/cli.py
@@ -19,6 +19,18 @@ _CLI_ARG_PIPELINE_PROFILE = click.option(
 )
 
 
+_CLI_ARG_PIPELINE_FILE_NAME = click.option(
+    "--pipeline_file_name",
+    type=click.STRING,
+    default="pipeline.yaml",
+    required=False,
+    help=(
+        "The name of the pipeline YAML file to use. If not provided, will default to" +
+        " 'pipeline.yaml'"
+    ),    
+)
+
+
 @click.group("pipelines")
 def commands():
     """
@@ -37,13 +49,14 @@ def commands():
     help="The name of the pipeline step to run.",
 )
 @_CLI_ARG_PIPELINE_PROFILE
+@_CLI_ARG_PIPELINE_FILE_NAME
 @experimental("command")
-def run(step, profile):
+def run(step, profile, pipeline_file_name):
     """
     Run the full pipeline, or run a particular pipeline step if specified, producing
     outputs and displaying a summary of results upon completion.
     """
-    Pipeline(profile=profile).run(step)
+    Pipeline(profile=profile, pipeline_file_name=pipeline_file_name).run(step)
 
 
 @commands.command(
@@ -61,14 +74,15 @@ def run(step, profile):
     help="The name of the pipeline step for which to remove cached outputs.",
 )
 @_CLI_ARG_PIPELINE_PROFILE
+@_CLI_ARG_PIPELINE_FILE_NAME
 @experimental("command")
-def clean(step, profile):
+def clean(step, profile, pipeline_file_name):
     """
     Remove all pipeline outputs from the cache, or remove the cached outputs of a particular
     pipeline step if specified. After cached outputs are cleaned for a particular step, the step
     will be re-executed in its entirety the next time it is run.
     """
-    Pipeline(profile=profile).clean(step)
+    Pipeline(profile=profile, pipeline_file_name=pipeline_file_name).clean(step)
 
 
 @commands.command(
@@ -85,11 +99,12 @@ def clean(step, profile):
     help="The name of the pipeline step to inspect.",
 )
 @_CLI_ARG_PIPELINE_PROFILE
+@_CLI_ARG_PIPELINE_FILE_NAME
 @experimental("command")
-def inspect(step, profile):
+def inspect(step, profile, pipeline_file_name):
     """
     Display a visual overview of the pipeline graph, or display a summary of results from a
     particular pipeline step if specified. If the specified step has not been executed,
     nothing is displayed.
     """
-    Pipeline(profile=profile).inspect(step)
+    Pipeline(profile=profile, pipeline_file_name=pipeline_file_name).inspect(step)
