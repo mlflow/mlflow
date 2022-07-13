ce84465bda9e51e7208d74ce4400e696fb6ebb6a
Tomas Sousa-Pereira <49151314+tospe@users.noreply.github.com>
2022-07-13 21:00:03 +0100

    Nested runs visualization (#6065)
    * Enable multi-experiment run comparison (#5971)
    
    * Enable multi-experiment run comparison
    
    Signed-off-by: Nikolay Ulmasov <ulmasov@hotmail.com>
    
    * Tidy up
    
    Signed-off-by: Nikolay Ulmasov <ulmasov@hotmail.com>
    
    * Refactor to improve readability
    
    Signed-off-by: Nikolay Ulmasov <ulmasov@hotmail.com>
    
    * Fix typo and add test
    
    Signed-off-by: Nikolay Ulmasov <ulmasov@hotmail.com>
    
    * fix css
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix handleCheck
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix lint error
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * require activeExperimentIds
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix route handling
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    Co-authored-by: harupy <hkawamura0130@gmail.com>
    Signed-off-by: Tomas Souper <tospe.ami@gmail.com>
    
    * feat: add nested vizualization
    
    Signed-off-by: Tomas Souper <tospe.ami@gmail.com>
    
    * test: backwards compability
    
    Signed-off-by: Tomas Souper <tospe.ami@gmail.com>
    
    * refactor: simplify the logic
    
    Signed-off-by: Tomas Souper <tospe.ami@gmail.com>
    
    * refactor: remove findLevel()
    
    Signed-off-by: Tomas Souper <tospe.ami@gmail.com>
    
    * test: add multi level test
    
    Signed-off-by: Tomas Souper <tospe.ami@gmail.com>
    
    * fix: remove console log
    
    Signed-off-by: Tomas Souper <tospe.ami@gmail.com>
    
    * fix: linter changes
    
    Signed-off-by: Tomas Souper <tospe.ami@gmail.com>
    
    * fix: linter changes, fix really deep alignment
    
    Signed-off-by: Tomas Souper <tospe.ami@gmail.com>
    
    * Revert "fix: linter changes, fix really deep alignment"
    
    This reverts commit 93a38e661aacee03a51329ea9f3512a6ded2e3d6.
    
    Signed-off-by: Tomas Souper <tospe.ami@gmail.com>
    
    * fix: run linter, make deep nesting alignment right
    
    Signed-off-by: Tomas Souper <tospe.ami@gmail.com>
    
    Co-authored-by: Nikolay Ulmasov <ulmasov@hotmail.com>
    Co-authored-by: harupy <hkawamura0130@gmail.com>
    Co-authored-by: Tomas Pereira <tomas.pereira@fraunhofer.pt>
    Co-authored-by: Tomas Souper <tospe.ami@gmail.com>

--- mlflow/server/js/src/experiment-tracking/components/ExperimentRunsTableMultiColumnView2.js
+++ mlflow/server/js/src/experiment-tracking/components/ExperimentRunsTableMultiColumnView2.js
@@ -361,68 +361,71 @@ export class ExperimentRunsTableMultiColumnView2Impl extends React.Component {
     const referenceTime = new Date();
     // Round reference time down to the nearest second, to avoid unnecessary re-renders
     referenceTime.setMilliseconds(0);
-    const runs = mergedRows.map(({ idx, isParent, hasExpander, expanderOpen, childrenIds }) => {
-      const tags = tagsList[idx];
-      const params = paramsList[idx];
-      const metrics = metricsList[idx].map(({ key, value }) => ({
-        key,
-        value: Utils.formatMetric(value),
-      }));
-      const runInfo = runInfos[idx];
+    const runs = mergedRows.map(
+      ({ idx, isParent, hasExpander, expanderOpen, childrenIds, level }) => {
+        const tags = tagsList[idx];
+        const params = paramsList[idx];
+        const metrics = metricsList[idx].map(({ key, value }) => ({
+          key,
+          value: Utils.formatMetric(value),
+        }));
+        const runInfo = runInfos[idx];
 
-      const runUuid = runInfo.getRunUuid();
-      const { experiment_id: experimentId } = runInfo;
-      const experimentName = experimentNameMap[experimentId];
-      const user = Utils.getUser(runInfo, tags);
-      const duration = Utils.getDuration(runInfo.start_time, runInfo.end_time);
-      const runName = Utils.getRunName(tags) || '-';
-      const visibleTags = Utils.getVisibleTagValues(tags).map(([key, value]) => ({
-        key,
-        value,
-      }));
+        const runUuid = runInfo.getRunUuid();
+        const { experiment_id: experimentId } = runInfo;
+        const experimentName = experimentNameMap[experimentId];
+        const user = Utils.getUser(runInfo, tags);
+        const duration = Utils.getDuration(runInfo.start_time, runInfo.end_time);
+        const runName = Utils.getRunName(tags) || '-';
+        const visibleTags = Utils.getVisibleTagValues(tags).map(([key, value]) => ({
+          key,
+          value,
+        }));
 
-      const runDateInfo = {
-        startTime: runInfo.start_time,
-        referenceTime,
-        experimentId,
-        runUuid,
-        runStatus: runInfo.status,
-        isParent,
-        hasExpander,
-        expanderOpen,
-        childrenIds,
-      };
+        const runDateInfo = {
+          startTime: runInfo.start_time,
+          referenceTime,
+          experimentId,
+          runUuid,
+          runStatus: runInfo.status,
+          isParent,
+          hasExpander,
+          expanderOpen,
+          childrenIds,
+          level,
+        };
 
-      const models = {
-        registeredModels: modelVersionsByRunUuid[runInfo.run_uuid] || [],
-        loggedModels: Utils.getLoggedModelsFromTags(tags),
-        experimentId: runInfo.experiment_id,
-        runUuid: runInfo.run_uuid,
-      };
+        const models = {
+          registeredModels: modelVersionsByRunUuid[runInfo.run_uuid] || [],
+          loggedModels: Utils.getLoggedModelsFromTags(tags),
+          experimentId: runInfo.experiment_id,
+          runUuid: runInfo.run_uuid,
+        };
 
-      const version = {
-        version: Utils.getSourceVersion(tags),
-        name: Utils.getSourceName(tags),
-        type: Utils.getSourceType(tags),
-      };
+        const version = {
+          version: Utils.getSourceVersion(tags),
+          name: Utils.getSourceName(tags),
+          type: Utils.getSourceType(tags),
+        };
 
-      return {
-        runUuid,
-        runDateInfo,
-        runInfo,
-        experimentName,
-        experimentId,
-        duration,
-        user,
-        runName,
-        tags,
-        models,
-        version,
-        ...getNameValueMapFromList(params, paramKeyList, PARAM_PREFIX),
-        ...getNameValueMapFromList(metrics, metricKeyList, METRIC_PREFIX),
-        ...getNameValueMapFromList(visibleTags, visibleTagKeyList, TAG_PREFIX),
-      };
-    });
+        return {
+          runUuid,
+          runDateInfo,
+          runInfo,
+          experimentName,
+          experimentId,
+          duration,
+          user,
+          runName,
+          tags,
+          models,
+          version,
+          ...getNameValueMapFromList(params, paramKeyList, PARAM_PREFIX),
+          ...getNameValueMapFromList(metrics, metricKeyList, METRIC_PREFIX),
+          ...getNameValueMapFromList(visibleTags, visibleTagKeyList, TAG_PREFIX),
+        };
+      },
+    );
 
     return runs;
   }
@@ -639,6 +642,7 @@ function DateCellRenderer(props) {
     hasExpander,
     expanderOpen,
     childrenIds,
+    level,
   } = props.value;
   const { onExpand } = props;
   return (
@@ -651,16 +655,17 @@ function DateCellRenderer(props) {
           key={'Expander-' + runUuid}
           style={{ paddingRight: 8, display: 'inline' }}
         >
+          <span style={{ paddingLeft: 18 * level }} />
           <i
             className={`ExperimentView-expander far fa-${expanderOpen ? 'minus' : 'plus'}-square`}
           />
         </div>
       ) : (
-        <span style={{ paddingLeft: 18 }} />
+        <span style={{ paddingLeft: level === 0 ? 12 : 18 * level + 12 }} />
       )}
       <Link
         to={Routes.getRunPageRoute(experimentId, runUuid)}
-        style={{ paddingLeft: isParent ? 0 : 16 }}
+        style={{ paddingLeft: isParent ? 0 : 8 }}
         title={Utils.formatTimestamp(startTime)}
       >
         {ExperimentViewUtil.getRunStatusIcon(runStatus)}{' '}
