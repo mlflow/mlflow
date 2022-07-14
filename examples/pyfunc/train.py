c8137fbea15d655aba332d5156152221550f64b6
Stavros Niafas <sniafas@tuta.io>
2022-07-14 13:49:54 +0300

    update example
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>

--- examples/pyfunc/train.py
+++ examples/pyfunc/train.py
@@ -1,6 +1,8 @@
-from mlflow.tracking import MlflowClient
+import os
+
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
+from custom_code.custom_code import iris_classes
 
 import mlflow
 import mlflow.sklearn
@@ -15,14 +17,12 @@ class CustomPredict(mlflow.pyfunc.PythonModel):
 
     def predict(self, context, model_input):
 
-        return self.model.predict(model_input)
+        prediction = self.model.predict(model_input)
+        return iris_classes(prediction)
 
 
 X, y = load_iris(return_X_y=True, as_frame=True)
 params = {"C": 1.0, "random_state": 42}
-
-# create an mlflow client
-client = MlflowClient()
 
 model_name = "base_model"
 custom_model_name = "custom_model"
@@ -31,28 +31,17 @@ with mlflow.start_run(run_name="test_pyfunc") as train_run:
 
     regression_model = LogisticRegression(**params).fit(X, y)
 
-    model_info = mlflow.sklearn.log_model(
-        sk_model=regression_model, artifact_path="model", registered_model_name=model_name
-    )
+    model_info = mlflow.sklearn.log_model(sk_model=regression_model, artifact_path="model")
 
     # start a child run to create custom imagine model
     with mlflow.start_run(run_name="test_custom_model", nested=True) as run:
 
         # log a custom model
         mlflow.pyfunc.log_model(
-            artifact_path="",
+            artifact_path="artifacts",
+            code_path=[
+                os.path.join(os.getcwd(), "custom_code/"),
+            ],
             artifacts={custom_model_name: model_info.model_uri},
             python_model=CustomPredict(),
-            registered_model_name=custom_model_name,
-        )
-
-        # load the latest model version
-        mv = client.get_latest_versions(custom_model_name, ["None"])[0]
-
-        # transition model to production
-        client.transition_model_version_stage(
-            name=custom_model_name,
-            version=mv.version,
-            stage="Production",
-            archive_existing_versions=True,
         )
