d18d13108ee1bb84be3d49fac84764ece064bbfa
harupy <hkawamura0130@gmail.com>
2022-07-14 21:05:55 +0900

    Mark search_experiments as experimental
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/tracking/client.py
+++ mlflow/tracking/client.py
@@ -28,6 +28,7 @@ from mlflow.tracking._tracking_service import utils
 from mlflow.tracking._tracking_service.client import TrackingServiceClient
 from mlflow.tracking.artifact_utils import _upload_artifacts_to_databricks
 from mlflow.tracking.registry import UnsupportedModelRegistryStoreURIException
+from mlflow.utils.annotations import experimental
 from mlflow.utils.databricks_utils import (
     is_databricks_default_tracking_uri,
     is_in_databricks_job,
@@ -400,6 +401,7 @@ class MlflowClient:
             view_type=view_type, max_results=max_results, page_token=page_token
         )
 
+    @experimental
     def search_experiments(
         self,
         view_type: int = ViewType.ACTIVE_ONLY,
