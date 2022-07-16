7bab46bc346794721be6836b6138b81eb1583973
harupy <hkawamura0130@gmail.com>
2022-07-16 14:55:54 +0900

    add docs to path
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath("."))
 
 from docutils.nodes import Text
