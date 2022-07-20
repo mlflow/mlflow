14f91c9ca172df368ca7adc4c63c23bab2a53505
Steve N <sniafas@tuta.io>
2022-07-20 08:57:34 +0300

    Add custom pyfunc model example (#6198)
    * add pyfunc example
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * simplify custom model example
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * rename executable
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * add to tests
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * add project dependencies
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * pyfunc model example
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * example restructure
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * restore previous examples
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * update example
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * add readme
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * simplify structure
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * minor updates
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * remove python version specifier
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * remove unused import
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * documentation updates
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * resolve conflicts
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * doc updates
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * cleanup
    
    Signed-off-by: Stavros Niafas <sniafas@tuta.io>
    
    * clean up
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    Co-authored-by: harupy <hkawamura0130@gmail.com>
    Signed-off-by: Michal Karzynski <michal.karzynski@intel.com>

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -194,6 +194,7 @@ def test_mlflow_run_example(directory, params, tmpdir):
         ("evaluation", ["python", "evaluate_with_custom_metrics_comprehensive.py"]),
         ("diviner", ["python", "train.py"]),
         ("spark_udf", ["python", "spark_udf_datetime.py"]),
+        ("pyfunc", ["python", "train.py"]),
     ],
 )
 def test_command_example(directory, command):
