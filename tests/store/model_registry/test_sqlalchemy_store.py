5a0808f3a2ca63484fb731d39b56ff2e7a4b462b
Weichen Xu <weichen.xu@databricks.com>
2022-07-25 22:30:25 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- tests/store/model_registry/test_sqlalchemy_store.py
+++ tests/store/model_registry/test_sqlalchemy_store.py
@@ -876,6 +876,7 @@ class TestSqlAlchemyStoreSqlite(unittest.TestCase):
             search_versions("run_id IN (")
         assert exception_context.exception.error_code == ErrorCode.Name(INVALID_PARAMETER_VALUE)
 
+        # TODO: fix
         # with self.assertRaisesRegex(MlflowException, r"Invalid filter '.+'") as exception_context:
         #     search_versions("run_id IN")
         assert exception_context.exception.error_code == ErrorCode.Name(INVALID_PARAMETER_VALUE)
