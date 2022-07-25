29d35c82aa49abebf32d3d01ec280bc58ebf411f
Kris Concepcion <84737625+kriscon-db@users.noreply.github.com>
2022-07-25 17:43:16 -0400

    Batch scoring framework (#6283)
    * Batch scoring pipeline added
    
    * Added gitmodule
    
    * updating the example?
    
    * Submodule fix and test fix
    
    * Submodule fix
    
    * black formatting
    
    * Added batch_scoring submodule correctly?
    
    * Lint errors
    
    * Fixed the integration test (removed predict for now)
    
    * Black formatting
    
    * Updated api doc
    
    * Documentation bloopers
    
    * Removed smart quotes
    
    * more documentation fixes
    
    * More fixes
    
    * Try to change the replacement name
    
    * Bad substitution

--- tests/pipelines/helper_functions.py
+++ tests/pipelines/helper_functions.py
@@ -18,6 +18,7 @@ import pytest
 
 PIPELINE_EXAMPLE_PATH_ENV_VAR_FOR_TESTS = "_PIPELINE_EXAMPLE_PATH"
 PIPELINE_EXAMPLE_PATH_FROM_MLFLOW_ROOT = "examples/pipelines/sklearn_regression"
+BATCH_SCORING_PIPELINE_EXAMPLE_PATH_FROM_MLFLOW_ROOT = "examples/pipelines/batch_scoring"
 
 ## Methods
 def setup_model_and_evaluate(tmp_pipeline_exec_path: Path):
@@ -62,6 +63,19 @@ def enter_pipeline_example_directory():
     if pipeline_example_path is None:
         mlflow_repo_root_directory = pathlib.Path(mlflow.__file__).parent.parent
         pipeline_example_path = mlflow_repo_root_directory / PIPELINE_EXAMPLE_PATH_FROM_MLFLOW_ROOT
+
+    with chdir(pipeline_example_path):
+        yield pipeline_example_path
+
+
+@pytest.fixture
+def enter_batch_scoring_pipeline_example_directory():
+    pipeline_example_path = os.environ.get(PIPELINE_EXAMPLE_PATH_ENV_VAR_FOR_TESTS)
+    if pipeline_example_path is None:
+        mlflow_repo_root_directory = pathlib.Path(mlflow.__file__).parent.parent
+        pipeline_example_path = (
+            mlflow_repo_root_directory / BATCH_SCORING_PIPELINE_EXAMPLE_PATH_FROM_MLFLOW_ROOT
+        )
 
     with chdir(pipeline_example_path):
         yield pipeline_example_path
