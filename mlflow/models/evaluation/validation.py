cc9e5a20387340ec16312605a2e5693f1befb38b
Zhe Wang <zhe.wang@databricks.com>
2022-07-26 10:16:01 -0700

    add more documentation for MetricThreshold
    Signed-off-by: Zhe Wang <zhe.wang@databricks.com>

--- mlflow/models/evaluation/validation.py
+++ mlflow/models/evaluation/validation.py
@@ -1,4 +1,33 @@
 class MetricThreshold:
+    """
+    This class allows users to define metric thresholds for Model Validation. Allowed thresholds are
+    - threshold
+    - min_absolute_change
+    - min_relative_change
+    :param threshold: A floating number representing the value threshold for the metric.
+                      - If higher is better for the metric, the metric value has to be
+                        >= threshold to pass validation.
+                      - Otherwise, the metric value has to be <= threshold to pass the validation.
+    :param min_absolute_change: A floating point number representing the minimum absolute change
+                                required for candidate model to pass the comparison with
+                                the baseline model.
+                                - If higher is better for the metric, metric value has to be
+                                  >= baseline model metric value + min_absolute_change
+                                  to pass the validation.
+                                - Otherwise, metric value has to be
+                                  <= baseline model metric value + min_absolute_change
+                                  to pass the validation.
+    :param min_relative_change: A floating point number between 0 and 1 representing
+                                the minimum relative change (in percentage of
+                                baseline model metric value) for candidate model
+                                to pass the comparison with the baseline model.
+                                - If higher is better for the metric, metric value has to be
+                                  >= baseline model metric value * (1 + min_relative_change)
+                                - Otherwise, metric value has to be
+                                  <= baseline model metric value * (1 - min_relative_change)
+    :param higher_is_better: A boolean representing whether higher value is better for the metric.
+    """
+
     def __init__(
         self,
         threshold=None,
@@ -25,22 +54,40 @@ class MetricThreshold:
         Value threshold.
         :rtype: float
         """
-        return self.threshold
+        return self._threshold
 
     @property
     def min_absolute_change(self):
         """
-        Minimum absolute change required to pass model comparsion with baseline model
+        Minimum absolute change required to pass model comparison with baseline model
+        :rtype: float
         """
+        return self._min_absolute_change
+
+    @property
+    def min_relative_change(self):
+        """
+        Minimum relative change required to pass model comparison wwith baseline model
+        :rtype: float
+        """
+        return self._min_relative_change
+
+    @property
+    def higher_is_better(self):
+        """
+        Whether higher value is better for the metric.
+        :rtype: bool
+        """
+        return self._higher_is_better
 
     def is_empty(self):
         """
         Return True if there is no threshold specified, False otherwise.
         """
         return (
-            self.threshold is None
-            and self.min_absolute_change is None
-            and self.min_relative_change is None
+            self._threshold is None
+            and self._min_absolute_change is None
+            and self._min_relative_change is None
         )
 
     def __str__(self):
@@ -48,14 +95,14 @@ class MetricThreshold:
         Return a string consisting of all sepcified thresholds.
         """
         threshold_strs = []
-        if self.threshold is not None:
-            threshold_strs.append(f"Threshold: {self.threshold}.")
-        if self.min_absolute_change is not None:
-            threshold_strs.append(f"Minimum Absolute Change: {self.min_absolute_change}.")
-        if self.min_relative_change is not None:
-            threshold_strs.append(f"Minimum Relative Change: {self.min_relative_change}.")
-        if self.higher_is_better is not None:
-            if self.higher_is_better:
+        if self._threshold is not None:
+            threshold_strs.append(f"Threshold: {self._threshold}.")
+        if self._min_absolute_change is not None:
+            threshold_strs.append(f"Minimum Absolute Change: {self._min_absolute_change}.")
+        if self._min_relative_change is not None:
+            threshold_strs.append(f"Minimum Relative Change: {self._min_relative_change}.")
+        if self._higher_is_better is not None:
+            if self._higher_is_better:
                 threshold_strs.append("Higher value is better.")
             else:
                 threshold_strs.append("Lower value is better.")
