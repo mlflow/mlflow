f21b499f694c13bcfa4b2b8f44bcba165cf4ab88
harupy <hkawamura0130@gmail.com>
2022-07-26 12:57:08 +0900

    preserve quotes
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- dev/update_requirements.py
+++ dev/update_requirements.py
@@ -52,6 +52,8 @@ def parse_args():
 def main():
     args = parse_args()
     yaml = YAML()
+    yaml.preserve_quotes = True
+
     with open(args.requirements_yaml_location, "r") as f:
         requirements_src = f.read()
         requirements = yaml.load(requirements_src)
