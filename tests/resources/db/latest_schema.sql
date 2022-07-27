48f0d29cc564857288acc79c4360ee9d5f807359
Jason Cheng <jasoncky96@gmail.com>
2022-07-27 17:04:36 +0800

    Updated latest_schema.sql to pass tests/store/tracking/test_sqlalchemy_store_schema.py
    Signed-off-by: Jason Cheng <jasoncky96@gmail.com>

--- tests/resources/db/latest_schema.sql
+++ tests/resources/db/latest_schema.sql
@@ -76,10 +76,11 @@ CREATE TABLE runs (
 	lifecycle_stage VARCHAR(20),
 	artifact_uri VARCHAR(200),
 	experiment_id INTEGER,
+	delete_time BIGINT,
 	CONSTRAINT run_pk PRIMARY KEY (run_uuid),
 	FOREIGN KEY(experiment_id) REFERENCES experiments (experiment_id),
-	CONSTRAINT source_type CHECK (source_type IN ('NOTEBOOK', 'JOB', 'LOCAL', 'UNKNOWN', 'PROJECT')),
 	CONSTRAINT runs_lifecycle_stage CHECK (lifecycle_stage IN ('active', 'deleted')),
+	CONSTRAINT source_type CHECK (source_type IN ('NOTEBOOK', 'JOB', 'LOCAL', 'UNKNOWN', 'PROJECT')),
 	CHECK (status IN ('SCHEDULED', 'FAILED', 'FINISHED', 'RUNNING', 'KILLED'))
 )
 
