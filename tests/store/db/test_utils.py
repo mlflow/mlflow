4422fafb94a0003cfa6170ca3a9692bea518234e
Michał Karzyński <michal.karzynski@intel.com>
2022-07-28 01:11:38 +0200

    Add `MLFLOW_SQLALCHEMYSTORE_POOL_RECYCLE` flag (#6344)
    * Add pool_recycle option to SQLAlchemy create_engine
    
    Signed-off-by: Michal Karzynski <michal.karzynski@intel.com>
    
    * Add new option to unit test
    
    Signed-off-by: Michal Karzynski <michal.karzynski@intel.com>
    
    * Add note in documentation
    
    Signed-off-by: Michal Karzynski <michal.karzynski@intel.com>

--- tests/store/db/test_utils.py
+++ tests/store/db/test_utils.py
@@ -7,12 +7,20 @@ from mlflow.store.db import utils
 def test_create_sqlalchemy_engine_inject_pool_options():
     with mock.patch.dict(
         os.environ,
-        {"MLFLOW_SQLALCHEMYSTORE_POOL_SIZE": "2", "MLFLOW_SQLALCHEMYSTORE_MAX_OVERFLOW": "4"},
+        {
+            "MLFLOW_SQLALCHEMYSTORE_POOL_SIZE": "2",
+            "MLFLOW_SQLALCHEMYSTORE_POOL_RECYCLE": "3600",
+            "MLFLOW_SQLALCHEMYSTORE_MAX_OVERFLOW": "4",
+        },
     ):
         with mock.patch("sqlalchemy.create_engine") as mock_create_engine:
             utils.create_sqlalchemy_engine("mydb://host:port/")
             mock_create_engine.assert_called_once_with(
-                "mydb://host:port/", pool_pre_ping=True, pool_size=2, max_overflow=4
+                "mydb://host:port/",
+                pool_pre_ping=True,
+                pool_size=2,
+                pool_recycle=3600,
+                max_overflow=4,
             )
 
 
