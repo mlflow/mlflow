e883c07ffcd416ae1e7e9319373422833ea7cf48
Zhe Wang <zhe.wang@databricks.com>
2022-07-28 23:27:10 -0700

    add unit tests for value threshold
    Signed-off-by: Zhe Wang <zhe.wang@databricks.com>

---
+++ tests/models/test_validation.py
@@ -0,0 +1,204 @@
+import mlflow
+from mlflow import MlflowClient
+
+from mlflow.models.evaluation import (
+    evaluate,
+    EvaluationResult,
+    ModelEvaluator,
+    EvaluationArtifact,
+    MetricThreshold,
+)
+from mlflow.models.evaluation.validation import _MetricValidationResult
+from mlflow.models.evaluation.default_evaluator import DefaultEvaluator
+from mlflow.models.evaluation.evaluator_registry import _model_evaluation_registry
+from unittest import mock
+import pytest
+from mlflow.exceptions import MlflowException
+from tests.models.test_evaluation import (
+    multiclass_logistic_regressor_model_uri,
+    iris_dataset,
+)
+
+message_separator = "\n"
+
+
+class MockEvaluator(ModelEvaluator):
+    def can_evaluate(self, *, model_type, evaluator_config, **kwargs):
+        raise RuntimeError()
+
+    def evaluate(self, *, model, model_type, dataset, run_id, evaluator_config, **kwargs):
+        raise RuntimeError()
+
+
+@pytest.fixture
+def value_threshold_test_spec(request):
+    acc_threshold = MetricThreshold(threshold=0.9, higher_is_better=True)
+    acc_validation_result = _MetricValidationResult("accuracy", 0.8, acc_threshold, None)
+    acc_validation_result.threshold_failed = True
+
+    f1score_threshold = MetricThreshold(threshold=0.8, higher_is_better=True)
+    f1score_validation_result = _MetricValidationResult("f1_score", 0.7, f1score_threshold, None)
+    f1score_validation_result.threshold_failed = True
+
+    log_loss_threshold = MetricThreshold(threshold=0.5, higher_is_better=False)
+    log_loss_validation_result = _MetricValidationResult("log_loss", 0.3, log_loss_threshold, None)
+
+    l1_loss_threshold = MetricThreshold(threshold=0.3, higher_is_better=False)
+    l1_loss_validation_result = _MetricValidationResult(
+        "custom_l1_loss", 0.5, l1_loss_threshold, None
+    )
+    l1_loss_validation_result.threshold_failed = True
+
+    if request.param == "single_metric_not_satisfied_higher_better":
+        return ({"accuracy": 0.8}, {"accuracy": acc_threshold}, {"accuracy": acc_validation_result})
+
+    if request.param == "multiple_metrics_not_satisfied_higher_better":
+        return (
+            {"accuracy": 0.8, "f1_score": 0.7},
+            {"accuracy": acc_threshold, "f1_score": f1score_threshold},
+            {"accuracy": acc_validation_result, "f1_score": f1score_validation_result},
+        )
+
+    if request.param == "single_metric_not_satisfied_lower_better":
+        return (
+            {"custom_l1_loss": 0.5},
+            {"custom_l1_loss": l1_loss_threshold},
+            {"custom_l1_loss": l1_loss_validation_result},
+        )
+
+    if request.param == "multiple_metrics_not_satisfied_lower_better":
+        log_loss_validation_result.candidate_metric_value = 0.8
+        log_loss_validation_result.threshold_failed = True
+        return (
+            {"custom_l1_loss": 0.5, "log_loss": 0.8},
+            {"custom_l1_loss": l1_loss_threshold, "log_loss": log_loss_threshold},
+            {"custom_l1_loss": l1_loss_validation_result, "log_loss": log_loss_validation_result},
+        )
+
+    if request.param == "missing_metric":
+        acc_validation_result.missing = True
+        return ({}, {"accuracy": acc_threshold}, {"accuracy": acc_validation_result})
+
+    if request.param == "multiple_metrics_not_all_satisfied":
+        return (
+            {"accuracy": 0.8, "f1_score": 0.7, "log_loss": 0.3},
+            {
+                "accuracy": acc_threshold,
+                "f1_score": f1score_threshold,
+                "log_loss": log_loss_threshold,
+            },
+            {"accuracy": acc_validation_result, "f1_score": f1score_validation_result},
+        )
+
+    if request.param == "equality_boundary":
+        return (
+            {"accuracy": 0.9, "log_loss": 0.5},
+            {"accuracy": acc_threshold, "log_loss": log_loss_threshold},
+            {},
+        )
+
+    if request.param == "single_metric_satisfied_higher_better":
+        return ({"accuracy": 0.91}, {"accuracy": acc_threshold}, {})
+
+    if request.param == "single_metric_satisfied_lower_better":
+        return ({"log_loss": 0.3}, {"log_loss": log_loss_threshold}, {})
+
+    if request.param == "multiple_metrics_all_satisfied":
+        return (
+            {"accuracy": 0.9, "f1_score": 0.8, "log_loss": 0.3},
+            {
+                "accuracy": acc_threshold,
+                "f1_score": f1score_threshold,
+                "log_loss": log_loss_threshold,
+            },
+            {},
+        )
+
+
+@pytest.mark.parametrize(
+    "value_threshold_test_spec",
+    [
+        ("single_metric_not_satisfied_higher_better"),
+        ("multiple_metrics_not_satisfied_higher_better"),
+        ("single_metric_not_satisfied_lower_better"),
+        ("missing_metric"),
+        ("multiple_metrics_not_satisfied_lower_better"),
+        ("multiple_metrics_not_all_satisfied"),
+    ],
+    indirect=["value_threshold_test_spec"],
+)
+def test_validation_value_threshold_should_fail(
+    multiclass_logistic_regressor_model_uri,
+    iris_dataset,
+    value_threshold_test_spec,
+):
+    metrics, validation_thresholds, validation_results = value_threshold_test_spec
+    with mock.patch.object(
+        _model_evaluation_registry, "_registry", {"test_evaluator1": MockEvaluator}
+    ):
+        evaluator1_config = {}
+        evaluator1_return_value = EvaluationResult(
+            metrics=metrics, artifacts={}, baseline_model_metrics=None
+        )
+        failure_message = message_separator.join(map(str, list(validation_results.values())))
+        with mock.patch.object(
+            MockEvaluator, "can_evaluate", return_value=True
+        ) as mock_can_evaluate, mock.patch.object(
+            MockEvaluator, "evaluate", return_value=evaluator1_return_value
+        ) as mock_evaluate:
+            with pytest.raises(
+                MlflowException,
+                match=failure_message,
+            ):
+                evaluate(
+                    multiclass_logistic_regressor_model_uri,
+                    data=iris_dataset._constructor_args["data"],
+                    model_type="classifier",
+                    targets=iris_dataset._constructor_args["targets"],
+                    dataset_name=iris_dataset.name,
+                    evaluators="test_evaluator1",
+                    evaluator_config=evaluator1_config,
+                    validation_thresholds=validation_thresholds,
+                    baseline_model=None,
+                )
+
+
+@pytest.mark.parametrize(
+    "value_threshold_test_spec",
+    [
+        ("single_metric_satisfied_higher_better"),
+        ("single_metric_satisfied_lower_better"),
+        ("equality_boundary"),
+        ("multiple_metrics_all_satisfied"),
+    ],
+    indirect=["value_threshold_test_spec"],
+)
+def test_validation_value_threshold_should_pass(
+    multiclass_logistic_regressor_model_uri,
+    iris_dataset,
+    value_threshold_test_spec,
+):
+    metrics, validation_thresholds, _ = value_threshold_test_spec
+    with mock.patch.object(
+        _model_evaluation_registry, "_registry", {"test_evaluator1": MockEvaluator}
+    ):
+        evaluator1_config = {}
+        evaluator1_return_value = EvaluationResult(
+            metrics=metrics, artifacts={}, baseline_model_metrics=None
+        )
+        with mock.patch.object(
+            MockEvaluator, "can_evaluate", return_value=True
+        ) as mock_can_evaluate, mock.patch.object(
+            MockEvaluator, "evaluate", return_value=evaluator1_return_value
+        ) as mock_evaluate:
+            evaluate(
+                multiclass_logistic_regressor_model_uri,
+                data=iris_dataset._constructor_args["data"],
+                model_type="classifier",
+                targets=iris_dataset._constructor_args["targets"],
+                dataset_name=iris_dataset.name,
+                evaluators="test_evaluator1",
+                evaluator_config=evaluator1_config,
+                validation_thresholds=validation_thresholds,
+                baseline_model=None,
+            )
