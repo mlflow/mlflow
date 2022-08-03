4ddeb9b4fb55303d59e7f0117c9585d3d490f745
harupy <hkawamura0130@gmail.com>
2022-08-03 11:01:43 +0900

    fix BINARY conditions in MySQL
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/utils/search_utils.py
+++ mlflow/utils/search_utils.py
@@ -106,14 +106,22 @@ class SearchUtils:
             column = column.collate("Japanese_Bushu_Kakusu_100_CS_AS_KS_WS")
 
         # Use non-binary ahead of binary comparison for runtime performance
+        col = f"{column.class_.__tablename__}.{column.key}"
+        # Use non-binary ahead of binary comparison for runtime performance
         def case_sensitive_mysql_eq(value):
-            return sa.and_(column.__eq__(value), column.op("= BINARY", is_comparison=True)(value))
+            return sa.text(f"{col} = :value AND BINARY {col} = :value").bindparams(
+                sa.bindparam("value", value=value, unique=True)
+            )
 
         def case_sensitive_mysql_ne(value):
-            return sa.or_(column.__ne__(value), column.op("!= BINARY", is_comparison=True)(value))
+            return sa.text(f"{col} != :value OR BINARY {col} != :value").bindparams(
+                sa.bindparam("value", value=value, unique=True)
+            )
 
         def case_sensitive_mysql_like(value):
-            return sa.and_(column.like(value), column.op("LIKE BINARY", is_comparison=True)(value))
+            return sa.text(f"{col} LIKE :value AND BINARY {col} LIKE :value").bindparams(
+                sa.bindparam("value", value=value, unique=True)
+            )
 
         sql_filter_ops = {
             "=": case_sensitive_mysql_eq if dialect == MYSQL else column.__eq__,
