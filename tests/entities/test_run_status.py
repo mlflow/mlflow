7522c7da1add1a0212d7272f9e0c5da2dad90025
harupy <hkawamura0130@gmail.com>
2022-08-04 08:53:39 +0900

    tests/entities/test_run_status.py
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/entities/test_run_status.py
+++ tests/entities/test_run_status.py
@@ -15,24 +15,24 @@ class TestRunStatus(unittest.TestCase):
                 RunStatus.KILLED,
             ]
         )
-        self.assertSequenceEqual(all_statuses, set(RunStatus.all_status()))
+        assert all_statuses == set(RunStatus.all_status())
 
     def test_status_mappings(self):
         # test enum to string mappings
-        self.assertEqual("RUNNING", RunStatus.to_string(RunStatus.RUNNING))
-        self.assertEqual(RunStatus.RUNNING, RunStatus.from_string("RUNNING"))
+        assert "RUNNING" == RunStatus.to_string(RunStatus.RUNNING)
+        assert RunStatus.RUNNING == RunStatus.from_string("RUNNING")
 
-        self.assertEqual("SCHEDULED", RunStatus.to_string(RunStatus.SCHEDULED))
-        self.assertEqual(RunStatus.SCHEDULED, RunStatus.from_string("SCHEDULED"))
+        assert "SCHEDULED" == RunStatus.to_string(RunStatus.SCHEDULED)
+        assert RunStatus.SCHEDULED == RunStatus.from_string("SCHEDULED")
 
-        self.assertEqual("FINISHED", RunStatus.to_string(RunStatus.FINISHED))
-        self.assertEqual(RunStatus.FINISHED, RunStatus.from_string("FINISHED"))
+        assert "FINISHED" == RunStatus.to_string(RunStatus.FINISHED)
+        assert RunStatus.FINISHED == RunStatus.from_string("FINISHED")
 
-        self.assertEqual("FAILED", RunStatus.to_string(RunStatus.FAILED))
-        self.assertEqual(RunStatus.FAILED, RunStatus.from_string("FAILED"))
+        assert "FAILED" == RunStatus.to_string(RunStatus.FAILED)
+        assert RunStatus.FAILED == RunStatus.from_string("FAILED")
 
-        self.assertEqual("KILLED", RunStatus.to_string(RunStatus.KILLED))
-        self.assertEqual(RunStatus.KILLED, RunStatus.from_string("KILLED"))
+        assert "KILLED" == RunStatus.to_string(RunStatus.KILLED)
+        assert RunStatus.KILLED == RunStatus.from_string("KILLED")
 
         with self.assertRaisesRegex(
             Exception, r"Could not get string corresponding to run status -120"
@@ -45,8 +45,8 @@ class TestRunStatus(unittest.TestCase):
             RunStatus.from_string("the IMPOSSIBLE status string")
 
     def test_is_terminated(self):
-        self.assertTrue(RunStatus.is_terminated(RunStatus.FAILED))
-        self.assertTrue(RunStatus.is_terminated(RunStatus.FINISHED))
-        self.assertTrue(RunStatus.is_terminated(RunStatus.KILLED))
-        self.assertFalse(RunStatus.is_terminated(RunStatus.SCHEDULED))
-        self.assertFalse(RunStatus.is_terminated(RunStatus.RUNNING))
+        assert RunStatus.is_terminated(RunStatus.FAILED)
+        assert RunStatus.is_terminated(RunStatus.FINISHED)
+        assert RunStatus.is_terminated(RunStatus.KILLED)
+        assert not RunStatus.is_terminated(RunStatus.SCHEDULED)
+        assert not RunStatus.is_terminated(RunStatus.RUNNING)
