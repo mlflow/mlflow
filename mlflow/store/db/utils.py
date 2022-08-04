6e331320566a3a6a9749a4daf9d206cd5b1a9626
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-08-05 08:51:28 +0900

    Migrate environment variables for SQLAlchemy store (#6396)
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/store/db/utils.py
+++ mlflow/store/db/utils.py
@@ -12,12 +12,14 @@ from mlflow.exceptions import MlflowException
 from mlflow.store.tracking.dbmodels.initial_models import Base as InitialBase
 from mlflow.protos.databricks_pb2 import BAD_REQUEST, INTERNAL_ERROR
 from mlflow.store.db.db_types import SQLITE
+from mlflow.environment_variables import (
+    MLFLOW_SQLALCHEMYSTORE_POOL_SIZE,
+    MLFLOW_SQLALCHEMYSTORE_POOL_RECYCLE,
+    MLFLOW_SQLALCHEMYSTORE_MAX_OVERFLOW,
+)
 
 _logger = logging.getLogger(__name__)
 
-MLFLOW_SQLALCHEMYSTORE_POOL_SIZE = "MLFLOW_SQLALCHEMYSTORE_POOL_SIZE"
-MLFLOW_SQLALCHEMYSTORE_POOL_RECYCLE = "MLFLOW_SQLALCHEMYSTORE_POOL_RECYCLE"
-MLFLOW_SQLALCHEMYSTORE_MAX_OVERFLOW = "MLFLOW_SQLALCHEMYSTORE_MAX_OVERFLOW"
 MAX_RETRY_COUNT = 15
 
 
@@ -178,9 +180,9 @@ def create_sqlalchemy_engine_with_retry(db_uri):
 
 
 def create_sqlalchemy_engine(db_uri):
-    pool_size = os.environ.get(MLFLOW_SQLALCHEMYSTORE_POOL_SIZE)
-    pool_max_overflow = os.environ.get(MLFLOW_SQLALCHEMYSTORE_MAX_OVERFLOW)
-    pool_recycle = os.environ.get(MLFLOW_SQLALCHEMYSTORE_POOL_RECYCLE)
+    pool_size = MLFLOW_SQLALCHEMYSTORE_POOL_SIZE.get()
+    pool_max_overflow = MLFLOW_SQLALCHEMYSTORE_MAX_OVERFLOW.get()
+    pool_recycle = MLFLOW_SQLALCHEMYSTORE_POOL_RECYCLE.get()
     pool_kwargs = {}
     # Send argument only if they have been injected.
     # Some engine does not support them (for example sqllite)
