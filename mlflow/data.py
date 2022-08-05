bbd5c56cdbe2c86ff3e523d2c71e73ca08cd4e4a
harupy <hkawamura0130@gmail.com>
2022-08-05 11:03:14 +0900

    Remove unused functions and variables
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/data.py
+++ mlflow/data.py
@@ -1,20 +1,4 @@
-import re
 import urllib.parse
-import logging
-
-
-DBFS_PREFIX = "dbfs:/"
-S3_PREFIX = "s3://"
-GS_PREFIX = "gs://"
-DBFS_REGEX = re.compile("^%s" % re.escape(DBFS_PREFIX))
-S3_REGEX = re.compile("^%s" % re.escape(S3_PREFIX))
-GS_REGEX = re.compile("^%s" % re.escape(GS_PREFIX))
-
-_logger = logging.getLogger(__name__)
-
-
-class DownloadException(Exception):
-    pass
 
 
 def parse_s3_uri(uri):
@@ -22,17 +6,6 @@ def parse_s3_uri(uri):
     parsed = urllib.parse.urlparse(uri)
     if parsed.scheme != "s3":
         raise Exception("Not an S3 URI: %s" % uri)
-    path = parsed.path
-    if path.startswith("/"):
-        path = path[1:]
-    return parsed.netloc, path
-
-
-def parse_gs_uri(uri):
-    """Parse an GCS URI, returning (bucket, path)"""
-    parsed = urllib.parse.urlparse(uri)
-    if parsed.scheme != "gs":
-        raise Exception("Not a GCS URI: %s" % uri)
     path = parsed.path
     if path.startswith("/"):
         path = path[1:]
