952f3f9998529b40eb1cc4a17281162fdd1a0a6a
Bowen Hu <Bowen_who@163.com>
2022-08-17 15:42:00 +0800

    Implement _download_file in hdfs_artifact_repo (#6482)
    * Implement _download_file in hdfs_artifact_repo
    
    Signed-off-by: bowen_who <hubw8@chinaunicom.cn>
    
    * add ut on _download_file in test_hdfs_artifact_repo
    
    Signed-off-by: bowen_who <hubw8@chinaunicom.cn>
    
    * rewrite _download_artifact, change back _download_file in hdfs_artifact_repo
    
    Signed-off-by: bowen_who <hubw8@chinaunicom.cn>
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * rewrite test__download_file in test_hdfs_artifact_repo
    
    Signed-off-by: bowen_who <hubw8@chinaunicom.cn>
    
    Signed-off-by: bowen_who <hubw8@chinaunicom.cn>
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    Co-authored-by: bowen_who <hubw8@chinaunicom.cn>

--- mlflow/server/handlers.py
+++ mlflow/server/handlers.py
@@ -1471,12 +1471,11 @@ def _download_artifact(artifact_path):
     """
     basename = posixpath.basename(artifact_path)
     tmp_dir = tempfile.TemporaryDirectory()
-    tmp_path = os.path.join(tmp_dir.name, basename)
     artifact_repo = _get_artifact_repo_mlflow_artifacts()
-    artifact_repo._download_file(artifact_path, tmp_path)
+    dst = artifact_repo.download_artifacts(artifact_path, tmp_dir.name)
 
     # Ref: https://stackoverflow.com/a/24613980/6943581
-    file_handle = open(tmp_path, "rb")
+    file_handle = open(dst, "rb")
 
     def stream_and_remove_file():
         yield from file_handle
