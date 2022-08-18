90b1d68f2faafb343eb520b4b3a53ac108b8f87d
Hubert Zub <hubert.zub@databricks.com>
2022-08-18 09:23:31 +0200

    chore: shell script improvement
    Signed-off-by: Hubert Zub <hubert.zub@databricks.com>

--- dev/run-test-container.sh
+++ dev/run-test-container.sh
@@ -1,5 +1,6 @@
 ROOT_DIR=$(git rev-parse --show-toplevel)
 DOCKER_BUILDKIT=1 docker build -f "${ROOT_DIR}/dev/Dockerfile.test" -t mlflow-test-env "$ROOT_DIR"
+docker container rm -f mlflow-test 2> /dev/null
 docker run \
   -v "${ROOT_DIR}"/tests:/app/tests \
   -v "${ROOT_DIR}"/mlflow:/app/mlflow \
