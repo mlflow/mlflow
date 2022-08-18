922dc0afc445568616f9b0037a1b19a93c65bda0
Hubert Zub <hubert.zub@databricks.com>
2022-08-18 09:15:42 +0200

    chore: CR fixes
    Signed-off-by: Hubert Zub <hubert.zub@databricks.com>

--- dev/run-test-container.sh
+++ dev/run-test-container.sh
@@ -1,4 +1,13 @@
 ROOT_DIR=$(git rev-parse --show-toplevel)
 DOCKER_BUILDKIT=1 docker build -f "${ROOT_DIR}/dev/Dockerfile.test" -t mlflow-test-env "$ROOT_DIR"
-docker run -v "${ROOT_DIR}"/tests:/app/tests -v "${ROOT_DIR}"/mlflow:/app/mlflow --name "mlflow-test" -it mlflow-test-env
+docker run \
+  -v "${ROOT_DIR}"/tests:/app/tests \
+  -v "${ROOT_DIR}"/mlflow:/app/mlflow \
+  -v "${ROOT_DIR}"/pylintrc:/app/pylintrc \
+  -v "${ROOT_DIR}"/pyproject.toml:/app/pyproject.toml \
+  -v "${ROOT_DIR}"/pytest.ini:/app/pytest.ini \
+  -v "${ROOT_DIR}"/conftest.py:/app/conftest.py \
+  --name "mlflow-test" \
+  -it mlflow-test-env
+
 docker container rm mlflow-test
