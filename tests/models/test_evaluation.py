c6c9fc83e7ad7603fb0cd17a229dfb858afa7fd9
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-08-19 21:44:06 +0900

    Remove unnecessary comprehension (#6516)
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/models/test_evaluation.py
+++ tests/models/test_evaluation.py
@@ -80,9 +80,8 @@ RunData = namedtuple("RunData", ["params", "metrics", "tags", "artifacts"])
 def get_run_data(run_id):
     client = MlflowClient()
     data = client.get_run(run_id).data
-    tags = {k: v for k, v in data.tags.items()}
     artifacts = [f.path for f in client.list_artifacts(run_id)]
-    return RunData(params=data.params, metrics=data.metrics, tags=tags, artifacts=artifacts)
+    return RunData(params=data.params, metrics=data.metrics, tags=data.tags, artifacts=artifacts)
 
 
 def get_raw_tag(run_id, tag_name):
