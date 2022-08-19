6a6af2fee05c7b76c4a1a556d2ec8d17f1b9c0be
harupy <hkawamura0130@gmail.com>
2022-08-19 16:37:17 +0900

    add how to disable artifact serving
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/utils/cli_args.py
+++ mlflow/utils/cli_args.py
@@ -209,6 +209,6 @@ SERVE_ARTIFACTS = click.option(
     "by routing these requests to the storage location that is specified by "
     "'--artifact-destination' directly through a proxy. The default location that "
     "these requests are served from is a local './mlartifacts' directory which can be "
-    "overridden via the '--artifacts-destination' argument. "
-    "Default: True",
+    "overridden via the '--artifacts-destination' argument. To disable artifact serving, "
+    "specify `--no-serve-artifacts`. Default: True",
 )
