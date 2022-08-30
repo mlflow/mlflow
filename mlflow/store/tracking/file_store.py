86e2cb06dad2a4fdc6d11ae1001e8d65e287d59b
dbczumar <corey.zumar@databricks.com>
2022-08-29 22:11:24 -0700

    Dont check active
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/store/tracking/file_store.py
+++ mlflow/store/tracking/file_store.py
@@ -480,7 +480,6 @@ class FileStore(AbstractStore):
             raise MlflowException(
                 "Run '%s' metadata is in invalid state." % run_id, databricks_pb2.INVALID_STATE
             )
-        check_run_is_active(run_info)
         new_info = run_info._copy_with_overrides(lifecycle_stage=LifecycleStage.DELETED)
         self._overwrite_run_info(new_info, deleted_time=int(time.time() * 1000))
 
