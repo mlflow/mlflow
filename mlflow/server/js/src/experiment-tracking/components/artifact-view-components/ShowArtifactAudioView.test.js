f503186554d2f1c69f601c991cd1d4fa4942b696
Noah Nethery <nethery.n@gmail.com>
2022-08-30 20:57:38 +0100

    Remove setStates from test, change to functional component Signed-off-by: Noah Nethery <noah@nethery.dev>
    Signed-off-by: Noah Nethery <nethery.n@gmail.com>

--- mlflow/server/js/src/experiment-tracking/components/artifact-view-components/ShowArtifactAudioView.test.js
+++ mlflow/server/js/src/experiment-tracking/components/artifact-view-components/ShowArtifactAudioView.test.js
@@ -4,7 +4,6 @@ import ShowArtifactAudioView from './ShowArtifactAudioView';
 
 describe('ShowArtifactAudioView', () => {
   let wrapper;
-  let instance;
   let minimalProps;
   let commonProps;
 
@@ -23,33 +22,5 @@ describe('ShowArtifactAudioView', () => {
   test('should render with minimal props without exploding', () => {
     wrapper = shallow(<ShowArtifactAudioView {...minimalProps} />);
     expect(wrapper.length).toBe(1);
-  });
-
-  test('should render loading text when view is loading', () => {
-    instance = wrapper.instance();
-    instance.setState({ loading: true });
-    expect(wrapper.find('.artifact-audio-view-loading').length).toBe(1);
-  });
-
-  test('should render error text when artifact cannot be loaded or when artifact can be loaded but audio cannot be decoded', (done) => {
-    const getArtifact = jest.fn((artifactLocation) => {
-      return Promise.reject(new Error('my error text'));
-    });
-    const props = { ...minimalProps, getArtifact: getArtifact };
-    wrapper = shallow(<ShowArtifactAudioView {...props} />);
-    setImmediate(() => {
-      expect(wrapper.find('.artifact-audio-view-error').length).toBe(1);
-      expect(wrapper.instance().state.loading).toBe(false);
-      expect(wrapper.instance().state.error).toBeDefined();
-      done();
-    });
-  });
-
-  test('should fetch artifacts on component update', () => {
-    instance = wrapper.instance();
-    instance.fetchArtifacts = jest.fn();
-    wrapper.setProps({ path: 'newpath', runUuid: 'newRunId' });
-    expect(instance.fetchArtifacts).toBeCalled();
-    expect(instance.props.getArtifact).toBeCalled();
   });
 });
