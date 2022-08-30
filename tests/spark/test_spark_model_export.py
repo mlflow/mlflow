7730de2fcd72433f993b63fcadf9e345032afa70
Xinyue Ruan <serena.rxy@gmail.com>
2022-08-30 19:06:14 +0800

    replace DFS_TMP with MLFLOW_DFS_TMP everywhere
    Signed-off-by: Xinyue Ruan <serena.rxy@gmail.com>

--- tests/spark/test_spark_model_export.py
+++ tests/spark/test_spark_model_export.py
@@ -21,6 +21,7 @@ import mlflow.pyfunc.scoring_server as pyfunc_scoring_server
 import mlflow.tracking
 from mlflow import pyfunc
 from mlflow import spark as sparkm
+from mlflow.environment_variables import MLFLOW_DFS_TMP
 from mlflow.models import Model, infer_signature
 from mlflow.models.utils import _read_example
 from mlflow.store.artifact.s3_artifact_repo import S3ArtifactRepository
@@ -211,7 +212,7 @@ def test_model_export(spark_model_iris, model_path, spark_custom_env):
     # 3. score and compare reloaded pyfunc Spark udf
     preds3 = score_model_as_udf(model_uri=model_path, pandas_df=spark_model_iris.pandas_df)
     assert spark_model_iris.predictions == preds3
-    assert os.path.exists(sparkm.DFS_TMP)
+    assert os.path.exists(MLFLOW_DFS_TMP)
 
 
 def test_model_export_with_signature_and_examples(iris_df, spark_model_iris):
@@ -407,7 +408,7 @@ def test_log_model_calls_register_model(tmpdir, spark_model_iris):
                 model_uri, "AdsModel1", await_registration_for=DEFAULT_AWAIT_MAX_SLEEP_SECONDS
             )
     finally:
-        x = dfs_tmp_dir or sparkm.DFS_TMP
+        x = dfs_tmp_dir or MLFLOW_DFS_TMP
         shutil.rmtree(x)
 
 
@@ -424,7 +425,7 @@ def test_log_model_no_registered_model_name(tmpdir, spark_model_iris):
             )
             mlflow.register_model.assert_not_called()
     finally:
-        x = dfs_tmp_dir or sparkm.DFS_TMP
+        x = dfs_tmp_dir or MLFLOW_DFS_TMP
         shutil.rmtree(x)
 
 
