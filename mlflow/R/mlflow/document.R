b1e692da127250d57565f8f9b78628e4f1bca660
harupy <hkawamura0130@gmail.com>
2022-08-31 10:56:28 +0900

    fix rst_header
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/R/mlflow/document.R
+++ mlflow/R/mlflow/document.R
@@ -50,7 +50,7 @@ To use the MLflow R API, you must install `the MLflow Python package <https://py
 
     pip install mlflow
 
-Optionally, you can set the ``MLFLOW_PYTHON_BIN`` and ``MLFLOW_BIN`` environment variables to specify the Python and MLflow binaries to use. By default, the R client automatically finds them using ``Sys.which("python")`` and ``Sys.which("mlflow")``.
+Optionally, you can set the ``MLFLOW_PYTHON_BIN`` and ``MLFLOW_BIN`` environment variables to specify the Python and MLflow binaries to use. By default, the R client automatically finds them using ``Sys.which('python')`` and ``Sys.which('mlflow')``.
 
 .. code-block:: bash
 
