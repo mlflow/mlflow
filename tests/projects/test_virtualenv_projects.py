31b607009aa61510e47a1a81ef63f2db64d90541
harupy <hkawamura0130@gmail.com>
2022-09-01 11:19:06 +0900

    fix tests
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/projects/test_virtualenv_projects.py
+++ tests/projects/test_virtualenv_projects.py
@@ -83,8 +83,28 @@ name: test
 entry_points:
   main:
     command: |
-      python --version
+      python test.py
+"""
+    )
+    tmp_path.joinpath("test.py").write_text(
+        """
+import os
+
+assert "VIRTUAL_ENV" in os.environ
 """
     )
     mlflow.projects.run(str(tmp_path))
+    create_virtualenv_spy.assert_called_once()
+
+
+@spy_on_create_virtualenv
+def test_virtualenv_project_no_mlmodel_file(create_virtualenv_spy, tmp_path):
+    tmp_path.joinpath("test.py").write_text(
+        """
+import os
+
+assert "VIRTUAL_ENV" in os.environ
+"""
+    )
+    mlflow.projects.run(str(tmp_path), entry_point="test.py")
     create_virtualenv_spy.assert_called_once()
