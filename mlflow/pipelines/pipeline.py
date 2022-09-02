mlflow/mlflow

commit 62f7232749ae1f464d03ffea1d9c0f8f7fd49410

CLI command suggestion after running the pipeline (#6376)
* CLI command suggestion after running the pipeline

Signed-off-by: Hubert Zub <hubert.zub@databricks.com>

* fix: added git commands to MLP CLI suggestion

* chore: lint fixes

Signed-off-by: Hubert Zub <hubert.zub@databricks.com>

* chore: test fixes

Signed-off-by: Hubert Zub <hubert.zub@databricks.com>

Signed-off-by: Hubert Zub <hubert.zub@databricks.com>

mlflow/pipelines/pipeline.py
@@ -41,6 +41,7 @@ class _BasePipeline:
                         pipeline.yaml to generate the configuration to run the pipeline.
         """
         self._pipeline_root_path = pipeline_root_path
+        self._run_args = {}
         self._profile = profile
         self._name = get_pipeline_name(pipeline_root_path)
         self._steps = self._resolve_pipeline_steps()
@@ -69,6 +70,10 @@ class _BasePipeline:
                      executed.
         :return: None
         """
+
+        # Save the run parameters for later
+        self._run_args = {"step": step}
+
         # TODO Record performance here.
         # Always resolve the steps to load latest step modules before execution.
         self._steps = self._resolve_pipeline_steps()
@@ -166,6 +171,8 @@ class _BasePipeline:
         Constructs and returns all pipeline step objects from the pipeline configuration.
         """
         pipeline_config = get_pipeline_config(self._pipeline_root_path, self._profile)
+        pipeline_config["profile"] = self.profile
+        pipeline_config["run_args"] = self._run_args
         return [
             s.from_pipeline_config(pipeline_config, self._pipeline_root_path)
             for s in self._get_step_classes()

mlflow/pipelines/steps/train.py
@@ -31,7 +31,12 @@ from mlflow.projects.utils import get_databricks_env_vars
 from mlflow.tracking import MlflowClient
 from mlflow.tracking.fluent import _get_experiment_id
 from mlflow.utils.databricks_utils import get_databricks_run_url
-from mlflow.utils.mlflow_tags import MLFLOW_SOURCE_TYPE, MLFLOW_PIPELINE_TEMPLATE_NAME
+from mlflow.utils.mlflow_tags import (
+    MLFLOW_SOURCE_TYPE,
+    MLFLOW_PIPELINE_TEMPLATE_NAME,
+    MLFLOW_PIPELINE_PROFILE_NAME,
+    MLFLOW_PIPELINE_STEP_NAME,
+)
 
 _logger = logging.getLogger(__name__)
 
@@ -111,9 +116,13 @@ class TrainStep(BaseStep):
         estimator = estimator_fn()
         mlflow.autolog(log_models=False)
 
+        run_args = self.step_config.get("run_args") or {}
+
         tags = {
             MLFLOW_SOURCE_TYPE: SourceType.to_string(SourceType.PIPELINE),
             MLFLOW_PIPELINE_TEMPLATE_NAME: self.step_config["template_name"],
+            MLFLOW_PIPELINE_PROFILE_NAME: self.step_config["profile"],
+            MLFLOW_PIPELINE_STEP_NAME: run_args.get("step", ""),
         }
         with mlflow.start_run(tags=tags) as run:
             estimator.fit(X_train, y_train)
@@ -443,6 +452,8 @@ class TrainStep(BaseStep):
             step_config = pipeline_config["steps"]["train"]
             step_config["metrics"] = pipeline_config.get("metrics")
             step_config["template_name"] = pipeline_config.get("template")
+            step_config["profile"] = pipeline_config.get("profile")
+            step_config["run_args"] = pipeline_config.get("run_args")
             step_config.update(
                 get_pipeline_tracking_config(
                     pipeline_root_path=pipeline_root,

tests/pipelines/test_train_step.py
@@ -56,6 +56,9 @@ def set_up_train_step(pipeline_root: Path):
         """
         template: "regression/v1"
         target_col: "y"
+        profile: "test_profile"
+        run_args:
+            step: "train"
         experiment:
           name: "demo"
           tracking_uri: {tracking_uri}
@@ -129,7 +132,7 @@ def test_train_steps_autologs(tmp_pipeline_root_path):
     assert "epsilon" in params
 
 
-def test_train_steps_with_correct_source_type(tmp_pipeline_root_path):
+def test_train_steps_with_correct_tags(tmp_pipeline_root_path):
     with mock.patch.dict(
         os.environ, {_MLFLOW_PIPELINES_EXECUTION_DIRECTORY_ENV_VAR: str(tmp_pipeline_root_path)}
     ):
@@ -145,3 +148,5 @@ def test_train_steps_with_correct_source_type(tmp_pipeline_root_path):
     tags = MlflowClient().get_run(run_id).data.tags
     assert tags["mlflow.source.type"] == "PIPELINE"
     assert tags["mlflow.pipeline.template.name"] == "regression/v1"
+    assert tags["mlflow.pipeline.step.name"] == "train"
+    assert tags["mlflow.pipeline.profile.name"] == "test_profile"

mlflow/server/js/src/common/utils/Utils.js
@@ -49,6 +49,8 @@ class Utils {
   static backendTag = 'mlflow.project.backend';
   static userTag = 'mlflow.user';
   static loggedModelsTag = 'mlflow.log-model.history';
+  static pipelineProfileNameTag = 'mlflow.pipeline.profile.name';
+  static pipeLineStepNameTag = 'mlflow.pipeline.step.name';
 
   static formatMetric(value) {
     if (value === 0) {
@@ -606,6 +608,22 @@ class Utils {
     const gitCommitTag = runTags[Utils.gitCommitTag];
     if (gitCommitTag) {
       return gitCommitTag.value;
+    }
+    return '';
+  }
+
+  static getPipelineProfileName(runTags) {
+    const tag = runTags[Utils.pipelineProfileNameTag];
+    if (tag) {
+      return tag.value;
+    }
+    return '';
+  }
+
+  static getPipelineStepName(runTags) {
+    const tag = runTags[Utils.pipeLineStepNameTag];
+    if (tag) {
+      return tag.value;
     }
     return '';
   }

mlflow/server/js/src/experiment-tracking/components/RunView.css
@@ -70,11 +70,6 @@ i.expander {
   padding-right: 10px;
 }
 
-textarea.run-command {
-  width: 100%;
-  font-family: Menlo, Consolas, monospace;
-}
-
 .header-container .mlflow-menu > li.mlflow-menu-item:hover {
   background-image: none;
   background-color: rgb(1, 147, 225);

mlflow/server/js/src/experiment-tracking/components/RunView.js
@@ -126,6 +126,26 @@ export class RunViewImpl extends Component {
     const sourceVersion = Utils.getSourceVersion(tags);
     const entryPointName = Utils.getEntryPointName(tags);
     const backend = Utils.getBackend(tags);
+
+    if (Utils.getSourceType(tags) === 'PIPELINE') {
+      const profileName = Utils.getPipelineProfileName(tags);
+      const stepName = Utils.getPipelineStepName(tags);
+      runCommand = '';
+      if (sourceName) {
+        runCommand += `git clone ${sourceName}\n`;
+      }
+
+      if (sourceVersion) {
+        runCommand += `git checkout ${sourceVersion}\n`;
+      }
+
+      runCommand += `mlflow pipelines run -p ${shellEscape(profileName)}`;
+
+      if (stepName) {
+        runCommand += ' -s ' + shellEscape(stepName);
+      }
+    }
+
     if (Utils.getSourceType(tags) === 'PROJECT') {
       runCommand = 'mlflow run ' + shellEscape(sourceName);
       if (sourceVersion && sourceVersion !== 'latest') {
@@ -394,7 +414,12 @@ export class RunViewImpl extends Component {
               onChange={this.handleCollapseChange('runCommand')}
               data-test-id='run-command-section'
             >
-              <textarea className='run-command text-area' readOnly value={runCommand} />
+              <textarea
+                css={styles.runCommandArea}
+                // Setting row count basing on the number of line breaks
+                rows={(runCommand.match(/\n/g) || []).length + 1}
+                value={runCommand}
+              />
             </CollapsibleSection>
           ) : null}
           <CollapsibleSection
@@ -620,4 +645,11 @@ const shellEscape = (str) => {
     return '"' + str.replace(/"/g, '\\"') + '"';
   }
   return str;
+};
+
+const styles = {
+  runCommandArea: (theme) => ({
+    fontFamily: 'Menlo, Consolas, monospace',
+    width: '100%',
+  }),
 };

mlflow/utils/mlflow_tags.py
@@ -13,6 +13,8 @@ MLFLOW_PARENT_RUN_ID = "mlflow.parentRunId"
 MLFLOW_USER = "mlflow.user"
 MLFLOW_SOURCE_TYPE = "mlflow.source.type"
 MLFLOW_PIPELINE_TEMPLATE_NAME = "mlflow.pipeline.template.name"
+MLFLOW_PIPELINE_STEP_NAME = "mlflow.pipeline.step.name"
+MLFLOW_PIPELINE_PROFILE_NAME = "mlflow.pipeline.profile.name"
 MLFLOW_SOURCE_NAME = "mlflow.source.name"
 MLFLOW_GIT_COMMIT = "mlflow.source.git.commit"
 MLFLOW_GIT_BRANCH = "mlflow.source.git.branch"

commit d03f227c9a54ac08f8f39eb5c0e4497697f8e056

fix: regression caused by #6376
Signed-off-by: Hubert Zub <hubert.zub@databricks.com>

mlflow/pipelines/pipeline.py
@@ -41,7 +41,6 @@ class _BasePipeline:
                         pipeline.yaml to generate the configuration to run the pipeline.
         """
         self._pipeline_root_path = pipeline_root_path
-        self._run_args = {}
         self._profile = profile
         self._name = get_pipeline_name(pipeline_root_path)
         self._steps = self._resolve_pipeline_steps()
@@ -74,9 +73,6 @@ class _BasePipeline:
                      executed.
         :return: None
         """
-
-        # Save the run parameters for later
-        self._run_args = {"step": step}
 
         # TODO Record performance here.
         # Always resolve the steps to load latest step modules before execution.
@@ -178,7 +174,6 @@ class _BasePipeline:
         """
         pipeline_config = get_pipeline_config(self._pipeline_root_path, self._profile)
         pipeline_config["profile"] = self.profile
-        pipeline_config["run_args"] = self._run_args
         return [
             s.from_pipeline_config(pipeline_config, self._pipeline_root_path)
             for s in self._get_step_classes()

mlflow/pipelines/steps/train.py
@@ -117,13 +117,11 @@ class TrainStep(BaseStep):
         estimator = estimator_fn()
         mlflow.autolog(log_models=False)
 
-        run_args = self.step_config.get("run_args") or {}
-
         tags = {
             MLFLOW_SOURCE_TYPE: SourceType.to_string(SourceType.PIPELINE),
             MLFLOW_PIPELINE_TEMPLATE_NAME: self.step_config["template_name"],
             MLFLOW_PIPELINE_PROFILE_NAME: self.step_config["profile"],
-            MLFLOW_PIPELINE_STEP_NAME: run_args.get("step", ""),
+            MLFLOW_PIPELINE_STEP_NAME: os.getenv("target_step_name"),
         }
         with mlflow.start_run(tags=tags) as run:
             estimator.fit(X_train, y_train)
@@ -455,7 +453,7 @@ class TrainStep(BaseStep):
             step_config["metrics"] = pipeline_config.get("metrics")
             step_config["template_name"] = pipeline_config.get("template")
             step_config["profile"] = pipeline_config.get("profile")
-            step_config["run_args"] = pipeline_config.get("run_args")
+            # step_config["run_args"] = pipeline_config.get("run_args")
             step_config.update(
                 get_pipeline_tracking_config(
                     pipeline_root_path=pipeline_root,

mlflow/pipelines/utils/execution.py
@@ -69,7 +69,10 @@ def run_pipeline_step(
     # Aggregate step-specific environment variables into a single environment dictionary
     # that is passed to the Make subprocess. In the future, steps with different environments
     # should be isolated in different subprocesses
-    make_env = {}
+    make_env = {
+        # Include target step name in the environment variable set
+        "target_step_name": target_step.name,
+    }
     for step in pipeline_steps:
         make_env.update(step.environment)
     # Use Make to run the target step and all of its dependencies

tests/pipelines/test_train_step.py
@@ -134,7 +134,11 @@ def test_train_steps_autologs(tmp_pipeline_root_path):
 
 def test_train_steps_with_correct_tags(tmp_pipeline_root_path):
     with mock.patch.dict(
-        os.environ, {_MLFLOW_PIPELINES_EXECUTION_DIRECTORY_ENV_VAR: str(tmp_pipeline_root_path)}
+        os.environ,
+        {
+            _MLFLOW_PIPELINES_EXECUTION_DIRECTORY_ENV_VAR: str(tmp_pipeline_root_path),
+            "target_step_name": "train",
+        },
     ):
         train_step, train_step_output_dir = set_up_train_step(tmp_pipeline_root_path)
         train_step._run(str(train_step_output_dir))