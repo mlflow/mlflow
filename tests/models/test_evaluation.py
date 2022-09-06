c8f362a26c0ee9be3f99c5f3ab77f85e5c7a827e
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-09-06 18:42:40 +0900

    Merge master branch into branch-2.0 (#6707)
    * merge master
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix dataset hash
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/models/test_evaluation.py
+++ tests/models/test_evaluation.py
@@ -324,7 +324,10 @@ def iris_pandas_df_dataset():
             "y": eval_y,
         }
     )
-    return EvaluationDataset(data=data, targets="y", name="iris_pandas_df_dataset")
+    constructor_args = {"data": data, "targets": "y", "name": "iris_pandas_df_dataset"}
+    ds = EvaluationDataset(**constructor_args)
+    ds._constructor_args = constructor_args
+    return ds
 
 
 @pytest.fixture
@@ -333,7 +336,33 @@ def iris_pandas_df_num_cols_dataset():
     eval_X, eval_y = X[0::3], y[0::3]
     data = pd.DataFrame(eval_X)
     data["y"] = eval_y
-    return EvaluationDataset(data=data, targets="y", name="iris_pandas_df_num_cols_dataset")
+    constructor_args = {"data": data, "targets": "y", "name": "iris_pandas_df_num_cols_dataset"}
+    ds = EvaluationDataset(**constructor_args)
+    ds._constructor_args = constructor_args
+    return ds
+
+
+@pytest.fixture
+def iris_pandas_df_longname_cols_dataset():
+    X, y = get_iris()
+    eval_X, eval_y = X[0::3], y[0::3]
+    data = pd.DataFrame(
+        {
+            "f1": eval_X[:, 0],
+            "f2": eval_X[:, 1],
+            "f3longnamelongnamelongname": eval_X[:, 2],
+            "f4longnamelongnamelongname": eval_X[:, 3],
+            "y": eval_y,
+        }
+    )
+    constructor_args = {
+        "data": data,
+        "targets": "y",
+        "name": "iris_pandas_df_longname_cols_dataset",
+    }
+    ds = EvaluationDataset(**constructor_args)
+    ds._constructor_args = constructor_args
+    return ds
 
 
 @pytest.fixture
@@ -598,7 +627,7 @@ def test_dataset_hash(
 ):
     assert iris_dataset.hash == "99329a790dc483e7382c0d1d27aac3f3"
     assert iris_pandas_df_dataset.hash == "799d4f50e2e353127f94a0e5300add06"
-    assert iris_pandas_df_num_cols_dataset.hash == "0194e59415d97e0f64631bcb31e6c6b7"
+    assert iris_pandas_df_num_cols_dataset.hash == "3c5fc56830a0646001253e25e17bdce4"
     assert diabetes_spark_dataset.hash == "e646b03e976240bd0c79c6bcc1ae0bda"
 
 
