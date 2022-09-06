be1b9e1c0b6ec33009cd718f54c7c87544a6a3d2
harupy <hkawamura0130@gmail.com>
2022-09-06 13:59:33 +0900

    remove tabulate
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/utils/string_utils.py
+++ mlflow/utils/string_utils.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def strip_prefix(original, prefix):
     if original.startswith(prefix):
         return original[len(prefix) :]
@@ -29,3 +32,32 @@ def truncate_str_from_middle(s, max_length):
         left_part_len = (max_length - 3) // 2
         right_part_len = max_length - 3 - left_part_len
         return f"{s[:left_part_len]}...{s[-right_part_len:]}"
+
+
+def _create_table(
+    rows: List[List[str]], headers: List[str], column_sep: str = "  ", min_column_width=4
+) -> str:
+    """
+    Creates a table from a list of rows and headers.
+
+    Example
+    =======
+    >>> print(_create_table([["a", "b", "c"], ["d", "e", "f"]], ["x", "y", "z"]))
+    x     y     z
+    ----  ----  ----
+    a     b     c
+    d     e     f
+    """
+    column_widths = [
+        max(len(max(col, key=len)), len(header) + 2, min_column_width)
+        for col, header in zip(zip(*rows), headers)
+    ]
+    aligned_rows = [
+        column_sep.join(header.ljust(width) for header, width in zip(headers, column_widths)),
+        column_sep.join("-" * width for width in column_widths),
+        *(
+            column_sep.join(cell.ljust(width) for cell, width in zip(row, column_widths))
+            for row in rows
+        ),
+    ]
+    return "\n".join(aligned_rows)
