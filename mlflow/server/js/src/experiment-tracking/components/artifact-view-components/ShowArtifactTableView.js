a20ee58bc18e1f693e308ee9870f0024e8f2f835
Noah Nethery <nethery.n@gmail.com>
2022-09-06 09:53:31 -0400

    Convert to functional component Signed-off-by: Noah Nethery <noah@nethery.dev>
    Signed-off-by: Noah Nethery <nethery.n@gmail.com>

--- mlflow/server/js/src/experiment-tracking/components/artifact-view-components/ShowArtifactTableView.js
+++ mlflow/server/js/src/experiment-tracking/components/artifact-view-components/ShowArtifactTableView.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { getSrc } from './ShowArtifactPage';
 import { getArtifactContent } from '../../../common/utils/ArtifactUtils';
@@ -6,112 +6,37 @@ import { Table } from '@databricks/design-system';
 import { FormattedMessage } from 'react-intl';
 import Papa from 'papaparse';
 
-class ShowArtifactTableView extends Component {
-  constructor(props) {
-    super(props);
-    this.fetchArtifacts = this.fetchArtifacts.bind(this);
+const ShowArtifactTableView = ({ runUuid, path, getArtifact }) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [data, setData] = useState();
+  const [headers, setHeaders] = useState();
+  const [text, setText] = useState();
+
+  const MAX_ROW_LENGTH = 500;
+
+  useEffect(() => {
+    resetState();
+    fetchArtifacts({ path, runUuid, getArtifact });
+  }, [runUuid, path]);
+
+  function resetState() {
+    setError();
+    setData();
+    setHeaders();
+    setText();
+    setLoading(true);
   }
 
-  static MAX_ROW_LENGTH = 500;
-
-  static propTypes = {
-    runUuid: PropTypes.string.isRequired,
-    path: PropTypes.string.isRequired,
-    getArtifact: PropTypes.func,
-  };
-
-  static defaultProps = {
-    getArtifact: getArtifactContent,
-  };
-
-  state = {
-    loading: true,
-    error: undefined,
-    data: undefined,
-    headers: undefined,
-    text: undefined,
-  };
-
-  componentDidMount() {
-    this.fetchArtifacts();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.path !== prevProps.path || this.props.runUuid !== prevProps.runUuid) {
-      this.setState({ data: undefined, headers: undefined, loading: true });
-      this.fetchArtifacts();
-    }
-  }
-
-  render() {
-    if (this.state.loading) {
-      return <div className='artifact-text-view-loading'>Loading...</div>;
-    }
-    if (this.state.error) {
-      return (
-        <div className='artifact-text-view-error'>
-          Oops we couldn't load your file because of an error.
-        </div>
-      );
-    }
-
-    if (this.state.data) {
-      const columns = this.state.headers.map((f) => ({
-        title: f,
-        dataIndex: f,
-        key: f,
-        sorter: (a, b) => {
-          if (isNaN(a)) {
-            return a[f].localeCompare(b[f]);
-          } else {
-            return a[f] - b[f];
-          }
-        },
-        width: 200,
-        ellipsis: {
-          showTitle: true,
-        },
-      }));
-
-      const numRows = this.state.data.length;
-
-      return (
-        <div css={{ overscrollBehaviorX: 'contain', overflowX: 'scroll', margin: 10 }}>
-          <span css={{ display: 'flex', justifyContent: 'center' }}>
-            <FormattedMessage
-              defaultMessage='Previewing the first {numRows} rows'
-              description='Title for showing the number of rows in the parsed data preview'
-              values={{ numRows }}
-            />
-          </span>
-          <Table
-            columns={columns}
-            dataSource={this.state.data}
-            pagination={false}
-            sticky
-            scroll={{ x: 'min-content', y: true }}
-          />
-        </div>
-      );
-    } else {
-      return (
-        <div className='ShowArtifactPage'>
-          <div className='text-area-border-box'>{this.state.text}</div>
-        </div>
-      );
-    }
-  }
-
-  /** Fetches artifacts and updates component state with the result */
-  fetchArtifacts() {
-    const artifactLocation = getSrc(this.props.path, this.props.runUuid);
-    this.props
+  function fetchArtifacts(artifactData) {
+    const artifactLocation = getSrc(artifactData.path, artifactData.runUuid);
+    artifactData
       .getArtifact(artifactLocation)
-      .then((text) => {
+      .then((artifactText) => {
         try {
-          const result = Papa.parse(text, {
+          const result = Papa.parse(artifactText, {
             header: true,
-            preview: ShowArtifactTableView.MAX_ROW_LENGTH,
+            preview: MAX_ROW_LENGTH,
           });
           const dataPreview = result.data;
 
@@ -122,19 +47,86 @@ class ShowArtifactTableView extends Component {
             dataPreview.pop();
           }
 
-          this.setState({
-            data: dataPreview,
-            headers: result.meta.fields,
-            loading: false,
-          });
+          setLoading(false);
+          setHeaders(result.meta.fields);
+          setData(dataPreview);
         } catch (_) {
-          this.setState({ text: text, loading: false });
+          setLoading(false);
+          setText(artifactText);
         }
       })
-      .catch((error) => {
-        this.setState({ error: error, loading: false });
+      .catch((e) => {
+        setError(e);
+        setLoading(false);
       });
   }
-}
+
+  if (loading) {
+    return <div className='artifact-text-view-loading'>Loading...</div>;
+  }
+  if (error) {
+    return (
+      <div className='artifact-text-view-error'>
+        Oops we couldn't load your file because of an error.
+      </div>
+    );
+  }
+
+  if (data) {
+    const columns = headers.map((f) => ({
+      title: f,
+      dataIndex: f,
+      key: f,
+      sorter: (a, b) => {
+        if (isNaN(a)) {
+          return a[f].localeCompare(b[f]);
+        } else {
+          return a[f] - b[f];
+        }
+      },
+      width: 200,
+      ellipsis: {
+        showTitle: true,
+      },
+    }));
+
+    const numRows = data.length;
+
+    return (
+      <div css={{ overscrollBehaviorX: 'contain', overflowX: 'scroll', margin: 10 }}>
+        <span css={{ display: 'flex', justifyContent: 'center' }}>
+          <FormattedMessage
+            defaultMessage='Previewing the first {numRows} rows'
+            description='Title for showing the number of rows in the parsed data preview'
+            values={{ numRows }}
+          />
+        </span>
+        <Table
+          columns={columns}
+          dataSource={data}
+          pagination={false}
+          sticky
+          scroll={{ x: 'min-content', y: true }}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className='ShowArtifactPage'>
+        <div className='text-area-border-box'>{text}</div>
+      </div>
+    );
+  }
+};
+
+ShowArtifactTableView.propTypes = {
+  runUuid: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  getArtifact: PropTypes.func,
+};
+
+ShowArtifactTableView.defaultProps = {
+  getArtifact: getArtifactContent,
+};
 
 export default ShowArtifactTableView;
