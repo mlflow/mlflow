734b004309d4f2d20152796728a6388883020f74
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-09-09 13:34:21 +0900

    Add `pos_label` to `mlflow.evaluate` (#6696)
    * Add pos_label to mlflow.evaluate
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix dataset_name
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix _get_classifier_per_class_metrics_collection_df
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix failed tests
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * use evaluator_config
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * remove average
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * revert
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * remove pos_label
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * revert changes in examples/pipelines/sklearn_regression
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * default value
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * add **
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Update mlflow/models/evaluation/base.py
    
    Co-authored-by: Corey Zumar <39497902+dbczumar@users.noreply.github.com>
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix metrics to log in default evaluator
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix doc
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * mention default value
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * add _score
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * add average and sett pos_label to 1 by default
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * remove _get_classifier_metrics
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix doc
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * refactor
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * add test
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * rename dataset
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * test pos_label=None
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix ternary
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix tests
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * remove redundant function parameters
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    Co-authored-by: Corey Zumar <39497902+dbczumar@users.noreply.github.com>

--- mlflow/models/evaluation/base.py
+++ mlflow/models/evaluation/base.py
@@ -930,6 +930,13 @@ def evaluate(
         - **log_metrics_with_dataset_info**: A boolean value specifying whether or not to include
           information about the evaluation dataset in the name of each metric logged to MLflow
           Tracking during evaluation, default value is True.
+        - **pos_label**: The positive label to use when computing classification metrics such as
+          precision, recall, f1, etc. for binary classification models (default: ``1``). For
+          multiclass classification and regression models, this parameter will be ignored.
+        - **average**: The averaging method to use when computing classification metrics such as
+          precision, recall, f1, etc. for multiclass classification models
+          (default: ``'weighted'``). For binary classification and regression models, this
+          parameter will be ignored.
 
      - Limitations of evaluation dataset:
         - For classification tasks, dataset labels are used to infer the total number of classes.
