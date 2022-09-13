e66847f16bb22f26a4c4e6a98dbb42d55bd619a8
Khambar Dussaliyev <anuarkaliyev23@gmail.com>
2022-09-13 10:18:04 +0600

    Update mlflow/models/cli.py
    Signed-off-by: Khambar Dussaliyev <anuarkaliyev23@gmail.com>
    
    Co-authored-by: Harutaka Kawamura <hkawamura0130@gmail.com>
    Signed-off-by: Khambar Dussaliyev <anuarkaliyev23@gmail.com>

--- mlflow/models/cli.py
+++ mlflow/models/cli.py
@@ -179,7 +179,7 @@ def generate_dockerfile(
     if model_uri:
         _logger.info("Generating Dockerfile for model %s", model_uri)
     else:
-        _logger.info("Generating Dockerfile for unspecified model")
+        _logger.info("Generating Dockerfile")
     env_manager = env_manager or _EnvManager.CONDA
     backend = _get_flavor_backend(model_uri, docker_build=True, env_manager=env_manager)
     if backend.can_build_image():
