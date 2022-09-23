1c6d6eb2fed3292b82e9ba7cda3e0e779ca5b686
WeichenXu <weichen.xu@databricks.com>
2022-09-24 01:37:08 +0800

    init (#6872)
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/utils/conda.py
+++ mlflow/utils/conda.py
@@ -132,6 +132,7 @@ def _create_conda_env(
                 project_env_name,
                 "--file",
                 conda_env_path,
+                "--quiet",
             ],
             extra_env=conda_extra_env_vars,
             capture_output=capture_output,
