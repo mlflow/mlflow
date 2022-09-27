cec355576603ce968204a4022ed90ad4543ce27e
harupy <hkawamura0130@gmail.com>
2022-09-27 15:49:43 +0900

    Fix syntax
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/R/mlflow/tests/testthat/test-tracking-runs.R
+++ mlflow/R/mlflow/tests/testthat/test-tracking-runs.R
@@ -24,7 +24,7 @@ test_that("mlflow_start_run()/mlflow_get_run() work properly", {
         list(key = "foz", value = "baz"),
         list(key = "foo", value = "bar"),
         list(key = "mlflow.user", value = "user1"),
-        list(key = "mlflow.runName", value = run$run_name)),
+        list(key = "mlflow.runName", value = run$run_name)
       )
     )
   )
