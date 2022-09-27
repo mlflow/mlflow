8ceef50ca6b4a22738e5bc5e22870143639c439e
Subraman <subramaniam02@gmail.com>
2022-09-27 11:53:36 -0700

    adding user_id attribute of RunInfo in search_runs (#6881)
    * user_id filter for search_runs
    
    Signed-off-by: Subramaniam Shanmugam <subramaniam02@gmail.com>
    
    * Fixing unit tests
    
    Signed-off-by: Subramaniam Shanmugam <subramaniam02@gmail.com>
    
    * Fixing unit tests test_get_attribute_name
    
    Signed-off-by: Subramaniam Shanmugam <subramaniam02@gmail.com>
    
    * conflicts resolved
    
    Signed-off-by: Subramaniam Shanmugam <subramaniam02@gmail.com>
    
    * docs updated
    
    Signed-off-by: Subramaniam Shanmugam <subramaniam02@gmail.com>
    
    Signed-off-by: Subramaniam Shanmugam <subramaniam02@gmail.com>
    Signed-off-by: Subraman <subramaniam02@gmail.com>

--- tests/store/tracking/test_sqlalchemy_store.py
+++ tests/store/tracking/test_sqlalchemy_store.py
@@ -2450,7 +2450,7 @@ def test_get_attribute_name():
     # we want this to break if a searchable or orderable attribute has been added
     # and not referred to in this test
     # searchable attributes are also orderable
-    assert len(entities.RunInfo.get_orderable_attributes()) == 4
+    assert len(entities.RunInfo.get_orderable_attributes()) == 5
 
 
 def test_get_orderby_clauses():
