5378185235d63f1e52dbb80356b21453ce4aeaf2
dbczumar <corey.zumar@databricks.com>
2022-09-26 18:31:27 -0700

    Fix
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- tests/tensorflow/test_load_saved_tensorflow_estimator.py
+++ tests/tensorflow/test_load_saved_tensorflow_estimator.py
@@ -203,8 +203,7 @@ def test_pyfunc_serve_and_score(tmp_path, monkeypatch):
         content_type=pyfunc_scoring_server.CONTENT_TYPE_JSON,
         extra_args=["--env-manager", "local"],
     )
-    print("CONTENT", resp.content)
-    actual = pd.DataFrame(json.loads(resp.content))["predictions"]["class_ids"].values
+    actual = pd.DataFrame(json.loads(resp.content)["predictions"])["class_ids"].values
     expected = (
         model_data_info.expected_results_df["predictions"].map(iris_data_utils.SPECIES.index).values
     )
