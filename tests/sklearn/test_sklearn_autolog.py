a108149e6a9e982b190a29ae60ac868df50ce67e
harupy <hkawamura0130@gmail.com>
2022-10-03 13:38:13 +0900

    Fix failed tests
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/sklearn/test_sklearn_autolog.py
+++ tests/sklearn/test_sklearn_autolog.py
@@ -1201,7 +1201,7 @@ def test_eval_and_log_metrics_for_regressor():
     run_id = run.info.run_id
     _, metrics, _, artifacts = get_run_data(run_id)
     assert metrics == eval_metrics
-    assert len(artifacts) == 0
+    assert artifacts == ["estimator.html"]
 
 
 def test_eval_and_log_metrics_for_binary_classifier():
@@ -1247,7 +1247,7 @@ def test_eval_and_log_metrics_for_binary_classifier():
         )
     assert eval_metrics == expected_metrics
 
-    eval_artifacts = []
+    eval_artifacts = ["estimator.html"]
     if _is_plotting_supported():
         eval_artifacts.extend(
             [
@@ -1304,7 +1304,7 @@ def test_eval_and_log_metrics_for_binary_classifier_with_pos_label():
         )
     assert eval_metrics == pytest.approx(expected_metrics)
 
-    eval_artifacts = []
+    eval_artifacts = ["estimator.html"]
     if _is_plotting_supported():
         eval_artifacts.extend(
             [
@@ -1430,9 +1430,9 @@ def test_eval_and_log_metrics_for_classifier_multi_class():
 
     assert eval_metrics == expected_metrics
 
-    eval_artifacts = []
+    eval_artifacts = ["estimator.html"]
     if _is_plotting_supported():
-        eval_artifacts = ["{}.png".format("eval_confusion_matrix")]
+        eval_artifacts.append("{}.png".format("eval_confusion_matrix"))
 
     # Check that the logged metrics/artifacts are the same as the ones returned by the method.
     run_id = run.info.run_id
@@ -1459,14 +1459,14 @@ def test_eval_and_log_metrics_with_estimator(fit_func_name):
         )
 
     # Check contents of returned artifacts/metrics
-    assert eval_metrics == {"eval_score": model.score(X_eval, y_eval)}
+    np.testing.assert_allclose(eval_metrics["eval_score"], model.score(X_eval, y_eval))
 
     # Check that the logged metrics are the same as returned by the method.
     run_id = run.info.run_id
     _, metrics, _, artifacts = get_run_data(run_id)
 
     assert metrics == eval_metrics
-    assert len(artifacts) == 0
+    assert artifacts == ["estimator.html"]
 
 
 def test_eval_and_log_metrics_with_meta_estimator():
@@ -1491,7 +1491,9 @@ def test_eval_and_log_metrics_with_meta_estimator():
             model=model, X=X_eval, y_true=y_eval, prefix="eval_"
         )
 
-    eval_artifacts = ["{}.png".format("eval_confusion_matrix")] if _is_plotting_supported() else []
+    eval_artifacts = ["estimator.html"]
+    if _is_plotting_supported():
+        eval_artifacts.append("{}.png".format("eval_confusion_matrix"))
 
     # Check that the logged metrics/artifacts for the run are exactly those returned by the call
     run_id = run.info.run_id
@@ -1543,7 +1545,7 @@ def test_eval_and_log_metrics_with_new_run():
     run_id = mlflow.active_run().info.run_id
     _, metrics, _, artifacts = get_run_data(run_id)
     assert eval_metrics == metrics
-    assert len(artifacts) == 0
+    assert artifacts == ["estimator.html"]
     mlflow.end_run()
 
 
@@ -1572,7 +1574,7 @@ def test_eval_and_log_metrics_with_noscore_estimator():
     # No artifacts should be generated
     assert len(metrics) == 0
     assert len(eval_metrics) == 0
-    assert len(artifacts) == 0
+    assert artifacts == ["estimator.html"]
 
 
 def test_eval_and_log_metrics_throws_with_invalid_args():
@@ -2057,7 +2059,10 @@ def test_autolog_print_warning_if_custom_estimator_pickling_raise_error():
 
     run_id = run.info.run_id
     params, metrics, tags, artifacts = get_run_data(run_id)
-    assert len(params) > 0 and len(metrics) > 0 and len(tags) > 0 and artifacts == []
+    assert len(params) > 0
+    assert len(metrics) > 0
+    assert len(tags) > 0
+    assert artifacts == ["estimator.html"]
 
 
 def test_autolog_registering_model():
