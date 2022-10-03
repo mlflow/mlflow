e1dfdb9eb0fdda409690830d28244f373e76cfac
Prithvi Kannan <prithvi.kannan@databricks.com>
2022-10-03 13:17:40 -0700

    merge issue
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- mlflow/pipelines/steps/train.py
+++ mlflow/pipelines/steps/train.py
@@ -494,10 +494,10 @@ class TrainStep(BaseStep):
             )
         )
 
-        # Tab 1: Model performance summary metrics.
+        # Tab 1: Model performance.
         card.add_tab(
-            "Model Performance Summary Metrics",
-            "<h3 class='section-title'>Summary Metrics</h3>{{ METRICS }} ",
+            "Model Performance",
+            "<h3 class='section-title'>Summary Metrics (Validation)</h3>{{ METRICS }} ",
         ).add_html("METRICS", metric_table_html)
 
         if not self.skip_data_profiling:
