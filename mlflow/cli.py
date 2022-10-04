87a70ad5b5824717975343cc40be02b082440db8
shaikmoeed <shaikmoeed@gmail.com>
2022-10-04 11:36:56 +0530

    fixed pylint C0301
    Signed-off-by: shaikmoeed <shaikmoeed@gmail.com>

--- mlflow/cli.py
+++ mlflow/cli.py
@@ -523,8 +523,8 @@ def server(
     "--experiment-ids",
     default=None,
     help="Optional comma separated list of experiments to be permanently deleted including"
-    "all of its runs. If run ids are not specified, data is removed for all runs in the `deleted`"
-    " lifecycle stage.",
+    "all of its runs. If experiment ids are not specified, data is removed for all"
+    "experiments in the `deleted` lifecycle stage.",
 )
 def gc(older_than, backend_store_uri, run_ids):
     """
