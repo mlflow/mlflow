9ad6f5ed70091a92650400e754f0391ba22bcc87
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-05 02:28:24 +0000

    Adding numeric_owner as keyword arguement

--- examples/flower_classifier/train.py
+++ examples/flower_classifier/train.py
@@ -50,7 +50,7 @@ def download_input():
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar, path="./")
--- tests/utils/test_file_utils.py
+++ tests/utils/test_file_utils.py
@@ -260,7 +260,7 @@ def test_make_tarfile(tmpdir):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(handle, path=extract_dir)
