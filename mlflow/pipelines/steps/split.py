d38a78f876073d083faf84ea9ceff59b035bd838
Prithvi Kannan <46332835+prithvikannan@users.noreply.github.com>
2022-10-10 16:45:05 -0700

    load and run post_split_filter_method (#6928)
    * load and run post_split_filter_method
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * make backward compatible with post_split_method
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- mlflow/pipelines/steps/split.py
+++ mlflow/pipelines/steps/split.py
@@ -179,6 +179,7 @@ class SplitStep(BaseStep):
         train_df, validation_df, test_df = _get_split_df(input_df, hash_buckets, self.split_ratios)
         # Import from user function module to process dataframes
         post_split_config = self.step_config.get("post_split_method", None)
+        post_split_filter_config = self.step_config.get("post_split_filter_method", None)
         if post_split_config is not None:
             (post_split_module_name, post_split_fn_name) = post_split_config.rsplit(".", 1)
             sys.path.append(self.pipeline_root)
@@ -187,6 +188,21 @@ class SplitStep(BaseStep):
             )
             _logger.debug(f"Running {post_split_fn_name} on train, validation and test datasets.")
             (train_df, validation_df, test_df) = post_split(train_df, validation_df, test_df)
+        elif post_split_filter_config is not None:
+            (
+                post_split_filter_module_name,
+                post_split_filter_fn_name,
+            ) = post_split_filter_config.rsplit(".", 1)
+            sys.path.append(self.pipeline_root)
+            post_split_filter = getattr(
+                importlib.import_module(post_split_filter_module_name), post_split_filter_fn_name
+            )
+            _logger.debug(
+                f"Running {post_split_filter_fn_name} on train, validation and test datasets."
+            )
+            train_df = train_df[post_split_filter(train_df)]
+            validation_df = validation_df[post_split_filter(validation_df)]
+            test_df = test_df[post_split_filter(test_df)]
         # Output train / validation / test splits
         train_df.to_parquet(os.path.join(output_directory, _OUTPUT_TRAIN_FILE_NAME))
         validation_df.to_parquet(os.path.join(output_directory, _OUTPUT_VALIDATION_FILE_NAME))
