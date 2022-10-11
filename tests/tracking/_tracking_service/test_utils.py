76c8c1cb5dbb5f891f7a9005f03370016d2967da
lamhoangtung <lamhoangtung.vz@gmail.com>
2022-10-11 21:50:03 +0700

    Attempt to fix some failed unittest
    Signed-off-by: lamhoangtung <lamhoangtung.vz@gmail.com>

--- tests/tracking/_tracking_service/test_utils.py
+++ tests/tracking/_tracking_service/test_utils.py
@@ -145,7 +145,7 @@ def test_get_store_sqlalchemy_store(tmp_wkdir, db_type):
         store = _get_store()
         assert isinstance(store, SqlAlchemyStore)
         assert store.db_uri == uri
-        assert store.artifact_root_uri == "./mlruns"
+        assert store.artifact_root_uri == os.abs.path("./mlruns")
 
     mock_create_engine.assert_called_once_with(uri, pool_pre_ping=True)
 
