cc78ad38f11cd268c86b290e28d4a55ff63ceb81
harupy <hkawamura0130@gmail.com>
2022-10-19 10:53:28 +0900

    test
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- dev/run-python-flavor-tests.sh
+++ dev/run-python-flavor-tests.sh
@@ -4,7 +4,7 @@ set -x
 export MLFLOW_HOME=$(pwd)
 
 # TODO: Run tests for h2o, shap, and paddle in the cross-version-tests workflow
-pytest \
+pytest -s \
   tests/tracking/fluent/test_fluent_autolog.py \
   tests/autologging \
   tests/test_mlflow_lazily_imports_ml_packages.py
