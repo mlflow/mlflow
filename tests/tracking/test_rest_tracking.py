d27c08d8f4115034a6967a342e1e25ebd907d8b8
Ben Wilson <benjamin.wilson@databricks.com>
2022-10-20 14:27:50 -0400

    adjust test_rest_tracking for non deterministic sorting in windows
    Signed-off-by: Ben Wilson <benjamin.wilson@databricks.com>

--- tests/tracking/test_rest_tracking.py
+++ tests/tracking/test_rest_tracking.py
@@ -761,13 +761,13 @@ def test_search_experiments(mlflow_client):
     assert [e.name for e in experiments] == ["Abc", "ab"]
     # page_token
     experiments = mlflow_client.search_experiments(page_token=experiments.token)
-    assert [e.name for e in experiments] == ["a", "Default"]
+    assert sorted(e.name for e in experiments) == ["Default", "a"]
 
     # view_type
     mlflow_client.delete_experiment(experiment_ids[1])
     experiments = mlflow_client.search_experiments(view_type=ViewType.ACTIVE_ONLY)
-    assert [e.name for e in experiments] == ["Abc", "a", "Default"]
+    assert sorted(e.name for e in experiments) == ["Abc", "Default", "a"]
     experiments = mlflow_client.search_experiments(view_type=ViewType.DELETED_ONLY)
     assert [e.name for e in experiments] == ["ab"]
     experiments = mlflow_client.search_experiments(view_type=ViewType.ALL)
-    assert [e.name for e in experiments] == ["Abc", "ab", "a", "Default"]
+    assert sorted(e.name for e in experiments) == ["Abc", "Default", "a", "ab"]
