e9eb9eb0c9f171de3f9fa61493c586510dbc0fbc
harupy <hkawamura0130@gmail.com>
2022-10-25 18:28:52 +0900

    Add comment
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/db/check_migration.sh
+++ tests/db/check_migration.sh
@@ -3,8 +3,10 @@ set -ex
 
 cd tests/db
 
+# Install the lastest version of mlflow from PyPI
 pip install mlflow
 python check_migration.py pre-migration
+# Install mlflow from the repository
 pip install -e ../..
 mlflow db upgrade $MLFLOW_TRACKING_URI
 python check_migration.py post-migration
