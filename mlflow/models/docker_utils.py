3647819bf1c0057f3c3e37edfa0d95f6813f9a1f
harupy <hkawamura0130@gmail.com>
2022-10-25 22:49:50 +0900

    add -y
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/models/docker_utils.py
+++ mlflow/models/docker_utils.py
@@ -31,8 +31,8 @@ RUN git clone \
     https://github.com/pyenv/pyenv.git /root/.pyenv
 ENV PYENV_ROOT="/root/.pyenv"
 ENV PATH="$PYENV_ROOT/bin:$PATH"
-RUN apt install software-properties-common \
-    && add-apt-repository ppa:deadsnakes/ppa \
+RUN apt install -y software-properties-common \
+    && add-apt-repository -y ppa:deadsnakes/ppa \
     && apt install -y python3.7
 RUN ln -s -f $(which python3.7) /usr/bin/python
 RUN wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
