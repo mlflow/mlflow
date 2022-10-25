fe4cf1e09098654e4225d049fb29b85431173af7
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-10-25 14:17:52 +0900

    Make `mlflow ui` an alias for `mlflow server` (#7150)
    * Make mlflow ui an alias for mlflow server
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Add test
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Fix command name
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/cli.py
+++ mlflow/cli.py
@@ -34,7 +34,14 @@ from mlflow.exceptions import MlflowException
 _logger = logging.getLogger(__name__)
 
 
-@click.group()
+class AliasedGroup(click.Group):
+    def get_command(self, ctx, cmd_name):
+        # `mlflow ui` is an alias for `mlflow server`
+        cmd_name = "server" if cmd_name == "ui" else cmd_name
+        return super().get_command(ctx, cmd_name)
+
+
+@click.group(cls=AliasedGroup)
 @click.version_option(version=version.VERSION)
 def cli():
     pass
@@ -251,103 +258,6 @@ def _validate_server_args(gunicorn_opts=None, workers=None, waitress_opts=None):
                 "gunicorn replaces waitress on non-Windows platforms, "
                 "cannot specify --waitress-opts"
             )
-
-
-@cli.command()
-@click.option(
-    "--backend-store-uri",
-    envvar="MLFLOW_BACKEND_STORE_URI",
-    metavar="PATH",
-    default=DEFAULT_LOCAL_FILE_AND_ARTIFACT_PATH,
-    help="URI to which to persist experiment and run data. Acceptable URIs are "
-    "SQLAlchemy-compatible database connection strings "
-    "(e.g. 'sqlite:///path/to/file.db') or local filesystem URIs "
-    "(e.g. 'file:///absolute/path/to/directory'). By default, data will be logged "
-    f"to {DEFAULT_LOCAL_FILE_AND_ARTIFACT_PATH}",
-)
-@click.option(
-    "--registry-store-uri",
-    envvar="MLFLOW_REGISTRY_STORE_URI",
-    metavar="URI",
-    default=None,
-    help="URI to which to persist registered models. Acceptable URIs are "
-    "SQLAlchemy-compatible database connection strings (e.g. 'sqlite:///path/to/file.db'). "
-    "If not specified, `backend-store-uri` is used.",
-)
-@click.option(
-    "--default-artifact-root",
-    envvar="MLFLOW_DEFAULT_ARTIFACT_ROOT",
-    metavar="URI",
-    default=None,
-    help="Directory in which to store artifacts for any new experiments created. For tracking "
-    "server backends that rely on SQL, this option is required in order to store artifacts. "
-    "Note that this flag does not impact already-created experiments with any previous "
-    "configuration of an MLflow server instance. "
-    "If the --serve-artifacts option is specified, the default artifact root is "
-    f"{DEFAULT_ARTIFACTS_URI}. Otherwise, the default artifact root is "
-    f"{DEFAULT_LOCAL_FILE_AND_ARTIFACT_PATH}.",
-)
-@cli_args.SERVE_ARTIFACTS
-@cli_args.ARTIFACTS_DESTINATION
-@cli_args.PORT
-@cli_args.HOST
-def ui(
-    backend_store_uri,
-    registry_store_uri,
-    default_artifact_root,
-    serve_artifacts,
-    artifacts_destination,
-    port,
-    host,
-):
-    """
-    Launch the MLflow tracking UI for local viewing of run results. To launch a production
-    server, use the "mlflow server" command instead.
-
-    The UI will be visible at http://localhost:5000 by default, and only accepts connections
-    from the local machine. To let the UI server accept connections from other machines, you will
-    need to pass ``--host 0.0.0.0`` to listen on all network interfaces (or a specific interface
-    address).
-    """
-    from mlflow.server import _run_server
-    from mlflow.server.handlers import initialize_backend_stores
-
-    # Ensure that both backend_store_uri and default_artifact_uri are set correctly.
-    if not backend_store_uri:
-        backend_store_uri = DEFAULT_LOCAL_FILE_AND_ARTIFACT_PATH
-
-    # the default setting of registry_store_uri is same as backend_store_uri
-    if not registry_store_uri:
-        registry_store_uri = backend_store_uri
-
-    default_artifact_root = resolve_default_artifact_root(
-        serve_artifacts, default_artifact_root, backend_store_uri, resolve_to_local=True
-    )
-
-    try:
-        initialize_backend_stores(backend_store_uri, registry_store_uri, default_artifact_root)
-    except Exception as e:
-        _logger.error("Error initializing backend store")
-        _logger.exception(e)
-        sys.exit(1)
-
-    # TODO: We eventually want to disable the write path in this version of the server.
-    try:
-        _run_server(
-            backend_store_uri,
-            registry_store_uri,
-            default_artifact_root,
-            serve_artifacts,
-            False,
-            artifacts_destination,
-            host,
-            port,
-            None,
-            1,
-        )
-    except ShellCommandException:
-        eprint("Running the mlflow server failed. Please see the logs above for details.")
-        sys.exit(1)
 
 
 def _validate_static_prefix(ctx, param, value):  # pylint: disable=unused-argument
