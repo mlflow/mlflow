05222980326e7973d8efa0f71a81732ab9538c74
harupy <hkawamura0130@gmail.com>
2022-10-26 20:40:31 +0900

    python3.8-distutils
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/models/docker_utils.py
+++ mlflow/models/docker_utils.py
@@ -31,7 +31,7 @@ RUN git clone \
     https://github.com/pyenv/pyenv.git /root/.pyenv
 ENV PYENV_ROOT="/root/.pyenv"
 ENV PATH="$PYENV_ROOT/bin:$PATH"
-RUN apt install -y python3.8
+RUN apt install -y python3.8 python3.8-distutils
 RUN ln -s -f $(which python3.8) /usr/bin/python
 RUN wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
 RUN python /tmp/get-pip.py
