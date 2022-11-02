f06e78f1fb6a811797571f442017f8a4e097d51b
dbczumar <corey.zumar@databricks.com>
2022-11-01 23:19:32 -0700

    Tweaks
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- docs/theme/mlflow/static/js/custom.js
+++ docs/theme/mlflow/static/js/custom.js
@@ -1,3 +1,6 @@
+import docsearch from '@docsearch/js';
+import '@docsearch/css';
+
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"sphinx-rtd-theme":[function(require,module,exports){ // NOLINT
     var jQuery = (typeof(window) != 'undefined') ? window.jQuery : require('jquery');
 
