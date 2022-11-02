460ea73dabc49dca304ebc7053fc46496f4b41a2
Sunish Sheth <sunishsheth2009@gmail.com>
2022-11-02 12:18:53 -0700

    Initial setup to migrate pipeline to recipe (#7221)
    * Initial setup to migrate pipeline to recipe
    
    Signed-off-by: Sunish Sheth <sunishsheth2009@gmail.com>
    
    * Update tests
    
    Signed-off-by: Sunish Sheth <sunishsheth2009@gmail.com>
    
    * Updating docs to pass build-ci
    
    Signed-off-by: Sunish Sheth <sunishsheth2009@gmail.com>
    
    * Fixing tests
    
    Signed-off-by: Sunish Sheth <sunishsheth2009@gmail.com>
    
    * Fixing docs test
    
    Signed-off-by: Sunish Sheth <sunishsheth2009@gmail.com>
    
    Signed-off-by: Sunish Sheth <sunishsheth2009@gmail.com>

---
+++ mlflow/recipes/regression/v1/__init__.py
@@ -0,0 +1,5 @@
+from mlflow.recipes.regression.v1.recipe import (
+    RegressionRecipe as RecipeImpl,
+)
+
+__all__ = ["RecipeImpl"]
--- setup.py
+++ setup.py
@@ -43,8 +43,8 @@ extra_files = [
     "pypi_package_index.json",
     "pyspark/ml/log_model_allowlist.txt",
 ]
-pipelines_template_files = package_files("mlflow/pipelines/resources")
-pipelines_files = package_files("mlflow/pipelines/cards/templates")
+recipes_template_files = package_files("mlflow/recipes/resources")
+recipes_files = package_files("mlflow/recipes/cards/templates")
 
 
 """
@@ -117,8 +117,8 @@ setup(
             + models_container_server_files
             + alembic_files
             + extra_files
-            + pipelines_template_files
-            + pipelines_files
+            + recipes_template_files
+            + recipes_files
         ),
     }
     if not _is_mlflow_skinny
@@ -157,7 +157,6 @@ setup(
     entry_points="""
         [console_scripts]
         mlflow=mlflow.cli:cli
-        mlp=mlflow.pipelines.cli:commands
     """,
     cmdclass={
         "dependencies": ListDependencies,
