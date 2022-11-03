52797093f087290b9685ec9f1c7211f1830024da
harupy <hkawamura0130@gmail.com>
2022-11-03 23:51:58 +0900

    Fix rel
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/recipes/cards/test_histogram_generator.py
+++ tests/recipes/cards/test_histogram_generator.py
@@ -25,10 +25,10 @@ class HistogramGeneratorTestCase(unittest.TestCase):
         for i in range(len(expected.buckets)):
             actual_bucket = actual.buckets[i]
             expected_bucket = expected.buckets[i]
-            assert actual_bucket.low_value == pytest.approx(expected_bucket.low_value, rel=1e-3)
-            assert actual_bucket.high_value == pytest.approx(expected_bucket.high_value, rel=1e-3)
+            assert actual_bucket.low_value == pytest.approx(expected_bucket.low_value, rel=1e-2)
+            assert actual_bucket.high_value == pytest.approx(expected_bucket.high_value, rel=1e-2)
             assert actual_bucket.sample_count == pytest.approx(
-                expected_bucket.sample_count, rel=1e-3
+                expected_bucket.sample_count, rel=1e-2
             )
 
 
