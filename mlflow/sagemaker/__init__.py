78df5c76ea344d7d0ee49af39f959becead0489d
harupy <hkawamura0130@gmail.com>
2022-11-08 12:38:06 +0900

    Set DataCaptureConfig
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/sagemaker/__init__.py
+++ mlflow/sagemaker/__init__.py
@@ -403,6 +403,7 @@ def _deploy(
             sage_client=sage_client,
             s3_client=s3_client,
             variant_name=variant_name,
+            data_capture_config=data_capture_config,
         )
     else:
         deployment_operation = _create_sagemaker_endpoint(
@@ -1578,6 +1579,7 @@ def _update_sagemaker_endpoint(
     sage_client,
     s3_client,
     variant_name=None,
+    data_capture_config=None,
 ):
     """
     :param endpoint_name: The name of the SageMaker endpoint to update.
@@ -1597,6 +1599,10 @@ def _update_sagemaker_endpoint(
     :param sage_client: A boto3 client for SageMaker.
     :param s3_client: A boto3 client for S3.
     :variant_name: The name to assign to the new production variant if it doesn't already exist.
+    :param: data_capture_config: A dictionary specifying the data capture configuration to use when
+                                 creating the new SageMaker model associated with this application.
+                                 For more information, see https://docs.aws.amazon.com/sagemaker/latest/APIReference/API_DataCaptureConfig.html.
+                                 Defaults to ``None``.
     """
     if mode not in [DEPLOYMENT_MODE_ADD, DEPLOYMENT_MODE_REPLACE]:
         msg = "Invalid mode `{md}` for deployment to a pre-existing application".format(md=mode)
@@ -1650,6 +1656,7 @@ def _update_sagemaker_endpoint(
     endpoint_config_response = sage_client.create_endpoint_config(
         EndpointConfigName=new_config_name,
         ProductionVariants=production_variants,
+        DataCaptureConfig=data_capture_config,
         Tags=[{"Key": "app_name", "Value": endpoint_name}],
     )
     _logger.info(
@@ -1657,11 +1664,7 @@ def _update_sagemaker_endpoint(
         endpoint_config_response["EndpointConfigArn"],
     )
 
-    sage_client.update_endpoint(
-        EndpointName=endpoint_name,
-        EndpointConfigName=new_config_name,
-        RetainAllVariantProperties=True,
-    )
+    sage_client.update_endpoint(EndpointName=endpoint_name, EndpointConfigName=new_config_name)
     _logger.info("Updated endpoint with new configuration!")
 
     operation_start_time = time.time()
