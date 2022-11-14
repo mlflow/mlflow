acbb353de1f3a30e65fe3087831ac2bd904f6d6b
Prithvi Kannan <46332835+prithvikannan@users.noreply.github.com>
2022-11-14 08:41:46 -0800

    FLAML classifier metrics (#7322)
    * f1 in _MLFLOW_TO_FLAML_METRICS
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * adding accuracy_score
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * enable sklearn metrics
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * remove confusion metrics
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * fix
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * pylint
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * fix
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * rename
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * custom metric
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- mlflow/recipes/steps/automl/flaml.py
+++ mlflow/recipes/steps/automl/flaml.py
@@ -7,7 +7,10 @@ from sklearn.base import BaseEstimator
 import mlflow
 from mlflow import MlflowException
 from mlflow.models import EvaluationMetric
-from mlflow.models.evaluation.default_evaluator import _get_regressor_metrics
+from mlflow.models.evaluation.default_evaluator import (
+    _get_regressor_metrics,
+    _get_binary_classifier_metrics,
+)
 from mlflow.recipes.utils.metrics import RecipeMetric, _load_custom_metrics
 
 _logger = logging.getLogger(__name__)
@@ -19,7 +22,12 @@ _MLFLOW_TO_FLAML_METRICS = {
     "root_mean_squared_error": "rmse",
     "r2_score": "r2",
     "mean_absolute_percentage_error": "mape",
+    "f1_score": "f1",
+    "accuracy_score": "accuracy",
 }
+
+# metrics that are not supported natively in FLAML
+_SKLEARN_METRICS = ["recall_score", "precision_score"]
 
 
 def get_estimator_and_best_params(
@@ -37,14 +45,18 @@ def get_estimator_and_best_params(
 
 
 def _create_custom_metric_flaml(
-    metric_name: str, coeff: int, eval_metric: EvaluationMetric
+    task: str, metric_name: str, coeff: int, eval_metric: EvaluationMetric
 ) -> callable:
     def calc_metric(X, y, estimator) -> Dict[str, float]:
         y_pred = estimator.predict(X)
-        builtin_metrics = _get_regressor_metrics(y, y_pred, sample_weights=None)
+        builtin_metrics = (
+            _get_regressor_metrics(y, y_pred, sample_weights=None)
+            if task == "regression"
+            else _get_binary_classifier_metrics(y_true=y, y_pred=y_pred)
+        )
         res_df = pd.DataFrame()
         res_df["prediction"] = y_pred
-        res_df["target"] = y.values
+        res_df["target"] = y if task == "classification" else y.values
         return eval_metric.eval_fn(res_df, builtin_metrics)
 
     # pylint: disable=keyword-arg-before-vararg
@@ -71,6 +83,34 @@ def _create_custom_metric_flaml(
     return custom_metric
 
 
+def _create_sklearn_metric_flaml(metric_name: str, coeff: int) -> callable:
+    # pylint: disable=keyword-arg-before-vararg
+    # pylint: disable=unused-argument
+    def sklearn_metric(
+        X_val,
+        y_val,
+        estimator,
+        labels,
+        X_train,
+        y_train,
+        weight_val=None,
+        weight_train=None,
+        *args,
+    ):
+        import importlib
+
+        custom_metrics_mod = importlib.import_module("sklearn.metrics")
+        eval_fn = getattr(custom_metrics_mod, metric_name)
+        val_metric = coeff * eval_fn(y_val, estimator.predict(X_val))
+        train_metric = coeff * eval_fn(y_train, estimator.predict(X_train))
+        return val_metric, {
+            f"{metric_name}_train": train_metric,
+            f"{metric_name}_val": val_metric,
+        }
+
+    return sklearn_metric
+
+
 def _create_model_automl(
     X,
     y,
@@ -88,8 +128,13 @@ def _create_model_automl(
     try:
         if primary_metric in _MLFLOW_TO_FLAML_METRICS and primary_metric in evaluation_metrics:
             metric = _MLFLOW_TO_FLAML_METRICS[primary_metric]
+        elif primary_metric in _SKLEARN_METRICS and primary_metric in evaluation_metrics:
+            metric = _create_sklearn_metric_flaml(
+                primary_metric, -1 if evaluation_metrics[primary_metric].greater_is_better else 1
+            )
         elif primary_metric in evaluation_metrics:
             metric = _create_custom_metric_flaml(
+                task,
                 primary_metric,
                 -1 if evaluation_metrics[primary_metric].greater_is_better else 1,
                 _load_custom_metrics(recipe_root, [evaluation_metrics[primary_metric]])[0],
