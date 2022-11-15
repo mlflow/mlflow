b8453a8d648925ef9c0c87904d8047d2b6920334
harupy <hkawamura0130@gmail.com>
2022-11-15 21:10:42 +0900

    fix
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Turn off git status check to speed up zsh prompt: https://stackoverflow.com/a/25864063
+# Turn off git status check to improve zsh response speed: https://stackoverflow.com/a/25864063
 git config --add oh-my-zsh.hide-status 1
 git config --add oh-my-zsh.hide-dirty 1
 pip install --no-deps -e .
