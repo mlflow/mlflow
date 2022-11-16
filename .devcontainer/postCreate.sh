1ac9d046b24a12ca00accc89402e040e38a13b6f
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-11-16 12:42:12 +0900

    Add dev container configuration for VSCode (#6493)
    * Codespaces
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Use core-requirements.txt
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Compile requirements
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Improve
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Fix DB file name
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Add skinny requirements
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Use script
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Update .devcontainer/README.md
    
    Co-authored-by: Ben Wilson <39283302+BenWilson2@users.noreply.github.com>
    Signed-off-by: Harutaka Kawamura <hkawamura0130@gmail.com>
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    Signed-off-by: Harutaka Kawamura <hkawamura0130@gmail.com>
    Co-authored-by: Ben Wilson <39283302+BenWilson2@users.noreply.github.com>

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Turn off git status check to improve zsh response speed: https://stackoverflow.com/a/25864063
+git config --add oh-my-zsh.hide-status 1
+git config --add oh-my-zsh.hide-dirty 1
+pip install --no-deps -e .
