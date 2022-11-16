7f753e498439e43171228f770e184c5e7bc67640
WeichenXu <weichen.xu@databricks.com>
2022-11-16 08:55:38 +0800

    Fix `_create_local_spark_session_for_recipes` function used in MLflow recipe (#7307)
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * refine doc
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * small refactor
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * refactor
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * linter
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * fix linter
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * fix linter
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * address comments
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/utils/_spark_utils.py
+++ mlflow/utils/_spark_utils.py
@@ -17,6 +17,23 @@ def _get_active_spark_session():
         return SparkSession._instantiatedSession
 
 
+# Suppose we have a parent process already initiate a spark session that connected to a spark
+# cluster, then the parent process spawns a child process, if child process directly creates
+# a local spark session, it does not work correctly, because of PYSPARK_GATEWAY_PORT and
+# PYSPARK_GATEWAY_SECRET are inherited from parent process and child process pyspark session
+# will try to connect to the port and cause error.
+# So the 2 lines here are to clear 'PYSPARK_GATEWAY_PORT' and 'PYSPARK_GATEWAY_SECRET' to
+# enforce launching a new pyspark JVM gateway.
+def _prepare_subprocess_environ_for_creating_local_spark_session():
+    from mlflow.utils.databricks_utils import is_in_databricks_runtime
+
+    if is_in_databricks_runtime():
+        os.environ["SPARK_DIST_CLASSPATH"] = "/databricks/jars/*"
+
+    os.environ.pop("PYSPARK_GATEWAY_PORT", None)
+    os.environ.pop("PYSPARK_GATEWAY_SECRET", None)
+
+
 def _create_local_spark_session_for_recipes():
     """Create a sparksession to be used within an recipe step run in a subprocess locally."""
 
@@ -25,6 +42,7 @@ def _create_local_spark_session_for_recipes():
     except ImportError:
         # Return None if user doesn't have PySpark installed
         return None
+    _prepare_subprocess_environ_for_creating_local_spark_session()
     return (
         SparkSession.builder.master("local[*]")
         .config("spark.jars.packages", "io.delta:delta-core_2.12:1.2.1")
@@ -33,6 +51,30 @@ def _create_local_spark_session_for_recipes():
             "spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog"
         )
         .config("spark.sql.execution.arrow.pyspark.enabled", "true")
+        .getOrCreate()
+    )
+
+
+def _create_local_spark_session_for_loading_spark_model():
+    from pyspark.sql import SparkSession
+
+    return (
+        SparkSession.builder.config("spark.python.worker.reuse", "true")
+        # The config is a workaround for avoiding databricks delta cache issue when loading
+        # some specific model such as ALSModel.
+        .config("spark.databricks.io.cache.enabled", "false")
+        # In Spark 3.1 and above, we need to set this conf explicitly to enable creating
+        # a SparkSession on the workers
+        .config("spark.executor.allowSparkContext", "true")
+        # Binding "spark.driver.bindAddress" to 127.0.0.1 helps avoiding some local hostname
+        # related issues (e.g. https://github.com/mlflow/mlflow/issues/5733).
+        .config("spark.driver.bindAddress", "127.0.0.1")
+        .config("spark.executor.allowSparkContext", "true")
+        .config(
+            "spark.driver.extraJavaOptions",
+            "-Dlog4j.configuration=file:/usr/local/spark/conf/log4j.properties",
+        )
+        .master("local[1]")
         .getOrCreate()
     )
 
