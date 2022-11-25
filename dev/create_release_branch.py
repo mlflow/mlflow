f5abf61969a53e639adf48f470555c6965d1b595
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-11-25 17:46:13 +0900

    Add a script to create a release branch (#7396)
    * Add a script to create a release branch
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * fix messages
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

---
+++ dev/create_release_branch.py
@@ -0,0 +1,52 @@
+import subprocess
+
+import click
+from packaging.version import Version
+
+
+@click.command(help="Create a release branch")
+@click.option("--new-version", required=True)
+@click.option("--remote", required=False, default="origin", show_default=True)
+@click.option(
+    "--dry-run/--no-dry-run", is_flag=True, default=True, show_default=True, envvar="DRY_RUN"
+)
+def main(new_version: str, remote: str, dry_run=False):
+    version = Version(new_version)
+    release_branch = f"branch-{version.major}.{version.minor}"
+    is_patch_release = version.micro != 0
+    if is_patch_release:
+        click.echo(f"{version} is a patch release, skipping branch creation")
+        return
+
+    exists_on_remote = (
+        subprocess.check_output(
+            ["git", "ls-remote", "--heads", remote, release_branch], text=True
+        ).strip()
+        != ""
+    )
+    if exists_on_remote:
+        click.echo(f"{release_branch} already exists on {remote}, skipping branch creation")
+        return
+
+    prev_branch = subprocess.check_output(["git", "branch", "--show-current"], text=True).strip()
+    try:
+        exists_on_local = (
+            subprocess.check_output(["git", "branch", "--list", release_branch], text=True).strip()
+            != ""
+        )
+        if exists_on_local:
+            click.echo(f"Deleting existing {release_branch}")
+            subprocess.check_call(["git", "branch", "-D", release_branch])
+
+        click.echo(f"Creating {release_branch}")
+        subprocess.check_call(["git", "checkout", "-b", release_branch, "master"])
+        click.echo(f"Pushing {release_branch} to {remote}")
+        subprocess.check_call(
+            ["git", "push", remote, release_branch, *(["--dry-run"] if dry_run else [])]
+        )
+    finally:
+        subprocess.check_call(["git", "checkout", prev_branch])
+
+
+if __name__ == "__main__":
+    main()  # pylint: disable=no-value-for-parameter
