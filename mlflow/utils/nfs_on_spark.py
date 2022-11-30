c05c664dfaace13cac29d37982176005987f7384
Weichen Xu <weichen.xu@databricks.com>
2022-11-30 16:57:39 +0800

    init
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/utils/nfs_on_spark.py
+++ mlflow/utils/nfs_on_spark.py
@@ -4,6 +4,7 @@ import shutil
 
 from mlflow.utils.databricks_utils import is_in_databricks_runtime
 from mlflow.utils._spark_utils import _get_active_spark_session
+from mlflow.utils.databricks_utils import _get_dbutils
 
 # Set spark config "spark.mlflow.nfs.rootDir" to specify a NFS (network file system) directory
 # which is shared with all spark cluster nodes.
@@ -27,18 +28,23 @@ def get_nfs_cache_root_dir():
             spark_sess.conf.get("spark.databricks.mlflow.nfs.enabled", "true").lower() == "true"
         )
         if nfs_enabled:
-            nfs_root_dir = "/local_disk0/.ephemeral_nfs"
-            # Test whether the NFS directory is writable.
-            test_path = os.path.join(nfs_root_dir, uuid.uuid4().hex)
             try:
-                os.makedirs(test_path)
-                return nfs_root_dir
+                # If databricks runtime has dbutils entrypoint function `getReplNFSTempDir`,
+                # the directory it returns always has available permission.
+                return _get_dbutils().entry_point.getReplNFSTempDir()
             except Exception:
-                # For databricks cluster enabled Table ACL, we have no permission to access NFS
-                # directory, in this case, return None representing NFS is not available.
-                return None
-            finally:
-                shutil.rmtree(test_path, ignore_errors=True)
+                nfs_root_dir = "/local_disk0/.ephemeral_nfs"
+                # Test whether the NFS directory is writable.
+                test_path = os.path.join(nfs_root_dir, uuid.uuid4().hex)
+                try:
+                    os.makedirs(test_path)
+                    return nfs_root_dir
+                except Exception:
+                    # For databricks cluster enabled Table ACL, we have no permission to access NFS
+                    # directory, in this case, return None representing NFS is not available.
+                    return None
+                finally:
+                    shutil.rmtree(test_path, ignore_errors=True)
         else:
             return None
     else:
