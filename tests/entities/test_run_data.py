39bd7bc766b9a9e456a6df9cc82176f3ac41edcb
Wiktor Wasowicz <wiktor7645@gmail.com>
2022-12-06 19:57:06 +0100

    converted test_run_data to pytest Signed-off-by: Wiktor Wasowicz wiktor7645@gmail.com

--- tests/entities/test_run_data.py
+++ tests/entities/test_run_data.py
@@ -1,56 +1,69 @@
-import unittest
+import pytest
 
 from mlflow.entities import Metric, RunData, Param, RunTag
 from mlflow.utils.time_utils import get_current_time_millis
 from tests.helper_functions import random_str, random_int
 
 
-class TestRunData(unittest.TestCase):
-    def _check_metrics(self, metric_objs, metrics_dict, expected_metrics):
-        assert {m.key for m in metric_objs} == {m.key for m in expected_metrics}
-        assert {m.value for m in metric_objs} == {m.value for m in expected_metrics}
-        assert {m.timestamp for m in metric_objs} == {m.timestamp for m in expected_metrics}
-        assert {m.step for m in metric_objs} == {m.step for m in expected_metrics}
-        assert len(metrics_dict) == len(expected_metrics)
-        assert metrics_dict == {m.key: m.value for m in expected_metrics}
+def _check_metrics(metric_objs, metrics_dict, expected_metrics):
+    assert {m.key for m in metric_objs} == {m.key for m in expected_metrics}
+    assert {m.value for m in metric_objs} == {m.value for m in expected_metrics}
+    assert {m.timestamp for m in metric_objs} == {m.timestamp for m in expected_metrics}
+    assert {m.step for m in metric_objs} == {m.step for m in expected_metrics}
+    assert len(metrics_dict) == len(expected_metrics)
+    assert metrics_dict == {m.key: m.value for m in expected_metrics}
 
-    def _check_params(self, params_dict, expected_params):
-        assert params_dict == {p.key: p.value for p in expected_params}
 
-    def _check_tags(self, tags_dict, expected_tags):
-        assert tags_dict == {t.key: t.value for t in expected_tags}
+def _check_params(params_dict, expected_params):
+    assert params_dict == {p.key: p.value for p in expected_params}
 
-    def _check(self, rd, metrics, params, tags):
-        assert isinstance(rd, RunData)
-        self._check_metrics(rd._metric_objs, rd.metrics, metrics)
-        self._check_params(rd.params, params)
-        self._check_tags(rd.tags, tags)
 
-    @staticmethod
-    def _create():
-        metrics = [
-            Metric(
-                key=random_str(10),
-                value=random_int(0, 1000),
-                timestamp=get_current_time_millis() + random_int(-1e4, 1e4),
-                step=random_int(),
-            )
-        ]
-        params = [Param(random_str(10), random_str(random_int(10, 35))) for _ in range(10)]
-        tags = [RunTag(random_str(10), random_str(random_int(10, 35))) for _ in range(10)]
-        rd = RunData(metrics=metrics, params=params, tags=tags)
-        return rd, metrics, params, tags
+def _check_tags(tags_dict, expected_tags):
+    assert tags_dict == {t.key: t.value for t in expected_tags}
 
-    def test_creation_and_hydration(self):
-        rd1, metrics, params, tags = self._create()
-        self._check(rd1, metrics, params, tags)
 
-        as_dict = {
-            "metrics": {m.key: m.value for m in metrics},
-            "params": {p.key: p.value for p in params},
-            "tags": {t.key: t.value for t in tags},
-        }
-        assert dict(rd1) == as_dict
-        proto = rd1.to_proto()
-        rd2 = RunData.from_proto(proto)
-        self._check(rd2, metrics, params, tags)
+def _check(rd, metrics, params, tags):
+    assert isinstance(rd, RunData)
+    _check_metrics(rd._metric_objs, rd.metrics, metrics)
+    _check_params(rd.params, params)
+    _check_tags(rd.tags, tags)
+
+
+@pytest.fixture(scope="module")
+def metrics():
+    yield [
+        Metric(
+            key=random_str(10),
+            value=random_int(0, 1000),
+            timestamp=get_current_time_millis() + random_int(-1e4, 1e4),
+            step=random_int(),
+        )
+    ]
+
+
+@pytest.fixture(scope="module")
+def params():
+    yield [Param(random_str(10), random_str(random_int(10, 35))) for _ in range(10)]
+
+
+@pytest.fixture(scope="module")
+def tags():
+    yield [RunTag(random_str(10), random_str(random_int(10, 35))) for _ in range(10)]
+
+
+@pytest.fixture(scope="module")
+def rd(metrics, params, tags):
+    yield RunData(metrics=metrics, params=params, tags=tags)
+
+
+def test_creation_and_hydration(rd, metrics, params, tags):
+    _check(rd, metrics, params, tags)
+    as_dict = {
+        "metrics": {m.key: m.value for m in metrics},
+        "params": {p.key: p.value for p in params},
+        "tags": {t.key: t.value for t in tags},
+    }
+    assert dict(rd) == as_dict
+    proto = rd.to_proto()
+    rd2 = RunData.from_proto(proto)
+    _check(rd2, metrics, params, tags)
