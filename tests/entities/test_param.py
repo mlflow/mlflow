286a0167fc00bfba331781714cd9814a2ae5d9b3
Wiktor Wasowicz <wiktor7645@gmail.com>
2022-12-06 19:44:59 +0100

    converted test_param.py to pytest Signed-off-by: Wiktor Wasowicz wiktor7645@gmail.com

--- tests/entities/test_param.py
+++ tests/entities/test_param.py
@@ -1,28 +1,35 @@
-import unittest
-
+import pytest
 from mlflow.entities import Param
 from tests.helper_functions import random_str, random_int
 
 
-class TestParam(unittest.TestCase):
-    def _check(self, param, key, value):
-        assert isinstance(param, Param)
-        assert param.key == key
-        assert param.value == value
+def _check(param, key, value):
+    assert isinstance(param, Param)
+    assert param.key == key
+    assert param.value == value
 
-    def test_creation_and_hydration(self):
-        key = random_str(random_int(10, 25))  # random string on size in range [10, 25]
-        value = random_str(random_int(55, 75))  # random string on size in range [55, 75]
 
-        param = Param(key, value)
-        self._check(param, key, value)
+@pytest.fixture(scope="module")
+def key():
+    yield random_str(random_int(10, 25))  # random string on size in range [10, 25]
 
-        as_dict = {"key": key, "value": value}
-        assert dict(param) == as_dict
 
-        proto = param.to_proto()
-        param2 = Param.from_proto(proto)
-        self._check(param2, key, value)
+@pytest.fixture(scope="module")
+def value():
+    yield random_str(random_int(55, 75))  # random string on size in range [55, 75]
 
-        param3 = Param.from_dictionary(as_dict)
-        self._check(param3, key, value)
+
+def test_creation_and_hydration(key, value):
+
+    param = Param(key, value)
+    _check(param, key, value)
+
+    as_dict = {"key": key, "value": value}
+    assert dict(param) == as_dict
+
+    proto = param.to_proto()
+    param2 = Param.from_proto(proto)
+    _check(param2, key, value)
+
+    param3 = Param.from_dictionary(as_dict)
+    _check(param3, key, value)
