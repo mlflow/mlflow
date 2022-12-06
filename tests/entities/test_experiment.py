4c540f5799c5fb90212ba877a05f738772ca3778
Wiktor Wasowicz <wiktor7645@gmail.com>
2022-12-06 19:07:13 +0100

    deleted unused import Signed-off-by: Wiktor Wasowicz wiktor7645@gmail.com

--- tests/entities/test_experiment.py
+++ tests/entities/test_experiment.py
@@ -1,67 +1,91 @@
-import unittest
-
+import pytest
 from mlflow.entities import Experiment, LifecycleStage
 from mlflow.utils.time_utils import get_current_time_millis
 from tests.helper_functions import random_int, random_file
 
 
-class TestExperiment(unittest.TestCase):
-    def _check(self, exp, exp_id, name, location, lifecyle_stage, creation_time, last_update_time):
-        assert isinstance(exp, Experiment)
-        assert exp.experiment_id == exp_id
-        assert exp.name == name
-        assert exp.artifact_location == location
-        assert exp.lifecycle_stage == lifecyle_stage
-        assert exp.creation_time == creation_time
-        assert exp.last_update_time == last_update_time
+def _check(exp, exp_id, name, location, lifecyle_stage, creation_time, last_update_time):
+    assert isinstance(exp, Experiment)
+    assert exp.experiment_id == exp_id
+    assert exp.name == name
+    assert exp.artifact_location == location
+    assert exp.lifecycle_stage == lifecyle_stage
+    assert exp.creation_time == creation_time
+    assert exp.last_update_time == last_update_time
 
-    def test_creation_and_hydration(self):
-        exp_id = str(random_int())
-        name = "exp_%d_%d" % (random_int(), random_int())
-        lifecycle_stage = LifecycleStage.ACTIVE
-        location = random_file(".json")
-        creation_time = get_current_time_millis()
-        last_update_time = get_current_time_millis()
 
-        exp = Experiment(
-            exp_id,
-            name,
-            location,
-            lifecycle_stage,
-            creation_time=creation_time,
-            last_update_time=last_update_time,
-        )
-        self._check(exp, exp_id, name, location, lifecycle_stage, creation_time, last_update_time)
+@pytest.fixture
+def exp_id():
+    yield str(random_int())
 
-        as_dict = {
-            "experiment_id": exp_id,
-            "name": name,
-            "artifact_location": location,
-            "lifecycle_stage": lifecycle_stage,
-            "tags": {},
-            "creation_time": creation_time,
-            "last_update_time": last_update_time,
-        }
-        assert dict(exp) == as_dict
 
-        proto = exp.to_proto()
-        exp2 = Experiment.from_proto(proto)
-        self._check(exp2, exp_id, name, location, lifecycle_stage, creation_time, last_update_time)
+@pytest.fixture
+def name():
+    yield "exp_%d_%d" % (random_int(), random_int())
 
-        exp3 = Experiment.from_dictionary(as_dict)
-        self._check(exp3, exp_id, name, location, lifecycle_stage, creation_time, last_update_time)
 
-    def test_string_repr(self):
-        exp = Experiment(
-            experiment_id=0,
-            name="myname",
-            artifact_location="hi",
-            lifecycle_stage=LifecycleStage.ACTIVE,
-            creation_time=1662004217511,
-            last_update_time=1662004217511,
-        )
-        assert (
-            str(exp)
-            == "<Experiment: artifact_location='hi', creation_time=1662004217511, experiment_id=0, "
-            "last_update_time=1662004217511, lifecycle_stage='active', name='myname', tags={}>"
-        )
+@pytest.fixture
+def lifecycle_stage():
+    yield LifecycleStage.ACTIVE
+
+
+@pytest.fixture
+def location():
+    yield random_file(".json")
+
+
+@pytest.fixture
+def creation_time():
+    yield get_current_time_millis()
+
+
+@pytest.fixture
+def last_update_time():
+    yield get_current_time_millis()
+
+
+def test_creation_and_hydration(
+    exp_id, name, lifecycle_stage, location, creation_time, last_update_time
+):
+    exp = Experiment(
+        exp_id,
+        name,
+        location,
+        lifecycle_stage,
+        creation_time=creation_time,
+        last_update_time=last_update_time,
+    )
+    _check(exp, exp_id, name, location, lifecycle_stage, creation_time, last_update_time)
+
+    as_dict = {
+        "experiment_id": exp_id,
+        "name": name,
+        "artifact_location": location,
+        "lifecycle_stage": lifecycle_stage,
+        "tags": {},
+        "creation_time": creation_time,
+        "last_update_time": last_update_time,
+    }
+    assert dict(exp) == as_dict
+    proto = exp.to_proto()
+    exp2 = Experiment.from_proto(proto)
+    _check(exp2, exp_id, name, location, lifecycle_stage, creation_time, last_update_time)
+
+    exp3 = Experiment.from_dictionary(as_dict)
+    _check(exp3, exp_id, name, location, lifecycle_stage, creation_time, last_update_time)
+
+
+def test_string_repr():
+    exp = Experiment(
+        experiment_id=0,
+        name="myname",
+        artifact_location="hi",
+        lifecycle_stage=LifecycleStage.ACTIVE,
+        creation_time=1662004217511,
+        last_update_time=1662004217511,
+    )
+    assert (
+        str(exp)
+        == "<Experiment: artifact_location='hi', creation_time=1662004217511, experiment_id=0, "
+        "last_update_time=1662004217511, lifecycle_stage='active', name='myname', tags={}>"
+    )
--- tests/entities/test_file_info.py
+++ tests/entities/test_file_info.py
@@ -1,29 +1,31 @@
-import unittest
+import pytest
 
 from mlflow.entities import FileInfo
 from tests.helper_functions import random_str, random_int
 
 
-class TestFileInfo(unittest.TestCase):
-    def _check(self, fi, path, is_dir, size_in_bytes):
-        assert isinstance(fi, FileInfo)
-        assert fi.path == path
-        assert fi.is_dir == is_dir
-        assert fi.file_size == size_in_bytes
+def _check(fi, path, is_dir, size_in_bytes):
+    assert isinstance(fi, FileInfo)
+    assert fi.path == path
+    assert fi.is_dir == is_dir
+    assert fi.file_size == size_in_bytes
 
-    def test_creation_and_hydration(self):
-        path = random_str(random_int(10, 50))
-        is_dir = random_int(10, 2500) % 2 == 0
-        size_in_bytes = random_int(1, 10000)
-        fi1 = FileInfo(path, is_dir, size_in_bytes)
-        self._check(fi1, path, is_dir, size_in_bytes)
 
-        as_dict = {"path": path, "is_dir": is_dir, "file_size": size_in_bytes}
-        assert dict(fi1) == as_dict
+@pytest.mark.parametrize(
+    "path, is_dir, size_in_bytes",
+    [(random_str(random_int(10, 50)), random_int(10, 2500) % 2 == 0, random_int(1, 10000))],
+)
+def test_creation_and_hydration(path, is_dir, size_in_bytes):
 
-        proto = fi1.to_proto()
-        fi2 = FileInfo.from_proto(proto)
-        self._check(fi2, path, is_dir, size_in_bytes)
+    fi1 = FileInfo(path, is_dir, size_in_bytes)
+    _check(fi1, path, is_dir, size_in_bytes)
 
-        fi3 = FileInfo.from_dictionary(as_dict)
-        self._check(fi3, path, is_dir, size_in_bytes)
+    as_dict = {"path": path, "is_dir": is_dir, "file_size": size_in_bytes}
+    assert dict(fi1) == as_dict
+
+    proto = fi1.to_proto()
+    fi2 = FileInfo.from_proto(proto)
+    _check(fi2, path, is_dir, size_in_bytes)
+
+    fi3 = FileInfo.from_dictionary(as_dict)
+    _check(fi3, path, is_dir, size_in_bytes)
