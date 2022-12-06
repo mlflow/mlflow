4160f8bef69bb4214124a30ae3ca19d6cc590108
Akira Noda <61897166+tsugumi-sys@users.noreply.github.com>
2022-12-07 08:31:37 +0900

    Convert `test-code` to `test-code-block` and fix some failed tests (#7459)
    * convert test-code to test-code-block and fix some failed tests
    
    Signed-off-by: tsugumi-sys <tidemark0105@gmail.com>
    
    * add plotly package
    
    Signed-off-by: tsugumi-sys <tidemark0105@gmail.com>
    
    Signed-off-by: tsugumi-sys <tidemark0105@gmail.com>

--- docs/conftest.py
+++ docs/conftest.py
@@ -7,3 +7,11 @@ def tracking_uri_mock(tmp_path, monkeypatch):
     tracking_uri = "sqlite:///{}".format(tmp_path / "mlruns.sqlite")
     mlflow.set_tracking_uri(tracking_uri)
     monkeypatch.setenv("MLFLOW_TRACKING_URI", tracking_uri)
+    yield
+    mlflow.set_tracking_uri(None)
+
+
+@pytest.fixture(autouse=True)
+def reset_active_experiment_id():
+    yield
+    mlflow.tracking.fluent._active_experiment_id = None
