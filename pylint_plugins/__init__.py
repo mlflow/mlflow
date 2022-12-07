42b4e2eb56fd36c37f2c474d259bba6dc3d49fa4
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-12-07 18:38:03 +0900

    Add custom `use-f-string` rule (#7469)
    * test
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Add use-f-string rule
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * improve test
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- pylint_plugins/__init__.py
+++ pylint_plugins/__init__.py
@@ -1,6 +1,7 @@
 from .pytest_raises_checker import PytestRaisesChecker
 from .print_function import PrintFunction
 from .unittest_assert_raises import UnittestAssertRaises
+from .string_checker import StringChecker  # pylint: disable=unused-import
 from .set_checker import SetChecker
 
 
@@ -9,3 +10,4 @@ def register(linter):
     linter.register_checker(PrintFunction(linter))
     linter.register_checker(UnittestAssertRaises(linter))
     linter.register_checker(SetChecker(linter))
+    # linter.register_checker(StringChecker(linter))
