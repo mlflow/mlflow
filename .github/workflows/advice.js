235c233b85146b38e2d5d67ad0badecc9ace82de
harupy <hkawamura0130@gmail.com>
2022-12-22 18:01:46 +0900

    Improve advice message
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- .github/workflows/advice.js
+++ .github/workflows/advice.js
@@ -27,12 +27,13 @@ module.exports = async ({ context, github }) => {
   const { number: issue_number } = context.issue;
   const { sha, label } = context.payload.pull_request.head;
   const { user } = context.payload.pull_request;
-  const messages = [""];
+  const messages = [];
 
   const dcoCheck = await getDcoCheck(github, owner, repo, sha);
   if (dcoCheck.conclusion !== "success") {
     messages.push(
-      "The DCO check failed. " +
+      "### DCO check\n\n" +
+        "The DCO check failed. " +
         `Please sign off your commit(s) by following the instructions [here](${dcoCheck.html_url}). ` +
         "See https://github.com/mlflow/mlflow/blob/master/CONTRIBUTING.md#sign-your-work for more " +
         "details."
@@ -41,14 +42,17 @@ module.exports = async ({ context, github }) => {
 
   if (label.endsWith(":master")) {
     messages.push(
-      "This PR is filed from the master branch in your fork, which is not recommended " +
+      "### PR branch check\n\n" +
+        "This PR was filed from the master branch in your fork, which is not recommended " +
         "and may cause our CI checks to fail. Please close this PR and file a new PR from " +
         "a non-master branch."
     );
   }
 
-  if (messages.length > 1) {
-    const body = `@${user.login} Thank you for the contribution!` + messages.join("\n\n---\n\n");
+  if (messages.length > 0) {
+    const body =
+      `@${user.login} Thank you for the contribution! Could you fix the following issue(s)?\n\n` +
+      messages.join("\n\n");
     await github.rest.issues.createComment({
       owner,
       repo,
