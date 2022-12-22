403ed00a35eddefe233862318181bef6db13a2f4
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-12-22 18:57:30 +0900

    Add a python script to create a release tag (#7583)
    * Add a script to create a release tag
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Modify instructions
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

---
+++ dev/create_release_tag.py
@@ -0,0 +1,57 @@
+"""
+How to test this script
+-----------------------
+# Ensure origin points to your fork
+git remote -v | grep origin
+
+# Pretend we're releasing MLflow 9.0.0
+git checkout -b branch-9.0
+
+# First, test the dry run mode
+python dev/create_release_tag.py --new-version 9.0.0 --dry-run
+git tag -d v9.0.0
+
+# Open https://github.com/<username>/mlflow/tree/v9.0.0 and verify that the tag does not exist.
+
+# Then, test the non-dry run mode
+python dev/create_release_tag.py --new-version 9.0.0 --no-dry-run
+git tag -d v9.0.0
+
+# Open https://github.com/<username>/mlflow/tree/v9.0.0 and verify that the tag exists now.
+
+# Clean up the remote tag
+git push --delete origin v9.0.0
+
+# Clean up the local release branch
+git checkout master
+git branch -D branch-9.0
+"""
+import subprocess
+
+import click
+from packaging.version import Version
+
+
+@click.command(help="Create a release tag")
+@click.option("--new-version", required=True)
+@click.option("--remote", required=False, default="origin", show_default=True)
+@click.option(
+    "--dry-run/--no-dry-run", is_flag=True, default=True, show_default=True, envvar="DRY_RUN"
+)
+def main(new_version: str, remote: str, dry_run: bool = False):
+    if not dry_run:
+        version = Version(new_version)
+        release_branch = f"branch-{version.major}.{version.minor}"
+        current_branch = subprocess.check_output(
+            ["git", "branch", "--show-current"], text=True
+        ).strip()
+        assert (
+            current_branch == release_branch
+        ), f"Expected to be on {release_branch} but on {current_branch}"
+    release_tag = f"v{new_version}"
+    subprocess.check_call(["git", "tag", release_tag])
+    subprocess.check_call(["git", "push", remote, release_tag, *(["--dry-run"] if dry_run else [])])
+
+
+if __name__ == "__main__":
+    main()  # pylint: disable=no-value-for-parameter
