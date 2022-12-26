69c38c7e971485fc85ab8dcfe283e94f7f5135f6
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-12-26 13:33:39 +0900

    [master] Ensure mlflow imports ML packages lazily (#7599)
    * Cherry pick
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Fix typo
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

---
+++ tests/check_mlflow_lazily_imports_ml_packages.py
@@ -0,0 +1,55 @@
+"""
+Tests that `import mlflow` and `mlflow.autolog()` do not import ML packages.
+"""
+
+import sys
+import importlib
+import mlflow  # pylint: disable=unused-import
+
+
+def main():
+    ml_packages = {
+        "catboost",
+        "fastai",
+        "mxnet",
+        "h2o",
+        "keras",
+        "lightgbm",
+        "mleap",
+        "onnx",
+        "pytorch_lightning",
+        "pyspark",
+        "pyspark.ml",
+        "shap",
+        "sklearn",
+        "spacy",
+        "statsmodels",
+        "tensorflow",
+        "torch",
+        "xgboost",
+        "pmdarima",
+        "diviner",
+    }
+    imported = ml_packages.intersection(set(sys.modules))
+    assert imported == set(), f"mlflow imports {imported} when it's imported but it should not"
+
+    mlflow.autolog()
+    imported = ml_packages.intersection(set(sys.modules)) - {"pyspark", "pyspark.ml"}
+    assert imported == set(), f"`mlflow.autolog` imports {imported} but it should not"
+
+    # Ensure that the ML packages are importable
+    failed_to_import = []
+    for package in sorted(ml_packages):
+        try:
+            importlib.import_module(package)
+        except ImportError:
+            failed_to_import.append(package)
+
+    message = (
+        f"Failed to import {failed_to_import}. Please install packages that provide these modules."
+    )
+    assert failed_to_import == [], message
+
+
+if __name__ == "__main__":
+    main()
