c31cdc24fad48bfe78bd159f1ad0d4765c8c018f
harupy <hkawamura0130@gmail.com>
2022-12-28 11:49:44 +0900

    Fix test_prune_packages again
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/utils/test_requirements_utils.py
+++ tests/utils/test_requirements_utils.py
@@ -199,7 +199,7 @@ def test_normalize_package_name():
 
 def test_prune_packages():
     assert _prune_packages(["mlflow"]) == {"mlflow"}
-    assert _prune_packages(["mlflow", "scikit-learn"]) == {"mlflow"}
+    assert _prune_packages(["mlflow", "scikit-learn"]) == {"mlflow", "scikit-learn"}
 
 
 def test_capture_imported_modules():
