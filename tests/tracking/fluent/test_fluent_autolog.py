9b85a1dbecbcfdff8a77fad4e50013daceb511ce
Weichen Xu <weichen.xu@databricks.com>
2022-12-28 18:32:07 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- tests/tracking/fluent/test_fluent_autolog.py
+++ tests/tracking/fluent/test_fluent_autolog.py
@@ -165,17 +165,20 @@ def test_universal_autolog_calls_pyspark_immediately_in_databricks():
             autolog_mock.assert_not_called()
 
 
-def test_universal_autolog_attaches_pyspark_import_hook_in_non_databricks():
-    with mock.patch("mlflow.spark.autolog", wraps=mlflow.spark.autolog) as autolog_mock:
+@pytest.mark.parametrize("config", [{"disable": False}, {"disable": True}])
+def test_universal_autolog_attaches_pyspark_import_hook_in_non_databricks(config):
+    with mock.patch(
+        "mlflow.spark.autolog", wraps=mlflow.spark.autolog, autospec=True
+    ) as autolog_mock:
         autolog_mock.integration_name = "spark"
 
-        mlflow.autolog()
+        mlflow.autolog(**config)
         autolog_mock.assert_not_called()
 
         mlflow.utils.import_hooks.notify_module_loaded(pyspark)
 
         # assert autolog is called once pyspark is imported
-        autolog_mock.assert_called_once()
+        autolog_mock.assert_called_once_with(**config, silent=False)
 
 
 def test_universal_autolog_makes_expected_event_logging_calls():
