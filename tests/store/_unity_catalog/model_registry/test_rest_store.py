154481d699eaf0672b009dbee10ef276f2d62d29
Sid Murching <sid.murching@databricks.com>
2023-01-31 22:06:28 -0800

    Add model version CRUD support for models in UC
    Signed-off-by: Sid Murching <sid.murching@databricks.com>

--- tests/store/_unity_catalog/model_registry/test_rest_store.py
+++ tests/store/_unity_catalog/model_registry/test_rest_store.py
@@ -1,7 +1,9 @@
+import unittest
 from itertools import combinations
 
 import json
 import pytest
+import uuid
 from unittest import mock
 import functools
 
@@ -12,6 +14,12 @@ from mlflow.protos.databricks_uc_registry_messages_pb2 import (
     UpdateRegisteredModelRequest,
     DeleteRegisteredModelRequest,
     GetRegisteredModelRequest,
+    CreateModelVersionRequest,
+    UpdateModelVersionRequest,
+    DeleteModelVersionRequest,
+    GetModelVersionRequest,
+    GetModelVersionDownloadUriRequest,
+    SearchModelVersionsRequest,
     SearchRegisteredModelsRequest,
 )
 from mlflow.store._unity_catalog.registry.rest_store import UcModelRegistryStore
@@ -216,3 +224,138 @@ def test_delete_registered_model_tag_unsupported(mock_http, store):
     with pytest.raises(MlflowException, match=expected_err_msg):
         store.delete_registered_model_tag(name=name, key="key")
 
+@mock_http_request
+def test_create_model_version(mock_http, store):
+    store.create_model_version("model_1", "path/to/source")
+    _verify_requests(
+        mock_http,
+        "model-versions/create",
+        "POST",
+        CreateModelVersionRequest(name="model_1", source="path/to/source"),
+    )
+    # test optional fields
+    run_id = uuid.uuid4().hex
+    description = "version description"
+    store.create_model_version(
+        "model_1",
+        "path/to/source",
+        run_id,
+        description=description,
+    )
+    _verify_requests(
+        mock_http,
+        "model-versions/create",
+        "POST",
+        CreateModelVersionRequest(
+            name="model_1",
+            source="path/to/source",
+            run_id=run_id,
+            description=description,
+        ),
+    )
+
+@mock_http_request
+def test_create_model_version_invalid_args(mock_http, store):
+    tags = [
+        ModelVersionTag(key="key", value="value"),
+        ModelVersionTag(key="anotherKey", value="some other value"),
+    ]
+    run_link = "https://localhost:5000"
+    params_list = [
+        {"run_link": run_link},
+        {"tags": [tags]},
+    ]
+    # test all combination of invalid params
+    for sz in range(1, 3):
+        for combination in combinations(params_list, sz):
+            params = {k: v for d in combination for k, v in d.items()}
+            with pytest.raises(MlflowException, match="unsupported for models in the Unity Catalog"):
+                store.create_model_version(**params, name="mymodel", source="mysource")
+
+
+@mock_http_request
+def test_transition_model_version_stage_unsupported(mock_http, store):
+    name = "model_1"
+    version = "5"
+    expected_err_msg = _expected_unsupported_method_error_message("transition_model_version_stage")
+    with pytest.raises(MlflowException, match=expected_err_msg):
+        store.transition_model_version_stage(
+            name=name, version=version, stage="prod", archive_existing_versions=True
+        )
+
+@mock_http_request
+def test_update_model_version_description(mock_http, store):
+    name = "model_1"
+    version = "5"
+    description = "test model version"
+    store.update_model_version(name=name, version=version, description=description)
+    _verify_requests(
+        mock_http,
+        "model-versions/update",
+        "PATCH",
+        UpdateModelVersionRequest(name=name, version=version, description="test model version"),
+    )
+
+
+@mock_http_request
+def test_delete_model_version(mock_http, store):
+    name = "model_1"
+    version = "12"
+    store.delete_model_version(name=name, version=version)
+    _verify_requests(
+        mock_http,
+        "model-versions/delete",
+        "DELETE",
+        DeleteModelVersionRequest(name=name, version=version),
+    )
+
+
+@mock_http_request
+def test_get_model_version_details(mock_http, store):
+    name = "model_11"
+    version = "8"
+    store.get_model_version(name=name, version=version)
+    _verify_requests(
+        mock_http, "model-versions/get", "GET", GetModelVersionRequest(name=name, version=version)
+    )
+
+
+@mock_http_request
+def test_get_model_version_download_uri(mock_http, store):
+    name = "model_11"
+    version = "8"
+    store.get_model_version_download_uri(name=name, version=version)
+    _verify_requests(
+        mock_http,
+        "model-versions/get-download-uri",
+        "GET",
+        GetModelVersionDownloadUriRequest(name=name, version=version),
+    )
+
+
+@mock_http_request
+def test_search_model_versions(mock_http, store):
+    store.search_model_versions(filter_string="name='model_12'")
+    _verify_requests(
+        mock_http,
+        "model-versions/search",
+        "GET",
+        SearchModelVersionsRequest(filter="name='model_12'"),
+    )
+
+
+@mock_http_request
+def test_set_model_version_tag_unsupported(mock_http, store):
+    name = "model_1"
+    tag = ModelVersionTag(key="key", value="value")
+    expected_err_msg = _expected_unsupported_method_error_message("set_model_version_tag")
+    with pytest.raises(MlflowException, match=expected_err_msg):
+        store.set_model_version_tag(name=name, version="1", tag=tag)
+
+
+@mock_http_request
+def test_delete_model_version_tag(mock_http, store):
+    name = "model_1"
+    expected_err_msg = _expected_unsupported_method_error_message("delete_model_version_tag")
+    with pytest.raises(MlflowException, match=expected_err_msg):
+        store.delete_model_version_tag(name=name, version="1", key="key")
