a426f345e58a0a1fdc5614d054f8d24394c336ad
Siddharth Murching <sid.murching@databricks.com>
2023-02-13 14:25:52 -0800

    Add REST client for UC model registry and support for registered model CRUD (#7766)
    * Fix proto generation
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Add support for registered model crud in UC
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Remove unused methods
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Lint
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Lint fix
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * fix lint
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Fix lint
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Fix lint
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Fix test
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Add comment
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Lint
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Address comments
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * WIP
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Address comments, remove currently-unused utils for model versions
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * lint
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * pylint
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Address comments
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Fix lint
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * address comments
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    ---------
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>

---
+++ mlflow/store/_unity_catalog/registry/rest_store.py
@@ -0,0 +1,243 @@
+import logging
+
+from mlflow.protos.databricks_uc_registry_messages_pb2 import (
+    CreateRegisteredModelRequest,
+    CreateRegisteredModelResponse,
+    UpdateRegisteredModelRequest,
+    UpdateRegisteredModelResponse,
+    DeleteRegisteredModelRequest,
+    DeleteRegisteredModelResponse,
+    CreateModelVersionRequest,
+    CreateModelVersionResponse,
+    FinalizeModelVersionRequest,
+    FinalizeModelVersionResponse,
+    UpdateModelVersionRequest,
+    UpdateModelVersionResponse,
+    DeleteModelVersionRequest,
+    DeleteModelVersionResponse,
+    GetModelVersionDownloadUriRequest,
+    GetModelVersionDownloadUriResponse,
+    SearchModelVersionsRequest,
+    SearchModelVersionsResponse,
+    GetRegisteredModelRequest,
+    GetRegisteredModelResponse,
+    GetModelVersionRequest,
+    GetModelVersionResponse,
+    SearchRegisteredModelsRequest,
+    SearchRegisteredModelsResponse,
+    GenerateTemporaryModelVersionCredentialsRequest,
+    GenerateTemporaryModelVersionCredentialsResponse,
+)
+from mlflow.exceptions import MlflowException
+from mlflow.protos.databricks_uc_registry_service_pb2 import UcModelRegistryService
+from mlflow.store.entities.paged_list import PagedList
+from mlflow.utils.proto_json_utils import message_to_json
+from mlflow.utils.rest_utils import (
+    extract_api_info_for_service,
+    extract_all_api_info_for_service,
+    _REST_API_PATH_PREFIX,
+)
+from mlflow.store.model_registry.rest_store import BaseRestStore
+from mlflow.store._unity_catalog.registry.utils import (
+    registered_model_from_uc_proto,
+)
+from mlflow.utils.annotations import experimental
+
+
+_METHOD_TO_INFO = extract_api_info_for_service(UcModelRegistryService, _REST_API_PATH_PREFIX)
+_METHOD_TO_ALL_INFO = extract_all_api_info_for_service(
+    UcModelRegistryService, _REST_API_PATH_PREFIX
+)
+
+_logger = logging.getLogger(__name__)
+
+
+def _require_arg_unspecified(arg_name, arg_value, default_value=None, message=None):
+    if arg_value != default_value:
+        _raise_unsupported_arg(arg_name, message)
+
+
+def _raise_unsupported_arg(arg_name, message=None):
+    messages = [
+        f"Argument '{arg_name}' is unsupported for models in the Unity Catalog.",
+    ]
+    if message is not None:
+        messages.append(message)
+    messages.append("See the user guide for more information")
+    raise MlflowException(" ".join(messages))
+
+
+def _raise_unsupported_method(method, message=None):
+    messages = [
+        f"Method '{method}' is unsupported for models in the Unity Catalog.",
+    ]
+    if message is not None:
+        messages.append(message)
+    messages.append("See the user guide for more information")
+    raise MlflowException(" ".join(messages))
+
+
+# TODO: re-enable the abstract-method check in a follow-up PR. It's disabled for now because
+# we haven't yet implemented required model version CRUD APIs in this class
+@experimental
+class UcModelRegistryStore(BaseRestStore):  # pylint: disable=abstract-method
+    """
+    Client for a remote model registry server accessed via REST API calls
+
+    :param get_host_creds: Method to be invoked prior to every REST request to get the
+      :py:class:`mlflow.rest_utils.MlflowHostCreds` for the request. Note that this
+      is a function so that we can obtain fresh credentials in the case of expiry.
+    """
+
+    def _get_response_from_method(self, method):
+        method_to_response = {
+            CreateRegisteredModelRequest: CreateRegisteredModelResponse,
+            UpdateRegisteredModelRequest: UpdateRegisteredModelResponse,
+            DeleteRegisteredModelRequest: DeleteRegisteredModelResponse,
+            CreateModelVersionRequest: CreateModelVersionResponse,
+            FinalizeModelVersionRequest: FinalizeModelVersionResponse,
+            UpdateModelVersionRequest: UpdateModelVersionResponse,
+            DeleteModelVersionRequest: DeleteModelVersionResponse,
+            GetModelVersionDownloadUriRequest: GetModelVersionDownloadUriResponse,
+            SearchModelVersionsRequest: SearchModelVersionsResponse,
+            GetRegisteredModelRequest: GetRegisteredModelResponse,
+            GetModelVersionRequest: GetModelVersionResponse,
+            SearchRegisteredModelsRequest: SearchRegisteredModelsResponse,
+            # pylint: disable=line-too-long
+            GenerateTemporaryModelVersionCredentialsRequest: GenerateTemporaryModelVersionCredentialsResponse,
+        }
+        return method_to_response[method]()
+
+    def _get_endpoint_from_method(self, method):
+        return _METHOD_TO_INFO[method]
+
+    def _get_all_endpoints_from_method(self, method):
+        return _METHOD_TO_ALL_INFO[method]
+
+    # CRUD API for RegisteredModel objects
+
+    def create_registered_model(self, name, tags=None, description=None):
+        """
+        Create a new registered model in backend store.
+
+        :param name: Name of the new model. This is expected to be unique in the backend store.
+        :param tags: A list of :py:class:`mlflow.entities.model_registry.RegisteredModelTag`
+                     instances associated with this registered model.
+        :param description: Description of the model.
+        :return: A single object of :py:class:`mlflow.entities.model_registry.RegisteredModel`
+                 created in the backend.
+        """
+        _require_arg_unspecified("tags", tags)
+        req_body = message_to_json(CreateRegisteredModelRequest(name=name, description=description))
+        response_proto = self._call_endpoint(CreateRegisteredModelRequest, req_body)
+        return registered_model_from_uc_proto(response_proto.registered_model)
+
+    def update_registered_model(self, name, description):
+        """
+        Update description of the registered model.
+
+        :param name: Registered model name.
+        :param description: New description.
+        :return: A single updated :py:class:`mlflow.entities.model_registry.RegisteredModel` object.
+        """
+        req_body = message_to_json(UpdateRegisteredModelRequest(name=name, description=description))
+        response_proto = self._call_endpoint(UpdateRegisteredModelRequest, req_body)
+        return registered_model_from_uc_proto(response_proto.registered_model)
+
+    def rename_registered_model(self, name, new_name):
+        """
+        Rename the registered model.
+
+        :param name: Registered model name.
+        :param new_name: New proposed name.
+        :return: A single updated :py:class:`mlflow.entities.model_registry.RegisteredModel` object.
+        """
+        _raise_unsupported_method(
+            method="rename_registered_model",
+            message="Use the Unity Catalog REST API to rename registered models",
+        )
+
+    def delete_registered_model(self, name):
+        """
+        Delete the registered model.
+        Backend raises exception if a registered model with given name does not exist.
+
+        :param name: Registered model name.
+        :return: None
+        """
+        req_body = message_to_json(DeleteRegisteredModelRequest(name=name))
+        self._call_endpoint(DeleteRegisteredModelRequest, req_body)
+
+    def search_registered_models(
+        self, filter_string=None, max_results=None, order_by=None, page_token=None
+    ):
+        """
+        Search for registered models in backend that satisfy the filter criteria.
+
+        :param filter_string: Filter query string, defaults to searching all registered models.
+        :param max_results: Maximum number of registered models desired.
+        :param order_by: List of column names with ASC|DESC annotation, to be used for ordering
+                         matching search results.
+        :param page_token: Token specifying the next page of results. It should be obtained from
+                            a ``search_registered_models`` call.
+        :return: A PagedList of :py:class:`mlflow.entities.model_registry.RegisteredModel` objects
+                that satisfy the search expressions. The pagination token for the next page can be
+                obtained via the ``token`` attribute of the object.
+        """
+        _require_arg_unspecified("filter_string", filter_string)
+        _require_arg_unspecified("order_by", order_by)
+        req_body = message_to_json(
+            SearchRegisteredModelsRequest(
+                max_results=max_results,
+                page_token=page_token,
+            )
+        )
+        response_proto = self._call_endpoint(SearchRegisteredModelsRequest, req_body)
+        registered_models = [
+            registered_model_from_uc_proto(registered_model)
+            for registered_model in response_proto.registered_models
+        ]
+        return PagedList(registered_models, response_proto.next_page_token)
+
+    def get_registered_model(self, name):
+        """
+        Get registered model instance by name.
+
+        :param name: Registered model name.
+        :return: A single :py:class:`mlflow.entities.model_registry.RegisteredModel` object.
+        """
+        req_body = message_to_json(GetRegisteredModelRequest(name=name))
+        response_proto = self._call_endpoint(GetRegisteredModelRequest, req_body)
+        return registered_model_from_uc_proto(response_proto.registered_model)
+
+    def get_latest_versions(self, name, stages=None):
+        """
+        Latest version models for each requested stage. If no ``stages`` argument is provided,
+        returns the latest version for each stage.
+
+        :param name: Registered model name.
+        :param stages: List of desired stages. If input list is None, return latest versions for
+                       each stage.
+        :return: List of :py:class:`mlflow.entities.model_registry.ModelVersion` objects.
+        """
+        _raise_unsupported_method(method="get_latest_versions")
+
+    def set_registered_model_tag(self, name, tag):
+        """
+        Set a tag for the registered model.
+
+        :param name: Registered model name.
+        :param tag: :py:class:`mlflow.entities.model_registry.RegisteredModelTag` instance to log.
+        :return: None
+        """
+        _raise_unsupported_method(method="set_registered_model_tag")
+
+    def delete_registered_model_tag(self, name, key):
+        """
+        Delete a tag associated with the registered model.
+
+        :param name: Registered model name.
+        :param key: Registered model tag key.
+        :return: None
+        """
+        _raise_unsupported_method(method="delete_registered_model_tag")
---
+++ mlflow/store/model_registry/base_rest_store.py
@@ -0,0 +1,42 @@
+from abc import ABCMeta, abstractmethod
+
+from mlflow.store.model_registry.abstract_store import AbstractStore
+from mlflow.utils.annotations import experimental
+from mlflow.utils.rest_utils import (
+    call_endpoint,
+    call_endpoints,
+)
+
+
+@experimental
+class BaseRestStore(AbstractStore):  # pylint: disable=abstract-method
+    """
+    Base class client for a remote model registry server accessed via REST API calls
+    """
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, get_host_creds):
+        super().__init__()
+        self.get_host_creds = get_host_creds
+
+    @abstractmethod
+    def _get_all_endpoints_from_method(self, method):
+        pass
+
+    @abstractmethod
+    def _get_endpoint_from_method(self, method):
+        pass
+
+    @abstractmethod
+    def _get_response_from_method(self, method):
+        pass
+
+    def _call_endpoint(self, api, json_body, call_all_endpoints=False):
+        response_proto = self._get_response_from_method(api)
+        if call_all_endpoints:
+            endpoints = self._get_all_endpoints_from_method(api)
+            return call_endpoints(self.get_host_creds(), endpoints, json_body, response_proto)
+        else:
+            endpoint, method = self._get_endpoint_from_method(api)
+            return call_endpoint(self.get_host_creds(), endpoint, method, json_body, response_proto)
