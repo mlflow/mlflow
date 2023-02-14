8ec241506372cf0047829e104bde676e8b50e0b4
harupy <17039389+harupy@users.noreply.github.com>
2023-02-14 12:24:23 +0900

    Migrate h2o, shap, and paddle flavors to cross-version tests
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- dev/run-python-flavor-tests.sh
+++ dev/run-python-flavor-tests.sh
@@ -6,8 +6,5 @@ export MLFLOW_HOME=$(pwd)
 # TODO: Run tests for h2o, shap, and paddle in the cross-version-tests workflow
 pytest \
   tests/utils/test_model_utils.py \
-  tests/h2o \
-  tests/shap \
-  tests/paddle \
   tests/tracking/fluent/test_fluent_autolog.py \
   tests/autologging
