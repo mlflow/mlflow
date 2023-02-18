fd28ec726ed45eb6f91c448b499a28a4a01c4b1b
Sid Murching <sid.murching@databricks.com>
2023-02-17 16:29:23 -0800

    Duplicates
    Signed-off-by: Sid Murching <sid.murching@databricks.com>

--- mlflow/store/_unity_catalog/registry/utils.py
+++ mlflow/store/_unity_catalog/registry/utils.py
@@ -11,30 +11,6 @@ from mlflow.store.artifact.artifact_repo import ArtifactRepository
 _STRING_TO_STATUS = {k: ProtoModelVersionStatus.Value(k) for k in ProtoModelVersionStatus.keys()}
 _STATUS_TO_STRING = {value: key for key, value in _STRING_TO_STATUS.items()}
 
-
-def uc_model_version_status_to_string(status):
-    return _STATUS_TO_STRING[status]
-
-
-def model_version_from_uc_proto(uc_proto: ProtoModelVersion) -> ModelVersion:
-    return ModelVersion(
-        name=uc_proto.name,
-        version=uc_proto.version,
-        creation_timestamp=uc_proto.creation_timestamp,
-        last_updated_timestamp=uc_proto.last_updated_timestamp,
-        description=uc_proto.description,
-        user_id=uc_proto.user_id,
-        source=uc_proto.source,
-        run_id=uc_proto.run_id,
-        status=uc_model_version_status_to_string(uc_proto.status),
-        status_message=uc_proto.status_message,
-    )
-
-
-_STRING_TO_STATUS = {k: ProtoModelVersionStatus.Value(k) for k in ProtoModelVersionStatus.keys()}
-_STATUS_TO_STRING = {value: key for key, value in _STRING_TO_STATUS.items()}
-
-
 def uc_model_version_status_to_string(status):
     return _STATUS_TO_STRING[status]
 
