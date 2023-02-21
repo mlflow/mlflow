7de78f993699841d62e1ee4d1728e058686474fa
harupy <hkawamura0130@gmail.com>
2023-02-21 17:35:29 +0900

    test
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/recipes/regression/v1/test_regression_pipeline_batch_scoring.py
+++ tests/recipes/regression/v1/test_regression_pipeline_batch_scoring.py
@@ -19,7 +19,7 @@ _STEP_NAMES = ["ingest_scoring", "predict"]
 
 # Because the Batch Scoring DAG takes a lot of time to run (around 5 minutes), we run the entire
 # DAG in this set up function and then assert various expected results in the tests.
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module", autouse=True)
 def run_batch_scoring():
     mlflow_repo_root_directory = pathlib.Path(mlflow.__file__).parent.parent
     recipe_example_path = mlflow_repo_root_directory / RECIPE_EXAMPLE_PATH_FROM_MLFLOW_ROOT
@@ -33,32 +33,30 @@ def run_batch_scoring():
         shutil.rmtree("./data/sample_output.parquet", ignore_errors=True)
 
 
-@pytest.mark.parametrize("_x", range(30))
-def test_recipe_batch_dag_get_artifacts(_x, run_batch_scoring):
+def test_recipe_batch_dag_get_artifacts(run_batch_scoring):
     r = run_batch_scoring
-    # assert isinstance(r.get_artifact("ingested_scoring_data"), pd.DataFrame)
+    assert isinstance(r.get_artifact("ingested_scoring_data"), pd.DataFrame)
     assert isinstance(r.get_artifact("scored_data"), pd.DataFrame)
-    r.clean("predict")
 
 
-# def test_recipe_batch_dag_execution_directories(enter_recipe_example_directory):
-#     expected_execution_directory_location = pathlib.Path(
-#         get_or_create_base_execution_directory(enter_recipe_example_directory)
-#     )
-#     for step_name in _STEP_NAMES:
-#         step_outputs_path = expected_execution_directory_location / "steps" / step_name / "outputs"
-#         assert step_outputs_path.exists()
-#         first_output = next(step_outputs_path.iterdir(), None)
-#         assert first_output is not None
+def test_recipe_batch_dag_execution_directories(enter_recipe_example_directory):
+    expected_execution_directory_location = pathlib.Path(
+        get_or_create_base_execution_directory(enter_recipe_example_directory)
+    )
+    for step_name in _STEP_NAMES:
+        step_outputs_path = expected_execution_directory_location / "steps" / step_name / "outputs"
+        assert step_outputs_path.exists()
+        first_output = next(step_outputs_path.iterdir(), None)
+        assert first_output is not None
 
 
 # This test should run last as it cleans the batch scoring steps
-# @pytest.mark.parametrize("_x", range(50))
-# def test_recipe_batch_dag_clean_step_works(_x, run_batch_scoring, enter_recipe_example_directory):
-#     r = run_batch_scoring
-#     r.clean("predict")
-#     expected_execution_directory_location = pathlib.Path(
-#         get_or_create_base_execution_directory(enter_recipe_example_directory)
-#     )
-#     step_outputs_path = expected_execution_directory_location / "steps" / "predict" / "outputs"
-#     assert not list(step_outputs_path.iterdir())
+@pytest.mark.parametrize("step", _STEP_NAMES)
+def test_recipe_batch_dag_clean_step_works(step, run_batch_scoring, enter_recipe_example_directory):
+    r = run_batch_scoring
+    r.clean(step)
+    expected_execution_directory_location = pathlib.Path(
+        get_or_create_base_execution_directory(enter_recipe_example_directory)
+    )
+    step_outputs_path = expected_execution_directory_location / "steps" / step / "outputs"
+    assert not list(step_outputs_path.iterdir())
