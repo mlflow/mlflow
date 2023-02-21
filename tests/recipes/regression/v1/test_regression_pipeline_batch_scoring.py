ae7f051b1e365b4f07cb4263bacb4c64d6377ee3
harupy <hkawamura0130@gmail.com>
2023-02-21 21:06:41 +0900

    Fix
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/recipes/regression/v1/test_regression_pipeline_batch_scoring.py
+++ tests/recipes/regression/v1/test_regression_pipeline_batch_scoring.py
@@ -54,15 +54,15 @@ def test_recipe_batch_dag_execution_directories(enter_recipe_example_directory):
 # This test should run last as it cleans the batch scoring steps
 @pytest.mark.parametrize("step", _STEP_NAMES)
 def test_recipe_batch_dag_clean_step_works(step, run_batch_scoring, enter_recipe_example_directory):
-    # On Windows, Running this test immediately after `test_recipe_batch_dag_get_artifacts`
-    # sometimes fails with an error that looks like the following:
+    # This test sometimes fails on Windows with an error that looks like the following:
     # ----------------------------------------------------------------------------------------------
     # PermissionError: [WinError 32] The process cannot access the file because it is being used by
     # another process: 'C:\\path\\to\\custom\\steps\\predict\\outputs\\scored.parquet\\part-00000-5
     # 4161f55-351d-4e90-bb18-7f754bfbd467-c000.snappy.parquet'
     # ----------------------------------------------------------------------------------------------
-    # To ensure all the file handles to the parquet files are released, sleep for 3 seconds.
-    time.sleep(3)
+    # To avoid this error, we sleep for 1 second to ensure all the handles to the parquet files are
+    # release before we try to delete them.
+    time.sleep(1)
     r = run_batch_scoring
     r.clean(step)
     expected_execution_directory_location = pathlib.Path(
