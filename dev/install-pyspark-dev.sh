1d1c047ebf02ddd94aea048d33e23a068c6b240a
harupy <17039389+harupy@users.noreply.github.com>
2023-02-22 15:16:48 +0900

    cd python
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- dev/install-pyspark-dev.sh
+++ dev/install-pyspark-dev.sh
@@ -4,5 +4,6 @@ temp_dir=$(mktemp -d)
 git clone --depth 1 --branch branch-3.4 https://github.com/apache/spark.git $temp_dir
 cd $temp_dir
 ./build/mvn -DskipTests --no-transfer-progress clean package
+cd python
 python setup.py bdist_wheel
 pip install dist/pyspark-*.whl
