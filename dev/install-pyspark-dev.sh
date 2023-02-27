e3332418dc61ab84d7aebafeacf0d705f95e0cb1
harupy <hkawamura0130@gmail.com>
2023-02-27 20:25:33 +0900

    rm
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- dev/install-pyspark-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-temp_dir=$(mktemp -d)
-git clone --depth 1 --branch branch-3.4 https://github.com/apache/spark.git $temp_dir
-cd $temp_dir
-./build/mvn -DskipTests --no-transfer-progress clean package
-cd python
-python setup.py bdist_wheel
-pip install dist/pyspark-*.whl
