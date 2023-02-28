4e60fc27a1446f17c5bfa0daadf7a6cd229fdba7
harupy <hkawamura0130@gmail.com>
2023-02-28 17:50:55 +0900

    Use runs URI
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/tracking/test_model_registry.py
+++ tests/tracking/test_model_registry.py
@@ -277,7 +277,7 @@ def test_search_model_versions_filter_string(
     for name in names + names[:10]:
         # Sleep for unique creation_time to make search results deterministic
         time.sleep(0.001)
-        mvs.append(client.create_model_version(name, "s3://path/to/model", "run_id"))
+        mvs.append(client.create_model_version(name, "runs:/run_id/model", "run_id"))
     for mv in mvs:
         assert isinstance(mv, ModelVersion)
     mvs = mvs[::-1]
@@ -313,7 +313,7 @@ def test_search_model_versions_max_results(client, max_results):
     for name in names + names[:10]:
         # Sleep for unique creation_time to make search results deterministic
         time.sleep(0.001)
-        mvs.append(client.create_model_version(name, "s3://path/to/model", "run_id"))
+        mvs.append(client.create_model_version(name, "runs:/run_id/model", "run_id"))
     for mv in mvs:
         assert isinstance(mv, ModelVersion)
     mvs = mvs[::-1]
@@ -363,7 +363,7 @@ def test_search_model_versions_order_by(
     for name in names + names[:10]:
         # Sleep for unique creation_time to make search results deterministic
         time.sleep(0.001)
-        mvs.append(client.create_model_version(name, "s3://path/to/model", "run_id"))
+        mvs.append(client.create_model_version(name, "runs:/run_id/model", "run_id"))
     for mv in mvs:
         assert isinstance(mv, ModelVersion)
     mvs = mvs[::-1]
@@ -397,7 +397,7 @@ def test_create_and_query_model_version_flow(client):
     name = "CreateMVTest"
     tags = {"key": "value", "another key": "some other value", "numeric value": 12345}
     client.create_registered_model(name)
-    mv1 = client.create_model_version(name, "s3://path/to/model", "run_id_1", tags)
+    mv1 = client.create_model_version(name, "runs:/run_id/model", "run_id_1", tags)
     assert mv1.version == "1"
     assert mv1.name == name
     assert mv1.tags == {"key": "value", "another key": "some other value", "numeric value": "12345"}
@@ -410,7 +410,7 @@ def test_create_and_query_model_version_flow(client):
     assert [rm.latest_versions for rm in client.search_registered_models() if rm.name == name] == [
         [mvd1]
     ]
-    mv2 = client.create_model_version(name, "s3://another_path/to/model", "run_id_1")
+    mv2 = client.create_model_version(name, "runs:/run_id/another_model", "run_id_1")
     assert mv2.version == "2"
     assert mv2.name == name
     mvd2 = client.get_model_version(name, "2")
@@ -421,18 +421,18 @@ def test_create_and_query_model_version_flow(client):
     assert {mv.version for mv in model_versions_by_name} == {"1", "2"}
     assert {mv.name for mv in model_versions_by_name} == {name}
 
-    mv3 = client.create_model_version(name, "s3://another_path/to/model", "run_id_2")
+    mv3 = client.create_model_version(name, "runs:/run_id/another_model", "run_id_2")
     assert mv3.version == "3"
-    assert client.search_model_versions("source_path = 's3://path/to/model'") == [mvd1]
+    assert client.search_model_versions("source_path = 'runs:/run_id/model'") == [mvd1]
     assert client.search_model_versions("run_id = 'run_id_1'") == [mvd2, mvd1]
 
-    assert client.get_model_version_download_uri(name, "1") == "s3://path/to/model"
+    assert client.get_model_version_download_uri(name, "1") == "runs:/run_id/model"
 
 
 def test_get_model_version(client):
     name = "GetModelVersionTest"
     client.create_registered_model(name)
-    client.create_model_version(name, "s3://path/to/model", "run_id_1")
+    client.create_model_version(name, "runs:/run_id/model", "run_id_1")
     model_version = client.get_model_version(name, "1")
     assert model_version.name == name
     assert model_version.version == "1"
@@ -453,7 +453,7 @@ def test_update_model_version_flow(client):
     assert_is_between(start_time_0, end_time_0, rmd1.last_updated_timestamp)
 
     start_time_1 = get_current_time_millis()
-    mv1 = client.create_model_version(name, "s3://path/to/model", "run_id_1")
+    mv1 = client.create_model_version(name, "runs:/run_id/model", "run_id_1")
     end_time_1 = get_current_time_millis()
     assert mv1.version == "1"
     assert mv1.name == name
@@ -470,7 +470,7 @@ def test_update_model_version_flow(client):
     assert [rm.latest_versions for rm in client.search_registered_models() if rm.name == name] == [
         [mvd1]
     ]
-    mv2 = client.create_model_version(name, "s3://another_path/to/model", "run_id_1")
+    mv2 = client.create_model_version(name, "runs:/run_id/another_model", "run_id_1")
     assert mv2.version == "2"
     assert mv2.name == name
     mvd2 = client.get_model_version(name, "2")
@@ -529,7 +529,7 @@ def test_latest_models(client):
     for version, stage in version_stage_mapping:
         # Sleep for unique creation_time to make search results deterministic
         time.sleep(0.001)
-        mv = client.create_model_version(name, "s3://path/to/model", "run_id")
+        mv = client.create_model_version(name, "runs:/run_id/model", "run_id")
         assert mv.version == version
         if stage != "None":
             client.transition_model_version_stage(name, version, stage=stage)
@@ -557,7 +557,7 @@ def test_delete_model_version_flow(client):
     assert_is_between(start_time_0, end_time_0, rmd1.last_updated_timestamp)
 
     start_time_1 = get_current_time_millis()
-    mv1 = client.create_model_version(name, "s3://path/to/model", "run_id_1")
+    mv1 = client.create_model_version(name, "runs:/run_id/model", "run_id_1")
     end_time_1 = get_current_time_millis()
     assert mv1.version == "1"
     assert mv1.name == name
@@ -570,10 +570,10 @@ def test_delete_model_version_flow(client):
     assert_is_between(start_time_0, end_time_0, rmd2.creation_timestamp)
     assert_is_between(start_time_1, end_time_1, rmd2.last_updated_timestamp)
 
-    mv2 = client.create_model_version(name, "s3://another_path/to/model", "run_id_1")
+    mv2 = client.create_model_version(name, "runs:/run_id/another_model", "run_id_1")
     assert mv2.version == "2"
     assert mv2.name == name
-    mv3 = client.create_model_version(name, "s3://a/b/c", "run_id_2")
+    mv3 = client.create_model_version(name, "runs:/run_id_2/a/b/c", "run_id_2")
     assert mv3.version == "3"
     assert mv3.name == name
     model_versions_detailed = [
@@ -613,7 +613,7 @@ def test_delete_model_version_flow(client):
     assert {mv.version for mv in client.search_model_versions("name = '%s'" % name)} == {"2"}
 
     # new model versions will not reuse existing version numbers
-    mv4 = client.create_model_version(name, "s3://a/b/c", "run_id_2")
+    mv4 = client.create_model_version(name, "runs:/run_id_2/a/b/c", "run_id_2")
     assert mv4.version == "4"
     assert mv4.name == name
     assert {mv.version for mv in client.search_model_versions("name = '%s'" % name)} == {
@@ -625,7 +625,7 @@ def test_delete_model_version_flow(client):
 def test_set_delete_model_version_tag_flow(client):
     name = "SetDeleteMVTagTest"
     client.create_registered_model(name)
-    client.create_model_version(name, "s3://path/to/model", "run_id_1")
+    client.create_model_version(name, "runs:/run_id/model", "run_id_1")
     model_version_detailed = client.get_model_version(name, "1")
     assert model_version_detailed.tags == {}
     tags = {"key": "value", "numeric value": 12345}
@@ -641,6 +641,6 @@ def test_set_delete_model_version_tag_flow(client):
 def test_set_model_version_tag_with_empty_string_as_value(client):
     name = "SetMVTagEmptyValueTest"
     client.create_registered_model(name)
-    client.create_model_version(name, "s3://path/to/model", "run_id_1")
+    client.create_model_version(name, "runs:/run_id/model", "run_id_1")
     client.set_model_version_tag(name, "1", "tag_key", "")
     assert {"tag_key": ""}.items() <= client.get_model_version(name, "1").tags.items()
