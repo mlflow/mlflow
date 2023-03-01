a94fc794ff1bd06466260dade1797af338df1d30
Arjun DCunha <arjun.dcunha@databricks.com>
2023-03-01 14:26:00 -0800

    oops

--- mlflow/models/wheeled_model.py
+++ mlflow/models/wheeled_model.py
@@ -204,10 +204,11 @@ class WheeledModel:
         )
 
         try:
-            subprocess.run([download_command], check=True, shell=True)
-        except Exception as e:
+            subprocess.run([download_command], check=True, shell=True, capture_output=True)
+        except subprocess.CalledProcessError as e:
             raise MlflowException(
-                "An error occurred while downloading the dependency wheels: {}".format(str(e))
+                "An error occurred while downloading the dependency wheels: "
+                "{}".format(str(e.stderr))
             )
 
     def _overwrite_pip_requirements_with_wheels(self, pip_requirements_path, wheels_dir):
