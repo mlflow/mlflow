f2abcf5d78c1c40746da8b8df9b8155c1f4b9aef
Justin <justinm088@hotmail.com>
2023-03-02 06:00:44 -0600

    [Server][Feature] add plugin to allow custom configs for mlflow.server (#7757)
    * [Server][Feature] add plugin to allow custom configs for mlflow.server
    
    Signed-off-by: Justin Mahlik <38999128+jmahlik@users.noreply.github.com>
    
    * chore: apply suggestions
    
    Co-authored-by: Harutaka Kawamura <hkawamura0130@gmail.com>
    Signed-off-by: Justin <justinm088@hotmail.com>
    
    * chore: update serverless plugin docs links
    
    Signed-off-by: Justin Mahlik <38999128+jmahlik@users.noreply.github.com>
    
    * chore: apply change from pr review
    
    Signed-off-by: Justin Mahlik <38999128+jmahlik@users.noreply.github.com>
    
    * chore: adjust example to return clean responses
    
    Signed-off-by: Justin Mahlik <38999128+jmahlik@users.noreply.github.com>
    
    * chore: fix server plugin test, can't import app twice
    
    Signed-off-by: Justin Mahlik <38999128+jmahlik@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Justin Mahlik <38999128+jmahlik@users.noreply.github.com>
    Signed-off-by: Justin <justinm088@hotmail.com>
    Co-authored-by: Harutaka Kawamura <hkawamura0130@gmail.com>

---
+++ tests/server/test_init.py
@@ -0,0 +1,65 @@
+from unittest import mock
+
+import entrypoints
+import pytest
+
+from mlflow.exceptions import MlflowException
+from mlflow import server
+
+
+@pytest.fixture()
+def mock_exec_cmd():
+    with mock.patch("mlflow.server._exec_cmd") as m:
+        yield m
+
+
+def test_get_app_name():
+    # Hard to test if the test plugin is installed.
+    with mock.patch("mlflow.server.entrypoints.get_group_all", return_value=[]):
+        assert server._get_app_name() == f"{server.__name__}:app"
+
+
+def test_get_app_name_two_plugins():
+    """Two server apps cannot exist, which one would be served?"""
+    plugins = [
+        entrypoints.EntryPoint("one", "one.app", "app"),
+        entrypoints.EntryPoint("two", "two.app", "two"),
+    ]
+    with mock.patch("mlflow.server.entrypoints.get_group_all", return_value=plugins):
+        with pytest.raises(MlflowException, match=r"one.*two"):
+            server._get_app_name()
+
+
+def test_get_app_name_custom_app_plugin():
+    """This test requires the package in tests/resources/mlflow-test-plugin to be installed"""
+    assert server._get_app_name() == "mlflow_test_plugin.app:custom_app"
+
+
+def test_build_waitress_command():
+    assert server._build_waitress_command("", "localhost", "5000", f"{server.__name__}:app") == [
+        "waitress-serve",
+        "--host=localhost",
+        "--port=5000",
+        "--ident=mlflow",
+        "mlflow.server:app",
+    ]
+
+
+def test_build_gunicorn_command():
+    assert server._build_gunicorn_command(
+        "", "localhost", "5000", "4", f"{server.__name__}:app"
+    ) == ["gunicorn", "-b", "localhost:5000", "-w", "4", "mlflow.server:app"]
+
+
+def test_run_server(mock_exec_cmd):
+    """Make sure this runs."""
+    with mock.patch("sys.platform", return_value="linux"):
+        server._run_server("", "", "", "", "", "", "", "")
+    mock_exec_cmd.assert_called_once()
+
+
+def test_run_server_win32(mock_exec_cmd):
+    """Make sure this runs."""
+    with mock.patch("sys.platform", return_value="win32"):
+        server._run_server("", "", "", "", "", "", "", "")
+    mock_exec_cmd.assert_called_once()
