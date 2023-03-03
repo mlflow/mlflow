e96df7e9925eca3096bd9ed28697de1ed49b001b
Benjamin Bluhm <40917599+benjaminbluhm@users.noreply.github.com>
2023-03-03 12:57:15 +0000

    Update example to revised wrapper interface
    Signed-off-by: Benjamin Bluhm <40917599+benjaminbluhm@users.noreply.github.com>

--- examples/sktime/train.py
+++ examples/sktime/train.py
@@ -1,5 +1,6 @@
 import mlflow
 import json
+import pandas as pd
 
 from sktime.datasets import load_longley
 from sktime.forecasting.model_selection import temporal_train_test_split
@@ -12,9 +13,8 @@ from sktime.performance_metrics.forecasting import (
 import flavor
 
 ARTIFACT_PATH = "model"
-COVERAGE = [0.8, 0.9]
 
-with mlflow.start_run():
+with mlflow.start_run() as run:
     y, X = load_longley()
     y_train, y_test, X_train, X_test = temporal_train_test_split(y, X)
 
@@ -25,21 +25,10 @@ with mlflow.start_run():
         fh=[1, 2, 3, 4],
     )
 
-    # create prediction configuration used in pyfunc flavor
-    pyfunc_predict_conf = {
-        "predict_method": {
-            "predict": {},
-            "predict_interval": {"coverage": COVERAGE},
-        }
-    }
-
-    # assign configuration dictionary to fitted model instance
-    forecaster.pyfunc_predict_conf = pyfunc_predict_conf
-
-    # extract parameters
+    # Extract parameters
     parameters = forecaster.get_params()
 
-    # evaluate model
+    # Evaluate model
     y_pred = forecaster.predict(X=X_test)
     metrics = {
         "mae": mean_absolute_error(y_test, y_pred),
@@ -49,20 +38,50 @@ with mlflow.start_run():
     print(f"Parameters: \n{json.dumps(parameters, indent=2)}")
     print(f"Metrics: \n{json.dumps(metrics, indent=2)}")
 
-    # log parameters and metrics
+    # Log parameters and metrics
     mlflow.log_params(parameters)
     mlflow.log_metrics(metrics)
 
-    # log model using custom model flavor
-    flavor.log_model(sktime_model=forecaster, artifact_path=ARTIFACT_PATH)
+    # Log model using custom model flavor with pickle serialization (default).
+    # Note that pickle serialization requires using the same python environment
+    # (version) in whatever environment you're going to use this model for
+    # inference to ensure that the model will load with appropriate version of
+    # pickle.
+    flavor.log_model(
+        sktime_model=forecaster,
+        artifact_path=ARTIFACT_PATH,
+        serialization_format="pickle",
+    )
     model_uri = mlflow.get_artifact_uri(ARTIFACT_PATH)
 
-# load model in native sktime flavor and pyfunc flavor
-loaded_model = flavor.load_model(model_uri)
-loaded_pyfunc = flavor.pyfunc.load_model(model_uri)
+# Load model in native sktime flavor and pyfunc flavor
+loaded_model = flavor.load_model(model_uri=model_uri)
+loaded_pyfunc = flavor.pyfunc.load_model(model_uri=model_uri)
 
-print(f"\nNative sktime 'predict':\n${loaded_model.predict(X=X_test)}")
-print(
-    f"\nNative sktime 'predict_interval':\n${loaded_model.predict_interval(X=X_test, coverage=COVERAGE)}"
+# Convert test data to 2D numpy array so it can be passed to pyfunc predict using
+# a single-row Pandas DataFrame configuration argument
+X_test_array = X_test.to_numpy()
+
+# Create configuration DataFrame for interval forecast with nominal coverage
+# value [0.9,0.95], future forecast horizon of 3 periods, and exogenous regressor.
+# Read more in the flavor.py module docstrings about the possible configurations.
+predict_conf = pd.DataFrame(
+    [
+        {
+            "fh": [1, 2, 3],
+            "predict_method": "predict_interval",
+            "coverage": [0.9, 0.95],
+            "X": X_test_array,
+        }
+    ]
 )
-print(f"\nPyfunc 'predict':\n${loaded_pyfunc.predict(X_test)}")
+
+# Generate interval forecasts with native sktime flavor and pyfunc flavor
+print(
+    f"\nNative sktime 'predict_interval':\n${loaded_model.predict_interval(fh=[1, 2, 3], X=X_test, coverage=[0.9, 0.95])}"
+)
+print(f"\nPyfunc 'predict_interval':\n${loaded_pyfunc.predict(predict_conf)}")
+
+# Print the run id wich is used for serving the model to a local REST API endpoint
+# in the request_prediction.py module
+print(f"\nMLflow run id:\n{run.info.run_id}")
