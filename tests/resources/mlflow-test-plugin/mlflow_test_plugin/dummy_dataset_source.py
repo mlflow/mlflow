027e665cdfe25753b5a6fe3de5e5c8d6fe475fdf
dbczumar <corey.zumar@databricks.com>
2023-03-11 00:57:50 -0800

    Dummy
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

---
+++ tests/resources/mlflow-test-plugin/mlflow_test_plugin/dummy_dataset_source.py
@@ -0,0 +1,69 @@
+import json
+
+from typing import TypeVar, Any
+from urllib.parse import urlparse
+
+from mlflow.artifacts import download_artifacts
+from mlflow.data.dataset_source import DatasetSource
+from mlflow.exceptions import MlflowException
+from mlflow.protos.databricks_pb2 import INVALID_PARAMETER_VALUE
+
+
+DBFSDatasetSourceType = TypeVar("DBFSDatasetSourceType", bound="DBFSDatasetSource")
+
+
+class DBFSDatasetSource(DatasetSource):
+
+    def __init__(self, uri):
+        self._uri = uri
+
+    @property
+    def uri(self):
+        return self._uri
+
+    @staticmethod
+    def _get_source_type() -> str:
+        return "dummy"
+
+    def download(self) -> str:
+        # Ignore the "dummy" URI scheme and download the local path
+        parsed_uri = urlparse(self._uri)
+        return download_artifacts(parsed_uri.path)
+
+    @staticmethod
+    def _can_resolve(raw_source: Any) -> bool:
+        if not isinstance(raw_source, str):
+            return False
+
+        try:
+            parsed_source = urlparse(raw_source)
+            return parsed_source.scheme == "dummy"
+        except Exception:
+            return False
+
+    @classmethod
+    def _resolve(cls, raw_source: Any) -> DBFSDatasetSourceType:
+        return cls(raw_source)
+
+    def to_json(self) -> str:
+        return json.dumps({
+            "uri": self.uri
+        })
+
+    @classmethod
+    def _from_json(cls, source_json: str) -> DBFSDatasetSourceType:
+        parsed_json = json.loads(source_json)
+        if not isinstance(parsed_json, dict):
+            raise MlflowException(
+                f"Failed to parse dummy dataset source from JSON. Expected a JSON dictionary, but received: {source_json}",
+                INVALID_PARAMETER_VALUE,
+            )
+
+        uri = parsed_json.get("uri")
+        if uri is None:
+            raise MlflowException(
+                f'Failed to parse dummy dataset source from JSON. Missing expected key: "uri"',
+                INVALID_PARAMETER_VALUE,
+            )
+
+        return cls(uri=uri)
