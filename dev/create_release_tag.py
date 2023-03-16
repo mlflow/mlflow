398bbe524104e8bfbe9dabadf19259051a8d6b95
Ben Wilson <benjamin.wilson@databricks.com>
2023-03-15 20:54:00 -0400

    adjust logic for dry release logic to prevent unfound branches
    Signed-off-by: Ben Wilson <benjamin.wilson@databricks.com>

--- dev/create_release_tag.py
+++ dev/create_release_tag.py
@@ -41,9 +41,16 @@ from packaging.version import Version
 def main(new_version: str, remote: str, dry_run: bool = False):
     version = Version(new_version)
     release_branch = f"branch-{version.major}.{version.minor}"
+    if dry_run:
+        subprocess.run(["git", "checkout", "-b", release_branch])
     release_tag = f"v{new_version}"
-    subprocess.check_call(["git", "tag", release_tag, release_branch])
-    subprocess.check_call(["git", "push", remote, release_tag, *(["--dry-run"] if dry_run else [])])
+    subprocess.run(["git", "tag", release_tag, release_branch], check=True)
+    subprocess.run(
+        ["git", "push", remote, release_tag, *(["--dry-run"] if dry_run else [])], check=True
+    )
+    if dry_run:
+        subprocess.run(["git", "checkout", "master"])
+        subprocess.run(["git", "branch", "-D", release_branch])
 
 
 if __name__ == "__main__":
