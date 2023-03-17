2cdec119fc891023b00af875771a0fc5bb4bc16f
dbczumar <corey.zumar@databricks.com>
2023-03-16 21:12:11 -0700

    Better docstrings
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/data/dataset.py
+++ mlflow/data/dataset.py
@@ -6,6 +6,12 @@ from mlflow.data.dataset_source import DatasetSource
 
 
 class Dataset:
+    """
+    Represents a dataset for use with MLflow Tracking, including the name, digest (hash),
+    schema, and profile of the dataset as well as source information (e.g. the S3 bucket or
+    managed Delta table from which the dataset was derived).
+    """
+
     def __init__(
         self, source: DatasetSource, name: Optional[str] = None, digest: Optional[str] = None
     ):
@@ -24,6 +30,9 @@ class Dataset:
         """
         Computes a digest for the dataset. Called if the user doesn't supply
         a digest when constructing the dataset.
+
+        :return: A string digest for the dataset. We recommend a maximum digest length
+                 of 10 characters with an ideal length of 8 characters.
         """
 
     @abstractmethod
@@ -49,25 +58,33 @@ class Dataset:
     @property
     def name(self) -> str:
         """
-        The name of the dataset. E.g. "myschema.mycatalog.mytable@2"
+        The name of the dataset.
+
+        :return: The name of the dataset. E.g. "myschema.mycatalog.mytable@2".
         """
         if self._name is not None:
             return self._name
         else:
             # TODO: Compute the name from the digest and source
-            return "dummy_name"
+            return "placeholder_name"
 
     @property
     def digest(self) -> str:
         """
-        The digest (hash) of the dataset, e.g. "498c74967f07246428783efd292cb9cc"
+        The digest (hash, fingerprint) of the dataset.
+
+        :return: The digest (hash, fingerprint) of the dataset,
+                 e.g. "498c74967f07246428783efd292cb9cc".
         """
         return self._digest
 
     @property
     def source(self) -> DatasetSource:
         """
-        Dataset source information
+        Dataset source information.
+
+        :return: A DatasetSource instance containing information about the dataset's source,
+                 e.g. the S3 bucket or managed Delta table from which the dataset was derived.
         """
         return self._source
 
@@ -75,13 +92,19 @@ class Dataset:
     @abstractmethod
     def profile(self) -> Optional[Any]:
         """
-        Summary statistics for the dataset, e.g. number of rows in a
-        table, mean / median / std of each column, etc.
+        Optional summary statistics for the dataset.
+
+        :return: Summary statistics for the dataset, e.g. number of rows in a
+                 table, mean / median / std of each column, etc. Returns None if no
+                 summary statistics are available for the dataset.
         """
 
     @property
     @abstractmethod
     def schema(self) -> Optional[Any]:
         """
-        Optional. The schema of the dataset, e.g. an instance of `mlflow.types.Schema`
+        Optional dataset schema.
+
+        :return: The schema of the dataset, e.g. an instance of `mlflow.types.Schema`. Returns None
+                 if no schema is defined for the dataset.
         """
