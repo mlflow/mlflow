c90dd126e006bd23a8a72e245fb33bf12ee6ea05
harupy <hkawamura0130@gmail.com>
2023-03-27 17:01:56 +0900

    Define empty before_request and after_request
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/server/auth/__init__.py
+++ mlflow/server/auth/__init__.py
@@ -15,16 +15,23 @@ from mlflow.server import app
 _logger = logging.getLogger(__name__)
 
 
-def authenticate():
+def before_request():
     # TODO: Implement authentication
+    # TODO: Implement authorization
     pass
+
+
+def after_request(resp):
+    # TODO: Implement post-request logic
+    return resp
 
 
 def enable_auth(app):
     _logger.warning(
         "This feature is still experimental and may change in a future release without warning"
     )
-    app.before_request(authenticate)
+    app.before_request(before_request)
+    app.after_request(after_request)
 
 
 enable_auth(app)
