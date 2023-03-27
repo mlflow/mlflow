1aa5497167e9ed371dd4ea1aa1255012d0772d24
harupy <hkawamura0130@gmail.com>
2023-03-27 16:51:59 +0900

    Fix
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/server/auth/__init__.py
+++ mlflow/server/auth/__init__.py
@@ -15,15 +15,16 @@ from mlflow.server import app
 _logger = logging.getLogger(__name__)
 
 
+def authenticate():
+    # TODO: Implement authentication
+    pass
+
+
 def enable_auth(app):
     _logger.warning(
         "This feature is still experimental and may change in a future release without warning"
     )
-
-    @app.before_request
-    def authenticate():
-        # TODO: Implement authentication
-        pass
+    app.before_request(authenticate)
 
 
 enable_auth(app)
