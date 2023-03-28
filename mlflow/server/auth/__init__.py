a29a513d38998063972ee0493141c875ef4dc716
gabrielfu <hfu.gabriel@gmail.com>
2023-03-28 17:59:55 +0800

    wip Signed-off-by: gabrielfu <hfu.gabriel@gmail.com>

--- mlflow/server/auth/__init__.py
+++ mlflow/server/auth/__init__.py
@@ -9,10 +9,12 @@ Usage
 
 import logging
 
-from flask import Flask, request
+from flask import Flask, request, make_response, Response
 
 from mlflow.server import app
+from mlflow.server.auth import store
 from mlflow.server.auth.config import app_config
+from mlflow.server.handlers import get_endpoints
 
 
 _logger = logging.getLogger(__name__)
@@ -21,19 +23,52 @@ _logger = logging.getLogger(__name__)
 PROTECTED_ROUTES = []
 
 
-def authenticate_user(username: str, password: str) -> bool:
+def is_protected_route(path: str) -> bool:
+    if path.startswith("/static-files"):
+        return True
+    return path in PROTECTED_ROUTES
+
+
+def make_forbidden_response() -> Response:
+    res = make_response("Permission denied")
+    res.status_code = 403
+    return res
+
+
+def make_invalid_login_response() -> Response:
+    res = make_response("Invalid username or password")
+    res.status_code = 403
+    return res
+
+
+def make_basic_auth_response() -> Response:
+    res = make_response()
+    res.status_code = 401
+    res.headers["WWW-Authenticate"] = 'Basic realm="mlflow"'
+    return res
+
+
+def validate_permission(path, username):
     # TODO
     pass
 
 
-def invalid_login():
-    # TODO
-    pass
-
-
-def verify_permission(path, username):
-    # TODO
-    pass
+def signup():
+    # TODO: add css
+    return """
+<form action="/users" method="post">
+  Username:
+  <br>
+  <input type=text name=username>
+  <br>
+  Password:
+  <br>
+  <input type=password name=password>
+  <br>
+  <br>
+  <input type="submit" value="Signup">
+</form>
+"""
 
 
 def _before_request():
@@ -41,19 +76,19 @@ def _before_request():
     # TODO: Implement authorization
     # TODO: remove
     _logger.info("before_request: %s %s %s", request.method, request.path, request.authorization)
-    if request.path not in PROTECTED_ROUTES:
+    if not is_protected_route(request.path):
         return
 
-    if request.authorization is not None:
-        ...
+    if request.authorization is None:
+        return make_basic_auth_response()
 
     username = request.authorization.username
     password = request.authorization.password
-    if not authenticate_user(username, password):
-        return invalid_login()
+    if not store.authenticate_user(username, password):
+        return make_invalid_login_response()
 
     # authorization
-    verify_permission(request.path, username)
+    validate_permission(request.path, username)
 
 
 def _after_request(resp):
