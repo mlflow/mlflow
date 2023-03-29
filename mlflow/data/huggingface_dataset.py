2da2929c836d128499e9a3978774c977b8ea6e20
dbczumar <corey.zumar@databricks.com>
2023-03-29 00:29:26 -0700

    Updates
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/data/huggingface_dataset.py
+++ mlflow/data/huggingface_dataset.py
@@ -6,13 +6,18 @@ from typing import Any, Union, Optional, Mapping, Sequence, Dict
 
 import datasets
 import numpy as np
+import pandas as pd
 
 from mlflow.data.dataset import Dataset
-from mlflow.data.huggingface_dataset_source import HuggingFaceDatasetSource 
+from mlflow.data.huggingface_dataset_source import HuggingFaceDatasetSource
+from mlflow.exceptions import MlflowException
+from mlflow.protos.databricks_pb2 import INVALID_PARAMETER_VALUE, RESOURCE_DOES_NOT_EXIST
 from mlflow.types import Schema
 from mlflow.types.utils import _infer_schema
 
 _logger = logging.getLogger(__name__)
+
+_MAX_ROWS_FOR_DIGEST_COMPUTATION_AND_SCHEMA_INFERENCE = 10000
 
 
 class HuggingFaceDataset(Dataset):
@@ -31,16 +36,30 @@ class HuggingFaceDataset(Dataset):
         Computes a digest for the dataset. Called if the user doesn't supply
         a digest when constructing the dataset.
         """
-        if hasattr(self._ds, "_fingerprint") and self._ds._fingerprint is not None:
-            return self._ds._fingerprint[:8]
-        else:
-            _logger.warning(
-                "The specified Hugging Face dataset does not have a _fingerprint attribute value."
-                " Falling back to Python object ID for the dataset digest, which may not be"
-                " consistent across different environments."
-            )
-            return hashlib.md5(np.int64(id(self._ds))).hexdigest()[:8]
-            
+
+        try:
+            df = next(self._ds.to_pandas(batch_size=_MAX_ROWS_FOR_DIGEST_COMPUTATION_AND_SCHEMA_INFERENCE, batched=True))
+        except Exception:
+            if hasattr(self._ds, "_fingerprint") and self._ds._fingerprint is not None:
+                return self._ds._fingerprint[:8]
+            else:
+                return hashlib.md5(np.int64(id(self._ds))).hexdigest()[:8]
+
+        
+        # drop object columns
+        df = df.select_dtypes(exclude=["object"])
+        # hash trimmed dataframe contents
+        md5 = hashlib.md5(pd.util.hash_pandas_object(df).values)
+        # hash dataframe dimensions
+        n_rows = len(df)
+        md5.update(np.int64(n_rows))
+        # hash column names
+        columns = df.columns
+        for x in columns:
+            md5.update(x.encode())
+        # TODO: Make this a normalize_hash function (truncation)
+        return md5.hexdigest()[:8]
+
     def _to_dict(self, base_dict: Dict[str, str]) -> Dict[str, str]:
         """
         :param base_dict: A string dictionary of base information about the
@@ -77,30 +96,39 @@ class HuggingFaceDataset(Dataset):
     @cached_property
     def schema(self) -> Schema:
         try:
-            return _infer_schema(self._ds.to_pandas())
+            df = next(self._ds.to_pandas(batch_size=_MAX_ROWS_FOR_DIGEST_COMPUTATION_AND_SCHEMA_INFERENCE, batched=True))
+            return _infer_schema(df)
         except Exception as e:
             _logger._warning("Failed to infer schema for Hugging Face dataset. Exception: %s", e)
             return None
 
 
-def from_huggingface_dataset(
+def from_huggingface(
     ds: datasets.Dataset,
+    path: str,
     data_dir: Optional[str] = None,
     data_files: Optional[
         Union[str, Sequence[str], Mapping[str, Union[str, Sequence[str]]]]
     ] = None,
     task: Optional[Union[str, datasets.TaskTemplate]] = None,
+    revision: Optional[Union[str, datasets.Version]] = None,
     name: Optional[str] = None,
     digest: Optional[str] = None,
 ) -> HuggingFaceDataset:
+    if not isinstance(ds, datasets.Dataset):
+        raise MlflowException(
+            f"The specified Hugging Face dataset must be an instance of `datasets.Dataset`."
+            f" Instead, found an instance of: {type(ds)}",
+            INVALID_PARAMETER_VALUE,
+        )
+
     source = HuggingFaceDatasetSource(
-        builder_name=ds.builder_name,
+        path=path,
         config_name=ds.config_name,
         data_dir=data_dir,
         data_files=data_files,
         split=ds.split,
-        features=ds.features,
-        revision=ds.version,
+        revision=revision,
         task=task,
     )
     return HuggingFaceDataset(ds=ds, source=source, name=name, digest=digest)
--- mlflow/data/huggingface_dataset_source.py
+++ mlflow/data/huggingface_dataset_source.py
@@ -20,23 +20,21 @@ HuggingFaceDatasetSourceType = TypeVar(
 class HuggingFaceDatasetSource(DatasetSource):
     def __init__(
         self,
-        builder_name: str,
+        path: str,
         config_name: Optional[str] = None,
         data_dir: Optional[str] = None,
         data_files: Optional[
             Union[str, Sequence[str], Mapping[str, Union[str, Sequence[str]]]]
         ] = None,
         split: Optional[Union[str, datasets.Split]] = None,
-        features: Optional[datasets.Features] = None,
         revision: Optional[Union[str, datasets.Version]] = None,
         task: Optional[Union[str, datasets.TaskTemplate]] = None,
     ):
-        self._builder_name = builder_name
+        self._path = path
         self._config_name = config_name
         self._data_dir = data_dir
         self._data_files = data_files
         self._split = split
-        self._features = features
         self._revision = revision
         self._task = task
 
@@ -53,29 +51,22 @@ class HuggingFaceDatasetSource(DatasetSource):
                        the Hugging Face `datasets.load_dataset()` method. The following keyword
                        arguments are used automatically from the dataset source but may be overriden
                        by values passed in **kwargs: path, name, data_dir, data_files, split,
-                       features, revision, task.
+                       revision, task.
         :throws: MlflowException if the Hugging Face dataset source does not define a path
                  from which to load the data.
         :return: An instance of `datasets.Dataset` or `datasets.DatasetDict`, depending on whether
                  multiple splits are defined by the source or not.
         """
-        if self._builder_name is not None:
-            raise MlflowException(
-                "Could not load Hugging Face dataset source because the source does not define"
-                " a dataset builder name (path).",
-                RESOURCE_DOES_NOT_EXIST,
-            )
-
         load_kwargs = {
-            "path": self._builder_name,
+            "path": self._path,
             "name": self._config_name,
             "data_dir": self._data_dir,
             "data_files": self._data_files,
             "split": self._split,
-            "features": self._features,
             "revision": self._revision,
             "task": self._task,
         }
+        print("LOAD KWARGS", load_kwargs)
         load_kwargs.update(kwargs)
 
         return datasets.load_dataset(**load_kwargs)
@@ -93,12 +84,11 @@ class HuggingFaceDatasetSource(DatasetSource):
 
     def _to_dict(self) -> Dict[str, Any]:
         return {
-            "builder_name": self._builder_name,
+            "path": self._path,
             "config_name": self._config_name,
             "data_dir": self._data_dir,
             "data_files": self._data_files,
-            "split": self._split,
-            "features": self._features,
+            "split": str(self._split),
             "revision": self._revision,
             "task": self._task,
         }
@@ -106,12 +96,11 @@ class HuggingFaceDatasetSource(DatasetSource):
     @classmethod
     def _from_dict(cls, source_dict: Dict[str, Any]) -> HuggingFaceDatasetSourceType:
         return cls(
-            builder_name=source_dict.get("builder_name"),
+            path=source_dict.get("path"),
             config_name=source_dict.get("config_name"),
             data_dir=source_dict.get("data_dir"),
             data_files=source_dict.get("data_files"),
             split=source_dict.get("split"),
-            features=source_dict.get("features"),
             revision=source_dict.get("revision"),
             task=source_dict.get("task"),
         )
