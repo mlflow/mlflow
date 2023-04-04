2fc7158a9d0d64a8351b23189e566eaa29461481
Prithvi Kannan <prithvi.kannan@databricks.com>
2023-04-04 13:08:09 -0700

    remove import
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- mlflow/data/numpy_dataset.py
+++ mlflow/data/numpy_dataset.py
@@ -8,7 +8,6 @@ from functools import cached_property
 from mlflow.data.dataset import Dataset
 from mlflow.data.dataset_source import DatasetSource
 from mlflow.data.digest_utils import compute_numpy_digest
-from mlflow.data.filesystem_dataset_source import FileSystemDatasetSource
 from mlflow.data.pyfunc_dataset_mixin import PyFuncConvertibleDatasetMixin, PyFuncInputsOutputs
 from mlflow.types import Schema
 from mlflow.types.utils import _infer_schema
