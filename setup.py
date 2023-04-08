c31da3b11ed27f60b5f9d92989d232d0e4abc8c9
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-04-08 09:07:36 +0900

    Remove dev suffix in `mlserver` requirements (#8192)
    * Remove dev prefix in mlserver requirements
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Default to []
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    ---------
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- setup.py
+++ setup.py
@@ -140,8 +140,8 @@ setup(
             # a remote Kubernetes cluster
             "kubernetes",
             # Required to serve models through MLServer
-            "mlserver>=1.2.0.dev13",
-            "mlserver-mlflow>=1.2.0.dev13",
+            "mlserver>=1.2.0",
+            "mlserver-mlflow>=1.2.0",
             "virtualenv",
             # Required for exporting metrics from the MLflow server to Prometheus
             # as part of the MLflow server monitoring add-on
