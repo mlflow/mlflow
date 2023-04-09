92edd869cacba7426f44b6d2b261031cbec11cd0
Liang Zhang <liang.zhang@databricks.com>
2023-04-09 15:42:12 -0700

    core save load log
    Signed-off-by: Liang Zhang <liang.zhang@databricks.com>

---
+++ mlflow/langchain.py
@@ -0,0 +1,325 @@
+"""
+The ``mlflow.langchain`` module provides an API for logging and loading LangChain models.
+This module exports multivariate LangChain models in the langchain flavor and univariate
+LangChain models in the pyfunc flavor:
+
+LangChain (native) format
+    This is the main flavor that can be accessed with LangChain APIs.
+:py:mod:`mlflow.pyfunc`
+    Produced for use by generic pyfunc-based deployment tools and for batch auditing
+    of historical forecasts.
+
+.. _LangChain:
+    https://python.langchain.com/en/latest/index.html
+"""
+import os
+import yaml
+
+import mlflow
+from mlflow import pyfunc
+from mlflow.utils.requirements_utils import _get_pinned_requirement
+from mlflow.utils.environment import (
+    _mlflow_conda_env,
+    _validate_env_arguments,
+    _process_pip_requirements,
+    _process_conda_env,
+    _CONDA_ENV_FILE_NAME,
+    _REQUIREMENTS_FILE_NAME,
+    _CONSTRAINTS_FILE_NAME,
+    _PYTHON_ENV_FILE_NAME,
+    _PythonEnv,
+)
+from mlflow.utils.file_utils import write_to
+from mlflow.utils.docstring_utils import format_docstring, LOG_MODEL_PARAM_DOCS
+from mlflow.models.signature import ModelSignature
+from mlflow.models.utils import _save_example
+from mlflow.models import Model, ModelInputExample
+from mlflow.tracking.artifact_utils import _download_artifact_from_uri
+from mlflow.utils.model_utils import (
+    _get_flavor_configuration,
+    _validate_and_copy_code_paths,
+    _add_code_from_conf_to_system_path,
+    _validate_and_prepare_target_save_path,
+)
+from mlflow.models.model import MLMODEL_FILE_NAME
+from mlflow.tracking._model_registry import DEFAULT_AWAIT_MAX_SLEEP_SECONDS
+
+FLAVOR_NAME = "langchain"
+_MODEL_DATA_FILE_NAME = "model.json"
+_MODEL_DATA_KEY = "model_data"
+_MODEL_TYPE_KEY = "model_type"
+
+
+def get_default_pip_requirements():
+    """
+    :return: A list of default pip requirements for MLflow Models produced by this flavor.
+             Calls to :func:`save_model()` and :func:`log_model()` produce a pip environment
+             that, at a minimum, contains these requirements.
+    """
+    return [_get_pinned_requirement("langchain")]
+
+
+def get_default_conda_env():
+    """
+    :return: The default Conda environment for MLflow Models produced by calls to
+             :func:`save_model()` and :func:`log_model()`.
+    """
+    return _mlflow_conda_env(additional_pip_deps=get_default_pip_requirements())
+
+
+@format_docstring(LOG_MODEL_PARAM_DOCS.format(package_name=FLAVOR_NAME))
+def save_model(
+    lc_model,
+    path,
+    conda_env=None,
+    code_paths=None,
+    mlflow_model=None,
+    signature: ModelSignature = None,
+    input_example: ModelInputExample = None,
+    pip_requirements=None,
+    extra_pip_requirements=None,
+    metadata=None,
+):
+    """
+    Save a LangChain model to a path on the local file system.
+
+    :param lc_model: LLMChain model (an instance of LangChain() forecaster that has been fit
+                     on a temporal series.
+    :param path: Local path where the serialized model (as JSON) is to be saved.
+    :param conda_env: {{ conda_env }}
+    :param code_paths: A list of local filesystem paths to Python file dependencies (or directories
+                       containing file dependencies). These files are *prepended* to the system
+                       path when the model is loaded.
+    :param mlflow_model: :py:mod:`mlflow.models.Model` this flavor is being added to.
+    :param signature: :py:class:`ModelSignature <mlflow.models.ModelSignature>`
+                      describes model input and output :py:class:`Schema <mlflow.types.Schema>`.
+                      The model signature can be :py:func:`inferred <mlflow.models.infer_signature>`
+                      from datasets with valid model input (e.g. the training dataset with target
+                      column omitted) and valid model output (e.g. model predictions generated on
+                      the training dataset), for example:
+
+                      .. code-block:: python
+
+                        from mlflow.models.signature import infer_signature
+
+                        model = LangChain().fit(df)
+                        train = model.history
+                        predictions = model.predict(model.make_future_dataframe(30))
+                        signature = infer_signature(train, predictions)
+    :param input_example: Input example provides one or several instances of valid
+                          model input. The example can be used as a hint of what data to feed the
+                          model. The given example will be converted to a Pandas DataFrame and then
+                          serialized to json using the Pandas split-oriented format. Bytes are
+                          base64-encoded.
+    :param pip_requirements: {{ pip_requirements }}
+    :param extra_pip_requirements: {{ extra_pip_requirements }}
+    :param metadata: Custom metadata dictionary passed to the model and stored in the MLmodel file.
+
+                     .. Note:: Experimental: This parameter may change or be removed in a future
+                                             release without warning.
+    """
+
+    import langchain
+
+    _validate_env_arguments(conda_env, pip_requirements, extra_pip_requirements)
+
+    path = os.path.abspath(path)
+    _validate_and_prepare_target_save_path(path)
+    code_dir_subpath = _validate_and_copy_code_paths(code_paths, path)
+
+    if mlflow_model is None:
+        mlflow_model = Model()
+    if signature is not None:
+        mlflow_model.signature = signature
+    if input_example is not None:
+        _save_example(mlflow_model, input_example, path)
+    if metadata is not None:
+        mlflow_model.metadata = metadata
+
+    model_data_path = os.path.join(path, _MODEL_DATA_FILE_NAME)
+    _save_model(lc_model, model_data_path)
+
+    model_data_kwargs = {_MODEL_DATA_KEY: _MODEL_DATA_FILE_NAME}
+    pyfunc.add_to_model(
+        mlflow_model,
+        loader_module="mlflow.langchain",
+        conda_env=_CONDA_ENV_FILE_NAME,
+        python_env=_PYTHON_ENV_FILE_NAME,
+        code=code_dir_subpath,
+        **model_data_kwargs,
+    )
+    flavor_conf = {
+        _MODEL_TYPE_KEY: lc_model.__class__.__name__,
+        **model_data_kwargs,
+    }
+    mlflow_model.add_flavor(
+        FLAVOR_NAME,
+        langchain_version=langchain.__version__,
+        code=code_dir_subpath,
+        **flavor_conf,
+    )
+    mlflow_model.save(os.path.join(path, MLMODEL_FILE_NAME))
+
+    if conda_env is None:
+        if pip_requirements is None:
+            default_reqs = get_default_pip_requirements()
+            inferred_reqs = mlflow.models.infer_pip_requirements(
+                str(path), FLAVOR_NAME, fallback=default_reqs
+            )
+            default_reqs = sorted(set(inferred_reqs).union(default_reqs))
+        else:
+            default_reqs = None
+        conda_env, pip_requirements, pip_constraints = _process_pip_requirements(
+            default_reqs, pip_requirements, extra_pip_requirements
+        )
+    else:
+        conda_env, pip_requirements, pip_constraints = _process_conda_env(conda_env)
+
+    with open(os.path.join(path, _CONDA_ENV_FILE_NAME), "w") as f:
+        yaml.safe_dump(conda_env, stream=f, default_flow_style=False)
+
+    if pip_constraints:
+        write_to(os.path.join(path, _CONSTRAINTS_FILE_NAME), "\n".join(pip_constraints))
+
+    write_to(os.path.join(path, _REQUIREMENTS_FILE_NAME), "\n".join(pip_requirements))
+
+    _PythonEnv.current().to_yaml(os.path.join(path, _PYTHON_ENV_FILE_NAME))
+
+
+@format_docstring(LOG_MODEL_PARAM_DOCS.format(package_name=FLAVOR_NAME))
+def log_model(
+    lc_model,
+    artifact_path,
+    conda_env=None,
+    code_paths=None,
+    registered_model_name=None,
+    signature: ModelSignature = None,
+    input_example: ModelInputExample = None,
+    await_registration_for=DEFAULT_AWAIT_MAX_SLEEP_SECONDS,
+    pip_requirements=None,
+    extra_pip_requirements=None,
+    metadata=None,
+):
+    """
+    Log a LangChain model as an MLflow artifact for the current run.
+
+    :param pr_model: LangChain model to be saved.
+    :param artifact_path: Run-relative artifact path.
+    :param conda_env: {{ conda_env }}
+    :param code_paths: A list of local filesystem paths to Python file dependencies (or directories
+                       containing file dependencies). These files are *prepended* to the system
+                       path when the model is loaded.
+
+    :param registered_model_name: This argument may change or be removed in a
+                                  future release without warning. If given, create a model
+                                  version under ``registered_model_name``, also creating a
+                                  registered model if one with the given name does not exist.
+    :param signature: :py:class:`ModelSignature <mlflow.models.ModelSignature>`
+                      describes model input and output
+                      :py:class:`Schema <mlflow.types.Schema>`.
+                      The model signature can be :py:func:`inferred
+                      <mlflow.models.infer_signature>` from datasets with valid model input
+                      (e.g. the training dataset with target column omitted) and valid model
+                      output (e.g. model predictions generated on the training dataset),
+                      for example:
+
+                      .. code-block:: python
+
+                        from mlflow.models.signature import infer_signature
+
+                        model = LangChain().fit(df)
+                        train = model.history
+                        predictions = model.predict(model.make_future_dataframe(30))
+                        signature = infer_signature(train, predictions)
+
+    :param input_example: Input example provides one or several instances of valid
+                          model input. The example can be used as a hint of what data to
+                          feed the model. The given example will be converted to a
+                          Pandas DataFrame and then serialized to json using the
+                          Pandas split-oriented format. Bytes are base64-encoded.
+
+    :param await_registration_for: Number of seconds to wait for the model version
+                        to finish being created and is in ``READY`` status.
+                        By default, the function waits for five minutes.
+                        Specify 0 or None to skip waiting.
+    :param pip_requirements: {{ pip_requirements }}
+    :param extra_pip_requirements: {{ extra_pip_requirements }}
+    :param metadata: Custom metadata dictionary passed to the model and stored in the MLmodel file.
+
+                     .. Note:: Experimental: This parameter may change or be removed in a future
+                                             release without warning.
+    :return: A :py:class:`ModelInfo <mlflow.models.model.ModelInfo>` instance that contains the
+             metadata of the logged model.
+    """
+    return Model.log(
+        artifact_path=artifact_path,
+        flavor=mlflow.langchain,
+        registered_model_name=registered_model_name,
+        lc_model=lc_model,
+        conda_env=conda_env,
+        code_paths=code_paths,
+        signature=signature,
+        input_example=input_example,
+        await_registration_for=await_registration_for,
+        pip_requirements=pip_requirements,
+        extra_pip_requirements=extra_pip_requirements,
+        metadata=metadata,
+    )
+
+
+def _save_model(model, path):
+    model.save(path)
+
+
+def _load_model(path):
+    from langchain.chains.loading import load_chain
+
+    model = load_chain(path)
+    return model
+
+
+def _load_pyfunc(path):
+    """
+    Load PyFunc implementation for LangChain. Called by ``pyfunc.load_model``.
+    :param path: Local filesystem path to the MLflow Model with the ``langchain`` flavor.
+    """
+    return _LangChainModelWrapper(_load_model(path))
+
+
+def load_model(model_uri, dst_path=None):
+    """
+    Load a LangChain model from a local file or a run.
+
+    :param model_uri: The location, in URI format, of the MLflow model. For example:
+
+                      - ``/Users/me/path/to/local/model``
+                      - ``relative/path/to/local/model``
+                      - ``s3://my_bucket/path/to/model``
+                      - ``runs:/<mlflow_run_id>/run-relative/path/to/model``
+
+                      For more information about supported URI schemes, see
+                      `Referencing Artifacts <https://www.mlflow.org/docs/latest/tracking.html#
+                      artifact-locations>`_.
+    :param dst_path: The local filesystem path to which to download the model artifact.
+                     This directory must already exist. If unspecified, a local output
+                     path will be created.
+
+    :return: A LangChain model instance
+    """
+    local_model_path = _download_artifact_from_uri(artifact_uri=model_uri, output_path=dst_path)
+    flavor_conf = _get_flavor_configuration(model_path=local_model_path, flavor_name=FLAVOR_NAME)
+    _add_code_from_conf_to_system_path(local_model_path, flavor_conf)
+    lc_model_path = os.path.join(
+        local_model_path, flavor_conf.get(_MODEL_DATA_KEY, _MODEL_DATA_FILE_NAME)
+    )
+
+    return _load_model(lc_model_path)
+
+
+class _LangChainModelWrapper:
+    def __init__(self, lc_model):
+        self.lc_model = lc_model
+
+    def predict(self, dataframe):
+        # TODO
+        return self.lc_model.run(dataframe)
---
+++ tests/langchain/fake_llm.py
@@ -0,0 +1,28 @@
+"""Fake LLM wrapper for testing purposes."""
+from typing import Any, List, Mapping, Optional
+
+from langchain.llms.base import LLM
+
+
+class FakeLLM(LLM):
+    """Fake LLM wrapper for testing purposes."""
+
+    queries: Optional[Mapping] = None
+
+    @property
+    def _llm_type(self) -> str:
+        """Return type of llm."""
+        return "fake"
+
+    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        """First try to lookup in queries, else return 'foo' or 'bar'."""
+        if self.queries is not None:
+            return self.queries[prompt]
+        if stop is None:
+            return "foo"
+        else:
+            return "bar"
+
+    @property
+    def _identifying_params(self) -> Mapping[str, Any]:
+        return {}
