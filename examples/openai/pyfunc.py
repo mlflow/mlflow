4077fe289e29722f459478d09dd118e878ac5720
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-04-17 14:50:10 +0900

    Add `openai` flavor in `model.rst` (#8242)
    * Add openai in model.rst
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Add MLFLOW_OPENAI_SECRET_SCOPE
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    ---------
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- examples/openai/pyfunc.py
+++ examples/openai/pyfunc.py
@@ -1,9 +1,17 @@
+import os
+import logging
+
 import openai
 import mlflow
 import pandas as pd
 import logging
 
 logging.getLogger("mlflow").setLevel(logging.DEBUG)
+
+# On Databricks, set the stored OpenAI API key scope here for automatically loading the API key
+# for real time inference. See https://docs.databricks.com/security/secrets/index.html on
+# how to add a scope and API key.
+os.environ["MLFLOW_OPENAI_SECRET_SCOPE"] = "<scope-name>"
 
 with mlflow.start_run():
     model_info = mlflow.openai.log_model(
@@ -12,6 +20,14 @@ with mlflow.start_run():
         artifact_path="model",
         messages=[{"role": "system", "content": "You are an MLflow expert!"}],
     )
+
+
+print(mlflow.openai.load_model(model_info.model_uri))
+# {
+#     "messages": [{"content": "You are an MLflow expert!", "role": "system"}],
+#     "model": "gpt-3.5-turbo",
+#     "task": "chat.completions",
+# }
 
 df = pd.DataFrame(
     {
