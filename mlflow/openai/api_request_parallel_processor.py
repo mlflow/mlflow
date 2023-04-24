c63cc7b693574b73f380c6d0b84e410812eb9ef7
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-04-24 11:38:28 +0900

    Fix openai flavor impelementation to support formatting messages with prediction input (#8291)
    * Fix openai implementation
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * default value
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * remove default
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * rename
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * reuse _has_content_and_role
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * rename key
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * improve error message
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Address comments
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Address comments
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Fix spark UDF example
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Doc fixes
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Reorganize docs
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    ---------
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/openai/api_request_parallel_processor.py
+++ mlflow/openai/api_request_parallel_processor.py
@@ -208,8 +208,11 @@ def process_api_requests(
     last_update_time = time.time()
     results: list[tuple[int, OpenAIObject]] = []
     requests_iter = enumerate(requests)
+    last_index = len(requests) - 1
+    requests_exhausted = False
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         while True:
+            time.sleep(0.001)
             # get next request (if one is not already waiting for capacity)
             if next_request is None:
                 if not retry_queue.empty():
@@ -228,6 +231,7 @@ def process_api_requests(
                         results=results,
                     )
                     status_tracker.start_task()
+                    requests_exhausted = index == last_index
 
             # update available capacity
             current_time = time.time()
@@ -265,7 +269,7 @@ def process_api_requests(
                     next_request = None  # reset next_request to empty
 
             # if all tasks are finished, break
-            if status_tracker.num_tasks_in_progress == 0:
+            if requests_exhausted and status_tracker.num_tasks_in_progress == 0:
                 break
 
             # if a rate limit error was hit recently, pause to cool down
@@ -283,15 +287,15 @@ def process_api_requests(
                 time.sleep(remaining_seconds_to_pause)
                 # ^e.g., if pause is 15 seconds and final limit was hit 5 seconds ago
 
-        # after finishing, log final status
-        if status_tracker.num_tasks_failed > 0:
-            raise mlflow.MlflowException(
-                f"{status_tracker.num_tasks_failed} tasks failed. See logs for details."
-            )
-        if status_tracker.num_rate_limit_errors > 0:
-            _logger.warning(
-                f"{status_tracker.num_rate_limit_errors} rate limit errors received. "
-                "Consider running at a lower rate."
-            )
+    # after finishing, log final status
+    if status_tracker.num_tasks_failed > 0:
+        raise mlflow.MlflowException(
+            f"{status_tracker.num_tasks_failed} tasks failed. See logs for details."
+        )
+    if status_tracker.num_rate_limit_errors > 0:
+        _logger.warning(
+            f"{status_tracker.num_rate_limit_errors} rate limit errors received. "
+            "Consider running at a lower rate."
+        )
 
-        return [res for _, res in sorted(results)]
+    return [res for _, res in sorted(results)]
