db20dbda08e566ef4d812fea1608dbf864c832c1
Corey Zumar <39497902+dbczumar@users.noreply.github.com>
2023-04-25 20:55:19 -0700

    [Feature branch] Small improvements to dataset sources (#8300)
    * Patches
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Make dataset sources importable
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Test
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Remove protocol
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Cherry pick
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Fix entrypoint loading error and revert dummy dataset
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Comment
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Fix typo
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * DS
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Fix attempt
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Fix silliness
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    ---------
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/data/__init__.py
+++ mlflow/data/__init__.py
@@ -1,6 +1,8 @@
 import sys
 
+from . import sources as mlflow_data_sources
 from mlflow.data import dataset_registry
+from mlflow.data import dataset_source_registry
 
 
 __all__ = []
@@ -17,3 +19,12 @@ def _define_dataset_constructors_in_current_module():
 
 
 _define_dataset_constructors_in_current_module()
+
+
+def _define_dataset_sources_in_sources_module():
+    for source in dataset_source_registry.get_registered_sources():
+        setattr(mlflow_data_sources, source.__name__, source)
+        mlflow_data_sources.__all__.append(source.__name__)
+
+
+_define_dataset_sources_in_sources_module()
