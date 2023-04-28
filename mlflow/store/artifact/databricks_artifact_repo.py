db7d6a4767693a7a14a234503e520487399199ea
dbczumar <corey.zumar@databricks.com>
2023-04-27 22:18:14 -0700

    100mb chunks
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/store/artifact/databricks_artifact_repo.py
+++ mlflow/store/artifact/databricks_artifact_repo.py
@@ -62,7 +62,7 @@ from mlflow.utils.uri import (
 )
 
 _logger = logging.getLogger(__name__)
-_DOWNLOAD_CHUNK_SIZE = 10_000_000
+_DOWNLOAD_CHUNK_SIZE = 100_000_000  # 100 MB
 _MULTIPART_UPLOAD_CHUNK_SIZE = 10_000_000  # 10 MB
 _MAX_CREDENTIALS_REQUEST_SIZE = 2000  # Max number of artifact paths in a single credentials request
 _SERVICE_AND_METHOD_TO_INFO = {
