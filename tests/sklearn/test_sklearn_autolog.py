78a54ccbd138f3013bf79a844623e693241818a7
Serena Ruan <82044803+serena-ruan@users.noreply.github.com>
2023-05-19 20:34:22 +0800

    fix: update SCORER usage sklearn version > 1.2.2 (#8466)
    * update scorer for sklearn version
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    * fix one failed test using SCORER
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    * fix test
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    * fix test
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    ---------
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- tests/sklearn/test_sklearn_autolog.py
+++ tests/sklearn/test_sklearn_autolog.py
@@ -1262,7 +1262,8 @@ def test_basic_post_training_metric_autologging():
         scorer1 = sklmetrics.make_scorer(sklmetrics.recall_score, average="micro")
         recall_score3_data2 = scorer1(model, eval2_X, eval2_y)
 
-        recall_score4_data2 = sklearn.metrics.SCORERS["recall_macro"](model, eval2_X, eval2_y)
+        scorer2 = sklmetrics.make_scorer(sklmetrics.recall_score, average="macro")
+        recall_score4_data2 = scorer2(model, eval2_X, eval2_y)
 
         eval1_X, eval1_y = eval1_X.copy(), eval1_y.copy()
         # In metric key, it will include dataset name as "eval1_X-2"
@@ -1290,9 +1291,7 @@ def test_basic_post_training_metric_autologging():
     }
 
     lor_score_3_cmd = "LogisticRegression.score(X=<ndarray>, y=<ndarray>)"
-    recall_score4_eval2_X_cmd = (
-        "recall_score(y_true=eval2_y, y_pred=y_pred, pos_label=None, average='macro')"
-    )
+    recall_score4_eval2_X_cmd = "recall_score(y_true=eval2_y, y_pred=y_pred, average='macro')"
     assert metric_info == {
         "LogisticRegression_score-2_eval1_X-2": "LogisticRegression.score(X=eval1_X, y=eval1_y)",
         "LogisticRegression_score-3_unknown_dataset": lor_score_3_cmd,
