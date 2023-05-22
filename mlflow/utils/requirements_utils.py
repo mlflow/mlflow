a0bd5906664e67ac7d7afcc17615e2d25d978614
Weichen Xu <weichen.xu@databricks.com>
2023-05-22 16:25:47 +0800

    fix lint
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/utils/requirements_utils.py
+++ mlflow/utils/requirements_utils.py
@@ -444,8 +444,7 @@ def _get_pinned_requirement(package, version=None, module=None):
         version_raw = _get_installed_version(package, module)
         local_version_label = _get_local_version_label(version_raw)
         if local_version_label and not (
-            is_in_databricks_runtime() and
-            package in ("torch", "torchvision")
+            is_in_databricks_runtime() and package in ("torch", "torchvision")
         ):
             version = _strip_local_version_label(version_raw)
             msg = (
