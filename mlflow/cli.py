8be55c6931b95b520b54323f549f4bde8a721e67
harupy <hkawamura0130@gmail.com>
2023-05-23 20:23:36 +0900

    Add --dev option to mlflow server
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/cli.py
+++ mlflow/cli.py
@@ -358,6 +358,17 @@ def _validate_static_prefix(ctx, param, value):  # pylint: disable=unused-argume
         "If not specified, 'mlflow.server:app' will be used."
     ),
 )
+@click.option(
+    "--dev",
+    is_flag=True,
+    default=False,
+    show_default=True,
+    help=(
+        "If enabled, run the server with debug logging and auto-reload. "
+        "Should only be used for development purposes. "
+        "Cannot be used with --gunicorn-opts."
+    ),
+)
 def server(
     backend_store_uri,
     registry_store_uri,
@@ -373,6 +384,7 @@ def server(
     waitress_opts,
     expose_prometheus,
     app_name,
+    dev,
 ):
     """
     Run the MLflow tracking server.
@@ -385,6 +397,10 @@ def server(
     from mlflow.server import _run_server
     from mlflow.server.handlers import initialize_backend_stores
 
+    if dev and gunicorn_opts:
+        raise click.UsageError("'--dev' and '--gunicorn-opts' cannot be specified together.")
+
+    gunicorn_opts = "--log-level debug --reload" if dev else gunicorn_opts
     _validate_server_args(gunicorn_opts=gunicorn_opts, workers=workers, waitress_opts=waitress_opts)
 
     # Ensure that both backend_store_uri and default_artifact_uri are set correctly.
