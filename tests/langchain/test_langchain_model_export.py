6edd240059c2f791419102ed651dc0fae62c28aa
Liang Zhang <liang.zhang@databricks.com>
2023-05-23 02:26:58 -0700

    checkpoint
    Signed-off-by: Liang Zhang <liang.zhang@databricks.com>

--- tests/langchain/test_langchain_model_export.py
+++ tests/langchain/test_langchain_model_export.py
@@ -5,7 +5,7 @@ import transformers
 import json
 
 from contextlib import contextmanager
-from langchain.chains import LLMChain, ConversationChain
+from langchain.chains import ConversationChain, LLMChain, SimpleSequentialChain
 from langchain.chains.base import Chain
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.evaluation.qa import QAEvalChain
@@ -79,6 +79,32 @@ def create_qa_eval_chain():
 def create_qa_with_sources_chain():
     # StuffDocumentsChain
     return load_qa_with_sources_chain(OpenAI(temperature=0), chain_type="stuff")
+
+
+def create_simple_sequential_chain():
+    # This is an LLMChain to write a synopsis given a title of a play.
+    llm = OpenAI(temperature=0.7)
+    template = """You are a playwright. Given the title of play, it is your job to 
+    write a synopsis for that title.
+
+    Title: {title}
+    Playwright: This is a synopsis for the above play:"""
+    prompt_template = PromptTemplate(input_variables=["title"], template=template)
+    synopsis_chain = LLMChain(llm=llm, prompt=prompt_template)
+
+    # This is an LLMChain to write a review of a play given a synopsis.
+    llm = OpenAI(temperature=0.7)
+    template = """You are a play critic from the New York Times. Given the synopsis 
+    of play, it is your job to write a review for that play.
+
+    Play Synopsis:
+    {synopsis}
+    Review from a New York Times play critic of the above play:"""
+    prompt_template = PromptTemplate(input_variables=["synopsis"], template=template)
+    review_chain = LLMChain(llm=llm, prompt=prompt_template)
+
+    # This is the overall chain where we run these two chains in sequence.
+    return SimpleSequentialChain(chains=[synopsis_chain, review_chain], verbose=True)
 
 
 def create_openai_llmagent():
@@ -192,6 +218,16 @@ def test_langchain_model_predict():
             logged_model = mlflow.langchain.log_model(model, "langchain_model")
         loaded_model = mlflow.pyfunc.load_model(logged_model.model_uri)
         result = loaded_model.predict([{"product": "MLflow"}])
+        assert result == [TEST_CONTENT]
+
+
+def test_simple_sequential_chain_predict():
+    with _mock_request(return_value=_mock_chat_completion_response()):
+        model = create_simple_sequential_chain()
+        with mlflow.start_run():
+            logged_model = mlflow.langchain.log_model(model, "langchain_model")
+        loaded_model = mlflow.pyfunc.load_model(logged_model.model_uri)
+        result = loaded_model.predict([{"title": "Tragedy at sunset on the beach"}])
         assert result == [TEST_CONTENT]
 
 
