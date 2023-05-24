b3c62c0e9ef883b2156a3829438def9824963bb4
Gabriel Fu <hfu.gabriel@gmail.com>
2023-05-24 18:20:21 +0800

    test wip Signed-off-by: Gabriel Fu <hfu.gabriel@gmail.com>

---
+++ tests/server/auth/test_client.py
@@ -0,0 +1,40 @@
+import pytest
+from unittest import mock
+
+from mlflow.server.auth.client import AuthServiceClient
+from tests.helper_functions import LOCALHOST, get_safe_port
+
+
+@pytest.fixture
+def client():
+    server_port = get_safe_port()
+    url = f"http://{LOCALHOST}:{server_port}"
+    yield AuthServiceClient(url)
+
+
+@pytest.fixture
+def mock_session():
+    with mock.patch(
+        "mlflow.utils.rest_utils._get_request_session"
+    ) as mock_session:
+        yield mock_session.return_value
+
+
+def test_client_create_run(client, mock_session):
+    experiment_id = mock.Mock()
+    username = mock.Mock()
+    permission = mock.Mock()
+    try:
+        client.create_experiment_permission(experiment_id, username, permission)
+    except Exception:
+        pass
+
+    call_args = mock_session.request.call_args
+    assert call_args.args[0] == "POST"
+    assert call_args.args[1] == f"{client.tracking_uri}/api/2.0/mlflow/experiments/permissions/create"
+    assert call_args.kwargs["json"] == {
+            "experiment_id": experiment_id,
+            "username": username,
+            "permission": permission,
+        }
+
