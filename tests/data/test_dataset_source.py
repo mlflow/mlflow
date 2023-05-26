9f8c83066ed7aa92feb08590605b5b22167fcb24
Prithvi Kannan <prithvi.kannan@databricks.com>
2023-05-26 11:17:07 -0700

    Add CodeDatasetSource to dataset_source_registry
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- tests/data/test_dataset_source.py
+++ tests/data/test_dataset_source.py
@@ -22,7 +22,7 @@ def test_conversion_to_json_and_back():
     assert reloaded_source.uri == source.uri
 
 
-def test_get_source_obtains_expected_source(tmp_path):
+def test_get_source_obtains_expected_file_source(tmp_path):
     df = pd.DataFrame([[1, 2, 3], [1, 2, 3]], columns=["a", "b", "c"])
     path = tmp_path / "temp.csv"
     df.to_csv(path)
@@ -45,6 +45,27 @@ def test_get_source_obtains_expected_source(tmp_path):
     assert json.loads(source3.to_json()) == json.loads(pandas_ds.source.to_json())
 
     assert source1.load() == source2.load() == source3.load() == str(path)
+
+
+def test_get_source_obtains_expected_code_source():
+    df = pd.DataFrame([[1, 2, 3], [1, 2, 3]], columns=["a", "b", "c"])
+    pandas_ds = mlflow.data.from_pandas(df)
+
+    source1 = mlflow.data.get_source(pandas_ds)
+    assert json.loads(source1.to_json()) == json.loads(pandas_ds.source.to_json())
+
+    with mlflow.start_run() as r:
+        mlflow.log_input(pandas_ds)
+
+    run = mlflow.get_run(r.info.run_id)
+
+    ds_input = run.inputs.dataset_inputs[0]
+    source2 = mlflow.data.get_source(ds_input)
+    assert json.loads(source2.to_json()) == json.loads(pandas_ds.source.to_json())
+
+    ds_entity = run.inputs.dataset_inputs[0].dataset
+    source3 = mlflow.data.get_source(ds_entity)
+    assert json.loads(source3.to_json()) == json.loads(pandas_ds.source.to_json())
 
 
 def test_get_source_throws_for_invalid_input(tmp_path):
