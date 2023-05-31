c4fe982bc9576f319687e7db1f3b0d24317befdd
Prithvi Kannan <prithvi.kannan@databricks.com>
2023-05-31 16:44:06 -0700

    docs format
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- mlflow/tracking/fluent.py
+++ mlflow/tracking/fluent.py
@@ -769,7 +769,7 @@ def log_input(
         import numpy as np
         import mlflow
 
-        array = np.asarray([ [1,2,3], [4,5,6], [7,8,9] ])
+        array = np.asarray([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
         dataset = mlflow.data.from_numpy(array, source="data.csv")
 
         # Log an input dataset used for training
