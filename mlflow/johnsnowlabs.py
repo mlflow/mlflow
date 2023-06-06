b566d73c44b908f226c7dfcb905aff42b4b14313
harupy <hkawamura0130@gmail.com>
2023-06-06 13:53:43 +0900

    Fix get_default_conda_env doc
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/johnsnowlabs.py
+++ mlflow/johnsnowlabs.py
@@ -177,12 +177,7 @@ def get_default_pip_requirements():
 def get_default_conda_env():
     """
     :return: The default Conda environment for MLflow Models produced by calls to
-             :func:`save_model()` and :func:`log_model()`. This Conda environment
-             contains the current version of PySpark that is installed on the caller's
-             system. ``dev`` versions of PySpark are replaced with stable versions in
-             the resulting Conda environment (e.g., if you are running PySpark version
-             ``2.4.5.dev0``, invoking this method produces a Conda environment with a
-             dependency on PySpark version ``2.4.5``).
+             :func:`save_model()` and :func:`log_model()`.
     """
     return _mlflow_conda_env(additional_pip_deps=get_default_pip_requirements())
 
