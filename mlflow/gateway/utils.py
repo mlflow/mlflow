06cafe05c6d422e40f4eca59d97c405fee33975b
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-06-09 22:03:54 +0900

    Remove unused _parse_url_path_for_base_url (#8663)
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/gateway/utils.py
+++ mlflow/gateway/utils.py
@@ -8,11 +8,6 @@ from mlflow.exceptions import MlflowException
 _logger = logging.getLogger(__name__)
 
 
-def _parse_url_path_for_base_url(url_string):
-    split_url = url_string.split("/")
-    return "/".join(split_url[:-1])
-
-
 def is_valid_endpoint_name(name: str) -> bool:
     """
     Check whether a string contains any URL reserved characters, spaces, or characters other
