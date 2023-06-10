915fa484b2a1d5cb2761ec48a15f8c720dfe5e67
dbczumar <corey.zumar@databricks.com>
2023-06-09 17:22:35 -0700

    Remove lrucache
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/utils/request_utils.py
+++ mlflow/utils/request_utils.py
@@ -5,7 +5,6 @@ import os
 import requests
 import urllib3
 
-from functools import lru_cache
 from packaging.version import Version
 from requests.adapters import HTTPAdapter
 from requests.exceptions import HTTPError
@@ -53,18 +52,16 @@ def download_chunk(range_start, range_end, headers, download_path, http_uri):
             f.write(response.content)
 
 
-@lru_cache(maxsize=64)
-def _cached_get_request_session(
-    max_retries,
-    backoff_factor,
-    retry_codes,
-    # To create a new Session object for each process, we use the process id as the cache key.
-    # This is to avoid sharing the same Session object across processes, which can lead to issues
-    # such as https://stackoverflow.com/q/3724900.
-    _pid,
-):
+def _get_request_session(max_retries, backoff_factor, retry_codes):
     """
-    This function should not be called directly. Instead, use `_get_request_session` below.
+    Returns a `Requests.Session` object for making an HTTP request.
+
+    :param max_retries: Maximum total number of retries.
+    :param backoff_factor: a time factor for exponential backoff. e.g. value 5 means the HTTP
+      request will be retried with interval 5, 10, 20... seconds. A value of 0 turns off the
+      exponential backoff.
+    :param retry_codes: a list of HTTP response error codes that qualifies for retry.
+    :return: requests.Session object.
     """
     assert 0 <= max_retries < 10
     assert 0 <= backoff_factor < 120
@@ -89,25 +86,6 @@ def _cached_get_request_session(
     session.mount("https://", adapter)
     session.mount("http://", adapter)
     return session
-
-
-def _get_request_session(max_retries, backoff_factor, retry_codes):
-    """
-    Returns a `Requests.Session` object for making an HTTP request.
-
-    :param max_retries: Maximum total number of retries.
-    :param backoff_factor: a time factor for exponential backoff. e.g. value 5 means the HTTP
-      request will be retried with interval 5, 10, 20... seconds. A value of 0 turns off the
-      exponential backoff.
-    :param retry_codes: a list of HTTP response error codes that qualifies for retry.
-    :return: requests.Session object.
-    """
-    return _cached_get_request_session(
-        max_retries,
-        backoff_factor,
-        retry_codes,
-        _pid=os.getpid(),
-    )
 
 
 def _get_http_response_with_retries(
