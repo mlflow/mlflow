01e323bbc764c3c1ce696ee0096d53a34d52c104
Sid Murching <sid.murching@databricks.com>
2023-06-13 20:39:29 -0700

    Fix logging
    Signed-off-by: Sid Murching <sid.murching@databricks.com>

--- mlflow/utils/model_utils.py
+++ mlflow/utils/model_utils.py
@@ -68,11 +68,9 @@ def _get_flavor_configuration_from_uri(model_uri, flavor_name, logger):
             ml_model_file = _download_artifact_from_uri(
                 artifact_uri=append_to_uri_path(resolved_uri, MLMODEL_FILE_NAME)
             )
-        except Exception as resolved_uri_download_ex:
+        except Exception:
             logger.exception(
-                f'Failed to download an "{MLMODEL_FILE_NAME}" model file from resolved URI {resolved_uri}',
-                resolved_uri_download_ex,
-            )
+                f'Failed to download an "{MLMODEL_FILE_NAME}" model file from resolved URI {resolved_uri}')
             logger.info(f"Falling back to downloading from original model URI {model_uri}")
             ml_model_file = _download_artifact_from_uri(
                 artifact_uri=append_to_uri_path(model_uri, MLMODEL_FILE_NAME)
