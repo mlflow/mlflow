f5fddfc9979a03e0639591e31c0244692c9855da
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-06-16 23:52:35 +0900

    Refactor gateway app (#8773)
    * Avoid mutation
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Refactorr gateway app
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Use dict
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Remove docstring
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Add test
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Use assignment expression
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Split test
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Rename to GatewayAPI
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    ---------
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/gateway/test_gateway_app.py
+++ tests/gateway/test_gateway_app.py
@@ -1,49 +1,61 @@
-from mlflow.gateway.app import create_app
 from fastapi.testclient import TestClient
-import yaml
+import pytest
+
+from mlflow.exceptions import MlflowException
+from mlflow.gateway.app import create_app_from_config, create_app_from_env
+from mlflow.gateway.config import GatewayConfig
 
 
-def test_create_app(tmp_path):
-    config = tmp_path.joinpath("config.yml")
-    conf = {
-        "routes": [
-            {
-                "name": "completions-gpt4",
-                "type": "llm/v1/completions",
-                "model": {
-                    "name": "gpt-4",
-                    "provider": "openai",
-                    "config": {
-                        "openai_api_key": "mykey",
-                        "openai_api_base": "https://api.openai.com/v1",
-                        "openai_api_version": "2023-05-10",
-                        "openai_api_type": "openai/v1/chat/completions",
+@pytest.fixture
+def client() -> TestClient:
+    config = GatewayConfig(
+        **{
+            "routes": [
+                {
+                    "name": "completions-gpt4",
+                    "type": "llm/v1/completions",
+                    "model": {
+                        "name": "gpt-4",
+                        "provider": "openai",
+                        "config": {
+                            "openai_api_key": "mykey",
+                            "openai_api_base": "https://api.openai.com/v1",
+                            "openai_api_version": "2023-05-10",
+                            "openai_api_type": "openai/v1/chat/completions",
+                        },
                     },
                 },
-            },
-            {
-                "name": "chat-gpt4",
-                "type": "llm/v1/chat",
-                "model": {
-                    "name": "gpt-4",
-                    "provider": "openai",
-                    "config": {
-                        "openai_api_key": "MY_API_KEY",
+                {
+                    "name": "chat-gpt4",
+                    "type": "llm/v1/chat",
+                    "model": {
+                        "name": "gpt-4",
+                        "provider": "openai",
+                        "config": {
+                            "openai_api_key": "MY_API_KEY",
+                        },
                     },
                 },
-            },
-        ]
-    }
-    config.write_text(yaml.safe_dump(conf))
+            ]
+        }
+    )
+    app = create_app_from_config(config)
+    return TestClient(app)
 
-    app = create_app(config)
 
-    client = TestClient(app)
+def test_index(client: TestClient):
+    response = client.get("/", follow_redirects=False)
+    assert response.status_code == 307
+    assert response.headers["Location"] == "/docs"
 
+
+def test_health(client: TestClient):
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "OK"}
 
+
+def test_search_routes(client: TestClient):
     response = client.get("/gateway/routes")
     assert response.status_code == 200
     assert response.json() == {
@@ -67,6 +79,8 @@ def test_create_app(tmp_path):
         ]
     }
 
+
+def test_get_route(client: TestClient):
     response = client.get("/gateway/routes/chat-gpt4")
     assert response.status_code == 200
     assert response.json() == {
@@ -79,3 +93,9 @@ def test_create_app(tmp_path):
             },
         }
     }
+
+
+def test_create_app_from_env_fails_if_MLFLOW_GATEWAY_CONFIG_is_not_set(monkeypatch):
+    monkeypatch.delenv("MLFLOW_GATEWAY_CONFIG", raising=False)
+    with pytest.raises(MlflowException, match="'MLFLOW_GATEWAY_CONFIG' is not set"):
+        create_app_from_env()
