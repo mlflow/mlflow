a9b75275b89a22e5c241497a2c59803ef721314d
Jerry Liang <66143562+jerrylian-db@users.noreply.github.com>
2023-06-21 10:30:56 -0700

    Implement automatic signature inference for other flavors (#8782)
    * initial commit
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * implement output casting
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * add tests
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * add test
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * fix issues and tests
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * deepcopy input example
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * fix tests
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * add doc string
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * fix issues
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * fix tests
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * small change
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * fix tests
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * add doc string
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * improve test
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * pivot approach
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * fix tests
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * small change
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * revert test change
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * small changes
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * augment warning message
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * adapt feedback
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * adapt feedback
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * implement many model flavors
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * format
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * wip
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * fix tests
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * docstrings
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * implement paddle
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * implement gluon, h2o, spacy
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * remove extra thing
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * remove extra imports
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * fix tests
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * fix lint
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * fix lint
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    ---------
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>

--- mlflow/prophet.py
+++ mlflow/prophet.py
@@ -33,6 +33,7 @@ from mlflow.utils.file_utils import write_to
 from mlflow.utils.docstring_utils import format_docstring, LOG_MODEL_PARAM_DOCS
 from mlflow.models.utils import _save_example
 from mlflow.models import Model, ModelInputExample, ModelSignature
+from mlflow.models.signature import _infer_signature_from_input_example
 from mlflow.tracking.artifact_utils import _download_artifact_from_uri
 from mlflow.utils.model_utils import (
     _get_flavor_configuration,
@@ -94,12 +95,16 @@ def save_model(
                        containing file dependencies). These files are *prepended* to the system
                        path when the model is loaded.
     :param mlflow_model: :py:mod:`mlflow.models.Model` this flavor is being added to.
-    :param signature: :py:class:`ModelSignature <mlflow.models.ModelSignature>`
-                      describes model input and output :py:class:`Schema <mlflow.types.Schema>`.
-                      The model signature can be :py:func:`inferred <mlflow.models.infer_signature>`
-                      from datasets with valid model input (e.g. the training dataset with target
-                      column omitted) and valid model output (e.g. model predictions generated on
-                      the training dataset), for example:
+    :param signature: an instance of the :py:class:`ModelSignature <mlflow.models.ModelSignature>`
+                      class that describes the model's inputs and outputs. If not specified but an
+                      ``input_example`` is supplied, a signature will be automatically inferred
+                      based on the supplied input example and model. To disable automatic signature
+                      inference when providing an input example, set ``signature`` to ``False``.
+                      To manually infer a model signature, call
+                      :py:func:`infer_signature() <mlflow.models.infer_signature>` on datasets
+                      with valid model inputs, such as a training dataset with the target column
+                      omitted, and valid model outputs, like model predictions made on the training
+                      dataset, for example:
 
                       .. code-block:: python
 
@@ -109,11 +114,7 @@ def save_model(
                         train = model.history
                         predictions = model.predict(model.make_future_dataframe(30))
                         signature = infer_signature(train, predictions)
-    :param input_example: Input example provides one or several instances of valid
-                          model input. The example can be used as a hint of what data to feed the
-                          model. The given example will be converted to a Pandas DataFrame and then
-                          serialized to json using the Pandas split-oriented format. Bytes are
-                          base64-encoded.
+    :param input_example: {{ input_example }}
     :param pip_requirements: {{ pip_requirements }}
     :param extra_pip_requirements: {{ extra_pip_requirements }}
     :param metadata: Custom metadata dictionary passed to the model and stored in the MLmodel file.
@@ -128,6 +129,12 @@ def save_model(
     path = os.path.abspath(path)
     _validate_and_prepare_target_save_path(path)
     code_dir_subpath = _validate_and_copy_code_paths(code_paths, path)
+
+    if signature is None and input_example is not None:
+        wrapped_model = _ProphetModelWrapper(pr_model)
+        signature = _infer_signature_from_input_example(input_example, wrapped_model)
+    elif signature is False:
+        signature = None
 
     if mlflow_model is None:
         mlflow_model = Model()
@@ -221,14 +228,16 @@ def log_model(
                                   future release without warning. If given, create a model
                                   version under ``registered_model_name``, also creating a
                                   registered model if one with the given name does not exist.
-    :param signature: :py:class:`ModelSignature <mlflow.models.ModelSignature>`
-                      describes model input and output
-                      :py:class:`Schema <mlflow.types.Schema>`.
-                      The model signature can be :py:func:`inferred
-                      <mlflow.models.infer_signature>` from datasets with valid model input
-                      (e.g. the training dataset with target column omitted) and valid model
-                      output (e.g. model predictions generated on the training dataset),
-                      for example:
+    :param signature: an instance of the :py:class:`ModelSignature <mlflow.models.ModelSignature>`
+                      class that describes the model's inputs and outputs. If not specified but an
+                      ``input_example`` is supplied, a signature will be automatically inferred
+                      based on the supplied input example and model. To disable automatic signature
+                      inference when providing an input example, set ``signature`` to ``False``.
+                      To manually infer a model signature, call
+                      :py:func:`infer_signature() <mlflow.models.infer_signature>` on datasets
+                      with valid model inputs, such as a training dataset with the target column
+                      omitted, and valid model outputs, like model predictions made on the training
+                      dataset, for example:
 
                       .. code-block:: python
 
@@ -239,12 +248,7 @@ def log_model(
                         predictions = model.predict(model.make_future_dataframe(30))
                         signature = infer_signature(train, predictions)
 
-    :param input_example: Input example provides one or several instances of valid
-                          model input. The example can be used as a hint of what data to
-                          feed the model. The given example will be converted to a
-                          Pandas DataFrame and then serialized to json using the
-                          Pandas split-oriented format. Bytes are base64-encoded.
-
+    :param input_example: {{ input_example }}
     :param await_registration_for: Number of seconds to wait for the model version
                         to finish being created and is in ``READY`` status.
                         By default, the function waits for five minutes.
