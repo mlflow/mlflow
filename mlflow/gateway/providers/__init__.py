ed6dc37a8a561b9b0dcb1ec980facdd9c255f9b6
Ben Wilson <benjamin.wilson@databricks.com>
2023-06-21 13:45:04 -0400

    add coverage for anthropic
    Signed-off-by: Ben Wilson <benjamin.wilson@databricks.com>

--- mlflow/gateway/providers/__init__.py
+++ mlflow/gateway/providers/__init__.py
@@ -1,12 +1,14 @@
 from mlflow.exceptions import MlflowException
 from .base import BaseProvider
 from .openai import OpenAIProvider
+from .anthropic import AnthropicProvider
 from ..config import Provider
 
 
 def get_provider(provider: Provider) -> BaseProvider:
     provider_to_class = {
         Provider.OPENAI: OpenAIProvider,
+        Provider.ANTHROPIC: AnthropicProvider,
     }
     if prov := provider_to_class.get(provider):
         return prov
