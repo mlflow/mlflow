c84fdf530d4040112395857c132c77cbd39fd8ce
Siddharth Murching <sid.murching@databricks.com>
2023-06-26 19:57:42 -0700

    Improve error messages when trying to use alias APIs with workspace model registry (#8846)
    * Improve error messages when trying to use alias APIs with Databricks workspace model registry
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Improve message
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * typo
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    
    * Update mlflow/store/model_registry/databricks_workspace_model_registry_rest_store.py
    
    Signed-off-by: Siddharth Murching <smurching@gmail.com>
    
    * Update mlflow/store/model_registry/databricks_workspace_model_registry_rest_store.py
    
    Signed-off-by: Siddharth Murching <smurching@gmail.com>
    
    ---------
    
    Signed-off-by: Sid Murching <sid.murching@databricks.com>
    Signed-off-by: Siddharth Murching <smurching@gmail.com>

---
+++ tests/store/model_registry/test_databricks_workspace_model_registry_store.py
@@ -0,0 +1,39 @@
+import pytest
+
+from mlflow.exceptions import MlflowException
+from mlflow.utils.rest_utils import MlflowHostCreds
+from mlflow.store.model_registry.databricks_workspace_model_registry_rest_store import (
+    DatabricksWorkspaceModelRegistryRestStore,
+)
+
+
+@pytest.fixture
+def creds():
+    return MlflowHostCreds("https://hello")
+
+
+@pytest.fixture
+def store(creds):
+    return DatabricksWorkspaceModelRegistryRestStore(lambda: creds)
+
+
+def _expected_unsupported_method_error_message(method):
+    return f"Method '{method}' is unsupported for models in the Workspace Model Registry"
+
+
+def test_workspace_model_registry_alias_apis_unsupported(store):
+    with pytest.raises(
+        MlflowException,
+        match=_expected_unsupported_method_error_message("set_registered_model_alias"),
+    ):
+        store.set_registered_model_alias(name="mycoolmodel", alias="myalias", version=1)
+    with pytest.raises(
+        MlflowException,
+        match=_expected_unsupported_method_error_message("delete_registered_model_alias"),
+    ):
+        store.delete_registered_model_alias(name="mycoolmodel", alias="myalias")
+    with pytest.raises(
+        MlflowException,
+        match=_expected_unsupported_method_error_message("get_model_version_by_alias"),
+    ):
+        store.get_model_version_by_alias(name="mycoolmodel", alias="myalias")
