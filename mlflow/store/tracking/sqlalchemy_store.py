32dff3b98f9854629328dd1c09cb0a8c6b71c4c0
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-06-27 12:24:15 +0900

    Remove _save_to_db (#8835)
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/store/tracking/sqlalchemy_store.py
+++ mlflow/store/tracking/sqlalchemy_store.py
@@ -215,16 +215,6 @@ class SqlAlchemyStore(AbstractStore):
         finally:
             self._unset_zero_value_insertion_for_autoincrement_column(session)
 
-    def _save_to_db(self, session, objs):
-        """
-        Store in db
-        """
-        if type(objs) is list:
-            session.add_all(objs)
-        else:
-            # single object
-            session.add(objs)
-
     def _get_or_create(self, session, model, **kwargs):
         instance = session.query(model).filter_by(**kwargs).first()
         created = False
@@ -233,7 +223,7 @@ class SqlAlchemyStore(AbstractStore):
             return instance, created
         else:
             instance = model(**kwargs)
-            self._save_to_db(objs=instance, session=session)
+            session.add(instance)
             created = True
 
         return instance, created
@@ -408,7 +398,7 @@ class SqlAlchemyStore(AbstractStore):
             runs = self._list_run_infos(session, experiment_id)
             for run in runs:
                 self._mark_run_deleted(session, run)
-            self._save_to_db(objs=experiment, session=session)
+            session.add(experiment)
 
     def _hard_delete_experiment(self, experiment_id):
         """
@@ -424,12 +414,12 @@ class SqlAlchemyStore(AbstractStore):
     def _mark_run_deleted(self, session, run):
         run.lifecycle_stage = LifecycleStage.DELETED
         run.deleted_time = get_current_time_millis()
-        self._save_to_db(objs=run, session=session)
+        session.add(run)
 
     def _mark_run_active(self, session, run):
         run.lifecycle_stage = LifecycleStage.ACTIVE
         run.deleted_time = None
-        self._save_to_db(objs=run, session=session)
+        session.add(run)
 
     def _list_run_infos(self, session, experiment_id):
         return session.query(SqlRun).filter(SqlRun.experiment_id == experiment_id).all()
@@ -442,7 +432,7 @@ class SqlAlchemyStore(AbstractStore):
             runs = self._list_run_infos(session, experiment_id)
             for run in runs:
                 self._mark_run_active(session, run)
-            self._save_to_db(objs=experiment, session=session)
+            session.add(experiment)
 
     def rename_experiment(self, experiment_id, new_name):
         with self.ManagedSessionMaker() as session:
@@ -452,7 +442,7 @@ class SqlAlchemyStore(AbstractStore):
 
             experiment.name = new_name
             experiment.last_update_time = get_current_time_millis()
-            self._save_to_db(objs=experiment, session=session)
+            session.add(experiment)
 
     def create_run(self, experiment_id, user_id, start_time, tags, run_name):
         with self.ManagedSessionMaker() as session:
@@ -496,7 +486,7 @@ class SqlAlchemyStore(AbstractStore):
             )
 
             run.tags = [SqlTag(key=tag.key, value=tag.value) for tag in tags]
-            self._save_to_db(objs=run, session=session)
+            session.add(run)
 
             return run.to_mlflow_entity()
 
@@ -606,7 +596,7 @@ class SqlAlchemyStore(AbstractStore):
                 else:
                     run_name_tag.value = run_name
 
-            self._save_to_db(objs=run, session=session)
+            session.add(run)
             run = run.to_mlflow_entity()
 
             return run.info
@@ -637,14 +627,14 @@ class SqlAlchemyStore(AbstractStore):
             run = self._get_run(run_uuid=run_id, session=session)
             run.lifecycle_stage = LifecycleStage.ACTIVE
             run.deleted_time = None
-            self._save_to_db(objs=run, session=session)
+            session.add(run)
 
     def delete_run(self, run_id):
         with self.ManagedSessionMaker() as session:
             run = self._get_run(run_uuid=run_id, session=session)
             run.lifecycle_stage = LifecycleStage.DELETED
             run.deleted_time = get_current_time_millis()
-            self._save_to_db(objs=run, session=session)
+            session.add(run)
 
     def _hard_delete_run(self, run_id):
         """
@@ -718,7 +708,7 @@ class SqlAlchemyStore(AbstractStore):
             self._check_run_is_active(run)
 
             def _insert_metrics(metric_instances):
-                self._save_to_db(session=session, objs=metric_instances)
+                session.add_all(metric_instances)
                 self._update_latest_metrics_if_necessary(metric_instances, session)
                 session.commit()
 
@@ -872,7 +862,7 @@ class SqlAlchemyStore(AbstractStore):
                 latest_metric = _overwrite_metric(logged_metric, latest_metric)
 
         if new_latest_metric_dict:
-            self._save_to_db(session=session, objs=list(new_latest_metric_dict.values()))
+            session.add_all(new_latest_metric_dict.values())
 
     def get_metric_history(self, run_id, metric_key, max_results=None, page_token=None):
         """
@@ -1092,7 +1082,7 @@ class SqlAlchemyStore(AbstractStore):
             if not new_params:
                 return
 
-            self._save_to_db(session=session, objs=new_params)
+            session.add_all(new_params)
 
     def set_experiment_tag(self, experiment_id, tag):
         """
@@ -1185,7 +1175,7 @@ class SqlAlchemyStore(AbstractStore):
                             new_tag_dict[tag.key] = new_tag
 
                     # finally, save new entries to DB.
-                    self._save_to_db(session=session, objs=list(new_tag_dict.values()))
+                    session.add_all(new_tag_dict.values())
                     session.commit()
                 except sqlalchemy.exc.IntegrityError:
                     session.rollback()
@@ -1484,7 +1474,7 @@ class SqlAlchemyStore(AbstractStore):
                             )
                         )
 
-            self._save_to_db(session, objs_to_write)
+            session.add_all(objs_to_write)
 
 
 def _get_attributes_filtering_clauses(parsed, dialect):
