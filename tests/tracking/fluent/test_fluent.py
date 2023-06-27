5b59337c5adb02ee4335641b47e7871feadd71eb
harupy <hkawamura0130@gmail.com>
2023-06-27 18:01:13 +0900

    Fix comment
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/tracking/fluent/test_fluent.py
+++ tests/tracking/fluent/test_fluent.py
@@ -371,8 +371,8 @@ def test_search_experiments(tmp_path):
     mlflow.set_tracking_uri(sqlite_uri)
     # Why do we need this line? If we didn't have this line, the first `mlflow.create_experiment`
     # call in the loop below would create two experiments, the default experiment (when the sqlite
-    # database is initialized) and a new experiment with the specified name. They might have
-    # the same creation time, which makes the search order non-deterministic and this test flaky.
+    # database is initialized) and another one with the specified name. They might have the same
+    # creation time, which makes the search order non-deterministic and this test flaky.
     mlflow.search_experiments()
 
     num_all_experiments = SEARCH_MAX_RESULTS_DEFAULT + 1  # +1 for the default experiment
