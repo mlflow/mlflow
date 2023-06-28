01831ce728f80fb7243c1df7457931ade026d598
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-06-28 10:24:20 +0900

    Use a context manager when creating a spark session (#8828)
    * Use context manager for spark session
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    * Add as spark
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    
    ---------
    
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- examples/spark_udf/spark_udf.py
+++ examples/spark_udf/spark_udf.py
@@ -4,22 +4,19 @@ from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.getOrCreate()
+with SparkSession.builder.getOrCreate() as spark:
+    X, y = datasets.load_iris(as_frame=True, return_X_y=True)
+    model = KNeighborsClassifier()
+    model.fit(X, y)
+    predictions = model.predict(X)
+    signature = infer_signature(X, predictions)
 
-X, y = datasets.load_iris(as_frame=True, return_X_y=True)
-model = KNeighborsClassifier()
-model.fit(X, y)
-predictions = model.predict(X)
-signature = infer_signature(X, predictions)
+    with mlflow.start_run():
+        model_info = mlflow.sklearn.log_model(model, "model", signature=signature)
 
-with mlflow.start_run():
-    model_info = mlflow.sklearn.log_model(model, "model", signature=signature)
+    infer_spark_df = spark.createDataFrame(X)
 
-infer_spark_df = spark.createDataFrame(X)
+    pyfunc_udf = mlflow.pyfunc.spark_udf(spark, model_info.model_uri, env_manager="conda")
+    result = infer_spark_df.select(pyfunc_udf(*X.columns).alias("predictions")).toPandas()
 
-pyfunc_udf = mlflow.pyfunc.spark_udf(spark, model_info.model_uri, env_manager="conda")
-result = infer_spark_df.select(pyfunc_udf(*X.columns).alias("predictions")).toPandas()
-
-print(result)
-
-spark.stop()
+    print(result)
