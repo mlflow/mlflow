b19c96b264af353dc4c579227e5de36a7ac5267c
Jerry Liang <66143562+jerrylian-db@users.noreply.github.com>
2023-06-29 19:44:13 -0700

    Use testing mode to raise errors during automatic signature inference (#8866)
    * implement it
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * change environment variable
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * fix test
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    * adapt feedback
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    
    ---------
    
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>

--- mlflow/spark.py
+++ mlflow/spark.py
@@ -672,6 +672,8 @@ def save_model(
                 prediction = pd.Series(wrapped_model.predict(input_ex))
                 signature = infer_signature(input_ex, prediction)
         except Exception as e:
+            if environment_variables._MLFLOW_TESTING.get():
+                raise
             _logger.warning(_LOG_MODEL_INFER_SIGNATURE_WARNING_TEMPLATE, repr(e))
             _logger.debug("", exc_info=True)
     elif signature is False:
