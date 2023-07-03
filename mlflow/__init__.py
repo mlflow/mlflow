914027d02567974b7cfd1e697a0f8cb533d2d471
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-07-03 11:45:53 +0900

    Update mlflow/__init__.py
    Signed-off-by: Harutaka Kawamura <hkawamura0130@gmail.com>

--- mlflow/__init__.py
+++ mlflow/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=wrong-import-position
 """
-THE ``mlflow`` module provides a high-level "fluent" API for starting and managing MLflow runs.
+The ``mlflow`` module provides a high-level "fluent" API for starting and managing MLflow runs.
 For example:
 
 .. code:: python
