4a3a22e629bb6da209c16d5e63c3b5af2aa00112
harupy <hkawamura0130@gmail.com>
2023-07-05 15:00:43 +0900

    Fix test_signature_and_examples_are_saved_correctly
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/sentence_transformers/test_sentence_transformers_model_export.py
+++ tests/sentence_transformers/test_sentence_transformers_model_export.py
@@ -387,13 +387,17 @@ SIGNATURE = infer_signature(
     model_input=SENTENCES,
     model_output=SentenceTransformer("all-MiniLM-L6-v2").encode(SENTENCES),
 )
+SIGNATURE_FROM_EXAMPLE = infer_signature(
+    model_input=pd.DataFrame([SENTENCES], columns=[0, 1]),
+    model_output=SentenceTransformer("all-MiniLM-L6-v2").encode(SENTENCES),
+)
 
 
 @pytest.mark.parametrize(
     "example, signature, expected_signature",
     [
         (None, None, mlflow.sentence_transformers._get_default_signature()),
-        (SENTENCES, None, SIGNATURE),
+        (SENTENCES, None, SIGNATURE_FROM_EXAMPLE),
         (None, SIGNATURE, SIGNATURE),
         (SENTENCES, SIGNATURE, SIGNATURE),
     ],
