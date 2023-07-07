3184942e3072e8c2a0ea2babae70e801c77170f0
Serena Ruan <82044803+serena-ruan@users.noreply.github.com>
2023-07-07 18:06:21 +0800

    [Support inference params-1]Add ParamSchema and ParamSpec in model signature (#8972)
    * add ParamSchema and ParamSpec in model signature
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    * update DataType and add tests
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    * convert all params to python native types
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    * remove useless variable
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    * add INVALID_PARAMETER_VALUE
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    * fix _infer_param_schema
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    * address comments
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    * add test case for _find_duplicates
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    * rename type to dtype
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    * fix test failure in windows
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    * remove pylint disable and fix windows test
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    
    ---------
    
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -339,6 +339,8 @@ nitpick_ignore = [
     ("py:class", "mlflow.types.schema.ColSpec"),
     ("py:class", "mlflow.types.schema.TensorSpec"),
     ("py:class", "mlflow.types.schema.Schema"),
+    ("py:class", "mlflow.types.schema.ParamSchema"),
+    ("py:class", "mlflow.types.schema.ParamSpec"),
     ("py:class", "mlflow.models.model.Model"),
     ("py:class", "mlflow.models.signature.ModelSignature"),
     ("py:class", "MlflowInferableDataset"),
