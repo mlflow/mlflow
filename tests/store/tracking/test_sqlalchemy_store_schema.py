c535b290378d6a2cbcb060d1fdcda75a8e3302da
Viktorius Suwandi <viktorius.tori@gmail.com>
2023-07-10 12:01:08 +0700

    Remove unused imports on mlflow/tests/store/tracking/test_sqlalchemy_store_schema.py
    Signed-off-by: Viktorius Suwandi <viktorius.tori@gmail.com>

--- tests/store/tracking/test_sqlalchemy_store_schema.py
+++ tests/store/tracking/test_sqlalchemy_store_schema.py
@@ -15,12 +15,6 @@ from mlflow.store.db.utils import _get_alembic_config, _verify_schema
 from mlflow.store.db.base_sql_model import Base
 
 # pylint: disable=unused-import
-from mlflow.store.model_registry.dbmodels.models import (
-    SqlRegisteredModel,
-    SqlModelVersion,
-    SqlRegisteredModelTag,
-    SqlModelVersionTag,
-)
 from mlflow.store.tracking.sqlalchemy_store import SqlAlchemyStore
 from mlflow.store.tracking.dbmodels.initial_models import Base as InitialBase
 from tests.store.dump_schema import dump_db_schema
