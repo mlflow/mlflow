79afd1f1180cd00a1f5ac5a28ea298ef071044f2
mlflow-automation <mlflow-automation@users.noreply.github.com>
2023-07-10 04:28:49 +0000

    Run python3 dev/update_mlflow_versions.py pre-release ...
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>

--- mlflow/java/scoring/src/test/java/org/mlflow/ScoringServerTest.java
+++ mlflow/java/scoring/src/test/java/org/mlflow/ScoringServerTest.java
@@ -78,7 +78,7 @@ public class ScoringServerTest {
     HttpResponse response = httpClient.execute(getRequest);
     Assert.assertEquals(HttpServletResponse.SC_OK, response.getStatusLine().getStatusCode());
     String responseBody = getHttpResponseBody(response);
-    Assert.assertEquals("2.4.1", responseBody);
+    Assert.assertEquals("2.4.2", responseBody);
     server.stop();
   }
 
