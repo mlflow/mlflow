9e752ef237822698dfc41fc61a0739316cd3de51
Viktorius Suwandi <viktorius.tori@gmail.com>
2023-07-10 12:02:11 +0700

    Remove unused imports on tests/projects/backend/test_local.py
    Signed-off-by: Viktorius Suwandi <viktorius.tori@gmail.com>

--- tests/projects/backend/test_local.py
+++ tests/projects/backend/test_local.py
@@ -31,7 +31,6 @@ def test_docker_s3_artifact_cmd_and_envs_from_home():
 
 def test_docker_wasbs_artifact_cmd_and_envs_from_home():
     # pylint: disable=unused-import
-    from azure.storage.blob import BlobServiceClient
 
     mock_env = {
         "AZURE_STORAGE_CONNECTION_STRING": "mock_connection_string",
