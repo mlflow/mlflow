cddf1fd5c7fb43690faccc4767e678db24737a76
Viktorius Suwandi <viktorius.tori@gmail.com>
2023-07-10 13:45:52 +0700

    Remove useless script on tests/check_mlflow_lazily_imports_ml_packages.py
    Signed-off-by: Viktorius Suwandi <viktorius.tori@gmail.com>

--- tests/check_mlflow_lazily_imports_ml_packages.py
+++ tests/check_mlflow_lazily_imports_ml_packages.py
@@ -5,7 +5,7 @@ Tests that `import mlflow` and `mlflow.autolog()` do not import ML packages.
 import sys
 import importlib
 import logging
-import mlflow  # pylint: disable=unused-import
+import mlflow
 
 logger = logging.getLogger()
 
