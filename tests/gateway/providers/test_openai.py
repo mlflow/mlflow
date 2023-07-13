e04aa25e464e1baa1a5214a8c7343c78d02a54eb
Ben Wilson <benjamin.wilson@databricks.com>
2023-07-13 15:46:08 -0400

    use the constant
    Signed-off-by: Ben Wilson <benjamin.wilson@databricks.com>

--- tests/gateway/providers/test_openai.py
+++ tests/gateway/providers/test_openai.py
@@ -6,10 +6,10 @@ from fastapi.encoders import jsonable_encoder
 import pytest
 
 from mlflow.exceptions import MlflowException
-from mlflow.gateway.config import OpenAIConfig
+from mlflow.gateway.config import OpenAIConfig, RouteConfig
+from mlflow.gateway.constants import MLFLOW_GATEWAY_ROUTE_TIMEOUT_SECONDS
 from mlflow.gateway.providers.openai import OpenAIProvider
 from mlflow.gateway.schemas import chat, completions, embeddings
-from mlflow.gateway.config import RouteConfig
 from tests.gateway.tools import MockAsyncResponse, mock_http_client
 
 
@@ -94,7 +94,7 @@ async def test_chat():
                 "temperature": 0,
                 **payload,
             },
-            timeout=ClientTimeout(total=300),
+            timeout=ClientTimeout(total=MLFLOW_GATEWAY_ROUTE_TIMEOUT_SECONDS),
         )
 
 
@@ -174,7 +174,7 @@ async def test_completions():
                 "temperature": 0,
                 "messages": [{"role": "user", "content": "This is a test"}],
             },
-            timeout=ClientTimeout(total=300),
+            timeout=ClientTimeout(total=MLFLOW_GATEWAY_ROUTE_TIMEOUT_SECONDS),
         )
 
 
@@ -268,7 +268,7 @@ async def test_embeddings():
         mock_client.post.assert_called_once_with(
             "https://api.openai.com/v1/embeddings",
             json={"model": "text-embedding-ada-002", "input": "This is a test"},
-            timeout=ClientTimeout(total=300),
+            timeout=ClientTimeout(total=MLFLOW_GATEWAY_ROUTE_TIMEOUT_SECONDS),
         )
 
 
@@ -338,7 +338,7 @@ async def test_embeddings_batch_input():
                 "model": "text-embedding-ada-002",
                 "input": ["1", "2"],
             },
-            timeout=ClientTimeout(total=300),
+            timeout=ClientTimeout(total=MLFLOW_GATEWAY_ROUTE_TIMEOUT_SECONDS),
         )
 
 
@@ -396,7 +396,7 @@ async def test_azure_openai():
                 "temperature": 0,
                 "messages": [{"role": "user", "content": "This is a test"}],
             },
-            timeout=ClientTimeout(total=300),
+            timeout=ClientTimeout(total=MLFLOW_GATEWAY_ROUTE_TIMEOUT_SECONDS),
         )
 
 
@@ -436,7 +436,7 @@ async def test_azuread_openai():
                 "temperature": 0,
                 "messages": [{"role": "user", "content": "This is a test"}],
             },
-            timeout=ClientTimeout(total=300),
+            timeout=ClientTimeout(total=MLFLOW_GATEWAY_ROUTE_TIMEOUT_SECONDS),
         )
 
 
