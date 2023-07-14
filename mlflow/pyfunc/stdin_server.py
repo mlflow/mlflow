3e7f0a3e009297676f92fd95df22e07ff8069e8f
Serena Ruan <serena.rxy@gmail.com>
2023-07-14 19:18:21 +0800

    add signature check for stdin_server.py
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- mlflow/pyfunc/stdin_server.py
+++ mlflow/pyfunc/stdin_server.py
@@ -1,10 +1,12 @@
 import argparse
 import sys
+import inspect
 import json
 import logging
 
 import mlflow
 from mlflow.pyfunc import scoring_server
+from mlflow.pyfunc.model import _log_warning_if_params_not_in_predict_signature
 
 _logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -29,7 +31,11 @@ for line in sys.stdin:
     data = scoring_server.infer_and_parse_data(data, input_schema)
 
     _logger.info("Making predictions")
-    preds = model.predict(data, params)
+    if inspect.signature(model.predict).parameters.get("params"):
+        preds = model.predict(data, params=params)
+    else:
+        _log_warning_if_params_not_in_predict_signature(_logger, params)
+        preds = model.predict(data)
 
     _logger.info("Writing predictions")
     with open(request["output_file"], "a") as f:
