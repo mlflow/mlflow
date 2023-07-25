85ab7eead9e783a5abeadf365fba56e1f1b14cea
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-07-25 12:26:26 +0900

    Free some disk space in python job (#9153)
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- tests/tracking/test_mlflow_artifacts.py
+++ tests/tracking/test_mlflow_artifacts.py
@@ -241,11 +241,13 @@ def test_mlflow_artifacts_example(tmp_path):
     # On GitHub Actions, remove generated images to save disk space
     rmi_option = "--rmi all" if is_github_actions() else ""
     cmd = f"""
-set -ex
+err=0
+trap 'err=1' ERR
 ./build.sh
 docker-compose run -v ${{PWD}}/example.py:/app/example.py client python example.py
 docker-compose logs
 docker-compose down {rmi_option} --volumes --remove-orphans
+test $err = 0
 """
     script_path = tmp_path.joinpath("test.sh")
     script_path.write_text(cmd)
