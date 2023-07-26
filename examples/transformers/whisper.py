231092aa8c2003cca5968f7f721c668afbb658e3
Serena Ruan <82044803+serena-ruan@users.noreply.github.com>
2023-07-26 10:50:34 +0800

    Update whisper example to use tiny (#9161)
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- examples/transformers/whisper.py
+++ examples/transformers/whisper.py
@@ -8,23 +8,12 @@ import mlflow
 audio = requests.get("https://www.nasa.gov/62283main_landing.wav").content
 
 task = "automatic-speech-recognition"
-architecture = "openai/whisper-small"
+architecture = "openai/whisper-tiny"
 
 model = transformers.WhisperForConditionalGeneration.from_pretrained(architecture)
 tokenizer = transformers.WhisperTokenizer.from_pretrained(architecture)
 feature_extractor = transformers.WhisperFeatureExtractor.from_pretrained(architecture)
-model.generation_config.alignment_heads = [
-    [5, 3],
-    [5, 9],
-    [8, 0],
-    [8, 4],
-    [8, 7],
-    [8, 8],
-    [9, 0],
-    [9, 7],
-    [9, 9],
-    [10, 5],
-]
+model.generation_config.alignment_heads = [[2, 2], [3, 0], [3, 2], [3, 3], [3, 4], [3, 5]]
 audio_transcription_pipeline = transformers.pipeline(
     task=task, model=model, tokenizer=tokenizer, feature_extractor=feature_extractor
 )
