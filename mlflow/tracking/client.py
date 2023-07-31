e930e280e8ed0f74ad1dae681955d0d258a87c12
lc3267 <melvin.strobl@kit.edu>
2023-07-31 14:50:15 +0200

    added kwargs to log figure and plotly/ mpl calls
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- mlflow/tracking/client.py
+++ mlflow/tracking/client.py
@@ -1234,6 +1234,7 @@ class MlflowClient:
         run_id: str,
         figure: Union["matplotlib.figure.Figure", "plotly.graph_objects.Figure"],
         artifact_file: str,
+        **kwargs,
     ) -> None:
         """
         Log a figure as an artifact. The following figure objects are supported:
@@ -1291,13 +1292,13 @@ class MlflowClient:
             # This allows logging a `plotly` figure in an environment where `matplotlib` is not
             # installed.
             if "matplotlib" in sys.modules and _is_matplotlib_figure(figure):
-                figure.savefig(tmp_path)
+                figure.savefig(tmp_path, **kwargs)
             elif "plotly" in sys.modules and _is_plotly_figure(figure):
                 file_extension = os.path.splitext(artifact_file)[1]
                 if file_extension == ".html":
-                    figure.write_html(tmp_path, include_plotlyjs="cdn", auto_open=False)
+                    figure.write_html(tmp_path, include_plotlyjs="cdn", auto_open=False, **kwargs)
                 elif file_extension in [".png", ".jpeg", ".webp", ".svg", ".pdf"]:
-                    figure.write_image(tmp_path)
+                    figure.write_image(tmp_path, **kwargs)
                 else:
                     raise TypeError(
                         f"Unsupported file extension for plotly figure: '{file_extension}'"
