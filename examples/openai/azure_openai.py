0be9ad0a645656d96be39d175821e3bc1ef4da8e
Serena Ruan <serena.rxy@gmail.com>
2023-08-03 10:52:57 +0800

    address comments
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- examples/openai/azure_openai.py
+++ examples/openai/azure_openai.py
@@ -1,24 +1,39 @@
 import mlflow
 import openai
-import os
 import pandas as pd
 
-os.environ["OPENAI_API_KEY"] = "<YOUR AZURE OPENAI KEY>"
-os.environ["OPENAI_API_BASE"] = "<YOUR AZURE OPENAI BASE>"
-os.environ["OPENAI_API_VERSION"] = "2023-05-15"
-os.environ["OPENAI_API_TYPE"] = "azure"
+"""
+Set environment variables for Azure OpenAI service in your terminal
+e.g. in ~/.bashrc or ~/.zshrc
+export OPENAI_API_KEY="<YOUR AZURE OPENAI KEY>"
+# OPENAI_API_BASE should be the endpoint of your Azure OpenAI resource
+# e.g. https://<service-name>.openai.azure.com/
+export OPENAI_API_BASE="<YOUR AZURE OPENAI BASE>"
+# OPENAI_API_VERSION e.g. 2023-05-15
+export OPENAI_API_VERSION="<YOUR AZURE OPENAI API VERSION>"
+export OPENAI_API_TYPE="azure"
+"""
 
 with mlflow.start_run():
     model_info = mlflow.openai.log_model(
-        # For Azure OpenAI, model doesn't matter in practice
-        model="gpt-3.5-turbo",
+        # Your Azure OpenAI model e.g. gpt-3.5-turbo
+        model="<YOUR AZURE OPENAI MODEL>",
         task=openai.ChatCompletion,
         artifact_path="model",
         messages=[{"role": "user", "content": "Tell me a joke about {animal}."}],
-        deployment_id="<YOUR AZURE DEPLOYMENT ID (ALSO CALLED DEPLOYMENT NAME)>",
+        deployment_id="<YOUR AZURE OPENAI DEPLOYMENT ID (ALSO CALLED DEPLOYMENT NAME)>",
     )
 
+# Load native OpenAI model
+native_model = mlflow.openai.load_model(model_info.model_uri)
+completion = openai.ChatCompletion.create(
+    model=native_model["model"],
+    messages=native_model["messages"],
+)
+print(completion["choices"][0]["message"]["content"])
 
+
+# Load as Pyfunc model
 model = mlflow.pyfunc.load_model(model_info.model_uri)
 df = pd.DataFrame(
     {
