8606ce68b0c4a537cc4c96139083d434cf34a058
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-08-04 08:09:31 +0900

    Replace percent formatting with `str.format` (#9203)
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    Signed-off-by: Harutaka Kawamura <hkawamura0130@gmail.com>

--- mlflow/utils/autologging_utils/safety.py
+++ mlflow/utils/autologging_utils/safety.py
@@ -594,9 +594,9 @@ def safe_patch(
                 if is_testing() and not preexisting_run_for_testing:
                     # If an MLflow run was created during the execution of patch code, verify that
                     # it is no longer active and that it contains expected autologging tags
-                    assert not mlflow.active_run(), (
-                        "Autologging integration %s leaked an active run" % autologging_integration
-                    )
+                    assert (
+                        not mlflow.active_run()
+                    ), f"Autologging integration {autologging_integration} leaked an active run"
                     if patch_function_run_for_testing:
                         _validate_autologging_run(
                             autologging_integration, patch_function_run_for_testing.info.run_id
