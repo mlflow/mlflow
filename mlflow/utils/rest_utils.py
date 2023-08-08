b81b78adb1ad510b38e792f569dbb41b7781a794
harupy <hkawamura0130@gmail.com>
2023-08-08 17:59:46 +0900

    Fix test
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/utils/rest_utils.py
+++ mlflow/utils/rest_utils.py
@@ -104,7 +104,8 @@ def http_request(
     except requests.exceptions.Timeout as to:
         raise MlflowException(
             f"API request to {url} failed with timeout exception {to}."
-            f" To increase the timeout, set the environment variable {MLFLOW_HTTP_REQUEST_TIMEOUT}"
+            " To increase the timeout, set the environment variable "
+            f"{MLFLOW_HTTP_REQUEST_TIMEOUT!s}"
             " to a larger value."
         ) from to
     except requests.exceptions.InvalidURL as iu:
