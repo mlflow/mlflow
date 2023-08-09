a2bc596d7b71e01482be65355615af425a5388fa
santiagxf <santiagof@outlook.com>
2023-08-09 17:39:45 +0000

    test fixes
    Signed-off-by: santiagxf <santiagof@outlook.com>

--- tests/pyfunc/test_pyfunc_inference_config.py
+++ tests/pyfunc/test_pyfunc_inference_config.py
@@ -3,12 +3,12 @@ import mlflow
 import pytest
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def model_path(tmp_path):
     return os.path.join(tmp_path, "model")
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def inference_config():
     return {
         "use_gpu": True,
@@ -24,7 +24,8 @@ class TestModel(mlflow.pyfunc.PythonModel):
 
 class InferenceContextModel(mlflow.pyfunc.PythonModel):
     def predict(self, context, model_input, params=None):
-        return context.inference_config
+        # This mock class returns the internal inference configuration keys and values available
+        return context.inference_config.items()
 
 
 def test_save_with_inference_config(model_path, inference_config):
@@ -62,3 +63,15 @@ def test_override_inference_config_ignore_invalid(model_path, inference_config):
 
     assert loaded_model.predict([[5]])
     assert all(k not in loaded_model.inference_config for k in inference_override.keys())
+
+
+def test_pyfunc_without_inference_config(model_path, inference_config):
+    model = TestModel()
+    mlflow.pyfunc.save_model(model_path, python_model=model)
+
+    loaded_model = mlflow.pyfunc.load_model(
+        model_uri=model_path, inference_config=inference_config
+    )
+
+    assert loaded_model.predict([[5]])
+    assert not loaded_model.inference_config
