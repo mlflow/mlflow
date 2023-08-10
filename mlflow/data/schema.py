d898704ed4987c5113be0cda47c28054df18f4c4
harupy <hkawamura0130@gmail.com>
2023-08-10 15:42:56 +0900

    Fix
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/data/schema.py
+++ mlflow/data/schema.py
@@ -17,7 +17,7 @@ class TensorDatasetSchema:
             )
         if targets is not None and not isinstance(targets, Schema):
             raise MlflowException(
-                "targets must be either None or mlflow.types.Schema, " f"got '{type(features)}'",
+                f"targets must be either None or mlflow.types.Schema, got '{type(features)}'",
                 INVALID_PARAMETER_VALUE,
             )
         self.features = features
@@ -70,4 +70,4 @@ class TensorDatasetSchema:
         )
 
     def __repr__(self) -> str:
-        return "features: \n" f"  {repr(self.features)}\n" "targets: \n" f"  {repr(self.targets)}\n"
+        return f"features:\n  {repr(self.features)}\ntargets:\n  {repr(self.targets)}\n"
