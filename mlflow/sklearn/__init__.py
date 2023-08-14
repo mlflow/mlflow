42f2759809b05e7e67e79aa7932b76862d151976
harupy <hkawamura0130@gmail.com>
2023-08-14 16:32:20 +0900

    Add new __init__.py
    Signed-off-by: harupy <hkawamura0130@gmail.com>

---
+++ mlflow/sklearn/__init__.py
@@ -0,0 +1,29 @@
+"""
+The ``mlflow.sklearn`` module provides an API for logging and loading scikit-learn models. This
+module exports scikit-learn models with the following flavors:
+
+Python (native) `pickle <https://scikit-learn.org/stable/modules/model_persistence.html>`_ format
+    This is the main flavor that can be loaded back into scikit-learn.
+
+:py:mod:`mlflow.pyfunc`
+    Produced for use by generic pyfunc-based deployment tools and batch inference.
+    NOTE: The `mlflow.pyfunc` flavor is only added for scikit-learn models that define `predict()`,
+    since `predict()` is required for pyfunc model inference.
+"""
+from ._sklearn import (
+    get_default_pip_requirements,
+    get_default_conda_env,
+    save_model,
+    log_model,
+    load_model,
+    autolog,
+)
+
+__all__ = [
+    "get_default_pip_requirements",
+    "get_default_conda_env",
+    "save_model",
+    "log_model",
+    "load_model",
+    "autolog",
+]
