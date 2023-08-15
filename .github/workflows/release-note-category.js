38fa4c7db5846191a5b842499c94c83951c799d2
harupy <hkawamura0130@gmail.com>
2023-08-15 12:42:30 +0900

    Add post-merge processing to release-note-category workflow
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- .github/workflows/release-note-category.js
+++ .github/workflows/release-note-category.js
@@ -1,61 +1,71 @@
-module.exports = async ({ core, context, github }) => {
+async function fetchRepoLabels({ github, owner, repo }) {
+  const { data } = await github.rest.issues.listLabelsForRepo({
+    owner,
+    repo,
+    per_page: 100, // the default value is 30, which is too small to fetch all labels
+  });
+  return data.map(({ name }) => name);
+}
+
+async function fetchPrLabels({ github, owner, repo, issue_number }) {
+  const { data } = await github.rest.issues.listLabelsOnIssue({
+    owner,
+    repo,
+    issue_number,
+  });
+  return data.map(({ name }) => name);
+}
+
+function isReleaseNoteLabel(name) {
+  return name.startsWith("rn/");
+}
+
+async function validateLabeled({ core, context, github }) {
   const { user, html_url: pr_url } = context.payload.pull_request;
   const { owner, repo } = context.repo;
   const { number: issue_number } = context.issue;
 
   // Skip validation on pull requests created by the automation bot
   if (user.login === "mlflow-automation") {
-    console.log("Skipping validation since this pull request was created by the automation bot");
+    console.log("This pull request was created by the automation bot, skipping");
     return;
   }
 
-  // Fetch release note category labels
-  const labelsForRepoResp = await github.rest.issues.listLabelsForRepo({
-    owner,
-    repo,
-    per_page: 100, // the default value is 30, which is too small to fetch all labels
-  });
-  const releaseNoteLabels = labelsForRepoResp.data
-    .map(({ name }) => name)
-    .filter((name) => name.startsWith("rn/"));
-  console.log("Available release note category labels:");
-  console.log(releaseNoteLabels);
+  const repoLabels = await fetchRepoLabels({ github, owner, repo });
+  const releaseNoteLabels = repoLabels.filter(isReleaseNoteLabel);
 
   // Fetch the release-note category labels applied on this PR
-  const getAppliedLabels = async () => {
+  const fetchAppliedLabels = async () => {
     const backoffs = [0, 1, 2, 4, 8, 16];
     for (const [index, backoff] of backoffs.entries()) {
       console.log(`Attempt ${index + 1}/${backoffs.length}`);
       await new Promise((r) => setTimeout(r, backoff * 1000));
-      const listLabelsOnIssueResp = await github.rest.issues.listLabelsOnIssue({
+      const prLabels = await fetchPrLabels({
+        github,
         owner,
         repo,
         issue_number,
       });
-      const appliedLabels = listLabelsOnIssueResp.data
-        .map(({ name }) => name)
-        .filter((name) => releaseNoteLabels.includes(name));
+      const prReleaseNoteLabels = prLabels.filter((name) => releaseNoteLabels.includes(name));
 
-      if (appliedLabels.length > 0) {
-        return appliedLabels;
+      if (prReleaseNoteLabels.length > 0) {
+        return prReleaseNoteLabels;
       }
     }
     return [];
   };
 
-  const appliedLabels = await getAppliedLabels();
-  console.log("Release note category labels applied to this PR:");
-  console.log(appliedLabels);
+  const prReleaseNoteLabels = await fetchAppliedLabels();
 
   // If no release note category label is applied to this PR, set the action status to "failed"
-  if (appliedLabels.length === 0) {
+  if (prReleaseNoteLabels.length === 0) {
     // Make sure '.github/pull_request_template.md' contains an HTML anchor with this name
     const anchorName = "release-note-category";
 
     // Fragmented URL to jump to the release note category section in the PR description
     const anchorUrl = `${pr_url}#user-content-${anchorName}`;
     const message = [
-      "No release note category label is applied to this PR. ",
+      "No release-note label is applied to this PR. ",
       `Please select a checkbox in the release note category section: ${anchorUrl} `,
       "or manually apply a release note category label (e.g. 'rn/bug-fix') ",
       "if you're a maintainer of this repository. ",
@@ -64,4 +74,56 @@ module.exports = async ({ core, context, github }) => {
     ].join("");
     core.setFailed(message);
   }
+}
+
+async function postMerge({ context, github }) {
+  const { user } = context.payload.pull_request;
+  const { owner, repo } = context.repo;
+  const { number: issue_number } = context.issue;
+
+  if (user.login === "mlflow-automation") {
+    console.log("This PR was created by the automation bot, skipping");
+    return;
+  }
+
+  const repoLabels = await fetchRepoLabels({ github, owner, repo });
+  const releaseNoteLabels = repoLabels.filter(isReleaseNoteLabel);
+  const prLabels = await fetchPrLabels({
+    github,
+    owner,
+    repo,
+    issue_number,
+  });
+  const prReleaseNoteLabels = prLabels.filter((name) => releaseNoteLabels.includes(name));
+
+  if (prReleaseNoteLabels.length === 0) {
+    const pull = await github.rest.pulls.get({
+      owner,
+      repo,
+      pull_number: issue_number,
+    });
+    const { login: mergedBy } = pull.data.merged_by;
+    const noneLabel = "rn/none";
+    const body = [
+      `@${mergedBy} This PR is missing a release-note label, adding \`${noneLabel}\`. `,
+      "If this label is incorrect, please replace it with the correct label.",
+    ].join("");
+    await github.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number,
+      body,
+    });
+    await github.rest.issues.addLabels({
+      owner,
+      repo,
+      issue_number,
+      labels: [noneLabel],
+    });
+  }
+}
+
+module.exports = {
+  validateLabeled,
+  postMerge,
 };
