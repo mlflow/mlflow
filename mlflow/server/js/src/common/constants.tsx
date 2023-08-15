693b694036bac735dae810eaad9622c519550751
mlflow-automation <61449322+mlflow-automation@users.noreply.github.com>
2023-08-15 13:17:42 +0900

    Run `python3 dev/update_mlflow_versions.py pre-release ...` (#9320)
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>
    Co-authored-by: mlflow-automation <mlflow-automation@users.noreply.github.com>

--- mlflow/server/js/src/common/constants.tsx
+++ mlflow/server/js/src/common/constants.tsx
@@ -9,7 +9,7 @@ export const ErrorCodes = {
   RESOURCE_CONFLICT: 'RESOURCE_CONFLICT',
 };
 
-export const Version = '2.5.1.dev0';
+export const Version = '2.6.0';
 
 const DOCS_VERSION = 'latest';
 
