05e6cc9b8487945522c87e3f1a9ffc04eb2ecc2d
harupy <hkawamura0130@gmail.com>
2023-08-21 21:41:27 +0900

    Compile proto from root directory to avoid file name collisions
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- dev/generate-protos.sh
+++ dev/generate-protos.sh
@@ -8,8 +8,8 @@ if [[ "$PROTOC_VERSION" != 'libprotoc 3.19.4' ]]; then
 	exit 1
 fi
 PROTOS="mlflow/protos"
-protoc -I="$PROTOS" \
-    --python_out="$PROTOS" \
+protoc \
+    --python_out=. \
     --java_out="mlflow/java/client/src/main/java" \
     "$PROTOS"/databricks.proto \
     "$PROTOS"/service.proto \
@@ -21,41 +21,19 @@ protoc -I="$PROTOS" \
 
 # Separate out building UC model registry protos to avoid autogenerating
 # Java stubs, for now
-protoc -I="$PROTOS" \
-    --python_out="$PROTOS" \
+protoc \
+    --python_out=. \
     "$PROTOS"/databricks_uc_registry_messages.proto \
     "$PROTOS"/databricks_uc_registry_service.proto
 
 
 PROTOS="mlflow/protos"
-protoc -I="$PROTOS" \
-    --python_out="$PROTOS" \
+protoc \
+    --python_out=. \
     "$PROTOS"/facet_feature_statistics.proto \
 
 # Generate only Python classes (no Java classes) for test protos.
 TEST_PROTOS="tests/protos"
-protoc -I="$TEST_PROTOS" \
-    --python_out="$TEST_PROTOS" \
+protoc \
+    --python_out=. \
     "$TEST_PROTOS"/test_message.proto
-
-OLD_SCALAPB="from scalapb import scalapb_pb2 as scalapb_dot_scalapb__pb2"
-NEW_SCALAPB="from .scalapb import scalapb_pb2 as scalapb_dot_scalapb__pb2"
-sed -i'.old' -e "s/$OLD_SCALAPB/$NEW_SCALAPB/g" "$PROTOS/databricks_pb2.py" "$PROTOS/service_pb2.py" "$PROTOS/model_registry_pb2.py" "$PROTOS/databricks_artifacts_pb2.py" "$PROTOS/mlflow_artifacts_pb2.py" "$PROTOS/internal_pb2.py" "$PROTOS/databricks_uc_registry_service_pb2.py" "$PROTOS/databricks_uc_registry_messages_pb2.py" "$TEST_PROTOS/test_message_pb2.py"
-
-OLD_DATABRICKS="import databricks_pb2 as databricks__pb2"
-NEW_DATABRICKS="from . import databricks_pb2 as databricks__pb2"
-sed -i'.old' -e "s/$OLD_DATABRICKS/$NEW_DATABRICKS/g" "$PROTOS/service_pb2.py" "$PROTOS/model_registry_pb2.py" "$PROTOS/databricks_artifacts_pb2.py" "$PROTOS/mlflow_artifacts_pb2.py" "$PROTOS/internal_pb2.py" "$PROTOS/databricks_uc_registry_service_pb2.py" "$PROTOS/databricks_uc_registry_messages_pb2.py" "$TEST_PROTOS/test_message_pb2.py"
-
-OLD_DATABRICKS_UC_REGISTRY="import databricks_uc_registry_messages_pb2 as databricks__uc__registry__messages__pb2"
-NEW_DATABRICKS_UC_REGISTRY="from . import databricks_uc_registry_messages_pb2 as databricks_uc_registry_messages_pb2"
-sed -i'.old' -e "s/$OLD_DATABRICKS_UC_REGISTRY/$NEW_DATABRICKS_UC_REGISTRY/g"  "$PROTOS/databricks_uc_registry_service_pb2.py" "$PROTOS/databricks_uc_registry_messages_pb2.py"  "$TEST_PROTOS/test_message_pb2.py"
-
-rm "$PROTOS/databricks_pb2.py.old"
-rm "$PROTOS/service_pb2.py.old"
-rm "$PROTOS/model_registry_pb2.py.old"
-rm "$PROTOS/databricks_artifacts_pb2.py.old"
-rm "$PROTOS/mlflow_artifacts_pb2.py.old"
-rm "$PROTOS/internal_pb2.py.old"
-rm "$PROTOS/databricks_uc_registry_messages_pb2.py.old"
-rm "$PROTOS/databricks_uc_registry_service_pb2.py.old"
-rm "$TEST_PROTOS/test_message_pb2.py.old"
