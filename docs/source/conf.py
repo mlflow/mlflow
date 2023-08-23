1a67eb6ce030f5771da54b417a112d793f1a0d86
Vladimir Kolovski <vladimir.kolovski@databricks.com>
2023-08-23 13:05:12 -0700

    Revert changes in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,7 +81,7 @@ release = mlflow.version.VERSION
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = "en"
+language = None
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
