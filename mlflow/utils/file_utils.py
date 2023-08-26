0d304d6c99ebc7aa17d47c026954152461a90c1d
Weicheng Zhao <zhaoweicheng98@gmail.com>
2023-08-25 19:40:12 -0700

    Fix style
    Signed-off-by: Weicheng Zhao <zhaoweicheng98@gmail.com>

--- mlflow/utils/file_utils.py
+++ mlflow/utils/file_utils.py
@@ -41,7 +41,6 @@ from mlflow.utils.process import _exec_cmd, cache_return_value_per_process
 from mlflow.utils.request_utils import cloud_storage_http_request, download_chunk
 from mlflow.utils.rest_utils import augmented_raise_for_status
 
-
 ENCODING = "utf-8"
 # Default to 2 if os.cpu_count() returns None (e.g. when run in a virtualenv). This is consistent.
 MAX_PARALLEL_DOWNLOAD_WORKERS = (os.cpu_count() or 1) * 2
