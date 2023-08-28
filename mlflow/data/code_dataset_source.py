8ddbf62e7d9cb33eb2ab1a59b0744cd44ae80ec8
Jeyveen Bhoyroo <Jeyveen7@users.noreply.github.com>
2023-08-28 17:17:42 +0400

    Extend CodeDatasetSource to download data from URLs and save as Parquet on DBFS
    This commit enhances the `CodeDatasetSource` class, enabling it to fetch data directly from external URLs and store the content as a Parquet file on the Databricks File System (DBFS).
    
    Changes:
    - Adapted the `load` method in the `CodeDatasetSource` class to support Parquet file storage on DBFS.
    - The `load` method now converts downloaded data to a DataFrame and subsequently saves it as a Parquet file on DBFS.
    - Integrated the `requests` and `pandas` libraries for data retrieval and processing.
    - Enhanced error handling for better user feedback.
    
    This update streamlines data ingestion, allowing users to efficiently fetch datasets from external sources, convert them to the Parquet format, and store them on DBFS for optimized analytics in MLflow experiments on Databricks.
    
    
    Signed-off-by: Jeyveen Bhoyroo <Jeyveen7@users.noreply.github.com>

--- mlflow/data/code_dataset_source.py
+++ mlflow/data/code_dataset_source.py
@@ -8,9 +8,32 @@ from mlflow.utils.annotations import experimental
 class CodeDatasetSource(DatasetSource):
     def __init__(
         self,
-        tags: Dict[Any, Any],
+        path: str,
+        config_name: Optional[str] = None,
+        data_dir: Optional[str] = None,
+        data_files: Optional[
+            Union[str, Sequence[str], Mapping[str, Union[str, Sequence[str]]]]
+        ] = None,
+        split: Optional[str] = None,
+        revision: Optional[str] = None,
+        task: Optional[str] = None,
     ):
-        self._tags = tags
+        """
+        :param path: The path of the code dataset.
+        :param config_name: The name of the code dataset configuration.
+        :param data_dir: The `data_dir` of the code dataset configuration.
+        :param data_files: Paths to source data file(s) for the code dataset configuration.
+        :param split: The split of the code dataset.
+        :param revision: Version of the code dataset script to load.
+        :param task: The task to prepare the code dataset for during training and evaluation.
+        """
+        self._path = path
+        self._config_name = config_name
+        self._data_dir = data_dir
+        self._data_files = data_files
+        self._split = split
+        self._revision = revision
+        self._task = task
 
     @staticmethod
     def _get_source_type() -> str:
@@ -18,9 +41,36 @@ class CodeDatasetSource(DatasetSource):
 
     def load(self, **kwargs):
         """
-        Load is not implemented for Code Dataset Source.
+        Load the code dataset based on the provided parameters.
+
+        :param kwargs: Additional parameters to customize the loading process.
+        :return: Path to the saved Parquet file on DBFS.
         """
-        raise NotImplementedError
+        # Load from DBFS (Databricks File System)
+        dbfs_path = kwargs.get('dbfs_path')
+        if dbfs_path:
+            with open(dbfs_path, 'r') as file:
+                data = file.read()
+            return data
+
+        # Download from a URL, convert to DataFrame, and save to DBFS as Parquet
+        url = kwargs.get('url')
+        dbfs_save_path = kwargs.get('dbfs_save_path', '/dbfs/downloaded_data.parquet')  # default DBFS save path
+        if url:
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an error for bad responses
+            
+            # Assuming the data is in CSV format; adjust if different
+            data = io.StringIO(response.text)
+            df = pd.read_csv(data)
+            
+            # Save DataFrame as Parquet to DBFS
+            df.to_parquet(dbfs_save_path)
+            
+            return dbfs_save_path
+
+        raise ValueError("Provide either a valid dbfs_path or url to load data.")
+
 
     @staticmethod
     def _can_resolve(raw_source: Any):
