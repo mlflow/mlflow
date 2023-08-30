9fcb12c517d0f3f426bd4d70b73706efd0fb0bdb
mgbckr <1241516+mgbckr@users.noreply.github.com>
2023-08-30 10:11:32 +0200

    Type hint Union to Optional

--- mlflow/tracking/_tracking_service/utils.py
+++ mlflow/tracking/_tracking_service/utils.py
@@ -3,7 +3,7 @@ import os
 from contextlib import contextmanager
 from functools import partial
 from pathlib import Path
-from typing import Union
+from typing import Optional, Union
 
 from mlflow.environment_variables import (
     MLFLOW_TRACKING_AWS_SIGV4,
@@ -95,7 +95,7 @@ def _resolve_tracking_uri(tracking_uri=None):
     return tracking_uri or get_tracking_uri()
 
 
-def get_tracking_uri(tracking_uri:Union[str,None]=None) -> str:
+def get_tracking_uri(tracking_uri:Optional[str]=None) -> str:
     """
     Get the current tracking URI. This may not correspond to the tracking URI of
     the currently active run, since the tracking URI can be updated via ``set_tracking_uri``.
