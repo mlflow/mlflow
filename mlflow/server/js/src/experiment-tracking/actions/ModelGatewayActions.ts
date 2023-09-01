a27f33ee2a7072ad321795d4bb22afc2e4fdc7d1
Prithvi Kannan <prithvi.kannan@databricks.com>
2023-09-01 14:03:39 -0700

    lint fix
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- mlflow/server/js/src/experiment-tracking/actions/ModelGatewayActions.ts
+++ mlflow/server/js/src/experiment-tracking/actions/ModelGatewayActions.ts
@@ -32,7 +32,7 @@ export interface GetModelGatewayRouteAction extends AsyncAction<ModelGatewayRout
 export const getModelGatewayRouteApi = (routeName: string): GetModelGatewayRouteAction => ({
   type: GET_MODEL_GATEWAY_ROUTE_API,
   payload: MlflowService.gatewayProxyGet({
-    gateway_path: `api/2.0/gateway/routes/${routeName}`
+    gateway_path: `api/2.0/gateway/routes/${routeName}`,
   }) as Promise<ModelGatewayRoute>,
   meta: { id: getUUID() },
 });
@@ -43,7 +43,6 @@ export const queryModelGatewayRouteApi = (
   route: ModelGatewayRoute,
   data: ModelGatewayQueryPayload,
 ) => {
-  
   const { inputText } = data;
   const textPayload = ModelGatewayService.createEvaluationTextPayload(inputText, route);
   const processed_data = {
@@ -55,7 +54,7 @@ export const queryModelGatewayRouteApi = (
     type: QUERY_MODEL_GATEWAY_ROUTE_API,
     payload: MlflowService.gatewayProxyPost({
       gateway_path: `gateway/${route.name}/invocations`,
-      json_data: processed_data ,
+      json_data: processed_data,
     }) as Promise<ModelGatewayResponseType>,
     meta: { id: getUUID(), startTime: performance.now() },
   };
