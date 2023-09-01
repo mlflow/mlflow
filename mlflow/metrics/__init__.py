fa3c85ec3a001691a152503cd3410f07e7f3e05d
Ann Zhang <ann.zhang@databricks.com>
2023-09-01 11:21:48 -0700

    Create/update classes for metrics (#9510)
    Signed-off-by: Ann Zhang <ann.zhang@databricks.com>

---
+++ mlflow/metrics/__init__.py
@@ -0,0 +1,13 @@
+from mlflow.metrics.base import (
+    MetricValue,
+)
+from mlflow.models import (
+    EvaluationMetric,
+    make_metric,
+)
+
+__all__ = [
+    "EvaluationMetric",
+    "MetricValue",
+    "make_metric",
+]
