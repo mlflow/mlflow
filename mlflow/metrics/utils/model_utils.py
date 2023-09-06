2c991add07b3d8a41ee8727fe9ef8828a089fd93
Prithvi Kannan <prithvi.kannan@databricks.com>
2023-09-06 10:14:23 -0700

    multiplexer fn score_model_on_payload
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- mlflow/metrics/utils/model_utils.py
+++ mlflow/metrics/utils/model_utils.py
@@ -1,4 +1,28 @@
 import os
+import re
+
+
+# TODO: improve this name
+def score_model_on_payload(model_uri, payload):
+    # Use re.split to split by the first occurrence of ":/"
+    parts = re.split(":/+", model_uri, maxsplit=1)
+
+    if len(parts) != 2:
+        # TODO: raise an exception
+        raise ValueError(f"Invalid model uri '{model_uri}'")
+
+    prefix, _ = parts
+
+    if prefix == "openai":
+        return call_openai_api(model_uri, payload)
+    elif prefix == "gateway":
+        # TODO: replace with call_gateway_api
+        pass
+    elif prefix == "model":
+        # TODO: replace with call_pyfunc_model_api
+        pass
+    else:
+        raise ValueError(f"Unknown model uri prefix '{prefix}'")
 
 
 def call_openai_api(openai_uri, payload):
