5cc0e437b593d7c403654a3a55086c5f9a4d85e6
Prithvi Kannan <prithvi.kannan@databricks.com>
2023-09-06 09:59:44 -0700

    call_openai_api fn
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- mlflow/metrics/utils/model_utils.py
+++ mlflow/metrics/utils/model_utils.py
@@ -1,3 +1,28 @@
-def get_openai_model(openai_uri):
-    # wrapper function to call the openai model
-    raise NotImplementedError
+import os
+
+
+def call_openai_api(openai_uri, payload):
+    """Wrapper around the OpenAI API to make it compatible with the MLflow Gateway API."""
+    from mlflow.gateway.config import Model, OpenAIConfig, RouteConfig
+    from mlflow.gateway.providers.openai import OpenAIProvider
+
+    # TODO: extract the model name from the uri
+    model_name = "gpt-3.5-turbo"
+
+    # TODO: extract the route type from the uri
+    route_type = "llm/v1/completions"
+
+    route_config = RouteConfig(
+        name="openai",
+        route_type=route_type,
+        model=Model(
+            name=model_name,
+            provider="openai",
+            config=OpenAIConfig(
+                openai_api_key=os.environ["OPENAI_API_KEY"],
+            ),
+        ),
+    )
+    openai_provider = OpenAIProvider(route_config)
+
+    return openai_provider.completions(payload)
