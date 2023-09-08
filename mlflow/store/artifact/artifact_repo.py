4af1ef9648402a4bac54f67de891a229d1ccbddd
harupy <hkawamura0130@gmail.com>
2023-09-08 20:46:10 +0900

    Fix truncation message
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/store/artifact/artifact_repo.py
+++ mlflow/store/artifact/artifact_repo.py
@@ -30,7 +30,7 @@ def _truncate_error(err: str, max_length: int = 10_000) -> str:
         return err
 
     half = max_length // 2
-    return err[:half] + "\n\n... (truncated)\n\n" + err[-half:]
+    return err[:half] + "\n\n*** Error message is too large, truncated ***\n\n" + err[-half:]
 
 
 @developer_stable
