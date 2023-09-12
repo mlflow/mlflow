3ecb87c3b082bf63a65cc123c2e4e17be5707f98
harupy <hkawamura0130@gmail.com>
2023-09-12 11:56:57 +0900

    Remove pkg_resources
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/utils/requirements_utils.py
+++ mlflow/utils/requirements_utils.py
@@ -17,7 +17,6 @@ from threading import Timer
 from typing import NamedTuple, Optional
 
 import importlib_metadata
-import pkg_resources  # noqa: TID251
 from packaging.requirements import Requirement
 from packaging.version import InvalidVersion, Version
 
