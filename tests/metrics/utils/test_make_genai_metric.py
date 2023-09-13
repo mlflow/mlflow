9b880ad5f7bcd59d4a7f88b9a7a26b35829c9c40
Sunish Sheth <sunishsheth2009@gmail.com>
2023-09-13 12:44:38 -0700

    [LLM eval] Updating code to use openai/gateway rather than pyfunc for judge predictions (#9576)
    Signed-off-by: Sunish Sheth <sunishsheth2009@gmail.com>

--- tests/metrics/utils/test_make_genai_metric.py
+++ tests/metrics/utils/test_make_genai_metric.py
@@ -1,35 +1,27 @@
 import re
+from unittest import mock
 
 import pandas as pd
 import pytest
 
-import mlflow
 from mlflow.exceptions import MlflowException
 from mlflow.metrics.base import EvaluationExample
+from mlflow.metrics.utils import model_utils
 from mlflow.metrics.utils.make_genai_metric import _format_variable_string, make_genai_metric
 
-
 # Create a custom mock class for MyModel
-class FakeModel(mlflow.pyfunc.PythonModel):
-    def predict(self, context, model_input, params=None):
-        return pd.DataFrame(
-            {
-                "Score": [3],
-                "Justification": [
-                    "The definition effectively explains what MLflow is "
-                    "its purpose, and its developer. It could be more concise for a 5-score."
-                ],
-            }
-        )
+predict_return_value = pd.DataFrame(
+    {
+        "Score": [3],
+        "Justification": [
+            "The definition effectively explains what MLflow is "
+            "its purpose, and its developer. It could be more concise for a 5-score."
+        ],
+    }
+)
 
 
-def test_make_genai_metric_pyfunc_success():
-    with mlflow.start_run():
-        llm_model_info = mlflow.pyfunc.log_model(
-            "model",
-            python_model=FakeModel(),
-        )
-
+def test_make_genai_metric_success():
     example = EvaluationExample(
         input="What is MLflow?",
         output="MLflow is an open-source platform for managing machine "
@@ -65,7 +57,7 @@ def test_make_genai_metric_pyfunc_success():
         "critical aspect. "
         "- Score 4: the answer correctly answer the question and not missing any major aspect",
         examples=[example],
-        model=llm_model_info.model_uri,
+        model="gateway:/gpt-3.5-turbo",
         variables=["ground_truth"],
         parameters={"temperature": 1.0},
         greater_is_better=True,
@@ -90,20 +82,77 @@ def test_make_genai_metric_pyfunc_success():
         }
     )
 
-    metric_value = custom_metric.eval_fn(eval_df)
+    with mock.patch.object(
+        model_utils,
+        "score_model_on_payload",
+        return_value=predict_return_value,
+    ):
+        metric_value = custom_metric.eval_fn(eval_df)
 
     assert metric_value.scores == [3]
-    assert metric_value.justifications == [
-        "The definition effectively "
-        "explains what MLflow is its purpose, and its developer. It could be "
-        "more concise for a 5-score."
-    ]
+    assert metric_value.justifications == predict_return_value["Justification"].tolist()
 
     assert metric_value.aggregate_results == {
         "mean": 3,
         "variance": 0,
         "p90": 3,
     }
+
+    custom_metric = make_genai_metric(
+        name="fake_metric",
+        version="v1",
+        definition="Fake metric definition",
+        grading_prompt="Fake metric grading prompt",
+        examples=[
+            EvaluationExample(
+                input="example-input",
+                output="example-output",
+                score=4,
+                justification="example-justification",
+                variables={"ground_truth": "example-ground_truth"},
+            )
+        ],
+        model="openai:/gpt-3.5-turbo",
+        variables=["ground_truth"],
+        greater_is_better=True,
+        aggregations=["mean", "variance", "p90"],
+    )
+    eval_df = pd.DataFrame(
+        {
+            "input": ["input"],
+            "prediction": ["prediction"],
+            "ground_truth": ["ground_truth"],
+        }
+    )
+    with mock.patch.object(
+        model_utils,
+        "score_model_on_payload",
+        return_value=predict_return_value,
+    ) as mock_predict_function:
+        metric_value = custom_metric.eval_fn(eval_df)
+        assert mock_predict_function.call_count == 1
+        assert mock_predict_function.call_args[0][0] == "openai:/gpt-3.5-turbo"
+        assert mock_predict_function.call_args[0][1] == [
+            {
+                "prompt": "\nPlease act as an impartial judge and evaluate the quality of "
+                "the provided output which\nattempts to produce output for the provided input "
+                "based on a provided information.\nYou'll be given a grading format below which "
+                "you'll call for each provided information,\ninput and provided output to submit "
+                "your justification and score to compute the fake_metric of\nthe output."
+                "\n\nInput:\ninput\n\nProvided output:\nprediction\n\nProvided ground_truth: "
+                "ground_truth\n\nMetric definition:\nFake metric definition\n\nGrading criteria:"
+                "\nFake metric grading prompt\n\nExamples:\n\nInput: example-input\nProvided "
+                "output: example-output\nProvided ground_truth: example-ground_truth\nScore: "
+                "4\nJustification: example-justification\n\n        \n\nAnd you'll need to submit "
+                "your grading for the fake_metric of the output,\nusing the following in json "
+                "format:\nScore: [your score number between 0 to 4 for the fake_metric of the "
+                "output]\nJustification: [your step by step reasoning about the fake_metric of the "
+                "output]\n    ",
+                "temperature": 0.0,
+                "max_tokens": 100,
+                "top_p": 1.0,
+            }
+        ]
 
 
 def test_make_genai_metric_failure():
@@ -166,28 +215,28 @@ def test_make_genai_metric_failure():
     ):
         custom_metric2.eval_fn(eval_df)
 
-    with mlflow.start_run():
-        llm_model_info = mlflow.pyfunc.log_model(
-            "model",
-            python_model=FakeModel(),
-        )
-    custom_metric3 = make_genai_metric(
-        name="correctness",
-        version="v1",
-        definition="definition",
-        grading_prompt="grading_prompt",
-        examples=[example],
-        model=llm_model_info.model_uri,
-        variables=["ground_truth"],
-        parameters={"temperature": 1.0},
-        greater_is_better=True,
-        aggregations=["random-fake"],
-    )
-    with pytest.raises(
-        MlflowException,
-        match=re.escape("Invalid aggregate option random-fake"),
+    with mock.patch.object(
+        model_utils,
+        "score_model_on_payload",
+        return_value=predict_return_value,
     ):
-        custom_metric3.eval_fn(eval_df)
+        custom_metric3 = make_genai_metric(
+            name="correctness",
+            version="v1",
+            definition="definition",
+            grading_prompt="grading_prompt",
+            examples=[example],
+            model="openai:/gpt-3.5-turbo",
+            variables=["ground_truth"],
+            parameters={"temperature": 1.0},
+            greater_is_better=True,
+            aggregations=["random-fake"],
+        )
+        with pytest.raises(
+            MlflowException,
+            match=re.escape("Invalid aggregate option random-fake"),
+        ):
+            custom_metric3.eval_fn(eval_df)
 
 
 def test_format_variable_string():
