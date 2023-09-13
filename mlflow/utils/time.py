cc784b7a3f41a063d2a7d05818866dff7584eabd
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-09-13 15:17:15 +0900

    Add `Timer` for quick benchmarking (#9607)
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/utils/time.py
+++ mlflow/utils/time.py
@@ -18,3 +18,37 @@ def conv_longdate_to_str(longdate, local_tz=True):
         str_long_date += " " + reference.LocalTimezone().tzname(date_time)
 
     return str_long_date
+
+
+class Timer:
+    """
+    Measures elapsed time.
+
+    .. code-block:: python
+
+        from mlflow.utils.time import Timer
+
+        with Timer() as t:
+            ...
+
+        print(f"Elapsed time: {t:.2f} seconds")
+    """
+
+    def __init__(self):
+        self.elapsed = 0.0
+
+    def __enter__(self):
+        self.elapsed = time.perf_counter()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.elapsed = time.perf_counter() - self.elapsed
+
+    def __format__(self, format_spec: str) -> str:
+        return self.elapsed.__format__(format_spec)
+
+    def __repr__(self) -> str:
+        return self.elapsed.__repr__()
+
+    def __str__(self) -> str:
+        return self.elapsed.__str__()
