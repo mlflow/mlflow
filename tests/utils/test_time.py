e7f29b9b32fdd3a1410a0dce5f07473c54e21fce
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-09-13 15:17:15 +0900

    Add `Timer` for quick benchmarking (#9607)
    Signed-off-by: harupy <hkawamura0130@gmail.com>

---
+++ tests/utils/test_time.py
@@ -0,0 +1,12 @@
+import time
+
+from mlflow.utils.time import Timer
+
+
+def test_timer():
+    with Timer() as t:
+        time.sleep(0.1)
+
+    assert str(t).startswith("0.1")
+    assert f"{t}" == f"{t.elapsed}"
+    assert f"{t:.3f}" == f"{t.elapsed:.3f}"
