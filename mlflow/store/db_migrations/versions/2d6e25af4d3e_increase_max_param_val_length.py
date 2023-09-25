1b20f67075734bf9885cc53036c28967be16daee
Serena Ruan <serena.rxy@gmail.com>
2023-09-25 15:23:21 +0800

    update max length of params to 6k
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

---
+++ mlflow/store/db_migrations/versions/2d6e25af4d3e_increase_max_param_val_length.py
@@ -0,0 +1,31 @@
+"""increase max param val length from 500 to 6000
+
+Revision ID: 2d6e25af4d3e
+Revises: 7f2a7d5fae7d
+Create Date: 2023-09-25 13:59:04.231744
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "2d6e25af4d3e"
+down_revision = "7f2a7d5fae7d"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table("params") as batch_op:
+        batch_op.alter_column(
+            "value",
+            existing_type=sa.String(500),
+            type_=sa.String(6000),
+            existing_nullable=False,
+            existing_server_default=None,
+        )
+
+
+def downgrade():
+    pass
