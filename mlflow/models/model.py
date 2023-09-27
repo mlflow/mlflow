2cfcac387bf0b238d0d2ee23806bcdb427e03fc3
Serena Ruan <serena.rxy@gmail.com>
2023-09-27 14:33:20 +0800

    fix
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- mlflow/models/model.py
+++ mlflow/models/model.py
@@ -544,15 +544,16 @@ class Model:
                             train = df.drop_column("target_label")
                             signature = infer_signature(train, model.predict(train))
 
-        :param input_example: one or several instances of valid model input. The input example is used
-                            as a hint of what data to feed the model. It will be converted to a Pandas
-                            DataFrame and then serialized to json using the Pandas split-oriented
-                            format, or a numpy array where the example will be serialized to json
-                            by converting it to a list. If input example is a tuple, then the first element
-                            should be a valid model input, and the second element should be a valid params
-                            dictionary that could be used for model inference. Bytes are base64-encoded.
-                            When the ``signature`` parameter is ``None``, the input example is used to
-                            infer a model signature.
+        :param input_example: one or several instances of valid model input. The input example is
+                            used as a hint of what data to feed the model. It will be converted to
+                            a Pandas DataFrame and then serialized to json using the Pandas
+                            split-oriented format, or a numpy array where the example will be
+                            serialized to json by converting it to a list. If input example is a
+                            tuple, then the first element should be a valid model input, and the
+                            second element should be a valid params dictionary that could be used
+                            for model inference. Bytes are base64-encoded. When the ``signature``
+                            parameter is ``None``, the input example is used to infer a model
+                            signature.
 
         :param await_registration_for: Number of seconds to wait for the model version to finish
                             being created and is in ``READY`` status. By default, the function
