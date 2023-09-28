94316a0d4f17a34d7f3c7adc7f708f981d4c4ad8
Jerry Liang <66143562+jerrylian-db@users.noreply.github.com>
2023-09-28 09:49:19 -0700

    Customize model version creation await functionality (#9706)
    Signed-off-by: Jerry Liang <jerry.liang@databricks.com>
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>
    Signed-off-by: Jerry Liang <66143562+jerrylian-db@users.noreply.github.com>
    Co-authored-by: mlflow-automation <mlflow-automation@users.noreply.github.com>

--- mlflow/store/model_registry/abstract_store.py
+++ mlflow/store/model_registry/abstract_store.py
@@ -1,6 +1,14 @@
+import logging
 from abc import ABCMeta, abstractmethod
+from time import sleep, time
 
+from mlflow.entities.model_registry.model_version_status import ModelVersionStatus
+from mlflow.exceptions import MlflowException
 from mlflow.utils.annotations import developer_stable
+
+_logger = logging.getLogger(__name__)
+
+AWAIT_MODEL_VERSION_CREATE_SLEEP_INTERVAL_SECONDS = 3
 
 
 @developer_stable
@@ -312,3 +320,35 @@ class AbstractStore:
         :return: A single :py:class:`mlflow.entities.model_registry.ModelVersion` object.
         """
         pass
+
+    def _await_model_version_creation(self, mv, await_creation_for):
+        """
+        Await for model version to become ready after creation.
+
+        :param mv: A :py:class:`mlflow.entities.model_registry.ModelVersion` object.
+        :param await_creation_for: Number of seconds to wait for the model version to finish being
+                                    created and is in ``READY`` status.
+        """
+        self._await_model_version_creation_impl(mv, await_creation_for)
+
+    def _await_model_version_creation_impl(self, mv, await_creation_for, hint=""):
+        _logger.info(
+            f"Waiting up to {await_creation_for} seconds for model version to finish creation. "
+            f"Model name: {mv.name}, version {mv.version}",
+        )
+        max_time = time() + await_creation_for
+        pending_status = ModelVersionStatus.to_string(ModelVersionStatus.PENDING_REGISTRATION)
+        while mv.status == pending_status:
+            if time() > max_time:
+                raise MlflowException(
+                    f"Exceeded max wait time for model name: {mv.name} version: {mv.version} "
+                    f"to become READY. Status: {mv.status} Wait Time: {await_creation_for}"
+                    f".{hint}"
+                )
+            mv = self.get_model_version(mv.name, mv.version)
+            sleep(AWAIT_MODEL_VERSION_CREATE_SLEEP_INTERVAL_SECONDS)
+        if mv.status != ModelVersionStatus.to_string(ModelVersionStatus.READY):
+            raise MlflowException(
+                f"Model version creation failed for model name: {mv.name} version: "
+                f"{mv.version} with status: {mv.status} and message: {mv.status_message}"
+            )
