470d696870a6b8b478a2be67adccca6eb430eb69
Serena Ruan <serena.rxy@gmail.com>
2023-09-28 17:25:32 +0800

    fix
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- tests/db/entrypoint.sh
+++ tests/db/entrypoint.sh
@@ -7,9 +7,7 @@ if [ "$INSTALL_MLFLOW_FROM_REPO" = "true" ]; then
 fi
 
 # For Microsoft SQL server, wait until the database is up and running
-if [ "$INSTALL_ODBC_DRIVER_18" = "true" ]; then
-  ./tests/db/init-mssql18-db.sh
-elif [[ $MLFLOW_TRACKING_URI == mssql* ]]; then
+if [[ $MLFLOW_TRACKING_URI == mssql* ]]; then
   ./tests/db/init-mssql-db.sh
 fi
 
