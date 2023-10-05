95e168ce6bf91cef86bcb3027b2f7fcfc078d70d
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-10-05 09:34:25 +0900

    merge fix langchain dev tests
    Signed-off-by: Ann Zhang <ann.zhang@databricks.com>

--- mlflow/langchain/__init__.py
+++ mlflow/langchain/__init__.py
@@ -331,7 +331,11 @@ def save_model(
 
 
 def _validate_and_wrap_lc_model(lc_model, loader_fn):
-    import langchain
+    import langchain.agents
+    import langchain.chains
+    import langchain.llms.huggingface_hub
+    import langchain.llms.openai
+    import langchain.schema
 
     if not isinstance(
         lc_model,
