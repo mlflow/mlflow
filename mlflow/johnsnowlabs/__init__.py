f4b5c0e796930d87d75d2a416ee855b1c8d39771
Christian Kasim Loan <christian.kasim.loan@gmail.com>
2023-10-07 20:22:03 +0200

    remove usage of mlflowdbfs
    Signed-off-by: Christian Kasim Loan <christian.kasim.loan@gmail.com>

--- mlflow/johnsnowlabs/__init__.py
+++ mlflow/johnsnowlabs/__init__.py
@@ -290,19 +290,22 @@ def log_model(
     run_id = mlflow.tracking.fluent._get_or_start_run().info.run_id
     run_root_artifact_uri = mlflow.get_artifact_uri()
     remote_model_path = None
-    if _should_use_mlflowdbfs(run_root_artifact_uri):
-        remote_model_path = append_to_uri_path(
-            run_root_artifact_uri, artifact_path, _JOHNSNOWLABS_MODEL_PATH_SUB
-        )
-        mlflowdbfs_path = _mlflowdbfs_path(run_id, artifact_path)
-        with databricks_utils.MlflowCredentialContext(
-            get_databricks_profile_uri_from_artifact_uri(run_root_artifact_uri)
-        ):
-            try:
-                _unpack_and_save_model(spark_model, mlflowdbfs_path)
 
-            except Exception as e:
-                raise MlflowException("failed to save johnsnowlabs model via mlflowdbfs") from e
+    # commented for now because spark flavor is mlflowdbfs it, but it has many issues on databricks
+    # See https://github.com/mlflow/mlflow/issues/9869
+    # if _should_use_mlflowdbfs(run_root_artifact_uri):
+    #     remote_model_path = append_to_uri_path(
+    #         run_root_artifact_uri, artifact_path, _JOHNSNOWLABS_MODEL_PATH_SUB
+    #     )
+    #     mlflowdbfs_path = _mlflowdbfs_path(run_id, artifact_path)
+    #     with databricks_utils.MlflowCredentialContext(
+    #         get_databricks_profile_uri_from_artifact_uri(run_root_artifact_uri)
+    #     ):
+    #         try:
+    #             _unpack_and_save_model(spark_model, mlflowdbfs_path)
+    #
+    #         except Exception as e:
+    #             raise MlflowException("failed to save johnsnowlabs model via mlflowdbfs") from e
 
     # If the artifact URI is a local filesystem path, defer to Model.log() to persist the model,
     # since Spark may not be able to write directly to the driver's filesystem. For example,
@@ -310,7 +313,7 @@ def log_model(
     # be incorrect on multi-node clusters.
     # If the artifact URI is not a local filesystem path we attempt to write directly to the
     # artifact repo via Spark. If this fails, we defer to Model.log().
-    elif is_local_uri(run_root_artifact_uri) or not _maybe_save_model(
+    if is_local_uri(run_root_artifact_uri) or not _maybe_save_model(
         spark_model,
         append_to_uri_path(run_root_artifact_uri, artifact_path),
     ):
