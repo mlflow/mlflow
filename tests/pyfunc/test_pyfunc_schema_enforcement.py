ee144943b88224bad669c76f0af26872a58410c6
Serena Ruan <serena.rxy@gmail.com>
2023-10-08 11:34:47 +0800

    fix infer signature from List[str]
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- tests/pyfunc/test_pyfunc_schema_enforcement.py
+++ tests/pyfunc/test_pyfunc_schema_enforcement.py
@@ -1898,13 +1898,11 @@ def test_pyfunc_model_input_example_with_params(sample_params_basic, param_schem
         model_info = mlflow.pyfunc.log_model(
             python_model=MyModel(),
             artifact_path="test_model",
-            input_example=(["input1"], sample_params_basic),
+            input_example=(["input1", "input2", "input3"], sample_params_basic),
         )
 
     # Test _infer_signature_from_input_example
-    # TODO: add this check back once we updated
-    # _infer_signature_from_input_example for List[str]
-    # assert model_info.signature.inputs == Schema([ColSpec(DataType.string)])
+    assert model_info.signature.inputs == Schema([ColSpec(DataType.string)])
     assert model_info.signature.outputs == Schema([ColSpec(DataType.string)])
     assert model_info.signature.params == param_schema_basic
 
