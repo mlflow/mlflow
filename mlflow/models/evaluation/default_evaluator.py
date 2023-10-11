bea4cb212061e4748b2f1f1eca77cd23cc3907d2
Prithvi Kannan <prithvi.kannan@databricks.com>
2023-10-10 22:23:59 -0700

    Optional latency in evaluate
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- mlflow/models/evaluation/default_evaluator.py
+++ mlflow/models/evaluation/default_evaluator.py
@@ -1235,15 +1235,57 @@ class DefaultEvaluator(ModelEvaluator):
             )
         return
 
-    def _generate_model_predictions(self):
+    def _generate_model_predictions(self, compute_latency=False):
         """
         Helper method for generating model predictions
         """
+
+        def predict_with_latency(X_copy):
+            y_pred_list = []
+            pred_latencies = []
+            if len(X_copy) == 0:
+                raise ValueError("Empty input data")
+
+            is_dataframe = isinstance(X_copy, pd.DataFrame)
+
+            for row in X_copy.iterrows() if is_dataframe else enumerate(X_copy):
+                i, row_data = row
+                single_input = row_data.to_frame().T if is_dataframe else row_data
+                start_time = time.time()
+                y_pred = self.model.predict(single_input)
+                end_time = time.time()
+                pred_latencies.append(end_time - start_time)
+                y_pred_list.append(y_pred)
+
+            # Update latency metric
+            self.metrics_values.update({_LATENCY_METRIC_NAME: MetricValue(scores=pred_latencies)})
+
+            # Aggregate all predictions into model_predictions
+            sample_pred = y_pred_list[0]
+            if isinstance(sample_pred, pd.DataFrame):
+                return pd.concat(y_pred_list)
+            elif isinstance(sample_pred, np.ndarray):
+                return np.concatenate(y_pred_list, axis=0)
+            elif isinstance(sample_pred, list):
+                return sum(y_pred_list, [])
+            elif isinstance(sample_pred, pd.Series):
+                return pd.concat(y_pred_list)
+            else:
+                raise MlflowException(
+                    message=f"Unsupported prediction type {type(sample_pred)} for model type "
+                    f"{self.model_type}.",
+                    error_code=INVALID_PARAMETER_VALUE,
+                )
+
         if self.model_type == _ModelType.CLASSIFIER:
             self.label_list = np.unique(self.y)
             self.num_classes = len(self.label_list)
 
-            model_predictions = self.predict_fn(self.X.copy_to_avoid_mutation())
+            X_copy = self.X.copy_to_avoid_mutation()
+            if compute_latency:
+                model_predictions = predict_with_latency(X_copy)
+            else:
+                model_predictions = self.model.predict(X_copy)
             self.is_binomial = self.num_classes <= 2
 
             if self.is_binomial:
@@ -1273,17 +1315,19 @@ class DefaultEvaluator(ModelEvaluator):
             _ModelType.TEXT_SUMMARIZATION,
             _ModelType.TEXT,
         ):
-            y_pred_list = []
-            pred_latencies = []
-            num_tokens_list = []
             X_copy = self.X.copy_to_avoid_mutation()
+            if compute_latency:
+                model_predictions = predict_with_latency(X_copy)
+            else:
+                model_predictions = self.model.predict(X_copy)
 
             import tiktoken
 
             # ref: https://github.com/openai/tiktoken/issues/75
             os.environ["TIKTOKEN_CACHE_DIR"] = ""
-
             encoding = tiktoken.get_encoding("cl100k_base")
+
+            num_tokens_list = []
 
             def compute_num_tokens(y_pred):
                 # parse out the output from y_pred
@@ -1299,45 +1343,19 @@ class DefaultEvaluator(ModelEvaluator):
                 else:
                     return None
 
-            if len(X_copy) == 0:
-                raise ValueError("Empty input data")
-
-            is_dataframe = isinstance(X_copy, pd.DataFrame)
-
-            for row in X_copy.iterrows() if is_dataframe else enumerate(X_copy):
-                i, row_data = row
-                single_input = row_data.to_frame().T if is_dataframe else row_data
-                start_time = time.time()
-                y_pred = self.model.predict(single_input)
-                end_time = time.time()
+            for y_pred in model_predictions:
                 num_tokens_list.append(compute_num_tokens(y_pred))
-                pred_latencies.append(end_time - start_time)
-                y_pred_list.append(y_pred)
 
-            # Aggregate all predictions into model_predictions
-            sample_pred = y_pred_list[0]
-            if isinstance(sample_pred, pd.DataFrame):
-                model_predictions = pd.concat(y_pred_list)
-            elif isinstance(sample_pred, np.ndarray):
-                model_predictions = np.concatenate(y_pred_list, axis=0)
-            elif isinstance(sample_pred, list):
-                model_predictions = sum(y_pred_list, [])
-            # handle if sample_pred is a pandas series
-            elif isinstance(sample_pred, pd.Series):
-                model_predictions = pd.concat(y_pred_list)
-            else:
-                raise MlflowException(
-                    message=f"Unsupported prediction type {type(sample_pred)} for model type "
-                    f"{self.model_type}.",
-                    error_code=INVALID_PARAMETER_VALUE,
-                )
-
-            self.metrics_values.update({_LATENCY_METRIC_NAME: MetricValue(scores=pred_latencies)})
             self.metrics_values.update(
                 {_TOKEN_COUNT_METRIC_NAME: MetricValue(scores=num_tokens_list)}
             )
+
         else:
-            model_predictions = self.model.predict(self.X.copy_to_avoid_mutation())
+            X_copy = self.X.copy_to_avoid_mutation()
+            if compute_latency:
+                model_predictions = predict_with_latency(X_copy)
+            else:
+                model_predictions = self.model.predict(X_copy)
 
         output_column_name = self.evaluator_config.get(_Y_PREDICTED_OUTPUT_COLUMN_NAME, "output")
         self.y_pred, self.other_output_columns = _extract_output_and_other_columns(
