7cc5ba4f8f0092e4014448bd19cb65e4c309a559
Ann Zhang <ann.zhang@databricks.com>
2023-10-12 23:14:10 -0700

    rework whitespace
    Signed-off-by: Ann Zhang <ann.zhang@databricks.com>

--- mlflow/models/evaluation/default_evaluator.py
+++ mlflow/models/evaluation/default_evaluator.py
@@ -1405,9 +1405,9 @@ class DefaultEvaluator(ModelEvaluator):
                     self.metrics_values.update({name: metric_value})
             except Exception as e:
                 if isinstance(e, MlflowException):
-                    exceptions.append(f"Metric '{metric.name}': Error '{e.message}'")
+                    exceptions.append(f"Metric '{metric.name}': Error:\n{e.message}")
                 else:
-                    exceptions.append(f"Metric '{metric.name}': Error '{e!r}'")
+                    exceptions.append(f"Metric '{metric.name}': Error:\n{e!r}")
         self._update_metrics()
         for metric in self.extra_metrics:
             try:
@@ -1415,9 +1415,9 @@ class DefaultEvaluator(ModelEvaluator):
                 metric.eval_fn(*eval_fn_args)
             except Exception as e:
                 if isinstance(e, MlflowException):
-                    exceptions.append(f"Metric '{metric.name}': Error '{e.message}'")
+                    exceptions.append(f"Metric '{metric.name}': Error:\n{e.message}")
                 else:
-                    exceptions.append(f"Metric '{metric.name}': Error '{e!r}'")
+                    exceptions.append(f"Metric '{metric.name}': Error:\n{e!r}")
 
         if len(exceptions) > 0:
             raise MlflowException("\n".join(exceptions))
