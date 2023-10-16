15ce32832dfb1e546a6d1c7881e071e6f8ffaba7
harupy <hkawamura0130@gmail.com>
2023-10-16 14:20:22 +0900

    Make storage_uri configurable
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- mlflow/gateway/app.py
+++ mlflow/gateway/app.py
@@ -10,7 +10,10 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
 
-from mlflow.environment_variables import MLFLOW_GATEWAY_CONFIG
+from mlflow.environment_variables import (
+    MLFLOW_GATEWAY_CONFIG,
+    MLFLOW_GATEWAY_RATE_LIMITS_STORAGE_URI,
+)
 from mlflow.exceptions import MlflowException
 from mlflow.gateway.base_models import SetLimitsModel
 from mlflow.gateway.config import (
@@ -160,7 +163,9 @@ def create_app_from_config(config: GatewayConfig) -> GatewayAPI:
     """
     Create the GatewayAPI app from the gateway configuration.
     """
-    limiter = Limiter(key_func=get_remote_address)
+    limiter = Limiter(
+        key_func=get_remote_address, storage_uri=MLFLOW_GATEWAY_RATE_LIMITS_STORAGE_URI.get()
+    )
     app = GatewayAPI(
         config=config,
         limiter=limiter,
