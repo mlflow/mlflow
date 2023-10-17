6729b26b73bc0e52a2f1839f9208519500089dfd
Prithvi Kannan <prithvi.kannan@databricks.com>
2023-10-17 08:47:49 -0700

    Advise to use temperature 0 for judge
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- mlflow/metrics/genai/genai_metric.py
+++ mlflow/metrics/genai/genai_metric.py
@@ -104,7 +104,8 @@ def make_genai_metric(
         the metric. These grading_context_columns are used by the LLM as a judge as additional
         information to compute the metric. The columns are extracted from the input dataset or
         output predictions based on col_mapping in evaluator_config.
-    :param parameters: (Optional) Parameters for the llm used to compute the metric.
+    :param parameters: (Optional) Parameters for the LLM used to compute the metric. We recommend
+        setting the temperature to 0.0 for the LLM used as a judge to ensure consistent results.
     :param aggregations: (Optional) The list of options to aggregate the scores. Currently supported
         options are: min, max, mean, median, variance, p90.
     :param greater_is_better: (Optional) Whether the metric is better when it is greater.
