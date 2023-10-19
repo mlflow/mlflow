04bcd83587b57a24b423282f82d3a552194e0ad4
harupy <hkawamura0130@gmail.com>
2023-10-19 16:16:52 +0900

    test
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- aliases.py
+++ aliases.py
@@ -5,18 +5,16 @@ import mlflow
 
 mlflow.set_tracking_uri("sqlite:///:memory:")
 with mlflow.start_run():
-    mlflow.sklearn.log_model(
-        "I am a model",
-        "model",
-        registered_model_name="sklearn",
-    )
+    for _ in range(3):
+        mlflow.sklearn.log_model(
+            "I am a model",
+            "model",
+            registered_model_name="sklearn",
+        )
 
-# set a registered model tag
-mlflow.MlflowClient().set_model_version_tag(
-    name="sklearn", version=1, key="tag-key", value="tag-value"
-)
-mlflow.MlflowClient().set_registered_model_alias(
-    name="sklearn", alias="sklearn-model-alias", version=1
-)
+mlflow.MlflowClient().set_registered_model_alias(name="sklearn", alias="alias-1", version=1)
+mlflow.MlflowClient().set_registered_model_alias(name="sklearn", alias="alias-2", version=1)
+mlflow.MlflowClient().set_registered_model_alias(name="sklearn", alias="alias-3", version=2)
+print("Fetching model versions")  # noqa: T201
 for mv in mlflow.search_model_versions():
     print(mv.aliases)  # noqa: T201
