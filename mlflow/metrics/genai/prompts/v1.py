672785acef7203ea18130405a78675476527fcc6
Ann Zhang <ann.zhang@databricks.com>
2023-10-22 15:55:01 -0700

    fix tests
    Signed-off-by: Ann Zhang <ann.zhang@databricks.com>

--- mlflow/metrics/genai/prompts/v1.py
+++ mlflow/metrics/genai/prompts/v1.py
@@ -9,7 +9,7 @@ from mlflow.metrics.genai.prompt_template import (
 )
 
 # TODO: Update the default_mode and default_parameters to the correct values post experimentation
-default_model = "openai:/gpt4"
+default_model = "openai:/gpt-4"
 default_parameters = {
     "temperature": 0.0,
     "max_tokens": 200,
