6cc752d58c69fae0f46dfe241a84049ed8f15569
SDonkelaarGDD <sander.vandonkelaar@xebia.com>
2023-10-22 13:42:00 +0200

    added huggingface text generation inference provider for mlflow gateway
    Signed-off-by: SDonkelaarGDD <sander.vandonkelaar@xebia.com>

--- mlflow/gateway/providers/__init__.py
+++ mlflow/gateway/providers/__init__.py
@@ -7,6 +7,7 @@ def get_provider(provider: Provider) -> BaseProvider:
     from mlflow.gateway.providers.ai21labs import AI21LabsProvider
     from mlflow.gateway.providers.anthropic import AnthropicProvider
     from mlflow.gateway.providers.cohere import CohereProvider
+    from mlflow.gateway.providers.huggingface import HFTextGenerationInferenceServerProvider
     from mlflow.gateway.providers.mlflow import MlflowModelServingProvider
     from mlflow.gateway.providers.mosaicml import MosaicMLProvider
     from mlflow.gateway.providers.openai import OpenAIProvider
@@ -20,6 +21,7 @@ def get_provider(provider: Provider) -> BaseProvider:
         Provider.MOSAICML: MosaicMLProvider,
         Provider.PALM: PaLMProvider,
         Provider.MLFLOW_MODEL_SERVING: MlflowModelServingProvider,
+        Provider.HUGGINGFACE_TEXT_GENERATION_INFERENCE: HFTextGenerationInferenceServerProvider,
     }
     if prov := provider_to_class.get(provider):
         return prov
