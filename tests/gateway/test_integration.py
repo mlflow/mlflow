28aa68681e88de72ddeb6a6ed1e8ce50eb6dada3
Ege Ucak <egeucak75@hotmail.com>
2023-10-25 20:07:47 +0200

    Fixed route length in integration tests
    Signed-off-by: Ege Ucak <egeucak75@hotmail.com>

--- tests/gateway/test_integration.py
+++ tests/gateway/test_integration.py
@@ -257,7 +257,7 @@ def test_create_gateway_client_with_declared_url(gateway):
     assert gateway_client.gateway_uri == gateway.url
     assert isinstance(gateway_client.get_route("chat-openai"), Route)
     routes = gateway_client.search_routes()
-    assert len(routes) == 16
+    assert len(routes) == 17
     assert all(isinstance(route, Route) for route in routes)
 
 
