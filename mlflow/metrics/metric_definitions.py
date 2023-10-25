7c561955a2aacb15ac195196bb7828758eae3695
Bryan Qiu <bryan.qiu@databricks.com>
2023-10-25 11:08:41 -0700

    fix
    Signed-off-by: Bryan Qiu <bryan.qiu@databricks.com>

--- mlflow/metrics/metric_definitions.py
+++ mlflow/metrics/metric_definitions.py
@@ -22,7 +22,7 @@ def standard_aggregations(scores):
     }
 
 
-def _validate_text_data(data, metric_name, column_name=None):
+def _validate_text_data(data, metric_name, column_name):
     """Validates that the data is a list of strs and is non-empty"""
     if data is None or len(data) == 0:
         return False
