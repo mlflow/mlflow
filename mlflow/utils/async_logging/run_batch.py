0d66ba2350a9869ec9a903dd90f85ce9e06a4fb6
sagarsumant <sagar.sumant@gmail.com>
2023-10-25 20:54:37 -0700

    Introduce experimental flag "synchronous" to facilitate asynchronous logging (metrics/params/tags) in mlflow. (#9705)
    Signed-off-by: Sagar Sumant <sasum@microsoft.com>
    Signed-off-by: sagarsumant <sagar.sumant@gmail.com>
    Signed-off-by: chenmoneygithub <chen.qian@databricks.com>
    Co-authored-by: Sagar Sumant <sasum@microsoft.com>
    Co-authored-by: Harutaka Kawamura <hkawamura0130@gmail.com>
    Co-authored-by: chenmoneygithub <chen.qian@databricks.com>

---
+++ mlflow/utils/async_logging/run_batch.py
@@ -0,0 +1,33 @@
+import threading
+from typing import List
+
+from mlflow.entities.metric import Metric
+from mlflow.entities.param import Param
+from mlflow.entities.run_tag import RunTag
+
+
+class RunBatch:
+    def __init__(
+        self,
+        run_id: str,
+        params: List[Param],
+        tags: List[RunTag],
+        metrics: List[Metric],
+        completion_event: threading.Event,
+    ) -> None:
+        """
+        Initializes an instance of RunBatch.
+
+        Args:
+            run_id: The ID of the run.
+            params: A list of parameters.
+            tags: A list of tags.
+            metrics: A list of metrics.
+            completion_event: A threading.Event object.
+        """
+        self.run_id = run_id
+        self.params = params or []
+        self.tags = tags or []
+        self.metrics = metrics or []
+        self.completion_event = completion_event
+        self.exception = None
