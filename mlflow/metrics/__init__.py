17cb1994c8b8706a9538438d5ad7d02f4d086e85
Bryan Qiu <bryan.qiu@databricks.com>
2023-10-26 02:27:55 -0700

    better docstring
    Signed-off-by: Bryan Qiu <bryan.qiu@databricks.com>

--- mlflow/metrics/__init__.py
+++ mlflow/metrics/__init__.py
@@ -250,7 +250,7 @@ def precision_at_k(k) -> EvaluationMetric:
     This metric computes a score between 0 and 1 for each row representing the precision of the
     retriever model at the given ``k`` value. If no relevant documents are retrieved, the score is
     0, indicating that no relevant docs were retrieved. Let ``x = min(k, # of retrieved doc IDs)``.
-    Then, the precision at k is calculated as follows:
+    Then, in all other cases, the precision at k is calculated as follows:
 
         ``precision_at_k`` = (# of relevant retrieved doc IDs in top-``x`` ranked docs) / ``x``.
     """
@@ -267,28 +267,14 @@ def recall_at_k(k) -> EvaluationMetric:
     """
     This function will create a metric for calculating ``recall_at_k`` for retriever models.
 
-    It is recommended to use a static dataset (Pandas Dataframe or MLflow Pandas Dataset)
-    containing columns for: input queries, retrieved relevant doc IDs, and ground-truth doc IDs. A
-    "doc ID" is a string that uniquely identifies a document. All doc IDs should be entered as a
-    tuple of doc ID strings.
-
-    The ``targets`` parameter should specify the column name of the ground-truth relevant doc IDs.
-
-    If you choose to use a static dataset, the ``predictions`` parameter should specify the column
-    name of the retrieved relevant doc IDs. Alternatively, if you choose to specify a function for
-    the ``model`` parameter, the function should take a Pandas DataFrame as input and return a
-    Pandas DataFrame with a column of retrieved relevant doc IDs, specified by the ``predictions``
-    parameter.
-
-    ``k`` should be a positive integer specifying the number of retrieved doc IDs to consider for
-    each input query. ``k`` defaults to 3.
-
     This metric computes a score between 0 and 1 for each row representing the recall ability of
-    the retriever model at the given ``k`` value.
+    the retriever model at the given ``k`` value. If no ground truth doc IDs are provided and no
+    documents were retrieved, the score is 1. However, if no ground truth doc IDs are provided and
+    documents were retrieved, the score is 0. In all other cases, the recall at k is calculated as
+    follows:
 
-
-    If no relevant documents are retrieved, the score
-    is 0, indicating that no relevant docs were retrieved. Then, the precision at k is calculated as follows:
+        ``recall_at_k`` = (# of relevant retrieved doc IDs in top-``k`` ranked docs) / (# of ground
+        truth doc IDs)
     """
     return make_metric(
         eval_fn=_recall_at_k_eval_fn(k),
