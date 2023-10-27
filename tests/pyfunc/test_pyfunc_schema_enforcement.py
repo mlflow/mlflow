4be2abc0918c650e071477b84946708a1b83ceec
Serena Ruan <82044803+serena-ruan@users.noreply.github.com>
2023-10-27 13:48:33 +0800

    Support objects & arrays in scoring server (#10094)
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- tests/pyfunc/test_pyfunc_schema_enforcement.py
+++ tests/pyfunc/test_pyfunc_schema_enforcement.py
@@ -1657,6 +1657,8 @@ def test_enforce_schema_in_python_model_serving(sample_params_basic):
         content_type=pyfunc_scoring_server.CONTENT_TYPE_JSON,
         extra_args=["--env-manager", "local"],
     )
+    # x = json.loads(response.content.decode("utf-8"))["message"]
+    # assert x == ""
     assert response.status_code == 400
     assert (
         "Incompatible types for param 'double_param'"
@@ -2211,6 +2213,25 @@ def test_pyfunc_model_serving_with_lists_of_dicts(data, schema, format_key):
             ),
         ),
         (
+            {"query": [{"name": "value", "age": 10}, {"name": "value"}], "table": ["some_table"]},
+            Schema(
+                [
+                    ColSpec(
+                        Array(
+                            Object(
+                                [
+                                    Property("name", DataType.string),
+                                    Property("age", DataType.long, required=False),
+                                ]
+                            )
+                        ),
+                        name="query",
+                    ),
+                    ColSpec(Array(DataType.string), name="table"),
+                ]
+            ),
+        ),
+        (
             [{"query": "sentence"}, {"query": "sentence"}],
             Schema([ColSpec(DataType.string, name="query")]),
         ),
@@ -2252,6 +2273,172 @@ def test_pyfunc_model_schema_enforcement_with_objects_and_arrays(data, schema):
     # Test pandas DataFrame input
     prediction = loaded_model.predict(df)
     pd.testing.assert_frame_equal(prediction, df)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        {"query": "sentence"},
+        {"query": ["sentence_1", "sentence_2"]},
+        {"query": ["sentence_1", "sentence_2"], "table": "some_table"},
+        {"query": [{"name": "value"}, {"name": "value"}], "table": ["some_table"]},
+        [{"query": "sentence"}, {"query": "sentence"}],
+        [
+            {"query": ["sentence_1", "sentence_2"], "table": "some_table"},
+            {"query": ["sentence_1", "sentence_2"]},
+        ],
+        [
+            {"query": [{"name": "value"}, {"name": "value"}], "table": ["some_table"]},
+            {"query": [{"name": "value", "age": 10}, {"name": "value"}], "table": ["some_table"]},
+        ],
+    ],
+)
+@pytest.mark.parametrize("format_key", ["inputs", "dataframe_split", "dataframe_records"])
+def test_pyfunc_model_scoring_with_objects_and_arrays(data, format_key):
+    class MyModel(mlflow.pyfunc.PythonModel):
+        def predict(self, context, model_input, params=None):
+            return model_input
+
+    with mlflow.start_run():
+        model_info = mlflow.pyfunc.log_model(
+            python_model=MyModel(),
+            artifact_path="test_model",
+            signature=infer_signature(data),
+        )
+
+    df = pd.DataFrame(data) if isinstance(data, list) else pd.DataFrame([data])
+
+    if format_key == "inputs":
+        payload = {format_key: data}
+    elif format_key == "dataframe_split":
+        payload = {format_key: df.to_dict(orient="split")}
+    elif format_key == "dataframe_records":
+        payload = {format_key: df.to_dict(orient="records")}
+
+    response = pyfunc_serve_and_score_model(
+        model_info.model_uri,
+        data=json.dumps(payload),
+        content_type=pyfunc_scoring_server.CONTENT_TYPE_JSON,
+        extra_args=["--env-manager", "local"],
+    )
+    assert response.status_code == 200, response.content
+    result = json.loads(response.content.decode("utf-8"))["predictions"]
+    expected_result = df.to_dict(orient="records")
+    np.testing.assert_equal(result, expected_result)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        {"query": "sentence"},
+        {"query": ["sentence_1", "sentence_2"]},
+        {"query": ["sentence_1", "sentence_2"], "table": "some_table"},
+        {"query": [{"name": "value"}, {"name": "value"}], "table": ["some_table"]},
+        [{"query": "sentence"}, {"query": "sentence"}],
+    ],
+)
+def test_pyfunc_model_scoring_with_objects_and_arrays_instances(data):
+    class MyModel(mlflow.pyfunc.PythonModel):
+        def predict(self, context, model_input, params=None):
+            return model_input
+
+    with mlflow.start_run():
+        model_info = mlflow.pyfunc.log_model(
+            python_model=MyModel(),
+            artifact_path="test_model",
+            signature=infer_signature(data),
+        )
+
+    df = pd.DataFrame(data) if isinstance(data, list) else pd.DataFrame([data])
+    response = pyfunc_serve_and_score_model(
+        model_info.model_uri,
+        data=json.dumps({"instances": data}),
+        content_type=pyfunc_scoring_server.CONTENT_TYPE_JSON,
+        extra_args=["--env-manager", "local"],
+    )
+    assert response.status_code == 200, response.content
+    result = json.loads(response.content.decode("utf-8"))["predictions"]
+    expected_result = df.to_dict(orient="records")
+    np.testing.assert_equal(result, expected_result)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        [{"query": {"a": "b"}, "name": "A"}, {"query": {"a": "c"}, "name": "B"}],
+        [
+            {"query": ["sentence_1", "sentence_2"], "table": "some_table"},
+            {"query": ["sentence_1", "sentence_2"]},
+        ],
+        [
+            {"query": [{"name": "value"}, {"name": "value"}], "table": ["some_table"]},
+            {"query": [{"name": "value", "age": 10}, {"name": "value"}], "table": ["some_table"]},
+        ],
+    ],
+)
+def test_pyfunc_model_scoring_with_objects_and_arrays_instances_errors(data):
+    class MyModel(mlflow.pyfunc.PythonModel):
+        def predict(self, context, model_input, params=None):
+            return model_input
+
+    with mlflow.start_run():
+        model_info = mlflow.pyfunc.log_model(
+            python_model=MyModel(),
+            artifact_path="test_model",
+            signature=infer_signature(data),
+        )
+
+    response = pyfunc_serve_and_score_model(
+        model_info.model_uri,
+        data=json.dumps({"instances": data}),
+        content_type=pyfunc_scoring_server.CONTENT_TYPE_JSON,
+        extra_args=["--env-manager", "local"],
+    )
+    assert response.status_code == 400, response.content
+    assert "Failed to enforce schema" in json.loads(response.content.decode("utf-8"))["message"]
+
+
+@pytest.mark.parametrize(
+    ("data", "schema"),
+    [
+        (
+            [{"query": "question1"}, {"query": "question2"}],
+            Schema([ColSpec(DataType.string, "query")]),
+        ),
+        (
+            [{"query": ["sentence_1", "sentence_2"]}, {"query": ["sentence_1", "sentence_2"]}],
+            Schema([ColSpec(DataType.string, "query")]),
+        ),
+        (
+            [
+                {"query": ["sentence_1", "sentence_2"], "table": "some_table"},
+                {"query": ["sentence_1", "sentence_2"], "table": "some_table"},
+            ],
+            Schema([ColSpec(DataType.string, "query"), ColSpec(DataType.string, "table")]),
+        ),
+    ],
+)
+def test_pyfunc_model_scoring_instances_backwards_compatibility(data, schema):
+    class MyModel(mlflow.pyfunc.PythonModel):
+        def predict(self, context, model_input, params=None):
+            return model_input
+
+    with mlflow.start_run():
+        model_info = mlflow.pyfunc.log_model(
+            python_model=MyModel(),
+            artifact_path="test_model",
+            signature=ModelSignature(schema),
+        )
+
+    response = pyfunc_serve_and_score_model(
+        model_info.model_uri,
+        data=json.dumps({"instances": data}),
+        content_type=pyfunc_scoring_server.CONTENT_TYPE_JSON,
+        extra_args=["--env-manager", "local"],
+    )
+    assert response.status_code == 200, response.content
+    result = json.loads(response.content.decode("utf-8"))["predictions"]
+    np.testing.assert_equal(result, data)
 
 
 @pytest.mark.parametrize(
@@ -2337,7 +2524,8 @@ def test_pyfunc_model_schema_enforcement_nested_array(data, schema):
         ),
     ],
 )
-def test_pyfunc_model_schema_enforcement_complex(data, schema):
+@pytest.mark.parametrize("format_key", ["inputs", "dataframe_split", "dataframe_records"])
+def test_pyfunc_model_schema_enforcement_complex(data, schema, format_key):
     class MyModel(mlflow.pyfunc.PythonModel):
         def predict(self, context, model_input, params=None):
             return model_input
@@ -2355,3 +2543,21 @@ def test_pyfunc_model_schema_enforcement_complex(data, schema):
     loaded_model = mlflow.pyfunc.load_model(model_info.model_uri)
     prediction = loaded_model.predict(df)
     pd.testing.assert_frame_equal(prediction, df)
+
+    if format_key == "inputs":
+        payload = {format_key: data}
+    elif format_key == "dataframe_split":
+        payload = {format_key: df.to_dict(orient="split")}
+    elif format_key == "dataframe_records":
+        payload = {format_key: df.to_dict(orient="records")}
+
+    response = pyfunc_serve_and_score_model(
+        model_info.model_uri,
+        data=json.dumps(payload),
+        content_type=pyfunc_scoring_server.CONTENT_TYPE_JSON,
+        extra_args=["--env-manager", "local"],
+    )
+    assert response.status_code == 200, response.content
+    result = json.loads(response.content.decode("utf-8"))["predictions"]
+    expected_result = df.to_dict(orient="records")
+    np.testing.assert_equal(result, expected_result)
