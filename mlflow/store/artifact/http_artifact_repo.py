da4e5f8c0bb46d272a9d695fc8c7ab22c38d54a7
Gabriel Fu <hfu.gabriel@gmail.com>
2023-10-30 17:35:33 +0800

    updated UnsupportedMultipartUploadException
    Signed-off-by: Gabriel Fu <hfu.gabriel@gmail.com>

--- mlflow/store/artifact/http_artifact_repo.py
+++ mlflow/store/artifact/http_artifact_repo.py
@@ -158,9 +158,9 @@ class HttpArtifactRepository(ArtifactRepository, MultipartUploadMixin):
             # return False if server does not support multipart upload
             error_message = e.response.json().get("message", "")
             if isinstance(error_message, str) and error_message.startswith(
-                "Multipart upload is not supported for artifact repository"
+                UnsupportedMultipartUploadException.message
             ):
-                raise UnsupportedMultipartUploadException(error_message)
+                raise UnsupportedMultipartUploadException()
             else:
                 raise
 
