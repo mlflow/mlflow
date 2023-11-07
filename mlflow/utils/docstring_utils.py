484b986733029c29555652ea11df67ef7fc5df20
mlflow-automation <mlflow-automation@users.noreply.github.com>
2023-11-07 22:24:47 +0000

    Autoformat: https://github.com/mlflow/mlflow/actions/runs/6791026658
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>

--- mlflow/utils/docstring_utils.py
+++ mlflow/utils/docstring_utils.py
@@ -159,10 +159,10 @@ def format_docstring(param_docs):
 LOG_MODEL_PARAM_DOCS = ParamDocs(
     {
         "conda_env": (
-"""Either a dictionary representation of a Conda environment or the path to a conda 
+            """Either a dictionary representation of a Conda environment or the path to a conda
 environment yaml file. If provided, this describes the environment this model should be run in.
-At a minimum, it should specify the dependencies contained in :func:`get_default_conda_env()`. 
-If  ``None``, a conda environment with pip requirements inferred by 
+At a minimum, it should specify the dependencies contained in :func:`get_default_conda_env()`.
+If  ``None``, a conda environment with pip requirements inferred by
 :func:`mlflow.models.infer_pip_requirements` is added
 to the model. If the requirement inference fails, it falls back to using
 :func:`get_default_pip_requirements`. pip requirements from ``conda_env`` are written to a pip
