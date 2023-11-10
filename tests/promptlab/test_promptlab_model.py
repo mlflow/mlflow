7c211ffda3cf579cc3b72eccbe2b03949ce3f8e1
Daniel Lok <daniel.lok@databricks.com>
2023-11-09 17:04:18 -0800

    fix history
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- tests/promptlab/test_promptlab_model.py
+++ tests/promptlab/test_promptlab_model.py
@@ -6,6 +6,14 @@ from mlflow._promptlab import _PromptlabModel
 from mlflow.entities.param import Param
 
 
+def construct_model(route):
+    prompt_parameters = [Param(key="thing", value="books")]
+    model_parameters = [Param(key="temperature", value=0.5), Param(key="max_tokens", value=10)]
+    prompt_template = "Write me a story about {{ thing }}."
+
+    return _PromptlabModel(prompt_template, prompt_parameters, model_parameters, route)
+
+
 def test_promptlab_prompt_replacement():
     data = pd.DataFrame(
         data=[
@@ -15,12 +23,7 @@ def test_promptlab_prompt_replacement():
         ]
     )
 
-    prompt_parameters = [Param(key="thing", value="books")]
-    model_parameters = [Param(key="temperature", value=0.5), Param(key="max_tokens", value=10)]
-    prompt_template = "Write me a story about {{ thing }}."
-    model_route = "completions"
-
-    model = _PromptlabModel(prompt_template, prompt_parameters, model_parameters, model_route)
+    model = construct_model("completions")
     with mock.patch("mlflow.gateway.query") as mock_query:
         model.predict(data)
 
@@ -37,3 +40,34 @@ def test_promptlab_prompt_replacement():
         ]
 
         mock_query.assert_has_calls(calls, any_order=True)
+
+
+def test_promptlab_works_with_chat_route(Model):
+    mock_response = {
+        "candidates": [
+            {"message": {"role": "user", "content": "test"}, "metadata": {"finish_reason": "stop"}}
+        ]
+    }
+    model = construct_model("chat")
+
+    with mock.patch("mlflow.gateway.query", return_value=mock_response):
+        response = model.predict(pd.DataFrame(data=[{"thing": "books"}]))
+
+        assert response == ["test"]
+
+
+def test_promptlab_works_with_completions_route(Model):
+    mock_response = {
+        "candidates": [
+            {
+                "text": "test",
+                "metadata": {"finish_reason": "stop"},
+            }
+        ]
+    }
+    model = construct_model("completions")
+
+    with mock.patch("mlflow.gateway.query", return_value=mock_response):
+        response = model.predict(pd.DataFrame(data=[{"thing": "books"}]))
+
+        assert response == ["test"]
