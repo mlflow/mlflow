1cf1607c83318a305a237f588900d413f3d03e7b
Ben Wilson <benjamin.wilson@databricks.com>
2023-11-13 23:06:07 -0500

    Validate that config submitted is a dict and not a Config instance of a provider
    Signed-off-by: Ben Wilson <benjamin.wilson@databricks.com>

--- mlflow/gateway/config.py
+++ mlflow/gateway/config.py
@@ -116,7 +116,10 @@ class OpenAIConfig(ConfigModel):
 
     @classmethod
     def _validate_field_compatibility(cls, info: Dict[str, Any]):
+        if not isinstance(info, Dict):
+            return info
         api_type = (info.get("openai_api_type") or OpenAIAPIType.OPENAI).lower()
+
         if api_type == OpenAIAPIType.OPENAI:
             if info.get("openai_deployment_name") is not None:
                 raise MlflowException.invalid_parameter_value(
