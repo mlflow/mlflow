bc89eb914d1994d9af36ca55efc4fa4f4fb6fe94
harupy <17039389+harupy@users.noreply.github.com>
2023-11-16 12:33:23 +0900

    Implement CRUD for endpoints
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- examples/deployments/databricks.py
+++ examples/deployments/databricks.py
@@ -1,14 +1,56 @@
+import argparse
+import json
+import uuid
+
 from mlflow.deployments import get_deploy_client
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--secret", type=str, help="Secret (e.g. secrets/scope/key)")
+    return parser.parse_args()
+
+
 def main():
+    args = parse_args()
     client = get_deploy_client("databricks")
+    name = f"test-endpoint-{uuid.uuid4()}"
     client.create_endpoint(
-        name="gpt4-chat",
+        name=name,
         config={
-            # TODO: doesn't work yet
+            "served_entities": [
+                {
+                    "name": "test",
+                    "external_model": {
+                        "name": "gpt-4",
+                        "provider": "openai",
+                        "openai_config": {
+                            "openai_api_key": "{{" + args.secret + "}}",
+                        },
+                    },
+                }
+            ],
+            "task": "llm/v1/chat",
         },
     )
+    try:
+        print(json.dumps(client.get_endpoint(endpoint=name), indent=2))
+        print(
+            json.dumps(
+                client.predict(
+                    endpoint=name,
+                    inputs={
+                        "messages": [
+                            {"role": "user", "content": "Hello!"},
+                        ],
+                        "max_tokens": 128,
+                    },
+                ),
+                indent=2,
+            )
+        )
+    finally:
+        client.delete_endpoint(endpoint=name)
 
 
 if __name__ == "__main__":
--- mlflow/deployments/databricks/__init__.py
+++ mlflow/deployments/databricks/__init__.py
@@ -1,4 +1,8 @@
+from typing import Any, Dict, Optional
+
 from mlflow.deployments import BaseDeploymentClient
+from mlflow.utils.databricks_utils import get_databricks_host_creds
+from mlflow.utils.rest_utils import augmented_raise_for_status, http_request
 
 
 class DatabricksDeploymentClient(BaseDeploymentClient):
@@ -17,23 +21,72 @@ class DatabricksDeploymentClient(BaseDeploymentClient):
     def get_deployment(self, name, endpoint=None):
         raise NotImplementedError
 
+    @property
+    def _host_creds(self):
+        """
+        NB: When `MlflowGatewayClient` is used as an instance variable in a custom pyfunc model, it
+        is pickled in the environment where the custom pyfunc model is defined (e.g. a notebook).
+        When the model is moved to a different environment, e.g. model serving, new credentials
+        need to be resolved from within the new environment. Accordingly, we re-resolve host
+        credentials every time a request is made.
+        """
+        return get_databricks_host_creds("databricks")
+
+    def _call_endpoint(
+        self,
+        method: str,
+        prefix: str = "/api/2.0",
+        route: Optional[str] = None,
+        json_body: Optional[Dict[str, Any]] = None,
+    ):
+        call_kwargs = {}
+        if method.lower() == "get":
+            call_kwargs["params"] = json_body
+        else:
+            call_kwargs["json"] = json_body
+
+        response = http_request(
+            host_creds=self._host_creds,
+            endpoint=f"{prefix}/serving-endpoints" + (f"/{route}" if route else ""),
+            method=method,
+            timeout=10,
+            retry_codes=frozenset(
+                [
+                    429,  # Too many requests
+                    500,  # Server Error
+                    502,  # Bad Gateway
+                    503,  # Service Unavailable
+                ]
+            ),
+            raise_on_status=False,
+            **call_kwargs,
+        )
+        augmented_raise_for_status(response)
+        return response.json()
+
     def predict(self, deployment_name=None, inputs=None, endpoint=None):
-        raise NotImplementedError("TODO")
+        return self._call_endpoint(
+            "POST", prefix="", route=f"{endpoint}/invocations", json_body=inputs
+        )
 
     def create_endpoint(self, name, config=None):
-        raise NotImplementedError("TODO")
+        config = config.copy() if config else {}
+        payload = {"name": name, "config": config}
+        if task := config.pop("task", None):
+            payload["task"] = task
+        return self._call_endpoint("POST", json_body=payload)
 
     def update_endpoint(self, endpoint, config=None):
-        raise NotImplementedError("TODO")
+        return self._call_endpoint("PUT", route=f"{endpoint}/config", json_body=config)
 
     def delete_endpoint(self, endpoint):
-        raise NotImplementedError("TODO")
+        return self._call_endpoint("DELETE", route=endpoint)
 
     def list_endpoints(self):
         raise NotImplementedError("TODO")
 
     def get_endpoint(self, endpoint):
-        raise NotImplementedError("TODO")
+        return self._call_endpoint("GET", route=endpoint)
 
 
 def run_local(name, model_uri, flavor=None, config=None):
