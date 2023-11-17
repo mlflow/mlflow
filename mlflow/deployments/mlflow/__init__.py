d898106c38e44aa7425c63ca4f818e2d9c78f177
harupy <17039389+harupy@users.noreply.github.com>
2023-11-17 13:31:39 +0900

    Add MLflowDeploymentClient
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

---
+++ mlflow/deployments/mlflow/__init__.py
@@ -0,0 +1,81 @@
+from mlflow.deployments import BaseDeploymentClient
+
+
+class MLflowDeploymentClient(BaseDeploymentClient):
+    def create_deployment(self, name, model_uri, flavor=None, config=None, endpoint=None):
+        """
+        .. warning::
+            This method is not implemented for `MLflowDeploymentClient`.
+        """
+        raise NotImplementedError
+
+    def update_deployment(self, name, model_uri=None, flavor=None, config=None, endpoint=None):
+        """
+        .. warning::
+            This method is not implemented for `MLflowDeploymentClient`.
+        """
+        raise NotImplementedError
+
+    def delete_deployment(self, name, config=None, endpoint=None):
+        """
+        .. warning::
+            This method is not implemented for `MLflowDeploymentClient`.
+        """
+        raise NotImplementedError
+
+    def list_deployments(self, endpoint=None):
+        """
+        .. warning::
+            This method is not implemented for `MLflowDeploymentClient`.
+        """
+        raise NotImplementedError
+
+    def get_deployment(self, name, endpoint=None):
+        """
+        TODO
+        """
+        raise NotImplementedError
+
+    def predict(self, deployment_name=None, inputs=None, endpoint=None):
+        """
+        TODO
+        """
+        raise NotImplementedError("TODO")
+
+    def create_endpoint(self, name, config=None):
+        """
+        TODO
+        """
+        raise NotImplementedError("TODO")
+
+    def update_endpoint(self, endpoint, config=None):
+        """
+        TODO
+        """
+        raise NotImplementedError("TODO")
+
+    def delete_endpoint(self, endpoint):
+        """
+        TODO
+        """
+        raise NotImplementedError("TODO")
+
+    def list_endpoints(self):
+        """
+        TODO
+        """
+        raise NotImplementedError("TODO")
+
+    def get_endpoint(self, endpoint):
+        """
+        TODO
+        """
+        raise NotImplementedError("TODO")
+
+
+def run_local(name, model_uri, flavor=None, config=None):
+    pass
+
+
+def target_help():
+    pass
