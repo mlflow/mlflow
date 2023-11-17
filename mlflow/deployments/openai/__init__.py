3c4db693900062d01ff93af226ca33a75af3356e
Prithvi Kannan <prithvi.kannan@databricks.com>
2023-11-16 21:24:21 -0800

    Create OpenAIDeploymentClient
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

---
+++ mlflow/deployments/openai/__init__.py
@@ -0,0 +1,104 @@
+import posixpath
+from typing import Any, Dict, Optional
+
+from mlflow.deployments import BaseDeploymentClient
+
+class OpenAIDeploymentClient(BaseDeploymentClient):
+    """
+    TODO
+    """
+
+    def create_deployment(self, name, model_uri, flavor=None, config=None, endpoint=None):
+        """
+        .. warning::
+
+            This method is not implemented for `OpenAIDeploymentClient`.
+        """
+        raise NotImplementedError
+
+    def update_deployment(self, name, model_uri=None, flavor=None, config=None, endpoint=None):
+        """
+        .. warning::
+
+            This method is not implemented for `OpenAIDeploymentClient`.
+        """
+        raise NotImplementedError
+
+    def delete_deployment(self, name, config=None, endpoint=None):
+        """
+        .. warning::
+
+            This method is not implemented for `OpenAIDeploymentClient`.
+        """
+        raise NotImplementedError
+
+    def list_deployments(self, endpoint=None):
+        """
+        .. warning::
+
+            This method is not implemented for `OpenAIDeploymentClient`.
+        """
+        raise NotImplementedError
+
+    def get_deployment(self, name, endpoint=None):
+        """
+        .. warning::
+
+            This method is not implemented for `OpenAIDeploymentClient`.
+        """
+        raise NotImplementedError
+
+
+    def predict(self, deployment_name=None, inputs=None, endpoint=None):
+        """
+        TODO
+        """
+        # TODO: fill this in
+        pass
+
+    def create_endpoint(self, name, config=None):
+        """
+        TODO
+        """
+        # TODO: fill this in
+        pass
+
+    def update_endpoint(self, endpoint, config=None):
+        """
+        .. warning::
+
+            This method is not implemented for `OpenAIDeploymentClient`.
+        """
+        raise NotImplementedError
+
+    def delete_endpoint(self, endpoint):
+        """
+        .. warning::
+
+            This method is not implemented for `OpenAIDeploymentClient`.
+        """
+        raise NotImplementedError
+
+    def list_endpoints(self):
+        """
+        .. warning::
+
+            This method is not implemented for `OpenAIDeploymentClient`.
+        """
+        raise NotImplementedError
+
+    def get_endpoint(self, endpoint):
+        """
+        .. warning::
+
+            This method is not implemented for `OpenAIDeploymentClient`.
+        """
+        raise NotImplementedError
+
+
+def run_local(name, model_uri, flavor=None, config=None):
+    pass
+
+
+def target_help():
+    pass
