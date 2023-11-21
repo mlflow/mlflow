87f6060b865432ebac2af56a65169ffb617eff56
harupy <17039389+harupy@users.noreply.github.com>
2023-11-21 10:46:39 +0900

    Remove warnings
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/gateway/__init__.py
+++ mlflow/gateway/__init__.py
@@ -4,8 +4,6 @@
     `mlflow.gateway` is deprecated and will be replaced by the deployments API in a future release.
     See :ref:`gateway-deprecation` for more details.",
 """
-import warnings
-
 from mlflow.gateway.client import MlflowGatewayClient
 from mlflow.gateway.fluent import (
     create_route,
@@ -17,12 +15,6 @@ from mlflow.gateway.fluent import (
     set_limits,
 )
 from mlflow.gateway.utils import get_gateway_uri, set_gateway_uri
-
-warnings.warn(
-    "`mlflow.gateway` is deprecated and will be replaced by the deployments API in a future "
-    "release.  See https://mlflow.org/docs/latest/llms/gateway/deprecation.html for more details.",
-    FutureWarning,
-)
 
 __all__ = [
     "create_route",
