a3ca46f35dd0e9d153dac6c13bab427eeacf0cb6
B-Step62 <yuki.watanabe@databricks.com>
2023-11-29 12:37:55 +0900

    tweak None handling
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- mlflow/server/validation.py
+++ mlflow/server/validation.py
@@ -17,13 +17,19 @@ def _validate_content_type(flask_request, allowed_content_types: List[str]):
     if flask_request.method not in ["POST", "PUT"]:
         return
 
+    if flask_request.content_type is None:
+        raise MlflowException(
+            message="Bad Request. Content-Type header is missing.",
+            error_code=INVALID_PARAMETER_VALUE,
+        )
+
     # Remove any parameters e.g. "application/json; charset=utf-8" -> "application/json"
-    content_type = flask_request.content_type.split(";")[0] if flask_request.content_type else None
+    content_type = flask_request.content_type.split(";")[0]
     if content_type not in allowed_content_types:
         message = f"Bad Request. Content-Type must be one of {allowed_content_types}."
 
         # Avoid XSS by restricting to a set of characters
-        if isinstance(content_type, str) and VALID_CONTENT_TYPE_CHAR_PATTERN.match(content_type):
+        if VALID_CONTENT_TYPE_CHAR_PATTERN.match(content_type):
             message += f" Got '{content_type}'."
 
         raise MlflowException(
