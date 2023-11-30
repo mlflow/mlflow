e76aa46bc23f9e6840f5caa9ba49096018bab9a6
Liang Zhang <liang.zhang@databricks.com>
2023-11-30 14:39:02 -0800

    fix csv files
    Signed-off-by: Liang Zhang <liang.zhang@databricks.com>

--- dev/mlflow-typo.sh
+++ dev/mlflow-typo.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
 
-# Patterns or paths to exclude (can include regex)
-EXCLUDE_PATTERNS=("^examples/llms/rag/.*(?<!\\.ipynb)$")
-
-for file in "$@"; do
-    # Check if the file matches any exclude patterns
-    for pattern in "${EXCLUDE_PATTERNS[@]}"; do
-        if [[ $file =~ $pattern ]]; then
-            # Skip this file
-            continue 2
-        fi
-    done
-
-    # Run the grep check
-    if grep -nP '\bM(lf|LF|lF)low\b' "$file"; then
-        exit 1
-    fi
-done
-
-exit 0
+if grep -nP '\bM(lf|LF|lF)low\b' "$@"; then
+    exit 1
+else
+    exit 0
+fi
