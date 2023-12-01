b9d916a5137c1f509e8384f83e71df9932ae89a2
Prithvi Kannan <prithvi.kannan@databricks.com>
2023-12-01 14:41:57 -0800

    fix test
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- tests/metrics/genai/test_model_utils.py
+++ tests/metrics/genai/test_model_utils.py
@@ -118,9 +118,7 @@ def test_score_model_openai(set_envs):
     with mock.patch(
         "mlflow.openai.api_request_parallel_processor.process_api_requests", return_value=[resp]
     ) as mock_post:
-        resp = score_model_on_payload(
-            "openai:/gpt-3.5-turbo", {"prompt": "my prompt", "temperature": 0.1}
-        )
+        resp = score_model_on_payload("openai:/gpt-3.5-turbo", "my prompt", {"temperature": 0.1})
         mock_post.assert_called_once_with(
             [
                 {
@@ -165,7 +163,7 @@ def test_score_model_azure_openai(set_azure_envs):
     with mock.patch(
         "mlflow.openai.api_request_parallel_processor.process_api_requests", return_value=[resp]
     ) as mock_post:
-        score_model_on_payload("openai:/gpt-3.5-turbo", {"prompt": "my prompt", "temperature": 0.1})
+        score_model_on_payload("openai:/gpt-3.5-turbo", "my prompt", {"temperature": 0.1})
         mock_post.assert_called_once_with(
             [
                 {
@@ -188,7 +186,7 @@ def test_score_model_azure_openai_bad_envs(set_bad_azure_envs):
     with pytest.raises(
         MlflowException, match="Either engine or deployment_id must be set for Azure OpenAI API"
     ):
-        score_model_on_payload("openai:/gpt-3.5-turbo", {"prompt": "my prompt", "temperature": 0.1})
+        score_model_on_payload("openai:/gpt-3.5-turbo", "my prompt", {"temperature": 0.1})
 
 
 def test_score_model_gateway_completions():
@@ -221,7 +219,7 @@ def test_score_model_gateway_completions():
 
 def test_score_model_gateway_chat():
     expected_output = {
-        "choices": [
+        "candidates": [
             {
                 "message": {
                     "role": "assistant",
@@ -254,7 +252,7 @@ def test_score_model_gateway_chat():
             assert response == expected_output["candidates"][0]["message"]["content"]
 
 
-def test_score_model_endpoints(set_deployment_envs):
+def test_score_model_endpoints_chat(set_deployment_envs):
     openai_response_format = {
         "id": "chatcmpl-123",
         "object": "chat.completion",
@@ -276,21 +274,70 @@ def test_score_model_endpoints(set_deployment_envs):
     expected_output = {
         "choices": [
             {
-                "text": "\n\nHello there, how may I assist you today?",
-                "metadata": {"finish_reason": "stop"},
+                "index": 0,
+                "message": {
+                    "role": "assistant",
+                    "content": "\n\nHello there, how may I assist you today?",
+                },
+                "finish_reason": "stop",
             }
-        ]
+        ],
     }
 
     with mock.patch("mlflow.deployments.get_deploy_client") as mock_get_deploy_client:
         mock_client = mock.MagicMock()
         mock_get_deploy_client.return_value = mock_client
+        # mock out mock_client.get_endpoint() to return chat
+        mock_client.get_endpoint.return_value = {
+            "task": "llm/v1/chat",
+        }
         # mock out mock_client.predict() to return expected_output
         mock_client.predict.return_value = openai_response_format
         response = score_model_on_payload(
             "endpoints:/my-endpoint", {"prompt": "my prompt", "temperature": 0.1}
         )
-        assert response == expected_output
+        assert response == expected_output["choices"][0]["message"]["content"]
+
+
+def test_score_model_endpoints_completions(set_deployment_envs):
+    openai_response_format = {
+        "id": "cmpl-8PgdiXapPWBN3pyUuHcELH766QgqK",
+        "object": "text_completion",
+        "created": 1701132798,
+        "model": "gpt-3.5-turbo-instruct",
+        "choices": [
+            {
+                "text": "\n\nHi there! How can I assist you today?",
+                "index": 0,
+                "finish_reason": "stop",
+            },
+        ],
+        "usage": {"prompt_tokens": 2, "completion_tokens": 106, "total_tokens": 108},
+    }
+
+    expected_output = {
+        "choices": [
+            {
+                "text": "\n\nHi there! How can I assist you today?",
+                "index": 0,
+                "finish_reason": "stop",
+            },
+        ],
+    }
+
+    with mock.patch("mlflow.deployments.get_deploy_client") as mock_get_deploy_client:
+        mock_client = mock.MagicMock()
+        mock_get_deploy_client.return_value = mock_client
+        # mock out mock_client.get_endpoint() to return completions
+        mock_client.get_endpoint.return_value = {
+            "task": "llm/v1/completions",
+        }
+        # mock out mock_client.predict() to return expected_output
+        mock_client.predict.return_value = openai_response_format
+        response = score_model_on_payload(
+            "endpoints:/my-endpoint", {"prompt": "my prompt", "temperature": 0.1}
+        )
+        assert response == expected_output["choices"][0]["text"]
 
 
 def test_openai_authentication_error(set_envs):
@@ -301,9 +348,7 @@ def test_openai_authentication_error(set_envs):
         with pytest.raises(
             MlflowException, match="Authentication Error for OpenAI. Error response"
         ):
-            score_model_on_payload(
-                "openai:/gpt-3.5-turbo", {"prompt": "my prompt", "temperature": 0.1}
-            )
+            score_model_on_payload("openai:/gpt-3.5-turbo", "my prompt", {"temperature": 0.1})
         mock_post.assert_called_once()
 
 
@@ -313,9 +358,7 @@ def test_openai_invalid_request_error(set_envs):
         side_effect=openai.error.InvalidRequestError("foo", "bar"),
     ) as mock_post:
         with pytest.raises(MlflowException, match="Invalid Request to OpenAI. Error response"):
-            score_model_on_payload(
-                "openai:/gpt-3.5-turbo", {"prompt": "my prompt", "temperature": 0.1}
-            )
+            score_model_on_payload("openai:/gpt-3.5-turbo", "my prompt", {"temperature": 0.1})
         mock_post.assert_called_once()
 
 
@@ -325,7 +368,5 @@ def test_openai_other_error(set_envs):
         side_effect=Exception("foo"),
     ) as mock_post:
         with pytest.raises(MlflowException, match="Error response from OpenAI"):
-            score_model_on_payload(
-                "openai:/gpt-3.5-turbo", {"prompt": "my prompt", "temperature": 0.1}
-            )
+            score_model_on_payload("openai:/gpt-3.5-turbo", "my prompt", {"temperature": 0.1})
         mock_post.assert_called_once()
