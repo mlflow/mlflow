a03b2d34789aeab15f9822ddcbf4bc24b9a56e68
Adam Reeve <adreeve@gmail.com>
2023-12-06 10:18:25 +1300

    Account for different stepping logic in Lightning < 1.6.0
    Signed-off-by: Adam Reeve <adreeve@gmail.com>

--- mlflow/pytorch/_lightning_autolog.py
+++ mlflow/pytorch/_lightning_autolog.py
@@ -86,7 +86,7 @@ class __MLflowPLCallback(pl.Callback, metaclass=ExceptionSafeAbstractClass):
         self.log_models = log_models
         self.log_every_n_epoch = log_every_n_epoch
         self.log_every_n_step = log_every_n_step
-        self._num_optimizers = 1
+        self._global_steps_per_training_step = 1
         # Sets for tracking which metrics are logged on steps and which are logged on epochs
         self._step_metrics = set()
         self._epoch_metrics = set()
@@ -213,9 +213,7 @@ class __MLflowPLCallback(pl.Callback, metaclass=ExceptionSafeAbstractClass):
         ]
         self._step_metrics.update(name for (name, _) in metric_items)
         step = trainer.global_step
-        # Lightning increments the global step once per optimizer, so to correctly handle
-        # the step interval we divide the step by the number of optimizers.
-        if ((step // self._num_optimizers) + 1) % self.log_every_n_step == 0:
+        if ((step // self._global_steps_per_training_step) + 1) % self.log_every_n_step == 0:
             self._log_metrics(trainer, step, metric_items)
 
     @rank_zero_only
@@ -239,7 +237,10 @@ class __MLflowPLCallback(pl.Callback, metaclass=ExceptionSafeAbstractClass):
         #    to rename the optimizer parameters based on keys in default dictionary.
 
         if hasattr(trainer, "optimizers"):
-            self._num_optimizers = len(trainer.optimizers)
+            # Lightning >= 1.6.0 increments the global step every time an optimizer is stepped.
+            # We assume every optimizer will be stepped in each training step.
+            if _pl_version >= Version("1.6.0"):
+                self._global_steps_per_training_step = len(trainer.optimizers)
             optimizer = trainer.optimizers[0]
             params["optimizer_name"] = _get_optimizer_name(optimizer)
 
