dbdbae3343eaec0f2bca641fda08ca33ee7cd498
Daniel Lok <daniel.lok@databricks.com>
2023-12-07 10:57:25 +0800

    add backward compat tests
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

---
+++ tests/pyfunc/test_backward_compatibility.py
@@ -0,0 +1,8 @@
+import pytest
+import mlflow
+import os
+
+@pytest.mark.parametrize("version", ["2.7.1", "2.8.1"])
+def test_backward_compatibility(version):
+    model = mlflow.pyfunc.load_model(f"tests/resources/pyfunc_models/{version}")
+    assert model.predict("MLflow is great!") == "MLflow is great!"
