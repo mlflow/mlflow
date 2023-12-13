32c888fc264edef54e2b806826a38088f71a7980
Serena Ruan <82044803+serena-ruan@users.noreply.github.com>
2023-12-13 03:45:42 -0800

    Fix ImportError in new langchain version (#10677)
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- mlflow/langchain/runnables.py
+++ mlflow/langchain/runnables.py
@@ -37,18 +37,15 @@ _BRANCHES_FOLDER_NAME = "branches"
 _RUNNABLE_BRANCHES_FILE_NAME = "branches.yaml"
 _DEFAULT_BRANCH_NAME = "default"
 
-try:
-    from langchain.prompts.loading import type_to_loader_dict as prompts_types
-except ImportError:
-    prompts_types = {"prompt", "few_shot_prompt"}
-
 
 def _load_model_from_config(path, model_config):
-    from langchain.chains.loading import load_chain
     from langchain.chains.loading import type_to_loader_dict as chains_type_to_loader_dict
-    from langchain.llms.loading import get_type_to_cls_dict as llms_get_type_to_cls_dict
-    from langchain.llms.loading import load_llm
-    from langchain.prompts.loading import load_prompt
+    from langchain.llms import get_type_to_cls_dict as llms_get_type_to_cls_dict
+
+    try:
+        from langchain.prompts.loading import type_to_loader_dict as prompts_types
+    except ImportError:
+        prompts_types = {"prompt", "few_shot_prompt"}
 
     config_path = os.path.join(path, model_config.get(_MODEL_DATA_KEY, _MODEL_DATA_YAML_FILE_NAME))
     # Load runnables from config file
@@ -62,10 +59,16 @@ def _load_model_from_config(path, model_config):
         )
     _type = config.get("_type")
     if _type in chains_type_to_loader_dict:
+        from langchain.chains.loading import load_chain
+
         return load_chain(config_path)
     elif _type in prompts_types:
+        from langchain.prompts.loading import load_prompt
+
         return load_prompt(config_path)
     elif _type in llms_get_type_to_cls_dict():
+        from langchain.llms.loading import load_llm
+
         return load_llm(config_path)
     elif _type in custom_type_to_loader_dict():
         return custom_type_to_loader_dict()[_type](config)
