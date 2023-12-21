939bb86caf193a859e76ad71e0ea40e22c8d969f
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-12-21 11:19:23 +0900

    Remove `mlflow.llm.log_predictions` (#10734)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/llm/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-The ``mlflow.llm`` module provides utilities for Large Language Models (LLMs).
-"""
-
-from mlflow.tracking.llm_utils import log_predictions
-
-__all__ = [
-    "log_predictions",
-]
