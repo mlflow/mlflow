6178512b94dec563f0ca7fe1f1bcd4b8d5e0d142
raphaelauv <raphaelauv@users.noreply.github.com>
2023-12-26 10:09:05 +0100

    fix: importlib.metadata get Signed-off-by: raphaelauv <raphaelauv@users.noreply.github.com>

--- mlflow/utils/os.py
+++ mlflow/utils/os.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import importlib.metadata
 
 
 def is_windows():
@@ -6,3 +8,13 @@ def is_windows():
     :return: Returns true if the local system/OS name is Windows.
     """
     return os.name == "nt"
+
+
+def get_entry_points(namespace):
+    if sys.version_info >= (3, 10):
+        return importlib.metadata.entry_points(group=namespace)
+    else:
+        try:
+            return importlib.metadata.entry_points().get(namespace, [])
+        except AttributeError:
+            return importlib.metadata.entry_points().select(group=namespace)
