9c850c8ea02544b29f888e7033a873ed9e5d614f
Gabriel Fu <hfu.gabriel@gmail.com>
2023-12-28 15:22:42 +0800

    pylint
    Signed-off-by: Gabriel Fu <hfu.gabriel@gmail.com>

--- mlflow/gateway/providers/openai.py
+++ mlflow/gateway/providers/openai.py
@@ -233,9 +233,9 @@ class OpenAIProvider(BaseProvider):
             resp = json.loads(data)
             yield completions.StreamResponsePayload(
                 id=resp["id"],
-                # The chat models response from OpenAI is of object type "chat.completion_chunk". Since
-                # we're using the completions response format here, we hardcode the "text_completion_chunk"
-                # object type in the response instead
+                # The chat models response from OpenAI is of object type "chat.completion_chunk".
+                # Since we're using the completions response format here, we hardcode the
+                # "text_completion_chunk" object type in the response instead
                 object="text_completion_chunk",
                 created=resp["created"],
                 model=resp["model"],
