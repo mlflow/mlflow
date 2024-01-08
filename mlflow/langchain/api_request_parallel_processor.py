ccf7cedfc0eb07b1c4b84fbedbfffa9f919f93bb
Corey Zumar <39497902+dbczumar@users.noreply.github.com>
2024-01-07 19:42:02 -0800

    Update mlflow/langchain/api_request_parallel_processor.py
    Co-authored-by: Serena Ruan <82044803+serena-ruan@users.noreply.github.com>
    Signed-off-by: Corey Zumar <39497902+dbczumar@users.noreply.github.com>

--- mlflow/langchain/api_request_parallel_processor.py
+++ mlflow/langchain/api_request_parallel_processor.py
@@ -169,9 +169,8 @@ class APIRequest:
                     # does not accept dictionaries as input, it leads to errors like
                     # Expected Scalar value for String field 'query_text'
                     try:
-                        response = self.lc_model.invoke(
-                            self._prepare_request_for_runnable_or_chain_inference(self.request_json)
-                        )
+                        self.request_json = self._prepare_request_for_runnable_or_chain_inference(self.request_json)
+                        response = self.lc_model.invoke(self.request_json))
                     except TypeError as e:
                         _logger.warning(
                             f"Failed to invoke {self.lc_model.__class__.__name__} "
