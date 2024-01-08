1c994004a7c9e25ae45c5c4dd951182c7cf67bf8
Ben Wilson <benjamin.wilson@databricks.com>
2024-01-08 12:54:06 -0500

    Add note to docstring for fluent API set_experiment
    Signed-off-by: Ben Wilson <benjamin.wilson@databricks.com>

--- mlflow/tracking/fluent.py
+++ mlflow/tracking/fluent.py
@@ -91,6 +91,9 @@ def set_experiment(
     name via `experiment_name` or by ID via `experiment_id`. The experiment name and ID cannot
     both be specified.
 
+    .. note:: If the experiment being set by name does not exist, a new experiment will be created with the 
+        given name. After the experiment has been created, it will be set as the active experiment.
+
     :param experiment_name: Case sensitive name of the experiment to be activated. If an experiment
                             with this name does not exist, a new experiment wth this name is
                             created. On certain platforms such as Databricks, the experiment name
