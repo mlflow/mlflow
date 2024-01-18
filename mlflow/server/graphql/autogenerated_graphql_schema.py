dd174424f3c80045f3a4803722920187973605bf
edwardfeng-db <jinghao.feng@databricks.com>
2024-01-18 17:20:01 +0100

    update
    Signed-off-by: edwardfeng-db <jinghao.feng@databricks.com>

--- mlflow/server/graphql/autogenerated_graphql_schema.py
+++ mlflow/server/graphql/autogenerated_graphql_schema.py
@@ -1,7 +1,7 @@
 # GENERATED FILE. PLEASE DON'T MODIFY. Run python3 ./dev/proto_to_graphql/code_generator.py to regenerate.
 import graphene
+
 import mlflow
-from mlflow.server.graphql import graphql_schema_extensions
 from mlflow.server.graphql.graphql_custom_scalars import LongString
 from mlflow.utils.proto_json_utils import parse_dict
 
