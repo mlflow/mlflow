8a4c59a275770d2cc4322be6c0b8fdd13f9ca4f8
Volodymyr Kochetkov <whysages@gmail.com>
2024-01-22 13:41:07 +0200

    feat: #2931 add tests for auto truncation of long tags
    Signed-off-by: Volodymyr Kochetkov <whysages@gmail.com>

--- tests/utils/test_base_tags.py
+++ tests/utils/test_base_tags.py
@@ -1,7 +1,17 @@
+from unittest import mock
+
 from mlflow.entities.base_tag import BaseTag
 from mlflow.utils.validation_common import MAX_TAG_VAL_LENGTH
 
 
 def test_base_tag_auto_truncate(set_tag_auto_truncate):
-    tag = BaseTag("key", "long_value" * 1000)
-    assert len(tag.value) == MAX_TAG_VAL_LENGTH
+    with mock.patch("mlflow.entities.base_tag.logger.warning") as logger_mock:
+        tag = BaseTag("key", "long_value" * 1000)
+        assert logger_mock.call_count == 1
+        assert len(tag.value) == MAX_TAG_VAL_LENGTH
+
+
+def test_base_tag_auto_truncate_short_value(set_tag_auto_truncate):
+    short_value = "short_value"
+    tag = BaseTag("key", short_value)
+    assert tag.value == short_value
