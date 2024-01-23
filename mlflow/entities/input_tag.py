12917272029222996c5cbbb59cd0a767e7e36f61
Volodymyr Kochetkov <whysages@gmail.com>
2024-01-23 19:09:44 +0200

    feat: #2931 fix input_tag test
    Signed-off-by: Volodymyr Kochetkov <whysages@gmail.com>

--- mlflow/entities/input_tag.py
+++ mlflow/entities/input_tag.py
@@ -12,3 +12,19 @@ class InputTag(BaseTag):
         param.key = self.key
         param.value = self.value
         return param
+
+    @property
+    def key(self):
+        """
+        String name of the tag.
+        To be compatible with _MLflowObject._get_properties_helper
+        """
+        return self._key
+
+    @property
+    def value(self):
+        """
+        String value of the tag.
+        To be compatible with _MLflowObject._get_properties_helper
+        """
+        return self._value
--- mlflow/entities/run_tag.py
+++ mlflow/entities/run_tag.py
@@ -10,3 +10,19 @@ class RunTag(BaseTag):
         param.key = self.key
         param.value = self.value
         return param
+
+    @property
+    def key(self):
+        """
+        String name of the tag.
+        To be compatible with _MLflowObject._get_properties_helper
+        """
+        return self._key
+
+    @property
+    def value(self):
+        """
+        String value of the tag.
+        To be compatible with _MLflowObject._get_properties_helper
+        """
+        return self._value
