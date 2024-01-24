f60e99bba868ba48c046d0f1c801fceb26fcde64
Kris Concepcion <kris.concepcion@databricks.com>
2024-01-24 15:25:15 -0500

    Overwrite the classes instance

--- tests/data/test_delta_dataset_source.py
+++ tests/data/test_delta_dataset_source.py
@@ -169,7 +169,6 @@ def test_uc_table_id_retrieval_works(spark_session, tmp_path):
 
 def test_uc_table_id_retrieval_throws(spark_session, tmp_path):
     def mock_resolve_table_name(table_name, spark):
-        print("IN THE MOCK AND TABLENAME IS " + table_name)
         if table_name == "temp_delta_versioned_with_id_throws":
             return "default.temp_delta_versioned_with_id_throws"
         return table_name
@@ -183,7 +182,7 @@ def test_uc_table_id_retrieval_throws(spark_session, tmp_path):
         return True
 
     with mock.patch(
-        "mlflow.utils._unity_catalog_utils.get_full_name_from_sc",
+        "mlflow.data.delta_dataset_source.DeltaDatasetSource.get_full_name_from_sc",
         side_effect=mock_resolve_table_name,
     ), mock.patch(
         "mlflow.data.delta_dataset_source.DeltaDatasetSource._lookup_table_id",
