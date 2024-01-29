d0302d9caa8e0de866fc512ac1191e39335c8df0
harupy <17039389+harupy@users.noreply.github.com>
2024-01-29 21:51:55 +0900

    test
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- docs/diff.py
+++ docs/diff.py
@@ -3,15 +3,26 @@ import pathlib
 import shutil
 import subprocess
 import tempfile
+import os
 
 
 def main():
     with tempfile.TemporaryDirectory() as d:
+        # master
+        subprocess.run(["git", "checkout", "origin/master"], check=True)
         subprocess.run(["make", "rsthtml"], check=True)
         now = next(pathlib.Path(".").glob("build/html/**/*.html")).stat().st_mtime
         shutil.copytree("build/html", pathlib.Path(d) / "build/html")
-        subprocess.run(["git", "checkout", "origin/master"], check=True)
+
+        # pr
+        pr_number = os.environ["CIRCLE_PR_NUMBER"]
+        subprocess.run(["git", "fetch", "origin", f"refs/pull/{pr_number}/merge:pr"], check=True)
+        subprocess.run(["git", "checkout", "pr"], check=True)
+        subprocess.run(["git", "diff", "pr", "origin/master"], check=True)
         subprocess.run(["make", "rsthtml"], check=True)
+
+        # diff
+        hrefs = []
         for p in pathlib.Path(".").glob("build/html/**/*.html"):
             if p.stat().st_mtime > now:
                 diff = list(
@@ -21,7 +32,24 @@ def main():
                     )
                 )
                 if diff:
-                    print(p)
+                    hrefs.append(p.relative_to("build/html"))
+
+        # generate html file for each rst file
+        links = "\n".join(f"<a href='{href}'>{href}</a>" for href in hrefs)
+        html = f"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  {links}
+</body>
+</html>
+"""
+        pathlib.Path("build/html/diff.html").write_text(html)
 
 
 if __name__ == "__main__":
