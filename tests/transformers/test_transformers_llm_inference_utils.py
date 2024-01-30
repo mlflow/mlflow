d0bdcf88e98f6e36b1fc476e2cccc1467dc0e91c
Ying Chen <ying.chen@databricks.com>
2024-01-29 18:27:40 -0800

    Mock all tokenizer usage in llm_inference_utils test
    Signed-off-by: Ying Chen <ying.chen@databricks.com>

--- tests/transformers/test_transformers_llm_inference_utils.py
+++ tests/transformers/test_transformers_llm_inference_utils.py
@@ -10,20 +10,22 @@ from mlflow.transformers.llm_inference_utils import (
 
 
 def test_output_dict_for_completions(text_generation_pipeline):
-    prompt = "How to learn Python in 3 weeks?"
-    output_tensor = text_generation_pipeline.tokenizer("How to learn Python in 3 weeks? First,")[
-        "input_ids"
-    ]
-
-    model_config = {"max_new_tokens": 10}
+    prompt = "a b c"
+    output_tensor = [1, 2, 3, 4, 5]
+    model_config = {"max_new_tokens": 2}
     inference_task = "llm/v1/completions"
+
+    text_generation_pipeline.tokenizer = mock.MagicMock(
+        return_value={"input_ids": torch.tensor([1, 2, 3])}
+    )
+    text_generation_pipeline.tokenizer.decode = mock.MagicMock(return_value="a b c d e")
 
     output_dict = _get_output_and_usage_from_tensor(
         prompt, output_tensor, text_generation_pipeline, model_config, inference_task
     )
 
-    assert output_dict["text"] == "First,"
-    assert output_dict["finish_reason"] == "stop"
+    assert output_dict["text"] == "d e"
+    assert output_dict["finish_reason"] == "length"
 
     usage = output_dict["usage"]
     assert usage["prompt_tokens"] + usage["completion_tokens"] == usage["total_tokens"]
