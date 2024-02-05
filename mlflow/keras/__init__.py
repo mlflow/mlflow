e311c3c94d062a9392893286a79ff467aea9cb77
Daniel Lok <daniel.lok@databricks.com>
2024-02-05 10:34:06 +0800

    remove some comments
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/keras/__init__.py
+++ mlflow/keras/__init__.py
@@ -6,11 +6,11 @@ if Version(keras.__version__) < Version("3.0.0"):
     from mlflow.tensorflow import (
         # Redirect `mlflow.keras._load_pyfunc` to `mlflow.tensorflow._load_pyfunc`,
         # For backwards compatibility on loading keras model saved by old mlflow versions.
-        _load_pyfunc,  # noqa: F401
-        autolog,  # noqa: F401
-        load_model,  # noqa: F401
-        log_model,  # noqa: F401
-        save_model,  # noqa: F401
+        _load_pyfunc,
+        autolog,
+        load_model,
+        log_model,
+        save_model,
     )
 
     __all__ = [
