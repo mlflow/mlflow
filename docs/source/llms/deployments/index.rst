7c60bc4ce16370adace3b3d91cfd851b715791b6
Gabriel Fu <hfu.gabriel@gmail.com>
2024-02-09 11:06:56 +0800

    Implement chat & chat streaming for Cohere (#10976)
    Signed-off-by: Gabriel Fu <hfu.gabriel@gmail.com>

--- docs/source/llms/deployments/index.rst
+++ docs/source/llms/deployments/index.rst
@@ -240,8 +240,8 @@ below can be used as a helpful guide when configuring a given endpoint for any n
 |                          | - claude-1.3-100k        |                          |                          |
 |                          | - claude-2               |                          |                          |
 +--------------------------+--------------------------+--------------------------+--------------------------+
-| Cohere                   | - command                | N/A                      | - embed-english-v2.0     |
-|                          | - command-light-nightly  |                          | - embed-multilingual-v2.0|
+| Cohere                   | - command                | - command                | - embed-english-v2.0     |
+|                          | - command-light          | - command-light          | - embed-multilingual-v2.0|
 +--------------------------+--------------------------+--------------------------+--------------------------+
 | Azure OpenAI             | - text-davinci-003       | - gpt-35-turbo           | - text-embedding-ada-002 |
 |                          | - gpt-35-turbo           | - gpt-4                  |                          |
@@ -825,7 +825,7 @@ In addition to the :ref:`standard_query_parameters`, you can pass any additional
 - ``top_k`` (supported by MosaicML, Anthropic, PaLM, Cohere)
 - ``frequency_penalty`` (supported by OpenAI, Cohere, AI21 Labs)
 - ``presence_penalty`` (supported by OpenAI, Cohere, AI21 Labs)
-- ``stream`` (supported by OpenAI)
+- ``stream`` (supported by OpenAI, Cohere)
 
 Below is an example of submitting a query request to an MLflow Deployments Server endpoint using additional parameters:
 
@@ -886,7 +886,7 @@ generated before receiving it. Streaming responses are supported by the followin
 +============+=====================+==============+
 | OpenAI     | ✓                   | ✓            |
 +------------+---------------------+--------------+
-| Cohere     | ✓                   | ✘            |
+| Cohere     | ✓                   | ✓            |
 +------------+---------------------+--------------+
 
 To enable streaming responses, set the ``stream`` parameter to ``true`` in your request. For example:
--- tests/gateway/providers/test_cohere.py
+++ tests/gateway/providers/test_cohere.py
@@ -9,9 +9,236 @@ from pydantic import ValidationError
 from mlflow.gateway.config import RouteConfig
 from mlflow.gateway.constants import MLFLOW_GATEWAY_ROUTE_TIMEOUT_SECONDS
 from mlflow.gateway.providers.cohere import CohereProvider
-from mlflow.gateway.schemas import completions, embeddings
+from mlflow.gateway.schemas import chat, completions, embeddings
 
 from tests.gateway.tools import MockAsyncResponse, MockAsyncStreamingResponse
+
+
+def chat_config():
+    return {
+        "name": "chat",
+        "route_type": "llm/v1/chat",
+        "model": {
+            "provider": "cohere",
+            "name": "command",
+            "config": {
+                "cohere_api_key": "key",
+            },
+        },
+    }
+
+
+def chat_response():
+    return {
+        "response_id": "abc123",
+        "text": "\n\nThis is a test!",
+        "generation_id": "def456",
+        "token_count": {
+            "prompt_tokens": 13,
+            "response_tokens": 7,
+            "total_tokens": 20,
+            "billed_tokens": 20,
+        },
+        "meta": {
+            "api_version": {"version": "1"},
+            "billed_units": {"input_tokens": 13, "output_tokens": 7},
+        },
+        "tool_inputs": None,
+    }
+
+
+def chat_payload(stream: bool = False):
+    payload = {
+        "messages": [
+            {"role": "user", "content": "Message 1"},
+            {"role": "assistant", "content": "Message 2"},
+            {"role": "user", "content": "Message 3"},
+        ],
+        "temperature": 0.5,
+    }
+    if stream:
+        payload["stream"] = True
+    return payload
+
+
+@pytest.mark.asyncio
+async def test_chat():
+    resp = chat_response()
+    config = chat_config()
+    with mock.patch("time.time", return_value=1677858242), mock.patch(
+        "aiohttp.ClientSession.post", return_value=MockAsyncResponse(resp)
+    ) as mock_post:
+        provider = CohereProvider(RouteConfig(**config))
+        payload = chat_payload()
+        response = await provider.chat(chat.RequestPayload(**payload))
+        assert jsonable_encoder(response) == {
+            "id": "abc123",
+            "object": "chat.completion",
+            "created": 1677858242,
+            "model": "command",
+            "choices": [
+                {
+                    "message": {
+                        "role": "assistant",
+                        "content": "\n\nThis is a test!",
+                    },
+                    "finish_reason": None,
+                    "index": 0,
+                }
+            ],
+            "usage": {
+                "prompt_tokens": 13,
+                "completion_tokens": 7,
+                "total_tokens": 20,
+            },
+        }
+        mock_post.assert_called_once_with(
+            "https://api.cohere.ai/v1/chat",
+            json={
+                "model": "command",
+                "chat_history": [
+                    {"role": "USER", "message": "Message 1"},
+                    {"role": "CHATBOT", "message": "Message 2"},
+                ],
+                "message": "Message 3",
+                "temperature": 1.25,
+            },
+            timeout=ClientTimeout(total=MLFLOW_GATEWAY_ROUTE_TIMEOUT_SECONDS),
+        )
+
+
+@pytest.mark.asyncio
+async def test_chat_with_system_messages():
+    resp = chat_response()
+    config = chat_config()
+    with mock.patch("time.time", return_value=1677858242), mock.patch(
+        "aiohttp.ClientSession.post", return_value=MockAsyncResponse(resp)
+    ) as mock_post:
+        provider = CohereProvider(RouteConfig(**config))
+        payload = {
+            "messages": [
+                {"role": "system", "content": "System Message 1"},
+                {"role": "user", "content": "Message 1"},
+                {"role": "assistant", "content": "Message 2"},
+                {"role": "system", "content": "System Message 2"},
+                {"role": "user", "content": "Message 3"},
+            ],
+            "temperature": 0.5,
+        }
+        await provider.chat(chat.RequestPayload(**payload))
+        mock_post.assert_called_once_with(
+            "https://api.cohere.ai/v1/chat",
+            json={
+                "model": "command",
+                "chat_history": [
+                    {"role": "USER", "message": "Message 1"},
+                    {"role": "CHATBOT", "message": "Message 2"},
+                ],
+                "message": "Message 3",
+                "preamble_override": "System Message 1\nSystem Message 2",
+                "temperature": 1.25,
+            },
+            timeout=ClientTimeout(total=MLFLOW_GATEWAY_ROUTE_TIMEOUT_SECONDS),
+        )
+
+
+@pytest.mark.parametrize(
+    "params",
+    [{"n": 2}, {"stop": ["test"]}],
+)
+@pytest.mark.asyncio
+async def test_chat_throws_if_parameter_not_permitted(params):
+    config = chat_config()
+    provider = CohereProvider(RouteConfig(**config))
+    payload = chat_payload()
+    payload.update(params)
+    with pytest.raises(HTTPException, match=r".*") as e:
+        await provider.chat(chat.RequestPayload(**payload))
+    assert e.value.status_code == 422
+
+
+def chat_stream_response():
+    return [
+        # first chunk
+        b'{"is_finished":false,"event_type":"stream-start","generation_id":"test-id2"}',
+        # subsequent chunks
+        b'{"text":" Hi","is_finished":false,"event_type":"text-generation"}',
+        b'{"text":" there","is_finished":false,"event_type":"text-generation"}',
+        # final chunk
+        b'{"is_finished":true,"event_type":"stream-end","response":{"response_id":"test-id1",'
+        b'"text":"How are you","generation_id":"test-id2","token_count":{"prompt_tokens":83,'
+        b'"response_tokens":63,"total_tokens":146,"billed_tokens":128},"tool_inputs":null},'
+        b'"finish_reason":"COMPLETE"}',
+    ]
+
+
+@pytest.mark.asyncio
+async def test_chat_stream():
+    resp = chat_stream_response()
+    config = chat_config()
+
+    with mock.patch("time.time", return_value=1677858242), mock.patch(
+        "aiohttp.ClientSession.post", return_value=MockAsyncStreamingResponse(resp)
+    ) as mock_post:
+        provider = CohereProvider(RouteConfig(**config))
+        payload = chat_payload(stream=True)
+        response = provider.chat_stream(chat.RequestPayload(**payload))
+        chunks = [jsonable_encoder(chunk) async for chunk in response]
+        assert chunks == [
+            {
+                "choices": [
+                    {
+                        "delta": {"role": None, "content": " Hi"},
+                        "finish_reason": None,
+                        "index": 0,
+                    }
+                ],
+                "created": 1677858242,
+                "id": None,
+                "model": "command",
+                "object": "chat.completion.chunk",
+            },
+            {
+                "choices": [
+                    {
+                        "delta": {"role": None, "content": " there"},
+                        "finish_reason": None,
+                        "index": 0,
+                    }
+                ],
+                "created": 1677858242,
+                "id": None,
+                "model": "command",
+                "object": "chat.completion.chunk",
+            },
+            {
+                "choices": [
+                    {
+                        "delta": {"role": None, "content": None},
+                        "finish_reason": "COMPLETE",
+                        "index": 0,
+                    }
+                ],
+                "created": 1677858242,
+                "id": "test-id1",
+                "model": "command",
+                "object": "chat.completion.chunk",
+            },
+        ]
+        mock_post.assert_called_once_with(
+            "https://api.cohere.ai/v1/chat",
+            json={
+                "model": "command",
+                "chat_history": [
+                    {"role": "USER", "message": "Message 1"},
+                    {"role": "CHATBOT", "message": "Message 2"},
+                ],
+                "message": "Message 3",
+                "temperature": 1.25,
+                "stream": True,
+            },
+            timeout=ClientTimeout(total=MLFLOW_GATEWAY_ROUTE_TIMEOUT_SECONDS),
+        )
 
 
 def completions_config():
