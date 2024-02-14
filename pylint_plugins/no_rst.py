d8e60bdfebd16299bbfe282f2b9e85cc67864f67
harupy <17039389+harupy@users.noreply.github.com>
2024-02-14 12:01:41 +0900

    Enforce no-rst rule on all files
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- pylint_plugins/no_rst.py
+++ pylint_plugins/no_rst.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import re
-from pathlib import Path
 
 from astroid import nodes
 from pylint import checkers
@@ -11,42 +10,6 @@ from pylint_plugins.errors import NO_RST, to_msgs
 
 PARAM_REGEX = re.compile(r"\s+:param\s+\w+:", re.MULTILINE)
 RETURN_REGEX = re.compile(r"\s+:returns?:", re.MULTILINE)
-
-# TODO: Remove this once all docstrings are updated
-IGNORE = {
-    str(Path(p).resolve())
-    for p in [
-        "dev/set-mlflow-spark-scala-version.py",
-        "mlflow/gateway/client.py",
-        "mlflow/gateway/providers/utils.py",
-        "mlflow/keras/callback.py",
-        "mlflow/legacy_databricks_cli/configure/provider.py",
-        "mlflow/metrics/base.py",
-        "mlflow/metrics/genai/base.py",
-        "mlflow/mleap/__init__.py",
-        "mlflow/models/evaluation/default_evaluator.py",
-        "mlflow/projects/databricks.py",
-        "mlflow/projects/kubernetes.py",
-        "mlflow/store/_unity_catalog/registry/rest_store.py",
-        "mlflow/store/artifact/azure_data_lake_artifact_repo.py",
-        "mlflow/store/artifact/gcs_artifact_repo.py",
-        "mlflow/store/model_registry/rest_store.py",
-        "mlflow/store/tracking/rest_store.py",
-        "mlflow/utils/docstring_utils.py",
-        "mlflow/utils/rest_utils.py",
-        "tests/conftest.py",
-        "tests/evaluate/test_validation.py",
-        "tests/helper_functions.py",
-        "tests/projects/test_project_spec.py",
-        "tests/resources/data/dataset.py",
-        "tests/resources/mlflow-test-plugin/mlflow_test_plugin/dummy_dataset.py",
-        "tests/sagemaker/mock/__init__.py",
-        "tests/store/artifact/test_cli.py",
-        "tests/tracking/fluent/test_fluent.py",
-        "tests/transformers/helper.py",
-        "tests/utils/test_docstring_utils.py",
-    ]
-}
 
 
 class NoRstChecker(checkers.BaseChecker):
@@ -66,8 +29,7 @@ class NoRstChecker(checkers.BaseChecker):
 
     def _check_docstring(self, node: nodes.Module | nodes.ClassDef | nodes.FunctionDef) -> None:
         if (
-            node.root().file not in IGNORE
-            and node.doc_node
+            node.doc_node
             and (doc := node.doc_node.value)
             and (PARAM_REGEX.search(doc) or RETURN_REGEX.search(doc))
         ):
