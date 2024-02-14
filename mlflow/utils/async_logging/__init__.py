eb3d3c6d102e8dae89862c810f84dc24e481a648
chenmoneygithub <chen.qian@databricks.com>
2024-02-14 11:48:56 -0800

    add enable async logging api
    Signed-off-by: chenmoneygithub <chen.qian@databricks.com>

--- mlflow/utils/async_logging/__init__.py
+++ mlflow/utils/async_logging/__init__.py
@@ -1,1 +1,10 @@
 from mlflow.utils.async_logging import run_operations  # noqa: F401
+
+from mlflow.environment_variables import MLFLOW_ENABLE_ASYNC_LOGGING
+from mlflow.utils.annotations import experimental
+
+
+def enable_async_logging(enable=True):
+    """Enable or disable async logging globally."""
+
+    MLFLOW_ENABLE_ASYNC_LOGGING.set(enable)
