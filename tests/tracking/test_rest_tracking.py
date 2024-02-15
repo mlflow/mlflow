1bdbf13342f68b231aa702bc2a60e6924b8b67d8
Edward Feng <67326663+edwardfeng-db@users.noreply.github.com>
2024-02-15 20:11:21 +0100

    Onboard getExperiment to GraphQL (#11000)
    Signed-off-by: edwardfeng-db <jinghao.feng@databricks.com>

--- tests/tracking/test_rest_tracking.py
+++ tests/tracking/test_rest_tracking.py
@@ -1750,3 +1750,20 @@ def test_graphql_handler(mlflow_client):
         headers={"content-type": "application/json; charset=utf-8"},
     )
     assert response.status_code == 200
+
+
+def test_get_experiment_graphql(mlflow_client):
+    experiment_id = mlflow_client.create_experiment("GraphqlTest")
+    response = requests.request(
+        "post",
+        f"{mlflow_client.tracking_uri}/graphql",
+        json={
+            "query": 'query testQuery {mlflowGetExperiment(input: {experimentId: "'
+            + experiment_id
+            + '"}) { experiment { name } }}',
+            "operationName": "testQuery",
+        },
+        headers={"content-type": "application/json; charset=utf-8"},
+    )
+    assert response.status_code == 200
+    assert "GraphqlTest" in response.text
