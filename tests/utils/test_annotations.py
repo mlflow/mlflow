dfa6e4cee2e28faa3f117a09d3aa413a1d1e727b
Michael Berk <michaelberk99@gmail.com>
2024-02-16 00:31:36 -0500

    Fix docstring decorators (#11160)
    Signed-off-by: Michael Berk <michael.berk@databricks.com>
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>
    Co-authored-by: Michael Berk <michael.berk@databricks.com>
    Co-authored-by: mlflow-automation <mlflow-automation@users.noreply.github.com>

--- tests/utils/test_annotations.py
+++ tests/utils/test_annotations.py
@@ -2,7 +2,7 @@ import re
 
 import pytest
 
-from mlflow.utils.annotations import deprecated
+from mlflow.utils.annotations import _get_min_indent_of_docstring, deprecated, keyword_only
 
 
 class MyClass:
@@ -22,6 +22,35 @@ def function():
     return 1
 
 
+@keyword_only
+@deprecated(since="0.0.0")
+def deprecated_and_keyword_only_first():
+    """Description
+
+    Args:
+        x: x
+
+    Returns:
+        y
+    """
+    return 1
+
+
+@deprecated(since="0.0.0")
+@keyword_only
+def deprecated_and_keyword_only_second():
+    """
+    Description
+
+    Args:
+        x: x
+
+    Returns:
+        y
+    """
+    return 1
+
+
 def test_deprecated_method():
     msg = "``tests.utils.test_annotations.MyClass.method`` is deprecated"
     with pytest.warns(FutureWarning, match=re.escape(msg)):
@@ -34,3 +63,75 @@ def test_deprecated_function():
     with pytest.warns(FutureWarning, match=re.escape(msg)):
         assert function() == 1
     assert msg in function.__doc__
+
+
+def test_empty_docstring():
+    docstring = ""
+    expected_indent = ""
+    assert _get_min_indent_of_docstring(docstring) == expected_indent
+
+
+def test_single_line_docstring():
+    docstring = """Single line with indent."""
+    expected_indent = ""
+    assert _get_min_indent_of_docstring(docstring) == expected_indent
+
+
+def test_multi_line_docstring_first_line():
+    first_line_docstring = """Description
+
+    Args:
+        x: x
+
+    Returns:
+        y
+    """
+    expected_indent = "    "
+    assert _get_min_indent_of_docstring(first_line_docstring) == expected_indent
+
+
+def test_multi_line_docstring_second_line():
+    second_line_docstring = """
+    Description
+
+    Args:
+        x: x
+
+    Returns:
+        y
+    """
+    expected_indent = "    "
+    assert _get_min_indent_of_docstring(second_line_docstring) == expected_indent
+
+
+def test_deprecated_and_keyword_first():
+    docstring = deprecated_and_keyword_only_first.__doc__
+    # pylint: disable=line-too-long
+    assert docstring.rstrip() == (
+        """    .. note:: This method requires all argument be specified by keyword.
+    .. Warning:: ``tests.utils.test_annotations.deprecated_and_keyword_only_first`` is deprecated since 0.0.0. This method will be removed in a future release.
+Description
+
+    Args:
+        x: x
+
+    Returns:
+        y"""
+    )
+
+
+def test_deprecated_and_keyword_second():
+    docstring = deprecated_and_keyword_only_second.__doc__
+    # pylint: disable=line-too-long
+    assert docstring.rstrip() == (
+        """    .. Warning:: ``tests.utils.test_annotations.deprecated_and_keyword_only_second`` is deprecated since 0.0.0. This method will be removed in a future release.
+    .. note:: This method requires all argument be specified by keyword.
+
+    Description
+
+    Args:
+        x: x
+
+    Returns:
+        y"""
+    )
