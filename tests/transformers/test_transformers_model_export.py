9c33070a6dc25daf08908712b4b73384ee301bce
Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
2024-02-16 14:10:34 +0900

    Add an API to download and save model weights and use it for UC registration (#11157)
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- tests/transformers/test_transformers_model_export.py
+++ tests/transformers/test_transformers_model_export.py
@@ -7,6 +7,7 @@ import pathlib
 import shutil
 import re
 import textwrap
+from pathlib import Path
 from unittest import mock
 
 import huggingface_hub
@@ -3618,3 +3619,41 @@ def test_save_and_load_pipeline_without_save_pretrained_false(
         if callable(input_example):
             input_example = input_example()
         mlflow.pyfunc.load_model(model_path).predict(input_example)
+
+
+# Patch tempdir just to verify the invocation
+@mock.patch("mlflow.transformers.TempDir", side_effect=mlflow.utils.file_utils.TempDir)
+def test_persist_pretrained_model(mock_tmpdir, small_seq2seq_pipeline):
+    with mlflow.start_run():
+        model_info = mlflow.transformers.log_model(
+            transformers_model=small_seq2seq_pipeline,
+            artifact_path="model",
+            save_pretrained=False,
+            pip_requirements=["mlflow"],  # For speed up logging
+        )
+
+    artifact_path = Path(mlflow.artifacts.download_artifacts(model_info.model_uri))
+    model_path = artifact_path / "model"
+    tokenizer_path = artifact_path / "components" / "tokenizer"
+
+    original_config = Model.load(artifact_path).flavors["transformers"]
+    assert "model_binary" not in original_config
+    assert "source_model_revision" in original_config
+    assert not model_path.exists()
+    assert not tokenizer_path.exists()
+
+    mlflow.transformers.persist_pretrained_model(model_info.model_uri)
+
+    mock_tmpdir.assert_called_once()
+    updated_config = Model.load(model_info.model_uri).flavors["transformers"]
+    assert "model_binary" in updated_config
+    assert "source_model_revision" not in updated_config
+    assert model_path.exists()
+    assert (model_path / "tf_model.h5").exists()
+    assert tokenizer_path.exists()
+    assert (tokenizer_path / "tokenizer.json").exists()
+
+    # Repeat persisting the model will no-op
+    mock_tmpdir.reset_mock()
+    mlflow.transformers.persist_pretrained_model(model_info.model_uri)
+    mock_tmpdir.assert_not_called()
