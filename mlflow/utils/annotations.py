9d40bffd5a99e57976e55cd0894dcc7ad57e2383
Michael Berk <michaelberk99@gmail.com>
2024-02-15 23:34:20 -0500

    Update mlflow/utils/annotations.py
    Co-authored-by: Harutaka Kawamura <hkawamura0130@gmail.com>
    Signed-off-by: Michael Berk <michaelberk99@gmail.com>

--- mlflow/utils/annotations.py
+++ mlflow/utils/annotations.py
@@ -58,8 +58,8 @@ def _experimental(api: C, api_type: str) -> C:
     indent = _get_min_indent_of_docstring(api.__doc__)
     notice = (
         indent
-        + f".. Note:: Experimental: This {api_type} may change or "
-        + "be removed in a future release without warning.\n\n"
+        f".. Note:: Experimental: This {api_type} may change or "
+        "be removed in a future release without warning.\n\n"
     )
     if api_type == "property":
         api.__doc__ = api.__doc__ + "\n\n" + notice if api.__doc__ else notice
