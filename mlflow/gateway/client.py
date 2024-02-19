37e2f1c0493aa59d78b99fd68931831e9c679b5b
harupy <17039389+harupy@users.noreply.github.com>
2024-02-19 16:17:23 +0900

    blank line
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/gateway/client.py
+++ mlflow/gateway/client.py
@@ -387,13 +387,14 @@ class MlflowGatewayClient:
             limits: Limits (Array of dictionary) to set on the route. Each limit is defined by a
                 dictionary representing the limit details to be associated with the route. This
                 dictionary should define:
-                - renewal_period: a string representing the length of the window to enforce limit on
-                    (only supports "minute" for now).
+
+                - renewal_period: a string representing the length of the window to enforce limit
+                  on (only supports "minute" for now).
                 - calls: a non-negative integer representing the number of calls allowed per
-                    renewal_period (e.g., 10, 0, 55).
+                  renewal_period (e.g., 10, 0, 55).
                 - key: an optional string represents per route limit or per user limit ("user" for
-                    per user limit, "route" for per route limit, if not supplied, default to per
-                    route limit).
+                  per user limit, "route" for per route limit, if not supplied, default to per
+                  route limit).
 
         Returns:
             The returned data structure is a serialized representation of the `Limit`
