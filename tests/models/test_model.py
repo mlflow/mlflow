d787c28a6ca0302e9eba24d541a11afdb93ac154
Harutaka Kawamura <hkawamura0130@gmail.com>
2024-02-19 12:29:44 +0900

    Resolve model metadata for saved models that do not define an artifact_path (#11166)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -479,3 +479,10 @@ def test_save_load_input_example_without_conversion(tmp_path):
     assert loaded_model.saved_input_example_info["type"] == "json_object"
     loaded_example = loaded_model.load_input_example(local_path)
     assert loaded_example == input_example
+
+
+def test_model_saved_by_save_model_can_be_loaded(tmp_path, sklearn_knn_model):
+    mlflow.sklearn.save_model(sklearn_knn_model, tmp_path)
+    info = Model.load(tmp_path).get_model_info()
+    assert info.run_id is None
+    assert info.artifact_path is None
