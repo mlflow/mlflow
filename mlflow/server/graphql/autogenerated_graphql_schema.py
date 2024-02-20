36a0b5d0736506ff037734cd0b059ed2ced9c08c
Edward Feng <67326663+edwardfeng-db@users.noreply.github.com>
2024-02-20 15:59:43 +0100

    Join run and experiment in graphql (#11173)
    Signed-off-by: edwardfeng-db <jinghao.feng@databricks.com>

--- mlflow/server/graphql/autogenerated_graphql_schema.py
+++ mlflow/server/graphql/autogenerated_graphql_schema.py
@@ -2,7 +2,6 @@
 # Run python3 ./dev/proto_to_graphql/code_generator.py to regenerate.
 import graphene
 import mlflow
-from mlflow.server.graphql import graphql_schema_extensions
 from mlflow.server.graphql.graphql_custom_scalars import LongString
 from mlflow.utils.proto_json_utils import parse_dict
 
@@ -81,7 +80,7 @@ class MlflowRun(graphene.ObjectType):
 
 
 class MlflowGetRunResponse(graphene.ObjectType):
-    run = graphene.Field(MlflowRun)
+    run = graphene.Field('mlflow.server.graphql.graphql_schema_extensions.MlflowRunExtension')
 
 
 class MlflowExperimentTag(graphene.ObjectType):
@@ -113,20 +112,20 @@ class MlflowGetExperimentInput(graphene.InputObjectType):
 
 
 class QueryType(graphene.ObjectType):
-    mlflow_get_run = graphene.Field(MlflowGetRunResponse, input=MlflowGetRunInput())
     mlflow_get_experiment = graphene.Field(MlflowGetExperimentResponse, input=MlflowGetExperimentInput())
-
-    def resolve_mlflow_get_run(self, info, input):
-        input_dict = vars(input)
-        request_message = mlflow.protos.service_pb2.GetRun()
-        parse_dict(input_dict, request_message)
-        return mlflow.server.handlers.get_run_impl(request_message)
+    mlflow_get_run = graphene.Field(MlflowGetRunResponse, input=MlflowGetRunInput())
 
     def resolve_mlflow_get_experiment(self, info, input):
         input_dict = vars(input)
         request_message = mlflow.protos.service_pb2.GetExperiment()
         parse_dict(input_dict, request_message)
         return mlflow.server.handlers.get_experiment_impl(request_message)
+
+    def resolve_mlflow_get_run(self, info, input):
+        input_dict = vars(input)
+        request_message = mlflow.protos.service_pb2.GetRun()
+        parse_dict(input_dict, request_message)
+        return mlflow.server.handlers.get_run_impl(request_message)
 
 
 class MutationType(graphene.ObjectType):
