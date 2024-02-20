6048836e0743406e914b8a77a5d951033203829d
Harutaka Kawamura <hkawamura0130@gmail.com>
2024-02-20 19:55:54 +0900

    Remove pylint (#11183)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    Signed-off-by: Arthur Jenoudet <arthur.jenoudet@databricks.com>

--- pylint_plugins/__init__.py
+++
@@ -1,7 +0,0 @@
-from pylint_plugins.import_checker import ImportChecker
-from pylint_plugins.no_rst import NoRstChecker
-
-
-def register(linter):
-    linter.register_checker(ImportChecker(linter))
-    linter.register_checker(NoRstChecker(linter))
