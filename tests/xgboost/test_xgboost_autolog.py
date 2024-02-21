c4280732ea8eaefd9c29e690742b815a8298dd6d
harupy <17039389+harupy@users.noreply.github.com>
2024-02-21 09:49:29 +0900

    skip
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/xgboost/test_xgboost_autolog.py
+++ tests/xgboost/test_xgboost_autolog.py
@@ -668,6 +668,20 @@ def test_xgb_autolog_log_models_configuration(bst_params, log_models):
     assert ("model" in artifacts) == log_models
 
 
+@pytest.mark.skipif(
+    Version(xgb.__version__) > Version("2.0.3"),
+    reason="XGBoost > 2.0.3 does not support `None` data",
+)
+def test_xgb_autolog_does_not_break_dmatrix_instantiation_with_data_none():
+    """
+    This test verifies that `xgboost.DMatrix(None)` doesn't fail after patching.
+    XGBoost internally calls `xgboost.DMatrix(None)` to create a blank `DMatrix` object.
+    Example: https://github.com/dmlc/xgboost/blob/v1.2.1/python-package/xgboost/core.py#L701
+    """
+    mlflow.xgboost.autolog()
+    xgb.DMatrix(None)
+
+
 def test_callback_func_is_pickable():
     cb = picklable_exception_safe_function(
         functools.partial(autolog_callback, BatchMetricsLogger(run_id="1234"), eval_results={})
