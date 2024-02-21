6f35b127696ac23bc9eb5ff107d5b3c4dc6a6073
harupy <17039389+harupy@users.noreply.github.com>
2024-02-21 10:00:53 +0900

    Add vscode
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- dev/taplo.sh
+++ dev/taplo.sh
@@ -3,10 +3,11 @@
 if ! command -v taplo &> /dev/null
 then
   echo "taplo could not be found."
-  echo "Install taplo using the following instructions:"
+  echo "Please install taplo using one of the following methods:"
   echo "- homebrew: https://taplo.tamasfe.dev/cli/installation/homebrew.html"
   echo "- cargo: https://taplo.tamasfe.dev/cli/installation/cargo.html"
   echo "- npm: https://taplo.tamasfe.dev/cli/installation/npm.html"
+  echo "- vscode: https://marketplace.visualstudio.com/items?itemName=tamasfe.even-better-toml"
   exit 1
 fi
 
