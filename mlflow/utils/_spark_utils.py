468151abf43b3499e56267aab82a2912014f4714
WeichenXu <weichen.xu@databricks.com>
2024-02-23 10:48:53 +0800

    Fix spark model loading failure (#11227)
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/utils/_spark_utils.py
+++ mlflow/utils/_spark_utils.py
@@ -116,9 +116,12 @@ def _create_local_spark_session_for_loading_spark_model():
         # In Spark 3.1 and above, we need to set this conf explicitly to enable creating
         # a SparkSession on the workers
         .config("spark.executor.allowSparkContext", "true")
-        # Binding "spark.driver.bindAddress" to 127.0.0.1 helps avoiding some local hostname
+        # Binding "spark.driver.host" to 127.0.0.1 helps avoiding some local hostname
         # related issues (e.g. https://github.com/mlflow/mlflow/issues/5733).
-        .config("spark.driver.bindAddress", "127.0.0.1")
+        # Note that we should set "spark.driver.host" instead of "spark.driver.bindAddress",
+        # the latter one only set server binding host, but it doesn't set client side request
+        # destination host.
+        .config("spark.driver.host", "127.0.0.1")
         .config("spark.executor.allowSparkContext", "true")
         .config(
             "spark.driver.extraJavaOptions",
