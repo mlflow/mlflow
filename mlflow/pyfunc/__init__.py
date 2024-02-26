1baba30bbf57aa145644eac6d491171f39eb1774
Daniel Lok <daniel.lok@databricks.com>
2024-02-26 11:43:43 +0900

    pass context to predict
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/pyfunc/__init__.py
+++ mlflow/pyfunc/__init__.py
@@ -257,7 +257,7 @@ from mlflow.protos.databricks_pb2 import (
 from mlflow.pyfunc.model import (
     ChatModel,
     PythonModel,
-    PythonModelContext,  # noqa: F401
+    PythonModelContext,
     _log_warning_if_params_not_in_predict_signature,
     _PythonModelPyfuncWrapper,
     get_default_conda_env,  # noqa: F401
@@ -2068,7 +2068,7 @@ def save_model(
             _logger.info("Predicting on input example to validate output")
             context = PythonModelContext(artifacts, model_config)
             python_model.load_context(context)
-            output = python_model.predict(None, messages, params)
+            output = python_model.predict(context, messages, params)
             if not isinstance(output, ChatResponse):
                 raise MlflowException(
                     "Failed to save ChatModel. Please ensure that the model's predict() method "
