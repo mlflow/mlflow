4fbf637d9bf78a85bd8809f02ebd502c35edc282
mlflow-automation <mlflow-automation@users.noreply.github.com>
2024-02-27 01:16:03 +0000

    Autoformat: https://github.com/mlflow/mlflow/actions/runs/8058047631
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>

--- mlflow/store/tracking/abstract_store.py
+++ mlflow/store/tracking/abstract_store.py
@@ -5,7 +5,7 @@ from mlflow.entities import DatasetInput, ViewType
 from mlflow.entities.metric import MetricWithRunId
 from mlflow.store.entities.paged_list import PagedList
 from mlflow.store.tracking import SEARCH_MAX_RESULTS_DEFAULT
-from mlflow.utils.annotations import developer_stable, experimental
+from mlflow.utils.annotations import developer_stable
 from mlflow.utils.async_logging.async_logging_queue import AsyncLoggingQueue
 from mlflow.utils.async_logging.run_operations import RunOperations
 
