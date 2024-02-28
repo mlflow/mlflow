1109acb4766e481acb6f9ce8b081a76c2184b574
chenmoneygithub <chen.qian@databricks.com>
2024-02-28 15:42:38 -0800

    move config APIs to mlflow/config
    Signed-off-by: chenmoneygithub <chen.qian@databricks.com>

--- mlflow/config/__init__.py
+++ mlflow/config/__init__.py
@@ -1,4 +1,18 @@
 from mlflow.environment_variables import MLFLOW_ENABLE_ASYNC_LOGGING
+from mlflow.system_metrics import (
+    disable_system_metrics_logging,
+    enable_system_metrics_logging,
+    set_system_metrics_node_id,
+    set_system_metrics_samples_before_logging,
+    set_system_metrics_sampling_interval,
+)
+from mlflow.tracking import (
+    get_registry_uri,
+    get_tracking_uri,
+    is_tracking_uri_set,
+    set_registry_uri,
+    set_tracking_uri,
+)
 
 
 def enable_async_logging(enable=True):
@@ -27,3 +41,18 @@ def enable_async_logging(enable=True):
     """
 
     MLFLOW_ENABLE_ASYNC_LOGGING.set(enable)
+
+
+__all__ = [
+    "enable_system_metrics_logging",
+    "disable_system_metrics_logging",
+    "enable_async_logging",
+    "get_registry_uri",
+    "get_tracking_uri",
+    "is_tracking_uri_set",
+    "set_registry_uri",
+    "set_system_metrics_sampling_interval",
+    "set_system_metrics_samples_before_logging",
+    "set_system_metrics_node_id",
+    "set_tracking_uri",
+]
