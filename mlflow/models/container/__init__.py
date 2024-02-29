0f308ef4783b5a8ebb564211129820fadefcff78
Serena Ruan <serena.rxy@gmail.com>
2024-02-29 17:33:06 +0800

    pin mlserver
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- mlflow/models/container/__init__.py
+++ mlflow/models/container/__init__.py
@@ -95,7 +95,10 @@ def _install_pyfunc_deps(
     # dependency of mlflow on pip and we expect mlflow to be part of the environment.
     server_deps = ["gunicorn[gevent]"]
     if enable_mlserver:
-        server_deps = ["'mlserver>=1.2.0,!=1.3.1'", "'mlserver-mlflow>=1.2.0,!=1.3.1'"]
+        server_deps = [
+            "'mlserver>=1.2.0,!=1.3.1,<1.4.0'",
+            "'mlserver-mlflow>=1.2.0,!=1.3.1,<1.4.0'",
+        ]
 
     install_server_deps = [f"pip install {' '.join(server_deps)}"]
     if Popen(["bash", "-c", " && ".join(activate_cmd + install_server_deps)]).wait() != 0:
