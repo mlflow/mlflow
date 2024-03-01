2b8bdd2c7c6eda3d0bc4ec96ce929fd79a8675fc
Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
2024-03-01 13:51:29 +0900

    Support model evaluation with an endpoint URL (#11262)
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>
    Signed-off-by: Arthur Jenoudet <arthur.jenoudet@databricks.com>

--- mlflow/metrics/genai/genai_metric.py
+++ mlflow/metrics/genai/genai_metric.py
@@ -275,8 +275,8 @@ def make_genai_metric(
         if not isinstance(eval_model, str):
             raise MlflowException(
                 message="The model argument must be a string URI referring to an openai model "
-                "(openai:/gpt-3.5-turbo) or  gateway (gateway:/my-route), "
-                f"passed {eval_model} instead",
+                "(openai:/gpt-3.5-turbo) or an MLflow Deployments endpoint "
+                f"(endpoints:/my-endpoint), passed {eval_model} instead",
                 error_code=INVALID_PARAMETER_VALUE,
             )
 
