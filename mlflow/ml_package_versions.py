42b5c6c6befa2bce72f6a362634e1fe171d38453
Brynn Yin <biyi@microsoft.com>
2024-03-04 14:20:27 +0800

    Reapply "Implement promptflow model flavor (#10104)" (#10903)
    This reverts commit 97e85f939c28e56bd8f5591e3ecbca05542fb215.
    
    Signed-off-by: Brynn Yin <biyi@microsoft.com>

--- mlflow/ml_package_versions.py
+++ mlflow/ml_package_versions.py
@@ -266,6 +266,15 @@ _ML_PACKAGE_VERSIONS = {
             "maximum": "0.1.9"
         }
     },
+    "promptflow": {
+        "package_info": {
+            "pip_release": "promptflow"
+        },
+        "models": {
+            "minimum": "1.1.0",
+            "maximum": "1.3.0"
+        }
+    },
     "sentence_transformers": {
         "package_info": {
             "pip_release": "sentence-transformers"
