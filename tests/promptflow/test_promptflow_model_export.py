e708f345391cf228a4ed612cd317f32368b13602
Brynn Yin <biyi@microsoft.com>
2024-03-06 14:27:27 +0800

    Update example to remove promptflow-tools dependency
    Signed-off-by: Brynn Yin <biyi@microsoft.com>

--- tests/promptflow/test_promptflow_model_export.py
+++ tests/promptflow/test_promptflow_model_export.py
@@ -100,7 +100,7 @@ def test_promptflow_model_serve_predict():
     }
 
 
-def test_promptflow_model_sparkudf_predict():
+def test_promptflow_model_sparkudf_predict(spark):
     # Assert predict with promptflow model
     logged_model = log_promptflow_example_model()
     # Assert predict with spark udf
@@ -118,7 +118,7 @@ def test_promptflow_model_sparkudf_predict():
 def test_unsupported_class():
     mock_model = object()
     with pytest.raises(
-        MlflowException, match="only supports instances loaded by ~promptflow.load_flow"
+        MlflowException, match="only supports instances defined with 'flow.dag.yaml' file"
     ):
         with mlflow.start_run():
             mlflow.promptflow.log_model(mock_model, "mock_model_path")
