1958b9e4c277651ac7f64b6cdb7d5dc1045a23d1
harupy <17039389+harupy@users.noreply.github.com>
2024-03-06 09:58:54 +0900

    fix
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/models/model.py
+++ mlflow/models/model.py
@@ -496,9 +496,9 @@ class Model:
         if self.model_size_bytes is not None:
             res["model_size_bytes"] = self.model_size_bytes
         if self.artifact_path is None:
-            res.pop("run_id", None)
-        if self.artifact_path is None:
             res.pop("artifact_path", None)
+        if self.run_id is None:
+            res.pop("run_id", None)
         return res
 
     def to_yaml(self, stream=None):
