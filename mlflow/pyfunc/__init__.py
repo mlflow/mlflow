422dd5d790e3b9d7c9b282061b11daf506613a8e
Michael Berk <michaelberk99@gmail.com>
2024-03-11 22:34:10 -0400

    Update mlflow/pyfunc/__init__.py
    Co-authored-by: Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
    Signed-off-by: Michael Berk <michaelberk99@gmail.com>

--- mlflow/pyfunc/__init__.py
+++ mlflow/pyfunc/__init__.py
@@ -240,7 +240,9 @@ Function-based Model
 ####################
 If you're looking to serialize a simple python function without additional dependent methods, you
 can simply log a predict method via the keyword argument ``python_model``.
+.. note::
 
+    Function-based model only supports a function with a single input argument. If you would like to pass more arguments or additional inference parameters, please use the class-based model below.
 .. code-block:: python
     :caption: python_function Example
 
