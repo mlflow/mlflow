722cfb67d1379e08fc344a1dde0d6c751ab4e4fd
B-Step62 <yuki.watanabe@databricks.com>
2024-03-15 14:14:02 +0900

    Avoid return None in get_deployment_client
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- tests/deployments/test_interface.py
+++ tests/deployments/test_interface.py
@@ -1,31 +1,32 @@
 import pytest
 
 from mlflow.deployments.interface import get_deploy_client
+from mlflow.exceptions import MlflowException
 
 
-def test_get_deploy_client_no_args():
-    assert get_deploy_client() is None
+def test_get_deploy_client():
+    client = get_deploy_client("databricks")
+    assert client.target_uri == "databricks"
 
 
-def test_get_deploy_client_none():
-    assert get_deploy_client(None) is None
+def test_get_deploy_client_from_set_deployments_target(monkeypatch):
+    monkeypatch.setattr("mlflow.deployments.utils._deployments_target", "http://localhost")
+    client = get_deploy_client()
+    assert client.target_uri == "http://localhost"
 
 
-def test_get_deploy_client_from_set_deployments_target():
-    from mlflow.deployments import set_deployments_target
-
-    set_deployments_target("databricks")
-    assert get_deploy_client(None) is not None
-
-
-@pytest.fixture
-def set_deployment_envs(monkeypatch):
+def test_get_deploy_client_from_env(monkeypatch):
     monkeypatch.setenvs(
         {
             "MLFLOW_DEPLOYMENTS_TARGET": "databricks",
         }
     )
+    client = get_deploy_client()
+    assert client.target_uri == "databricks"
 
 
-def test_get_deploy_client_from_env(set_deployment_envs):
-    assert get_deploy_client(None) is not None
+def test_get_deploy_client_raise_if_no_target():
+    with pytest.raises(
+        MlflowException, match="No deployments target is found. Please either pass `target_uri`"
+    ):
+        get_deploy_client()
