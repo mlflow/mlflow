afd90f5676ddb157ed39007fa543bdd6f6665e70
Sunish Sheth <sunishsheth2009@gmail.com>
2024-03-18 21:35:41 -0700

    Ability to check for both imports of langchain and lanchain_community dependency (#11450)
    Signed-off-by: Sunish Sheth <sunishsheth2009@gmail.com>
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>
    Co-authored-by: mlflow-automation <mlflow-automation@users.noreply.github.com>
    Signed-off-by: Arthur Jenoudet <arthur.jenoudet@databricks.com>

--- tests/langchain/test_langchain_databricks_dependency_extraction.py
+++ tests/langchain/test_langchain_databricks_dependency_extraction.py
@@ -100,8 +100,52 @@ def test_parsing_dependency_from_databricks_retriever(monkeypatch: pytest.Monkey
 @pytest.mark.skipif(
     Version(langchain.__version__) < Version("0.0.311"), reason="feature not existing"
 )
+def test_parsing_dependency_from_databricks_retriever(monkeypatch: pytest.MonkeyPatch):
+    from langchain_community.embeddings import DatabricksEmbeddings
+    from langchain_community.vectorstores import DatabricksVectorSearch
+
+    vsc = MockVectorSearchClient()
+    vs_index = vsc.get_index(endpoint_name="dbdemos_vs_endpoint", index_name="mlflow.rag.vs_index")
+    mock_get_deploy_client = MagicMock()
+
+    monkeypatch.setattr("mlflow.deployments.get_deploy_client", mock_get_deploy_client)
+    embedding_model = DatabricksEmbeddings(endpoint="databricks-bge-large-en")
+
+    mock_module = MagicMock()
+    mock_module.VectorSearchIndex = MockVectorSearchIndex
+
+    monkeypatch.setitem(sys.modules, "databricks.vector_search.client", mock_module)
+
+    vectorstore = DatabricksVectorSearch(vs_index, text_column="content", embedding=embedding_model)
+    retriever = vectorstore.as_retriever()
+    d = defaultdict(list)
+    _extract_databricks_dependencies_from_retriever(retriever, d)
+    assert d.get(_DATABRICKS_EMBEDDINGS_ENDPOINT_NAME_KEY) == ["databricks-bge-large-en"]
+    assert d.get(_DATABRICKS_VECTOR_SEARCH_INDEX_NAME_KEY) == ["mlflow.rag.vs_index"]
+    assert d.get(_DATABRICKS_VECTOR_SEARCH_ENDPOINT_NAME_KEY) == ["dbdemos_vs_endpoint"]
+
+
+@pytest.mark.skipif(
+    Version(langchain.__version__) < Version("0.0.311"), reason="feature not existing"
+)
 def test_parsing_dependency_from_databricks_chat(monkeypatch: pytest.MonkeyPatch):
     from langchain.chat_models import ChatDatabricks
+
+    mock_get_deploy_client = MagicMock()
+
+    monkeypatch.setattr("mlflow.deployments.get_deploy_client", mock_get_deploy_client)
+
+    chat_model = ChatDatabricks(endpoint="databricks-llama-2-70b-chat", max_tokens=500)
+    d = defaultdict(list)
+    _extract_databricks_dependencies_from_chat_model(chat_model, d)
+    assert d.get(_DATABRICKS_CHAT_ENDPOINT_NAME_KEY) == ["databricks-llama-2-70b-chat"]
+
+
+@pytest.mark.skipif(
+    Version(langchain.__version__) < Version("0.0.311"), reason="feature not existing"
+)
+def test_parsing_dependency_from_databricks_chat(monkeypatch: pytest.MonkeyPatch):
+    from langchain_community.chat_models import ChatDatabricks
 
     mock_get_deploy_client = MagicMock()
 
