a4abc3cf3c5e9dd4958dfac2ce7aac8c37be6868
B-Step62 <yuki.watanabe@databricks.com>
2024-03-21 19:24:44 +0900

    Rename fluent.py to python_api.py
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- mlflow/__init__.py
+++ mlflow/__init__.py
@@ -105,7 +105,7 @@ from mlflow.config import (
 from mlflow.exceptions import MlflowException
 from mlflow.models import evaluate
 from mlflow.projects import run
-from mlflow.tracing.fluent import get_traces, start_span, trace
+from mlflow.tracing.python_api import start_span, trace
 from mlflow.tracking._model_registry.fluent import (
     register_model,
     search_model_versions,
