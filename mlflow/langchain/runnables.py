42936f07ce1e0562838ac447a32037fd124d4b11
Serena Ruan <82044803+serena-ruan@users.noreply.github.com>
2024-03-26 01:45:48 -0700

    Improve langchain runnable save exception handling (#11516)
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- mlflow/langchain/runnables.py
+++ mlflow/langchain/runnables.py
@@ -248,11 +248,8 @@ def _save_internal_runnables(runnable, path, loader_fn, persist_dir):
         # TODO: check if `dict` is enough to load it back
         elif hasattr(runnable, "dict"):
             runnable_dict = runnable.dict()
-            try:
-                with open(path, "w") as f:
-                    yaml.dump(runnable_dict, f, default_flow_style=False)
-            except Exception as e:
-                return e
+            with open(path, "w") as f:
+                yaml.dump(runnable_dict, f, default_flow_style=False)
         else:
             return Exception(f"Runnable {runnable} is not supported for saving.")
     return conf
@@ -312,11 +309,12 @@ def _save_runnable_with_steps(model, file_path: Union[Path, str], loader_fn=None
         # Save each step into a subfolder named by step
         save_runnable_path = steps_path / step
         save_runnable_path.mkdir()
-        result = _save_internal_runnables(runnable, save_runnable_path, loader_fn, persist_dir)
-        if isinstance(result, Exception):
-            unsaved_runnables[step] = f"{runnable} -- {result}"
-        else:
-            steps_conf[step] = result
+        try:
+            steps_conf[step] = _save_internal_runnables(
+                runnable, save_runnable_path, loader_fn, persist_dir
+            )
+        except Exception as e:
+            unsaved_runnables[step] = f"{runnable} -- {e}"
 
     if unsaved_runnables:
         raise MlflowException(f"Failed to save runnable sequence: {unsaved_runnables}.")
@@ -346,21 +344,22 @@ def _save_runnable_branch(model, file_path, loader_fn, persist_dir):
             save_runnable_path.mkdir(parents=True)
             branches_conf[f"{index}-{i}"] = {}
 
-            result = _save_internal_runnables(runnable, save_runnable_path, loader_fn, persist_dir)
-            if isinstance(result, Exception):
-                unsaved_runnables[f"{index}-{i}"] = f"{runnable} -- {result}"
-            else:
-                branches_conf[f"{index}-{i}"] = result
+            try:
+                branches_conf[f"{index}-{i}"] = _save_internal_runnables(
+                    runnable, save_runnable_path, loader_fn, persist_dir
+                )
+            except Exception as e:
+                unsaved_runnables[f"{index}-{i}"] = f"{runnable} -- {e}"
 
     # save default branch
     default_branch_path = branches_path / _DEFAULT_BRANCH_NAME
     default_branch_path.mkdir()
-    result = _save_internal_runnables(model.default, default_branch_path, loader_fn, persist_dir)
-    if isinstance(result, Exception):
-        unsaved_runnables[_DEFAULT_BRANCH_NAME] = f"{model.default} -- {result}"
-    else:
-        branches_conf[_DEFAULT_BRANCH_NAME] = result
-
+    try:
+        branches_conf[_DEFAULT_BRANCH_NAME] = _save_internal_runnables(
+            model.default, default_branch_path, loader_fn, persist_dir
+        )
+    except Exception as e:
+        unsaved_runnables[_DEFAULT_BRANCH_NAME] = f"{model.default} -- {e}"
     if unsaved_runnables:
         raise MlflowException(f"Failed to save runnable branch: {unsaved_runnables}.")
 
