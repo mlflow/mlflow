bb2939a319274fdaf2d7d56255c969a3bb2654f8
Daniel Lok <daniel.lok@databricks.com>
2024-03-26 10:55:43 +0900

    Add mimetype to trace (#11522)
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/tracing/types/model.py
+++ mlflow/tracing/types/model.py
@@ -22,6 +22,21 @@ class Trace:
     def to_json(self) -> str:
         return json.dumps(asdict(self), default=str)
 
+    def _repr_mimebundle_(self, include=None, exclude=None):
+        """
+        This method is used to trigger custom display logic in IPython notebooks.
+        See https://ipython.readthedocs.io/en/stable/config/integrating.html#MyObject
+        for more details.
+
+        At the moment, the only supported MIME type is "application/databricks.mlflow.trace",
+        which contains a JSON representation of the Trace object. This object is deserialized
+        in Databricks notebooks to display the Trace object in a nicer UI.
+        """
+        return {
+            "application/databricks.mlflow.trace": self.to_json(),
+            "text/plain": self.__repr__(),
+        }
+
 
 @dataclass
 class TraceInfo:
