9e1bfbc0497cb3c3a1e6655e68205bbdda428eff
Weichen Xu <weichen.xu@databricks.com>
2024-03-28 18:09:43 +0800

    fix
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/pyfunc/__init__.py
+++ mlflow/pyfunc/__init__.py
@@ -658,7 +658,7 @@ class PyFuncModel:
                 "Input data is a Spark DataFrame. Note that behaviour for "
                 "Spark DataFrames is model dependent."
             )
-        return data
+        return data, params
 
     def predict(self, data: PyFuncInput, params: Optional[Dict[str, Any]] = None) -> PyFuncOutput:
         """
@@ -692,7 +692,7 @@ class PyFuncModel:
             Model predictions as one of pandas.DataFrame, pandas.Series, numpy.ndarray or list.
         """
 
-        data = self._validate_prediction_input(data)
+        data, params = self._validate_prediction_input(data, params)
         if inspect.signature(self._predict_fn).parameters.get("params"):
             return self._predict_fn(data, params=params)
         return self._predict_fn(data)
@@ -701,7 +701,7 @@ class PyFuncModel:
         if self._predict_stream_fn is None:
             raise MlflowException("This model does not support predict_stream method.")
 
-        data = self._validate_prediction_input(data)
+        data, params = self._validate_prediction_input(data, params)
         if inspect.signature(self._predict_fn).parameters.get("params"):
             return self._predict_stream_fn(data, params=params)
         return self._predict_stream_fn(data)
