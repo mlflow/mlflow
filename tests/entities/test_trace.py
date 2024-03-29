79107fb941b8c4f4ee25478366fb076a8d9e6865
B-Step62 <yuki.watanabe@databricks.com>
2024-03-29 19:47:36 +0900

    Handle serialization for pydantic model
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- tests/entities/test_trace.py
+++ tests/entities/test_trace.py
@@ -1,7 +1,12 @@
+import importlib
 import json
+
+import pytest
+from packaging.version import Version
 
 import mlflow
 from mlflow.entities import SpanType
+from mlflow.entities.trace import _TraceJSONEncoder
 
 from tests.tracing.conftest import mock_client as mock_trace_client  # noqa: F401
 
@@ -78,4 +83,69 @@ def test_json_deserialization(mock_trace_client):
                 },
             ],
         },
+    }
+
+
+@pytest.mark.skipif(
+    importlib.util.find_spec("pydantic") is None, reason="Pydantic is not installed"
+)
+def test_trace_serialize_pydantic_model():
+    from pydantic import BaseModel
+
+    class MyModel(BaseModel):
+        x: int
+        y: str
+
+    data = MyModel(x=1, y="foo")
+    data_json = json.dumps(data, cls=_TraceJSONEncoder)
+    assert data_json == '{"x": 1, "y": "foo"}'
+    assert json.loads(data_json) == {"x": 1, "y": "foo"}
+
+
+def _is_langchain_v0_1():
+    try:
+        import langchain
+
+        return Version(langchain.__version__) >= Version("0.1")
+    except ImportError:
+        return None
+
+
+@pytest.mark.skipif(not _is_langchain_v0_1(), reason="langchain is not installed")
+def test_trace_serialize_langchain_base_message():
+    from langchain_core.messages import BaseMessage
+
+    message = BaseMessage(
+        content=[
+            {
+                "role": "system",
+                "content": "Hello, World!",
+            },
+            {
+                "role": "user",
+                "content": "Hi!",
+            },
+        ],
+        type="chat",
+    )
+
+    message_json = json.dumps(message, cls=_TraceJSONEncoder)
+    # "additional_kwargs" field is added by Langchain
+    assert message_json == (
+        '{"content": [{"role": "system", "content": "Hello, World!"}, '
+        '{"role": "user", "content": "Hi!"}], "additional_kwargs": {}, "type": "chat"}'
+    )
+    assert json.loads(message_json) == {
+        "content": [
+            {
+                "role": "system",
+                "content": "Hello, World!",
+            },
+            {
+                "role": "user",
+                "content": "Hi!",
+            },
+        ],
+        "type": "chat",
+        "additional_kwargs": {},
     }
