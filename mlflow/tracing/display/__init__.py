9901a37d710488aa8f6168fc7b47b8980265c5b9
Daniel Lok <daniel.lok@databricks.com>
2024-04-06 15:00:58 +0900

    add test
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/tracing/display/__init__.py
+++ mlflow/tracing/display/__init__.py
@@ -1,6 +1,6 @@
 from mlflow.tracing.display.display_client import IPythonTraceDisplayClient
 
-__all__ = ["IPythonTraceDisplayClient"]
+__all__ = ["IPythonTraceDisplayClient", "get_display_client"]
 
 
 def get_display_client() -> IPythonTraceDisplayClient:
