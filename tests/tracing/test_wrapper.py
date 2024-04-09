e745a9f168ed0f988a00a9cbb96afe7216b21189
B-Step62 <yuki.watanabe@databricks.com>
2024-04-09 14:27:31 +0900

    Add 'tr-' prefix to trace request IDs
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- tests/tracing/test_wrapper.py
+++ tests/tracing/test_wrapper.py
@@ -14,7 +14,7 @@ def test_wrapper_property():
     end_time = start_time + 1_000_000
 
     mock_otel_span = mock.MagicMock()
-    mock_otel_span.get_span_context().trace_id = "trace_id"
+    mock_otel_span.get_span_context().trace_id = "12345"
     mock_otel_span.get_span_context().span_id = "span_id"
     mock_otel_span._start_time = start_time
     mock_otel_span._end_time = end_time
@@ -22,7 +22,7 @@ def test_wrapper_property():
 
     span = MlflowSpanWrapper(mock_otel_span)
 
-    assert span.request_id == "trace_id"
+    assert span.request_id == "tr-12345"
     assert span.span_id == "span_id"
     assert span.start_time == start_time // 1_000
     assert span.end_time == end_time // 1_000
