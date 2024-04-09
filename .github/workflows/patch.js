37dfa02210292cff4922e49dc5b7cbc08b8cc1f0
harupy <17039389+harupy@users.noreply.github.com>
2024-04-09 18:01:16 +0900

    Fix
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- .github/workflows/patch.js
+++ .github/workflows/patch.js
@@ -13,9 +13,9 @@ module.exports = async ({ context, github, core }) => {
 
   const patchSection = body.split(marker)[1];
   const yesMatch = patchSection.match(/- \[( |x)\] yes/gi);
-  const yes = yesMatch ? yesSelected[0].toLowerCase() === "x" : false;
+  const yes = yesMatch ? yesMatch[0].toLowerCase() === "x" : false;
   const noMatch = patchSection.match(/- \[( |x)\] no/gi);
-  const no = noMatch ? noSelected[0].toLowerCase() === "x" : false;
+  const no = noMatch ? noMatch[0].toLowerCase() === "x" : false;
 
   if (yes && no) {
     core.setFailed("Both yes and no are selected. Please select only one.");
