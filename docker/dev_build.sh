ae19fe5d36c45b8615588c3b9fc19bd3df90ed76
Jose Maria Lizano <jmlizlac@gmail.com>
2024-04-09 18:09:54 +0200

    Remove unnecessary script
    Signed-off-by: Jose Maria Lizano <jmlizlac@gmail.com>

--- docker/dev_build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-ROOT_DIR=$(git rev-parse --show-toplevel)
-
-DOCKER_BUILDKIT=1 docker build -f "${ROOT_DIR}/docker/Dockerfile.full.dev" -t mlflow-docker-test "$ROOT_DIR"
