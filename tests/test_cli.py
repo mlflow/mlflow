8ec8a8cff6dd1f62eddece3e7dca5e1221ea127e
Michael Li <minghao_li@outlook.com>
2024-04-17 10:21:27 -0700

    fix: get run id from run obj
    Signed-off-by: Michael Li <minghao_li@outlook.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -411,7 +411,7 @@ def test_mlflow_gc_experiments(get_store_details, request):
     exp_id_6 = store.create_experiment("6")
     run_id_2 = store.create_run(exp_id_6, user_id="user", start_time=1, tags=[], run_name="2")
     run_id_2_datasets = [DatasetInput(dataset=numpy_dataset.from_numpy(np.array([1, 2, 3])))]
-    store.log_inputs(run_id_2, datasets=run_id_2_datasets)
+    store.log_inputs(run_id_2.info.run_id, datasets=run_id_2_datasets)
     store.delete_experiment(exp_id_6)
     invoke_gc("--backend-store-uri", uri, "--experiment-ids", exp_id_6)
     assert sorted([e.experiment_id for e in experiments]) == sorted(
