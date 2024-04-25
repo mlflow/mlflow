6ced8b2aca7adee5edbf6ddc33ff1a0d77429e9d
Ann Zhang <ann.zhang@databricks.com>
2024-04-24 17:06:59 -0700

    check code_paths existence
    Signed-off-by: Ann Zhang <ann.zhang@databricks.com>

--- mlflow/langchain/__init__.py
+++ mlflow/langchain/__init__.py
@@ -255,7 +255,7 @@ def save_model(
                 "file path containing the code for defining the chain instance."
             )
 
-        if len(code_paths) > 1:
+        if code_paths and len(code_paths) > 1:
             raise mlflow.MlflowException.invalid_parameter_value(
                 "When the model is a string, and if the code_paths are specified, "
                 "it should contain only one path."
