ea4d0c447a43d6b0fcce90f4a9f09fd9523498ac
Ann Zhang <ann.zhang@databricks.com>
2024-04-26 22:22:53 -0700

    remove extra pyfunc code
    Signed-off-by: Ann Zhang <ann.zhang@databricks.com>

--- mlflow/utils/model_utils.py
+++ mlflow/utils/model_utils.py
@@ -301,11 +301,6 @@ def _validate_pyfunc_model_config(model_config):
     if not model_config:
         return
 
-    if isinstance(model_config, str):
-        # TODO: check that model_config is a valid path, and can be read. validate that it is yaml?
-        # raise exception if it is not
-        return
-
     if not isinstance(model_config, dict) or not all(isinstance(key, str) for key in model_config):
         raise MlflowException(
             "An invalid ``model_config`` structure was passed. ``model_config`` must be of type "
