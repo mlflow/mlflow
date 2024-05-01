3025f959b52436c849277a15f1faf8e3387dba47
B-Step62 <yuki.watanabe@databricks.com>
2024-05-01 16:31:40 +0900

    update test sql schema
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- tests/resources/db/latest_schema.sql
+++ tests/resources/db/latest_schema.sql
@@ -136,7 +136,7 @@ CREATE TABLE trace_info (
 	execution_time_ms BIGINT,
 	status VARCHAR(50) NOT NULL,
 	CONSTRAINT trace_info_pk PRIMARY KEY (request_id),
-	FOREIGN KEY(experiment_id) REFERENCES experiments (experiment_id)
+	CONSTRAINT fk_trace_info_experiment_id FOREIGN KEY(experiment_id) REFERENCES experiments (experiment_id)
 )
 
 
@@ -199,7 +199,7 @@ CREATE TABLE trace_request_metadata (
 	value VARCHAR(8000) NOT NULL,
 	request_id VARCHAR(50) NOT NULL,
 	CONSTRAINT trace_request_metadata_pk PRIMARY KEY (key, request_id),
-	FOREIGN KEY(request_id) REFERENCES trace_info (request_id)
+	CONSTRAINT fk_trace_request_metadata_request_id FOREIGN KEY(request_id) REFERENCES trace_info (request_id)
 )
 
 
@@ -208,6 +208,6 @@ CREATE TABLE trace_tags (
 	value VARCHAR(8000) NOT NULL,
 	request_id VARCHAR(50) NOT NULL,
 	CONSTRAINT trace_tag_pk PRIMARY KEY (key, request_id),
-	FOREIGN KEY(request_id) REFERENCES trace_info (request_id)
+	CONSTRAINT fk_trace_tags_request_id FOREIGN KEY(request_id) REFERENCES trace_info (request_id)
 )
 
