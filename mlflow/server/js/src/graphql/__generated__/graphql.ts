aed1c547255361c955e852bb3aaecdc3ff27b7eb
edwardfeng-db <jinghao.feng@databricks.com>
2024-05-10 14:35:22 +0200

    update

--- mlflow/server/js/src/graphql/__generated__/graphql.ts
+++ mlflow/server/js/src/graphql/__generated__/graphql.ts
@@ -3,13 +3,11 @@ export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
 export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: { input: string; output: string; }
   String: { input: string; output: string; }
-  Boolean: { input: boolean; output: boolean; }
   Int: { input: number; output: number; }
   Float: { input: number; output: number; }
   /** LongString Scalar type to prevent truncation to max integer in JavaScript. */
