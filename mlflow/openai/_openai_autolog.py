17dab45d68aeaef0005e75f2f503af9b0505f219
Gabriel Fu <hfu.gabriel@gmail.com>
2024-05-13 10:27:27 +0800

    remove unused func
    Signed-off-by: Gabriel Fu <hfu.gabriel@gmail.com>

--- mlflow/openai/_openai_autolog.py
+++ mlflow/openai/_openai_autolog.py
@@ -52,16 +52,6 @@ def _get_input_from_model(model, kwargs):
     )
 
 
-def _convert_data_to_dict(data, key):
-    if isinstance(data, dict):
-        return {f"{key}-{k}": v for k, v in data.items()}
-    if isinstance(data, list):
-        return {key: data}
-    if isinstance(data, str):
-        return {key: [data]}
-    raise MlflowException(f"Unsupported data type: {type(data).__name__}")
-
-
 @contextmanager
 def _set_api_key_env_var(client):
     """
