79217809e32fce79a190a1d82419534105109083
Weichen Xu <weichen.xu@databricks.com>
2024-05-15 07:55:03 +0800

    address comments
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/utils/docstring_utils.py
+++ mlflow/utils/docstring_utils.py
@@ -296,7 +296,8 @@ limitations, see the
         "infer_code_paths": (
             """If set to ``True``, MLflow automatically infers model code paths. The inferred
             code path files only include necessary python module files. Only python code files
-            under current working directory is automatic inferrable. Default value is ``False``.
+            under current working directory are automatically inferrable. Default value is
+            ``False``.
 
 .. warning::
     Please ensure that the custom python module code does not contain sensitive data such as
