125402cea648b40e10f52e9aa60d5c7851f59ded
harupy <17039389+harupy@users.noreply.github.com>
2024-05-16 22:03:20 +0900

    Set daemon to True
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/pyfunc/docker/test_docker_flavors.py
+++ tests/pyfunc/docker/test_docker_flavors.py
@@ -98,7 +98,7 @@ def start_container(port: int):
             sys.stdout.write(line.decode("utf-8"))
 
     # Start a thread to stream logs from the container
-    thread = threading.Thread(target=log_stream)
+    thread = threading.Thread(target=log_stream, daemon=True)
     thread.start()
 
     try:
