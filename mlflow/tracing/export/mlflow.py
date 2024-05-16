f1a49d4d90d382c4de6edd25bd25beb18f6fbed6
Jesse Chan <jesse.chan@databricks.com>
2024-05-16 14:30:08 -0700

    Store span names & types, input names & types as internal trace tag (#12015)
    Signed-off-by: Jesse Chan <jesse.chan@databricks.com>

--- mlflow/tracing/export/mlflow.py
+++ mlflow/tracing/export/mlflow.py
@@ -72,6 +72,11 @@ class MlflowSpanExporter(SpanExporter):
             self._log_trace(trace)
 
     def _log_trace(self, trace: Trace):
+        try:
+            self._client._upload_trace_spans_as_tag(trace.info, trace.data)
+        except Exception as e:
+            _logger.debug(f"Failed to log trace spans as tag to MLflow backend: {e}", exc_info=True)
+
         # TODO: Make this async
         # The trace is already updated in processor.on_end method
         # so we just log to backend store here
