fca865cac9cf9b96d120aedc8ae56a4fc11496cb
Jesse Chan <jesse.chan@databricks.com>
2024-05-16 14:30:08 -0700

    Store span names & types, input names & types as internal trace tag (#12015)
    Signed-off-by: Jesse Chan <jesse.chan@databricks.com>

--- tests/tracking/test_client.py
+++ tests/tracking/test_client.py
@@ -1,3 +1,4 @@
+import json
 import pickle
 import time
 from unittest import mock
@@ -540,6 +541,8 @@ def test_log_trace_with_databricks_tracking_uri(
         "mlflow.tracking._tracking_service.client.TrackingServiceClient._upload_trace_data"
     ) as mock_upload_trace_data, mock.patch(
         "mlflow.tracking._tracking_service.client.TrackingServiceClient.set_trace_tags",
+    ), mock.patch(
+        "mlflow.tracking._tracking_service.client.TrackingServiceClient.set_trace_tag",
     ), mock.patch(
         "mlflow.tracking._tracking_service.client.TrackingServiceClient.get_trace_info",
     ), mock.patch(
@@ -1360,3 +1363,75 @@ def test_file_store_download_upload_trace_data(clear_singleton, tmp_path):
         trace_data = client.get_trace(span.request_id).data
         assert trace_data.request == trace.data.request
         assert trace_data.response == trace.data.response
+
+
+def test_store_trace_spans_tag():
+    client = MlflowClient()
+
+    trace_spans_tag_value = {
+        "name": "test",
+        "type": "UNKNOWN",
+        "inputs": ["test"],
+        "outputs": ["result"],
+    }
+
+    with mock.patch(
+        "mlflow.tracking._tracking_service.client.TrackingServiceClient.set_trace_tag",
+    ) as mock_set_trace_tag:
+        span = client.start_trace("test", inputs={"test": 1})
+        client.end_trace(span.request_id, outputs={"result": 2})
+        mock_set_trace_tag.assert_called_once()
+        assert mock_set_trace_tag.call_args[0][0] == span.request_id
+        assert mock_set_trace_tag.call_args[0][1] == "mlflow.traceSpans"
+        result = json.loads(mock_set_trace_tag.call_args[0][2])
+        for key in trace_spans_tag_value:
+            assert result[0][key] == trace_spans_tag_value[key]
+
+
+def test_store_trace_span_tag_when_not_dict_input_outputs():
+    client = MlflowClient()
+
+    with mock.patch(
+        "mlflow.tracking._tracking_service.client.TrackingServiceClient.set_trace_tag",
+    ) as mock_set_trace_tag:
+        span = client.start_trace("trace_name", inputs="test")
+        client.end_trace(span.request_id, outputs={"result": 2})
+        mock_set_trace_tag.assert_called_once()
+        assert mock_set_trace_tag.call_args[0][0] == span.request_id
+        assert mock_set_trace_tag.call_args[0][1] == "mlflow.traceSpans"
+        result = json.loads(mock_set_trace_tag.call_args[0][2])
+        assert result[0]["name"] == "trace_name"
+        assert result[0]["type"] == "UNKNOWN"
+        assert result[0]["outputs"] == ["result"]
+        assert "inputs" not in result[0]
+
+    with mock.patch(
+        "mlflow.tracking._tracking_service.client.TrackingServiceClient.set_trace_tag",
+    ) as mock_set_trace_tag:
+        span = client.start_trace("trace_name", inputs="{'input': 2}")
+        client.end_trace(span.request_id, outputs="result")
+        mock_set_trace_tag.assert_called_once()
+        assert mock_set_trace_tag.call_args[0][0] == span.request_id
+        assert mock_set_trace_tag.call_args[0][1] == "mlflow.traceSpans"
+        result = json.loads(mock_set_trace_tag.call_args[0][2])
+        assert result[0]["name"] == "trace_name"
+        assert result[0]["type"] == "UNKNOWN"
+        assert "outputs" not in result[0]
+        assert "inputs" not in result[0]
+
+
+# when JSON is too large, we skip logging the tag. The trace should still be logged.
+def test_store_trace_span_tag_when_exception_raised():
+    client = MlflowClient()
+
+    with mock.patch(
+        "mlflow.tracking._tracking_service.client.TrackingServiceClient._upload_trace_data"
+    ) as mock_upload_trace_data, mock.patch(
+        "mlflow.tracking._tracking_service.client.TrackingServiceClient.set_trace_tag",
+        side_effect=MlflowException("Failed to log parameters"),
+    ) as mock_set_trace_tag:
+        # This should not raise an exception
+        span = client.start_trace("trace_name", inputs={"input": "a" * 1000000})
+        client.end_trace(span.request_id, outputs={"result": "b" * 1000000})
+        mock_set_trace_tag.assert_called_once()
+        mock_upload_trace_data.assert_called_once()
