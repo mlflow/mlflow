19848d09d4541012f74add10809cf467e8236592
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2024-05-16 13:05:37 +0000

    Remove json tags from models
    Signed-off-by: Jonathan Giannuzzi <jonathan@giannuzzi.me>

--- mlflow/go/pkg/store/sql/model/experiments.go
+++ mlflow/go/pkg/store/sql/model/experiments.go
@@ -10,13 +10,13 @@ import (
 
 // Experiment mapped from table <experiments>.
 type Experiment struct {
-	ExperimentID     *int32          `gorm:"column:experiment_id;primaryKey;autoIncrement:true" json:"experiment_id"`
-	Name             *string         `gorm:"column:name;not null"                               json:"name"`
-	ArtifactLocation *string         `gorm:"column:artifact_location"                           json:"artifact_location"`
-	LifecycleStage   *string         `gorm:"column:lifecycle_stage"                             json:"lifecycle_stage"`
-	CreationTime     *int64          `gorm:"column:creation_time"                               json:"creation_time"`
-	LastUpdateTime   *int64          `gorm:"column:last_update_time"                            json:"last_update_time"`
-	ExperimentTags   []ExperimentTag `gorm:"foreignKey:experiment_id;references:experiment_id"  json:"experiment_tags"`
+	ExperimentID     *int32          `gorm:"column:experiment_id;primaryKey;autoIncrement:true"`
+	Name             *string         `gorm:"column:name;not null"`
+	ArtifactLocation *string         `gorm:"column:artifact_location"`
+	LifecycleStage   *string         `gorm:"column:lifecycle_stage"`
+	CreationTime     *int64          `gorm:"column:creation_time"`
+	LastUpdateTime   *int64          `gorm:"column:last_update_time"`
+	ExperimentTags   []ExperimentTag `gorm:"foreignKey:experiment_id;references:experiment_id"`
 }
 
 func (e Experiment) ToProto() *protos.Experiment {
