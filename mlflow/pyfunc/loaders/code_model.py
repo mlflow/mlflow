1368889a086b5b163bf24ae98815dc96bc8a3eb7
Ann Zhang <ann.zhang@databricks.com>
2024-05-16 17:49:46 -0700

    check for chatmodel type
    Signed-off-by: Ann Zhang <ann.zhang@databricks.com>

--- mlflow/pyfunc/loaders/code_model.py
+++ mlflow/pyfunc/loaders/code_model.py
@@ -1,6 +1,8 @@
 from typing import Any, Dict, Optional
 
+from mlflow.pyfunc.loaders.chat_model import _ChatModelPyfuncWrapper
 from mlflow.pyfunc.model import (
+    ChatModel,
     _load_context_model_and_signature,
     _PythonModelPyfuncWrapper,
 )
@@ -8,4 +10,7 @@ from mlflow.pyfunc.model import (
 
 def _load_pyfunc(local_path: str, model_config: Optional[Dict[str, Any]] = None):
     context, model, signature = _load_context_model_and_signature(local_path, model_config)
-    return _PythonModelPyfuncWrapper(model, context, signature)
+    if isinstance(model, ChatModel):
+        return _ChatModelPyfuncWrapper(model, context, signature)
+    else:
+        return _PythonModelPyfuncWrapper(model, context, signature)
