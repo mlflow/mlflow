b11d1bd184eac5ecaf9f7903c7f3473a9a224dd6
Serena Ruan <serena.rxy@gmail.com>
2024-05-19 18:43:40 +0800

    update
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- mlflow/langchain/__init__.py
+++ mlflow/langchain/__init__.py
@@ -644,20 +644,22 @@ class _LangChainModelWrapper:
         from mlflow.langchain.langchain_tracer import MlflowLangchainTracer
 
         if callback_handlers:
-            if tracer := next(
-                (c for c in callback_handlers if isinstance(c, MlflowLangchainTracer)), None
+            if (
+                tracer := next(
+                    (c for c in callback_handlers if isinstance(c, MlflowLangchainTracer)), None
+                )
+                and self.model_path
             ):
-                if isinstance(tracer, MlflowLangchainTracer) and self.model_path:
-                    model = Model.load(self.model_path)
-                    context = tracer._prediction_context
-                    if model.metadata and context:
-                        dependencies_schema = model.metadata.get("dependencies_schemas", {})
-                        context.update(
-                            dependencies_schema={
-                                dependency: json.dumps(schema)
-                                for dependency, schema in dependencies_schema.items()
-                            }
-                        )
+                model = Model.load(self.model_path)
+                context = tracer._prediction_context
+                if model.metadata and context:
+                    dependencies_schema = model.metadata.get("dependencies_schemas", {})
+                    context.update(
+                        dependencies_schema={
+                            dependency: json.dumps(schema)
+                            for dependency, schema in dependencies_schema.items()
+                        }
+                    )
 
     @experimental
     def _predict_with_callbacks(
