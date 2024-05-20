feb0e8adc338962b10160f55aefd2016ea60ade8
Weichen Xu <weichen.xu@databricks.com>
2024-05-20 11:20:23 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/johnsnowlabs/__init__.py
+++ mlflow/johnsnowlabs/__init__.py
@@ -76,7 +76,10 @@ from mlflow.tracking.artifact_utils import (
 )
 from mlflow.utils import databricks_utils
 from mlflow.utils.annotations import experimental
-from mlflow.utils.databricks_utils import is_in_databricks_runtime
+from mlflow.utils.databricks_utils import (
+    is_in_databricks_runtime,
+    is_in_databricks_model_serving_environment,
+)
 from mlflow.utils.docstring_utils import LOG_MODEL_PARAM_DOCS, format_docstring
 from mlflow.utils.environment import (
     _CONDA_ENV_FILE_NAME,
@@ -759,11 +762,10 @@ def _load_pyfunc(path, spark=None):
 
     Returns:
         None.
-
     """
-    if not is_in_databricks_runtime():
+    try:
         popped_envs = {}
-        try:
+        if not is_in_databricks_runtime() or is_in_databricks_model_serving_environment():
             # Johnsnowlab code uses incorrect logic to check if it is in Databricks Runtime
             # if there is any environmental variable key contains 'DATABRICKS', it is regarded
             # as the databricks runtime.
@@ -772,16 +774,12 @@ def _load_pyfunc(path, spark=None):
                 if 'DATABRICKS' in k:
                     v = os.environ.pop(k)
                     popped_envs[k] = v
-            spark = spark or _get_or_create_sparksession(path)
-        finally:
-            os.environ.update(popped_envs)
-    else:
-        spark = spark or _get_or_create_sparksession(path)
-
-    return _PyFuncModelWrapper(
-        _load_model(model_uri=path),
-        spark,
-    )
+        return _PyFuncModelWrapper(
+            _load_model(model_uri=path),
+            spark or _get_or_create_sparksession(path),
+        )
+    finally:
+        os.environ.update(popped_envs)
 
 
 def _get_or_create_sparksession(model_path=None):
