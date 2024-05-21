e92ebcbc3644edf5c3117d5b85eed78302c1317e
Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
2024-05-21 19:02:19 +0900

    Convert spans to non-live once completed (#12078)
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- mlflow/tracing/trace_manager.py
+++ mlflow/tracing/trace_manager.py
@@ -26,7 +26,8 @@ class _Trace:
     def to_mlflow_trace(self) -> Trace:
         trace_data = TraceData()
         for span in self.span_dict.values():
-            trace_data.spans.append(span)
+            # Convert LiveSpan, mutable objects, into immutable Span objects before persisting.
+            trace_data.spans.append(span.to_immutable_span())
             if span.parent_id is None:
                 # Accessing the OTel span directly get serialized value directly.
                 trace_data.request = span._span.attributes.get(SpanAttributeKey.INPUTS)
