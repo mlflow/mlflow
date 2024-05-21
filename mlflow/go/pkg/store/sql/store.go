a6895a12a65a59432433d4a336067a667e68767f
nojaf <florian.verdonck@outlook.com>
2024-05-21 12:17:35 +0000

    Add order by and next page token
    Signed-off-by: nojaf <florian.verdonck@outlook.com>

--- mlflow/go/pkg/store/sql/store.go
+++ mlflow/go/pkg/store/sql/store.go
@@ -6,12 +6,15 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
+	"regexp"
 	"strconv"
 	"strings"
 
+	"github.com/gofiber/fiber/v2/log"
 	"github.com/ncruces/go-sqlite3/gormlite"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 	"gorm.io/gorm/logger"
 
 	"github.com/mlflow/mlflow/mlflow/go/pkg/config"
@@ -101,6 +104,10 @@ type PageToken struct {
 	Offset int32 `json:"offset"`
 }
 
+var runOrder = regexp.MustCompile(
+	`^(attribute|metric|param|tag)s?\.("[^"]+"|` + "`[^`]+`" + `|[\w\.]+)(?i:\s+(ASC|DESC))?$`,
+)
+
 func (s Store) SearchRuns(
 	experimentIDs []string,
 	filter *string,
@@ -153,6 +160,63 @@ func (s Store) SearchRuns(
 	}
 	tx.Offset(offset)
 
+	// Filter
+
+	// OrderBy
+	startTimeOrder := false
+	for n, o := range orderBy {
+		components := runOrder.FindStringSubmatch(o)
+		log.Debugf("Components: %#v", components)
+		if len(components) < 3 {
+			return nil, nil, contract.NewError(
+				protos.ErrorCode_INVALID_PARAMETER_VALUE,
+				"invalid order by clause: "+o,
+			)
+		}
+
+		column := strings.Trim(components[2], "`\"")
+
+		var kind any
+		switch components[1] {
+		case "attribute":
+			if column == "start_time" {
+				startTimeOrder = true
+			}
+		case "metric":
+			kind = &model.LatestMetric{}
+		case "param":
+			kind = &model.Param{}
+		case "tag":
+			kind = &model.Tag{}
+		default:
+			return nil, nil, contract.NewError(
+				protos.ErrorCode_INVALID_PARAMETER_VALUE,
+				fmt.Sprintf(
+					"invalid entity type '%s'. Valid values are ['metric', 'parameter', 'tag', 'attribute']",
+					components[1],
+				),
+			)
+		}
+		if kind != nil {
+			table := fmt.Sprintf("order_%d", n)
+			tx.Joins(
+				fmt.Sprintf("LEFT OUTER JOIN (?) AS %s ON runs.run_uuid = %s.run_uuid", table, table),
+				s.db.Select("run_uuid", "value").Where("key = ?", column).Model(kind),
+			)
+			column = table + ".value"
+		}
+		tx.Order(clause.OrderByColumn{
+			Column: clause.Column{
+				Name: column,
+			},
+			Desc: len(components) == 4 && strings.ToUpper(components[3]) == "DESC",
+		})
+	}
+	if !startTimeOrder {
+		tx.Order("runs.start_time DESC")
+	}
+	tx.Order("runs.run_uuid")
+
 	// Actual query
 	var runs []model.Run
 	tx.Preload("LatestMetrics").
@@ -176,7 +240,24 @@ func (s Store) SearchRuns(
 		contractRuns = append(contractRuns, run.ToProto())
 	}
 
-	return contractRuns, nil, nil
+	var nextPageToken string
+	if len(runs) == maxResults {
+		var token strings.Builder
+		if err := json.NewEncoder(
+			base64.NewEncoder(base64.StdEncoding, &token),
+		).Encode(PageToken{
+			Offset: int32(offset + maxResults),
+		}); err != nil {
+			return nil, nil, contract.NewErrorWith(
+				protos.ErrorCode_INTERNAL_ERROR,
+				"error encoding 'nextPageToken' value",
+				err,
+			)
+		}
+		nextPageToken = token.String()
+	}
+
+	return contractRuns, &nextPageToken, nil
 }
 
 func NewSQLStore(config *config.Config) (store.MlflowStore, error) {
