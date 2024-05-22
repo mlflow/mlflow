fd53046ddc416a19345eb6ab6a6058a046158726
Serena Ruan <82044803+serena-ruan@users.noreply.github.com>
2024-05-21 18:50:31 -0700

    Auto set prediction_context for offline prediction (#12084)
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- tests/pyfunc/sample_code/code_with_dependencies.py
+++ tests/pyfunc/sample_code/code_with_dependencies.py
@@ -1,9 +1,24 @@
+import os
+
 import mlflow
 from mlflow.models import set_model, set_retriever_schema
 from mlflow.pyfunc import PythonModel
 
+test_trace = os.environ.get("TEST_TRACE", "true").lower() == "true"
+
 
 class MyModel(PythonModel):
+    def _call_retriver(self, id):
+        return f"Retriever called with ID: {id}. Output: 42."
+
+    def predict(self, context, model_input):
+        return f"Input: {model_input}. {self._call_retriver(model_input)}"
+
+    def predict_stream(self, context, model_input, params=None):
+        yield f"Input: {model_input}. {self._call_retriver(model_input)}"
+
+
+class MyModelWithTrace(PythonModel):
     def _call_retriver(self, id):
         return f"Retriever called with ID: {id}. Output: 42."
 
@@ -16,7 +31,8 @@ class MyModel(PythonModel):
         yield f"Input: {model_input}. {self._call_retriver(model_input)}"
 
 
-set_model(MyModel())
+model = MyModelWithTrace() if test_trace else MyModel()
+set_model(model)
 set_retriever_schema(
     primary_key="primary-key",
     text_column="text-column",
