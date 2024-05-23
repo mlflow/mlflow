171c1510834d10586cb3b6acbe58e50338505d83
B-Step62 <yuki.watanabe@databricks.com>
2024-05-23 09:54:13 +0900

    minor
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- mlflow/entities/trace_info.py
+++ mlflow/entities/trace_info.py
@@ -44,11 +44,9 @@ class TraceInfo(_MlflowObject):
         proto.request_id = self.request_id
         proto.experiment_id = self.experiment_id
         proto.timestamp_ms = self.timestamp_ms
-        # TODO: Protobuf does not support nullable fields (even with 'optional' keyword),
-        #   so we substitute None with 0 for execution_time_ms. This should be not too
-        #   confusing as we only put None when starting a trace i.e. the execution time
-        #   is actually 0, but this assumption may not hold in the future. Ideally, we
-        #   should define a special value for representing null/default value.
+        # NB: Proto setter does not support nullable fields (even with 'optional' keyword),
+        # so we substitute None with 0 for execution_time_ms. This should be not too confusing
+        # as we only put None when starting a trace i.e. the execution time is actually 0.
         proto.execution_time_ms = self.execution_time_ms or 0
         proto.status = self.status.to_proto()
 
