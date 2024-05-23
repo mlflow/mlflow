514a8b959efcc6facb27b412533d84f02516e6e0
Gabriel Fu <hfu.gabriel@gmail.com>
2024-05-23 16:37:24 +0800

    fix example.py
    Signed-off-by: Gabriel Fu <hfu.gabriel@gmail.com>

--- examples/deployments/deployments_server/plugin/example.py
+++ examples/deployments/deployments_server/plugin/example.py
@@ -5,17 +5,21 @@ def main():
     client = get_deploy_client("http://127.0.0.1:7000")
 
     print(f"Plugin endpoints: {client.list_endpoints()}\n")
-    print(f"Plugin completions endpoint info: {client.get_endpoint(endpoint='completions')}\n")
+    print(f"Plugin chat endpoint info: {client.get_endpoint(endpoint='chat')}\n")
 
-    # Completions request
-    response_completions = client.predict(
-        endpoint="completions",
+    # Chat request
+    response_chat = client.predict(
+        endpoint="chat",
         inputs={
-            "prompt": "Testing",
-            "temperature": 0.1,
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "Tell me a joke",
+                }
+            ]
         },
     )
-    print(f"Plugin response for completions: {response_completions}")
+    print(f"Plugin response for chat: {response_chat}")
 
 
 if __name__ == "__main__":
