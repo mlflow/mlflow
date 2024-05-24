c196a154850dd92a03a3d72ffe811ceab213b20f
B-Step62 <yuki.watanabe@databricks.com>
2024-05-24 15:31:27 +0900

    fix test
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- mlflow/utils/databricks_utils.py
+++ mlflow/utils/databricks_utils.py
@@ -171,7 +171,7 @@ def is_in_databricks_job():
 
 
 def is_in_databricks_model_serving_environment():
-    return "IS_IN_DATABRICKS_MODEL_SERVING_ENV" in os.environ
+    return os.environ.get("IS_IN_DATABRICKS_MODEL_SERVING_ENV", "false").lower() == "true"
 
 
 # this should only be the case when we are in model serving environment
