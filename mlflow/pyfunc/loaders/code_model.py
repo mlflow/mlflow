413c30a593bd26d9ab1ac9dcc85cfb7ce0204b21
Prithvi Kannan <prithvi.kannan@databricks.com>
2024-05-24 13:06:44 -0700

    update
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- mlflow/pyfunc/loaders/code_model.py
+++ mlflow/pyfunc/loaders/code_model.py
@@ -1,11 +1,10 @@
 from typing import Any, Dict, Optional
 
 from mlflow.pyfunc.loaders.chat_model import _ChatModelPyfuncWrapper
-from mlflow.pyfunc.loaders.flexible_model import _OompaLoopaModelPyfuncWrapper
 from mlflow.pyfunc.model import (
-    _PythonModelPyfuncWrapper,
     ChatModel,
     _load_context_model_and_signature,
+    _PythonModelPyfuncWrapper,
 )
 
 
@@ -13,7 +12,5 @@ def _load_pyfunc(local_path: str, model_config: Optional[Dict[str, Any]] = None)
     context, model, signature = _load_context_model_and_signature(local_path, model_config)
     if isinstance(model, ChatModel):
         return _ChatModelPyfuncWrapper(model, context, signature)
-    elif isinstance(model, OompaLoopaModel):
-        return _OompaLoopaModelPyfuncWrapper(model, context, signature)
     else:
         return _PythonModelPyfuncWrapper(model, context, signature)
