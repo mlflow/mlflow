064e280ee573c2ceb7a3b070ba6f387fb416e88c
Prithvi Kannan <prithvi.kannan@databricks.com>
2024-05-28 13:23:57 -0700

    input example
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- tests/pyfunc/test_chat_model.py
+++ tests/pyfunc/test_chat_model.py
@@ -359,19 +359,25 @@ def test_chat_model_works_with_infer_signature_multi_input_example(tmp_path):
 def test_rag_model_works_with_type_hint(tmp_path):
     model = TestRagModel()
     signature = ModelSignature(inputs=ChatCompletionRequest(), outputs=ChatCompletionResponse())
-    mlflow.pyfunc.save_model(python_model=model, path=tmp_path, signature=signature)
+    input_example = {"messages": [{"role": "user", "content": "What is mlflow?"}]}
+    mlflow.pyfunc.save_model(
+        python_model=model, path=tmp_path, signature=signature, input_example=input_example
+    )
 
     # test that the model can be loaded and invoked
     loaded_model = mlflow.pyfunc.load_model(tmp_path)
-    request = {"messages": [{"role": "user", "content": "What is mlflow?"}]}
 
-    response = loaded_model.predict(request)
+    response = loaded_model.predict(input_example)
     assert response["choices"][0]["message"]["content"] == "What is mlflow?"
+
+    # confirm the input example is set
+    mlflow_model = Model.load(tmp_path)
+    assert mlflow_model.load_input_example(tmp_path).to_dict(orient="records")[0] == input_example
 
     # test that the model can be served
     response = pyfunc_serve_and_score_model(
         model_uri=tmp_path,
-        data=json.dumps(request),
+        data=json.dumps(input_example),
         content_type="application/json",
         extra_args=["--env-manager", "local"],
     )
