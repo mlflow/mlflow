cffd8a1dd949566020030a4e76b7194cf05c73e2
harupy <17039389+harupy@users.noreply.github.com>
2024-05-29 13:27:28 +0900

    test fluent search_traces
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/tracing/fluent.py
+++ mlflow/tracing/fluent.py
@@ -306,6 +306,7 @@ def search_traces(
 
 
     .. code-block:: python
+        :test:
         :caption: Search traces with extract_fields and non-dictionary span inputs and outputs
 
         import mlflow
