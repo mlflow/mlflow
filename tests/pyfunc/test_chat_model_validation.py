612cb790af0b3dc0240a7fd530f9bf790047fcf5
Serena Ruan <serena.rxy@gmail.com>
2024-05-30 17:24:08 +0800

    add logprobs
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- tests/pyfunc/test_chat_model_validation.py
+++ tests/pyfunc/test_chat_model_validation.py
@@ -32,6 +32,142 @@ MOCK_RESPONSE = {
     },
 }
 
+MOCK_OPENAI_CHAT_COMPLETION_RESPONSE = {
+    "id": "chatcmpl-123",
+    "object": "chat.completion",
+    "created": 1702685778,
+    "model": "gpt-3.5-turbo-0125",
+    "choices": [
+        {
+            "index": 0,
+            "message": {"role": "assistant", "content": "Hello! How can I assist you today?"},
+            "logprobs": {
+                "content": [
+                    {
+                        "token": "Hello",
+                        "logprob": -0.31725305,
+                        "bytes": [72, 101, 108, 108, 111],
+                        "top_logprobs": [
+                            {
+                                "token": "Hello",
+                                "logprob": -0.31725305,
+                                "bytes": [72, 101, 108, 108, 111],
+                            },
+                            {"token": "Hi", "logprob": -1.3190403, "bytes": [72, 105]},
+                        ],
+                    },
+                    {
+                        "token": "!",
+                        "logprob": -0.02380986,
+                        "bytes": [33],
+                        "top_logprobs": [
+                            {"token": "!", "logprob": -0.02380986, "bytes": [33]},
+                            {
+                                "token": " there",
+                                "logprob": -3.787621,
+                                "bytes": [32, 116, 104, 101, 114, 101],
+                            },
+                        ],
+                    },
+                    {
+                        "token": " How",
+                        "logprob": -0.000054669687,
+                        "bytes": [32, 72, 111, 119],
+                        "top_logprobs": [
+                            {
+                                "token": " How",
+                                "logprob": -0.000054669687,
+                                "bytes": [32, 72, 111, 119],
+                            },
+                            {"token": "<|end|>", "logprob": -10.953937, "bytes": None},
+                        ],
+                    },
+                    {
+                        "token": " can",
+                        "logprob": -0.015801601,
+                        "bytes": [32, 99, 97, 110],
+                        "top_logprobs": [
+                            {"token": " can", "logprob": -0.015801601, "bytes": [32, 99, 97, 110]},
+                            {"token": " may", "logprob": -4.161023, "bytes": [32, 109, 97, 121]},
+                        ],
+                    },
+                    {
+                        "token": " I",
+                        "logprob": -3.7697225e-6,
+                        "bytes": [32, 73],
+                        "top_logprobs": [
+                            {"token": " I", "logprob": -3.7697225e-6, "bytes": [32, 73]},
+                            {
+                                "token": " assist",
+                                "logprob": -13.596657,
+                                "bytes": [32, 97, 115, 115, 105, 115, 116],
+                            },
+                        ],
+                    },
+                    {
+                        "token": " assist",
+                        "logprob": -0.04571125,
+                        "bytes": [32, 97, 115, 115, 105, 115, 116],
+                        "top_logprobs": [
+                            {
+                                "token": " assist",
+                                "logprob": -0.04571125,
+                                "bytes": [32, 97, 115, 115, 105, 115, 116],
+                            },
+                            {
+                                "token": " help",
+                                "logprob": -3.1089056,
+                                "bytes": [32, 104, 101, 108, 112],
+                            },
+                        ],
+                    },
+                    {
+                        "token": " you",
+                        "logprob": -5.4385737e-6,
+                        "bytes": [32, 121, 111, 117],
+                        "top_logprobs": [
+                            {
+                                "token": " you",
+                                "logprob": -5.4385737e-6,
+                                "bytes": [32, 121, 111, 117],
+                            },
+                            {
+                                "token": " today",
+                                "logprob": -12.807695,
+                                "bytes": [32, 116, 111, 100, 97, 121],
+                            },
+                        ],
+                    },
+                    {
+                        "token": " today",
+                        "logprob": -0.0040071653,
+                        "bytes": [32, 116, 111, 100, 97, 121],
+                        "top_logprobs": [
+                            {
+                                "token": " today",
+                                "logprob": -0.0040071653,
+                                "bytes": [32, 116, 111, 100, 97, 121],
+                            },
+                            {"token": "?", "logprob": -5.5247097, "bytes": [63]},
+                        ],
+                    },
+                    {
+                        "token": "?",
+                        "logprob": -0.0008108172,
+                        "bytes": [63],
+                        "top_logprobs": [
+                            {"token": "?", "logprob": -0.0008108172, "bytes": [63]},
+                            {"token": "?\n", "logprob": -7.184561, "bytes": [63, 10]},
+                        ],
+                    },
+                ]
+            },
+            "finish_reason": "stop",
+        }
+    ],
+    "usage": {"prompt_tokens": 9, "completion_tokens": 9, "total_tokens": 18},
+}
+
 
 @pytest.mark.parametrize(
     ("data", "error", "match"),
@@ -90,15 +226,17 @@ def test_list_validation_throws_on_invalid_lists(data, match):
         ChatRequest(**data)
 
 
-def test_dataclass_constructs_nested_types_from_dict():
-    response = ChatResponse(**MOCK_RESPONSE)
+@pytest.mark.parametrize("sample_output", [MOCK_RESPONSE, MOCK_OPENAI_CHAT_COMPLETION_RESPONSE])
+def test_dataclass_constructs_nested_types_from_dict(sample_output):
+    response = ChatResponse(**sample_output)
     assert isinstance(response.usage, TokenUsageStats)
     assert isinstance(response.choices[0], ChatChoice)
     assert isinstance(response.choices[0].message, ChatMessage)
 
 
-def test_to_dict_converts_nested_dataclasses():
-    response = ChatResponse(**MOCK_RESPONSE).to_dict()
+@pytest.mark.parametrize("sample_output", [MOCK_RESPONSE, MOCK_OPENAI_CHAT_COMPLETION_RESPONSE])
+def test_to_dict_converts_nested_dataclasses(sample_output):
+    response = ChatResponse(**sample_output).to_dict()
     assert isinstance(response["choices"][0], dict)
     assert isinstance(response["usage"], dict)
     assert isinstance(response["choices"][0]["message"], dict)
