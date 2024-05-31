75936edc51745baa81d888968177d1ddbc723161
WeichenXu <weichen.xu@databricks.com>
2024-05-31 09:49:20 +0800

    Fix root user checking in get_databricks_nfs_temp_dir and get_databricks_local_temp_dir (#12186)
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/utils/databricks_utils.py
+++ mlflow/utils/databricks_utils.py
@@ -957,7 +957,7 @@ if is_in_databricks_runtime():
 
 def get_databricks_nfs_temp_dir():
     entry_point = _get_dbutils().entry_point
-    if getpass.getuser() == "ROOT":
+    if getpass.getuser().lower() == "root":
         return entry_point.getReplNFSTempDir()
     else:
         try:
@@ -972,7 +972,7 @@ def get_databricks_nfs_temp_dir():
 
 def get_databricks_local_temp_dir():
     entry_point = _get_dbutils().entry_point
-    if getpass.getuser() == "ROOT":
+    if getpass.getuser().lower() == "root":
         return entry_point.getReplLocalTempDir()
     else:
         try:
