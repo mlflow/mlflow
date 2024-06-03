aea19e30d890e88d8eb6f335ae6fe3792e2580cc
Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
2024-06-03 20:00:35 +0900

    Add get_last_active_trace() API (#12207)
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- tests/entities/test_trace.py
+++ tests/entities/test_trace.py
@@ -14,7 +14,7 @@ from mlflow.tracing.utils import TraceJSONEncoder
 from mlflow.utils.mlflow_tags import MLFLOW_ARTIFACT_LOCATION
 
 from tests.tracing.conftest import clear_singleton  # noqa: F401
-from tests.tracing.helper import create_test_trace_info, get_first_trace
+from tests.tracing.helper import create_test_trace_info
 
 
 def _test_model(datetime=datetime.now()):
@@ -49,7 +49,7 @@ def test_json_deserialization(clear_singleton, monkeypatch):
     model = _test_model(datetime_now)
     model.predict(2, 5)
 
-    trace = get_first_trace()
+    trace = mlflow.get_last_active_trace()
     trace_json = trace.to_json()
 
     trace_json_as_dict = json.loads(trace_json)
@@ -194,7 +194,7 @@ def test_trace_to_from_dict_and_json(clear_singleton):
     model = _test_model()
     model.predict(2, 5)
 
-    trace = get_first_trace()
+    trace = mlflow.get_last_active_trace()
     trace_dict = trace.to_dict()
     trace_from_dict = Trace.from_dict(trace_dict)
     trace_json = trace.to_json()
