2e065e74de78bd588e8d5e4a3c9807dd05545760
Harutaka Kawamura <hkawamura0130@gmail.com>
2024-06-03 17:40:10 +0900

    Call `_flatten_nested_params` only when `model_config` is truthy (#12214)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/models/model.py
+++ mlflow/models/model.py
@@ -697,8 +697,7 @@ class Model:
             mlflow.tracking.fluent.log_artifacts(local_path, mlflow_model.artifact_path, run_id)
 
             # if the model_config kwarg is passed in, then log the model config as an params
-            if "model_config" in kwargs:
-                model_config = kwargs["model_config"]
+            if model_config := kwargs.get("model_config"):
                 if isinstance(model_config, str):
                     try:
                         file_extension = os.path.splitext(model_config)[1].lower()
