e9d1c2b7fb240f6a59148b69fec4ee77d1e35e42
Harutaka Kawamura <hkawamura0130@gmail.com>
2024-06-05 09:20:19 +0900

    Support span and field name containing dot for `search_traces` (#12172)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/tracing/test_utils.py
+++ tests/tracing/test_utils.py
@@ -1,11 +1,11 @@
-import re
-
 import pytest
 
 from mlflow.entities import LiveSpan
 from mlflow.exceptions import MlflowException
 from mlflow.tracing.utils import (
+    _FieldParser,
     _parse_fields,
+    _ParsedField,
     deduplicate_span_names_in_place,
     encode_span_id,
     maybe_get_request_id,
@@ -54,6 +54,63 @@ def test_maybe_get_request_id():
             maybe_get_request_id(is_evaluate=True)
 
 
+@pytest.mark.parametrize(
+    ("field", "expected"),
+    [
+        # no dot
+        ("span.inputs", _ParsedField("span", "inputs", None)),
+        ("span.outputs", _ParsedField("span", "outputs", None)),
+        ("`span`.inputs", _ParsedField("span", "inputs", None)),
+        ("`span`.outputs", _ParsedField("span", "outputs", None)),
+        ("span.inputs.field", _ParsedField("span", "inputs", "field")),
+        ("`span`.inputs.field", _ParsedField("span", "inputs", "field")),
+        ("span.inputs.`field`", _ParsedField("span", "inputs", "field")),
+        ("`span`.inputs.`field`", _ParsedField("span", "inputs", "field")),
+        # dot in span name
+        ("`span.name`.inputs.field", _ParsedField("span.name", "inputs", "field")),
+        ("`span.inputs.name`.inputs.field", _ParsedField("span.inputs.name", "inputs", "field")),
+        (
+            "`span.outputs.name`.outputs.field",
+            _ParsedField("span.outputs.name", "outputs", "field"),
+        ),
+        # dot in field name
+        ("span.inputs.`field.name`", _ParsedField("span", "inputs", "field.name")),
+        ("span.inputs.`field.inputs.name`", _ParsedField("span", "inputs", "field.inputs.name")),
+        (
+            "span.outputs.`field.outputs.name`",
+            _ParsedField("span", "outputs", "field.outputs.name"),
+        ),
+        # dot in both span and field name
+        ("`span.name`.inputs.`field.name`", _ParsedField("span.name", "inputs", "field.name")),
+        (
+            "`span.inputs.name`.inputs.`field.inputs.name`",
+            _ParsedField("span.inputs.name", "inputs", "field.inputs.name"),
+        ),
+        (
+            "`span.outputs.name`.outputs.`field.outputs.name`",
+            _ParsedField("span.outputs.name", "outputs", "field.outputs.name"),
+        ),
+    ],
+)
+def test_field_parser(field, expected):
+    assert _FieldParser(field).parse() == expected
+
+
+@pytest.mark.parametrize(
+    ("input_string", "error_message"),
+    [
+        ("`span.inputs.field", "Expected closing backtick"),
+        ("`span`a.inputs.field", "Expected dot after span name"),
+        ("span.foo.field", "Invalid field type"),
+        ("span.inputs.`field", "Expected closing backtick"),
+        ("span.inputs.`field`name", "Unexpected characters after closing backtick"),
+    ],
+)
+def test_field_parser_invalid_value(input_string, error_message):
+    with pytest.raises(MlflowException, match=error_message):
+        _FieldParser(input_string).parse()
+
+
 def test_parse_fields():
     fields = ["span1.inputs", "span2.outputs.field1", "span3.outputs"]
     parsed_fields = _parse_fields(fields)
@@ -71,10 +128,3 @@ def test_parse_fields():
     assert parsed_fields[2].span_name == "span3"
     assert parsed_fields[2].field_type == "outputs"
     assert parsed_fields[2].field_name is None
-
-    # Test invalid fields
-    with pytest.raises(
-        MlflowException,
-        match=re.escape("Field must be of the form 'span_name.[inputs|outputs]'"),
-    ):
-        _parse_fields(["span1"])
