7461006fd173619bfb3c8aa4f6282c6fbb717ae2
Ann Zhang <ann.zhang@databricks.com>
2024-06-05 13:22:28 -0700

    use tmp_path
    Signed-off-by: Ann Zhang <ann.zhang@databricks.com>

--- tests/pyfunc/test_pyfunc_model_config.py
+++ tests/pyfunc/test_pyfunc_model_config.py
@@ -87,19 +87,18 @@ def test_override_model_config(model_path, model_config):
         "tests/pyfunc/../pyfunc/sample_code/config.yml",
     ],
 )
-def test_override_model_config_path(model_path, model_config_path):
-    with tempfile.TemporaryDirectory() as temp_dir:
-        model = TestModel()
-        inference_override = {"timeout": 400}
-        config_path = _get_temp_file_with_content(
-            temp_dir, "config.yml", yaml.dump(inference_override), "w"
-        )
+def test_override_model_config_path(tmp_path, model_path, model_config_path):
+    model = TestModel()
+    inference_override = {"timeout": 400}
+    config_path = _get_temp_file_with_content(
+        str(tmp_path), "config.yml", yaml.dump(inference_override), "w"
+    )
 
-        mlflow.pyfunc.save_model(model_path, python_model=model, model_config=model_config_path)
-        loaded_model = mlflow.pyfunc.load_model(model_uri=model_path, model_config=config_path)
+    mlflow.pyfunc.save_model(model_path, python_model=model, model_config=model_config_path)
+    loaded_model = mlflow.pyfunc.load_model(model_uri=model_path, model_config=config_path)
 
-        assert loaded_model.model_config["timeout"] == 400
-        assert all(loaded_model.model_config[k] == v for k, v in inference_override.items())
+    assert loaded_model.model_config["timeout"] == 400
+    assert all(loaded_model.model_config[k] == v for k, v in inference_override.items())
 
 
 def test_override_model_config_ignore_invalid(model_path, model_config):
@@ -120,19 +119,18 @@ def test_override_model_config_ignore_invalid(model_path, model_config):
         "tests/pyfunc/../pyfunc/sample_code/config.yml",
     ],
 )
-def test_override_model_config_path_ignore_invalid(model_path, model_config_path):
-    with tempfile.TemporaryDirectory() as temp_dir:
-        model = TestModel()
-        inference_override = {"invalid_key": 400}
-        config_path = _get_temp_file_with_content(
-            temp_dir, "config.yml", yaml.dump(inference_override), "w"
-        )
+def test_override_model_config_path_ignore_invalid(tmp_path model_path, model_config_path):
+    model = TestModel()
+    inference_override = {"invalid_key": 400}
+    config_path = _get_temp_file_with_content(
+        str(tmp_path), "config.yml", yaml.dump(inference_override), "w"
+    )
 
-        mlflow.pyfunc.save_model(model_path, python_model=model, model_config=model_config_path)
-        loaded_model = mlflow.pyfunc.load_model(model_uri=model_path, model_config=config_path)
+    mlflow.pyfunc.save_model(model_path, python_model=model, model_config=model_config_path)
+    loaded_model = mlflow.pyfunc.load_model(model_uri=model_path, model_config=config_path)
 
-        assert loaded_model.predict([[5]])
-        assert all(k not in loaded_model.model_config for k in inference_override.keys())
+    assert loaded_model.predict([[5]])
+    assert all(k not in loaded_model.model_config for k in inference_override.keys())
 
 
 def test_pyfunc_without_model_config(model_path, model_config):
