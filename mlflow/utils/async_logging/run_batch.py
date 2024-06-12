331c2c09bd8e1c7abde48d533bb24dea8e805017
Chen Qian <chen.qian@databricks.com>
2024-06-12 01:37:52 -0700

    Support async logging per X seconds (#12324)
    Signed-off-by: chenmoneygithub <chen.qian@databricks.com>

--- mlflow/utils/async_logging/run_batch.py
+++ mlflow/utils/async_logging/run_batch.py
@@ -1,5 +1,5 @@
 import threading
-from typing import List
+from typing import List, Optional
 
 from mlflow.entities.metric import Metric
 from mlflow.entities.param import Param
@@ -10,19 +10,19 @@ class RunBatch:
     def __init__(
         self,
         run_id: str,
-        params: List[Param],
-        tags: List[RunTag],
-        metrics: List[Metric],
-        completion_event: threading.Event,
-    ) -> None:
+        params: Optional[List["Param"]] = None,
+        tags: Optional[List["RunTag"]] = None,
+        metrics: Optional[List["Metric"]] = None,
+        completion_event: Optional[threading.Event] = None,
+    ):
         """Initializes an instance of `RunBatch`.
 
         Args:
             run_id: The ID of the run.
-            params: A list of parameters.
-            tags: A list of tags.
-            metrics: A list of metrics.
-            completion_event: A threading.Event object.
+            params: A list of parameters. Default is None.
+            tags: A list of tags. Default is None.
+            metrics: A list of metrics. Default is None.
+            completion_event: A threading.Event object. Default is None.
         """
         self.run_id = run_id
         self.params = params or []
@@ -30,6 +30,7 @@ class RunBatch:
         self.metrics = metrics or []
         self.completion_event = completion_event
         self._exception = None
+        self.child_batches = []
 
     @property
     def exception(self):
@@ -39,3 +40,11 @@ class RunBatch:
     @exception.setter
     def exception(self, exception):
         self._exception = exception
+
+    def add_child_batch(self, child_batch):
+        """Add a child batch to the current batch.
+
+        This is useful when merging child batches into a parent batch. Child batches are kept so
+        that we can properly notify the system when child batches have been processed.
+        """
+        self.child_batches.append(child_batch)
