3ad2f7a1d16e3a7cdb7bd99dbdc081a67b402b50
Michael Feller <michael.feller@dmgmori.com>
2024-06-17 09:07:01 +0200

    add error message to exception instead of removing try-except
    Signed-off-by: Michael Feller <michael.feller@dmgmori.com>

--- mlflow/langchain/runnables.py
+++ mlflow/langchain/runnables.py
@@ -255,11 +255,14 @@ def _save_internal_runnables(runnable, path, loader_fn, persist_dir):
         if hasattr(runnable, "save"):
             runnable.save(model_path)
         elif hasattr(runnable, "dict"):
-            runnable_dict = runnable.dict()
-            with open(model_path, "w") as f:
-                yaml.dump(runnable_dict, f, default_flow_style=False)
-            # if the model cannot be loaded back, then `dict` is not enough for saving.
-            _load_model_from_config(path, conf)
+            try:
+                runnable_dict = runnable.dict()
+                with open(model_path, "w") as f:
+                    yaml.dump(runnable_dict, f, default_flow_style=False)
+                # if the model cannot be loaded back, then `dict` is not enough for saving.
+                _load_model_from_config(path, conf)
+            except Exception as e:
+                raise Exception(f"Cannot save runnable without `save` method. Error: {e}")
         else:
             raise Exception("Cannot save runnable without `save` or `dict` methods.")
     return conf
