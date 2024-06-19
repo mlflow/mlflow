fd1b51bf28904a3a59bc31b3f26a976f9c0865a8
WeichenXu <weichen.xu@databricks.com>
2024-06-19 17:39:53 +0800

    Merge Databricks-sdk feature branch to master (#12313)
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/environment_variables.py
+++ mlflow/environment_variables.py
@@ -611,7 +611,6 @@ MLFLOW_DATABRICKS_ENDPOINT_HTTP_RETRY_TIMEOUT = _EnvironmentVariable(
     "MLFLOW_DATABRICKS_ENDPOINT_HTTP_RETRY_TIMEOUT", int, 500
 )
 
-
 #: Specifies the number of connection pools to cache in urllib3. This environment variable sets the
 #: `pool_connections` parameter in the `requests.adapters.HTTPAdapter` constructor. By adjusting
 #: this variable, users can enhance the concurrency of HTTP requests made by MLflow.
@@ -631,3 +630,10 @@ MLFLOW_ENABLE_UC_FUNCTIONS = _BooleanEnvironmentVariable("MLFLOW_ENABLE_UC_FUNCT
 MLFLOW_ASYNC_LOGGING_BUFFERING_SECONDS = _EnvironmentVariable(
     "MLFLOW_ASYNC_LOGGING_BUFFERING_SECONDS", int, None
 )
+
+#: Whether to enable Databricks SDK. If true, MLflow uses databricks-sdk to send HTTP requests
+#: to Databricks endpoint, otherwise MLflow uses ``requests`` library to send HTTP requests
+#: to Databricks endpoint. Note that if you want to use OAuth authentication, you have to
+#: set this environment variable to true.
+#: (default: ``True``)
+MLFLOW_ENABLE_DB_SDK = _BooleanEnvironmentVariable("MLFLOW_ENABLE_DB_SDK", True)
