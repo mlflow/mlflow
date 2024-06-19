5896acdbb7c747aba0dfe697bbae84502d9c2ff8
harupy <17039389+harupy@users.noreply.github.com>
2024-06-19 15:24:40 +0900

    test
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/tracking/fluent/test_fluent.py
+++ tests/tracking/fluent/test_fluent.py
@@ -167,6 +167,12 @@ def reload_context_registry():
     reload(mlflow.tracking.context.registry)
 
 
+@pytest.fixture(autouse=True)
+def flush_async_logging():
+    yield
+    mlflow.flush_async_logging()
+
+
 @pytest.fixture(params=["list", "pandas"])
 def search_runs_output_format(request):
     if "MLFLOW_SKINNY" in os.environ and request.param == "pandas":
