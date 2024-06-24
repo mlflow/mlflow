b0ecee237de990908f242bdfd6b9521749e9ee79
dbczumar <corey.zumar@databricks.com>
2024-06-24 13:52:21 -0700

    Eval tag
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

---
+++ mlflow/entities/evaluation_tag.py
@@ -0,0 +1,24 @@
+from mlflow.entities._mlflow_object import _MlflowObject
+
+
+class EvaluationTag(_MlflowObject):
+    """Tag object associated with an evaluation."""
+
+    def __init__(self, key, value):
+        self._key = key
+        self._value = value
+
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return self.__dict__ == other.__dict__
+        return False
+
+    @property
+    def key(self):
+        """String name of the tag."""
+        return self._key
+
+    @property
+    def value(self):
+        """String value of the tag."""
+        return self._value
