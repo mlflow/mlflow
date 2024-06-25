f5856ec8db6680c21ed1775d8635a9e41fe1b2d8
Weichen Xu <weichen.xu@databricks.com>
2024-06-25 21:35:38 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/pyfunc/__init__.py
+++ mlflow/pyfunc/__init__.py
@@ -1142,6 +1142,7 @@ def _load_model_or_server(
             # the scoring server is still running but client can't connect to it.
             # kill the server.
             scoring_server_proc.kill()
+            scoring_server_proc.wait()
         server_output = scoring_server_proc.stdout.read()
         raise MlflowException(
             "MLflow model server failed to launch, server process stdout / stderr outputs are:\n" +
