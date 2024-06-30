df17b5f0ecac1dd609fa2242146e6ede4ac34adc
dbczumar <corey.zumar@databricks.com>
2024-06-30 00:18:16 -0700

    Add to init
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/evaluation/__init__.py
+++ mlflow/evaluation/__init__.py
@@ -1,4 +1,5 @@
 from mlflow.evaluation.assessment import Assessment
 from mlflow.evaluation.evaluation import Evaluation
+from mlflow.evaluation.fluent import log_evaluations
 
-__all__ = ["Assessment", "Evaluation"]
+__all__ = ["Assessment", "Evaluation", "log_evaluations"]
