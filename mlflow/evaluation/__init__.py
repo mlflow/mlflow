1086c6d3ac390d1ad16a9c2af84a823842c5866f
dbczumar <corey.zumar@databricks.com>
2024-07-01 13:55:44 -0700

    fix
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/evaluation/__init__.py
+++ mlflow/evaluation/__init__.py
@@ -1,6 +1,12 @@
-from mlflow.entities.assessment_source import AssessmentSource
+from mlflow.entities.assessment_source import AssessmentSource, AssessmentSourceType
 from mlflow.evaluation.assessment import Assessment
 from mlflow.evaluation.evaluation import Evaluation
 from mlflow.evaluation.fluent import log_evaluations
 
-__all__ = ["Assessment", "AssessmentSource", "Evaluation", "log_evaluations"]
+__all__ = [
+    "Assessment",
+    "AssessmentSource",
+    "AssessmentSourceType",
+    "Evaluation",
+    "log_evaluations",
+]
