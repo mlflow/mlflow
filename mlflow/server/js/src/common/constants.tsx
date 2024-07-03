20c31eb83d83ca1a3391d14f84591100163b8a8f
mlflow-automation <61449322+mlflow-automation@users.noreply.github.com>
2024-07-04 05:46:52 +0900

    Run `python3 dev/update_mlflow_versions.py pre-release ...` (#12566)
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>
    Co-authored-by: mlflow-automation <mlflow-automation@users.noreply.github.com>

--- mlflow/server/js/src/common/constants.tsx
+++ mlflow/server/js/src/common/constants.tsx
@@ -9,7 +9,7 @@ export const ErrorCodes = {
   RESOURCE_CONFLICT: 'RESOURCE_CONFLICT',
 };
 
-export const Version = '2.14.1';
+export const Version = '2.14.2';
 
 const DOCS_VERSION = 'latest';
 
--- mlflow/version.py
+++ mlflow/version.py
@@ -1,7 +1,7 @@
 # Copyright 2018 Databricks, Inc.
 import re
 
-VERSION = "2.14.1"
+VERSION = "2.14.2"
 
 
 def is_release_version():
