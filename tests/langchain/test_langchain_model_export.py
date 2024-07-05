98058778d28bb12d1fc5185746048c94ef7be392
Harutaka Kawamura <hkawamura0130@gmail.com>
2024-07-05 11:20:51 +0900

    Handle `_infer_signature_from_input_example` returning `None` (#12570)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/langchain/test_langchain_model_export.py
+++ tests/langchain/test_langchain_model_export.py
@@ -3403,3 +3403,24 @@ def test_agent_executor_model_with_messages_input():
             "messages": [AIMessage(content="Databricks")],
         }
     ]
+
+
+@pytest.mark.skipif(
+    Version(langchain.__version__) < _LC_MIN_VERSION_SUPPORT_RUNNABLE,
+    reason="feature not existing",
+)
+def test_signature_inference_fails(monkeypatch: pytest.MonkeyPatch):
+    from langchain.schema.runnable import RunnableLambda
+
+    monkeypatch.setenv("MLFLOW_TESTING", "false")
+
+    model = RunnableLambda(lambda x: x)
+
+    with mlflow.start_run():
+        model_info = mlflow.langchain.log_model(
+            model,
+            "model",
+            # Use an empty array to trigger an error in signature inference
+            input_example={"chat": []},
+        )
+        assert model_info.signature is None
