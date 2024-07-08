d70cb179a3ce3f479185c34c2bbc864e752a9db3
Michael Berk <michaelberk99@gmail.com>
2024-07-08 11:38:46 -0400

    Initial commit
    Signed-off-by: Michael Berk <michaelberk99@gmail.com>

---
+++ tests/llama_index/test_llama_index_model_export.py
@@ -0,0 +1,223 @@
+import pytest
+
+import mlflow
+import mlflow.llama_index
+import mlflow.pyfunc
+
+from tests.llama_index._llama_index_test_fixtures import (
+    document,  # noqa: F401
+    embed_model,  # noqa: F401
+    multi_index,  # noqa: F401
+    settings,  # noqa: F401
+    single_graph,  # noqa: F401
+    single_index,  # noqa: F401
+    spark,  # noqa: F401
+)
+
+
+@pytest.mark.parametrize(
+    "index_fixture",
+    [
+        "single_index",
+        "multi_index",
+        "single_graph",
+    ],
+)
+def test_llama_index_native_save_and_load_model(request, index_fixture, tmp_path):
+    index = request.getfixturevalue(index_fixture)
+    mlflow.llama_index.save_model(index, tmp_path, engine_type="query")
+    loaded_model = mlflow.llama_index.load_model(tmp_path)
+
+    assert type(loaded_model) == type(index)
+    assert loaded_model.as_query_engine().query("Spell llamaindex").response != ""
+
+
+@pytest.mark.parametrize(
+    "index_fixture",
+    [
+        "single_index",
+        "multi_index",
+        "single_graph",
+    ],
+)
+def test_llama_index_native_log_and_load_model(request, index_fixture):
+    index = request.getfixturevalue(index_fixture)
+    with mlflow.start_run():
+        logged_model = mlflow.llama_index.log_model(index, "model", engine_type="query")
+
+    loaded_model = mlflow.llama_index.load_model(logged_model.model_uri)
+
+    assert "llama_index" in logged_model.flavors
+    assert type(loaded_model) == type(index)
+    engine = loaded_model.as_query_engine()
+    assert engine is not None
+    assert engine.query("Spell llamaindex").response != ""
+
+
+# def test_spark_udf_query(tmp_path, spark, single_index):
+#     df = spark.createDataFrame(
+#         [
+#             ("dummy text", "dummy text"),
+#         ],
+#         ["x"],
+#     )
+#     signature = infer_signature(model_input={"query_str": "hi"}, model_output=["output"])
+#     mlflow.llama_index.save_model(
+#         index=single_index, signature=signature, path=tmp_path, engine_type="query"
+#     )
+#     udf = mlflow.pyfunc.spark_udf(spark, tmp_path)
+#     pdf = df.withColumn("z", udf("x")).toPandas()
+#     assert list(map(json.loads, pdf["z"])) == [
+#         [{"content": "a b", "role": "user"}],
+#         [{"content": "c d", "role": "user"}],
+#     ]
+
+
+# @pytest.mark.parametrize("with_signature", [True, False])
+# def test_query_engine_str(tmp_path, single_index, with_signature):
+#     payload = "string"
+
+#     signature = infer_signature(model_input=payload) if with_signature else None
+#     mlflow.llama_index.save_model(
+#         index=single_index, signature=signature, path=tmp_path, engine_type="query"
+#     )
+#     model = mlflow.pyfunc.load_model(tmp_path)
+#     predictions = model.predict(payload)
+#     assert isinstance(predictions, Response)
+#     assert predictions.response
+
+
+# @pytest.mark.parametrize("with_signature", [True, False])
+# def test_query_engine_numeric(tmp_path, single_index, with_signature):
+#     payload = 1
+
+#     signature = infer_signature(model_input=payload) if with_signature else None
+#     mlflow.llama_index.save_model(
+#         index=single_index, signature=signature, path=tmp_path, engine_type="query"
+#     )
+#     model = mlflow.pyfunc.load_model(tmp_path)
+#     with pytest.raises(ValueError, match="Unsupported input type"):
+#         _ = model.predict(payload)
+
+
+# @pytest.mark.parametrize("with_signature", [True, False])
+# def test_query_engine_list(tmp_path, single_index, with_signature):
+#     payload = ["string", "string"]
+
+#     signature = infer_signature(model_input=payload) if with_signature else None
+#     mlflow.llama_index.save_model(
+#         index=single_index, signature=signature, path=tmp_path, engine_type="query"
+#     )
+#     model = mlflow.pyfunc.load_model(tmp_path)
+#     predictions = model.predict(payload)
+#     assert isinstance(predictions, list)
+#     assert isinstance(predictions[0], Response)
+#     assert predictions[0].response
+
+
+# @pytest.mark.parametrize("with_signature", [True, False])
+# def test_query_engine_array(tmp_path, single_index, with_signature):
+#     payload = np.array(["string", "string"])
+
+#     signature = infer_signature(model_input=payload) if with_signature else None
+#     mlflow.llama_index.save_model(
+#         index=single_index, signature=signature, path=tmp_path, engine_type="query"
+#     )
+#     model = mlflow.pyfunc.load_model(tmp_path)
+#     predictions = model.predict(payload)
+#     assert isinstance(predictions, list)
+#     assert isinstance(predictions[0], Response)
+#     assert predictions[0].response
+
+
+# @pytest.mark.parametrize("with_signature", [True, False])
+# def test_query_engine_pandas_dataframe(tmp_path, single_index, with_signature):
+#     payload = pd.DataFrame({"query_str": ["string", "string"]})
+
+#     signature = infer_signature(model_input=payload) if with_signature else None
+#     mlflow.llama_index.save_model(
+#         index=single_index, signature=signature, path=tmp_path, engine_type="query"
+#     )
+#     model = mlflow.pyfunc.load_model(tmp_path)
+#     predictions = model.predict(payload)
+#     assert isinstance(predictions, list)
+#     assert isinstance(predictions[0], Response)
+#     assert predictions[0].response
+
+
+# @pytest.mark.parametrize("with_signature", [True, False])
+# def test_pyfunc_predict_with_index_valid_schema_pandas(tmp_path, single_index, with_signature):
+#     payload = pd.DataFrame(
+#         {"query_str": ["hi"], "custom_embedding_strs": [["a"]], "embedding": [[1.0]]}
+#     )
+
+#     signature = infer_signature(model_input=payload) if with_signature else None
+#     mlflow.llama_index.save_model(
+#         index=single_index, signature=signature, path=tmp_path, engine_type="query"
+#     )
+#     model = mlflow.pyfunc.load_model(tmp_path)
+#     predictions = model.predict(payload)
+#     assert isinstance(predictions, Response)
+#     assert predictions.response
+
+
+# @pytest.mark.parametrize("with_signature", [True, False])
+# def test_pyfunc_predict_with_index_valid_schema_dict(tmp_path, single_index, with_signature):
+#     payload = {"query_str": "hi", "custom_embedding_strs": ["a"], "embedding": [1.0]}
+
+#     signature = infer_signature(model_input=payload) if with_signature else None
+#     mlflow.llama_index.save_model(
+#         index=single_index, signature=signature, path=tmp_path, engine_type="query"
+#     )
+#     model = mlflow.pyfunc.load_model(tmp_path)
+#     predictions = model.predict(payload)
+#     assert isinstance(predictions, Response)
+#     assert predictions.response
+
+
+# @pytest.mark.parametrize("with_signature", [True, False])
+# def test_chat_engine_str(tmp_path, single_index, with_signature):
+#     payload = "string"
+
+#     signature = infer_signature(model_input=payload) if with_signature else None
+#     mlflow.llama_index.save_model(
+#         index=single_index, signature=signature, path=tmp_path, engine_type="chat"
+#     )
+#     model = mlflow.pyfunc.load_model(tmp_path)
+#     predictions = model.predict(payload)
+#     assert isinstance(predictions, AgentChatResponse)
+#     assert predictions.response
+
+
+# @pytest.mark.parametrize("with_signature", [True, False])
+# def test_chat_engine_dict(tmp_path, single_index, with_signature):
+#     payload = {
+#         "message": "string",
+#         "chat_history": [{"role": "user", "content": "string"}] * 3,
+#     }
+
+#     signature = infer_signature(model_input=payload) if with_signature else None
+#     mlflow.llama_index.save_model(
+#         index=single_index, signature=signature, path=tmp_path, engine_type="chat"
+#     )
+#     model = mlflow.pyfunc.load_model(tmp_path)
+#     predictions = model.predict(payload)
+#     assert isinstance(predictions, AgentChatResponse)
+#     assert predictions.response
+
+
+# @pytest.mark.parametrize("with_signature", [True, False])
+# def test_chat_engine_dict_raises(tmp_path, single_index, with_signature):
+#     payload = {
+#         "message": "string",
+#         "key_that_no_exist": [str(ChatMessage(role="user", content="string"))],
+#     }
+
+#     signature = infer_signature(model_input=payload) if with_signature else None
+#     mlflow.llama_index.save_model(
+#         index=single_index, signature=signature, path=tmp_path, engine_type="chat"
+#     )
+
+#     model = mlflow.pyfunc.load_model(tmp_path)
+#     with pytest.raises(TypeError, match="unexpected keyword argument"):
+#         _ = model.predict(payload)
---
+++ tests/llama_index/test_llama_index_serialization.py
@@ -0,0 +1,141 @@
+import json
+from collections import Counter, deque
+
+import pytest
+from llama_index.core import PromptTemplate
+
+from mlflow.llama_index.serialize_objects import (
+    _construct_prompt_template_object,
+    _deserialize_dict_of_objects,
+    _get_object_import_path,
+    _sanitize_api_key,
+    deserialize_settings,
+    object_to_dict,
+    serialize_settings,
+)
+
+from tests.llama_index._llama_index_test_fixtures import (
+    MockChatLLM,  # noqa: F401
+    embed_model,  # noqa: F401
+    llm,  # noqa: F401
+    qa_prompt_template,  # noqa: F401
+    settings,  # noqa: F401
+    single_index,  # noqa: F401
+)
+
+
+def test_get_object_import_path_class_instantiated():
+    # The rationale for using collections as the library to test
+    # import path is it's very stable and unlikely to change.
+    expected_path = "collections.deque"
+    assert _get_object_import_path(deque()) == expected_path
+
+
+def test_get_object_import_path_class_not_instantiated():
+    expected_path = "collections.Counter"
+    assert _get_object_import_path(Counter) == expected_path
+
+
+def test_object_is_class_do_validate_passes():
+    expected_path = "collections.Counter"
+    assert _get_object_import_path(Counter, do_validate_import=True) == expected_path
+
+
+def test_object_is_class_do_validate_raises():
+    class CustomClass:
+        pass
+
+    with pytest.raises(ValueError, match="does not have"):
+        _get_object_import_path(CustomClass, do_validate_import=True)
+
+
+def test_sanitize_api_key_keys_present():
+    data = {"openai_api_key": "sk-123456", "api_key": "sk-abcdef", "other_key": "value"}
+    sanitized_data = _sanitize_api_key(data)
+    assert "openai_api_key" not in sanitized_data
+    assert "api_key" not in sanitized_data
+    assert "other_key" in sanitized_data
+    assert sanitized_data["other_key"] == "value"
+
+
+def test_sanitize_api_key_keys_not_present():
+    data = {"some_key": "some_value", "another_key": "another_value"}
+    sanitized_data = _sanitize_api_key(data)
+    assert "some_key" in sanitized_data
+    assert "another_key" in sanitized_data
+    assert sanitized_data["some_key"] == "some_value"
+    assert sanitized_data["another_key"] == "another_value"
+
+
+def test_object_to_dict_one_required_param(embed_model):
+    o = embed_model
+    result = object_to_dict(o)
+    assert (
+        result["object_constructor"] == "llama_index.core.embeddings.mock_embed_model.MockEmbedding"
+    )
+    expected_kwargs = {k: v for k, v in o.to_dict().items() if k != "class_name"}
+    assert result["object_kwargs"] == expected_kwargs
+
+
+def test_construct_prompt_template_object_success(qa_prompt_template):
+    kwargs = qa_prompt_template.dict()
+    observed = _construct_prompt_template_object(PromptTemplate, kwargs)
+    assert observed == qa_prompt_template
+
+
+def test_construct_prompt_template_object_no_template_kwarg():
+    kwargs = {}
+    with pytest.raises(ValueError, match="'template' is a required kwargs and is not present"):
+        _construct_prompt_template_object(PromptTemplate, kwargs)
+
+
+def test_settings_serialization_full_object(tmp_path, settings):
+    path = tmp_path / "serialized_settings.json"
+    serialize_settings(path)
+
+    with open(path) as f:
+        objects = json.load(f)
+
+    assert len(set(objects.keys()) - set(settings.__dict__.keys())) == 0
+
+
+def test_deserialize_dict_of_objects(tmp_path, settings):
+    path = tmp_path / "serialized_settings.json"
+    serialize_settings(path)
+    observed = _deserialize_dict_of_objects(path)
+
+    assert len(set(observed.keys()) - set(settings.__dict__.keys())) == 0
+    assert observed["_llm"] == settings.llm
+    assert observed["_embed_model"] == settings.embed_model
+    assert "_callback_manager" not in observed.keys()
+    assert "_tokenizer" not in observed.keys()  # TODO
+    assert observed["_node_parser"] == settings.node_parser
+    assert observed["_prompt_helper"] == settings.prompt_helper
+    assert observed["_transformations"] == settings.transformations
+
+
+def test_settings_serde(tmp_path, settings):
+    path = tmp_path / "serialized_settings.json"
+    _llm = settings.llm
+    _embed_model = settings.embed_model
+    _callback_manager = settings.callback_manager
+    _tokenizer = settings.tokenizer
+    _node_parser = settings.node_parser
+    _prompt_helper = settings.prompt_helper
+    _transformations = settings.transformations
+
+    serialize_settings(path)
+    del settings
+
+    deserialize_settings(path)
+
+    from llama_index.core import Settings
+
+    assert Settings is not None
+    assert Settings.llm == _llm
+    assert Settings.embed_model == _embed_model
+    assert Settings.callback_manager == _callback_manager  # Auto-generated from defaults
+    assert Settings.tokenizer == _tokenizer  # Auto-generated from defaults
+    assert Settings.node_parser == _node_parser
+    assert Settings.prompt_helper == _prompt_helper
+    assert Settings.transformations == _transformations
