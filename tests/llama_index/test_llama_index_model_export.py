7d9c1df66d7f2f054fbc0f453c7b0446a309e5aa
Michael Berk <michaelberk99@gmail.com>
2024-07-09 20:48:18 -0400

    Llama index flavor base structure and object serialization functionality (PR 1) (#12606)
    Signed-off-by: Michael Berk <michaelberk99@gmail.com>
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>
    Co-authored-by: Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
    Co-authored-by: mlflow-automation <mlflow-automation@users.noreply.github.com>

---
+++ tests/llama_index/test_llama_index_model_export.py
@@ -0,0 +1,42 @@
+import pytest
+
+import mlflow
+
+
+@pytest.mark.parametrize(
+    "index_fixture",
+    [
+        "single_index",
+        "multi_index",
+        "single_graph",
+    ],
+)
+def test_llama_index_native_save_and_load_model(request, index_fixture, tmp_path):
+    index = request.getfixturevalue(index_fixture)
+    mlflow.llama_index.save_model(index, tmp_path, engine_type="query")
+    loaded_model = mlflow.llama_index.load_model(tmp_path)
+
+    assert type(loaded_model) == type(index)
+    assert loaded_model.as_query_engine().query("Spell llamaindex").response != ""
+
+
+@pytest.mark.parametrize(
+    "index_fixture",
+    [
+        "single_index",
+        "multi_index",
+        "single_graph",
+    ],
+)
+def test_llama_index_native_log_and_load_model(request, index_fixture):
+    index = request.getfixturevalue(index_fixture)
+    with mlflow.start_run():
+        logged_model = mlflow.llama_index.log_model(index, "model", engine_type="query")
+
+    loaded_model = mlflow.llama_index.load_model(logged_model.model_uri)
+
+    assert "llama_index" in logged_model.flavors
+    assert type(loaded_model) == type(index)
+    engine = loaded_model.as_query_engine()
+    assert engine is not None
+    assert engine.query("Spell llamaindex").response != ""
