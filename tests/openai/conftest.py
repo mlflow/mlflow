7c59c96f800b0deff94a53ebd2774a5268bef462
Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
2024-07-10 12:03:05 +0900

    Extract mock openai fixture so it can be used for testing other flavors (#12622)
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- tests/openai/conftest.py
+++ tests/openai/conftest.py
@@ -1,44 +1,14 @@
-import subprocess
-import sys
-import time
-
 import pytest
-import requests
 from packaging.version import Version
 
 import mlflow
 
-from tests.helper_functions import get_safe_port
+from tests.helper_functions import start_mock_openai_server
 
 is_v1 = Version(mlflow.openai._get_openai_package_version()).major >= 1
 
 
 @pytest.fixture(scope="module", autouse=True)
 def mock_openai():
-    port = get_safe_port()
-    with subprocess.Popen(
-        [
-            sys.executable,
-            "-m",
-            "uvicorn",
-            "tests.openai.mock_openai:app",
-            "--host",
-            "localhost",
-            "--port",
-            str(port),
-        ]
-    ) as proc:
-        base_url = f"http://localhost:{port}"
-        for _ in range(3):
-            try:
-                resp = requests.get(f"{base_url}/health")
-            except requests.ConnectionError:
-                time.sleep(1)
-                continue
-            if resp.ok:
-                break
-        else:
-            raise RuntimeError("Failed to start mock OpenAI server")
-
+    with start_mock_openai_server() as base_url:
         yield base_url
-        proc.kill()
