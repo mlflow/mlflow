033e01e648021d48cf95287822291678a530a548
Gabriel Fu <hfu.gabriel@gmail.com>
2024-07-10 21:11:00 +0800

    remove default
    Signed-off-by: Gabriel Fu <hfu.gabriel@gmail.com>

--- mlflow/gateway/providers/base.py
+++ mlflow/gateway/providers/base.py
@@ -15,7 +15,7 @@ class BaseProvider(ABC):
 
     NAME: str = ""
     SUPPORTED_ROUTE_TYPES: Tuple[str, ...]
-    CONFIG_TYPE: Type[ConfigModel] = None
+    CONFIG_TYPE: Type[ConfigModel]
 
     def __init__(self, config: RouteConfig):
         if self.NAME == "":
@@ -24,10 +24,10 @@ class BaseProvider(ABC):
                 f"override 'NAME' attribute as a non-empty string."
             )
 
-        if not self.CONFIG_TYPE:
+        if not hasattr(self, "CONFIG_TYPE") or not self.CONFIG_TYPE:
             raise ValueError(
                 f"{self.__class__.__name__} is a subclass of BaseProvider and must "
-                f"override 'CONFIG_TYPE' attribute as a subclass of ConfigModel."
+                f"define 'CONFIG_TYPE' attribute as a subclass of ConfigModel."
             )
 
         self.config = config
