10cd567033dea135f9badc25c6744874468ada71
Serena Ruan <serena.rxy@gmail.com>
2024-07-12 13:19:23 +0800

    comment
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -84,6 +84,9 @@ def clean_up_mlflow_virtual_environments():
     ],
 )
 def test_mlflow_run_example(directory, params, tmp_path):
+    # Use tmp_path+uuid as tmp directory to avoid the same
+    # directory being reused when re-trying the test since
+    # tmp_path is named as the test name
     random_tmp_path = tmp_path / str(uuid.uuid4())
     mlflow.set_tracking_uri(random_tmp_path.joinpath("mlruns").as_uri())
     example_dir = Path(EXAMPLES_DIR, directory)
