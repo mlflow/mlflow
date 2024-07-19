2c8cd6d50a15822abe5c6d284860cc61d61f4954
WeichenXu <weichen.xu@databricks.com>
2024-07-19 09:28:40 +0800

    Fix log_metric timestamp argument breaking change (#12670)
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/tracking/fluent.py
+++ mlflow/tracking/fluent.py
@@ -829,8 +829,8 @@ def log_metrics(
     metrics: Dict[str, float],
     step: Optional[int] = None,
     synchronous: Optional[bool] = None,
-    timestamp: Optional[int] = None,
     run_id: Optional[str] = None,
+    timestamp: Optional[int] = None,
 ) -> Optional[RunOperations]:
     """
     Log multiple metrics for the current run. If no run is active, this method will create a new
@@ -843,11 +843,11 @@ def log_metrics(
             max / min float values.
         step: A single integer step at which to log the specified
             Metrics. If unspecified, each metric is logged at step zero.
-        timestamp: Time when these metrics were calculated. Defaults to the current system time.
         synchronous: *Experimental* If True, blocks until the metrics are logged
             successfully. If False, logs the metrics asynchronously and
             returns a future representing the logging operation. If None, read from environment
             variable `MLFLOW_ENABLE_ASYNC_LOGGING`, which defaults to False if not set.
+        timestamp: Time when these metrics were calculated. Defaults to the current system time.
 
     Returns:
         When `synchronous=True`, returns None. When `synchronous=False`, returns an
