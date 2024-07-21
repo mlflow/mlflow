b82feb9178f785d5f5c0a81787110d8cd803af36
Serena Ruan <82044803+serena-ruan@users.noreply.github.com>
2024-07-21 15:10:40 +0800

    Fix tensorflow hanging test suite (#12676)
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>
    Signed-off-by: serena-ruan <serena.ruan@databricks.com>

--- mlflow/tracking/fluent.py
+++ mlflow/tracking/fluent.py
@@ -667,6 +667,11 @@ def flush_async_logging() -> None:
     _get_store().flush_async_logging()
 
 
+def _shut_down_async_logging() -> None:
+    """Shutdown the async logging and flush all pending data."""
+    _get_store().shut_down_async_logging()
+
+
 def flush_artifact_async_logging() -> None:
     """Flush all pending artifact async logging."""
     run_id = _get_or_start_run().info.run_id
