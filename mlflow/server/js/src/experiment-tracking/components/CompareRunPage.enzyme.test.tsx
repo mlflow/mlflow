2f9e99c7c57bc4b4e79dd3fa7bf9b4ee82bf57e7
Christian Gilles <136780849+cgilviadee@users.noreply.github.com>
2024-07-22 03:37:03 +0200

    Creating compatibility with Kubeflow Dashboard UI (#12663)

--- mlflow/server/js/src/experiment-tracking/components/CompareRunPage.enzyme.test.tsx
+++ mlflow/server/js/src/experiment-tracking/components/CompareRunPage.enzyme.test.tsx
@@ -55,3 +55,42 @@ describe('CompareRunPage', () => {
     expect(wrapper.find(CompareRunPage).length).toBe(1);
   });
 });
+
+describe('CompareRunPage URI encoded', () => {
+  let wrapper;
+  let minimalProps: any;
+  let minimalStore: any;
+  const mockStore = configureStore([thunk, promiseMiddleware()]);
+
+  beforeEach(() => {
+    // TODO: remove global fetch mock by explicitly mocking all the service API calls
+    // @ts-expect-error TS(2322): Type 'Mock<Promise<{ ok: true; status: number; tex... Remove this comment to see the full error message
+    global.fetch = jest.fn(() => Promise.resolve({ ok: true, status: 200, text: () => Promise.resolve('') }));
+    minimalProps = {
+      location: {
+        search:
+          '?runs=%5B%252281d708375e574d6cbf4985b8701d67d2%2522,%25225f70fea1ef004d3180a6c34fe2d0d94e%2522%5D&experiments=%5B%25220%2522%5D',
+      },
+      experimentIds: ['12345'],
+      runUuids: ['runn-1234-5678-9012', 'runn-1234-5678-9034'],
+      dispatch: jest.fn(),
+    };
+    minimalStore = mockStore({
+      entities: {},
+      apis: jest.fn((key) => {
+        return {};
+      }),
+    });
+  });
+
+  test('should render with minimal props without exploding', () => {
+    wrapper = mountWithIntl(
+      <Provider store={minimalStore}>
+        <MemoryRouter>
+          <CompareRunPage {...minimalProps} />
+        </MemoryRouter>
+      </Provider>,
+    );
+    expect(wrapper.find(CompareRunPage).length).toBe(1);
+  });
+});
