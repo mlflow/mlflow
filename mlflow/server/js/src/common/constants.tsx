f0058d0080b08c672a487ef6ea5ecfced4736ba3
mlflow-automation <mlflow-automation@users.noreply.github.com>
2024-07-22 09:05:01 +0000

    Run python3 dev/update_mlflow_versions.py pre-release ...
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>

--- mlflow/server/js/src/common/constants.tsx
+++ mlflow/server/js/src/common/constants.tsx
@@ -9,7 +9,7 @@ export const ErrorCodes = {
   RESOURCE_CONFLICT: 'RESOURCE_CONFLICT',
 };
 
-export const Version = '2.14.4.dev0';
+export const Version = '2.15.0rc0';
 
 const DOCS_VERSION = 'latest';
 
