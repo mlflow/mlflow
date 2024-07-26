c58dcb32feb4b1362548cb021f0090957ac66d14
Danilo Peixoto <danilopeixoto@outlook.com>
2024-07-26 02:50:00 +0000

    Simplify tests
    Signed-off-by: GitHub <noreply@github.com>

--- tests/deployments/server/test_server_app.py
+++ tests/deployments/server/test_server_app.py
@@ -198,7 +198,11 @@ def test_dynamic_endpoint():
         assert resp.json() == test_response
 
 
-def test_rate_limit():
+@pytest.mark.parametrize(
+    "endpoint_path",
+    [f"{MLFLOW_DEPLOYMENTS_ENDPOINTS_BASE}chat/invocations", "/v1/chat/completions"],
+)
+def test_rate_limit(endpoint_path: str):
     config = GatewayConfig(
         **{
             "endpoints": [
@@ -220,48 +224,6 @@ def test_rate_limit():
     )
     app = create_app_from_config(config)
     client = TestClient(app)
-
-    with mock.patch(
-        "aiohttp.ClientSession.post", return_value=MockAsyncResponse(model_response)
-    ) as mock_post:
-        resp = client.post(
-            f"{MLFLOW_DEPLOYMENTS_ENDPOINTS_BASE}chat/invocations",
-            json={"messages": [{"role": "user", "content": "Tell me a joke"}]},
-        )
-        mock_post.assert_called_once()
-        assert resp.status_code == 200
-        assert resp.json() == test_response
-        # second call
-        resp = client.post(
-            f"{MLFLOW_DEPLOYMENTS_ENDPOINTS_BASE}chat/invocations",
-            json={"messages": [{"role": "user", "content": "Tell me a joke again"}]},
-        )
-        assert resp.status_code == 429
-
-
-def test_openai_rate_limit():
-    config = GatewayConfig(
-        **{
-            "endpoints": [
-                {
-                    "name": "chat",
-                    "endpoint_type": "llm/v1/chat",
-                    "model": {
-                        "name": "gpt-4",
-                        "provider": "openai",
-                        "config": {
-                            "openai_api_key": "mykey",
-                            "openai_api_base": "https://api.openai.com/v1",
-                        },
-                    },
-                    "limit": {"calls": 1, "key": None, "renewal_period": "minute"},
-                }
-            ]
-        }
-    )
-    app = create_app_from_config(config)
-    client = TestClient(app)
-    endpoint_path = "/v1/chat/completions"
 
     with mock.patch(
         "aiohttp.ClientSession.post", return_value=MockAsyncResponse(model_response)
