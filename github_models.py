dbf25b4356415f6cd55c2d9e2800b42521859171
harupy <17039389+harupy@users.noreply.github.com>
2024-08-02 08:57:07 +0900

    test
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

---
+++ github_models.py
@@ -0,0 +1,31 @@
+import os
+
+from openai import OpenAI
+
+token = os.environ["GITHUB_TOKEN"]
+endpoint = "https://models.inference.ai.azure.com"
+model_name = "gpt-4o-mini"
+
+client = OpenAI(
+    base_url=endpoint,
+    api_key=token,
+)
+
+response = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant.",
+        },
+        {
+            "role": "user",
+            "content": "What is the capital of France?",
+        },
+    ],
+    model=model_name,
+    temperature=1.0,
+    max_tokens=1000,
+    top_p=1.0,
+)
+
+print(response.choices[0].message.content)  # noqa
