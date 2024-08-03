778fbc2832acb185ab28135977d8b1fd6b96c376
Momchil Konstantinov <konstantinov.momchil@gmail.com>
2024-08-03 10:23:39 +0100

    Add async support to the @trace decorator
        - @trace decorator now checks whether the function its wrapping is async and, if so, returns an async wrapper
        - add a couple of async tests for the new functionality
        - also add pytest-asyncio to the requirements, so we can run async tests
    
    Signed-off-by: Momchil Konstantinov <konstantinov.momchil@gmail.com>

--- tests/tracing/test_fluent.py
+++ tests/tracing/test_fluent.py
@@ -51,6 +51,24 @@ class DefaultTestModel:
         return res
 
 
+class DefaultAsyncTestModel:
+    @mlflow.trace()
+    async def predict(self, x, y):
+        z = x + y
+        z = await self.add_one(z)
+        z = await mlflow.trace(self.square)(z)
+        return z  # noqa: RET504
+
+    @mlflow.trace(span_type=SpanType.LLM, name="add_one_with_custom_name", attributes={"delta": 1})
+    async def add_one(self, z):
+        return z + 1
+
+    async def square(self, t):
+        res = t**2
+        time.sleep(0.1)
+        return res
+
+
 @pytest.fixture
 def mock_client():
     client = mock.MagicMock()
@@ -68,6 +86,67 @@ def test_trace(with_active_run):
             run_id = run.info.run_id
     else:
         model.predict(2, 5)
+
+    trace = mlflow.get_last_active_trace()
+    trace_info = trace.info
+    assert trace_info.request_id is not None
+    assert trace_info.experiment_id == "0"  # default experiment
+    assert trace_info.execution_time_ms >= 0.1 * 1e3  # at least 0.1 sec
+    assert trace_info.status == SpanStatusCode.OK
+    assert trace_info.request_metadata[TraceMetadataKey.INPUTS] == '{"x": 2, "y": 5}'
+    assert trace_info.request_metadata[TraceMetadataKey.OUTPUTS] == "64"
+    if with_active_run:
+        assert trace_info.request_metadata[TraceMetadataKey.SOURCE_RUN] == run_id
+
+    assert trace.data.request == '{"x": 2, "y": 5}'
+    assert trace.data.response == "64"
+    assert len(trace.data.spans) == 3
+
+    span_name_to_span = {span.name: span for span in trace.data.spans}
+    root_span = span_name_to_span["predict"]
+    assert root_span.start_time_ns // 1e6 == trace.info.timestamp_ms
+    assert root_span.parent_id is None
+    assert root_span.attributes == {
+        "mlflow.traceRequestId": trace_info.request_id,
+        "mlflow.spanFunctionName": "predict",
+        "mlflow.spanType": "UNKNOWN",
+        "mlflow.spanInputs": {"x": 2, "y": 5},
+        "mlflow.spanOutputs": 64,
+    }
+
+    child_span_1 = span_name_to_span["add_one_with_custom_name"]
+    assert child_span_1.parent_id == root_span.span_id
+    assert child_span_1.attributes == {
+        "delta": 1,
+        "mlflow.traceRequestId": trace_info.request_id,
+        "mlflow.spanFunctionName": "add_one",
+        "mlflow.spanType": "LLM",
+        "mlflow.spanInputs": {"z": 7},
+        "mlflow.spanOutputs": 8,
+    }
+
+    child_span_2 = span_name_to_span["square"]
+    assert child_span_2.parent_id == root_span.span_id
+    assert child_span_2.start_time_ns <= child_span_2.end_time_ns - 0.1 * 1e6
+    assert child_span_2.attributes == {
+        "mlflow.traceRequestId": trace_info.request_id,
+        "mlflow.spanFunctionName": "square",
+        "mlflow.spanType": "UNKNOWN",
+        "mlflow.spanInputs": {"t": 8},
+        "mlflow.spanOutputs": 64,
+    }
+
+
+@pytest.mark.parametrize("with_active_run", [True, False])
+async def test_trace_async(with_active_run):
+    model = DefaultAsyncTestModel()
+
+    if with_active_run:
+        with mlflow.start_run() as run:
+            await model.predict(2, 5)
+            run_id = run.info.run_id
+    else:
+        await model.predict(2, 5)
 
     trace = mlflow.get_last_active_trace()
     trace_info = trace.info
@@ -336,6 +415,37 @@ def test_trace_handle_exception_during_prediction():
 
     with pytest.raises(ValueError, match=r"Some error"):
         model.predict(2, 5)
+        # model.some_operation_raise_error(2, 4)
+
+    # Trace should be logged even if the function fails, with status code ERROR
+    trace = mlflow.get_last_active_trace()
+    assert trace.info.request_id is not None
+    assert trace.info.status == TraceStatus.ERROR
+    assert trace.info.request_metadata[TraceMetadataKey.INPUTS] == '{"x": 2, "y": 5}'
+    assert trace.info.request_metadata[TraceMetadataKey.OUTPUTS] == ""
+
+    assert trace.data.request == '{"x": 2, "y": 5}'
+    assert trace.data.response is None
+    assert len(trace.data.spans) == 2
+
+
+async def test_trace_handle_exception_during_prediction_async():
+    # This test is to make sure that the exception raised by the main prediction
+    # logic is raised properly and the trace is still logged.
+    class TestModel:
+        @mlflow.trace()
+        async def predict(self, x, y):
+            return await self.some_operation_raise_error(x, y)
+
+        @mlflow.trace()
+        async def some_operation_raise_error(self, x, y):
+            raise ValueError("Some error")
+
+    model = TestModel()
+
+    with pytest.raises(ValueError, match=r"Some error"):
+        await model.predict(2, 5)
+        # model.some_operation_raise_error(2, 4)
 
     # Trace should be logged even if the function fails, with status code ERROR
     trace = mlflow.get_last_active_trace()
