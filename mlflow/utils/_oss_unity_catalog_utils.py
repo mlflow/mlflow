fd47db1d2a894590358e5767906e10b2f76408c4
Arthur Jenoudet <arthur.jenoudet@databricks.com>
2024-08-05 13:25:15 -0700

    add test for converting uc oss proto to mlflow RegisteredModel object
    Signed-off-by: Arthur Jenoudet <arthur.jenoudet@databricks.com>

--- mlflow/utils/_oss_unity_catalog_utils.py
+++ mlflow/utils/_oss_unity_catalog_utils.py
@@ -4,12 +4,12 @@ from mlflow.protos.databricks_managed_catalog_messages_pb2 import RegisteredMode
 
 def registered_model_from_uc_oss_proto(uc_oss_proto: RegisteredModelInfo) -> RegisteredModel:
     return RegisteredModel(
-        name=uc_oss_proto.name,
+        name=f"{uc_oss_proto.catalog_name}.{uc_oss_proto.schema_name}.{uc_oss_proto.name}",
         creation_timestamp=uc_oss_proto.created_at,
         last_updated_timestamp=uc_oss_proto.updated_at,
         description=uc_oss_proto.comment,
         aliases=[
-            RegisteredModelAlias(alias=alias.alias_name, version=alias.version_num)
+            RegisteredModelAlias(alias=alias.alias_name, version=str(alias.version_num))
             for alias in (uc_oss_proto.aliases or [])
         ],
         tags=[
