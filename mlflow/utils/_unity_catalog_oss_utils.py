1140f4f10821840bd9c905729a3134d4ded6d41f
rohitarun-db <rohit.arunachalam@databricks.com>
2024-08-15 11:51:55 -0700

    fixing attribute name to model_name
    Signed-off-by: rohitarun-db <rohit.arunachalam@databricks.com>

--- mlflow/utils/_unity_catalog_oss_utils.py
+++ mlflow/utils/_unity_catalog_oss_utils.py
@@ -16,7 +16,7 @@ def registered_model_from_uc_oss_proto(uc_oss_proto: RegisteredModelInfo) -> Reg
 
 def model_version_from_uc_oss_proto(uc_oss_proto: ModelVersionInfo) -> ModelVersion:
     return ModelVersion(
-        name=f"{uc_oss_proto.catalog_name}.{uc_oss_proto.schema_name}.{uc_oss_proto.name}",
+        name=f"{uc_oss_proto.catalog_name}.{uc_oss_proto.schema_name}.{uc_oss_proto.model_name}",
         version=uc_oss_proto.version,
         creation_timestamp=uc_oss_proto.created_at,
         last_updated_timestamp=uc_oss_proto.updated_at,
