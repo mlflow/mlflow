6dc38ed1d5151fe0283e7bbe62d5992c3413fbfe
rohitarun-db <rohit.arunachalam@databricks.com>
2024-08-14 17:04:33 -0700

    fixing get model version
    Signed-off-by: rohitarun-db <rohit.arunachalam@databricks.com>

--- mlflow/store/_unity_catalog/registry_oss/rest_store_oss.py
+++ mlflow/store/_unity_catalog/registry_oss/rest_store_oss.py
@@ -155,7 +155,7 @@ class UnityCatalogOssStore(BaseRestStore):
 
     def get_model_version(self, name, version):
         full_name = get_full_name_from_sc(name, None)
-        req_body = message_to_json(GetModelVersion(name=full_name, version=str(version)))
+        req_body = message_to_json(GetModelVersion(full_name_arg=full_name, version_arg=version))
         endpoint, method = _METHOD_TO_INFO[GetRegisteredModel]
         final_endpoint = endpoint.replace("{full_name_arg}", full_name).replace("{version_arg}", str(version))
         registered_model_version = call_endpoint(get_databricks_host_creds(), endpoint=final_endpoint, method=method, json_body=req_body, response_proto=self._get_response_from_method(GetRegisteredModel))
