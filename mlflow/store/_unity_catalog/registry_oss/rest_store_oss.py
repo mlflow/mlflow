37ad7122401444bd1180db9e0a4019287fab2229
rohitarun-db <rohit.arunachalam@databricks.com>
2024-08-15 12:54:51 -0700

    fix to delete model version to call correct proto
    Signed-off-by: rohitarun-db <rohit.arunachalam@databricks.com>

--- mlflow/store/_unity_catalog/registry_oss/rest_store_oss.py
+++ mlflow/store/_unity_catalog/registry_oss/rest_store_oss.py
@@ -175,7 +175,7 @@ class UnityCatalogOssStore(BaseRestStore):
 
     def delete_model_version(self, name, version):
         full_name = get_full_name_from_sc(name, None)
-        req_body = message_to_json(DeleteRegisteredModel(full_name_arg=full_name, version_arg=version))
+        req_body = message_to_json(DeleteModelVersion(full_name_arg=full_name, version_arg=version))
         endpoint, method = _METHOD_TO_INFO[DeleteModelVersion]
         final_endpoint = endpoint.replace("{full_name_arg}", full_name).replace("{version_arg}", str(version))
         registered_model_info = call_endpoint(get_databricks_host_creds(), endpoint=final_endpoint, method=method, json_body=req_body, response_proto=self._get_response_from_method(DeleteModelVersion))
