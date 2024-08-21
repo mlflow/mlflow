260c95cfbb5d20ad0c73ad22e77bf1db16f55a9c
Serena Ruan <serena.rxy@gmail.com>
2024-08-21 14:33:55 +0800

    fix data conversion for serving in langchain
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- mlflow/models/utils.py
+++ mlflow/models/utils.py
@@ -1652,6 +1652,10 @@ def _convert_llm_ndarray_to_list(data):
         return [_convert_llm_ndarray_to_list(d) for d in data]
     if isinstance(data, dict):
         return {k: _convert_llm_ndarray_to_list(v) for k, v in data.items()}
+    # scalar values are also converted to numpy types, but they are
+    # not acceptable by the model
+    if np.isscalar(data) and isinstance(data, np.generic):
+        return data.item()
     return data
 
 
