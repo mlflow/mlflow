696a29747c9d61a2f3b69a0d8df27715de2854e8
harupy <17039389+harupy@users.noreply.github.com>
2024-09-03 19:45:28 +0900

    Use variable
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- dev/install-taplo.sh
+++ dev/install-taplo.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-mkdir -p /tmp/taplo/bin
-wget -q -O - 'https://github.com/tamasfe/taplo/releases/download/0.9.3/taplo-linux-x86_64.gz' | gunzip -c > /tmp/taplo/bin/taplo
+DST="/tmp/taplo/bin"
+mkdir -p $DST
+wget -q -O - 'https://github.com/tamasfe/taplo/releases/download/0.9.3/taplo-linux-x86_64.gz' | gunzip -c > $DST/taplo
 if [[ $? -eq 0 ]]; then
-  chmod +x /tmp/taplo/bin/taplo
-  echo "/tmp/taplo/bin" >> $GITHUB_PATH
+  chmod +x $DST/taplo
+  echo $DST >> $GITHUB_PATH
 else
   # Fall back to cargo install if binary download fails
   cargo install taplo-cli@0.9.3 --locked
