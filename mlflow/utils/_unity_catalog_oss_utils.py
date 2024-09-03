af8ffc7c350053c314fca459038a10dcdb72e390
rohitarun-db <rohit.arunachalam@databricks.com>
2024-09-03 13:36:45 -0700

    fix proto conversion

--- mlflow/utils/_unity_catalog_oss_utils.py
+++ mlflow/utils/_unity_catalog_oss_utils.py
@@ -42,7 +42,7 @@ def registered_model_search_from_uc_oss_proto(uc_oss_proto: RegisteredModelInfo)
 
 def model_version_search_from_uc_oss_proto(uc_oss_proto: ModelVersionInfo) -> ModelVersionSearch:
     return ModelVersionSearch(
-        name=f"{uc_oss_proto.catalog_name}.{uc_oss_proto.schema_name}.{uc_oss_proto.name}",
+        name=f"{uc_oss_proto.catalog_name}.{uc_oss_proto.schema_name}.{uc_oss_proto.model_name}",
         version=uc_oss_proto.version,
         creation_timestamp=uc_oss_proto.created_at,
         last_updated_timestamp=uc_oss_proto.updated_at,
