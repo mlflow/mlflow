8634e7174e91a60b2a4d6eef36ee0b92ab64460f
rohitarun-db <rohit.arunachalam@databricks.com>
2024-09-03 11:47:07 -0700

    proto change

--- mlflow/utils/_unity_catalog_oss_utils.py
+++ mlflow/utils/_unity_catalog_oss_utils.py
@@ -37,7 +37,7 @@ def registered_model_search_from_uc_oss_proto(uc_oss_proto: RegisteredModelInfo)
         name=f"{uc_oss_proto.catalog_name}.{uc_oss_proto.schema_name}.{uc_oss_proto.name}",
         creation_timestamp=uc_oss_proto.created_at,
         last_updated_timestamp=uc_oss_proto.updated_at,
-        description=uc_oss_proto.description,
+        description=uc_oss_proto.comment,
     )
 
 def model_version_search_from_uc_oss_proto(uc_oss_proto: ModelVersionInfo) -> ModelVersionSearch:
