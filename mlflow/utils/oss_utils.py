47c7b819cb93f206b0b9626bcaea4b469f414ffd
rohitarun-db <rohit.arunachalam@databricks.com>
2024-09-04 13:37:47 -0700

    doc string
    Signed-off-by: rohitarun-db <rohit.arunachalam@databricks.com>

--- mlflow/utils/oss_utils.py
+++ mlflow/utils/oss_utils.py
@@ -8,9 +8,18 @@ from mlflow.utils.uri import (
 
 
 def get_oss_host_creds(server_uri=None):
+    """
+    Retrieve the host credentials for the OSS server.
+    Args:
+        server_uri (str): The URI of the server.
+    Returns:
+        MlflowHostCreds: The host credentials for the OSS server.
+    """
     parsed_uri = urllib.parse.urlparse(server_uri)
-    new_uri = parsed_uri.path
+    # new_uri is the parsed_uri WITHOUT the scheme 
+    new_uri = parsed_uri.path 
     new_parsed_uri = urllib.parse.urlparse(new_uri)
+    #checking if the server_uri scheme is "uc"
     if parsed_uri.scheme == "uc":
         if parsed_uri.path == _DATABRICKS_UNITY_CATALOG_SCHEME:
             return get_databricks_host_creds(parsed_uri.path)
