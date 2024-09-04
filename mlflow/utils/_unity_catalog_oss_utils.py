9af1b29effbcb48cf8dfd233fd7a14d72b344b54
rohitarun-db <rohit.arunachalam@databricks.com>
2024-09-04 11:58:20 -0700

    UC OSS Finalized Protos (#13065)
    Signed-off-by: rohitarun-db <rohit.arunachalam@databricks.com>
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>
    Co-authored-by: mlflow-automation <mlflow-automation@users.noreply.github.com>

--- mlflow/utils/_unity_catalog_oss_utils.py
+++ mlflow/utils/_unity_catalog_oss_utils.py
@@ -1,7 +1,5 @@
-from typing import List, Optional
-
-from mlflow.entities.model_registry import RegisteredModel, RegisteredModelAlias, RegisteredModelTag
-from mlflow.protos.unity_catalog_oss_messages_pb2 import RegisteredModelInfo, TagKeyValue
+from mlflow.entities.model_registry import RegisteredModel
+from mlflow.protos.unity_catalog_oss_messages_pb2 import RegisteredModelInfo
 
 
 def registered_model_from_uc_oss_proto(uc_oss_proto: RegisteredModelInfo) -> RegisteredModel:
@@ -10,19 +8,4 @@ def registered_model_from_uc_oss_proto(uc_oss_proto: RegisteredModelInfo) -> Reg
         creation_timestamp=uc_oss_proto.created_at,
         last_updated_timestamp=uc_oss_proto.updated_at,
         description=uc_oss_proto.comment,
-        aliases=[
-            RegisteredModelAlias(alias=alias.alias_name, version=str(alias.version_num))
-            for alias in (uc_oss_proto.aliases or [])
-        ],
-        tags=[
-            RegisteredModelTag(key=tag.key, value=tag.value) for tag in (uc_oss_proto.tags or [])
-        ],
     )
-
-
-def uc_oss_registered_model_tag_from_mlflow_tags(
-    tags: Optional[List[RegisteredModelTag]],
-) -> List[TagKeyValue]:
-    if tags is None:
-        return []
-    return [TagKeyValue(key=t.key, value=t.value) for t in tags]
