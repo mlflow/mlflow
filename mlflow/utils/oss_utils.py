a19593a8ef2bf14f9448c80a744abb6876ce05aa
rohitarun-db <rohit.arunachalam@databricks.com>
2024-09-05 09:51:00 -0700

    clean up oss_utils

--- mlflow/utils/oss_utils.py
+++ mlflow/utils/oss_utils.py
@@ -1,34 +1,29 @@
 import urllib.parse
 
-from mlflow.exceptions import MlflowException
 from mlflow.utils.databricks_utils import get_databricks_host_creds
 from mlflow.utils.rest_utils import MlflowHostCreds
 from mlflow.utils.uri import (
     _DATABRICKS_UNITY_CATALOG_SCHEME,
 )
+from mlflow.exceptions import MlflowException
 
 
 def get_oss_host_creds(server_uri=None):
     """
     Retrieve the host credentials for the OSS server.
-
+    
     Args:
         server_uri (str): The URI of the server.
-
+    
     Returns:
         MlflowHostCreds: The host credentials for the OSS server.
     """
     parsed_uri = urllib.parse.urlparse(server_uri)
-    # new_uri is the parsed_uri WITHOUT the scheme
-    new_uri = parsed_uri.path
-    new_parsed_uri = urllib.parse.urlparse(new_uri)
-    # checking if the server_uri scheme is "uc"
-    if parsed_uri.scheme == "uc":
-        if parsed_uri.path == _DATABRICKS_UNITY_CATALOG_SCHEME:
-            return get_databricks_host_creds(parsed_uri.path)
-        else:
-            return MlflowHostCreds(
-                host=f"{new_parsed_uri.scheme}://{new_parsed_uri.netloc}",
-            )
-    else:
-        raise MlflowException("The scheme of the server_uri is should be 'uc'")
+    
+    if parsed_uri.scheme != "uc":
+            raise MlflowException("The scheme of the server_uri should be 'uc'")
+        
+    if parsed_uri.path == _DATABRICKS_UNITY_CATALOG_SCHEME:
+        return get_databricks_host_creds(parsed_uri.path)
+
+    return MlflowHostCreds(host=parsed_uri.path)
--- tests/utils/test_oss_utils.py
+++ tests/utils/test_oss_utils.py
@@ -2,21 +2,38 @@ from unittest import mock
 
 from mlflow.utils.oss_utils import get_oss_host_creds
 from mlflow.utils.rest_utils import MlflowHostCreds
+import pytest
+from unittest import mock
+from mlflow.utils.oss_utils import get_oss_host_creds
+from mlflow.utils.rest_utils import MlflowHostCreds
+from mlflow.exceptions import MlflowException
 
 
-def test_get_oss_host_creds():
-    # When the scheme is "uc" and the new scheme is not "_DATABRICKS_UNITY_CATALOG_SCHEME"
-    server_uri = "uc:http://localhost:8081"
-    expected_creds = MlflowHostCreds(host="http://localhost:8081")
-    actual_creds = get_oss_host_creds(server_uri)
-    assert actual_creds == expected_creds
+@pytest.mark.parametrize(
+    "server_uri, expected_creds",
+    [
+        ("uc:databricks-uc", MlflowHostCreds(host="databricks-uc")),
+        ("uc:http://localhost:8081", MlflowHostCreds(host="http://localhost:8081")),
+        ("invalid_scheme:http://localhost:8081", MlflowException),
+    ],
+)
+def test_get_oss_host_creds(server_uri, expected_creds):
+    with mock.patch(
+        "mlflow.utils.oss_utils.get_databricks_host_creds", return_value=MlflowHostCreds(host="databricks-uc")
+    ) as mock_get_databricks_host_creds:
+        if expected_creds == MlflowException:
+            with pytest.raises(MlflowException):
+                get_oss_host_creds(server_uri)
+        else:
+            actual_creds = get_oss_host_creds(server_uri)
+            assert actual_creds == expected_creds
 
 
 def test_get_databricks_host_creds():
-    # When the scheme is "uc" and the new scheme is "_DATABRICKS_UNITY_CATALOG_SCHEME"
+    # Test case: When the scheme is "uc" and the new scheme is "_DATABRICKS_UNITY_CATALOG_SCHEME"
     server_uri = "uc:databricks-uc"
     with mock.patch(
         "mlflow.utils.oss_utils.get_databricks_host_creds", return_value=mock.MagicMock()
     ) as mock_get_databricks_host_creds:
-        get_oss_host_creds(server_uri)
+        expected_creds = get_oss_host_creds(server_uri)
         assert mock_get_databricks_host_creds.call_args_list == [mock.call("databricks-uc")]
