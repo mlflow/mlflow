38ebaebfb687ecc5ed3b72b422f25d11df16b2af
harupy <17039389+harupy@users.noreply.github.com>
2024-09-06 13:55:17 +0900

    Deprecate mlflow deployments start-server
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/deployments/cli.py
+++ mlflow/deployments/cli.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import warnings
 from inspect import signature
 
 import click
@@ -8,7 +9,6 @@ from mlflow.deployments import interface
 from mlflow.environment_variables import MLFLOW_DEPLOYMENTS_CONFIG
 from mlflow.utils import cli_args
 from mlflow.utils.annotations import experimental
-from mlflow.utils.os import is_windows
 from mlflow.utils.proto_json_utils import NumpyEncoder, _get_jsonable_obj
 
 
@@ -496,8 +496,11 @@ def validate_config_path(_ctx, _param, value):
     help="The number of workers.",
 )
 def start_server(config_path: str, host: str, port: str, workers: int):
-    if is_windows():
-        raise click.ClickException("MLflow Deployments Server does not support Windows.")
+    warnings.warn(
+        "This command is deprecated and will be removed in a future release. "
+        "Use `mlflow gateway start` instead.",
+        FutureWarning,
+    )
 
     from mlflow.deployments.server.runner import run_app
 
