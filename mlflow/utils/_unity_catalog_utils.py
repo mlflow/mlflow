e7470d6e22114228a9cb82dbeb74e3601572f49b
rohitarun-db <rohit.arunachalam@databricks.com>
2024-09-06 12:05:47 -0700

    test

--- mlflow/utils/_unity_catalog_utils.py
+++ mlflow/utils/_unity_catalog_utils.py
@@ -261,7 +261,7 @@ def _get_artifact_repo_from_storage_info_oss(
     print("Scoped Token: ", scoped_token)
     print("AWS Cred Info: ", scoped_token.aws_temp_credentials, scoped_token.aws_temp_credentials is not None, "Scoped token empty?", scoped_token.aws_temp_credentials == "")
     print(scoped_token.aws_temp_credentials.access_key_id, scoped_token.aws_temp_credentials.access_key_id is not None)
-    print("exist", len(scoped_token.aws_temp_credentials), len(scoped_token.azure_user_delegation_sas), len(scoped_token.gcp_oauth_token))
+    # print("exist", len(scoped_token.aws_temp_credentials), len(scoped_token.azure_user_delegation_sas), len(scoped_token.gcp_oauth_token))
     print("key length", len(scoped_token.aws_temp_credentials.access_key_id), len(scoped_token.azure_user_delegation_sas.sas_token), len(scoped_token.gcp_oauth_token.oauth_token))
     if scoped_token.aws_temp_credentials:
         # Verify upfront that boto3 is importable
