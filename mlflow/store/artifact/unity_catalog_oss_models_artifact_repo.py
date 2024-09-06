2971ca757d91ca008f89336e17e9d6232a3fc678
mlflow-automation <mlflow-automation@users.noreply.github.com>
2024-09-06 00:21:58 +0000

    Autoformat: https://github.com/mlflow/mlflow/actions/runs/10730164706
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>

--- mlflow/store/artifact/unity_catalog_oss_models_artifact_repo.py
+++ mlflow/store/artifact/unity_catalog_oss_models_artifact_repo.py
@@ -81,7 +81,7 @@ class UnityCatalogOSSModelsArtifactRepository(ArtifactRepository):
         )
         if registry_uri_from_artifact_uri is not None:
             registry_uri = registry_uri_from_artifact_uri
-        
+
         _, key_prefix = get_db_info_from_uri(urllib.parse.urlparse(registry_uri).path)
         if key_prefix is not None:
             raise MlflowException(
