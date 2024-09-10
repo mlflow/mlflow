ebb33753d61fcafe2503473980f1afb121dcec57
harupy <17039389+harupy@users.noreply.github.com>
2024-09-10 20:57:08 +0900

    test
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/resources/example_virtualenv_no_python_env/entrypoint.py
+++ tests/resources/example_virtualenv_no_python_env/entrypoint.py
@@ -20,6 +20,7 @@ parser.add_argument(
 args = parser.parse_args()
 if args.test:
     assert "VIRTUAL_ENV" in os.environ
+    print("VIRTUAL_ENV:", os.environ["VIRTUAL_ENV"])  # noqa: T201
     assert sys.version_info[:3] == (3, 8, 18), sys.version_info
     assert sklearn.__version__ == "1.0.2", sklearn.__version__
 
