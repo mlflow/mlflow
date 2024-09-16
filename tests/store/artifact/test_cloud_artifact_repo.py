4782695574d79137e5e4f44e3ea6dc56bbd2cc98
rohitarun-db <rohit.arunachalam@databricks.com>
2024-09-16 16:08:10 -0700

    use tmp_path

--- tests/store/artifact/test_cloud_artifact_repo.py
+++ tests/store/artifact/test_cloud_artifact_repo.py
@@ -24,20 +24,26 @@ def test_chunk_size_validation_failure():
     with pytest.raises(MlflowException, match="Multipart chunk size"):
         _validate_chunk_size_aws(5 * 1024**3 + 1)
 
-@pytest.mark.parametrize("future_result, expected_call_count", [
-    (None, 2),  # Simulate where creds are expired, but successfully download after refresh
-    (Exception("fake_exception"),4)  # Simulate where there is a download failure and retries are exhausted
-])
-def test__parallelized_download_from_cloud(monkeypatch, future_result, expected_call_count):
-
+@pytest.mark.parametrize(
+    "future_result, expected_call_count",
+    [
+        (None, 2),  # Simulate where creds are expired, but successfully download after refresh
+        (Exception("fake_exception"), 4),  # Simulate where there is a download failure and retries are exhausted
+    ],
+)
+def test__parallelized_download_from_cloud(
+    monkeypatch, future_result, expected_call_count, tmp_path
+):
     # Mock environment variables
-    monkeypatch.setenv("_MLFLOW_MPD_NUM_RETRIES", 3)
-    monkeypatch.setenv("_MLFLOW_MPD_RETRY_INTERVAL_SECONDS", 0)
+    monkeypatch.setenv("_MLFLOW_MPD_NUM_RETRIES", "3")
+    monkeypatch.setenv("_MLFLOW_MPD_RETRY_INTERVAL_SECONDS", "0")
 
-    with mock.patch("mlflow.store.artifact.cloud_artifact_repo.CloudArtifactRepository") as cloud_artifact_mock:
+    with mock.patch(
+        "mlflow.store.artifact.cloud_artifact_repo.CloudArtifactRepository"
+    ) as cloud_artifact_mock:
         cloud_artifact_instance = cloud_artifact_mock.return_value
 
-        # Mock all methods except 'method_to_test'
+        # Mock all methods except '_parallelized_download_from_cloud'
         cloud_artifact_instance._parallelized_download_from_cloud.side_effect = (
             lambda *args, **kwargs: CloudArtifactRepository._parallelized_download_from_cloud(
                 cloud_artifact_instance, *args, **kwargs
@@ -55,15 +61,27 @@ def test__parallelized_download_from_cloud(monkeypatch, future_result, expected_
         fake_chunk_1 = FakeChunk(index=1, start=0, end=100, path="fake_path_1")
         mock_failed_downloads = {fake_chunk_1: "fake_chunk_1"}
 
-        with mock.patch("mlflow.store.artifact.cloud_artifact_repo.parallelized_download_file_using_http_uri") as mock_download:
-            mock_download.return_value = mock_failed_downloads
-
+        with mock.patch(
+            "mlflow.store.artifact.cloud_artifact_repo.parallelized_download_file_using_http_uri",
+            return_value=mock_failed_downloads,
+        ):
             # Create a new ArtifactCredentialInfo object
-            fake_credential = ArtifactCredentialInfo(signed_uri="fake_signed_uri", type=ArtifactCredentialType.AWS_PRESIGNED_URL)
-            fake_credential.headers.extend([ArtifactCredentialInfo.HttpHeader(name="fake_header_name", value="fake_header_value")])
+            fake_credential = ArtifactCredentialInfo(
+                signed_uri="fake_signed_uri",
+                type=ArtifactCredentialType.AWS_PRESIGNED_URL,
+            )
+            fake_credential.headers.extend(
+                [
+                    ArtifactCredentialInfo.HttpHeader(
+                        name="fake_header_name", value="fake_header_value"
+                    )
+                ]
+            )
 
             # Set the return value of _get_read_credential_infos to the fake_credential object
-            cloud_artifact_instance._get_read_credential_infos.return_value = [fake_credential]
+            cloud_artifact_instance._get_read_credential_infos.return_value = [
+                fake_credential
+            ]
 
             # Wrap fake_chunk_1 in a Future
             future = Future()
@@ -74,22 +92,36 @@ def test__parallelized_download_from_cloud(monkeypatch, future_result, expected_
 
             futures = {future: fake_chunk_1}
 
-            with mock.patch("mlflow.store.artifact.cloud_artifact_repo.as_completed", return_value=futures):
+            with mock.patch(
+                "mlflow.store.artifact.cloud_artifact_repo.as_completed",
+                return_value=futures,
+            ):
                 # Mocks
-                cloud_artifact_instance._get_read_credential_infos.return_value = [fake_credential]
+                cloud_artifact_instance._get_read_credential_infos.return_value = [
+                    fake_credential
+                ]
                 cloud_artifact_instance._get_uri_for_path.return_value = "fake_uri_path"
 
                 cloud_artifact_instance.chunk_thread_pool.submit.return_value = future
 
-                #  Method Call
+                # Create a fake local path using tmp_path
+                fake_local_path = tmp_path / "downloaded_file"
+
                 if future_result:
                     with pytest.raises(MlflowException):
-                        cloud_artifact_instance._parallelized_download_from_cloud(1, "fake_remote_path", "fake_local_path")
+                        cloud_artifact_instance._parallelized_download_from_cloud(
+                            1, "fake_remote_path", str(fake_local_path)
+                        )
                 else:
-                    cloud_artifact_instance._parallelized_download_from_cloud(1, "fake_remote_path", "fake_local_path")
+                    cloud_artifact_instance._parallelized_download_from_cloud(
+                        1, "fake_remote_path", str(fake_local_path)
+                    )
 
-                #Assert
-                assert cloud_artifact_instance._get_read_credential_infos.call_count == expected_call_count
+                # Assert
+                assert (
+                    cloud_artifact_instance._get_read_credential_infos.call_count
+                    == expected_call_count
+                )
 
                 for call in cloud_artifact_instance.chunk_thread_pool.submit.call_args_list:
                     assert call == mock.call(
@@ -97,6 +129,6 @@ def test__parallelized_download_from_cloud(monkeypatch, future_result, expected_
                         range_start=fake_chunk_1.start,
                         range_end=fake_chunk_1.end,
                         headers=mock.ANY,
-                        download_path="fake_local_path",
+                        download_path=str(fake_local_path),
                         http_uri="fake_signed_uri",
                     )
