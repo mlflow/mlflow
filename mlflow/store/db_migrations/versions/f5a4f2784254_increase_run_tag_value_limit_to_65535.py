7c4a355d98710e77da04a1988d35c5e365a161c2
Weichen Xu <weichen.xu@databricks.com>
2024-09-18 08:59:54 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/store/db_migrations/versions/f5a4f2784254_increase_run_tag_value_limit_to_65535.py
+++ mlflow/store/db_migrations/versions/f5a4f2784254_increase_run_tag_value_limit_to_65535.py
@@ -17,7 +17,19 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    # Use batch mode so that we can run "ALTER TABLE" statements against SQLite
+    # databases (see more info at https://alembic.sqlalchemy.org/en/latest/
+    # batch.html#running-batch-migrations-for-sqlite-and-other-databases)
+    # We specify existing_type, existing_nullable, existing_server_default
+    # because MySQL alter column statements require a full column description.
+    with op.batch_alter_table("tags") as batch_op:
+        batch_op.alter_column(
+            "value",
+            existing_type=sa.String(5000),
+            type_=sa.String(65535),
+            existing_nullable=True,
+            existing_server_default=None,
+        )
 
 
 def downgrade():
