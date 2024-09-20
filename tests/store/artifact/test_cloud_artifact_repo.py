44956dc3d7bfa82c9096acc2127ef559ccdb3b9c
rohitarun-db <rohit.arunachalam@databricks.com>
2024-09-20 01:05:07 -0700

    fix lint

--- tests/store/artifact/test_cloud_artifact_repo.py
+++ tests/store/artifact/test_cloud_artifact_repo.py
@@ -26,10 +26,11 @@ def test_chunk_size_validation_failure():
         _validate_chunk_size_aws(5 * 1024**3 + 1)
 
 @pytest.mark.parametrize(
-    ("future_result, expected_call_count"),
+    ("future_result", "expected_call_count"),
     [
         (None, 2),  # Simulate where creds are expired, but successfully download after refresh
-        (Exception("fake_exception"), 4),  # Simulate where there is a download failure and retries are exhausted
+        (Exception("fake_exception"), 4),  
+        # Simulate where there is a download failure and retries are exhausted
     ],
 )
 def test__parallelized_download_from_cloud(
@@ -109,7 +110,7 @@ def test__parallelized_download_from_cloud(
         ):
 
             if future_result:
-                with pytest.raises(MlflowException):
+                with pytest.raises(MlflowException, match="All retries have been exhausted. Download has failed."):
                     cloud_artifact_instance._parallelized_download_from_cloud(
                         1, "fake_remote_path", str(fake_local_path)
                     )
