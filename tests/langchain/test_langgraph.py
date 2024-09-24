f4b3a135ce9ad3b82e02d06a1a674c564300fd3d
Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
2024-09-24 12:38:56 +0900

    Fix tracing for LangGraph (#13215)
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- tests/langchain/test_langgraph.py
+++ tests/langchain/test_langgraph.py
@@ -6,6 +6,8 @@ from packaging.version import Version
 
 import mlflow
 
+from tests.tracing.helper import get_traces
+
 
 @pytest.mark.skipif(
     Version(langchain.__version__) < Version("0.2.0"),
@@ -56,3 +58,47 @@ def test_langgraph_save_as_code():
     response = loaded_pyfunc.predict_stream(input_example)
     for chunk, (role, expected_content) in zip(response, expected_messages[1:]):
         assert chunk[role]["messages"][0]["content"] == expected_content
+
+
+@pytest.mark.skipif(
+    Version(langchain.__version__) < Version("0.2.0"),
+    reason="Agent behavior is not stable across minor versions",
+)
+def test_langgraph_tracing():
+    mlflow.langchain.autolog()
+
+    input_example = {"messages": [{"role": "user", "content": "what is the weather in sf?"}]}
+
+    with mlflow.start_run():
+        model_info = mlflow.langchain.log_model(
+            lc_model="tests/langchain/sample_code/langgraph.py",
+            artifact_path="langgraph",
+            input_example=input_example,
+        )
+
+    loaded_graph = mlflow.langchain.load_model(model_info.model_uri)
+
+    # No trace should be created for the first call
+    assert mlflow.get_last_active_trace() is None
+
+    loaded_graph.invoke(input_example)
+
+    traces = get_traces()
+    assert len(traces) == 1
+    assert traces[0].info.status == "OK"
+    assert traces[0].data.spans[0].name == "LangGraph"
+    assert traces[0].data.spans[0].inputs == input_example
+
+    # (type, content)
+    expected_messages = [
+        ("human", "what is the weather in sf?"),
+        ("ai", ""),  # tool message does not have content
+        ("tool", "It's always sunny in sf"),
+        ("ai", "The weather in San Francisco is always sunny!"),
+    ]
+
+    messages = traces[0].data.spans[0].outputs["messages"]
+    assert len(messages) == 4
+    for msg, (type, expected_content) in zip(messages, expected_messages):
+        assert msg["type"] == type
+        assert msg["content"] == expected_content
