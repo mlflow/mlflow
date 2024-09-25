0136894370cd82c2311471af722caa7a0fb05658
harupy <17039389+harupy@users.noreply.github.com>
2024-09-25 14:54:11 +0900

    comment
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- examples/mlflow-3/sklearn_autolog.py
+++ examples/mlflow-3/sklearn_autolog.py
@@ -17,13 +17,28 @@ mlflow.sklearn.autolog()
 X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
+# LogisticRegression
 model = LogisticRegression()
 with mlflow.start_run() as run:
     model.fit(X_train, y_train)
 
+outputs = mlflow.get_run(run.info.run_id).outputs
+print(outputs)
+model_id = outputs.model_outputs[0].model_id
+print(model_id)
+model = mlflow.get_logged_model(model_id)
+print(model)
 
+# GridSearchCV
 model = LogisticRegression()
 params = {"C": [0.1, 1.0, 10.0], "max_iter": [100, 200]}
 clf = GridSearchCV(model, params)
 with mlflow.start_run() as run:
     clf.fit(X_train, y_train)
+
+outputs = mlflow.get_run(run.info.run_id).outputs
+print(outputs)
+model_id = outputs.model_outputs[0].model_id
+print(model_id)
+model = mlflow.get_logged_model(model_id)
+print(model)
