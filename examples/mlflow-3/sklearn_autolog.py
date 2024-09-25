38d36b20c9ffd3e8419b1ac3f34dbd854062417c
harupy <17039389+harupy@users.noreply.github.com>
2024-09-25 14:51:18 +0900

    Improve example
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- examples/mlflow-3/sklearn_autolog.py
+++ examples/mlflow-3/sklearn_autolog.py
@@ -1,15 +1,13 @@
-# ruff: noqa
 """
-python examples/sklearn_autolog.py
+python examples/mlflow-3/sklearn_autolog.py
 """
 
-import logging
 import os
-import tempfile
 
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split
+
 import mlflow
 
 os.environ["MLFLOW_AUTOLOGGING_TESTING"] = "true"
@@ -20,5 +18,12 @@ X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
 model = LogisticRegression()
-with mlflow.start_run():
+with mlflow.start_run() as run:
     model.fit(X_train, y_train)
+
+
+model = LogisticRegression()
+params = {"C": [0.1, 1.0, 10.0], "max_iter": [100, 200]}
+clf = GridSearchCV(model, params)
+with mlflow.start_run() as run:
+    clf.fit(X_train, y_train)
