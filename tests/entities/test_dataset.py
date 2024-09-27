e6062545790fc5d912f6ec47a17b4d6f11489c69
Florian Verdonck <florian.verdonck@outlook.com>
2024-09-27 21:38:15 +0200

    Use None for schema and profile when fields are absent in proto (#13233)
    Signed-off-by: nojaf <florian.verdonck@outlook.com>

--- tests/entities/test_dataset.py
+++ tests/entities/test_dataset.py
@@ -37,3 +37,29 @@ def test_creation_and_hydration():
 
     dataset3 = Dataset.from_dictionary(as_dict)
     _check(dataset3, name, digest, source_type, source, schema, profile)
+
+
+def test_absent_fields():
+    name = "my_name"
+    digest = "my_digest"
+    source_type = "my_source_type"
+    source = "my_source"
+    dataset = Dataset(name, digest, source_type, source)
+    _check(dataset, name, digest, source_type, source)
+
+    as_dict = {
+        "name": name,
+        "digest": digest,
+        "source_type": source_type,
+        "source": source,
+        "profile": None,
+        "schema": None,
+    }
+    assert dict(dataset) == as_dict
+
+    proto = dataset.to_proto()
+    dataset2 = Dataset.from_proto(proto)
+    _check(dataset2, name, digest, source_type, source)
+
+    dataset3 = Dataset.from_dictionary(as_dict)
+    _check(dataset3, name, digest, source_type, source)
