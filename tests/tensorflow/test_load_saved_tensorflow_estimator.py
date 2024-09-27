4e16555cdf2614ecc217f7a6b654fce6c8388474
Daniel Lok <daniel.lok@databricks.com>
2024-09-27 15:34:54 +0800

    fix
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- tests/tensorflow/test_load_saved_tensorflow_estimator.py
+++ tests/tensorflow/test_load_saved_tensorflow_estimator.py
@@ -55,7 +55,7 @@ def save_or_log_tf_model_by_mlflow128(tmp_path, model_type, task_type, save_path
     exec_py_path = os.path.join(tf_tests_dir, "save_tf_estimator_model.py")
     mlflow_repo_path = os.path.dirname(os.path.dirname(tf_tests_dir))
 
-    conda_env.execute("pip show setuptools >&2")
+    conda_env.execute("pip install setuptools==74.1.3")
 
     conda_env.execute(
         f"python {exec_py_path} "
