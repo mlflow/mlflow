938950b8c62eb1850732207aa790465583847932
Daniel Lok <daniel.lok@databricks.com>
2024-10-01 08:58:52 +0800

    remove
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/ml_package_versions.py
+++ mlflow/ml_package_versions.py
@@ -140,7 +140,7 @@ _ML_PACKAGE_VERSIONS = {
         },
         "models": {
             "minimum": "2.2.4",
-            "maximum": "3.7.6"
+            "maximum": "3.7.5"
         }
     },
     "statsmodels": {
