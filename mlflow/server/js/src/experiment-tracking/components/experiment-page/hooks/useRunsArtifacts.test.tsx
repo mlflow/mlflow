6c541993fbba9a7531aa9540d82120a9c5aa30b8
Juan Escalada <97265671+jescalada@users.noreply.github.com>
2024-10-02 12:54:31 +0900

    Add Artifact support for Compare Run page (#13145)
    Signed-off-by: Juan Escalada <juanescalada175@gmail.com>
    Signed-off-by: Juan Escalada <97265671+jescalada@users.noreply.github.com>
    Co-authored-by: Daniel Lok <daniel.lok@databricks.com>

---
+++ mlflow/server/js/src/experiment-tracking/components/experiment-page/hooks/useRunsArtifacts.test.tsx
@@ -0,0 +1,73 @@
+import { listArtifactsApi } from '../../../actions';
+import { useRunsArtifacts } from './useRunsArtifacts';
+import { ArtifactListFilesResponse } from '../../../types';
+import { renderHook, cleanup, waitFor } from '@testing-library/react';
+
+const mockArtifactsData: Record<string, ArtifactListFilesResponse> = {
+  'run-1': {
+    root_uri: 'run-1',
+    files: [
+      {
+        path: 'artifact1.txt',
+        is_dir: false,
+        file_size: 300,
+      },
+    ],
+  },
+  'run-2': {
+    root_uri: 'run-2',
+    files: [
+      {
+        path: 'artifact2.txt',
+        is_dir: false,
+        file_size: 300,
+      },
+    ],
+  },
+};
+
+jest.mock('../../../actions', () => ({
+  ...jest.requireActual('../../../actions'),
+  listArtifactsApi: jest.fn((runUuid: string) => {
+    return {
+      payload: mockArtifactsData[runUuid],
+    };
+  }),
+}));
+
+describe('useRunsArtifacts', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+    cleanup();
+  });
+
+  test('fetches artifacts for given run UUIDs', async () => {
+    const runUuids = ['run-1', 'run-2'];
+    const { result } = renderHook(() => useRunsArtifacts(runUuids));
+
+    await waitFor(() => {
+      expect(result.current.isLoading).toBe(false);
+    });
+
+    // Make sure API is called correctly
+    expect(listArtifactsApi).toHaveBeenCalledWith('run-1');
+    expect(listArtifactsApi).toHaveBeenCalledWith('run-2');
+    expect(listArtifactsApi).toHaveBeenCalledTimes(2);
+
+    expect(result.current.artifactsKeyedByRun).toEqual(mockArtifactsData);
+  });
+
+  test('returns empty object when no run UUIDs are provided', () => {
+    const runUuids: string[] = [];
+    const { result } = renderHook(() => useRunsArtifacts(runUuids));
+
+    expect(result.current.artifactsKeyedByRun).toEqual({});
+  });
+
+  test('returns empty object when no artifacts are found', () => {
+    const runUuids = ['run-3'];
+    const { result } = renderHook(() => useRunsArtifacts(runUuids));
+
+    expect(result.current.artifactsKeyedByRun).toEqual({});
+  });
+});
