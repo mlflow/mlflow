f94e37d059681853904d2895a141f42bc41e71ba
Weichen Xu <weichen.xu@databricks.com>
2024-10-10 10:11:08 +0800

    init
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/utils/rest_utils.py
+++ mlflow/utils/rest_utils.py
@@ -108,12 +108,23 @@ def http_request(
             # Databricks SDK `APIClient.do` API is for making request using
             # HTTP
             # https://github.com/databricks/databricks-sdk-py/blob/a714146d9c155dd1e3567475be78623f72028ee0/databricks/sdk/core.py#L134
+
+            url_params = kwargs.get("params")
+            """"
+            if url_params is not None and not isinstance(url_params, dict):
+                raise RuntimeError(
+                    f"http_request got unexpected params: {str(url_params)}, type: {type(url_params)}"
+                )
+            """
+            if isinstance(url_params, str):
+                url_params = json.loads(url_params)
+
             raw_response = ws_client.api_client.do(
                 method=method,
                 path=endpoint,
                 headers=extra_headers,
                 raw=True,
-                query=kwargs.get("params"),
+                query=url_params,
                 body=kwargs.get("json"),
                 files=kwargs.get("files"),
                 data=kwargs.get("data"),
