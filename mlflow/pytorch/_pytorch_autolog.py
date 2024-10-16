724f698b7b9abf5ded7500e1f23d5dabece10b5d
Weichen Xu <weichen.xu@databricks.com>
2024-10-16 20:57:49 +0800

    update pytorch
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/pytorch/_pytorch_autolog.py
+++ mlflow/pytorch/_pytorch_autolog.py
@@ -9,26 +9,13 @@ from mlflow.utils.autologging_utils.metrics_queue import (
     flush_metrics_queue,
 )
 
-DISABLED = False
-
-
-@contextmanager
-def disable_pytorch_autologging():
-    global DISABLED
-    old_value = DISABLED
-    DISABLED = True
-    try:
-        yield
-    finally:
-        DISABLED = old_value
-
 
 def patched_add_hparams(original, self, hparam_dict, metric_dict, *args, **kwargs):
     """use a synchronous call here since this is going to get called very infrequently."""
 
     run = mlflow.active_run()
 
-    if not DISABLED and run is not None and hparam_dict:
+    if run is not None and hparam_dict:
         run_id = run.info.run_id
         # str() is required by mlflow :(
         params_arr = [Param(key, str(value)) for key, value in hparam_dict.items()]
@@ -43,8 +30,7 @@ def patched_add_hparams(original, self, hparam_dict, metric_dict, *args, **kwarg
 def patched_add_event(original, self, event, *args, mlflow_log_every_n_step, **kwargs):
     run = mlflow.active_run()
     if (
-        not DISABLED
-        and run is not None
+        run is not None
         and event.WhichOneof("what") == "summary"
         and mlflow_log_every_n_step
     ):
