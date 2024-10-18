39c3e3f4be8bf89bc295d3432da007cacc92e401
Daniel Lok <daniel.lok@databricks.com>
2024-10-18 13:25:59 +0800

    env var
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://mlflow.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: process.env.CIRCLECI_BASE_URL || "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
