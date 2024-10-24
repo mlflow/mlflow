892a1a5d17fa716cfad9c7805467b0f43049ccbb
WeichenXu <weichen.xu@databricks.com>
2024-10-24 23:14:46 +0800

    [FOLLOW-UP] Simplify Spark udf serverless API and add doc section (#13496)
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- examples/spark_udf/spark_udf_with_prebuilt_env.py
+++ examples/spark_udf/spark_udf_with_prebuilt_env.py
@@ -4,7 +4,6 @@ outside Databricks runtime.
 """
 
 import os
-import tempfile
 
 from databricks.connect import DatabricksSession
 from databricks.sdk import WorkspaceClient
@@ -33,21 +32,13 @@ model_uri = model_info.model_uri
 # The prebuilt model environment archive file path.
 # To build the model environment, run the following line code in Databricks runtime:
 # `model_env_uc_path = mlflow.pyfunc.build_model_env(model_uri, "/Volumes/...")`
-model_env_uc_path = "/Volumes/..."
-
-tmp_dir = tempfile.mkdtemp()
-local_model_env_path = os.path.join(tmp_dir, os.path.basename(model_env_uc_path))
-
-# Download model env file from UC volume.
-with ws.files.download(model_env_uc_path).contents as rf, open(local_model_env_path, "wb") as wf:
-    while chunk := rf.read(4096):
-        wf.write(chunk)
+model_env_uc_path = "dbfs:/Volumes/..."
 
 infer_spark_df = spark.createDataFrame(X)
 
-# Setting 'prebuilt_env_path' parameter so that `spark_udf` can use the
+# Setting 'prebuilt_env_uri' parameter so that `spark_udf` can use the
 # prebuilt python environment and skip rebuilding python environment.
-pyfunc_udf = mlflow.pyfunc.spark_udf(spark, model_uri, prebuilt_env_path=local_model_env_path)
+pyfunc_udf = mlflow.pyfunc.spark_udf(spark, model_uri, prebuilt_env_uri=model_env_uc_path)
 result = infer_spark_df.select(pyfunc_udf(*X.columns).alias("predictions")).toPandas()
 
 print(result)
