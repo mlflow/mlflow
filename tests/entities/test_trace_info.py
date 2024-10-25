dd31a10bf6633c97552cd5eef3a4b8e2ea96e318
Software Developer <7852635+dsuhinin@users.noreply.github.com>
2024-10-25 18:07:30 +0200

    *fixing unit tests.
    Signed-off-by: dsuhinin <suhinin.dmitriy@gmail.com>
    Signed-off-by: Software Developer <7852635+dsuhinin@users.noreply.github.com>

--- tests/entities/test_trace_info.py
+++ tests/entities/test_trace_info.py
@@ -166,8 +166,8 @@ def test_absent_fields():
         timestamp_ms,
         execution_time_ms,
         status,
-        request_metadata=None,
-        tags=None,
+        request_metadata={},
+        tags={},
     )
 
     as_dict = {
