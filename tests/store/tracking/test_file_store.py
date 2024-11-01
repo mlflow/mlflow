c1a74c642c8d2960d57cdf0ab180a75472879046
Harutaka Kawamura <hkawamura0130@gmail.com>
2024-11-01 12:14:56 +0900

    Remove `mlflow/utils/insecure_hash.py` (#13626)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- tests/store/tracking/test_file_store.py
+++ tests/store/tracking/test_file_store.py
@@ -1,3 +1,4 @@
+import hashlib
 import json
 import os
 import posixpath
@@ -43,7 +44,6 @@ from mlflow.store.tracking import SEARCH_MAX_RESULTS_DEFAULT
 from mlflow.store.tracking.file_store import FileStore
 from mlflow.tracing.constant import TraceMetadataKey, TraceTagKey
 from mlflow.tracking._tracking_service.utils import _use_tracking_uri
-from mlflow.utils import insecure_hash
 from mlflow.utils.file_utils import (
     TempDir,
     path_to_local_file_uri,
@@ -2659,7 +2659,7 @@ def test_log_inputs_uses_expected_input_and_dataset_ids_for_storage(store):
         inputs_dir = os.path.join(run_dir, FileStore.INPUTS_FOLDER_NAME)
         expected_input_storage_ids = []
         for dataset_storage_id in dataset_storage_ids:
-            md5 = insecure_hash.md5(dataset_storage_id.encode("utf-8"))
+            md5 = hashlib.md5(dataset_storage_id.encode("utf-8"), usedforsecurity=False)
             md5.update(run.info.run_id.encode("utf-8"))
             expected_input_storage_ids.append(md5.hexdigest())
         assert set(os.listdir(inputs_dir)) == set(expected_input_storage_ids)
