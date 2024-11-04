5d67098b55ce71119ab7e83c648453dd3f9edc11
Weichen Xu <weichen.xu@databricks.com>
2024-11-04 12:04:36 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/utils/databricks_utils.py
+++ mlflow/utils/databricks_utils.py
@@ -283,14 +283,14 @@ def is_databricks_connect(spark=None):
     """
     from mlflow.utils.spark_utils import is_spark_connect_mode
 
-    if not is_spark_connect_mode():
-        return False
-
     if is_in_databricks_serverless_runtime() or is_in_databricks_shared_cluster_runtime():
         return True
 
     if spark is None:
         spark = _get_active_spark_session()
+
+    if not is_spark_connect_mode():
+        return False
 
     if hasattr(spark.client, "metadata"):
         metadata = spark.client.metadata
@@ -358,6 +358,7 @@ def is_databricks_serverless(spark):
     on Databricks Connect client that connects to Databricks Serverless.
     """
     from mlflow.utils.spark_utils import is_spark_connect_mode
+
     if not is_spark_connect_mode():
         return False
 
