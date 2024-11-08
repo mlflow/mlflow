f3cc9f6307c9f6c0d19faf4ab7d80a305890cbb6
Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
2024-11-08 12:59:49 +0900

    Avoid global import of fastapi in Gateway Adapters. (#13721)
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- mlflow/gateway/providers/base.py
+++ mlflow/gateway/providers/base.py
@@ -1,10 +1,9 @@
 from abc import ABC, abstractmethod
 from typing import AsyncIterable
 
-from fastapi import HTTPException
-
 from mlflow.gateway.base_models import ConfigModel
 from mlflow.gateway.config import RouteConfig
+from mlflow.gateway.exceptions import AIGatewayException
 from mlflow.gateway.schemas import chat, completions, embeddings
 from mlflow.utils.annotations import developer_stable
 
@@ -37,13 +36,13 @@ class BaseProvider(ABC):
     async def chat_stream(
         self, payload: chat.RequestPayload
     ) -> AsyncIterable[chat.StreamResponsePayload]:
-        raise HTTPException(
+        raise AIGatewayException(
             status_code=501,
             detail=f"The chat streaming route is not implemented for {self.NAME} models.",
         )
 
     async def chat(self, payload: chat.RequestPayload) -> chat.ResponsePayload:
-        raise HTTPException(
+        raise AIGatewayException(
             status_code=501,
             detail=f"The chat route is not implemented for {self.NAME} models.",
         )
@@ -51,29 +50,27 @@ class BaseProvider(ABC):
     async def completions_stream(
         self, payload: completions.RequestPayload
     ) -> AsyncIterable[completions.StreamResponsePayload]:
-        raise HTTPException(
+        raise AIGatewayException(
             status_code=501,
             detail=f"The completions streaming route is not implemented for {self.NAME} models.",
         )
 
     async def completions(self, payload: completions.RequestPayload) -> completions.ResponsePayload:
-        raise HTTPException(
+        raise AIGatewayException(
             status_code=501,
             detail=f"The completions route is not implemented for {self.NAME} models.",
         )
 
     async def embeddings(self, payload: embeddings.RequestPayload) -> embeddings.ResponsePayload:
-        raise HTTPException(
+        raise AIGatewayException(
             status_code=501,
             detail=f"The embeddings route is not implemented for {self.NAME} models.",
         )
 
     @staticmethod
     def check_for_model_field(payload):
-        from fastapi import HTTPException
-
         if "model" in payload:
-            raise HTTPException(
+            raise AIGatewayException(
                 status_code=422,
                 detail="The parameter 'model' is not permitted to be passed. The route being "
                 "queried already defines a model instance.",
@@ -131,6 +128,6 @@ class ProviderAdapter(ABC):
     def check_keys_against_mapping(cls, mapping, payload):
         for k1, k2 in mapping.items():
             if k2 in payload:
-                raise HTTPException(
+                raise AIGatewayException(
                     status_code=400, detail=f"Invalid parameter {k2}. Use {k1} instead."
                 )
