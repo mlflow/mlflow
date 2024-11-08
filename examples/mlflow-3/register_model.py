4d5385321cd732e6193b4f5e21c1284bed220906
Harutaka Kawamura <hkawamura0130@gmail.com>
2024-11-08 11:41:46 +0900

    Apply suggestions from code review
    Co-authored-by: Corey Zumar <39497902+dbczumar@users.noreply.github.com>
    Signed-off-by: Harutaka Kawamura <hkawamura0130@gmail.com>

--- examples/mlflow-3/register_model.py
+++ examples/mlflow-3/register_model.py
@@ -10,10 +10,10 @@ with mlflow.start_run():
 
 mlflow.register_model(model_info.model_uri, name="model")
 m = mlflow.get_logged_model(model_info.model_id)
-assert len(json.loads(m.tags["mlflow.registeredModels"])) == 1
+assert len(json.loads(m.tags["mlflow.modelVersions"])) == 1
 print(m.tags)
 
 mlflow.register_model(model_info.model_uri, name="hello")
 m = mlflow.get_logged_model(model_info.model_id)
-assert len(json.loads(m.tags["mlflow.registeredModels"])) == 2
+assert len(json.loads(m.tags["mlflow.modelVersions"])) == 2
 print(m.tags)
