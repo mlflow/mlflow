22ae87526b3bf19c5ecf9e4231253b7dd71dd479
Ann Zhang <ann.zhang@databricks.com>
2024-11-08 14:46:08 -0800

    update utils
    Signed-off-by: Ann Zhang <ann.zhang@databricks.com>

--- mlflow/data/spark_delta_utils.py
+++ mlflow/data/spark_delta_utils.py
@@ -85,8 +85,9 @@ def _try_get_delta_table_latest_version_from_table_name(table_name: str) -> Opti
 
     try:
         spark = SparkSession.builder.getOrCreate()
+        backticked_table_name = ".".join(f"`{part}`" for part in table_name.split("."))
         j_delta_table = spark._jvm.io.delta.tables.DeltaTable.forName(
-            spark._jsparkSession, table_name
+            spark._jsparkSession, backticked_table_name
         )
         return _get_delta_table_latest_version(j_delta_table)
     except Exception as e:
