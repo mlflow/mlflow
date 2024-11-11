1da55509d88cba59c94f8a3e9930db013082a842
mlflow-automation <mlflow-automation@users.noreply.github.com>
2024-11-11 19:48:04 +0000

    Autoformat: https://github.com/mlflow/mlflow/actions/runs/11785174301
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>

--- mlflow/langchain/__init__.py
+++ mlflow/langchain/__init__.py
@@ -50,7 +50,7 @@ from mlflow.models.dependencies_schemas import (
     _get_dependencies_schemas,
 )
 from mlflow.models.model import MLMODEL_FILE_NAME, MODEL_CODE_PATH, MODEL_CONFIG
-from mlflow.models.resources import DatabricksFunction, _ResourceBuilder, Resource
+from mlflow.models.resources import DatabricksFunction, Resource, _ResourceBuilder
 from mlflow.models.signature import _infer_signature_from_input_example
 from mlflow.models.utils import (
     _convert_llm_input_data,
@@ -364,13 +364,15 @@ def save_model(
     needs_databricks_auth = False
     if Version(langchain.__version__) >= Version("0.0.311") and mlflow_model.resources is None:
         if databricks_resources := _detect_databricks_dependencies(lc_model):
-            logger.info("Attempting to auto-detect Databricks resource dependencies for the "
-                        "current langchain model. Dependency auto-detection is "
-                        "best-effort and may not capture all dependencies of your langchain "
-                        "model, resulting in authorization errors when serving or querying "
-                        "your model. We recommend that you explicitly pass `resources` "
-                        "to mlflow.langchain.log_model() to ensure authorization to "
-                        "dependent resources succeeds when the model is deployed.")
+            logger.info(
+                "Attempting to auto-detect Databricks resource dependencies for the "
+                "current langchain model. Dependency auto-detection is "
+                "best-effort and may not capture all dependencies of your langchain "
+                "model, resulting in authorization errors when serving or querying "
+                "your model. We recommend that you explicitly pass `resources` "
+                "to mlflow.langchain.log_model() to ensure authorization to "
+                "dependent resources succeeds when the model is deployed."
+            )
             serialized_databricks_resources = _ResourceBuilder.from_resources(databricks_resources)
             mlflow_model.resources = serialized_databricks_resources
             needs_databricks_auth = any(
@@ -441,7 +443,7 @@ def log_model(
     run_id=None,
     model_config=None,
     streamable=None,
-    resources: Optional[Union[list[Resource], str]]=None,
+    resources: Optional[Union[list[Resource], str]] = None,
 ):
     """
     Log a LangChain model as an MLflow artifact for the current run.
