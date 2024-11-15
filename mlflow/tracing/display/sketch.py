907ba6c526b7a24dae6f8381817a558667d64161
Daniel Lok <daniel.lok@databricks.com>
2024-11-15 17:11:44 +0800

    fix
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/tracing/display/sketch.py
+++ mlflow/tracing/display/sketch.py
@@ -1,7 +1,7 @@
 sketch = """
 <html>
   <div>
-    <iframe id="trace-renderer" style="width: 100%; height: 500px;" src="http://localhost:3000/static-files/lib/ml-model-trace-renderer/index.html?traceId={trace_id}" />
+    <iframe id="trace-renderer" style="width: 100%; height: 500px;" src="http://localhost:3000/static-files/lib/ml-model-trace-renderer/index.html?requestId={trace_id}" />
   </div>
 </html>
 """
