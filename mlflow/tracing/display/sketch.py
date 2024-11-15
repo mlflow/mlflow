f920898d2145dc97a50003202ea849a6aa79726d
Daniel Lok <daniel.lok@databricks.com>
2024-11-15 17:06:34 +0800

    test
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/tracing/display/sketch.py
+++ mlflow/tracing/display/sketch.py
@@ -1,20 +1,7 @@
 sketch = """
 <html>
-  <head>
-    <script>
-      var traceData = JSON.parse({traces});
-      window.addEventListener('message', (event) => {{
-        if (event.data.type === 'READY') {{
-          document.getElementById('trace-renderer').contentWindow.postMessage({{ 
-            type: 'UPDATE_TRACE', 
-            traceData: traceData, 
-          }}, "*");
-        }}
-      }});
-    </script>
-  </head>
   <div>
-    <iframe id="trace-renderer" style="width: 100%; height: 500px;" src="http://localhost:3000/static-files/lib/ml-model-trace-renderer/index.html" />
+    <iframe id="trace-renderer" style="width: 100%; height: 500px;" src="http://localhost:3000/static-files/lib/ml-model-trace-renderer/index.html?traceId={trace_id}" />
   </div>
 </html>
 """
