c3b5ad8544d9bab3866d84a0dfc44091a2ca100a
B-Step62 <yuki.watanabe@databricks.com>
2024-11-27 13:07:57 +0900

    llama_index test
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- tests/llama_index/test_llama_index_evaluate.py
+++ tests/llama_index/test_llama_index_evaluate.py
@@ -3,29 +3,71 @@ import pytest
 
 import mlflow
 from mlflow.metrics import latency
+from mlflow.tracing.constant import TraceMetadataKey
+
+import mlflow.utils
+import mlflow.utils.autologging_utils
+from tests.tracing.helper import get_traces
+
+
+_EVAL_DATA = pd.DataFrame(
+    {
+        "inputs": [
+            "What is MLflow?",
+            "What is Spark?",
+        ],
+        "ground_truth": ["What is MLflow?", "Not what is Spark?"],
+    }
+)
+
+
+@pytest.fixture(autouse=True)
+def reset_autolog_state():
+    # Autologging state is global, so we need to reset it between tests
+    mlflow.llama_index.autolog(disable=True)
+    # Reset the fact that it is 'disabled'
+    del mlflow.utils.autologging_utils.AUTOLOGGING_INTEGRATIONS["llama_index"]
 
 
 @pytest.mark.parametrize(
-    "data",
-    [
-        # Single row
-        pd.DataFrame(
-            {
-                "inputs": ["What is MLflow?"],
-                "ground_truth": ["What is MLflow?"],
-            }
-        ),
-        # Multiple rows
-        pd.DataFrame(
-            {
-                "inputs": ["What is MLflow?", "What is Spark?"],
-                "ground_truth": ["What is MLflow?", "Not what is Spark?"],
-            }
-        ),
-    ],
+    "original_autolog_config", [
+        None,
+        {"log_traces": False},
+        {"log_traces": True},
+    ]
 )
+def test_llama_index_evaluate(single_index, original_autolog_config):
+    if original_autolog_config:
+        mlflow.llama_index.autolog(**original_autolog_config)
+
+    engine = single_index.as_query_engine()
+
+    def model(inputs):
+        return [engine.query(question) for question in inputs["inputs"]]
+
+    with mlflow.start_run() as run:
+        eval_result = mlflow.evaluate(
+            model,
+            data=_EVAL_DATA,
+            targets="ground_truth",
+            extra_metrics=[latency()],
+        )
+    assert eval_result.metrics["latency/mean"] > 0
+
+    # Traces should be automatically enabled during evaluation
+    assert len(get_traces()) == 2
+    assert run.info.run_id == get_traces()[0].info.request_metadata[TraceMetadataKey.SOURCE_RUN]
+
+    # Test original autolog configs is restored
+    engine.query("text")
+    if original_autolog_config and original_autolog_config.get("log_traces", True):
+        assert len(get_traces()) == 3
+    else:
+        assert len(get_traces()) == 2
+
+
 @pytest.mark.parametrize("engine_type", ["query", "chat"])
-def test_llama_index_evaluate(data, engine_type, single_index):
+def test_llama_index_pyfunc_evaluate(engine_type, single_index):
     with mlflow.start_run():
         model_info = mlflow.llama_index.log_model(
             single_index,
@@ -34,12 +76,35 @@ def test_llama_index_evaluate(data, engine_type, single_index):
         )
 
     pyfunc_model = mlflow.pyfunc.load_model(model_info.model_uri)
-    eval_dataset = mlflow.data.from_pandas(data, targets="ground_truth")
 
-    with mlflow.start_run():
+    with mlflow.start_run() as run:
         eval_result = mlflow.evaluate(
             pyfunc_model,
-            data=eval_dataset,
+            data=_EVAL_DATA,
+            targets="ground_truth",
             extra_metrics=[latency()],
         )
     assert eval_result.metrics["latency/mean"] > 0
+
+    # Traces should be automatically enabled during evaluation
+    assert len(get_traces()) == 2
+    assert run.info.run_id == get_traces()[0].info.request_metadata[TraceMetadataKey.SOURCE_RUN]
+
+
+
+def test_llama_index_evaluate_should_not_log_traces_when_disabled(single_index):
+    mlflow.llama_index.autolog(disable=True)
+
+    def model(inputs):
+        engine = single_index.as_query_engine()
+        return [engine.query(question) for question in inputs["inputs"]]
+
+    with mlflow.start_run() as run:
+        eval_result = mlflow.evaluate(
+            model,
+            data=_EVAL_DATA,
+            targets="ground_truth",
+            extra_metrics=[latency()],
+        )
+    assert eval_result.metrics["latency/mean"] > 0
+    assert len(get_traces()) == 0
