8be85a7c815652c73df31f1ca53655d719f5e5e3
harupy <17039389+harupy@users.noreply.github.com>
2024-11-28 20:51:24 +0900

    Log more git info
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/tracking/context/git_context.py
+++ mlflow/tracking/context/git_context.py
@@ -1,32 +1,22 @@
-import logging
+import functools
+from typing import Optional
 
 from mlflow.tracking.context.abstract_context import RunContextProvider
 from mlflow.tracking.context.default_context import _get_main_file
-from mlflow.utils.git_utils import get_git_commit
-from mlflow.utils.mlflow_tags import MLFLOW_GIT_COMMIT
-
-_logger = logging.getLogger(__name__)
-
-
-def _get_source_version():
-    main_file = _get_main_file()
-    if main_file is not None:
-        return get_git_commit(main_file)
-    return None
+from mlflow.utils.git_utils import GitInfo
 
 
 class GitRunContext(RunContextProvider):
-    def __init__(self):
-        self._cache = {}
-
-    @property
-    def _source_version(self):
-        if "source_version" not in self._cache:
-            self._cache["source_version"] = _get_source_version()
-        return self._cache["source_version"]
+    @functools.cached_property
+    def _git_info(self) -> Optional[GitInfo]:
+        if main_file := _get_main_file():
+            return GitInfo.load(main_file)
+        return None
 
     def in_context(self):
-        return self._source_version is not None
+        return self._git_info is not None
 
-    def tags(self):
-        return {MLFLOW_GIT_COMMIT: self._source_version}
+    def tags(self) -> dict[str, str]:
+        if self._git_info:
+            return self._git_info.to_mlflow_tags()
+        return {}
--- mlflow/utils/git_utils.py
+++ mlflow/utils/git_utils.py
@@ -1,8 +1,57 @@
 import logging
-import os
+from dataclasses import dataclass
+from pathlib import Path
 from typing import Optional
 
+from mlflow.utils import mlflow_tags
+
 _logger = logging.getLogger(__name__)
+
+
+@dataclass
+class GitInfo:
+    commit: str
+    branch: str
+    url: Optional[str] = None
+
+    def to_mlflow_tags(self) -> dict[str, str]:
+        tags = {
+            mlflow_tags.MLFLOW_GIT_COMMIT: self.commit,
+            mlflow_tags.LEGACY_MLFLOW_GIT_BRANCH_NAME: self.branch,
+            mlflow_tags.MLFLOW_GIT_BRANCH: self.branch,
+        }
+        if self.url:
+            tags[mlflow_tags.MLFLOW_GIT_REPO_URL] = self.url
+            tags[mlflow_tags.LEGACY_MLFLOW_GIT_REPO_URL] = self.url
+        return tags
+
+    @classmethod
+    def load(cls, path: str) -> Optional["GitInfo"]:
+        try:
+            from git import Repo
+        except ImportError as e:
+            _logger.warning(
+                "Failed to import Git (the Git executable is probably not on your PATH),"
+                " so Git SHA is not available. Error: %s",
+                e,
+            )
+            return None
+
+        try:
+            path = Path(path)
+            if path.is_file():
+                path = path.parent
+            repo = Repo(path, search_parent_directories=True)
+            if str(path) in repo.ignored(path):
+                return None
+
+            return cls(
+                commit=repo.head.commit.hexsha,
+                branch=repo.active_branch.name,
+                url=next((r.url for r in repo.remotes), None),
+            )
+        except Exception:
+            return None
 
 
 def get_git_repo_url(path: str) -> Optional[str]:
@@ -10,21 +59,7 @@ def get_git_repo_url(path: str) -> Optional[str]:
     Obtains the url of the git repository associated with the specified path,
     returning ``None`` if the path does not correspond to a git repository.
     """
-    try:
-        from git import Repo
-    except ImportError as e:
-        _logger.warning(
-            "Failed to import Git (the Git executable is probably not on your PATH),"
-            " so Git SHA is not available. Error: %s",
-            e,
-        )
-        return None
-
-    try:
-        repo = Repo(path, search_parent_directories=True)
-        return next((remote.url for remote in repo.remotes), None)
-    except Exception:
-        return None
+    return info.url if (info := GitInfo.load(path)) else None
 
 
 def get_git_commit(path: str) -> Optional[str]:
@@ -33,24 +68,7 @@ def get_git_commit(path: str) -> Optional[str]:
     with the specified path, returning ``None`` if the path does not correspond to a git
     repository.
     """
-    try:
-        from git import Repo
-    except ImportError as e:
-        _logger.warning(
-            "Failed to import Git (the Git executable is probably not on your PATH),"
-            " so Git SHA is not available. Error: %s",
-            e,
-        )
-        return None
-    try:
-        if os.path.isfile(path):
-            path = os.path.dirname(os.path.abspath(path))
-        repo = Repo(path, search_parent_directories=True)
-        if path in repo.ignored(path):
-            return None
-        return repo.head.commit.hexsha
-    except Exception:
-        return None
+    return info.commit if (info := GitInfo.load(path)) else None
 
 
 def get_git_branch(path: str) -> Optional[str]:
@@ -58,20 +76,4 @@ def get_git_branch(path: str) -> Optional[str]:
     Obtains the name of the current branch of the git repository associated with the specified
     path, returning ``None`` if the path does not correspond to a git repository.
     """
-    try:
-        from git import Repo
-    except ImportError as e:
-        _logger.warning(
-            "Failed to import Git (the Git executable is probably not on your PATH),"
-            " so Git SHA is not available. Error: %s",
-            e,
-        )
-        return None
-
-    try:
-        if os.path.isfile(path):
-            path = os.path.dirname(path)
-        repo = Repo(path, search_parent_directories=True)
-        return repo.active_branch.name
-    except Exception:
-        return None
+    return info.branch if (info := GitInfo.load(path)) else None
