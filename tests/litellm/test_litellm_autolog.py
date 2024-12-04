cc54e481716f6344a00e325a4e7e095e5cf8dd4c
Harutaka Kawamura <hkawamura0130@gmail.com>
2024-12-04 11:19:54 +0900

    Update tests/litellm/test_litellm_autolog.py
    Signed-off-by: Harutaka Kawamura <hkawamura0130@gmail.com>

--- tests/litellm/test_litellm_autolog.py
+++ tests/litellm/test_litellm_autolog.py
@@ -153,7 +153,6 @@ async def test_litellm_tracing_async(is_in_databricks):
     assert spans[0].attributes["usage"] is not None
 
 
-@pytest.mark.repeat(3)
 @pytest.mark.asyncio
 async def test_litellm_tracing_async_streaming(is_in_databricks):
     mlflow.litellm.autolog()
