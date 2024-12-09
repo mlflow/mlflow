bb1c368d56c504dd714645503f8ba4f1e4d40686
Serena Ruan <82044803+serena-ruan@users.noreply.github.com>
2024-12-09 19:36:42 +0800

    Support uv in mlflow.models.predict (#13824)
    Signed-off-by: serena-ruan <serena.rxy@gmail.com>

--- conftest.py
+++ conftest.py
@@ -385,7 +385,9 @@ def serve_wheel(request, tmp_path_factory):
             if existing_url := os.environ.get("PIP_EXTRA_INDEX_URL"):
                 url = f"{existing_url} {url}"
             os.environ["PIP_EXTRA_INDEX_URL"] = url
-
+            # Set the `UV_INDEX` environment variable to allow fetching the wheel from the
+            # url when using `uv` as environment manager
+            os.environ["UV_INDEX"] = f"mlflow={url}"
             yield
         finally:
             prc.terminate()
