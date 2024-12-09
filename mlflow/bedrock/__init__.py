bcfd557f6471f1e7470e227206b9cc79a12b4d06
B-Step62 <yuki.watanabe@databricks.com>
2024-12-09 22:20:48 +0900

    patch already created clients
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- mlflow/bedrock/__init__.py
+++ mlflow/bedrock/__init__.py
@@ -1,3 +1,6 @@
+import gc
+import inspect
+
 from mlflow.utils.annotations import experimental
 from mlflow.utils.autologging_utils import autologging_integration, safe_patch
 
@@ -24,12 +27,18 @@ def autolog(
     """
     from botocore.client import ClientCreator
 
-    from mlflow.bedrock._autolog import patched_create_client
+    from mlflow.bedrock._autolog import patch_bedrock_runtime_client, patched_create_client
 
     # NB: In boto3, the client class for each service is dynamically created at
     # runtime via the ClientCreator factory class. Therefore, we cannot patch
     # the service client directly, and instead patch the factory to return
     # a patched client class.
-    # TODO: This logic only works when the client is created *after* the autologging
-    # is enabled. Add some logic to handle the opposite case.
     safe_patch(FLAVOR_NAME, ClientCreator, "create_client", patched_create_client)
+
+    # If the boto3 client has already been created, we need to patch them as well.
+    # This is a bit hacky, but since the class is generated dynamically, we need to
+    # check all existing objects to find the client class.
+    for obj in gc.get_objects():
+        if isinstance(obj, type) and obj.__name__ == "BedrockRuntime":
+            patch_bedrock_runtime_client(obj)
+            break
