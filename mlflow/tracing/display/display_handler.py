4f7c989d9d0931705c159f4a7374eb632499ff61
Daniel Lok <daniel.lok@databricks.com>
2024-12-09 10:18:39 +0800

    fix badge issue
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/tracing/display/display_handler.py
+++ mlflow/tracing/display/display_handler.py
@@ -2,6 +2,7 @@ import json
 import logging
 from typing import TYPE_CHECKING
 from urllib.parse import urlencode, urljoin
+import uuid
 
 import mlflow
 from mlflow.environment_variables import MLFLOW_MAX_TRACES_TO_DISPLAY_IN_NOTEBOOK
@@ -19,7 +20,7 @@ TRACE_RENDERER_ASSET_PATH = "/static-files/lib/notebook-trace-renderer/index.htm
 IFRAME_HTML = """
 <iframe
   id="trace-renderer"
-  style="width: 100%; height: 600px; border: none; resize: vertical;"
+  style="width: 100%; height: 500px; border: none; resize: vertical;"
   src="{src}"
 />
 """
