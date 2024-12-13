af6ce1c4e270d8628da4806ce79e69bf0a7d5df6
Daniel Lok <daniel.lok@databricks.com>
2024-12-13 10:30:21 +0800

    comments
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- dev/check-notebooks.sh
+++ dev/check-notebooks.sh
@@ -5,7 +5,8 @@ TRACE_UI_SRC="static-files/lib/notebook-trace-renderer/index.html"
 # If offending files are found, list them and throw an exception
 if grep -In $TRACE_UI_SRC --include="*.ipynb" "$@"; then
     echo -e "\nError: Found the MLflow Trace UI iframe in the above notebooks."
-    echo "Please remove the iframe from the notebook source, as it will not render properly on previews or the website"
+    echo "The trace UI in cell outputs will not render correctly in previews or the website"
+    echo "Please run \`mlflow.tracing.disable_notebook_display()\` and rerun the cell to remove the iframe."
     exit 1
 else
   exit 0
