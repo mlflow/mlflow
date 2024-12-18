834c2abaa4e4f2c6a84c039e77aac55644d67cf9
Daniel Smilkov <daniel.smilkov@databricks.com>
2024-12-18 12:27:28 -0500

    Display a recipe for model evaluation when logging a Mosaic AI agent (#14084)
    Signed-off-by: Daniel Smilkov <daniel.smilkov@databricks.com>
    Co-authored-by: Daniel Lok <daniel.lok@databricks.com>
    Co-authored-by: Harutaka Kawamura <hkawamura0130@gmail.com>

---
+++ tests/models/test_display_utils.py
@@ -0,0 +1,81 @@
+from unittest import mock
+
+import pytest
+
+from mlflow.models import infer_signature
+from mlflow.models.display_utils import should_render_agent_eval_template
+from mlflow.models.rag_signatures import StringResponse
+from mlflow.types.llm import (
+    ChatChoice,
+    ChatCompletionRequest,
+    ChatCompletionResponse,
+    ChatMessage,
+)
+
+_CHAT_REQUEST = ChatCompletionRequest(
+    messages=[
+        ChatMessage(
+            role="user",
+            content="What is the primary function of control rods in a nuclear reactor?",
+        ),
+        ChatMessage(role="user", content="What is MLflow?"),
+    ]
+).to_dict()
+_CHAT_RESPONSE = ChatCompletionResponse(
+    choices=[
+        ChatChoice(
+            index=0,
+            message=ChatMessage(
+                role="assistant",
+                content="MLflow is an open source platform for the machine learning lifecycle.",
+            ),
+        )
+    ]
+).to_dict()
+
+_STRING_RESPONSE = StringResponse(
+    content="MLflow is an open source platform for the machine learning lifecycle."
+)
+
+
+@pytest.fixture
+def enable_databricks_env():
+    with (
+        mock.patch("mlflow.utils.databricks_utils.is_in_databricks_runtime", return_value=True),
+        mock.patch("IPython.get_ipython", return_value=True),
+    ):
+        yield
+
+
+def test_should_render_eval_template_when_signature_is_chat_completion(enable_databricks_env):
+    signature = infer_signature(_CHAT_REQUEST, _CHAT_RESPONSE)
+    assert should_render_agent_eval_template(signature)
+
+
+def test_should_render_eval_template_with_string_response(enable_databricks_env):
+    signature = infer_signature(_CHAT_REQUEST, _STRING_RESPONSE)
+    assert should_render_agent_eval_template(signature)
+
+
+def test_should_render_eval_template_with_vanilla_string(enable_databricks_env):
+    signature = infer_signature(_CHAT_REQUEST, "A vanilla string response")
+    assert should_render_agent_eval_template(signature)
+
+
+def test_should_not_render_eval_template_generic_signature(enable_databricks_env):
+    signature = infer_signature({"input": "string"}, {"output": "string"})
+    assert not should_render_agent_eval_template(signature)
+
+
+def test_should_not_render_eval_template_outside_databricks_env():
+    with mock.patch("mlflow.utils.databricks_utils.is_in_databricks_runtime", return_value=False):
+        with mock.patch("IPython.get_ipython", return_value=True):
+            signature = infer_signature(_CHAT_REQUEST, _STRING_RESPONSE)
+            assert not should_render_agent_eval_template(signature)
+
+
+def test_should_not_render_eval_template_outside_notebook_env():
+    with mock.patch("mlflow.utils.databricks_utils.is_in_databricks_runtime", return_value=True):
+        with mock.patch("IPython.get_ipython", return_value=None):
+            signature = infer_signature(_CHAT_REQUEST, _STRING_RESPONSE)
+            assert not should_render_agent_eval_template(signature)
