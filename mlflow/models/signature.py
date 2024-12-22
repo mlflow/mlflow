2bf7cd0aafcdc865ff04a5d61bcae08c002c7bb2
Eric Riddoch <32227767+phitoduck@users.noreply.github.com>
2024-12-22 14:46:45 -0700

    Fix: inappropriately raising `ValueError` when `log_model`'s `input_example` param is a numpy array.
    I kept getting this error when calling `log_model(..., input_example=...)` and passing in a numpy array.
    
    ```python
    ValueError: The truth value of an array with more than one element is ambiguous. Use a.any() or a.all()
    ```
    
    Signed-off-by: Eric Riddoch <32227767+phitoduck@users.noreply.github.com>

--- mlflow/models/signature.py
+++ mlflow/models/signature.py
@@ -353,7 +353,7 @@ def _infer_signature_from_type_hints(func, input_arg_index, input_example=None):
     input_schema = _infer_schema_from_type_hint(hints.input, input_example) if hints.input else None
     params_schema = _infer_param_schema(params) if params else None
     input_arg_name = _get_arg_names(func)[input_arg_index]
-    if input_example:
+    if input_example is not None:
         inputs = {input_arg_name: input_example}
         if params and params_key in inspect.signature(func).parameters:
             inputs[params_key] = params
