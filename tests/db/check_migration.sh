a106143240a72b85431c941340c66e2ab07e1ba7
Daniel Lok <daniel.lok@databricks.com>
2024-12-23 10:49:54 +0800

    revert
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- tests/db/check_migration.sh
+++ tests/db/check_migration.sh
@@ -4,9 +4,9 @@ set -ex
 cd tests/db
 
 # Install the lastest version of mlflow from PyPI
-# pip install mlflow
+pip install mlflow
+python check_migration.py pre-migration
 # Install mlflow from the repository
 pip install -e ../..
-python check_migration.py pre-migration
 mlflow db upgrade $MLFLOW_TRACKING_URI
 python check_migration.py post-migration
