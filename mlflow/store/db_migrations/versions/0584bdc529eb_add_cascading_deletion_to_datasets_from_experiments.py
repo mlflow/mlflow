a4baefb05cf27917b2bc4da716ba8f4b33f739aa
Daniel Lok <daniel.lok@databricks.com>
2024-12-23 11:36:10 +0800

    fix
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/store/db_migrations/versions/0584bdc529eb_add_cascading_deletion_to_datasets_from_experiments.py
+++ mlflow/store/db_migrations/versions/0584bdc529eb_add_cascading_deletion_to_datasets_from_experiments.py
@@ -29,6 +29,12 @@ def get_datasets_experiment_fk_name():
         autoload_with=conn,
     )
 
+    print(
+        "@@@@ Unable to find the foreign key constraint name from datasets to experiments. "
+        "All foreign key constraints in datasets table: \n"
+        f"{datasets_table.foreign_key_constraints}"
+    )
+
     for constraint in datasets_table.foreign_key_constraints:
         if (constraint.referred_table.name == SqlExperiment.__tablename__ and
             constraint.column_keys[0] == "experiment_id"):
@@ -44,8 +50,6 @@ def get_datasets_experiment_fk_name():
 def upgrade():
     dialect_name = op.get_context().dialect.name
 
-    old_fk_constraint_name = get_datasets_experiment_fk_name()
-
     # standardize the constraint to sqlite naming convention
     new_fk_constraint_name = f"fk_{SqlDataset.__tablename__}_experiment_id_{SqlExperiment.__tablename__}"
 
@@ -59,7 +63,8 @@ def upgrade():
                 "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
             },
         ) as batch_op:
-            batch_op.drop_constraint(old_fk_constraint_name, type_="foreignkey")
+            # in SQLite, constraint.name is None, so we have to hardcode it
+            batch_op.drop_constraint(new_fk_constraint_name, type_="foreignkey")
             # Need to explicitly name the fk constraint with batch alter table
             batch_op.create_foreign_key(
                 new_fk_constraint_name,
@@ -69,6 +74,7 @@ def upgrade():
                 ondelete="CASCADE",
             )
     else:
+        old_fk_constraint_name = get_datasets_experiment_fk_name()
         op.drop_constraint(old_fk_constraint_name, SqlDataset.__tablename__, type_="foreignkey")
         op.create_foreign_key(
             new_fk_constraint_name,
