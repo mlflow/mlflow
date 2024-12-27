64fbe2290e1790b71757c4dc03ea591d9cc20723
Kris Concepcion <kris.concepcion@databricks.com>
2024-12-27 17:30:56 -0500

    logging

--- tests/uc_oss/test_uc_oss_integration.py
+++ tests/uc_oss/test_uc_oss_integration.py
@@ -26,11 +26,25 @@ Server = namedtuple(
 
 @pytest.fixture(scope="module")
 def setup_servers():
+    # Get the current working directory
+    cwd = os.getcwd()
+
+    # List the contents of the directory
+    contents = os.listdir(cwd)
+
+    # Print the current working directory and its contents
+    print(f"Current Working Directory: {cwd}")
+    print("Contents:")
+    for item in contents:
+        print(f"- {item}")
+
+    assert False
+    
     with (
         subprocess.Popen(["mlflow", "server", "--port", "5000"]) as p1,
         subprocess.Popen(["bin/start-uc-server"]) as p2,
     ):
-        _await_server_up_or_die(5000)
+        #_await_server_up_or_die(5000)
         _await_server_up_or_die(8080)
         mlflow_tracking_url = "http://127.0.0.1:5000"
         uc_oss_url = "uc:http://127.0.0.1:8080"
