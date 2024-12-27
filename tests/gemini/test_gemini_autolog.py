f22c0d976378ff4bd51f1c70be2cbe0804264265
Tomu Hirata <tomu.hirata@gmail.com>
2024-12-27 21:09:01 +0900

    add tool attributes for gemini flavor
    Signed-off-by: Tomu Hirata <tomu.hirata@gmail.com>

--- tests/gemini/test_gemini_autolog.py
+++ tests/gemini/test_gemini_autolog.py
@@ -8,39 +8,62 @@ from mlflow.entities.span import SpanType
 
 from tests.tracing.helper import get_traces
 
-CANDIDATES = [{"content": {"parts": [{"text": "test answer"}], "role": "model"}}]
+_CONTENT = {"parts": [{"text": "test answer"}], "role": "model"}
 
-USER_METADATA = {
+_USER_METADATA = {
     "prompt_token_count": 6,
     "candidates_token_count": 6,
     "total_token_count": 6,
     "cached_content_token_count": 0,
 }
 
-DUMMY_GENERATE_CONTENT_RESPONSE = {
-    "candidates": CANDIDATES,
-    "usage_metadata": USER_METADATA,
-}
 
-DUMMY_COUNT_TOKENS_RESPONSE = {"total_count": 10}
+def _generate_content_response(content):
+    return {
+        "candidates": [
+            {
+                "content": content,
+                "avg_logprobs": 0.0,
+                "finish_reason": 0,
+                "grounding_attributions": [],
+                "safety_ratings": [],
+                "token_count": 0,
+            }
+        ],
+        "usage_metadata": _USER_METADATA,
+    }
 
-DUMMY_EMBEDDING_RESPONSE = {"embedding": [1, 2, 3]}
+
+_GENERATE_CONTENT_RESPONSE = _generate_content_response(_CONTENT)
+
+_DUMMY_GENERATE_CONTENT_RESPONSE = genai.types.GenerateContentResponse.from_response(
+    genai.protos.GenerateContentResponse(_GENERATE_CONTENT_RESPONSE)
+)
+
+_DUMMY_COUNT_TOKENS_RESPONSE = {"total_count": 10}
+
+_DUMMY_EMBEDDING_RESPONSE = {"embedding": [1, 2, 3]}
+
+_CHAT_MESSAGES = [
+    {"role": "user", "content": "test content"},
+    {"role": "model", "content": "test answer"},
+]
 
 
 def generate_content(self, contents):
-    return DUMMY_GENERATE_CONTENT_RESPONSE
+    return _DUMMY_GENERATE_CONTENT_RESPONSE
 
 
 def send_message(self, content):
-    return DUMMY_GENERATE_CONTENT_RESPONSE
+    return _DUMMY_GENERATE_CONTENT_RESPONSE
 
 
 def count_tokens(self, contents):
-    return DUMMY_COUNT_TOKENS_RESPONSE
+    return _DUMMY_COUNT_TOKENS_RESPONSE
 
 
 def embed_content(model, content):
-    return DUMMY_EMBEDDING_RESPONSE
+    return _DUMMY_EMBEDDING_RESPONSE
 
 
 def test_generate_content_enable_disable_autolog():
@@ -57,7 +80,8 @@ def test_generate_content_enable_disable_autolog():
         assert span.name == "GenerativeModel.generate_content"
         assert span.span_type == SpanType.LLM
         assert span.inputs == {"contents": "test content", "model_name": "models/gemini-1.5-flash"}
-        assert span.outputs == DUMMY_GENERATE_CONTENT_RESPONSE
+        assert span.outputs == _GENERATE_CONTENT_RESPONSE
+        assert span.get_attribute("mlflow.chat.messages") == _CHAT_MESSAGES
 
         mlflow.gemini.autolog(disable=True)
         model = genai.GenerativeModel("gemini-1.5-flash")
@@ -103,7 +127,8 @@ def test_chat_session_autolog():
         assert span.name == "ChatSession.send_message"
         assert span.span_type == SpanType.CHAT_MODEL
         assert span.inputs == {"content": "test content"}
-        assert span.outputs == DUMMY_GENERATE_CONTENT_RESPONSE
+        assert span.outputs == _GENERATE_CONTENT_RESPONSE
+        assert span.get_attribute("mlflow.chat.messages") == _CHAT_MESSAGES
 
         mlflow.gemini.autolog(disable=True)
         model = genai.GenerativeModel("gemini-1.5-flash")
@@ -113,6 +138,96 @@ def test_chat_session_autolog():
         # No new trace should be created
         traces = get_traces()
         assert len(traces) == 1
+
+
+def test_chat_session_tool_calling_autolog():
+    def multiply(a: float, b: float):
+        """returns a * b."""
+        return a * b
+
+    tool_attribute = [
+        {
+            "type": "function",
+            "function": {
+                "name": "multiply",
+                "description": "returns a * b.",
+                "parameters": {
+                    "properties": {
+                        "a": {"type": "number", "description": "", "enum": [], "items": None},
+                        "b": {"type": "number", "description": "", "enum": [], "items": None},
+                    },
+                    "type": "object",
+                    "required": ["a", "b"],
+                    "additionalProperties": None,
+                },
+                "strict": None,
+            },
+        },
+    ]
+
+    tool_call_content = {
+        "parts": [
+            {
+                "function_call": {
+                    "name": "multiply",
+                    "args": {
+                        "a": 57.0,
+                        "b": 44.0,
+                    },
+                }
+            }
+        ],
+        "role": "model",
+    }
+
+    raw_response = _generate_content_response(tool_call_content)
+    response = genai.types.GenerateContentResponse.from_response(
+        genai.protos.GenerateContentResponse(raw_response)
+    )
+
+    chat_messages = [
+        {
+            "content": "I have 57 cats, each owns 44 mittens, how many mittens is that in total?",
+            "role": "user",
+        },
+        {
+            "role": "model",
+            "content": None,
+            "tool_calls": [
+                {
+                    "id": "multiply",
+                    "type": "function",
+                    "function": {"name": "multiply", "arguments": '{"a": 57.0, "b": 44.0}'},
+                }
+            ],
+        },
+    ]
+
+    def generate_content(self, content):
+        return response
+
+    with patch("google.generativeai.GenerativeModel.generate_content", new=generate_content):
+        mlflow.gemini.autolog()
+        model = genai.GenerativeModel("gemini-1.5-flash", tools=[multiply])
+        model.generate_content(
+            "I have 57 cats, each owns 44 mittens, how many mittens is that in total?"
+        )
+
+        traces = get_traces()
+        assert len(traces) == 1
+        assert traces[0].info.status == "OK"
+        assert len(traces[0].data.spans) == 1
+        span = traces[0].data.spans[0]
+        assert span.name == "GenerativeModel.generate_content"
+        assert span.span_type == SpanType.LLM
+        assert span.inputs == {
+            "content": "I have 57 cats, each owns 44 mittens, how many mittens is that in total?",
+            "model_name": "models/gemini-1.5-flash",
+        }
+        assert span.get_attribute("mlflow.chat.tools") == tool_attribute
+        assert span.get_attribute("mlflow.chat.messages") == chat_messages
+
+        mlflow.gemini.autolog(disable=True)
 
 
 def test_count_tokens_autolog():
@@ -129,7 +244,7 @@ def test_count_tokens_autolog():
         assert span.name == "GenerativeModel.count_tokens"
         assert span.span_type == SpanType.LLM
         assert span.inputs == {"contents": "test content", "model_name": "models/gemini-1.5-flash"}
-        assert span.outputs == DUMMY_COUNT_TOKENS_RESPONSE
+        assert span.outputs == _DUMMY_COUNT_TOKENS_RESPONSE
 
         mlflow.gemini.autolog(disable=True)
         model = genai.GenerativeModel("gemini-1.5-flash")
@@ -153,7 +268,7 @@ def test_embed_content_autolog():
         assert span.name == "embed_content"
         assert span.span_type == SpanType.EMBEDDING
         assert span.inputs == {"content": "Hello World", "model": "models/text-embedding-004"}
-        assert span.outputs == DUMMY_EMBEDDING_RESPONSE
+        assert span.outputs == _DUMMY_EMBEDDING_RESPONSE
 
         mlflow.gemini.autolog(disable=True)
         genai.embed_content(model="models/text-embedding-004", content="Hello World")
