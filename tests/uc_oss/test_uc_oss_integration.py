dfa73956648e207b2e370e67b89dcd573b3f93d1
Kris Concepcion <kris.concepcion@databricks.com>
2024-12-28 20:07:10 -0500

    another fix

--- tests/uc_oss/test_uc_oss_integration.py
+++ tests/uc_oss/test_uc_oss_integration.py
@@ -44,7 +44,7 @@ def test_integration(setup_servers):
         assert False, "Expected exception for missing model not raised"
 
     X, y = datasets.load_iris(return_X_y=True, as_frame=True)
-    X_train, X_test, y_train, y_test = (train_test_split(X, y, test_size=0.2, random_state=42))
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     def build_model():
         with mlflow.start_run():
