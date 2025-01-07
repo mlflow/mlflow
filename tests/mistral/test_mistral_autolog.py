2d2eb35b3cc00a3cfa694fa22174e218be46f6a6
Gabriel Fu <hfu.gabriel@gmail.com>
2025-01-07 17:03:34 +0800

    fix tests
    Signed-off-by: Gabriel Fu <hfu.gabriel@gmail.com>

--- tests/mistral/test_mistral_autolog.py
+++ tests/mistral/test_mistral_autolog.py
@@ -124,7 +124,7 @@ DUMMY_CHAT_COMPLETION_WITH_TOOLS_RESPONSE = ChatCompletionResponse(
                     ToolCall(
                         function=FunctionCall(
                             name="get_weather",
-                            arguments='{"location": "San Francisco, CA", "unit": "celsius"}',
+                            arguments='{"location": "San Francisco", "unit": "celsius"}',
                         ),
                         id="tool_456",
                         type="function",
@@ -183,119 +183,73 @@ def test_chat_complete_autolog(mock_complete):
             "tool_call_id": None,
         },
     ]
-    #
-    # mlflow.anthropic.autolog(disable=True)
-    # client = anthropic.Anthropic(api_key="test_key")
-    # client.messages.create(**DUMMY_CREATE_MESSAGE_REQUEST)
-    #
-    # # No new trace should be created
-    # traces = get_traces()
-    # assert len(traces) == 1
 
+    mlflow.mistral.autolog(disable=True)
+    client = mistralai.Mistral(api_key="test_key")
+    client.chat.complete(**DUMMY_CHAT_COMPLETION_REQUEST)
 
-@patch("mistralai.chat.Chat.complete", return_value=DUMMY_CHAT_COMPLETION_RESPONSE)
-def test_messages_autolog_multi_modal(mock_post):
-    mlflow.anthropic.autolog()
-    client = anthropic.Anthropic(api_key="test_key")
-    client.messages.create(**DUMMY_CREATE_MESSAGE_REQUEST)
-
+    # No new trace should be created
     traces = get_traces()
     assert len(traces) == 1
-    assert traces[0].info.status == "OK"
-    assert len(traces[0].data.spans) == 1
-    span = traces[0].data.spans[0]
-    assert span.name == "Messages.create"
-    assert span.span_type == SpanType.CHAT_MODEL
-    assert span.inputs == DUMMY_CREATE_MESSAGE_REQUEST
-    # Only keep input_tokens / output_tokens fields in usage dict.
-    span.outputs["usage"] = {
-        key: span.outputs["usage"][key] for key in ["input_tokens", "output_tokens"]
-    }
-    assert span.outputs == DUMMY_CREATE_MESSAGE_RESPONSE.to_dict()
-
-    assert span.get_attribute(SpanAttributeKey.CHAT_MESSAGES) == [
-        {
-            "role": "user",
-            "content": "test message",
-        },
-        {
-            "role": "assistant",
-            "content": [
-                {
-                    "text": "test answer",
-                    "type": "text",
-                }
-            ],
-        },
-    ]
 
 
 @patch(
-    "mistralai.chat.Chat.complete",
-    return_value=DUMMY_CHAT_COMPLETION_WITH_TOOLS_RESPONSE,
+    "mistralai.chat.Chat.do_request",
+    return_value=_make_httpx_response(DUMMY_CHAT_COMPLETION_WITH_TOOLS_RESPONSE),
 )
-def test_messages_autolog_tool_calling(mock_post):
-    mlflow.anthropic.autolog()
-    client = anthropic.Anthropic(api_key="test_key")
-    client.messages.create(**DUMMY_CREATE_MESSAGE_WITH_TOOLS_REQUEST)
+def test_chat_complete_autolog_tool_calling(mock_complete):
+    mlflow.mistral.autolog()
+    client = mistralai.Mistral(api_key="test_key")
+    client.chat.complete(**DUMMY_CHAT_COMPLETION_WITH_TOOLS_REQUEST)
 
     traces = get_traces()
     assert len(traces) == 1
     assert traces[0].info.status == "OK"
     assert len(traces[0].data.spans) == 1
     span = traces[0].data.spans[0]
-    assert span.name == "Messages.create"
+    assert span.name == "Chat.complete"
     assert span.span_type == SpanType.CHAT_MODEL
-    assert span.inputs == DUMMY_CREATE_MESSAGE_WITH_TOOLS_REQUEST
-    assert span.outputs == DUMMY_CREATE_MESSAGE_WITH_TOOLS_RESPONSE.to_dict()
+    assert span.inputs == DUMMY_CHAT_COMPLETION_WITH_TOOLS_REQUEST
+    assert span.outputs == DUMMY_CHAT_COMPLETION_WITH_TOOLS_RESPONSE.model_dump()
 
     assert span.get_attribute(SpanAttributeKey.CHAT_MESSAGES) == [
         {
             "role": "user",
             "content": "What's the weather like in San Francisco?",
+            "tool_calls": None,
+            "tool_call_id": None,
         },
         {
             "role": "assistant",
-            "content": [
-                {
-                    "text": "<thinking>I need to use the get_unit first.</thinking>",
-                    "type": "text",
-                }
-            ],
+            "content": "",
             "tool_calls": [
                 {
-                    "id": "tool_123",
-                    "type": "function",
                     "function": {
                         "name": "get_unit",
-                        "arguments": '{"location": "San Francisco"}',
+                        "arguments": '"{\\"location\\": \\"San Francisco\\"}"',
                     },
+                    "id": "tool_123",
+                    "type": "function",
                 }
             ],
+            "tool_call_id": None,
         },
-        {
-            "role": "tool",
-            "content": [{"text": "celsius", "type": "text"}],
-            "tool_call_id": "tool_123",
-        },
+        {"role": "tool", "content": "celsius", "tool_call_id": "tool_123", "tool_calls": None},
         {
             "role": "assistant",
-            "content": [
-                {
-                    "text": "<thinking>Next, I need to use the get_weather</thinking>",
-                    "type": "text",
-                }
-            ],
+            "content": "",
             "tool_calls": [
                 {
-                    "id": "tool_456",
-                    "type": "function",
                     "function": {
                         "name": "get_weather",
-                        "arguments": '{"location": "San Francisco", "unit": "celsius"}',
+                        "arguments": '"{\\"location\\": \\"San Francisco\\", '
+                        '\\"unit\\": \\"celsius\\"}"',
                     },
+                    "id": "tool_456",
+                    "type": "function",
                 }
             ],
+            "tool_call_id": None,
         },
     ]
 
