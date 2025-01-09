e561a3092837386968e52fba4518fad92d1e661a
serena-ruan <serena.rxy@gmail.com>
2025-01-09 17:23:13 +0800

    fix test
    Signed-off-by: serena-ruan <serena.rxy@gmail.com>

--- tests/pyfunc/test_pyfunc_model_with_type_hints.py
+++ tests/pyfunc/test_pyfunc_model_with_type_hints.py
@@ -296,21 +296,17 @@ def test_pyfunc_model_infer_signature_from_type_hints_errors(recwarn):
     with mlflow.start_run():
         with mock.patch("mlflow.pyfunc._logger.warning") as mock_warning:
             mlflow.pyfunc.log_model("test_model", python_model=Model())
-        assert "cannot be used to infer model signature." in mock_warning.call_args[0][0]
         assert (
-            "Input example is not provided, model signature cannot be inferred."
-            in mock_warning.call_args[0][0]
-        )
+            "cannot be used to infer model signature, input example is not provided, "
+            "model signature cannot be inferred."
+        ) in mock_warning.call_args[0][0]
 
     with mlflow.start_run():
         with mock.patch("mlflow.pyfunc._logger.warning") as mock_warning:
             mlflow.pyfunc.log_model(
                 "test_model", python_model=Model(), input_example=pd.DataFrame()
             )
-        assert "cannot be used to infer model signature." in mock_warning.call_args[0][0]
-        assert (
-            "Inferring model signature from input example failure" in mock_warning.call_args[0][0]
-        )
+        assert "Failed to infer model signature from input example" in mock_warning.call_args[0][0]
 
 
 @pytest.mark.skipif(sys.version_info < (3, 10), reason="Requires Python 3.10 or higher")
