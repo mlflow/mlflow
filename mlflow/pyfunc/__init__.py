1d1261ebe6337222d5fa98311b58c4b23d8e492d
bbqiu <bryan.qiu@databricks.com>
2025-01-09 16:56:12 -0800

    test
    Signed-off-by: bbqiu <bryan.qiu@databricks.com>

--- mlflow/pyfunc/__init__.py
+++ mlflow/pyfunc/__init__.py
@@ -3019,6 +3019,7 @@ def save_model(
             **(input_params or {}),
         }
         _logger.info("Predicting on input example to validate output")
+        print(messages, params)
         output = python_model.predict(messages, params)
         if not isinstance(output, ChatAgentResponse):
             raise MlflowException(
