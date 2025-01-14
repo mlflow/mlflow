7a9a4a458c3e5e7dc4186e4193c67b31df071e23
Prithvi Kannan <prithvi.kannan@databricks.com>
2025-01-14 10:07:04 -0800

    test
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- examples/mlflow-3/register_model.py
+++ examples/mlflow-3/register_model.py
@@ -21,9 +21,19 @@ m = mlflow.get_logged_model(model_info.model_id)
 assert len(json.loads(m.tags["mlflow.modelVersions"])) == 1
 print(m.tags)
 assert m.model_id == model_info.model_id
-assert m.params == [LoggedModelParameter(key="alpha", value="0.5"), LoggedModelParameter(key="l1_ratio", value="0.5")]
 
 mlflow.register_model(model_info.model_uri, name="hello")
 m = mlflow.get_logged_model(model_info.model_id)
 assert len(json.loads(m.tags["mlflow.modelVersions"])) == 2
 print(m.tags)
+
+client = mlflow.MlflowClient()
+
+client.create_registered_model("model_client")
+client.create_model_version("model_client", model_info.model_uri, model_id = model_info.model_id)
+m = client.get_model_version("model_client", 1)
+assert m.model_id == model_info.model_id
+assert m.params == [
+    LoggedModelParameter(key="alpha", value="0.5"),
+    LoggedModelParameter(key="l1_ratio", value="0.5"),
+]
