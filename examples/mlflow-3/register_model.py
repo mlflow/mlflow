63ac9fb5695fdd0a1e5cfb1a90cb6700505fc583
Raymond Zhou <raymond.zhou@databricks.com>
2025-01-14 15:34:39 -0800

    Update register_model.py
    Signed-off-by: Raymond Zhou <raymond.zhou@databricks.com>

--- examples/mlflow-3/register_model.py
+++ examples/mlflow-3/register_model.py
@@ -21,7 +21,11 @@ print(m.tags)
 # Register model in log_model() directly
 with mlflow.start_run():
     model_1 = LinearRegression().fit([[1], [2]], [3, 4])
-    model_info_1 = mlflow.sklearn.log_model(model_1, "model1", registered_model_name="model1")
+    model_info_1 = mlflow.sklearn.log_model(model_1, "model_1", registered_model_name="model_1")
+
 m = mlflow.get_logged_model(model_info_1.model_id)
 assert len(json.loads(m.tags["mlflow.modelVersions"])) == 1
 print(m.tags)
+
+client = mlflow.MlflowClient()
+m = client.get_model_version("model_1", 1)
