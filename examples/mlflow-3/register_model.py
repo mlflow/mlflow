57522778a6195eb9f04fd4f743897774afadd552
Prithvi Kannan <prithvi.kannan@databricks.com>
2025-01-14 10:02:38 -0800

    extend test
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- examples/mlflow-3/register_model.py
+++ examples/mlflow-3/register_model.py
@@ -3,15 +3,25 @@ import json
 from sklearn.linear_model import LinearRegression
 
 import mlflow
+from mlflow.entities.logged_model_parameter import LoggedModelParameter
 
 with mlflow.start_run():
     model = LinearRegression().fit([[1], [2]], [3, 4])
-    model_info = mlflow.sklearn.log_model(model, "model")
+    model_info = mlflow.sklearn.log_model(
+        model, 
+        "model", 
+        params={
+            "alpha": 0.5,
+            "l1_ratio": 0.5,
+        },
+    )
 
 mlflow.register_model(model_info.model_uri, name="model")
 m = mlflow.get_logged_model(model_info.model_id)
 assert len(json.loads(m.tags["mlflow.modelVersions"])) == 1
 print(m.tags)
+assert m.model_id == model_info.model_id
+assert m.params == [LoggedModelParameter(key="alpha", value="0.5"), LoggedModelParameter(key="l1_ratio", value="0.5")]
 
 mlflow.register_model(model_info.model_uri, name="hello")
 m = mlflow.get_logged_model(model_info.model_id)
