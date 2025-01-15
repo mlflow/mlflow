cab0b243e6331b88c5f8bf7e38a3728343c81a78
Raymond Zhou <raymond.zhou@databricks.com>
2025-01-14 16:21:09 -0800

    Fix Registering Model within log_model() for MLflow 3.0 (#14246)
    Signed-off-by: Raymond Zhou <raymond.zhou@databricks.com>

--- examples/mlflow-3/register_model.py
+++ examples/mlflow-3/register_model.py
@@ -17,3 +17,15 @@ mlflow.register_model(model_info.model_uri, name="hello")
 m = mlflow.get_logged_model(model_info.model_id)
 assert len(json.loads(m.tags["mlflow.modelVersions"])) == 2
 print(m.tags)
+
+# Register model in log_model() directly
+with mlflow.start_run():
+    model_1 = LinearRegression().fit([[1], [2]], [3, 4])
+    model_info_1 = mlflow.sklearn.log_model(model_1, "model_1", registered_model_name="model_1")
+
+m = mlflow.get_logged_model(model_info_1.model_id)
+assert len(json.loads(m.tags["mlflow.modelVersions"])) == 1
+print(m.tags)
+
+client = mlflow.MlflowClient()
+m = client.get_model_version("model_1", 1)
