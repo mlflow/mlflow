4e7f1b5d105185b9ff06723ceaec1f0ae66efc76
harupy <17039389+harupy@users.noreply.github.com>
2025-01-21 13:32:28 +0900

    skip
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/crewai/test_crewai_autolog.py
+++ tests/crewai/test_crewai_autolog.py
@@ -284,6 +284,12 @@ def test_kickoff_enable_disable_autolog(simple_agent_1, task_1, autolog):
     assert len(traces) == 1
 
 
+@pytest.mark.skip(
+    reason=(
+        "https://github.com/crewAIInc/crewAI/issues/1934. "
+        "Remove skip when the issue is resolved."
+    )
+)
 def test_kickoff_failure(simple_agent_1, task_1, autolog):
     crew = Crew(
         agents=[
