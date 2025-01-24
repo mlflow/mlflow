8d002a9cae4f86fc4442b9e70c43f8eee1e253cb
B-Step62 <yuki.watanabe@databricks.com>
2025-01-24 13:59:44 +0900

    wip
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -171,6 +171,14 @@ const config: Config = {
             to: "/tracing",
             from: ["/llms/tracing"],
           },
+          {
+            to: "dataset",
+            from: ["/tracking/data-api/index"],
+          },
+          {
+            to: "model",
+            from: ["/models"],
+          }
         ],
       },
     ],
