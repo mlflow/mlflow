719e0d3f91eb4021ec95a02788dc8bf58a53f791
Software Developer <7852635+dsuhinin@users.noreply.github.com>
2025-01-27 21:04:33 +0100

    Fix validation for rename registered model logic. (#14353)
    Signed-off-by: dsuhinin <suhinin.dmitriy@gmail.com>

--- mlflow/utils/validation.py
+++ mlflow/utils/validation.py
@@ -466,6 +466,11 @@ def _validate_model_name(model_name):
         raise MlflowException(missing_value("name"), error_code=INVALID_PARAMETER_VALUE)
 
 
+def _validate_model_renaming(model_new_name):
+    if model_new_name is None or model_new_name == "":
+        raise MlflowException(missing_value("new_name"), error_code=INVALID_PARAMETER_VALUE)
+
+
 def _validate_model_version(model_version):
     try:
         model_version = int(model_version)
