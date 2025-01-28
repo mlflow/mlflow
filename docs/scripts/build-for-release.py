1c13cb920889b1b0c438c52a6e76129a362c2375
Daniel Lok <daniel.lok@databricks.com>
2025-01-28 15:40:10 +0800

    lint
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- docs/scripts/build-for-release.py
+++ docs/scripts/build-for-release.py
@@ -8,46 +8,46 @@ import mlflow
 
 mlflow_version = mlflow.version.VERSION
 
+
 def build_docs(version):
-  env = os.environ.copy()
-  output_path = f"_build/{version}"
-  base_url = f"/docs/{version}"
+    env = os.environ.copy()
+    output_path = f"_build/{version}"
+    base_url = f"/docs/{version}"
 
-  print(f"Building for `{base_url}`...")
+    print(f"Building for `{base_url}`...")
 
-  if os.path.exists(output_path):
-    shutil.rmtree(output_path)
+    if os.path.exists(output_path):
+        shutil.rmtree(output_path)
 
-  if os.path.exists("build"):
-    shutil.rmtree("build")
+    if os.path.exists("build"):
+        shutil.rmtree("build")
 
-  subprocess.run(["yarn", "build"], env={**env, "DOCS_BASE_URL": base_url})
-  shutil.copytree("build", output_path)
+    subprocess.run(["yarn", "build"], env={**env, "DOCS_BASE_URL": base_url})
+    shutil.copytree("build", output_path)
+
 
 @click.command()
 def main():
-  env = os.environ.copy()
-  gtm_id = env.get("GTM_ID")
+    env = os.environ.copy()
+    gtm_id = env.get("GTM_ID")
 
-  assert gtm_id, "Google Tag Manager ID is missing, please ensure that the GTM_ID environment variable is set"
+    assert (
+        gtm_id
+    ), "Google Tag Manager ID is missing, please ensure that the GTM_ID environment variable is set"
 
-  subprocess.run(["yarn", "install"])
-  subprocess.run(["yarn", "build-api-docs"], env=env)
-  subprocess.run(["yarn", "convert-notebooks"], env=env)
+    subprocess.run(["yarn", "install"])
+    subprocess.run(["yarn", "build-api-docs"], env=env)
+    subprocess.run(["yarn", "convert-notebooks"], env=env)
 
-  if os.path.exists("_build"):
-    shutil.rmtree("_build")
-  os.makedirs("_build", exist_ok=True)
+    if os.path.exists("_build"):
+        shutil.rmtree("_build")
+    os.makedirs("_build", exist_ok=True)
 
-  for v in [mlflow_version, "latest"]:
-    build_docs(v)
+    for v in [mlflow_version, "latest"]:
+        build_docs(v)
 
-  print("Finished building! Output can be found in the `_build` directory")
+    print("Finished building! Output can be found in the `_build` directory")
+
 
 if __name__ == "__main__":
-  main()
-
-
-
-
-
+    main()
