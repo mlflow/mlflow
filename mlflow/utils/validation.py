647e69d4b00d2261ddf5a12fae4911529cff74d1
dsuhinin <suhinin.dmitriy@gmail.com>
2025-01-28 14:31:14 +0100

    fix linter remarks.
    Signed-off-by: dsuhinin <suhinin.dmitriy@gmail.com>

--- mlflow/utils/validation.py
+++ mlflow/utils/validation.py
@@ -114,8 +114,10 @@ def invalid_value(path, value, message=None):
 def missing_value(path):
     return f"Missing value for required parameter '{path}'."
 
+
 def not_integer_value(path, value):
     return f"Parameter '{path}' must be an integer, got '{value}'."
+
 
 def exceeds_maximum_length(path, limit):
     return f"'{path}' exceeds the maximum length of {limit} characters"
@@ -477,7 +479,9 @@ def _validate_model_version(model_version):
     try:
         model_version = int(model_version)
     except ValueError:
-        raise MlflowException(not_integer_value("version", model_version), error_code=INVALID_PARAMETER_VALUE)
+        raise MlflowException(
+            not_integer_value("version", model_version), error_code=INVALID_PARAMETER_VALUE
+        )
 
 
 def _validate_model_alias_name(model_alias_name):
