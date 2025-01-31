a18b8b2c40cdaf8fe76c01e66df7c7044668dda2
harupy <17039389+harupy@users.noreply.github.com>
2025-01-31 13:33:37 +0900

    test
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+import locale
+import sys
+
+print(sys.getdefaultencoding())  # noqa
+print(locale.getpreferredencoding())  # noqa
+
 with open("foo.txt", "w") as f:
     print(f.encoding)  # noqa
     f.write("α")
