8cb1ef24c535f0c428e32b59f610f7e5ab976fd0
Daniel Lok <daniel.lok@databricks.com>
2025-01-31 13:35:37 +0800

    fix
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- tests/litellm/test_litellm_autolog.py
+++ tests/litellm/test_litellm_autolog.py
@@ -186,22 +186,22 @@ async def test_litellm_tracing_async_streaming(is_in_databricks):
     assert spans[0].outputs["choices"][0]["message"]["content"] == "Hello world"
 
 
-def test_litellm_tracing_disable(is_in_databricks):
+async def test_litellm_tracing_disable(is_in_databricks):
     mlflow.litellm.autolog()
 
-    litellm.completion("gpt-4o-mini", [{"role": "system", "content": "Hello"}])
+    await litellm.completion("gpt-4o-mini", [{"role": "system", "content": "Hello"}])
     _wait_if_not_in_databricks()
     assert mlflow.get_last_active_trace() is not None
     assert len(get_traces()) == 1
 
     mlflow.litellm.autolog(disable=True)
-    litellm.completion("gpt-4o-mini", [{"role": "system", "content": "Hello"}])
+    await litellm.completion("gpt-4o-mini", [{"role": "system", "content": "Hello"}])
     _wait_if_not_in_databricks()
     # no additional trace should be created
     assert len(get_traces()) == 1
 
     mlflow.litellm.autolog(log_traces=False)
-    litellm.completion("gpt-4o-mini", [{"role": "system", "content": "Hello"}])
+    await litellm.completion("gpt-4o-mini", [{"role": "system", "content": "Hello"}])
     _wait_if_not_in_databricks()
     # no additional trace should be created
     assert len(get_traces()) == 1
