52d2e04c5a53e0d1829e2056e8e16a9a9806db59
Harutaka Kawamura <hkawamura0130@gmail.com>
2025-02-03 19:39:19 +0900

    Fix `_parse_model_id_if_present` to handle pathlib.Path (#14424)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/store/artifact/utils/models.py
+++ mlflow/store/artifact/utils/models.py
@@ -1,5 +1,6 @@
 import urllib.parse
-from typing import NamedTuple, Optional
+from pathlib import Path
+from typing import NamedTuple, Optional, Union
 
 import mlflow.tracking
 from mlflow.exceptions import MlflowException
@@ -93,7 +94,7 @@ def _parse_model_uri(uri):
         return ParsedModelUri(parts[0])
 
 
-def _parse_model_id_if_present(possible_model_uri: str) -> Optional[str]:
+def _parse_model_id_if_present(possible_model_uri: Union[str, Path]) -> Optional[str]:
     """
     Parses the model ID from the given string. If the string is not a models:/ URI, returns None.
 
@@ -103,8 +104,9 @@ def _parse_model_id_if_present(possible_model_uri: str) -> Optional[str]:
     Returns:
         The model ID if the string is a models:/ URI, otherwise None.
     """
-    if is_models_uri(possible_model_uri):
-        parsed_model_uri = _parse_model_uri(possible_model_uri)
+    uri = str(possible_model_uri)
+    if is_models_uri(uri):
+        parsed_model_uri = _parse_model_uri(uri)
         return parsed_model_uri.model_id
 
     return None
