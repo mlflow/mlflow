851c0c0eda4a7cc67311b0c1ac021f45340c0546
bbqiu <bryan.qiu@databricks.com>
2025-02-04 12:31:56 -0600

    .
    Signed-off-by: bbqiu <bryan.qiu@databricks.com>

--- tests/langchain/test_langchain_output_parsers.py
+++ tests/langchain/test_langchain_output_parsers.py
@@ -92,9 +92,4 @@ def test_chat_agent_output_parser_parse_response():
     base_messages = [BaseMessage(content=m, type="test") for m in streaming_messages]
     streaming_chunks = parser.transform(base_messages, RunnableConfig())
     for i, chunk in enumerate(streaming_chunks):
-        for msg in chunk["messages"]:
-            assert isinstance(msg["id"], str)
-            del msg["id"]
-        assert chunk == {
-            "messages": [{"content": streaming_messages[i], "role": "assistant"}],
-        }
+        assert chunk == {"message": {"content": streaming_messages[i], "role": "assistant"}}
