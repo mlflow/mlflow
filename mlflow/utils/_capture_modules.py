90ad8637d201ea2974c182932128544b2f58bd77
bbqiu <bryan.qiu@databricks.com>
2025-02-06 13:48:33 -0600

    maybe
    Signed-off-by: bbqiu <bryan.qiu@databricks.com>

--- mlflow/utils/_capture_modules.py
+++ mlflow/utils/_capture_modules.py
@@ -13,6 +13,7 @@ import sys
 import mlflow
 from mlflow.models.model import MLMODEL_FILE_NAME, Model
 from mlflow.pyfunc import MAIN
+from mlflow.pyfunc.loaders.chat_agent import _ChatAgentPyfuncWrapper
 from mlflow.utils._spark_utils import _prepare_subprocess_environ_for_creating_local_spark_session
 from mlflow.utils.exception_utils import get_stacktrace
 from mlflow.utils.file_utils import write_to
@@ -163,7 +164,10 @@ def store_imported_modules(
             model = original_load_fn(*args, **kwargs)
             if input_example is not None:
                 try:
-                    model.predict(input_example, params=params)
+                    if isinstance(model, _ChatAgentPyfuncWrapper):
+                        model.predict(input_example)
+                    else:
+                        model.predict(input_example, params=params)
                 except Exception as e:
                     if error_file:
                         stack_trace = get_stacktrace(e)
