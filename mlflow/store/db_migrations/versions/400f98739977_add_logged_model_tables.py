57e139a2275f469244bff545c700dc87e62684fb
harupy <17039389+harupy@users.noreply.github.com>
2025-02-07 22:37:36 +0900

    Fix
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/store/db_migrations/versions/400f98739977_add_logged_model_tables.py
+++ mlflow/store/db_migrations/versions/400f98739977_add_logged_model_tables.py
@@ -6,9 +6,8 @@ Create Date: 2025-02-06 22:05:35.542613
 
 """
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "400f98739977"
