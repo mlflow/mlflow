36d61dfde2054865e90acd07222fbfbd659a095a
Nathan Brake <nathan@mozilla.ai>
2025-02-10 17:18:05 -0500

    making it work as a decorator
    Signed-off-by: Nathan Brake <nathan@mozilla.ai>

--- mlflow/utils/pydantic_utils.py
+++ mlflow/utils/pydantic_utils.py
@@ -6,21 +6,15 @@ from pydantic import BaseModel
 
 IS_PYDANTIC_V2_OR_NEWER = Version(pydantic.VERSION).major >= 2
 
-if IS_PYDANTIC_V2_OR_NEWER:
-    from pydantic import field_validator as py_validator
 
-    def validator(field, pre=True):
-        mode = "before" if pre else "after"
-        def decorator(func):
-            return py_validator(field, mode=mode)(func)
-        return decorator
-else:
-    from pydantic import validator as py_validator
+def validator(field, pre=True):
+    if IS_PYDANTIC_V2_OR_NEWER:
+        from pydantic import field_validator as pydantic_validator
+        return pydantic_validator(field, mode="before" if pre else "after")
+    else:
+        from pydantic import validator as pydantic_validator
+        return pydantic_validator(field, pre=pre)
 
-    def validator(field, pre=True):
-        def decorator(func):
-            return py_validator(field, pre=pre)(func)
-        return decorator
 
 
 def model_dump_compat(pydantic_model: BaseModel, **kwargs: Any) -> dict[str, Any]:
