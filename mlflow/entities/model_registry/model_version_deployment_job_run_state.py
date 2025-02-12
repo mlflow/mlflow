0e50f2abb53a6ad1a1ace6e66fa6d5ed5f4a5c40
Arpit Jasapara <arpit.jasapara@databricks.com>
2025-02-11 23:57:51 -0800

    test proto fix
    Signed-off-by: Arpit Jasapara <arpit.jasapara@databricks.com>

--- mlflow/entities/model_registry/model_version_deployment_job_run_state.py
+++ mlflow/entities/model_registry/model_version_deployment_job_run_state.py
@@ -1,18 +1,15 @@
-# from mlflow.protos.databricks_uc_registry_messages_pb2 import DeploymentJobRunState
+from mlflow.protos.databricks_uc_registry_messages_pb2 import ModelVersionDeploymentJobState as ProtoModelVersionDeploymentJobState
 
 
 class ModelVersionDeploymentJobRunState:
     """Enum for model version deployment state of an :py:class:`mlflow.entities.model_registry.ModelVersion`."""
 
-    """
-    NO_VALID_DEPLOYMENT_JOB_FOUND = DeploymentJobRunState.Value("NO_VALID_DEPLOYMENT_JOB_FOUND")
-    RUNNING = DeploymentJobRunState.Value("RUNNING")
-    SUCCEEDED = DeploymentJobRunState.Value("SUCCEEDED")
-    FAILED = DeploymentJobRunState.Value("FAILED")
-    PENDING = DeploymentJobRunState.Value("PENDING")
-    _STRING_TO_STATE = {k: DeploymentJobRunState.Value(k) for k in DeploymentJobRunState.keys()}
-    """
-    _STRING_TO_STATE = {}
+    NO_VALID_DEPLOYMENT_JOB_FOUND = ProtoModelVersionDeploymentJobState.DeploymentJobRunState.Value("NO_VALID_DEPLOYMENT_JOB_FOUND")
+    RUNNING = ProtoModelVersionDeploymentJobState.DeploymentJobRunState.Value("RUNNING")
+    SUCCEEDED = ProtoModelVersionDeploymentJobState.DeploymentJobRunState.Value("SUCCEEDED")
+    FAILED = ProtoModelVersionDeploymentJobState.DeploymentJobRunState.Value("FAILED")
+    PENDING = ProtoModelVersionDeploymentJobState.DeploymentJobRunState.Value("PENDING")
+    _STRING_TO_STATE = {k: ProtoModelVersionDeploymentJobState.DeploymentJobRunState.Value(k) for k in ProtoModelVersionDeploymentJobState.DeploymentJobRunState.keys()}
     _STATE_TO_STRING = {value: key for key, value in _STRING_TO_STATE.items()}
 
     @staticmethod
