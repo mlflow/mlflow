dd3755510c724f6be2f915e8616b07022ce336a8
Harutaka Kawamura <hkawamura0130@gmail.com>
2025-02-12 16:20:46 +0900

    Adjust `test_python_model_predict_compatible_without_params` (#14554)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/pyfunc/test_model_export_with_class_and_artifacts.py
+++ tests/pyfunc/test_model_export_with_class_and_artifacts.py
@@ -256,18 +256,16 @@ def test_python_model_predict_compatible_without_params(sklearn_knn_model, iris_
         return sk_model.predict(model_input) * 2
 
     pyfunc_artifact_path = "pyfunc_model"
-    with mlflow.start_run() as run:
+    with mlflow.start_run():
         model_info = mlflow.pyfunc.log_model(
             pyfunc_artifact_path,
             artifacts={"sk_model": sklearn_model_uri},
             python_model=CustomSklearnModelWithoutParams(test_predict),
         )
-        pyfunc_model_uri = f"runs:/{run.info.run_id}/{pyfunc_artifact_path}"
-        assert model_info.model_uri == pyfunc_model_uri
-        pyfunc_model_path = _download_artifact_from_uri(pyfunc_model_uri)
+        pyfunc_model_path = _download_artifact_from_uri(model_info.model_uri)
         model_config = Model.load(os.path.join(pyfunc_model_path, "MLmodel"))
 
-    loaded_pyfunc_model = mlflow.pyfunc.load_model(model_uri=pyfunc_model_uri)
+    loaded_pyfunc_model = mlflow.pyfunc.load_model(model_uri=model_info.model_uri)
     assert model_config.to_yaml() == loaded_pyfunc_model.metadata.to_yaml()
     np.testing.assert_array_equal(
         loaded_pyfunc_model.predict(iris_data[0]),
