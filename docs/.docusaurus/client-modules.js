cc2b944b0debf5d488f6b5d3c6dfe059160c95c9
serena-ruan <serena.rxy@gmail.com>
2025-02-13 14:23:24 +0800

    fix
    Signed-off-by: serena-ruan <serena.rxy@gmail.com>

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/serena.ruan/Documents/repos/mlflow/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/serena.ruan/Documents/repos/mlflow/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/serena.ruan/Documents/repos/mlflow/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/serena.ruan/Documents/repos/mlflow/docs/src/css/custom.css"),
-];
