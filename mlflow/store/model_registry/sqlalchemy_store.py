2575a04f0e0f92c9b3e94a155aadabea3a465636
Prithvi Kannan <prithvi.kannan@databricks.com>
2025-02-12 17:15:53 -0800

    fix
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- mlflow/store/model_registry/sqlalchemy_store.py
+++ mlflow/store/model_registry/sqlalchemy_store.py
@@ -660,6 +660,7 @@ class SqlAlchemyStore(AbstractStore):
             local_model_path: Unused.
             model_id: The ID of the model (from an Experiment) that is being promoted to a
                 registered model version, if applicable.
+            model_params: The parameters of the model (from an Experiment) that is being promoted
 
         Returns:
             A single object of :py:class:`mlflow.entities.model_registry.ModelVersion`
