dc84dafea5e200f5ec08d00def69dd961089bd20
Bryan Qiu <55931436+bbqiu@users.noreply.github.com>
2025-02-12 20:04:40 -0600

    Update LangGraph connector code and remove ChatAgent wrappers (#14567)
    Signed-off-by: bbqiu <bryan.qiu@databricks.com>

--- tests/langchain/sample_code/langchain_chat_agent.py
+++ tests/langchain/sample_code/langchain_chat_agent.py
@@ -1,14 +1,17 @@
 from operator import itemgetter
+from typing import Any, Generator, Optional
 
 from langchain_core.messages import AIMessage, AIMessageChunk
 from langchain_core.outputs import ChatGeneration, ChatGenerationChunk, ChatResult
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableLambda
+from langchain_core.runnables.base import Runnable
 from langchain_openai import ChatOpenAI
 
 import mlflow
-from mlflow.langchain.chat_agent_langchain import LangChainChatAgent
 from mlflow.langchain.output_parsers import ChatAgentOutputParser
+from mlflow.pyfunc.model import ChatAgent
+from mlflow.types.agent import ChatAgentChunk, ChatAgentMessage, ChatAgentResponse, ChatContext
 
 
 class FakeOpenAI(ChatOpenAI, extra="allow"):
@@ -66,6 +69,39 @@ chain = (
     | model
     | output_parser
 )
+
+
+class LangChainChatAgent(ChatAgent):
+    """
+    Helper class to wrap a LangChain runnable as a :py:class:`ChatAgent <mlflow.pyfunc.ChatAgent>`.
+    Use this class with
+    :py:class:`ChatAgentOutputParser <mlflow.langchain.output_parsers.ChatAgentOutputParser>`.
+    """
+
+    def __init__(self, agent: Runnable):
+        self.agent = agent
+
+    # TODO trace this by default once manual tracing of predict_stream is supported
+    def predict(
+        self,
+        messages: list[ChatAgentMessage],
+        context: Optional[ChatContext] = None,
+        custom_inputs: Optional[dict[str, Any]] = None,
+    ) -> ChatAgentResponse:
+        response = self.agent.invoke({"messages": self._convert_messages_to_dict(messages)})
+        return ChatAgentResponse(**response)
+
+    # TODO trace this by default once manual tracing of predict_stream is supported
+    def predict_stream(
+        self,
+        messages: list[ChatAgentMessage],
+        context: Optional[ChatContext] = None,
+        custom_inputs: Optional[dict[str, Any]] = None,
+    ) -> Generator[ChatAgentChunk, None, None]:
+        for event in self.agent.stream({"messages": self._convert_messages_to_dict(messages)}):
+            yield ChatAgentChunk(**event)
+
+
 chat_agent = LangChainChatAgent(chain)
 
 mlflow.models.set_model(chat_agent)
