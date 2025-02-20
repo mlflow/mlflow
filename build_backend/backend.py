08b0b3d2c7af9be79e02343870f716401f916e2a
harupy <17039389+harupy@users.noreply.github.com>
2025-02-20 13:04:08 +0900

    sha
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

---
+++ build_backend/backend.py
@@ -0,0 +1,16 @@
+# https://setuptools.pypa.io/en/latest/build_meta.html#dynamic-build-dependencies-and-other-build-meta-tweaks
+import subprocess
+import sys
+from pathlib import Path
+
+from setuptools import build_meta as _orig_build_meta
+from setuptools.build_meta import *  # noqa: F403
+
+
+def get_requires_for_build_sdist(config_settings=None):
+    try:
+        sha = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip()
+        Path("mlflow", "sha.py").write_text(f'sha = "{sha}"\n')
+    except Exception as e:
+        sys.stderr.write(f"Failed to write SHA to mlflow/sha.py: {e}\n")
+    return _orig_build_meta.get_requires_for_build_sdist(config_settings)
