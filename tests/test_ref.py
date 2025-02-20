ae7f53536168ee53c185bb63da5a3b68a3ef7b4d
harupy <17039389+harupy@users.noreply.github.com>
2025-02-20 18:08:41 +0900

    test
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/test_ref.py
+++ tests/test_ref.py
@@ -5,6 +5,6 @@ import pytest
 from mlflow.ref import REF
 
 
-@pytest.mark.skipif("CI" not in os.environ, reason="This test should only be run in CI")
+@pytest.mark.skipif("CI" not in os.environ, reason="Only works in CI")
 def test_ref():
     assert REF is not None
