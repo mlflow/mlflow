8aa5696ab6311ca77ffcdd9a582023bec68b7bee
harupy <17039389+harupy@users.noreply.github.com>
2025-02-20 13:04:43 +0900

    a
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- build_backend/backend.py
+++ build_backend/backend.py
@@ -9,6 +9,7 @@ from setuptools.build_meta import *  # noqa: F403
 
 def get_requires_for_build_sdist(config_settings=None):
     try:
+        sys.stdout.write("Updating SHA in mlflow/sha.py...\n")
         sha = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip()
         Path("mlflow", "sha.py").write_text(f'sha = "{sha}"\n')
     except Exception as e:
