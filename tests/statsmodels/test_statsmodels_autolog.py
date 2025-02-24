a5801f8569883f52de6403288a154483fd509879
TomuHirata <tomu.hirata@gmail.com>
2025-02-24 19:23:32 +0900

    Fix statsmodels autolog test on mlflow3 (#14694)
    Signed-off-by: Tomu Hirata <tomu.hirata@gmail.com>

--- tests/statsmodels/test_statsmodels_autolog.py
+++ tests/statsmodels/test_statsmodels_autolog.py
@@ -184,7 +184,7 @@ def test_statsmodels_autolog_respects_log_models_flag(log_models):
     run = get_latest_run()
     client = MlflowClient()
     artifact_paths = [artifact.path for artifact in client.list_artifacts(run.info.run_id)]
-    assert ("model" in artifact_paths) == log_models
+    assert (mlflow.last_logged_model() is not None) == log_models
 
 
 def test_statsmodels_autolog_loads_model_from_artifact():
