dd609c654a1b679d2aca9c6881ce816a65f895b4
harupy <17039389+harupy@users.noreply.github.com>
2025-02-25 18:37:33 +0900

    Deprecate MLserver integration
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/models/cli.py
+++ mlflow/models/cli.py
@@ -1,4 +1,5 @@
 import logging
+import warnings
 
 import click
 
@@ -97,6 +98,9 @@ def serve(
         }'
 
     """
+    if enable_mlserver:
+        warnings.warn("MLServer integration is deprecated and will be removed in MLflow 3.0.")
+
     env_manager = _EnvManager.LOCAL if no_conda else env_manager
 
     return get_flavor_backend(
