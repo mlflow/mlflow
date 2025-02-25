9e401e4ca41e605937d1a402d8b6bf4ea587c0a7
Tomu Hirata <tomu.hirata@gmail.com>
2025-02-25 20:52:37 +0900

    implement patch based auto-logging
    Signed-off-by: Tomu Hirata <tomu.hirata@gmail.com>

--- mlflow/autogen/__init__.py
+++ mlflow/autogen/__init__.py
@@ -1,31 +1,22 @@
-from autogen_agentchat.agents import AssistantAgent
-from autogen_agentchat.messages import TextMessage
-from autogen_agentchat.ui import Console
-from autogen_core import CancellationToken
-from autogen_ext.models.openai import OpenAIChatCompletionClient
+import logging
+from typing import Optional
 
-# Define a tool that searches the web for information.
-async def web_search(query: str) -> str:
-    """Find information on the web"""
-    return "AutoGen is a programming framework for building multi-agent applications."
+from opentelemetry.context import Context
+from opentelemetry.sdk.trace import ReadableSpan
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 
-
-# Create an agent that uses the OpenAI GPT-4o model.
-# model_client = OpenAIChatCompletionClient(
-#     model="gpt-4o",
-#     # api_key="YOUR_API_KEY",
-# )
-# agent = AssistantAgent(
-#     name="assistant",
-#     model_client=model_client,
-#     tools=[web_search],
-#     system_message="Use tools to solve tasks.",
-# )
-
-from mlflow.utils.autologging_utils import autologging_integration, safe_patch
+from mlflow.entities.span import create_mlflow_span
+from mlflow.tracing.constant import SpanAttributeKey
+from mlflow.tracing.provider import _get_tracer_provider
+from mlflow.tracing.trace_manager import InMemoryTraceManager
+from mlflow.tracing.utils import get_otel_attribute
 from mlflow.utils.annotations import experimental
+from mlflow.utils.autologging_utils import _logger, autologging_integration, safe_patch
 
 FLAVOR_NAME = "autogen"
+
+_logger = logging.getLogger(__name__)
+
 
 @experimental
 @autologging_integration(FLAVOR_NAME)
@@ -34,10 +25,44 @@ def autolog(
     disable: bool = False,
     silent: bool = False,
 ):
-    async def patch(original, self, *args, **kwargs):
-        result = await original(self, *args, **kwargs)
-        print("hello", result)
-        return result
+    from autogen_core import SingleThreadedAgentRuntime
 
-    safe_patch(FLAVOR_NAME, AssistantAgent, "on_messages_stream", patch)
-    
+    tracer_provider = _get_tracer_provider()
+
+    def patched_call(original, self, *args, **kwargs):
+        if _tracer_provider := kwargs.get("tracer_provider"):
+            _logger.warning(
+                f"Tracer provider is provided to {original.__class__}."
+                "The original provider is replaced by mlflow autologging and is not effective."
+            )
+            kwargs.pop("tracer_provider")
+        return original(self, *args, **kwargs, tracer_provider=tracer_provider)
+
+    safe_patch(FLAVOR_NAME, SingleThreadedAgentRuntime, "__init__", patched_call)
+
+    try:
+        from autogen_ext.runtimes.grpc import GrpcWorkerAgentRuntimeHost
+
+        safe_patch(FLAVOR_NAME, GrpcWorkerAgentRuntimeHost, "__init__", patched_call)
+    except ImportError:
+        pass
+
+
+class AutogenSpanProcessor(SimpleSpanProcessor):
+    """
+    An span processor that resister the span to the mlflow trace manager.
+    This class fills the gap between Otel and mlflow tracing where we store the spans
+    in memory until the root span is ended.
+    """
+
+    def __init__(self):
+        pass
+
+    def on_start(self, span, parent_context: Optional[Context] = None):
+        request_id = get_otel_attribute(span, SpanAttributeKey.REQUEST_ID)
+        mlflow_span = create_mlflow_span(span, request_id)
+        # TODO: convert native span attributes into mlflow attributes
+        InMemoryTraceManager.get_instance().register_span(mlflow_span)
+
+    def on_end(self, span: ReadableSpan) -> None:
+        pass
