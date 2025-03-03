da5a0cc70051717cfc38824396e2ad05a31f10a8
harupy <17039389+harupy@users.noreply.github.com>
2025-03-03 14:47:24 +0900

    Improve tests
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/pyfunc/test_logged_models.py
+++ tests/pyfunc/test_logged_models.py
@@ -2,22 +2,32 @@ import mlflow
 from mlflow.tracing.constant import TraceMetadataKey
 
 
-def test_model_id_tracking():
-    class TraceModel(mlflow.pyfunc.PythonModel):
-        @mlflow.trace
-        def predict(self, model_input: list[int]) -> list[int]:
-            return model_input
+class TraceModel(mlflow.pyfunc.PythonModel):
+    @mlflow.trace
+    def predict(self, model_input):
+        return len(model_input) * [0]
 
+
+def test_model_id_tracking():
     model = TraceModel()
-    assert model.predict([1, 2, 3]) == [1, 2, 3]
+    model.predict([1, 2, 3])
     trace = mlflow.get_last_active_trace()
     assert TraceMetadataKey.MODEL_ID not in trace.info.request_metadata
 
     with mlflow.start_run():
-        info = mlflow.pyfunc.log_model("my_model", python_model=TraceModel())
+        info = mlflow.pyfunc.log_model("my_model", python_model=model)
 
     model = mlflow.pyfunc.load_model(info.model_uri)
-    assert model.predict([4, 5, 6]) == [4, 5, 6]
+    model.predict([4, 5, 6])
 
     trace = mlflow.get_last_active_trace()
-    assert trace.info.request_metadata[TraceMetadataKey.MODEL_ID] == model.model_id
+    assert trace.info.request_metadata[TraceMetadataKey.MODEL_ID] == info.model_id
+
+
+def test_model_id_tracking_evaluate():
+    with mlflow.start_run():
+        info = mlflow.pyfunc.log_model("my_model", python_model=TraceModel())
+
+    mlflow.evaluate(model=info.model_uri, data=[[1, 2, 3]], model_type="regressor", targets=[1])
+    trace = mlflow.get_last_active_trace()
+    assert trace.info.request_metadata[TraceMetadataKey.MODEL_ID] == info.model_id
