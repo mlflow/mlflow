c43468a2ad1378ffb5634facf5f117b7206ee0d2
Daniel Lok <daniel.lok@databricks.com>
2025-03-03 10:39:43 +0800

    Delete unnecessary code from design system library (#14784)
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/server/js/vendor/design-system/src/design-system/Skeleton/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './GenericSkeleton';
-export * from './ParagraphSkeleton';
-export * from './TitleSkeleton';
-export * from './TableSkeleton';
--- mlflow/server/js/vendor/design-system/src/design-system/Tabs/index.tsx
+++
@@ -1,2 +0,0 @@
-export * as Tabs from './Tabs';
-export { type TriggerProps } from './Tabs';
