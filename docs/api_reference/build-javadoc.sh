da814bf4fc9a437f699cf36164ac14db878cbfe8
Daniel Lok <daniel.lok@databricks.com>
2025-03-04 14:56:20 +0800

    Update docs/api_reference/build-javadoc.sh
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- docs/api_reference/build-javadoc.sh
+++ docs/api_reference/build-javadoc.sh
@@ -4,6 +4,9 @@
 
 set -ex
 pushd ../../mlflow/java/client/
+# the MAVEN_JAVADOC_ARGS env var is used to dynamically pass
+# args to the mvn command. this can be used to direct maven to use
+# a mirror, in case we encounter rate limiting from maven central
 mvn clean javadoc:javadoc ${MAVEN_JAVADOC_ARGS} -q
 popd
 rm -rf build/html/java_api/
