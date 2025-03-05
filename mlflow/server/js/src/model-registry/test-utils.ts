90166a400cb55a80d8fc8a7c47fc7f6bd7ac3d9e
Keita Nonaka <iKonnyaku40@gmail.com>
2025-03-05 08:39:02 -0800

    Revert "fix: type fixes in `mlflow/server/js/src/model-registry/test-utils.ts`"
    This reverts commit 7ba82dbfe69c904c2dfec1e555e082aace368cca.
    
    Signed-off-by: Keita Nonaka <iKonnyaku40@gmail.com>

--- mlflow/server/js/src/model-registry/test-utils.ts
+++ mlflow/server/js/src/model-registry/test-utils.ts
@@ -1,3 +1,10 @@
+/**
+ * NOTE: this code file was automatically migrated to TypeScript using ts-migrate and
+ * may contain multiple `any` type annotations and `@ts-expect-error` directives.
+ * If possible, please improve types while making changes to this file. If the type
+ * annotations are already looking good, please remove this comment.
+ */
+
 /* prettier-ignore */
 export const mockRegisteredModelDetailed = (
   name: any,
@@ -16,10 +23,10 @@ export const mockRegisteredModelDetailed = (
 };
 
 export const mockModelVersionDetailed = (
-  name: string,
-  version: number,
-  stage: string,
-  status: string,
+  name: any,
+  version: any,
+  stage: any,
+  status: any,
   tags = [],
   run_link = undefined,
   run_id = 'b99a0fc567ae4d32994392c800c0b6ce',
@@ -44,7 +51,7 @@ export const mockModelVersionDetailed = (
   };
 };
 
-export const mockGetFieldValue = (comment: string, archive: boolean | undefined) => {
+export const mockGetFieldValue = (comment: any, archive: any) => {
   return (key: any) => {
     if (key === 'comment') {
       return comment;
