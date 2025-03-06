4d598d0916e21478363886c3ed492188f134d3d0
Keita Nonaka <iKonnyaku40@gmail.com>
2025-03-06 02:14:10 -0800

    fix: remove optional chains
    Signed-off-by: Keita Nonaka <iKonnyaku40@gmail.com>

--- mlflow/server/js/src/common/forms/validations.ts
+++ mlflow/server/js/src/common/forms/validations.ts
@@ -6,32 +6,33 @@ export const getExperimentNameValidator = (getExistingExperimentNames: () => str
     if (!value) {
       // no need to execute below validations when no value is entered
       // eslint-disable-next-line callback-return
-      callback?.(undefined);
+      callback(undefined);
     } else if (getExistingExperimentNames().includes(value)) {
       // getExistingExperimentNames returns the names of all active experiments
       // check whether the passed value is part of the list
       // eslint-disable-next-line callback-return
-      callback?.(`Experiment "${value}" already exists.`);
+      callback(`Experiment "${value}" already exists.`);
     } else {
       // on-demand validation whether experiment already exists in deleted state
       MlflowService.getExperimentByName({ experiment_name: value })
         .then((res) =>
-          callback?.(`Experiment "${value}" already exists in deleted state.
+          callback(`Experiment "${value}" already exists in deleted state.
                                  You can restore the experiment, or permanently delete the
                                  experiment from the .trash folder (under tracking server's
                                  root folder) in order to use this experiment name again.`),
         )
-        .catch((e) => callback?.(undefined)); // no experiment returned
+        .catch((e) => callback(undefined)); // no experiment returned
     }
   };
 };
 
-export const modelNameValidator = (rule: unknown, name?: string, callback?: (arg?: string) => void) => {
+export const modelNameValidator = (rule: unknown, name: string | undefined, callback: (arg?: string) => void) => {
   if (!name) {
-    return callback?.(undefined);
+    callback(undefined);
+    return;
   }
 
   ModelRegistryService.getRegisteredModel({ name: name })
-    .then(() => callback?.(`Model "${name}" already exists.`))
-    .catch((e) => callback?.(undefined));
+    .then(() => callback(`Model "${name}" already exists.`))
+    .catch((e) => callback(undefined));
 };
