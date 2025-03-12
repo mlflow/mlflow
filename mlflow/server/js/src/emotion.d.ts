acaf2baa6890baa24d1f91c0b26b8a697db1a078
Keita Nonaka <iKonnyaku40@gmail.com>
2025-03-12 09:59:23 -0700

    fix: eslint
    Signed-off-by: Keita Nonaka <iKonnyaku40@gmail.com>

--- mlflow/server/js/src/emotion.d.ts
+++ mlflow/server/js/src/emotion.d.ts
@@ -4,6 +4,6 @@ import { DesignSystemThemeInterface } from '@databricks/design-system';
 type ThemeType = DesignSystemThemeInterface['theme'];
 
 declare module '@emotion/react' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/no-empty-object-type
   export interface Theme extends ThemeType {}
 }
