0d4d813b20554cfecad281d8323aeccde40acccb
Brian Lee <brian.kihoon.lee@gmail.com>
2025-03-13 07:42:33 -0400

    Update Assessment to allow Feedback/Expectation to also have an Error (#14892)
    Signed-off-by: brilee <brian.kihoon.lee@gmail.com>

--- tests/entities/test_assessment.py
+++ tests/entities/test_assessment.py
@@ -36,7 +36,7 @@ def test_assessment_creation():
     assessment_with_error = Assessment(
         **{
             **default_params,
-            "feedback": None,
+            "feedback": Feedback(None),
             "error": AssessmentError(error_code="E001", error_message="An error occurred."),
         }
     )
@@ -79,6 +79,7 @@ def test_assessment_equality():
     )
     assessment_5 = Assessment(
         source=source_1,
+        feedback=Feedback(None),
         error=AssessmentError(
             error_code="E002",
             error_message="A different error occurred.",
@@ -87,6 +88,7 @@ def test_assessment_equality():
     )
     assessment_6 = Assessment(
         source=source_1,
+        feedback=Feedback(None),
         error=AssessmentError(
             error_code="E001",
             error_message="Another error message.",
@@ -114,16 +116,39 @@ def test_assessment_value_validation():
     # Valid cases
     Assessment(expectation=Expectation("MLflow"), **common_args)
     Assessment(feedback=Feedback("This is correct."), **common_args)
+    Assessment(feedback=Feedback(None), error=AssessmentError(error_code="E001"), **common_args)
+    Assessment(
+        feedback=Feedback("This is correct."),
+        error=AssessmentError(error_code="E001"),
+        **common_args,
+    )
 
     # Invalid case: no value specified
     with pytest.raises(MlflowException, match=r"Exactly one of"):
         Assessment(**common_args)
 
-    # Invalid case: both value and error specified
+    # Invalid case: both feedback and expectation specified
     with pytest.raises(MlflowException, match=r"Exactly one of"):
         Assessment(
             expectation=Expectation("MLflow"),
-            error=AssessmentError(error_code="E001", error_message="An error occurred."),
+            feedback=Feedback("This is correct."),
+            **common_args,
+        )
+
+    # Invalid case: Expectation with an error
+    with pytest.raises(MlflowException, match=r"Expectations cannot have"):
+        Assessment(
+            expectation=Expectation("MLflow"),
+            error=AssessmentError(error_code="E001"),
+            **common_args,
+        )
+
+    # Invalid case: All three are set
+    with pytest.raises(MlflowException, match=r"Exactly one of"):
+        Assessment(
+            expectation=Expectation("MLflow"),
+            feedback=Feedback("This is correct."),
+            error=AssessmentError(error_code="E001"),
             **common_args,
         )
 
@@ -133,8 +158,12 @@ def test_assessment_value_validation():
     [
         (Expectation("MLflow"), None, None),
         (None, Feedback("This is correct."), None),
-        (None, None, AssessmentError(error_code="E001")),
-        (None, None, AssessmentError(error_code="E001", error_message="An error occurred.")),
+        (None, Feedback(None), AssessmentError(error_code="E001")),
+        (
+            None,
+            Feedback(None),
+            AssessmentError(error_code="E001", error_message="An error occurred."),
+        ),
     ],
 )
 @pytest.mark.parametrize(
