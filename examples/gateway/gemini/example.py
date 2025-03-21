3b1796af133763a9ec25c0b2650f0feb8659d15c
joelrobin18 <joelrobin1818@gmail.com>
2025-03-22 01:41:35 +0530

    Completion for Gemini AI Gateway
    Signed-off-by: joelrobin18 <joelrobin1818@gmail.com>

--- examples/gateway/gemini/example.py
+++ examples/gateway/gemini/example.py
@@ -5,7 +5,22 @@ def main():
     client = get_deploy_client("http://localhost:7000")
 
     print(f"Gemini endpoints: {client.list_endpoints()}\n")
-    print(f"Gemini Embeddings endpoint info: {client.get_endpoint(endpoint='embeddings')}\n")
+    print(f"Gemini completions endpoint info: {client.get_endpoint(endpoint='completions')}\n")
+
+    # Completions request
+    response_completions = client.predict(
+        endpoint="completions",
+        inputs={
+            "prompt": "Describe the main differences between renewable and nonrenewable energy sources.",
+            "temperature": 0.1,
+            "stop": ["."],
+            "n": 3,
+            "max_tokens": 100,
+            "top_k": 40,
+            "top_p": 0.5,
+        },
+    )
+    print(f"Gemini response for completions: {response_completions}")
 
     # Embeddings request
     # TODO: Replace this example with chat completion once completed
--- mlflow/gateway/providers/gemini.py
+++ mlflow/gateway/providers/gemini.py
@@ -1,12 +1,179 @@
+import time
 from typing import Any
 
 from mlflow.gateway.config import GeminiConfig, RouteConfig
+from mlflow.gateway.exceptions import AIGatewayException
 from mlflow.gateway.providers.base import BaseProvider, ProviderAdapter
-from mlflow.gateway.providers.utils import send_request
-from mlflow.gateway.schemas import embeddings
+from mlflow.gateway.providers.utils import rename_payload_keys, send_request
+from mlflow.gateway.schemas import completions, embeddings
 
 
 class GeminiAdapter(ProviderAdapter):
+    @classmethod
+    def chat_to_model(cls, payload, config):
+        # Documentation: https://ai.google.dev/api/generate-content
+        # Example payload for the chat API.
+        #
+        # {
+        #     "contents": [
+        #         {
+        #             "role": "model",
+        #             "parts": [
+        #                 {
+        #                     "text": "System: You are an advanced AI Assistant"
+        #                 }
+        #             ]
+        #         },
+        #         {
+        #             "role": "user",
+        #             "parts": [
+        #                 {
+        #                     "text": "Please give the code for addition of two numbers"
+        #                 }
+        #             ]
+        #         }
+        #     ],
+        #     "generationConfig": {
+        #         "temperature": 0.1,
+        #         "topP": 1,
+        #         "stopSequences": ["\n"],
+        #         "candidateCount": 1,
+        #         "maxOutputTokens": 100,
+        #         "topK": 40
+        #     }
+        # }
+        key_mapping = {
+            "stop": "stopSequences",
+            "n": "candidateCount",
+            "max_tokens": "maxOutputTokens",
+            "top_k": "topK",
+            "top_p": "topP",
+        }
+
+        for k1, k2 in key_mapping.items():
+            if k2 in payload:
+                raise AIGatewayException(
+                    status_code=422, detail=f"Invalid parameter {k2}. Use {k1} instead."
+                )
+
+        if "top_p" in payload and payload["top_p"] > 1:
+            raise AIGatewayException(
+                status_code=422, detail="top_p should be less than or equal to 1"
+            )
+
+        payload = rename_payload_keys(payload, key_mapping)
+
+        contents = []
+        for message in payload["messages"]:
+            role = message["role"]
+
+            if role == "assistant":
+                role = "model"
+            elif role == "system":
+                role = "user"
+                message["content"] = f"System: {message['content']}"
+
+            contents.append({"role": role, "parts": [{"text": message["content"]}]})
+
+        gemini_payload = {
+            "contents": contents,
+        }
+
+        generation_config = {}
+        for param in [
+            "temperature",
+            "topP",
+            "stopSequences",
+            "candidateCount",
+            "maxOutputTokens",
+            "topK",
+        ]:
+            if param in payload:
+                generation_config[param] = payload[param]
+
+        if generation_config:
+            gemini_payload["generationConfig"] = generation_config
+
+        return gemini_payload
+
+    @classmethod
+    def completions_to_model(cls, payload, config):
+        # Documentation: https://ai.google.dev/api/generate-content
+        # Example payload for the completions API.
+        #
+        # {
+        #     "prompt": "What is the world record for flapjack consumption in a single sitting?",
+        #     "temperature": 0.1,
+        #     "topP": 1,
+        #     "stop": ["\n"],
+        #     "n": 1,
+        #     "max_tokens": 100,
+        #     "top_k": 40,
+        # }
+
+        chat_payload = {"messages": [{"role": "user", "content": payload.pop("prompt")}], **payload}
+        return cls.chat_to_model(chat_payload, config)
+
+    @classmethod
+    def model_to_completions(cls, resp, config):
+        # Documentation: https://ai.google.dev/api/generate-content
+        #
+        # Example Response:
+        # {
+        #   "candidates": [
+        #     {
+        #       "content": {
+        #         "parts": [
+        #           {
+        #             "text": "Blue is often seen as a calming and soothing color."
+        #           }
+        #         ]
+        #       },
+        #       "finishReason": "stop"
+        #     }
+        #   ],
+        #   "usageMetadata": {
+        #     "promptTokenCount": 10,
+        #     "candidatesTokenCount": 10,
+        #     "totalTokenCount": 20
+        #   }
+        # }
+        choices = []
+
+        for idx, candidate in enumerate(resp.get("candidates", [])):
+            text = ""
+            if "content" in candidate and candidate.get("content", {}).get("parts", {}):
+                text = candidate["content"]["parts"][0].get("text", "")
+
+            finish_reason = candidate.get("finishReason", "stop")
+            if finish_reason == "MAX_TOKENS":
+                finish_reason = "length"
+
+            choices.append(
+                completions.Choice(
+                    index=idx,
+                    text=text,
+                    finish_reason=finish_reason,
+                )
+            )
+
+        usage_metadata = resp.get("usageMetadata", {})
+        prompt_tokens = usage_metadata.get("promptTokenCount", None)
+        completion_tokens = usage_metadata.get("candidatesTokenCount", None)
+        total_tokens = usage_metadata.get("totalTokenCount", None)
+
+        return completions.ResponsePayload(
+            created=int(time.time()),
+            object="text_completion",
+            model=config.model.name,
+            choices=choices,
+            usage=completions.CompletionsUsage(
+                prompt_tokens=prompt_tokens,
+                completion_tokens=completion_tokens,
+                total_tokens=total_tokens,
+            ),
+        )
+
     @classmethod
     def embeddings_to_model(cls, payload, config):
         # Example payload for the embedding API.
@@ -148,3 +315,26 @@ class GeminiProvider(BaseProvider):
             embedding_payload,
         )
         return self.adapter_class.model_to_embeddings(resp, self.config)
+
+    async def completions(self, payload: completions.RequestPayload) -> completions.ResponsePayload:
+        from fastapi.encoders import jsonable_encoder
+
+        payload = jsonable_encoder(payload, exclude_none=True)
+        self.check_for_model_field(payload)
+
+        completions_payload = self.adapter_class.completions_to_model(payload, self.config)
+        # Documentation: https://ai.google.dev/api/generate-content
+
+        if payload.get("stream", False):
+            # TODO: Implement streaming for completions
+            raise AIGatewayException(
+                status_code=422,
+                detail="Streaming is not yet supported for completions with Gemini AI Gateway",
+            )
+
+        resp = await self._request(
+            f"{self.config.model.name}:generateContent",
+            completions_payload,
+        )
+
+        return self.adapter_class.model_to_completions(resp, self.config)
