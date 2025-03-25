824303461b4b6de646f607e7fbc03b9428b467f9
bbqiu <bryan.qiu@databricks.com>
2025-03-25 19:05:48 -0400

    wip
    Signed-off-by: bbqiu <bryan.qiu@databricks.com>

--- mlflow/types/responses.py
+++ mlflow/types/responses.py
@@ -1,13 +1,35 @@
+from typing import Any, Literal, Optional
+
+from openai.types.responses.response import Response
+
 from mlflow.types.chat import BaseModel
 
 
-class ResponsesRequest(BaseModel):
+class BaseRequestPayload(BaseModel):
     pass
 
 
-class ResponsesResponse(BaseModel):
+class ResponseInputParam(BaseModel):
     pass
 
 
-class ResponsesStreamingEvent(BaseModel):
+class FunctionTool(BaseModel):
+    name: str
+    parameters: dict[str, object]
+    strict: bool
+    type: Literal["function"]
+    description: Optional[str] = None
+
+
+class ResponsesRequest(BaseRequestPayload):
+    input: list[ResponseInputParam]
+    tools: Optional[list[FunctionTool]] = None
+    custom_inputs: dict[str, Any]
+
+
+class ResponsesResponse(Response):
+    custom_outputs: dict[str, Any]
+
+
+class ResponsesStreamEvent(BaseModel):
     pass
