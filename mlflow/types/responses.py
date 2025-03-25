1f4647af7d4ac39e5e0f1a07839a8001f2cc1a93
bbqiu <bryan.qiu@databricks.com>
2025-03-25 17:13:58 -0400

    .
    Signed-off-by: bbqiu <bryan.qiu@databricks.com>

---
+++ mlflow/types/responses.py
@@ -0,0 +1,13 @@
+from mlflow.types.chat import BaseModel
+
+
+class ResponsesRequest(BaseModel):
+    pass
+
+
+class ResponsesResponse(BaseModel):
+    pass
+
+
+class ResponsesStreamingEvent(BaseModel):
+    pass
