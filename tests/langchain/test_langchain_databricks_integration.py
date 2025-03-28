28b1c34d94bc984a2079a615f24f11571bcf50c2
Roshni Malani <roshni.malani@databricks.com>
2025-03-28 12:10:45 -0700

    manually add databricks-langchain as pip requirements if it is not being inferred
    Signed-off-by: Roshni Malani <roshni.malani@databricks.com>

--- tests/langchain/test_langchain_databricks_integration.py
+++ tests/langchain/test_langchain_databricks_integration.py
@@ -54,7 +54,8 @@ def test_save_and_load_chat_databricks(model_path):
     prompt = PromptTemplate.from_template("What is {product}?")
     chain = prompt | llm | StrOutputParser()
 
-    mlflow.langchain.save_model(chain, path=model_path)
+    mlflow.langchain.save_model(chain, path=model_path,
+        extra_pip_requirements=["databricks-langchain"])
 
     with model_path.joinpath("requirements.txt").open() as f:
         reqs = {req.split("==")[0] for req in f.read().split("\n")}
