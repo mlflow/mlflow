d6cbaf90495eea1c3de61bc88cb81e078b7d3f39
Richard Zang <Zangr@users.noreply.github.com>
2025-03-31 10:09:01 +0200

    Project import generated by Copybara.
    GitOrigin-RevId: 47b35b49a1dae6cbd23ad08a727bc74e4c890a74
    Signed-off-by: Hubert Zub <hubert.zub@databricks.com>

--- mlflow/server/js/vendor/design-system/dist/theme/_generated/BorderRadii.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const borderRadii: {
-    borderRadius0: number;
-    borderRadiusSm: number;
-    borderRadiusMd: number;
-    borderRadiusLg: number;
-    borderRadiusFull: number;
-};
-//# sourceMappingURL=BorderRadii.d.ts.map
