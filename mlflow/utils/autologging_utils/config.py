5f4cc51e8af65de0df9b21d5e3c528e73fd01c93
Serena Ruan <82044803+serena-ruan@users.noreply.github.com>
2025-04-04 13:44:03 +0800

    [llama_index] create LoggedModel in autolog (#15106)
    Signed-off-by: serena-ruan <serena.rxy@gmail.com>

--- mlflow/utils/autologging_utils/config.py
+++ mlflow/utils/autologging_utils/config.py
@@ -13,17 +13,11 @@ class AutoLoggingConfig:
     A dataclass to hold common autologging configuration options.
     """
 
-    log_models: bool
     log_input_examples: bool
     log_model_signatures: bool
     log_traces: bool
     extra_tags: Optional[dict] = None
-
-    def should_log_optional_artifacts(self):
-        """
-        Check if any optional artifacts should be logged to MLflow.
-        """
-        return self.log_models or self.log_input_examples or self.log_model_signatures
+    log_models: bool = True
 
     @classmethod
     def init(cls, flavor_name: str):
