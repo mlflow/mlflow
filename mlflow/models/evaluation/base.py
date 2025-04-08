e18d384d83f594fa5f5418720e3eff7e49f8fb51
Arthur Jenoudet <arthur.jenoudet@databricks.com>
2025-04-08 11:37:50 -0700

    less verbose
    Signed-off-by: Arthur Jenoudet <arthur.jenoudet@databricks.com>

--- mlflow/models/evaluation/base.py
+++ mlflow/models/evaluation/base.py
@@ -1784,9 +1784,7 @@ def evaluate(  # noqa: D417
 
         # if model_id is specified log metrics to the eval run and logged model
         if model_id is not None:
-            from mlflow import log_metrics
-
-            log_metrics(metrics=evaluate_result.metrics, dataset=data, model_id=model_id)
+            mlflow.log_metrics(metrics=evaluate_result.metrics, dataset=data, model_id=model_id)
 
     # TODO: Remove this block in a future release when we
     # remove the deprecated arguments.
