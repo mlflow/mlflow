66cab67ce4cdb66e2ec2b889cc5990e0c7be8f26
Weichen Xu <weichen.xu@databricks.com>
2025-04-08 17:40:23 +0800

    init
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/ml_package_versions.py
+++ mlflow/ml_package_versions.py
@@ -100,19 +100,6 @@ _ML_PACKAGE_VERSIONS = {
             "maximum": "1.2.7"
         }
     },
-    "fastai": {
-        "package_info": {
-            "pip_release": "fastai"
-        },
-        "models": {
-            "minimum": "2.4.1",
-            "maximum": "2.7.19"
-        },
-        "autologging": {
-            "minimum": "2.4.1",
-            "maximum": "2.7.19"
-        }
-    },
     "onnx": {
         "package_info": {
             "pip_release": "onnx"
