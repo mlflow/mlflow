cccb88b0902c99092165635640ac68fddd57308a
Dhruv Yadav <dhruyads@gmail.com>
2025-04-10 12:47:58 -0700

    lint fixes
    Signed-off-by: Dhruv Yadav <dhruyads@gmail.com>

--- mlflow/genai/scorers/base.py
+++ mlflow/genai/scorers/base.py
@@ -1,10 +1,13 @@
+import functools
+import inspect
+import types
+from typing import Any, Callable, Literal, Optional, Union
+
 from pydantic import BaseModel
-from typing import Any, Optional, Union, Literal, Callable
+
 from mlflow.entities import Trace
 from mlflow.evaluation import Assessment
-import inspect
-import functools
-import types
+
 
 class Scorer(BaseModel):
     name: str
@@ -22,6 +25,7 @@ class Scorer(BaseModel):
         """
         raise NotImplementedError("Please use an instance of BuiltInScorer")
 
+
 # TODO: ML-52304: Inherit the following class for every builtin scorer made available
 class BuiltInScorer(Scorer):
     def __call__(evaluation_config) -> dict:
@@ -32,16 +36,20 @@ class BuiltInScorer(Scorer):
         https://docs.databricks.com/aws/en/generative-ai/agent-evaluation/llm-judge-reference
         """
         raise NotImplementedError("Please use an instance of BuiltInScorer")
-    
+
+
 def scorer(
-        func=None,
-        *,
-        name: Optional[str] = None,
-        aggregations: list[Union[Literal["min", "max", "mean", "median", "variance", "p90", "p99"], Callable]] = None
-    ):
+    func=None,
+    *,
+    name: Optional[str] = None,
+    aggregations: Optional[
+        list[Union[Literal["min", "max", "mean", "median", "variance", "p90", "p99"], Callable]]
+    ] = None,
+):
     """
     Syntactic sugar that ensures the decorated function has the correct parameters (with inputs
-    as required, outputs, expectations, trace as optional) and returns float | bool | str | Assessment.
+    as required, outputs, expectations, trace as optional) and returns float | bool |
+    str | Assessment.
     """
 
     if func is None:
@@ -52,7 +60,9 @@ def scorer(
 
     # Verify required param presence
     if "inputs" not in params:
-        raise TypeError(f"{func.__name__} must accept 'inputs' as a required keyword-only argument.")
+        raise TypeError(
+            f"{func.__name__} must accept 'inputs' as a required keyword-only argument."
+        )
 
     # Check param details
     required_param = params["inputs"]
@@ -70,11 +80,14 @@ def scorer(
         result = func(inputs=inputs, outputs=outputs, expectations=expectations, trace=trace)
         if not isinstance(result, (float, bool, str, Assessment)):
             raise ValueError(
-                f"{func.__name__} must return one of float, bool, str, or Assessment. Got {type(result).__name__}"
+                (
+                    f"{func.__name__} must return one of float, bool, str, ",
+                    "or Assessment. Got {type(result).__name__}",
+                )
             )
         return result
 
     instance = Scorer(name=name or func.__name__)
     instance.__call__ = types.MethodType(wrapped_call, instance)
-    
+
     return instance
--- mlflow/genai/scorers/dummy_builtin_scorer.py
+++ mlflow/genai/scorers/dummy_builtin_scorer.py
@@ -1,7 +1,7 @@
 from mlflow.genai.scorers import BuiltInScorer
 
+
 # TODO: ML-52304 example builtin scorer implementation
 class DummyBuiltInScorer(BuiltInScorer):
     def __call__(evaluation_config) -> dict:
         evaluation_config["databricks-agents"]["metrics"].append("dummy_metric")
-    
