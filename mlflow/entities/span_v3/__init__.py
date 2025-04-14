aae8ef098660a80be1aa29f53a5fcabcefc1cb98
harupy <17039389+harupy@users.noreply.github.com>
2025-04-14 21:58:53 +0900

    Update __all__
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/entities/span_v3/__init__.py
+++ mlflow/entities/span_v3/__init__.py
@@ -1,4 +1,9 @@
 from mlflow.entities.span_v3.span_event import SpanEvent
 from mlflow.entities.span_v3.span_kind import SpanKind
 
-__all__ = ["SpanEvent", "SpanKind"]
+__all__ = [
+    "SpanEvent",
+    "SpanKind",
+    "SpanStatus",
+    "SpanStatusCode",
+]
