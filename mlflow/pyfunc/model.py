51e13f914c891962cfcb7ee6058cedb8f3853077
serena-ruan <serena.rxy@gmail.com>
2025-04-14 11:10:39 +0800

    fix
    Signed-off-by: serena-ruan <serena.rxy@gmail.com>

--- mlflow/pyfunc/model.py
+++ mlflow/pyfunc/model.py
@@ -942,6 +942,9 @@ def _save_model_with_class_artifacts_params(  # noqa: D417
     )
     if size := get_total_file_size(path):
         mlflow_model.model_size_bytes = size
+    # `mlflow_model.save` must be called before _validate_infer_and_copy_code_paths as it
+    # internally infers dependency, and MLmodel file is required to successfully load the model
+    mlflow_model.save(os.path.join(path, MLMODEL_FILE_NAME))
 
     saved_code_subpath = _validate_infer_and_copy_code_paths(
         code_paths,
@@ -951,6 +954,7 @@ def _save_model_with_class_artifacts_params(  # noqa: D417
     )
     mlflow_model.flavors[mlflow.pyfunc.FLAVOR_NAME][mlflow.pyfunc.CODE] = saved_code_subpath
 
+    # `mlflow_model.code` is updated, re-generate `MLmodel` file.
     mlflow_model.save(os.path.join(path, MLMODEL_FILE_NAME))
 
     if conda_env is None:
