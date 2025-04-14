05f3e68d548d7f6cad2d3c5f96abca604fed94df
harupy <17039389+harupy@users.noreply.github.com>
2025-04-14 21:59:09 +0900

    Add missing imports
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/entities/span_v3/__init__.py
+++ mlflow/entities/span_v3/__init__.py
@@ -1,5 +1,7 @@
 from mlflow.entities.span_v3.span_event import SpanEvent
 from mlflow.entities.span_v3.span_kind import SpanKind
+from mlflow.entities.span_v3.span_status import SpanStatus
+from mlflow.entities.span_v3.span_status_code import SpanStatusCode
 
 __all__ = [
     "SpanEvent",
