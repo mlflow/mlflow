b907184c595dae2ff55348d9daf2ea3b74864413
Weichen Xu <weichen.xu@databricks.com>
2025-04-23 13:57:29 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/metrics/genai/model_utils.py
+++ mlflow/metrics/genai/model_utils.py
@@ -325,32 +325,6 @@ def call_deployments_api(
     return _parse_response(response, endpoint_type)
 
 
-def _call_gateway_api(gateway_uri, payload, eval_parameters):
-    from mlflow.gateway import get_route, query
-
-    route_info = get_route(gateway_uri).dict()
-    if route_info["endpoint_type"] == "llm/v1/completions":
-        completions_payload = {
-            "prompt": payload,
-            **eval_parameters,
-        }
-        response = query(gateway_uri, completions_payload)
-        return _parse_completions_response_format(response)
-    elif route_info["endpoint_type"] == "llm/v1/chat":
-        chat_payload = {
-            "messages": [{"role": "user", "content": payload}],
-            **eval_parameters,
-        }
-        response = query(gateway_uri, chat_payload)
-        return _parse_chat_response_format(response)
-    else:
-        raise MlflowException(
-            f"Unsupported gateway route type: {route_info['endpoint_type']}. Use a "
-            "route of type 'llm/v1/completions' or 'llm/v1/chat' instead.",
-            error_code=INVALID_PARAMETER_VALUE,
-        )
-
-
 def _construct_payload_from_str(prompt: str, endpoint_type: str) -> dict[str, Any]:
     """
     Construct the payload from the input string based on the endpoint type.
