893e740947265fc29fd0760385152c4b67773ae6
Weichen Xu <weichen.xu@databricks.com>
2025-04-23 18:51:51 +0800

    remove gateway client
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/gateway/__init__.py
+++ mlflow/gateway/__init__.py
@@ -1,8 +1,6 @@
-from mlflow.gateway.client import MlflowGatewayClient
 from mlflow.gateway.utils import get_gateway_uri, set_gateway_uri
 
 __all__ = [
     "get_gateway_uri",
-    "MlflowGatewayClient",
     "set_gateway_uri",
 ]
