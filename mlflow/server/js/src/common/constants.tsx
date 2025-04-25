dd1538ab7cb747ec37cc2770f8275b9ca11e369d
harupy <17039389+harupy@users.noreply.github.com>
2025-04-25 17:53:26 +0900

    Bump versions
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/server/js/src/common/constants.tsx
+++ mlflow/server/js/src/common/constants.tsx
@@ -9,7 +9,7 @@ export const ErrorCodes = {
   RESOURCE_CONFLICT: 'RESOURCE_CONFLICT',
 };
 
-export const Version = '2.22.1.dev0';
+export const Version = '3.0.0rc1';
 
 const DOCS_VERSION = 'latest';
 
--- mlflow/version.py
+++ mlflow/version.py
@@ -1,7 +1,7 @@
 # Copyright 2018 Databricks, Inc.
 import re
 
-VERSION = "2.22.1.dev0"
+VERSION = "3.0.0rc1"
 
 
 def is_release_version():
