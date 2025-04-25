6f4dffe3853e69a23413ef240e0c2c277ca7dd63
Daniel Lok <daniel.lok@databricks.com>
2025-04-25 11:55:03 +0800

    fix
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/server/js/src/experiment-tracking/components/traces/TracesViewTablePreviewCell.tsx
+++ mlflow/server/js/src/experiment-tracking/components/traces/TracesViewTablePreviewCell.tsx
@@ -117,7 +117,7 @@ const ExpandedParamCell = ({ value }: { value: string }) => {
       }}
     >
       <CodeSnippet
-        language={structuredJSONValue ? "json" : "text"}
+        language={structuredJSONValue ? 'json' : 'text'}
         wrapLongLines
         style={{
           padding: theme.spacing.sm,
