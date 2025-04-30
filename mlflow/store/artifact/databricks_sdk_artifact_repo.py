66d3e45d7def1a24daac22d4fe595a66d5bdc6f8
harupy <17039389+harupy@users.noreply.github.com>
2025-04-30 19:06:43 +0900

    test
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/store/artifact/databricks_sdk_artifact_repo.py
+++ mlflow/store/artifact/databricks_sdk_artifact_repo.py
@@ -30,6 +30,7 @@ class DatabricksSdkArtifactRepository(ArtifactRepository):
         from databricks.sdk.config import Config
 
         super().__init__(artifact_uri)
+        print(os.environ.get("DATABRICKS_MULTIPART_UPLOAD_BATCH_URL_COUNT"))  # noqa: T201
         self.wc = WorkspaceClient(
             config=(
                 Config(
