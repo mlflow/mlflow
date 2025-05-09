013493c4f8b07d17e9cd545cc59de6a7044b5584
joelrobin18 <joelrobin1818@gmail.com>
2025-05-09 17:27:54 +0530

    Lint Fix
    Signed-off-by: joelrobin18 <joelrobin1818@gmail.com>

--- mlflow/tracking/client.py
+++ mlflow/tracking/client.py
@@ -584,9 +584,10 @@ class MlflowClient:
                 to retrieve the next page of results.  Defaults to `None`.
 
         Returns:
+
             :py:class:`Prompt <mlflow.entities.model_registry.Prompt>`:
-                A pageable list of :py:class:`Prompt <mlflow.entities.model_registry.Prompt>` 
-                entities representing prompt templates. Inspect the returned object's 
+                A pageable list of :py:class:`Prompt <mlflow.entities.model_registry.Prompt>`
+                entities representing prompt templates. Inspect the returned object's
                 `.token` attribute to fetch subsequent pages.
         """
         fls = f"tag.`{IS_PROMPT_TAG_KEY}` = 'true'"
