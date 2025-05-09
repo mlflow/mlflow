b74d4f4f133de2aa9097768ddd7b9964e8f0a3bd
B-Step62 <yuki.watanabe@databricks.com>
2025-05-09 18:11:04 +0900

    Avoid using short timeout for trace logging for read path
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- mlflow/environment_variables.py
+++ mlflow/environment_variables.py
@@ -750,9 +750,9 @@ MLFLOW_ASYNC_TRACE_LOGGING_MAX_QUEUE_SIZE = _EnvironmentVariable(
 
 
 #: Timeout seconds for retrying trace logging.
-#: (default: ``5``)
+#: (default: ``15``)
 MLFLOW_ASYNC_TRACE_LOGGING_RETRY_TIMEOUT = _EnvironmentVariable(
-    "MLFLOW_ASYNC_TRACE_LOGGING_RETRY_TIMEOUT", int, 5
+    "MLFLOW_ASYNC_TRACE_LOGGING_RETRY_TIMEOUT", int, 15
 )
 
 
