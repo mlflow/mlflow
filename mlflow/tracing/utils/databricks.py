72aa4cd1c8282cd8ecd8725501eb0f2df9bb49f4
dbczumar <corey.zumar@databricks.com>
2025-05-09 19:57:26 -0700

    fix
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/tracing/utils/databricks.py
+++ mlflow/tracing/utils/databricks.py
@@ -44,16 +44,14 @@ def get_full_traces_databricks(trace_infos: list[TraceInfo]):
     full_traces = []
     for trace_info in trace_infos:
         trace_data = trace_ids_and_data.get(trace_info.request_id)
-        if trace_data is None:
+        if trace_data is not None:
+            trace_data = TraceData.from_dict(trace_data)
+        else:
             _logger.warning(
                 f"Trace data not found for trace ID {trace_info.request_id}. "
                 "This may indicate a failure in the download process."
             )
 
-        full_trace = Trace(
-            info=trace_info,
-            data=TraceData.from_dict(trace_data),
-        )
-        full_traces.append(full_trace)
+        full_traces.append(Trace(info=trace_info, data=trace_data))
 
     return full_traces
