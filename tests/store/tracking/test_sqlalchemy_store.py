ecd7864adf14f054228727fa26e966a56f3f69dc
harupy <17039389+harupy@users.noreply.github.com>
2025-05-12 13:21:17 +0900

    test
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/store/tracking/test_sqlalchemy_store.py
+++ tests/store/tracking/test_sqlalchemy_store.py
@@ -4876,7 +4876,6 @@ def test_delete_logged_model_tag(store: SqlAlchemyStore):
 
 
 def test_search_logged_models(store: SqlAlchemyStore):
-    # TODO: Support filtering, ordering, and pagination
     exp_id_1 = store.create_experiment(f"exp-{uuid.uuid4()}")
 
     model_1 = store.create_logged_model(experiment_id=exp_id_1)
@@ -4925,6 +4924,34 @@ def test_search_logged_models_filter_string(store: SqlAlchemyStore):
     )
     assert [m.name for m in models] == [model_1.name]
     assert models.token is None
+
+    for val in (
+        # A single item without a comma
+        f"('{model_1.name}')",
+        # A single item with a comma
+        f"('{model_1.name}',)",
+        # Multiple items
+        f"('{model_1.name}', 'foo')",
+    ):
+        # IN
+        models = store.search_logged_models(
+            experiment_ids=[exp_id_1],
+            filter_string=f"name IN {val}",
+        )
+        assert [m.name for m in models] == [model_1.name]
+        assert models.token is None
+        # NOT IN
+        models = store.search_logged_models(
+            experiment_ids=[exp_id_1],
+            filter_string=f"name NOT IN {val}",
+        )
+        assert [m.name for m in models] == []
+
+    models = store.search_logged_models(
+        experiment_ids=[exp_id_1],
+        filter_string=f"name NOT IN ('{model_1.name}', 'foo')",
+    )
+    assert [m.name for m in models] == []
 
     # Search by numeric attribute
     models = store.search_logged_models(
