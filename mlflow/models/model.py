612a70f3183ba287da2a604b0c70836c1f1562fa
Shaylan Dias <shaylandias@gmail.com>
2025-05-11 19:06:13 -0700

    Print a link to a UC registered model when logging it (#15633)
    Signed-off-by: Shaylan Dias <shaylandias@gmail.com>

--- mlflow/models/model.py
+++ mlflow/models/model.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os
 import shutil
+import sys
 import uuid
 from datetime import datetime
 from pathlib import Path
@@ -30,7 +31,13 @@ from mlflow.tracking._tracking_service.utils import _resolve_tracking_uri
 from mlflow.tracking.artifact_utils import _download_artifact_from_uri, _upload_artifact_to_uri
 from mlflow.tracking.fluent import _use_logged_model
 from mlflow.utils.annotations import experimental
-from mlflow.utils.databricks_utils import get_databricks_runtime_version, is_in_databricks_runtime
+from mlflow.utils.databricks_utils import (
+    _construct_databricks_uc_registered_model_url,
+    get_databricks_runtime_version,
+    get_workspace_id,
+    get_workspace_url,
+    is_in_databricks_runtime,
+)
 from mlflow.utils.docstring_utils import LOG_MODEL_PARAM_DOCS, format_docstring
 from mlflow.utils.environment import (
     _CONDA_ENV_FILE_NAME,
@@ -46,6 +53,7 @@ from mlflow.utils.mlflow_tags import MLFLOW_MODEL_IS_EXTERNAL
 from mlflow.utils.uri import (
     append_to_uri_path,
     get_uri_scheme,
+    is_databricks_unity_catalog_uri,
 )
 
 _logger = logging.getLogger(__name__)
@@ -1108,6 +1116,21 @@ class Model:
             model_info = mlflow_model.get_model_info(model)
             if registered_model is not None:
                 model_info.registered_model_version = registered_model.version
+
+                # Print a link to the UC model version page if the model is in UC.
+                registry_uri = mlflow.get_registry_uri()
+                if is_databricks_unity_catalog_uri(registry_uri) and (url := get_workspace_url()):
+                    uc_model_url = _construct_databricks_uc_registered_model_url(
+                        url,
+                        registered_model_name,
+                        registered_model.version,
+                        get_workspace_id(),
+                    )
+                    # Use sys.stdout.write to make the link clickable in the UI
+                    sys.stdout.write(
+                        f"🔗 View model version '{registered_model.version}' of "
+                        + f"'{registered_model_name}' in Unity Catalog at: {uc_model_url}\n"
+                    )
 
         # If the model signature is Mosaic AI Agent compatible, render a recipe for evaluation.
         from mlflow.models.display_utils import maybe_render_agent_eval_recipe
