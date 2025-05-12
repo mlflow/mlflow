a788cfc6f633d2a17597094629df18bdf5362d91
Tomu Hirata <tomu.hirata@gmail.com>
2025-05-12 18:34:04 +0900

    replace deprecated min_items with min_length
    Signed-off-by: Tomu Hirata <tomu.hirata@gmail.com>

--- mlflow/gateway/schemas/chat.py
+++ mlflow/gateway/schemas/chat.py
@@ -66,7 +66,7 @@ _REQUEST_PAYLOAD_EXTRA_SCHEMA = {
 
 
 class RequestPayload(ChatCompletionRequest, RequestModel):
-    messages: list[RequestMessage] = Field(..., min_items=1)
+    messages: list[RequestMessage] = Field(..., min_length=1)
     tools: Optional[list[ChatToolWithUC]] = None
 
     class Config:
