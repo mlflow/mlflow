799a6d79221535d2b28f2710b26ef3b44a90bf83
harupy <17039389+harupy@users.noreply.github.com>
2025-05-13 14:51:06 +0900

    Run tests/genai in databricks-agents RC
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/genai/conftest.py
+++ tests/genai/conftest.py
@@ -1,14 +1,14 @@
-# TODO: Remove this file once `databricks-agents` releases a new version that's compatible with
-# trace v3.
-from unittest import mock
+# # TODO: Remove this file once `databricks-agents` releases a new version that's compatible with
+# # trace v3.
+# from unittest import mock
 
-import pytest
+# import pytest
 
-from mlflow.entities import TraceInfoV2
+# from mlflow.entities import TraceInfoV2
 
 
-@pytest.fixture(scope="module", autouse=True)
-def mock_trace_info():
-    # Monkey patch TraceInfo (V3) to use TraceInfoV2
-    with mock.patch("mlflow.entities.TraceInfo", wraps=TraceInfoV2):
-        yield
+# @pytest.fixture(scope="module", autouse=True)
+# def mock_trace_info():
+#     # Monkey patch TraceInfo (V3) to use TraceInfoV2
+#     with mock.patch("mlflow.entities.TraceInfo", wraps=TraceInfoV2):
+#         yield
