fc4c7a12d7ba7ca3fccb65b56fa11f8ccddd6553
Tomu Hirata <tomu.hirata@gmail.com>
2025-05-14 17:13:41 +0900

    rename vars
    Signed-off-by: Tomu Hirata <tomu.hirata@gmail.com>

--- tests/tracing/export/test_async_export_queue.py
+++ tests/tracing/export/test_async_export_queue.py
@@ -12,12 +12,12 @@ def test_async_queue_handle_tasks():
 
     counter = 0
 
-    def _increment(delta):
+    def increment(delta):
         nonlocal counter
         counter += delta
 
     for _ in range(10):
-        task = Task(handler=_increment, args=(1,))
+        task = Task(handler=increment, args=(1,))
         queue.put(task)
 
     queue.flush(terminate=True)
@@ -29,13 +29,13 @@ def exporter_process(counter):
 
     queue = AsyncTraceExportQueue()
 
-    def _increment(counter):
+    def increment(counter):
         time.sleep(1)
         with counter.get_lock():
             counter.value += 1
 
     for _ in range(10):
-        task = Task(handler=_increment, args=(counter,))
+        task = Task(handler=increment, args=(counter,))
         queue.put(task)
 
 
@@ -52,7 +52,7 @@ def test_async_queue_complete_task_process_finished():
 def test_async_queue_activate_thread_safe(mock_atexit):
     queue = AsyncTraceExportQueue()
 
-    def _count_threads():
+    def count_threads():
         main_thread = threading.main_thread()
         return sum(
             t.is_alive()
@@ -66,14 +66,14 @@ def test_async_queue_activate_thread_safe(mock_atexit):
             executor.submit(queue.activate)
 
     assert queue.is_active()
-    assert _count_threads() > 0  # Logging thread + max 5 worker threads
+    assert count_threads() > 0  # Logging thread + max 5 worker threads
     mock_atexit.assert_called_once()
     mock_atexit.reset_mock()
 
     # 2. Validate flush (continue)
     queue.flush(terminate=False)
     assert queue.is_active()
-    assert _count_threads() > 0  # New threads should be created
+    assert count_threads() > 0  # New threads should be created
     mock_atexit.assert_not_called()  # Exit callback should not be registered again
 
     # 3. Validate flush with termination
@@ -82,7 +82,7 @@ def test_async_queue_activate_thread_safe(mock_atexit):
             executor.submit(queue.flush(terminate=True))
 
     assert not queue.is_active()
-    assert _count_threads() == 0
+    assert count_threads() == 0
 
 
 def test_async_queue_drop_task_when_full(monkeypatch):
