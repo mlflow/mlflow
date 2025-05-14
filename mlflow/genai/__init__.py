b0f6dda1b2741b493d6a7b39837c9c4bcfe9f014
harupy <17039389+harupy@users.noreply.github.com>
2025-05-14 16:05:27 +0900

    Expose more items
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/genai/__init__.py
+++ mlflow/genai/__init__.py
@@ -1,4 +1,34 @@
 from mlflow.genai.evaluation import evaluate, to_predict_fn
-from mlflow.genai.scorers import Scorer, scorer
+from mlflow.genai.scorers import (
+    BuiltInScorer,
+    Scorer,
+    chunk_relevance,
+    context_sufficiency,
+    correctness,
+    global_guideline_adherence,
+    groundedness,
+    guideline_adherence,
+    rag_scorers,
+    relevance_to_query,
+    safety,
+    scorer,
+)
 
-__all__ = ["evaluate", "to_predict_fn", "Scorer", "scorer"]
+__all__ = [
+    "evaluate",
+    "to_predict_fn",
+    "Scorer",
+    "scorer",
+    "chunk_relevance",
+    "context_sufficiency",
+    "correctness",
+    "global_guideline_adherence",
+    "groundedness",
+    "guideline_adherence",
+    "rag_scorers",
+    "relevance_to_query",
+    "safety",
+    "BuiltInScorer",
+    "scorer",
+    "chunk_relevance",
+]
