0ddd75fd9a0c364436d6c6b6cc858d401879c924
Samraj Moorjani <samraj.moorjani@databricks.com>
2025-05-20 16:15:01 -0700

    add everything to mlflow.genai
    Signed-off-by: Samraj Moorjani <samraj.moorjani@databricks.com>

--- mlflow/genai/__init__.py
+++ mlflow/genai/__init__.py
@@ -1,9 +1,46 @@
 from mlflow.genai.evaluation import evaluate, to_predict_fn
 from mlflow.genai.scorers import Scorer, scorer
 
+try:
+    from mlflow.genai.labeling import (
+        Agent,
+        LabelingSession,
+        ReviewApp,
+        get_review_app,
+        create_labeling_session,
+        get_labeling_sessions,
+        get_labeling_session,
+        delete_labeling_session,
+    )
+    # Stick this at the end to avoid unnecessary warnings
+    from mlflow.genai.datasets import (
+        EvaluationDataset,
+        create_dataset,
+        delete_dataset,
+        get_dataset,
+    )
+except ImportError:
+    # Silently fail if the databricks-agents package is not installed
+    pass
+
+
 __all__ = [
     "evaluate",
     "to_predict_fn",
     "Scorer",
     "scorer",
+    # Labeling
+    "Agent",
+    "LabelingSession",
+    "ReviewApp",
+    "get_review_app",
+    "create_labeling_session",
+    "get_labeling_sessions",
+    "get_labeling_session",
+    "delete_labeling_session",
+    # Datasets
+    "EvaluationDataset",
+    "create_dataset",
+    "delete_dataset",
+    "get_dataset",
 ]
