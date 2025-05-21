787fb373050516564eafdd792d3b00e357587b63
harupy <17039389+harupy@users.noreply.github.com>
2025-05-21 13:38:47 +0900

    Bump versions to 3.0.0rc3
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/server/js/src/common/constants.tsx
+++ mlflow/server/js/src/common/constants.tsx
@@ -9,7 +9,7 @@ export const ErrorCodes = {
   RESOURCE_CONFLICT: 'RESOURCE_CONFLICT',
 };
 
-export const Version = '3.0.0.dev0';
+export const Version = '3.0.0rc3';
 
 const DOCS_VERSION = 'latest';
 
