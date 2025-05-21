60a471fb223551bb1f244519826f835ba9a197c4
harupy <17039389+harupy@users.noreply.github.com>
2025-05-22 00:47:53 +0900

    Support pyspark >= 3.2.1
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- dev/update_ml_package_versions.py
+++ dev/update_ml_package_versions.py
@@ -261,7 +261,9 @@ def update(skip_yml=False):
 
                 if category in config and "minimum" in config[category]:
                     old_min_version = config[category]["minimum"]
-                    if min_supported_version is None:
+                    if flavor_key == "spark":
+                        pass
+                    elif min_supported_version is None:
                         # The latest release version was 2 years ago.
                         # set the min version to be the same with the max version.
                         max_ver = config[category]["maximum"]
