7d4ce8ab0a7639f59d60a0be744a3c16fe8e56f5
Harutaka Kawamura <hkawamura0130@gmail.com>
2025-05-23 13:41:01 +0900

    Allow `autoformat` workflow to be run on PRs filed by Copilot (#15845)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- .github/actions/validate-author/index.js
+++ .github/actions/validate-author/index.js
@@ -1,10 +1,20 @@
-function isAllowed(author_association) {
-  return ["owner", "member", "collaborator"].includes(author_association.toLowerCase());
+function isAllowed({ author_association, user }) {
+  return (
+    ["owner", "member", "collaborator"].includes(author_association.toLowerCase()) ||
+    // Allow Copilot to run this workflow
+    (user && user.login.toLowerCase() === "copilot" && user.type.toLowerCase() === "bot")
+  );
 }
 
 module.exports = async ({ context, github, core }) => {
   if (context.eventName === "issue_comment") {
-    if (!isAllowed(context.payload.comment.author_association)) {
+    const { comment } = context.payload;
+    if (
+      !isAllowed({
+        author_association: comment.author_association,
+        user: comment.user,
+      })
+    ) {
       core.setFailed(`${comment.author_association} is not allowed to use this workflow.`);
     }
 
@@ -13,7 +23,12 @@ module.exports = async ({ context, github, core }) => {
       repo: context.repo.repo,
       pull_number: context.issue.number,
     });
-    if (!isAllowed(pullRequest.author_association)) {
+    if (
+      !isAllowed({
+        author_association: pullRequest.author_association,
+        user: pullRequest.user,
+      })
+    ) {
       core.setFailed(
         `This workflow is not allowed to run on PRs from ${pullRequest.author_association}.`
       );
