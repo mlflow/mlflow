7dd731b6b7a867e407765140bd0998021fec5900
harupy <17039389+harupy@users.noreply.github.com>
2025-05-26 22:43:04 +0900

    Add set -euo pipefail and quote variables
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- dev/install-conftest.sh
+++ dev/install-conftest.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
 DESTINATION="/tmp/conftest/bin"
-mkdir -p $DESTINATION
+mkdir -p "$DESTINATION"
 curl -s -L "https://github.com/open-policy-agent/conftest/releases/download/v0.56.0/conftest_0.56.0_Linux_x86_64.tar.gz" | tar -xz -C $DESTINATION
 if [[ $? -eq 0 ]]; then
   if [[ $GITHUB_ACTIONS == 'true' ]]; then
-    echo $DESTINATION >> $GITHUB_PATH
+    echo "$DESTINATION" >> "$GITHUB_PATH"
   fi
 fi
