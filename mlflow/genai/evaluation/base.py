61b8e4d6aa61cfbf43195161e498f1ef019c7cfa
Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
2025-05-27 20:34:59 +0900

    Remove result_df from mlflow.genai.evaluation result (#15896)
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>
    Signed-off-by: Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>

--- mlflow/genai/evaluation/base.py
+++ mlflow/genai/evaluation/base.py
@@ -1,6 +1,5 @@
 import logging
 import warnings
-from dataclasses import dataclass
 from typing import TYPE_CHECKING, Any, Callable, Optional
 
 import mlflow
@@ -14,6 +13,7 @@ from mlflow.genai.scorers.builtin_scorers import GENAI_CONFIG_NAME
 from mlflow.genai.scorers.validation import valid_data_for_builtin_scorers, validate_scorers
 from mlflow.genai.utils.trace_utils import convert_predict_fn
 from mlflow.models.evaluation.base import (
+    EvaluationResult,
     _is_model_deployment_endpoint_uri,
 )
 from mlflow.utils.annotations import experimental
@@ -22,22 +22,8 @@ from mlflow.utils.uri import is_databricks_uri
 if TYPE_CHECKING:
     from genai.evaluation.utils import EvaluationDatasetTypes
 
-try:
-    # `pandas` is not required for `mlflow-skinny`.
-    import pandas as pd
-except ImportError:
-    pass
-
 
 logger = logging.getLogger(__name__)
-
-
-@experimental
-@dataclass
-class EvaluationResult:
-    run_id: str
-    metrics: dict[str, float]
-    result_df: "pd.DataFrame"
 
 
 @experimental
@@ -218,6 +204,9 @@ def evaluate(
             the evaluation results. Can be also set globally via the
             :py:func:`mlflow.set_active_model` function.
 
+    Returns:
+        An :py:class:`mlflow.models.EvaluationResult~` object.
+
     Note:
         This function is only supported on Databricks. The tracking URI must be
         set to Databricks.
@@ -288,7 +277,7 @@ def evaluate(
             module="mlflow.data.evaluation_dataset",
         )
 
-        result = mlflow.models.evaluate(
+        return mlflow.models.evaluate(
             model=predict_fn,
             data=data,
             evaluator_config=evaluation_config,
@@ -297,12 +286,6 @@ def evaluate(
             model_id=model_id,
             _called_from_genai_evaluate=True,
         )
-
-    return EvaluationResult(
-        run_id=result._run_id,
-        metrics=result.metrics,
-        result_df=result.tables["eval_results"],
-    )
 
 
 @experimental
