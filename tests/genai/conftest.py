ac1de82be07b273ef01bf4326e908de2d03d1bc5
Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
2025-05-28 10:55:06 +0900

    Prohibit passing extra kwargs to scorers (#15901)
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- tests/genai/conftest.py
+++ tests/genai/conftest.py
@@ -2,17 +2,6 @@ from unittest import mock
 
 import pytest
 
-from mlflow.entities import TraceInfoV2
-
-
-# TODO: Remove this fixture once `databricks-agents` releases a new version that's compatible with
-# trace v3
-@pytest.fixture(scope="module", autouse=True)
-def mock_trace_info():
-    # Monkey patch TraceInfo (V3) to use TraceInfoV2
-    with mock.patch("mlflow.entities.TraceInfo", wraps=TraceInfoV2):
-        yield
-
 
 @pytest.fixture(autouse=True)
 def mock_init_auth():
--- tests/genai/test_scorer.py
+++ tests/genai/test_scorer.py
@@ -106,31 +106,6 @@ def test_scorer_is_called_with_correct_arguments(sample_data):
         )
 
 
-def test_scorer_receives_extra_arguments():
-    received_args = []
-
-    @scorer
-    def dummy_scorer(inputs, outputs, retrieved_context) -> float:
-        received_args.append((inputs, outputs, retrieved_context))
-        return 0
-
-    mlflow.genai.evaluate(
-        data=[
-            {
-                "inputs": {"question": "What is Spark?"},
-                "outputs": "actual response for first question",
-                "retrieved_context": [{"doc_uri": "document_1", "content": "test"}],
-            },
-        ],
-        scorers=[dummy_scorer],
-    )
-
-    inputs, outputs, retrieved_context = received_args[0]
-    assert inputs == {"question": "What is Spark?"}
-    assert outputs == "actual response for first question"
-    assert retrieved_context == [{"doc_uri": "document_1", "content": "test"}]
-
-
 def test_trace_passed_correctly():
     @mlflow.trace
     def predict_fn(question):
