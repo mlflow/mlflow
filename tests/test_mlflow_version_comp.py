c804042410abb49447261397fada2182a2bbc107
harupy <17039389+harupy@users.noreply.github.com>
2025-05-29 22:23:06 +0900

    test for 3.x
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/test_mlflow_version_comp.py
+++ tests/test_mlflow_version_comp.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import numpy
 import sklearn
+from sklearn.linear_model import LinearRegression
 
 import mlflow
 
@@ -43,10 +44,9 @@ import sys
 import mlflow
 from sklearn.linear_model import LinearRegression
 
-fitted_model= LinearRegression().fit([[1, 2]], [3])
-
 assert mlflow.__version__.startswith("2."), mlflow.__version__
 
+fitted_model= LinearRegression().fit([[1, 2]], [3])
 with mlflow.start_run():
     model_info = mlflow.sklearn.log_model(fitted_model, artifact_path="model")
     assert model_info.model_uri.startswith("runs:/")
@@ -71,6 +71,57 @@ with mlflow.start_run():
     assert len(client.list_artifacts(run_id=run_id)) == 1
     assert len(client.list_artifacts(run_id=run_id, path="model")) == 6
     assert len(client.list_artifacts(run_id=run_id, path="model/MLmodel")) == 0
+    # Download artifacts
+    out_path = mlflow.artifacts.download_artifacts(
+        artifact_uri=model_uri, dst_path=tmp_path / str(uuid.uuid4())
+    )
+    assert next(Path(out_path).rglob("MLmodel")) is not None
+    out_path = mlflow.artifacts.download_artifacts(
+        run_id=run_id, artifact_path="model", dst_path=tmp_path / str(uuid.uuid4())
+    )
+    assert next(Path(out_path).rglob("MLmodel")) is not None
+    out_path = client.download_artifacts(
+        run_id=run_id, path="model", dst_path=tmp_path / str(uuid.uuid4())
+    )
+    assert next(Path(out_path).rglob("MLmodel")) is not None
+    # Model evaluation
+    eval_res = mlflow.models.evaluate(
+        model=model_uri,
+        data=numpy.array([[1, 2]]),
+        targets=numpy.array([3]),
+        model_type="regressor",
+    )
+    assert "mean_squared_error" in eval_res.metrics
+
+
+def test_mlflow_3_x_comp(tmp_path: Path) -> None:
+    """
+    Does the same as `test_mlflow_2_x_comp`, but with a model logged in MLflow 3.x.
+    """
+    tracking_uri = (tmp_path / "tracking").as_uri()
+    mlflow.set_tracking_uri(tracking_uri)
+    artifact_location = (tmp_path / "artifacts").as_uri()
+    exp_id = mlflow.create_experiment("test", artifact_location=artifact_location)
+    mlflow.set_experiment(experiment_id=exp_id)
+
+    fitted_model = LinearRegression().fit([[1, 2]], [3])
+    with mlflow.start_run() as run:
+        mlflow.sklearn.log_model(fitted_model, name="model")
+
+    # Model loading
+    run_id = run.info.run_id
+    model_uri = f"runs:/{run_id}/model"
+    mlflow.sklearn.load_model(model_uri)
+    mlflow.pyfunc.load_model(model_uri)
+    # Model registration
+    mv = mlflow.register_model(model_uri, "model")
+    mlflow.pyfunc.load_model(f"models:/{mv.name}/{mv.version}")
+    # List artifacts
+    client = mlflow.MlflowClient()
+    # TODO: Add support to list model artifacts
+    # assert len(client.list_artifacts(run_id=run_id)) == 1
+    # assert len(client.list_artifacts(run_id=run_id, path="model")) == 6
+    # assert len(client.list_artifacts(run_id=run_id, path="model/MLmodel")) == 0
     # Download artifacts
     out_path = mlflow.artifacts.download_artifacts(
         artifact_uri=model_uri, dst_path=tmp_path / str(uuid.uuid4())
