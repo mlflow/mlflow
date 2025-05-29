4c95a928200cc57f6175ebd95e2f549dddb54271
Fatih Altinok <fatihaltinok@live.com>
2025-05-29 17:51:42 +0300

    Use custom table component with horizontal scroll
    Signed-off-by: Fatih Altinok <fatihaltinok@live.com>

---
+++ docs/src/components/Table/index.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from "react";
+
+export function Table({ children }: PropsWithChildren) {
+  return (
+    <div className="w-full overflow-x-auto">
+      <table>
+        {children}
+      </table>
+    </div>
+  )
+}
