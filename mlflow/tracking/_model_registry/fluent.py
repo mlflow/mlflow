278b63b1666d61d7426df9e8f243ed1607e44dc7
harupy <17039389+harupy@users.noreply.github.com>
2025-05-29 15:19:49 +0900

    Fix
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/tracking/_model_registry/fluent.py
+++ mlflow/tracking/_model_registry/fluent.py
@@ -136,7 +136,6 @@ def _register_model(
         # If the uri is of the form runs:/...
         (run_id, artifact_path) = RunsArtifactRepository.parse_runs_uri(model_uri)
         runs_artifact_repo = RunsArtifactRepository(model_uri)
-        #
         if runs_artifact_repo._is_directory(""):
             # First check if run has artifact at artifact_path,
             # if so use the run's artifact location as source
