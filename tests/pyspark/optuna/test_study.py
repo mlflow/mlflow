8be4dc0db699c7bb8b378ac26455f1231949d220
Harutaka Kawamura <hkawamura0130@gmail.com>
2025-06-04 13:59:58 +0900

    Skip `test_study_optimize_run` (#16058)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/pyspark/optuna/test_study.py
+++ tests/pyspark/optuna/test_study.py
@@ -55,6 +55,7 @@ spark.executor.extraJavaOptions="-Dio.netty.tryReflectionSetAccessible=true"
         yield spark
 
 
+@pytest.mark.skip(reason="TODO: Deflake this test")
 def test_study_optimize_run(setup_storage):
     storage = setup_storage
     study_name = "test-study"
