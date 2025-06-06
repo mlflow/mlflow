c9570081c4cd32ce4e47908e527e2a85cef902f7
Samraj Moorjani <samraj.moorjani@databricks.com>
2025-06-06 15:02:30 -0700

    .
    Signed-off-by: Samraj Moorjani <samraj.moorjani@databricks.com>

--- mlflow/genai/utils/annotations.py
+++ mlflow/genai/utils/annotations.py
@@ -1,11 +1,11 @@
-from mlflow.annotations import utils as annotations_utils
+from mlflow.utils import annotations
 
 
 def databricks_api(func):
     """
     Decorator to prepend a Databricks-only note to the docstring of a function or class.
     """
-    indent = annotations_utils._get_min_indent_of_docstring(func.__doc__) if func.__doc__ else ""
+    indent = annotations._get_min_indent_of_docstring(func.__doc__) if func.__doc__ else ""
     notice = (
         indent + ".. note:: This functionality is only available in Databricks. "
         "Please install `mlflow[databricks]` to use it.\n\n"
