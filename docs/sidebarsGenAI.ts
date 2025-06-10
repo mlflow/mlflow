223bc2ef196002a3fa957c57e104326e2c67fcf7
Tomu Hirata <tomu.hirata@gmail.com>
2025-06-10 04:28:31 -0700

    add emoji
    Signed-off-by: Tomu Hirata <tomu.hirata@gmail.com>

--- docs/sidebarsGenAI.ts
+++ docs/sidebarsGenAI.ts
@@ -286,7 +286,7 @@ const sidebarsGenAI: SidebarsConfig = {
     },
     {
       type: 'category',
-      label: 'Evaluate & Monitor',
+      label: 'Evaluate & Monitor 📊',
       className: 'sidebar-top-level-category',
       collapsed: false,
       items: [
