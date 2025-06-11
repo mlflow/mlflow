03c6f0879da252575d8187510f01e5eb196256cb
Daniel Lok <daniel.lok@databricks.com>
2025-06-11 17:33:54 +0800

    Fix footer image CSS processing (#16198)
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- docs/src/components/Footer/Footer.tsx
+++ docs/src/components/Footer/Footer.tsx
@@ -1,26 +1,31 @@
 import { cva, type VariantProps, cx } from "class-variance-authority";
 
 import Logo from "@site/static/images/mlflow-logo-white.svg";
+import BlueBg from "@site/static/images/footer-blue-bg.png";
+import RedBg from "@site/static/images/footer-red-bg.png";
+import ColorfulBg from "@site/static/images/footer-colorful-bg.png";
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 import { FooterMenuItem } from "../FooterMenuItem/FooterMenuItem";
 
 const footerVariants = cva(
-  "pb-150 flex flex-col pt-37 bg-linear-to-b from-brand-black to-brand-black bg-bottom bg-no-repeat bg-cover w-full bg-(--background-color-dark)",
-  {
-    variants: {
-      variant: {
-        blue: ` bg-[url('/images/footer-blue-bg.png')] bg-size-[100%_340px]`,
-        red: `bg-[url('/images/footer-red-bg.png')] bg-size-[100%_340px]`,
-        colorful: `bg-[url('/images/footer-colorful-bg.png')] bg-size-[100%_340px]`,
-      },
-    },
-  },
+  "pb-150 flex flex-col pt-37 bg-linear-to-b from-brand-black to-brand-black bg-bottom bg-no-repeat bg-cover w-full bg-(--background-color-dark) bg-size-[100%_340px]",
 );
 
-export const Footer = ({ variant }: VariantProps<typeof footerVariants>) => {
+const getBackgroundImage = (variant: "blue" | "red" | "colorful") => {
+  switch (variant) {
+    case "blue":
+      return BlueBg;
+    case "red":
+      return RedBg;
+    case "colorful":
+      return ColorfulBg;
+  }
+};
+
+export const Footer = ({ variant }: { variant: "blue" | "red" | "colorful" }) => {
   return (
-    <footer className={cx(footerVariants({ variant }))}>
+    <footer className={cx(footerVariants())} style={{ backgroundImage: `url(${getBackgroundImage(variant)})` }}>
       <div className="flex flex-row justify-between items-start md:items-center px-6 lg:px-20 gap-10 xs:gap-0 max-w-container">
         <Logo className="h-[36px] shrink-0" />
 
