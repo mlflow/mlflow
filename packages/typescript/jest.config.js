d4f5fefa0e7c12a98733b851f94cff3ddef10c94
B-Step62 <yuki.watanabe@databricks.com>
2025-06-18 08:08:37 +0900

    Initial package skeleton
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

---
+++ packages/typescript/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests', '<rootDir>/src'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }]
+  }
+};
