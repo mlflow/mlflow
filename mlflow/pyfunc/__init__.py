c49f4c6beaf7928c1fc4b23c36339dff68867177
Weichen Xu <weichen.xu@databricks.com>
2025-06-17 18:40:10 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/pyfunc/__init__.py
+++ mlflow/pyfunc/__init__.py
@@ -1899,7 +1899,9 @@ def _prebuild_env_internal(local_model_path, archive_name, save_path):
 
         pyfunc_backend.prepare_env(model_uri=local_model_path, capture_output=False)
         # exclude pip cache from the archive file.
-        shutil.rmtree(os.path.join(env_root_dir, "pip_cache_pkgs"))
+        cache_path = os.path.join(env_root_dir, "pip_cache_pkgs")
+        if os.path.exists(cache_path):
+            shutil.rmtree(cache_path)
 
         return archive_directory(env_root_dir, archive_path)
     finally:
