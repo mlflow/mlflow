21f5b64db618c4f17cf6bafd59dfafc60bc12fa7
B-Step62 <yuki.watanabe@databricks.com>
2025-06-18 09:49:18 +0900

    Add trace entities
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

---
+++ packages/typescript/src/core/entities/trace_data.ts
@@ -0,0 +1,47 @@
+import { ISpan, SerializedSpan, Span } from './span';
+
+/**
+ * Represents the spans and associated data for a trace
+ */
+export class TraceData {
+  /**
+   * The spans that make up this trace
+   */
+  spans: ISpan[];
+
+  /**
+   * Create a new TraceData instance
+   * @param spans List of spans
+   */
+  constructor(spans: ISpan[] = []) {
+    this.spans = spans;
+  }
+
+  /**
+   * Convert this TraceData instance to JSON format
+   * @returns JSON object representation of the TraceData
+   */
+  toJson(): SerializedTraceData {
+    return {
+      spans: this.spans.map((span) => span.toJson())
+    };
+  }
+
+  /**
+   * Create a TraceData instance from JSON data (following Python implementation)
+   * @param json JSON object containing trace data
+   * @returns TraceData instance
+   */
+  static fromJson(json: SerializedTraceData): TraceData {
+    if (!json || typeof json !== 'object') {
+      throw new Error(`TraceData.fromJson() expects an object. Got: ${typeof json}`);
+    }
+    const spans: ISpan[] = json.spans.map((spanData) => Span.fromJson(spanData));
+    return new TraceData(spans);
+  }
+}
+
+
+export interface SerializedTraceData {
+  spans: SerializedSpan[];
+}
