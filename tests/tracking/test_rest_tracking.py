a3c207c890eae28dcef994434857dc47106de66d
B-Step62 <yuki.watanabe@databricks.com>
2025-06-20 17:09:56 +0900

    minor

--- tests/tracking/test_rest_tracking.py
+++ tests/tracking/test_rest_tracking.py
@@ -2377,7 +2377,7 @@ def test_get_run_and_experiment_graphql(mlflow_client):
 
 
 def test_start_and_end_trace(mlflow_client):
-    skip_if_v3_backend(mlflow_client)
+    skip_if_v3_backend()
 
     experiment_id = mlflow_client.create_experiment("start end trace")
 
@@ -2448,7 +2448,7 @@ def test_start_and_end_trace(mlflow_client):
 
 
 def test_search_traces(mlflow_client):
-    skip_if_v3_backend(mlflow_client)
+    skip_if_v3_backend()
 
     mlflow.set_tracking_uri(mlflow_client.tracking_uri)
     experiment_id = mlflow_client.create_experiment("search traces")
@@ -2494,7 +2494,7 @@ def test_search_traces(mlflow_client):
 
 
 def test_delete_traces(mlflow_client):
-    skip_if_v3_backend(mlflow_client)
+    skip_if_v3_backend()
 
     mlflow.set_tracking_uri(mlflow_client.tracking_uri)
     experiment_id = mlflow_client.create_experiment("delete traces")
@@ -2550,7 +2550,7 @@ def test_delete_traces(mlflow_client):
 
 
 def test_set_and_delete_trace_tag(mlflow_client):
-    skip_if_v3_backend(mlflow_client)
+    skip_if_v3_backend()
 
     mlflow.set_tracking_uri(mlflow_client.tracking_uri)
     experiment_id = mlflow_client.create_experiment("set delete tag")
@@ -2578,7 +2578,7 @@ def test_set_and_delete_trace_tag(mlflow_client):
 
 
 def test_get_trace_artifact_handler(mlflow_client):
-    skip_if_v3_backend(mlflow_client)
+    skip_if_v3_backend()
 
     mlflow.set_tracking_uri(mlflow_client.tracking_uri)
 
