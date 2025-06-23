2c45963b70b59fb32ab1e72efc67e5a1c596da0c
serena-ruan <serena.rxy@gmail.com>
2025-06-23 19:07:22 +0800

    remove temporarily_disable_telemetry
    Signed-off-by: serena-ruan <serena.rxy@gmail.com>

--- mlflow/telemetry/utils.py
+++ mlflow/telemetry/utils.py
@@ -1,25 +1,7 @@
 import inspect
 import os
-import threading
-from contextlib import contextmanager
 
 from mlflow.environment_variables import MLFLOW_DISABLE_TELEMETRY
-
-_mlflow_disable_telemetry_lock = threading.RLock()
-
-
-@contextmanager
-def temporarily_disable_telemetry():
-    original_value = MLFLOW_DISABLE_TELEMETRY.get() if MLFLOW_DISABLE_TELEMETRY.is_set() else None
-    with _mlflow_disable_telemetry_lock:
-        try:
-            MLFLOW_DISABLE_TELEMETRY.set(True)
-            yield
-        finally:
-            if original_value is None:
-                MLFLOW_DISABLE_TELEMETRY.unset()
-            else:
-                MLFLOW_DISABLE_TELEMETRY.set(original_value)
 
 
 # TODO: add check for CI  and mlflow dev versions,
--- tests/telemetry/test_utils.py
+++ tests/telemetry/test_utils.py
@@ -1,7 +1,4 @@
-from mlflow.telemetry.utils import (
-    is_telemetry_disabled,
-    temporarily_disable_telemetry,
-)
+from mlflow.telemetry.utils import is_telemetry_disabled
 
 
 def test_is_telemetry_disabled(monkeypatch):
@@ -16,17 +13,3 @@ def test_is_telemetry_disabled(monkeypatch):
     with monkeypatch.context() as m:
         m.setenv("DO_NOT_TRACK", "true")
         assert is_telemetry_disabled() is True
-
-
-def test_temporarily_disable_telemetry(monkeypatch):
-    with temporarily_disable_telemetry():
-        assert is_telemetry_disabled() is True
-
-    assert is_telemetry_disabled() is False
-    monkeypatch.setenv("MLFLOW_DISABLE_TELEMETRY", "true")
-    assert is_telemetry_disabled() is True
-
-    with temporarily_disable_telemetry():
-        assert is_telemetry_disabled() is True
-
-    assert is_telemetry_disabled() is True
