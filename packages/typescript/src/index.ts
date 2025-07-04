fbcc9b37cf29864c41a34c9af54e3b34154f2263
B-Step62 <yuki.watanabe@databricks.com>
2025-07-04 11:16:32 +0900

    Implement trace wrapper
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- packages/typescript/src/index.ts
+++ packages/typescript/src/index.ts
@@ -1,5 +1,13 @@
 import { init } from './core/config';
-import { getLastActiveTraceId, startSpan, withSpan } from './core/api';
+import { getLastActiveTraceId, startSpan, trace, withSpan } from './core/api';
 import { flushTraces } from './core/provider';
 
-export { getLastActiveTraceId, flushTraces, init, startSpan, withSpan };
+export { getLastActiveTraceId, flushTraces, init, startSpan, trace, withSpan };
+
+// Export entities
+export { SpanType } from './core/constants';
+export type { LiveSpan } from './core/entities/span';
+export type { Trace } from './core/entities/trace';
+export type { TraceInfo } from './core/entities/trace_info';
+export type { TraceData } from './core/entities/trace_data';
+export { SpanStatusCode } from './core/entities/span_status';
