6166b966be522a124cfb28d045710b8607b87645
Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
2025-07-07 19:46:32 +0900

    Fix hallucinations in prompt docs (#16605)
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- docs/sidebarsGenAI.ts
+++ docs/sidebarsGenAI.ts
@@ -510,10 +510,6 @@ const sidebarsGenAI: SidebarsConfig = {
               type: 'doc',
               id: 'prompt-version-mgmt/prompt-registry/log-with-model',
             },
-            {
-              type: 'doc',
-              id: 'prompt-version-mgmt/prompt-registry/use-prompts-in-deployed-apps',
-            },
           ],
           link: {
             type: 'doc',
