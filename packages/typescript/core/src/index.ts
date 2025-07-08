82127fb11d694853aca89289ce24880417a4558a
B-Step62 <yuki.watanabe@databricks.com>
2025-07-08 17:29:28 +0900

    Add updateCurrentTrace API
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- packages/typescript/core/src/index.ts
+++ packages/typescript/core/src/index.ts
@@ -1,9 +1,26 @@
 import { init } from './core/config';
-import { getLastActiveTraceId, startSpan, trace, withSpan } from './core/api';
+import {
+  getLastActiveTraceId,
+  getCurrentActiveSpan,
+  updateCurrentTrace,
+  startSpan,
+  trace,
+  withSpan
+} from './core/api';
 import { flushTraces } from './core/provider';
 import { MlflowClient } from './clients';
 
-export { getLastActiveTraceId, flushTraces, init, startSpan, trace, withSpan, MlflowClient };
+export {
+  getLastActiveTraceId,
+  getCurrentActiveSpan,
+  updateCurrentTrace,
+  flushTraces,
+  init,
+  startSpan,
+  trace,
+  withSpan,
+  MlflowClient
+};
 
 // Export entities
 export * from './core/constants';
@@ -12,3 +29,4 @@ export type { Trace } from './core/entities/trace';
 export type { TraceInfo } from './core/entities/trace_info';
 export type { TraceData } from './core/entities/trace_data';
 export { SpanStatusCode } from './core/entities/span_status';
+export type { UpdateCurrentTraceOptions } from './core/api';
