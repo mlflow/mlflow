358300e4209391d2ea1a20f3a055c6a91df6efb9
Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
2025-07-12 11:33:06 +0900

    Add linter to prevent broke image links (#16661)
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>
    Signed-off-by: Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>

--- docs/eslint-plugin-mlflow-docs/index.js
+++ docs/eslint-plugin-mlflow-docs/index.js
@@ -7,6 +7,9 @@ module.exports = {
   rules: {
     /** Rule to validate NotebookDownloadButton URLs */
     'valid-notebook-url': require('./rules/valid-notebook-url'),
+    /** Rule to detect raw image paths that should use useBaseUrl */
+    'use-base-url-for-images': require('./rules/use-base-url-for-images'),
+    /** Rule to enforce <APILink> usage when referencing the API doc **/
     'prefer-apilink-component': require('./rules/prefer-apilink-component'),
   },
 };
