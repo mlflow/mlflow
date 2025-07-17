05e1908e64f538e890940a00659964aa859fb43d
Harshil Prajapati <47331144+harshilprajapati96@users.noreply.github.com>
2025-07-17 04:00:44 -0500

    Add support for chat-style prompts with structured output with prompt object (#16341)
    Signed-off-by: harshilprajapati96 <harshil.prajapati@coalitioninc.com>

--- dev/run-python-skinny-tests.sh
+++ dev/run-python-skinny-tests.sh
@@ -36,7 +36,8 @@ pytest \
   tests/utils/test_requirements_utils.py::test_infer_requirements_excludes_mlflow \
   tests/utils/test_search_utils.py \
   tests/store/tracking/test_file_store.py \
-  tests/utils/test_doctor.py
+  tests/utils/test_doctor.py \
+  --import-mode=importlib
 
 python -m pip install pandas
 pytest tests/test_skinny_client_autolog_without_scipy.py
