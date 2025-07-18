423854c93d14e25d35911ebf428b66eecf121ece
Harutaka Kawamura <hkawamura0130@gmail.com>
2025-07-18 16:47:19 +0900

    Revert "Replace `license.file` with `license-files` in `pyproject.toml`" (#16780)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/dev/test_update_mlflow_versions.py
+++ tests/dev/test_update_mlflow_versions.py
@@ -50,7 +50,7 @@ _PYPROJECT_TOML_FILES = {
     },
     "pyproject.release.toml": {
         12: 'version = "{new_version}"',
-        31: '  "mlflow-skinny=={new_version}",',
+        30: '  "mlflow-skinny=={new_version}",',
     },
     "libs/skinny/pyproject.toml": {
         10: 'version = "{new_version}"',
