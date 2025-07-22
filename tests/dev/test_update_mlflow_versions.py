123c614a3f168b9ccfc10c40c0a81cf6146fb06b
Daniel Lok <daniel.lok@databricks.com>
2025-07-22 22:04:59 +0800

    Fix releasing to central (#16820)
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- tests/dev/test_update_mlflow_versions.py
+++ tests/dev/test_update_mlflow_versions.py
@@ -21,7 +21,7 @@ _JAVA_FILES = {}
 _JAVA_XML_FILES = {
     "mlflow/java/pom.xml": {
         6: "  <version>{new_version}</version>",
-        65: "    <mlflow-version>{new_version}</mlflow-version>",
+        76: "    <mlflow-version>{new_version}</mlflow-version>",
     },
     "mlflow/java/client/pom.xml": {
         8: "    <version>{new_version}</version>",
