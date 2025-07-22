421aa656467f515153aee87f67209c3569e7c75d
Michael Berk <michaelberk99@gmail.com>
2025-07-22 12:15:56 -0400

    Update mlflow/semantic_kernel/__init__.py
    Co-authored-by: Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
    Signed-off-by: Michael Berk <michaelberk99@gmail.com>

--- mlflow/semantic_kernel/__init__.py
+++ mlflow/semantic_kernel/__init__.py
@@ -33,7 +33,7 @@ from mlflow.utils.autologging_utils import autologging_integration, safe_patch
 FLAVOR_NAME = "semantic_kernel"
 
 
-@experimental(version="3.0.1")
+@experimental(version="3.2.0")
 @autologging_integration(FLAVOR_NAME)
 def autolog(
     log_traces: bool = True,
