4d45d6885230affcdf953b8783d2cc22e09e9c6e
bbqiu <bryan.qiu@databricks.com>
2025-07-22 00:48:28 -0700

    .
    Signed-off-by: bbqiu <bryan.qiu@databricks.com>

--- tests/utils/test_rest_utils.py
+++ tests/utils/test_rest_utils.py
@@ -31,13 +31,6 @@ from mlflow.utils.rest_utils import (
 
 from tests import helper_functions
 
-try:
-    import databricks.sdk  # noqa: F401
-
-    DATABRICKS_SDK_AVAILABLE = True
-except ImportError:
-    DATABRICKS_SDK_AVAILABLE = False
-
 
 @pytest.mark.parametrize(
     "response_mock",
@@ -831,7 +824,7 @@ def test_databricks_sdk_retry_backoff_calculation():
     assert call_count == 4  # Initial + 3 retries
 
 
-@pytest.mark.skipif(not DATABRICKS_SDK_AVAILABLE, reason="databricks.sdk not available")
+@pytest.mark.skip
 def test_timeout_parameter_propagation_with_timeout():
     """Test timeout parameter propagation from http_request to get_workspace_client with timeout."""
     with (
@@ -859,7 +852,7 @@ def test_timeout_parameter_propagation_with_timeout():
         )
 
 
-@pytest.mark.skipif(not DATABRICKS_SDK_AVAILABLE, reason="databricks.sdk not available")
+@pytest.mark.skip
 def test_timeout_parameter_propagation_without_timeout():
     """Test timeout param propagation from http_request to get_workspace_client without timeout."""
     with (
