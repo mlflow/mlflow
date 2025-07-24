bc6328c9b5568e9e9695024f1fed0d7ab047bc20
Weichen Xu <weichen.xu@databricks.com>
2025-07-24 18:23:49 +0800

    update test
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- tests/tracing/test_fluent.py
+++ tests/tracing/test_fluent.py
@@ -22,7 +22,10 @@ from mlflow.entities import (
 )
 from mlflow.entities.trace_location import TraceLocation
 from mlflow.entities.trace_state import TraceState
-from mlflow.environment_variables import MLFLOW_TRACKING_USERNAME
+from mlflow.environment_variables import (
+    MLFLOW_ENABLE_THREAD_LOCAL_TRACING_DESTINATION,
+    MLFLOW_TRACKING_USERNAME,
+)
 from mlflow.exceptions import MlflowException
 from mlflow.store.entities.paged_list import PagedList
 from mlflow.store.tracking import SEARCH_TRACES_DEFAULT_MAX_RESULTS
@@ -2113,8 +2116,11 @@ def test_search_traces_with_run_id_validates_store_filter_string(is_databricks):
         )
 
 
+@pytest.mark.skipif(
+    not MLFLOW_ENABLE_THREAD_LOCAL_TRACING_DESTINATION.get(),
+    reason="The testcase requires enabling thread-local tracing destination feature."
+)
 def test_set_destination_in_threads(async_logging_enabled, tmp_path):
-    from mlflow.utils.os import is_windows
     from mlflow.client import MlflowClient
     from mlflow.tracing.destination import MlflowExperiment
 
@@ -2158,6 +2164,8 @@ def test_set_destination_in_threads(async_logging_enabled, tmp_path):
 
     thread1.join()
     thread2.join()
+
+    time.sleep(0.2)
 
     traces = get_traces(experiment_id1)
     assert len(traces) == 1
