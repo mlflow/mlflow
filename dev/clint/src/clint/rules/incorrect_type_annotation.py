d6cf4543eb648976e0d43e8218b687027d137914
harupy <17039389+harupy@users.noreply.github.com>
2025-07-25 14:17:23 +0900

    Fix invalid type annotations
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- dev/clint/src/clint/rules/incorrect_type_annotation.py
+++ dev/clint/src/clint/rules/incorrect_type_annotation.py
@@ -1,25 +1,33 @@
 import ast
 
+from clint.resolver import Resolver
 from clint.rules.base import Rule
 
 
 class IncorrectTypeAnnotation(Rule):
     MAPPING = {
-        "callable": "Callable",
-        "any": "Any",
+        "callable": "typing.Callable",
+        "any": "typing.Any",
+        "numpy.array": "numpy.ndarray",
+        "dataclasses.dataclass": "Any",
     }
 
-    def __init__(self, type_hint: str) -> None:
-        self.type_hint = type_hint
+    def __init__(self, actual: str, expected: str) -> None:
+        self.actual = actual
+        self.expected = expected
 
-    @staticmethod
-    def check(node: ast.Name) -> bool:
-        return node.id in IncorrectTypeAnnotation.MAPPING
+    @classmethod
+    def check(cls, node: ast.expr, resolver: Resolver) -> "IncorrectTypeAnnotation":
+        names = resolver.resolve(node)
+        if names is None:
+            return None
+
+        actual = ".".join(names)
+        if expected := cls.MAPPING.get(actual):
+            return cls(actual=actual, expected=expected)
 
     def _message(self) -> str:
-        if correct_hint := self.MAPPING.get(self.type_hint):
-            return f"Did you mean `{correct_hint}` instead of `{self.type_hint}`?"
-
-        raise ValueError(
-            f"Unexpected type: {self.type_hint}. It must be one of {list(self.MAPPING)}."
-        )
+        if self.actual == "dataclasses.dataclass":
+            return "`dataclass` is an invalid type annotation. Use `Any` instead as a workaround."
+        else:
+            return f"Did you mean `{self.expected}` instead of `{self.actual}`?"
