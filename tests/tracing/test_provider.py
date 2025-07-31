1a259bbbaadcbf863dfc2f9dec4d269531563524
serena-ruan <serena.rxy@gmail.com>
2025-07-31 19:56:26 +0800

    fix set_destination
    Signed-off-by: serena-ruan <serena.rxy@gmail.com>

--- tests/tracing/test_provider.py
+++ tests/tracing/test_provider.py
@@ -18,6 +18,7 @@ from mlflow.tracing.fluent import start_span_no_context
 from mlflow.tracing.processor.inference_table import InferenceTableSpanProcessor
 from mlflow.tracing.processor.mlflow_v3 import MlflowV3SpanProcessor
 from mlflow.tracing.provider import (
+    _MLFLOW_TRACE_USER_DESTINATION,
     _get_tracer,
     _setup_tracer_provider,
     is_tracing_enabled,
@@ -114,6 +115,17 @@ def test_set_destination_databricks(monkeypatch):
     assert len(processors) == 1
     assert isinstance(processors[0], MlflowV3SpanProcessor)
     assert isinstance(processors[0].span_exporter, MlflowV3SpanExporter)
+
+
+def test_set_destination_databricks_serving(mock_databricks_serving_with_tracing_env):
+    mlflow.tracing.set_destination(destination=Databricks(experiment_id="123"))
+
+    tracer = _get_tracer("test")
+    processors = tracer.span_processor._span_processors
+    assert len(processors) == 1
+    assert isinstance(processors[0], MlflowV3SpanProcessor)
+    assert isinstance(processors[0].span_exporter, MlflowV3SpanExporter)
+    assert _MLFLOW_TRACE_USER_DESTINATION.get().experiment_id == "123"
 
 
 def test_disable_enable_tracing():
