a1d035249da76c532f88560ee6354df5bcde9dc4
B-Step62 <yuki.watanabe@databricks.com>
2025-07-31 17:49:11 +0900

    fix
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- mlflow/ml_package_versions.py
+++ mlflow/ml_package_versions.py
@@ -403,7 +403,7 @@ _ML_PACKAGE_VERSIONS = {
         },
         "autologging": {
             "minimum": "1.52.9",
-            "maximum": "1.58.4"
+            "maximum": "1.74.12"
         }
     },
     "groq": {
