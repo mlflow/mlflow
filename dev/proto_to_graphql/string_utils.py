821ac8e8704a8983159d8344749f284b38aecc62
ayush2202ksr12111 <143083048+ayush2202ksr12111@users.noreply.github.com>
2025-08-03 05:08:52 +0530

    Update string_utils.py
    Removed comments
    
    Signed-off-by: ayush2202ksr12111 <143083048+ayush2202ksr12111@users.noreply.github.com>

--- dev/proto_to_graphql/string_utils.py
+++ dev/proto_to_graphql/string_utils.py
@@ -15,15 +15,9 @@ def snake_to_pascal(string):
 
 
 def camel_to_snake_(string):
-    # we will result an empty array
     result=[]
-    #Iterating using enumerate to get both string index i and index value which is character char
     for i, char in enumerate(string):
-# using if condition to check whether it is upper or lower excepth 0th position character
         if(char.isupper() & (i!=0)):
-            # we will add _ if any upper character found in here
             result.append('_')
-        # this will make every character lower even if it is upper
         result.append(char.lower())
-# now we will return the list here
     return ''.join(result)
