e3d6c6f8782a90e00af96e4a2e017bb949d12f41
serena-ruan <serena.rxy@gmail.com>
2025-08-04 12:57:32 +0800

    update
    Signed-off-by: serena-ruan <serena.rxy@gmail.com>

--- docs/communitySidebar.ts
+++ docs/communitySidebar.ts
@@ -4,67 +4,13 @@ const communitySidebar: SidebarsConfig = {
   communitySidebar: [
     {
       type: 'doc',
+      id: 'index',
+      label: 'Community',
+    },
+    {
+      type: 'doc',
       id: 'usage-tracking',
       label: 'Usage Tracking',
-    },
-    {
-      type: 'category',
-      label: 'Community Resources',
-      collapsed: false,
-      items: [
-        {
-          type: 'link',
-          label: 'GitHub Repository',
-          href: 'https://github.com/mlflow/mlflow',
-        },
-        {
-          type: 'link',
-          label: 'Slack Community',
-          href: 'https://mlflow.org/slack',
-        },
-        {
-          type: 'link',
-          label: 'Twitter/X',
-          href: 'https://twitter.com/mlflow',
-        },
-        {
-          type: 'link',
-          label: 'LinkedIn',
-          href: 'https://www.linkedin.com/company/mlflow-org',
-        },
-        {
-          type: 'link',
-          label: 'Stack Overflow',
-          href: 'https://stackoverflow.com/questions/tagged/mlflow',
-        },
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Get Involved',
-      collapsed: false,
-      items: [
-        {
-          type: 'link',
-          label: 'Contributing Guide',
-          href: 'https://github.com/mlflow/mlflow/blob/master/CONTRIBUTING.md',
-        },
-        {
-          type: 'link',
-          label: 'MLflow Blog',
-          href: 'https://mlflow.org/blog/index.html',
-        },
-        {
-          type: 'link',
-          label: 'MLflow Ambassador Program',
-          href: 'https://mlflow.org/ambassadors',
-        },
-        {
-          type: 'link',
-          label: 'Report Issues',
-          href: 'https://github.com/mlflow/mlflow/issues/new/choose',
-        },
-      ],
     },
   ],
 };
