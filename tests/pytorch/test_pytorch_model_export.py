00116d35b8714035410e65e257ab876eb52bbde3
serena-ruan <serena.rxy@gmail.com>
2025-08-04 19:37:16 +0800

    update test
    Signed-off-by: serena-ruan <serena.rxy@gmail.com>

--- tests/pytorch/test_pytorch_model_export.py
+++ tests/pytorch/test_pytorch_model_export.py
@@ -26,7 +26,7 @@ from mlflow.models.utils import _read_example, load_serving_example
 from mlflow.pytorch import pickle_module as mlflow_pytorch_pickle_module
 from mlflow.store.artifact.s3_artifact_repo import S3ArtifactRepository
 from mlflow.tracking.artifact_utils import _download_artifact_from_uri
-from mlflow.types.schema import Schema, TensorSpec
+from mlflow.types.schema import DataType, Schema, TensorSpec
 from mlflow.utils.environment import _mlflow_conda_env
 from mlflow.utils.file_utils import TempDir
 from mlflow.utils.model_utils import _get_flavor_configuration
@@ -1203,8 +1203,7 @@ def test_passing_params_to_model(data):
         )
 
 
-@pytest.mark.parametrize("scripted_model", [False])
-def test_log_model_with_datetime_input(sequential_model, data):
+def test_log_model_with_datetime_input():
     df = pd.DataFrame(
         {
             "datetime": pd.date_range("2022-01-01", periods=5, freq="D"),
@@ -1213,10 +1212,12 @@ def test_log_model_with_datetime_input(sequential_model, data):
             "z": np.random.uniform(0, 10, 5),
         }
     )
-    model_info = mlflow.pytorch.log_model(sequential_model, name="pytorch")
+    model = get_sequential_model()
+    model_info = mlflow.pytorch.log_model(model, name="pytorch", input_example=df)
+    assert model_info.signature.inputs.inputs[0].type == DataType.datetime
     pyfunc_model = mlflow.pyfunc.load_model(model_info.model_uri)
     with torch.no_grad():
         input_tensor = torch.from_numpy(df.to_numpy(dtype=np.float32))
-        expected_result = sequential_model(input_tensor)
+        expected_result = model(input_tensor)
     with torch.no_grad():
         np.testing.assert_array_almost_equal(pyfunc_model.predict(df), expected_result, decimal=4)
