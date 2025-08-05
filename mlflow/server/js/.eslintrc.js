705d94ce8a0edbc5fe66f26b2b89d8ff2c316826
Daniel Lok <daniel.lok@databricks.com>
2025-08-05 10:08:07 +0800

    fix
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/server/js/.eslintrc.js
+++ mlflow/server/js/.eslintrc.js
@@ -622,7 +622,7 @@ module.exports = {
       },
     },
     {
-      files: ['*.test.js', '*-test.js', '*-test.jsx', 'test/**'],
+      files: ['*.test.js', '*-test.js', '*-test.jsx', '*.test.ts', '*-test.ts', '*.test.tsx', '*-test.tsx', 'test/**'],
       plugins: ['jest', 'chai-expect', 'chai-friendly'],
       globals: {
         sinon: true,
@@ -639,6 +639,7 @@ module.exports = {
         'testing-library/no-debugging-utils': 'error',
         'testing-library/no-dom-import': 'error',
         'testing-library/await-async-utils': 'error',
+        '@typescript-eslint/no-non-null-assertion': 'off',
       },
     },
     {
