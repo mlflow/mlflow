9d8b095aca192c5cc2b33c6e50cd2ab75ab31e2b
Harutaka Kawamura <hkawamura0130@gmail.com>
2025-08-12 18:32:48 +0900

    Update `down_revision` in migration script to correct reference (#17176)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>
    Signed-off-by: mlflow-app[bot] <mlflow-app[bot]@users.noreply.github.com>
    Co-authored-by: mlflow-app[bot] <mlflow-app[bot]@users.noreply.github.com>

--- mlflow/store/db_migrations/versions/1a0cddfcaa16_add_webhooks_and_webhook_events_tables.py
+++ mlflow/store/db_migrations/versions/1a0cddfcaa16_add_webhooks_and_webhook_events_tables.py
@@ -13,7 +13,7 @@ from mlflow.store.model_registry.dbmodels.models import SqlWebhook, SqlWebhookEv
 
 # revision identifiers, used by Alembic.
 revision = "1a0cddfcaa16"
-down_revision = "a1b2c3d4e5f6"
+down_revision = "de4033877273"
 branch_labels = None
 depends_on = None
 
