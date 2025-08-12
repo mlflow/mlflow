f1f2a7c6c79f21151faec26eda76eea73f1ad204
Harutaka Kawamura <hkawamura0130@gmail.com>
2025-08-12 22:53:46 +0900

    Enable diagnostic settings for agents in semantic kernel autolog (#17182)
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/semantic_kernel/autolog.py
+++ mlflow/semantic_kernel/autolog.py
@@ -37,6 +37,17 @@ def _enable_experimental_genai_tracing():
     MODEL_DIAGNOSTICS_SETTINGS.enable_otel_diagnostics = True
     MODEL_DIAGNOSTICS_SETTINGS.enable_otel_diagnostics_sensitive = True
 
+    try:
+        # This only exists in Semantic Kernel 1.35.1 or later.
+        from semantic_kernel.utils.telemetry.agent_diagnostics.decorators import (
+            MODEL_DIAGNOSTICS_SETTINGS as AGENT_DIAGNOSTICS_SETTINGS,
+        )
+
+        AGENT_DIAGNOSTICS_SETTINGS.enable_otel_diagnostics = True
+        AGENT_DIAGNOSTICS_SETTINGS.enable_otel_diagnostics_sensitive = True
+    except ImportError:
+        pass
+
     _logger.info("Semantic Kernel Otel diagnostics is turned on for enabling tracing.")
 
 
