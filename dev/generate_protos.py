362221956f8b7cfa66227de25ced153166bffd8b
harupy <17039389+harupy@users.noreply.github.com>
2025-08-15 12:23:37 +0900

    wip
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- dev/generate_protos.py
+++ dev/generate_protos.py
@@ -44,6 +44,7 @@ def gen_stub_files(
     protoc_include_paths: list[Path],
     out_dir: Path,
 ) -> None:
+    out_dir.mkdir(parents=True, exist_ok=True)
     subprocess.check_call(
         [
             protoc_bin,
@@ -51,6 +52,27 @@ def gen_stub_files(
             *(f"-I={p}" for p in protoc_include_paths),
             f"-I={proto_dir}",
             f"--pyi_out={out_dir}",
+            *[proto_dir / pf for pf in proto_files],
+        ]
+    )
+
+
+def gen_proto_docs(
+    proto_dir: Path,
+    proto_files: list[Path],
+    protoc_bin: Path,
+    protoc_include_path: Path,
+    out_dir: Path,
+) -> None:
+    plugin_path = Path("dev/proto-plugin.sh").resolve()
+    subprocess.check_call(
+        [
+            protoc_bin,
+            f"-I={protoc_include_path}",
+            f"-I={proto_dir}",
+            f"--pyi_out={out_dir}",
+            f"--plugin=protos-gen-docs={plugin_path}",
+            f"--doc_out={out_dir}",
             *[proto_dir / pf for pf in proto_files],
         ]
     )
@@ -280,6 +302,14 @@ def main() -> None:
         protoc5260,
         protoc5260_includes,
         Path("mlflow/protos/"),
+    )
+
+    gen_proto_docs(
+        MLFLOW_PROTOS_DIR,
+        basic_proto_files,
+        protoc5260,
+        protoc5260_include,
+        Path("mlflow/protos"),
     )
 
 
