f30e989d0a14d09a0526ebfdcafe9a42de87090f
harupy <17039389+harupy@users.noreply.github.com>
2025-08-19 15:40:49 +0900

    wip
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

---
+++ mlflow/mcp/server.py
@@ -0,0 +1,88 @@
+import contextlib
+import io
+from typing import Any, Callable
+
+import click
+from click import Parameter, ParamType
+from click.types import BOOL, FLOAT, INT, STRING, UUID
+from fastmcp import FastMCP
+from fastmcp.tools import FunctionTool
+
+
+@click.group()
+def cli():
+    """
+    Test CLI
+    """
+
+
+@cli.command()
+@click.argument("a")
+@click.option("--b", default=1, type=int, help="An optional integer parameter")
+def test(a: str, b: int = 1):
+    """
+    Test command
+    """
+    click.echo(f"Test command called with a={a} and b={b}")
+
+
+def param_type_to_json_schema_type(pt: ParamType) -> str:
+    if pt is STRING:
+        return "string"
+    if pt is BOOL:
+        return "boolean"
+    if pt is INT:
+        return "integer"
+    if pt is FLOAT:
+        return "number"
+    if pt is UUID:
+        return "string"
+    return "string"
+
+
+def get_input_schema(params: list[Parameter]) -> dict[str, Any]:
+    """
+    Converts click params to JSON schema
+    """
+    return {
+        "type": "object",
+        "properties": {
+            p.name: {
+                "type": param_type_to_json_schema_type(p.type),
+                "default": p.default,
+                "required": p.required,
+                "description": p.help if hasattr(p, "help") else None,
+            }
+            for p in params
+        },
+    }
+
+
+mcp = FastMCP("Demo 🚀")
+
+
+def wrapper(command: click.Command) -> Callable[..., str]:
+    def wrapper(**kwargs: Any) -> str:
+        string_io = io.StringIO()
+        with contextlib.redirect_stdout(string_io), contextlib.redirect_stderr(string_io):
+            command.callback(**kwargs)
+        return string_io.getvalue()
+
+    return wrapper
+
+
+mcp.add_tool(
+    FunctionTool(
+        fn=wrapper(test),
+        name=test.name,
+        description=test.help,
+        parameters={
+            "name": test.name,
+            "description": test.help,
+            "inputSchema": get_input_schema(test.params),
+        },
+    )
+)
+
+if __name__ == "__main__":
+    mcp.run()
