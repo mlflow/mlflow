6c2c71b8f4088e77d8f6e4b14a3fda7ac2b5833b
harupy <17039389+harupy@users.noreply.github.com>
2025-08-22 11:29:10 +0900

    wip
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/mcp/cli.py
+++ mlflow/mcp/cli.py
@@ -3,14 +3,14 @@ import click
 from mlflow.mcp.server import create_mcp
 
 
-@click.group("mcp", help="Command Line Interface for the MCP")
+@click.group("mcp", help="Command Line Interface for the MLflow MCP server")
 def cli():
     """
-    Command Line Interface for the MCP
+    Command Line Interface for the MLflow MCP server.
     """
 
 
-@cli.command(help="Run the MCP server")
+@cli.command(help="Run the MLflow MCP server")
 def run():
     # TODO: Add telemetry here
     mcp = create_mcp()
