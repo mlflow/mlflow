efa838859cef4b117592478f13390262f4658350
serena-ruan <serena.rxy@gmail.com>
2025-08-27 11:09:45 +0800

    add fixture for local test
    Signed-off-by: serena-ruan <serena.rxy@gmail.com>

--- tests/telemetry/conftest.py
+++ tests/telemetry/conftest.py
@@ -84,6 +84,11 @@ def mock_telemetry_client(mock_requests_get, mock_requests):
     client._clean_up()
 
 
+@pytest.fixture(autouse=True)
+def is_mlflow_testing(monkeypatch):
+    monkeypatch.setattr(mlflow.telemetry.utils, "_IS_MLFLOW_TESTING", True)
+
+
 @pytest.fixture
 def bypass_env_check(monkeypatch):
     monkeypatch.setattr(mlflow.telemetry.utils, "_IS_MLFLOW_TESTING", False)
