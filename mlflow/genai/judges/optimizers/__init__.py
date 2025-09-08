0d97c70c69588a29f722059918a4518e4f806b9c
Corey Zumar <39497902+dbczumar@users.noreply.github.com>
2025-09-08 11:29:08 -0700

    Judges - import SIMBA optimizer in mlflow.genai.judges.optimizers (#17594)
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/genai/judges/optimizers/__init__.py
+++ mlflow/genai/judges/optimizers/__init__.py
@@ -1,7 +1,7 @@
 """MLflow GenAI Judge Optimizers."""
 
-from mlflow.genai.judges.optimizers.dspy import DSPyAlignmentOptimizer
+from mlflow.genai.judges.optimizers.simba import SIMBAAlignmentOptimizer
 
 __all__ = [
-    "DSPyAlignmentOptimizer",
+    "SIMBAAlignmentOptimizer",
 ]
