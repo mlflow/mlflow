3c9b0c79aee730da6e42e8c952c8495679a1bafb
Harutaka Kawamura <hkawamura0130@gmail.com>
2025-09-10 23:06:38 +0900

    Update tests/evaluate/test_default_evaluator_delta.py
    Signed-off-by: Harutaka Kawamura <hkawamura0130@gmail.com>

--- tests/evaluate/test_default_evaluator_delta.py
+++ tests/evaluate/test_default_evaluator_delta.py
@@ -34,7 +34,6 @@ def test_write_to_delta_fails_without_spark():
             )
 
 
-@pytest.fixture(scope="module")
 def spark_session_with_delta():
     # Kill any existing Spark session to avoid conflicts.
     if session := SparkSession.getActiveSession():
