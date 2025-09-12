3a912e61d3550580e2ee92eb5e4720f44f92dc5b
Serena Ruan <serena.rxy@gmail.com>
2025-09-12 19:26:54 +0800

    update
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- mlflow/telemetry/utils.py
+++ mlflow/telemetry/utils.py
@@ -31,15 +31,9 @@ def _is_ci_env_or_testing() -> bool:
         "BITBUCKET_BUILD_NUMBER",  # https://support.atlassian.com/bitbucket-cloud/docs/variables-and-secrets/
         "CODEBUILD_BUILD_ARN",  # https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-env-vars.html
         "BUILDKITE",  # https://buildkite.com/docs/pipelines/configure/environment-variables
-        "APPVEYOR",  # https://www.appveyor.com/docs/environment-variables/
-        "DRONE",  # https://docs.drone.io/pipeline/environment/reference/
-        "SEMAPHORE",  # https://docs.semaphoreci.com/ci-cd-environment/environment-variables/
         "TEAMCITY_VERSION",  # https://www.jetbrains.com/help/teamcity/predefined-build-parameters.html#Predefined+Server+Build+Parameters
         "BAMBOO_buildKey",  # https://confluence.atlassian.com/bamboo/bamboo-variables-289277087.html
-        "HUDSON_URL",  # Hudson (Jenkins predecessor)
-        "CODEFRESH",  # https://codefresh.io/docs/docs/pipelines/variables/
-        "SOURCEHUT",  # https://man.sr.ht/builds.sr.ht/#environment
-        "WOODPECKER_CI",  # https://woodpecker-ci.org/docs/usage/environment
+        "CLOUD_RUN_EXECUTION",  # https://cloud.google.com/run/docs/reference/container-contract#env-vars
         # runbots
         "RUNBOT_HOST_URL",
         "RUNBOT_BUILD_NAME",
