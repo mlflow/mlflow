6b9ccfcee279f2dcfab33b754e657791f48ccd7f
Ben Wilson <benjamin.wilson@databricks.com>
2025-09-15 12:17:00 -0400

    prevent duplicate redirects
    Signed-off-by: Ben Wilson <benjamin.wilson@databricks.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -33,6 +33,7 @@ const config: Config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
   onBrokenAnchors: 'throw',
+  onDuplicateRoutes: 'throw', // Fail build on duplicate redirects
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
