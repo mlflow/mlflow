6fd54c12d32bdf19b78ca0230033208539da5971
Serena Ruan <serena.rxy@gmail.com>
2025-09-24 13:01:14 +0800

    entities update
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- mlflow/utils/databricks_tracing_utils.py
+++ mlflow/utils/databricks_tracing_utils.py
@@ -1,3 +1,8 @@
+from google.protobuf.duration_pb2 import Duration
+from google.protobuf.timestamp_pb2 import Timestamp
+
+from mlflow.entities import Span, Trace, TraceData, TraceInfo, TraceState
+from mlflow.entities.trace_info_v2 import _truncate_request_metadata, _truncate_tags
 from mlflow.entities.trace_location import (
     InferenceTableLocation,
     MlflowExperimentLocation,
@@ -6,12 +11,15 @@ from mlflow.entities.trace_location import (
     UCSchemaLocation,
 )
 from mlflow.protos import databricks_tracing_pb2 as pb
+from mlflow.tracing.utils import parse_trace_id_v4
 
 
 def uc_schema_location_to_proto(uc_schema_location: UCSchemaLocation) -> pb.UCSchemaLocation:
     return pb.UCSchemaLocation(
         catalog_name=uc_schema_location.catalog_name,
         schema_name=uc_schema_location.schema_name,
+        otel_spans_table_name=uc_schema_location.otel_spans_table_name,
+        otel_logs_table_name=uc_schema_location.otel_logs_table_name,
     )
 
 
@@ -45,3 +53,88 @@ def trace_location_to_proto(trace_location: TraceLocation) -> pb.TraceLocation:
         )
     else:
         raise ValueError(f"Unsupported trace location type: {trace_location.type}")
+
+
+def trace_location_from_databricks_uc_schema(
+    catalog_name: str,
+    schema_name: str,
+    otel_spans_table_name: str | None = None,
+    otel_logs_table_name: str | None = None,
+) -> TraceLocation:
+    return TraceLocation(
+        type=TraceLocationType.UC_SCHEMA,
+        uc_schema=UCSchemaLocation(
+            catalog_name=catalog_name,
+            schema_name=schema_name,
+            otel_spans_table_name=otel_spans_table_name,
+            otel_logs_table_name=otel_logs_table_name,
+        ),
+    )
+
+
+def trace_location_type_from_proto(proto: pb.TraceLocation.TraceLocationType) -> TraceLocationType:
+    return TraceLocationType(pb.TraceLocation.TraceLocationType.Name(proto))
+
+
+def trace_location_from_proto(proto: pb.TraceLocation) -> TraceLocation:
+    type_ = trace_location_type_from_proto(proto.type)
+    if proto.WhichOneof("identifier") == "uc_schema":
+        return TraceLocation(type=type_, uc_schema=UCSchemaLocation.from_proto(proto.uc_schema))
+    elif proto.WhichOneof("identifier") == "mlflow_experiment":
+        return TraceLocation(
+            type=type_,
+            mlflow_experiment=MlflowExperimentLocation.from_proto(proto.mlflow_experiment),
+        )
+    elif proto.WhichOneof("identifier") == "inference_table":
+        return TraceLocation(
+            type=type_, inference_table=InferenceTableLocation.from_proto(proto.inference_table)
+        )
+    else:
+        return TraceLocation(type=type_)
+
+
+def trace_state_to_proto(trace_state: TraceState) -> pb.TraceInfo.State:
+    return pb.TraceInfo.State.Value(trace_state)
+
+
+def trace_info_to_proto(trace_info: TraceInfo) -> pb.TraceInfo:
+    request_time = Timestamp()
+    request_time.FromMilliseconds(trace_info.request_time)
+    execution_duration = None
+    if trace_info.execution_duration is not None:
+        execution_duration = Duration()
+        execution_duration.FromMilliseconds(trace_info.execution_duration)
+
+    if trace_info.trace_location.uc_schema:
+        _, trace_id = parse_trace_id_v4(trace_info.trace_id)
+    else:
+        trace_id = trace_info.trace_id
+
+    return pb.TraceInfo(
+        trace_id=trace_id,
+        client_request_id=trace_info.client_request_id,
+        trace_location=trace_location_to_proto(trace_info.trace_location),
+        request_preview=trace_info.request_preview,
+        response_preview=trace_info.response_preview,
+        request_time=request_time,
+        execution_duration=execution_duration,
+        state=trace_state_to_proto(trace_info.state),
+        trace_metadata=_truncate_request_metadata(trace_info.trace_metadata),
+        tags=_truncate_tags(trace_info.tags),
+        # TODO: update once assessment proto is updated
+        assessments=[a.to_proto() for a in trace_info.assessments],
+    )
+
+
+def trace_to_proto(trace: Trace) -> pb.Trace:
+    return pb.Trace(
+        trace_info=trace_info_to_proto(trace.info),
+        spans=[span.to_otel_proto() for span in trace.data.spans],
+    )
+
+
+def trace_from_proto(proto: pb.Trace) -> Trace:
+    return Trace(
+        info=TraceInfo.from_proto(proto.trace_info),
+        data=TraceData(spans=[Span.from_otel_proto(span) for span in proto.spans]),
+    )
