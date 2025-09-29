d2ab0d7ecb2e724594c0006186c99a7f77113274
Weichen Xu <weichen.xu@databricks.com>
2025-09-29 11:17:21 +0800

    migration script
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/store/db_migrations/versions/4209a452f04b_add_env_vars_column_to_job_table.py
+++ mlflow/store/db_migrations/versions/4209a452f04b_add_env_vars_column_to_job_table.py
@@ -9,14 +9,19 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '4209a452f04b'
-down_revision = 'bf29a5ff90ea'
+down_revision = 'cbc13b556ace'
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    pass
+    # Add env_vars column to jobs table
+    op.add_column(
+        "jobs",
+        sa.Column("env_vars", sa.Text(), nullable=True),
+    )
 
 
 def downgrade():
-    pass
+    # Remove env_vars column from jobs table
+    op.drop_column("jobs", "env_vars")
