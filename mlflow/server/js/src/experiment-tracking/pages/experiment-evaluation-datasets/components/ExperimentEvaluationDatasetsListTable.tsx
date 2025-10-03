84537c4f5a8a0866405761735787037c796829f5
Daniel Lok <daniel.lok@databricks.com>
2025-10-03 14:17:46 +0800

    wip empty

--- mlflow/server/js/src/experiment-tracking/pages/experiment-evaluation-datasets/components/ExperimentEvaluationDatasetsListTable.tsx
+++ mlflow/server/js/src/experiment-tracking/pages/experiment-evaluation-datasets/components/ExperimentEvaluationDatasetsListTable.tsx
@@ -134,10 +134,12 @@ export const ExperimentEvaluationDatasetsListTable = ({
   experimentId,
   selectedDataset,
   setSelectedDataset,
+  setIsLoading,
 }: {
   experimentId: string;
   selectedDataset?: EvaluationDataset;
   setSelectedDataset: (dataset: EvaluationDataset | undefined) => void;
+  setIsLoading: (isLoading: boolean) => void;
 }) => {
   const intl = useIntl();
   const { theme } = useDesignSystemTheme();
@@ -203,6 +205,11 @@ export const ExperimentEvaluationDatasetsListTable = ({
       setSelectedDataset(sortedRows[0].original);
     }
   }
+
+  // update loading state in parent
+  useEffect(() => {
+    setIsLoading(isLoading);
+  }, [isLoading, setIsLoading]);
 
   if (error) {
     return <div>Error loading datasets</div>;
