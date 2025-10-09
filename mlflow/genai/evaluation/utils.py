c7cc306e619cb62798803617297024cde861b1ed
Tomu Hirata <tomu.hirata@gmail.com>
2025-10-09 17:09:23 +0900

    comment
    Signed-off-by: Tomu Hirata <tomu.hirata@gmail.com>

--- mlflow/genai/evaluation/utils.py
+++ mlflow/genai/evaluation/utils.py
@@ -8,7 +8,6 @@ from mlflow.entities import Assessment, Trace, TraceData
 from mlflow.entities.assessment import DEFAULT_FEEDBACK_NAME, Feedback
 from mlflow.entities.assessment_source import AssessmentSource, AssessmentSourceType
 from mlflow.exceptions import MlflowException
-from mlflow.genai.datasets import EvaluationDataset
 from mlflow.genai.evaluation.constant import (
     AgentEvaluationReserverKey,
 )
@@ -55,6 +54,9 @@ def _convert_eval_set_to_df(data: "EvaluationDatasetTypes") -> "pd.DataFrame":
     Takes in a dataset in the format that `mlflow.genai.evaluate()` expects and
     converts it into a pandas DataFrame.
     """
+    from mlflow.entities.evaluation_dataset import EvaluationDataset as EntityEvaluationDataset
+    from mlflow.genai.datasets import EvaluationDataset as ManagedEvaluationDataset
+
     if isinstance(data, list):
         # validate that every item in the list is a dict and has inputs as key
         for item in data:
@@ -66,7 +68,7 @@ def _convert_eval_set_to_df(data: "EvaluationDatasetTypes") -> "pd.DataFrame":
     elif isinstance(data, pd.DataFrame):
         # Data is already a pd DataFrame, just copy it
         df = data.copy()
-    elif isinstance(data, EvaluationDataset):
+    elif isinstance(data, (EntityEvaluationDataset, ManagedEvaluationDataset)):
         df = data.to_df()
     else:
         try:
