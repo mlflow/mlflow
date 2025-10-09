9e22332119c75c46f959998aa3448efcf7a88712
Tomu Hirata <tomu.hirata@gmail.com>
2025-10-09 13:31:57 +0900

    lint
    Signed-off-by: Tomu Hirata <tomu.hirata@gmail.com>

--- tests/genai/evaluate/test_utils.py
+++ tests/genai/evaluate/test_utils.py
@@ -277,13 +277,21 @@ def test_convert_to_legacy_eval_raise_for_invalid_json_columns(spark):
     with pytest.raises(MlflowException, match="Failed to parse `expectations` column."):
         _convert_to_eval_set(df)
 
+
 def test_convert_to_eval_set_evaluation_dataset():
     dataset = create_dataset("test")
     dataset.merge_records(
         [
-            {"inputs": {"question": "What is Spark?"}, "outputs": "actual response for first question", "expectations": {"expected_response": "expected response for first question"}},
-            {"inputs": {"question": "How can you minimize data shuffling in Spark?"}, "outputs": "actual response for second question", "expectations": {"expected_response": "expected response for second question"}},
-            {"inputs": {"question": "What is MLflow?"}, "outputs": "actual response for third question", "expectations": {"expected_response": "expected response for third question"}},
+            {
+                "inputs": {"question": "What is Spark?"},
+                "outputs": "actual response for first question",
+                "expectations": {"expected_response": "expected response for first question"},
+            },
+            {
+                "inputs": {"question": "How can you minimize data shuffling in Spark?"},
+                "outputs": "actual response for second question",
+                "expectations": {"expected_response": "expected response for second question"},
+            },
         ]
     )
     transformed_data = _convert_to_eval_set(dataset)
