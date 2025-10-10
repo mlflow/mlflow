9e3f750e081dc1ea67c58b1b57d1b0302c534f5b
Ben Wilson <benjamin.wilson@databricks.com>
2025-10-10 14:25:27 -0400

    add return types
    Signed-off-by: Ben Wilson <benjamin.wilson@databricks.com>

--- mlflow/store/tracking/sqlalchemy_store.py
+++ mlflow/store/tracking/sqlalchemy_store.py
@@ -34,6 +34,7 @@ from mlflow.entities import (
     RunOutputs,
     RunStatus,
     RunTag,
+    ScorerVersion,
     SourceType,
     TraceInfo,
     ViewType,
@@ -1970,7 +1971,9 @@ class SqlAlchemyStore(AbstractStore):
                     RESOURCE_DOES_NOT_EXIST,
                 )
 
-    def register_scorer(self, experiment_id: str, name: str, serialized_scorer: str):
+    def register_scorer(
+        self, experiment_id: str, name: str, serialized_scorer: str
+    ) -> ScorerVersion:
         """
         Register a scorer for an experiment.
 
@@ -2030,7 +2033,7 @@ class SqlAlchemyStore(AbstractStore):
 
             return sql_scorer_version.to_mlflow_entity()
 
-    def list_scorers(self, experiment_id):
+    def list_scorers(self, experiment_id) -> list[ScorerVersion]:
         """
         List all scorers for an experiment.
 
@@ -2081,14 +2084,13 @@ class SqlAlchemyStore(AbstractStore):
                 .all()
             )
 
-            # Convert to mlflow.entities.scorer.ScorerVersion objects
             scorers = []
             for sql_scorer_version in sql_scorer_versions:
                 scorers.append(sql_scorer_version.to_mlflow_entity())
 
             return scorers
 
-    def get_scorer(self, experiment_id, name, version=None):
+    def get_scorer(self, experiment_id, name, version=None) -> ScorerVersion:
         """
         Get a specific scorer for an experiment.
 
@@ -2152,7 +2154,7 @@ class SqlAlchemyStore(AbstractStore):
 
             return sql_scorer_version.to_mlflow_entity()
 
-    def delete_scorer(self, experiment_id, name, version=None):
+    def delete_scorer(self, experiment_id, name, version=None) -> None:
         """
         Delete a scorer for an experiment.
 
@@ -2217,7 +2219,7 @@ class SqlAlchemyStore(AbstractStore):
             if version is None:
                 session.delete(scorer)
 
-    def list_scorer_versions(self, experiment_id, name):
+    def list_scorer_versions(self, experiment_id, name) -> list[ScorerVersion]:
         """
         List all versions of a specific scorer for an experiment.
 
