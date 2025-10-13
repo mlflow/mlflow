ec9a29ddcfa5f0c002b443913690456ad0025c3d
Weichen Xu <weichen.xu@databricks.com>
2025-10-13 22:46:29 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/gateway/providers/anthropic.py
+++ mlflow/gateway/providers/anthropic.py
@@ -11,6 +11,7 @@ from mlflow.gateway.exceptions import AIGatewayException
 from mlflow.gateway.providers.base import BaseProvider, ProviderAdapter
 from mlflow.gateway.providers.utils import rename_payload_keys, send_request, send_stream_request
 from mlflow.gateway.schemas import chat, completions
+from mlflow.types.chat import ToolCallDelta, Function
 
 
 class AnthropicAdapter(ProviderAdapter):
@@ -170,6 +171,28 @@ class AnthropicAdapter(ProviderAdapter):
         content = resp.get("delta") or resp.get("content_block") or {}
         if (stop_reason := content.get("stop_reason")) is not None:
             stop_reason = "length" if stop_reason == "max_tokens" else "stop"
+        if content.get("type") == "tool_use":
+            delta = chat.StreamDelta(
+                tool_calls=[ToolCallDelta(
+                    index=0,
+                    id=content.get("id"),
+                    type="function",
+                    function=Function(name=content.get("name")),
+                )]
+            )
+        elif content.get("type") == "input_json_delta":
+            delta = chat.StreamDelta(
+                tool_calls=[ToolCallDelta(
+                    index=0,
+                    function=Function(arguments=content.get("partial_json"))
+                )]
+            )
+        else:
+            delta = chat.StreamDelta(
+                role=None,
+                content=content.get("text"),
+            )
+
         return chat.StreamResponsePayload(
             id=resp["id"],
             created=int(time.time()),
@@ -178,10 +201,7 @@ class AnthropicAdapter(ProviderAdapter):
                 chat.StreamChoice(
                     index=resp["index"],
                     finish_reason=stop_reason,
-                    delta=chat.StreamDelta(
-                        role=None,
-                        content=content.get("text"),
-                    ),
+                    delta=delta,
                 )
             ],
         )
@@ -357,7 +377,9 @@ class AnthropicProvider(BaseProvider, AnthropicAdapter):
                         self.config,
                     )
             else:
-                yield AnthropicAdapter.model_to_chat_streaming(resp, self.config)
+                yield AnthropicAdapter.model_to_chat_streaming(
+                    resp, self.config
+                )
 
     async def chat(self, payload: chat.RequestPayload) -> chat.ResponsePayload:
         from fastapi.encoders import jsonable_encoder
