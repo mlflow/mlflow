16117a4d778f914c34b65d59cda8f943c1fcd64c
Weichen Xu <weichen.xu@databricks.com>
2025-10-22 15:54:50 +0800

    add test
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- tests/pytorch/test_pytorch_autolog.py
+++ tests/pytorch/test_pytorch_autolog.py
@@ -754,3 +754,43 @@ def test_automatic_checkpoint_per_epoch_save_best_only_max_monitor_callback():
         ]
         == 2
     )
+
+
+def test_autologging_disabled_for_forecasting_model_predict():
+    from lightning.pytorch import Trainer
+    from pytorch_forecasting import DeepAR, TimeSeriesDataSet
+    from pytorch_forecasting.data.examples import generate_ar_data
+
+    mlflow.pytorch.autolog()
+
+    n_series = 10
+    timesteps = 100
+    data = generate_ar_data(seasonality=10.0, timesteps=timesteps, n_series=n_series)
+    max_encoder_length = 30
+    max_prediction_length = 20
+
+    time_series_dataset = TimeSeriesDataSet(
+        data[lambda x: x.time_idx <= timesteps - max_prediction_length],
+        time_idx="time_idx",
+        target="value",
+        group_ids=["series"],
+        max_encoder_length=max_encoder_length,
+        max_prediction_length=max_prediction_length,
+        time_varying_unknown_reals=["value"],
+    )
+    deepar = DeepAR.from_dataset(
+        time_series_dataset,
+        learning_rate=1e-3,
+        hidden_size=16,
+        rnn_layers=2,
+    )
+
+    dataloader = time_series_dataset.to_dataloader(train=True, batch_size=32)
+    trainer = Trainer(max_epochs=2, gradient_clip_val=0.1, accelerator="auto")
+    trainer.fit(deepar, train_dataloaders=dataloader)
+
+    last_run_id = mlflow.last_active_run().info.run_id
+    deepar.predict(data)
+
+    # assert `deepar.predict` does not trigger autologging (i.e. no new run is created)
+    assert mlflow.last_active_run().info.run_id == last_run_id
