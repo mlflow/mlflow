902169771f7c3522dda2dcc317063bd26f38522e
WeichenXu <weichen.xu@databricks.com>
2025-10-22 23:47:47 +0800

    Disable autologging for pytorch forecasting model predict method (#18444)
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- tests/pytorch/test_forecasting_model.py
+++ tests/pytorch/test_forecasting_model.py
@@ -15,12 +15,9 @@ def model_path(tmp_path):
     return os.path.join(tmp_path, "model")
 
 
-def test_forecasting_model_pyfunc_loader(model_path):
-    n_series = 10
-    timesteps = 100
+def _gen_forecasting_model_and_data(n_series, timesteps, max_prediction_length):
     data = generate_ar_data(seasonality=10.0, timesteps=timesteps, n_series=n_series)
     max_encoder_length = 30
-    max_prediction_length = 20
 
     time_series_dataset = TimeSeriesDataSet(
         data[lambda x: x.time_idx <= timesteps - max_prediction_length],
@@ -40,6 +37,19 @@ def test_forecasting_model_pyfunc_loader(model_path):
     dataloader = time_series_dataset.to_dataloader(train=True, batch_size=32)
     trainer = Trainer(max_epochs=2, gradient_clip_val=0.1, accelerator="auto")
     trainer.fit(deepar, train_dataloaders=dataloader)
+
+    return deepar, data
+
+
+def test_forecasting_model_pyfunc_loader(model_path: str):
+    n_series = 10
+    max_prediction_length = 20
+    deepar, data = _gen_forecasting_model_and_data(
+        n_series=n_series,
+        timesteps=100,
+        max_prediction_length=max_prediction_length,
+    )
+
     torch.manual_seed(42)
     predicted = deepar.predict(data).numpy()
     assert predicted.shape == (n_series, max_prediction_length)
