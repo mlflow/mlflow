1d2e21cb93ca8a9659e9246c85207135ac037c78
B-Step62 <yuki.watanabe@databricks.com>
2025-10-23 06:29:10 +0900

    Bump version to 2.22.3
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- mlflow/server/js/src/common/constants.tsx
+++ mlflow/server/js/src/common/constants.tsx
@@ -9,7 +9,7 @@ export const ErrorCodes = {
   RESOURCE_CONFLICT: 'RESOURCE_CONFLICT',
 };
 
-export const Version = '2.22.2';
+export const Version = '2.22.3';
 
 const DOCS_VERSION = 'latest';
 
--- mlflow/version.py
+++ mlflow/version.py
@@ -1,7 +1,7 @@
 # Copyright 2018 Databricks, Inc.
 import re
 
-VERSION = "2.22.2"
+VERSION = "2.22.3"
 
 
 def is_release_version():
