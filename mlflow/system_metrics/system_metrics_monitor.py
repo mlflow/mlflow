bb6de62c37516f49a8b118c3a4bff997f3dd72df
Emilio Graff <1@emil.io>
2025-10-23 20:54:28 -0700

    Logging step uses tracking URI
    Signed-off-by: Emilio Graff <1@emil.io>

--- mlflow/system_metrics/system_metrics_monitor.py
+++ mlflow/system_metrics/system_metrics_monitor.py
@@ -80,8 +80,8 @@ class SystemMetricsMonitor:
         self._process = None
         self._metrics_prefix = "system/"
         self.node_id = MLFLOW_SYSTEM_METRICS_NODE_ID.get() or node_id
-        self._logging_step = self._get_next_logging_step(run_id) if resume_logging else 0
         self._tracking_uri = tracking_uri if tracking_uri else get_tracking_uri()
+        self._logging_step = self._get_next_logging_step(run_id) if resume_logging else 0
         self.mlflow_logger = BatchMetricsLogger(self._run_id, tracking_uri=self._tracking_uri)
 
     def _get_next_logging_step(self, run_id):
