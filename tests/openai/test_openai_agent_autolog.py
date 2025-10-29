845221284a571168c766d8ba2b823959b700ebfc
bbqiu <bryan.qiu@databricks.com>
2025-10-29 11:32:55 -0400

    .
    Signed-off-by: bbqiu <bryan.qiu@databricks.com>

--- tests/openai/test_openai_agent_autolog.py
+++ tests/openai/test_openai_agent_autolog.py
@@ -11,7 +11,6 @@ except ImportError:
     pytest.skip("OpenAI SDK is not installed. Skipping tests.", allow_module_level=True)
 
 from agents import Agent, Runner, function_tool, set_default_openai_client, trace
-from agents.tracing import set_trace_processors
 from openai.types.responses.function_tool import FunctionTool
 from openai.types.responses.response import Response
 from openai.types.responses.response_output_item import (
@@ -38,12 +37,6 @@ def set_dummy_client(expected_responses):
     async_client.responses.create = _mocked_create
 
     set_default_openai_client(async_client)
-
-
-@pytest.fixture(autouse=True)
-def disable_default_tracing():
-    # Disable default OpenAI tracer
-    set_trace_processors([])
 
 
 @pytest.mark.asyncio
@@ -389,3 +382,138 @@ async def test_disable_enable_autolog():
     await Runner.run(agent, messages)
 
     assert get_traces() == []
+
+
+@pytest.mark.asyncio
+async def test_autolog_agent_with_enabled_openai_agent_tracer():
+    mlflow.openai.autolog(disable_openai_agent_tracer=False)
+
+    # NB: We have to mock the OpenAI SDK responses to make agent works
+    DUMMY_RESPONSES = [
+        Response(
+            id="123",
+            created_at=12345678.0,
+            error=None,
+            model="gpt-4o-mini",
+            object="response",
+            instructions="Handoff to the appropriate agent based on the language of the request.",
+            output=[
+                ResponseFunctionToolCall(
+                    id="123",
+                    arguments="{}",
+                    call_id="123",
+                    name="transfer_to_spanish_agent",
+                    type="function_call",
+                    status="completed",
+                )
+            ],
+            tools=[
+                FunctionTool(
+                    name="transfer_to_spanish_agent",
+                    parameters={"type": "object", "properties": {}, "required": []},
+                    type="function",
+                    description="Handoff to the Spanish_Agent agent to handle the request.",
+                    strict=False,
+                ),
+            ],
+            tool_choice="auto",
+            temperature=1,
+            parallel_tool_calls=True,
+        ),
+        Response(
+            id="123",
+            created_at=12345678.0,
+            error=None,
+            model="gpt-4o-mini",
+            object="response",
+            instructions="You only speak Spanish",
+            output=[
+                ResponseOutputMessage(
+                    id="123",
+                    content=[
+                        ResponseOutputText(
+                            annotations=[],
+                            text="¡Hola! Estoy bien, gracias. ¿Y tú, cómo estás?",
+                            type="output_text",
+                        )
+                    ],
+                    role="assistant",
+                    status="completed",
+                    type="message",
+                )
+            ],
+            tools=[],
+            tool_choice="auto",
+            temperature=1,
+            parallel_tool_calls=True,
+        ),
+    ]
+
+    set_dummy_client(DUMMY_RESPONSES)
+
+    english_agent = Agent(name="English Agent", instructions="You only speak English")
+    spanish_agent = Agent(name="Spanish Agent", instructions="You only speak Spanish")
+    triage_agent = Agent(
+        name="Triage Agent",
+        instructions="Handoff to the appropriate agent based on the language of the request.",
+        handoffs=[spanish_agent, english_agent],
+    )
+
+    messages = [{"role": "user", "content": "Hola.  ¿Como estás?"}]
+    response = await Runner.run(starting_agent=triage_agent, input=messages)
+
+    assert response.final_output == "¡Hola! Estoy bien, gracias. ¿Y tú, cómo estás?"
+    traces = get_traces()
+    assert len(traces) == 1
+    trace = traces[0]
+    assert trace.info.status == "OK"
+    assert json.loads(trace.info.request_preview) == messages
+    assert json.loads(trace.info.response_preview) == response.final_output
+    spans = trace.data.spans
+    assert len(spans) == 6  # 1 root + 2 agent + 1 handoff + 2 response
+    assert spans[0].name == "AgentRunner.run"
+    assert spans[0].span_type == SpanType.AGENT
+    assert spans[0].inputs == messages
+    assert spans[0].outputs == response.final_output
+    assert spans[1].name == "Triage Agent"
+    assert spans[1].parent_id == spans[0].span_id
+    assert spans[2].name == "Response_1"
+    assert spans[2].parent_id == spans[1].span_id
+    assert spans[2].inputs == [{"role": "user", "content": "Hola.  ¿Como estás?"}]
+    assert spans[2].outputs == [
+        {
+            "id": "123",
+            "arguments": "{}",
+            "call_id": "123",
+            "name": "transfer_to_spanish_agent",
+            "type": "function_call",
+            "status": "completed",
+        }
+    ]
+    assert spans[2].attributes["temperature"] == 1
+    assert spans[3].name == "Handoff"
+    assert spans[3].span_type == SpanType.CHAIN
+    assert spans[3].parent_id == spans[1].span_id
+    assert spans[4].name == "Spanish Agent"
+    assert spans[4].parent_id == spans[0].span_id
+    assert spans[5].name == "Response_2"
+    assert spans[5].parent_id == spans[4].span_id
+
+    # Validate chat attributes
+    assert spans[2].attributes[SpanAttributeKey.CHAT_TOOLS] == [
+        {
+            "function": {
+                "description": "Handoff to the Spanish_Agent agent to handle the request.",
+                "name": "transfer_to_spanish_agent",
+                "parameters": {
+                    "additionalProperties": None,
+                    "properties": {},
+                    "required": [],
+                    "type": "object",
+                },
+                "strict": False,
+            },
+            "type": "function",
+        },
+    ]
+    assert SpanAttributeKey.CHAT_TOOLS not in spans[5].attributes
