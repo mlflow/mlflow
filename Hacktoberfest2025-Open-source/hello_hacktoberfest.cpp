1979c2ba2c2b1e4ea3f5fbf7524b8e0ebd0810f1
manikonda-anjali <anjalireddy02010@gmail.com>
2025-10-31 19:04:55 +0530

    Added hello_hacktoberfest.cpp program by Anjali

---
+++ Hacktoberfest2025-Open-source/hello_hacktoberfest.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Hacktoberfest 2025! 🎉" << endl;
+    cout << "This is Anjali contributing to open source!" << endl;
+    return 0;
+}
