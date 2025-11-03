606c1f21ff1f7f86cbc89e63f9aad19cb0f85704
Daniel Lok <daniel.lok@databricks.com>
2025-11-03 15:23:49 +0800

    make row clickable

--- mlflow/server/js/src/shared/web-shared/genai-traces-table/sessions-table/cell-renderers/SessionIdCellRenderer.tsx
+++ mlflow/server/js/src/shared/web-shared/genai-traces-table/sessions-table/cell-renderers/SessionIdCellRenderer.tsx
@@ -2,23 +2,18 @@ import type { Row } from '@tanstack/react-table';
 
 import { Tag, Typography } from '@databricks/design-system';
 
-import MlflowUtils from '../../utils/MlflowUtils';
-import { Link } from '../../utils/RoutingUtils';
 import type { SessionTableRow } from '../utils';
 
 export const SessionIdCellRenderer = ({ row }: { row: Row<SessionTableRow> }) => {
-  const experimentId = row.original.experimentId;
   const sessionId = row.original.sessionId;
 
   return (
-    <Link to={MlflowUtils.getExperimentChatSessionPageRoute(experimentId, sessionId)}>
-      <Tag
-        componentId="mlflow.chat-sessions.session-id-tag"
-        color="indigo"
-        css={{ maxWidth: '100%', overflow: 'hidden' }}
-      >
-        <Typography.Text ellipsis>{sessionId}</Typography.Text>
-      </Tag>
-    </Link>
+    <Tag
+      componentId="mlflow.chat-sessions.session-id-tag"
+      color="indigo"
+      css={{ maxWidth: '100%', overflow: 'hidden' }}
+    >
+      <Typography.Text ellipsis>{sessionId}</Typography.Text>
+    </Tag>
   );
 };
