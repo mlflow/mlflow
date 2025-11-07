3925a648dc19bdc77213d6e8068740297bdb5880
bbqiu <bryan.qiu@databricks.com>
2025-11-06 17:32:53 -0800

    .
    Signed-off-by: bbqiu <bryan.qiu@databricks.com>

--- docs/sidebarsGenAI.ts
+++ docs/sidebarsGenAI.ts
@@ -647,6 +647,11 @@ const sidebarsGenAI: SidebarsConfig = {
       items: [
         {
           type: 'doc',
+          id: 'serving/agent-server',
+          label: 'Agent Server',
+        },
+        {
+          type: 'doc',
           id: 'serving/responses-agent',
           label: 'Responses Agent',
         },
