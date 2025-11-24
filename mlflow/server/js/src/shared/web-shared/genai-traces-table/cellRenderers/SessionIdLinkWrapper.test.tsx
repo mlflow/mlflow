f0b0c0185c34ccb793d14e9f924a358a3c0fdc3c
dbczumar <corey.zumar@databricks.com>
2025-11-24 07:42:06 -0800

    fix
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/server/js/src/shared/web-shared/genai-traces-table/cellRenderers/SessionIdLinkWrapper.test.tsx
+++ mlflow/server/js/src/shared/web-shared/genai-traces-table/cellRenderers/SessionIdLinkWrapper.test.tsx
@@ -1,3 +1,4 @@
+import { describe, test, expect } from '@jest/globals';
 import { render, screen } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
 import { TestRouter, testRoute } from '../utils/RoutingTestUtils';
