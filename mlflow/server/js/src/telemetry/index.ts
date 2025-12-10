da54e84c2a3f5d8914e519c5dd05ea50ec91f7a9
Daniel Lok <daniel.lok@databricks.com>
2025-12-10 12:11:37 +0800

    remove

--- mlflow/server/js/src/telemetry/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Telemetry module
- *
- * Provides telemetry logging functionality using a SharedWorker
- * to coordinate logging across multiple browser tabs.
- */
-
-export { telemetryClient } from './TelemetryClient';
-export type { TelemetryRecord } from './worker/types';
