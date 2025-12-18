8943f66b2672c6ececcbf1ab1928dfea12b164b7
Daniel Lok <daniel.lok@databricks.com>
2025-12-18 16:40:42 +0800

    [UI Telemetry] Add rudimentary logging for navbar clicks (#19480)

--- mlflow/server/js/src/telemetry/types.ts
+++ mlflow/server/js/src/telemetry/types.ts
@@ -1,7 +1,12 @@
+import type {
+  DesignSystemEventProviderComponentTypes,
+  DesignSystemEventProviderAnalyticsEventTypes,
+} from '@databricks/design-system';
+
 export interface DesignSystemObservabilityEvent {
   componentId: string;
   componentViewId: string;
-  componentType: string;
+  componentType: DesignSystemEventProviderComponentTypes;
   componentSubType?: string | null;
-  eventType: string;
+  eventType: DesignSystemEventProviderAnalyticsEventTypes;
 }
