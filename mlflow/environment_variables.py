ca4c33b20d17684ef87d3455b2c30a5fb237cfe2
Weichen Xu <weichen.xu@databricks.com>
2025-12-19 10:57:01 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/environment_variables.py
+++ mlflow/environment_variables.py
@@ -1193,4 +1193,6 @@ _SERVERLESS_GPU_COMPUTE_ASSOCIATED_JOB_RUN_ID = _EnvironmentVariable(
 
 #: Whether to nable authorization for graphQL routes in MLflow server.
 #: (default: ``True``)
-MLFLOW_SERVER_ENABLE_GRAPHQL_AUTH = _BooleanEnvironmentVariable("MLFLOW_ENABLE_GRAPHQL_AUTH", True)
+MLFLOW_SERVER_ENABLE_GRAPHQL_AUTH = _BooleanEnvironmentVariable(
+    "MLFLOW_SERVER_ENABLE_GRAPHQL_AUTH", True
+)
