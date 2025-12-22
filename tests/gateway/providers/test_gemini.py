b0c8d96f5c130a48ae7fabecada19a0034ab5531
Tomu Hirata <tomu.hirata@gmail.com>
2025-12-23 00:21:10 +0900

    test
    Signed-off-by: Tomu Hirata <tomu.hirata@gmail.com>

--- tests/gateway/providers/test_gemini.py
+++ tests/gateway/providers/test_gemini.py
@@ -369,7 +369,6 @@ async def test_gemini_chat():
             "Invalid parameter maxOutputTokens. Use max_tokens instead.",
         ),
         ({"topK": 40}, [], "Invalid parameter topK. Use top_k instead."),
-        ({"top_p": 1.1}, [], "top_p should be less than or equal to 1"),
     ],
 )
 async def test_invalid_parameters_chat(override, exclude_keys, expected_msg):
