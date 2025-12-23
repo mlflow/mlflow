b654ec046e8ebb372c8938e27b79e6ca1fd3af03
Hubert Zub <hubert.zub@databricks.com>
2025-12-23 06:50:27 +0100

    Traces selector
    Signed-off-by: Hubert Zub <hubert.zub@databricks.com>

---
+++ mlflow/server/js/src/experiment-tracking/components/SelectTracesModal.tsx
@@ -0,0 +1,54 @@
+import { Modal } from '@databricks/design-system';
+import { useState } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { useParams } from '../../common/utils/RoutingUtils';
+import { TracesV3Logs } from './experiment-page/components/traces-v3/TracesV3Logs';
+import { GenAiTraceTableRowSelectionProvider } from '../../shared/web-shared/genai-traces-table/hooks/useGenAiTraceTableRowSelection';
+import { TracesTableColumn } from '../../shared/web-shared/genai-traces-table';
+
+export const SelectTracesModal = ({
+  onClose,
+  onSuccess,
+}: {
+  onClose?: () => void;
+  onSuccess?: (traceIds: string[]) => void;
+  customDefaultSelectedColumns?: (column: TracesTableColumn) => boolean;
+}) => {
+  const { experimentId } = useParams();
+
+  const [rowSelection, setRowSelection] = useState<Record<string, boolean>>({});
+
+  const handleOk = async () => {
+    const selectedTraceIds = Object.entries(rowSelection)
+      .filter(([_, isSelected]) => isSelected)
+      .map(([traceId]) => traceId);
+    onSuccess?.(selectedTraceIds);
+  };
+
+  if (!experimentId) {
+    return null;
+  }
+
+  return (
+    <Modal
+      visible
+      title="Select traces" // TODO
+      componentId="TODO"
+      onCancel={onClose}
+      css={{ width: '90% !important' }}
+      size="wide"
+      verticalSizing="maxed_out"
+      okText={<FormattedMessage defaultMessage="Select" description="Confirm button in the select traces modal" />}
+      okButtonProps={{
+        type: 'primary',
+        disabled: Object.values(rowSelection).every((isSelected) => !isSelected),
+      }}
+      onOk={handleOk}
+      cancelText={<FormattedMessage defaultMessage="Cancel" description="Cancel button in the select traces modal" />}
+    >
+      <GenAiTraceTableRowSelectionProvider rowSelection={rowSelection} setRowSelection={setRowSelection}>
+        <TracesV3Logs endpointName="" experimentId={experimentId} />
+      </GenAiTraceTableRowSelectionProvider>
+    </Modal>
+  );
+};
