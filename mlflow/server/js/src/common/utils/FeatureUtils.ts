3b791edc97fbfca677acb4cb75faf617cf517671
Hubert Zub <hubert.zub@databricks.com>
2025-12-29 17:10:26 +0100

    Session selector
    Signed-off-by: Hubert Zub <hubert.zub@databricks.com>

--- mlflow/server/js/src/common/utils/FeatureUtils.ts
+++ mlflow/server/js/src/common/utils/FeatureUtils.ts
@@ -46,14 +46,14 @@ export const shouldRerunExperimentUISeeding = () => false;
  * Feature flag to enable Scorers UI tab in experiment page
  */
 export const enableScorersUI = () => {
-  return false;
+  return true;
 };
 
 /**
  * Determines if running scorers feature is enabled (ability to run LLM scorers on sample traces)
  */
 export const isRunningScorersEnabled = () => {
-  return false;
+  return true;
 };
 
 /**
@@ -61,9 +61,9 @@ export const isRunningScorersEnabled = () => {
  */
 export const isEvaluatingSessionsInScorersEnabled = () => {
   if (!enableScorersUI() || !isRunningScorersEnabled()) {
-    return false;
+    return true;
   }
-  return false;
+  return true;
 };
 
 /**
