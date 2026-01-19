4d545dc9d4afeaccda3a36e0c9baf290f13f4632
SomtochiUmeh <somtochiumeh@gmail.com>
2026-01-19 00:00:48 -0800

    use `useQueries`
    Signed-off-by: SomtochiUmeh <somtochiumeh@gmail.com>

--- mlflow/server/js/src/experiment-tracking/pages/experiment-evaluation-datasets/hooks/useCheckMultiturnDatasets.tsx
+++ mlflow/server/js/src/experiment-tracking/pages/experiment-evaluation-datasets/hooks/useCheckMultiturnDatasets.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@databricks/web-shared/query-client';
+import { useQueries } from '@databricks/web-shared/query-client';
 import { fetchAPI, getAjaxUrl } from '@mlflow/mlflow/src/common/utils/FetchUtils';
 import { parseJSONSafe } from '@mlflow/mlflow/src/common/utils/TagUtils';
 import { CHECK_MULTITURN_DATASETS_QUERY_KEY } from '../constants';
@@ -7,36 +7,35 @@ import { CHECK_MULTITURN_DATASETS_QUERY_KEY } from '../constants';
  * Hook to check if any of the selected datasets is a multiturn dataset.
  */
 export const useCheckMultiturnDatasets = ({ datasetIds }: { datasetIds: string[] }) => {
-  return useQuery({
-    queryKey: [CHECK_MULTITURN_DATASETS_QUERY_KEY, datasetIds],
-    queryFn: async () => {
-      const results = await Promise.all(
-        datasetIds.map(async (datasetId) => {
-          const queryParams = new URLSearchParams();
-          queryParams.set('dataset_id', datasetId);
-          queryParams.set('max_results', '1');
+  const results = useQueries({
+    queries: datasetIds.map((datasetId) => ({
+      queryKey: [CHECK_MULTITURN_DATASETS_QUERY_KEY, datasetId],
+      queryFn: async () => {
+        const queryParams = new URLSearchParams();
+        queryParams.set('dataset_id', datasetId);
+        queryParams.set('max_results', '1');
 
-          const response = await fetchAPI(
-            getAjaxUrl(`ajax-api/3.0/mlflow/datasets/${datasetId}/records?${queryParams.toString()}`),
-            'GET',
-          ).catch(() => null);
-          if (!response) return false;
+        const response = await fetchAPI(
+          getAjaxUrl(`ajax-api/3.0/mlflow/datasets/${datasetId}/records?${queryParams.toString()}`),
+          'GET',
+        ).catch(() => null);
+        if (!response) return false;
 
-          const records = parseJSONSafe(response.records);
-          if (records && records.length > 0) {
-            const firstRecordInputs = records[0].inputs;
-            if (firstRecordInputs && firstRecordInputs.goal) {
-              return true;
-            }
+        const records = parseJSONSafe(response.records);
+        if (records && records.length > 0) {
+          const firstRecordInputs = records[0].inputs;
+          if (firstRecordInputs && firstRecordInputs.goal) {
+            return true;
           }
+        }
 
-          return false;
-        }),
-      );
-
-      return results.includes(true);
-    },
-    enabled: datasetIds.length > 0,
-    refetchOnWindowFocus: false,
+        return false;
+      },
+      refetchOnWindowFocus: false,
+    })),
   });
+  const isLoading = results.some((r) => r.isLoading);
+  const data = results.some((r) => r.data === true);
+
+  return { isLoading, data };
 };
