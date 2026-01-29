338d4d950e606d0e49716a59f36bddfc5785a153
Serena Ruan <serena.rxy@gmail.com>
2026-01-29 16:59:19 +0800

    simplify
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- mlflow/langchain/langchain_tracer.py
+++ mlflow/langchain/langchain_tracer.py
@@ -30,7 +30,6 @@ from mlflow.tracing.trace_manager import InMemoryTraceManager
 from mlflow.tracing.utils import (
     maybe_set_prediction_context,
     set_span_chat_tools,
-    set_span_cost_attribute,
 )
 from mlflow.tracing.utils.token import SpanWithToken
 from mlflow.types.chat import ChatTool, FunctionToolDefinition
@@ -436,7 +435,6 @@ class MlflowLangchainTracer(BaseCallbackHandler, metaclass=ExceptionSafeAbstract
         try:
             if usage := parse_token_usage(generations):
                 llm_span.set_attribute(SpanAttributeKey.CHAT_USAGE, usage)
-                set_span_cost_attribute(llm_span)
         except Exception as e:
             _logger.debug(f"Failed to log token usage for LangChain: {e}", exc_info=True)
 
