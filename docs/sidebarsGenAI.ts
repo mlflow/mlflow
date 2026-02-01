acb3ae5478ddfbb55e181cd2dbf92a15db2090ef
Somtochi Umeh <74836691+SomtochiUmeh@users.noreply.github.com>
2026-02-01 12:55:46 -0800

    code-based scorer examples (#20407)
    Signed-off-by: SomtochiUmeh <somtochiumeh@gmail.com>

--- docs/sidebarsGenAI.ts
+++ docs/sidebarsGenAI.ts
@@ -700,6 +700,11 @@ const sidebarsGenAI: SidebarsConfig = {
               items: [
                 {
                   type: 'doc',
+                  id: 'eval-monitor/scorers/custom/code-examples',
+                  label: 'Code-based Scorer Examples',
+                },
+                {
+                  type: 'doc',
                   id: 'eval-monitor/scorers/custom/tutorial',
                   label: 'Tutorial: Develop code-based scorers',
                 },
