da0116f76bfa3c3b0c73c6e5dcb8c45f77e2f1f1
TomuHirata <tomu.hirata@gmail.com>
2026-02-12 14:25:58 +0900

    Add back gateway usage tab (#20746)
    Signed-off-by: Tomu Hirata <tomu.hirata@gmail.com>

--- mlflow/server/js/src/common/components/MlflowSidebarGatewayItems.tsx
+++ mlflow/server/js/src/common/components/MlflowSidebarGatewayItems.tsx
@@ -1,4 +1,4 @@
-import { ChainIcon, CloudModelIcon, KeyIcon, useDesignSystemTheme } from '@databricks/design-system';
+import { ChainIcon, ChartLineIcon, CloudModelIcon, KeyIcon, useDesignSystemTheme } from '@databricks/design-system';
 import { FormattedMessage } from '@databricks/i18n';
 import GatewayRoutes from '../../gateway/routes';
 import { matchPath } from '../utils/RoutingUtils';
@@ -6,6 +6,7 @@ import type { Location } from '../utils/RoutingUtils';
 import { MlflowSidebarLink } from './MlflowSidebarLink';
 
 const isEndpointsActive = (location: Location) => Boolean(matchPath('/gateway', location.pathname));
+const isUsageActive = (location: Location) => Boolean(matchPath('/gateway/usage', location.pathname));
 const isApiKeysActive = (location: Location) => Boolean(matchPath('/gateway/api-keys', location.pathname));
 
 export const MlflowSidebarGatewayItems = ({ collapsed }: { collapsed: boolean }) => {
@@ -39,6 +40,16 @@ export const MlflowSidebarGatewayItems = ({ collapsed }: { collapsed: boolean })
         collapsed={collapsed}
       >
         <FormattedMessage defaultMessage="Endpoints" description="Sidebar link for gateway endpoints" />
+      </MlflowSidebarLink>
+      <MlflowSidebarLink
+        css={{ paddingLeft: collapsed ? undefined : theme.spacing.lg }}
+        to={GatewayRoutes.usagePageRoute}
+        componentId="mlflow.sidebar.gateway_usage_tab_link"
+        isActive={isUsageActive}
+        icon={<ChartLineIcon />}
+        collapsed={collapsed}
+      >
+        <FormattedMessage defaultMessage="Usage" description="Sidebar link for gateway usage" />
       </MlflowSidebarLink>
       <MlflowSidebarLink
         css={{ paddingLeft: collapsed ? undefined : theme.spacing.lg }}
