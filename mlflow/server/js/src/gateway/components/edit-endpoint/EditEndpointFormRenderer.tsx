b3d1221f5a8431dbe780fe3f9cd9f4674fb6254a
TomuHirata <tomu.hirata@gmail.com>
2026-02-13 14:18:25 +0900

    Gateway style fixes (#20767)
    Signed-off-by: Tomu Hirata <tomu.hirata@gmail.com>

--- mlflow/server/js/src/gateway/components/edit-endpoint/EditEndpointFormRenderer.tsx
+++ mlflow/server/js/src/gateway/components/edit-endpoint/EditEndpointFormRenderer.tsx
@@ -328,7 +328,6 @@ export const EditEndpointFormRenderer = ({
                     <Link
                       to={`/experiments/${experimentId}/traces`}
                       css={{
-                        fontSize: theme.typography.fontSizeSm,
                         color: theme.colors.actionPrimaryBackgroundDefault,
                         textDecoration: 'none',
                         '&:hover': {
