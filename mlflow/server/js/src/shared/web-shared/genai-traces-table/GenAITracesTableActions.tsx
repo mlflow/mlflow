d8bf8c7cfc5ffa64469545579ea0c8a659b29c0a
Joel Robin P <76001552+joelrobin18@users.noreply.github.com>
2026-02-17 07:06:24 +0530

    Fix Disable action button in Traces (#20883)
    Signed-off-by: joelrobin18 <joelrobin1818@gmail.com>

--- mlflow/server/js/src/shared/web-shared/genai-traces-table/GenAITracesTableActions.tsx
+++ mlflow/server/js/src/shared/web-shared/genai-traces-table/GenAITracesTableActions.tsx
@@ -169,7 +169,9 @@ const TraceActionsDropdown = (props: TraceActionsDropdownProps) => {
             })}
           >
             <div>
-              <DropdownMenu.Trigger asChild>{ActionButton}</DropdownMenu.Trigger>
+              <DropdownMenu.Trigger disabled asChild>
+                {ActionButton}
+              </DropdownMenu.Trigger>
             </div>
           </Tooltip>
         ) : (
