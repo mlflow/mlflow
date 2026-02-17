b2bb04adeb35a5c0e8312df79a57ec7dd175dbb7
Pat Sukprasert <pattara.sk127@gmail.com>
2026-02-17 15:02:41 +0700

    Use trace.text in place of span.attributes.`mlflow.spanInputs`
    Signed-off-by: Pat Sukprasert <pattara.sk127@gmail.com>

--- tests/store/tracking/test_sqlalchemy_store.py
+++ tests/store/tracking/test_sqlalchemy_store.py
@@ -7460,13 +7460,13 @@ def test_search_traces_with_span_attributute_backticks(store: SqlAlchemyStore):
     store.log_spans(exp_id, [span2])
 
     traces, _ = store.search_traces(
-        [exp_id], filter_string='span.attributes.`mlflow.spanInputs` ILIKE "%test1%"'
+        [exp_id], filter_string='trace.text ILIKE "%test1%"'
     )
     assert len(traces) == 1
     assert traces[0].request_id == trace_info_1.trace_id
 
     traces, _ = store.search_traces(
-        [exp_id], filter_string='span.attributes.`mlflow.spanInputs` ILIKE "%test2%"'
+        [exp_id], filter_string='trace.text ILIKE "%test2%"'
     )
     assert len(traces) == 1
     assert traces[0].request_id == trace_info_2.trace_id
