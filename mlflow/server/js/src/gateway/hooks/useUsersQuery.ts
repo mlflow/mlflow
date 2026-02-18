fd4b1f74dd4fa427acd2be5ac58c1f009f816156
Tomu Hirata <tomu.hirata@gmail.com>
2026-02-18 12:26:08 +0900

    Add user selector in the gateway usage page
    Signed-off-by: Tomu Hirata <tomu.hirata@gmail.com>

---
+++ mlflow/server/js/src/gateway/hooks/useUsersQuery.ts
@@ -0,0 +1,31 @@
+import { useQuery } from '@mlflow/mlflow/src/common/utils/reactQueryHooks';
+import { fetchEndpoint } from '../../common/utils/FetchUtils';
+
+interface UserInfo {
+  id: number;
+  username: string;
+}
+
+interface ListUsersResponse {
+  users: UserInfo[];
+}
+
+const queryFn = () => {
+  return fetchEndpoint({
+    relativeUrl: 'ajax-api/2.0/mlflow/users/list',
+  }) as Promise<ListUsersResponse>;
+};
+
+export const useUsersQuery = () => {
+  const queryResult = useQuery<ListUsersResponse, Error>(['auth_users'], {
+    queryFn,
+    retry: false,
+    refetchOnWindowFocus: false,
+  });
+
+  return {
+    data: queryResult.data?.users ?? [],
+    error: queryResult.error ?? undefined,
+    isLoading: queryResult.isLoading,
+  };
+};
