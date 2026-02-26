ef01b0cbe1f8cef9d59fd51a02d719c20b92286b
Tomu Hirata <tomu.hirata@gmail.com>
2026-02-26 17:29:46 +0900

    lint
    Signed-off-by: Tomu Hirata <tomu.hirata@gmail.com>

--- tests/store/tracking/test_file_store.py
+++ tests/store/tracking/test_file_store.py
@@ -328,9 +328,7 @@ def test_search_experiments_filter_by_tag_is_null(store):
     assert [e.name for e in results] == ["exp2"]
 
     # Combined with value filter
-    results = store.search_experiments(
-        filter_string="tag.key1 = 'value' AND tag.key2 IS NULL"
-    )
+    results = store.search_experiments(filter_string="tag.key1 = 'value' AND tag.key2 IS NULL")
     assert [e.name for e in results] == ["exp2"]
 
     # Error: IS NULL on attribute
