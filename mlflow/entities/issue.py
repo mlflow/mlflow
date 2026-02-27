c1d0e8ea1b2ec240b9e5b3525d9d9bb677b12358
Serena Ruan <serena.rxy@gmail.com>
2026-02-27 15:10:19 +0800

    add to/from proto methods
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- mlflow/entities/issue.py
+++ mlflow/entities/issue.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from typing import Any
 
 from mlflow.entities._mlflow_object import _MlflowObject
+from mlflow.protos.issues_pb2 import Issue as ProtoIssue
 
 
 @dataclass
@@ -96,4 +97,55 @@ class Issue(_MlflowObject):
             created_timestamp=issue_dict["created_timestamp"],
             last_updated_timestamp=issue_dict["last_updated_timestamp"],
             created_by=issue_dict.get("created_by"),
+        )
+
+    def to_proto(self) -> ProtoIssue:
+        """Convert Issue to protobuf representation."""
+
+        proto_issue = ProtoIssue()
+        proto_issue.issue_id = self.issue_id
+        proto_issue.experiment_id = self.experiment_id
+        proto_issue.name = self.name
+        proto_issue.description = self.description
+        proto_issue.frequency = self.frequency
+        proto_issue.status = self.status
+        proto_issue.created_timestamp = self.created_timestamp
+        proto_issue.last_updated_timestamp = self.last_updated_timestamp
+
+        if self.run_id:
+            proto_issue.run_id = self.run_id
+        if self.root_cause:
+            proto_issue.root_cause = self.root_cause
+        if self.confidence:
+            proto_issue.confidence = self.confidence
+        if self.rationale_examples:
+            proto_issue.rationale_examples.extend(self.rationale_examples)
+        if self.example_trace_ids:
+            proto_issue.example_trace_ids.extend(self.example_trace_ids)
+        if self.trace_ids:
+            proto_issue.trace_ids.extend(self.trace_ids)
+        if self.created_by:
+            proto_issue.created_by = self.created_by
+
+        return proto_issue
+
+    @classmethod
+    def from_proto(cls, proto: ProtoIssue) -> Issue:
+        """Create Issue from protobuf representation."""
+        return cls(
+            issue_id=proto.issue_id,
+            experiment_id=proto.experiment_id,
+            name=proto.name,
+            description=proto.description,
+            frequency=proto.frequency,
+            status=proto.status,
+            created_timestamp=proto.created_timestamp,
+            last_updated_timestamp=proto.last_updated_timestamp,
+            run_id=proto.run_id or None,
+            root_cause=proto.root_cause or None,
+            confidence=proto.confidence or None,
+            rationale_examples=list(proto.rationale_examples) or None,
+            example_trace_ids=list(proto.example_trace_ids) or None,
+            trace_ids=list(proto.trace_ids) or None,
+            created_by=proto.created_by or None,
         )
