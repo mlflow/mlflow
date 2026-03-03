c3861221143abaafa9f28e1ae8cefd2910020538
Serena Ruan <serena.rxy@gmail.com>
2026-03-03 13:19:05 +0800

    fix flush trace async logging
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- mlflow/tracking/fluent.py
+++ mlflow/tracking/fluent.py
@@ -886,7 +886,10 @@ def flush_trace_async_logging(terminate=False) -> None:
         terminate: If True, shut down the logging threads after flushing.
     """
     try:
-        _get_trace_exporter()._async_queue.flush(terminate=terminate)
+        trace_exporter = _get_trace_exporter()
+        if not hasattr(trace_exporter, "_async_queue"):
+            return
+        trace_exporter._async_queue.flush(terminate=terminate)
     except Exception as e:
         _logger.error(f"Failed to flush trace async logging: {e}")
 
