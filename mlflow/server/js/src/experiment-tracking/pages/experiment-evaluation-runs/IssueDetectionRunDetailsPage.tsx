f7eb9b0e45ce34cc19bd560aa7c2e462158a218a
Serena Ruan <serena.rxy@gmail.com>
2026-03-05 18:23:11 +0800

    display run details for issue detection runs
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

---
+++ mlflow/server/js/src/experiment-tracking/pages/experiment-evaluation-runs/IssueDetectionRunDetailsPage.tsx
@@ -0,0 +1,63 @@
+import { useMemo } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { Link, useNavigate, useParams } from '../../../common/utils/RoutingUtils';
+import { RunPage } from '../../components/run-page/RunPage';
+import { ExperimentPageTabName, RunPageTabName } from '../../constants';
+import Routes from '../../routes';
+import { useGetExperimentQuery } from '../../hooks/useExperimentQuery';
+
+/**
+ * Thin wrapper around RunPage for issue detection run details.
+ * Customizes breadcrumbs and tab navigation to stay within /evaluation-runs/ routes.
+ */
+export const IssueDetectionRunDetailsPage = () => {
+  const { experimentId } = useParams<{ experimentId: string }>();
+  const navigate = useNavigate();
+
+  const safeExperimentId = experimentId as string;
+
+  // Fetch experiment data for breadcrumb
+  const { data: experiment } = useGetExperimentQuery({ experimentId: safeExperimentId });
+
+  const customBreadcrumbs = useMemo(() => {
+    const experimentName = experiment?.name ?? safeExperimentId;
+    return [
+      <Link key="experiments" to={Routes.experimentsObservatoryRoute}>
+        <FormattedMessage
+          defaultMessage="Experiments"
+          description="Issue detection run details > Breadcrumb > Experiments"
+        />
+      </Link>,
+      <Link key="experiment" to={Routes.getExperimentPageRoute(safeExperimentId)}>
+        {experimentName}
+      </Link>,
+      <Link
+        key="evaluation-runs"
+        to={Routes.getExperimentPageTabRoute(safeExperimentId, ExperimentPageTabName.EvaluationRuns)}
+      >
+        <FormattedMessage
+          defaultMessage="Evaluation runs"
+          description="Issue detection run details > Breadcrumb > Evaluation runs"
+        />
+      </Link>,
+    ];
+  }, [experiment?.name, safeExperimentId]);
+
+  const handleDeleteSuccess = (expId: string) => {
+    navigate(Routes.getExperimentPageTabRoute(expId, ExperimentPageTabName.EvaluationRuns));
+  };
+
+  return (
+    <RunPage
+      customBreadcrumbs={customBreadcrumbs}
+      tabSwitchProps={{
+        getBaseRoute: Routes.getIssueDetectionRunDetailsRoute,
+        getTabRoute: Routes.getIssueDetectionRunDetailsTabRoute,
+        visibleTabs: [RunPageTabName.OVERVIEW, RunPageTabName.TRACES],
+      }}
+      onDeleteSuccess={handleDeleteSuccess}
+    />
+  );
+};
+
+export default IssueDetectionRunDetailsPage;
