d6ccbb9bf38925e0ea215e04970358a61f98c8b5
Serena Ruan <serena.rxy@gmail.com>
2026-03-16 11:28:07 +0800

    move API keys section out
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- mlflow/server/js/src/common/components/MlflowSidebarGatewayItems.tsx
+++ mlflow/server/js/src/common/components/MlflowSidebarGatewayItems.tsx
@@ -3,7 +3,6 @@ import {
   ChartLineIcon,
   CloudModelIcon,
   CreditCardIcon,
-  KeyIcon,
   useDesignSystemTheme,
 } from '@databricks/design-system';
 import { FormattedMessage } from '@databricks/i18n';
@@ -15,7 +14,6 @@ import { MlflowSidebarLink } from './MlflowSidebarLink';
 const isEndpointsActive = (location: Location) =>
   Boolean(matchPath('/gateway', location.pathname)) || Boolean(matchPath('/gateway/endpoints/*', location.pathname));
 const isUsageActive = (location: Location) => Boolean(matchPath('/gateway/usage', location.pathname));
-const isApiKeysActive = (location: Location) => Boolean(matchPath('/gateway/api-keys', location.pathname));
 const isBudgetsActive = (location: Location) => Boolean(matchPath('/gateway/budgets', location.pathname));
 
 export const MlflowSidebarGatewayItems = ({ collapsed }: { collapsed: boolean }) => {
@@ -59,16 +57,6 @@ export const MlflowSidebarGatewayItems = ({ collapsed }: { collapsed: boolean })
         collapsed={collapsed}
       >
         <FormattedMessage defaultMessage="Usage" description="Sidebar link for gateway usage" />
-      </MlflowSidebarLink>
-      <MlflowSidebarLink
-        css={{ paddingLeft: collapsed ? undefined : theme.spacing.lg }}
-        to={GatewayRoutes.apiKeysPageRoute}
-        componentId="mlflow.sidebar.gateway_api_keys_tab_link"
-        isActive={isApiKeysActive}
-        icon={<KeyIcon />}
-        collapsed={collapsed}
-      >
-        <FormattedMessage defaultMessage="API Keys" description="Sidebar link for gateway API keys" />
       </MlflowSidebarLink>
       <MlflowSidebarLink
         css={{ paddingLeft: collapsed ? undefined : theme.spacing.lg }}
