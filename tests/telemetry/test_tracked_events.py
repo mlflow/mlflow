35e2d70df857cb3e37adcdaf0476ad929617781c
Serena Ruan <serena.rxy@gmail.com>
2026-03-27 10:38:33 +0800

    fix test
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- tests/telemetry/test_tracked_events.py
+++ tests/telemetry/test_tracked_events.py
@@ -2276,7 +2276,7 @@ def test_tracing_context_propagation_get_and_set_success(
 
 
 def test_update_issue_telemetry(mock_requests, mock_telemetry_client: TelemetryClient, db_uri):
-    store = SqlAlchemyStore(db_uri)
+    store = SqlAlchemyStore(db_uri, "/tmp")
 
     exp_id = store.create_experiment("test-exp")
     issue = store.create_issue(
