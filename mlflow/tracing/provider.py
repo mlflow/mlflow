ae9c84ad21626ac33c3955132f315df1c998cc6c
Siddharth Murching <sid.murching@databricks.com>
2026-04-06 01:00:12 -0700

    Revert "Register `InferenceTableSpanProcessor` alongside `DatabricksUCTableSpanProcessor` in model serving (#22332)" (#22362)
    Signed-off-by: Sid Murching <sid.murching@databricks.com>

--- mlflow/tracing/provider.py
+++ mlflow/tracing/provider.py
@@ -693,22 +693,6 @@ def _get_span_processors(disabled: bool = False) -> list[SpanProcessor]:
             processors.append(processor)
             _logger.debug("Added DatabricksUCTableSpanProcessor based on trace destination")
 
-            # In Databricks model serving, also register InferenceTableSpanProcessor so that
-            # traces are written to the in-memory buffer and returned in the API response.
-            # Without this, endpoints return `trace: null` even though traces are logged to UC.
-            if (
-                is_in_databricks_model_serving_environment()
-                and is_mlflow_tracing_enabled_in_model_serving()
-            ):
-                from mlflow.tracing.export.inference_table import InferenceTableSpanExporter
-                from mlflow.tracing.processor.inference_table import InferenceTableSpanProcessor
-
-                processors.append(InferenceTableSpanProcessor(InferenceTableSpanExporter()))
-                _logger.debug(
-                    "Added InferenceTableSpanProcessor alongside DatabricksUCTableSpanProcessor "
-                    "for model serving trace buffer support"
-                )
-
         elif isinstance(trace_destination, MlflowExperimentLocation):
             if is_in_databricks_model_serving_environment():
                 _logger.info(
