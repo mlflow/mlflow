716276de97f9d6abb221daec8a52d0cabd7d917d
Weichen Xu <weichen.xu@databricks.com>
2026-04-07 10:26:21 +0000

    init
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- docs/src/constants.ts
+++ docs/src/constants.ts
@@ -1,1 +1,1 @@
-export const Version = '3.11.0';
+export const Version = '3.11.1';
--- mlflow/server/js/src/common/constants.tsx
+++ mlflow/server/js/src/common/constants.tsx
@@ -9,7 +9,7 @@ export const ErrorCodes = {
   RESOURCE_CONFLICT: 'RESOURCE_CONFLICT',
 };
 
-export const Version = '3.11.0';
+export const Version = '3.11.1';
 
 const DOCS_VERSION = 'latest';
 
