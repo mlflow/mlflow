e6d054307e2d97871d1cfd151096d1dd48081725
Rasaboun <40967731+Rasaboun@users.noreply.github.com>
2026-04-10 10:51:08 +0200

    refactor: remove backward compat re-exports in transformers/hub_utils.py
    Update imports to point directly to mlflow.utils.huggingface_utils
    and delete the re-export shim since these are not public APIs.
    
    Signed-off-by: Rasaboun <40967731+Rasaboun@users.noreply.github.com>

--- mlflow/transformers/flavor_config.py
+++ mlflow/transformers/flavor_config.py
@@ -8,9 +8,9 @@ from packaging.version import Version
 
 from mlflow.exceptions import MlflowException
 from mlflow.protos.databricks_pb2 import ALREADY_EXISTS, INVALID_PARAMETER_VALUE
-from mlflow.transformers.hub_utils import get_latest_commit_for_repo
 from mlflow.transformers.peft import _PEFT_ADAPTOR_DIR_NAME, get_peft_base_model, is_peft_model
 from mlflow.transformers.torch_utils import _extract_torch_dtype_if_set
+from mlflow.utils.huggingface_utils import get_latest_commit_for_repo
 
 if TYPE_CHECKING:
     import transformers
--- mlflow/transformers/hub_utils.py
+++
@@ -1,4 +0,0 @@
-# Re-export from the shared location for backward compatibility.
-from mlflow.utils.huggingface_utils import get_latest_commit_for_repo, is_valid_hf_repo_id
-
-__all__ = ["get_latest_commit_for_repo", "is_valid_hf_repo_id"]
