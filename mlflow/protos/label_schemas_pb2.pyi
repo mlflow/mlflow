c75ffa58d400e50537fce3c91d02ef00d5aed1b0
kriscon-db <84737625+kriscon-db@users.noreply.github.com>
2026-05-29 07:20:36 +0000

    Autoformat: https://github.com/mlflow/mlflow/actions/runs/26623620107
    Signed-off-by: kriscon-db <84737625+kriscon-db@users.noreply.github.com>

--- mlflow/protos/label_schemas_pb2.pyi
+++ mlflow/protos/label_schemas_pb2.pyi
@@ -13,18 +13,9 @@ class LabelSchemaType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     LABEL_SCHEMA_TYPE_UNSPECIFIED: _ClassVar[LabelSchemaType]
     FEEDBACK: _ClassVar[LabelSchemaType]
     EXPECTATION: _ClassVar[LabelSchemaType]
-
-class CategoricalSemanticPolarity(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
-    __slots__ = ()
-    CATEGORICAL_SEMANTIC_POLARITY_UNSPECIFIED: _ClassVar[CategoricalSemanticPolarity]
-    ASCENDING: _ClassVar[CategoricalSemanticPolarity]
-    DESCENDING: _ClassVar[CategoricalSemanticPolarity]
 LABEL_SCHEMA_TYPE_UNSPECIFIED: LabelSchemaType
 FEEDBACK: LabelSchemaType
 EXPECTATION: LabelSchemaType
-CATEGORICAL_SEMANTIC_POLARITY_UNSPECIFIED: CategoricalSemanticPolarity
-ASCENDING: CategoricalSemanticPolarity
-DESCENDING: CategoricalSemanticPolarity
 
 class InputPassFail(_message.Message):
     __slots__ = ("positive_label", "negative_label")
@@ -35,14 +26,12 @@ class InputPassFail(_message.Message):
     def __init__(self, positive_label: _Optional[str] = ..., negative_label: _Optional[str] = ...) -> None: ...
 
 class InputCategorical(_message.Message):
-    __slots__ = ("options", "semantic_polarity", "multi_select")
+    __slots__ = ("options", "multi_select")
     OPTIONS_FIELD_NUMBER: _ClassVar[int]
-    SEMANTIC_POLARITY_FIELD_NUMBER: _ClassVar[int]
     MULTI_SELECT_FIELD_NUMBER: _ClassVar[int]
     options: _containers.RepeatedScalarFieldContainer[str]
-    semantic_polarity: CategoricalSemanticPolarity
     multi_select: bool
-    def __init__(self, options: _Optional[_Iterable[str]] = ..., semantic_polarity: _Optional[_Union[CategoricalSemanticPolarity, str]] = ..., multi_select: bool = ...) -> None: ...
+    def __init__(self, options: _Optional[_Iterable[str]] = ..., multi_select: bool = ...) -> None: ...
 
 class InputNumeric(_message.Message):
     __slots__ = ("min_value", "max_value")
@@ -52,23 +41,30 @@ class InputNumeric(_message.Message):
     max_value: float
     def __init__(self, min_value: _Optional[float] = ..., max_value: _Optional[float] = ...) -> None: ...
 
+class InputText(_message.Message):
+    __slots__ = ("max_length",)
+    MAX_LENGTH_FIELD_NUMBER: _ClassVar[int]
+    max_length: int
+    def __init__(self, max_length: _Optional[int] = ...) -> None: ...
+
 class LabelSchemaInput(_message.Message):
-    __slots__ = ("pass_fail", "categorical", "numeric")
+    __slots__ = ("pass_fail", "categorical", "numeric", "text")
     PASS_FAIL_FIELD_NUMBER: _ClassVar[int]
     CATEGORICAL_FIELD_NUMBER: _ClassVar[int]
     NUMERIC_FIELD_NUMBER: _ClassVar[int]
+    TEXT_FIELD_NUMBER: _ClassVar[int]
     pass_fail: InputPassFail
     categorical: InputCategorical
     numeric: InputNumeric
-    def __init__(self, pass_fail: _Optional[_Union[InputPassFail, _Mapping]] = ..., categorical: _Optional[_Union[InputCategorical, _Mapping]] = ..., numeric: _Optional[_Union[InputNumeric, _Mapping]] = ...) -> None: ...
+    text: InputText
+    def __init__(self, pass_fail: _Optional[_Union[InputPassFail, _Mapping]] = ..., categorical: _Optional[_Union[InputCategorical, _Mapping]] = ..., numeric: _Optional[_Union[InputNumeric, _Mapping]] = ..., text: _Optional[_Union[InputText, _Mapping]] = ...) -> None: ...
 
 class LabelSchema(_message.Message):
-    __slots__ = ("schema_id", "experiment_id", "name", "type", "title", "instruction", "enable_comment", "input", "created_by", "created_at", "last_updated_at")
+    __slots__ = ("schema_id", "experiment_id", "name", "type", "instruction", "enable_comment", "input", "created_by", "created_at", "last_updated_at")
     SCHEMA_ID_FIELD_NUMBER: _ClassVar[int]
     EXPERIMENT_ID_FIELD_NUMBER: _ClassVar[int]
     NAME_FIELD_NUMBER: _ClassVar[int]
     TYPE_FIELD_NUMBER: _ClassVar[int]
-    TITLE_FIELD_NUMBER: _ClassVar[int]
     INSTRUCTION_FIELD_NUMBER: _ClassVar[int]
     ENABLE_COMMENT_FIELD_NUMBER: _ClassVar[int]
     INPUT_FIELD_NUMBER: _ClassVar[int]
@@ -79,17 +75,16 @@ class LabelSchema(_message.Message):
     experiment_id: str
     name: str
     type: LabelSchemaType
-    title: str
     instruction: str
     enable_comment: bool
     input: LabelSchemaInput
     created_by: str
     created_at: int
     last_updated_at: int
-    def __init__(self, schema_id: _Optional[str] = ..., experiment_id: _Optional[str] = ..., name: _Optional[str] = ..., type: _Optional[_Union[LabelSchemaType, str]] = ..., title: _Optional[str] = ..., instruction: _Optional[str] = ..., enable_comment: bool = ..., input: _Optional[_Union[LabelSchemaInput, _Mapping]] = ..., created_by: _Optional[str] = ..., created_at: _Optional[int] = ..., last_updated_at: _Optional[int] = ...) -> None: ...
+    def __init__(self, schema_id: _Optional[str] = ..., experiment_id: _Optional[str] = ..., name: _Optional[str] = ..., type: _Optional[_Union[LabelSchemaType, str]] = ..., instruction: _Optional[str] = ..., enable_comment: bool = ..., input: _Optional[_Union[LabelSchemaInput, _Mapping]] = ..., created_by: _Optional[str] = ..., created_at: _Optional[int] = ..., last_updated_at: _Optional[int] = ...) -> None: ...
 
 class CreateLabelSchema(_message.Message):
-    __slots__ = ("experiment_id", "name", "type", "title", "input", "instruction", "enable_comment")
+    __slots__ = ("experiment_id", "name", "type", "input", "instruction", "enable_comment")
     class Response(_message.Message):
         __slots__ = ("label_schema",)
         LABEL_SCHEMA_FIELD_NUMBER: _ClassVar[int]
@@ -98,18 +93,16 @@ class CreateLabelSchema(_message.Message):
     EXPERIMENT_ID_FIELD_NUMBER: _ClassVar[int]
     NAME_FIELD_NUMBER: _ClassVar[int]
     TYPE_FIELD_NUMBER: _ClassVar[int]
-    TITLE_FIELD_NUMBER: _ClassVar[int]
     INPUT_FIELD_NUMBER: _ClassVar[int]
     INSTRUCTION_FIELD_NUMBER: _ClassVar[int]
     ENABLE_COMMENT_FIELD_NUMBER: _ClassVar[int]
     experiment_id: str
     name: str
     type: LabelSchemaType
-    title: str
     input: LabelSchemaInput
     instruction: str
     enable_comment: bool
-    def __init__(self, experiment_id: _Optional[str] = ..., name: _Optional[str] = ..., type: _Optional[_Union[LabelSchemaType, str]] = ..., title: _Optional[str] = ..., input: _Optional[_Union[LabelSchemaInput, _Mapping]] = ..., instruction: _Optional[str] = ..., enable_comment: bool = ...) -> None: ...
+    def __init__(self, experiment_id: _Optional[str] = ..., name: _Optional[str] = ..., type: _Optional[_Union[LabelSchemaType, str]] = ..., input: _Optional[_Union[LabelSchemaInput, _Mapping]] = ..., instruction: _Optional[str] = ..., enable_comment: bool = ...) -> None: ...
 
 class GetLabelSchema(_message.Message):
     __slots__ = ("schema_id",)
@@ -153,7 +146,7 @@ class ListLabelSchemas(_message.Message):
     def __init__(self, experiment_id: _Optional[str] = ..., max_results: _Optional[int] = ..., page_token: _Optional[str] = ...) -> None: ...
 
 class UpdateLabelSchema(_message.Message):
-    __slots__ = ("schema_id", "name", "title", "instruction", "enable_comment", "input")
+    __slots__ = ("schema_id", "name", "instruction", "enable_comment", "input")
     class Response(_message.Message):
         __slots__ = ("label_schema",)
         LABEL_SCHEMA_FIELD_NUMBER: _ClassVar[int]
@@ -161,20 +154,18 @@ class UpdateLabelSchema(_message.Message):
         def __init__(self, label_schema: _Optional[_Union[LabelSchema, _Mapping]] = ...) -> None: ...
     SCHEMA_ID_FIELD_NUMBER: _ClassVar[int]
     NAME_FIELD_NUMBER: _ClassVar[int]
-    TITLE_FIELD_NUMBER: _ClassVar[int]
     INSTRUCTION_FIELD_NUMBER: _ClassVar[int]
     ENABLE_COMMENT_FIELD_NUMBER: _ClassVar[int]
     INPUT_FIELD_NUMBER: _ClassVar[int]
     schema_id: str
     name: str
-    title: str
     instruction: str
     enable_comment: bool
     input: LabelSchemaInput
-    def __init__(self, schema_id: _Optional[str] = ..., name: _Optional[str] = ..., title: _Optional[str] = ..., instruction: _Optional[str] = ..., enable_comment: bool = ..., input: _Optional[_Union[LabelSchemaInput, _Mapping]] = ...) -> None: ...
+    def __init__(self, schema_id: _Optional[str] = ..., name: _Optional[str] = ..., instruction: _Optional[str] = ..., enable_comment: bool = ..., input: _Optional[_Union[LabelSchemaInput, _Mapping]] = ...) -> None: ...
 
 class UpsertLabelSchema(_message.Message):
-    __slots__ = ("experiment_id", "name", "type", "title", "input", "instruction", "enable_comment")
+    __slots__ = ("experiment_id", "name", "type", "input", "instruction", "enable_comment")
     class Response(_message.Message):
         __slots__ = ("label_schema",)
         LABEL_SCHEMA_FIELD_NUMBER: _ClassVar[int]
@@ -183,18 +174,16 @@ class UpsertLabelSchema(_message.Message):
     EXPERIMENT_ID_FIELD_NUMBER: _ClassVar[int]
     NAME_FIELD_NUMBER: _ClassVar[int]
     TYPE_FIELD_NUMBER: _ClassVar[int]
-    TITLE_FIELD_NUMBER: _ClassVar[int]
     INPUT_FIELD_NUMBER: _ClassVar[int]
     INSTRUCTION_FIELD_NUMBER: _ClassVar[int]
     ENABLE_COMMENT_FIELD_NUMBER: _ClassVar[int]
     experiment_id: str
     name: str
     type: LabelSchemaType
-    title: str
     input: LabelSchemaInput
     instruction: str
     enable_comment: bool
-    def __init__(self, experiment_id: _Optional[str] = ..., name: _Optional[str] = ..., type: _Optional[_Union[LabelSchemaType, str]] = ..., title: _Optional[str] = ..., input: _Optional[_Union[LabelSchemaInput, _Mapping]] = ..., instruction: _Optional[str] = ..., enable_comment: bool = ...) -> None: ...
+    def __init__(self, experiment_id: _Optional[str] = ..., name: _Optional[str] = ..., type: _Optional[_Union[LabelSchemaType, str]] = ..., input: _Optional[_Union[LabelSchemaInput, _Mapping]] = ..., instruction: _Optional[str] = ..., enable_comment: bool = ...) -> None: ...
 
 class DeleteLabelSchema(_message.Message):
     __slots__ = ("schema_id",)
