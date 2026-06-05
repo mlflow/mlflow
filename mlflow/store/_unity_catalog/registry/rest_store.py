e48309fc18e46b39b6265b1dec8a33c502f69920
ZZIPP <3490870833@qq.com>
2026-06-06 01:38:52 +0800

    Handle empty model_id in UC model version copy
    Signed-off-by: ZZIPP <3490870833@qq.com>
    (cherry picked from commit ff2c861ce57b61ef64665d1e43dec7ffe9e5f71f)

--- mlflow/store/_unity_catalog/registry/rest_store.py
+++ mlflow/store/_unity_catalog/registry/rest_store.py
@@ -892,7 +892,7 @@ class UcModelRegistryStore(BaseRestStore):
                     shutil.rmtree(local_model_dir)
 
     def _get_logged_model_from_model_id(self, model_id) -> LoggedModel | None:
-        if model_id is None:
+        if model_id in (None, ""):
             return None
         try:
             return mlflow.get_logged_model(model_id)
