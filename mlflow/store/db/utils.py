1419ee0e742697e447f0f52542420385ad0799d7
botbikamordehai2-sketch <botbikamordehai2@gmail.com>
2026-06-10 12:30:31 +0000

    fix: handle table already exists in db initialization (closes #19943)

--- mlflow/store/db/utils.py
+++ mlflow/store/db/utils.py
@@ -85,7 +85,9 @@ def _is_empty_database(engine):
 
 def _initialize_tables(engine):
     _logger.info("Creating initial MLflow database tables...")
-    InitialBase.metadata.create_all(engine)
+    # Use checkfirst=True to avoid 'table already exists' errors
+    # when called on a database that has partial tables.
+    InitialBase.metadata.create_all(engine, checkfirst=True)
     _upgrade_db(engine)
 
 
