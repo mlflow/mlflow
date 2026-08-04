5819c6517947856ace29cf94ca5a08876af5969f
Daniel Lok <daniel.lok@databricks.com>
2026-08-04 20:37:46 +0800

    Apply suggestion from @daniellok-db
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/server/js/src/experiment-tracking/components/runs-charts/components/RunsChartsDraggableCardsGrid.test.tsx
+++ mlflow/server/js/src/experiment-tracking/components/runs-charts/components/RunsChartsDraggableCardsGrid.test.tsx
@@ -58,7 +58,6 @@ jest.mock('@tanstack/react-virtual', () => {
         getTotalSize: () => opts.count * 360,
         measureElement: () => {},
         options: { scrollMargin: opts.scrollMargin ?? 0 },
-        options: { scrollMargin: opts.scrollMargin ?? 0 },
       };
     },
   };
