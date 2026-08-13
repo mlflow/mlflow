6bc06f1221a67cd3db6a21f19a5e21c2e4fcf1d7
Dmitry <45711841+darkdi@users.noreply.github.com>
2026-08-13 13:43:06 +0300

    Fix docstring `Args` entries that name a parameter the function does not take (#24925)
    Signed-off-by: darkdi <rantovov5@gmail.com>
    Co-authored-by: darkdi <rantovov5@gmail.com>

--- mlflow/store/tracking/sqlalchemy_store.py
+++ mlflow/store/tracking/sqlalchemy_store.py
@@ -2105,7 +2105,6 @@ class SqlAlchemyStore(SqlAlchemyMCPServerRegistryMixin, SqlAlchemyGatewayStoreMi
         Args:
             run_id: String ID of the run
             tags: List of RunTag instances to log
-            path: current json path for error messages
         """
         if not tags:
             return
