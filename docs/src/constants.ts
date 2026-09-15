218bebb87d690300ff1cc34bfa1af0b2a6cd9a29
Yuki Watanabe <31463517+B-Step62@users.noreply.github.com>
2026-09-14 19:20:00 -0700

    Bump version to 3.16.1 (#25854)
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- docs/src/constants.ts
+++ docs/src/constants.ts
@@ -1,1 +1,1 @@
-export const Version = '3.16.0';
+export const Version = '3.16.1';
--- mlflow/server/js/src/common/constants.tsx
+++ mlflow/server/js/src/common/constants.tsx
@@ -9,7 +9,7 @@ export const ErrorCodes = {
   RESOURCE_CONFLICT: 'RESOURCE_CONFLICT',
 };
 
-export const Version = '3.16.0';
+export const Version = '3.16.1';
 
 const DOCS_VERSION = 'latest';
 
