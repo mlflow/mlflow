a0fd3915396b797951fc0e0f633d9082f7b4a37f
B-Step62 <yuki.watanabe@databricks.com>
2026-09-17 22:02:32 -0700

    Restore hosted setup wizard URL
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- mlflow/server/js/src/experiment-tracking/components/onboarding/AgentActionCard.tsx
+++ mlflow/server/js/src/experiment-tracking/components/onboarding/AgentActionCard.tsx
@@ -6,8 +6,7 @@ import { AssistantSparkleIcon, useAssistant } from '../../../assistant';
 import { CopyButton } from '@mlflow/mlflow/src/shared/building_blocks/CopyButton';
 import { CodeSnippet, type CodeSnippetLanguage } from '@mlflow/mlflow/src/shared/web-shared/snippet';
 
-const AGENT_SETUP_COMMAND =
-  'curl -LsSf https://raw.githubusercontent.com/mlflow/mlflow/master/mlflow/agent/setup/setup.sh | sh';
+const AGENT_SETUP_COMMAND = 'curl -LsSf https://mlflow.org/wizard/setup.sh | sh';
 
 type TabKey = 'agent-setup' | 'copy-prompt' | 'code-snippet' | 'assistant';
 
